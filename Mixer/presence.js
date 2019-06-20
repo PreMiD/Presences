7909414c5b67134a7c567041877e6a2d97e0cb9b
kaikecarlos <caheasta@gmail.com>
2019-06-20 14:10:44 -0300

    Mixer.js

---
+++ Mixer/presence.js
@@ -0,0 +1,127 @@
+//! DO NOT REMOVE | USED FOR CHECKING IF ALREADY INJECTED
+var PreMiD_Presence = true;
+
+//* PreMiD events
+window.addEventListener('PreMiD_MediaKeys', handleMediaKeys);
+window.addEventListener('PreMiD_UpdateData', updateData);
+
+//* Request data from PreMiD
+setTimeout(function() {
+	var event = new CustomEvent('PreMiD_RequestExtensionData', {
+		detail: {
+			strings: {
+				playing: 'presence.playback.playing',
+				paused: 'presence.playback.paused',
+				live: 'presence.activity.live'
+			},
+			version: "chrome.runtime.getManifest().name + ' V' + chrome.runtime.getManifest().version"
+		}
+	});
+
+	//* Trigger the event
+	window.dispatchEvent(event);
+});
+
+//* Receive data from PreMiD
+window.addEventListener('PreMiD_ReceiveExtensionData', function(data) {
+	extensionData = data.detail;
+});
+
+//* Presence specific variables
+var duration,
+	currentTime,
+	paused,
+	title,
+	author,
+	videoStream = null,
+	live = false,
+	data,
+	videoTimestamps,
+	watchingSinceTimestamp,
+	oldUrl;
+
+/**
+ * Media keys received from application
+ * @param {Object} mediaKey 
+ */
+function handleMediaKeys(mediaKey) {
+	if (!videoStream) return;
+  switch (mediaKey.detail) {
+     case "pause":
+        paused ? $(".light-player").pause() : $(".light-player").play();
+        break;
+  }
+}
+
+/**
+ * Update Data when priorityTab focused
+ */
+function updateData() {
+	if (oldUrl != window.location.href) {
+		oldUrl = window.location.href;
+		watchingSinceTimestamp = Math.floor(Date.now() / 1000);
+	}
+
+	if (typeof document.querySelector('.light-player') != 'undefined') {
+		videoStream = document.querySelector('.light-player');
+		if (isNaN(videoStream.duration)) return;
+
+		duration = Math.floor(videoStream.duration);
+		currentTime = Math.floor(videoStream.currentTime);
+		paused = videoStream.paused;
+		title = document.querySelectorAll('.stream-title span')[0].innerText;
+		author = document.querySelector('.profile-header h2').innerText;
+    live = Boolean(document.querySelector('.offline-message h3'));
+    
+
+		data = {
+			clientID: '534832037132107779',
+			presenceData: {
+				details: title,
+				state: author,
+				largeImageKey: 'mixer_lg',
+				largeImageText: extensionData.version,
+				smallImageKey: paused ? 'pause' : 'play',
+				smallImageText: paused ? extensionData.strings.paused : extensionData.strings.playing
+			},
+			trayTitle: title,
+			playback: !paused,
+			service: 'Mixer'
+		};
+
+		if (!paused) {
+			videoTimestamps = getTimestamps(currentTime, duration);
+			data.presenceData.startTimestamp = live ? watchingSinceTimestamp : videoTimestamps[0];
+
+			if (live) {
+				data.presenceData.smallImageKey = 'live';
+				data.presenceData.smallImageText = extensionData.strings.live;
+			} else data.presenceData.endTimestamp = videoTimestamps[1];
+		} else {
+			delete data.presenceData.startTimestamp;
+			delete data.presenceData.endTimestamp;
+		}
+	} else {
+		videoStream = null;
+		data = {
+			clientID: '534832037132107779',
+			service: 'Mixer',
+			hidden: true
+		};
+	}
+
+	//* Send data back to PreMiD
+	var event = new CustomEvent('PreMiD_UpdatePresence', { detail: data });
+	window.dispatchEvent(event);
+}
+
+/**
+ * Get Timestamps
+ * @param {Number} videoTime Current video time seconds
+ * @param {Number} videoDuration Video duration seconds
+ */
+function getTimestamps(videoTime, videoDuration) {
+	var startTime = Date.now();
+	var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+	return [ Math.floor(startTime / 1000), endTime ];
+}
