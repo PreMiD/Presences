1a2dcccaac6cea239300cc3ef50edb168fed1b14
Florian Metz <timeraa@Florians-Mac-mini.local>
2019-08-06 04:29:23 +0200

    🎨 Small definition changes

--- @types/premid/index.d.ts
+++ @types/premid/index.d.ts
@@ -1,11 +1,14 @@
 declare class iFrame {
     /**
      * Send data from iFrames back to the presence script
-     * @param {*} data Data to send
+     * @param data Data to send
      */
     send(data: any): void;
 }
 declare var iframe: iFrame;
+/**
+ * @link https://docs.premid.app/presence-development/coding/presence-class#getpagevariable-string
+ */
 interface presenceData {
     state?: string;
     details?: string;
@@ -17,11 +20,14 @@ interface presenceData {
 }
 interface PresenceOptions {
     /**
-     * Client Id of presence
+     * ClientId of Discord application
+     * @link https://docs.premid.app/presence-development/coding/presence-class#clientid
      */
     clientId: string;
     /**
      * Wether or not this presence supports media keys
+     * @default {mediaKeys: false}
+     * @link https://docs.premid.app/presence-development/coding/presence-class#mediakeys
      */
     mediaKeys?: boolean;
 }
@@ -40,26 +46,31 @@ declare class Presence {
      *
      * @param presenceData presenceData
      * @param playback Is presence playing
+     * @link https://docs.premid.app/presence-development/coding/presence-class#setactivity-presencedata-boolean
      */
     setActivity(presenceData?: presenceData, playback?: boolean): void;
     /**
      * Clears the activity shown in discord as well as the Tray and keybinds
+     * @link https://docs.premid.app/presence-development/coding/presence-class#clearactivity
      */
     clearActivity(): void;
     /**
      * Sets the tray title on the Menubar in Mac OS (Mac OS only)
      * @param trayTitle Tray Title
+     * @link https://docs.premid.app/presence-development/coding/presence-class#settraytitle-string
      */
     setTrayTitle(trayTitle?: string): void;
     /**
      * Get translations from the extension
      * @param strings String object with keys being the key for string, keyValue is the string value
+     * @link https://docs.premid.app/presence-development/coding/presence-class#getstrings-object
      */
     getStrings(strings: Object): Promise<any>;
     /**
      * Get variables from the actual site.
      * @param {Array} variables Array of variable names to get
-     * @example var pageVar = getPageVariable('pageVar') -> pageVar -> "Variable content"
+     * @example var pageVar = getPageVariable('pageVar') -> "Variable content"
+     * @link https://docs.premid.app/presence-development/coding/presence-class#getpagevariable-string
      */
     getPageVariable(variable: string): Promise<any>;
     /**
@@ -71,6 +82,7 @@ declare class Presence {
      * Subscribe to events emitted by the extension
      * @param eventName EventName to subscribe to
      * @param callback Callback function for event
+     * @link https://docs.premid.app/presence-development/coding/presence-class#events
      */
     on(eventName: "UpdateData" | "MediaKeys" | "iFrameData", callback: Function): void;
 }
