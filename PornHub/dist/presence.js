bd9f965a99bf62bcd292927f9bd29a20283f20d3
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-08-07 23:23:17 +0300

    :star: Added Romanian description in metadata.

--- PornHub/dist/presence.js
+++ PornHub/dist/presence.js
@@ -17,12 +17,11 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     var video = document.querySelector(".mhp1138_videoWrapper video");
     if (video[0] !== null && !isNaN(video.duration)) {
         var title;
-        title =
-            document.querySelector(".video-wrapper .title-container .title");
+        title = document.querySelector(".video-wrapper .title-container .title");
         var uploader = document.querySelector(".video-actions-container .video-info-row .usernameWrap a"), timestamps = getTimestamps(Math.floor(video.currentTime), Math.floor(video.duration)), presenceData = {
             details: title.innerText,
             state: uploader.textContent,
-            largeImageKey: "ph3",
+            largeImageKey: "lg",
             smallImageKey: video.paused ? "pause" : "play",
             smallImageText: video.paused
                 ? (yield strings).pause
