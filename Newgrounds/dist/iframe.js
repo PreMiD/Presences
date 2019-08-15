1d178ac7d8e589a5f631a32508a3515566615a50
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-08-15 22:01:54 +0300

    :sparkles: Added Newgrounds Presence.

---
+++ Newgrounds/dist/iframe.js
@@ -0,0 +1,15 @@
+setInterval(function () {
+    if (document.location.hostname == "www.newgrounds.com") {
+        var video = document.querySelector("#ngVideoPlayer_html5_api");
+        if (video != undefined && !isNaN(video.duration)) {
+            iframe.send({
+                iframe_video: {
+                    iFrameVideo: video,
+                    currTime: video.currentTime,
+                    dur: video.duration,
+                    paused: video.paused
+                }
+            });
+        }
+    }
+}, 100);
