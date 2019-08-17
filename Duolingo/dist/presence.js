5c543486635ddf32bc94c241f904bfe687bcffb2
TheSlowly <41243997+TheSlowly1@users.noreply.github.com>
2019-08-17 05:00:26 +0200

    hopefully solved a unknown bug

--- Duolingo/dist/presence.js
+++ Duolingo/dist/presence.js
@@ -13,39 +13,39 @@ var presence = new Presence({
     largeImageKey: "logo"
 };
 let lang = new Map();
-lang.set('de', 'German').
-    set('ep', 'Spanish').
-    set('fr', 'French').
-    set('ja', 'Japanese').
-    set('it', 'Italian').
-    set('ko', 'Korean').
-    set('zs', 'Chinese').
-    set('ru', 'Russian').
-    set('pt', 'Portuguese').
-    set('tr', 'Turkish').
-    set('dn', 'Dutch').
-    set('sv', 'Swedish').
-    set('el', 'Greek').
-    set('hi', 'Hindi').
-    set('hv', 'high valyrian').
-    set('ga', 'Irish').
-    set('pl', 'Polish').
-    set('he', 'Hebrew').
-    set('nb', 'Norwegian').
-    set('vi', 'Vietnamese').
-    set('ar', 'Arabic').
-    set('hw', 'Hawaiian').
-    set('da', 'Danish').
-    set('kl', 'Klingon').
-    set('ro', 'Romanian').
-    set('cs', 'Czech').
-    set('sw', 'Swahili').
-    set('cy', 'Walsh').
-    set('id', 'Indonesian').
-    set('hu', 'Hungarian').
-    set('uk', 'Ukrainian').
-    set('eo', 'Esperanto').
-    set('nv', 'Navajo');
+lang.set('de', 'German');
+lang.set('es', 'Spanish');
+lang.set('fr', 'French');
+lang.set('ja', 'Japanese');
+lang.set('it', 'Italian');
+lang.set('ko', 'Korean');
+lang.set('zs', 'Chinese');
+lang.set('ru', 'Russian');
+lang.set('pt', 'Portuguese');
+lang.set('tr', 'Turkish');
+lang.set('dn', 'Dutch');
+lang.set('sv', 'Swedish');
+lang.set('el', 'Greek');
+lang.set('hi', 'Hindi');
+lang.set('hv', 'high valyrian');
+lang.set('ga', 'Irish');
+lang.set('pl', 'Polish');
+lang.set('he', 'Hebrew');
+lang.set('nb', 'Norwegian');
+lang.set('vi', 'Vietnamese');
+lang.set('ar', 'Arabic');
+lang.set('hw', 'Hawaiian');
+lang.set('da', 'Danish');
+lang.set('kl', 'Klingon');
+lang.set('ro', 'Romanian');
+lang.set('cs', 'Czech');
+lang.set('sw', 'Swahili');
+lang.set('cy', 'Walsh');
+lang.set('id', 'Indonesian');
+lang.set('hu', 'Hungarian');
+lang.set('uk', 'Ukrainian');
+lang.set('eo', 'Esperanto');
+lang.set('nv', 'Navajo');
 presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     var path = document.location.pathname;
     if (path.length > 1 && path.split("/")[2] !== null && path.split("/")[2].length == 2) {
--- Duolingo/presence.ts
+++ Duolingo/presence.ts
@@ -7,55 +7,55 @@ var presence = new Presence({
 };
 
 let lang = new Map();
-lang.set('de', 'German').
-set('ep', 'Spanish').
-set('fr', 'French').
-set('ja', 'Japanese').
-set('it', 'Italian').
-set('ko', 'Korean').
-set('zs', 'Chinese').
-set('ru', 'Russian').
-set('pt', 'Portuguese').
-set('tr', 'Turkish').
-set('dn', 'Dutch').
-set('sv', 'Swedish').
-set('el', 'Greek').
-set('hi', 'Hindi').
-set('hv', 'high valyrian').
-set('ga', 'Irish').
-set('pl', 'Polish').
-set('he', 'Hebrew').
-set('nb', 'Norwegian').
-set('vi', 'Vietnamese').
-set('ar', 'Arabic').
-set('hw', 'Hawaiian').
-set('da', 'Danish').
-set('kl', 'Klingon').
-set('ro', 'Romanian').
-set('cs', 'Czech').
-set('sw', 'Swahili').
-set('cy', 'Walsh').
-set('id', 'Indonesian').
-set('hu', 'Hungarian').
-set('uk', 'Ukrainian').
-set('eo', 'Esperanto').
-set('nv', 'Navajo');
+lang.set('de', 'German');
+lang.set('es', 'Spanish');
+lang.set('fr', 'French');
+lang.set('ja', 'Japanese');
+lang.set('it', 'Italian');
+lang.set('ko', 'Korean');
+lang.set('zs', 'Chinese');
+lang.set('ru', 'Russian');
+lang.set('pt', 'Portuguese');
+lang.set('tr', 'Turkish');
+lang.set('dn', 'Dutch');
+lang.set('sv', 'Swedish');
+lang.set('el', 'Greek');
+lang.set('hi', 'Hindi');
+lang.set('hv', 'high valyrian');
+lang.set('ga', 'Irish');
+lang.set('pl', 'Polish');
+lang.set('he', 'Hebrew');
+lang.set('nb', 'Norwegian');
+lang.set('vi', 'Vietnamese');
+lang.set('ar', 'Arabic');
+lang.set('hw', 'Hawaiian');
+lang.set('da', 'Danish');
+lang.set('kl', 'Klingon');
+lang.set('ro', 'Romanian');
+lang.set('cs', 'Czech');
+lang.set('sw', 'Swahili');
+lang.set('cy', 'Walsh');
+lang.set('id', 'Indonesian');
+lang.set('hu', 'Hungarian');
+lang.set('uk', 'Ukrainian');
+lang.set('eo', 'Esperanto');
+lang.set('nv', 'Navajo');
 
 presence.on("UpdateData", async () => {
-    var path = document.location.pathname
+    var path = document.location.pathname;
 
     if(path.length > 1 && path.split("/")[2] !== null && path.split("/")[2].length == 2) {
-      var language:string
+      var language:string;
       for (let value of lang.keys()) {
         if(path.split("/")[2] == value) {
-          language = lang.get(value)
+          language = lang.get(value);
           break
         }
       }
       
-      presenceData.details = "Taking a " + language + " lesson"
-      presenceData.state = path.split("/")[3].replace("-", " ")
-      presenceData.largeImageKey = "logo"
+      presenceData.details = "Taking a " + language + " lesson";
+      presenceData.state = path.split("/")[3].replace("-", " ");
+      presenceData.largeImageKey = "logo";
 
       presence.setActivity(presenceData);
   
