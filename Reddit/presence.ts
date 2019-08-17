a7b4cdb8e96c9be16d9446cb601bbaf37810158c
Viruz <gustavomelati@gmail.com>
2019-08-17 16:16:38 -0300

    Forgot to add the things

---
+++ Reddit/presence.ts
@@ -0,0 +1,45 @@
+let presence : Presence = new Presence({
+    clientId: "609183409440555018"
+}),
+startedBrowsingTimestamp : number = Math.floor(Date.now() / 1000),
+presenceData : presenceData = {
+    largeImageKey: "reddit_lg",
+    startTimestamp: startedBrowsingTimestamp
+},
+subReddit : string,
+postTitle : string,
+profile : string,
+nickname : string,
+path : string,
+strings = presence.getStrings({
+    browsing: "presence.activity.browsing"
+});
+
+presence.on("UpdateData", async () => {
+    path = window.location.pathname;
+    presenceData.startTimestamp = startedBrowsingTimestamp;
+    if (path.includes("comments")) {
+        postTitle = document.querySelector("div._2SdHzo12ISmrC8H86TgSCp._29WrubtjAcKqzJSPdQqQ4h") != undefined ? document.querySelector("div._2SdHzo12ISmrC8H86TgSCp._29WrubtjAcKqzJSPdQqQ4h").textContent : "";
+        subReddit = document.querySelector('span._1GieMuLljOrqnVpRAwz7VP').textContent;
+        subReddit = subReddit == "Home" && document.querySelectorAll('._19bCWnxeTjqzBElWZfIlJb')[1] != undefined ? document.querySelectorAll('._19bCWnxeTjqzBElWZfIlJb')[1].textContent : subReddit;
+        presenceData.details = "Reading '" + postTitle + "'";
+        presenceData.state = subReddit;
+        delete presenceData.smallImageKey;
+    } else if (path.includes("user")) {
+        profile = document.querySelector('span._1GieMuLljOrqnVpRAwz7VP').textContent;
+        nickname = document.querySelector('h4._3W1eUu5jHdcamkzFiJDITJ') ? document.querySelector('h4._3W1eUu5jHdcamkzFiJDITJ').textContent : "";
+        presenceData.details = nickname == "" ? "Viewing a profile" : "Viewing " + nickname + "'s profile";
+        presenceData.state = profile;
+        delete presenceData.smallImageKey;
+    } else if (path.includes('search')) {
+        presenceData.details = "Searching...";
+        delete presenceData.state;
+        presenceData.smallImageKey = "search";
+    } else {
+        subReddit = document.querySelector('span._1GieMuLljOrqnVpRAwz7VP').textContent;
+        presenceData.details = (await strings).browsing;
+        presenceData.state = subReddit;
+        delete presenceData.smallImageKey;
+    }
+    presence.setActivity(presenceData, true);
+});
