c7d9325622d1ffec5d2069f9bf795751627c3a4b
Viruz <gustavomelati@gmail.com>
2019-08-17 16:15:40 -0300

    Just recoded the presence in ts, probably fixes some bugs

--- Reddit/dist/presence.js
+++ Reddit/dist/presence.js
@@ -1,33 +1,47 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let presence = new Presence({
     clientId: "609183409440555018"
-}),
-startedBrowsingTimestamp = Math.floor(Date.now() / 1000),
-presenceData = {
+}), startedBrowsingTimestamp = Math.floor(Date.now() / 1000), presenceData = {
     largeImageKey: "reddit_lg",
-    startTimestamp: startedBrowsingTimestamp,
-    state: ""
-},
-subReddit,
-postTitle,
-profile;
-
-presence.on("UpdateData", () => {
+    startTimestamp: startedBrowsingTimestamp
+}, subReddit, postTitle, profile, nickname, path, strings = presence.getStrings({
+    browsing: "presence.activity.browsing"
+});
+presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
+    path = window.location.pathname;
     presenceData.startTimestamp = startedBrowsingTimestamp;
-    if (window.location.pathname.includes("comments")) {
-        postTitle = document.querySelectorAll("div._2SdHzo12ISmrC8H86TgSCp._29WrubtjAcKqzJSPdQqQ4h")[0].textContent;
-        subReddit = document.querySelectorAll('span._1GieMuLljOrqnVpRAwz7VP')[0].textContent;
-        subReddit = subReddit == "Home" ? document.querySelectorAll('a._3ryJoIoycVkA88fy40qNJc')[51].textContent : subReddit;
+    if (path.includes("comments")) {
+        postTitle = document.querySelector("div._2SdHzo12ISmrC8H86TgSCp._29WrubtjAcKqzJSPdQqQ4h") != undefined ? document.querySelector("div._2SdHzo12ISmrC8H86TgSCp._29WrubtjAcKqzJSPdQqQ4h").textContent : "";
+        subReddit = document.querySelector('span._1GieMuLljOrqnVpRAwz7VP').textContent;
+        subReddit = subReddit == "Home" && document.querySelectorAll('._19bCWnxeTjqzBElWZfIlJb')[1] != undefined ? document.querySelectorAll('._19bCWnxeTjqzBElWZfIlJb')[1].textContent : subReddit;
         presenceData.details = "Reading '" + postTitle + "'";
-        presenceData.state = subReddit; 
-    } else if (window.location.pathname.includes("user")) {
-        profile = document.querySelectorAll('span._1GieMuLljOrqnVpRAwz7VP')[0].textContent,
-        nickname = document.querySelectorAll('h4._3W1eUu5jHdcamkzFiJDITJ')[0]
-        presenceData.details = nickname == undefined ? "Viewing a profile" : "Viewing " + nickname.textContent + "'s profile";
-        presenceData.state = profile;
-    } else {
-        subReddit = document.querySelectorAll('span._1GieMuLljOrqnVpRAwz7VP')[0].textContent;
-        presenceData.details = "Browsing...";
         presenceData.state = subReddit;
+        delete presenceData.smallImageKey;
+    }
+    else if (path.includes("user")) {
+        profile = document.querySelector('span._1GieMuLljOrqnVpRAwz7VP').textContent;
+        nickname = document.querySelector('h4._3W1eUu5jHdcamkzFiJDITJ') ? document.querySelector('h4._3W1eUu5jHdcamkzFiJDITJ').textContent : "";
+        presenceData.details = nickname == "" ? "Viewing a profile" : "Viewing " + nickname + "'s profile";
+        presenceData.state = profile;
+        delete presenceData.smallImageKey;
+    }
+    else if (path.includes('search')) {
+        presenceData.details = "Searching...";
+        delete presenceData.state;
+        presenceData.smallImageKey = "search";
+    }
+    else {
+        subReddit = document.querySelector('span._1GieMuLljOrqnVpRAwz7VP').textContent;
+        presenceData.details = (yield strings).browsing;
+        presenceData.state = subReddit;
+        delete presenceData.smallImageKey;
     }
     presence.setActivity(presenceData, true);
-});
+}));
