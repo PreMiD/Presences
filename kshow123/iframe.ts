ad7f057ce327fc706466a91bbc54052737c4951e
Bas950 <34792838+Bas950@users.noreply.github.com>
2019-08-22 21:47:49 +0200

    Added Viki Presence and added extra video service to Kshow123
    Added Viki Presence and added extra video service to Kshow123

--- kshow123/iframe.ts
+++ kshow123/iframe.ts
@@ -171,6 +171,25 @@ setInterval(function() {
 
         }
 
+    } else if(document.querySelector("#mgvideo_html5_api") !== null) {
+
+        var video : HTMLVideoElement = document.querySelector("#mgvideo_html5_api");
+
+        if(video != undefined && !isNaN(video.duration)) {
+
+            iframe.send(
+            {
+                iframe_video: {
+                    iFrameVideo: video,
+                    currTime: video.currentTime,
+                    dur: video.duration,
+                    paused: video.paused
+                }
+            });
+
+
+        }
+
     } else if(document.querySelector("#vstr > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video") !== null) {
 
         var video : HTMLVideoElement = document.querySelector("#vstr > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video");
