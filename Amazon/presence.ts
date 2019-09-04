5febda37c4b237cca786def3f224d3589e07dfd1
Bruno Henrique Paiva <bruno@sigane.com.br>
2019-09-04 15:21:35 -0300

    Amazon presence cleanup

--- Amazon/presence.ts
+++ Amazon/presence.ts
@@ -1,270 +1,130 @@
-var genericStyle = "font-weight: 800; padding: 2px 5px; color: white;";
-
-function PMD_info(message) {
-  console.log(
-    "%cPreMiD%cINFO%c " + message,
-    genericStyle + "border-radius: 25px 0 0 25px; background: #596cae;",
-    genericStyle + "border-radius: 0 25px 25px 0; background: #5050ff;",
-    "color: unset;"
-  );
-}
-
-function PMD_error(message) {
-  console.log(
-    "%cPreMiD%cERROR%c " + message,
-    genericStyle + "border-radius: 25px 0 0 25px; background: #596cae;",
-    genericStyle + "border-radius: 0 25px 25px 0; background: #ff5050;",
-    "color: unset;"
-  );
-}
-
-function PMD_success(message) {
-  console.log(
-    "%cPreMiD%cSUCCESS%c " + message,
-    genericStyle + "border-radius: 25px 0 0 25px; background: #596cae;",
-    genericStyle +
-      "border-radius: 0 25px 25px 0; background: #50ff50; color: black;",
-    "color: unset;"
-  );
-}
-
-var presence = new Presence({
-  clientId: "618138980273094695", // CLIENT ID FOR YOUR PRESENCE
+const presence = new Presence({
+  clientId: "618138980273094695",
   mediaKeys: false
-})
+});
+const AMAZON_URL_REGEX = /www\.amazon\.(de|ca|cn|es|fr|nl|in|co\.jp|ae|co\.uk|com(\.(tr|br|au))?)/;
+const startTimestamp = Math.floor(Date.now() / 1000);
 
-var item : any, dropdown : any, dropdownfinal : any, dropdownplus1 : any, search : any, dropdowninnertext : any, split : any, personal2 : any, profile : any, board2 : any;
- 
-var browsingStamp = Math.floor(Date.now()/1000);
+function updateDropdownPresenceData(
+  dropdown: HTMLSelectElement,
+  presenceData: presenceData
+): void {
+  const selectedValueStr = dropdown.value;
+  const selectedOption: HTMLOptionElement = document.querySelector(
+    `#searchDropdownBox > option[value="${selectedValueStr}"]`
+  );
+
+  presenceData.details = "Browsing category:";
+  presenceData.state = selectedOption.innerText;
+}
 
 presence.on("UpdateData", async () => {
-
-  let presenceData: presenceData = {
-    largeImageKey: "amazon"
+  const { hostname, pathname } = window.location;
+  const presenceData: presenceData = {
+    largeImageKey: "amazon",
+    startTimestamp
   };
 
-  presenceData.startTimestamp = browsingStamp;
-
-  if(document.location.hostname == "www.amazon.com" || document.location.hostname == "www.amazon.com.au" || document.location.hostname == "www.amazon.de" || document.location.hostname == "www.amazon.ca" || document.location.hostname == "www.amazon.cn" || document.location.hostname == "www.amazon.es" || document.location.hostname == "www.amazon.fr" || document.location.hostname == "www.amazon.nl" || document.location.hostname == "www.amazon.in" || document.location.hostname == "www.amazon.co.jp" || document.location.hostname == "www.amazon.ae" || document.location.hostname == "www.amazon.co.uk" || document.location.hostname == "www.amazon.com.tr" || document.location.hostname == "www.amazon.com.br") {
-    item = document.querySelector("#search > span > h1 > div > div.sg-col-14-of-20.sg-col-26-of-32.sg-col-18-of-24.sg-col.sg-col-22-of-28.s-breadcrumb.sg-col-10-of-16.sg-col-30-of-36.sg-col-6-of-12 > div > div > span.a-color-state.a-text-bold");
-
-    if(document.querySelector("#productTitle") !== null) {
-      item = document.querySelector("#productTitle");
-
-      presenceData.details = "Viewing product:";
-      if (item.innerText.length > 128) {
-        presenceData.state = item.innerText.substring(0,125) + "...";
-      } else {
-        presenceData.state = item.innerText;
-      }
-            
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-
-    } else if(document.querySelector("#a-page > div.av-page-desktop.avu-retail-page > div.avu-content.avu-section > div > div > div.DVWebNode-detail-atf-wrapper.DVWebNode > div.av-detail-section > div > h1") !== null) {
-      item = document.querySelector("#a-page > div.av-page-desktop.avu-retail-page > div.avu-content.avu-section > div > div > div.DVWebNode-detail-atf-wrapper.DVWebNode > div.av-detail-section > div > h1");
-
-      presenceData.details = "Viewing Prime Video:";
-      presenceData.state = item.innerText;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-
-    } else if (document.location.pathname.includes("/s") && item !== null) {
-      item = document.querySelector("#search > span > h1 > div > div.sg-col-14-of-20.sg-col-26-of-32.sg-col-18-of-24.sg-col.sg-col-22-of-28.s-breadcrumb.sg-col-10-of-16.sg-col-30-of-36.sg-col-6-of-12 > div > div > span.a-color-state.a-text-bold");
-      
-      presenceData.details = "Searching for:";
-      presenceData.state = item.innerText;
-      
-      presenceData.smallImageKey = "search";
-      
-      presence.setActivity(presenceData); 
-    } else if (document.querySelector("#gc-asin-title") !== null) {
-      item = document.querySelector("#gc-asin-title");
-
-      presenceData.details = "Viewing product:";
-      if (item.innerText.length > 128) {
-        presenceData.state = item.innerText.substring(0,125) + "...";
-      } else {
-        presenceData.state = item.innerText;
-      }
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-
-    } else if (document.location.pathname.includes("/profile")) {
-      item = document.querySelector("#customer-profile-name-header > div.a-row.a-spacing-none.name-container > span");
-      presenceData.details = "Viewing profile:";
-      presenceData.state = item.innerText;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/store")) {
-      item = document.title.split(":");
-      presenceData.details = "Viewing store:";
-      presenceData.state = item[1];
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/history")) {
-      presenceData.details = "Viewing their history";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/gift-cards")) {
-      presenceData.details = "Viewing Giftcards";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/yourstore")) {
-      presenceData.details = "Viewing recommended";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/wishlist")) {
-      presenceData.details = "Viewing their wishlist";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/cart")) {
-      presenceData.details = "Viewing their cart";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/order-history")) {
-      presenceData.details = "Viewing their";
-      presenceData.state = "order history";
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/order-details")) {
-      presenceData.details = "Viewing their";
-      presenceData.state = "order details";
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/amazonprime")) {
-      presenceData.details = "Viewing Amazon Prime";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/site-directory")) {
-      presenceData.details = "Viewing all categories";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/yourpets")) {
-      presenceData.details = "Viewing pets";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/addresses")) {
-      presenceData.details = "Viewing addresses";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/managepaymentmethods")) {
-      presenceData.details = "Viewing payment methods";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/balance")) {
-      presenceData.details = "Viewing their balance";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/adprefs")) {
-      presenceData.details = "Viewing their adprefs";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/yourmembershipsandsubscriptions")) {
-      presenceData.details = "Viewing subscriptions";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.search.includes("nav_youraccount_ya") || document.location.pathname.includes("/your-account")) {
-      presenceData.details = "Viewing their account";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else if (document.location.pathname.includes("/ref=nav_logo")) {
-      presence.setActivity();
-      presence.setTrayTitle(); 
-    } else if (document.location.pathname.includes("/help/")) {
-      presenceData.details = "Viewing Help Center";
-      delete presenceData.state;
-      
-      delete presenceData.smallImageKey;
-      
-      presence.setActivity(presenceData); 
-    } else { 
-      if (document.querySelector("#searchDropdownBox") !== null) {
-        dropdown = document.querySelector("#searchDropdownBox").getAttribute("data-nav-selected");
-        dropdownplus1 = +dropdown + 1;
-        dropdownfinal = "#searchDropdownBox > option:nth-child(" + dropdownplus1 + ")";
-        dropdowninnertext = document.querySelector(dropdownfinal).innerText;
-        split = document.location.pathname.split("/" , 3);
-        if (dropdown !== "0" || split[1] !== "") {
-          presenceData.details = "Browsing category:";
-          presenceData.state = dropdowninnertext;
-        
-          delete presenceData.smallImageKey;
-        
-          presence.setActivity(presenceData); 
-        } else {
-  
-          presence.setActivity();
-          presence.setTrayTitle(); 
-  
-        }
-      } else {
-
-        presence.setActivity();
-        presence.setTrayTitle(); 
-
-      }
-    }
-  } else { 
-
+  if (hostname.match(AMAZON_URL_REGEX) === null) {
     presence.setActivity();
-    presence.setTrayTitle(); 
-    
+    presence.setTrayTitle();
+    return;
   }
 
+  const productTitleSpan: HTMLSpanElement = document.querySelector(
+    "span#productTitle"
+  );
+  const primeVideoTitle: HTMLHeadingElement = document.querySelector(
+    "#a-page > div.av-page-desktop.avu-retail-page > div.avu-content.avu-section > div > div > div.DVWebNode-detail-atf-wrapper.DVWebNode > div.av-detail-section > div > h1"
+  );
+  const searchTermSpan: HTMLSpanElement = document.querySelector(
+    "#search > span > h1 > div > div.sg-col-14-of-20.sg-col-26-of-32.sg-col-18-of-24.sg-col.sg-col-22-of-28.s-breadcrumb.sg-col-10-of-16.sg-col-30-of-36.sg-col-6-of-12 > div > div > span.a-color-state.a-text-bold"
+  );
+  const gcAsinTitle: HTMLSpanElement = document.querySelector("#gc-asin-title");
+
+  if (productTitleSpan !== null) {
+    const productTitle = productTitleSpan.innerText;
+
+    presenceData.details = "Viewing product:";
+    presenceData.state =
+      productTitle.length > 128
+        ? productTitle.substring(0, 125) + "..."
+        : productTitle;
+  } else if (primeVideoTitle !== null) {
+    presenceData.details = "Viewing Prime Video:";
+    presenceData.state = primeVideoTitle.innerText;
+  } else if (pathname.includes("/s") && searchTermSpan !== null) {
+    presenceData.details = "Searching for:";
+    presenceData.state = searchTermSpan.innerText;
+    presenceData.smallImageKey = "search";
+  } else if (gcAsinTitle !== null) {
+    const title = gcAsinTitle.innerText;
+
+    presenceData.details = "Viewing product:";
+    presenceData.state =
+      title.length > 128 ? title.substring(0, 125) + "..." : title;
+  } else if (pathname.includes("/profile")) {
+    const nameSpan: HTMLSpanElement = document.querySelector(
+      "#customer-profile-name-header > div.a-row.a-spacing-none.name-container > span"
+    );
+    presenceData.details = "Viewing profile:";
+    presenceData.state = nameSpan.innerText;
+  } else if (pathname.includes("/store")) {
+    presenceData.details = "Viewing store:";
+    presenceData.state = document.title.split(":")[1];
+  } else if (pathname.includes("/history")) {
+    presenceData.details = "Viewing their history";
+  } else if (pathname.includes("/gift-cards")) {
+    presenceData.details = "Viewing Giftcards";
+  } else if (pathname.includes("/yourstore")) {
+    presenceData.details = "Viewing recommended";
+  } else if (pathname.includes("/wishlist")) {
+    presenceData.details = "Viewing their wishlist";
+  } else if (pathname.includes("/cart")) {
+    presenceData.details = "Viewing their cart";
+  } else if (pathname.includes("/order-history")) {
+    presenceData.details = "Viewing their";
+    presenceData.state = "order history";
+  } else if (pathname.includes("/order-details")) {
+    presenceData.details = "Viewing their";
+    presenceData.state = "order details";
+  } else if (pathname.includes("/amazonprime")) {
+    presenceData.details = "Viewing Amazon Prime";
+  } else if (pathname.includes("/site-directory")) {
+    presenceData.details = "Viewing all categories";
+  } else if (pathname.includes("/yourpets")) {
+    presenceData.details = "Viewing pets";
+  } else if (pathname.includes("/addresses")) {
+    presenceData.details = "Viewing addresses";
+  } else if (pathname.includes("/managepaymentmethods")) {
+    presenceData.details = "Viewing payment methods";
+  } else if (pathname.includes("/balance")) {
+    presenceData.details = "Viewing their balance";
+  } else if (pathname.includes("/adprefs")) {
+    presenceData.details = "Viewing their adprefs";
+  } else if (pathname.includes("/yourmembershipsandsubscriptions")) {
+    presenceData.details = "Viewing subscriptions";
+  } else if (
+    document.location.search.includes("nav_youraccount_ya") ||
+    pathname.includes("/your-account")
+  ) {
+    presenceData.details = "Viewing their account";
+  } else if (pathname.includes("/help/")) {
+    presenceData.details = "Viewing Help Center";
+  } else if (document.querySelector("#searchDropdownBox") !== null) {
+    const dropdown: HTMLSelectElement = document.querySelector(
+      "#searchDropdownBox"
+    );
+
+    updateDropdownPresenceData(dropdown, presenceData);
+    dropdown.onchange = () =>
+      updateDropdownPresenceData(dropdown, presenceData);
+  }
+
+  if (!presenceData.details) {
+    presence.setActivity();
+    presence.setTrayTitle();
+  } else {
+    presence.setActivity(presenceData);
+  }
 });
