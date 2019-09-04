681ba6830c5b7d2ec645bba416f8fdb344c1f5aa
Bruno Henrique Paiva <bruno@sigane.com.br>
2019-09-04 16:59:33 -0300

    AniList presence cleanup

--- Anilist/dist/presence.js
+++ Anilist/dist/presence.js
@@ -6,72 +6,69 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let presence = new Presence({
+const presence = new Presence({
     clientId: "614220272790274199"
-}), startedBrowsing = Math.floor(Date.now() / 1000), presenceData = {
-    largeImageKey: "anilist_lg",
-    startTimestamp: startedBrowsing
-}, strings = presence.getStrings({
-    "browsing": "presence.activity.browsing"
-}), path, user, title;
+});
+const USER_PAGE_REGEX = /^\/user\/(?<user>\w+)\/(?<pageType>\w+)?/;
+const SEARCH_PAGE_REGEX = /^\/search\/(?<type>\w+)/;
+const startTimestamp = Math.floor(Date.now() / 1000);
 presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
-    path = window.location.pathname;
-    if (path.includes('home')) {
-        presenceData.details = (yield strings).browsing;
+    const { pathname } = window.location;
+    const presenceData = {
+        largeImageKey: "anilist_lg",
+        startTimestamp
+    };
+    const contentTitleElement = document.querySelector("div.content > h1:first-child");
+    const containerTitleElement = document.querySelector("div.container > h1:first-child");
+    const forumPostTitleElement = document.querySelector(".forum-thread > h1.title");
+    let regexResult;
+    if (pathname.includes("home")) {
+        const strings = yield presence.getStrings({
+            browsing: "presence.activity.browsing"
+        });
+        presenceData.details = strings.browsing;
         presenceData.state = "Home";
     }
-    else if (path.includes('animelist')) {
-        user = document.querySelector('.name').textContent;
-        presenceData.details = user + "'s animelist";
-        delete presenceData.state, presenceData.smallImageKey;
+    else if ((regexResult = pathname.match(USER_PAGE_REGEX)) !== null) {
+        const { user, pageType } = regexResult.groups;
+        presenceData.details = `Viewing ${user}'s ${!pageType ? "profile" : pageType}`;
     }
-    else if (path.includes('mangalist')) {
-        user = document.querySelector('.name').textContent;
-        presenceData.details = user + "'s mangalist";
-        delete presenceData.state, presenceData.smallImageKey;
-    }
-    else if (path.includes('user')) {
-        user = document.querySelector('.name').textContent;
-        presenceData.details = "Viewing " + user + "'s profile";
-        delete presenceData.state, presenceData.smallImageKey;
-    }
-    else if (path.includes('search')) {
-        presenceData.details = "Searching for an anime";
-        presenceData.state = "'" + document.querySelector('input.el-input__inner').value + "'";
+    else if ((regexResult = pathname.match(SEARCH_PAGE_REGEX)) !== null) {
+        const { type } = regexResult.groups;
+        const input = document.querySelector("input.el-input__inner");
+        presenceData.details = `Searching for an ${type.toLowerCase()}`;
+        if (input.value.trim().length > 0) {
+            presenceData.state = `'${input.value}'`;
+        }
         presenceData.smallImageKey = "search";
         presenceData.smallImageText = "Searching";
     }
-    else if (path.includes('anime')) {
-        title = document.querySelector('div.content > h1').textContent.trim();
+    else if (pathname.startsWith("/anime")) {
         presenceData.details = "Looking an anime";
-        presenceData.state = title;
-        delete presenceData.smallImageKey;
+        presenceData.state = contentTitleElement.innerText;
     }
-    else if (path.includes('manga')) {
-        title = document.querySelector('div.content > h1').textContent.trim();
+    else if (pathname.startsWith("/manga")) {
         presenceData.details = "Looking an manga";
-        presenceData.state = title;
-        delete presenceData.smallImageKey;
+        presenceData.state = contentTitleElement.innerText;
     }
-    else if (path.includes('forum')) {
-        if (path.split('/').length > 3) {
+    else if (pathname.startsWith("/forum")) {
+        if (pathname.split("/").length > 3) {
             presenceData.details = "Reading a forum post";
-            presenceData.state = "'" + document.querySelector('h1.title').textContent.trim() + "'";
+            presenceData.state = `'${forumPostTitleElement.innerText.trim()}'`;
         }
         else {
             presenceData.details = "Browsing the forum";
-            delete presenceData.state;
         }
-        delete presenceData.smallImageKey;
     }
-    else if (path.includes('studio')) {
+    else if (pathname.startsWith("/studio")) {
         presenceData.details = "Looking a studio";
-        presenceData.state = document.querySelector('div.container > h1').textContent;
-        delete presenceData.smallImageKey;
+        presenceData.state = containerTitleElement.innerText;
+    }
+    if (!presenceData.details) {
+        presence.setActivity();
+        presence.setTrayTitle();
     }
     else {
-        presenceData.details = "Somewhere unknown";
-        delete presenceData.state, presenceData.smallImageKey;
+        presence.setActivity(presenceData, true);
     }
-    presence.setActivity(presenceData, true);
 }));
--- Anilist/presence.ts
+++ Anilist/presence.ts
@@ -1,66 +1,75 @@
-let presence : Presence = new Presence({
-    clientId: "614220272790274199"
-}),
-startedBrowsing : number = Math.floor(Date.now() / 1000),
-presenceData : presenceData = {
-    largeImageKey: "anilist_lg",
-    startTimestamp: startedBrowsing
-},
-strings = presence.getStrings({
-    "browsing": "presence.activity.browsing"
-}),
-path : string,
-user : string,
-title : string;
+const presence = new Presence({
+  clientId: "614220272790274199"
+});
+const USER_PAGE_REGEX = /^\/user\/(?<user>\w+)\/(?<pageType>\w+)?/;
+const SEARCH_PAGE_REGEX = /^\/search\/(?<type>\w+)/;
+const startTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-    path = window.location.pathname;
-    if (path.includes('home')) {
-        presenceData.details = (await strings).browsing;
-        presenceData.state = "Home";
-    } else if (path.includes('animelist')) {
-        user = document.querySelector('.name').textContent;
-        presenceData.details = user + "'s animelist";
-        delete presenceData.state, presenceData.smallImageKey;
-    } else if (path.includes('mangalist')) {
-        user = document.querySelector('.name').textContent;
-        presenceData.details = user + "'s mangalist";
-        delete presenceData.state, presenceData.smallImageKey;
-    } else if (path.includes('user')) {
-        user = document.querySelector('.name').textContent;
-        presenceData.details = "Viewing " + user + "'s profile";
-        delete presenceData.state, presenceData.smallImageKey;
-    } else if (path.includes('search')) {
-        presenceData.details = "Searching for an anime";
-        presenceData.state = "'" + (document.querySelector('input.el-input__inner') as HTMLInputElement).value + "'";
-        presenceData.smallImageKey = "search";
-        presenceData.smallImageText = "Searching";
-    } else if (path.includes('anime')) {
-        title = document.querySelector('div.content > h1').textContent.trim();
-        presenceData.details = "Looking an anime";
-        presenceData.state = title;
-        delete presenceData.smallImageKey;
-    } else if (path.includes('manga')) {
-        title = document.querySelector('div.content > h1').textContent.trim();
-        presenceData.details = "Looking an manga";
-        presenceData.state = title;
-        delete presenceData.smallImageKey;
-    } else if (path.includes('forum')) {
-        if (path.split('/').length > 3) {
-            presenceData.details = "Reading a forum post"
-            presenceData.state = "'" + document.querySelector('h1.title').textContent.trim() + "'";
-        } else {
-            presenceData.details = "Browsing the forum";
-            delete presenceData.state;
-        }
-        delete presenceData.smallImageKey;
-    } else if (path.includes('studio')) {
-        presenceData.details = "Looking a studio";
-        presenceData.state = document.querySelector('div.container > h1').textContent;
-        delete presenceData.smallImageKey;
-    } else {
-        presenceData.details = "Somewhere unknown"
-        delete presenceData.state, presenceData.smallImageKey;
+  const { pathname } = window.location;
+  const presenceData: presenceData = {
+    largeImageKey: "anilist_lg",
+    startTimestamp
+  };
+
+  const contentTitleElement: HTMLHeadingElement = document.querySelector(
+    "div.content > h1:first-child"
+  );
+  const containerTitleElement: HTMLHeadingElement = document.querySelector(
+    "div.container > h1:first-child"
+  );
+  const forumPostTitleElement: HTMLHeadingElement = document.querySelector(
+    ".forum-thread > h1.title"
+  );
+
+  let regexResult: RegExpMatchArray;
+
+  if (pathname.includes("home")) {
+    const strings = await presence.getStrings({
+      browsing: "presence.activity.browsing"
+    });
+    presenceData.details = strings.browsing;
+    presenceData.state = "Home";
+  } else if ((regexResult = pathname.match(USER_PAGE_REGEX)) !== null) {
+    // @ts-ignore
+    const { user, pageType } = regexResult.groups;
+    presenceData.details = `Viewing ${user}'s ${
+      !pageType ? "profile" : pageType
+    }`;
+  } else if ((regexResult = pathname.match(SEARCH_PAGE_REGEX)) !== null) {
+    // @ts-ignore
+    const { type } = regexResult.groups;
+    const input: HTMLInputElement = document.querySelector(
+      "input.el-input__inner"
+    );
+    presenceData.details = `Searching for an ${type.toLowerCase()}`;
+    if (input.value.trim().length > 0) {
+      presenceData.state = `'${input.value}'`;
     }
+    presenceData.smallImageKey = "search";
+    presenceData.smallImageText = "Searching";
+  } else if (pathname.startsWith("/anime")) {
+    presenceData.details = "Looking an anime";
+    presenceData.state = contentTitleElement.innerText;
+  } else if (pathname.startsWith("/manga")) {
+    presenceData.details = "Looking an manga";
+    presenceData.state = contentTitleElement.innerText;
+  } else if (pathname.startsWith("/forum")) {
+    if (pathname.split("/").length > 3) {
+      presenceData.details = "Reading a forum post";
+      presenceData.state = `'${forumPostTitleElement.innerText.trim()}'`;
+    } else {
+      presenceData.details = "Browsing the forum";
+    }
+  } else if (pathname.startsWith("/studio")) {
+    presenceData.details = "Looking a studio";
+    presenceData.state = containerTitleElement.innerText;
+  }
+
+  if (!presenceData.details) {
+    presence.setActivity();
+    presence.setTrayTitle();
+  } else {
     presence.setActivity(presenceData, true);
-})
+  }
+});
