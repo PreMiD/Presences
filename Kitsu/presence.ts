194a4cac5d11ae8e744a1df737001417838f5455
April <fazingin@gmail.com>
2019-10-04 18:23:45 -0400

    major clean up + some new states

--- Kitsu/presence.ts
+++ Kitsu/presence.ts
@@ -16,42 +16,53 @@ presence.on('UpdateData', async () => {
     if (path == '/' || path.startsWith('/explore')) {
         presenceData.details = (await strings).browsing;
         delete presenceData.state;
-    } else if (path.includes('/users') && !path.includes('/library')) {
+    } else if (path.includes('/users')) {
         user = document.querySelector(".cover-username").textContent.trim();
         presenceData.details = `Viewing ${user} profile`;
 
-        delete presenceData.state, presenceData.startTimestamp;
-    } else if (path.includes('/library')) {
-        user = document.querySelector(".cover-username").textContent.trim();
-        presenceData.details = `Viewing ${user} profile`;
-        presenceData.state = 'Viewing their library';
-    } else if (path.startsWith('/anime/')) {
+        switch (path.split('/')[3]) {
+            case 'activity':
+                presenceData.state = 'Viewing their activity'
+                break;
+            case 'library':
+                presenceData.state = 'Viewing their library'
+                break;
+            case 'reactions':
+                presenceData.state = 'Viewing their reactions'
+                break;
+            case 'followers':
+                presenceData.state = 'Viewing their followers'
+                break;
+            case 'following':
+                presenceData.state = 'Viewing who they follow'
+                break;
+            case 'groups':
+                presenceData.state = 'Viewing their groups'
+                break;
+            default:
+                presenceData.state = 'I don\'t think they even know what they\'re viewing'
+        }
+    } else if (path.startsWith('/anime')) {
         presenceData.details = 'Looking through anime'
-        presenceData.state = `Viewing ${document.querySelector('h3').textContent.trim()}`;
-    } else if (path.startsWith('/anime') && !path.startsWith('/anime/')) {
-        presenceData.details = 'Browsing for anime'
-
-        delete presenceData.state
-    } else if (path.startsWith('/manga/')) {
+        if (path.split('/')[2]) {
+            presenceData.state = `Viewing ${document.querySelector('h3').textContent.trim()}`;
+        } else delete presenceData.state
+    } else if (path.startsWith('/manga')) {
         presenceData.details = 'Looking through manga'
-        presenceData.state = `Viewing ${document.querySelector('h3').textContent.trim()}`;
-    } else if (path.startsWith('/manga') && !path.startsWith('/manga/')) {
-        presenceData.details = 'Browsing for manga'
-
-        delete presenceData.state
-    } else if (path.startsWith('/groups/')) {
+        if (path.split('/')[2]) {
+            presenceData.state = `Viewing ${document.querySelector('h3').textContent.trim()}`;
+        } else delete presenceData.state
+    } else if (path.startsWith('/groups')) {
         presenceData.details = 'Looking through groups'
-        presenceData.state = `Viewing ${document.querySelector('.cover-username').textContent.trim()}`;
-    } else if (path.startsWith('/groups') && !path.startsWith('/groups/')) {
-        presenceData.details = 'Browsing for groups'
-
-        delete presenceData.state
+        if (path.split('/')[2]) {
+            presenceData.state = `Viewing ${document.querySelector('.cover-username').textContent.trim()}`;
+        } else delete presenceData.state
     } else if (path.startsWith('/feedback')) {
         presenceData.details = 'Browsing feedback section'
         switch (path.split('/')[2]) {
             case 'bugs':
                 presenceData.state = 'Viewing bugs'
-            
+
                 break;
 
             case 'feature-requests':
