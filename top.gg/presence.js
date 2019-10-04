da09b514ae1718fa67d08855ba1dee41759b1b4c
marco_rennmaus <17mh2000@gmail.com>
2019-10-05 00:45:33 +0200

    turns out that i didnt actually need that

--- top.gg/presence.js
+++ top.gg/presence.js
@@ -1,38 +1,3 @@
-//ty to eggsy
-//seems to be required as everyone uses it
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-        return value instanceof P ? value : new P(function (resolve) {
-            resolve(value);
-        });
-    }
-    return new(P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) {
-            try {
-                step(generator.next(value));
-            } catch (e) {
-                reject(e);
-            }
-        }
-
-        function rejected(value) {
-            try {
-                step(generator["throw"](value));
-            } catch (e) {
-                reject(e);
-            }
-        }
-
-        function step(result) {
-            result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-        }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-
-
-
-
 
 
 var presence = new Presence({
@@ -40,7 +5,7 @@ var presence = new Presence({
     mediaKeys: false
 });
 
-presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
+presence.on("UpdateData", async () => {
     console.log("UpdateData");
     console.log(window.location.pathname);
     var presenceData = {};
@@ -182,5 +147,5 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
 
 
     presence.setActivity(presenceData);
-}));
+});
     
