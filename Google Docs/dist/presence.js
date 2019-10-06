48018292a6633700076b4d2c1fc52d2cb4daa2f0
callumok2004 <35779365+callumok2004@users.noreply.github.com>
2019-10-06 20:13:28 +0100

    Update presence.js

--- Google Docs/dist/presence.js
+++ Google Docs/dist/presence.js
@@ -29,6 +29,61 @@ presence.on("UpdateData", () => {
 
     }
 
+    if(window.location.pathname.toLowerCase().includes("/forms")) {
+        if(window.location.pathname.toLowerCase().includes("/edit")) {
+            presenceData.details = "Editing a form:"
+            presenceData.state =  document.title.replace("- Google Forms", "");
+        } else {
+            if(window.location.pathname.toLowerCase() ==="/forms/u/0/") {
+                presenceData.details = "Browsing forms"
+            } else {
+                if(window.location.pathname.toLowerCase() ==="/forms/u/0") {
+                    presenceData.details = "Browsing forms"
+                } else {
+                    presenceData.details = "Viewing a form:"
+                    presenceData.state =  document.title.replace("- Google Forms", "");
+                }
+            }
+        }
+
+    }
+
+    if(window.location.pathname.toLowerCase().includes("/spreadsheets")) {
+        if(window.location.pathname.toLowerCase().includes("/edit")) {
+            presenceData.details = "Editing a spreadsheet:"
+            presenceData.state =  document.title.replace("- Google Sheets", "");
+        } else {
+            if(window.location.pathname.toLowerCase() ==="/spreadsheets/u/0/") {
+                presenceData.details = "Browsing spreadsheets"
+            } else {
+                if(window.location.pathname.toLowerCase() ==="/spreadsheets/u/0") {
+                    presenceData.details = "Browsing spreadsheets"
+                } else {
+                    presenceData.details = "Viewing a spreadsheet:"
+                    presenceData.state =  document.title.replace("- Google Sheets", "");
+                }
+            }
+        }
+
+    }
+    if(window.location.pathname.toLowerCase().includes("/presentation")) {
+        if(window.location.pathname.toLowerCase().includes("/edit")) {
+            presenceData.details = "Editing a slidesheet:"
+            presenceData.state =  document.title.replace("- Google Slides", "");
+        } else {
+            if(window.location.pathname.toLowerCase() ==="/document/u/0/") {
+                presenceData.details = "Browsing slidesheets"
+            } else {
+                if(window.location.pathname.toLowerCase() ==="/presentation/u/0") {
+                    presenceData.details = "Browsing slidesheets"
+                } else {
+                    presenceData.details = "Viewing a slidesheet:"
+                    presenceData.state =  document.title.replace("- Google Slides", "");
+                }
+            }
+        }
+
+    }
 
 
 
