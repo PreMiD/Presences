1521cc2833e1467394986147eac0788ba36ff680
callumok2004 <35779365+callumok2004@users.noreply.github.com>
2019-10-07 16:30:41 +0100

    Create presence.js

---
+++ Slither.io/dist/presence.js
@@ -0,0 +1,16 @@
+var presence = new Presence({
+    clientId: "630783537221468182",
+    mediaKeys: false
+});
+
+presence.on("UpdateData", () => {
+    let presenceData = {
+        largeImageKey: "slitherlogo"
+    };
+
+    let length=document.querySelector("[style=\"opacity: .35;\"]").innerHTML
+    let rank= document.querySelector("[style=\"opacity: .35;\"]").innerHTML
+presenceData.details="Length: "+length;
+presenceData.staet= "Rank: " +rank;
+    presence.setActivity(presenceData);
+});
