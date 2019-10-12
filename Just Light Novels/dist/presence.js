98e2b10c1aaa30bebb6679be0fd26fd7ee59891e
Sun <24259556+sunx2@users.noreply.github.com>
2019-10-13 01:41:11 +0530

    Update presence.js
    fixed capital letters

--- Just Light Novels/dist/presence.js
+++ Just Light Novels/dist/presence.js
@@ -10,7 +10,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     if (document.location.pathname == "/") {
         elapsed =  Math.floor(Date.now() / 1000)
         let presenceData = {
-            details: "browsing Novels",
+            details: "Browsing novels",
             state: "at Homepage",
             //largeImageKey: "banner",
             largeImageKey: "logo",
@@ -25,7 +25,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         var stext = document.location.search.split("=")
         elapsed =  Math.floor(Date.now() / 1000)
         let presenceData = {
-            details: "searching Novels",
+            details: "Searching novels",
             state: `Keyword: ${stext[1].split("+").join(" ").capitalize()}`,
             //largeImageKey: "banner",
             largeImageKey: "logo",
@@ -40,7 +40,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         var stext = document.location.pathname.split("/")
         elapsed =  Math.floor(Date.now() / 1000)
         let presenceData = {
-            details: `searching Novels `,
+            details: `Searching novels `,
             state: `${stext[1].capitalize()}: ${stext[2].split("-").join(" ").capitalize()}`,
             //largeImageKey: "banner",
             largeImageKey: "logo",
@@ -54,7 +54,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     else if(["/reviews/","/ln-fest-series/"].includes(document.location.pathname)){
         elapsed =  Math.floor(Date.now() / 1000)
         let presenceData = {
-            details: `browsing site`,
+            details: `Browsing site`,
             state: `looking at ${document.location.pathname.split("/").join("").split("-").join(" ").capitalize()}`,
             //largeImageKey: "banner",
             largeImageKey: "logo",
@@ -72,7 +72,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
             elapsed =  Math.floor(Date.now() / 1000)
         let presenceData = {
             details: `Reading ${d[3].split("-").join(" ").capitalize()}(${d[1]})`,
-            state: `looking at ${(document.location.hash.length==0)?"Novel":document.location.hash.replace("#",'').capitalize()}`,
+            state: `Looking at ${(document.location.hash.length==0)?"Novel":document.location.hash.replace("#",'').capitalize()}`,
             //largeImageKey: "banner",
             largeImageKey: "logo",
             startTimestamp:elapsed
