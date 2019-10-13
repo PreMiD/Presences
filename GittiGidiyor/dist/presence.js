860542da8f700b2de589ec856387031e70a430bf
Abdulbaki Dursun <eggsydev@outlook.com>
2019-10-13 18:21:35 +0300

    Added, GittiGidiyor, Medium, Lichess and Edited Some Old Files!
    Well everything works really fine, except the Medium thingy, it'll never work "really fine" but it works ~~

--- GittiGidiyor/dist/presence.js
+++ GittiGidiyor/dist/presence.js
@@ -29,45 +29,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 
 const presence = new Presence({
-        clientId: "629650221634158592"
+        clientId: "632914600949710868"
     }),
     pages = {
-        "/siparislerim": "Siparişlerim",
-        "/hesabim": "Hesabım",
-        "/ayagina-gelsin/teslimat-adreslerim": "Teslimat Adreslerim",
-        "/ayagina-gelsin/fatura-adreslerim": "Fatura Adreslerim",
+        "/yildiz-firsatlar": "Yıldız Fırsatlar",
+        "/tum-kampanyalar": "Tüm Kampanyalar",
+        "/cadde": "Cadde",
+        "/nasil-iade-yapilir": "Nasıl İade Yapılır?",
         "/ayagina-gelsin/hediye-ceklerim": "Hedi Çeklerim",
-        "/yorumlarim": "Yorumlarım",
-        "/mesajlarim": "Müşteri Hizmetleri Mesajlarım",
-        "/ayagina-gelsin/sepetim": "Sepet",
-        "/gunun-firsati-teklifi": "Günün Fırsatları",
-        "/hepsiglobal": "Yurt Dışı Alışveriş (Hepsiglobal)",
-        "/kampanyalar": "Kampanyalar",
-        "/girisimci-kadinlar": "Girişimci Kadınlar",
-        "/yardim": "Yardım & Destek",
-        "/hesabim/bize-sorun": "Bize Sorun"
+        "/BanaOzel/satin-aldiklarim.php": "Siparişlerim",
+        "/BanaOzel/sattiklarim.php": "Sattığım Ürünler",
+        "/BanaOzel/satislarim.php": "Satışlarım",
+        "/hesabim/izlediklerim": "İzlediklerim",
+        "/bana-ozel/mesajlarim": "Mesajlarım",
+        "/BanaOzel/favorilerim.php": "Favorilerim",
+        "/BanaOzel/performans_raporu.php": "Performans Raporu",
+        "/BanaOzel/hesaplarim.php": "Hesap Hareketlerim",
+        "/BanaOzel/ayarlarim.php": "Bilgilerim",
+        "/bana-ozel/promosyonlarim": "Promosyonlarım",
+        "/sepetim": "Sepetim"
     };
 
 presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     const page = document.location.pathname,
-        productName = document.querySelector("#product-name"),
-        price = document.querySelector("#offering-price") && document.querySelector("#offering-price").attributes["content"]  && document.querySelector("#offering-price").attributes["content"].textContent != "" ? document.querySelector("#offering-price").attributes["content"].textContent : null,
-        seller = document.querySelector("#productResult > div > div > div > div.col.lg-1.md-1.sm-1.filter-content > section > div > div.title-wrapper.with-bg.for-desktop.brand > h1");
+        productName = document.querySelector("#sp-title") || document.querySelector("#ProductTitle > div.h1-container > h1 > span.title"),
+        price = document.querySelector(".lastPrice") || document.querySelector("#col-center > div > div.border-e3e3e3 > ul.table-ul.gray-content.border-top-e3e3e3 > li.row-li.product-detail-product-price-info-ab-variant > ul > li.col-li.gg-w-17.gg-d-17.gg-t-16.gg-m-16.posr > div:nth-child(2) > div > div.low-price.extra-price.all-price.robotobold.clear.pl8.pt5"),
+        seller = document.querySelector("#store-page-title > div.store-name > h1");
 
     let data = {
-        largeImageKey: "hb-logo",
+        largeImageKey: "gg-logo",
         startTimestamp: Math.floor(Date.now() / 1000)
     };
 
     if (productName && productName.textContent != "") {
         data.details = "Bir ürüne göz atıyor:"
-        data.state = `${productName.textContent.trim()}${price ? " - " + price + " TL" : ""}`;
-    } else if (pages[page] || pages[page.slice(0, -1)]) {
+        data.state = `${productName.textContent}${price ? " - " + price.textContent.trim() : ""}`;
+    } else if (pages[page] || pages[page.slice(0, -1)]) { // We are using slice(0, -1) here to slice the last "/" and check if there's a record on pages variable.
         data.details = "Bir sayfaya göz atıyor:"
         data.state = pages[page] || pages[page.slice(0, -1)];
-    } else if (page.includes("/ara")) {
+    } else if (page.includes("/arama")) {
         data.details = "Bir şey arıyor:"
-        data.state = document.title && document.title.includes(" - Hepsiburada") ? document.title.replace(" - Hepsiburada", "") : "";
+        data.state = document.title && document.title.includes(" - GittiGidiyor") ? document.title.replace(" - GittiGidiyor", "") : "";
         data.smallImageKey = "search";
     } else if (seller && seller.textContent != "") {
         data.details = "Bir mağazaya göz atıyor:"
