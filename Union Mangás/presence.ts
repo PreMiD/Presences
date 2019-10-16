f9f838510fc346fae156c03f0d1728de1b717ac0
Viruz <gustavomelati@gmail.com>
2019-10-16 19:30:17 -0300

    Added new icons and refactored some code

--- Union Mangás/presence.ts
+++ Union Mangás/presence.ts
@@ -1,40 +1,44 @@
-let presence = new Presence({
+const presence = new Presence({
     clientId: "609791567540256780"
 });
 
-let startedBrowsingTimestamp = Math.floor(Date.now() / 1000),
-mangaName : string,
-mangaPage : string,
-mangaChapter : string,
-element : HTMLSelectElement,
-presenceData : presenceData = {
-    largeImageKey: "union_lg",
-    startTimestamp: startedBrowsingTimestamp
-},
+const startTimestamp = Math.floor(Date.now() / 1000),
+{ pathname } = window.location,
 strings = presence.getStrings({
     browsing: "presence.activity.browsing"
 });
 
 presence.on("UpdateData", async () => {
-    if (window.location.pathname.includes("lista-mangas")) {
+    const presenceData : presenceData = {
+        largeImageKey: "union_lg",
+        startTimestamp
+    };
+
+    if (pathname.startsWith(`/lista-mangas`)) {
         presenceData.details = "Procurando um mangá";
-    } else if (window.location.pathname.includes("manga")) {
-        mangaName = document.querySelector("div.col-md-12 > h2").textContent;
-        presenceData.details = "Olhando um mangá";
+        presenceData.smallImageKey = `search`;
+        presenceData.smallImageText = `Procurando`;
+    } else if (pathname.startsWith(`/manga`)) {
+        const mangaName = document.querySelector("div.col-md-12 > h2").textContent;
+        presenceData.details = `Olhando um mangá`;
         presenceData.state = mangaName;
-    } else if (window.location.pathname.includes("leitor")) {
-        mangaName = document.querySelector(".titulo-leitura").textContent.split(' - ')[0];
-        mangaChapter = document.querySelector(".titulo-leitura").textContent.split(' - ')[1];
-        presenceData.details = "Lendo " + mangaName;
-        if (!document.querySelector('#paginas').getAttribute('style').split(';')[1].includes('none')) {
-            element = document.getElementById("paginas") as HTMLSelectElement;
-            mangaPage = element.options[element.selectedIndex].value;
-            presenceData.state = mangaChapter + " - Página " + mangaPage;
+    } else if (pathname.startsWith(`/leitor`)) {
+        const title = document.querySelector(".titulo-leitura").textContent.split(' - ');
+        const mangaName = title[0];
+        const mangaChapter = title[1];
+        presenceData.details = mangaName;
+        if (!document.querySelector('#paginas').getAttribute('style').match(/display:\Wnone/)) {
+            const mangaPage = (document.querySelector(`#paginas`) as HTMLSelectElement).options.selectedIndex + 1;
+            presenceData.state = `${mangaChapter} - Página ${mangaPage}`;
         } else {
             presenceData.state = mangaChapter;
         };
-    } else if (window.location.pathname.includes("scans")) {
-        presenceData.details = "Procurando uma Scan";
+        presenceData.smallImageKey = `reading`;
+        presenceData.smallImageText = `Lendo`;
+    } else if (pathname.startsWith(`/scans`)) {
+        presenceData.details = `Procurando uma Scan`;
+        presenceData.smallImageKey = `search`;
+        presenceData.smallImageText = `Procurando`;
     } else {
         presenceData.details = (await strings).browsing;
     }
