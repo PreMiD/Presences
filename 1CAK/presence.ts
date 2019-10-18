b59c30b1f5ee45c93e45365083c0c0bf6712f6ee
Allvaa <alvamailindo@gmail.com>
2019-10-18 15:13:56 +0700

    presences for some pages

--- 1CAK/presence.ts
+++ 1CAK/presence.ts
@@ -11,10 +11,21 @@ presence.on("UpdateData", async () => {
     presenceData.startTimestamp = startTimestamp;
     if (document.location.pathname === "/") {
         presenceData.details = "Home Page";
+    } else if (document.location.pathname === "/trending") {
+        presenceData.details = "Trending Page";
+    } else if (document.location.pathname === "/recent") {
+        presenceData.details = "Recent Page";
+    } else if (document.location.pathname === "/legendary") {
+        presenceData.details = "Legendary Page";
+    } else if (document.location.pathname === "/friends") {
+        presenceData.details = "Friends Page";
+    } else if (document.location.pathname === "/rules") {
+        presenceData.details = "Reading the rules";
+    } else if (document.location.pathname.slice(1).startsWith("of")) {
+        presenceData.details = document.querySelector("#content > h3").textContent.trim();
     } else if (!isNaN(parseInt(document.location.pathname.slice(1)))) {
         const author = document.querySelector("#content > div > table > tbody > tr > td > div > .blur a > b").textContent.trim();
-        presenceData.details = `Viewing a post from:`;
-        presenceData.state = author;
+        presenceData.details = `Viewing ${author}'s post`;
     } else {
         presenceData = {
             largeImageKey: "logo"
