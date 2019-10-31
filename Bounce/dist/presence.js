9ea5c737ba5f570ad814d0f546eae6df8907a545
callumok2004 <35779365+callumok2004@users.noreply.github.com>
2019-10-31 13:06:33 +0000

    🎉🦄 Bounce
    Recoded from scratch - Uses new API now.

--- Bounce/dist/presence.js
+++ Bounce/dist/presence.js
@@ -1,4 +1,3 @@
-
 var presence = new Presence({
     clientId: "629819064642043925",
     mediaKeys: false
@@ -8,20 +7,21 @@ var browsingStamp = Math.floor(Date.now() / 1000);
 let sartist, strack, slisteners, sdj;
 setInterval(newStats, 6000)
 newStats();
+
 function newStats() {
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      let data = JSON.parse(this.responseText);
-	strack = data.track;
-	sartist = data.artist;
-	sdj = data.dj;
-	slisteners= data.listeners;
-    }
-  };
-  xhttp.open('GET', 'https://wearebounce.net/api/index.php?stats2=1&key=GivemeaccessBitCH!', true);
-  xhttp.send();
-	
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.responseText);
+            strack = data.track;
+            sartist = data.artist;
+            sdj = data.dj;
+            slisteners = data.listeners;
+        }
+    };
+    xhttp.open('GET', 'https://callumdev.wearebounce.net/premidStats', true);
+    xhttp.send();
+
 }
 
 
@@ -32,9 +32,92 @@ presence.on("UpdateData", () => {
     let presenceData = {
         largeImageKey: "bouncelogo"
     };
-	  presenceData.startTimestamp = browsingStamp;
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.smallImageKey = "play";
+    presenceData.details = strack + " - " + sartist
+    presenceData.state = sdj + " - " + slisteners + " listeners"
 
-    if (document.URL.includes("#Bounce.News")) {
+    switch (document.location.hash) {
+        case "Bounce.Home":
+            play = document.querySelector("body > div.web > div > div > div > div > div.player > div.stats-p > div > div.buttons > center > div > i");
+            play = play.className;
+            switch (play) {
+                case "togl fa fa-play":
+                    presenceData.smallImageKey = "pause";
+                    presenceData.details = "Viewing the home page";
+                    break;
+                case "togl fa fa-pause":
+                    presenceData.smallImageKey = "play";
+                    presenceData.details = strack + " - " + sartist
+                    presenceData.state = sdj + " - " + slisteners + " listeners"
+            }
+            break;
+        case "Bounce.Login":
+            presenceData.details = "Logging in"
+    }
+
+
+    if (document.location.hash.includes("Bounce.Timetable")) {
+        let day;
+        switch (document.location.hash.replace("Bounce.Timetable.", "").replace("#", "")) {
+
+            case "1":
+                day = "Monday"
+                break;
+            case "2":
+                day = "Tuesday"
+                break;
+            case "3":
+                day = "Wednesday"
+                break;
+            case "4":
+                day = "Thursday"
+                break;
+            case "5":
+                day = "Friday"
+                break;
+            case "6":
+                day = "Saturday"
+                break;
+            case "7":
+                day = "Sunday"
+                break;
+        }
+        presenceData.details = "Viewing the timetable";
+        presenceData.state = "Day: " + day;
+        presence.setActivity(presenceData)
+
+    }
+    if (document.location.hash.includes("Bounce.BookSlots")) {
+        let day;
+        switch (document.location.hash.replace("Bounce.BookSlots.", "").replace("#", "")) {
+            case "1":
+                day = "Monday"
+                break;
+            case "2":
+                day = "Tuesday"
+                break;
+            case "3":
+                day = "Wednesday"
+                break;
+            case "4":
+                day = "Thursday"
+                break;
+            case "5":
+                day = "Friday"
+                break;
+            case "6":
+                day = "Saturday"
+                break;
+            case "7":
+                day = "Sunday"
+                break;
+        }
+        presenceData.details = "Booking a slot";
+        presenceData.state = "Day: " + day;
+    }
+
+    if (document.location.hash.includes("Bounce.News")) {
         title = document.querySelector("body > div.web > div > div > div > div > div.pageajax > div.leftnews1 > center > b:nth-child(1)");
         presenceData.details = "Reading article:";
         if (title.innerText.length > 128) {
@@ -43,33 +126,6 @@ presence.on("UpdateData", () => {
             presenceData.state = title.innerText;
         }
         presenceData.smallImageKey = "reading";
-        presence.setActivity(presenceData);
-    } else if (document.URL.includes("#Bounce.Timetable")) {
-        presenceData.details = "Viewing the timetable";
-        presence.setActivity(presenceData);
-    } else if (document.URL.includes("#Bounce.Home")) {
-        play = document.querySelector("body > div.web > div > div > div > div > div.player > div.stats-p > div > div.buttons > center > div > i");
-        play = play.className;
-        switch (play) {
-            case "togl fa fa-play":
-presenceData.smallImageKey = "pause";
-                presenceData.details = "Viewing the home page";
-                presence.setActivity(presenceData);
-                break;
-            case "togl fa fa-pause":
-presenceData.smallImageKey = "play";
-	presenceData.details = strack +" - "+ sartist
-	presenceData.state = sdj +" - "+ slisteners + " listeners"
-	presence.setActivity(presenceData)
-
-
-
-
-        }
-    } else {
-	presenceData.smallImageKey = "play";
-	presenceData.details = strack +" - "+ sartist
-	presenceData.state = sdj +" - "+ slisteners + " listeners"
     }
-	presence.setActivity(presenceData)
+    presence.setActivity(presenceData)
 });
