8d67d2ae768df7ebbf60c68e492b70238fc7a760
Bas950 <34792838+Bas950@users.noreply.github.com>
2019-11-02 14:20:39 +0100

    :star: Valtox update, mostly emojis.

--- Valtox Gaming Group/dist/presence.js
+++ Valtox Gaming Group/dist/presence.js
@@ -25,48 +25,73 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     if (document.location.hostname == "valtoxgaminggroup.com") {
         if (document.location.pathname == "/") {
             presenceData.startTimestamp = browsingStamp;
-            presenceData.details = "Viewing home page";
+            presenceData.details = "🌐 Viewing home page";
         }
         else if (document.location.pathname.includes("/profile/")) {
             presenceData.startTimestamp = browsingStamp;
             user = document.querySelector("body > div:nth-child(3) > div > div:nth-child(2) > h2");
-            presenceData.details = "Viewing user:";
-            presenceData.state = user.innerText.replace("Sign out", "").replace(document.querySelector("body > div:nth-child(3) > div > div:nth-child(2) > h2 > span").textContent, "");
+            presenceData.details = "🌐 Viewing user:";
+            presenceData.state = "📰 " + user.innerText.replace("Sign out", "").replace(document.querySelector("body > div:nth-child(3) > div > div:nth-child(2) > h2 > span").textContent, "");
         }
         else if (document.location.pathname.includes("/logistics")) {
             presenceData.startTimestamp = browsingStamp;
-            presenceData.details = "Reading about the logistics";
+            presenceData.details = "📰 Reading about the logistics";
             presenceData.smallImageKey = "reading";
         }
         else if (document.location.pathname.includes("/about")) {
             presenceData.startTimestamp = browsingStamp;
-            presenceData.details = "Reading about Valtox";
+            presenceData.details = "📰 Reading about Valtox";
             presenceData.smallImageKey = "reading";
         }
         else if (document.location.pathname.includes("/fivem")) {
             presenceData.startTimestamp = browsingStamp;
-            presenceData.details = "Reading about";
-            presenceData.state = "Valtox FiveM";
+            presenceData.details = "🌐 Reading about";
+            presenceData.state = "📰 Valtox FiveM";
             presenceData.smallImageKey = "reading";
         }
         else if (document.location.pathname.includes("/minecraft")) {
             presenceData.startTimestamp = browsingStamp;
-            presenceData.details = "Reading about";
-            presenceData.state = "Valtox Minecraft";
+            presenceData.details = "🌐 Reading about";
+            presenceData.state = "📰 Valtox Minecraft";
             presenceData.smallImageKey = "reading";
-        }
-        else if (document.location.pathname.includes("/tag")) {
-            title = document.querySelector("#container > div.page > div.main-wrap > div.profile > section > header > h1");
-            presenceData.startTimestamp = browsingStamp;
-            presenceData.details = "Viewing tag:";
-            presenceData.state = title.innerText;
         }
     }
     else if (document.location.hostname == "vtc.valtoxgaminggroup.com") {
         if (document.location.pathname.includes("/truckinglive")) {
             presenceData.startTimestamp = browsingStamp;
-            presenceData.details = "Viewing the trucking";
-            presenceData.state = "live tracker";
+            presenceData.details = "🌐 Viewing the trucking";
+            presenceData.state = "🌐 live tracker";
+        }
+    }
+    else if (document.location.hostname == "hub.valtoxgaminggroup.com") {
+        if (document.location.pathname.includes("/logbook")) {
+            presenceData.startTimestamp = browsingStamp;
+            presenceData.details = "🌐 Viewing their logbook";
+            presenceData.state = "📰 " + document.querySelector("#jobskm").textContent + " " + document.querySelector("#page-content-wrapper > div > div.row > div:nth-child(1) > div > span.count-name.white").textContent + " | " + document.querySelector("#jobscount").textContent + " " + document.querySelector("#page-content-wrapper > div > div.row > div:nth-child(2) > div > span.count-name").textContent;
+        }
+        else if (document.location.pathname.includes("/downloads")) {
+            presenceData.startTimestamp = browsingStamp;
+            presenceData.details = "🌐 Viewing the";
+            presenceData.state = "📰 downloads page";
+        }
+        else if (document.location.pathname.includes("/events")) {
+            presenceData.startTimestamp = browsingStamp;
+            presenceData.details = "🌐 Viewing the";
+            presenceData.state = "📰 upcoming events";
+        }
+        else if (document.location.pathname.includes("/rules")) {
+            presenceData.startTimestamp = browsingStamp;
+            presenceData.details = "🌐 Viewing the";
+            presenceData.state = "📰 VTC rules";
+        }
+        else if (document.location.pathname.includes("/login")) {
+            presenceData.startTimestamp = browsingStamp;
+            presenceData.details = "🌐 Logging in...";
+        }
+        else {
+            presenceData.startTimestamp = browsingStamp;
+            presenceData.details = "🌐 Viewing their";
+            presenceData.state = "📰 VTC dashboard";
         }
     }
     if (presenceData.details == null) {
