2f36cc55576602015c260ac61ca59a2a141c12bf
Ben <ben@benferch.de>
2019-11-22 22:41:13 +0100

    More simple code

--- WIRED/presence.ts
+++ WIRED/presence.ts
@@ -14,29 +14,21 @@ presence.on("UpdateData", async() => {
         elapsed = Math.floor(Date.now() / 1000);
     }
 
-    if(document.location.pathname === "/") {
-        details = "Browsing: ";
-        state = title;
-    }
-
     if(document.location.pathname.includes("/gallery/") || document.location.pathname.includes("/story/")) {
         details = "Reading: ";
         state = title.replace(" | WIRED", "");
-    }
-
-    if(document.location.pathname === "/video") {
+    } else if(document.location.pathname === "/video") {
         details = "Browsing: ";
         state = title.replace(" | WIRED", "");
-    }
-
-    if(document.location.pathname.includes("/video/watch/")) {
+    } else if(document.location.pathname.includes("/video/watch/")) {
         details = "Watching: ";
         state = title.replace(" | WIRED Video | CNE", "");
-    }
-
-    if(document.location.pathname.includes("/author/") || document.location.pathname.includes("/category/") || document.location.pathname.includes("/coupons") || document.location.pathname.includes("/subscribe/") || document.location.pathname.includes("/wired-advertising/") || document.location.pathname.includes("/sitemap/") || document.location.pathname.includes("/about/") || document.location.pathname.includes("/securedrop/") || document.location.pathname.includes("/newsletter")) {
+    } else if(document.location.pathname.includes("/author/") || document.location.pathname.includes("/category/") || document.location.pathname.includes("/coupons") || document.location.pathname.includes("/subscribe/") || document.location.pathname.includes("/wired-advertising/") || document.location.pathname.includes("/sitemap/") || document.location.pathname.includes("/about/") || document.location.pathname.includes("/securedrop/") || document.location.pathname.includes("/newsletter")) {
         details = "Browsing: ";
         state = title.replace(" | WIRED", "");
+    } else {
+        details = "Browsing: ";
+        state = title;
     }
     
     var data: presenceData = {
