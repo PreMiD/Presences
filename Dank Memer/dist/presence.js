4e491144299e78bf2d5b793a2bee76f437b28c44
eren885 <58482989+eren885@users.noreply.github.com>
2019-12-14 00:57:49 +0300

    Update presence.js

--- Dank Memer/dist/presence.js
+++ Dank Memer/dist/presence.js
@@ -9,27 +9,27 @@ presence.on("UpdateData", () => {
     };
 
     if (window.location.pathname.endsWith("commands")) {
-        presenceData.details = "Wieving a page:";
+        presenceData.details = "Viewing a page:";
      presenceData.state = "Commands"
     }
  else if (window.location.pathname.endsWith("about")) {
-       presenceData.details = "Wieving a page:";
+       presenceData.details = "Viewing a page:";
     presenceData.state = "About"
    }
    else if (window.location.pathname.endsWith("blogs")) {
-         presenceData.details = "Wieving a page:";
+         presenceData.details = "Viewing a page:";
       presenceData.state = "Blogs"
     }
      else if (window.location.pathname.startsWith("/blogs/")) {
-           presenceData.details = "Wieving a blog:";
+           presenceData.details = "Viewing a blog:";
            presenceData.state = document.querySelector("#root > div.psuedoBody > header").textContent
        }
        else if (window.location.pathname.includes("/store/presences/")) {
-             presenceData.details = "Wieving a presence page:";
+             presenceData.details = "Viewing a presence page:";
           presenceData.state = document.querySelector("#app > div.page-wrapper > div.content > div > div > div.fullpresence__header > div.header__title > h1").textContent
          }
          else if (window.location.pathname.endsWith("loot")) {
-               presenceData.details = "Wieving a page:";
+               presenceData.details = "Viewing a page:";
             presenceData.state = "Lootboxes"
            }
 
