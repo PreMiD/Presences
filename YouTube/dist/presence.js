39d7416e37b790ff4679c25050531d361fcb8b66
Bas950 <34792838+Bas950@users.noreply.github.com>
2019-12-14 21:32:55 +0100

    Half youtube recode by me and doom

--- YouTube/dist/presence.js
+++ YouTube/dist/presence.js
@@ -8,23 +8,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 var presence = new Presence({
     clientId: "463097721130188830",
-    mediaKeys: true
+    mediaKeys: false
 }), strings = presence.getStrings({
     play: "presence.playback.playing",
     pause: "presence.playback.paused",
     live: "presence.activity.live"
 });
 var pattern = "•";
-var truncateAfter = function (str, pattern) {
+function truncateAfter(str, pattern) {
     return str.slice(0, str.indexOf(pattern));
-};
+}
 presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     var video = document.querySelector(".video-stream");
     if (video !== null && !isNaN(video.duration)) {
-        var oldYouTube = null;
-        var YouTubeTV = null;
-        var YouTubeEmbed = null;
-        var title;
+        var oldYouTube = null, YouTubeTV = null, YouTubeEmbed = null, title;
         document.querySelector(".watch-title") !== null
             ? (oldYouTube = true)
             : (oldYouTube = false);
@@ -34,47 +31,37 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         document.location.pathname.includes("/embed")
             ? (YouTubeEmbed = true)
             : (YouTubeEmbed = false);
-        if (!oldYouTube && !YouTubeTV) {
-            if (YouTubeEmbed) {
-                title = document.querySelector("div.ytp-title-text > a");
-            }
-            else {
-                title =
-                    document.location.pathname !== "/watch"
-                        ? document.querySelector(".ytd-miniplayer .title")
-                        : document.querySelector("h1 yt-formatted-string.ytd-video-primary-info-renderer");
-            }
-        }
-        else {
-            if (oldYouTube) {
-                if (document.location.pathname == "/watch")
-                    title = document.querySelector(".watch-title");
-            }
-            else if (YouTubeTV) {
-                title = document.querySelector(".player-video-title");
-            }
-        }
+        YouTubeEmbed
+            ? title = document.querySelector("div.ytp-title-text > a")
+            : (oldYouTube && document.location.pathname.includes("/watch"))
+                ? title = document.querySelector(".watch-title")
+                : YouTubeTV
+                    ? title = document.querySelector(".player-video-title")
+                    : (!document.location.pathname.includes("/watch"))
+                        ? title = document.querySelector(".ytd-miniplayer .title")
+                        : title = document.querySelector("h1 yt-formatted-string.ytd-video-primary-info-renderer");
         var uploaderTV, uploaderMiniPlayer, uploader2, edited, uploaderEmbed;
-        edited = false;
-        uploaderTV = document.querySelector(".player-video-details") || document.querySelector(".ytd-channel-name");
-        uploaderEmbed = document.querySelector("div.ytp-title-expanded-heading > h2 > a");
-        uploaderMiniPlayer = document.querySelector("yt-formatted-string#owner-name");
-        if (uploaderMiniPlayer !== null) {
-            if (uploaderMiniPlayer.innerText == "YouTube") {
-                edited = true;
-                uploaderMiniPlayer.setAttribute("premid-value", "Listening to a playlist");
-            }
+        edited = false,
+            uploaderTV = document.querySelector(".player-video-details") || document.querySelector(".ytd-channel-name"),
+            uploaderEmbed = document.querySelector("div.ytp-title-expanded-heading > h2 > a"),
+            uploaderMiniPlayer = document.querySelector("yt-formatted-string#owner-name"),
+            uploader2 = document.querySelector("#owner-name a");
+        if (uploaderMiniPlayer != null && uploaderMiniPlayer.textContent == "YouTube") {
+            edited = true;
+            uploaderMiniPlayer.setAttribute("premid-value", "Listening to a playlist");
         }
-        uploader2 = document.querySelector("#owner-name a");
-        var uploader = uploaderMiniPlayer !== null && uploaderMiniPlayer.innerText.length > 0
+        var uploader = uploaderMiniPlayer !== null && uploaderMiniPlayer.textContent.length > 0
             ? uploaderMiniPlayer
-            : uploader2 !== null && uploader2.innerText.length > 0
+            : uploader2 !== null && uploader2.textContent.length > 0
                 ? uploader2
                 : document.querySelector("#upload-info yt-formatted-string.ytd-channel-name a") !== null
                     ? document.querySelector("#upload-info yt-formatted-string.ytd-channel-name a")
-                    : uploaderEmbed !== null && YouTubeEmbed && uploaderEmbed.innerText.length > 0
+                    : uploaderEmbed !== null && YouTubeEmbed && uploaderEmbed.textContent.length > 0
                         ? uploaderEmbed
-                        : uploaderTV = truncateAfter(uploaderTV.textContent.replace(/\s+/g, ''), pattern), timestamps = getTimestamps(Math.floor(video.currentTime), Math.floor(video.duration)), live = Boolean(document.querySelector(".ytp-live")), ads = Boolean(document.querySelector(".ytp-ad-player-overlay")), presenceData = {
+                        : uploaderTV = truncateAfter(uploaderTV.textContent.replace(/\s+/g, ''), pattern);
+        var timestamps = getTimestamps(Math.floor(video.currentTime), Math.floor(video.duration));
+        var live = Boolean(document.querySelector(".ytp-live")), ads = Boolean(document.querySelector(".ytp-ad-player-overlay"));
+        var presenceData = {
             details: title.textContent.replace(/\s+/g, '') == "" ? document.querySelector("div.ytp-title-text > a").textContent : title.textContent,
             state: edited == true
                 ? uploaderMiniPlayer.getAttribute("premid-value")
@@ -89,7 +76,11 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
             startTimestamp: timestamps[0],
             endTimestamp: timestamps[1]
         };
-        presence.setTrayTitle(video.paused ? "" : title.innerText);
+        presence.setTrayTitle(video.paused
+            ? ""
+            : title == null
+                ? document.querySelector(".title.style-scope.ytd-video-primary-info-renderer").textContent
+                : title.textContent);
         if (video.paused || live) {
             delete presenceData.startTimestamp;
             delete presenceData.endTimestamp;
@@ -98,24 +89,25 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
                 presenceData.smallImageText = (yield strings).live;
             }
         }
-        if (uploader == null && document.querySelector(".style-scope.ytd-channel-name > a") !== null) {
-            uploader = document.querySelector(".style-scope.ytd-channel-name > a");
-            presenceData.state = uploader.textContent;
-        }
         if (title == null && document.querySelector(".title.style-scope.ytd-video-primary-info-renderer") !== null) {
-            title = document.querySelector(".title.style-scope.ytd-video-primary-info-renderer");
-            presenceData.details = title.textContent;
+            presenceData.details = document.querySelector(".title.style-scope.ytd-video-primary-info-renderer").textContent;
+        }
+        if (uploader == null && document.querySelector(".style-scope.ytd-channel-name > a") !== null) {
+            presenceData.state = document.querySelector(".style-scope.ytd-channel-name > a").textContent;
         }
         if (ads) {
             presenceData.details = "Currently watching an ad";
             delete presenceData.state;
         }
-        uploader = uploader; 
-        if (title !== null && uploader !== null) {
+        if (presenceData.details == null) {
+            presence.setTrayTitle();
+            presence.setActivity();
+        }
+        else {
             presence.setActivity(presenceData);
         }
     }
-    else if (document.location.hostname == "www.youtube.com") {
+    else if (document.location.hostname == "www.youtube.com" || document.location.hostname == "youtube.com") {
         let presenceData = {
             largeImageKey: "yt_lg"
         };
@@ -134,12 +126,9 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         }
         else if (document.location.pathname.includes("/channel") || document.location.pathname.includes("/user")) {
             user = document.querySelector(".ytd-channel-name").textContent.replace(/\s+/g, '');
-            if (user == "" || user == "‌") {
-                user = "null";
-            }
-            else {
-                user = document.querySelector(".ytd-channel-name").textContent;
-            }
+            (user == "" || user == "‌")
+                ? user = "null"
+                : user = document.querySelector(".ytd-channel-name").textContent;
             if (document.location.pathname.includes("/videos")) {
                 presenceData.details = "Browsing through videos";
                 presenceData.state = "of channel: " + user;
@@ -204,7 +193,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
             if (title == null) {
                 title = document.querySelector("#title > yt-formatted-string > a");
             }
-            presenceData.state = title.innerText;
+            presenceData.state = title.textContent;
             presenceData.startTimestamp = browsingStamp;
         }
         else if (document.location.pathname.includes("/premium")) {
@@ -230,7 +219,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         else if (document.location.pathname.includes("/intl")) {
             presenceData.details = "Reading about:";
             title = document.querySelector("head > title");
-            presenceData.state = title.innerText.replace(" - YouTube", "");
+            presenceData.state = title.textContent.replace(" - YouTube", "");
             presenceData.smallImageKey = "reading";
             presenceData.startTimestamp = browsingStamp;
         }
@@ -285,19 +274,19 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
             presenceData.startTimestamp = browsingStamp;
             if (document.location.pathname.includes("/edit")) {
                 presenceData.details = "Editing video:";
-                presenceData.state = title.innerText;
+                presenceData.state = title.textContent;
             }
             else if (document.location.pathname.includes("/analytics")) {
                 presenceData.details = "Viewing analytics of video:";
-                presenceData.state = title.innerText;
+                presenceData.state = title.textContent;
             }
             else if (document.location.pathname.includes("/comments")) {
                 presenceData.details = "Viewing comments of video:";
-                presenceData.state = title.innerText;
+                presenceData.state = title.textContent;
             }
             else if (document.location.pathname.includes("/translations")) {
                 presenceData.details = "Viewing translations of video:";
-                presenceData.state = title.innerText;
+                presenceData.state = title.textContent;
             }
         }
         else if (document.location.pathname.includes("/analytics")) {
@@ -333,20 +322,6 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         }
     }
 }));
-presence.on("MediaKeys", (key) => {
-    switch (key) {
-        case "pause":
-            var video = document.querySelector(".video-stream");
-            video.paused ? video.play() : video.pause();
-            break;
-        case "nextTrack":
-            document.querySelector(".ytp-next-button").click();
-            break;
-        case "previousTrack":
-            document.querySelector(".ytp-prev-button").click();
-            break;
-    }
-});
 function getTimestamps(videoTime, videoDuration) {
     var startTime = Date.now();
     var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
