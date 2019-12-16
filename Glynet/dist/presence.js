0fdb6cbcdd52eea952342979373b9d08f68034a9
Arda Soytürk <vardasoyturk@gmail.com>
2019-12-16 07:51:39 +0300

    Removed the awaiter.

--- Glynet/dist/presence.js
+++ Glynet/dist/presence.js
@@ -1,38 +1,8 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-        return value instanceof P ? value : new P(function (resolve) {
-            resolve(value);
-        });
-    }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) {
-            try {
-                step(generator.next(value));
-            } catch (e) {
-                reject(e);
-            }
-        }
-
-        function rejected(value) {
-            try {
-                step(generator["throw"](value));
-            } catch (e) {
-                reject(e);
-            }
-        }
-
-        function step(result) {
-            result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-        }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-
 const presence = new Presence({
     clientId: "655480486046466098"
 });
 
-presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
+presence.on("UpdateData", () => {
     const page = document.location.pathname;
     const browsingStamp = Math.floor(Date.now() / 1000);
 
