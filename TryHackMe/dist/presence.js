58b5e44753105bd9156cd6d11799d990f26268e4
Dan <dan@sherlock-security.com>
2019-12-18 15:31:38 +0000

    Double brainlet fix

--- TryHackMe/dist/presence.js
+++ TryHackMe/dist/presence.js
@@ -46,8 +46,6 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     else if (document.location.pathname == "/leaderboards") {
         presenceData.details = "Checking the leaderboards!";
     }
-    else if (document.location.pathname == "") {
-    }
     else {
         presenceData.details = "Breaking stuff!";
     }
