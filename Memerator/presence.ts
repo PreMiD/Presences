2e8be4e66ddab424559604a45a2884f312576f60
Chew <chew@chew.pw>
2019-12-20 08:57:32 -0600

    remove comments and small image key

--- Memerator/presence.ts
+++ Memerator/presence.ts
@@ -1,29 +1,19 @@
 var presence = new Presence({
-    clientId: "657402289132273668", //The client ID of the Application created at https://discordapp.com/developers/applications
-    mediaKeys: false //Enable use and detection of media key presses
+    clientId: "657402289132273668",
+    mediaKeys: false
 }),
 
 strings = presence.getStrings({
     play: "presence.playback.playing",
     pause: "presence.playback.paused"
-    //You can use this to get translated strings
 });
 
 
 
 presence.on("UpdateData", async () => {
-    /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
-
-    It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
 
     var presenceData = {
-        largeImageKey: "memerator", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-        smallImageKey: "memerator", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-        //smallImageText: "Some hover text", //The text which is displayed when hovering over the small image
-        //details: "Browsing Page Name", //The upper section of the presence text
-        //state: "Reading section A", //The lower section of the presence text
-        //startTimestamp: 1577232000, //The unix epoch timestamp for when to start counting from
-        //endTimestamp: 1577151472000 //If you want to show Time Left instead of Elapsed, this is the unix epoch timestamp at which the timer ends
+        largeImageKey: "memerator",
     };
 
       if (document.location.pathname == "/") {
@@ -95,14 +85,11 @@ presence.on("UpdateData", async () => {
         presenceData.details = "Viewing the Pro Page";
       }
 
-     /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
 
     if (presenceData.details == null) {
-        //This will fire if you do not set presence details
-        presence.setTrayTitle(); //Clears the tray title for mac users
-        presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+        presence.setTrayTitle();
+        presence.setActivity();
     } else {
-        //This will fire if you set presence details
-        presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+        presence.setActivity(presenceData);
     }
 });
