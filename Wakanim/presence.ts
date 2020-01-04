4fba7a493e590ad0b21de7331259ca77b5001b0b
MrSpinne <43505515+MrSpinne@users.noreply.github.com>
2020-01-05 00:58:22 +0100

    Updated

--- Wakanim/presence.ts
+++ Wakanim/presence.ts
@@ -1,15 +1,15 @@
 var presence, strings, browsingStamp;
-
 presence = new Presence({
     clientId: "662841394171346955",
     mediaKeys: false,
 });
 strings = presence.getStrings({
-    playing: "presence.playback.playing",
+    browsing: "presence.activity.browsing",
     paused: "presence.playback.paused",
-    browsing: "presence.activity.browsing"
+    playing: "presence.playback.playing",
 });
 browsingStamp = Math.floor(Date.now()/1000);
+
 
 presence.on("UpdateData", async () => {
     var presenceData: presenceData = {largeImageKey: "wakanim"};
@@ -25,9 +25,8 @@ presence.on("UpdateData", async () => {
             presenceData.smallImageKey = "paused";
             presenceData.smallImageText = (await strings).paused;
         } else {
-            var timestamps = getTimestamps(video.currentTime, video.duration);
-            presenceData.startTimestamp = timestamps[0];
-            presenceData.endTimestamp = timestamps[1];
+            presenceData.startTimestamp = Math.floor(Date.now()/1000);
+            presenceData.endTimestamp = Math.floor(presenceData.startTimestamp + (video.duration - video.currentTime));
             presenceData.smallImageKey = "playing";
             presenceData.smallImageText = (await strings).playing;
         }
@@ -44,18 +43,11 @@ presence.on("UpdateData", async () => {
     }
 });
 
-function getTimestamps(videoTime: number, videoDuration: number) {
-    var startTime = Math.floor(Date.now()/1000);
-    var endTime = Math.floor(startTime + (videoDuration - videoTime));
-
-    return [startTime, endTime];
-}
-
-function capitalize(text) {
-    text = text.toLowerCase().split(" ");
+const capitalize = function(str) {
+    var text = str.toLowerCase().split(" ");
     for (var i = 0, x = text.length; i < x; i++) {
         text[i] = text[i][0].toUpperCase() + text[i].substr(1);
     }
 
     return text.join(" ");
-}
+};
