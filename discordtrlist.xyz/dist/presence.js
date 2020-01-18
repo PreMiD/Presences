d0f56a3b73da49ef4bb2d10b86004fa2d806df40
samet99 <41552216+samet99@users.noreply.github.com>
2020-01-18 17:21:41 +0000

    update presence.js

--- discordtrlist.xyz/dist/presence.js
+++ discordtrlist.xyz/dist/presence.js
@@ -1,38 +1,44 @@
 var presence = new Presence({
     clientId: "668013997760708618",
     mediaKeys: false
+}), strings = presence.getStrings({
+    play: "presence.playback.playing",
+    pause: "presence.playback.paused"
 });
+var browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
     let presenceData = {
-        largeImageKey: "logo"
-    };
+        largeImageKey: "__logo"
+};
+    if (document.location.hostname == "discordtrlist.xyz") {
+        presenceData.details = "Görültülenen sayfa:";
+        presenceData.state = ('Anasayfa')
 
-    const browsingStamp = Math.floor(Date.now() / 1000);
-    const page = document.location.pathname;
+    if (document.location.pathname.includes("/api")) {
+          presenceData.details = "Görültülenen Sayfa:";
+          presenceData.state = ('Api sayfası')
 
-    if (window.location.pathname.endsWith("botlar")) {
-        presenceData.details = "Tüm botlara göz atıyor.";
-        presenceData.startTimestamp = browsingStamp;
-    }
-else if (window.location.pathname.endsWith("")) {
-        presenceData.details = "Anasayfada Geziniyor.";
-        presenceData.startTimestamp = browsingStamp;
-    }
-    else if (window.location.pathname.endsWith("api")) {
-        presenceData.details = "Api dökümasyonlarına bakıyor.";
-        presenceData.startTimestamp = browsingStamp;
-    }
-    else if (window.location.pathname.endsWith("sertifika")) {
-        presenceData.details = "Sertifika kurallarına bakıyor.";
-        presenceData.startTimestamp = browsingStamp;
-    } else if (window.location.pathname.endsWith("kurucu")) {
-        presenceData.details = "Kurucu panelde bakıyor.";
-        presenceData.startTimestamp = browsingStamp;
-    } else if (window.location.pathname.endsWith("yetkili")) {
-        presenceData.details = "Yetkili panele bakıyor..";
-        presenceData.startTimestamp = browsingStamp;
-    }
+    } else if (document.location.pathname.includes("/profil/")) {
+        var priceEls = document.getElementsByClassName("uname");
+        for (var i = 0; i < priceEls.length; i++) {
+         var profilename = priceEls[i].innerText;
+            presenceData.details = "Bir kişinin profiline bakıyor:";
+            presenceData.state = profilename
+        }
+    } else if (document.location.pathname.includes("/botekle")) {
+            presenceData.details = "Görültülenen Sayfa:";    
+            presenceData.state = "Bot ekleme sayfası";
+
+    } else if (document.location.pathname.includes("/bot/")) {
+        var priceEls = document.getElementsByClassName("ubot");
+        for (var i = 0; i < priceEls.length; i++) {
+         var botname = priceEls[i].innerText;
+            presenceData.details = "Bir bot görüntüleniyor:";
+            presenceData.state = botname
+        }
+                    }
+        }
     if (presenceData.details == null) {
         presence.setTrayTitle();
         presence.setActivity();
@@ -41,3 +47,8 @@ else if (window.location.pathname.endsWith("")) {
         presence.setActivity(presenceData);
     }
 });
+function getTimestamps(videoTime, videoDuration) {
+    var startTime = Date.now();
+    var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+    return [Math.floor(startTime / 1000), endTime];
+}
