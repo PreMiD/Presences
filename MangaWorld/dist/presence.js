d83a6a513eb431654d95dca9680b666b19fde88e
Tellen Official <59364733+TellenOfficial@users.noreply.github.com>
2020-01-18 20:47:05 +0100

    📝 Updated and added functions !

--- MangaWorld/dist/presence.js
+++ MangaWorld/dist/presence.js
@@ -20,7 +20,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     data.details = "Navigando...",
     data.startTimestamp = browsingStamp;
     presence.setActivity(data);
-    }
+	}
     else if (document.location.pathname.endsWith("/contact/")) {
     data.details = "Guardando i Termini e Condizioni",
     data.startTimestamp = browsingStamp;
@@ -31,30 +31,35 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
     data.startTimestamp = browsingStamp;
     presence.setActivity(data);
     }
-    else if (document.location.pathname.startsWith("/user-settings/")) {
-    data.details = "Nel suo profilo",
+    else if (document.location.href.endsWith("/user-settings/")) {
+	data.details = "Nel suo profilo",
     data.startTimestamp = browsingStamp;
     presence.setActivity(data);
     }
-    else if (document.location.pathname.endsWith("/listing-simple-list/")) {
-    data.details = "Sfogliando le Novità",
-    data.startTimestamp = browsingStamp;
+	else if (document.location.href.endsWith("?tab=bookmark")) {
+	data.details = "Nei suoi Preferiti",
+	data.startTimestamp = browsingStamp;
     presence.setActivity(data);
     }
-    else if (document.location.pathname.startsWith("/manga/")) {
-		var mangauno = document.querySelector("head > title").textContent;
-		 if (document.location.pathname.match("/capitolo")) {
-    data.details = "Legge il Manga:",
-	data.state = mangauno.replace("MangaWorld","").replace(/–/g, ""),
+	else if (document.location.href.endsWith("?tab=history")) {
+    data.details = "Nella sua Cronologia",
 	data.startTimestamp = browsingStamp;
     presence.setActivity(data);
 	}
-    else {
-    data.details = "Visualizza il Manga:",
-	data.state = mangauno.replace("MangaWorld","").replace(/–/g, ""),
+	else if (document.location.href.endsWith("?tab=reader-settings")) {
+    data.details = "Nelle sue Opzioni",
 	data.startTimestamp = browsingStamp;
     presence.setActivity(data);
-			}
+	}
+	else if (document.location.href.endsWith("?tab=account-settings")) {
+    data.details = "Nelle sue Impostazioni",
+	data.startTimestamp = browsingStamp;
+    presence.setActivity(data);
+	}
+    else if (document.location.pathname.endsWith("/listing-simple-list/")) {
+    data.details = "Sfogliando le Ultime Uscite",
+    data.startTimestamp = browsingStamp;
+    presence.setActivity(data);
 	}
     else if (document.location.pathname.startsWith("/manga-genre/")) {
 	var mangaexploring = document.querySelector("head > title").textContent;
@@ -62,6 +67,48 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
 	data.state = mangaexploring.replace("MangaWorld","").replace(/–/g, ""),
     data.startTimestamp = browsingStamp;
     presence.setActivity(data);
-    }
-    ;
+	}
+    else if (document.location.pathname.startsWith("/manga-release/")) {
+	var mangarelease = document.querySelector("head > title").textContent;
+    data.details = "Naviga nei manga del " + mangarelease.replace("MangaWorld","").replace(/–/g, ""),
+    data.startTimestamp = browsingStamp;
+    presence.setActivity(data);
+	}
+    else if (document.location.pathname.startsWith("/manga/")) {
+	var mangauno = document.querySelector("head > title").textContent;
+	var mangadue = mangauno.replace(" MangaWorld","").replace( /–/g, "");
+	if (document.location.pathname.match("/capitolo")) {
+	var capitolouno = document.querySelector("meta[property='og:title']").getAttribute("content");
+	var capitolodue = capitolouno.replace(mangadue, "").replace(/-/g, "").replace("MangaWorld", "").replace("Capitolo", "Capitolo: ");
+    data.details = "Legge il Manga: " + mangadue,
+	data.state = capitolodue,
+	data.startTimestamp = browsingStamp;
+    presence.setActivity(data);
+	}
+    else if (document.location.href.endsWith("/?m_orderby=trending")) {
+    data.details = "Visualizza le Tendenze",
+	data.startTimestamp = browsingStamp;
+    presence.setActivity(data);
+	}
+    else {
+    data.details = "Visualizza il Manga:",
+	data.state = mangadue,
+	data.startTimestamp = browsingStamp;
+    presence.setActivity(data);
+	}
+	}
+    else if (document.location.pathname.startsWith("/manga-artist/")) {
+	var mangaartist = document.querySelector("head > title").textContent;
+    data.details = "Visualizza l'Artista:",
+	data.state = mangaartist.replace("MangaWorld","").replace(/–/g, ""),
+    data.startTimestamp = browsingStamp;
+    presence.setActivity(data);
+	}
+    else if (document.location.pathname.startsWith("/manga-author/")) {
+	var mangaauthor = document.querySelector("head > title").textContent;
+    data.details = "Visualizza l'Autore:",
+	data.state = mangaauthor.replace("MangaWorld","").replace(/–/g, ""),
+    data.startTimestamp = browsingStamp;
+    presence.setActivity(data);
+    };
 }));
