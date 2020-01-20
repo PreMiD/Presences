4b9cd13d2c17da51aa79f3d7cc02a98d476bacca
Chezzer <46093784+Chezzer-ub@users.noreply.github.com>
2020-01-20 17:48:55 +1300

    Final update, hopefully.

--- bargrooves/dist/presence.js
+++ bargrooves/dist/presence.js
@@ -9,14 +9,14 @@ presence.on("UpdateData", () => {
         smallImageKey: "logo-square",
         smallImageText: "bargrooves.live",
         spectateSecret: "MTIzNDV8MTIzNDV8MTMyNDU0",
-        details: "null",
-        state: "null",
+        details: "Bargrooves.live",
+        state: "Luxury House Music",
         startTimestamp: 0,
         endTimestamp: 0
     };
 
     if (window.location.pathname == "/") {
-        if (document.getElementById("play-pause-i").classList[2] == 'fa-pause') {
+        if (document.getElementById("play-pause-i").classList.value.includes("fa-pause")) {
             presenceData.details = "Listening to the radio";
             presenceData.state = document.getElementById("song").innerHTML + " - " + document.getElementById("artist").innerHTML;
             presenceData.smallImageKey = "play";
@@ -26,14 +26,14 @@ presence.on("UpdateData", () => {
             presenceData.smallImageKey = "pause";
         }
     } else if (window.location.pathname == "/player/") {
-        if (document.getElementsByClassName("play-toggle")[0].classList[1] == "fa-pause") {
+        if (document.getElementsByClassName("play-toggle")[0].classList.value.includes("fa-play")) {
+            presenceData.details = "Browsing the player";
+            presenceData.state = 'Looking at ' + document.title.replace("Bargrooves FM - ", "");
+            presenceData.smallImageKey = "pause";
+        } else {
             presenceData.details = "Listening to the player";
             presenceData.state = document.getElementById("csong").innerHTML + " - " + document.getElementById("cartist").innerHTML;
             presenceData.smallImageKey = "play";
-        } else {
-            presenceData.details = "Browsing the player";
-            presenceData.state = 'Looking at' + document.title.replace("Bargrooves FM - ", "");
-            presenceData.smallImageKey = "pause";
         }
     }
 
