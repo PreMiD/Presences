5fe88a3e8a18ee1db7e44fc5afcb33705e736a63
Tellen Official <59364733+TellenOfficial@users.noreply.github.com>
2020-01-22 13:28:28 +0100

    📝 Update Presence for AnimeSaturn

--- AnimeSaturn/presence.ts
+++ AnimeSaturn/presence.ts
@@ -50,13 +50,23 @@ data.state = animeviewer.replace("AnimeSaturn - ","").replace(" Streaming SUB IT
 data.startTimestamp = browsingStamp;
 presence.setActivity(data);
 }
+}
 else if (document.location.pathname.match("/ep/")) {
 var animefwt1 = document.querySelector("head > title").textContent;
 var animefwt2 = animefwt1.replace("AnimeSaturn - ","").split(" Episodio")[0];
 var animefwe = animefwt1.replace(animefwt2, "").replace("AnimeSaturn - ", "").replace("Episodio ", "").replace(" Streaming SUB ITA e ITA", "");
+localStorage.setItem("Anime", animefwt2);
+localStorage.setItem("Episode", animefwe);
+data.details = "Sta per guardare: " + animefwt2,
+data.state = "Episodio: " + animefwe,
+data.startTimestamp = browsingStamp;
+presence.setActivity(data);
+} else if (document.location.pathname.match("/watch")) {
+var animefwt2 = localStorage.getItem("Anime");
+var animefwe = localStorage.getItem("Episode");
 data.details = "Sta guardando: " + animefwt2,
 data.state = "Episodio: " + animefwe,
 data.startTimestamp = browsingStamp;
 presence.setActivity(data);
-};
+}
 }));
