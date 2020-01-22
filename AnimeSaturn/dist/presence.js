61ecebccff7b4ddb916ca0a4735fe086f31d4166
Tellen Official <59364733+TellenOfficial@users.noreply.github.com>
2020-01-22 21:12:53 +0100

    Update presence.js

--- AnimeSaturn/dist/presence.js
+++ AnimeSaturn/dist/presence.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var presence = new Presence({
     clientId: "669254632400355358",
     mediaKeys: false
