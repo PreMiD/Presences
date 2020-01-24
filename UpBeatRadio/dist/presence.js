192f136111bda86ba854bc33335efff0d5d916b5
Chezzer <46093784+Chezzer-ub@users.noreply.github.com>
2020-01-24 16:20:22 +1300

    Fix staff panel pages
    Developer at UpBeat, and we don't want the staff pages being on our public discord profile.

--- UpBeatRadio/dist/presence.js
+++ UpBeatRadio/dist/presence.js
@@ -9,24 +9,29 @@ presence.on("UpdateData", () => {
     }
 
     let pageName = (document.title).slice(9);
-	
-    if (window.location.pathname.includes("News.Article")) {
-        presenceData.details = "Reading an article";
-
-        let newsArticleName = (document.title).slice(9);
-        presenceData.state = newsArticleName;
-    }  else if(pageName === "Home") {
-        let songArtist = document.querySelector(".stats-artist").innerHTML;
-        let songName = document.querySelector(".stats-song").innerHTML;
-
-        presenceData.details = songName;
-        presenceData.state = songArtist;
-        
+    
+    if (window.location.pathname.includes("/staff/")) {
+        presenceData.details = "On staff panel.";
+        presenceData.state = "Browsing protected content.";
     } else {
-        presenceData.details = "Viewing a page";
-        pageName = (document.title).slice(9);
-        presenceData.state = pageName;
-      }
+        if (window.location.pathname.includes("News.Article")) {
+            presenceData.details = "Reading an article";
+
+            let newsArticleName = (document.title).slice(9);
+            presenceData.state = newsArticleName;
+        }  else if(pageName === "Home") {
+            let songArtist = document.querySelector(".stats-artist").innerHTML;
+            let songName = document.querySelector(".stats-song").innerHTML;
+
+            presenceData.details = songName;
+            presenceData.state = songArtist;
+
+        } else {
+            presenceData.details = "Viewing a page";
+            pageName = (document.title).slice(9);
+            presenceData.state = pageName;
+        }
+    }   
 	  
       presence.setActivity(presenceData);
       presence.setTrayTitle();
