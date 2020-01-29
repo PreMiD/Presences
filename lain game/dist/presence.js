9628e7bc3b764e4d076d7fddec9e876fc12a2966
frozaum <finnzzingames758@gmail.com>
2020-01-29 20:24:51 -0300

    welp

--- lain game/dist/presence.js
+++ lain game/dist/presence.js
@@ -15,7 +15,7 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         largeImageKey: "logo"
     };
 
-    if (url == "0" || document.location.pathname == "/index.php") {
+    if (url == "0") {
         presenceData.startTimestamp = browsingStamp;
         presenceData.details = "viewing site A";
     }
@@ -27,13 +27,13 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         presenceData.startTimestamp = browsingStamp;
         presenceData.details = "viewing an file:";
         presenceData.state = document.querySelector("body > center > table > tbody > tr:nth-child(1) > td.ta4d01 > table > tbody > tr:nth-child(1) > td.ta4d2 > a:nth-child(2)").textContent;
-       }
-       else if (new URLSearchParams(window.location.search).has('tag')) {
+    }
+    else if (new URLSearchParams(window.location.search).has('tag')) {
         presenceData.startTimestamp = browsingStamp;
         presenceData.details = "viewing an tag:";
         presenceData.state = document.querySelector("body > center > h1").textContent;
-       }
-    else if (document.location.pathname == "/" || (document.location.pathname == "/index.html")) {
+    }
+       else if (document.location.pathname == "/" || (document.location.pathname == "/index.html")) {
         presenceData.startTimestamp = browsingStamp;
         presenceData.details = "index";
     }
