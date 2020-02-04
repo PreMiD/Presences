2ea65305b6fb36a11f9c1f6de06b70c7a3bcbd74
ModBotStudio <55330408+ModBotStudio@users.noreply.github.com>
2020-02-04 16:00:27 +0000

    Divine Discord Bot List

---
+++ divinediscordbots/dist/presence.js
@@ -0,0 +1,44 @@
+var presence = new Presence({
+    clientId: "668933003434655781",
+    mediaKeys: false
+}), strings = presence.getStrings({
+    play: "presence.playback.playing",
+    pause: "presence.playback.paused"
+});
+var browsingStamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", () => {
+    let presenceData = {
+        largeImageKey: "logo"
+    };
+
+    if (document.location.pathname.includes("/bot/")) {
+        presenceData.details = `Viewing A Discord Bot:`; 
+        presenceData.state =  document.querySelector("#main-container > div.hero-static > div > div > div > center > h1 > span").textContent
+    } else if (document.location.pathname.includes("/user/")) {
+        presenceData.details = `Viewing a profile:`; 
+        presenceData.state =  document.querySelector("#main-container > div.hero-static > div > div > div > h1 > span.white-white").textContent
+    } else if (document.location.pathname.includes("/api")) {
+        presenceData.details = `Viewing Page:`; 
+        presenceData.state =  `Devine Discord Bots Api`;
+    } else if (document.location.pathname.includes("/add/bot")) {
+        presenceData.details = `Adding Discord Bot`; 
+    } else {
+        presenceData.details = `Viewing Page:`;
+        presenceData.state = `Discord Bot List`;
+    }
+    
+    if (presenceData.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    }
+    else {
+        presence.setActivity(presenceData);
+    }
+
+});
+function getTimestamps(videoTime, videoDuration) {
+    var startTime = Date.now();
+    var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+    return [Math.floor(startTime / 1000), endTime];
+}
