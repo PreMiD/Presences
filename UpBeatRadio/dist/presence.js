fc039bc344dfef4d27c468a97fba9a07f8e08c36
ModBotStudio <55330408+ModBotStudio@users.noreply.github.com>
2020-02-06 18:40:43 +0000

    Updated Fully UpBeatRadio Presence + Owner Change as other one got banned and i fully recoded this one

--- UpBeatRadio/dist/presence.js
+++ UpBeatRadio/dist/presence.js
@@ -1,45 +1,71 @@
 var presence = new Presence({
-    clientId: "630447400716206114",
+    clientId: "674714012541386752",
     mediaKeys: true
 });
 
 presence.on("UpdateData", () => {
     let presenceData = {
-        largeImageKey: "upbeatlogo"
+        largeImageKey: "logo"
     }
 
-    let pageName = (document.title).slice(9);
-	
-    if (window.location.pathname.includes("News.Article")) {
-        presenceData.details = "Reading an article";
+    let request = document.querySelector("#modalrequestFormModal > div > div > div.modal-body.m-t-n-lg") !==null;
+    let feedback = document.querySelector("#modalundefined > div > div > div.blurBG.b0") !==null;
+    let djapply = document.querySelector("#modaldjAppButton > div > div") !==null;
+    let newsreporterapply = document.querySelector("#modalmediaAppButton > div > div > div.modal-body > form > div") !==null;
+    let editingbio = document.querySelector("#accountBio") !==null;
+ if(request){
+    presenceData.details = "Sending UpBeat"
+    presenceData.state = "Request"
+ } else if (feedback) {
+    presenceData.details = "Sending UpBeat"
+    presenceData.state = "Feedback"
+} else if (djapply) {
+    presenceData.details = "Applying For"
+    presenceData.state = "Radio Presenter"
+} else if (newsreporterapply) {
+    presenceData.details = "Applying For"
+    presenceData.state = "News Reporter"
+} else if (editingbio) {
+    presenceData.details = "Editing"
+    presenceData.state = "Profile Information"
 
-        let newsArticleName = (document.title).slice(9);
-        presenceData.state = newsArticleName;
-    }  else if(pageName === "Home") {
-        let songArtist = document.querySelector(".stats-artist").innerHTML;
-        let songName = document.querySelector(".stats-song").innerHTML;
+ } else if (document.location.pathname.includes("/UpBeat")) {
+    let songArtist = document.querySelector("#stats > div > div.left.a > div.statbox.top.artist > div > span").innerHTML;
+    let songName = document.querySelector("#stats > div > div.left.a > div.statbox.bottom.song > div > span").innerHTML;
+    presenceData.details = songArtist.replace('&amp;', '&');
+    presenceData.state = songName.replace('&amp;', '&')
 
-        presenceData.details = songName;
-        presenceData.state = songArtist;
-        
-    } else if (document.location.pathname.includes("/core")) {
-        presenceData.details = "Viewing a page";
-        presenceData.state = (document.title).slice(11);
+ } else  if (document.location.pathname.includes("/News.Article")) {
+    let article = document.querySelector("#mainContent > div > div:nth-child(1) > div.glassBox.p-n > div.articleHeader > div > div.col-md-8.col-xs-12 > div.titleContainer > div").innerHTML;
+    let author = document.querySelector("#mainContent > div > div:nth-child(1) > div.glassBox.p-n > div.articleHeader > div > div.col-md-8.col-xs-12 > div.info > a").textContent;
+    presenceData.details = "Article: " + article;
+    presenceData.state = "Made By: " + author;
+    presenceData.smallImageKey = "read";
+    presenceData.smallImageText = "Reading Article";
+ 
+} else if (document.location.pathname.includes("/Account.Profile")) {
+    let porfile = document.querySelector("#mainContent > div > div.col-sm-12 > div.profileBanner > div.profileNameContainer > div > span").textContent;
+  let profile = document.querySelector("#mainContent > div > div.col-sm-12 > div.profileBanner > div.profileNameContainer > div > span").textContent;
+    presenceData.details = "Viewing "+ profile +" Profile"
 
-    } else if (document.location.pathname.includes("/radio")) {
-        presenceData.details = "Viewing a page";
-        presenceData.state = (document.title).slice(11);
+} else if (document.location.pathname.includes("/core")) {
+    presenceData.details = "Viewing Page:";
+    presenceData.state = (document.title).slice(11);
 
-    } else if (document.location.pathname.includes("/news")) {
-        presenceData.details = "Viewing a page";
-        presenceData.state = (document.title).slice(11);
+} else if (document.location.pathname.includes("/radio")) {
+    presenceData.details = "Viewing Page:";
+    presenceData.state = (document.title).slice(11);
 
-    } else {
-        presenceData.details = "Viewing a page";
-        pageName = (document.title).slice(9);
-        presenceData.state = pageName;
-      }
-	  
+} else if (document.location.pathname.includes("/news")) {
+    presenceData.details = "Viewing Page:";
+    presenceData.state = (document.title).slice(11);
+
+ } else {
+    presenceData.details = "Viewing Page:";
+    pageName = (document.title).slice(9);
+    presenceData.state = pageName;
+  }
+
       presence.setActivity(presenceData);
       presence.setTrayTitle();
 });
