e6b23f92015eeebde24ea8e855be74eeae825227
Tellen Official <59364733+TellenOfficial@users.noreply.github.com>
2020-02-12 21:22:04 +0100

    🔄 Update: Added English language & Solved errors...

--- HentaiWorld/dist/iframe.js
+++ HentaiWorld/dist/iframe.js
@@ -1,23 +1,15 @@
 var iframe = new iFrame();
 iframe.on("UpdateData", () => {
-    if (document.querySelector("video#video-player") !== null) {
-    var hentai = document.querySelector("video#video-player");
-    if (hentai !== isNaN) {
-        var currentTime = hentai.currentTime;
-        var duration = hentai.duration;
-        var pause = hentai.paused;
-        iframe.send({
-            iframe_video: {
-                iFrameVideo: true,
-                currTime: currentTime,
-                duration: duration,
-                paused: pause
-                }});
-            }
-        } else
-        iframe.send({
-            iframe_video: {
-                iFrameVideo: false
-                }});
+    if (document.querySelector("#video-player") !== null) {
+        var hentai = document.querySelector("#video-player");
+        if (hentai != undefined && !isNaN(hentai.duration)) {
+            iframe.send({
+                iframe_video: {
+                    iFrameVideo: true,
+                    currTime: hentai.currentTime,
+                    duration: hentai.duration,
+                    paused: hentai.paused
+                }
+        });}
     }
-);
+});
