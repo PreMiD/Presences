345add72935a34cc7c33155c50a88857a37f670e
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2020-02-21 17:33:18 +0000

    Update presence.js

--- Bounce/dist/presence.js
+++ Bounce/dist/presence.js
@@ -65,6 +65,96 @@ if(window.location.pathname.startsWith("/staff")) {
             presenceData.state = "Staff • Rules"
             presence.setActivity(presenceData)
         break;
+        case "radio.requests":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Radio • Requests"
+            presence.setActivity(presenceData)
+        break;
+        case "radio.schedule":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Radio • Schedule"
+            presence.setActivity(presenceData)
+        break;
+        case "radio.resources":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Radio • Resources"
+            presence.setActivity(presenceData)
+        break;
+        case "radio.connectioninformation":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Radio • Connection Information"
+            presence.setActivity(presenceData)
+        break;
+        case "radio.bannedsongs":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Radio • Banned Songs"
+            presence.setActivity(presenceData)
+        break;
+        case "radio.testradioserver":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Radio • Test Radio Server"
+            presence.setActivity(presenceData)
+        break;
+        case "news.createarticle":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "News • Create Article"
+            presence.setActivity(presenceData)
+        break;
+        case "news.allarticles":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "News • Articles"
+            presence.setActivity(presenceData)
+        break;
+        case "news.ideas":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "News • Ideas"
+            presence.setActivity(presenceData)
+        break;
+        case "seniorpresenter.resetconnectioninformation":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Senior Presenter • Reset Connection Information"
+            presence.setActivity(presenceData)
+        break;
+        case "seniorpresenter.updatebannedsongs":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Senior Presenter • Update banned songs"
+            presence.setActivity(presenceData)
+        break;
+        case "seniorreporter.newarticles":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Senior Reporter • New Articles"
+            presence.setActivity(presenceData)
+        break;
+        case "manager.createaccount":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Manager • Create Account"
+            presence.setActivity(presenceData)
+        break;
+        case "manager.allusers":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Manager • All Users"
+            presence.setActivity(presenceData)
+        break;
+        case "manager.bookedaway":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Manager • Booked Away"
+            presence.setActivity(presenceData)
+        break;
+        case "manager.apps":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Manager • Applications"
+            presence.setActivity(presenceData)
+        break;
+        case "manager.addreputation":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "Manager • Add Repution"
+            presence.setActivity(presenceData)
+        break;
+        case "user.editprofile":
+            presenceData.details = "Viewing • Staff Panel"
+            presenceData.state = "User • Edit Profile"
+            presence.setActivity(presenceData)
+        break;
     }
 } else if(window.location.pathname.startsWith("/countdown") || window.location.pathname.startsWith("/mobile")) {
     presenceData.details = (strack || "Loading...") + " - " + (sartist || "Loading...");
