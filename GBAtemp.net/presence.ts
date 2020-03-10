0b07d5b67b25784fef400b94e0b5c657fe9550e3
NightScript <maorninja@outlook.com>
2020-03-10 14:05:18 -0700

    Fix bugs I didn't catch
    Also, remove a bunch of variables that were only used once

--- GBAtemp.net/presence.ts
+++ GBAtemp.net/presence.ts
@@ -1,65 +1,53 @@
-let timeElapsed = Math.floor(Date.now() / 1000),
-  threadName,
-  authorName,
-  newsAuthor,
-  profileName,
-  reviewTitle,
-  blogAuthor,
-  blogTitle,
-  gName,
-  pName,
-  cName;
-
-let presenceData: presenceData = {
-  details: "Browsing...",
-  largeImageKey: "tempy",
-  startTimestamp: timeElapsed
-};
-
 let presence = new Presence({
   clientId: "632110854543769601",
   mediaKeys: false
 });
 
 presence.on("UpdateData", () => {
+  let timeElapsed = Math.floor(Date.now() / 1000);
+  let title, author;
+  let presenceData: presenceData = {
+    details: "Browsing...",
+    largeImageKey: "tempy",
+    startTimestamp: timeElapsed
+  };
+
   if (document.location.pathname.startsWith("/threads")) {
-    threadName = document.querySelector("a#threadTitle");
-    authorName = document.querySelector("span.postedBy > span.posted.iconKey > a.username");
-    newsAuthor = document.querySelector("div.news-author > a.username > b");
+    title = document.querySelector("a#threadTitle");
+    let authorName = document.querySelector("span.postedBy > span.posted.iconKey > a.username");
+    let newsAuthor = document.querySelector("div.news-author > a.username > b");
 
     presenceData.details = `Reading a ${authorName ? "thread" : "news post"} by ` + authorName ? authorName.innerText : newsAuthor.innerText;
-    presenceData.state = threadName.innerText;
+    presenceData.state = title.innerText;
 
   } else if (document.location.pathname.startsWith("/game")) {
-    gName = document.querySelector("h1.dynamicTitle");
-    if (gName.innerText !== "GBAtemp Game Center Home") {
-      presenceData.details: "Reading about a game";
-      presenceData.state: gName.innerText;
+    title = document.querySelector("h1.dynamicTitle");
+    if (title.innerText !== "GBAtemp Game Center Home") {
+      presenceData.details = "Reading about a game";
+      presenceData.state = title.innerText;
     }
   } else if (document.location.pathname.startsWith("/platform")) {
-    pName = document.querySelector("h1.dynamicTitle");
-    if (pName.innerText !== "Game Database" && pName.innerText !== "Game Center Platform List") {
+    title = document.querySelector("h1.dynamicTitle");
+    if (title.innerText !== "Game Database" && title.innerText !== "Game Center Platform List") {
       presenceData.details = "Reading about a platform";
-      presenceData.state = pName.innerText;
+      presenceData.state = title.innerText;
     }
   } else if (document.location.pathname.startsWith("/company")) {
-    cName = document.querySelector("h1.dynamicTitle");
-    if (cName.innerText !== "List of video game companies") {
+    title = document.querySelector("h1.dynamicTitle");
+    if (title.innerText !== "List of video game companies") {
       presenceData.details = "Reading about a company";
-      presenceData.state = cName.innerText;
+      presenceData.state = title.innerText;
     }
   } else if (document.location.pathname.startsWith("/questions")) {
-    threadName = document.querySelector("h1.blueHeader");
+    title = document.querySelector("h1.blueHeader");
 
     presenceData.details = "Reading a question";
-    presenceData.state = threadName.innerText;
+    presenceData.state = title.innerText;
   } else if (document.location.pathname.startsWith("/members")) {
-    profileName = document.querySelector("div.mainText.secondaryContent > h1.username");
+    title = document.querySelector("div.mainText.secondaryContent > h1.username");
 
-    if (profileName) {
-      presenceData.details = `Looking at ${profileName.innerText}'s profile`;
-      presenceData.state = threadName.innerText;
-    }
+    if (title)
+      presenceData.details = `Looking at ${title.innerText}'s profile`;
   } else if (document.location.pathname.startsWith("/chat")) {
     presenceData.details = "Chatting in IRC";
   } else if (document.location.pathname.startsWith("/shoutbox")) {
@@ -67,17 +55,17 @@ presence.on("UpdateData", () => {
   } else if (document.location.pathname.startsWith("/search")) {
     presenceData.details = "Searching...";
   } else if (document.location.pathname.startsWith("/review")) {
-    let reviewAuthor = document.querySelector("span.review_author > a.username");
-    reviewTitle = document.querySelector("h1#review_title > a");
+    author = document.querySelector("span.review_author > a.username");
+    title = document.querySelector("h1#review_title > a");
     
-    presenceData.details = "Reading a review by " + reviewAuthor.innerText;
-    presenceData.state = reviewTitle.innerText;
+    presenceData.details = "Reading a review by " + author.innerText;
+    presenceData.state = title.innerText;
   } else if (document.location.pathname.startsWith("/entry")) {
-    blogAuthor = document.querySelector("span.postedBy > span.posted.iconKey > a.username");
-    blogTitle = document.querySelector("a.newsTitle");
+    author = document.querySelector("span.postedBy > span.posted.iconKey > a.username");
+    title = document.querySelector("a.newsTitle");
 
-    presenceData.details = "Reading a blog post by " + blogAuthor.innerText;
-    presenceData.state = blogTitle.innerText;
+    presenceData.details = "Reading a blog post by " + author.innerText;
+    presenceData.state = title.innerText;
   }
 
   presence.setActivity(presenceData);
