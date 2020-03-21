423782383dabb18827ef4d16e3f1e5317d35aaa9
tepiloxtl <tepiloxtl@int.pl>
2020-03-21 03:24:36 +0100

    1.1 for new website layout

--- Radio Garden/dist/presence.js
+++ Radio Garden/dist/presence.js
@@ -4,16 +4,37 @@ var presence = new Presence({
 });
 
 presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
-    if(document.location.pathname.startsWith("/listen/")){
-        elapsed =  Math.floor(Date.now() / 1000)
+    if(!document.title.includes("Explore live radio by rotating")){
         //var container = document.querySelector(".channel-list")
         //console.log(document.querySelector("div.mod-active").innerText)
-        let presenceData = {
-            details: `${document.querySelector("div.mod-active").innerText}`,
-            state: `${document.querySelector(".location-info-location").innerText}`,
+        var elapsed =  Math.floor(Date.now() / 1000)
+        var presenceData
+        presenceData = {
+            details: `${document.querySelector(".ChannelTitle_title__2QQj5").innerText}`,
+            state: `${document.querySelector(".ChannelTitle_subtitle__DZ_ZQ").innerText}`,
             largeImageKey: "bigglobe",
             startTimestamp: elapsed
         };
- 
+        if(document.getElementsByClassName('ListItem_isPlaying__E3wWB').length == 1){
+            presenceData.smallImageKey = "statusplay";
+            presenceData.smallImageText = "Playing";
+            presenceData.startTimestamp = elapsed;
+        }
+        else if(document.getElementsByClassName('ListItem_isLoading__2rDhr').length == 1){
+            presenceData.smallImageKey = "statusplay";
+            presenceData.smallImageText = "Loading";
+            delete presenceData.startTimestamp;
+        }
+        else if(document.getElementsByClassName('ListItem_isPaused__3xqrt').length == 1){
+            presenceData.smallImageKey = "statusstop";
+            presenceData.smallImageText = "Stopped";
+            delete presenceData.startTimestamp;
+        }
+        else {
+            console.log("broke")
+        }
+        console.log(presenceData)
+        //presenceData.smallImageText = "smallImageText"
+        //delete presenceData.startTimestamp
         presence.setActivity(presenceData)
 }}));
