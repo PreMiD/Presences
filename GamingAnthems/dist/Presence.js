75b79aa0a95e39bb509b39af1b8d2193fd45e577
GamingAnthems <51294309+techlewi@users.noreply.github.com>
2020-03-26 18:11:14 +0000

    Update Presence.js

--- GamingAnthems/dist/Presence.js
+++ GamingAnthems/dist/Presence.js
@@ -8,42 +8,27 @@ strings = presence.getStrings({
     pause: "presence.playback.paused"
 });
 
-/*
-
-function myOutsideHeavyLiftingFunction(){
-    //Grab and process all your data here
-
-    // element grabs //
-    // api calls //
-    // variable sets //
-}
-
-setInterval(10000, myOutsideHeavyLiftingFunction);
-//Run the function seperate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
-
-*/
-
 
 presence.on("UpdateData", () => {
-    //UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
+   
 
-    //It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.
+  
 var browsingStamp = Math.floor(Date.now() / 1000);
     var presenceData = {
         startTimestamp: browsingStamp,
-        largeImageKey: "large", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-        smallImageKey: "small", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
+        largeImageKey: "large",
+        smallImageKey: "small",
         smallImageText: "GamingAnthems Logo", 
         details: "Listening To The Station", 
         state: "No1 Gaming Radio Station", 
-    }; /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
+    }; 
 
     if (presenceData.details == null) {
-        //This will fire if you do not set presence details
-        presence.setTrayTitle(); //Clears the tray title for mac users
-        presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+      
+        presence.setTrayTitle(); 
+        presence.setActivity(); 
     } else {
-        //This will fire if you set presence details
-        presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+       
+        presence.setActivity(presenceData);
     }
 });
