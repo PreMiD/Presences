6a2ba4176d059a054f2507c6932ac7802b92f0b3
ModBotStudio <55330408+ModBotStudio@users.noreply.github.com>
2020-03-27 09:23:35 +0000

    Update presence.js

--- DiscordLabs.org/dist/presence.js
+++ DiscordLabs.org/dist/presence.js
@@ -36,13 +36,10 @@ presence.on("UpdateData", () => {
                 presenceData.details = "Viewing a profile:";
                 presenceData.state = profilename
             }
-        } else if (document.location.pathname.includes("/submit/")) {
-            presenceData.details = "Viewing Page:";
-            presenceData.state = "DiscordLabs Bot Sumbit";
 
         } else if (document.location.pathname.includes("/submit")) {
             presenceData.details = "Viewing Page:";
-            presenceData.state = "DiscordLabs Bot Sumbit";
+            presenceData.state = "Bot Sumbit";
 
         } else if (document.location.pathname.includes("/search/")) {
             presenceData.details = `Search for:`;
