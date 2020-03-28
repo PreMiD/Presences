d8a74e81840883fc24612420d77d9354acb63385
Callum <callumokane123@gmail.com>
2020-03-28 22:48:45 +0000

    Updates google presence • Added AdSense

--- Google/dist/presence.js
+++ Google/dist/presence.js
@@ -1,18 +1,38 @@
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+    return new(P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) {
+            try {
+                step(generator.next(value));
+            } catch (e) {
+                reject(e);
+            }
+        }
+
+        function rejected(value) {
+            try {
+                step(generator["throw"](value));
+            } catch (e) {
+                reject(e);
+            }
+        }
+
+        function step(result) {
+            result.done ? resolve(result.value) : new P(function (resolve) {
+                resolve(result.value);
+            }).then(fulfilled, rejected);
+        }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 var presence = new Presence({
-    clientId: "612704158826496028",
-    mediaKeys: false
-}), strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-});
+        clientId: "612704158826496028",
+        mediaKeys: false
+    }),
+    strings = presence.getStrings({
+        play: "presence.playback.playing",
+        pause: "presence.playback.paused"
+    });
+var pagePath = document.location.pathname;
 var browsingStamp = Math.floor(Date.now() / 1000);
 var doodleTitle;
 var homepageImage;
@@ -31,34 +51,29 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
         presenceData.state = "Home";
         presenceData.startTimestamp = browsingStamp;
         delete presenceData.details;
-    }
-    else if (document.location.pathname.startsWith("/doodles/")) {
+    } else if (pagePath.startsWith("/doodles/")) {
         var searchURL = new URL(document.location.href);
         var doodleResult = searchURL.searchParams.get("q");
         doodleTitle = document.querySelector("#title-card > div > h2");
-        if (document.location.pathname.includes("/about")) {
+        if (pagePath.includes("/about")) {
             presenceData.details = "Doodles";
             presenceData.state = "About";
             presenceData.startTimestamp = browsingStamp;
-        }
-        else if (doodleTitle != null) {
+        } else if (doodleTitle != null) {
             presenceData.details = "Viewing a doodle:";
             presenceData.state = doodleTitle.innerText;
             presenceData.startTimestamp = browsingStamp;
-        }
-        else if (doodleResult && document.location.pathname == "/doodles/") {
+        } else if (doodleResult && pagePath == "/doodles/") {
             presenceData.details = "Searching for a doodle:";
             presenceData.state = doodleResult;
             presenceData.startTimestamp = browsingStamp;
             presenceData.smallImageKey = "search";
-        }
-        else {
+        } else {
             presenceData.details = "Current page:";
             presenceData.state = "Doodles";
             presenceData.startTimestamp = browsingStamp;
         }
-    }
-    else if (document.location.pathname.startsWith("/search")) {
+    } else if (pagePath.startsWith("/search")) {
         var searchURL = new URL(document.location.href);
         searchTab = searchURL.searchParams.get("tbm");
         resultsInfo = document.querySelector("#result-stats");
@@ -67,45 +82,108 @@ presence.on("UpdateData", () => __awaiter(this, void 0, void 0, function* () {
             presenceData.details = "Searching for " + homepageInput.value;
             presenceData.state = resultsInfo.textContent;
             presenceData.startTimestamp = browsingStamp;
-        }
-        else if (searchTab == "isch") {
+        } else if (searchTab == "isch") {
             presenceData.details = "Google Images";
             presenceData.state = "Searching for " + imgInput.value;
             presenceData.startTimestamp = browsingStamp;
-        }
-        else if (searchTab == "vid") {
+        } else if (searchTab == "vid") {
             pageInput = document.querySelector("#lst-ib");
             presenceData.details = "Google Videos";
             presenceData.state = "Searching for " + pageInput.value;
             presenceData.startTimestamp = browsingStamp;
-        }
-        else if (searchTab == "nws") {
+        } else if (searchTab == "nws") {
             pageInput = document.querySelector("#lst-ib");
             presenceData.details = "Google News";
             presenceData.state = "Searching for " + pageInput.value;
             presenceData.startTimestamp = browsingStamp;
-        }
-        else if (searchTab == "bks") {
+        } else if (searchTab == "bks") {
             pageInput = document.querySelector("#lst-ib");
             presenceData.details = "Google Books";
             presenceData.state = "Searching for " + pageInput.value;
             presenceData.startTimestamp = browsingStamp;
-        }
-        else if (searchTab == "fin") {
+        } else if (searchTab == "fin") {
             pageInput = document.querySelector("#lst-ib");
             presenceData.details = "Google Finance";
             presenceData.state = "Searching for " + pageInput.value;
             presenceData.startTimestamp = browsingStamp;
-        }
-        else if (searchTab == "pers") {
+        } else if (searchTab == "pers") {
             pageInput = document.querySelector("#lst-ib");
             presenceData.details = "Google Personal";
             presenceData.state = "Searching for " + pageInput.value;
             presenceData.startTimestamp = browsingStamp;
         }
+    } else if (pagePath.startsWith("/adsense")) {
+        var pt = pagePath.replace("/adsense/")
+        presenceData.details = "Google Adsense";
+        function state(state) {
+            presenceData.state = state
+        };
+        if (pt.startsWith("start/")) {
+            state("Homepage")
+        } 
+        else if (pt.startsWith("start/solutions")) {
+            state("Solutions")
+        } 
+        else if (pt.startsWith("start/solutions/auto-ads/")) {
+            state("Solutions - Auto Ads")
+        } 
+        else if (pt.startsWith("start/solutions/responsive-ads/")) {
+            state("Solutions - Responsive Ads")
+        } 
+        else if (pt.startsWith("start/success-stories/")) {
+            state("Success Stories")
+        } 
+        else if (pt.startsWith("start/resources/")) {
+            state("Resources")
+        } 
+        else if (pt.startsWith("signup/new/lead")) {
+            state("Sign-up")
+        } 
+        else if (pt.inclues("/home") || pt.includes("/new")) {
+            state("Viewing income and stats")
+        } 
+        else if (pt.includes("/myads/sites") || pt.includes("/myads/units")) {
+            state("Viewing custom ads & sites")
+        } 
+        else if (pt.includes("/main/myads-viewall-channels")) {
+            state("Viewing custom channels")
+        } 
+        else if (pt.includes("/main/myads-viewall-url-channels")) {
+            state("Viewing URL Channels")
+        } 
+        else if (pt.includes("/main/admob")) {
+            state("Viewing AdMob configuration")
+        } 
+        else if (pt.includes("/sites/my-sites/new")) {
+            state("Adding a site")
+        } 
+        else if (pt.includes("/sites/my-sites")) {
+            state("Viewing sites")
+        } 
+        else if (pt.includes("/main/viewreports")) {
+            state("Viewing reports")
+        } 
+        else if (pt.includes("/payments")) {
+            state("Viewing payments")
+        } 
+        else if (pt.includes("/main/accountInformation")) {
+            state("Account - Information")
+        } 
+        else if (pt.includes("/main/personal-settings")) {
+            state("Account - Settings")
+        } 
+        else if (pt.includes("/main/messages")) {
+            state("Viewing messages")
+        } 
+        else if (pt.includes("/new/localized-terms")) {
+            state("Viewing Terms of Service")
+        }
+
+
     }
     presence.setActivity(presenceData);
 }));
+
 function getTimestamps(videoTime, videoDuration) {
     var startTime = Date.now();
     var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
