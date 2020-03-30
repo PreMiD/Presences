41d40d670a9d020ba3b869ccf3e14e55dcfb750e
doomlerd <doomlordcs@gmail.com>
2020-03-30 23:41:15 +0100

    💩 EGGSY STOP

--- Trendyol/presence.ts
+++ Trendyol/presence.ts
@@ -1,5 +1,5 @@
 const presence = new Presence({
-    clientId: "643758670131363840",
+    clientId: "643758670131363840"
   }),
   pages = {
     "/butik/liste/erkek": "Erkek Giyim",
@@ -26,7 +26,7 @@ const presence = new Presence({
     "/Hesabim/KrediKartlarim": "Kredi Kartları",
     "/Hesabim/Elite": "Trendyol Elite",
     "/Hesabim/DuyuruTercihleri": "Duyuru Tercihleri",
-    "/Hesabim/Favoriler": "Favoriler",
+    "/Hesabim/Favoriler": "Favoriler"
   };
 
 presence.on("UpdateData", () => async () => {
@@ -63,7 +63,7 @@ presence.on("UpdateData", () => async () => {
       largeImageKey: "ty-logo",
       details: "Bir sayfaya göz atıyor:",
       state: "Yardım & Sorular",
-      startTimestamp: Math.floor(Date.now() / 1000),
+      startTimestamp: Math.floor(Date.now() / 1000)
     });
   } else if (
     (page == "/tum--urunler" && document.location.search.includes("?q=")) ||
@@ -74,44 +74,42 @@ presence.on("UpdateData", () => async () => {
       details: "Bir şey arıyor:",
       state: searchingFor.textContent || "Belirsiz",
       smallImageKey: "search",
-      startTimestamp: Math.floor(Date.now() / 1000),
+      startTimestamp: Math.floor(Date.now() / 1000)
     });
   } else if (category && category.textContent != "") {
     presence.setActivity({
       largeImageKey: "ty-logo",
       details: "Bir kategoriye göz atıyor:",
       state: category.textContent || "Belirsiz",
-      startTimestamp: Math.floor(Date.now() / 1000),
+      startTimestamp: Math.floor(Date.now() / 1000)
     });
   } else if (
     product &&
     product2 &&
     product.textContent != "" &&
-    product2.textContent != "" &&
-    price &&
-    price.textContent != ""
+    product2.textContent != ""
   ) {
     presence.setActivity({
       largeImageKey: "ty-logo",
       details: "Bir ürüne göz atıyor:",
       state: `${product.textContent} | ${product2.textContent} ${
-        price ? `(${price.textContent})` : ""
+        price && price.textContent != "" ? `(${price.textContent})` : null
       }`,
-      startTimestamp: Math.floor(Date.now() / 1000),
+      startTimestamp: Math.floor(Date.now() / 1000)
     });
   } else if (pages[page] || pages[page.slice(0, -1)]) {
     presence.setActivity({
       largeImageKey: "ty-logo",
       details: "Bir sayfaya göz atıyor:",
       state: pages[page] || pages[page.slice(0, -1)],
-      startTimestamp: Math.floor(Date.now() / 1000),
+      startTimestamp: Math.floor(Date.now() / 1000)
     });
   } else {
     presence.setActivity({
       largeImageKey: "ty-logo",
       details: "Bir sayfaya göz atıyor:",
       state: "Ana Sayfa",
-      startTimestamp: Math.floor(Date.now() / 1000),
+      startTimestamp: Math.floor(Date.now() / 1000)
     });
   }
 });
