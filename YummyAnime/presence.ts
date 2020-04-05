6847ef3344aaea4fbb5c129d31d81405027d1e46
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-06 01:15:13 +0200

    Deepscan Code Quality fix YummyAnime

--- YummyAnime/presence.ts
+++ YummyAnime/presence.ts
@@ -15,17 +15,11 @@ var video: HTMLVideoElement, videoDuration: any, videoCurrentTime: any;
 
 var lastPlaybackState = null;
 var playback;
-var browsingStamp = Math.floor(Date.now() / 1000);
 
 var user: any;
 var search: any;
 
-if (lastPlaybackState != playback) {
-  lastPlaybackState = playback;
-  browsingStamp = Math.floor(Date.now() / 1000);
-}
-
-presence.on("iFrameData", data => {
+presence.on("iFrameData", (data) => {
   playback = data.iframe_video.duration !== null ? true : false;
 
   if (playback) {
@@ -33,6 +27,10 @@ presence.on("iFrameData", data => {
     currentTime = data.iframe_video.currTime;
     duration = data.iframe_video.dur;
     paused = data.iframe_video.paused;
+  }
+  if (lastPlaybackState != playback) {
+    lastPlaybackState = playback;
+    browsingStamp = Math.floor(Date.now() / 1000);
   }
 });
 
