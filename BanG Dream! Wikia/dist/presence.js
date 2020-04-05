175ef566ecf84f07e9c5a2eb991de2d9e467b889
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-05 23:04:15 +0200

    Deepscan Code Quality fix BanG Dream! Wikia

--- BanG Dream! Wikia/dist/presence.js
+++ BanG Dream! Wikia/dist/presence.js
@@ -1,18 +1,18 @@
 var presence = new Presence({
-  clientId: "651145049811451924",
+  clientId: "651145049811451924"
 });
 presence.on("UpdateData", async () => {
   if (document.location.pathname.startsWith("/wiki/")) {
-    let page = "N/A";
-    try {
-      page = document.getElementsByClassName("page-header__title")[0]
-        .textContent;
-    } finally {
+    let page = document.getElementsByClassName("page-header__title")[0];
+    if (page == null) {
+      page = "Unknown Page";
+    } else {
+      page = page.textContent;
     }
     let presenceData = {
       details: "Viewing a page...",
       state: page,
-      largeImageKey: "logo",
+      largeImageKey: "logo"
     };
     presence.setActivity(presenceData);
   }
