5fc2e640f953aa65103f3306bd4504ac9a25b159
doomlerd <doomlordcs@gmail.com>
2020-04-06 13:41:19 +0100

    🚨 DeepScan is outdated

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,50 +4,31 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: "eslint:recommended",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    project: "./tsconfig.json"
   },
   rules: {
     semi: ["warn", "always", { omitLastInOneLineBlock: true }],
     quotes: "off",
     indent: "off",
-    camelcase: "off",
     "comma-dangle": ["warn", "never"],
     "linebreak-style": "off",
     "no-console": "off",
     "no-var": "off",
-    "no-undef": "off"
+    "no-undef": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-explicit-any": "off"
   },
-  overrides: [
-    {
-      files: "**/*.ts",
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
-      ],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: "./tsconfig.json"
-      },
-      rules: {
-        semi: ["warn", "always", { omitLastInOneLineBlock: true }],
-        quotes: "off",
-        indent: "off",
-        "comma-dangle": ["warn", "never"],
-        "linebreak-style": "off",
-        "no-console": "off",
-        "no-var": "off",
-        "no-undef": "off",
-        "@typescript-eslint/camelcase": "off",
-        "@typescript-eslint/no-explicit-any": "off"
-      },
-      plugins: ["@typescript-eslint"]
-    }
-  ]
+  plugins: ["@typescript-eslint"]
 };
