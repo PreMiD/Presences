87ea43b2ea91bc39cb57454fbd401c60036fcb08
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-07 15:44:48 +0200

    ESLint Code Quality Fix FlipAnim

--- FlipAnim/presence.ts
+++ FlipAnim/presence.ts
@@ -1,20 +1,11 @@
 var presence = new Presence({
-    clientId: "642119548803219466"
-  }),
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
+  clientId: "642119548803219466"
+});
 
 var browsingStamp = Math.floor(Date.now() / 1000);
 
-var user: any;
-var title: any;
-var replace: any;
-var search: any;
-
 presence.on("UpdateData", async () => {
-  let presenceData: presenceData = {
+  const presenceData: presenceData = {
     largeImageKey: "fa"
   };
 
@@ -51,14 +42,3 @@ presence.on("UpdateData", async () => {
     presence.setActivity(presenceData);
   }
 });
-
-/**
- * Get Timestamps
- * @param {Number} videoTime Current video time seconds
- * @param {Number} videoDuration Video duration seconds
- */
-function getTimestamps(videoTime: number, videoDuration: number) {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
-  return [Math.floor(startTime / 1000), endTime];
-}
