8839f76e193e38291e35ff3ccea84ec7f350aaac
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-07 16:11:01 +0200

    ESLint Code Quality Fix Genius

--- Genius/presence.ts
+++ Genius/presence.ts
@@ -6,11 +6,25 @@ var presence = new Presence({
     pause: "presence.playback.paused"
   });
 
+/**
+ * Get Timestamps
+ * @param {Number} videoTime Current video time seconds
+ * @param {Number} videoDuration Video duration seconds
+ */
+function getTimestamps(
+  videoTime: number,
+  videoDuration: number
+): Array<number> {
+  var startTime = Date.now();
+  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  return [Math.floor(startTime / 1000), endTime];
+}
+
 var elapsed = Math.floor(Date.now() / 1000);
 var artist;
 
 presence.on("UpdateData", async () => {
-  let data: presenceData = {
+  const data: presenceData = {
     largeImageKey: "genius-logo"
   };
 
@@ -103,9 +117,3 @@ presence.on("UpdateData", async () => {
   }
   presence.setActivity(data);
 });
-
-function getTimestamps(videoTime: number, videoDuration: number) {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
-  return [Math.floor(startTime / 1000), endTime];
-}
