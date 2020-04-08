e9f01dd5f3b49778436455546bd82396f3f8e7b0
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 11:46:12 +0200

    ESLint Code Quality Fix Humble Bundle

--- Humble Bundle/presence.ts
+++ Humble Bundle/presence.ts
@@ -1,20 +1,14 @@
 var presence = new Presence({
-    clientId: "633816611022962708"
-  }),
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
+  clientId: "633816611022962708"
+});
 
 var browsingStamp = Math.floor(Date.now() / 1000);
 
-var user: any;
 var title: any;
-var replace: any;
 var search: any;
 
 presence.on("UpdateData", async () => {
-  let presenceData: presenceData = {
+  const presenceData: presenceData = {
     largeImageKey: "humble"
   };
 
@@ -108,14 +102,3 @@ presence.on("UpdateData", async () => {
     presence.setActivity(presenceData);
   }
 });
-
-/**
- * Get Timestamps
- * @param {Number} videoTime Current video time seconds
- * @param {Number} videoDuration Video duration seconds
- */
-function getTimestamps(videoTime: number, videoDuration: number) {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
-  return [Math.floor(startTime / 1000), endTime];
-}
