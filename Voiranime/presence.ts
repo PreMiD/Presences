1856620e23f512121a0988db17210f2a1b981810
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 17:35:59 +0200

    ESLint Code Quality Fix Voiranime

--- Voiranime/presence.ts
+++ Voiranime/presence.ts
@@ -12,6 +12,20 @@ var presence = new Presence({
     paused: true
   };
 
+  /**
+ * Get Timestamps
+ * @param {Number} videoTime Current video time seconds
+ * @param {Number} videoDuration Video duration seconds
+ */
+function getTimestamps(
+  videoTime: number,
+  videoDuration: number
+): Array<number> {
+  var startTime = Date.now();
+  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  return [Math.floor(startTime / 1000), endTime];
+}
+
 presence.on("iFrameData", data => {
   video = data;
 });
@@ -82,9 +96,3 @@ presence.on("UpdateData", async () => {
     presence.setActivity(data);
   }
 });
-
-function getTimestamps(videoTime: number, videoDuration: number) {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
-  return [Math.floor(startTime / 1000), endTime];
-}
