7a6cae2191a860551c15db0806633e5826c84704
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 14:05:58 +0200

    ESLint Code Quality Fix Pandora

--- Pandora/presence.ts
+++ Pandora/presence.ts
@@ -6,6 +6,36 @@ const strings = presence.getStrings({
   pause: "presence.playback.paused"
 });
 
+function stripText(element: HTMLElement, id = "None", log = true): any {
+  if (element && element.firstChild) {
+    return element.firstChild.textContent;
+  } else {
+    if (log)
+      console.log(
+        "%cPandora%cERROR%c An error occurred while stripping data off the page. Please contact Alanexei on the PreMiD Discord server, and send him a screenshot of this error. ID: " +
+          id,
+        "font-weight: 800; padding: 2px 5px; color: white; border-radius: 25px 0 0 25px; background: #596cae;",
+        "font-weight: 800; padding: 2px 5px; color: white; border-radius: 0 25px 25px 0; background: #ff5050;",
+        "color: unset;"
+      );
+    return null;
+  }
+}
+
+/**
+ * Get Timestamps
+ * @param {Number} videoTime Current video time seconds
+ * @param {Number} videoDuration Video duration seconds
+ */
+function getTimestamps(
+  videoTime: number,
+  videoDuration: number
+): Array<number> {
+  var startTime = Date.now();
+  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  return [Math.floor(startTime / 1000), endTime];
+}
+
 var state;
 
 presence.on("UpdateData", async () => {
@@ -70,33 +100,3 @@ presence.on("UpdateData", async () => {
 
   presence.setActivity(data, audioElement ? !audioElement.paused : true);
 });
-
-function capitalize(text: string) {
-  return text.charAt(0).toUpperCase() + text.slice(1);
-}
-
-function stripText(
-  element: HTMLElement,
-  id: string = "None",
-  log: boolean = true
-) {
-  if (element && element.firstChild) {
-    return element.firstChild.textContent;
-  } else {
-    if (log)
-      console.log(
-        "%cPandora%cERROR%c An error occurred while stripping data off the page. Please contact Alanexei on the PreMiD Discord server, and send him a screenshot of this error. ID: " +
-          id,
-        "font-weight: 800; padding: 2px 5px; color: white; border-radius: 25px 0 0 25px; background: #596cae;",
-        "font-weight: 800; padding: 2px 5px; color: white; border-radius: 0 25px 25px 0; background: #ff5050;",
-        "color: unset;"
-      );
-    return null;
-  }
-}
-
-function getTimestamps(videoTime: number, videoDuration: number) {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
-  return [Math.floor(startTime / 1000), endTime];
-}
