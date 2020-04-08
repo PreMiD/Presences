29f43df9fb5ff5a43989ffad72edb08ab6fcd8a5
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 12:03:06 +0200

    ESLint Code Quality Fix Jet Set Radio Live

--- Jet Set Radio Live/presence.ts
+++ Jet Set Radio Live/presence.ts
@@ -2,13 +2,8 @@ var presence = new Presence({
   clientId: "613786642800705569"
 });
 
-let timeElapsed = Math.floor(Date.now() / 1000),
-  strings = presence.getStrings({
-    pause: "presence.playback.paused",
-    live: "presence.playback.live"
-  }),
-  songName,
-  presenceState;
+const timeElapsed = Math.floor(Date.now() / 1000);
+let songName, presenceState;
 
 presence.on("UpdateData", async () => {
   songName = document.querySelector(
@@ -18,14 +13,14 @@ presence.on("UpdateData", async () => {
     "html > body > font#dateTextField.objectSettings.touchableOff"
   );
   if (songName.innerText.length < 1) {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: "Not tuned in.",
       largeImageKey: "jsrl",
       smallImageKey: "pause"
     };
     presence.setActivity(presenceData);
   } else {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: songName.firstChild.textContent,
       state: presenceState.innerText,
       largeImageKey: "jsrl",
