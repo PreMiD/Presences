08f3cbb10cba49806600d2ec7759986cdf977bb5
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 12:04:30 +0200

    ESLint Code Quality Fix JeuxVideo.com

--- JeuxVideo.com/presence.ts
+++ JeuxVideo.com/presence.ts
@@ -2,42 +2,56 @@ var presence = new Presence({
   clientId: "651930315279040512"
 });
 
+/**
+ * Truncate a string by "..." if needed
+ * @param {String} text The string to truncate
+ * @param {Number} length The desized length
+ */
+function truncateString(text: string, length: number): string {
+  if (text.length > length) {
+    return text.substring(0, length - 3) + "...";
+  } else {
+    return text;
+  }
+}
+
 presence.on("UpdateData", async () => {
-  let presenceData: presenceData = {
+  const presenceData: presenceData = {
     largeImageKey: "logo"
   };
 
   if (document.location.pathname == "/") {
     presenceData.details = "Page d'accueil";
   } else if (document.location.pathname.includes("/news/")) {
-    let article = document.querySelector("div.titre-wrapper").textContent;
+    const article = document.querySelector("div.titre-wrapper").textContent;
     presenceData.details = "Lis une actualité";
     presenceData.state = truncateString(article, 128);
   } else if (document.location.pathname.includes("/videos/")) {
-    let video = document.querySelector("div.titre-video").textContent;
+    const video = document.querySelector("div.titre-video").textContent;
     presenceData.details = "Regarde une vidéo";
     presenceData.state = truncateString(video, 128);
   } else if (document.location.pathname.includes("/test/")) {
-    let test = document.querySelector(".gameHeaderBanner__title").textContent;
-    let note = document.querySelector(".bloc-avis-testeur > .note > strong")
+    const test = document.querySelector(".gameHeaderBanner__title").textContent;
+    const note = document.querySelector(".bloc-avis-testeur > .note > strong")
       .textContent;
     presenceData.details = "Lis un test";
     presenceData.state = truncateString(test, 128) + " (" + note + "/20)";
   } else if (document.location.pathname.includes("/messages-prives/")) {
     presenceData.details = "Lis ses MP";
   } else if (document.location.pathname.includes("/forums/0-")) {
-    let forum = document.querySelector(
+    const forum = document.querySelector(
       "#forum-main-col > .titre-head-bloc > .titre-bloc-forum"
     ).textContent;
-    let connected = document.querySelector(".panel-heading > .nb-connect-fofo")
-      .textContent;
+    const connected = document.querySelector(
+      ".panel-heading > .nb-connect-fofo"
+    ).textContent;
     presenceData.details = truncateString(forum, 64);
     presenceData.state = connected;
   } else if (document.location.pathname.includes("/forums/")) {
-    let forum = document.querySelector(
+    const forum = document.querySelector(
       ".bloc-fil-ariane-crumb-forum > .fil-ariane-crumb > span:last-of-type > a"
     ).textContent;
-    let thread = document.querySelector(
+    const thread = document.querySelector(
       "#forum-main-col > .titre-head-bloc > .titre-bloc-forum > #bloc-title-forum"
     ).textContent;
     presenceData.details = truncateString(forum, 64);
@@ -51,16 +65,3 @@ presence.on("UpdateData", async () => {
     presence.setActivity(presenceData);
   }
 });
-
-/**
- * Truncate a string by "..." if needed
- * @param {String} text The string to truncate
- * @param {Number} length The desized length
- */
-function truncateString(text: string, length: number) {
-  if (text.length > length) {
-    return text.substring(0, length - 3) + "...";
-  } else {
-    return text;
-  }
-}
