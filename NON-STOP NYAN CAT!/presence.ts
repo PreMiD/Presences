7dfdd6e775d955e956328b1c84e661f059a952c3
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 13:44:39 +0200

    ESLint Code Quality Fix NON-STOP NYAN CAT!

--- NON-STOP NYAN CAT!/presence.ts
+++ NON-STOP NYAN CAT!/presence.ts
@@ -2,25 +2,25 @@ var presence = new Presence({
   clientId: "631039621656084480"
 });
 
-let timeNyaned = Math.floor(Date.now() / 1000);
+const timeNyaned = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   if (document.location.pathname == "/") {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: "Nyaning",
       largeImageKey: "nyan",
       startTimestamp: timeNyaned
     };
     presence.setActivity(presenceData);
   } else if (document.location.pathname.startsWith("/index.php")) {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: "Nyaning",
       largeImageKey: "nyan",
       startTimestamp: timeNyaned
     };
     presence.setActivity(presenceData);
   } else if (document.location.pathname == "/credits.php") {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: "Looking at the credits",
       state: "...and probably nyaning",
       largeImageKey: "nyan",
@@ -28,7 +28,7 @@ presence.on("UpdateData", async () => {
     };
     presence.setActivity(presenceData);
   } else if (document.location.pathname == "/stats.php") {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: "Looking at their stats",
       state: "...and probably nyaning",
       largeImageKey: "nyan",
