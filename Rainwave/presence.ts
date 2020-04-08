b193bfb65d465eb17d837440623a2082175ae78f
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 14:48:29 +0200

    ESLint Code Quality Fix Rainwave

--- Rainwave/presence.ts
+++ Rainwave/presence.ts
@@ -2,53 +2,51 @@ var presence = new Presence({
   clientId: "618233809481236491"
 });
 
-let timeElapsed = Math.floor(Date.now() / 1000),
-  strings = presence.getStrings({
-    live: "presence.activity.live",
-    pause: "presence.playback.paused"
-  });
+const timeElapsed = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   if (document.location.pathname.startsWith("/pages/playback_history")) {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: "Looking at playback history...",
       largeImageKey: "rainwv"
     };
     presence.setActivity(presenceData);
   } else if (document.location.pathname.startsWith("/forums")) {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: "Browsing the forums...",
       largeImageKey: "rainwv"
     };
     presence.setActivity(presenceData);
   } else if (document.location.pathname.startsWith("/api4")) {
-    let presenceData: presenceData = {
+    const presenceData: presenceData = {
       details: "Looking at the API...",
       largeImageKey: "rainwv"
     };
     presence.setActivity(presenceData);
   } else {
-    let stationName = document.querySelector(
+    const stationName = document.querySelector(
         "a.station.selected_station > div.station_details > div.station_name"
       ),
-      songName = document.querySelector(
-        "div.song.now_playing > div.song_content > div.title"
-      ) as HTMLDivElement,
-      artistName = document.querySelector(
-        "div.song.now_playing > div.song_content > div.artist"
-      ) as HTMLDivElement,
+      songName =
+        document.querySelector(
+          "div.song.now_playing > div.song_content > div.title"
+        ) as HTMLDivElement,
+      artistName =
+        document.querySelector(
+          "div.song.now_playing > div.song_content > div.artist"
+        ) as HTMLDivElement,
       playCheck = document.querySelector(
         "div#r4_audio_player.unselectable.playing"
       );
     if (playCheck == null) {
-      let presenceData: presenceData = {
+      const presenceData: presenceData = {
         details: "Not listening.",
         largeImageKey: "rainwv",
         smallImageKey: "pause"
       };
       presence.setActivity(presenceData);
     } else {
-      let presenceData: presenceData = {
+      const presenceData: presenceData = {
         details: songName.innerText + " by " + artistName.innerText,
         state: "Listening on " + stationName.textContent,
         largeImageKey: "rainwv",
