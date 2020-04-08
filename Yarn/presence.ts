9a8f97a993fed221826e4c386af777fe87e71ebd
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 18:13:55 +0200

    ESLint Code Quality Fix Yarn

--- Yarn/presence.ts
+++ Yarn/presence.ts
@@ -2,15 +2,25 @@ const presence = new Presence({
   clientId: "690635264124518493"
 });
 
-var user: any;
-var title: any;
-var replace: any;
-var search: any;
-
-var browsingStamp = Math.floor(Date.now() / 1000);
+function parseQueryString(queryString?: string): any {
+  if (!queryString) {
+    queryString = window.location.search.substring(1);
+  }
+  const params = {};
+  const queries = queryString.split("&");
+  queries.forEach((indexQuery: string) => {
+    const indexPair = indexQuery.split("=");
+    const queryKey = decodeURIComponent(indexPair[0]);
+    const queryValue = decodeURIComponent(
+      indexPair.length > 1 ? indexPair[1] : ""
+    );
+    params[queryKey] = queryValue;
+  });
+  return params;
+}
 
 presence.on("UpdateData", async () => {
-  let presenceData: presenceData = {
+  const presenceData: presenceData = {
     largeImageKey: "logo"
   };
 
@@ -68,20 +78,3 @@ presence.on("UpdateData", async () => {
     presence.setActivity(presenceData);
   }
 });
-
-function parseQueryString(queryString?: string): any {
-  if (!queryString) {
-    queryString = window.location.search.substring(1);
-  }
-  const params = {};
-  const queries = queryString.split("&");
-  queries.forEach((indexQuery: string) => {
-    const indexPair = indexQuery.split("=");
-    const queryKey = decodeURIComponent(indexPair[0]);
-    const queryValue = decodeURIComponent(
-      indexPair.length > 1 ? indexPair[1] : ""
-    );
-    params[queryKey] = queryValue;
-  });
-  return params;
-}
