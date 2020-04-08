193b3ceb004f34665469f5a25fb9b3dd50df7745
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-08 14:58:42 +0200

    ESLint Code Quality Fix Rocket League Garage

--- Rocket League Garage/presence.ts
+++ Rocket League Garage/presence.ts
@@ -1,25 +1,16 @@
 var presence = new Presence({
-    clientId: "636622538356686871"
-  }),
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
+  clientId: "636622538356686871"
+});
 
 var browsingStamp = Math.floor(Date.now() / 1000);
-
-var user: any;
 var title: any;
-var replace: any;
-var search: any;
 
 presence.on("UpdateData", async () => {
-  let presenceData: presenceData = {
+  const presenceData: presenceData = {
     largeImageKey: "rlg"
   };
 
   title = document.querySelector("#offer-balance");
-  user = document.querySelector("#receive-balance");
 
   if (document.location.pathname == "/") {
     presenceData.startTimestamp = browsingStamp;
@@ -99,14 +90,3 @@ presence.on("UpdateData", async () => {
     presence.setActivity(presenceData);
   }
 });
-
-/**
- * Get Timestamps
- * @param {Number} videoTime Current video time seconds
- * @param {Number} videoDuration Video duration seconds
- */
-function getTimestamps(videoTime: number, videoDuration: number) {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
-  return [Math.floor(startTime / 1000), endTime];
-}
