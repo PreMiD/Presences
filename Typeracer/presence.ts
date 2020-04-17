60a4d694b4ac416e3fb9d938faf2e254045901b0
Hans5958 <email5958.1@gmail.com>
2020-04-17 15:44:22 +0700

    Update Typeracer (1.1.2)

--- Typeracer/presence.ts
+++ Typeracer/presence.ts
@@ -2,18 +2,17 @@ var presence = new Presence({
   clientId: "655247212728811530"
 });
 
-var browsingStamp = Math.floor(Date.now() / 1000),
-  href = new URL(document.location.href),
-  presenceData = {
-    details: "In construction" as string,
-    state: null as string,
-    largeImageKey: "lg" as string,
-    startTimestamp: browsingStamp as number,
-    endTimestamp: null as number
+var currentURL = new URL(document.location.href),
+  currentPath = currentURL.pathname.slice(1).split("/"),
+  browsingStamp = Math.floor(Date.now() / 1000),
+  presenceData: presenceData = {
+    details: "Viewing an unsupported page",
+    largeImageKey: "lg",
+    startTimestamp: browsingStamp
   },
   updateCallback = {
-    _function: null,
-    get function(): any {
+    _function: null as Function,
+    get function(): Function {
       return this._function;
     },
     set function(parameter) {
@@ -22,18 +21,29 @@ var browsingStamp = Math.floor(Date.now() / 1000),
     get present(): boolean {
       return this._function !== null;
     }
-  },
-  raceStamp = null;
+  };
+
+/**
+ * Initialize/reset presenceData.
+ */
+function resetData(): void {
+  currentURL = new URL(document.location.href);
+  currentPath = currentURL.pathname.slice(1).split("/");
+  presenceData = {
+    details: "Viewing an unsupported page",
+    largeImageKey: "lg",
+    startTimestamp: browsingStamp
+  };
+}
 
 ((): void => {
-  if (href.hostname === "play.typeracer.com") {
-    /*
+  var raceStamp = null;
 
-		Part 1
-		play.typeracer.com (game page)
-
-		*/
-
+  if (currentURL.hostname === "play.typeracer.com") {
+    //
+    //		Part 1
+    //		play.typeracer.com (game page)
+    //
     updateCallback.function = (): void => {
       if (document.querySelector(".gameView")) {
         presenceData.details = "Playing a race";
@@ -63,7 +73,7 @@ var browsingStamp = Math.floor(Date.now() / 1000),
           );
           const lettersTotal = textBox.textContent.length;
           let lettersTyped = 0;
-          for (var i in textBox.children) {
+          for (const i in textBox.children) {
             if (
               typeof textBox.children[i] !== "number" &&
               typeof textBox.children[i] !== "function"
@@ -105,35 +115,30 @@ var browsingStamp = Math.floor(Date.now() / 1000),
         presenceData.details = "Viewing the home page";
       }
     };
-  } else if (href.hostname === "data.typeracer.com") {
-    /*
-
-		Part 2
-		data.typeracer.com (pit stop and misc. pages)
-
-		*/
-
-    const path = href.pathname.slice(1).split("/");
-
-    if (path[0] === "pit") {
-      if (path[1] === "profile") {
+  } else if (currentURL.hostname === "data.typeracer.com") {
+    //
+    //		Part 2
+    //		data.typeracer.com (pit stop and misc. pages)
+    //
+    if (currentPath[0] === "pit") {
+      if (currentPath[1] === "profile") {
         presenceData.details = "Viewing a racer profile";
         presenceData.state =
           document.querySelector("#profileUsername").textContent || null;
-      } else if (path[1] === "text_info") {
+      } else if (currentPath[1] === "text_info") {
         presenceData.details = "Viewing a text";
-        presenceData.state = href.searchParams.get("id");
-      } else if (path[1] === "result") {
+        presenceData.state = currentURL.searchParams.get("id");
+      } else if (currentPath[1] === "result") {
         presenceData.details = "Viewing a race result";
         presenceData.state = `Race ${
-          href.searchParams.get("id").split("|")[2]
-        } of ${href.searchParams.get("id").split("|")[1].slice(3)}`;
-      } else if (path[1] === "race_history") {
+          currentURL.searchParams.get("id").split("|")[2]
+        } of ${currentURL.searchParams.get("id").split("|")[1].slice(3)}`;
+      } else if (currentPath[1] === "race_history") {
         presenceData.details = "Viewing someone's race history";
-        presenceData.state = href.searchParams.get("user") || null;
-      } else if (path[1] === "home") {
+        presenceData.state = currentURL.searchParams.get("user") || null;
+      } else if (currentPath[1] === "home") {
         presenceData.details = "Viewing the pit stop";
-      } else if (path[1] === "competitions") {
+      } else if (currentPath[1] === "competitions") {
         presenceData.details = "Viewing the competition result";
         const option = document
           .querySelector("option[selected]")
@@ -149,7 +154,7 @@ var browsingStamp = Math.floor(Date.now() / 1000),
         else if (option === "month")
           presenceData.state = `${strong[3]} ${strong[4]}`;
         else if (option === "year") presenceData.state = strong[2];
-      } else if (path[1] === "login") {
+      } else if (currentPath[1] === "login") {
         presenceData.details = "Logging in";
       } else {
         const pageNames = {
@@ -158,51 +163,26 @@ var browsingStamp = Math.floor(Date.now() / 1000),
           privacy_poicy: "Privacy Policy"
         };
         presenceData.details = "Viewing a page";
-        presenceData.state = pageNames[path[1]];
+        presenceData.state = pageNames[currentPath[1]];
       }
-    } else if (path[0] === "misc") {
-      if (path[1] === "about") {
+    } else if (currentPath[0] === "misc") {
+      if (currentPath[1] === "about") {
         presenceData.details = "Viewing a page";
         presenceData.state = "About";
       }
-    } else if (path[0] === "admin") {
+    } else if (currentPath[0] === "admin") {
       presenceData.details = "Viewing school admin pages";
     }
   }
 })();
 
-/**
- * Initialize presenceData
- */
-function resetData(): void {
-  presenceData = {
-    details: "In construction" as string,
-    state: null as string,
-    largeImageKey: "lg" as string,
-    startTimestamp: browsingStamp as number,
-    endTimestamp: null as number
-  };
-}
-
-/**
- * Cleans presenceData
- */
-function cleanData(): void {
-  if (presenceData.details === null) delete presenceData.details;
-  if (presenceData.state === null) delete presenceData.state;
-  if (presenceData.startTimestamp === null) delete presenceData.startTimestamp;
-  if (presenceData.endTimestamp === null) delete presenceData.endTimestamp;
-}
-
 if (updateCallback.present) {
   presence.on("UpdateData", async () => {
     resetData();
     updateCallback.function();
-    cleanData();
     presence.setActivity(presenceData);
   });
 } else {
-  cleanData();
   presence.on("UpdateData", async () => {
     presence.setActivity(presenceData);
   });
