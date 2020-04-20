74928d7d277bf02ec3d6a8db871ccf26d3ede75d
Ｐｎｆｒｌ　Ｅｎｍ <35457994+PnfrlEnm@users.noreply.github.com>
2020-04-20 17:37:11 -0500

    Add files via upload

---
+++ presence.js
@@ -0,0 +1,21 @@
+let presence = new Presence({
+  clientId: "701863684728946799"
+});
+
+var elapsed = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+  var presenceData = {
+    largeImageKey: "irentae",
+    startTimestamp: elapsed,
+    details: window.location.href.slice(24),
+  };
+
+  if (window.location.href == "http://angusnicneven.com/") {
+    presenceData.details = "/index";
+  }
+  
+  presence.setTrayTitle("Terminal 00 - " + presenceData.details);
+  presence.setActivity(presenceData);
+    
+});
