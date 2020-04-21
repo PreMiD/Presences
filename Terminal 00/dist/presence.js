1a78df71e9f1faa2d804c4f667a3c5d550947c8d
doomlerd <doomlordcs@gmail.com>
2020-04-21 02:11:01 +0000

    🐛 Some fixes

--- Terminal 00/dist/presence.js
+++ Terminal 00/dist/presence.js
@@ -2,20 +2,23 @@ let presence = new Presence({
   clientId: "701863684728946799"
 });
 
-var elapsed = Math.floor(Date.now() / 1000);
+let elapsed = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-  var presenceData = {
+  let presenceData = {
     largeImageKey: "irentae",
     startTimestamp: elapsed,
-    details: window.location.href.slice(24),
+    details: location.href.split(location.host)[1].replace(/^\//, "")
   };
 
-  if (window.location.href == "http://angusnicneven.com/") {
-    presenceData.details = "/index";
+  if (
+    location.pathname === "/" ||
+    location.href.split(location.host)[1].replace(/^\//, "").toLowerCase() ===
+      "index"
+  ) {
+    presenceData.details = "Index";
   }
-  
+
   presence.setTrayTitle("Terminal 00 - " + presenceData.details);
   presence.setActivity(presenceData);
-    
 });
