3fea3e86f8fa0cd16bcbc8b31465e426b86ea3ae
KeremRR <64226309+KeremRR@users.noreply.github.com>
2020-04-24 13:12:07 +0300

    Update presence.js

--- RunoPW/dist/presence.js
+++ RunoPW/dist/presence.js
@@ -12,14 +12,42 @@ presence.on("UpdateData", () => {
   if (document.location.hostname == "runo.pw") {
 	  
     if (document.location.pathname.startsWith("/index")) {
-      presenceData.details = "Giriş sayfasına bakıyor...";
-      presenceData.state = "İndex Sayfası";
+      presenceData.details = "Şuanda İndex Sayfasında,";
+      presenceData.state = "Hesabına Giriş Yapıyor...";
+	    
     } else if (document.location.pathname.startsWith("/me")) {
-      presenceData.details = "Me sayfasına bakıyor....";
-      presenceData.state = "Me Sayfası";
-    } else if (document.location.pathname.startsWith("/client")) {
-      presenceData.details = "Şuanda client sayfasında!";
-      presenceData.state = "www.Runo.Pw";
+      presenceData.details = "Şuanda Kendi Sayfasında,";
+      presenceData.state = "Hotele Giriş Yapmaya Hazırlanıyor...";
+	    
+      } else if (document.location.pathname.startsWith("/client")) {
+      presenceData.details = "Hotele Giriş Yaptı,";
+      presenceData.state = "Şuanda Oyunda...";
+	    
+      } else if (document.location.pathname.startsWith("/register")) {
+      presenceData.details = "Şuanda Kayıt Sayfasında,";
+      presenceData.state = "Hotele Kayıt Oluyor..."
+		   
+      } else if (document.location.pathname.startsWith("/forgot")) {
+      presenceData.details = "Şuanda Şifremi Unuttum Sayfasında";
+      presenceData.state = "Galiba Şifresini Unutmuş...";
+				   
+      } else if (document.location.pathname.startsWith("/settings/1")) {
+      presenceData.details = "Şuanda Genel Ayarlar Sayfasında,";
+      presenceData.state = "Genel Ayarları Değiştiriyor";
+	      
+      } else if (document.location.pathname.startsWith("/settings")) {
+      presenceData.details = "Şuanda Genel Ayarlar Sayfasında,";
+      presenceData.state = "Genel Ayarları Değiştiriyor...";
+	      	      
+      } else if (document.location.pathname.startsWith("/settings/2")) {
+      presenceData.details = "Şuanda E-Posta Değiştirme Sayfasında,";
+      presenceData.state = "E-Postasını Değiştiriyor...";
+	      	      
+      } else if (document.location.pathname.startsWith("/settings/3")) {
+      presenceData.details = "Şuanda Şifre Değiştirme Sayfasında,";
+      presenceData.state = "Şifresini Değiştiriyor...";
+	      	      
+								   
     }
   }
   if (presenceData.details == null) {
