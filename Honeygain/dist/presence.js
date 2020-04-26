c7bb3d32b85a66b9273b9f3bfc7aa88f8001395d
qwerty142 <43281665+chaos511@users.noreply.github.com>
2020-04-26 15:04:00 -0500

    add support for honeygain.com (2/2)

--- Honeygain/dist/presence.js
+++ Honeygain/dist/presence.js
@@ -9,9 +9,43 @@ presence.on("UpdateData", () => {
     details: "Browsing Honeygain",
     startTimestamp: elapsed
   };
-  if (path == "/") {
-    presenceData.details = "Looking at Dashboard";
-    try {
+  if (document.location.hostname == "www.honeygain.com") {
+    if (document.getElementById("comp-jvf30y3iinlineContent")) {
+      presenceData.details = "Reading Privacy Policy";
+    } else if (document.getElementById("comp-jvf2al4einlineContent")) {
+      presenceData.details = "Reading Terms of Use";
+    } else if (path == "/") {
+      var estEarningsPos = document
+        .getElementById("comp-jv3eiz6p")
+        .getBoundingClientRect();
+      var whatIsHGPos = document
+        .getElementById("comp-jv0xq201")
+        .getBoundingClientRect();
+      var howItWorksPos = document
+        .getElementById("comp-jvgnw4tj")
+        .getBoundingClientRect();
+      if (
+        estEarningsPos.top < window.innerHeight &&
+        estEarningsPos.bottom >= 0
+      ) {
+        presenceData.details = "Estimating Earnings";
+      }
+      if (whatIsHGPos.top < window.innerHeight && whatIsHGPos.bottom >= 0) {
+        presenceData.details = "Reading What is Honeygain";
+      }
+      if (howItWorksPos.top < window.innerHeight && howItWorksPos.bottom >= 0) {
+        presenceData.details = "Reading How It Works";
+      }
+    } else if (path.includes("/faq")) {
+      presenceData.details = "Reading FAQ";
+    } else if (path.includes("/contact-us")) {
+      presenceData.details = "Seeking Help From Honeygain";
+    } else if (path.includes("/download")) {
+      presenceData.details = "Downloading Honeygain";
+    }
+  } else {
+    if (path == "/") {
+      presenceData.details = "Viewing Dashboard";
       presenceData.state =
         "Balance: " +
         document
@@ -22,29 +56,25 @@ presence.on("UpdateData", () => {
         document
           .getElementsByClassName("sc-dnqmqq kpUaxq")[1]
           .innerText.split("Equals to ")[1];
-    } catch (e) {}
-  } else if (path.includes("/transactions")) {
-    presenceData.details = "Viewing Transaction History";
-    try {
+    } else if (path.includes("/transactions")) {
+      presenceData.details = "Viewing Transaction History";
       presenceData.state =
         "Balance: " +
         document
           .getElementsByClassName("sc-dnqmqq kpUaxq")[1]
           .innerText.split("Equals to ")[1];
-    } catch (e) {}
-  } else if (path.includes("/referrals")) {
-    presenceData.details = "Viewing Referrals";
-    try {
+    } else if (path.includes("/referrals")) {
+      presenceData.details = "Viewing Referrals";
       presenceData.state =
         "Referral Code: " +
         document
           .getElementsByClassName("sc-RefOD izklIM sc-bwCtUz gqfLZQ")[0]
           .value.split("https://r.honeygain.money/")[1];
-    } catch (e) {}
-  } else if (path.includes("/faq")) {
-    presenceData.details = "Viewing FAQ";
-  } else if (path.includes("/profile")) {
-    presenceData.details = "Viewing Profile settings";
+    } else if (path.includes("/faq")) {
+      presenceData.details = "Viewing FAQ";
+    } else if (path.includes("/profile")) {
+      presenceData.details = "Viewing Profile settings";
+    }
   }
   if (presenceData.details == null) {
     presence.setTrayTitle();
