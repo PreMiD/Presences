60432b71583185312a4077492027f48ecf5fc372
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-04-27 23:58:37 +0200

    :recycle:

--- @types/premid/index.d.ts
+++ @types/premid/index.d.ts
@@ -91,25 +91,6 @@ declare class Presence {
    * @since 2.1
    */
   showSetting(settings: string | Array<string>): Promise<void>;
-  /**
-   * Get a setting from the presence metadata
-   * @param setting Id of setting as defined in metadata.
-   */
-  getSetting(setting: string): Promise<any>;
-  /**
-   * Hide a setting
-   * @param setting Id of setting
-   */
-  hideSetting(setting: string): Promise<void>;
-  /**
-   * Show a setting
-   * @param setting Id of setting
-   */
-  showSetting(setting: string): Promise<void>;
-  /**
-   * Sends data back to application
-   * @param data Data to send back to application
-   */
   private sendData;
   /**
    * Subscribe to events emitted by the extension
