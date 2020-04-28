a49e722659ff298ae83b47615be4c49ce84f0ca1
Alexx <56901798+SnoweyFoxx@users.noreply.github.com>
2020-04-27 21:04:02 -0500

    Added NetEase Cloud Music

---
+++ NetEase Cloud Music/dist/presence.js
@@ -0,0 +1,28 @@
+var presence = new Presence({
+  clientId: "704509278253350964",
+});
+var browsingStamp = Math.floor(Date.now() / 1000);
+var title;
+presence.on("UpdateData", async () => {
+  const presenceData = {
+    largeImageKey: "logo",
+  };
+  if (document.location.pathname == "/#") {
+    songName = document.querySelector("a.f-thide.name.fc1.f-fl");
+    artistName = document.querySelector("span.by.f-thide.f-fl");
+    presenceData.details = "Listening to: " + songName.innerText;
+    presenceData.state = "by: " + artistName.innerText;
+  } else {
+    songName = document.querySelector("a.f-thide.name.fc1.f-fl");
+    artistName = document.querySelector("span.by.f-thide.f-fl");
+    presenceData.details = "Listening to: " + songName.innerText;
+    presenceData.state = "by: " + artistName.innerText;
+  }
+
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
