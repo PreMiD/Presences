025ca391953138fba14cebee014aec25952aa105
Alexx <56901798+SnoweyFoxx@users.noreply.github.com>
2020-04-27 21:14:40 -0500

    Update presence.js

--- NetEase Cloud Music/dist/presence.js
+++ NetEase Cloud Music/dist/presence.js
@@ -1,21 +1,21 @@
 var presence = new Presence({
-  clientId: "704509278253350964",
+  clientId: "704509278253350964"
 });
 var browsingStamp = Math.floor(Date.now() / 1000);
 var title;
 presence.on("UpdateData", async () => {
   const presenceData = {
-    largeImageKey: "logo",
+    largeImageKey: "logo"
   };
   if (document.location.pathname == "/#") {
     songName = document.querySelector("a.f-thide.name.fc1.f-fl");
     artistName = document.querySelector("span.by.f-thide.f-fl");
     presenceData.details = "Listening to: " + songName.innerText;
     presenceData.state = "by: " + artistName.innerText;
-  } else{
+  } else {
     presenceData.startTimestamp == browsingStamp;
     presenceData.details = "Can't read page";
-}
+  }
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
