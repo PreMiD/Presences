9b9294b49799f2e4be5f547bc72c9bee46bc6591
Drull <pcaetanop@gmail.com>
2020-04-29 10:21:27 -0300

    Compiled file

--- Championgg/dist/presence.js
+++ Championgg/dist/presence.js
@@ -1,40 +1,78 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
 var presence = new Presence({
     clientId: "704756386860499089"
-  });
-  
-  var time = Math.floor(Date.now() / 1000);
-  presence.on("UpdateData", () => {
-    let data = {
-        largeImageKey: "championgg"
-      },
-      path = document.location.pathname.toLowerCase();
-    if (path === "/") {
-      data.details = "Initial page";
-      data.state = "Viewing champions";
-      data.startTimestamp = time;
-      presence.setActivity(data);
-    }
-    if (path === "/statistics/") {
-      data.details = "Statistics";
-      data.state = "Viewing win rates";
-      data.startTimestamp = time;
-      presence.setActivity(data);
-    }
-    if (path.startsWith("/matchup")) {
-      data.details = "Analysing matchup";
-      data.state = "Preparing to fight";
-      data.startTimestamp = time;
-      presence.setActivity(data);
-    }
-    if (path.startsWith("/champion")){
-      let string = path.replace("/champion/", "")
-      data.details = "Checking Runes";
-      data.state = string;
-      data.startTimestamp = time;
-      presence.setActivity(data);
-    }
-    else {
-      presence.setActivity(presenceData);
-    }
 });
-  
+var time = Math.floor(Date.now() / 1000);
+presence.on("UpdateData", function () { return __awaiter(_this, void 0, void 0, function () {
+    var presenceData, path, name_1;
+    return __generator(this, function (_a) {
+        presenceData = {
+            largeImageKey: "championgg"
+        };
+        path = document.location.pathname.toLowerCase();
+        if (path === "/") {
+            presenceData.details = "Initial page";
+            presenceData.state = "Viewing champions";
+            presenceData.startTimestamp = time;
+        }
+        else if (path === "/statistics/") {
+            presenceData.details = "Statistics";
+            presenceData.state = "Viewing win rates";
+            presenceData.startTimestamp = time;
+        }
+        else if (path.startsWith("/matchup")) {
+            presenceData.details = "Analysing matchup";
+            presenceData.state = "Preparing to fight";
+            presenceData.startTimestamp = time;
+        }
+        else if (path.startsWith("/champion")) {
+            name_1 = path.replace("/champion/", "");
+            presenceData.details = "Checking Runes";
+            presenceData.state = name_1;
+            presenceData.startTimestamp = time;
+        }
+        else if (presenceData.details == null) {
+            presence.setTrayTitle();
+            presence.setActivity();
+        }
+        else {
+            presence.setActivity(presenceData);
+        }
+        return [2 /*return*/];
+    });
+}); });
