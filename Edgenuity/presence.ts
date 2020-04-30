284783f55f837b1624a2e0f79d412fd9bf4933e9
Alexx <56901798+SnoweyFoxx@users.noreply.github.com>
2020-04-30 12:00:25 -0500

    Update Presence.ts and Rebuild

--- Edgenuity/presence.ts
+++ Edgenuity/presence.ts
@@ -1,10 +1,8 @@
 var presence = new Presence({
-    clientId: "705454018343862362"
-  });
+  clientId: "705454018343862362"
+});
 
-let courseName: any,
-  lessonName: any,
-  lessonActivity: any;
+let courseName: any, lessonName: any, lessonActivity: any;
 
 presence.on("UpdateData", async () => {
   const presenceData: presenceData = {
@@ -13,14 +11,15 @@ presence.on("UpdateData", async () => {
 
   if (document.location.pathname == "/") {
     presenceData.details = "Viewing Edgenuity Home";
-  }else if (document.location.pathname == "/Login/Login/Student") {
+  } else if (document.location.pathname == "/Login/Login/Student") {
     presenceData.details = "Logging in...";
-  }else if (document.location.pathname == "/Player/") {
+  } else if (document.location.pathname == "/Player/") {
     courseName = document.querySelector("span.course");
     lessonName = document.querySelector("[data-bind='html: Title']");
     lessonActivity = document.querySelector("[data-bind='html: ActivityName']");
     presenceData.details = courseName.innerText;
-    presenceData.state = lessonName.innerText + " - " + lessonActivity.innerText;
+    presenceData.state =
+      lessonName.innerText + " - " + lessonActivity.innerText;
   } else {
     presenceData.details = "Can't read page";
   }
