59dc3c695c8fb89aa9e2deb3a269bad9c55e9452
Tyler <34461107+TylerCousins@users.noreply.github.com>
2020-04-30 03:44:29 +0100

    add Complex Radio

---
+++ Complex Radio/presence.ts
@@ -0,0 +1,27 @@
+var presence = new Presence({ clientId: "705189441484095508" });
+
+let ssong, slisteners, spresenter, strack;
+
+setInterval(newStats, 2500);
+newStats();
+
+
+async function newStats() {
+    let data = await window
+        .fetch("https://radio.complexr.pw/api/nowplaying/1")
+        .then((res) => res.json());
+    ssong = data.now_playing.song.text;
+    spresenter = data.live.is_live ? "Presenter " + data.live.streamer_name : "AutoDJ";
+    slisteners = data.listeners.unique + " Listeners";
+}
+
+presence.on("UpdateData", () => {
+    const stamp = Math.floor(Date.now());
+    const info:presenceData = {
+        largeImageKey: "complexlogo",
+        details: `${spresenter} • ${slisteners || "Loading statistics"}`,
+        state: `${ssong || "Loading song"}`,
+        startTimestamp: stamp
+    };
+    presence.setActivity(info);
+});
