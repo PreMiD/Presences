41c243cba8639606e8b94c197cbaa6ce5e704cf0
MateusOFCZ <mateusdacruz357@hotmail.com>
2020-05-03 13:07:24 -0300

    New Oglle 1.2
    New Oglle 1.2

--- Oglle/dist/presence.js
+++ Oglle/dist/presence.js
@@ -16,26 +16,35 @@ presence.on("UpdateData", () => {
     if(window.location.pathname.includes("/")) {
         presenceData.details = "Inicio"
     }
-    if(window.location.pathname.includes("/nossos-programas")) {
-        presenceData.details = "Programas"
+    if(window.location.pathname.includes("/registro")) {
+        presenceData.details = "Registro"
     }
-    if(window.location.pathname.includes("/servicos")) {
-        presenceData.details = "Serviços"
+    if(window.location.pathname.includes("/blog-da-comunidade")) {
+        presenceData.details = "Blog da Comunidade"
     }
-    if(window.location.pathname.includes("/noticias")) {
-        presenceData.details = "Notícias"
+    if(window.location.pathname.includes("/faq")) {
+        presenceData.details = "FAQ"
     }
-    if(window.location.pathname.includes("/noticia")) {
-        presenceData.details = "Lendo Notícia"
+    if(window.location.pathname.includes("/perfil")) {
+        presenceData.details = "Perfil"
     }
-    if(window.location.pathname.includes("/trabalhe-conosco")) {
-        presenceData.details = "Trabalhe Conosco"
+    if(window.location.pathname.includes("/about")) {
+        presenceData.details = "Sobre"
     }
-    if(window.location.pathname.includes("/politica-de-privacidade")) {
-        presenceData.details = "Politica de Privacidade"
+    if(window.location.pathname.includes("/preferences")) {
+        presenceData.details = "Preferências"
     }
-    if(window.location.pathname.includes("/anuncios")) {
-        presenceData.details = "Ganhando Dinheiro"
+    if(window.location.pathname.includes("/notifications")) {
+        presenceData.details = "Notificações"
+    }
+    if(window.location.pathname.includes("/account")) {
+        presenceData.details = "Conta"
+    }
+    if(window.location.pathname.includes("/friends")) {
+        presenceData.details = "Amigos"
+    }
+    if(window.location.pathname.includes("/membros")) {
+        presenceData.details = "Membros"
     }
     if(window.location.pathname.includes("/admin")) {
         presenceData.details = "Painel De Administração"
@@ -46,7 +55,6 @@ presence.on("UpdateData", () => {
     if(window.location.pathname.includes("/wp-admin")) {
         presenceData.details = "Painel De Administração"
     }
-    
 
     presence.setActivity(presenceData);
 });
