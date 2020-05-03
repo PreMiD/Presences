4eedb1cbed03afc7927e041e050fdb75a285904f
Mert Bhey <41897689+MertBhey@users.noreply.github.com>
2020-05-03 10:38:54 +0300

    Hu

---
+++ SimsekBot/presence.ts
@@ -0,0 +1,29 @@
+var presence = new Presence({
+    clientId: "706234362575847536"
+})
+ 
+var browsingStamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", () => {
+  let presenceData = {
+    largeImageKey: "simsekbot_logo"
+  };
+  if (document.location.hostname == "simsekbot.com" || document.location.hostname == "www.simsekbot.com") {  
+    if (document.location.pathname === "/") {
+      presenceData.details = "Anasayfayı inceliyor.";
+      presenceData.startTimestamp = browsingStamp;
+    } else if (document.location.pathname.includes("/komutlar")) {
+      presenceData.details = "Bot komutlarını inceliyor.";
+      presenceData.startTimestamp = browsingStamp;
+    }
+  } else if(document.location.hostname === "istatistik.simsekbot.com") {
+      presenceData.details = "Bot istatistiklerini inceliyor.";
+      presenceData.startTimestamp = browsingStamp;
+  }
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
