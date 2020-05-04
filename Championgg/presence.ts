a5513db389766df6f22a6d41ed669ed9e20f6061
Drull <pcaetanop@gmail.com>
2020-05-04 15:25:10 -0300

    Updated presence.ts

--- Championgg/presence.ts
+++ Championgg/presence.ts
@@ -6,43 +6,31 @@ var time = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   var presenceData: presenceData = {
-      largeImageKey: "championgg"
+    largeImageKey: "championgg"
   };
-    var path = document.location.pathname.toLowerCase();
+  var path = document.location.pathname.toLowerCase();
   if (path === "/") {
-      presenceData.details = "Initial page";
-      presenceData.state = "Viewing champions";
-      presenceData.startTimestamp = time;
-      presence.setActivity(presenceData);
-
-
-  }
-  else if (path === "/statistics/") {
-      presenceData.details = "Statistics";
-      presenceData.state = "Viewing win rates";
-      presenceData.startTimestamp = time;
-      presence.setActivity(presenceData);
-
-  }
-  else if (path.startsWith("/matchup")) {
-      presenceData.details = "Analysing matchup";
-      presenceData.state = "Preparing to fight";
-      presenceData.startTimestamp = time;
-      presence.setActivity(presenceData);
-
-  }
-  else if (path.startsWith("/champion")){
+    presenceData.details = "Initial page";
+    presenceData.state = "Viewing champions";
+    presenceData.startTimestamp = time;
+  } else if (path === "/statistics/") {
+    presenceData.details = "Statistics";
+    presenceData.state = "Viewing win rates";
+    presenceData.startTimestamp = time;
+  } else if (path.startsWith("/matchup")) {
+    presenceData.details = "Analysing matchup";
+    presenceData.state = "Preparing to fight";
+    presenceData.startTimestamp = time;
+  } else if (path.startsWith("/champion")) {
     const name = path.replace("/champion/", "");
     presenceData.details = "Checking Runes";
     presenceData.state = name;
     presenceData.startTimestamp = time;
-    presence.setActivity(presenceData);
-
   }
-  else if (presenceData.details == null) {
-      presence.setTrayTitle();
-      presence.setActivity();
-    } else {
-      presence.setActivity(presenceData);
-    }
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
 });
