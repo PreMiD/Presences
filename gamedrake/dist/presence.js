0e7fd92913c648a604c400a78e42cc77f8aff410
slimmoe <64897419+slimmoe@users.noreply.github.com>
2020-05-06 17:27:34 +0300

    Create presence.js

---
+++ gamedrake/dist/presence.js
@@ -0,0 +1,50 @@
+var presence = new Presence({
+    clientId: "707525132322865152"
+});
+var browsingStamp = Math.floor(Date.now() / 1000);
+var user;
+presence.on("UpdateData", async () => {
+    const data = {
+        largeImageKey: "gd-logo",
+        startTimestamp: 1577232000,
+        details: "Anasayfaya göz atıyor.."
+    };
+    if (document.location.pathname == "/" ||
+        document.location.pathname == "/") {
+        data.startTimestamp = browsingStamp;
+        data.details = "Anasayfaya göz atıyor..";
+    }
+    else if (document.location.pathname.includes("/k/oyun-haberleri")) {
+        data.startTimestamp = browsingStamp;
+        data.details = "Oyun Haberlerini inceliyor:";
+    }
+    else if (document.location.pathname.includes("/k/donanim")) {
+        data.startTimestamp = browsingStamp;
+        data.details = "Donanım Haberlerini göz atıyor:";
+    }
+    else if (document.location.pathname.includes("/giveaway")) {
+        data.startTimestamp = browsingStamp;
+        data.details = "Viewing Giveaways";
+    }
+    else if (document.location.pathname.includes("/k/inceleme/")) {
+        data.startTimestamp = browsingStamp;
+        data.details = "Oyun İncelemelerine göz atıyor:";
+    }
+    else if (document.location.pathname.includes("/gizlilik-politikasi/")) {
+        data.startTimestamp = browsingStamp;
+        data.details = "Gizlilik Politasını okuyor:";
+    }
+    else if (document.location.pathname.includes("/editor-ol/")) {
+        data.startTimestamp = browsingStamp;
+        data.details = "Editör Başvuru Formuna bakıyor:";
+    }
+    
+
+    if (data.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    }
+    else {
+        presence.setActivity(data);
+    }
+});
