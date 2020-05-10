919fc7f0053dd57a3db0e37fa49ed4d19e16ae3d
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-05-11 00:38:32 +0200

    :rocket: temp

--- websites/D/temp/presence.ts
+++ websites/D/temp/presence.ts
@@ -1,25 +1,22 @@
-var presence = new Presence({
-    clientId: "642728621596999690"
-  }),
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
-var browsingStamp = Math.floor(Date.now() / 1000);
-var title, views, air, air2, title2;
-var iFrameVideo, currentTime, duration, paused;
-var video, videoDuration, videoCurrentTime;
-var lastPlaybackState = null;
-var playback;
-if (lastPlaybackState != playback) {
-  lastPlaybackState = playback;
-  browsingStamp = Math.floor(Date.now() / 1000);
-}
+const presence = new Presence({
+  clientId: "642728621596999690"
+});
+
+let browsingStamp = Math.floor(Date.now() / 1000),
+  lastPlaybackState,
+  playback;
 
 presence.on("UpdateData", async () => {
-  let presenceData = {
+  const presenceData: presenceData = {
     largeImageKey: "logo"
   };
+
+  if (lastPlaybackState != playback) {
+    lastPlaybackState = playback;
+    browsingStamp = Math.floor(Date.now() / 1000);
+  }
+
+  presenceData.startTimestamp = browsingStamp;
 
   if (document.location.pathname == "/") {
     presenceData.details = "Browsing in mainpage...";
@@ -36,16 +33,10 @@ presence.on("UpdateData", async () => {
     presenceData.details = "Edit profile";
     presence.setActivity(presenceData);
   } else if (document.location.pathname.includes("/p/")) {
-    nameofprofile = document.location.pathname.split("/");
+    const nameofprofile = document.location.pathname.split("/");
     presenceData.details = "Viewing profile";
     presenceData.state = "dsc.bio/" + nameofprofile[2];
     presence.setActivity(presenceData);
   }
   presence.setActivity(presenceData);
 });
-
-function getTimestamps(videoTime, videoDuration) {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
-  return [Math.floor(startTime / 1000), endTime];
-}
