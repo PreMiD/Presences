989e823d1829d4d63cc1887d8706ad26bb82393f
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-05-11 00:06:21 +0200

    :recycle: Virgin Media now in TypeScript

--- websites/V/Virgin Media/dist/presence.js
+++ websites/V/Virgin Media/dist/presence.js
@@ -1,25 +1,21 @@
-var presence = new Presence({
-  clientId: "630505174032449537"
+const presence = new Presence({
+    clientId: "630505174032449537"
 });
-
 presence.on("UpdateData", () => {
-  let presenceData = {
-    largeImageKey: "virginmedia"
-  };
-
-  let pageTitle = document.title;
-  let currentPage;
-
-  presenceData.details = "Viewing a page";
-
-  if (pageTitle.includes("Virgin Media - ")) {
-    currentPage = "Homepage";
-    presenceData.state = currentPage;
-  } else {
-    currentPage = pageTitle.substring(0, pageTitle.length - 15);
-    presenceData.state = currentPage;
-  }
-
-  presence.setActivity(presenceData);
-  presence.setTrayTitle();
+    const presenceData = {
+        largeImageKey: "virginmedia"
+    };
+    const pageTitle = document.title;
+    let currentPage;
+    presenceData.details = "Viewing a page";
+    if (pageTitle.includes("Virgin Media - ")) {
+        currentPage = "Homepage";
+        presenceData.state = currentPage;
+    }
+    else {
+        currentPage = pageTitle.substring(0, pageTitle.length - 15);
+        presenceData.state = currentPage;
+    }
+    presence.setActivity(presenceData);
 });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJlc2VuY2UuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9wcmVzZW5jZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxNQUFNLFFBQVEsR0FBRyxJQUFJLFFBQVEsQ0FBQztJQUM1QixRQUFRLEVBQUUsb0JBQW9CO0NBQy9CLENBQUMsQ0FBQztBQUVILFFBQVEsQ0FBQyxFQUFFLENBQUMsWUFBWSxFQUFFLEdBQUcsRUFBRTtJQUM3QixNQUFNLFlBQVksR0FBaUI7UUFDakMsYUFBYSxFQUFFLGFBQWE7S0FDN0IsQ0FBQztJQUVGLE1BQU0sU0FBUyxHQUFHLFFBQVEsQ0FBQyxLQUFLLENBQUM7SUFDakMsSUFBSSxXQUFXLENBQUM7SUFFaEIsWUFBWSxDQUFDLE9BQU8sR0FBRyxnQkFBZ0IsQ0FBQztJQUV4QyxJQUFJLFNBQVMsQ0FBQyxRQUFRLENBQUMsaUJBQWlCLENBQUMsRUFBRTtRQUN6QyxXQUFXLEdBQUcsVUFBVSxDQUFDO1FBQ3pCLFlBQVksQ0FBQyxLQUFLLEdBQUcsV0FBVyxDQUFDO0tBQ2xDO1NBQU07UUFDTCxXQUFXLEdBQUcsU0FBUyxDQUFDLFNBQVMsQ0FBQyxDQUFDLEVBQUUsU0FBUyxDQUFDLE1BQU0sR0FBRyxFQUFFLENBQUMsQ0FBQztRQUM1RCxZQUFZLENBQUMsS0FBSyxHQUFHLFdBQVcsQ0FBQztLQUNsQztJQUVELFFBQVEsQ0FBQyxXQUFXLENBQUMsWUFBWSxDQUFDLENBQUM7QUFDckMsQ0FBQyxDQUFDLENBQUMifQ==
