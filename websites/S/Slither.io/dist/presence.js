8dd23024b96ba859a707285058d6471fdd786416
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-05-10 23:05:28 +0200

    :recycle: Slither.io now in TypeScript

--- websites/S/Slither.io/dist/presence.js
+++ websites/S/Slither.io/dist/presence.js
@@ -1,18 +1,16 @@
-var presence = new Presence({
-  clientId: "630783537221468182"
+const presence = new Presence({
+    clientId: "630783537221468182"
 });
-
 presence.on("UpdateData", () => {
-  let presenceData = {
-    largeImageKey: "slitherlogo"
-  };
-  if (document.querySelector('[style="opacity: .8; font-weight: bold;"]')) {
-    let length = document.querySelector(
-      '[style="opacity: .8; font-weight: bold;"]'
-    ).innerHTML;
-    let rank = document.querySelector('[style="opacity: .35;"]').innerHTML;
-    presenceData.details = "Length: " + length;
-    presenceData.state = "Rank: " + rank;
-  }
-  presence.setActivity(presenceData);
+    const presenceData = {
+        largeImageKey: "slitherlogo"
+    };
+    if (document.querySelector('[style="opacity: .8; font-weight: bold;"]')) {
+        const length = document.querySelector('[style="opacity: .8; font-weight: bold;"]').innerHTML;
+        const rank = document.querySelector('[style="opacity: .35;"]').innerHTML;
+        presenceData.details = "Length: " + length;
+        presenceData.state = "Rank: " + rank;
+    }
+    presence.setActivity(presenceData);
 });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJlc2VuY2UuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9wcmVzZW5jZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxNQUFNLFFBQVEsR0FBRyxJQUFJLFFBQVEsQ0FBQztJQUM1QixRQUFRLEVBQUUsb0JBQW9CO0NBQy9CLENBQUMsQ0FBQztBQUVILFFBQVEsQ0FBQyxFQUFFLENBQUMsWUFBWSxFQUFFLEdBQUcsRUFBRTtJQUM3QixNQUFNLFlBQVksR0FBaUI7UUFDakMsYUFBYSxFQUFFLGFBQWE7S0FDN0IsQ0FBQztJQUVGLElBQUksUUFBUSxDQUFDLGFBQWEsQ0FBQywyQ0FBMkMsQ0FBQyxFQUFFO1FBQ3ZFLE1BQU0sTUFBTSxHQUFHLFFBQVEsQ0FBQyxhQUFhLENBQ25DLDJDQUEyQyxDQUM1QyxDQUFDLFNBQVMsQ0FBQztRQUNaLE1BQU0sSUFBSSxHQUFHLFFBQVEsQ0FBQyxhQUFhLENBQUMseUJBQXlCLENBQUMsQ0FBQyxTQUFTLENBQUM7UUFDekUsWUFBWSxDQUFDLE9BQU8sR0FBRyxVQUFVLEdBQUcsTUFBTSxDQUFDO1FBQzNDLFlBQVksQ0FBQyxLQUFLLEdBQUcsUUFBUSxHQUFHLElBQUksQ0FBQztLQUN0QztJQUVELFFBQVEsQ0FBQyxXQUFXLENBQUMsWUFBWSxDQUFDLENBQUM7QUFDckMsQ0FBQyxDQUFDLENBQUMifQ==
