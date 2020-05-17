0257846ba1e2abe261fd8b33801d00ea46eeca0c
Nanashin <63855357+Nanashinn@users.noreply.github.com>
2020-05-17 17:10:20 -0300

    New Presence (Animes House)

---
+++ websites/A/Animes House/presence.ts
@@ -0,0 +1,107 @@
+var presence = new Presence ({
+    clientId: "711660622621966398"
+});
+
+let browsingStamp = Math.floor(Date.now() / 1000);
+
+function getTimestamps(videoTime, videoDuration) {
+    var startTime = Date.now();
+    var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+    return [Math.floor(startTime / 1000), endTime]; 
+};
+
+let currentTime: any, duration: any, paused: any, played: any, timestamps: any, pesquisaText: any = document.querySelector("#s"), paginaText: any = document.querySelector("#contenedor > div.module > div.content > div.pagination > span:nth-child(1)"), nomeObraText: any = document.querySelector("#single > div.content > div.sheader > div.data > h1"), episodioText: any = document.querySelector("#info"), lancamentoText: any = document.querySelector("#single > div.content > div.sheader > div.data > div.extra > span.date"), generoText: any = document.querySelector("#contenedor > div.module > div.content > header > h1"), nomeObraEpisodioText: any = document.querySelector("#info > h1"), episodioEpisodioText: any = document.querySelector("#info > div > h3"), filmeNomeText: any = document.querySelector("#single > div.content > div.sheader > div.data > h1"), novoPedidoText: any = document.querySelector("#contenedor > div > nav > ul.requests_menu_filter > li > a.active"), contaText: any = document.querySelector("#contenedor > div > nav > ul > li > a.selected");
+
+presence.on("iFrameData", (data) => {
+    currentTime = data.currentTime;
+    duration = data.duration;
+    paused = data.paused;
+    played = data.played;
+});
+
+presence.on("UpdateData", async () => {
+    const presenceData: presenceData = {
+        largeImageKey: "logo"
+    };
+
+    const path = document.location.pathname;
+
+    if (path == '/') {
+        if (document.title.includes('Resultados da pesquisa por ')) {
+            presenceData.details = 'Pesquisando por: ';
+            presenceData.state = pesquisaText.value;
+            presenceData.startTimestamp = browsingStamp;
+        } else {
+            presenceData.details = 'Pagina inícial';
+            presenceData.startTimestamp = browsingStamp;
+        }
+    } else if (path.includes('anime')) {
+        (path.split('/').length - 1) == 2 || (path.split('/').length - 1) == 4 ? (
+            presenceData.details = 'Lista de animes',
+            presenceData.state = paginaText.innerText,
+            presenceData.startTimestamp = browsingStamp
+        ) : (
+            presenceData.details = nomeObraText.innerText,
+            presenceData.state = lancamentoText.innerText,
+            presenceData.startTimestamp = browsingStamp
+        )
+    } else if (path.includes('generos')) {
+        presenceData.details = 'Gênero: ' + generoText.innerText;
+        presenceData.state = paginaText.innerText;
+        presenceData.startTimestamp = browsingStamp;
+    } else if (path.includes('episodio')) {
+        presenceData.details = nomeObraEpisodioText.innerText;
+        presenceData.state = episodioEpisodioText.innerText;
+        presenceData.smallImageKey = '';
+        presenceData.smallImageText = '';
+        if (played) {
+            !paused ? (
+                timestamps = getTimestamps(Math.floor(currentTime), Math.floor(duration)),
+                presenceData.startTimestamp = timestamps[0],
+                presenceData.endTimestamp = timestamps[1],
+                presenceData.smallImageKey = 'play',
+                presenceData.smallImageText = 'Assistindo'
+            ) : (
+                presenceData.smallImageKey = 'pause',
+                presenceData.smallImageText = 'Pausado'
+            )
+        }; 
+    } else if (path.includes('filme')) {
+        if ((path.split('/').length - 1) == 2 || (path.split('/').length - 1) == 4) {
+            presenceData.details = 'Lista de filmes';
+            presenceData.state = paginaText.innerText;
+            presenceData.startTimestamp = browsingStamp;
+        } else {
+            presenceData.details = 'Assistindo um filme';
+            presenceData.state = filmeNomeText.innerText;
+            if (played) {
+                (!paused) ? (
+                    timestamps = getTimestamps(Math.floor(currentTime), Math.floor(duration)),
+                    presenceData.startTimestamp = timestamps[0],
+                    presenceData.endTimestamp = timestamps[1],
+                    presenceData.smallImageKey = 'play',
+                    presenceData.smallImageText = 'Assistindo'
+                ) : (
+                    presenceData.smallImageKey = 'pause',
+                    presenceData.smallImageText = 'Pausado'
+                )
+            };
+        };
+    } else if (path.includes('/pedidos')) {
+        presenceData.details = 'Página de pedidos';
+        presenceData.startTimestamp = browsingStamp;
+        if (document.querySelector("div.discover.hidde.show")) {
+            presenceData.state = 'Fazendo um novo pedido...';
+        }
+    } else if (path.includes('/calendario')) {
+        presenceData.details = 'Calendário de lançamentos';
+        presenceData.startTimestamp = browsingStamp;
+    } else if (path.includes('/account')) {
+        presenceData.details = 'Minha Conta';
+        presenceData.state = contaText.innerText;
+        presenceData.startTimestamp = browsingStamp;
+    } else {
+        presenceData.details = 'Navegando...';
+    }
+    presence.setActivity(presenceData);
+});
