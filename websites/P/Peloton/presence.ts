7602a0472eb9c2d3919d625823035d62819d0e5d
The Gamerzs <thgamerzs74@gmail.com>
2020-05-20 15:56:35 +0100

    :sparkles: Peloton Presence

--- websites/P/Peloton/presence.ts
+++ websites/P/Peloton/presence.ts
@@ -3,9 +3,17 @@ const presence = new Presence({
 });
 
 let path;
+let strings;
+let clipTitle;
+let clipAuthor;
 const browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
+    strings = await presence.getStrings({
+        live: "presence.activity.live",
+        play: "presence.playback.playing",
+        pause:  "presence.playback.paused"
+    });
     const presenceData: presenceData = {
         largeImageKey: "peloton"
     };
@@ -14,32 +22,78 @@ presence.on("UpdateData", async () => {
         switch(window.location.pathname){
             case "/":
                 presenceData.startTimestamp = browsingStamp;
-                presenceData.details = "Veiwing the Home Page"
+                presenceData.details = "Browsing the Home Page";
                 break;
             case "/bike":
                 presenceData.startTimestamp = browsingStamp;
-                presenceData.details = "Browsing the bikes"
+                presenceData.details = "Browsing the bikes";
                 break;
             case "/app":
                 presenceData.startTimestamp = browsingStamp;
-                presenceData.details = "Viewing the app"
+                presenceData.details = "Browsing the app";
                 break;
             case "/membership":
                 presenceData.startTimestamp = browsingStamp;
-                presenceData.details = "Browsing memberships"
+                presenceData.details = "Browsing memberships";
                 break;
             case "/terms":
                 presenceData.startTimestamp = browsingStamp;
-                presenceData.details = "Viewing the terms of service"
+                presenceData.details = "Browsing the terms of service";
+                break;
+            case "/company":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing Peloton's Mission";
+                break;
+            case "/showrooms":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing the showrooms";
+                break;
+            case "/membership-terms":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing the membership terms";
                 break;
         }
     }
 
-    if(window.location.hostname.includes("members.onepeloton")){
+    else if(window.location.hostname.includes("support.onepeloton")){
+        presenceData.startTimestamp = browsingStamp;
+        presenceData.details = "Browsing the support pages";
+    }
+
+    else if(window.location.hostname.includes("blog.onepeloton")){
+        switch(window.location.pathname){
+            case "/":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing the blogs";
+                break;
+            case "/category/community/":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing the community blogs";
+                break;
+            case "/category/lifestyle/":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing the lifestyle blogs";
+                break;
+            case "/category/news/":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing the news blogs";
+                break;
+            case "/category/tech/":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing the tech blogs";
+                break;
+            case "/category/wellness/":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Browsing the wellness blogs";
+                break;
+        }
+    }
+
+    else if(window.location.hostname.includes("members.onepeloton")){
         //Class categories
         if(window.location.pathname == "/classes"){
             presenceData.startTimestamp = browsingStamp;
-            presenceData.details = "Browsing classes";
+            return presenceData.details = "Browsing classes";;
         }
 
         //Class category
@@ -48,53 +102,89 @@ presence.on("UpdateData", async () => {
             switch(path){
                 case "strength":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing strength classes"
+                    presenceData.details = "Browsing strength classes";
                     break;
                 case "yoga":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing yoga classes"
+                    presenceData.details = "Browsing yoga classes";
                     break;
                 case "meditation":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing meditation classes"
+                    presenceData.details = "Browsing meditation classes";
                     break;
                 case "cardio":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing cardio classes"
+                    presenceData.details = "Browsing cardio classes";
                     break;
                 case "stretching":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing stretching classes"
+                    presenceData.details = "Browsing stretching classes";
                     break;
                 case "cycling":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing cycling classes"
+                    presenceData.details = "Browsing cycling classes";
                     break;
                 case "outdoor":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing outdoor classes"
+                    presenceData.details = "Browsing outdoor classes";
                     break;
                 case "running":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing running classes"
+                    presenceData.details = "Browsing running classes";
                     break;
                 case "walking":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing walking classes";
+                    presenceData.details = "Browsing walking classes";;
                     break;
                 case "bootcamp":
                     presenceData.startTimestamp = browsingStamp;
-                    presenceData.details = "Browsing bootcamp classes";
+                    presenceData.details = "Browsing bootcamp classes";;
                     break;
             }
 
             //Video
             if(path.includes("player/")){
-                console.log("Here1");
-                var video: HTMLVideoElement = document.querySelector(".jw-media jw-reset > video");
-                presenceData.endTimestamp = new Date(Date.now() + (video.duration - video.currentTime) * 1000).getTime();
-                presenceData.details = "Browsing classes";
+                var video: HTMLVideoElement = document.querySelector(".jw-video");
+                clipTitle = document.querySelector(".jw-controlbar > div:nth-child(3) > div > div > div > h1").textContent.replace("&amp;", "&");
+                clipAuthor = document.querySelector(".jw-controlbar > div:nth-child(3) > div > div > div > p").textContent.split("<");
+                
+                switch(!video.paused){
+                    case true:
+                        presenceData.smallImageKey = 'play';
+                        presenceData.smallImageText = strings.play;
+                        presenceData.endTimestamp = new Date(Date.now() + (video.duration - video.currentTime) * 1000).getTime();
+                        break;
+                    case false:
+                        presenceData.smallImageKey = 'pause';
+                        presenceData.smallImageText = strings.pause;
+                        presenceData.endTimestamp = null;
+                        break;
+                }
+                presenceData.details = clipTitle;
+                presenceData.state = clipAuthor[0];
             }
+        }
+
+        //Live
+        if(window.location.pathname.includes("/player/live")){
+            var video: HTMLVideoElement = document.querySelector(".jw-video");
+            if(clipTitle = document.querySelector(".jw-controlbar > div:nth-child(3) > div > div > div > h1")){}
+            else{clipTitle = null;}
+            if(clipAuthor = document.querySelector(".jw-controlbar > div:nth-child(3) > div > div > div > p").textContent.split("<")){}
+            else{clipAuthor = null;}
+            
+            switch(!video.paused){
+                case true:
+                    presenceData.smallImageKey = 'live';
+                    presenceData.smallImageText = strings.live;                   
+                    break;
+                case false:
+                    presenceData.smallImageKey = 'live';
+                    presenceData.smallImageText = strings.live;
+                    break;
+            }
+            presenceData.details = clipTitle.textContent.replace("&amp;", "&");
+            presenceData.state = clipAuthor[0];
         }
 
         //schedule
@@ -195,9 +285,9 @@ presence.on("UpdateData", async () => {
             presenceData.startTimestamp = browsingStamp;
             presenceData.details = "Browsing their profile";
         }
+
         
     } else {
-        presenceData.startTimestamp = browsingStamp;
         presenceData.details = "Browsing Peloton";
     }
 
@@ -207,5 +297,4 @@ presence.on("UpdateData", async () => {
     } else {
         presence.setActivity(presenceData);
     }
-    console.log(window.location);
 });
