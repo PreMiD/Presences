cbdc99191480875cd530aff1c2cb7f3ba84704c1
PeeQueno <65591076+PeeQueno@users.noreply.github.com>
2020-05-22 21:49:51 -0300

    Update presence.ts

--- websites/M/MixMods/presence.ts
+++ websites/M/MixMods/presence.ts
@@ -1,10 +1,10 @@
-var presence = new Presence({
+const presence = new Presence({
   clientId: "706574162331697163"
 });
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "mlogo"
+    largeImageKey: "logo"
   };
 
   if (document.location.pathname == "/") {
@@ -17,27 +17,27 @@ presence.on("UpdateData", async () => {
       switch(document.location.pathname){
         case "/p/about.html":
             presenceData.details = "Lendo o Sobre Nós";
-        break
+        break;
         case "/p/lista-de-crash-e-solucoes.html":
             presenceData.details = "Lendo a Lista de Crash e Soluções";
-        break
+        break;
         case "/p/recomendados.html":
             presenceData.details = "Vendo os Recomendados";
-        break
+        break;
         case "/p/disclaimer.html":
             presenceData.details = "Lendo o Disclaimer";
-        break
-    }
+        break;
+    };
   } else if (document.getElementsByClassName("label-info breadcrumbs")[0]) {
       presenceData.details = "Vendo uma postagem";
-      let name = document.getElementsByClassName("post-title entry-title")[0].textContent;
+      const name = document.getElementsByClassName("post-title entry-title")[0].textContent;
       presenceData.state = name;
   } else { 
       presenceData.details = ("Navegando no site");
-      var url = document.location.href.split("#")[1];
-      var text = url.split("=")[1];
-      presenceData.state = `Página ${text}`
-  }
+      const url = document.location.href.split("#")[1];
+      const text = url.split("=")[1];
+      presenceData.state = `Página ${text}`;
+  };
 
   presence.setActivity(presenceData);
 });
