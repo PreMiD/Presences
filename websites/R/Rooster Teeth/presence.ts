e8fe7a36110dc8f6cae561bf3b16cb2e8f0c8d28
qwerty142 <43281665+chaos511@users.noreply.github.com>
2020-05-24 08:57:12 -0500

    change let to const

--- websites/R/Rooster Teeth/presence.ts
+++ websites/R/Rooster Teeth/presence.ts
@@ -67,11 +67,10 @@ presence.on("UpdateData", () => {
     }
   } else if (path.includes("/schedule")) {
     presenceData.details = "Viewing Schedule";
-    for (let x in document.getElementsByClassName("schedule-day")) {
-      let scheduleDay=document.getElementsByClassName("schedule-day")[x];
-      var position = scheduleDay.getBoundingClientRect();
+    for (const x in document.getElementsByClassName("schedule-day")) {
+      var position = document.getElementsByClassName("schedule-day")[x].getBoundingClientRect();
       if (position.top < window.innerHeight && position.bottom >= 0) {
-        presenceData.state = scheduleDay
+        presenceData.state = document.getElementsByClassName("schedule-day")[x]
           .querySelector(".schedule-day__heading")
           .textContent.toLowerCase();
         presenceData.state =
