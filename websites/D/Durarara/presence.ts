7ad0ef4944f77eb2998018ab878284cfb0555e0d
Dawkin Nguyen <dawkinnguyen@gmail.com>
2020-05-25 00:37:47 +0700

    fix bug :D

--- websites/D/Durarara/presence.ts
+++ websites/D/Durarara/presence.ts
@@ -13,13 +13,13 @@ presence.on("UpdateData", async () => {
     presenceData.startTimestamp = browsingStamp;
     if (document.location.pathname.includes("/lounge")) {
       presenceData.details = "Searching for a room";
-      presenceData.state = "Username: " + document.querySelector(".name").textContent;;
+      presenceData.state = "Username: " + document.querySelector(".name").textContent;
       presenceData.smallImageKey = "search";
     } else if (document.location.pathname.includes("/create_room")) {
       presenceData.details = "Creating a room";
     } else if (document.location.pathname.includes("/room/")) {
-      presenceData.details = "In a room: " + document.querySelector(".room-title-name").textContent;;
-      presenceData.state = "Members: " + document.querySelector(".room-title-capacity").textContent;;
+      presenceData.details = "In a room: " + document.querySelector(".room-title-name").textContent;
+      presenceData.state = "Members: " + document.querySelector(".room-title-capacity").textContent;
       presenceData.smallImageKey = "chat";
     }
   }
