5187e3a795768b5e989fb06b2c97d52b41f95373
outapzaza <40026698+outapzaza@users.noreply.github.com>
2020-05-24 21:16:12 +0700

    A litle change 2

--- websites/A/Anime-Sugoi/presence.ts
+++ websites/A/Anime-Sugoi/presence.ts
@@ -2,6 +2,7 @@ const presence = new Presence({
   clientId: "709308577701036074"
 });
 
+// Const thing
 const browsingStamp = Math.floor(Date.now() / 1000);
 const title = document.querySelector("body > div:nth-child(3) > div > div.col-lg-9 > div > div.panel-heading > h3");
 const ep = document.querySelector("body > div:nth-child(3) > div > div.col-lg-9 > div > div.panel-body > center:nth-child(2) > h3");
@@ -18,25 +19,34 @@ const presenceData: PresenceData = {
   largeImageKey: "icon"
 };
 
-
+// Presence
 if (path.hostname == "anime-sugoi.com" || path.hostname.includes ("www.")) {
   if (document.location.pathname == "/") {
     presenceData.startTimestamp = browsingStamp;
-    presenceData.details = title1;
+    presenceData.details = "อนิเมะอัพเดตล่าสุด";
   } else if (path.pathname.includes ("index.html")) {
     presenceData.startTimestamp = browsingStamp;
-    presenceData.details = title1;
+    presenceData.details = "อนิเมะอัพเดตล่าสุด";
   } else if (path.pathname.includes ("catalog")) {
     presenceData.startTimestamp = browsingStamp;
-    presenceData.details = "หมวดหมู่ " + title1;
+    presenceData.details = "หมวดหมู่ ";
+    presenceData.state = title1;
+  } else if (path.pathname.includes ("tag")) {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "หมวดหมู่ ";
+    presenceData.state = title1;
+  } else if (path.search.includes ("search")) {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "ค้นหา ";
+    presenceData.state = title1;
   } else if (path.pathname.includes ("play")) {
     presenceData.startTimestamp = browsingStamp;
-    presenceData.details = "กำลังดู " + title1;
+    presenceData.details = "รับชม " + title1;
     presenceData.state = subauthorplay1;
   } else if (path.href) {
     presenceData.startTimestamp = browsingStamp;
-    presenceData.details = "กำลังเลือกตอน " + ep1;
-    presenceData.state = subauthor1;
+    presenceData.details = "เลือกตอน ";
+    presenceData.state = ep1;
   }
 }
 
