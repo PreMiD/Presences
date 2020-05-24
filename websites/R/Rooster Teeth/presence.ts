9cd657e0475ab63ee75f8baab6b6f106030fe9c7
qwerty142 <43281665+chaos511@users.noreply.github.com>
2020-05-24 08:35:49 -0500

    fix error 'Type 'HTMLCollectionOf<Element>' is not an array type or a string type.'

--- websites/R/Rooster Teeth/presence.ts
+++ websites/R/Rooster Teeth/presence.ts
@@ -67,7 +67,8 @@ presence.on("UpdateData", () => {
     }
   } else if (path.includes("/schedule")) {
     presenceData.details = "Viewing Schedule";
-    for (var scheduleDay of document.getElementsByClassName("schedule-day")) {
+    for (var x in document.getElementsByClassName("schedule-day")) {
+      var scheduleDay=document.getElementsByClassName("schedule-day")[x]
       var position = scheduleDay.getBoundingClientRect();
       if (position.top < window.innerHeight && position.bottom >= 0) {
         presenceData.state = scheduleDay
