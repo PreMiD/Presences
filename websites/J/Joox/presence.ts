1d6c39e6df2e6def19887f17a0a1a448c4b88b10
Yusaki <40026698+Yusaki-Akito@users.noreply.github.com>
2020-05-27 16:00:53 +0700

    Joox

---
+++ websites/J/Joox/presence.ts
@@ -0,0 +1,79 @@
+const presence = new Presence({
+  clientId: "715116675346989096"
+});
+
+function getTime(list: string[]): number {
+  let ret = 0;
+  for (let index = list.length - 1; index >= 0; index--) {
+    ret += parseInt(list[index]) * 60 ** index;
+  }
+  return ret;
+}
+
+function getTimestamps(
+  audioTime: string,
+  audioDuration: string
+): Array<number> {
+  const splitAudioTime = audioTime.split(":").reverse();
+  const splitAudioDuration = audioDuration.split(":").reverse();
+
+  const parsedAudioTime = getTime(splitAudioTime);
+  const parsedAudioDuration = getTime(splitAudioDuration);
+
+  const startTime = Date.now();
+  const endTime =
+    Math.floor(startTime / 1000) - parsedAudioTime + parsedAudioDuration;
+  return [Math.floor(startTime / 1000), endTime];
+}
+
+presence.on("UpdateData", async () => {
+  const player = document.querySelector("#__next > div.sc-bdVaJa.izSScG > div.sc-bwzfXH.kIpMXu > div.sc-EHOje.kLlKoV > div.sc-gzVnrw.jgaZgQ > button.sc-htpNat.jEJMuB > i").getAttribute("class");
+
+  if (player) {
+
+    const paused = player.includes("pause") === false;
+
+    try {
+      var title = document.querySelector("#__next > div.sc-bdVaJa.izSScG > div.sc-bwzfXH.kIpMXu > div.sc-htoDjs.frIPNj > div.sc-dnqmqq.hGsYSo > div > div.sc-cJSrbW.hSnhBW > b > a").textContent;
+      var author = document.querySelector("#__next > div.sc-bdVaJa.izSScG > div.sc-bwzfXH.kIpMXu > div.sc-htoDjs.frIPNj > div.sc-dnqmqq.hGsYSo > div > div.sc-cJSrbW.hSnhBW > span").textContent;
+      var audioTime = document.querySelector("#currentTime").textContent;
+      var audioDuration = document.querySelector("#__next > div.sc-bdVaJa.izSScG > div.sc-bwzfXH.kIpMXu > div.sc-htoDjs.frIPNj > small.sc-iwsKbI.sc-gqjmRU.GeFxq").textContent;
+      var timestamps = getTimestamps(audioTime, audioDuration);
+    } catch (err) {
+      console.log(
+        "Oof error?!:"
+      );
+      console.error(err);
+    }
+
+    const data: PresenceData = {
+      details: title,
+      state: author,
+      largeImageKey: "icon",
+      smallImageKey: paused ? "pause" : "playing",
+      smallImageText: paused ? "Paused" : "Playing",
+      startTimestamp: timestamps[0],
+      endTimestamp: timestamps[1]
+    };
+
+    //if (player.includes("radio")) {
+    // data.details = title;
+    //  data.state = author;
+    //  data.largeImageKey = "icon";
+    //  data.smallImageKey = "playing";
+    //  data.smallImageText = "Radio";
+    //  presence.setActivity(data);
+    //}
+
+    if (paused) {
+      delete data.startTimestamp;
+      delete data.endTimestamp;
+    }
+
+    if (title !== null && author !== null) {
+      presence.setActivity(data, !paused);
+    }
+  } else {
+    presence.clearActivity();
+  }
+});
