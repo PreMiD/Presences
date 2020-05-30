61f2faa556b072ec539fafc75c80723e29600a44
Limerio <44976231+Limerio@users.noreply.github.com>
2020-05-30 19:08:27 +0200

    Create presence.ts
    New status in PerMID, this website is Discord Permissions Calculator

---
+++ websites/D/Discord Permissions Calculator/presence.ts
@@ -0,0 +1,27 @@
+var presence = new Presence({
+    clientId: "716334859039277087"
+});
+
+presence.on("UpdateData", async() => {
+    let presenceData: presenceData = {
+        largeImageKey: "logo"
+    }
+
+    if(document.location.pathname.includes("/permissions.html")) {
+        presenceData.details = "Discord Permissions Calculator";
+    }
+
+    let route = document.location.hash.split("/permissions.html");
+
+    if(route[0]) {
+        presenceData.details = "Settings the permissions of her bot.";
+        presenceData.state = `Permissions : ${route[1] ? document.querySelector("div.result > p > span.permissions > code#number").textContent : 0}`;
+    }
+
+    if (presenceData.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    } else {
+        presence.setActivity(presenceData);
+    }
+})
