f1b171b1a932e417cc45d2d23d2dfc2c35ed4073
frozaum <finnzzingames758@gmail.com>
2020-06-04 23:41:06 -0300

    stuff to not make deepscan mad

--- websites/0-9/8muses/presence.ts
+++ websites/0-9/8muses/presence.ts
@@ -23,9 +23,9 @@ presence.on("UpdateData", async () => {
     } else if (document.location.pathname.includes("/category/")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Viewing a category:";
-      presenceData.state = (<HTMLAnchorElement> document.querySelector("#top-menu > div.top-menu-breadcrumb > ol > li:nth-child(2) > a")).text;
+      presenceData.state = (document.querySelector("#top-menu > div.top-menu-breadcrumb > ol > li:nth-child(2) > a")as HTMLAnchorElement).text;
     } else if (document.location.href.includes("/#")) {
-      const comicName = (<HTMLMetaElement> document.querySelector("head > meta:nth-child(17)")).content;
+      const comicName = (document.querySelector("head > meta:nth-child(17)")as HTMLMetaElement).content;
       const issueName = document.location.pathname.split('/')[2].replace(/_/g, " ");
       const issueNumber = document.querySelector("#left-menu > ol > li:nth-child(3) > div > span").textContent.trim();
       if (document.location.pathname.split('/')[2].includes('')) {
