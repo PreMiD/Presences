0affd2312b9f2fd95cad8b89265d26bef412ddae
Holfz <holfzcompilation@gmail.com>
2020-06-09 18:56:38 +0700

    AIS Play: Inital commit

---
+++ websites/A/AIS Play/iframe.ts
@@ -0,0 +1,14 @@
+const iframe = new iFrame();
+
+iframe.on("UpdateData", async () => {
+    let video: HTMLVideoElement = document.querySelector("#vimmi_video_player_html5_api");
+    
+    if (video != undefined && !isNaN(video.duration)) {
+        iframe.send({
+            current: video.currentTime,
+            duration: video.duration,
+            paused: video.paused,
+            isLive: !document.querySelector(".vjs-live-control").classList.contains("vjs-hidden")
+        });
+    }
+});
