1be0ccc6a093d3b7cd8418ac700678e960bbe520
Brandon Liang <54skyxenon@gmail.com>
2020-06-08 23:03:59 -0700

    Works well so far, except no image :(

--- websites/L/LeetCode/presence.ts
+++ websites/L/LeetCode/presence.ts
@@ -1,72 +1,40 @@
-var presence = new Presence({
-    // The client ID of the Application created at https://discordapp.com/developers/applications
+// Application ID on Discord developer page
+const presence = new Presence({
     clientId: "719373053028728894"
-}),
+});
 
-    // contains strings for our presence
-    strings = presence.getStrings({
-        play: "presence.playback.playing",
-        pause: "presence.playback.paused"
-    });
+// time spent on current URL
+var timeElapsed :number = new Date().getTime()
 
 presence.on("UpdateData", async () => {
-    var example: PresenceData = {
-        largeImageKey: "logo", // metadata key of the large image on the presence
-        smallImageText: "Some hover text", // the text which is displayed when hovering over the small image
-        details: questionTitle, // The upper section of the presence text
-        state: "Reading section A", // The lower section of the presence text
-        startTimestamp: 1577232000, //The unix epoch timestamp for when to start counting from
+    // default settings
+    const presenceData: PresenceData = {
+        details: document.title,
+        largeImageKey: "logo",
+        startTimestamp: timeElapsed
     };
 
+    // edit attributes based on path
     if (document.location.pathname == "/") {
-        const homepagePresence: PresenceData = {
-            details: "Homepage",
-            largeImageKey: "logo"
-        };
-        presence.setActivity(homepagePresence);
+        presenceData.details = "Homepage";
     } else if (document.location.pathname.startsWith("/problemset")) {
-        const presenceData: PresenceData = {
-            details: "Viewing Problems",
-            largeImageKey: "logo"
-        };
-        presence.setActivity(presenceData);
+        presenceData.details = "Viewing Problems";
     } else if (document.location.pathname.startsWith("/problems")) {
-        var questionTitle: string = document.getElementsByClassName("css-v3d350")[0].innerHTML
-        const presenceData: PresenceData = {
-            details: questionTitle,
-            state: "#Question Number, Difficulty",
-            largeImageKey: "logo"
-        };
-        presence.setActivity(presenceData);
+        presenceData.details = document.querySelectorAll("div[data-cy=question-title]")[0].textContent;
+        presenceData.state = document.querySelectorAll('div[diff]')[0].textContent;
     } else if (document.location.pathname.startsWith("/explore")) {
-        const presenceData: PresenceData = {
-            details: "Explore",
-            state: "Visiting Explore Modules",
-            largeImageKey: "logo"
-        };
-        presence.setActivity(presenceData);
+        presenceData.details = "Explore";
+
+        if (document.getElementsByClassName("question-title")) {
+            presenceData.state = document.getElementsByClassName("question-title")[0].textContent;
+        }
     } else if (document.location.pathname.startsWith("/articles")) {
-        const presenceData: PresenceData = {
-            details: "Reading Solutions",
-            largeImageKey: "logo"
-        };
-        presence.setActivity(presenceData);
+        presenceData.details = "Reading Solutions";
     } else if (document.location.pathname.startsWith("/discuss")) {
-        const presenceData: PresenceData = {
-            details: "Browsing Forums",
-            largeImageKey: "logo"
-        };
-        presence.setActivity(presenceData);
+        presenceData.details = "Browsing Forums";
     } else if (document.location.pathname.startsWith("/interview")) {
-        const presenceData: PresenceData = {
-            details: "Mock Interviewing...",
-            largeImageKey: "logo"
-        };
-        presence.setActivity(presenceData);
-    } else {
-        const presenceData: PresenceData = {
-            details: document.title
-        };
-        presence.setActivity(presenceData);
+        presenceData.details = "Mock Interviewing...";
     }
+
+    presence.setActivity(presenceData);
 });
