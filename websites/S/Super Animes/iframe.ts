7f5a3b8dc1f843d90cbc4bdc6cdbbf9cc038bd49
Nanashin <63855357+Nanashinn@users.noreply.github.com>
2020-06-20 06:09:02 -0300

    Update iframe.ts

--- websites/S/Super Animes/iframe.ts
+++ websites/S/Super Animes/iframe.ts
@@ -1,73 +1,8 @@
 var iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
-    if (document.querySelector("#play0") != null) {
-        const video: HTMLVideoElement = document.querySelector("#play0");
-        if (video != null) {
-            const played = video.duration != 0;
-            iframe.send({
-                currentTime: video.currentTime,
-                duration: video.duration,
-                played,
-                paused: video.paused
-            });
-        }
-    }
-
-    if (document.querySelector("#play1_html5_api") != null) {
-        const video: HTMLVideoElement = document.querySelector("#play1_html5_api");
-        if (video != null) {
-            const played = video.duration != 0;
-            iframe.send ({
-                currentTime: video.currentTime,
-                duration: video.duration,
-                played,
-                paused: video.paused
-            });
-        }
-    }
-
-    if (document.querySelector("#video") != null) {
-        const video: HTMLVideoElement = document.querySelector("#video");
-        if (video != null) {
-            const played = video.duration != 0;
-            iframe.send({
-                currentTime: video.currentTime,
-                duration: video.duration,
-                played,
-                paused: video.paused
-            });
-        }
-    }
-
-    if (document.querySelector("#video-js-video_html5_api") != null) {
-        const video: HTMLVideoElement = document.querySelector("#video-js-video_html5_api");
-        if (video != null) {
-            const played = video.duration != 0;
-            iframe.send({
-                currentTime: video.currentTime,
-                duration: video.duration,
-                played,
-                paused: video.paused
-            });
-        }
-    }
-
-    if (document.querySelector("video") != null) {
-        const video: HTMLVideoElement = document.querySelector("video");
-        if (video != null) {
-            const played = video.duration != 0;
-            iframe.send({
-                currentTime: video.currentTime,
-                duration: video.duration,
-                played,
-                paused: video.paused
-            });
-        }
-    }
-
-    if (document.querySelector("#vstr > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video") != null) {
-        const video: HTMLVideoElement = document.querySelector("#vstr > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video");
+    if (document.querySelector("#play0") != null || document.querySelector("#play1_html5_api") != null || document.querySelector("#video") != null || document.querySelector("#video-js-video_html5_api") != null || document.querySelector("video") != null || document.querySelector("#vstr > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video") != null) {
+        const video: HTMLVideoElement = document.querySelector("#play0") || document.querySelector("#play1_html5_api") || document.querySelector("#video") || document.querySelector("#video-js-video_html5_api") || document.querySelector("video") || document.querySelector("#vstr > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video");
         if (video != null) {
             const played = video.duration != 0;
             iframe.send({
