cb2b27f1effefc97256269d116e3a0165f8c5afe
Eren_1000 <58482989+eren885@users.noreply.github.com>
2020-06-27 19:09:04 +0300

    Update presence.ts

--- websites/T/Tatsumaki/presence.ts
+++ websites/T/Tatsumaki/presence.ts
@@ -6,41 +6,47 @@ presence.on("UpdateData", () => {
   const presenceData: PresenceData = {
     largeImageKey: "logo"
   };
+  
   const browsingStamp = Math.floor(Date.now() / 1000);
-  if (window.location.pathname.endsWith("about")) {
-    presenceData.details = "Hakkında kısmına göz atıyor...";
+  const page = window.location.pathname
+
+  if (page.startsWith("/settings.html")) {
+    presenceData.details = "Viewing a page:";
+    presenceData.state = "Settings";
     presenceData.startTimestamp = browsingStamp;
-  } else if (window.location.pathname.endsWith("settings.html")) {
-    presenceData.details = "Sunucu ayarları sekmesine göz atıyor...";
+  } else if (page.startsWith("/commands.html")) {
+    presenceData.details = "Viewing a page:";
+    presenceData.state = "Commands";
     presenceData.startTimestamp = browsingStamp;
-  } else if (window.location.pathname.endsWith("commands.html")) {
-    presenceData.details = "Tüm komutlara göz atıyor...";
+  } else if (page.startsWith("/faq.html")) {
+    presenceData.details = "Viewing a page:";
+    presenceData.state = "FAQ & Guides";
     presenceData.startTimestamp = browsingStamp;
-  } else if (window.location.pathname.endsWith("faq.html")) {
-    presenceData.details = "Sıkça sorulan sorulara göz atıyor...";
+  } else if (page.startsWith("/about")) {
+    presenceData.details = "Viewing a page:";
+    presenceData.state = "About Tatsumaki";
     presenceData.startTimestamp = browsingStamp;
-  } else if (window.location.pathname.endsWith("globalRankings")) {
-    presenceData.details = "Küresel sıralamaya göz atıyor...";
+  } else if (page.startsWith("/globalRankings")) {
+    presenceData.details = "Viewing a page:";
+    presenceData.state = "Global Rankings";
     presenceData.startTimestamp = browsingStamp;
-  } else if (window.location.pathname.endsWith("dashboard")) {
-    presenceData.details = "Dashboard'da bir şeyleri kontrol ediyor...";
+  } else if (page.startsWith("/dashboard")) {
+    presenceData.details = "Viewing dashboard";
     presenceData.startTimestamp = browsingStamp;
-  } else if (window.location.pathname.endsWith("profile")) {
-    presenceData.details = "Profilini düzenliyor...";
+  } else if (page.startsWith("/profile")) {
+    presenceData.details = "Viewing/editing their profile";
     presenceData.startTimestamp = browsingStamp;
-  } else if (window.location.pathname.startsWith("/serverRankings/")) {
-    presenceData.details = "Bir sunucunun level sıralamasına bakıyor...";
-    presenceData.state = document.querySelector(
-      "#top > div.jumbotron > div > div > div.col-md-10 > p"
-    ).textContent;
+  } else if (page.startsWith("/serverRankings/")) {
+    const yArraabeni = document.querySelector("#top > div.jumbotron > div > div > div.col-md-10 > p").textContent;
+    const sgoc = "Besto Rankings @"
+    const ahmetabibanimiac = yArraabeni.replace(sgoc, "")
+    presenceData.details = "Viewing " + ahmetabibanimiac + "'s leaderboard";
     presenceData.startTimestamp = browsingStamp;
-  } else if (window.location.pathname.startsWith("/guild/")) {
-    presenceData.details = "Bir sunucunun ayarlarını düzenliyor:";
-    presenceData.state = document.querySelector(
-      "#top > div.jumbotron > div > div > div.col-md-10 > h1"
-    ).textContent;
-    presenceData.startTimestamp = browsingStamp;
+  } else if (page.startsWith("/guild/")) {
+    presenceData.details = "Managing a server";
+    presenceData.state = document.querySelector("#top > div.jumbotron > div > div > div.col-md-10 > h1").textContent;
   }
+
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
