b97809ff6dd8ea9f9415aaed9a6b9c76f9a87650
Gurrman375 <james.gurr375@gmail.com>
2020-06-29 11:18:29 -0700

    DeepScan.io fix #3

--- websites/V/VidCloud9/presence.ts
+++ websites/V/VidCloud9/presence.ts
@@ -7,12 +7,12 @@ const presence = new Presence({
   });
 
 let iFrameVideo: boolean, currentTime: number, duration: number, paused: boolean;
-let video: { iframe_video: { duration: number; iFrameVideo: boolean; currTime: number; dur: number; paused: boolean;};};
+let video:{ iframe_video:{ duration: number; iFrameVideo: boolean; currTime: number; dur: number; paused: boolean;}}
 let lastPlaybackState: void;
 let playback: boolean;
 let title;
 
-presence.on("iFrameData", (data: { iframe_video: { duration: number; iFrameVideo: boolean; currTime: number; dur: number; paused: boolean; };}) => {
+presence.on("iFrameData", (data: { iframe_video: { duration: number; iFrameVideo: boolean; currTime: number; dur: number; paused: boolean}}) => {
   video = data;
   playback = data.iframe_video.duration !== null ? true : false;
   if (playback) {
