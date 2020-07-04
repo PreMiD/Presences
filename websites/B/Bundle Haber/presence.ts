c51af6225fa2311fa60c9524ff675fab5fe53bdb
SeMiD <contact@premid.app>
2020-07-04 00:24:44 +0000

    ♻️ Refactoring
    Signed-off-by: SeMiD <contact@premid.app>

--- websites/B/Bundle Haber/presence.ts
+++ websites/B/Bundle Haber/presence.ts
@@ -10,12 +10,13 @@ presence.on("UpdateData", () => {
   const page = window.location.pathname;
   const browsingStamp = Math.floor(Date.now() / 1000);
 
-
   if (page.startsWith("/detay/")) {
     presenceData.details = "Bir haber okuyor";
-    presenceData.state = document.querySelector("body > div.site > div.detailpage > div > h2").textContent;
+    presenceData.state = document.querySelector(
+      "body > div.site > div.detailpage > div > h2"
+    ).textContent;
     presenceData.startTimestamp = browsingStamp;
-  } 
+  }
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
