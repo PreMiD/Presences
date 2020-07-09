4822e9af749994d43aab2ac7b82430e4569015d3
Zerlingg <picofuego@gmail.com>
2020-07-09 22:49:08 +0200

    Update iframe.ts

--- websites/A/AnimeTW/iframe.ts
+++ websites/A/AnimeTW/iframe.ts
@@ -1,11 +1,11 @@
 
 
-let iframe = new iFrame();
+const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
 
 	if(document.querySelector("video[id$='_html5_api']") != null || document.querySelector("div.jw-media.jw-reset > video") != null) {
-		let video: HTMLVideoElement = document.querySelector("video[id$='_html5_api']") != null
+		const video: HTMLVideoElement = document.querySelector("video[id$='_html5_api']") != null
 	 ? document.querySelector("video[id$='_html5_api']") : document.querySelector("div.jw-media.jw-reset > video");
 		 if(video != null && !isNaN(video.duration)) {
 			iframe.send({
