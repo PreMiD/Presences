8a4561ba5c88794444f3c0e2857fc0f27f9f8ae5
leojacondev <legendmccljr@gmail.com>
2020-07-14 16:00:59 -0300

    Fixing problems in  ClarityClan Presence

--- websites/C/ClarityClan/presence.ts
+++ websites/C/ClarityClan/presence.ts
@@ -2,14 +2,13 @@ const presence = new Presence({
     clientId: "732636451933782047" 
 });
 
-function getPointsString() {
-    let x = document.getElementsByClassName("elementor-shortcode");
+function getPointsString() : string {
+    const x = document.getElementsByClassName("elementor-shortcode");
     return x[0].textContent;
-    
 }
 
 presence.on('UpdateData', async() =>{
-    let presenceData: PresenceData = {
+    const presenceData: PresenceData = {
         largeImageKey: "clarity", 
         smallImageKey: "mcc", 
         smallImageText: "mc-central.net",
