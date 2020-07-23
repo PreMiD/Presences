9d03b4bc166f4b0d78763c9afe73235a4f5edb84
Coding <dev.tandpfun@gmail.com>
2020-07-22 17:57:20 -0700

    Add TakeB1nzyToSpace Presence

---
+++ websites/T/TakeB1nzyToSpace/presence.ts
@@ -0,0 +1,28 @@
+const presence = new Presence({
+    clientId: "735314055861895288"
+});
+
+const browsingStamp = Math.floor(Date.now() / 1000); // Time Ratelimited
+const path = document.location; // Website URL
+
+/* When Website Updates */
+presence.on("UpdateData", async () => {
+    const presenceData: PresenceData = {
+        largeImageKey: "icon"
+    };
+
+    /* Update Presence */
+    if (path.hostname === "takeb1nzyto.space") {
+        var song = document.querySelector(
+            "#song"
+        ).textContent
+        var ratelimited = document.querySelector(
+            "#ratelimited-time"
+        ).textContent
+
+        presenceData.startTimestamp = browsingStamp
+        presenceData.details = "Playing " + song
+        presenceData.state = "Ratelimited for " + ratelimited + "s"
+        presence.setActivity(presenceData);
+    }
+});
