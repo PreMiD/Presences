ff5c0b024ccfc4f7bc1d5059d28361311345ea4b
Miray <cinziapansini1973@gmail.com>
2020-07-30 03:26:17 +0200

    Update Fortnite-API.com

--- websites/F/Fortnite-API/presence.ts
+++ websites/F/Fortnite-API/presence.ts
@@ -11,15 +11,11 @@ presence.on("UpdateData", () => {
   const path = document.location.pathname;
 
   if (path === "/") {
-    presenceData.details = "Home page";
-  } else if (path === "/documentation") {
-    presenceData.details = "Documentation page";
+    presenceData.details = "Documentation Home page";
   } else if (path === "/about") {
     presenceData.details = "About page";
-  } else if (path === "/profile") {
-    presenceData.details = "Profile page";
-  } else if (path === "/privacy") {
-    presenceData.details = "Privacy page";
+  } else if (path === "/account") {
+    presenceData.details = "Account page";
   } else {
     presenceData.details = "Browsing";
   }
