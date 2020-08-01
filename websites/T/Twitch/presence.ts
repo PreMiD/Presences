245b070f88c444e2564fb1ce84bd32bf607b02b7
Alanexei <cpowers619@gmail.com>
2020-08-01 15:21:32 -0500

    Remove Twitch Settings

--- websites/T/Twitch/presence.ts
+++ websites/T/Twitch/presence.ts
@@ -102,10 +102,10 @@ const statics = {
 presence.on("UpdateData", async () => {
   const path = location.pathname.replace(/\/?$/, "/");
 
-  const showBrowsing = await presence.getSetting("browse");
-  const showLive = await presence.getSetting("live");
-  const showVideo = await presence.getSetting("video");
-  const showTimestamps = await presence.getSetting("timestamp");
+  const showBrowsing = true;
+  const showLive = true;
+  const showVideo = true;
+  const showTimestamps = true;
 
   let data: PresenceData = {
     details: undefined,
