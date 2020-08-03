1a2023800df70f2570e353851e22068ea3df55f3
MauricePascal <larivieremauricepascal112mail.com>
2020-08-03 03:06:38 +0200

    do request

--- websites/D/discord-botlist/presence.ts
+++ websites/D/discord-botlist/presence.ts
@@ -1,36 +1,13 @@
 var presence = new Presence({
-    clientId: "739615293474603068" //The client ID of the Application created at https://discordapp.com/developers/applications
+    clientId: "739615293474603068"
 }),
 
 strings = presence.getStrings({
     play: "presence.playback.playing",
     pause: "presence.playback.paused"
-    //You can use this to get translated strings
 });
 
-console.log("NO u")
-
-/*
-
-function myOutsideHeavyLiftingFunction(){
-    //Grab and process all your data here
-
-    // element grabs //
-    // api calls //
-    // variable sets //
-}
-
-setInterval(myOutsideHeavyLiftingFunction, 10000);
-//Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
-
-*/
-
-
 presence.on("UpdateData", async () => {
-    /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
-
-    It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
-
     var needState = false
     var browsingStamp = Math.floor(Date.now() / 1000);
 
@@ -60,27 +37,24 @@ presence.on("UpdateData", async () => {
 
     if(!needState) {
         var presenceData: PresenceData = {
-            largeImageKey: "dbl_eu-logo", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-            details: "Viewing "+titlenName, //The upper section of the presence text
-            startTimestamp: browsingStamp, //The unix epoch timestamp for when to start counting from
-        }; /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
+            largeImageKey: "dbl_eu-logo",
+            details: "Viewing "+titlenName,
+            startTimestamp: browsingStamp, 
+        }; 
     
     } else {
         var presenceData: PresenceData = {
-            largeImageKey: "dbl_eu-logo", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-            details: "Viewing "+titlenName, //The upper section of the presence text
+            largeImageKey: "dbl_eu-logo", 
+            details: "Viewing "+titlenName,
             state: "Vote",
-            startTimestamp: browsingStamp, //The unix epoch timestamp for when to start counting from
-        }; /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
-    
+            startTimestamp: browsingStamp, 
+        }; 
     }
 
     if (presenceData.details == null) {
-        //This will fire if you do not set presence details
-        presence.setTrayTitle(); //Clears the tray title for mac users
-        presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+        presence.setTrayTitle(); 
+        presence.setActivity();
     } else {
-        //This will fire if you set presence details
-        presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+        presence.setActivity(presenceData); 
     }
 });
