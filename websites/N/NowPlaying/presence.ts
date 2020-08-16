52434b063db4b2deddbfb4aca2f8bfd16b7331f5
Stephan <tollenaarstephan@gmail.com>
2020-08-16 02:50:02 +0200

    Update presence.ts

--- websites/N/NowPlaying/presence.ts
+++ websites/N/NowPlaying/presence.ts
@@ -1,10 +1,6 @@
 var presence = new Presence({
     clientId: "744332419104243712"
   }),
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
 
 presence.on("UpdateData", async () => {
   var presenceData: presenceData = {
