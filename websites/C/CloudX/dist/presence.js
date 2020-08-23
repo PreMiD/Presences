9757eb1819febb880def3bbc8c9abc1c2c26c5fe
Marino <48334228+marinofranz@users.noreply.github.com>
2020-08-23 21:49:47 +0000

    Add files via upload

---
+++ websites/C/CloudX/dist/presence.js
@@ -0,0 +1,57 @@
+const presence = new Presence({
+    clientId: "743793403753660426"
+}), browsingStamp = Math.floor(Date.now() / 1000);
+let title;
+let artist;
+let dj;
+let playbackStatus;
+function listener() {
+    const json = JSON.parse(this.responseText);
+    title = json.song.title;
+    artist = json.song.artist;
+    dj = json.dj.username;
+}
+function getData() {
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", listener);
+    req.open("GET", "https://api.thisiscloudx.com/stats");
+    req.send();
+}
+function getStatus() {
+    const playPauseBtn = document.querySelector("#play");
+    if (playPauseBtn.className === "fas fa-play") {
+        return "Paused";
+    }
+    else if (playPauseBtn.className === "fas fa-pause") {
+        return "Playing";
+    }
+    return "Playing";
+}
+getData();
+setInterval(getData, 5000);
+presence.on("UpdateData", async () => {
+    const presenceData = {
+        largeImageKey: "logo"
+    };
+    if (document.location.hostname === "www.thisiscloudx.com" ||
+        document.location.hostname === "thisiscloudx.com") {
+        presenceData.startTimestamp = browsingStamp;
+        playbackStatus = getStatus();
+        if (playbackStatus === "Paused") {
+            presenceData.smallImageKey = "pause";
+        }
+        else if (playbackStatus === "Playing") {
+            presenceData.smallImageKey = "play";
+        }
+        presenceData.details = `🎵 | ${artist} - ${title}`;
+        presenceData.state = `🎙️ | ${dj}`;
+        presenceData.smallImageText = playbackStatus;
+    }
+    if (presenceData.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    }
+    else {
+        presence.setActivity(presenceData);
+    }
+});
