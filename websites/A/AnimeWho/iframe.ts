c6d1a04f2db0b188791488be856b4309618ff533
Prenses <57987392+Hime-Dev@users.noreply.github.com>
2020-08-24 04:02:08 +0300

    Update iframe.ts

--- websites/A/AnimeWho/iframe.ts
+++ websites/A/AnimeWho/iframe.ts
@@ -4,7 +4,7 @@ i_frame.on("UpdateData", async () => {
   const video: HTMLVideoElement = document.querySelector("video");
 
   if (video) {
-    const videoMessage: any = {
+    const videoMessage: "any" = {
       paused: video.paused,
       duration: video.duration,
       currentTime: video.currentTime
