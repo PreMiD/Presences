fd79896a54561afdba6c0e8f49370fb88565d2d0
Prenses <57987392+Hime-Dev@users.noreply.github.com>
2020-08-24 12:15:28 +0300

    Update presence.ts

--- websites/A/AnimeWho/presence.ts
+++ websites/A/AnimeWho/presence.ts
@@ -1,8 +1,8 @@
-let presence = new Presence({
+const presence = new Presence({
   clientId: "747190301676011550"
 });
 
-const strings = await presence.getStrings({
+const strings = presence.getStrings({
   playing: "presence.playback.playing",
   paused: "presence.playback.paused",
   browsing: "presence.activity.browsing"
@@ -13,13 +13,12 @@ const strings = await presence.getStrings({
  * @param {Number} videoTime Current video time seconds
  * @param {Number} videoDuration Video duration seconds
  */
-
 function getTimestamps(
   videoTime: number,
   videoDuration: number
 ): Array<number> {
-  const startTime = Date.now();
-  const endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  var startTime = Date.now(),
+  endTime =  Math.floor(startTime / 1000) - videoTime + videoDuration;
   return [Math.floor(startTime / 1000), endTime];
 }
 
@@ -27,17 +26,18 @@ const startTimestamp = Math.floor(Date.now() / 1000);
 
 let video: HTMLVideoElement;
 
-presence.on("iFrameData", async (msg: "any") => {
+presence.on("iFrameData", async (msg: any) => {
   if (!msg) return;
   video = msg;
 });
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "animewho"
-  };
-
-  const title = document.querySelector(
+    largeImageKey: "animewho",
+	details: "Ana Sayfada Anime Arıyor",
+    startTimestamp: startTimestamp
+  },
+  title = document.querySelector(
     "head > title"  
   );
 
@@ -49,15 +49,47 @@ presence.on("UpdateData", async () => {
 
   if (title) {
     presenceData.details = title.textContent;
-	presenceData.startTimestamp = startTimestamp;
   }
 
   // Arama
   else {
     presenceData.details = (await strings).browsing;
-    presenceData.startTimestamp = startTimestamp;
   }
+  
+	const anime = title.textContent.replace("Türkçe İzle - AnimeWho? Anime", "").replace("Türkçe Oku - AnimeWho? Manga", "").replace("AnimeWho?", "").replace(" Türkçe İzle ve İndir", "");
+	presenceData.details = anime;
+	if (document.location.pathname == "/"){
+		presenceData.details = "Ana Sayfada";
+		presenceData.state = "Anime & Manga Arıyor";
+	}else if ((new RegExp('izle/[a-zA-Z0_9]+')).test(document.location.pathname)){
+		const bolum = document.querySelector("div.MuiBox-root > button.MuiButton-outlinedSecondary > span.MuiButton-label");
+		presenceData.state = bolum.innerHTML + "'ü İzliyor";
+	}else if ((new RegExp('oku/[0-9]+/[0-9]+')).test(document.location.pathname)){
+		const read_type = document.querySelector("#scroll-node > div > section > div > div:nth-child(2) > div > div.jss80 > button > span.MuiButton-label"),
+		arr = document.location.pathname.split("/");
+		presenceData.state = `${arr[5]}.Bölüm`;
+		if (read_type && read_type.innerHTML.includes("Webtoon")){
+			presenceData.state = `${arr[5]}.Bölüm ${arr[6]}.Sayfa`;
+		}
+	}else if (document.location.pathname.includes("/izle") || document.location.pathname.includes("/oku")){
+		presenceData.details = anime;
+		presenceData.state = "Bölüm Seçiyor";
+	}else if ((new RegExp('ceviriler/anime')).test(document.location.pathname)){
+		presenceData.state = "Göz Gezdiriyor";
+	}else if(document.location.pathname.includes("/ekip-alimlari")){
+		presenceData.details = "Ekip Alımları";
+		presenceData.state = "Potansiyel Ekip Üyesi";
+	}else if(document.location.pathname.includes("/ara")){
+		const tur = document.querySelector("div.MuiGrid-root > div.MuiBox-root > button.MuiButton-outlinedPrimary > span.MuiButton-label");
+		if (tur.innerHTML == "Anime"){
+			presenceData.state = "Ne İzlesem Diye Bakıyor";
+		}else{
+			presenceData.state = "Ne Okusam Diye Bakıyor";
+		}
+	}
 
+	
+	
   if (video) {
     presenceData.smallImageKey = video.paused ? "pause" : "play";
     presenceData.smallImageText = video.paused
