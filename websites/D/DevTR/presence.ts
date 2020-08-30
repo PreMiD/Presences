1458f25344726f8bee832bd8b6c1b0d6dc4b70fb
Eren <58482989+eren885@users.noreply.github.com>
2020-08-30 20:04:44 +0100

    Update presence.ts

--- websites/D/DevTR/presence.ts
+++ websites/D/DevTR/presence.ts
@@ -11,35 +11,35 @@ presence.on("UpdateData", () => {
     presenceData.startTimestamp = browsingStamp;
 
     if (page.startsWith("/kullanici/")) {
-        presenceData.details = "Bir kullanıcının profilini görüntülüyor:"
-        presenceData.state = document.querySelector("body > div:nth-child(6) > div > h3").textContent
+        presenceData.details = "Bir kullanıcının profilini görüntülüyor:";
+        presenceData.state = document.querySelector("body > div:nth-child(6) > div > h3").textContent;
     } else if (page.startsWith("/hakkimizda")) {
-        presenceData.details = "Bir sayfayı görüntülüyor:"
-        presenceData.state = "Hakkımızda"
+        presenceData.details = "Bir sayfayı görüntülüyor:";
+        presenceData.state = "Hakkımızda";
     } else if (page.startsWith("/javascript")) {
-        presenceData.details = "Bir sayfayı görüntülüyor:"
-        presenceData.state = "JS Komutlar"
+        presenceData.details = "Bir sayfayı görüntülüyor:";
+        presenceData.state = "JS Komutlar";
         presenceData.smallImageKey = "js"
     } else if (page.startsWith("/kod")) {
-        presenceData.details = "Bir kodu görüntülüyor:"
-        presenceData.state = document.querySelector("body > div.post.post-single > div.post-thumbnail > center > h1").textContent
+        presenceData.details = "Bir kodu görüntülüyor:";
+        presenceData.state = document.querySelector("body > div.post.post-single > div.post-thumbnail > center > h1").textContent;
     } else if (page.startsWith("/html")) {
-        presenceData.details = "Bir sayfayı görüntülüyor:"
-        presenceData.state = "HTML kodları"
-        presenceData.smallImageKey = "html"
+        presenceData.details = "Bir sayfayı görüntülüyor:";
+        presenceData.state = "HTML kodları";
+        presenceData.smallImageKey = "html";
     } else if (page.endsWith("/rapor")) {
-        presenceData.details = "Bir şeyi bildiriyor..."
+        presenceData.details = "Bir şeyi bildiriyor...";
     } else if (page.endsWith("/yetersiz-rol")) {
-        presenceData.details = "Erişemeyeceği bir yere erişmeye çalışıyor... 👀"
-        presenceData.smallImageKey = "x"
+        presenceData.details = "Erişemeyeceği bir yere erişmeye çalışıyor... 👀";
+        presenceData.smallImageKey = "x";
     } else if (page.startsWith("/altyapi")) {
-        presenceData.details = "Altyapıları görüntülüyor..."
-        presenceData.smallImageKey = "alt"
+        presenceData.details = "Altyapıları görüntülüyor...";
+        presenceData.smallImageKey = "alt";
     } else if (page.startsWith("/booster")) {
-        presenceData.details = "Booster kısmını görüntülüyor..."
-        presenceData.smallImageKey = "booster"
+        presenceData.details = "Booster kısmını görüntülüyor...";
+        presenceData.smallImageKey = "booster";
     } else if (page.startsWith("/yetkili")) {
-        presenceData.details = "Yetkili sayfasında dolaşıyor..."
+        presenceData.details = "Yetkili sayfasında dolaşıyor...";
     }
 
     if (presenceData.details == null) {
