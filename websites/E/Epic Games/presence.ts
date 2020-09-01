e5157f40d89d3f43a8bdbd30956e3fef2f091ed5
Marino <48334228+marinofranz@users.noreply.github.com>
2020-09-01 16:31:55 +0000

    add support for all languages

--- websites/E/Epic Games/presence.ts
+++ websites/E/Epic Games/presence.ts
@@ -17,51 +17,51 @@ presence.on("UpdateData", async () => {
     ){
       // Epic Games Store
 
-      if(pathname === "/store/en-US/"){
+      if(pathname.startsWith("/store")){
         presenceData.details = "Viewing:";
         presenceData.state = "Store";
       }
 
-      if(pathname.startsWith("/store/en-US/product")){
+      if(pathname.includes("/store/") && pathname.includes("/product")){
         const gameName = document.querySelector("#dieselReactWrapper > div > div.css-igz6h5-AppPage__bodyContainer > main > div > nav.css-1v9fujt-PageNav__topNav-PageNav__desktopNav-PageNav__themed > div > nav > div > div.css-eizwrh-NavigationBar__contentPrimary > ul > li:nth-child(2) > a > h2 > span").textContent;
         presenceData.details = "Viewing game:";
         presenceData.state = gameName;
       }
 
-      if(pathname.startsWith("/store/en-US/bundles")){
+      if(pathname.includes("/store/") && pathname.includes("/bundles")){
         const bundleName = document.querySelector("#dieselReactWrapper > div > div.css-igz6h5-AppPage__bodyContainer > main > div > nav.css-1v9fujt-PageNav__topNav-PageNav__desktopNav-PageNav__themed > div > nav > div > div.css-eizwrh-NavigationBar__contentPrimary > ul > li:nth-child(2) > a > h2 > span").textContent;
         presenceData.details = "Viewing bundle:";
         presenceData.state = bundleName;
       }
 
-      if(pathname === "/store/en-US/news"){
+      if(pathname.includes("/store/") && pathname.includes("/news")){
         presenceData.details = "Viewing:";
         presenceData.state = "News";
       }
 
-      if(pathname.startsWith("/store/en-US/news/")){
+      if(pathname.startsWith("/store/") && pathname.endsWith("/news/")){
         const articleName = document.querySelector("#storeNews > div > section > div > article > div > div.blog-header-info > div:nth-child(1) > div > h1").textContent;
         presenceData.details = "Reading article:";
         presenceData.state = articleName;
       }
 
-      if(pathname === "/store/en-US/about"){
+      if(pathname.includes("/store/") && pathname.includes("/about")){
         presenceData.details = "Viewing:";
         presenceData.state = "About Page";
       }
 
-      if(pathname === "/store/en-US/redeem"){
+      if(pathname.includes("/store/") && pathname.includes("/redeem")){
         presenceData.details = "Redeeming";
       }
 
       // Epic Games Site
 
-      if(pathname === "/site/en-US/home"){
+      if(pathname.includes("/site/") && pathname.includes("/home")){
         presenceData.details = "Viewing:";
         presenceData.state = "Home Page";
       }
 
-      if(pathname === "/site/en-US/epic-games-store-faq"){
+      if(pathname.includes("/site/") && pathname.includes("/epic-games-store-faq")){
         presenceData.details = "Viewing:";
         presenceData.state = "FAQ Page";
       }
@@ -77,12 +77,12 @@ presence.on("UpdateData", async () => {
 
       // Epic Games Help page
 
-      if(pathname === "/help/en-US/"){
+      if(pathname.startsWith("/help")){
         presenceData.details = "Viewing:";
         presenceData.state = "Help Page";
       }
 
-      if(pathname.startsWith("/help/en-US/")){
+      if(pathname.startsWith("/help/"))){
         const helpHeading = document.querySelector("#epicGamesReactWrapper > div > div.eg-content > div > div.sspgrid-container > div.Layout2Cols__wrapper.LayoutCol7Col4__wrapper > div > div.Layout2Cols__body-col--main.sspgrid-col-lg-7 > div.Article__wrapper > h1");
         if(helpHeading === null){
           presenceData.details = null;
