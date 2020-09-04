91eaef0b40457d5ebadef68059da088c2d375e8f
markinhozz1 <38191703+markinhozz1@users.noreply.github.com>
2020-09-04 01:24:36 -0500

    Update presence.ts

--- websites/M/Mark Scans/presence.ts
+++ websites/M/Mark Scans/presence.ts
@@ -1,23 +1,20 @@
 const presence = new Presence ({
     clientId: "751285675277680811"
-})
-
-
+});
 presence.on('UpdateData', async () =>
 {
-
-    const presenceData: PresenceData = {startTimestamp: Math.floor(Date.now() / 1000), largeImageKey: 'logo'}
-    const path: any = document.location.pathname;
-    const title: any = document.title; 
-    const tituloobra: HTMLElement = document.querySelector("body > div.wrap > div > div > div > div.profile-manga > div > div > div > div.post-title > h1");
-    const tipoobra: HTMLElement = document.querySelector("body > div.wrap > div > div > div > div.profile-manga > div > div > div > div.tab-summary > div.summary_content_wrap > div > div.post-content > div:nth-child(9) > div.summary-content");
-    const primeirogeneroobra: HTMLElement = document.querySelector("body > div.wrap > div > div > div > div.profile-manga > div > div > div > div.tab-summary > div.summary_content_wrap > div > div.post-content > div:nth-child(8) > div.summary-content > div > a:nth-child(1)");
-    const filtroprojetos: HTMLElement = document.querySelector("body > div.wrap > div > div > div.c-page-content.style-1 > div > div > div > div > div.main-col-inner > div > div.c-page__content > div.tab-wrap > div > div.c-nav-tabs > ul > li.active");
-    const pesquisatexto: HTMLInputElement = document.querySelector("body > div.wrap > div > div > div.c-search-header__wrapper > div > div.search-content > form > input.search-field.manga-search-field.ui-autocomplete-input");
-    const paginaLogin: HTMLElement = document.querySelector("#post-8 > div.entry-content > div > div > div.col-md-3.col-sm-3 > div > ul > li.active");  
-    const selecaoLeitura: HTMLSelectElement = document.querySelector("#manga-reading-nav-head > div > div.select-view > div.c-selectpicker.selectpicker_load > label > select");
-    const paginacao: HTMLSelectElement = document.querySelector("#single-pager");
-    const usuarioNome: HTMLSpanElement = document.querySelector("body > div.wrap > div > header > div.c-sub-header-nav.hide-sticky-menu > div > div > div.c-modal_item > div > span");
+    const presenceData: PresenceData = {startTimestamp: Math.floor(Date.now() / 1000), largeImageKey: 'logo'},
+    path = document.location.pathname,
+    title: HTMLElement = document.querySelector('head title'),
+    tituloobra: HTMLElement = document.querySelector("body > div.wrap > div > div > div > div.profile-manga > div > div > div > div.post-title > h1"),
+    tipoobra: HTMLElement = document.querySelector("body > div.wrap > div > div > div > div.profile-manga > div > div > div > div.tab-summary > div.summary_content_wrap > div > div.post-content > div:nth-child(9) > div.summary-content"),
+    primeirogeneroobra: HTMLElement = document.querySelector("body > div.wrap > div > div > div > div.profile-manga > div > div > div > div.tab-summary > div.summary_content_wrap > div > div.post-content > div:nth-child(8) > div.summary-content > div > a:nth-child(1)"),
+    filtroprojetos: HTMLElement = document.querySelector("body > div.wrap > div > div > div.c-page-content.style-1 > div > div > div > div > div.main-col-inner > div > div.c-page__content > div.tab-wrap > div > div.c-nav-tabs > ul > li.active"),
+    pesquisatexto: HTMLInputElement = document.querySelector("body > div.wrap > div > div > div.c-search-header__wrapper > div > div.search-content > form > input.search-field.manga-search-field.ui-autocomplete-input"),
+    paginaLogin: HTMLElement = document.querySelector("#post-8 > div.entry-content > div > div > div.col-md-3.col-sm-3 > div > ul > li.active"),
+    selecaoLeitura: HTMLSelectElement = document.querySelector("#manga-reading-nav-head > div > div.select-view > div.c-selectpicker.selectpicker_load > label > select"),
+    paginacao: HTMLSelectElement = document.querySelector("#single-pager"),
+    usuarioNome: HTMLSpanElement = document.querySelector("body > div.wrap > div > header > div.c-sub-header-nav.hide-sticky-menu > div > div > div.c-modal_item > div > span");
     if (usuarioNome !== null)
     {
         presenceData.smallImageKey = 'user';
@@ -35,7 +32,7 @@ presence.on('UpdateData', async () =>
         }
     } else if (path.includes('/generos/')) 
     {
-        presenceData.details = `Categoria: ${title.slice(0, title.indexOf('Mark') - 2)}`;
+        presenceData.details = `Categoria: ${title.innerText.slice(0, title.innerText.indexOf('Mark') - 2)}`;
         
         if (filtroprojetos !== null)
         {
@@ -50,17 +47,17 @@ presence.on('UpdateData', async () =>
         {
             0: function()
             {
-                presenceData.state = `${title.slice(title.indexOf('- Capítulo') + 2, title.indexOf('- Mark') - 1)}`;
+                presenceData.state = `${title.innerText.slice(title.innerText.indexOf('- Capítulo') + 2, title.innerText.indexOf('- Mark') - 1)}`;
             },
             1: function()
             {
-                presenceData.state = `${title.slice(title.indexOf('- Capítulo') + 2, title.indexOf('- Mark') - 1)} | ${paginacao[paginacao.selectedIndex].innerText}`;
+                presenceData.state = `${title.innerText.slice(title.innerText.indexOf('- Capítulo') + 2, title.innerText.indexOf('- Mark') - 1)} | ${paginacao[paginacao.selectedIndex].innerText}`;
             }
         }
-        if ((tituloobra != null) && !tituloobra.textContent.includes('Privado:') || !title.includes('Privado:')) {
+        if ((tituloobra != null) && !tituloobra.textContent.includes('Privado:') || !title.innerText.includes('Privado:')) {
             if ((path.split('/').length - 1) != 3)
             {
-                presenceData.details = title.slice(0, title.indexOf('- Capítulo') - 1);
+                presenceData.details = title.innerText.slice(0, title.innerText.indexOf('- Capítulo') - 1);
                 opcoes[selecaoLeitura.selectedIndex]();
                 
             } else {
@@ -77,8 +74,7 @@ presence.on('UpdateData', async () =>
     {
         presenceData.details = 'Meu perfil';
         presenceData.state = paginaLogin.innerText;
-    } else 
-    { 
+    } else { 
         presenceData.details = 'Navegando...';
     }
     presence.setActivity(presenceData); 
