08e0c89f6b9c1f5726243269104a14a163034c03
Ikramullah Latif <sky_m4n@outlook.com>
2020-09-06 01:02:36 +0800

    Fixing some state

--- websites/R/Radiko/presence.ts
+++ websites/R/Radiko/presence.ts
@@ -1,63 +1,46 @@
 const presence = new Presence({
-    clientId: "736620343279484959"
+    clientId: '736620343279484959'
   }),
   _preStrings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused",
-    browsing: "presence.activity.browsing"
+    play: 'presence.playback.playing',
+    pause: 'presence.playback.paused',
+    browsing: 'presence.activity.browsing'
   });
-
-function generateArgs (args: string[]) {
-  type ArgReturn = {
-    mode: string
-    radioStation?: string
-  }
-
-  const mode = args[0];
-  let ret: ArgReturn;
-  switch (mode) {
-    case 'live':
-      ret = {
-        mode: 'live',
-        radioStation: args[1]
-      };
-      break;
-  
-    default:
-      ret = {
-        mode: 'index'
-      };
-      break;
-  }
-
-  return ret;
-}
 
 presence.on('UpdateData', async () => {
   // code
   const preStrings = await _preStrings,
-    defURL = document.URL.toString(),
-    urlArgs = defURL.substring(
-      defURL.startsWith('http://') ? 'http://'.length : 'https://'.length
-    ),
-    args = generateArgs(urlArgs.split('/').filter(url => url !== '#!').slice(1)),
+    streamPlayer = document.getElementById('stream-player') as HTMLElement,
+    whenPlayerIsOn = streamPlayer.style.display,
     state: PresenceData = {
-      details: 'Radiko',
+      // details: 'Radiko',
       // state: 'Searching radio.',
-      largeImageKey: 'largeimage',
-      smallImageKey: 'largeimage',
+      // smallImageKey: 'largeimage',
       // smallImageText: preStrings.browsing
+      largeImageKey: 'largeimage'
     };
 
-  // Mode apabila tidak melakukan apapun
-  if (args.mode === 'index') {
-    state.state = 'Idle';
+  // console.log(whenPlayerIsOn);
+  // Apabila idling
+  if (!whenPlayerIsOn || whenPlayerIsOn === 'none') {
+    state.details = 'Idling';
+    state.smallImageKey = 'largeimage';
     state.smallImageText = preStrings.browsing;
   }
-  // Mode ketika sedang membuka saluran radio
-  else if (args.mode === 'live') {
-    state.state = `Listening to ${args.radioStation} radio.`;
-    state.smallImageText = preStrings.browsing;
+  // Apabila lagi putar lagu
+  else if (whenPlayerIsOn === 'block') {
+    const _showTitle = document.querySelector(
+      'a.slick-slide:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)'
+    ) as HTMLElement,
+      showTitle = _showTitle.textContent,
+      defURL = document.querySelector('a.slick-slide:nth-child(1)').getAttribute('href')
+        .split('/').slice(-1)[0]
+      ;
+
+    state.details = 'Listening to radio.';
+    state.state = `${defURL} | ${showTitle}`;
+    state.smallImageKey = 'largeimage';
+    state.smallImageText = preStrings.play;
   }
 
   presence.setActivity(state);
