c017370d89d63761e822e052edb64d3c5d9111f8
a6doff <67640562+a6doff@users.noreply.github.com>
2020-09-06 04:05:07 +0200

    Create presence.ts

---
+++ websites/O/OnlyHit/presence.ts
@@ -0,0 +1,37 @@
+  const presence = new Presence({
+    clientId: "666412985513672715"
+  });
+  
+  presence.on("UpdateData", () => {
+    const title = `${
+      document.querySelector(".artist").textContent
+    } - ${document.querySelector(".title").textContent} `;
+    const radio = document.querySelector(".stream-name").textContent;
+    let logo = 'logo_onlyhit';
+    let status = 'play';
+    if (document.querySelector(".stream-name").textContent == 'OnlyHit Gold') {
+      logo = 'logo_gold'
+    }
+    if (document.querySelector(".stream-name").textContent == 'OnlyHit J-Music') {
+      logo = 'logo_j-music'
+    }
+    if (document.querySelector(".stream-name").textContent == 'OnlyHit K-Pop') {
+      logo = 'onlyhit_k-pop'
+    }
+    const pageName = document.title.slice(13);
+    const presenceData: PresenceData = {
+      largeImageKey: `${logo}`,
+      smallImageKey: `${status}`,
+      smallImageText: `Viewing: ${pageName}`
+    };
+  
+    presenceData.details = `${title}`;
+    presenceData.state = `${radio} on OnlyHit.us`;
+
+    var lastTimeStart = Math.floor(Date.now() / 1000);
+    lastTimeStart = Math.floor(Date.now() / 1000);
+    presenceData.startTimestamp = lastTimeStart;
+  
+    presence.setActivity(presenceData);
+    presence.setTrayTitle();
+  });
