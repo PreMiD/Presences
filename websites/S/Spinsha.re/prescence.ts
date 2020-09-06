946bfff4517a3446748e7fbc70b16b1b6127477d
Amy Y <ctrlalternativedelete@gmail.com>
2020-09-06 23:40:46 +1000

    simplified code and fixed up things

--- websites/S/Spinsha.re/prescence.ts
+++ websites/S/Spinsha.re/prescence.ts
@@ -1,11 +1,5 @@
 var presence = new Presence({
     clientId: "752151960743837817" //The client ID of the Application created at https://discordapp.com/developers/applications
-}),
-
-strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-    //You can use this to get translated strings
 });
 /*
 
@@ -24,21 +18,17 @@ setInterval(myOutsideHeavyLiftingFunction, 10000);
 
 
 presence.on("UpdateData", async () => {
-    /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
-
-    It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
-    const { pathname } = window.location;
     const presenceData: PresenceData = {
-        largeImageKey: "logo"
+        largeImageKey: "bigdefault"
     };
-    console.log(pathname, presenceData)
+    if (document.location.hostname == "spinsha.re") {
+        console.log(document.location.pathname, presenceData)
+    }
 
-    // if (presenceData.details == null) {
-    //     //This will fire if you do not set presence details
-    //     presence.setTrayTitle(); //Clears the tray title for mac users
-    //     presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
-    // } else {
-    //     //This will fire if you set presence details
-    //     presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
-    // }
+    if (presenceData.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    } else {
+        presence.setActivity(presenceData);
+    }
 });
