c4d19888a4a0d371e2c2ee41557c7f172a6c1978
Amy Y <ctrlalternativedelete@gmail.com>
2020-09-07 19:47:21 +1000

    init Itch.io

---
+++ websites/I/Itch.io/presence.ts
@@ -0,0 +1,33 @@
+const presence = new Presence({
+    clientId: "752464948965408768"
+}),
+browsingStamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+    const presenceData: PresenceData = {
+        largeImageKey: "bigdefault"
+    };
+    
+    if (document.location.hostname == "spinsha.re") {
+        const pathname = document.location.pathname;
+        switch (pathname) {
+            case "/":
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Viewing Frontpage";
+                break;
+            default:
+                //Idle
+                presenceData.startTimestamp = browsingStamp;
+                presenceData.details = "Idling";
+                presenceData.state = "Doing... Something?";
+                break;
+        }
+    }
+
+    if (presenceData.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    } else {
+        presence.setActivity(presenceData);
+    }
+});
