c9b22af83e089f8b10e16510ec024b2e3e8aee41
a6doff <67640562+a6doff@users.noreply.github.com>
2020-09-07 04:50:46 +0200

    Update presence.ts

--- websites/O/OnlyHit/presence.ts
+++ websites/O/OnlyHit/presence.ts
@@ -3,10 +3,8 @@
   });
   
   presence.on("UpdateData", () => {
-    const title = `${
-      document.querySelector(".artist").textContent
-    } - ${document.querySelector(".title").textContent} `;
-    const radio = document.querySelector(".stream-name").textContent;
+    const title = `${document.querySelector(".artist").textContent} - ${document.querySelector(".title").textContent} `,
+          radio = document.querySelector(".stream-name").textContent;
     let logo = 'logo_onlyhit';
     const status = 'play';
     if (document.querySelector(".stream-name").textContent == 'OnlyHit Gold') {
@@ -18,8 +16,8 @@
     if (document.querySelector(".stream-name").textContent == 'OnlyHit K-Pop') {
       logo = 'onlyhit_k-pop';
     }
-    const pageName = document.title.slice(13);
-    const presenceData: PresenceData = {
+    const pageName = document.title.slice(13),
+          presenceData: PresenceData = {
       largeImageKey: `${logo}`,
       smallImageKey: `${status}`,
       smallImageText: `Viewing: ${pageName}`
