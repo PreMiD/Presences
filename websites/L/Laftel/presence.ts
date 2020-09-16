dcdc69b83a223a238cdae8f14535f8cd334df222
PIKOSTUDIO <parangee9706@gmail.com>
2020-09-16 20:10:22 +0900

    add icon and thumbnail

--- websites/L/Laftel/presence.ts
+++ websites/L/Laftel/presence.ts
@@ -4,6 +4,28 @@ var presence = new Presence({
 
 var prev = ''
 
+type Detail = {
+  name?: string
+}
+
+function getTimestamps(
+  videoTime: number,
+  videoDuration: number
+): Array<number> {
+  var startTime = Date.now();
+  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  return [Math.floor(startTime / 1000), endTime];
+}
+
+var last: Detail = {}
+
+type Episode = {
+  title?: string
+  subject?: string
+}
+
+var lastEpisode: Episode = {}
+
 function getQuery() {
   var search = location.search.substring(1);
   var query = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
@@ -11,7 +33,6 @@ function getQuery() {
 }
 
 presence.on("UpdateData", async () => {
-  console.log(presence.getActivity())
   var presenceData: PresenceData = {}
 
   if (window.location.pathname === '/') {
@@ -20,6 +41,46 @@ presence.on("UpdateData", async () => {
     var query = getQuery()
     presenceData.details = '검색'
     presenceData.state = query.keyword
+  } else if (window.location.pathname.match(/^\/item\/\d/)) {
+    if (prev === window.location.pathname && last.name) {
+      presenceData.details = last.name
+    } else {
+      prev = window.location.pathname
+      last = await (await fetch(`https://laftel.net/api/v1.0/items/${window.location.pathname.split('/')[2]}/detail/`, {
+        headers: {
+          laftel: 'TeJava'
+        }
+      })).json()
+      presenceData.details = last.name
+    }
+  } else if (location.pathname.match(/\/player\/\d*\/\d/)) {
+    var video : HTMLVideoElement = document.querySelector('video')
+    if (video !== null && !isNaN(video.duration)) {
+      if (prev !== window.location.pathname) {
+        prev = window.location.pathname
+        lastEpisode = await (await fetch(`https://laftel.net/api/episodes/v1/${window.location.pathname.split('/')[3]}`, {
+          headers: {
+            laftel: 'TeJava'
+          }
+        })).json()
+      }
+
+      presenceData.details = `${lastEpisode.title} - ${lastEpisode.subject}`
+
+      if (!!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2)) {
+        var timestamps = getTimestamps(
+          Math.floor(video.currentTime),
+          Math.floor(video.duration)
+        )
+  
+        presenceData.startTimestamp = timestamps[0]
+        presenceData.endTimestamp = timestamps[1]
+      } else {
+        presenceData.startTimestamp = null
+        presenceData.endTimestamp = null
+        presenceData.state = '일시 정지됨'
+      }
+    }
   }
 
   if (presenceData.details == null) {
