ec3cc9dfb99a3adccf13bd0da5600f0b6e6fb5bb
SeMiD <contact@premid.app>
2020-09-16 00:37:32 +0000

    ♻️ Refactoring
    Signed-off-by: SeMiD <contact@premid.app>

--- websites/M/Mixcloud/presence.ts
+++ websites/M/Mixcloud/presence.ts
@@ -1,20 +1,25 @@
 const presence = new Presence({
-  clientId: "610102236374368267"
-}),
-strings = presence.getStrings({
-  play: "presence.playback.playing",
-  pause: "presence.playback.paused",
-  live: "presence.activity.live"
-});
+    clientId: "610102236374368267"
+  }),
+  strings = presence.getStrings({
+    play: "presence.playback.playing",
+    pause: "presence.playback.paused",
+    live: "presence.activity.live"
+  });
 
 let live, prevLive, author: string, title: string;
 
 presence.on("UpdateData", async () => {
-  const player = document.querySelector(".PlayerControls__PlayerContainer-vo7mt3-4");
+  const player = document.querySelector(
+    ".PlayerControls__PlayerContainer-vo7mt3-4"
+  );
 
   if (player) {
-    const paused = document.querySelector(".PlayButton__PlayerControl-rvh8d9-1") === null,
-    on_air = document.querySelector(".LiveStreamPage__LiveStreamPageHeader-hwzri8-1");
+    const paused =
+        document.querySelector(".PlayButton__PlayerControl-rvh8d9-1") === null,
+      on_air = document.querySelector(
+        ".LiveStreamPage__LiveStreamPageHeader-hwzri8-1"
+      );
 
     if (on_air) {
       live = true;
@@ -26,10 +31,15 @@ presence.on("UpdateData", async () => {
     }
 
     if (!live) {
-      title = document.querySelector(".shared__ShowDetails-gqlx6k-1 > a:nth-child(1)").textContent;
-      author = document.querySelector("p.Typography__BodyExtraSmall-sc-1swbs0s-16:nth-child(2) > a:nth-child(1)").textContent;
+      title = document.querySelector(
+        ".shared__ShowDetails-gqlx6k-1 > a:nth-child(1)"
+      ).textContent;
+      author = document.querySelector(
+        "p.Typography__BodyExtraSmall-sc-1swbs0s-16:nth-child(2) > a:nth-child(1)"
+      ).textContent;
     } else {
-      title = document.querySelector(".Typography__Headline1Small-sc-1swbs0s-8").textContent;
+      title = document.querySelector(".Typography__Headline1Small-sc-1swbs0s-8")
+        .textContent;
       author = "Mixcloud Live";
     }
 
@@ -50,7 +60,7 @@ presence.on("UpdateData", async () => {
       delete data.startTimestamp;
       delete data.endTimestamp;
     }
-    
+
     presence.setActivity(
       {
         details: data.details,
@@ -59,7 +69,7 @@ presence.on("UpdateData", async () => {
       },
       true
     );
-   } else if (title !== null && author !== null) {
-     presence.setActivity(data, !paused);
-   }
+  } else if (title !== null && author !== null) {
+    presence.setActivity(data, !paused);
+  }
 });
