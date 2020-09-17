bc94b53ca131e5479cc83bb0400748f4e3cb6d0c
파랑이 <68010770+pikokr@users.noreply.github.com>
2020-09-17 17:04:51 +0900

    Update presence.ts

--- websites/L/Laftel/presence.ts
+++ websites/L/Laftel/presence.ts
@@ -1,8 +1,8 @@
-var presence = new Presence({
+const presence = new Presence({
   clientId: "755666356141293568"
-})
+});
 
-var prev = ''
+let prev = '';;
 
 type Detail = {
   name?: string
@@ -12,52 +12,52 @@ function getTimestamps(
   videoTime: number,
   videoDuration: number
 ): Array<number> {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  let startTime = Date.now();
+  let endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
   return [Math.floor(startTime / 1000), endTime];
-}
+};
 
-var last: Detail = {}
+let last: Detail = {};
 
 type Episode = {
   title?: string
   subject?: string
-}
+};
 
-var lastEpisode: Episode = {}
+let lastEpisode: Episode = {};
 
 function getQuery() {
-  var search = location.search.substring(1);
-  var query = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+  let search = location.search.substring(1);
+  let query = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
   return query
 }
 
 presence.on("UpdateData", async () => {
-  var presenceData: PresenceData = {}
+  let presenceData: PresenceData = {};
 
   if (window.location.pathname === '/') {
     presenceData.details = '메인'
   } else if (window.location.pathname.startsWith('/search')) {
-    var query = getQuery()
-    presenceData.details = '검색'
-    presenceData.state = query.keyword
+    let query = getQuery();
+    presenceData.details = '검색';
+    presenceData.state = query.keyword;
   } else if (window.location.pathname.match(/^\/item\/\d/)) {
     if (prev === window.location.pathname && last.name) {
-      presenceData.details = last.name
+      presenceData.details = last.name;
     } else {
-      prev = window.location.pathname
+      prev = window.location.pathname;
       last = await (await fetch(`https://laftel.net/api/v1.0/items/${window.location.pathname.split('/')[2]}/detail/`, {
         headers: {
           laftel: 'TeJava'
         }
-      })).json()
-      presenceData.details = last.name
+      })).json();
+      presenceData.details = last.name;
     }
   } else if (location.pathname.match(/\/player\/\d*\/\d/)) {
-    var video : HTMLVideoElement = document.querySelector('video')
+    let video : HTMLVideoElement = document.querySelector('video');
     if (video !== null && !isNaN(video.duration)) {
       if (prev !== window.location.pathname) {
-        prev = window.location.pathname
+        prev = window.location.pathname;
         lastEpisode = await (await fetch(`https://laftel.net/api/episodes/v1/${window.location.pathname.split('/')[3]}`, {
           headers: {
             laftel: 'TeJava'
@@ -68,7 +68,7 @@ presence.on("UpdateData", async () => {
       presenceData.details = `${lastEpisode.title} - ${lastEpisode.subject}`
 
       if (!!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2)) {
-        var timestamps = getTimestamps(
+        let timestamps = getTimestamps(
           Math.floor(video.currentTime),
           Math.floor(video.duration)
         )
