87166b802476bfdada80c0c19c096275bcffdb3e
파랑이 <68010770+pikokr@users.noreply.github.com>
2020-09-17 17:12:22 +0900

    Update presence.ts

--- websites/L/Laftel/presence.ts
+++ websites/L/Laftel/presence.ts
@@ -2,7 +2,7 @@ const presence = new Presence({
   clientId: "755666356141293568"
 });
 
-let prev = '';;
+let prev = '';
 
 type Detail = {
   name?: string
@@ -12,10 +12,9 @@ function getTimestamps(
   videoTime: number,
   videoDuration: number
 ): Array<number> {
-  let startTime = Date.now();
-  let endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  const startTime = Date.now(), endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
   return [Math.floor(startTime / 1000), endTime];
-};
+}
 
 let last: Detail = {};
 
@@ -27,18 +26,17 @@ type Episode = {
 let lastEpisode: Episode = {};
 
 function getQuery() {
-  let search = location.search.substring(1);
-  let query = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-  return query
+  const search = location.search.substring(1), query = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+  return query;
 }
 
 presence.on("UpdateData", async () => {
-  let presenceData: PresenceData = {};
+  const presenceData: PresenceData = {};
 
   if (window.location.pathname === '/') {
-    presenceData.details = '메인'
+    presenceData.details = '메인''
   } else if (window.location.pathname.startsWith('/search')) {
-    let query = getQuery();
+    const query = getQuery();
     presenceData.details = '검색';
     presenceData.state = query.keyword;
   } else if (window.location.pathname.match(/^\/item\/\d/)) {
@@ -54,7 +52,7 @@ presence.on("UpdateData", async () => {
       presenceData.details = last.name;
     }
   } else if (location.pathname.match(/\/player\/\d*\/\d/)) {
-    let video : HTMLVideoElement = document.querySelector('video');
+    const video : HTMLVideoElement = document.querySelector('video');
     if (video !== null && !isNaN(video.duration)) {
       if (prev !== window.location.pathname) {
         prev = window.location.pathname;
@@ -62,31 +60,31 @@ presence.on("UpdateData", async () => {
           headers: {
             laftel: 'TeJava'
           }
-        })).json()
+        })).json();
       }
 
-      presenceData.details = `${lastEpisode.title} - ${lastEpisode.subject}`
+      presenceData.details = `${lastEpisode.title} - ${lastEpisode.subject}`;
 
-      if (!!(video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2)) {
-        let timestamps = getTimestamps(
+      if (video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2) {
+        const timestamps = getTimestamps(
           Math.floor(video.currentTime),
           Math.floor(video.duration)
-        )
+        );
   
-        presenceData.startTimestamp = timestamps[0]
-        presenceData.endTimestamp = timestamps[1]
+        presenceData.startTimestamp = timestamps[0];
+        presenceData.endTimestamp = timestamps[1];
       } else {
-        presenceData.startTimestamp = null
-        presenceData.endTimestamp = null
-        presenceData.state = '일시 정지됨'
+        presenceData.startTimestamp = null;
+        presenceData.endTimestamp = null;
+        presenceData.state = '일시 정지됨';
       }
     }
   }
 
   if (presenceData.details == null) {
-    presence.setTrayTitle()
-    presence.setActivity()
+    presence.setTrayTitle();
+    presence.setActivity();
   } else {
-    presence.setActivity(presenceData)
+    presence.setActivity(presenceData);
   }
 });
