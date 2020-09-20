85a9a7d2c6f96d0dc1103c4b052cbdebe93c7088
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2020-09-20 09:57:35 -0300

    Add files via upload

---
+++ websites/B/Broken Picturephone/presence.ts
@@ -0,0 +1,24 @@
+var presence = new Presence({
+    clientId: "756196794727399617"
+}), strings = presence.getStrings({
+    play: "presence.playback.playing",
+    pause: "presence.playback.paused"
+});
+var browsingStamp = Math.floor(Date.now() / 1000);
+presence.on("UpdateData", async () => {
+
+var presenceData = {
+        largeImageKey: "large_image",
+        smallImageKey: "playing",
+        smallImageText: "Playing",
+        details: "In game",
+        startTimestamp: browsingStamp
+            } 
+    if (presenceData.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    }
+    else {
+        presence.setActivity(presenceData);
+    }
+});
