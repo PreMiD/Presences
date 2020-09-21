56e1817d1593d1d5d0a3b491c9912ed2b02ceb0a
luisgabriel53 <harrycorpmail@gmail.com>
2020-09-21 16:17:07 -0300

    adding

---
+++ websites/B/Broken Picturephone/presence.ts
@@ -0,0 +1,41 @@
+const presence = new Presence({
+    clientId: "756196794727399617"
+});
+
+presence.on("UpdateData", async () => {
+
+    const browsingStamp = Math.floor(Date.now() / 1000);
+
+    const button = document.getElementsByTagName('button');
+    const valor = button.length;
+    const players = document.getElementsByClassName('userActive');
+    const nump = `(${players.length} of 6 players)`;
+
+        const data: PresenceData = {
+            largeImageKey: "large_image",
+            startTimestamp: browsingStamp;
+                };
+
+        if (valor == 1) {
+            data.details = "Creating a Room";
+            data.smallImageKey = "home";
+            data.smallImageText = "On homepage";
+        };
+        if (valor >= 6) {
+            data.details = "Playing";
+            data.state = nump;
+            data.smallImageKey = "playing";
+            data.smallImageText = "On game";
+            data.startTimestamp = browsingStamp
+        };
+        if (players.length > 6) {
+            data.state = "(6 of 6 players)";
+        };
+        if (data.details == null) {
+            presence.setTrayTitle();
+            presence.setActivity();
+        };
+        else {
+            presence.setActivity(data);
+        };
+});
