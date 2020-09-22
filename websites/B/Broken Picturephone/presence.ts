de842501472763913ad8fa8b6848cde41d3b169f
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2020-09-22 14:00:39 -0300

    Update presence.ts

--- websites/B/Broken Picturephone/presence.ts
+++ websites/B/Broken Picturephone/presence.ts
@@ -20,22 +20,22 @@ presence.on("UpdateData", async () => {
             data.details = "Creating a Room";
             data.smallImageKey = "home";
             data.smallImageText = "On homepage";
-        };
+        }
         if (valor >= 6) {
             data.details = "Playing";
             data.state = nump;
             data.smallImageKey = "playing";
             data.smallImageText = "On game";
             data.startTimestamp = browsingStamp
-        };
+        }
         if (players.length > 6) {
             data.state = "(6 of 6 players)";
-        };
+        }
         if (data.details == null) {
             presence.setTrayTitle();
             presence.setActivity();
-        };
+        }
         else {
             presence.setActivity(data);
-        };
-})
+        }
+});
