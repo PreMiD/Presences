e21dacd1d40cab6f3b12054cb57e27d314517934
Umbara <gfjaru@gmail.com>
2020-09-23 17:55:42 +0700

    Update presence.ts

--- websites/C/CatchPlay/presence.ts
+++ websites/C/CatchPlay/presence.ts
@@ -7,7 +7,6 @@ const presence = new Presence({
     browsing: "presence.activity.browsing"
   });
 
-
 /**
  * Get Timestamps
  * @param {Number} videoTime Current video time seconds
@@ -17,8 +16,8 @@ function getTimestamps(
   videoTime: number,
   videoDuration: number
 ): Array<number> {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  const startTime = Date.now(),
+          endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
   return [Math.floor(startTime / 1000), endTime];
 }
 
@@ -26,46 +25,36 @@ presence.on("UpdateData", async () => {
   const data: PresenceData = {
     largeImageKey: "cp"
   };
-
   if (document.location.pathname.includes("/watch")) {
-    var video: HTMLVideoElement = document.querySelector(
+    const video: HTMLVideoElement = document.querySelector(
       ".player-box video"
     );
-
     if (video && !isNaN(video.duration)) {
-
       if (document.querySelector(".CPplayer-header-subtitle")) {
         data.state = " " + document.querySelector(".CPplayer-header-subtitle").textContent;
       } else {
         data.state = "Movie";
       }
-
-      var timestamps = getTimestamps(
+      const timestamps = getTimestamps(
         Math.floor(video.currentTime),
         Math.floor(video.duration)
       );
-      
       data.details = " " +
         document.querySelector(".CPplayer-header-title span").textContent;
-
       (data.smallImageKey = video.paused ? "pause" : "play"),
         (data.smallImageText = video.paused
           ? (await strings).pause
           : (await strings).play),
         (data.startTimestamp = timestamps[0]),
         (data.endTimestamp = timestamps[1]);
-
       if (video.paused) {
         delete data.startTimestamp;
         delete data.endTimestamp;
       }
-
       presence.setActivity(data, !video.paused);
-
     }
   } else {
     data.details = (await strings).browsing;
     presence.setActivity(data);
   }
-
 });
