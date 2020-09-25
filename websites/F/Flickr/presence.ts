139c07dcce8272d17deb7249dd16773f4ad08ebf
MySixSenses <ryannotryan1190@gmail.com>
2020-09-25 08:19:23 -0700

    Update presence.ts

--- websites/F/Flickr/presence.ts
+++ websites/F/Flickr/presence.ts
@@ -25,11 +25,22 @@ presence.on("UpdateData", () => {
       } else if (document.location.pathname == "/about"){
         presenceData.details = "Viewing what Flickr is about";
       } else if (document.location.pathname == "/jobs"){
-        presenceData.details = "Viewing job oppurtunities at flickr";
+        presenceData.details = "Viewing job oppurtunities at Flickr";
       } else if (document.location.pathname == "/services/developer"){
         presenceData.details = "Viewing the Flickr Developer Guide";
-      } else if (document.location.pathname == "/help/guidelines"){
-        presenceData.details = "Viewing the Flickr Guidelines";
+      } else if (document.location.pathname.startsWith("/help")){
+        if (document.location.pathname.endsWith("/guidelines")){
+          presenceData.details = "Viewing the Flickr Guidelines";
+        } else if (document.location.pathname.endsWith("/terms")){
+          presenceData.details = "Viewing the Flickr Terms & Conditions of Use";
+        } else if (document.location.pathname.endsWith("/privacy")){
+          presenceData.details = "Viewing the Flickr Privacy Policy";
+        } else if (document.location.pathname.endsWith("/api")){
+          presenceData.details = "Viewing the Flickr APIs Terms of Use";
+        } else if (document.location.pathname.endsWith("/cookies")){
+          presenceData.details = "Viewing the Flickr Policy on Cookies";
+
+        }
       }
   }
   if (presenceData.details == null) {
