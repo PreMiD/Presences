4becbd412b44c0155dcfb2b64bc473ece67f138a
MySixSenses <ryannotryan1190@gmail.com>
2020-09-26 09:44:49 -0700

    Update presence.ts
    Adding other Flickr websites

--- websites/F/Flickr/presence.ts
+++ websites/F/Flickr/presence.ts
@@ -28,20 +28,21 @@ presence.on("UpdateData", () => {
         presenceData.details = "Viewing job oppurtunities at Flickr";
       } else if (document.location.pathname == "/services/developer"){
         presenceData.details = "Viewing the Flickr Developer Guide";
-      } else if (document.location.pathname.startsWith("/help")){
-        if (document.location.pathname.endsWith("/guidelines")){
+      } else if (document.location.pathname == "/help/guidelines"){
           presenceData.details = "Viewing the Flickr Guidelines";
-        } else if (document.location.pathname.endsWith("/terms")){
+      } else if (document.location.pathname == "/help/terms"){
           presenceData.details = "Viewing the Flickr Terms & Conditions of Use";
-        } else if (document.location.pathname.endsWith("/privacy")){
+      } else if (document.location.pathname == "/help/privacy"){
           presenceData.details = "Viewing the Flickr Privacy Policy";
-        } else if (document.location.pathname.endsWith("/api")){
+      }else if (document.location.pathname.endsWith("/help/api")){
           presenceData.details = "Viewing the Flickr APIs Terms of Use";
-        } else if (document.location.pathname.endsWith("/cookies")){
+      } else if (document.location.pathname.endsWith("/help/cookies")){
           presenceData.details = "Viewing the Flickr Policy on Cookies";
-
-        }
-      }
+       } else if (document.location.pathname == "/help/dpa"){
+        presenceData.details = "Viewing the Flickr Data Processing Addendum";
+       } else if (document.location.pathname == "/explore"){
+        presenceData.details = "Exploring cool images";
+       }
   }
   if (presenceData.details == null) {
       presence.setTrayTitle();
