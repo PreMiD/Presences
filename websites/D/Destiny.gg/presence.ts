4483d150cc5c9da95a0d5b82e43917377501f733
mmatt625 <notmmatt@gmail.com>
2020-09-27 19:04:28 -0500

    added d.gg

---
+++ websites/D/Destiny.gg/presence.ts
@@ -0,0 +1,33 @@
+  var presence = new Presence({
+  clientId: "759921592926339072"
+});
+
+var browsingStamp = Math.floor(Date.now() / 1000);
+
+var title: any;
+var search: any;
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "destiny",
+    smallImageKey: "really_bad_d_gg_logo_upscale"
+  };
+
+  if (
+    document.location.pathname == "/" ||
+    document.location.pathname == "/home/"
+  ) {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Viewing the homepage.";
+  } else if (document.location.pathname.includes("/bigscreen")) {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Watching Destiny.";
+  }
+
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
