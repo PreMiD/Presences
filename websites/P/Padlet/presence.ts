ec9e52dd71c23eab7e11191aaf284f4a63a0b277
Mirazex <mirax.developer@gmail.com>
2020-10-01 01:00:30 +0300

    New Presence: Padlet
    added new presence (simple version) for padlet.com

---
+++ websites/P/Padlet/presence.ts
@@ -0,0 +1,45 @@
+var presence = new Presence({
+  clientId: "760981805695762441"
+});
+
+// Timestamp
+function getTimeStamp() {
+  return Math.floor(Date.now() / 1000);
+}
+
+// Variables 
+let isPadlet: boolean,
+  PadletTitle: string;
+
+presence.on("UpdateData", async () => {
+  // Presence Data
+  const data: PresenceData = {
+    largeImageKey: "padlet_image"
+  };
+
+  // Setup Routes & Query (For Features)
+  // Routes = document.location.href.replace(document.location.search, '').split("/").splice(3);
+  // Queries = Object.fromEntries(document.location.search.slice(1).split("&").map((k, i, a) => {
+  //   const item: string[] = k.replace(/\[(.*?)\]+/g, '').split('='),
+  //     Keys = a.map(i => i.replace(/\[(.*?)\]+/g, '').split('=')).filter(i => i[0] === item[0]),
+  //     Values = Keys.map(i => i[1]);
+  //   if (Keys.length === 1) return item;
+  //   else return [item[0], Values];
+  // }));
+
+  const PadletElement = document.querySelector('#surface-header .surface-title .title-heading');
+
+  PadletTitle = PadletElement && PadletElement.textContent;
+  isPadlet = !!PadletTitle;
+
+  if (isPadlet) {
+    data.details = PadletTitle;
+    data.state = 'Working with Padlet...';
+    data.startTimestamp = getTimeStamp();
+  } else {
+    data.details = 'Idle';
+    data.state = 'Idle';
+  }
+
+  presence.setActivity(data, true);
+});
