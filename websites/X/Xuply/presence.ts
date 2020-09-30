24df2412321919161c5374a1cad9a5f0b146a942
baybeku <tunanamazcihersey@gmail.com>
2020-09-30 21:45:01 +0300

    Fixed Tabs

--- websites/X/Xuply/presence.ts
+++ websites/X/Xuply/presence.ts
@@ -1,10 +1,10 @@
 const presence = new Presence({
-  clientId: "760926411090690074"
+  clientId: "760926411090690074",
 });
 
 presence.on("UpdateData", () => {
   const presenceData: PresenceData = {
-      largeImageKey: "xuply"
+      largeImageKey: "xuply",
     },
     browsingStamp = Math.floor(Date.now() / 1000),
     page = window.location.pathname;
@@ -12,36 +12,45 @@ presence.on("UpdateData", () => {
     presenceData.details = "Bir sayfa görüntülüyor:";
     presenceData.state = "Anasayfa";
     presenceData.startTimestamp = browsingStamp;
-  } else if(page.toLowerCase() === "/about") {
+  } else if (page.toLowerCase() === "/about") {
     presenceData.details = "Bir sayfa görüntülüyor:";
     presenceData.state = "Bilgilendirme";
     presenceData.startTimestamp = browsingStamp;
-  } else if(page.toLowerCase() === "/contact") {
+  } else if (page.toLowerCase() === "/contact") {
     presenceData.details = "Bir sayfa görüntülüyor:";
     presenceData.state = "İletişim";
     presenceData.startTimestamp = browsingStamp;
-  } else if( page.startsWith('/category')) {
+  } else if (page.startsWith("/category")) {
     presenceData.details = "Bir kategori görüntülüyor:";
-    presenceData.state = document.location.search ? document.querySelector("body > main > div > div > div > div > nav > ol > li:nth-child(3) > a").textContent : "Tüm Kodlar";
+    presenceData.state = document.location.search
+      ? document.querySelector(
+          "body > main > div > div > div > div > nav > ol > li:nth-child(3) > a"
+        ).textContent
+      : "Tüm Kodlar";
     presenceData.startTimestamp = browsingStamp;
-  } else if(page.toLowerCase() === "/codesharer") {
+  } else if (page.toLowerCase() === "/codesharer") {
     presenceData.details = "Bir sayfa görüntülüyor:";
     presenceData.state = "Kod paylaşım";
     presenceData.startTimestamp = browsingStamp;
-  } else if(page.startsWith("/admin")) {
+  } else if (page.startsWith("/admin")) {
     presenceData.details = "Bir sayfa görüntülüyor:";
     presenceData.state = "Admin Dashboard";
     presenceData.startTimestamp = browsingStamp;
   } else if (page.startsWith("/profile")) {
     presenceData.details = "Bir profil görüntülüyor:";
     presenceData.state =
-      document.querySelector("body > main > div > div > div > div > div > h2").textContent +
+      document.querySelector("body > main > div > div > div > div > div > h2")
+        .textContent +
       " " +
       "adlı kullanıcının profilini görüntülüyor!";
     presenceData.startTimestamp = browsingStamp;
-  } else if(page.startsWith('/code')) {
-	presenceData.details = "Bir kod görüntülüyor:";
-	presenceData.state = document.querySelector("body > main > div > div > div > div > div > h2").textContent + " " + "adlı kodu görüntülüyor."
+  } else if (page.startsWith("/code")) {
+    presenceData.details = "Bir kod görüntülüyor:";
+    presenceData.state =
+      document.querySelector("body > main > div > div > div > div > div > h2")
+        .textContent +
+      " " +
+      "adlı kodu görüntülüyor.";
     presenceData.startTimestamp = browsingStamp;
   }
   if (presenceData.details == null) {
