fa92a4947a69001db8afc55826445e98f793881a
Zallom DEV <zallomsuryt@gmail.com>
2020-09-30 23:35:07 +0200

    Presence Fix

--- websites/D/Discord FR/presence.ts
+++ websites/D/Discord FR/presence.ts
@@ -1,72 +1,54 @@
-var presence = new Presence({
+const presence = new Presence({
   clientId: "563836644579606528" // CLIENT DISCORD FR
-});
-
-var title: any;
-
-var browsingStamp = Math.floor(Date.now() / 1000);
+}),
+browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-  const presenceData: PresenceData
-
+  const presenceData: PresenceData = {
+    largeImageKey: "discordfr",
+    smallImageKey: "rooster"
+  };
   presenceData.startTimestamp = browsingStamp;
-  if (document.location.hostname == "discord.fr") {
-    if (document.location.pathname == "") {
+
+  if (document.location.hostname == "") {
+    if (document.location.pathname == "/") {
       presenceData.details = "Wiki, blog et tutoriels en français";
       presenceData.state = "discord.fr";
-
-      presenceData.largeImageKey = "discordfr";
-      presenceData.smallImageKey = "rooster";
 
       presence.setActivity(presenceData);
     } else if (document.location.pathname == "/serveur/") {
       presenceData.details = "Serveur d'entraide français.";
       presenceData.state = "discord.gg/fr";
 
-      presenceData.smallImageKey = "dfr";
       presenceData.largeImageKey = "serveur";
+      presenceData.smallImageKey = "dfr";
 
       presence.setActivity(presenceData);
-    }
-    else if (document.location.pathname == "/blog/") {
+    } else if (document.location.pathname == "/blog/") {
       presenceData.details = "Consulte le Blog";
 
-      presenceData.smallImageKey = "dfr";
       presenceData.largeImageKey = "blog";
+      presenceData.smallImageKey = "dfr";
 
       presence.setActivity(presenceData);
     } else if (document.location.pathname == "/wiki/") {
       presenceData.details = "Consulte le wiki";
 
-      presenceData.smallImageKey = "dfr";
       presenceData.largeImageKey = "wiki";
+      presenceData.smallImageKey = "dfr";
 
       presence.setActivity(presenceData);
     } else if (document.location.pathname == "/recrutement/") {
       presenceData.details = "Consulte la page de recrutement";
 
-      presenceData.smallImageKey = "dfr";
       presenceData.largeImageKey = "recrutement";
+      presenceData.smallImageKey = "dfr";
 
       presence.setActivity(presenceData);
     } else if (document.location.pathname == "/partenariat/") {
       presenceData.details = "Consulte la page de partenariat";
 
-      presenceData.smallImageKey = "dfr";
       presenceData.largeImageKey = "partenariat";
-
-      presence.setActivity(presenceData);
-    } else {
-      presence.setActivity();
-      presence.setTrayTitle();
-    }
-  }
-  if (document.location.hostname == "i.discord.fr") {
-    if (document.location.pathname == "/") {
-      presenceData.details = "Consulte une image";
-      presenceData.state = "i.discord.fr";
-
-      presenceData.largeImageKey = "image";
       presenceData.smallImageKey = "dfr";
 
       presence.setActivity(presenceData);
@@ -74,9 +56,16 @@ presence.on("UpdateData", async () => {
       presence.setActivity();
       presence.setTrayTitle();
     }
-  }
-  if (document.location.hostname == "support.discord.fr") {
-    if (document.location.pathname == "") {
+  } else if (document.location.hostname == "i.discord.fr") {
+    presenceData.details = "Consulte une image";
+    presenceData.state = "i.discord.fr";
+
+    presenceData.largeImageKey = "image";
+    presenceData.smallImageKey = "dfr";
+
+    presence.setActivity(presenceData);
+  } else if (document.location.hostname == "support.discord.fr") {
+    if (document.location.pathname == "/") {
       presenceData.details = "Team Support";
       presenceData.state = "via ModMail";
 
@@ -89,42 +78,42 @@ presence.on("UpdateData", async () => {
       presenceData.details = "Consulte une archive de ticket";
       presenceData.state = codelogs[2];
 
-      presenceData.smallImageKey = "dfr";
       presenceData.largeImageKey = "logs";
+      presenceData.smallImageKey = "dfr";
 
       presence.setActivity(presenceData);
     } else {
       presence.setActivity();
       presence.setTrayTitle();
     }
-  }
-  if (document.location.hostname == "mc.discord.fr") {
-    if (document.location.pathname == "") {
+  } else if (document.location.hostname == "mc.discord.fr") {
+    if (document.location.pathname == "/") {
       presenceData.details = "Serveur Minecraft de Discord.FR";
       presenceData.state = "mc.discord.fr (Minecraft 1.16.3)";
 
-      presenceData.smallImageKey = "dfr";
       presenceData.largeImageKey = "minecraft";
+      presenceData.smallImageKey = "dfr";
 
       presence.setActivity(presenceData);
     } else {
       presence.setActivity();
       presence.setTrayTitle();
     }
-  }
-  if (document.location.hostname == "giveaways.discord.fr") {
-    if (document.location.pathname == "") {
+  } else if (document.location.hostname == "giveaways.discord.fr") {
+    if (document.location.pathname == "/") {
       presenceData.details = "Dashboard d'inscription";
       presenceData.state = "Giveaways sur discord.gg/fr";
 
-      presenceData.smallImageKey = "dfr";
       presenceData.largeImageKey = "giveaways";
+      presenceData.smallImageKey = "dfr";
 
       presence.setActivity(presenceData);
     } else {
       presence.setActivity();
       presence.setTrayTitle();
     }
+  } else {
+    presence.setActivity();
+    presence.setTrayTitle();
   }
-
 });
