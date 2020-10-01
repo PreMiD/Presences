f224e84737caf7b10aa4389c41b25c218162baa9
Amy Y <ctrlalternativedelete@gmail.com>
2020-10-01 18:42:17 +1000

    updated everything

--- websites/J/Jstris/presence.ts
+++ websites/J/Jstris/presence.ts
@@ -9,29 +9,47 @@ presence.on("UpdateData", async () => {
   };
   
   if (document.location.hostname == "jstris.jezevec10.com") {
-      const pathname = document.location.pathname;
-      switch (pathname) {
-
-          case "/":
-              presenceData.startTimestamp = browsingStamp;
+      const pathname = document.location.pathname.split("/").splice(1);
+      presenceData.startTimestamp = browsingStamp;
+      switch (pathname[0]) {
+          //Play Modes
+          case "":
               const queryString = document.location.search.substring(1)
+              presenceData.smallImageKey = "play";
+              presenceData.smallImageText = "Ingame...";
               if (queryString) {
-                var queryObj = parseQuery(document.location.search.substring(1));
+                const queryObj = parseQuery(document.location.search.substring(1));
                 switch (queryObj.play) {
                   case "1":
-                  const LinesMode = {"2":"20 Lines", "1": "40 Lines", "3": "100 Lines", "4": "1000 Lines"};
-                  presenceData.details = "Sprint"
-
-                  presenceData.state = LinesMode[queryObj.mode];
+                    presenceData.details = "Sprint"
+                    if(queryObj.rule) {
+                      presenceData.state = "Special Ruleset"
+                    }
+                    else {
+                      presenceData.state = sprintLineMode(queryObj.mode);
+                    }
                     break;
                   case "2":
-  
+                    presenceData.details = "Practise"
                     break;
                   case "3":
                     presenceData.details = "Cheese Race"
                     break;
-                  default:
-                  
+                  case "4":
+                    presenceData.details = "Survival"
+                    break;
+                  case "5":
+                    presenceData.details = "Ultra"
+                    break;
+                  case "6":
+                    presenceData.details = "Playing Custom Map"
+                    presenceData.state = "Map ID: " + queryObj.map;
+                    break;
+                  case "7":
+                    presenceData.details = "20TSD"
+                    break;
+                  case "8":
+                    presenceData.details = "PC Mode"
                     break;
                 }
               }
@@ -39,20 +57,42 @@ presence.on("UpdateData", async () => {
                 presenceData.details = "Live"
               }
               break;
-
+          //Leaderboards
+          case "sprint": 
+            presenceData.details = leaderboardText("Sprint");
+            break;
+          case "cheese": 
+            presenceData.details = leaderboardText("Cheese Race");
+            break;
+          case "survival": 
+            presenceData.details = leaderboardText("Survival");
+            break;
+          case "ultra": 
+            presenceData.details = leaderboardText("Ultra");
+            break;
+          case "20TSD": 
+            presenceData.details = leaderboardText("20TSD");
+            break;
+          case "PC-mode": 
+            presenceData.details = leaderboardText("PC Mode");
+            break;
+          //Maps
+          case "maps": 
+            presenceData.details = "Browsing Maps";
+            break;
+          case "map": 
+            presenceData.details = "Viewing Map: " + document.querySelector("h1").innerText;
+            presenceData.state = "Map ID: " + pathname[1]
+            break;
+          //User
+          case "u": 
+            presenceData.details = "Viewing User: " + pathname[1];
+            presenceData.state = (<HTMLElement> document.querySelector(".col-md-8")).innerText;
+            break;
           default:
-              //Idle
-              presenceData.startTimestamp = browsingStamp;
-              presenceData.details = "Idling";
-              break;
-      }
-      if (pathname.startsWith("/song")) {
-          presenceData.startTimestamp = browsingStamp;
-          presenceData.details = document.querySelector(".song-title").innerHTML;
-          presenceData.state = document.querySelector(".song-artist").innerHTML;
-          if (document.querySelector(".player-active")) {
-              presenceData.smallImageKey = "play";
-          }
+            //Idle
+            presenceData.details = "Idling";
+            break;
       }
   }
 
@@ -66,4 +106,23 @@ presence.on("UpdateData", async () => {
 
 function parseQuery(search : string) {
   return JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) });
+};
+function leaderboardText(innerText : string) {
+  return "Browsing "+innerText+" Leaderboards";
+};
+function sprintLineMode(mode : string) {
+  switch (mode) {
+    case "1":
+       return "40 Lines";
+      break;
+    case "2":
+       return "20 Lines";
+      break;
+    case "3":
+      return "100 Lines";
+      break;
+    case "4":
+      return "1000 Lines";
+      break;
+  }
 }
