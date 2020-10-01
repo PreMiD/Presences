bd90a5977a55f6e91edeb71ef18da9ce068ef865
Amy Y <ctrlalternativedelete@gmail.com>
2020-10-01 19:20:18 +1000

    fixed formatting

--- websites/J/Jstris/presence.ts
+++ websites/J/Jstris/presence.ts
@@ -8,92 +8,91 @@ presence.on("UpdateData", async () => {
       largeImageKey: "bigdefault"
   };
   
-      const pathname = document.location.pathname.split("/").splice(1),
-        queryString = document.location.search.substring(1);
-      presenceData.startTimestamp = browsingStamp;
-      switch (pathname[0]) {
-          //Play Modes
-          case "":
-              presenceData.smallImageKey = "play";
-              presenceData.smallImageText = "Ingame...";
-              if (queryString) {
-                const queryObj = parseQuery(queryString);
-                switch (queryObj.play) {
-                  case "1":
-                    presenceData.details = "Sprint";
-                    if(queryObj.rule) {
-                      presenceData.state = "Special Ruleset";
-                    }
-                    else {
-                      presenceData.state = sprintLineMode(queryObj.mode);
-                    }
-                    break;
-                  case "2":
-                    presenceData.details = "Practise";
-                    break;
-                  case "3":
-                    presenceData.details = "Cheese Race";
-                    break;
-                  case "4":
-                    presenceData.details = "Survival";
-                    break;
-                  case "5":
-                    presenceData.details = "Ultra";
-                    break;
-                  case "6":
-                    presenceData.details = "Playing Custom Map";
-                    presenceData.state = "Map ID: " + queryObj.map;
-                    break;
-                  case "7":
-                    presenceData.details = "20TSD";
-                    break;
-                  case "8":
-                    presenceData.details = "PC Mode";
-                    break;
-                }
+    const pathname = document.location.pathname.split("/").splice(1),
+      queryString = document.location.search.substring(1);
+    presenceData.startTimestamp = browsingStamp;
+    switch (pathname[0]) {
+        //Play Modes
+        case "":
+            presenceData.smallImageKey = "play";
+            presenceData.smallImageText = "Ingame...";
+            if (queryString) {
+              const queryObj = parseQuery(queryString);
+              switch (queryObj.play) {
+                case "1":
+                  presenceData.details = "Sprint";
+                  if(queryObj.rule) {
+                    presenceData.state = "Special Ruleset";
+                  }
+                  else {
+                    presenceData.state = sprintLineMode(queryObj.mode);
+                  }
+                  break;
+                case "2":
+                  presenceData.details = "Practise";
+                  break;
+                case "3":
+                  presenceData.details = "Cheese Race";
+                  break;
+                case "4":
+                  presenceData.details = "Survival";
+                  break;
+                case "5":
+                  presenceData.details = "Ultra";
+                  break;
+                case "6":
+                  presenceData.details = "Playing Custom Map";
+                  presenceData.state = "Map ID: " + queryObj.map;
+                  break;
+                case "7":
+                  presenceData.details = "20TSD";
+                  break;
+                case "8":
+                  presenceData.details = "PC Mode";
+                  break;
               }
-              else {
-                presenceData.details = "Live";
-              }
-              break;
-          //Leaderboards
-          case "sprint": 
-            presenceData.details = leaderboardText("Sprint");
+            }
+            else {
+              presenceData.details = "Live";
+            }
             break;
-          case "cheese": 
-            presenceData.details = leaderboardText("Cheese Race");
-            break;
-          case "survival": 
-            presenceData.details = leaderboardText("Survival");
-            break;
-          case "ultra": 
-            presenceData.details = leaderboardText("Ultra");
-            break;
-          case "20TSD": 
-            presenceData.details = leaderboardText("20TSD");
-            break;
-          case "PC-mode": 
-            presenceData.details = leaderboardText("PC Mode");
-            break;
-          //Maps
-          case "maps": 
-            presenceData.details = "Browsing Maps";
-            break;
-          case "map": 
-            presenceData.details = "Viewing Map: " + document.querySelector("h1").innerText;
-            presenceData.state = "Map ID: " + pathname[1];
-            break;
-          //User
-          case "u": 
-            presenceData.details = "Viewing User: " + pathname[1];
-            presenceData.state = (<HTMLElement> document.querySelector(".col-md-8")).innerText;
-            break;
-          default:
-            //Idle
-            presenceData.details = "Idling";
-            break;
-      }
-
+        //Leaderboards
+        case "sprint": 
+          presenceData.details = leaderboardText("Sprint");
+          break;
+        case "cheese": 
+          presenceData.details = leaderboardText("Cheese Race");
+          break;
+        case "survival": 
+          presenceData.details = leaderboardText("Survival");
+          break;
+        case "ultra": 
+          presenceData.details = leaderboardText("Ultra");
+          break;
+        case "20TSD": 
+          presenceData.details = leaderboardText("20TSD");
+          break;
+        case "PC-mode": 
+          presenceData.details = leaderboardText("PC Mode");
+          break;
+        //Maps
+        case "maps": 
+          presenceData.details = "Browsing Maps";
+          break;
+        case "map": 
+          presenceData.details = "Viewing Map: " + document.querySelector("h1").innerText;
+          presenceData.state = "Map ID: " + pathname[1];
+          break;
+        //User
+        case "u": 
+          presenceData.details = "Viewing User: " + pathname[1];
+          presenceData.state = (<HTMLElement> document.querySelector(".col-md-8")).innerText;
+          break;
+        default:
+          //Idle
+          presenceData.details = "Idling";
+          break;
+    }
 
   if (presenceData.details == null) {
       presence.setTrayTitle();
