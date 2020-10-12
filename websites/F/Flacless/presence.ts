3775bd9df79ed9b0ea323bdca78b21e8224f5e2c
Dandy Raka <dandyraka13@gmail.com>
2020-10-12 17:57:12 +0700

    Update presence.ts

--- websites/F/Flacless/presence.ts
+++ websites/F/Flacless/presence.ts
@@ -1,8 +1,7 @@
 const presence = new Presence({
   clientId: "765137283661692948"
-});
-
-const browsingStamp = Math.floor(Date.now() / 1000);
+}),
+browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
@@ -14,12 +13,12 @@ presence.on("UpdateData", async () => {
   if (document.location.pathname == "/") {
     presenceData.details = "Starting Flacless";
   } else if (document.location.pathname.includes("/search")) {
-    let title = document.title.split(" | ")[0].replace('on Flacless', '').trim();
+    const title = document.title.split(" | ")[0].replace('on Flacless', '').trim();
     presenceData.details = "Searching";
     presenceData.state = title;
   } else if (document.location.pathname.includes("/track")) {
-    let title = document.title.split(" by ")[0].trim();
-    let artist = document.querySelector("meta[property='article:tag']").getAttribute('content');
+    const title = document.title.split(" by ")[0].trim();
+    const artist = document.querySelector("meta[property='article:tag']").getAttribute('content');
     presenceData.details = title;
     presenceData.state = 'by ' + artist;
   }
