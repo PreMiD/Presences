c4c5aba486589bf5b4de36c50bc855f52fad6353
mmatt625 <notmmatt@gmail.com>
2020-10-12 22:33:26 -0500

    .

--- websites/D/Destiny.gg/presence.ts
+++ websites/D/Destiny.gg/presence.ts
@@ -1,7 +1,6 @@
-  const browsingStamp = Math.floor(Date.now() / 1000)
   const presence = new Presence({
   clientId: "759921592926339072"
-});
+}); const browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
