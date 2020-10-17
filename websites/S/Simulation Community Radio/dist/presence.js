e73b0418127d1b1b15a6d62e53da4de0206ad2aa
scrdevelopment <72659793+scrdevelopment@users.noreply.github.com>
2020-10-18 00:53:46 +0100

    Add files via upload

---
+++ websites/S/Simulation Community Radio/dist/presence.js
@@ -0,0 +1,36 @@
+const presence = new Presence({
+    clientId: "764480822580871198"
+});
+queueMicrotask;
+let title, artist, dj, listeners;
+function getData() {
+    window
+        .fetch("https://simulation-communinty-radio.com:448/api/nowplaying/1")
+        .then((res) => res.json())
+        .then((res) => {
+        title = res.now_playing.song.title || "Loading..";
+        artist = res.now_playing.song.artist || "Loading..";
+        dj = res.live.streamer_name || "AutoDJ";
+        listeners = res.listeners.unique || 0;
+    });
+}
+getData();
+setInterval(getData, 5000);
+presence.on("UpdateData", async () => {
+    const presenceData = {
+        largeImageKey: "scr_rich",
+        smallImageKey: "listeners"
+    };
+    if (document.location.hostname === "scradio.pw") {
+        presenceData.details = `🎵 | ${title} - ${artist}`;
+        presenceData.state = `🎙️ | ${dj}`;
+        presenceData.smallImageText = `Listeners: ${listeners}`;
+    }
+    if (presenceData.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    }
+    else {
+        presence.setActivity(presenceData);
+    }
+});
--- websites/S/Simulation Community Radio/presence.ts
+++ websites/S/Simulation Community Radio/presence.ts
@@ -1,95 +1,40 @@
 const presence = new Presence({
   clientId: "764480822580871198"
-});
+});queueMicrotask
 
-let sname, sartist, listeners, islive, presenter;
+let title: string, artist: string, dj: string, listeners: number;
 
-function metadataListener(): void {
-  const data = JSON.parse(this.responseText);
-  sname = data.currentsong.song;
-  sartist = data.currentsong.artist;
-  listeners = data.listeners.current;
-  islive = data.presenter.isencoderon;
-  presenter = data.presenter.djname;
+function getData(): void {
+  window
+    .fetch("https://simulation-communinty-radio.com:448/api/nowplaying/1")
+    .then((res) => res.json())
+    .then((res) => {
+      title = res.now_playing.song.title || "Loading..";
+      artist = res.now_playing.song.artist || "Loading..";
+      dj = res.live.streamer_name || "AutoDJ";
+      listeners = res.listeners.unique || 0;
+    });
 }
 
-function updateMetaData(): void {
-  const xhttp = new XMLHttpRequest();
-  xhttp.addEventListener("load", metadataListener);
-  xhttp.open("GET", "https://scradio.pw/api/nowplaying.json", true);
-  xhttp.send();
-}
-
-setInterval(updateMetaData, 10000);
-window.onload = function (): void {
-  updateMetaData();
-};
-
+getData();
+setInterval(getData, 5000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-      largeImageKey: "scr_rich",
-      smallImageKey: "listeners"
-    },
-    changedetails = await presence.getSetting("changedetails"),
-    changestate = await presence.getSetting("changestate"),
-    changesmalltext = await presence.getSetting("changesmalltext");
+    largeImageKey: "scr_rich",
+    smallImageKey: "listeners"
+  };
 
-  if (lastTitle != sname) {
-    lastTitle = sname;
-    lastTimeStart = Math.floor(Date.now() / 1000);
+  if (document.location.hostname === "scradio.pw") {
+    presenceData.details = `🎵 | ${title} - ${artist}`;
+    presenceData.state = `🎙️ | ${dj}`;
+    presenceData.smallImageText = `Listeners: ${listeners}`;
   }
 
-  presenceData.startTimestamp = lastTimeStart;
-
-  if (!sname) {
-    lastTitle = "Loading...";
-    sname = "Loading...";
-  } else if (!sartist) {
-    sartist = "Loading...";
-  } else if (!presenter) {
-    presenter = "Loading...";
-  } else if (!listeners) {
-    listeners = "Loading...";
-  }
-
-  if (!islive) {
-    if (changedetails) {
-      presenceData.details = changedetails
-        .replace("%song%", sname)
-        .replace("%artist%", sartist);
-    } else {
-      presenceData.details = "🎵 | " + sartist + " - " + sname;
-    }
-    if (changestate) {
-      presenceData.state = changestate.replace("%presenter%", presenter);
-    } else {
-      presenceData.state = "🎙️ | " + presenter;
-    }
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
   } else {
-    if (changedetails) {
-      presenceData.details = changedetails
-        .replace("%song%", sname)
-        .replace("%artist%", sartist);
-    } else {
-      presenceData.details = "🎵 | " + sartist + " - " + sname;
-    }
-    if (changestate) {
-      presenceData.state = changestate.replace("%presenter%", "AutoDJ");
-    } else {
-      presenceData.state = "🎙️ | " + "AutoDJ";
-    }
+    presence.setActivity(presenceData);
   }
-
-  if (changesmalltext) {
-    presenceData.smallImageText = changesmalltext.replace(
-      "%listeners%",
-      listeners
-    );
-  } else {
-    presenceData.smallImageText = "Listeners: " + listeners;
-  }
-
-  presence.setActivity(presenceData, true);
-  presence.setTrayTitle();
 });
