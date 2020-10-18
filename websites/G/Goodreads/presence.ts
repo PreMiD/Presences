5db7243ed958126387cd21235f99d241022aeffd
ilikestohack <47259933+ilikestohack@users.noreply.github.com>
2020-10-17 21:48:22 -0400

    Fixing spacing stuffs

--- websites/G/Goodreads/presence.ts
+++ websites/G/Goodreads/presence.ts
@@ -1,35 +1,35 @@
 const presence = new Presence({
-  clientId: "765622778317373440"
+	clientId: "765622778317373440"
 }),
 elapsed = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-  const presenceData: PresenceData = {
-    largeImageKey: "logo-1"
-  },
-  path = window.location.href.replace(window.location.origin, ''),
-  urlParamshere = new URLSearchParams(window.location.search);
-  
-  if(path === '/'){
-	  presenceData.details = "Browsing Home";
-  }else if(path.startsWith('/book/show/')){
-	  bookname = path.replace('/book/show/', '').replace(/[0-9]+\./, '').replace(/[0-9]+\-/, '').replace(/_/g, ' ').split('?')[0];
-	  presenceData.details = "Browsing " + bookname;
-  }else if(path.startsWith('/search')){
-	  const searchTerm = urlParamshere.get('q').replace(/\+/g, ' ');
-	  presenceData.details = "Searching for: " + searchTerm;
-  }else if(path.startsWith('/author/show/')){
-	  author = path.replace('/author/show/', '').replace(/[0-9]+\./, '').replace(/[0-9]+\-/, '').replace(window.location.search, '').replace(/_/g, ' ').split('?')[0];
-	  presenceData.details = "Viewing " + author + "'s profile";
-  }else{
-	  presenceData.details = "Browsing a goodreads page";
-  }
-  presenceData.startTimestamp = elapsed;
+	const presenceData: PresenceData = {
+		largeImageKey: "logo-1"
+	},
+	path = window.location.href.replace(window.location.origin, ''),
+	urlParamshere = new URLSearchParams(window.location.search);
 
-  if (presenceData.details == null) {
-    presence.setTrayTitle();
-    presence.setActivity();
-  } else {
-    presence.setActivity(presenceData);
-  }
+	if(path === '/'){
+		presenceData.details = "Browsing Home";
+	}else if(path.startsWith('/book/show/')){
+		bookname = path.replace('/book/show/', '').replace(/[0-9]+\./, '').replace(/[0-9]+\-/, '').replace(/_/g, ' ').split('?')[0];
+		presenceData.details = "Browsing " + bookname;
+	}else if(path.startsWith('/search')){
+		const searchTerm = urlParamshere.get('q').replace(/\+/g, ' ');
+		presenceData.details = "Searching for: " + searchTerm;
+	}else if(path.startsWith('/author/show/')){
+		author = path.replace('/author/show/', '').replace(/[0-9]+\./, '').replace(/[0-9]+\-/, '').replace(window.location.search, '').replace(/_/g, ' ').split('?')[0];
+		presenceData.details = "Viewing " + author + "'s profile";
+	}else{
+		presenceData.details = "Browsing a goodreads page";
+	}
+	presenceData.startTimestamp = elapsed;
+
+	if (presenceData.details == null) {
+		presence.setTrayTitle();
+		presence.setActivity();
+	} else {
+		presence.setActivity(presenceData);
+	}
 });
