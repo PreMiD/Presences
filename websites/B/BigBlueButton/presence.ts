d1a90f160757fbd2527ed829f6afac40687c23b3
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2020-10-20 19:12:24 +0200

    Cleanup code. Add more activities.

--- websites/B/BigBlueButton/presence.ts
+++ websites/B/BigBlueButton/presence.ts
@@ -1,33 +1,43 @@
-var presence = new Presence({
-    clientId: "768028596035649536" //The client ID of the Application created at https://discordapp.com/developers/applications
-}),
+const presence = new Presence({
+    clientId: "768028596035649536"
+});
 
-    strings = presence.getStrings({
-        play: "presence.playback.playing",
-        pause: "presence.playback.paused"
-        //You can use this to get translated strings
-    });
-
-var roomName: string;
-var inCall: boolean;
-var userCount: number;
-var userState: string;
+let roomName: string,
+    inCall: boolean,
+    userCount: number,
+    userState: string,
+    joinedRoomName: string,
+    joinedRoomTimestamp: number;
 
 function getData() {
+    if (document.location.pathname.search('client/guest-wait.html') >= 0) {
+        roomName = "Guest Lobby";
+        userCount = 0;
+        inCall = false;
+        userState = "Waiting in the Guest Lobby...";
+        return;
+    }
+
     if (document.querySelectorAll("h1")[0]) {
         roomName = document.querySelectorAll("h1")[0].textContent;
 
-        userCount = undefined;
+        userCount = null;
 
         document.querySelectorAll("div").forEach(el => {
             if (el.className.startsWith("userListColumn")) {
-                let txt = el.querySelector("h2").textContent;
-                let count = txt.split("(")[1].split(")")[0];
+                const txt = el.querySelector("h2").textContent,
+                    count = txt.split("(")[1].split(")")[0];
+
                 userCount = parseInt(count);
             }
         });
 
-        inCall = userCount !== undefined;
+        inCall = userCount !== null;
+
+        if (roomName && joinedRoomName != roomName) {
+            joinedRoomName = roomName;
+            joinedRoomTimestamp = new Date().getTime();
+        }
     }
     else {
         roomName = null;
@@ -36,22 +46,27 @@ function getData() {
     }
 
     if (inCall) {
-        document.querySelectorAll("div").forEach(el => {
-            if (el.className.startsWith("notificationsBar")) {
-                userState = el.textContent;
-                inCall = false;
-                return;
-            }
-        });
+        if (presence.getSetting("readNotificationBar")) {
+            document.querySelectorAll("div").forEach(el => {
+                if (el.className.startsWith("notificationsBar")) {
+                    userState = el.textContent;
+                    inCall = false;
+                    return;
+                }
+            });
+        }
 
         document.querySelectorAll("section").forEach(el => {
             if (el.className.startsWith("actionsbar")) {
                 userState =
-                el.querySelector('i.icon-bbb-unmute') ? "microphone"
-                    : el.querySelector('i.icon-bbb-mute') ? "muted"
-                    : el.querySelector('i.icon-bbb-audio_on') ? "headphones"
-                    : el.querySelector('i.icon-bbb-listen') ? "headphones"
-                    : "disconnected";
+                    el.querySelector('i.icon-bbb-desktop') ? "screen"
+                        : el.querySelector('i.icon-bbb-video') ? "video"
+                            : el.querySelector('i.icon-bbb-desktop_off') ? "presentation"
+                                : el.querySelector('i.icon-bbb-unmute') ? "microphone"
+                                    : el.querySelector('i.icon-bbb-mute') ? "muted"
+                                        : el.querySelector('i.icon-bbb-audio_on') ? "headphones"
+                                            : el.querySelector('i.icon-bbb-listen') ? "headphones"
+                                                : "disconnected";
             }
         });
     }
@@ -76,26 +91,29 @@ function getData() {
 
 setInterval(getData, 1000);
 
-
 presence.on("UpdateData", async () => {
-    /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
+    const userStateText =
+        userState == "screen" ? "Sharing screen..."
+            : userState == "presentation" ? "Presenting..."
+                : userState == "video" ? "Video call"
+                    : userState == "microphone" ? "Speaking..."
+                        : userState == "muted" ? "Muted"
+                            : userState == "headphones" ? "Listening..."
+                                : "Disconnected";
 
-    It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
-
-    let userStateText =
-        userState == "microphone" ? "Speaking..."
-        : userState == "muted" ? "Muted"
-        : userState == "headphones" ? "Listening..."
-        : "Disconnected";
-
-    var presenceData = {
-        largeImageKey: "logo", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-        smallImageKey: (inCall ? userState : "logo"), /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-        smallImageText: (inCall ? userStateText : "No call"), //The text which is displayed when hovering over the small image
-        details: (roomName ? roomName : userState), //The upper section of the presence text
-        state: (inCall ? `${userCount} users` : roomName ? userState : null), //The lower section of the presence text
-        startTimestamp: new Date().getTime() //The unix epoch timestamp for when to start counting from
+    const presenceData = {
+        largeImageKey: "logo",
+        smallImageKey: (inCall ? userState : "logo"),
+        smallImageText: (inCall ? userStateText : userState),
+        details: (roomName ? roomName : userState),
+        state: (inCall ? `${userCount} users` : roomName ? userState : null),
+        startTimestamp: (joinedRoomTimestamp ? joinedRoomTimestamp : new Date().getTime())
     };
+
+    if (!presenceData.details)
+        delete presenceData.details;
+    if (!presenceData.state)
+        delete presenceData.state;
 
     if (presenceData.details == null) {
         presence.setTrayTitle();
