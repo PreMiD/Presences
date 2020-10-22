1d27b2a411b3bae0aca672b28b3724cedabee66f
ArsenDevX <71617416+Xyszx@users.noreply.github.com>
2020-10-22 17:38:51 +0800

    Update presence.ts

--- websites/H/Hacktoberfest/presence.ts
+++ websites/H/Hacktoberfest/presence.ts
@@ -15,25 +15,25 @@ presence.on("UpdateData", () => {
       presenceData.details = "Reading Page:";
       presenceData.state = "FAQ";
     } else if (document.location.pathname.includes("/profile")) {
-      presenceData.details = `Viewing Page:`;
-      presenceData.state = `Checking Profile`;
+      presenceData.details = "Viewing Page:";
+      presenceData.state = "Checking Profile";
     } else if (document.location.pathname.includes("/details")) {
       presenceData.details = `Reading Page:`;
       presenceData.state = `Resources`;
     } else if (document.location.pathname.includes("/events")) {
-      presenceData.details = `Viewing Page:`;
-      presenceData.state = `all events`;
+      presenceData.details = "Viewing Page:";
+      presenceData.state = "All events";
     } else if (document.location.pathname.includes("/eventkit")) {
-      presenceData.details = `Viewing Page:`;
-      presenceData.state = `Event Organizer Kit`;
+      presenceData.details = "Viewing Page:";
+      presenceData.state = "Event Organizer Kit";
     }
     else if (document.location.pathname.includes("/updates")) {
-      presenceData.details = `Reading Page:`;
-      presenceData.state = `Hacktoberfest Update`;
+      presenceData.details = "Reading Page:";
+      presenceData.state = "Hacktoberfest Update";
     }
     else if (document.location.pathname.includes("/login")) {
-      presenceData.details = `Viewing Page:`;
-      presenceData.state = `Login`;
+      presenceData.details = "Viewing Page:";
+      presenceData.state = "Login";
     }
   }
 
