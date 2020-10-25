98074c68c8fa0b39e73b1e2d439093bd7f520d61
59054736 <59054736+GhaithAlHallak8@users.noreply.github.com>
2020-10-26 02:54:15 +0300

    Adding Custom Icons. v1.0.1

--- websites/B/Blkom/presence.ts
+++ websites/B/Blkom/presence.ts
@@ -74,6 +74,8 @@ presence.on("UpdateData", async () => {
     data.state = "Downloading Anime";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/anime/")) {
+    data.smallImageKey = "location";
+    data.smallImageText = "Viewing";
     data.details = document
       .querySelector(".name")
       .textContent.trim()
@@ -81,48 +83,64 @@ presence.on("UpdateData", async () => {
     data.state = "Viewing an Anime";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/anime-list")) {
-
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Browsing";
     data.details = "Browsing for Anime";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/series-list")) {
-
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Browsing";
     data.details = "Browsing for Series";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/movie-list")) {
-
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Browsing";
     data.details = "Browsing for Movie";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/ova-list")) {
-
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Browsing";
     data.details = "Browsing for Ova";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/ona-list")) {
-
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Browsing";
     data.details = "Browsing for Ona";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/special-list")) {
-
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Browsing";
     data.details = "Browsing for Special";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/premium")) {
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Discovering";
     data.details = "Discovering Premium";
     presence.setActivity(data);
   } else if (location.pathname === "/blog") {
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Discovering";
     data.details = "Discovering Blog";
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/post")) {
+    data.smallImageKey = "blog";
+    data.smallImageText = "Reading";
     data.details = document.querySelector(".post-title").textContent.trim();
     data.state = `Viewing ${document
       .querySelector(".publisher")
       .textContent.trim()}'s Post`;
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/timeline")) {
+    data.smallImageKey = "discovery";
+    data.smallImageText = "Discovering";
     data.details = "Discovering Timeline";
     presence.setActivity(data);
   } else if (
     location.pathname.startsWith("/user") &&
     location.pathname.includes("/ratings")
   ) {
+    data.smallImageKey = "profile";
+    data.smallImageText = "Viewing";
     data.details = `Ratings List`;
     data.state = `Viewing ${document
       .querySelector(".profile-usertitle-name")
@@ -132,6 +150,8 @@ presence.on("UpdateData", async () => {
     location.pathname.startsWith("/user") &&
     location.pathname.includes("/watching")
   ) {
+    data.smallImageKey = "profile";
+    data.smallImageText = "Viewing";
     data.details = `Watching List`;
     data.state = `Viewing ${document
       .querySelector(".profile-usertitle-name")
@@ -141,6 +161,8 @@ presence.on("UpdateData", async () => {
     location.pathname.startsWith("/user") &&
     location.pathname.includes("/completed")
   ) {
+    data.smallImageKey = "profile";
+    data.smallImageText = "Viewing";
     data.details = `Completed List`;
     data.state = `Viewing ${document
       .querySelector(".profile-usertitle-name")
@@ -150,6 +172,8 @@ presence.on("UpdateData", async () => {
     location.pathname.startsWith("/user") &&
     location.pathname.includes("/on-hold")
   ) {
+    data.smallImageKey = "profile";
+    data.smallImageText = "Viewing";
     data.details = `On-Hold List`;
     data.state = `Viewing ${document
       .querySelector(".profile-usertitle-name")
@@ -159,6 +183,8 @@ presence.on("UpdateData", async () => {
     location.pathname.startsWith("/user") &&
     location.pathname.includes("/dropped")
   ) {
+    data.smallImageKey = "profile";
+    data.smallImageText = "Viewing";
     data.details = `Dropped List`;
     data.state = `Viewing ${document
       .querySelector(".profile-usertitle-name")
@@ -168,12 +194,16 @@ presence.on("UpdateData", async () => {
     location.pathname.startsWith("/user") &&
     location.pathname.includes("/plan-to-watch")
   ) {
+    data.smallImageKey = "profile";
+    data.smallImageText = "Viewing";
     data.details = `Planned List`;
     data.state = `Viewing ${document
       .querySelector(".profile-usertitle-name")
       .textContent.trim()}'s Profile`;
     presence.setActivity(data);
   } else if (location.pathname.startsWith("/user")) {
+    data.smallImageKey = "profile";
+    data.smallImageText = "Viewing";
     data.details = `Main Page`;
     data.state = `Viewing ${document
       .querySelector(".profile-usertitle-name")
