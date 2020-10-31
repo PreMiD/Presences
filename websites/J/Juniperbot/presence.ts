ba9c5a47e7479124592e5253fd48f026589d1f9b
mrlivixx <nikidimich@ya.ru>
2020-10-31 23:36:34 +0300

    Add russian lang to presence

--- websites/J/Juniperbot/presence.ts
+++ websites/J/Juniperbot/presence.ts
@@ -1,64 +1,94 @@
 const presence = new Presence({ clientId: "739908991274057870" }),
   browsingStamp = Math.floor(Date.now() / 1000);
-
+const locales: Record<string, Record<string, string>> = {
+  "ru": {
+    "leaderboard":"Смотрит на таблицу лидеров сервера:",
+    "dashone": "В панели управления",
+    "dashtwo":"Сервер:",
+    "donate":"Читает информацию об пожертвовании",
+    "mainpage":"Главная страница",
+    "servers":"Выбирает сервер",
+    "cmds":"Смотрит список команд",
+    "stats":"Смотрит статистику бота",
+    "rank":"Меняет каточку рейтинга",
+    "fbackread":"Читает"
+  },
+  "en": {
+    "leaderboard":"Looks at the server's leaderboard:",
+    "dashone":"In dashboard",
+    "dashtwo":"Guild:",
+    "donate":"Reads information about the donation",
+    "mainpage":"Main page",
+    "servers":"Selects the server",
+    "cmds":"Looks at the list of commands",
+    "stats":"Looks at the bot's statistics",
+    "rank":"Changes the rank card",
+    "fbackread":"Reading"
+  }
+};
+let lang = ''
 presence.on("UpdateData", async () => {
-  const presenceData: PresenceData = { largeImageKey: "logo" },
-    juniper: boolean = await presence.getSetting("juniper"),
-    docs: boolean = await presence.getSetting("docs"),
-    fback: boolean = await presence.getSetting("fback");
-
+  const presenceData: PresenceData = { largeImageKey: "logo" };
+  let userLang = navigator.language;
+  if (userLang === "ru") {
+    lang = 'ru'
+  } else {
+    lang = 'en'
+  }
   if (document.location.hostname == "juniper.bot") {
-    if (juniper) {
       presenceData.startTimestamp = browsingStamp;
       if (document.location.pathname.includes("/ranking")) {
-        presenceData.details = "Looks at the server's leaderboard:";
+        presenceData.details = locales[userLang].leaderboard;
         presenceData.state = document.querySelector(
           ".guild--info h1.font-weight-thin.display-2"
         ).innerHTML;
         presenceData.smallImageKey = "list";
+        presenceData.smallImageText = 'juniper.bot'
       } else if (document.location.pathname.includes("/dashboard/")) {
-        presenceData.details = "In dashboard";
-        presenceData.state = `Guild: ${
+        presenceData.details = locales[userLang].dashone;
+        presenceData.state = `${locales[userLang].dashtwo} ${
           document.querySelector(".guild--info h1.font-weight-thin.display-2")
             .innerHTML
         }`;
       } else if (document.location.pathname.includes("/donate")) {
-        presenceData.details = "Reads information about the donation";
+        presenceData.details = locales[userLang].donate;
         presenceData.smallImageKey = "donate";
+        presenceData.smallImageText = 'juniper.bot'
       } else if (document.location.pathname == "/") {
-        presenceData.details = "Main page";
+        presenceData.details = locales[userLang].mainpage;
       } else if (document.location.pathname == "/servers") {
-        presenceData.details = "Selects the server";
+        presenceData.details = locales[userLang].servers;
         presenceData.smallImageKey = "list";
+        presenceData.smallImageText = 'juniper.bot'
       } else if (document.location.pathname == "/commands") {
-        presenceData.details = "Looks at the list of commands";
+        presenceData.details = locales[userLang].cmds;
         presenceData.smallImageKey = "list";
+        presenceData.smallImageText = 'juniper.bot'
       } else if (document.location.pathname == "/status") {
-        presenceData.details = "Looks at the bot's statistics";
+        presenceData.details = locales[userLang].stats;
         presenceData.smallImageKey = "stats";
+        presenceData.smallImageText = 'juniper.bot'
       } else if (document.location.pathname == "/user/card") {
-        presenceData.details = "Changes the rank card";
+        presenceData.details = locales[userLang].rank;
       }
     }
-  } else if (docs) {
     if (document.location.hostname == "docs.juniper.bot") {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = document.title;
       presenceData.state = "docs.juniper.bot";
       presenceData.smallImageKey = "list";
+      presenceData.smallImageText = 'juniper.bot'
     }
-  } else if (fback) {
     if (document.location.hostname == "feedback.juniper.bot") {
       presenceData.startTimestamp = browsingStamp;
       presenceData.state = "feedback.juniper.bot";
       if (document.location.pathname == "/") {
-        presenceData.details = "Main page.";
+        presenceData.details = locales[userLang].mainpage;
       } else if (document.location.pathname.includes("/posts")) {
-        presenceData.details = `Читает: ${
+        presenceData.details = `${locales[userLang].fbackread}: ${
           document.querySelector(".post-header h1").innerHTML
         }`;
       }
-    }
   }
   if (presenceData.details == null) {
     presence.setTrayTitle();
