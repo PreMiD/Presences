0eb519c6f1cc2824023b863d4395cccb0ee20522
KnuffelBeestje <bolleboormc@gmail.com>
2020-11-08 14:59:23 +0100

    Fixed bug (World Instance)
    Fixed a bug that occoured when user was viewing a world instance link.

--- websites/V/VRChat/presence.ts
+++ websites/V/VRChat/presence.ts
@@ -93,97 +93,101 @@ presence.on("UpdateData", async () => {
         break;
     }
   } else if (document.location.hostname == "vrchat.com") {
-    /* Portal */
-    if (privacymode === false) {
-      getUserName();
-      presenceData.state = "User: " + profile;
-    }
-
     if (document.location.pathname.includes("/home")) {
-      if (document.location.pathname.includes("/worlds")) {
-        presenceData.details = "Browsing Worlds";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/world")) {
-        /* Viewing a specific world*/
-        const worldname = document.querySelector(".col-md-12 > h3").textContent;
-        presenceData.details = "Viewing World:";
-        if (privacymode === false) {
-          presenceData.state = worldname;
-        } else {
-          presenceData.details = "Viewing a world";
-        }
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/avatars")) {
-        /* Viewing Avatars*/
-        presenceData.details = "Browsing Avatars";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/messages")) {
-        /* Viewing Messages*/
-        presenceData.details = "Viewing Messages";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/user")) {
-        /* Viewing a specific user*/
-        getProfileDetails();
-      } else if (document.location.pathname.includes("/profile")) {
-        /* Viewing Profile*/
-        presenceData.details = "Viewing Profile";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/search")) {
-        /* Searching */
-        const searchresult = window.location
-          .toString()
-          .substr(window.location.toString().lastIndexOf("/") + 1);
-        presenceData.details = "Searching:";
-        presenceData.state = searchresult;
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/avatar")) {
-        /* Viewing a specific avatar*/
-        const avatarname = document.querySelector("div.col-12 > h3")
-            .textContent,
-          avatarpublicstatus = document.querySelector(
-            "div.col-12.col-md-8 > h4 > span > small"
-          ).textContent;
-        presenceData.details = "Viewing Avatar:";
-        if (privacymode === false) {
-          presenceData.state = avatarname + " " + avatarpublicstatus;
-        } else {
-          presenceData.details = "Viewing an avatar";
-        }
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/playermoderations")) {
-        /* Viewing blocks & mutes*/
-        presenceData.details = "Viewing Blocks & Mutes";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/accountlink")) {
-        /* Viewing the account link page*/
-        presenceData.details = "Merging Account";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/download")) {
-        /* Viewing Download Page*/
-        presenceData.details = "Download Page";
-        presence.setActivity(presenceData);
-      } else if (
-        document.location.pathname.includes("/login") /* Login Page*/
-      ) {
-        presenceData.state = "Logging in";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/register")) {
-        /* Register Page*/
-        presenceData.state = "Creating an account";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/password")) {
-        /* Password Page*/
-        presenceData.state = "Resetting Password";
-        presence.setActivity(presenceData);
-      } else if (document.location.pathname.includes("/twofactorauth")) {
-        /* 2FA Page*/
-        presenceData.state = "Awaiting Authentication";
+      if(document.location.pathname.includes("/launch")) {
+        presenceData.details = "Launching world instance";
         presence.setActivity(presenceData);
       } else {
-        presenceData.details = "Home Page";
-        presence.setActivity(presenceData);
-      }
+        /* Portal */
+        if (privacymode === false) {
+        getUserName();
+        presenceData.state = "User: " + profile;
+        }
+        if (document.location.pathname.includes("/worlds")) {
+          presenceData.details = "Browsing Worlds";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/world")) {
+          /* Viewing a specific world*/
+          const worldname = document.querySelector(".col-md-12 > h3").textContent;
+          presenceData.details = "Viewing World:";
+          if (privacymode === false) {
+            presenceData.state = worldname;
+          } else {
+            presenceData.details = "Viewing a world";
+          }
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/avatars")) {
+          /* Viewing Avatars*/
+          presenceData.details = "Browsing Avatars";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/messages")) {
+          /* Viewing Messages*/
+          presenceData.details = "Viewing Messages";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/user")) {
+          /* Viewing a specific user*/
+          getProfileDetails();
+        } else if (document.location.pathname.includes("/profile")) {
+          /* Viewing Profile*/
+          presenceData.details = "Viewing Profile";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/search")) {
+          /* Searching */
+          const searchresult = window.location
+            .toString()
+            .substr(window.location.toString().lastIndexOf("/") + 1);
+          presenceData.details = "Searching:";
+          presenceData.state = searchresult;
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/avatar")) {
+          /* Viewing a specific avatar*/
+          const avatarname = document.querySelector("div.col-12 > h3")
+              .textContent,
+            avatarpublicstatus = document.querySelector(
+              "div.col-12.col-md-8 > h4 > span > small"
+            ).textContent;
+          presenceData.details = "Viewing Avatar:";
+          if (privacymode === false) {
+            presenceData.state = avatarname + " " + avatarpublicstatus;
+          } else {
+            presenceData.details = "Viewing an avatar";
+          }
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/playermoderations")) {
+          /* Viewing blocks & mutes*/
+          presenceData.details = "Viewing Blocks & Mutes";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/accountlink")) {
+          /* Viewing the account link page*/
+          presenceData.details = "Merging Account";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/download")) {
+          /* Viewing Download Page*/
+          presenceData.details = "Download Page";
+          presence.setActivity(presenceData);
+        } else if (
+          document.location.pathname.includes("/login") /* Login Page*/
+        ) {
+          presenceData.state = "Logging in";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/register")) {
+          /* Register Page*/
+          presenceData.state = "Creating an account";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/password")) {
+          /* Password Page*/
+          presenceData.state = "Resetting Password";
+          presence.setActivity(presenceData);
+        } else if (document.location.pathname.includes("/twofactorauth")) {
+          /* 2FA Page*/
+          presenceData.state = "Awaiting Authentication";
+          presence.setActivity(presenceData);
+        } else {
+          presenceData.details = "Home Page";
+          presence.setActivity(presenceData);
+        }
     }
+  }
   } else if (document.location.hostname == "feedback.vrchat.com") {
     if (document.location.pathname.includes("/p/")) {
       /* Viewing a post */
