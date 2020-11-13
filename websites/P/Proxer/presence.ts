9e15aa8b9b6d473ac9f3e5b9608862a6d53298c3
Paul1365972 <paul1365972@gmail.com>
2020-11-13 02:52:11 +0100

    Proxer: Include watchtime

--- websites/P/Proxer/presence.ts
+++ websites/P/Proxer/presence.ts
@@ -2,15 +2,19 @@ const presence = new Presence({
   clientId: "776479405009666098"
 });
 
-const browsingStamp = Math.floor(Date.now() / 1000);
-
-function getByXpath(xpath: string) {
-  try {
-    return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE,null).iterateNext().textContent;
-  } catch (e) {
-    return null;
-  }
+class VideoData {
+  time: number;
+  duration: number;
+  paused: boolean;
 }
+
+let videoData: VideoData = null;
+
+presence.on("iFrameData", (data: VideoData) => {
+  videoData = data;
+});
+
+const browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
   const presenceData: PresenceData = {
@@ -19,23 +23,53 @@ presence.on("UpdateData", () => {
     state: "Browsing Proxer.me"
   };
 
-  if (document.location.pathname.includes("/watch")) {
-    const anime_name = getByXpath("//*[@class='wName']") || "Unknown Anime";
-    const anime_ep = getByXpath("//*[@class='wEp']") || "?";
-    const anime_ep_max = getByXpath("//*[@id='wContainer']/tbody/tr[1]/td/text()[4]") || "";
-    const anime_lang = getByXpath("//*[@class='wLanguage']") || "?";
-    presenceData.details = `Watching ${anime_name}`;
-    presenceData.state = `Ep ${anime_ep}${anime_ep_max} (${anime_lang})`;
-    presenceData.startTimestamp = browsingStamp;
-  } else if (document.location.pathname.includes("chat")) {
+  if (document.location.pathname.startsWith("/watch")) {
+    const name = getByXpath("//*[@class='wName']") || "Unknown Anime",
+     ep = getByXpath("//*[@class='wEp']"),
+     maxEp = getByXpath("//*[@id='wContainer']/tbody/tr[1]/td/text()[4]"),
+     lang = getByXpath("//*[@class='wLanguage']"),
+     now = Date.now() / 1000;
+
+    presenceData.details = (videoData) ? (videoData.paused ? "Paused" : "Watching") : "Viewing";
+    presenceData.details += ` ${name}`;
+
+    presenceData.state = "";
+    if (ep) {
+      presenceData.state += ep;
+      if (maxEp) {
+        presenceData.state += maxEp;
+      }
+    }
+    if (lang) {
+      presenceData.state += `(${lang})`;
+    }
+
+    if (videoData) {
+      presenceData.startTimestamp = Math.floor(now - videoData.time);
+      presenceData.endTimestamp = Math.floor(now + videoData.duration - videoData.time);
+    } else {
+      presenceData.startTimestamp = browsingStamp;
+    }
+    /*
+    // For the future to make watch together requests
+    const id = /(?:\/watch\/)([0-9]*)\/([0-9]*)\/([a-z]*)/.exec(document.location.pathname);
+    if (id.length == 3) {
+      const animeId = id[0],
+       epId = id[1],
+       langId = id[2];
+    }
+    */
+  } else if (document.location.pathname.startsWith("/chat")) {
     presenceData.details = "Chatting";
-  } else if (document.location.pathname.includes("forum")) {
+  } else if (document.location.pathname.startsWith("/forum")) {
     presenceData.details = "Checking the forum";
-  } else if (document.location.pathname.includes("airing")) {
+  } else if (document.location.pathname.startsWith("/anime/airing")) {
+    presenceData.details = "Checking out Anime";
+  } else if (document.location.pathname.startsWith("/season")) {
     presenceData.details = "Checking seasonal Anime";
-  } else if (document.location.pathname.includes("gallery")) {
+  } else if (document.location.pathname.startsWith("/gallery")) {
     presenceData.details = "Checking the gallery";
-  } else if (document.location.pathname.includes("news")) {
+  } else if (document.location.pathname.startsWith("/news")) {
     presenceData.details = "Checking the news";
   }
 
@@ -46,3 +80,11 @@ presence.on("UpdateData", () => {
     presence.setActivity(presenceData);
   }
 });
+
+function getByXpath(xpath: string) {
+  try {
+    return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE,null).iterateNext().textContent;
+  } catch (e) {
+    return null;
+  }
+}
