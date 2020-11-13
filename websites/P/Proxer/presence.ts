0bb8f9936df4e89a55ff9d06fa281d2978d679a3
Paul1365972 <paul1365972@gmail.com>
2020-11-13 17:43:16 +0100

    Proxer: Add more details and states

--- websites/P/Proxer/presence.ts
+++ websites/P/Proxer/presence.ts
@@ -21,55 +21,66 @@ presence.on("UpdateData", () => {
     largeImageKey: "proxer_icon",
     details: "Idle",
     state: "Browsing Proxer.me"
-  };
+  },
+    path = document.location.pathname;
 
-  if (document.location.pathname.startsWith("/watch")) {
-    const name = getByXpath("//*[@class='wName']") || "Unknown Anime",
-     ep = getByXpath("//*[@class='wEp']"),
-     maxEp = getByXpath("//*[@id='wContainer']/tbody/tr[1]/td/text()[4]"),
-     lang = getByXpath("//*[@class='wLanguage']"),
+  if (path.startsWith("/watch")) {
+    const name = getByXpath("//*[@id='wContainer']//*[@class='wName']", e => e.textContent) || "Unknown Anime",
+       ep = getByXpath("//*[@id='wContainer']//*[@class='wEp']", e => e.textContent),
+     maxEp = getByXpath("//*[@id='wContainer']//*[@class='wEp']", e => e.nextSibling.textContent.substr(1).trim()),
+     lang = getByXpath("//*[@id='wContainer']//*[@class='wLanguage']", e => e.textContent),
+     missing = getByXpath("//*[@id='wContainer']//*[@class='wStream']/div/@style", e => e.textContent.includes("/images/misc/streamfehlt.png")),
      now = Date.now() / 1000;
 
-    presenceData.details = (videoData) ? (videoData.paused ? "Paused" : "Watching") : "Viewing";
+    if (videoData) {
+      if (!videoData.paused) {
+        presenceData.details = "Watching";
+        presenceData.startTimestamp = Math.floor(now - videoData.time);
+        presenceData.endTimestamp = Math.floor(now + videoData.duration - videoData.time);
+      } else {
+        presenceData.details = "Paused";
+      }
+    } else {
+      if (missing) {
+        presenceData.details = "Awaiting";
+      } else {
+        presenceData.details = "Watching";
+        presenceData.startTimestamp = browsingStamp;
+      }
+    }
     presenceData.details += ` ${name}`;
 
     presenceData.state = "";
     if (ep) {
       presenceData.state += ep;
       if (maxEp) {
-        presenceData.state += maxEp;
+        presenceData.state += `/${maxEp}`;
       }
-    }
-    if (lang) {
-      presenceData.state += `(${lang})`;
-    }
-
-    if (videoData) {
-      presenceData.startTimestamp = Math.floor(now - videoData.time);
-      presenceData.endTimestamp = Math.floor(now + videoData.duration - videoData.time);
-    } else {
-      presenceData.startTimestamp = browsingStamp;
+      if (lang) {
+        presenceData.state += ` (${lang})`;
+      }
     }
     /*
     // For the future to make watch together requests
-    const id = /(?:\/watch\/)([0-9]*)\/([0-9]*)\/([a-z]*)/.exec(document.location.pathname);
+    const id = /(?:\/watch\/)([0-9]*)\/([0-9]*)\/([a-z]*)/.exec(path);
     if (id.length == 3) {
       const animeId = id[0],
        epId = id[1],
        langId = id[2];
     }
     */
-  } else if (document.location.pathname.startsWith("/chat")) {
-    presenceData.details = "Chatting";
-  } else if (document.location.pathname.startsWith("/forum")) {
-    presenceData.details = "Checking the forum";
-  } else if (document.location.pathname.startsWith("/anime/airing")) {
+  } else if (path.startsWith("/info")) {
+    const info = document.title.replace(/ - Proxer\.Me$/, "");
+    presenceData.details = `Checking out ${info}`;
+  } else if (path.startsWith("/anime") || path.startsWith("/season")) {
     presenceData.details = "Checking out Anime";
-  } else if (document.location.pathname.startsWith("/season")) {
-    presenceData.details = "Checking seasonal Anime";
-  } else if (document.location.pathname.startsWith("/gallery")) {
+  } else if (path.startsWith("/chat")) {
+    presenceData.details = "Chatting";
+  } else if (path.startsWith("/forum")) {
+    presenceData.details = "Checking the forum";
+  } else if (path.startsWith("/gallery")) {
     presenceData.details = "Checking the gallery";
-  } else if (document.location.pathname.startsWith("/news")) {
+  } else if (path.startsWith("/news")) {
     presenceData.details = "Checking the news";
   }
 
@@ -81,9 +92,10 @@ presence.on("UpdateData", () => {
   }
 });
 
-function getByXpath(xpath: string) {
+function getByXpath<T>(xpath: string, extractor?: (e: Node) => T): T | Node {
+  const transformer = extractor || function (e: Node) { return e };
   try {
-    return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE,null).iterateNext().textContent;
+    return transformer(document.evaluate(xpath, document, null, XPathResult.ANY_TYPE,null).iterateNext());
   } catch (e) {
     return null;
   }
