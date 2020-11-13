1473607d9d203574c72c70cb3e71b913cbcfcfc5
xxdjbobbyxx <70226753+xxdjbobbyxx@users.noreply.github.com>
2020-11-13 09:51:37 +0000

    Added Harmony Presence

---
+++ websites/H/Harmony/presence.ts
@@ -0,0 +1,38 @@
+const presence = new Presence({
+  clientId: "529569623406936065"
+});
+
+let title: string, artist: string, dj: string, listeners: number;
+
+function getData(): void {
+  window
+    .fetch("https://api.harmonyradio.pw/stats")
+    .then((res) => res.json())
+    .then((res) => {
+      title = res.song.title || "Loading..";
+      dj = res.dj.livedj || "Loading..";
+      artist = res.song.artist || "Loading..."
+    });
+}
+
+getData();
+setInterval(getData, 5000);
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "premid",
+    smallImageKey: "live"
+  };
+
+  if (document.location.hostname === "harmonyradio.pw") {
+    presenceData.details = `💿 | ${title} by ${artist}`;
+    presenceData.state = `🎙️ | ${dj}`;
+  }
+
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
