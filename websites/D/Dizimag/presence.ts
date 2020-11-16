270b473572cd15169d9be73d6935768e0bc1a834
erdem <erdemhso@mail.ru>
2020-11-16 22:14:34 +0300

    Added Dizimag (dizimag2.org)

---
+++ websites/D/Dizimag/presence.ts
@@ -0,0 +1,117 @@
+const presence = new Presence({
+  clientId: "777146104117854230"
+});
+
+let stream: { duration: number; currentTime: number; paused: boolean };
+presence.on("iFrameData",(data: { duration: number; currentTime: number; paused: boolean }) => stream = data);
+
+/**
+ * Get Timestamps
+ * @param {Number} videoTime Current video time seconds
+ * @param {Number} videoDuration Video duration seconds
+ */
+
+function getTimestamps(
+  videoTime: number,
+  videoDuration: number
+): Array<number> {
+  var startTime = Date.now();
+  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  return [Math.floor(startTime / 1000), endTime];
+}
+
+presence.on("UpdateData", async () => {
+  const path: string = document.location.pathname;
+  const ayrac: string[] = path.split("/");
+  const test: PresenceData = {
+      largeImageKey: "dizimag"
+  };
+    if(path.startsWith("/uye-ol")){
+
+      test.details = "Bir sayfaya bakıyor:";
+      test.state = "Üye Ol";
+      test.startTimestamp = Date.now();
+
+    }else if(path.startsWith("/yabanci-diziler")){
+
+      test.details = "Bir sayfaya bakıyor:";
+      test.state = "Yabancı Diziler";
+      test.startTimestamp = Date.now();
+
+    }else if(path.startsWith("/iletisim")){
+
+      test.details = "Bir sayfaya bakıyor:";
+      test.state = "İletişim";
+      test.startTimestamp = Date.now();
+
+    }else if(path.startsWith("/gelisme-sureci")){
+
+      test.details = "Bir sayfaya bakıyor:";
+      test.state = "Gelişme Süreci";
+      test.startTimestamp = Date.now();
+
+    }else if(path.startsWith("/ne-izlesem")){
+
+      test.details = "Bir sayfaya bakıyor:";
+      test.state = "Ne İzlesem?";
+      test.startTimestamp = Date.now();
+
+    }else if(path.startsWith("/trend")){
+
+      test.details = "Bir sayfaya bakıyor:";
+      test.state = "Trendler";
+      test.startTimestamp = Date.now();
+
+    }else if(path.startsWith("/uye")){
+      let name: string = document.querySelector("span.text-white").textContent
+      test.details = "Bir profile bakıyor:";
+      test.state = name;
+      test.startTimestamp = Date.now();
+
+    }else if(path.startsWith("/oyuncu")){
+
+      let name: string = document.querySelector("div.text-orange > div.pull-left").textContent
+      test.details = "Bir oyuncuya bakıyor:";
+      test.state = name || "Bulunamadı";
+      test.startTimestamp = Date.now();
+
+    }else if(path.startsWith("/dizi") && ayrac[3] == ""){
+        let name: string = document.querySelector("h1.text-nowrap").textContent;
+        test.details = "Bir diziye bakıyor:";
+        test.state = name || "Bulunamadı"
+        test.startTimestamp = Date.now();
+
+    }else if(!isNaN(stream.duration) && !isNaN(stream.currentTime) && typeof stream.paused == "boolean"){
+
+      let name: string[] = document.querySelector("h1.text-sans > a").textContent.split("-")
+        // let name1: string = document.querySelector("span.text-orange").textContent;
+        test.details = name[0] || "Bulunamadı";
+        test.state = name[1] || "Bulunamadı";
+
+      if(stream.paused == true){
+
+        test.smallImageKey = "pause";
+        test.smallImageText = "Durduruldu";
+        delete test.startTimestamp;
+        delete test.endTimestamp;
+
+      }else{
+        let timestamps = getTimestamps(
+          Math.floor(stream?.currentTime),
+          Math.floor(stream?.duration)
+        );
+
+        test.smallImageKey = "play";
+        test.smallImageText = "Oynatılıyor";
+        test.startTimestamp = timestamps[0];
+        test.endTimestamp = timestamps[1];
+
+      }
+    }else if(path == "/" || path == "/profil"){
+      test.details = "Bir sayfaya bakıyor:";
+      test.state = "Ana Sayfa";
+      test.startTimestamp = Date.now();
+    }
+
+    presence.setActivity(test);
+});
