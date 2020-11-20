bd02e630123a8bd963ef179bdd61495a7715bfb2
Snazzah <7025343+Snazzah@users.noreply.github.com>
2020-11-20 11:49:16 -0600

    Remove comment

--- websites/N/Newgrounds/presence.ts
+++ websites/N/Newgrounds/presence.ts
@@ -214,25 +214,6 @@ presence.on('UpdateData', async () => {
       }
     }
   }
-  /*
-  if (document.location.pathname.startsWith('/skins.html')) {
-    presenceData.details = 'in Skin Manager';
-    if (document.querySelector(addPremadeSkinSelector)) {
-      const skinCategory = document.querySelector('#smpskins[style*='block'] #smpcatid');
-      presenceData.state = skinCategory ? `Adding a ${skinCategory.textContent} skin` : 'Adding a skin';
-    } else if (document.querySelector(addBonkIOSkinSelector)) {
-      presenceData.state = 'Adding a bonk2.io skin';
-    }
-  } else if (document.location.pathname.startsWith('/editor.html')) {
-    presenceData.details = 'in Skin Editor';
-  } else if (document.location.pathname.startsWith('/xpchecker.html')) {
-    presenceData.details = 'in Player XP Checker';
-  } else if (document.location.pathname.startsWith('/mapchecker.html')) {
-    presenceData.details = 'in Quick Play Map Checker';
-  } else if (document.location.pathname.startsWith('/serverstatus.html')) {
-    presenceData.details = 'Viewing Server Status';
-  }
-  */
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
