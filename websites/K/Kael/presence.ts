d8a157ce996dd60f93737346858664ff4732f3dc
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-11-28 17:43:07 -0300

    fix: codacy errors

--- websites/K/Kael/presence.ts
+++ websites/K/Kael/presence.ts
@@ -1,45 +1,42 @@
-const presence = new Presence({ clientId: "412593783696261121" });
-
-const presenceData: PresenceData = {
-  state: "Página Inicial",
-  smallImageKey: "viewing_icon",
-  smallImageText: "Website",
-  largeImageKey: "large_logo",
-  details: "Kael o bot mais divertido do discord",
-  startTimestamp: Date.now()
-};
-
-const polices = [
-  { name: "terms", title: "de Termos de Uso" },
-  { name: "privacy", title: "de Privacidade" },
-  { name: "partners", title: "para Parceiros" }
-];
-const dashCategories = [
-  { name: "general", title: "geral" },
-  { name: "welcome", title: "de boas vindas" },
-  { name: "vanity", title: "do sistema de vip" }
-];
-
-const POLICES_REGEX = /polices\?p=([a-zA-Z]+)$/;
-const COMMANDS_REGEX = /commands\?category=([a-zA-Z]+)$/;
-const DASH_CATEGORIES_REGEX = /guilds\/([0-9]{18})$/;
-const DASH_CATEGORY_REGEX = /guilds\/([0-9]{18})\/([a-zA-Z]+)$/;
+const presence = new Presence({ clientId: "412593783696261121" }),
+  presenceData: PresenceData = {
+    state: "Página Inicial",
+    smallImageKey: "viewing_icon",
+    smallImageText: "Website",
+    largeImageKey: "large_logo",
+    details: "Kael o bot mais divertido do discord",
+    startTimestamp: Date.now()
+  },
+  polices = [
+    { name: "terms", title: "de Termos de Uso" },
+    { name: "privacy", title: "de Privacidade" },
+    { name: "partners", title: "para Parceiros" }
+  ],
+  dashCategories = [
+    { name: "general", title: "geral" },
+    { name: "welcome", title: "de boas vindas" },
+    { name: "vanity", title: "do sistema de vip" }
+  ],
+  POLICES_REGEX = /polices\?p=([a-zA-Z]+)$/,
+  COMMANDS_REGEX = /commands\?category=([a-zA-Z]+)$/,
+  DASH_CATEGORIES_REGEX = /guilds\/([0-9]{18})$/,
+  DASH_CATEGORY_REGEX = /guilds\/([0-9]{18})\/([a-zA-Z]+)$/;
 
 presence.on("UpdateData", () => {
-  const { location } = document;
-  const host = location.hostname;
-  const path = `${location.pathname}${location.search || ""}`;
+  const { location } = document,
+    host = location.hostname,
+    path = `${location.pathname}${location.search || ""}`;
 
   if (host === "dash.kaelbot.xyz") {
     presenceData.smallImageText = "Dashboard";
     presenceData.smallImageKey = "viewing_black_icon";
 
     const serverNameElement = document.querySelector(
-      "#root > div > header > div:nth-child(2) > span"
-    );
-    const serverName = serverNameElement
-      ? serverNameElement.textContent
-      : "Servidor Desconhecido";
+        "#root > div > header > div:nth-child(2) > span"
+      ),
+      serverName = serverNameElement
+        ? serverNameElement.textContent
+        : "Servidor Desconhecido";
 
     if (path === "/") presenceData.state = "Seleção de Servidores";
     if (DASH_CATEGORIES_REGEX.test(path)) {
@@ -47,10 +44,8 @@ presence.on("UpdateData", () => {
     }
 
     if (DASH_CATEGORY_REGEX.test(path)) {
-      const [, , category = ""] = DASH_CATEGORY_REGEX.exec(path);
-      const categorySearch = dashCategories.find(
-        ({ name }) => name === category
-      );
+      const [, , category = ""] = DASH_CATEGORY_REGEX.exec(path),
+        categorySearch = dashCategories.find(({ name }) => name === category);
 
       presenceData.smallImageKey = "editing_black_icon";
       presenceData.state = `Editando informações ${
@@ -69,9 +64,9 @@ presence.on("UpdateData", () => {
     }
 
     if (POLICES_REGEX.test(path)) {
-      const [, police = "não definida"] = POLICES_REGEX.exec(path);
-      const policeSearch = polices.find(({ name }) => name === police);
-      const policeViewing = policeSearch ? policeSearch.title : police;
+      const [, police = "não definida"] = POLICES_REGEX.exec(path),
+        policeSearch = polices.find(({ name }) => name === police),
+        policeViewing = policeSearch ? policeSearch.title : police;
 
       presenceData.state = `Visualizando a política ${policeViewing}`;
     }
