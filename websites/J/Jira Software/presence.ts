1343043512cdb1ed90980bb386788903fca31c87
Luca Biagetti <notciabaku@gmail.com>
2020-11-29 16:40:45 +0100

    [Jira Software] Added Settings section + subpages, timestamps, code ref.

--- websites/J/Jira Software/presence.ts
+++ websites/J/Jira Software/presence.ts
@@ -11,29 +11,36 @@ presence.on("UpdateData", async () => {
     largeImageKey: "jira_bg"
   };
 
+  const path = document.location.pathname;
+
   if (document.location.hostname.match(/([a-z0-9]+)[.]atlassian[.]net/)) {
-    if (document.location.pathname == "/projects") {
-      //Projects homepage section.
-      presenceData.startTimestamp = browsingStamp;
+    //Projects homepage section.
+    if (path == "/projects" || path == "/secure/BrowseProjects.jspa") {
       presenceData.details = "Browsing projects";
-    } else if (
-      document.location.pathname.includes("/jira/software/projects/")
-    ) {
-      projectName = document.location.pathname
+      presenceData.startTimestamp = browsingStamp;
+    }
+    //Project Sections.
+    else if (path.includes("/jira/software/projects/")) {
+      projectName = path
         .replace("/jira/software/projects/", "")
         .split("/")
         .shift();
-      //Project Board page.
-      if (document.location.pathname.match(/\/[a-zA-Z0-9]+\/boards\/[0-9]$/)) {
+
+      //Project Board section.
+      if (path.match(/\/[a-zA-Z0-9]+\/boards\/[0-9]$/)) {
         var boardNumber: String = document.querySelector(
           '#jira-frontend > #helpPanelContainer > div:first-child > div > div[data-testid="Content"] > div:first-child > div:first-child > div:first-child > div > div:nth-child(2) > div:first-child > div:first-child > div > h1'
         ).innerHTML;
+
         presenceData.details = `Viewing ${boardNumber}.`;
         presenceData.state = `Project: ${projectName}`;
+        presenceData.startTimestamp = browsingStamp;
       }
-      //Project Report page.
-      else if (document.location.pathname.includes("/reports")) {
-        var reportType: String = document.location.pathname.split("/").pop();
+      //Project Report section.
+      else if (path.includes("/reports")) {
+        var reportType: String = path.split("/").pop();
+
+        presenceData.startTimestamp = browsingStamp;
         switch (reportType) {
           case "burnup":
             presenceData.details = "Analyzing Burnup report.";
@@ -57,9 +64,11 @@ presence.on("UpdateData", async () => {
             break;
         }
       }
-      //Project Issues page.
-      else if (document.location.pathname.includes("/issues/")) {
-        var issueName: String = document.location.pathname.split("/").pop();
+      //Project Issues section.
+      else if (path.includes("/issues/")) {
+        var issueName: String = path.split("/").pop();
+
+        presenceData.startTimestamp = browsingStamp;
         if (issueName == "") {
           presenceData.details = "Tracking Issues.";
           presenceData.state = `Project: ${projectName}`;
@@ -68,9 +77,56 @@ presence.on("UpdateData", async () => {
           presenceData.state = `Project: ${projectName}`;
         }
       }
-      //Project related pages with no sub-links.
+      //Project Settings section.
+      else if (path.includes("/settings/")) {
+        presenceData.startTimestamp = browsingStamp;
+        //Settings sections with no sub-links.
+        if (!path.includes("/issuetypes") && !path.includes("/apps")) {
+          var settingsSection: String = path.split("/").pop();
+
+          switch (settingsSection) {
+            case "details":
+              presenceData.details = `Modifying Details Settings.`;
+              presenceData.state = `Project: ${projectName}`;
+              break;
+            case "access":
+              presenceData.details = `Modifying Access Settings.`;
+              presenceData.state = `Project: ${projectName}`;
+              break;
+            case "notifications":
+              presenceData.details = `Modifying Notifications Settings.`;
+              presenceData.state = `Project: ${projectName}`;
+              break;
+            case "features":
+              presenceData.details = `Modifying Features Settings.`;
+              presenceData.state = `Project: ${projectName}`;
+              break;
+          }
+        }
+        //Settings sections with sub-links.
+        else {
+          presenceData.startTimestamp = browsingStamp;
+          if (path.includes("/apps/")) {
+            if (path.includes("/app-fields")) {
+              presenceData.details = `Modifying Apps Settings - App Fields.`;
+              presenceData.state = `Project: ${projectName}`;
+            } else {
+              presenceData.details = `Modifying Apps Settings - Project Automation`;
+              presenceData.state = `Project: ${projectName}`;
+            }
+          } else {
+            var issueType: String = document.querySelector(
+              '#jira-frontend > #helpPanelContainer > div > div > div[data-testid="Content"] > div:first-child > div:first-child > div > div:nth-child(2) > div > div > div:first-child > div:nth-child(3) > div > div > div:first-child > div > form > div > div > div > h1'
+            ).innerHTML;
+            presenceData.details = `Modifying Issue types - ${issueType}.`;
+            presenceData.state = `Project: ${projectName}`;
+          }
+        }
+      }
+      //Project sections with no sub-links.
       else {
-        switch (document.location.pathname.split("/").pop()) {
+        presenceData.startTimestamp = browsingStamp;
+        switch (path.split("/").pop()) {
           case "roadmap":
             presenceData.details = "Viewing Roadmap.";
             presenceData.state = `Project: ${projectName}`;
@@ -89,14 +145,14 @@ presence.on("UpdateData", async () => {
             break;
         }
       }
-    } else if (document.location.pathname.includes("/projects/")) {
-      //Project Releases page, the only one with a different link-path.
-      projectName = document.location.pathname
-        .replace("/projects/", "")
-        .split("/")
-        .pop();
+    }
+    //Project Releases section, the only one with a different path.
+    else if (path.includes("/projects/")) {
+      projectName = path.replace("/projects/", "").split("/").pop();
+
       presenceData.details = "Viewing Releases.";
       presenceData.state = `Project: ${projectName}`;
+      presenceData.startTimestamp = browsingStamp;
     }
   }
 
