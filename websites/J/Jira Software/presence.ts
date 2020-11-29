722e7be4a82bd316232825ed6951b694fcb1c7e8
Luca Biagetti <notciabaku@gmail.com>
2020-11-29 01:42:15 +0100

    [Jira Software] Added no sub-links Project related pages + Releases.

--- websites/J/Jira Software/presence.ts
+++ websites/J/Jira Software/presence.ts
@@ -4,15 +4,52 @@ var presence = new Presence({
 
 var browsingStamp = Math.floor(Date.now() / 1000);
 
+var projectName: any;
+
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "jira"
+    largeImageKey: "jira_bg"
   };
 
   if (document.location.hostname.match(/([a-z0-9]+)[.]atlassian[.]net/)) {
     if (document.location.pathname == "/projects") {
+      //Projects homepage section.
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Browsing projects";
+    } else if (
+      document.location.pathname.includes("/jira/software/projects/")
+    ) {
+      //Project related pages with no sub-links.
+      projectName = document.location.pathname
+        .replace("/jira/software/projects/", "")
+        .split("/")
+        .shift();
+      switch (document.location.pathname.split("/").pop()) {
+        case "roadmap":
+          presenceData.details = "Viewing Roadmap.";
+          presenceData.state = `Project: ${projectName}`;
+          break;
+        case "backlog":
+          presenceData.details = "Viewing Backlog.";
+          presenceData.state = `Project: ${projectName}`;
+          break;
+        case "code":
+          presenceData.details = "Reviewing Code.";
+          presenceData.state = `Project: ${projectName}`;
+          break;
+        case "pages":
+          presenceData.details = "Viewing Pages.";
+          presenceData.state = `Project: ${projectName}`;
+          break;
+      }
+    } else if (document.location.pathname.includes("/projects/")) {
+      //Project Releases page, the only one with a different link-path.
+      projectName = document.location.pathname
+        .replace("/projects/", "")
+        .split("/")
+        .pop();
+      presenceData.details = "Viewing Releases.";
+      presenceData.state = `Project: ${projectName}`;
     }
   }
 
