4f5e7a2c40172c73728a84978efdc6c75b55a0cd
geenva <geenva@users.noreply.github.com>
2020-11-29 17:43:29 +0800

    ✨ marcuscodes.me

---
+++ websites/M/marcuscodes.me/presence.ts
@@ -0,0 +1,32 @@
+const presence = new Presence({
+  clientId: "782532120047779850"
+});
+
+const timeStamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "circlegrad",
+    startTimestamp: timeStamp
+  };
+  const page = window.location.href;
+  if (page == "https://marcuscodes.me/") {
+    presenceData.details = "Home";
+  } else if (page == "https://marcuscodes.me/inkthought-legacy") {
+    presenceData.details = "inkthought labs legacy";
+  } else if (page == "https://marcuscodes.me/blog") {
+    presenceData.details = "Blog";
+  } else if (page == "https://marcuscodes.me/inkthought") {
+    presenceData.details = "inkthought labs website";
+  } else if (page == "https://marcuscodes.me/sfx") {
+    presenceData.details = "SFX page";
+  } else if (page == "https://marcuscodes.me/sfx1") {
+    presenceData.details = "SFX 1";
+  } else if (page == "https://marcuscodes.me/sfx2") {
+    presenceData.details = "SFX 2";
+  } else {
+    presenceData.details = "404";
+  }
+
+  presence.setActivity(presenceData);
+});
