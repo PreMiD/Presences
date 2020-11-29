94646296a3cdd4906e94b5510c64050f551561b6
breixopd <32527555+breixopd@users.noreply.github.com>
2020-11-29 19:57:08 +0000

    Update presence.ts

--- websites/H/havesexwith.me/presence.ts
+++ websites/H/havesexwith.me/presence.ts
@@ -1,34 +1,26 @@
 var presence = new Presence({
-    clientId: "782635291105689631" //The client ID of the Application created at https://discordapp.com/developers/applications
+    clientId: "782635291105689631"
 }),
 
 strings = presence.getStrings({
     play: "presence.playback.playing",
     pause: "presence.playback.paused"
-    //You can use this to get translated strings
 });
 
 presence.on("UpdateData", async () => {
-    /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
-
-    It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
 
 	var presenceData: PresenceData = {
-        largeImageKey: "woman", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-        smallImageKey: "doggo", /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/
-        smallImageText: "£2.50 lifetime emails", //The text which is displayed when hovering over the small image
-        details: "On: " + location.hostname, //The upper section of the presence text
-        state: "Being epic", //The lower section of the presence text
-        startTimestamp: Date.now() //The unix epoch timestamp for when to start counting from
-        // endTimestamp: 1577151472000 //If you want to show Time Left instead of Elapsed, this is the unix epoch timestamp at which the timer ends
-    }; /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
+        largeImageKey: "woman",
+        smallImageKey: "doggo",
+        smallImageText: "£2.50 lifetime emails",
+        details: "On: " + location.hostname,
+        state: "Being epic",
+        startTimestamp: Date.now()
+    };
 	
     if (presenceData.details == null) {
-        //This will fire if you do not set presence details
-        presence.setTrayTitle(); //Clears the tray title for mac users
-        presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
-    } else {
-        //This will fire if you set presence details
-        presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+        presence.setTrayTitle();
+        presence.setActivity();
+        presence.setActivity(presenceData);
     }
 });
