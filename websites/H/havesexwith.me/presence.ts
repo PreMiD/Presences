0a27a733fa588994f1050bb6cd10d602db6cf7fd
breixopd <32527555+breixopd@users.noreply.github.com>
2020-11-29 20:17:53 +0000

    Update presence.ts

--- websites/H/havesexwith.me/presence.ts
+++ websites/H/havesexwith.me/presence.ts
@@ -1,3 +1,6 @@
+var state = ["Being epic", "Just browsing", "Chilling", "You should check this out", "Now this, is epic"][Math.floor(Math.random() * 3)]
+var smolimage = ["You should cop an email", "Hello there!", "Smol doggo"][Math.floor(Math.random() * 3)]
+
 var presence = new Presence({
     clientId: "782635291105689631"
 }),
@@ -8,19 +11,20 @@ strings = presence.getStrings({
 });
 
 presence.on("UpdateData", async () => {
-
 	var presenceData: PresenceData = {
         largeImageKey: "woman",
         smallImageKey: "doggo",
-        smallImageText: "£2.50 lifetime emails",
+        smallImageText: smolimage,
         details: "On: " + location.hostname,
-        state: "Being epic",
+        state: state,
         startTimestamp: Date.now()
     };
+	console.log("Why")
 	
     if (presenceData.details == null) {
         presence.setTrayTitle();
         presence.setActivity();
+    } else {
         presence.setActivity(presenceData);
-    }
+	}
 });
