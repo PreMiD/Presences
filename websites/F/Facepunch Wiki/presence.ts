aaf7cfba48d593e55f0d65068618c0970e57f45b
MatStef132 <mateuszstefanek88@wp.pl>
2020-11-30 14:56:13 +0100

    Added facepunch wiki presence

---
+++ websites/F/Facepunch Wiki/presence.ts
@@ -0,0 +1,32 @@
+const presence = new Presence({
+  clientId: "782958551123361822"
+});
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "icon"
+  };
+
+  function setTimestamp(): number {
+    return Math.floor(Date.now() / 1000);
+  }
+
+  if (
+    document.URL === "https://wiki.facepunch.com/" ||
+    document.URL === "https://wiki.facepunch.com"
+  ) {
+    console.log(document.URL)
+    presenceData.details = await presence.getSetting("homepageMessage");
+    presenceData.startTimestamp = setTimestamp();
+  } else {
+    presenceData.details = await presence.getSetting("viewingMessage") + " " + document.title;
+    presenceData.startTimestamp = setTimestamp();
+  }
+
+  if (presenceData.details === null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
