b15ec8c464d3f8dc4930b6f8804d575b99c6f0cc
VyrekXD <vyrekxd@gmail.com>
2020-12-10 07:59:11 -0600

    Added JavaScript docs presence

---
+++ websites/J/JavaScript/presence.ts
@@ -0,0 +1,47 @@
+let presence = new Presence({
+    clientId: "786389332042711040"
+})
+
+var Stamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+    let data: PresenceData = {
+        largeImageKey: "jslogo",
+        startTimestamp: Stamp
+    }, paths: any = document.location.pathname.split('/')
+    paths.splice(0, 1)
+
+    if(paths[3] !== 'JavaScript')return;
+
+    if(paths[0] === 'es'){
+        if(!paths[4]){
+            data.details = `Viendo la pagina principal...`
+        } else {
+            data.details = `Viendo ${paths[4]}`
+            paths.splice(0, 1)
+            paths.splice(0, 1)
+            paths.splice(0, 1)
+            paths.splice(0, 1)
+            paths.splice(0, 1)
+            if(paths[0]){
+                data.state = `Tema: ${paths.join(', ')}`
+            }
+        }
+    } else {
+        if(!paths[4]){
+            data.details = `Looking the main page...`
+        } else {
+            data.details = `Looking at ${paths[4]}`
+            paths.splice(0, 1)
+            paths.splice(0, 1)
+            paths.splice(0, 1)
+            paths.splice(0, 1)
+            paths.splice(0, 1)
+            if(paths[0]){
+                data.state = `Topic: ${paths.join(', ')}`
+            }
+        }
+    }
+
+    presence.setActivity(data)
+})
