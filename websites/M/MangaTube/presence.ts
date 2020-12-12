6ce942b1800e70e068c991c2815a3819df32f30c
Emijor <55028218+GitEmir@users.noreply.github.com>
2020-12-12 16:51:46 +0100

    update

--- websites/M/MangaTube/presence.ts
+++ websites/M/MangaTube/presence.ts
@@ -1,33 +1,30 @@
-var presence = new Presence({
+let presence = new Presence({
     clientId: "770342692462526465"
 
 });
-var browsingStamp = Math.floor(Date.now() / 1000);
+let browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {    
     const data: PresenceData = {
         largeImageKey: "mangatube"
     };
 
-
-
     //Startseite
 
     if (document.location.pathname == "/") {
             data.details = "| Startseite";
 
-
     //Serien
 
         } else if (document.location.pathname.includes("/search/")) {
-            var search = document.querySelector("#in_title > span.text").textContent
-            data.details = "| Erweiterte Suche"
+            let search = document.querySelector("#in_title > span.text").textContent
+            data.details = "| Erweiterte Suche";
             data.state = search;
 
         } else if (document.location.pathname.includes("/read/")) {
-            var chapter = document.querySelector("body > div.blur-content > div > div.reader-navigation > div.container > div.pages-control > div.dropdown.chapter-dropdown > button").textContent
-            var manganame = document.querySelector("head > title").textContent.split("- Kapitel ")[0]
-            var seite = document.querySelector("body > div.blur-content > div > div.reader-navigation > div.container > div.pages-control > div.dropdown.page-dropdown > button > span.page-text").textContent
+            let chapter = document.querySelector("body > div.blur-content > div > div.reader-navigation > div.container > div.pages-control > div.dropdown.chapter-dropdown > button").textContent
+            let manganame = document.querySelector("head > title").textContent.split("- Kapitel ")[0]
+            let seite = document.querySelector("body > div.blur-content > div > div.reader-navigation > div.container > div.pages-control > div.dropdown.page-dropdown > button > span.page-text").textContent
             data.details = manganame;
             data.state = chapter + " | " + seite;
             data.startTimestamp = browsingStamp;
@@ -36,14 +33,14 @@ presence.on("UpdateData", async () => {
 
 
         } else if (document.location.pathname == "/series" || document.location.pathname == "/series/") {
-            var filter = document.querySelector("#series_list > div.panel-body > div.col-md-12 > div:nth-child(3) > a").textContent
+            let filter = document.querySelector("#series_list > div.panel-body > div.col-md-12 > div:nth-child(3) > a").textContent
             data.details = "| Serien";
             data.state = filter;
 
         } else if (document.location.pathname.startsWith("/series/")) {
-            var manga = document.querySelector("body > div.blur-content > div.wraper > div > div > div.content-container > div.row > div.col-md-8.series-detailed > h1").textContent;
-            var form = document.querySelector("body > div.blur-content > div.wraper > div > div > div.content-container > div.row > div.col-md-8.series-detailed > div.row > div.col-md-8.col-sm-8.col-offest-xs-2.col-xs-12 > ul > li:nth-child(5) > a").textContent.split("(japanische Comics)")[0]
-            data.details = "- " + form + " -";
+            let manga = document.querySelector("body > div.blur-content > div.wraper > div > div > div.content-container > div.row > div.col-md-8.series-detailed > h1").textContent;
+            let form = document.querySelector("body > div.blur-content > div.wraper > div > div > div.content-container > div.row > div.col-md-8.series-detailed > div.row > div.col-md-8.col-sm-8.col-offest-xs-2.col-xs-12 > ul > li:nth-child(5) > a").textContent.split("(japanische Comics)")[0]
+            data.details = "| " + form;
             data.state = manga;
 
     //Community
@@ -55,7 +52,7 @@ presence.on("UpdateData", async () => {
             data.details = "| Partner";
 
         } else if (document.location.pathname.startsWith("/gewinnspiel")) {
-            var giveaway = document.querySelector("head > title").textContent.split(" - Manga-Tube")[0]
+            let giveaway = document.querySelector("head > title").textContent.split(" - Manga-Tube")[0]
             data.details = "| Gewinnspiel";
             data.state = giveaway;
 
@@ -72,7 +69,7 @@ presence.on("UpdateData", async () => {
             data.details = "| Laufende Umfragen";
 
         } else {
-            var poll = document.querySelector("div.poll-question").textContent
+            let poll = document.querySelector("div.poll-question").textContent
             data.details = "| Umfrage"
             data.state = poll;
 
@@ -85,18 +82,15 @@ presence.on("UpdateData", async () => {
             data.details = "| Profil bearbeiten";
 
         } else if (document.location.pathname.startsWith("/profile/")) {
-            var profile = document.querySelector("#profile > div.profile-header.hide-md > div > div.profile-header-content > div.col-md-10.col-xs-8 > h4.profile-username.pull-left > b").textContent
-            var level = document.querySelector("#user_level").textContent
+            let profile = document.querySelector("#profile > div.profile-header.hide-md > div > div.profile-header-content > div.col-md-10.col-xs-8 > h4.profile-username.pull-left > b").textContent
+            let level = document.querySelector("#user_level").textContent
             data.details = "| Profil";
             data.state = profile + " | " + level;
 
         } else if (document.location.pathname.startsWith("/blog/")) {
-            var blog = document.querySelector("body > div.blur-content > div.wraper > div > div.content-container > div.row > div.col-md-12 > h3").textContent
+            let blog = document.querySelector("body > div.blur-content > div.wraper > div > div.content-container > div.row > div.col-md-12 > h3").textContent
             data.details = "| Blog";
             data.state = blog;
-
-        } else if (document.location.pathname.startsWith("/login")) {
-            data.details = "| Login"
 
     //Datenschutz & Impressum
 
