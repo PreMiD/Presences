f62df9dfe50f1df87f6103d0db816e30b8834dc6
Emijor <55028218+GitEmir@users.noreply.github.com>
2020-12-12 17:31:14 +0100

    update 2

--- websites/M/MangaTube/presence.ts
+++ websites/M/MangaTube/presence.ts
@@ -22,9 +22,9 @@ presence.on("UpdateData", async () => {
             data.state = search;
 
         } else if (document.location.pathname.includes("/read/")) {
-            const chapter = document.querySelector("body > div.blur-content > div > div.reader-navigation > div.container > div.pages-control > div.dropdown.chapter-dropdown > button").textContent;
-            const manganame = document.querySelector("head > title").textContent.split("- Kapitel ")[0];
-            const seite = document.querySelector("body > div.blur-content > div > div.reader-navigation > div.container > div.pages-control > div.dropdown.page-dropdown > button > span.page-text").textContent;
+            const chapter = document.querySelector("body > div.blur-content > div > div.reader-navigation > div.container > div.pages-control > div.dropdown.chapter-dropdown > button").textContent,
+            manganame = document.querySelector("head > title").textContent.split("- Kapitel ")[0],
+            seite = document.querySelector("body > div.blur-content > div > div.reader-navigation > div.container > div.pages-control > div.dropdown.page-dropdown > button > span.page-text").textContent;
             data.details = manganame;
             data.state = chapter + " | " + seite;
             data.startTimestamp = browsingStamp;
@@ -36,8 +36,8 @@ presence.on("UpdateData", async () => {
             data.state = filter;
 
         } else if (document.location.pathname.startsWith("/series/")) {
-            const manga = document.querySelector("body > div.blur-content > div.wraper > div > div > div.content-container > div.row > div.col-md-8.series-detailed > h1").textContent;
-            const form = document.querySelector("body > div.blur-content > div.wraper > div > div > div.content-container > div.row > div.col-md-8.series-detailed > div.row > div.col-md-8.col-sm-8.col-offest-xs-2.col-xs-12 > ul > li:nth-child(5) > a").textContent.split("(japanische Comics)")[0];
+            const manga = document.querySelector("body > div.blur-content > div.wraper > div > div > div.content-container > div.row > div.col-md-8.series-detailed > h1").textContent,
+            form = document.querySelector("body > div.blur-content > div.wraper > div > div > div.content-container > div.row > div.col-md-8.series-detailed > div.row > div.col-md-8.col-sm-8.col-offest-xs-2.col-xs-12 > ul > li:nth-child(5) > a").textContent.split("(japanische Comics)")[0];
             data.details = "| " + form;
             data.state = manga;
 
@@ -80,8 +80,8 @@ presence.on("UpdateData", async () => {
             data.details = "| Profil bearbeiten";
 
         } else if (document.location.pathname.startsWith("/profile/")) {
-            const profile = document.querySelector("#profile > div.profile-header.hide-md > div > div.profile-header-content > div.col-md-10.col-xs-8 > h4.profile-username.pull-left > b").textContent;
-            const level = document.querySelector("#user_level").textContent;
+            const profile = document.querySelector("#profile > div.profile-header.hide-md > div > div.profile-header-content > div.col-md-10.col-xs-8 > h4.profile-username.pull-left > b").textContent,
+            level = document.querySelector("#user_level").textContent;
             data.details = "| Profil";
             data.state = profile + " | " + level;
 
