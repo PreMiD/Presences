5e6a23d52f6176614c67f85b78e6b69cafab64a4
SeMiD <contact@premid.app>
2020-12-13 00:43:19 +0000

    ♻️ Refactoring
    Signed-off-by: SeMiD <contact@premid.app>

--- websites/P/pxls.space/presence.ts
+++ websites/P/pxls.space/presence.ts
@@ -1,15 +1,17 @@
 const startTimestamp = Math.floor(Date.now() / 1000),
-      presence = new Presence({
-        clientId: "785958064192749600"
-      });
+  presence = new Presence({
+    clientId: "785958064192749600"
+  });
 presence.on("UpdateData", async () => {
-    const data: PresenceData = {
-      largeImageKey: "logo",
-      startTimestamp
-    };
-    if (document.location.hostname == 'pxls.space'){
-        data.state = 'Canvas: ' + document.getElementById('current-pixel-count').innerHTML;
-        data.details = 'All time: ' + document.getElementById('alltime-pixel-count').innerHTML;
-        presence.setActivity(data);
-    }
+  const data: PresenceData = {
+    largeImageKey: "logo",
+    startTimestamp
+  };
+  if (document.location.hostname == "pxls.space") {
+    data.state =
+      "Canvas: " + document.getElementById("current-pixel-count").innerHTML;
+    data.details =
+      "All time: " + document.getElementById("alltime-pixel-count").innerHTML;
+    presence.setActivity(data);
+  }
 });
