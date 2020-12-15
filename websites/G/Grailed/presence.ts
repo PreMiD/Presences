4a3c7d0062a98a8f1e1d2e09dbd6ec8e914fa812
kznvii <pinkybinky524@gmail.com>
2020-12-15 11:22:42 -0800

    Update presence.ts

--- websites/G/Grailed/presence.ts
+++ websites/G/Grailed/presence.ts
@@ -2,8 +2,8 @@ const presence = new Presence({
     clientId: "786739998011293717"
 });
 
-var item: any,
-item2: any;
+let item: Element,
+item2: Element;
 
 const browsingStamp = Math.floor(Date.now() / 1000);
 
@@ -20,10 +20,10 @@ presence.on("UpdateData", async () => {
             item = document.querySelector("body > div.container > div > div.show-listing > div.listing-details-wrapper > div.-listing-details-and-likes-wrapper > div.-listing-designer-title-size > h1");
             
             presenceData.details = "Viewing a listing:";
-            if (item.innerText.length > 128) {
-                presenceData.state = item.innerText.substring(0, 125) + "...";
+            if (item.innerHTML.length > 128) {
+                presenceData.state = item.innerHTML.substring(0, 125) + "...";
             } else {
-                presenceData.state = item.innerText;
+                presenceData.state = item.innerHTML;
             } 
             delete presenceData.smallImageKey;
             presence.setActivity(presenceData);
@@ -49,10 +49,10 @@ presence.on("UpdateData", async () => {
             if (document.location.href.includes("www.grailed.com/collections/")) {
                 presenceData.details = "Browsing a Collection:";
                 item = document.querySelector("#CapsulePage > div.CapsuleHeader > div.-container._has-hero > div > div.-name-container > h1");
-                if (item.innerText.length > 128) {
-                    presenceData.state = item.innerText.substring(0, 125) + "...";
+                if (item.innerHTML.length > 128) {
+                    presenceData.state = item.innerHTML.substring(0, 125) + "...";
                 } else {
-                    presenceData.state = item.innerText;
+                    presenceData.state = item.innerHTML;
                 } 
             } else {
                 presenceData.details = "Browsing:";
@@ -70,18 +70,18 @@ presence.on("UpdateData", async () => {
 
                     item = document.querySelector("#__next > div > main > div:nth-child(2) > div.ProfileWrapper > div > div.DetailPageProfile-Info > div > h1");
                     
-                    if (item.innerText.length > 128) {
-                        presenceData.state = item.innerText.substring(0, 125) + "...";
+                    if (item.innerHTML.length > 128) {
+                        presenceData.state = item.innerHTML.substring(0, 125) + "...";
                     } else {
-                        presenceData.state = item.innerText;
+                        presenceData.state = item.innerHTML;
                     } 
                 } else {
                     item = document.querySelector("#designer-category > div.FiltersInstantSearch > div.feed-and-filters > div.right > h2");
                     presenceData.details = "Browsing:";
-                    if (item.innerText.length > 128) {
-                        presenceData.state = item.innerText.substring(29, 125) + "...";
+                    if (item.innerHTML.length > 128) {
+                        presenceData.state = item.innerHTML.substring(29, 125) + "...";
                     } else {
-                        presenceData.state = item.innerText.substring(29, item.length);
+                        presenceData.state = item.innerHTML.substring(29, item.textContent.length);
                     }
                 } 
             } else if (document.location.href.endsWith("www.grailed.com/designers")) {
@@ -96,10 +96,10 @@ presence.on("UpdateData", async () => {
             presenceData.details = "Browsing:";
 
             item = document.querySelector("#CategoryPage > div.-pageHeader > div > div.DetailPage--Header > div.-details > h1");
-            if (item.innerText.length > 128) {
-                presenceData.state = item.innerText.substring(0, 125) + "...";
+            if (item.innerHTML.length > 128) {
+                presenceData.state = item.innerHTML.substring(0, 125) + "...";
             } else {
-                presenceData.state = item.innerText;
+                presenceData.state = item.innerHTML;
             } 
             delete presenceData.smallImageKey;     
             presence.setActivity(presenceData);
@@ -109,10 +109,10 @@ presence.on("UpdateData", async () => {
             presenceData.details = "Reading:";
 
             item = document.querySelector("#blog > div.container.tagged-articles > div.filtered-articles-wrapper > h1");
-            if (item.innerText.length > 128) {
-                presenceData.state = item.innerText.substring(0, 125) + "...";
+            if (item.innerHTML.length > 128) {
+                presenceData.state = item.innerHTML.substring(0, 125) + "...";
             } else {
-                presenceData.state = item.innerText;
+                presenceData.state = item.innerHTML;
             } 
             delete presenceData.smallImageKey;     
             presence.setActivity(presenceData);
@@ -123,7 +123,7 @@ presence.on("UpdateData", async () => {
 
             item = document.querySelector("#ProductPage > div.ProductPageHeader > div.-info > h1.-product-name");
             item2 = document.querySelector("#ProductPage > div.ProductPageHeader > div.-info > h1.-designers-names");
-            if (item.innerText.length > 108) {
+            if (item.innerHTML.length > 108) {
                 presenceData.state = item2.textContent + ": " + item.textContent.substring(0, 105) + "...";
             } else {
                 presenceData.state = item2.textContent + ": " + item.textContent;
@@ -139,16 +139,16 @@ presence.on("UpdateData", async () => {
                 presenceData.details = "Reading:";
 
                 if (item != null) {
-                    if (item.innerText.length > 128) {
-                        presenceData.state = item.innerText.substring(0, 125) + "...";
+                    if (item.innerHTML.length > 128) {
+                        presenceData.state = item.innerHTML.substring(0, 125) + "...";
                     } else {
-                        presenceData.state = item.innerText;
+                        presenceData.state = item.innerHTML;
                     }
                 } else if (item2 != null) {
-                    if (item2.innerText.length > 128) {
-                        presenceData.state = item2.innerText.substring(0, 125) + "...";
+                    if (item2.innerHTML.length > 128) {
+                        presenceData.state = item2.innerHTML.substring(0, 125) + "...";
                     } else {
-                        presenceData.state = item2.innerText;
+                        presenceData.state = item2.innerHTML;
                     }
                 }
             } else {
@@ -168,17 +168,17 @@ presence.on("UpdateData", async () => {
                 
                 item = document.querySelector("#shop > div > div > div.feed-and-filters > div.right > h2");
 
-                if (item.innerText.includes("Available listings related to")) {
+                if (item.innerHTML.includes("Available listings related to")) {
 
                     presenceData.details = "Searching for:";
-                    presenceData.state = item.innerText.substring(29, 125);
+                    presenceData.state = item.innerHTML.substring(29, 125);
 
                 } else {
                     presenceData.details = "Browsing:"; 
                     presenceData.state = "The Feed";  
                 }
             }
-            delete presenceData.smallImageKey
+            delete presenceData.smallImageKey;
             presence.setActivity(presenceData);
             
         } else if (document.location.href.includes("grailed.com") && document.location.href.endsWith("grailed.com/")) {
@@ -193,10 +193,10 @@ presence.on("UpdateData", async () => {
             
             item = document.querySelector("#wardrobe > div > div.UserInfo > div.--header > div.--info > div.--user-container > div.--user-info > div.--username-container > span");
             presenceData.details = "Viewing a User:";
-            if (item.innerText.length > 128) {
-                presenceData.state = item.innerText.substring(0, 125) + "...";
+            if (item.innerHTML.length > 128) {
+                presenceData.state = item.innerHTML.substring(0, 125) + "...";
             } else {
-                presenceData.state = item.innerText;
+                presenceData.state = item.innerHTML;
             } 
         
             delete presenceData.smallImageKey;
