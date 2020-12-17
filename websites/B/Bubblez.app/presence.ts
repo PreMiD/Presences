2eaee8f4510141a2428cf2fed9943021636b60ba
Vexion <38048026+ProjectVexion@users.noreply.github.com>
2020-12-17 00:30:11 +0000

    Updated presence.ts
    Added login in status moved code from if's to switch

--- websites/B/Bubblez.app/presence.ts
+++ websites/B/Bubblez.app/presence.ts
@@ -3,90 +3,83 @@ const presence = new Presence({
 });
 
 
+function pathIncludes(string: string): boolean {
+  return document.location.pathname.toLowerCase().includes(string);
+}
+
+
 presence.on("UpdateData", async () => {
-    let state,
-	details,
-	smallImageText = "Bubblez.app | v1.0.0",
-	smallImageKey = "rebrand",
-	largeImageKey = "bubblez-logo",
-	dreading = "Reading...";
+	//const details = "..." ? "Logged in as " + document.querySelector("b.user-data").textContent.replace(/[\s\n]+/gi, "") : "...",
+    let detailsraw = "";
+	
+	if (document.querySelector("b.user-data").textContent.replace(/[\s\n]+/gi, "") == "Login") {
+		detailsraw = "Not logged in";
+	}
+	else {
+		detailsraw = "Logged in as " + document.querySelector("b.user-data").textContent.replace(/[\s\n]+/gi, "");
+	}
+	
+	const Data: PresenceData = {
+        largeImageKey: "bubblez-logo",
+        smallImageKey: "rebrand",
+        smallImageText: "Bubblez.app | v1.0.0",
+		details: detailsraw
+    };
 
     const path = window.location.pathname;
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	
-	state = "Reading some posts.";
-	details = "Browsing...";
-	
-	if (path.match("/home")) {
-		state = "Some posts.";
-		details = dreading;
+	switch (true) {
+		case pathIncludes("/home"):
+			Data.state = "Some posts.";
+		break;
+		case pathIncludes("/postedit"):
+			Data.state = "Editing a post.";
+		break;
+		case pathIncludes("/post"):
+			Data.state = "A post.";
+		break;
+		case pathIncludes("/p"):
+			let trimstring = urlParams.toString().substring(0, urlParams.toString().length - 1)
+			Data.state = trimstring + "'s profile.";
+		break;
+		case pathIncludes("/register"):
+			Data.details = "User Register.";
+		break;
+		case pathIncludes("/login"):
+			Data.details = "User Login.";
+		break;
+		case pathIncludes("/likedby"):
+			Data.state = "Likes.";
+		break;
+		case pathIncludes("/settings/"):
+			Data.state = "Settings " + path.split("/")[2] + ".";
+		break;
+		case pathIncludes("/following"):
+			Data.state = "Following list.";
+		break;
+		case pathIncludes("/followers"):
+			Data.state = "Followers list.";
+		break;
+		case pathIncludes("/leaderboard"):
+			Data.state = "Leaderboard.";
+		break;
+		case pathIncludes("/blogs"):
+			Data.state = "Devlogs.";
+		break;
+		case pathIncludes("/tos"):
+			Data.state = "TOS.";
+		break;
+		case pathIncludes("/adminpanel/"):
+			Data.state = "Browsing Adminpanel";
+		break;
+		case pathIncludes("/secret"):
+			Data.state = "Found a Secret";
+		break;
+		default:
+			Data.state = "Reading some posts.";
 	}
 	
-	if (path.match("/postedit")) {
-		state = "A post.";
-		details = "Editing..";
-	}
-	else if (path.match("/post")) {
-		state = "A post.";
-		details = dreading;
-	}
-	else if (path.match("/p")) {
-		let trimstring = urlParams.toString().substring(0, urlParams.toString().length - 1)
-		state = trimstring + "'s profile.";
-	}
-	
-	if (path.match("likedby")) {
-		state = "Likes.";
-		details = dreading;
-	}
-	
-	if (path.match("/settings/")) {
-		state = "Settings " + path.split("/")[2] + ".";
-	}
-	
-	if (path.match("/following")) {
-		state = "Following list.";
-		details = dreading;
-	}
-	
-	if (path.match("/followers")) {
-		state = "Followers list.";
-		details = dreading;
-	}
-	
-	if (path.match("/leaderboard")) {
-		state = "Leaderboard.";
-		details = dreading;
-	}
-	
-	if (path.match("/blogs")) {
-		state = "Devlogs.";
-		details = dreading;
-	}
-	
-	if (path.match("/tos")) {
-		state = "TOS.";
-		details = dreading;
-	}
-	
-	if (path.match("/adminpanel/")) {
-		state = "Admin";
-		details = "Browsing...";
-	}
-	
-	if (path.match("/secret")) {
-		state = "Secret";
-		details = "Found a";
-	}
-	
-	
-	const PresenceData: PresenceData = {
-        largeImageKey: largeImageKey,
-        smallImageKey: smallImageKey,
-        smallImageText: smallImageText,
-        details: details,
-		state: state
-    };
-	presence.setActivity(PresenceData);
+	presence.setActivity(Data);
 });
