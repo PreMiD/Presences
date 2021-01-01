7208627531940b53841bd91fe07977104e4fe7b4
Emir Kabal <kabalemir@gmail.com>
2021-01-01 23:24:49 +0300

    fix: codacy errors

--- websites/E/Exxen/presence.ts
+++ websites/E/Exxen/presence.ts
@@ -1,32 +1,30 @@
 const presence = new Presence({
-    clientId: "794408060847390760"
-});
-
-const strings = presence.getStrings({
-    playing: "presence.playback.playing",
-    paused: "presence.playback.paused",
-    browsing: "presence.activity.browsing",
-    episode: "presence.media.info.episode"
-});
-
-const startTimestamp = Math.floor(Date.now() / 1000);
+        clientId: "794408060847390760"
+    }),
+    strings = presence.getStrings({
+        playing: "presence.playback.playing",
+        paused: "presence.playback.paused",
+        browsing: "presence.activity.browsing",
+        episode: "presence.media.info.episode"
+    }),
+    startTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
     const data: PresenceData = {
-        largeImageKey: "exxen",
+        largeImageKey: "exxen"
     };
     if (document.location.pathname.indexOf('watch') > -1) {
-        const video: HTMLVideoElement = document.querySelector('.rmp-video');
-        const episodeName = document.querySelector('.content-name').textContent;
-        const episode = episodeName.split('.Bölüm')[0].split(' ')[episodeName.split('.Bölüm')[0].split(' ').length - 1];
+        const video: HTMLVideoElement = document.querySelector('.rmp-video'),
+            episodeName = document.querySelector('.content-name').textContent,
+            episode = episodeName.split('.Bölüm')[0].split(' ')[episodeName.split('.Bölüm')[0].split(' ').length - 1],
+            timestamps = presence.getTimestamps(video.currentTime, video.duration);
         data.details = episodeName.replace(`${episode}.Bölüm`, '').replace(`Episode ${episode}`, '');
         data.state = (await strings).episode.replace('{0}', episode);
-        const timestamps = presence.getTimestamps(video.currentTime, video.duration);
         if (!video.paused) {
             data.smallImageKey = "playing";
             data.smallImageText = (await strings).playing;
-            data.startTimestamp = timestamps[0]
-            data.endTimestamp = timestamps[1]
+            data.startTimestamp = timestamps[0];
+            data.endTimestamp = timestamps[1];
         } else {
             data.smallImageKey = "paused";
             data.smallImageText = (await strings).paused;
