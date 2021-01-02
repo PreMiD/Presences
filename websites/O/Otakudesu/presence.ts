d569031a5ebdc529ba1f4492fc6585e6f31f79b7
KurokuTetsuya <perumnas.good@gmail.com>
2021-01-02 22:44:08 +0700

    feat(Otakudesu): Add some enchancement

--- websites/O/Otakudesu/presence.ts
+++ websites/O/Otakudesu/presence.ts
@@ -1,7 +1,6 @@
 const presence = new Presence({
     clientId: "794916348761210920"
-}), 
-$ = document.querySelector,
+}),
 presenceData: PresenceData = {
     largeImageKey: "logo"
 };
@@ -24,59 +23,58 @@ interface iFrameData {
 // }
 
 presence.on("UpdateData", async () => {
-    const startTimestamp: number = Date.now();
-    presenceData.startTimestamp = startTimestamp;
-    switch (
-        document.location.pathname.endsWith("/") &&
-        document.location.pathname.length > 1
-        ? document.location.pathname.slice(
-            0,
-            document.location.pathname.length - 1
-            )
-        : document.location.pathname
-    ) {
-        case "/":
-            presenceData.details = "Viewing homepage";
-            break;
-        case "/anime-list":
-            presenceData.details = "Viewing anime list";
-            break;
-        case "/jadwal-rilis":
-            presenceData.details = "Viewing release schedule";
-            break;
-        case "/ongoing-anime":
-            presenceData.details = "Viewing ongoing anime list";
-            break;
-        case "/genre-list":
-            presenceData.details = "Viewing anime genre list";
-            break;
-        default: {
-            if (document.location.search.startsWith("?s")) {
-              const params = document.location.search.substring(1),
-              { s }: Record<"s"|"post_type", string> = JSON.parse('{"' + decodeURI(params).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+  const startTimestamp = Date.now();
+  presenceData.startTimestamp = startTimestamp;
+  switch (document.location.pathname.endsWith("/") &&
+      document.location.pathname.length > 1
+      ? document.location.pathname.slice(0, document.location.pathname.length - 1)
+      : document.location.pathname) {
+      case "/":
+          presenceData.details = "Viewing homepage";
+          break;
+      case "/anime-list":
+          presenceData.details = "Viewing anime list";
+          break;
+      case "/jadwal-rilis":
+          presenceData.details = "Viewing release schedule";
+          break;
+      case "/ongoing-anime":
+          presenceData.details = "Viewing ongoing anime list";
+          break;
+      case "/genre-list":
+          presenceData.details = "Viewing anime genre list";
+          break;
+      default: {
+          if (document.location.search.startsWith("?s")) {
+              const params = document.location.search.substring(1), { s } = JSON.parse('{"' + decodeURI(params).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
               presenceData.details = "Searching for:";
               presenceData.state = s;
               presenceData.smallImageKey = "search";
-            }
-            const mirrorStream: HTMLDivElement = $(".mirrorstream");
-            if (mirrorStream && video) {
+          }
+          if (document.location.pathname.startsWith("/anime")) {
+            presenceData.details = "Viewing anime";
+            presenceData.state = document.querySelector(".jdlrx > h1").textContent.replace(/Subtitle Indonesia/gi, "");
+          }
+          const mirrorStream = document.querySelector(".mirrorstream");
+          if (mirrorStream && video) {
               if (video.paused) {
-                presenceData.smallImageText = "Paused playback";
-                presenceData.smallImageKey = "pause";
-              } else {
-                presenceData.smallImageText = "Resumed playback";
-                presenceData.smallImageKey = "play";
+                  presenceData.smallImageText = "Paused playback";
+                  presenceData.smallImageKey = "pause";
               }
-              presenceData.details = `Watching ${$(".posttl").textContent.replace(/Subtitle Indonesia/gi, "")}`;
-              presenceData.state = $(".posttl").textContent;
-            } else {
-              presenceData.details = "Viewing anime:";
-              presenceData.state = $(".subheading > h2").textContent;
-            }
+              else {
+                  presenceData.smallImageText = "Resumed playback";
+                  presenceData.smallImageKey = "play";
+              }
+              presenceData.details = "Watching anime";
+              presenceData.state = document.querySelector(".posttl").textContent.replace(/Subtitle Indonesia/gi, "");
+          } else if (mirrorStream && !video) {
+            presenceData.details = "Watching anime";
+            presenceData.state = document.querySelector(".posttl").textContent.replace(/Subtitle Indonesia/gi, "");
+          }
           break;
-        }
-    }
-    presence.setActivity(presenceData);
+      }
+  }
+  presence.setActivity(presenceData);
 });
 
 presence.on("iFrameData", (data: iFrameData) => {
