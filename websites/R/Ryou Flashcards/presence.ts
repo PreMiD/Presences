0f3ce1d384762226c2ccf38e4f91baa6f8e23e30
AvikRao <2021arao@tjhsst.edu>
2021-01-02 01:16:00 -0500

    Clean up formatting

--- websites/R/Ryou Flashcards/presence.ts
+++ websites/R/Ryou Flashcards/presence.ts
@@ -1,40 +1,16 @@
 const presence = new Presence({
-    clientId: "794689032411283476" //The client ID of the Application created at https://discordapp.com/developers/applications
+    clientId: "794689032411283476"
     }),
 
-    startTimestamp = Math.floor(Date.now() / 1000),
-
-    strings = presence.getStrings({
-        play: "presence.playback.playing",
-        pause: "presence.playback.paused"
-        //You can use this to get translated strings in their browser language
-    });
-
-/*
-
-function myOutsideHeavyLiftingFunction(){
-    //Grab and process all your data here
-
-    // element grabs //
-    // api calls //
-    // variable sets //
-}
-
-setInterval(myOutsideHeavyLiftingFunction, 10000);
-//Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
-
-*/
+    startTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-    /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
-  
-      It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
 
     const presenceData: PresenceData = {
         largeImageKey:
-            "ryou" /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/,
+            "ryou",
         startTimestamp
-    }; /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
+    }; 
 
     const { pathname } = window.location;
     const title = document.querySelector("title").textContent;
@@ -136,13 +112,10 @@ presence.on("UpdateData", async () => {
         }
     }
 
-
     if (presenceData.details == null) {
-        //This will fire if you do not set presence details
-        presence.setTrayTitle(); //Clears the tray title for mac users
-        presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+        presence.setTrayTitle();
+        presence.setActivity();
     } else {
-        //This will fire if you set presence details
-        presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+        presence.setActivity(presenceData);
     }
 });
