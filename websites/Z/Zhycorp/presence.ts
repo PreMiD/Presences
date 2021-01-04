bc4d32036a23072ccd28cc7ea4f85802ca85111f
KurokuTetsuya <perumnas.good@gmail.com>
2021-01-04 19:45:49 +0700

    feat(websites): Add zhycorp.com presence

---
+++ websites/Z/Zhycorp/presence.ts
@@ -0,0 +1,44 @@
+const presence = new Presence({
+  clientId: "795629338401832990"
+}),
+presenceData: PresenceData = {
+  largeImageKey: "zhycorp"
+};
+
+presence.on("UpdateData", async () => {
+  presenceData.startTimestamp = Math.floor(Date.now() / 1000);
+  if (document.location.hostname.startsWith("bin")) {
+    presenceData.details = "Viewing zhycorp's hastebin";
+    presenceData.largeImageKey = "hastebin";
+  } else {
+    switch (document.location.pathname.endsWith("/") &&
+        document.location.pathname.length > 1
+        ? document.location.pathname.slice(0, document.location.pathname.length - 1)
+        : document.location.pathname) {
+        case "/":
+          presenceData.details = "Viewing homepage";
+          break;
+        case "/member":
+          presenceData.details = "Viewing supporter page";
+          break;
+        case "/staff":
+          presenceData.details = "Viewing staff list page";
+          break;
+        default: {
+          if (document.location.pathname.startsWith("/bots")) {
+            presenceData.details = "Viewing bot list";
+            presenceData.state = `Page ${document.querySelector(".white-text.text-center.mt-2").textContent.split("/").join("of")}`;
+          } else if (document.location.pathname.startsWith("/bot")) {
+            presenceData.details = "Viewing bot";
+            presenceData.state = document.querySelector("title").textContent.substring("Zhycorp -".length).trim();
+          }
+          break;
+        }
+    }
+  }
+  presence.setActivity(presenceData);
+});
+
+function toTitleCase(string: string) {
+  return string.split(" ").map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(" ");
+}
