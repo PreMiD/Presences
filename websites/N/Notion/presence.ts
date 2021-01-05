945605088709552ee53926d796dc118c51470a52
DecadeDecaf <decadedecaf@gmail.com>
2021-01-05 04:01:14 -0500

    Improved code quality

--- websites/N/Notion/presence.ts
+++ websites/N/Notion/presence.ts
@@ -7,18 +7,18 @@ const presence = new Presence({
   });
 
 presence.on("UpdateData", async () => {
-  const viewingString = (await strings).viewing;
-  //const editingString = (await strings).editing;
+  const viewingString = (await strings).viewing,
+  //editingString = (await strings).editing;
   
   //i'm not too sure how to check if the page is currently being viewed or edited, so it just says "viewing" for every page right now
   
-  const presenceData: PresenceData = {
-    largeImageKey: "icon",
+  presenceData: PresenceData = {
+    largeImageKey: "icon"
   };
   
   if (document.location.hostname.includes("notion.so")) {
     const pageName = document.title; //page title seems like a safe way to get the page name
-    presenceData.details = viewingString //this would need to be replaced with an if-else for "editing" to show up
+    presenceData.details = viewingString; //this would need to be replaced with an if-else for "editing" to show up
     presenceData.state = pageName;
   }
   
