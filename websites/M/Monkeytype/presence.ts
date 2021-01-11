1dc1b1ecf491b754b997e64602ebb51b52775846
Miodec <bartnikjack@gmail.com>
2021-01-11 21:54:42 +0000

    merged constants

--- websites/M/Monkeytype/presence.ts
+++ websites/M/Monkeytype/presence.ts
@@ -11,15 +11,14 @@ const presenceData: PresenceData = {
 presenceData.startTimestamp = time;
 if (path === "/") {
   presenceData.details = "Idling";
-  const resultPageVisible = !document.querySelector('.pageTest #result').classList.contains('hidden');
-  const testActive = document.querySelector('#words letter.correct') || document.querySelector('#words letter.incorrect');
-  const lbVisible = !document.querySelector('#leaderboardsWrapper').classList.contains('hidden');
+  const resultPageVisible = !document.querySelector('.pageTest #result').classList.contains('hidden'),
+  testActive = document.querySelector('#words letter.correct') || document.querySelector('#words letter.incorrect'),
+  lbVisible = !document.querySelector('#leaderboardsWrapper').classList.contains('hidden');
   if(resultPageVisible){
-    const testType = document.querySelector('.testType .bottom').innerHTML.replace(/<br>/g,' ');
-    presenceData.details = `Finished ${testType}`;
-    const statsElem = document.querySelector('.pageTest #result .stats');
-    const moreStatsElem = document.querySelector('.pageTest #result .morestats');
-    const stats = {
+    const testType = document.querySelector('.testType .bottom').innerHTML.replace(/<br>/g,' '),
+    statsElem = document.querySelector('.pageTest #result .stats'),
+    moreStatsElem = document.querySelector('.pageTest #result .morestats'),
+    stats = {
       wpm: statsElem.querySelector('.wpm .bottom').textContent,
       acc: statsElem.querySelector('.acc .bottom').textContent,
       raw: moreStatsElem.querySelector('.raw .bottom').textContent,
@@ -27,18 +26,16 @@ if (path === "/") {
       con: moreStatsElem.querySelector('.consistency .bottom').textContent,
       time: moreStatsElem.querySelector('.time .bottom').textContent
     }
+    presenceData.details = `Finished ${testType}`;
     presenceData.state = `${stats.wpm} wpm ${stats.acc} acc ${stats.raw} raw ${stats.char} ${stats.con} consistency ${stats.time}`;
   }else if(testActive){
-    const mode = document.querySelector('#top .config .mode .text-button.active').textContent.replace(/[\t\r\n]/g,'');
-    const testType = document.querySelector('.pageTest #premidTestMode').textContent;
-
-    presenceData.details = `Typing ${testType}`;
-
-    const wpm = document.querySelector('.pageTest #largeLiveWpmAndAcc #liveWpm').textContent;
-    const acc = document.querySelector('.pageTest #largeLiveWpmAndAcc #liveAcc').textContent;
+    const mode = document.querySelector('#top .config .mode .text-button.active').textContent.replace(/[\t\r\n]/g,''),
+    testType = document.querySelector('.pageTest #premidTestMode').textContent,
+    wpm = document.querySelector('.pageTest #largeLiveWpmAndAcc #liveWpm').textContent,
+    acc = document.querySelector('.pageTest #largeLiveWpmAndAcc #liveAcc').textContent;
     
+    presenceData.details = `Typing ${testType}`;
     presenceData.state = `${wpm} wpm ${acc} acc`;
-
 
     if(mode == "time"){
       presenceData.endTimestamp = Math.floor(Date.now()/1000) + (parseInt(document.querySelector('.pageTest #premidSecondsLeft').textContent) + 1);
