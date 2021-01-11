a3e7f99bf494400254303b6dfac15b01c5e1bf40
Miodec <bartnikjack@gmail.com>
2021-01-11 21:34:03 +0000

    reworked presence: - changed the details text - removed some state text - showing more presence when typing / viewing result / checking leaderboard - updated clientid

--- websites/M/Monkeytype/presence.ts
+++ websites/M/Monkeytype/presence.ts
@@ -1,37 +1,61 @@
 const presence = new Presence({
-    clientId: "790535819009064970"
-  }),
-  time = Math.floor(Date.now() / 1000);
+  clientId: "798272335035498557"
+}),
+time = Math.floor(Date.now() / 1000); 
 
 presence.on("UpdateData", () => {
-  const presenceData: PresenceData = {
-      largeImageKey: "monkeytype"
-    },
-    path = document.location.pathname.toLowerCase();
-  if (path === "/") {
-    presenceData.details = "Initial page";
-    presenceData.state = "Playing";
-    presenceData.startTimestamp = time;
-    presence.setActivity(presenceData);
-  } else if (path === "/about") {
-    presenceData.details = "About page";
-    presenceData.state = "Reading";
-    presenceData.startTimestamp = time;
-    presence.setActivity(presenceData);
-  } else if (path === "/settings") {
-    presenceData.details = "Settings page";
-    presenceData.state = "Changing settings";
-    presenceData.startTimestamp = time;
-    presence.setActivity(presenceData);
-  } else if (path === "/login") {
-    presenceData.details = "Login page";
-    presenceData.state = "Logging in";
-    presenceData.startTimestamp = time;
-    presence.setActivity(presenceData);
-  } else if (path === "/account") {
-    presenceData.details = "Account page";
-    presenceData.state = "Checking stats";
-    presenceData.startTimestamp = time;
-    presence.setActivity(presenceData);
+const presenceData: PresenceData = {
+    largeImageKey: "logo"
+  },
+  path = document.location.pathname.toLowerCase();
+presenceData.startTimestamp = time;
+if (path === "/") {
+  presenceData.details = "Idling";
+  const resultPageVisible = !document.querySelector('.pageTest #result').classList.contains('hidden');
+  const testActive = document.querySelector('#words letter.correct') || document.querySelector('#words letter.incorrect');
+  const lbVisible = !document.querySelector('#leaderboardsWrapper').classList.contains('hidden');
+  if(resultPageVisible){
+    const testType = document.querySelector('.testType .bottom').innerHTML.replace(/<br>/g,' ');
+    presenceData.details = `Finished ${testType}`;
+    const statsElem = document.querySelector('.pageTest #result .stats');
+    const moreStatsElem = document.querySelector('.pageTest #result .morestats');
+    const stats = {
+      wpm: statsElem.querySelector('.wpm .bottom').textContent,
+      acc: statsElem.querySelector('.acc .bottom').textContent,
+      raw: moreStatsElem.querySelector('.raw .bottom').textContent,
+      char: moreStatsElem.querySelector('.key .bottom').textContent,
+      con: moreStatsElem.querySelector('.consistency .bottom').textContent,
+      time: moreStatsElem.querySelector('.time .bottom').textContent
+    }
+    presenceData.state = `${stats.wpm} wpm ${stats.acc} acc ${stats.raw} raw ${stats.char} ${stats.con} consistency ${stats.time}`;
+  }else if(testActive){
+    const mode = document.querySelector('#top .config .mode .text-button.active').textContent.replace(/[\t\r\n]/g,'');
+    const testType = document.querySelector('.pageTest #premidTestMode').textContent;
+
+    presenceData.details = `Typing ${testType}`;
+
+    const wpm = document.querySelector('.pageTest #largeLiveWpmAndAcc #liveWpm').textContent;
+    const acc = document.querySelector('.pageTest #largeLiveWpmAndAcc #liveAcc').textContent;
+    
+    presenceData.state = `${wpm} wpm ${acc} acc`;
+
+
+    if(mode == "time"){
+      presenceData.endTimestamp = Math.floor(Date.now()/1000) + (parseInt(document.querySelector('.pageTest #premidSecondsLeft').textContent) + 1);
+    }
+
+  }else if(lbVisible){
+    presenceData.details = 'Checking leaderboards';
   }
+} else if (path === "/about") {
+  presenceData.details = "Reading about page";
+} else if (path === "/settings") {
+  presenceData.details = "Changing settings";
+} else if (path === "/login") {
+  presenceData.details = "Logging in";
+} else if (path === "/account") {
+  presenceData.details = "Checking stats";
+}
+presence.setActivity(presenceData);
 });
+
