8b98c8a11773fb87f92e766d4388a0b47496f4d7
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-01-12 10:12:24 +0700

    Update presence.ts

--- websites/T/Tokopedia/presence.ts
+++ websites/T/Tokopedia/presence.ts
@@ -1,18 +1,26 @@
 const presence = new Presence({
     clientId: "798368817318330400"
-  }),
-  strings = presence.getStrings({
-    search: "presence.playback.search",
-  });
+}),
+    strings = presence.getStrings({
+        search: "presence.playback.search",
+    });
 
+var elapsed = Math.floor(Date.now() / 1000);
 presence.on("UpdateData", async () => {
-  const presenceData: PresenceData = {
-    largeImageKey: "tokopedia"
-  };
- 
-  
-  var path = document.location.pathname;
-  if(path.includes("/p")){
-     presenceData.details="Viewing Product List....";
-     }
+    const presenceData: PresenceData = {
+        largeImageKey: "tokopedia"
+    };
+
+    if (document.location.hostname == "www.tokopedia.com") {
+        var path = document.location.pathname;
+        if (path.includes("/p")) {
+            presenceData.details = "Viewing Product List....";
+            presenceData.startTimestamp = elapsed;
+            presence.setActivity(presenceData)
+        } else {
+            presenceData.details = "Viewing a Homepage";
+            presenceData.startTimestamp = elapsed;
+            presence.setActivity(presenceData)
+        }
+    }
 });
