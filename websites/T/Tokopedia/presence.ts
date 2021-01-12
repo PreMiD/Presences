700940985952ac3af53f7506d5f75ced036516d4
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-01-12 09:10:53 +0700

    demo

--- websites/T/Tokopedia/presence.ts
+++ websites/T/Tokopedia/presence.ts
@@ -1,1 +1,19 @@
+const presence = new Presence({
+    clientId: "798368817318330400" //The client ID of the Application created at https://discordapp.com/developers/applications
+  }),
+  strings = presence.getStrings({
+    search: "presence.playback.search",
+    //You can use this to get translated strings in their browser language
+  });
 
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "tokopedia"
+  };
+ 
+  
+  var path = document.location.pathname;
+  if(path.includes("/p"){
+     presenceData.details="Viewing Product List....";
+     }
+});
