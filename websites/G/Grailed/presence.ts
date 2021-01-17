8e185b46cc5067445ed73544bdfd27d822738e4b
kznvii <pinkybinky524@gmail.com>
2021-01-17 01:07:19 -0800

    grailed update

--- websites/G/Grailed/presence.ts
+++ websites/G/Grailed/presence.ts
@@ -190,9 +190,10 @@ presence.on("UpdateData", async () => {
         );
         if (item.innerHTML.includes("Available listings related to")) {
           presenceData.details = "Searching for:";
-          presenceData.state = item.innerHTML
-            .replace(/amp;/g, "")
-            .substring(29, 125);
+          presenceData.state = item.innerHTML.replace(/amp;/g, '').substring(29, 125);
+        } else if (item.innerHTML.endsWith("Listings")) {
+          presenceData.details = "Searching for:";
+          presenceData.state = item.innerHTML.replace(/amp;|Listings/g, '');
         } else {
           presenceData.details = "Browsing:";
           presenceData.state = "The Feed";
