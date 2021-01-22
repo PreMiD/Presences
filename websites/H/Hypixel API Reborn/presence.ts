8ec218d04b99bd60e2bc0823e2c776c8f7a9bd38
SeMiD <contact@premid.app>
2021-01-22 01:19:00 +0000

    ♻️ Refactoring
    Signed-off-by: SeMiD <contact@premid.app>

--- websites/H/Hypixel API Reborn/presence.ts
+++ websites/H/Hypixel API Reborn/presence.ts
@@ -4,24 +4,52 @@ const presence = new Presence({
 
 presence.on("UpdateData", () => {
   const route = document.location.hash.split("/"),
-  data: PresenceData = {
-    largeImageKey: "logo"
-  };
+    data: PresenceData = {
+      largeImageKey: "logo"
+    };
 
   if (route.length === 1 || route[1] === "") {
     data.details = "Browsing the main page...";
   } else if (route[1] === "docs") {
     data.smallImageKey = route[2];
-    data.smallImageText = `${route[2].replace(/^[a-z]/i, (c) => c.toUpperCase())} - ${Number(route[3][0]) ? `v${route[3]}` : `${route[3]}`}`;
+    data.smallImageText = `${route[2].replace(/^[a-z]/i, (c) =>
+      c.toUpperCase()
+    )} - ${Number(route[3][0]) ? `v${route[3]}` : `${route[3]}`}`;
     if (route[4].startsWith("search?q=")) {
       data.details = "Searching for:";
       data.state = route[4].slice(9);
     } else if (route[4] === "general") {
       data.details = "Viewing:";
-      data.state = route[5] === "faq" ? route[5].toUpperCase() : `${route[5].replace(/^[a-z]/i, (c) => c.toUpperCase())}`;
+      data.state =
+        route[5] === "faq"
+          ? route[5].toUpperCase()
+          : `${route[5].replace(/^[a-z]/i, (c) => c.toUpperCase())}`;
     } else {
-      data.details = `Looking for a${(route[5].split("?scrollTo=")[1] && route[5].split("?scrollTo=")[1].startsWith("e-")) || route[4] === "examples" ? "n" : ""} ${route[5].split("?scrollTo=")[1] && route[5].split("?scrollTo=")[1].startsWith("e-") ? "event" : `${route[4].endsWith("s") && route[4] !== "class" ? route[4].substring(0, route[4].length - 1) : route[4].replace(/^[a-z]/i, (c) => c.toUpperCase())}`}`;
-      data.state = `${route[5].split("?scrollTo=")[0]}${route[5].split("?scrollTo=")[1] ? `#${route[5].split("?scrollTo=")[1][1] === "-" ? route[5].split("?scrollTo=")[1].slice(2) : route[5].split("?scrollTo=")[1]}` : ""}`;
+      data.details = `Looking for a${
+        (route[5].split("?scrollTo=")[1] &&
+          route[5].split("?scrollTo=")[1].startsWith("e-")) ||
+        route[4] === "examples"
+          ? "n"
+          : ""
+      } ${
+        route[5].split("?scrollTo=")[1] &&
+        route[5].split("?scrollTo=")[1].startsWith("e-")
+          ? "event"
+          : `${
+              route[4].endsWith("s") && route[4] !== "class"
+                ? route[4].substring(0, route[4].length - 1)
+                : route[4].replace(/^[a-z]/i, (c) => c.toUpperCase())
+            }`
+      }`;
+      data.state = `${route[5].split("?scrollTo=")[0]}${
+        route[5].split("?scrollTo=")[1]
+          ? `#${
+              route[5].split("?scrollTo=")[1][1] === "-"
+                ? route[5].split("?scrollTo=")[1].slice(2)
+                : route[5].split("?scrollTo=")[1]
+            }`
+          : ""
+      }`;
     }
   }
 
