3cda882f2fee7faa16bea509e67a9848f6c31997
Iván Caldera <magumboss1@gmail.com>
2021-01-22 20:30:43 -0600

    Update presence.ts

--- websites/S/Salty Bet/presence.ts
+++ websites/S/Salty Bet/presence.ts
@@ -1,13 +1,18 @@
 const presence = new Presence({
-    clientId: "802246778010730548" //The client ID of the Application created at https://discordapp.com/developers/applications
-  });
+  clientId: "802246778010730548" //The client ID of the Application created at https://discordapp.com/developers/applications
+});
 
 const browsingStamp = Math.floor(Date.now() / 1000);
 
 var estatus: any;
 var red: any;
 var blue: any;
-var bet: any;
+var betBlue: any;
+var betRed: any;
+var betView: any;
+var oddsBlue: any;
+var oddsRed: any;
+var prize: any;
 
 presence.on("UpdateData", () => {
   const presenceData: PresenceData = {
@@ -26,14 +31,37 @@ presence.on("UpdateData", () => {
   estatus = document.querySelector(
     "#betstatus"
   );
-  bet = document.querySelector(
+  betView = document.querySelector(
     "span#lastbet.dynamic-view"
+  );
+  betRed = document.querySelector(
+    "span#lastbet.dynamic-view > span.redtext"
+  );
+  betBlue = document.querySelector(
+    "span#lastbet.dynamic-view > span.bluetext"
+  );
+  prize = document.querySelector(
+    "span#lastbet.dynamic-view > span.greentext"
+  );
+  oddsRed = document.querySelector(
+    "span#lastbet.dynamic-view > span.redtext:nth-last-child(2)"
+  );
+  oddsBlue = document.querySelector(
+    "span#lastbet.dynamic-view > span.bluetext:nth-last-child(1)"
   );
 
   presenceData.details = red.innerText + " vs " + blue.innerText;
 
-  if(estatus.innerText.includes('locked'))
-    presenceData.state = bet.innerText;
+  if(betView.innerText.length > 1) {
+    if((betRed.innerText+betBlue.innerText).includes("$")) {
+      if (betRed.innerText.includes('$'))
+        presenceData.state = betRed.innerText + "(Red) → " + prize.innerText + " | " + oddsRed.innerText + ":" + oddsBlue.innerText;
+      else
+        presenceData.state = betBlue.innerText + "(Blue) → " + prize.innerText + " | " + oddsRed.innerText + ":" + oddsBlue.innerText;
+    }
+    else
+      presenceData.state = "O: " + oddsRed.innerText + ":" + oddsBlue.innerText;
+  }
   else
     presenceData.state = estatus.innerText;
 
