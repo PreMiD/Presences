9388c0b5a5a9a12eb28f5069a4eb20d092d3a338
Iván Caldera <magumboss1@gmail.com>
2021-01-22 22:43:05 -0600

    Update presence.ts

--- websites/S/Salty Bet/presence.ts
+++ websites/S/Salty Bet/presence.ts
@@ -1,70 +1,55 @@
 const presence = new Presence({
   clientId: "802246778010730548" //The client ID of the Application created at https://discordapp.com/developers/applications
-});
+}),
+  URLMap: { [index: string]: string } = {
+    Red: 'getText("div#sbettors1 > span.redtext > strong")',
+    Blue: 'getText("div#sbettors2 > span.bluetext > strong")',
+    estatus: 'getText("#betstatus")',
+    betview: 'getText("span#lastbet.dynamic-view")',
+    betRed: 'getText("span#lastbet.dynamic-view > span.redtext")',
+    betBlue: 'getText("span#lastbet.dynamic-view > span.bluetext")',
+    prize: 'getText("span#lastbet.dynamic-view > span.greentext")',
+    oddsRed: 'getText("span#lastbet.dynamic-view > span.redtext:nth-last-child(2)")',
+    oddsBlue: 'getText("span#lastbet.dynamic-view > span.bluetext:nth-last-child(1)")'
 
-const browsingStamp = Math.floor(Date.now() / 1000);
+  }, browsingStamp = Math.floor(Date.now() / 1000);
 
-var estatus: any;
-var red: any;
-var blue: any;
-var betBlue: any;
-var betRed: any;
-var betView: any;
-var oddsBlue: any;
-var oddsRed: any;
-var prize: any;
+function getText(selector: string) {
+  return document.querySelector(selector).textContent;
+}
 
 presence.on("UpdateData", () => {
   const presenceData: PresenceData = {
     largeImageKey: "salty",
     smallImageKey: "salero"
-  };
+  },
+    red = eval(URLMap['Red']),
+    blue = eval(URLMap['Blue']),
+    estatus = eval(URLMap['estatus']),
+    betView = eval(URLMap['betview']),
+    betRed = eval(URLMap['betRed']),
+    betBlue = eval(URLMap['betBlue']),
+    prize = eval(URLMap['prize']),
+    oddsBlue = eval(URLMap['oddsBlue']),
+    oddsRed = eval(URLMap['oddsRed']);
 
   presenceData.startTimestamp = browsingStamp;
 
-  red = document.querySelector(
-    "div#sbettors1 > span.redtext > strong"
-  );
-  blue = document.querySelector(
-    "div#sbettors2 > span.bluetext > strong"
-  );
-  estatus = document.querySelector(
-    "#betstatus"
-  );
-  betView = document.querySelector(
-    "span#lastbet.dynamic-view"
-  );
-  betRed = document.querySelector(
-    "span#lastbet.dynamic-view > span.redtext"
-  );
-  betBlue = document.querySelector(
-    "span#lastbet.dynamic-view > span.bluetext"
-  );
-  prize = document.querySelector(
-    "span#lastbet.dynamic-view > span.greentext"
-  );
-  oddsRed = document.querySelector(
-    "span#lastbet.dynamic-view > span.redtext:nth-last-child(2)"
-  );
-  oddsBlue = document.querySelector(
-    "span#lastbet.dynamic-view > span.bluetext:nth-last-child(1)"
-  );
+  presenceData.details = red + " vs " + blue;
 
-  presenceData.details = red.innerText + " vs " + blue.innerText;
-
-  if (betView.innerText.length > 1) {
-    if(!estatus.innerText.includes("Payouts")) {
-      if ((betRed.innerText + betBlue.innerText).includes("$")) {
-        if (betRed.innerText.includes('$'))
-          presenceData.state = betRed.innerText + "(Red) → " + prize.innerText + " | " + oddsRed.innerText + ":" + oddsBlue.innerText;
+  if (betView.length > 1) {
+    if(!estatus.includes("Payouts")) {
+      if ((betRed + betBlue).includes("$")) {
+        if (betRed.includes('$'))
+          presenceData.state = betRed + "(Red) → " + prize + " | " + oddsRed + ":" + oddsBlue;
         else
-          presenceData.state = betBlue.innerText + "(Blue) → " + prize.innerText + " | " + oddsRed.innerText + ":" + oddsBlue.innerText;
+          presenceData.state = betBlue + "(Blue) → " + prize + " | " + oddsRed + ":" + oddsBlue;
       } else
-        presenceData.state = "Odds: " + oddsRed.innerText + ":" + oddsBlue.innerText;
+        presenceData.state = "Odds: " + oddsRed + ":" + oddsBlue;
     } else
-      presenceData.state = estatus.innerText;
+      presenceData.state = estatus;
   } else
-    presenceData.state = estatus.innerText;
+    presenceData.state = estatus;
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
