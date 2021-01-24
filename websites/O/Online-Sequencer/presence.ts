c9fd8b5b72ea96aa32c394a7acb70b92d38ba2e9
Yam Jun Jie <yjjcoolcool@gmail.com>
2021-01-24 11:20:07 +0800

    Removed comments and minor bug fix for song title

--- websites/O/Online-Sequencer/presence.ts
+++ websites/O/Online-Sequencer/presence.ts
@@ -1,26 +1,6 @@
 const presence = new Presence({
-    clientId: "802379096122196050" //The client ID of the Application created at https://discordapp.com/developers/applications
+    clientId: "802379096122196050"
 });
-  /*strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-    //You can use this to get translated strings in their browser language
-  });*/
-
-/*
-
-function myOutsideHeavyLiftingFunction(){
-    //Grab and process all your data here
-
-    // element grabs //
-    // api calls //
-    // variable sets //
-}
-
-setInterval(myOutsideHeavyLiftingFunction, 10000);
-//Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
-
-*/
 let timestart = Math.round((new Date()).getTime() / 1000);
 
 function refreshTime(){
@@ -28,9 +8,6 @@ function refreshTime(){
 }
 
 presence.on("UpdateData", async () => {
-  /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
-
-    It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
     let upperText="", lowerText="", refreshtime=true;
     if (document.getElementsByClassName("fas fa-stop")[0]!=undefined){
       upperText="Listening to a sequence";
@@ -80,7 +57,11 @@ presence.on("UpdateData", async () => {
         upperText="Editing a sequence";
       }
       const str = (<HTMLElement>document.getElementsByClassName("text")[1]).innerHTML.trim();
-      lowerText="Title: "+str.substring(0, str.indexOf('by <a'));
+      if (str.includes("by <a")){
+        lowerText="Title: "+str.substring(0, str.indexOf('by <a'));
+      } else {
+        lowerText="Title: "+str;
+      }
     }
 
     if (document.getElementById("chatbox")!=null){
@@ -95,11 +76,10 @@ presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey:"online_sequencer_icon",
     smallImageKey:"",
-    smallImageText: "", //The text which is displayed when hovering over the small image
-    details: upperText, //The upper section of the presence text
-    state: lowerText, //The lower section of the presence text
-    startTimestamp: timestart //The unix epoch timestamp for when to start counting from
-    //endTimestamp: 0 //If you want to show Time Left instead of Elapsed, this is the unix epoch timestamp at which the timer ends
+    smallImageText: "",
+    details: upperText,
+    state: lowerText,
+    startTimestamp: timestart
   };
 
   if (presenceData.details == null) {
