b14f08743df2d550f79fc76542dd1983af67ec62
NinjaLabs-Dev <fraser@ninjalabs.co.uk>
2021-01-26 12:28:00 +0000

    Merging consts

--- websites/T/TruckersFM/presence.ts
+++ websites/T/TruckersFM/presence.ts
@@ -3,11 +3,8 @@ const presence = new Presence({
 });
 
 presence.on("UpdateData", () => {
-  const title = `${document.querySelector(".player-artist-text").textContent} - ${document.querySelector(".player-title-text").textContent}`;
-  const presenter = document.querySelector(".live-time") 
-		? `${document.querySelector(".live-name").textContent} till ${document.querySelector(".live-time").textContent.slice(6)}` 
-		: `${document.querySelector(".live-name").textContent}`;
-		
+  const title, presenter  = `${document.querySelector(".player-artist-text").textContent} - ${document.querySelector(".player-title-text").textContent}`,
+							document.querySelector(".live-time") ? `${document.querySelector(".live-name").textContent} till ${document.querySelector(".live-time").textContent.slice(6)}` : `${document.querySelector(".live-name").textContent}`;;		
   const presenceData: PresenceData = {
     largeImageKey: "tfmlogo"
   };
