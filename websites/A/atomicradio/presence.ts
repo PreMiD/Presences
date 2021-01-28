9221f9fb2daaa44d8830d0dd48f7cac5b50946ba
VocalZero <kacper.mura@web.de>
2021-01-28 16:37:24 +0100

    Add atomicradio presence 📜

---
+++ websites/A/atomicradio/presence.ts
@@ -0,0 +1,95 @@
+const presence = new Presence({
+    clientId: "777954584013963265"
+});
+
+let channelInfo = {name: "", artist: "", title: "", end: 0};
+let browsingTimestamp = Date.now();
+var presenceData: PresenceData = {
+    largeImageKey: "atr-logo",
+    smallImageKey: "play-button"
+};
+
+async function getStationData(channel: string) {
+    if(channelInfo.name != channel) {
+        channelInfo.name = channel;
+        var data = await window.fetch("https://api.atomicradio.eu/channels/" + channelInfo.name).then((res) => res.json());
+        presenceData.state = data.song.artist;
+        presenceData.details = data.song.title;
+        presenceData.startTimestamp = data.song.start_at;
+        presenceData.endTimestamp = data.song.end_at;
+        presenceData.smallImageText = `ATR.${channelInfo.name} • ${data.listeners} listeners`;
+        presenceData.smallImageKey = "play-button";
+        presence.setActivity(presenceData, true);
+        return;
+    }
+
+    if (Date.now() >= presenceData.endTimestamp * 1000) {
+        var data = await window.fetch("https://api.atomicradio.eu/channels/" + channelInfo.name).then((res) => res.json());
+        presenceData.state = data.song.artist;
+        presenceData.details = data.song.title;
+        presenceData.startTimestamp = data.song.start_at;
+        presenceData.endTimestamp = data.song.end_at;
+        presenceData.smallImageText = `ATR.${channelInfo.name} • ${data.listeners} listeners`;
+        presenceData.smallImageKey = "play-button";
+        presence.setActivity(presenceData, true);
+    }
+}
+
+function clearPresenceData() {
+    channelInfo = {name: '', artist: '', title: '', end: 0};
+    delete presenceData.smallImageKey;
+    delete presenceData.state;
+    delete presenceData.details;
+    delete presenceData.startTimestamp;
+    delete presenceData.endTimestamp;
+    delete presenceData.smallImageText;
+}
+
+presence.on('UpdateData', async () => {
+    let playerOpen = false;
+    let playBar = document.getElementById("PlayBar");
+    if(playBar.style.display == "block") {
+        playerOpen = true;
+        let playerButtonState = document.getElementById("Player_Play_Button_State");
+        if(playerButtonState.className.includes('fa-play')) {
+            playerOpen = false;
+        }
+    }
+
+    if(playerOpen) {
+        let channelName = document.getElementById("Player_Station_Name");
+        let channel = String(channelName.innerText).split(".")[1];
+        getStationData(channel);
+    } else {
+        clearPresenceData();
+        if(window.location.pathname == "/" || window.location.pathname.startsWith('/home')) {
+            presenceData.details = "Browsing...";
+            delete presenceData.startTimestamp;
+            presence.setActivity(presenceData, true);
+        } else if(window.location.pathname.startsWith("/partner")) {
+            presenceData.details = "Viewing the partner information";
+            presenceData.startTimestamp = browsingTimestamp;
+            presence.setActivity(presenceData, true);
+        } else if(window.location.pathname.startsWith("/history")) {
+            presenceData.details = "Viewing the song history";
+            presenceData.startTimestamp = browsingTimestamp;
+            presence.setActivity(presenceData, true);
+        } else if(window.location.pathname.startsWith("/streams")) {
+            presenceData.details = "Viewing the streamurls";
+            presenceData.startTimestamp = browsingTimestamp;
+            presence.setActivity(presenceData, true);
+        } else if(window.location.pathname.startsWith("/imprint")) {
+            presenceData.details = "Viewing the imprint";
+            presenceData.startTimestamp = browsingTimestamp;
+            presence.setActivity(presenceData, true);
+        } else if(window.location.pathname.startsWith("/privacy")) {
+            presenceData.details = "Viewing the privacy";
+            presenceData.startTimestamp = browsingTimestamp;
+            presence.setActivity(presenceData, true);
+        } else {
+            presenceData.details = "Browsing...";
+            delete presenceData.startTimestamp;
+            presence.setActivity(presenceData, true);
+        }
+    }
+});
