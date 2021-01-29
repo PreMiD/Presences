2d83c92a4ee1c95f672c1154606991ed75ed2c6d
Iván Caldera <magumboss1@gmail.com>
2021-01-28 20:55:13 -0600

    bugfixes

--- websites/S/Salty Bet/presence.ts
+++ websites/S/Salty Bet/presence.ts
@@ -9,10 +9,13 @@ const presence = new Presence({ clientId: "802246778010730548" }),
     betBlue: 'span#lastbet.dynamic-view > span.bluetext',
     prize: 'span#lastbet.dynamic-view > span.greentext',
     oddsRed: 'span#lastbet.dynamic-view > span.redtext:nth-last-child(2)',
-    oddsBlue: 'span#lastbet.dynamic-view > span.bluetext:nth-last-child(1)'
-  }, browsingStamp = Math.floor(Date.now() / 1000);
+    oddsBlue: 'span#lastbet.dynamic-view > span.bluetext:nth-last-child(1)',
+    betsView: 'span#lastbet.dynamic-view'
+  };
 
-let blueCheck: string, redCheck: string, host: string;
+let browsingStamp = Math.floor(Date.now() / 1000),
+  blueCheck: string,
+  redCheck: string;
 
 function getText(selector: string) {
   if (document.querySelector(selector) !== null)
@@ -26,21 +29,18 @@ presence.on("UpdateData", async () => {
     largeImageKey: "salty"
   };
 
-  host = document.location.hostname;
-
-  if (host === "www.saltybet.com") {
+  if (document.location.hostname === "www.saltybet.com") {
     if (document.location.pathname == "/" || document.location.pathname ==  "/index") {
 
       const tmode: string = getText(SelectorMap['tmode']),
         emode: string = getText(SelectorMap['emode']);
-      presenceData.startTimestamp = browsingStamp;
 
-      if (redCheck == getText(SelectorMap['Red']) || blueCheck == getText(SelectorMap['Blue'])) {
+      if (getText(SelectorMap['Red']) == null || getText(SelectorMap['Blue']) == null || redCheck == null || blueCheck == null ) {
+        presenceData.details = "Loading Fighters...";
+      } else if (redCheck == getText(SelectorMap['Red']) || blueCheck == getText(SelectorMap['Blue']) ){
         blueCheck = getText(SelectorMap['Blue']) + "‎";
         redCheck = getText(SelectorMap['Red']) + "‎";
         presenceData.details = redCheck + " VS " + blueCheck;
-      } else if (getText(SelectorMap['Red']) == null || getText(SelectorMap['Blue']) == null){
-        presenceData.details = "Loading Fighters...";
       } else
         presenceData.details = redCheck + " VS " + blueCheck;
 
@@ -56,14 +56,20 @@ presence.on("UpdateData", async () => {
       }
 
       if (!getText(SelectorMap['estatus']).includes("OPEN!")) {
+        browsingStamp = Math.floor(Date.now() / 1000);
         if (!getText(SelectorMap['estatus']).includes("Payouts")) {
-          if ((getText(SelectorMap['betRed']) + getText(SelectorMap['betBlue'])).includes("$")) {
+          if ((getText(SelectorMap['betsView'])).includes("|")) {
+            presenceData.startTimestamp = browsingStamp;
             if (getText(SelectorMap['betRed']).includes('$'))
               presenceData.state = getText(SelectorMap['betRed']) + "(Red) → " + getText(SelectorMap['prize']) + " | " + getText(SelectorMap['oddsRed']) + ":" + getText(SelectorMap['oddsBlue']);
             else
               presenceData.state = getText(SelectorMap['betBlue']) + "(Blue) → " + getText(SelectorMap['prize']) + " | " + getText(SelectorMap['oddsRed']) + ":" + getText(SelectorMap['oddsBlue']);
-          } else
-            presenceData.state = "Odds: " + getText(SelectorMap['oddsRed']) + ":" + getText(SelectorMap['oddsBlue']);
+          } else {
+            if(getText(SelectorMap['oddsRed']) !== null)
+              presenceData.state = "Odds: " + getText(SelectorMap['oddsRed']) + ":" + getText(SelectorMap['oddsBlue']);
+            else
+              presenceData.state = "Loading...";
+          }
         } else
           presenceData.state = getText(SelectorMap['estatus']);
       } else
