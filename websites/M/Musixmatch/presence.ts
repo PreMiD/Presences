f547635d707ad6e00ddc4322e3bbeac968c96b8d
Nəriman Məmmədli <neriman.memmedli001@gmail.com>
2021-01-30 23:04:56 +0400

    fix codacy errors

--- websites/M/Musixmatch/presence.ts
+++ websites/M/Musixmatch/presence.ts
@@ -4,38 +4,36 @@ const presence = new Presence({
   browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
-  console.log("test");
-
   const presenceData: PresenceData = {
     largeImageKey: 'logo',
     startTimestamp: browsingTimestamp
   },
-    path = (text: string): Boolean => {
+    path = (text: string): boolean => {
       return document.location.pathname.includes(text);
-    }
+    };
 
   if (path("/explore")) {
-    presenceData.details = "Exploring lyrics"
+    presenceData.details = "Exploring lyrics";
   } else if (path("/lyrics/")) {
-    const name = document.querySelector("#site h1").textContent.replace("Lyrics", "")
-    presenceData.details = "Viewing lyrics:"
-    presenceData.state = name
+    const name = document.querySelector("#site h1").textContent.replace("Lyrics", "");
+    presenceData.details = "Viewing lyrics:";
+    presenceData.state = name;
   } else if (path("/community")) {
-    presenceData.details = "Viewing community"
+    presenceData.details = "Viewing community";
   } else if (path("/profile/")) {
     const user = document.querySelector("h1").textContent;
-    presenceData.details = "Viewing the profile:"
+    presenceData.details = "Viewing the profile:";
     presenceData.state = user;
   } else if (path("/artist/")) {
     const artist = document.querySelector("#content h1").textContent;
-    presenceData.details = "Viewing an artist:"
+    presenceData.details = "Viewing an artist:";
     presenceData.state = artist;
   } else if (path("/search/")) {
     const search = document.querySelector("#content span > span").textContent;
-    presenceData.details = "Searching"
+    presenceData.details = "Searching";
     presenceData.state = search;
-    presenceData.smallImageKey = "search"
+    presenceData.smallImageKey = "search";
   }
 
-  presence.setActivity(presenceData)
-})
+  presence.setActivity(presenceData);
+});
