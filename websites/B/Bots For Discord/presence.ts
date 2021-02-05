c397e57ff6e41422b0426aa924e1e78b8e6c4288
brisdevs <kaymak210621@gmail.com>
2021-02-05 18:44:53 +0300

    Missing Semicolon Fixed.

--- websites/B/Bots For Discord/presence.ts
+++ websites/B/Bots For Discord/presence.ts
@@ -9,45 +9,45 @@ presence.on("UpdateData", async () => {
         browsingStamp = Math.floor(Date.now() / 1000);
     presenceData.startTimestamp = browsingStamp;
     if (window.location.hostname == 'botsfordiscord.com') {
-        presenceData.details = "Viewing page:"
+        presenceData.details = "Viewing page:";
         if (document.querySelector('head > title').innerHTML.split("|")[0].trim().startsWith('Error')) {
-            presenceData.state = document.querySelector('#errortitle').innerHTML.trim() + ' (' + document.querySelector('#errorsubtitle').innerHTML.trim() + ')'
+            presenceData.state = document.querySelector('#errortitle').innerHTML.trim() + ' (' + document.querySelector('#errorsubtitle').innerHTML.trim() + ')';
         }
         if (window.location.pathname.toLowerCase() == "/") {
-            presenceData.state = "Homepage"
+            presenceData.state = "Homepage";
         } else if (window.location.pathname.toLowerCase() == '/bots') {
-            presenceData.state = "Discord bots"
+            presenceData.state = "Discord bots";
         } else if (window.location.pathname.toLowerCase() == '/bot/add') {
-            presenceData.state = "Add a new bot"
+            presenceData.state = "Add a new bot";
         } else if (window.location.pathname.toLowerCase() == '/verification') {
-            presenceData.state = "Verification program"
+            presenceData.state = "Verification program";
         } else if (window.location.pathname.startsWith("/house")) {
-            presenceData.state = "House program"
+            presenceData.state = "House program";
         } else if (window.location.pathname.toLowerCase() == '/me' || window.location.pathname.startsWith("/user/")) {
-            presenceData.details = "Viewing a profile:"
-            presenceData.state = document.querySelectorAll("div.container #highlight")[1].innerHTML.trim()
+            presenceData.details = "Viewing a profile:";
+            presenceData.state = document.querySelectorAll("div.container #highlight")[1].innerHTML.trim();
         } else if (window.location.pathname.toLowerCase() == '/me/favorites') {
             presenceData.state = document.querySelectorAll("div.navbar-item a")[8].innerHTML.split("#")[0].trim() + " favorite bots";
         } else if (window.location.pathname.toLowerCase() == '/me/history') {
-            presenceData.state = "Voting history"
+            presenceData.state = "Voting history";
         } else if (window.location.pathname.startsWith("/privacy")) {
-            presenceData.state = "Privacy policy"
+            presenceData.state = "Privacy policy";
         } else if (window.location.pathname.toLowerCase() == '/license') {
-            presenceData.state = 'Website license'
+            presenceData.state = 'Website license';
         } else if (window.location.pathname.toLowerCase() == '/tos') {
-            presenceData.state = "Terms of Service (ToS)"
+            presenceData.state = "Terms of Service (ToS)";
         } else if (window.location.pathname.toLowerCase() == '/partners') {
-            presenceData.state = 'Partners'
+            presenceData.state = 'Partners';
         } else if (window.location.pathname.toLowerCase() == '/contributors') {
-            presenceData.state = 'Contributors'
+            presenceData.state = 'Contributors';
         } else if (window.location.pathname.startsWith('/tag/')) {
-            presenceData.details = "Viewing bots with tag:"
-            presenceData.state = document.querySelector('div.container h1').innerHTML.split('</i>')[1].trim()
+            presenceData.details = "Viewing bots with tag:";
+            presenceData.state = document.querySelector('div.container h1').innerHTML.split('</i>')[1].trim();
         } else if (window.location.pathname.toLowerCase() == '/login') {
-            presenceData.state = 'Login'
+            presenceData.state = 'Login';
         } else if (window.location.pathname.startsWith("/bot/")) {
-            presenceData.details = 'Viewing a bot:'
-            presenceData.state = document.querySelectorAll("div.container #highlight")[0].innerHTML.split("<a")[0].trim()
+            presenceData.details = 'Viewing a bot:';
+            presenceData.state = document.querySelectorAll("div.container #highlight")[0].innerHTML.split("<a")[0].trim();
         }
     } if (window.location.hostname == 'docs.botsfordiscord.com') {
         if (window.location.pathname.startsWith("/")) {
