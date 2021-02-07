27c1fa8fd4bf293bea3f87f80fd0134505ed34b6
Hibanka <hibankaoff@gmail.com>
2021-02-07 15:06:14 +0200

    Binance presence fixes and improves

--- websites/B/Binance/presence.ts
+++ websites/B/Binance/presence.ts
@@ -18,18 +18,57 @@ presence.on("UpdateData", async () => {
     if (window.location.pathname.includes("/markets")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Markets";
+    } else if (window.location.pathname.includes("/my/wallet/account/main")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Main Wallet";
+    } else if (window.location.pathname.includes("/my/wallet/account/margin")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Margin Wallet";
+    } else if (window.location.pathname.includes("/my/wallet/account/futures")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Futures Wallet";
+    } else if (window.location.pathname.includes("/my/wallet/account/c2c")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "P2P Wallet";
+    } else if (window.location.pathname.includes("/my/wallet/account/saving")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Earn Wallet";
+    } else if (window.location.pathname.includes("/my/wallet/account/mining")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Pool Wallet";
+    } else if (window.location.pathname.includes("/my/wallet")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Wallet";
     } else if (window.location.pathname.includes("/earn")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Binance Earn";
-    } else if (window.location.pathname.includes("/earn")) {
+    } else if (window.location.pathname.includes("/support")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Binance Support";
+    } else if (window.location.pathname.includes("/multipleChart")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Charts";
+    } else if (window.location.pathname.includes("/futuresng-activity/battle")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Futures Battle";
+    } else if (window.location.pathname.includes("/futuresng-activity/leaderboard")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Futures Leaderboard";
     } else if (window.location.pathname.includes("/trade")) {
       presenceData.startTimestamp = browsingStamp;
-      presenceData.details = "Trading " + getLastPath(window.location.pathname).replace("_", "/");
+      presenceData.details = "Trading";
+      if (getLastPath(window.location.pathname).includes("_")) {
+        presenceData.state = "Exchange: " + getLastPath(window.location.pathname).replace("_", "/");
+      }
+    } else if (window.location.pathname.includes("/futures")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Futures Trading";
+      if (getLastPath(window.location.pathname).includes("_")) {
+        presenceData.state = "Exchange: " + getLastPath(window.location.pathname).replace("_", "/");
+      }
     } else {
       presenceData.startTimestamp = browsingStamp;
-      presenceData.details = "Home";
+      presenceData.details = "Browsing...";
     }
   } else if (window.location.host.startsWith("pool")) {
     presenceData.startTimestamp = browsingStamp;
