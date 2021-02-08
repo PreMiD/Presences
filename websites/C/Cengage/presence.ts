bdbac627293478a42ffafd678d97481dbbcbfa30
Alexx <cwmills03@gmail.com>
2021-02-08 02:49:42 -0600

    Updated SiriusXM
    SiriusXM changed an element from "On Demand Episode" to "Episode," updated code to read that.

--- websites/C/Cengage/presence.ts
+++ websites/C/Cengage/presence.ts
@@ -48,7 +48,7 @@ presence.on("UpdateData", async () => {
     presenceData.state = channelName.innerText + " - Xtra Channel";
   } else if (
     document.querySelector("p.now-playing-info-label__text").textContent ===
-    "Episode"
+    "On Demand Episode"
   ) {
     songName = document.querySelector("p.track-name");
     songArtist = document.querySelector("p.show-title");
