db709520030eb2ba74c6b9fad6b091bcddabd7fe
Striker <zzstrikerzzyt@gmail.com>
2021-02-09 20:57:28 +0000

    Recode IF logic for ADN

--- websites/A/AnimeDigitalNetwork/presence.ts
+++ websites/A/AnimeDigitalNetwork/presence.ts
@@ -9,40 +9,50 @@ const presence = new Presence({
 presence.on("UpdateData", async () => {
   const video: HTMLVideoElement = document.querySelector("video.vjs-tech");
 
-  if (video && !isNaN(video.duration)) {
-    const title = document.querySelector(".adn-player-header a").textContent,
-     subtitle = document.querySelector(".adn-player-header span")
-      .textContent,
-     timestamps = presence.getTimestamps(
-      Math.floor(video.currentTime),
-      Math.floor(video.duration)
-    ),
-
-     data: PresenceData = {
-      details: title,
-      state: subtitle,
-      largeImageKey: "logo",
-      smallImageKey: video.paused ? "pause" : "play",
-      smallImageText: video.paused
-        ? (await strings).pause
-        : (await strings).play,
-      startTimestamp: timestamps[0],
-      endTimestamp: timestamps[1]
-    };
-
-    if (video.paused) {
-      delete data.startTimestamp;
-      delete data.endTimestamp;
+  if (document.location.pathname.includes("video") && video) {
+    if (video && !isNaN(video.duration)) {
+      const title = document.querySelector(".adn-player-header a").textContent,
+       subtitle = document.querySelector(".adn-player-header span")
+        .textContent,
+       timestamps = presence.getTimestamps(
+        Math.floor(video.currentTime),
+        Math.floor(video.duration)
+      ),
+  
+       data: PresenceData = {
+        details: title,
+        state: subtitle,
+        largeImageKey: "logo",
+        smallImageKey: video.paused ? "pause" : "play",
+        smallImageText: video.paused
+          ? (await strings).pause
+          : (await strings).play,
+        startTimestamp: timestamps[0],
+        endTimestamp: timestamps[1]
+      };
+  
+      if (video.paused) {
+        delete data.startTimestamp;
+        delete data.endTimestamp;
+      }
+  
+      if (title !== null && subtitle !== null) {
+        presence.setActivity(data, !video.paused);
+      }
     }
-
-    if (title !== null && subtitle !== null) {
-      presence.setActivity(data, !video.paused);
-    }
+  } else if (document.location.pathname.includes("video") && !video) {
+      const title = document.querySelector("h1.sc-pzMyG.sc-jHngDS.efMrJJ").textContent,
+        data: PresenceData = {
+          details: "Browsing...",
+          state: title,
+          largeImageKey: "logo",
+        };
+      presence.setActivity(data)
   } else {
-    const browsingPresence: PresenceData = {
-      details: "Browsing...",
-      largeImageKey: "logo"
-    };
-    presence.setActivity(browsingPresence);
-  }
+      const browsingPresence: PresenceData = {
+        details: "Browsing...",
+        largeImageKey: "logo"
+      };
+      presence.setActivity(browsingPresence);
+    }
 });
