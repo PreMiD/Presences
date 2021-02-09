0d1e5f0257b8937d6f012c89b5c01ffa2c1516ca
marzeq <marzeqmarzeq@gmail.com>
2021-02-09 12:08:23 +0100

    oops forgot these 2

--- websites/R/ReadTheDocs/presence.ts
+++ websites/R/ReadTheDocs/presence.ts
@@ -26,7 +26,7 @@ presence.on("UpdateData", async () => {
     else presenceData.details = await getStringFromSettings(presence, "viewing_docs", {name: name});
 
     if (loc.match(/search/g)) { // if searching on docs
-      let term = loc.replace(/([a-z0-9-/:]+)[.]readthedocs[.](io|org)\/.+\/.+\/search.*\?q\=([^\&]+).*/g, "$3"); // get search term
+      let term = loc.replace(/([a-z0-9-/:]+)[.]readthedocs[.](io|org)\/.+\/.+\/search.*\?q\=([^&]+).*/g, "$3"); // get search term
 
       if (term.endsWith("#")) term = term.slice(0, -1); // remove trailing hashtag
       if (!loc.endsWith("/search") && !loc.endsWith("/search.html")) presenceData.state = await getStringFromSettings(presence, "searching_for", {term: term});
@@ -42,7 +42,7 @@ presence.on("UpdateData", async () => {
   // if searching for docs with the search term in the url
   else if (loc.match(/search/)) {
     if (!loc.endsWith("/search")) {
-      const term = loc.replace(/https?:\/\/readthedocs[.](io|org)\/search\/\?q=([^\&]+).*/g, "$2"); // get search term
+      const term = loc.replace(/https?:\/\/readthedocs[.](io|org)\/search\/\?q=([^&]+).*/g, "$2"); // get search term
       presenceData.state = await getStringFromSettings(presence, "searching_for", {term: term});
     }
 
