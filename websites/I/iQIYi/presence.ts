c84bf30aed2e81578473847640c461135ce310d1
Slow_Life <mrboo889911@gmail.com>
2021-02-12 21:53:15 +0700

    Update

---
+++ websites/I/iQIYi/presence.ts
@@ -0,0 +1,68 @@
+interface Data {
+    title?: string | null,
+    ep?: string | null
+}
+
+const presence = new Presence({
+    clientId: "809748404963770398"
+})
+
+let browsingStamp = Math.floor(Date.now() / 1000),
+    prevUrl = document.location.href
+
+presence.on("UpdateData", async () => {
+
+    if (document.location.href !== prevUrl){
+        prevUrl = document.location.href;
+        browsingStamp = Math.floor(Date.now() / 1000);
+    }
+
+    let presenceData: PresenceData = {  
+          largeImageKey: "iqiyi_logo",
+          details: "Browsering...",
+          smallImageKey: "search",
+          startTimestamp: browsingStamp
+      };
+
+    if (document.location.pathname.includes('/play')){
+
+        const data: Data = {
+            title: document.querySelector('h1 a').textContent,
+            ep: document.querySelector('h1').textContent.replace(document.querySelector('h1 a').textContent, ''),
+        }
+    
+        const video: HTMLVideoElement = document.querySelector('video'),
+        timestamps: number[] = presence.getTimestampsfromMedia(video);
+    
+        if (video && !isNaN(video.duration)){
+            presenceData.details = data.title
+            presenceData.state = data.ep
+
+            presenceData.smallImageKey = video.paused ? "pause" : "play"
+            presenceData.smallImageText = video.paused ? "Paused" : "Playing"
+
+            presenceData.startTimestamp = timestamps[0]
+            presenceData.endTimestamp = timestamps[1]
+
+            if (video.paused){
+                delete presenceData.startTimestamp;
+                delete presenceData.endTimestamp;
+            }
+        }
+
+        if (isNaN(video.duration)){
+            presenceData.details = "Looking at:";
+            presenceData.state = data.title;
+            presenceData.startTimestamp = browsingStamp;
+        }
+    }
+
+
+    if (document.location.pathname.includes('/search')){
+        presenceData.details = "Searching for something";
+        presenceData.startTimestamp = browsingStamp;
+        presenceData.smallImageKey = "search";
+    }
+
+    presence.setActivity(presenceData)
+})
