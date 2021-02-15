24ca1dfc44da2b2893bcf8168726655c4c30b6fa
Striker <zzstrikerzzyt@gmail.com>
2021-02-15 15:39:41 +0000

    Rewrite presence

--- websites/A/Amazon Music/presence.ts
+++ websites/A/Amazon Music/presence.ts
@@ -1,61 +1,63 @@
 const presence = new Presence({
     clientId: "808756700022702120"
-  }),
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
   });
 
-function getTime(list: string[]): number {
-  let ret = 0;
-  for (let index = list.length - 1; index >= 0; index--) {
-    ret += parseInt(list[index]) * 60 ** index;
-  }
-  return ret;
-}
-
-function getTimestamps(audioDuration: string): Array<number> {
-  const splitAudioDuration = audioDuration.split(":").reverse(),
-
-   parsedAudioDuration = getTime(splitAudioDuration),
-
-   startTime = Date.now(),
-   endTime = Math.floor(startTime / 1000) + parsedAudioDuration;
-  return [Math.floor(startTime / 1000), endTime];
-}
+let fullscreen: boolean, player = false;
 
 presence.on("UpdateData", async () => {
-  const player = document.querySelector(".playbackActive");
-  if (player) {
-    const title = document.querySelector(".trackTitle span").textContent,
-     artist = document.querySelector(".trackArtist span").textContent,
-     durationTime = document.querySelector(
-      ".listViewDurationContextButton .listViewDuration"
-    ).textContent,
-     timestamps = getTimestamps(durationTime.replace("-", "")),
-     paused = document.querySelector(
-      ".playbackControls span.playerIconPause"
-    )
-      ? false
-      : true,
+  player =
+        document
+          .querySelector(
+            "body > div#root > music-app.hydrated > div.BAibzabUKijQgULVQbqCf > div#transport._333T0bVoft6GGOqUYjsnIA > div._3l2xsX5-KkYUgDHJDu-L0r > music-horizontal-item"
+          ) != undefined
+      ? true
+      : false;
 
-     data: PresenceData = {
-      details: title,
-      state: artist,
-      largeImageKey: "logo",
-      smallImageKey: paused ? "pause" : "play",
-      smallImageText: paused ? (await strings).pause : (await strings).play,
-      startTimestamp: timestamps[0],
-      endTimestamp: timestamps[1]
-    };
+  if (player == true) {
+    const exitFSButton = document.querySelector(
+      "div._2kGtEHAlQ5t5sY3jvz-wwl > div._1Wgs9MKFGuL58IFgKSM811 > div._2HXusrWftEtKAYukKt5IuO > music-button"
+    );
+    exitFSButton != null ? (fullscreen = true) : (fullscreen = false);
+    if (fullscreen == true) {
+      const title = document.querySelector("a.music-headline-2").textContent,
+        artist = document
+          .querySelector("a.music-primary-text")
+          .textContent.split(" - ")[0],
 
-    if (paused) {
-      delete data.startTimestamp;
-      delete data.endTimestamp;
-    }
+        data: PresenceData = {
+          details: title,
+          state: artist,
+          largeImageKey: "logo"
+        };
 
-    if (title !== null && artist !== null) {
-      presence.setActivity(data, !paused);
+      if (title !== null && artist !== null) {
+        presence.setActivity(data);
+      }
+    } else if (fullscreen == false) {
+      const title = document
+          .querySelector(
+            "body > div#root > music-app.hydrated > div.BAibzabUKijQgULVQbqCf > div#transport._333T0bVoft6GGOqUYjsnIA > div._3l2xsX5-KkYUgDHJDu-L0r > music-horizontal-item"
+          )
+          .shadowRoot.querySelector(
+            "div.item.parent-undefined > div.center > music-link.hydrated > a"
+          ).textContent,
+        artist = document
+          .querySelector(
+            "body > div#root > music-app.hydrated > div.BAibzabUKijQgULVQbqCf > div#transport._333T0bVoft6GGOqUYjsnIA > div._3l2xsX5-KkYUgDHJDu-L0r > music-horizontal-item"
+          )
+          .shadowRoot.querySelector(
+            "div.item.parent-undefined > div.center > span"
+          )
+          .textContent.split("-")[0],
+        data: PresenceData = {
+          details: title,
+          state: artist,
+          largeImageKey: "logo"
+        };
+
+        if (title !== null && artist !== null) {
+          presence.setActivity(data);
+        }
     }
   } else {
     const data: PresenceData = {
