5c77f022ee99aea3fe3a899fd9996bab74728813
Striker <zzstrikerzzyt@gmail.com>
2021-02-17 21:34:01 +0000

    Update presence.ts

--- websites/A/Amazon Music/presence.ts
+++ websites/A/Amazon Music/presence.ts
@@ -1,15 +1,36 @@
 const presence = new Presence({
     clientId: "808756700022702120"
+  }),
+  strings = presence.getStrings({
+    play: "presence.playback.playing",
+    pause: "presence.playback.paused"
   });
 
-let fullscreen: boolean, player = false;
+let fullscreen: boolean,
+  player = false,
+  paused,
+  currentTime,
+  timeLeft,
+  timestamps;
+
+function getDuration(time1: string, time2: string) {
+  return (
+    parseInt(time1.split(":")[0]) * 60 +
+    parseInt(time1.split(":")[1]) +
+    parseInt(time2.split(":")[0]) * 60 +
+    parseInt(time2.split(":")[1])
+  );
+}
+
+function convertToSeconds(time: string) {
+  return parseInt(time.split(":")[0]) * 60 + parseInt(time.split(":")[1]);
+}
 
 presence.on("UpdateData", async () => {
   player =
-        document
-          .querySelector(
-            "body > div#root > music-app.hydrated > div.BAibzabUKijQgULVQbqCf > div#transport._333T0bVoft6GGOqUYjsnIA > div._3l2xsX5-KkYUgDHJDu-L0r > music-horizontal-item"
-          ) != undefined
+    document.querySelector(
+      "body > div#root > music-app.hydrated > div.BAibzabUKijQgULVQbqCf > div#transport._333T0bVoft6GGOqUYjsnIA > div._3l2xsX5-KkYUgDHJDu-L0r > music-horizontal-item"
+    ) != undefined
       ? true
       : false;
 
@@ -23,17 +44,42 @@ presence.on("UpdateData", async () => {
         artist = document
           .querySelector("a.music-primary-text")
           .textContent.split(" - ")[0],
+        pausedIcon = document
+          .querySelector("music-button.hydrated:nth-child(4)")
+          .shadowRoot.querySelector("button > div > music-icon");
+      paused = pausedIcon.attributes[1].value == "pause" ? false : true,
+      currentTime = document.querySelector(
+        "div.sXaGQzYs9WqImj2uxDCBs > span:nth-child(1)"
+      ).textContent;
+      timeLeft = document
+        .querySelector("div.sXaGQzYs9WqImj2uxDCBs > span:nth-child(2)")
+        .textContent.replace(" - ", "");
 
-        data: PresenceData = {
-          details: title,
-          state: artist,
-          largeImageKey: "logo"
-        };
+        timestamps = presence.getTimestamps(
+          convertToSeconds(currentTime),
+          getDuration(currentTime, timeLeft)
+        );
+
+      const data: PresenceData = {
+        details: title,
+        state: artist,
+        smallImageKey: paused == true ? "pause" : "play",
+        smallImageText:
+          paused == true ? (await strings).pause : (await strings).play,
+        largeImageKey: "logo",
+        startTimestamp: timestamps[0],
+        endTimestamp: timestamps[1]
+      };
+
+      if (paused) {
+        delete data.startTimestamp;
+        delete data.endTimestamp;
+      }
 
       if (title !== null && artist !== null) {
         presence.setActivity(data);
       }
-    } else if (fullscreen == false) {
+    } else {
       const title = document
           .querySelector(
             "body > div#root > music-app.hydrated > div.BAibzabUKijQgULVQbqCf > div#transport._333T0bVoft6GGOqUYjsnIA > div._3l2xsX5-KkYUgDHJDu-L0r > music-horizontal-item"
@@ -49,15 +95,40 @@ presence.on("UpdateData", async () => {
             "div.item.parent-undefined > div.center > span"
           )
           .textContent.split("-")[0],
-        data: PresenceData = {
-          details: title,
-          state: artist,
-          largeImageKey: "logo"
-        };
+        pausedIcon = document
+          .querySelector("music-button.hydrated:nth-child(4)")
+          .shadowRoot.querySelector("button > div > music-icon");
+      paused = pausedIcon.attributes[1].value == "pause" ? false : true,
+      currentTime = document
+        .querySelector("div.sXaGQzYs9WqImj2uxDCBs._1KQKoAP31YB14fDTsoEmwh")
+        .textContent.split(" - ")[0];
+      timeLeft = document
+        .querySelector("div.sXaGQzYs9WqImj2uxDCBs._1KQKoAP31YB14fDTsoEmwh")
+        .textContent.split(" - ")[1],
+        timestamps = presence.getTimestamps(
+          convertToSeconds(currentTime),
+          getDuration(currentTime, timeLeft)
+        );
 
-        if (title !== null && artist !== null) {
-          presence.setActivity(data);
-        }
+      const data: PresenceData = {
+        details: title,
+        state: artist,
+        largeImageKey: "logo",
+        startTimestamp: timestamps[0],
+        smallImageKey: paused == true ? "pause" : "play",
+        smallImageText:
+          paused == true ? (await strings).pause : (await strings).play,
+        endTimestamp: timestamps[1]
+      };
+
+      if (paused) {
+        delete data.startTimestamp;
+        delete data.endTimestamp;
+      }
+
+      if (title !== null && artist !== null) {
+        presence.setActivity(data);
+      }
     }
   } else {
     const data: PresenceData = {
