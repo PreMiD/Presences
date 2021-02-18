20a0705e8163a75ac632ec7450775f7fda6c772e
QkeleQ10 <quintenra@gmail.com>
2021-02-18 17:44:09 +0100

    Made it a little prettier 👀

--- websites/S/Satchel One/presence.ts
+++ websites/S/Satchel One/presence.ts
@@ -7,24 +7,24 @@ presence.on("UpdateData", async () => {
     largeImageKey: "eyyv17uu4aq4ri0"
   };
 
-   if (document.location.href.includes("calendar/personal")) {
+  if (document.location.href.includes("calendar/personal")) {
     presenceData.details = "Viewing timetable";
   } else if (document.location.href.includes("behaviour")) {
     presenceData.details = "Viweing behaviour Dashboard";
-  } else if (document.location.href.includes("gradebook")){
+  } else if (document.location.href.includes("gradebook")) {
     presenceData.details = "Viweing Grades";
-  } else if (document.location.href.includes("calendar/school")){
+  } else if (document.location.href.includes("calendar/school")) {
     presenceData.details = "Viweing whole school calendar";
-  } else if (document.location.href.includes("classwork")){
+  } else if (document.location.href.includes("classwork")) {
     presenceData.details = "Viweing task";
-  } else if (document.location.href.includes("homeworks")){
-    presenceData.details ="Viweing task";
-  } else if(document.location.href.includes("login")){
-  presenceData.details ="logging in";
-  } else if(document.location.href.includes("todo")){
-  presenceData.details ="task list";
+  } else if (document.location.href.includes("homeworks")) {
+    presenceData.details = "Viweing task";
+  } else if (document.location.href.includes("login")) {
+    presenceData.details = "Logging in";
+  } else if (document.location.href.includes("todo")) {
+    presenceData.details = "Task list";
   }
-  
+
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
