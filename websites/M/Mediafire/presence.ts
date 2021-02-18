72df97f6140e5e687bc0e0d08daa59e3a65a20b9
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-02-18 16:55:03 +0100

    Update presence.ts

--- websites/M/Mediafire/presence.ts
+++ websites/M/Mediafire/presence.ts
@@ -17,99 +17,99 @@ presence.on("UpdateData", async () => {
         presenceData.details = "Viewing the homepage";
       }
       if (page.includes("/file/")) {
-        title = document.querySelector("div.dl-btn-label")
+        title = document.querySelector("div.dl-btn-label");
         presenceData.details = "Viewing:";
-        presenceData.state = title.textContent
+        presenceData.state = title.textContent;
       }
-    if (page == "/software/mobile/") {
+    if (page == "/software/mobile/" || page == "/software/mobile") {
       presenceData.details = "Viewing:";
-      presenceData.state = "Mediafire Mobile Apps"
+      presenceData.state = "Mediafire Mobile Apps";
     }
     if (page == "/about" || page == "/about/") {
       title = document.querySelector("#titlebar > div > h1")
       presenceData.details = "Viewing:";
-      presenceData.state = title.textContent
+      presenceData.state = title.textContent;
     } else if (page.includes("/about/contact_us")) {
-      title = document.querySelector("#content > h2")
+      title = document.querySelector("#content > h2");
       presenceData.details = "Viewing:";
-      presenceData.state = title.textContent
+      presenceData.state = title.textContent;
     } else if (page.includes("/about/jobs")) {
-      title = document.querySelector("#jobs_pics_section > div.opener > h2")
+      title = document.querySelector("#jobs_pics_section > div.opener > h2");
       presenceData.details = "Viewing:";
-      presenceData.state = title.textContent
+      presenceData.state = title.textContent;
     }
     if (page == "/advertising" || page == "/advertising/") {
-      title = document.querySelector("#content > h2")
+      title = document.querySelector("#content > h2");
       presenceData.details = "Viewing:";
       presenceData.state = title.textContent
     } else if (page.includes("/advertising/")) {
-      title = document.querySelector("#content > h2")
+      title = document.querySelector("#content > h2");
       if (!title) {
-        title = document.querySelector("#adInquiryWrap > h2")
+        title = document.querySelector("#adInquiryWrap > h2");
         presenceData.details = "Viewing:";
-        presenceData.state = title.textContent
+        presenceData.state = title.textContent;
       } else {
       presenceData.details = "Viewing:";
-      presenceData.state = title.textContent
+      presenceData.state = title.textContent;
       }
     }
     if (page == "/press" || page == "/press/") {
       title = document.querySelector("#content > h2.h2.mb-4")
       presenceData.details = "Viewing:";
-      presenceData.state = title.textContent
+      presenceData.state = title.textContent;
     } else if (page.includes("/press/inquiries")) {
-      title = document.querySelector("#content > h2")
+      title = document.querySelector("#content > h2");
       presenceData.details = "Viewing:";
-      presenceData.state = title.textContent
+      presenceData.state = title.textContent;
     }
-    if (page.includes("/software")) {
-      title = document.querySelector("#titlebar > div > h1")
-      presenceData.details = "Viewing:";
-      presenceData.state = title.textContent
-    }
+    // if (page.includes("/software")) {
+    //   title = document.querySelector("#titlebar > div > h1")
+    //   presenceData.details = "Viewing:";
+    //   presenceData.state = title.textContent
+    // }
     if (page.includes("/policies/")) {
-      title = document.querySelector("#content > div > div > h1 > strong")
+      title = document.querySelector("#content > div > div > h1 > strong");
       if (!title) {
-        title = document.querySelector("#content > h2")
+        title = document.querySelector("#content > h2");
         presenceData.details = "Viewing:";
-        presenceData.state = title.textContent
+        presenceData.state = title.textContent;
       } else {
         presenceData.details = "Viewing:";
-        presenceData.state = title.textContent
+        presenceData.state = title.textContent;
       }
 
     }
     if (page.includes("/policy_violation/")) {
-      title = document.querySelector("#content > h2:nth-child(1)")
+      title = document.querySelector("#content > h2:nth-child(1)");
       if (!title) {
-        title = document.querySelector("#content > div > h2")
+        title = document.querySelector("#content > div > h2");
         presenceData.details = "Viewing:";
-        presenceData.state = title.textContent
+        presenceData.state = title.textContent;
       } else {
       presenceData.details = "Viewing:";
-      presenceData.state = title.textContent
+      presenceData.state = title.textContent;
       }
     }
    if (page.includes("/subscriptions")) {
-    title = document.querySelector("#content > h2")
+    title = document.querySelector("#content > h2");
     presenceData.details = "Viewing:";
-    presenceData.state = title.textContent
+    presenceData.state = title.textContent;
    }
    if (page.includes("/credits/")) {
-    title = document.querySelector("#content > h2")
+    title = document.querySelector("#content > h2");
     presenceData.details = "Viewing:";
-    presenceData.state = title.textContent
+    presenceData.state = title.textContent;
    }
    if (page.includes("/help/")) {
     presenceData.details = "Viewing:";
-    presenceData.state = "Help"
+    presenceData.state = "Help";
    }
     if (page.includes("/login/")) {
       presenceData.details = "Logging in";
     }
     if (page.includes("/upgrade/")) {
       presenceData.details = "Viewing:";
-      presenceData.state = "Upgrade Plans"
+      presenceData.state = "Upgrade Plans";
     }
     // if (page.includes("/c/")) {
     //   const numberPat = '[0-9]+',
@@ -131,19 +131,19 @@ presence.on("UpdateData", async () => {
   if (host == "app.mediafire.com") {
     if (page.includes("/myfiles")) {
       presenceData.details = "Viewing:";
-      presenceData.state = "My Files"
+      presenceData.state = "My Files";
     } else if(page.includes("/recent")) {
       presenceData.details = "Viewing:";
-      presenceData.state = "My Recent Files"
+      presenceData.state = "My Recent Files";
     } else if (page.includes("/following")) {
       presenceData.details = "Viewing:";
-      presenceData.state = "Files Shared With Me"
+      presenceData.state = "Files Shared With Me";
     } else if (page.includes("/trash")) {
       presenceData.details = "Viewing:";
-      presenceData.state = "The Trashcan"
+      presenceData.state = "The Trashcan";
     } else {
       presenceData.details = "Viewing:";
-      presenceData.state = "My Files"
+      presenceData.state = "My Files";
     }
   }
   if (host == "mediafire.zendesk.com") {
@@ -168,10 +168,10 @@ presence.on("UpdateData", async () => {
   if (page == "/") {
    title = document.querySelector("#fl-post-3434 > header > h2 > a")
    presenceData.details = "Viewing Blog Post About:";
-   presenceData.state = title.textContent
+   presenceData.state = title.textContent;
   } else {
     presenceData.details = "Viewing:";
-    presenceData.state = "Oops- We don't know this location."
+    presenceData.state = "Oops- We don't know this location.";
   }
   }
 
