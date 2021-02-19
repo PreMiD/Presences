1f2557620f10527e7e0ef5b08225fe4fa847c598
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-02-19 11:54:53 +0100

    Some Updates
    Added Search
    Added Cars
    Added Car Search

--- websites/M/Marktplaats/presence.ts
+++ websites/M/Marktplaats/presence.ts
@@ -1,92 +1,103 @@
 const presence = new Presence({
-    clientId: "811572600294735902"
-  }),
-  browsingStamp = Math.floor(Date.now() / 1000);
-let search: HTMLInputElement, title: Element;
+  clientId: "811572600294735902"
+}), browsingStamp = Math.floor(Date.now() / 1000);
+let search: HTMLInputElement,
+title: Element;
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-      largeImageKey: "logo"
-    },
-    page = window.location.pathname,
-    host = document.location.hostname;
+    largeImageKey: "logo"
+  },
+  page = window.location.pathname,
+  host = document.location.hostname; 
 
-  presenceData.startTimestamp = browsingStamp;
-  if (host == "www.marktplaats.nl") {
-    if (page == "/") {
-      presenceData.details = "Viewing the homepage";
-    }
+    presenceData.startTimestamp = browsingStamp;
+    if (host == "www.marktplaats.nl") {
+    if (page == "/" || !page) {
+        presenceData.details = "Viewing the homepage";
+      }
     if (page.includes("/c/")) {
-      const numberPat = "[0-9]+",
+      if (page.includes("/auto-s/")) {
+        presenceData.details = "Viewing Category:";
+        presenceData.state = "Auto's"
+      } else {
+        const numberPat = '[0-9]+',
         r = new RegExp("/c" + numberPat),
         r2 = new RegExp("/c" + numberPat + "/");
-      if (r2.test(page)) {
-        title = document.querySelector(
-          "div.bucket-page.active > h2.bucket-title.heading.heading-3"
-        );
-        if (title.textContent.includes("Alle categorieën in")) {
-          title.textContent = title.textContent.replace(
-            "Alle categorieën in",
-            ""
-          );
+        if (r2.test(page)) {
+          title = document.querySelector("div.bucket-page.active > h2.bucket-title.heading.heading-3");
+          if (title.textContent.includes("Alle categorieën in")) {
+            title.textContent = title.textContent.replace("Alle categorieën in", "");
+          }
+          presenceData.details = "Viewing Category:";
+          presenceData.state = title.textContent;
+        } else if (r.test(page)) {
+          title = document.querySelector("#content > h1");
+          presenceData.details = "Viewing Category:";
+          presenceData.state = title.textContent;
         }
-        presenceData.details = "Viewing Category:";
-        presenceData.state = title.textContent;
-      } else if (r.test(page)) {
-        title = document.querySelector("#content > h1");
-        presenceData.details = "Viewing Category:";
-        presenceData.state = title.textContent;
       }
     } else if (page.includes("/a/")) {
       title = document.querySelector("#title");
       presenceData.details = "Viewing Item:";
       presenceData.state = title.textContent;
     } else if (page.includes("/u/")) {
-      title = document.querySelector(
-        "#content > section > div > div.mp-TopSection > div > div"
-      );
+      title = document.querySelector("#content > section > div > div.mp-TopSection > div > div");
       presenceData.details = "Viewing User:";
       presenceData.state = title.textContent;
+    } 
+    if (page.includes("/q/")) {
+      search = document.querySelector("#input");
+      if (search.value != "") {
+        presenceData.startTimestamp = browsingStamp;
+        presenceData.details = "Searching For:";
+        presenceData.state = search.value;
+        presenceData.smallImageKey = "searching";
+      } else {
+        presenceData.startTimestamp = browsingStamp;
+        presenceData.details = "Viewing Items About:";
+        presenceData.state = window.location.href.replace("https://www.marktplaats.nl/q/", "").replace("/", "");
+      }
+    }
+    if (page.includes("/l/auto-s/")) {
+      search = document.querySelector("#input");
+      if (search.value != "") {
+        presenceData.startTimestamp = browsingStamp;
+        presenceData.details = "Searching For:";
+        presenceData.state = search.value;
+        presenceData.smallImageKey = "searching";
+      } else {
+        presenceData.startTimestamp = browsingStamp;
+        presenceData.details = "Viewing:";
+        presenceData.state = "Auto's";
+      }
     }
     if (page.includes("/veilig-en-succesvol/")) {
-      title = document.querySelector(
-        "#hero-top > section > div > div.column > div > div:nth-child(1) > h1"
-      );
-      presenceData.details = "Reading about:";
-      presenceData.state = title.textContent;
-    }
+       title = document.querySelector("#hero-top > section > div > div.column > div > div:nth-child(1) > h1");
+       presenceData.details = "Reading about:";
+       presenceData.state = title.textContent;
+    } 
     if (page == "/i/help/contact/") {
       presenceData.details = "Reading about:";
       presenceData.state = "Contact";
     } else if (page.includes("/i/help/")) {
-      if (
-        page ==
-        "/i/help/over-marktplaats/voorwaarden-en-privacybeleid/privacyverklaring/"
-      ) {
+        if (page == "/i/help/over-marktplaats/voorwaarden-en-privacybeleid/privacyverklaring/") {
         presenceData.details = "Reading about:";
         presenceData.state = "Privacyverklaring";
       } else if (page.includes("/voorwaarden-en-privacybeleid/")) {
-        title = document.querySelector(
-          "#content > div.tabs-submenu > div.main-content > h3"
-        );
+        title = document.querySelector("#content > div.tabs-submenu > div.main-content > h3");
         if (title == null) {
-          title = document.querySelector(
-            "#page-wrapper > div > div.content > main > h2"
-          );
+          title = document.querySelector("#page-wrapper > div > div.content > main > h2");
           if (title == null) {
-            title = document.querySelector(
-              "#page-wrapper > div > div.content > main > h3"
-            );
+           title = document.querySelector("#page-wrapper > div > div.content > main > h3");
           }
         }
         presenceData.details = "Reading about:";
         presenceData.state = title.textContent;
       } else {
-        title = document.querySelector(
-          "#page-wrapper > div > div.content > main > div > h3"
-        );
-        presenceData.details = "Reading about:";
-        presenceData.state = title.textContent;
+      title = document.querySelector("#page-wrapper > div > div.content > main > div > h3");
+      presenceData.details = "Reading about:";
+      presenceData.state = title.textContent;
       }
     }
     if (page == "/messages") {
