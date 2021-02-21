e013518a1abe8f2df30b2efd6d15a8a00082ca92
QkeleQ10 <quintenra@gmail.com>
2021-02-21 19:58:17 +0100

    semicolons -_-

--- websites/B/bol.com/presence.ts
+++ websites/B/bol.com/presence.ts
@@ -11,10 +11,10 @@ presence.on("UpdateData", async () => {
   if (document.querySelector("span[class*=h-boxed][data-test*=title]")) {
     presenceData.details = `Bekijkt '${document.querySelector("span[class*=h-boxed][data-test*=title]").innerHTML}'`;
     presenceData.state = `In ${document.querySelector("ul[class*=breadcrumbs][data-test*=breadcrumb]").lastElementChild.querySelector("span[class*=breadcrumbs][data-test*=breadcrumb-name]").innerHTML}`;
-    presenceData.buttons = [{ label: "Naar bol.com", url: "https://www.bol.com" }, { label: "Product bekijken", url: document.location.href }]
+    presenceData.buttons = [{ label: "Naar bol.com", url: "https://www.bol.com" }, { label: "Product bekijken", url: document.location.href }];
   } else if (document.querySelector("h1[class*=bol_header][data-test*=page-title]")) {
     presenceData.details = `Bekijkt ${document.querySelector("h1[class*=bol_header][data-test*=page-title]").innerHTML}`;
-    presenceData.buttons = [{ label: "Naar bol.com", url: "https://www.bol.com" }, { label: "Categorie bekijken", url: document.location.href }]
+    presenceData.buttons = [{ label: "Naar bol.com", url: "https://www.bol.com" }, { label: "Categorie bekijken", url: document.location.href }];
   }
 
   if (document.location.pathname.toLowerCase().includes("order")) { presenceData.details = "Bestelt iets"; }
