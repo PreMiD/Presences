0cdfc056613d97065c95b54bcf230810286bf269
QkeleQ10 <quintenra@gmail.com>
2021-02-21 19:57:48 +0100

    Add bol.com presence

---
+++ websites/B/bol.com/presence.ts
@@ -0,0 +1,38 @@
+const presence = new Presence({
+  clientId: "813110347165728849"
+});
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "bol",
+    buttons: [{ label: "Naar bol.com", url: "https://www.bol.com" }, { label: "Pagina bekijken", url: document.location.href }]
+  };
+
+  if (document.querySelector("span[class*=h-boxed][data-test*=title]")) {
+    presenceData.details = `Bekijkt '${document.querySelector("span[class*=h-boxed][data-test*=title]").innerHTML}'`;
+    presenceData.state = `In ${document.querySelector("ul[class*=breadcrumbs][data-test*=breadcrumb]").lastElementChild.querySelector("span[class*=breadcrumbs][data-test*=breadcrumb-name]").innerHTML}`;
+    presenceData.buttons = [{ label: "Naar bol.com", url: "https://www.bol.com" }, { label: "Product bekijken", url: document.location.href }]
+  } else if (document.querySelector("h1[class*=bol_header][data-test*=page-title]")) {
+    presenceData.details = `Bekijkt ${document.querySelector("h1[class*=bol_header][data-test*=page-title]").innerHTML}`;
+    presenceData.buttons = [{ label: "Naar bol.com", url: "https://www.bol.com" }, { label: "Categorie bekijken", url: document.location.href }]
+  }
+
+  if (document.location.pathname.toLowerCase().includes("order")) { presenceData.details = "Bestelt iets"; }
+  if (document.location.pathname.toLowerCase().includes("basket")) { presenceData.details = "Bekijkt winkelwagentje"; }
+  if (document.location.pathname.toLowerCase().includes("account")) { presenceData.details = "Beheert account"; }
+  if (document.location.pathname.toLowerCase().includes("bestellingen")) { presenceData.details = "Bekijkt bestellingen"; }
+  if (document.location.pathname.toLowerCase().includes("lijstje")) { presenceData.details = "Bekijkt verlanglijstje"; }
+
+  if (!presenceData.details) {
+    presenceData.details = "Bladert op bol.com";
+    presenceData.state = `Pagina '${document.title.replace("| ", "|").split("|")[1]}'`;
+    if (document.location.pathname === "/" || document.location.pathname === "/nl/") { presenceData.state = "Startpagina"; }
+  }
+
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
