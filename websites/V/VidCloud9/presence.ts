75bd71465ecf696065924c85be8719f83718f171
Gurrman375 <james.gurr375@gmail.com>
2021-02-22 09:44:00 -0800

    Long awaited fixes to Vidcloud9

--- websites/V/VidCloud9/presence.ts
+++ websites/V/VidCloud9/presence.ts
@@ -1,18 +1,29 @@
-var presence = new Presence({
-    clientId: "697552926876368917"
-  }),
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
+const presence = new Presence({
+  clientId: "697552926876368917"
+}),
+strings = presence.getStrings({
+  play: "presence.playback.playing",
+  pause: "presence.playback.paused"
+});
 
-var iFrameVideo, currentTime, duration, paused;
-var video;
-var lastPlaybackState = null;
-var playback;
-var title;
+let iFrameVideo: boolean, currentTime: number, duration: number, paused: boolean;
+let video: {
+iframe_video: {
+  duration: number;
+  iFrameVideo: boolean;
+  currTime: number;
+  dur: number;
+  paused: boolean;
+};
+};
+let playback: boolean;
+let title;
 
-presence.on("iFrameData", (data) => {
+presence.on("iFrameData", (data:{
+  iframe_video: {
+    duration: number; iFrameVideo: boolean; currTime: number; dur: number; paused: boolean;
+  };
+  }) => {
   video = data;
   playback = data.iframe_video.duration !== null ? true : false;
   if (playback) {
@@ -23,114 +34,113 @@ presence.on("iFrameData", (data) => {
   }
 });
 
-function getTimestamps(videoTime, videoDuration): any {
-  var startTime = Date.now();
-  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+function getTimestamps(videoTime: number, videoDuration: number): Array<number> {
+  const startTime = Date.now();
+  const endTime = Math.floor(Math.floor(startTime / 1000) - videoTime + videoDuration);
   return [Math.floor(startTime / 1000), endTime];
 }
 
 presence.on("UpdateData", async () => {
-  const info = await presence.getSetting("sSI");
-  const elapsed = await presence.getSetting("sTE");
-  const videoTime = await presence.getSetting("sVT");
+const info = await presence.getSetting("sSI");
+const elapsed = await presence.getSetting("sTE");
+const videoTime = await presence.getSetting("sVT");
 
-  if (elapsed) {
-    var browsingStamp = Math.floor(Date.now() / 1000);
-    console.log("Elapsed is on");
+if (elapsed) {
+  var browsingStamp = Math.floor(Date.now() / 1000);
+  console.log("Elapsed is on");
+}
+var timestamps = getTimestamps(Math.floor(currentTime), Math.floor(duration));
+var presenceData: PresenceData = {
+  largeImageKey: "logo"
+};
+if (videoTime) {
+  console.log("IS ON");
+  if (playback == true) {
+    // lastPlaybackState = playback;
+    browsingStamp = Math.floor(Date.now() / 1000);
   }
-  var timestamps = getTimestamps(Math.floor(currentTime), Math.floor(duration));
-  var presenceData: PresenceData = {
-    largeImageKey: "logo"
-  };
-  if (videoTime) {
-    console.log("IS ON");
-    if (lastPlaybackState != playback) {
-      lastPlaybackState = playback;
-      browsingStamp = Math.floor(Date.now() / 1000);
-    }
-  } else {
-    lastPlaybackState = console.log("Video time is off");
+} else {
+  console.log("Video time is off");
+}
+if (info) {
+  if (document.location.pathname == "/") {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Viewing home page";
+  } else if (document.location.pathname == "/movies") {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Viewing the recently added movies";
+  } else if (document.location.pathname == "/series") {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Viewing the recently added series";
+  } else if (document.location.pathname == "/cinema-movies") {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Viewing the recently added cinema movies.";
+  } else if (document.location.pathname == "/recommended-series") {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Viewing recommened series";
   }
-  if (info) {
-    if (document.location.pathname == "/") {
-      presenceData.startTimestamp = browsingStamp;
-      presenceData.details = "Viewing home page";
-    } else if (document.location.pathname == "/movies") {
-      presenceData.startTimestamp = browsingStamp;
-      presenceData.details = "Viewing the recently added movies";
-    } else if (document.location.pathname == "/series") {
-      presenceData.startTimestamp = browsingStamp;
-      presenceData.details = "Viewing the recently added series";
-    } else if (document.location.pathname == "/cinema-movies") {
-      presenceData.startTimestamp = browsingStamp;
-      presenceData.details = "Viewing the recently added cinema movies.";
-    } else if (document.location.pathname == "/recommended-series") {
-      presenceData.startTimestamp = browsingStamp;
-      presenceData.details = "Viewing recommened series";
-    }
-    //Used for the video files (Needs some work done here)
-    else if (document.location.pathname.includes("/videos/")) {
-      title = document.querySelector(
-        "#main_bg > div:nth-child(5) > div > div.video-info-left > h1"
-      );
-      if (title != null) {
-        presenceData.state = title.innerText;
-        if (
-          iFrameVideo == true &&
-          !isNaN(duration) &&
-          title != null &&
-          video != null
-        ) {
-          if (!paused) {
-            presenceData.details = "Watching:";
-            presenceData.smallImageKey = paused ? "pause" : "play";
-            if (videoTime) {
-              presenceData.smallImageText = paused
-                ? (await strings).pause
-                : (await strings).play;
-              presenceData.startTimestamp = timestamps[0];
-              presenceData.endTimestamp = timestamps[1];
-            }
-          } else if (paused) {
-            delete presenceData.startTimestamp;
-            delete presenceData.endTimestamp;
-            presenceData.details = "Paused:";
-            presenceData.smallImageKey = "pause";
+  //Used for the video files (Needs some work done here)
+  else if (document.location.pathname.includes("/videos/")) {
+    title = document.querySelector(
+      "#main_bg > div:nth-child(5) > div > div.video-info-left > h1"
+    );
+    if (title != null) {
+      presenceData.state = (title as HTMLTextAreaElement).innerText;
+      if (
+        iFrameVideo == true &&
+        !isNaN(duration) &&
+        title != null &&
+        video != null
+      ) {
+        if (!paused) {
+          presenceData.details = "Watching:";
+          presenceData.smallImageKey = paused ? "pause" : "play";
+          if (videoTime) {
+            presenceData.smallImageText = paused
+              ? (await strings).pause
+              : (await strings).play;
+            presenceData.startTimestamp = timestamps[0];
+            presenceData.endTimestamp = timestamps[1];
           }
-        } else if (iFrameVideo == null && isNaN(duration) && title != null) {
-          presenceData.details = "Viewing:";
-          presenceData.state = title.innerText;
-          presenceData.startTimestamp = browsingStamp;
-        } else {
-          presenceData.details = "Error 03: Watching unknown show/movie.";
-          presenceData.state = "Can't tell if playing or not.";
-          presenceData.startTimestamp = browsingStamp;
-          presenceData.smallImageKey = "search";
-          presenceData.smallImageText = "Error 3";
+        } else if (paused) {
+          delete presenceData.startTimestamp;
+          delete presenceData.endTimestamp;
+          presenceData.details = "Paused:";
+          presenceData.smallImageKey = "pause";
         }
-      } else {
-        //Can't get the basic site information
+      } else if (iFrameVideo == null && isNaN(duration) && title != null) {
+        presenceData.details = "Viewing:";
+        presenceData.state = (title as HTMLTextAreaElement).innerText;
         presenceData.startTimestamp = browsingStamp;
-        presenceData.details = "Error 02: Watching unknown show/movie.";
+      } else {
+        presenceData.details = "Error 03: Watching unknown show/movie.";
+        presenceData.state = "Can't tell if playing or not.";
+        presenceData.startTimestamp = browsingStamp;
         presenceData.smallImageKey = "search";
+        presenceData.smallImageText = "Error 3";
       }
-    } //If it can't get the page it will output an error
-    else {
+    } else {
+      //Can't get the basic site information
       presenceData.startTimestamp = browsingStamp;
-      presenceData.details = "Error 01: Can't Read Page";
+      presenceData.details = "Error 02: Watching unknown show/movie.";
       presenceData.smallImageKey = "search";
     }
-  } else {
-    presence.setActivity(presenceData);
-    return;
+  } //If it can't get the page it will output an error
+  else {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Error 01: Can't Read Page";
+    presenceData.smallImageKey = "search";
   }
+} else {
+  presenceData.details = null;
+}
 
-  if (presenceData.details == null) {
-    //This will fire if you do not set presence details
-    presence.setTrayTitle();
-    presence.setActivity();
-  } else {
-    //This will fire if you set presence details
-    presence.setActivity(presenceData);
-  }
+if (presenceData.details == null) {
+  //This will fire if you do not set presence details
+  presence.setTrayTitle();
+  presence.setActivity();
+} else {
+  //This will fire if you set presence details
+  presence.setActivity(presenceData);
+}
 });
