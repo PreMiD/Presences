0f31385ed509d471afaffef2db22c32d4b7e1f58
N0chteil <53608074+N0chteil@users.noreply.github.com>
2021-02-22 09:42:48 +0100

    Added buttons

--- websites/J/Joyn/presence.ts
+++ websites/J/Joyn/presence.ts
@@ -1,11 +1,12 @@
 const presence = new Presence({
-  clientId: "799629862620758046"
-});
-presence.on("UpdateData", async function () {
-  const presenceData = {
-      largeImageKey: "logo"
+    clientId: "799629862620758046"
+  });
+  presence.on("UpdateData", async function () {
+    const presenceData = {
+        largeImageKey: "logo"
     },
     set_timeRemaining = await presence.getSetting("timeRemaining"),
+    set_showButtons = await presence.getSetting("showButtons"),
     urlpath = window.location.pathname.split("/"),
     video = document.querySelector('div video');
     if (document.location.hostname == "www.joyn.de" || document.location.hostname == "joyn.de") {
@@ -15,6 +16,51 @@ presence.on("UpdateData", async function () {
         }
         else if ((urlpath[1] == '' || document.location.pathname.includes("/#home")) && urlpath[2] != '') {
             presenceData.details = "Durchstöbert";
+        }
+        else if (urlpath[1] == 'compilation') {
+            var compilation = document.querySelector('.artLogo');
+            presenceData.details = "Betrachtet Compilation:";
+            if(compilation) presenceData.state = compilation.alt;
+            if(!compilation) presenceData.state = document.querySelector('.hXdaOG').textContent;
+
+            if(set_showButtons) {
+                presenceData.buttons = [
+                    {
+                    label: 'Compilation anschauen',
+                    url: 'https://www.joyn.de/compilation/' + urlpath[2]
+                    }
+                ];
+            }
+        }
+        else if (urlpath[1] == 'filme') {
+            var film = document.querySelector('.artLogo');
+            presenceData.details = "Betrachtet Film:";
+            if(film) presenceData.state = film.alt;
+            if(!film) presenceData.state = document.querySelector('.hXdaOG').textContent;
+
+            if(set_showButtons) {
+                presenceData.buttons = [
+                    {
+                    label: 'Film anschauen',
+                    url: 'https://www.joyn.de/filme/' + urlpath[2]
+                    }
+                ];
+            }
+        }
+        else if (urlpath[1] == 'serien') {
+            var serie = document.querySelector('.artLogo');
+            presenceData.details = "Betrachtet Serie:";
+            if(serie) presenceData.state = serie.alt;
+            if(!serie) presenceData.state = document.querySelector('.hXdaOG').textContent;
+
+            if(set_showButtons) {
+                presenceData.buttons = [
+                    {
+                    label: 'Serie anschauen',
+                    url: 'https://www.joyn.de/serien/' + urlpath[2]
+                    }
+                ];
+            }
         }
         else if (urlpath[1] != 'play' && (document.location.pathname.includes("/serien") || document.location.pathname.includes("/filme") || document.location.pathname.includes("/sport"))) {
             presenceData.details = "Durchstöbert";
@@ -39,6 +85,14 @@ presence.on("UpdateData", async function () {
                 presenceData.smallImageKey = "pause";
                 presenceData.smallImageText = 'Pausiert';
             }
+            if(set_showButtons) {
+                presenceData.buttons = [
+                    {
+                    label: 'Film anschauen',
+                    url: 'https://www.joyn.de/filme/' + urlpath[3]
+                    }
+                ];
+            }
         }
         else if (urlpath[1] == 'play' && urlpath[2] == 'serien') {
             const video_startTime = Date.now(),
@@ -55,6 +109,15 @@ presence.on("UpdateData", async function () {
             } else {
                 presenceData.smallImageKey = "pause";
                 presenceData.smallImageText = 'Pausiert';
+            }
+
+            if(set_showButtons) {
+                presenceData.buttons = [
+                    {
+                    label: 'Serie anschauen',
+                    url: 'https://www.joyn.de/serien/' + urlpath[3]
+                    }
+                ];
             }
         }
         else if (urlpath[1] == 'play' && urlpath[2] == 'trailer') {
@@ -79,6 +142,76 @@ presence.on("UpdateData", async function () {
             presenceData.state = 'Live-TV';
             presenceData.smallImageKey = "live";
             presenceData.smallImageText = 'Live';
+
+            if(set_showButtons) {
+                presenceData.buttons = [
+                    {
+                    label: 'Sendung anschauen',
+                    url: window.location.href
+                    }
+                ];
+            }
+        }
+        else if (urlpath[1] == 'mein-account') {
+            if (urlpath[2] == 'details') {
+                presenceData.details = 'Mein Account';
+                presenceData.state = 'Details';
+            }
+            else {
+                presenceData.details = 'Mein Account';
+            }
+        }
+        else if (urlpath[1] == 'play' && urlpath[2] == 'compilation') {
+            const video_startTime = Date.now(),
+            video_endTime = Math.floor(video_startTime / 1000) - video.currentTime + video.duration;
+            presenceData.details = document.title.replace('| Joyn','');
+            presenceData.state = 'Compilation';
+            if(!video.paused) {
+                if(set_timeRemaining) {
+                  presenceData.startTimestamp = video_startTime;
+                  presenceData.endTimestamp = video_endTime;
+                }
+                presenceData.smallImageKey = "play";
+                presenceData.smallImageText = 'Wiedergabe';
+            } else {
+                presenceData.smallImageKey = "pause";
+                presenceData.smallImageText = 'Pausiert';
+            }
+
+            if(set_showButtons) {
+                presenceData.buttons = [
+                    {
+                    label: 'Compilation anschauen',
+                    url: 'https://www.joyn.de/compilation/' + urlpath[3]
+                    }
+                ];
+            }
+        }
+        else if (urlpath[1] == 'play' && urlpath[2] == 'sport') {
+            const video_startTime = Date.now(),
+            video_endTime = Math.floor(video_startTime / 1000) - video.currentTime + video.duration;
+            presenceData.details = document.querySelector(".metadataWrapper .metadataTitle").textContent;
+            presenceData.state = 'Sport';
+            if(!video.paused) {
+                if(set_timeRemaining) {
+                  presenceData.startTimestamp = video_startTime;
+                  presenceData.endTimestamp = video_endTime;
+                }
+                presenceData.smallImageKey = "play";
+                presenceData.smallImageText = 'Wiedergabe';
+            } else {
+                presenceData.smallImageKey = "pause";
+                presenceData.smallImageText = 'Pausiert';
+            }
+
+            if(set_showButtons) {
+                presenceData.buttons = [
+                    {
+                    label: 'Sportsendung anschauen',
+                    url: window.location.href
+                    }
+                ];
+            }
         }
         else if (urlpath[1] == 'mein-account') {
             if (urlpath[2] == 'details') {
@@ -123,5 +256,8 @@ presence.on("UpdateData", async function () {
     if (presenceData.details == null) {
         presence.setTrayTitle();
         presence.setActivity();
+    }
+    else {
+        presence.setActivity(presenceData);
     }
   });
