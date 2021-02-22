5a0151fe25a0f45e9e238d7726fcf24e5074f342
fantomitechno <fantomitechno@gmail.com>
2021-02-22 12:39:40 +0100

    build the button

--- websites/B/BuildTheEarth/presence.ts
+++ websites/B/BuildTheEarth/presence.ts
@@ -13,51 +13,66 @@ presence.on("UpdateData", async () => {
   presenceData.startTimestamp = browsingStamp;
   if (privacy) {
     presenceData.details = "Browsing";
-  } else if (window.location.pathname.endsWith("me")) {
-    presenceData.details = "Viewing a page:";
-    presenceData.state = "Profil";
-  } else if (window.location.pathname.endsWith("faq")) {
-    presenceData.details = "Viewing a page:";
-    presenceData.state = "FAQ";
-  } else if (window.location.pathname.endsWith("map")) {
-    presenceData.smallImageKey = "map";
-    presenceData.details = "Viewing a page:";
-    if (pmap) presenceData.state = "Map";
-    else {
-      const XO =
-          Math.floor(
-            Number(
-              document
-                .getElementById("map_tpll_command")
-                .textContent.replace("/tpll ", "")
-                .split(" ")[0]
-            ) * 10
-          ) / 10,
-        OX =
-          Math.floor(
-            Number(
-              document
-                .getElementById("map_tpll_command")
-                .textContent.replace("/tpll ", "")
-                .split(" ")[1]
-            ) * 10
-          ) / 10;
-      presenceData.state = `Map : ${XO} | ${OX}`;
-    }
-  } else if (window.location.pathname.endsWith("buildteams")) {
-    presenceData.details = "Viewing a page:";
-    presenceData.state = "BuildTeams";
-  } else if (window.location.pathname.endsWith("contact")) {
-    presenceData.details = "Viewing a page:";
-    presenceData.state = "Contact";
-  } else if (window.location.pathname.endsWith("upload")) {
-    presenceData.details = "Viewing a page:";
-    presenceData.state = "Upload your world";
   } else {
-    presenceData.details = "Viewing a page:";
-    presenceData.state = document.title.replace(" - BuildTheEarth", "");
-    if (window.location.pathname.length != 1)
-      presenceData.details = "Viewing a BuildTeam:";
+    presenceData.buttons = [
+      {
+        label: "Visite Website",
+        url: "https://buildtheearth.net/"
+      }
+    ]
+    if (window.location.pathname.endsWith("me")) {
+      presenceData.details = "Viewing a page:";
+      presenceData.state = "Profil";
+    } else if (window.location.pathname.endsWith("faq")) {
+      presenceData.details = "Viewing a page:";
+      presenceData.state = "FAQ";
+    } else if (window.location.pathname.endsWith("map")) {
+      presenceData.smallImageKey = "map";
+      presenceData.details = "Viewing a page:";
+      if (pmap) presenceData.state = "Map";
+      else {
+        const XO =
+            Math.floor(
+              Number(
+                document
+                  .getElementById("map_tpll_command")
+                  .textContent.replace("/tpll ", "")
+                  .split(" ")[0]
+              ) * 10
+            ) / 10,
+          OX =
+            Math.floor(
+              Number(
+                document
+                  .getElementById("map_tpll_command")
+                  .textContent.replace("/tpll ", "")
+                  .split(" ")[1]
+              ) * 10
+            ) / 10;
+        presenceData.state = `Map : ${XO} | ${OX}`;
+      }
+    } else if (window.location.pathname.endsWith("buildteams")) {
+      presenceData.details = "Viewing a page:";
+      presenceData.state = "BuildTeams";
+    } else if (window.location.pathname.endsWith("contact")) {
+      presenceData.details = "Viewing a page:";
+      presenceData.state = "Contact";
+    } else if (window.location.pathname.endsWith("upload")) {
+      presenceData.details = "Viewing a page:";
+      presenceData.state = "Upload your world";
+    } else {
+      presenceData.details = "Viewing a page:";
+      presenceData.state = document.title.replace(" - BuildTheEarth", "");
+      if (window.location.pathname.length != 1) {
+        presenceData.details = "Viewing a BuildTeam:";
+        presenceData.buttons = [
+          {
+            label: "View the team",
+            url: document.URL
+          }
+        ]
+      }
+    }
   }
 
   if (presenceData.details == null) {
