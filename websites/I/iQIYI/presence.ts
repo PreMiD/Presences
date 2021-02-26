bfc6826927bf8e6f72fefd92e6fe6ad84f823958
Slow_Life <mrboo889911@gmail.com>
2021-02-26 20:36:33 +0700

    Fixes

--- websites/I/iQIYI/presence.ts
+++ websites/I/iQIYI/presence.ts
@@ -21,7 +21,7 @@ const presence = new Presence({
         watchVideo: "general.buttonWatchVideo",
         watchMovie: "general.buttonViewMovie",
         watchEpisode: "general.buttonViewEpisode"
-    }, await presence.getSetting('lang'));
+    }, await presence.getSetting('lang') ?? "en");
 
 let browsingStamp = Math.floor(Date.now() / 1000),
     prevUrl = document.location.href,
@@ -54,32 +54,44 @@ presence.on("UpdateData", async () => {
         strings = getStrings();
     }
 
-    if (document.location.pathname.includes('/play')){
+    if (document.location.pathname.includes('/play') || document.location.pathname.includes("/intl-common/")){
 
         const data = {
-            title: document.querySelector('h1 a').textContent,
-            ep: document.querySelector('h1').textContent.replace(document.querySelector('h1 a').textContent, '')
+            title: (document.querySelector('h1 a') || document.querySelector('title'))?.textContent,
+            ep: (document.querySelector('h1') || document.querySelector(".topice-source-list-item.item-active"))?.textContent.replace(document.querySelector('h1 a')?.textContent || "", '')
         },
 
-        URLItem: string = JSON['parse'](document.querySelectorAll('script[type="application/ld+json"]')[1].innerHTML)[0].itemListElement[0].item,
+        URLItem: string = JSON['parse'](document.querySelectorAll('script[type="application/ld+json"]')[1]?.innerHTML || "{}")[0]?.itemListElement[0]?.item ?? document.URL,
         video: HTMLVideoElement = document.querySelector('video'),
-        timestamps: number[] = presence.getTimestampsfromMedia(video),
         isMovie = URLItem.includes('movie'),
         isVShow = URLItem.includes('variety-show'),
+        isVShowToo = document.location.pathname.includes("/intl-common/"),
         isTrial = document.querySelector('.iqp-player-g.iqp-player .iqp-tip-stream .iqp-txt-vip')?.textContent !== undefined,
-        lastestEp: string[] = document.querySelector('div.broken-line').nextSibling.nextSibling.nextSibling?.textContent.match(/[1-9]?[0-9]?[0-9]/g),
-        contentEp: string[] = data.ep.match(/[1-9]?[0-9]?[0-9]/g),
-        isPreview = (lastestEp && contentEp) ? parseInt(contentEp[0]) > parseInt(lastestEp[0]) : false;
-    
+        lastestEp: string[] = document.querySelector('div.broken-line')?.nextSibling?.nextSibling?.nextSibling?.textContent?.match(/[1-9]?[0-9]?[0-9]/g),
+        contentEp: string[] = isVShowToo ?  data.ep.match(/([1-9]?[0-9]?[0-9]? ?\([1-9]?[0-9]\))/g) : data.ep.match(/[1-9]?[0-9]?[0-9]/g),
+        isPreview = (lastestEp && contentEp) ? parseInt(contentEp[0]) > parseInt(lastestEp[0]) : data.ep.toLowerCase().includes("preview");
+
         if (!data.ep && !isVShow && isMovie) data.ep = "Movie";
-        if (isVShow) data.ep = "Variety show";
-        if (!isVShow && !isMovie) data.ep = `${(await strings).episode} ${contentEp[0]}`;
+        if (isVShowToo) { 
+
+            if (contentEp?.length){
+                data.ep = `${(await strings).episode} ${contentEp[0].match(/.+?(?=\()/g)[0]} ${contentEp[0].includes("(") ? `- ${contentEp[0].match(/(\([1-9]?[0-9]\))/g)[0]}` : "Variety show"}`;
+            } else {
+                data.ep = `Variety show`;
+            }
+
+            data.title = data.title.match(/.+?(?=\s{2})/g)[0];
+        }
+        if (isVShow && !isVShowToo) data.ep = "Variety show";
+        if (!isVShow && !isVShowToo && !isMovie && contentEp !== null) data.ep = `${(await strings).episode} ${contentEp[0]}`;
+        else if (!isVShow && !isVShowToo && !isMovie) data.ep = "Highlight";
 
         if (isTrial && !isPreview) data.ep = `${data.ep} (Trial)`;
 
-        if (video && !isNaN(video.duration)){
+        if (video !== null && !isNaN(video.duration)){
+            const timestamps: number[] = presence.getTimestampsfromMedia(video);
 
-            if (isPreview && !isMovie && !isVShow) data.ep = `${data.ep} preview`;
+            if (isPreview && !isMovie) data.ep = `${data.ep} preview`;
             else if (video.duration < 270 && !isMovie && !isPreview && !isTrial) data.ep = "Highlight";
 
             presenceData.details = data.title;
@@ -102,14 +114,12 @@ presence.on("UpdateData", async () => {
                 delete presenceData.startTimestamp;
                 delete presenceData.endTimestamp;
             }
-        }
-
-        if (isNaN(video.duration)){
+        } else {
             presenceData.details = "Looking at:";
             presenceData.state = data.title;
             presenceData.startTimestamp = browsingStamp;
         }
-        
+
     } else if (document.location.pathname.includes('/search')){
         const searchQuery_ = decodeURI(document.location.search.replace('?query=', '')),
         result = document.querySelector('div.has-result')?.textContent.match(/[0-9]?[0-9]?[0-9]?[0-9]/)[0];
@@ -123,38 +133,6 @@ presence.on("UpdateData", async () => {
         } else {
           presenceData.state = `No matching result`;
         }
-    } else if (document.location.pathname.includes("/intl-common")){
-        const video = document.querySelector("video"),
-        title = document.querySelector("title").textContent.match(/.+?(?=[1-9]|-)/)[0];
-
-        if (video){
-            const timestamps = presence.getTimestampsfromMedia(video);
-
-            presenceData.details = `${title} ${document.title.match(/[1-9]/)}`;
-            presenceData.state = "Variety show";
-
-            presenceData.startTimestamp = timestamps[0];
-            presenceData.endTimestamp = timestamps[1];
-
-            presenceData.smallImageKey = video.paused ? "pause" : "play";
-            presenceData.smallImageText = video.paused ? (await strings).pause : (await strings).play;
-
-            if (showButtons){
-                presenceData.buttons = [{
-                    label: (await strings).watchVideo,
-                    url: document.URL
-                }];
-            } else delete presenceData.buttons;
-
-            if (video.paused){
-                delete presenceData.startTimestamp;
-                delete presenceData.endTimestamp;
-            }
-        } else {
-            presenceData.details = "Looking at:";
-            presenceData.state = title;
-        }
-
     }
 
     presence.setActivity(presenceData);
