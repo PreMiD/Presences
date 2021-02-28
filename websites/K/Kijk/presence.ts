383c80eb6404e6023e4630d206e546881539bfe3
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-02-28 10:09:05 +0100

    Final Final Final Release Canidate (1.0.0)
    Removed the weird startTimestamps

--- websites/K/Kijk/presence.ts
+++ websites/K/Kijk/presence.ts
@@ -17,27 +17,24 @@ presence.on("UpdateData", async () => {
     if (page === "/") {
       search = document.querySelector("#__next > div > div > div.SearchModalstyle__SearchModalStyle-sc-1h6b5wy-0.knmuVj > div.SearchModalstyle__SearchModalHeaderStyle-sc-1h6b5wy-1.kNvWZE > div > div:nth-child(2) > div.SearchModalstyle__SearchModalInputWrapperStyle-sc-1h6b5wy-5.iwOFOK > input");
       if (!search) {
-        presenceData.startTimestamp = browsingStamp;
         presenceData.details = "Viewing the homepage";
       } else if (search.value !== "") {
-        presenceData.startTimestamp = browsingStamp;
         presenceData.details = "Searching For:";
         presenceData.state = search.value;
         presenceData.smallImageKey = "searching";
       } else { 
-        presenceData.startTimestamp = browsingStamp;
         presenceData.details = "Viewing the homepage";
       }
     }
     if (page === "/programmas" || page === "/programmas/") {
       presenceData.details = "Discovering:";
       presenceData.state = "Programma's";
-      presenceData.startTimestamp = browsingStamp;
+     
     }
     if (page === "/films" || page === "/films/") {
       presenceData.details = "Discovering:";
       presenceData.state = "Films";
-      presenceData.startTimestamp = browsingStamp;
+     
     }
     if (page.includes("/films/video/")) {
       delete presenceData.startTimestamp;
@@ -87,7 +84,7 @@ presence.on("UpdateData", async () => {
           presenceData.smallImageKey = "play";
       } else {
       title = document.querySelector("#__next > div > div > div.Mainstyle__MainContentStyle-l53ku6-1.kgsvtU > div > div.Layoutstyle__LightStyle-ecv4pg-0.Formatstyle__FormatLightStyleWrapperStyle-q2lvra-0.beAxHC > div.PromoImageHeadingstyle__PromoImageHeadingStyle-sc-13od09h-0.bwAzes > div.PromoImageHeadingstyle__PromoImageHeadingInnerWrapperStyle-sc-13od09h-2.klAHSa > div > div > div > div.FormatHeadingstyle__FormatHeadingImageWithTextWrapperStyle-sc-148hpqf-1.YJtQc > div.FormatHeadingstyle__FormatHeadingTextStyle-sc-148hpqf-2.glOvjd > h1");
-      presenceData.startTimestamp = browsingStamp;
+     
       presenceData.details = "Viewing:";
       presenceData.state = title.textContent;
     }
