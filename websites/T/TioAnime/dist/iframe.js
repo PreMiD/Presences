926cf387a7becf8ac9a4358848ee5aa5543ac0e8
Giingu <picofuego@gmail.com>
2021-02-28 20:28:19 +0100

    Add files via upload

---
+++ websites/T/TioAnime/dist/iframe.js
@@ -0,0 +1,16 @@
+const iframe = new iFrame();
+iframe.on("UpdateData", async () => {
+    const video = document.querySelector(".jw-video.jw-reset") ||
+        document.querySelector(".video");
+    if (video != null && !isNaN(video.duration)) {
+        iframe.send({
+            video: true,
+            duration: video.duration,
+            currentTime: video.currentTime,
+            paused: video.paused
+        });
+    }
+    else {
+        iframe.send({ video: false });
+    }
+});
---
+++ websites/T/TioAnime/iframe.ts
@@ -0,0 +1,17 @@
+const iframe = new iFrame();
+
+iframe.on("UpdateData", async () => {
+  const video: HTMLVideoElement =
+    document.querySelector(".jw-video.jw-reset") ||
+    document.querySelector(".video");
+  if (video != null && !isNaN(video.duration)) {
+    iframe.send({
+      video: true,
+      duration: video.duration,
+      currentTime: video.currentTime,
+      paused: video.paused
+    });
+  } else {
+    iframe.send({ video: false });
+  }
+});
