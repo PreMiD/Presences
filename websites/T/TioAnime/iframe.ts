1bbc54eb7fd4b47baf316ba9a3440a7203d0ec96
Giingu <picofuego@gmail.com>
2021-02-28 21:38:07 +0100

    Update iframe.ts

--- websites/T/TioAnime/iframe.ts
+++ websites/T/TioAnime/iframe.ts
@@ -2,7 +2,7 @@ const iframe = new iFrame();
 iframe.on("UpdateData", async () => {
     const video = document.querySelector(".jw-video.jw-reset") ||
         document.querySelector(".video");
-    if (video != null && !isNaN(video.duration)) {
+    if (video != null && !Number.isNaN(video.duration)) {
         iframe.send({
             video: true,
             duration: video.duration,
