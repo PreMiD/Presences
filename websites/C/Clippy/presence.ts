7460c351eddfc0333a4ac6806b5753beb21a4e0e
Shrunkie <Shrunkie123@gmail.com>
2021-03-05 15:42:04 +0100

    Fix bad code

--- websites/C/Clippy/presence.ts
+++ websites/C/Clippy/presence.ts
@@ -14,48 +14,59 @@ presence.on("UpdateData", async () => {
         {label: "Website", url: "https://clippy.gg"},
         {label: "Discord", url: "https://discord.gg/image"}]
     };
-  
-  if (path.endsWith("/")) {
-    presenceData.details = "Viewing Clippy.gg.";
-    presenceData.state = "A private image uploader";
-  } else if (path.endsWith("/dashboard")) {
-    presenceData.state = "Viewing the dasboard";
-  } else if (path.endsWith("/settings")) {
-    const intervalEditor = document.getElementsByClassName("ant-modal-wrap ant-modal-centered")[0] as HTMLElement,
-      embedEditor = document.getElementsByClassName("embedEditor")[0] as HTMLElement,
-      domainPicker = document.getElementsByClassName("ant-select-open")[0] as HTMLElement,
-      fakeUrlManager = document.getElementsByClassName("fakeUrlManager")[0] as HTMLElement;
 
-    if (embedEditor && embedEditor.parentElement.style.display !== "none") {
-      presenceData.state = "Editing their embed";
-    } else if (domainPicker) {
-      presenceData.state = "Choosing a domain";
-    } else if (intervalEditor && intervalEditor.style.display !== "none") {
-      presenceData.state = "Editing their auto-wipe interval";
-    } else if (fakeUrlManager && fakeUrlManager.parentElement.style.display !== "none") {
-      presenceData.state = "Editing their fake url";
-    } else {
-      presenceData.state = "Viewing their settings";
-    }
-  } else if (path.endsWith("/domains")) {
-    const randomDomainPicker = document.getElementsByClassName("ant-select-open")[0] as HTMLElement;
-    if(randomDomainPicker){
-      presenceData.state = `Adding a random domain`;
-    }else if(!document.body.textContent.includes("Loading")){
-      presenceData.state = `Viewing ${document.body.textContent.match(/\d/g).join("").substring(1, 4)} domains`;
-    }
-  } else if (path.endsWith("/upload")) {
-    presenceData.state = "Uploading a file";
-    presenceData.details = "Viewing the uploader";
-  } else if (path.endsWith("/shorten")) {
-    presenceData.state = "Shortening a link";
-    presenceData.details = "Viewing the shortener";
-  } else if (path.endsWith("/account")) {
-    presenceData.details = "Viewing their account";
-  } else if (!isNaN(parseInt(path.charAt(path.length - 1), 10))) {
+  switch (path) {
+    case "/":
+      presenceData.details = "Viewing Clippy.gg.";
+      presenceData.state = "A private image uploader";
+      break;
+    case "/dashboard":
+      presenceData.details = "Viewing the dasboard";
+      const uploads = document.getElementsByClassName("statContent___2xKiA")[0];
+      presenceData.state = `${uploads ? uploads.innerHTML.replace(/\D/g, "") : "0"} files uploaded`
+      break;
+    case "/settings":
+      const intervalEditor = document.getElementsByClassName("ant-modal-wrap ant-modal-centered")[0] as HTMLElement,
+        embedEditor = document.getElementsByClassName("embedEditor")[0] as HTMLElement,
+        domainPicker = document.getElementsByClassName("ant-select-open")[0] as HTMLElement,
+        fakeUrlManager = document.getElementsByClassName("fakeUrlManager")[0] as HTMLElement;
+
+      if (embedEditor && embedEditor.parentElement.style.display !== "none") {
+        presenceData.state = "Editing their embed";
+      } else if (domainPicker) {
+        presenceData.state = "Choosing a domain";
+      } else if (intervalEditor && intervalEditor.style.display !== "none") {
+        presenceData.state = "Editing their auto-wipe interval";
+      } else if (fakeUrlManager && fakeUrlManager.parentElement.style.display !== "none") {
+        presenceData.state = "Editing their fake url";
+      } else {
+        presenceData.state = "Viewing their settings";
+      }
+      break;
+    case "/settings/domains":
+      const randomDomainPicker = document.getElementsByClassName("ant-select-open")[0] as HTMLElement;
+      if (randomDomainPicker) {
+        presenceData.state = `Adding a random domain`;
+      } else if (!document.body.textContent.includes("Loading")) {
+        presenceData.state = `Viewing ${document.body.textContent.match(/\d/g).join("").substring(1, 4)} domains`;
+      }
+      break;
+    case "/tools/upload":
+      presenceData.state = "Uploading a file";
+      presenceData.details = "Viewing the uploader";
+      break;
+    case "/tools/shorten":
+      presenceData.state = "Shortening a link";
+      presenceData.details = "Viewing the shortener";
+      break;
+    case "/account":
+      presenceData.state = "Viewing their account";
+      presenceData.details = "Clippy Account";
+      break;
+  }
+  if (!isNaN(parseInt(path.charAt(path.length - 1), 10))) {
     presenceData.state = `Viewing ${document.getElementsByClassName("title___aqmzM")[0].innerHTML}'s profile`;
   }
-
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
