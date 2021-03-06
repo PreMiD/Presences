051905d9e3e273cc2d297c320096e1b63cb93f4f
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-03-06 09:29:05 +0100

    Removed English Details => Added Dutch Details
    Removed English Details => Added Dutch Details

--- websites/K/Kijk/presence.ts
+++ websites/K/Kijk/presence.ts
@@ -15,22 +15,22 @@ presence.on("UpdateData", async () => {
     if (page === "/") {
       search = document.querySelector("#__next > div > div > div.SearchModalstyle__SearchModalStyle-sc-1h6b5wy-0.knmuVj > div.SearchModalstyle__SearchModalHeaderStyle-sc-1h6b5wy-1.kNvWZE > div > div:nth-child(2) > div.SearchModalstyle__SearchModalInputWrapperStyle-sc-1h6b5wy-5.iwOFOK > input");
       if (!search) {
-        presenceData.details = "Viewing the homepage";
+        presenceData.details = "Bekijkt de homepagina";
       } else if (search.value !== "") {
-        presenceData.details = "Searching For:";
+        presenceData.details = "Zoekt voor:";
         presenceData.state = search.value;
         presenceData.smallImageKey = "searching";
       } else { 
-        presenceData.details = "Viewing the homepage";
+        presenceData.details = "Bekijkt de homepagina";
       }
     }
     if (page === "/programmas" || page === "/programmas/") {
-      presenceData.details = "Discovering:";
+      presenceData.details = "Ontdekt:";
       presenceData.state = "Programma's";
      
     }
     if (page === "/films" || page === "/films/") {
-      presenceData.details = "Discovering:";
+      presenceData.details = "Ontdekt:";
       presenceData.state = "Films";
      
     }
@@ -83,7 +83,7 @@ presence.on("UpdateData", async () => {
       } else {
       title = document.querySelector("#__next > div > div > div.Mainstyle__MainContentStyle-l53ku6-1.kgsvtU > div > div.Layoutstyle__LightStyle-ecv4pg-0.Formatstyle__FormatLightStyleWrapperStyle-q2lvra-0.beAxHC > div.PromoImageHeadingstyle__PromoImageHeadingStyle-sc-13od09h-0.bwAzes > div.PromoImageHeadingstyle__PromoImageHeadingInnerWrapperStyle-sc-13od09h-2.klAHSa > div > div > div > div.FormatHeadingstyle__FormatHeadingImageWithTextWrapperStyle-sc-148hpqf-1.YJtQc > div.FormatHeadingstyle__FormatHeadingTextStyle-sc-148hpqf-2.glOvjd > h1");
      
-      presenceData.details = "Viewing:";
+      presenceData.details = "Bekijkt:";
       presenceData.state = title.textContent;
     }
   }
