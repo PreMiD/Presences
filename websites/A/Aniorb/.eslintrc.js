ce6485903b9b556d14477f432cd68ad28be5a04d
ICEPrey <40725969+ICEPrey@users.noreply.github.com>
2021-03-08 10:55:38 -0800

    Fix linebreak? ⚛️

---
+++ websites/A/Aniorb/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //extends: 'airbnb',
+    parser: 'babel-eslint',
+    env: {
+      browser: true
+    },
+    plugins: ['react'],
+      extends: ['eslint:recommended'],
+    rules: {
+  }
+    }
