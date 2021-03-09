895b8cd27d0bc2e859decb8ec4ce9b07c1bc77ec
erdem <erdemhacisalihoglu@hotmail.com>
2021-03-09 13:12:27 +0300

    Add files via upload

---
+++ websites/D/Diziroll/iframe.ts
@@ -0,0 +1,12 @@
+const iframe = new iFrame();
+
+iframe.on("UpdateData", () => {
+  const vidyo: HTMLVideoElement = document.querySelector("video");
+  if (vidyo && !isNaN(vidyo.duration) && !isNaN(vidyo.currentTime)) {
+    return iframe.send({
+      duration: vidyo.duration,
+      currentTime: vidyo.currentTime,
+      paused: vidyo.paused
+    });
+  }
+});
---
+++ websites/D/Diziroll/presence.ts
@@ -0,0 +1,82 @@
+const presence = new Presence({
+    clientId: "818550584994168934"
+});
+
+let stream: { duration: number; currentTime: number; paused: boolean };
+presence.on(
+  "iFrameData",
+  (data: { duration: number; currentTime: number; paused: boolean }) =>
+    (stream = data)
+);
+
+/**
+ * Get Timestamps
+ * @param {Number} videoTime Current video time seconds
+ * @param {Number} videoDuration Video duration seconds
+ */
+
+function getTimestamps(
+  videoTime: number,
+  videoDuration: number
+): Array<number> {
+  const startTime = Date.now(),
+    endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  return [Math.floor(startTime / 1000), endTime];
+}
+
+
+presence.on("UpdateData", async () => {
+    const path: string = document.location.pathname,
+    presenceData: PresenceData = {
+      largeImageKey: "diziroll"
+    };
+    if(path.startsWith("/arsiv")){
+    presenceData.details = "Bir sayfaya bakıyor:";
+    presenceData.state = "Arşiv";
+    presenceData.startTimestamp = Date.now();
+    }else if(path.startsWith("/listeler")){
+    presenceData.details = "Bir sayfaya bakıyor:";
+    presenceData.state = "Listeler";
+    presenceData.startTimestamp = Date.now(); 
+    }else if(path.startsWith("/hesabim")){
+        presenceData.details = "Bir sayfaya bakıyor:";
+        presenceData.state = "Hesabım";
+        presenceData.startTimestamp = Date.now();    
+    }else if(path == "/"){
+        presenceData.details = "Bir sayfaya bakıyor:";
+        presenceData.state = "Ana Sayfa";
+        presenceData.startTimestamp = Date.now();  
+    }else if(document.getElementById("archive-page")){
+        presenceData.details = "Bir dizi türünü inceliyor: ";
+        presenceData.state = document.querySelector("div.title").innerText;
+        presenceData.startTimestamp = Date.now();    
+    }else if(document.getElementById("series-page")){
+        presenceData.details = "Bir diziyi inceliyor:";
+        presenceData.state = document.querySelector("div.top > h1").innerText;
+        presenceData.startTimestamp = Date.now();  
+    }else if(document.getElementsByClassName("episode-detail").length > 0){
+        presenceData.details = document.getElementsByClassName("series-name")[0].title || "Bulunamadı";
+        presenceData.state = `${document.querySelector("div.select-season > a").innerText ? document.querySelector("div.select-season > a").innerText : "Bulunamadı"}- ${document.querySelector("div.select-episode > a").innerText ? document.querySelector("div.select-episode > a").innerText : "Bulunamadı"}`;
+        presenceData.buttons = [
+          {label: "İzle", url: document.location.href},
+          {label: "Diziyi Görüntüle", url: document.location.origin + "/" +document.location.pathname.split("/")[1]}
+        ];
+        if (stream.paused == true) {
+            console.log("test")
+            presenceData.smallImageKey = "pause";
+            presenceData.smallImageText = "Durduruldu";
+          } else {
+            const timestamps = getTimestamps(
+              Math.floor(stream?.currentTime),
+              Math.floor(stream?.duration)
+            );
+      
+            presenceData.smallImageKey = "play";
+            presenceData.smallImageText = "Oynatılıyor";
+            presenceData.startTimestamp = timestamps[0];
+            presenceData.endTimestamp = timestamps[1];
+          }
+
+    }
+    presence.setActivity(presenceData);
+})
