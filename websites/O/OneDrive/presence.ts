7a46fae2ea668e29bcc1813d0dd44857a4a47270
Tarık Coşkun <imtarikcoskun@gmail.com>
2021-03-09 18:59:40 +0300

    add onedrive

---
+++ websites/O/OneDrive/presence.ts
@@ -0,0 +1,27 @@
+const Office = new Presence({ clientId: "818822000176791553" });
+
+Office.on("UpdateData", async () => {
+  const
+    title = document.title.split('-'),
+    fileName = title[0],
+    fileType = title[1].replace('Microsoft', '').replace('Online', '').trim(),
+    presenceData: PresenceData = {
+      largeImageKey: "onedrive-logo",
+      startTimestamp: Math.round(Date.now() / 1000)
+    };
+
+  if (fileType == "OneDrive") {
+    presenceData.details = "Browsing a directory";
+    presenceData.state = fileName;
+
+    Office.setActivity(presenceData);
+  } else if (fileType == "Word" || fileType == "Excel" || fileType == "PowerPoint") {
+    presenceData.largeImageKey = `${fileType.toLowerCase()}-logo`;
+    presenceData.smallImageKey = "office-logo";
+    presenceData.smallImageText = "Microsoft Office Online";
+    presenceData.details = `Editing ${fileType.charAt(0) == "E"? "an": "a"} ${fileType} file`;
+    presenceData.state = fileName;
+
+    Office.setActivity(presenceData);
+  }
+});
