4f8de6873cc1041e7bbc821e4a673f61ebed0b66
OGMatrix <luki.welke@gmail.com>
2021-03-09 15:43:05 +0100

    Added missing semicolons

--- websites/M/MatrixBots/presence.ts
+++ websites/M/MatrixBots/presence.ts
@@ -2,36 +2,35 @@ const matrixPresence = new Presence({
   clientId: "748098665033498735"
 }),
   matrixBrowsing = Math.floor(Date.now() / 1000);
-let matrixSearch: HTMLInputElement, matrixTitle: Element;
 
 matrixPresence.on("UpdateData", async () => {
   const matrixPData: PresenceData = {
-    largeImageKey: "logo",
+    largeImageKey: "logo"
   },
     matrixPage = window.location.pathname;
 
   matrixPData.startTimestamp = matrixBrowsing;
 
   if (matrixPage == "/") {
-    matrixPData.details = "Browsing"
-    matrixPData.smallImageKey = "browsing"
-    matrixPData.smallImageText = "Browsing Bots"
+    matrixPData.details = "Browsing";
+    matrixPData.smallImageKey = "browsing";
+    matrixPData.smallImageText = "Browsing Bots";
   }
   if (matrixPage.includes("/bots")) {
-    const bot: any = document.evaluate("/html/body/div[4]/h2/text()", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+    const bot: string = document.evaluate("/html/body/div[4]/h2/text()", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
     matrixPData.buttons = [
       {
         label: "View Bot",
         url: document.URL
       }
-    ]
+    ];
     matrixPData.details = "Watching Bot:";
     matrixPData.state = bot;
-    matrixPData.smallImageText = "Browsing Bot"
-    matrixPData.smallImageKey = "browsing"
+    matrixPData.smallImageText = "Browsing Bot";
+    matrixPData.smallImageKey = "browsing";
   }
   if (matrixPage == "/me") {
-    const username: any = document.getElementsByTagName("h1")[0].innerHTML;
+    const username: string = document.getElementsByTagName("h1")[0].innerHTML;
     matrixPData.details = "Watching Profile:";
     matrixPData.state = username;
   }
@@ -47,4 +46,4 @@ matrixPresence.on("UpdateData", async () => {
   } else {
     matrixPresence.setActivity(matrixPData);
   }
-})
+});
