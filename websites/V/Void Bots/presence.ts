6e4bbc7d98fa1bc46280c0e061a82908b6a16b09
DanPlayz <12685647+DanPlayz0@users.noreply.github.com>
2021-03-09 13:40:49 -0800

    Debug Hlap

--- websites/V/Void Bots/presence.ts
+++ websites/V/Void Bots/presence.ts
@@ -1,10 +1,8 @@
 const presence = new Presence({
-  clientId: "765261270814949417",
-  injectOnComplete: true
+  clientId: "765261270814949417"
 });
 
-let oldLang: string = null,
-  strings: object = {}
+let oldLang: string = null
 
 function getMeta(metaName: string): string {
   metaName = "PreMiD_"+metaName;
@@ -28,6 +26,7 @@ presence.on("UpdateData", async () => {
   const incognito: boolean = await presence.getSetting("incognito"),
     showTimestamp: boolean = await presence.getSetting("showTimestamp"),
     showButtons: boolean = await presence.getSetting("buttons"),
+    debugMode: boolean = await presence.getSetting('debug'),
     newLang: string = await presence.getSetting("lang");
   if (!oldLang) { oldLang = newLang; } else if (oldLang !== newLang) { oldLang = newLang; }
 
@@ -35,7 +34,7 @@ presence.on("UpdateData", async () => {
     largeImageKey: "img_logo"
   };
 
-  if(showTimestamp == true) {
+  if (showTimestamp == true) {
     const browsingStamp = Math.floor(Date.now() / 1000);
     presenceData.startTimestamp = browsingStamp;
   } else {
@@ -44,8 +43,8 @@ presence.on("UpdateData", async () => {
   }
 
   let presenceButtons: { label: string; url: string }[] = [];
-  if(incognito === false) {
-    if(['voidbots.net', 'beta.voidbots.net'].includes(window.location.hostname)) {
+  if (incognito == false) {
+    if (['voidbots.net', 'beta.voidbots.net'].includes(window.location.hostname)) {
       let strings = await translateStrings({
         details: getMeta('details'),
         state: getMeta('state'),
@@ -59,18 +58,29 @@ presence.on("UpdateData", async () => {
       if (hasMeta('smallImageText')) presenceData.smallImageText = strings.smallImageText || getMeta('smallImageText');
       // if (hasMeta('button_1_label') && hasMeta('button_1_url')) presenceButtons.push({label: strings.button1label || getMeta('button_1_label'), url: getMeta('button_1_url') })
       // if (hasMeta('button_2_label') && hasMeta('button_2_url')) presenceButtons.push({label: strings.button2label || getMeta('button_2_label'), url: getMeta('button_1_url') })
-    } else if (window.location.hostname === 'docs.voidbots.net') {
+    } else if (window.location.hostname == 'docs.voidbots.net') {
       presenceData.details = "Viewing the docs";
       presenceData.state = document.querySelector("title").textContent || "Home";
     }
-  } else if(incognito && window.location.hostname.includes('voidbots.net')) {
+  } else {
     presenceData.details = 'Visiting';
     presenceButtons = [ { label: 'Website', url: 'https://voidbots.net/'}, { label: 'Discord', url: 'https://voidbots.net/join'} ]
   }
-  if(showButtons === true) presenceData.buttons = presenceButtons;
 
-  console.log(presenceData);
-  // presence.setActivity(presenceData);
+  if (showButtons == true) presenceData.buttons = presenceButtons;
+
+  if (debugMode) {
+    console.log('------------------')
+    console.log(`Details (${hasMeta('details')}): ${getMeta('details')}`)
+    console.log(`State (${hasMeta('state')}): ${getMeta('state')}`)
+    console.log(`smallImageKey (${hasMeta('smallImageKey')}): ${getMeta('smallImageKey')}`)
+    console.log(`smallImageText (${hasMeta('smallImageText')}): ${getMeta('smallImageText')}`)
+    console.log(`button_1_label (${hasMeta('button_1_label')}): ${getMeta('button_1_label')}`)
+    console.log(`button_2_label (${hasMeta('button_2_label')}): ${getMeta('button_2_label')}`)
+    console.log(`Buttons Array:`, presenceButtons)
+    console.log('Presence Data: ', presenceData)
+    console.log(`Set Presence? ${presenceData.details == null ? 'false' : 'true'}`)
+  }
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
