ae3855b51980f0e1c98e83757c771bd2fcbffabc
DanPlayz <12685647+DanPlayz0@users.noreply.github.com>
2021-03-09 16:41:36 -0800

    Merged consts

--- websites/V/Void Bots/presence.ts
+++ websites/V/Void Bots/presence.ts
@@ -1,8 +1,5 @@
-const presence = new Presence({
-  clientId: "765261270814949417"
-});
-
-const browsingStamp = Math.floor(Date.now() / 1000);
+const presence = new Presence({ clientId: "765261270814949417" }), 
+  browsingStamp = Math.floor(Date.now() / 1000);
 let oldLang: string = null,
   strings: any = {}
 
