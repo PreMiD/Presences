ddd0d267a0f90223da14020d8be73e1f7504fca8
DanPlayz <12685647+DanPlayz0@users.noreply.github.com>
2021-03-09 16:37:40 -0800

    Translate Key Name: smallImgText -> smallImageText

--- websites/V/Void Bots/presence.ts
+++ websites/V/Void Bots/presence.ts
@@ -37,7 +37,7 @@ presence.on("UpdateData", async () => {
       privacyVisit: 'general.browsing',
       details: getMeta('details'),
       state: getMeta('state'),
-      smallImgText: getMeta('smallImageText'),
+      smallImageText: getMeta('smallImageText'),
       button1: getMeta('button_1_Label'),
       button2: getMeta('button_2_Label')
     })
@@ -54,7 +54,7 @@ presence.on("UpdateData", async () => {
       if (hasMeta('details')) presenceData.details = strings.details || getMeta('details');
       if (hasMeta('state')) presenceData.state = strings.state || getMeta('state');
       if (hasMeta('smallImageKey')) presenceData.smallImageKey = getMeta('smallImageKey');
-      if (hasMeta('smallImageText')) presenceData.smallImageText = strings.smallImgText || getMeta('smallImageText');
+      if (hasMeta('smallImageText')) presenceData.smallImageText = strings.smallImageText || getMeta('smallImageText');
       if (showButtons === true) {
         if ((hasMeta('button_1_Label') && hasMeta('button_1_Url')) || (hasMeta('button_2_Label') && hasMeta('button_2_Url'))) presenceData.buttons = [];
         if (hasMeta('button_1_Label') && hasMeta('button_1_Url')) presenceData.buttons.push({label: getMeta('button_1_Label'), url: getMeta('button_1_Url') })
