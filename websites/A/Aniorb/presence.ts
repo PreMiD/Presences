1283a877db27464e3dd6be537abc1311ce2768e1
ICEPrey <abuwswork@gmail.com>
2021-03-09 18:06:42 -0800

    Saving files before refreshing line endings

--- websites/A/Aniorb/presence.ts
+++ websites/A/Aniorb/presence.ts
@@ -1,16 +1,16 @@
 const presence = new Presence({
-  clientId: "776312991939428373"
+  clientId: "776312991939428373",
 }),
 strings = presence.getStrings({
   play: "presence.playback.playing",
   pause: "presence.playback.paused",
-  browsing: "presence.activity.browsing"
+  browsing: "presence.activity.browsing",
 });
 let video = {
 video: false,
 duration: 0,
 currentTime: 0,
-paused: true
+paused: true,
 };
 presence.on(
 "iFrameData",
@@ -25,9 +25,9 @@ presence.on(
 );
 presence.on("UpdateData", async () => {
 const presenceData: PresenceData = {
-  largeImageKey: "aniorb"
+  largeImageKey: "aniorb",
 };
-   if (document.location.pathname.includes("/genre")) {
+if (document.location.pathname.includes("/genre")) {
   presenceData.details = "Browsing Through";
   presenceData.state = "The Genres";
   presenceData.smallImageKey = "reading";
@@ -50,44 +50,59 @@ const presenceData: PresenceData = {
   presenceData.details = "Browsing through";
   presenceData.state = "Anime Details";
   presenceData.smallImageKey = "reading";
-}
-else if (document.querySelector("#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.w-full.py-4.uppercase.flex.flex-col.items-start.lg\\:items-start > span").textContent.toUpperCase() !== null) {
+} else if (
+  document
+    .querySelector(
+      "#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.w-full.py-4.uppercase.flex.flex-col.items-start.lg\\:items-start > span"
+    )
+    .textContent.toUpperCase() !== null
+) {
   presenceData.details = "Watching:";
-  presenceData.state = document.querySelector("#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.w-full.py-4.uppercase.flex.flex-col.items-start.lg\\:items-start > span").textContent.toUpperCase();
+  presenceData.state = document
+    .querySelector(
+      "#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.w-full.py-4.uppercase.flex.flex-col.items-start.lg\\:items-start > span"
+    )
+    .textContent.toUpperCase();
   presenceData.smallImageKey = "watching";
-}
-else if (
-  document.querySelector("#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.w-full.py-4.uppercase.flex.flex-col.items-start.lg\\:items-start > span").textContent.toUpperCase() !== null &&
+} else if (
+  document
+    .querySelector(
+      "#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.w-full.py-4.uppercase.flex.flex-col.items-start.lg\\:items-start > span"
+    )
+    .textContent.toUpperCase() !== null &&
   document.querySelector("video") !== null &&
   video.video
-) if (video !== null && !isNaN(video.duration)) {
-  const series =   document.querySelector("#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.w-full.py-4.uppercase.flex.flex-col.items-start.lg\\:items-start > span"),
-    seriesTitle = series.textContent.toUpperCase(),
-    episode = document.querySelector("#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.flex.w-full.justify-between.items-end > span").textContent,
-    timestamps = presence.getTimestampsfromMedia(video);
-  presenceData.smallImageKey = video.paused ? "pause" : "play";
-  presenceData.smallImageText = video.paused
-    ? (await strings).pause
-    : (await strings).play;
-  presenceData.startTimestamp = timestamps[0];
-  presenceData.endTimestamp = timestamps[1];
-  if (!video.paused) {
-    presence.setTrayTitle(seriesTitle);
+)
+  if (video !== null && !isNaN(video.duration)) {
+    const series = document.querySelector(
+        "#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.w-full.py-4.uppercase.flex.flex-col.items-start.lg\\:items-start > span"
+      ),
+      seriesTitle = series.textContent.toUpperCase(),
+      episode = document.querySelector(
+        "#__next > div > div.flex.justify-between > div.w-full.justify-center.items-center.min-h-screen.lg\\:h-full.lg\\:w-10\\/12 > div > div.flex.flex-col.pb-2.xl\\:w-player.justify-between.items-center.w-full.text-white.my-4 > div.flex.w-full.justify-between.items-end > span"
+      ).textContent,
+      timestamps = presence.getTimestampsfromMedia(video);
+    presenceData.smallImageKey = video.paused ? "pause" : "play";
+    presenceData.smallImageText = video.paused
+      ? (await strings).pause
+      : (await strings).play;
+    presenceData.startTimestamp = timestamps[0];
+    presenceData.endTimestamp = timestamps[1];
+    if (!video.paused) presence.setTrayTitle(seriesTitle);
     presenceData.details = seriesTitle;
     presenceData.state = episode;
-  }
-  if (video.paused) {
-    delete presenceData.startTimestamp;
-    delete presenceData.endTimestamp;
-  }
-    presenceData.buttons = [
-    {
-      label: "Watch Episode",
-      url: document.baseURI
+    if (video.paused) {
+      delete presenceData.startTimestamp;
+      delete presenceData.endTimestamp;
     }
-  ];
-  presence.setActivity(presenceData, !video.paused);
-}
+    presenceData.buttons = [
+      {
+        label: "Watch Episode",
+        url: document.baseURI,
+      },
+    ];
+    presence.setActivity(presenceData, !video.paused);
+  }
 if (presenceData.details == null) {
   presenceData.details = (await strings).browsing;
   presenceData.smallImageKey = "reading";
