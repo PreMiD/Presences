ff84a2ade858bc845b2dbd8a0182b92be0a149af
Kiara <frida.shivaa@gmail.com>
2021-03-15 18:49:52 +0100

    Update presence.ts

--- websites/M/MoodlePresence/presence.ts
+++ websites/M/MoodlePresence/presence.ts
@@ -1,54 +1,67 @@
 const presence = new Presence({
-    clientId: "821059536768335902" //The client ID of the Application created at https://discordapp.com/developers/applications
+    clientId: "821059536768335902"
 }),
     timebrowsed = Math.floor(Date.now() / 1000);
 
-console.log('qwq #1')
 
-/*
-
-function myOutsideHeavyLiftingFunction(){
-    //Grab and process all your data here
-
-    // element grabs //
-    // api calls //
-    // variable sets //
+function query(q: string) {
+    return document.querySelector(q);
 }
 
-setInterval(myOutsideHeavyLiftingFunction, 10000);
-//Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
-
-*/
-
 presence.on("UpdateData", async () => {
-    console.log('qwq', document.location.pathname)
-    //const moodleHost = document.location.hostname;
-    const pathName = document.location.pathname
+    const pathName = document.location.pathname,
+        pages = pathName.split('/').filter(p => p);
 
     const presenceData: PresenceData = {
         largeImageKey:
-            "smallimage" /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/,
+            "largeimage",
         smallImageKey:
-            "smallimage" /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/,
-        smallImageText: 'reading I guess', //The text which is displayed when hovering over the small image
-        details: pathName, //The upper section of the presence text
-        //state: "Reading something . . .", //The lower section of the presence text
+            "smallimage",
+        smallImageText: 'reading I guess',
+        details: pathName,
+        state: "Reading something . . .",
         startTimestamp: timebrowsed,
-    }; /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
+    };
 
-    if (pathName.includes('course')) {
-        const courseName = document.getElementById("page-header-headings");
-        console.log(courseName)
+    if (pages[0] === 'course') {
         presenceData.details = 'Viewing Course'
+        presenceData.state = query(".page-header-headings").textContent;
+    }
+    if (pages[0] === 'my') {
+        presenceData.details = 'Viewing Dashboard'
+    }
+    if (pages.length === 0) {
+        presenceData.details = 'Viewing Landing Page'
+    }
+    if (pages[0] === 'calendar') {
+        presenceData.details = 'Viewing Calender'
+    }
+    if (pages[0] === 'user' && pages[1] === 'files.php') {
+        presenceData.details = 'Viewing Files'
+        presenceData.state = query(".fp-iconview").children.length + ' Files uploaded'
+    }
+    if (pages[0] === 'user' && pages[1].includes('profile.php')) {
+        presenceData.details = 'Viewing Profile'
+    }
+    if (pages[0] === 'user' && pages[1].includes('preferences.php')) {
+        presenceData.details = 'Editing Profile'
+    }
+    if (pages[0] === 'message') {
+        presenceData.details = 'Viewing Messages'
+        presenceData.state = (query('.count-container').textContent || 'No') + ' unread Messages'
+    }
+    if (pages[0] === 'grade' && pages[1] === 'report' && pages[2] === 'overview') {
+        presenceData.details = 'Viewing Grade Reports'
+    }
+    if (pages[0] === 'mod' && pages[1] === 'assign' && pages[2].includes('view.php')) {
+        presenceData.details = 'Submitting Assignment'
+        presenceData.state = query(".page-header-headings").textContent;
     }
 
-
     if (presenceData.details == null) {
-        //This will fire if you do not set presence details
-        presence.setTrayTitle(); //Clears the tray title for mac users
-        presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+        presence.setTrayTitle();
+        presence.setActivity();
     } else {
-        //This will fire if you set presence details
-        presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+        presence.setActivity(presenceData);
     }
 });
