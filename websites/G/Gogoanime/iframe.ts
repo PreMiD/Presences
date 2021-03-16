d7284bde460c621d4691e6e3a39da5c578f39f88
SeMiD <contact@premid.app>
2021-03-16 00:40:02 +0000

    ♻️ Refactoring
    Signed-off-by: SeMiD <contact@premid.app>

--- websites/G/Gogoanime/iframe.ts
+++ websites/G/Gogoanime/iframe.ts
@@ -1,8 +1,9 @@
 const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
-  const videoInfos: HTMLVideoElement = document.querySelector("div.jw-media.jw-reset > video") ??
-                                       document.querySelector("div.html5-video-container > video"); // youtube embed
+  const videoInfos: HTMLVideoElement =
+    document.querySelector("div.jw-media.jw-reset > video") ??
+    document.querySelector("div.html5-video-container > video"); // youtube embed
   if (videoInfos && !isNaN(videoInfos.duration)) {
     iframe.send({
       duration: videoInfos.duration,
