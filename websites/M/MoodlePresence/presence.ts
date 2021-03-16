b596b83af69960df328075b7347e93b733307072
Kiara <frida.shivaa@gmail.com>
2021-03-16 18:16:46 +0100

    Fixing Review Stuffu
    A) Removed useless premid info line 13, 14
    B) removed uneeded function
    C) added else if
    D) Added more tags
    E) added official description
    F) added setting for display coursename

--- websites/M/MoodlePresence/presence.ts
+++ websites/M/MoodlePresence/presence.ts
@@ -3,54 +3,51 @@ const presence = new Presence({
 }),
     timebrowsed = Math.floor(Date.now() / 1000);
 
-function query(q: string) {
-    return document.querySelector(q);
-}
 presence.on("UpdateData", async () => {
     const pathName = document.location.pathname,
         pages = pathName.split('/').filter(p => p),
         data: PresenceData = {
             largeImageKey: "largeimage",
-            smallImageText: 'reading I guess',
-            details: pathName,
+            smallImageText: 'Reading . . .',
             startTimestamp: timebrowsed
         };
+    var displayCourse = await presence.getSetting('displayCourse')
 
     if (pages[0] === 'course') {
         data.details = 'Viewing Course';
-        data.state = query(".page-header-headings").textContent;
+        if (displayCourse) data.state = document.querySelector(".page-header-headings").textContent;
     }
-    if (pages[0] === 'my') {
+    else if (pages[0] === 'my') {
         data.details = 'Viewing Dashboard';
     }
-    if (pages.length === 0) {
+    else if (pages.length === 0) {
         data.details = 'Viewing Landing Page';
     }
-    if (pages[0] === 'calendar') {
+    else if (pages[0] === 'calendar') {
         data.details = 'Viewing Calender';
     }
-    if (pages[0] === 'user' && pages[1] === 'files.php') {
+    else if (pages[0] === 'user' && pages[1] === 'files.php') {
         data.details = 'Viewing Files';
-        data.state = query(".fp-iconview").children.length + ' Files uploaded';
+        data.state = document.querySelector(".fp-iconview").children.length + ' Files uploaded';
     }
-    if (pages[0] === 'user' && pages[1].includes('profile.php')) {
+    else if (pages[0] === 'user' && pages[1].includes('profile.php')) {
         data.details = 'Viewing Profile';
     }
-    if (pages[0] === 'user' && pages[1].includes('preferences.php')) {
+    else if (pages[0] === 'user' && pages[1].includes('preferences.php')) {
         data.details = 'Editing Profile';
     }
-    if (pages[0] === 'message') {
+    else if (pages[0] === 'message') {
         data.details = 'Viewing Messages';
-        data.state = (query('.count-container').textContent || 'No') + ' unread Messages';
+        data.state = (document.querySelector('.count-container').textContent || 'No') + ' unread Messages';
     }
-    if (pages[0] === 'grade' && pages[1] === 'report' && pages[2] === 'overview') {
+    else if (pages[0] === 'grade' && pages[1] === 'report' && pages[2] === 'overview') {
         data.details = 'Viewing Grade Reports';
     }
-    if (pages[0] === 'mod' && pages[1] === 'assign' && pages[2].includes('view.php')) {
+    else if (pages[0] === 'mod' && pages[1] === 'assign' && pages[2].includes('view.php')) {
         data.details = 'Submitting Assignment';
-        data.state = query(".page-header-headings").textContent;
+        if (displayCourse) data.state = document.querySelector(".page-header-headings").textContent;
     }
-    if (pages[0] === 'login' && pages[1].includes('index.php')) {
+    else if (pages[0] === 'login' && pages[1].includes('index.php')) {
         data.details = 'Logging In . . .';
     }
 
