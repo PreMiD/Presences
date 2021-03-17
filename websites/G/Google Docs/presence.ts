746283d30bac2e39adb22a55e09166b36b38a16d
callumok2004 <callumokane123@gmail.com>
2021-03-17 15:36:56 +0000

    Refactoring + New Presence

--- websites/G/Google Docs/presence.ts
+++ websites/G/Google Docs/presence.ts
@@ -35,66 +35,56 @@ presence.on("UpdateData", async () => {
     privacy = await presence.getSetting("privacy"),
     newLang = await presence.getSetting("lang");
 
-  if (!oldLang) {
-    oldLang = newLang;
-  } else if (oldLang !== newLang) {
+  if (!oldLang) oldLang = newLang;
+  else if (oldLang !== newLang) {
     oldLang = newLang;
     strings = getStrings();
   }
 
+  title = document.title
+    .replace("- Google Docs", "")
+    .replace("- Google Forms", "")
+    .replace("- Google Sheets", "")
+    .replace("- Google Slides", "")
+
   if (document.location.pathname.includes("/document")) {
     presenceData.largeImageKey = "docslogo";
-    title = document.title.replace("- Google Docs", "");
-    if (document.location.pathname.includes("/edit")) {
+    if (document.location.pathname.includes("/edit"))
       presenceData.details = (await strings).editingDoc;
-      if (!privacy) presenceData.state = title;
-    } else if (document.location.pathname.includes("/document/u/")) {
+    else if (document.location.pathname.includes("/document/u/"))
       presenceData.details = (await strings).browsingDoc;
-    } else {
+    else 
       presenceData.details = (await strings).viewingDoc;
-      if (!privacy) presenceData.state = title;
-    }
   } else if (document.location.pathname.includes("/forms/")) {
-    presenceData.largeImageKey = "formslogo";
-    title = document.title.replace("- Google Forms", "");
-    if (document.location.pathname.includes("/edit")) {
+      presenceData.largeImageKey = "formslogo";
+    if (document.location.pathname.includes("/edit"))
       presenceData.details = (await strings).editingForm;
-      if (!privacy) presenceData.state = title;
-    } else if (document.location.pathname.includes("/forms/u/")) {
+    else if (document.location.pathname.includes("/forms/u/"))
       presenceData.details = (await strings).browsingForm;
-    } else {
+    else
       presenceData.details = (await strings).viewingForm;
-      if (!privacy) presenceData.state = title;
-    }
   } else if (document.location.pathname.includes("/spreadsheets")) {
-    presenceData.largeImageKey = "sheetslogo";
-    title = document.title.replace("- Google Sheets", "");
-    if (document.location.pathname.includes("/edit")) {
+      presenceData.largeImageKey = "sheetslogo";
+    if (document.location.pathname.includes("/edit"))
       presenceData.details = (await strings).editingSheet;
-      if (!privacy) presenceData.state = title;
-    } else if (document.location.pathname.includes("/spreadsheets/u/")) {
+    else if (document.location.pathname.includes("/spreadsheets/u/"))
       presenceData.details = (await strings).browsingSheet;
-    } else {
+    else 
       presenceData.details = (await strings).viewingSheet;
-      if (!privacy) presenceData.state = title;
-    }
   } else if (document.location.pathname.includes("/presentation/")) {
-    presenceData.largeImageKey = "slideslogo";
-    title = document.title.replace("- Google Slides", "");
-    if (document.location.pathname.includes("/edit")) {
+      presenceData.largeImageKey = "slideslogo";
+    if (document.location.pathname.includes("/edit"))
       presenceData.details = (await strings).editingPresentation;
-      if (!privacy) presenceData.state = title;
-    } else if (document.location.pathname.includes("/presentation/u/")) {
+    else if (document.location.pathname.includes("/presentation/u/"))
       presenceData.details = (await strings).browsingPresentation;
-    } else {
+    else
       presenceData.details = (await strings).vieiwngPresentation;
-      if (!privacy) presenceData.state = title;
-    }
   }
-  if (presenceData.details == null) {
+
+  if (!privacy) presenceData.state = title;
+
+  if (!presenceData.details) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else {
-    presence.setActivity(presenceData);
-  }
+  } else presence.setActivity(presenceData);
 });
