0effc9d683a2d994fee2b94225d6f9576c8d34d7
IF64 <62923115+IF64@users.noreply.github.com>
2021-03-22 11:15:49 -0400

    Submit required changes for review

--- websites/P/Paramount+/presence.ts
+++ websites/P/Paramount+/presence.ts
@@ -1,8 +1,8 @@
 const presence = new Presence({
   clientId: "821433038335377418"
 });
-(async function () {
-  const strings = await presence.getStrings({
+
+  const strings = presence.getStrings({
     play: "presence.playback.playing",
     pause: "presence.playback.paused",
     live: "presence.activity.live",
@@ -17,17 +17,13 @@ const presence = new Presence({
     elapsed: number = undefined,
     oldUrl: string = undefined;
 
-  presence.on("UpdateData", () => {
+  presence.on("UpdateData", async () => {
 
     vidArea = document.querySelector(".video__player-area");
     let video: HTMLVideoElement = null,
-      details = undefined,
-      state = undefined,
-      smallImageKey = undefined,
-      smallImageText = undefined,
-      startTimestamp = undefined,
-      endTimestamp = undefined;
-
+    PresenceData: PresenceData = { 
+      largeImageKey: "logo",
+    }
     const href = window.location.href,
       path = window.location.pathname;
 
@@ -36,19 +32,19 @@ const presence = new Presence({
       elapsed = Math.floor(Date.now() / 1000);
     }
 
-    state = undefined;
+    PresenceData.state = undefined;
 
     if (path.includes("/home")) {
 
-      details = "Browsing:";
-      state = "Home Page";
-      startTimestamp = elapsed;
+      PresenceData.details = "Browsing:";
+      PresenceData.state = "Home Page";
+      PresenceData.startTimestamp = elapsed;
 
     } else if (path.includes("/shows") && document.querySelector(".subnav__items--tuneInfo") == null) {
 
-      details = "Browsing:";
-      state = "Viewing Shows";
-      startTimestamp = elapsed;
+      PresenceData.details = "Browsing:";
+      PresenceData.state = "Viewing Shows";
+      PresenceData.startTimestamp = elapsed;
 
     } else if (!vidArea && path.includes("/shows")) {
 
@@ -56,81 +52,81 @@ const presence = new Presence({
 
       if (title) {
         
-        state = title;
-        details = "Viewing Series:";
+        PresenceData.state = title;
+        PresenceData.details = "Viewing Series:";
       
       }
     } else if (!vidArea && path.includes("/movies")) {
 
-      state = "Viewing Movies";
-      details = "Browsing:";
-      startTimestamp = elapsed;
+      PresenceData.state = "Viewing Movies";
+      PresenceData.details = "Browsing:";
+      PresenceData.startTimestamp = elapsed;
 
     } else if (path.includes("/search")) {
       
-      details = "Searching";
-      smallImageKey = "search";
-      smallImageText = strings.search;
+      PresenceData.details = "Searching";
+      PresenceData.smallImageKey = "search";
+      PresenceData.smallImageText = (await strings).search;
     
     } else if (path.includes("/account")) {
       
-      details = "Viewing Account";
+      PresenceData.details = "Viewing Account";
 
       if (path.includes("/signin")) {
-        details = null;
+        PresenceData.details = null;
       }
 
     } else if (path.includes("/user-profile/whos-watching")) {
       
-      details = "User Profiles";
-      state = "Selecting User...";
+      PresenceData.details = "User Profiles";
+      PresenceData.state = "Selecting User...";
       
     } else if (path.includes("/news/") && !path.includes("video")) {
 
-      details = "Browsing News";
-      state = "CBSN";
-      startTimestamp = elapsed;
+      PresenceData.details = "Browsing News";
+      PresenceData.state = "CBSN";
+      PresenceData.startTimestamp = elapsed;
 
     } else if (path.includes("/brands")) {
 
-      details = "Browsing Brands:";
-      state = "Selecting Brand...";
+      PresenceData.details = "Browsing Brands:";
+      PresenceData.state = "Selecting Brand...";
 
       if (path.includes("/cbs/")) {
 
-        state = "CBS";
-        startTimestamp = elapsed;
-        smallImageKey = "cbs";
+        PresenceData.state = "CBS";
+        PresenceData.startTimestamp = elapsed;
+        PresenceData.smallImageKey = "cbs";
 
       } else if (path.includes("/bet/")) {
 
-        state = "BET";
-        startTimestamp = elapsed;
-        smallImageKey = "bet";
+        PresenceData.state = "BET";
+        PresenceData.startTimestamp = elapsed;
+        PresenceData.smallImageKey = "bet";
 
       } else if (path.includes("/comedy-central/")) {
 
-        state = "Comedy Central";
-        startTimestamp = elapsed;
-        smallImageKey = "comedycentral";
+        PresenceData.state = "Comedy Central";
+        PresenceData.startTimestamp = elapsed;
+        PresenceData.smallImageKey = "comedycentral";
 
       } else if (path.includes("/mtv/")) {
 
-        state = "MTV";
-        startTimestamp = elapsed;
-        smallImageKey = "mtv";
+        PresenceData.state = "MTV";
+        PresenceData.startTimestamp = elapsed;
+        PresenceData.smallImageKey = "mtv";
 
       } else if (path.includes("/nickelodeon/")) {
 
-        state = "Nickelodeon";
-        startTimestamp = elapsed;
-        smallImageKey = "nickelodeon";
+        PresenceData.state = "Nickelodeon";
+        PresenceData.startTimestamp = elapsed;
+        PresenceData.smallImageKey = "nickelodeon";
 
       } else if (path.includes("/smithsonian-channel/")) {
 
-        state = "Smithsonian Channel";
-        startTimestamp = elapsed;
-        smallImageKey = "smithsonian";
+        PresenceData.state = "Smithsonian Channel";
+        PresenceData.startTimestamp = elapsed;
+        PresenceData.smallImageKey = "smithsonian";
       }
 
     } else if (path.includes("/live")) {
@@ -138,27 +134,27 @@ const presence = new Presence({
       vidMdTl = document.getElementsByClassName("video__player-area")[0].querySelector("h1");
       const liveTitle = document.getElementsByClassName("video__player-area")[0].querySelector("span.subTitle").textContent;
 
-      smallImageKey = "live";
-      smallImageText = strings.live;
-      startTimestamp = elapsed;
+      PresenceData.smallImageKey = "live";
+      PresenceData.smallImageText = (await strings).live;
+      PresenceData.startTimestamp = elapsed;
 
-      details = "Watching WCBS Live";
-      state = liveTitle;
+      PresenceData.details = "Watching WCBS Live";
+      PresenceData.state = liveTitle;
 
       if (path.includes("/cbsn/")) {
 
-        details = "Watching CBSN News";
-        state = vidMdTl.textContent;
+        PresenceData.details = "Watching CBSN News";
+        PresenceData.state = vidMdTl.textContent;
 
       } else if (path.includes("/sports/")) {
 
-        details = "Watching CBS Sports";
-        state = vidMdTl.textContent;
+        PresenceData.details = "Watching CBS Sports";
+        PresenceData.state = vidMdTl.textContent;
 
       } else if (path.includes("/etl/")) {
 
-        details = "Watching ET Live";
-        state = vidMdTl.textContent;
+        PresenceData.details = "Watching ET Live";
+        PresenceData.state = vidMdTl.textContent;
 
       }
 
@@ -187,49 +183,40 @@ const presence = new Presence({
           live = timestamps[1] === Infinity;
         
         if (movTitle) {
-          state = movTitle;
-          details = "Watching Movie:";
+          PresenceData.state = movTitle;
+          PresenceData.details = "Watching Movie:";
         }
 
         if (title) {
-          details = title;
+          PresenceData.details = title;
         }
 
         if (content) {
-          state = content;
+          PresenceData.state = content;
         }
 
         if (path.includes("/news/")) {
-          details = "Watching News Content";
-          state = jsonData.partOfSeries.name + ": " + jsonData.name;
+          PresenceData.details = "Watching News Content";
+          PresenceData.state = jsonData.partOfSeries.name + ": " + jsonData.name;
         }
 
-        smallImageKey = live ? "live" : video.paused ? "pause" : "play";
-        smallImageText = live ?
-          strings.live :
+        PresenceData.smallImageKey = live ? "live" : video.paused ? "pause" : "play";
+        PresenceData.smallImageText = live ?
+          (await strings).live :
           video.paused ?
-          strings.pause :
-          strings.play;
-        startTimestamp = live ? elapsed : timestamps[0];
-        endTimestamp = live ? undefined : timestamps[1];
+          (await strings).pause :
+          (await strings).play;
+        PresenceData.startTimestamp = live ? elapsed : timestamps[0];
+        PresenceData.endTimestamp = live ? undefined : timestamps[1];
 
         if (video.paused) {
-          startTimestamp = undefined;
-          endTimestamp = undefined;
+          PresenceData.startTimestamp = undefined;
+          PresenceData.endTimestamp = undefined;
         }
       }
     }
 
-    const data: PresenceData = {
-      details: details,
-      state: state,
-      largeImageKey: "logo",
-      smallImageKey: smallImageKey,
-      smallImageText: smallImageText,
-      startTimestamp: startTimestamp,
-      endTimestamp: endTimestamp
-    };
-    presence.setActivity(data, video ? !video.paused : true);
-    presence.setTrayTitle(details);
+    presence.setActivity(PresenceData, video ? !video.paused : true);
+    presence.setTrayTitle(PresenceData.details);
   });
-})();
+;
