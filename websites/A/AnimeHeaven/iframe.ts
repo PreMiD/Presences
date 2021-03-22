b53bbfe431d2dc09142284e8b006cf3bc3017536
Gurrman375 <james.gurr375@gmail.com>
2021-03-21 17:45:01 -0700

    Added Anime Heaven

---
+++ websites/A/AnimeHeaven/iframe.ts
@@ -0,0 +1,23 @@
+const iframe = new iFrame();
+iframe.on("UpdateData", async () => {
+  let video;
+  if (
+    document.querySelector(
+        "#player_container > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video"
+    ) !== null
+  ) {
+    video = document.querySelector(
+        "#player_container > div.jw-wrapper.jw-reset > div.jw-media.jw-reset > video"
+    ) as HTMLVideoElement;
+    if (video != undefined && !isNaN(video.duration)) {
+      iframe.send({
+        iframe_video: {
+          iFrameVideo: true,
+          currTime: video.currentTime,
+          dur: video.duration,
+          paused: video.paused
+        }
+      });
+    }
+  }
+});
