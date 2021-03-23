facc212cb61b316c0e1a28ee3152bed3f1ca075c
NinjaLabs-Dev <fraser@ninjalabs.co.uk>
2021-03-23 12:44:51 +0000

    Added buttons & timestamp to TFM

--- websites/T/TruckersFM/presence.ts
+++ websites/T/TruckersFM/presence.ts
@@ -1,16 +1,34 @@
 const presence = new Presence({
   clientId: "640914619082211338"
 });
+let browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
   const presenceData: PresenceData = {
-    largeImageKey: "tfmlogo"
+    largeImageKey: "tfmlogo",
+	startTimestamp: browsingStamp
   };
 
   presenceData.details = `${document.querySelector(".player-artist-text").textContent} - ${document.querySelector(".player-title-text").textContent}`;
   presenceData.state = document.querySelector(".live-name").textContent
     ? document.querySelector(".live-name").textContent
     : 'AutoDJ';
+  console.log(presenceData)	
+	
+  presenceData.buttons = [
+	{
+		label: 'Tune into TFM',
+		url: 'https://truckers.fm'
+	}
+  ]		
+	
+  const spotifyUrl = document.querySelector(".player-artist-text a").getAttribute('href');	
+  if(spotifyUrl) {
+	presenceData.buttons.push({
+		label: 'Listen on Spotify',
+		url: spotifyUrl
+	})
+  }	
 
   presence.setActivity(presenceData);
   presence.setTrayTitle();
