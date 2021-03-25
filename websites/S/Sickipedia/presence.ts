6815a3f312510d6475dc7ae86670939b7c23c6d6
callumok2004 <callumokane123@gmail.com>
2021-03-25 20:52:27 +0000

    Finish Sickipedia

--- websites/S/Sickipedia/presence.ts
+++ websites/S/Sickipedia/presence.ts
@@ -3,45 +3,72 @@ const presence = new Presence({
   }), data: {
     presenceData?: PresenceData;
     privacyMode?: boolean;
-    dataSent?: boolean;
   } = {};  
 
 presence.on("UpdateData", async () => {
-  console.log(1)
-
   data.privacyMode = await presence.getSetting("privacy");
   data.presenceData = {
     largeImageKey: "logo"
   };
 
-  if(await matchPage("/"))
+  if(matchPage("/"))
     updateData("Viewing page:", "Homepage", false);
-  else if(await matchPage("/pics"))
-    updateData("Viewing page:", "Memes and Pictures")
-  else if(await matchPage("/LeaderBoard"))
-    updateData("Viewing page:", "Leaderboard")
-  else if(await matchPage("/Upload/joke"))
-    updateData("Viewing page", "Upload (Joke)")
-  else if(await matchPage("/upload/picture"))
-    updateData("Viewing page", "Upload (Picture)")
-  else if(await matchPage("/manage/profile"))
-    updateData("Viewing page", "Manage Profile")
-  else if(await matchPage("/message"))
+  else if(matchPage("/pics"))
+    updateData("Viewing page:", "Memes and Pictures");
+  else if(matchPage("/LeaderBoard"))
+    updateData("Viewing page:", "Leaderboard");
+  else if(matchPage("/Upload/joke"))
+    updateData("Viewing page:", "Upload (Joke)");
+  else if(matchPage("/upload/picture"))
+    updateData("Viewing page:", "Upload (Picture)");
+  else if(matchPage("/message"))
+    updateData("Viewing page:", "Messages");
+  else if(matchPage("/notification"))
+    updateData("Viewing page:", "Notifications");
+  else if(matchPage("/Rules"))
+    updateData("Viewing pageL", "Rules");
+  else if(matchPage("/Announcement"))
+    updateData("Viewing page:", "Announcements");
+  else if(matchPage("/account/register"))
+    updateData("Viewing page:", "Register");
+  else if(matchPage("/account/login"))
+    updateData("Viewing page:", "Login");
+  else if(matchPage("/account/ChangePassword"))
+    updateData("Viewing page:", "Change Password");
+  else if(matchPage("/account/MergeAccount"))
+    updateData("Viewing page:", "Merge Account");
+  else if(matchPage("/account/BlockedUser"))
+    updateData("Viewing page:", "Blocked users");
+  else if(matchPage("/manage/ReportPost"))
+    updateData("Viewing page:", "Reported posts");
+  else if(matchPage("/manage/profile"))
+    updateData("Viewing page:", "Manage Profile");
 
-
-  if(data.dataSent) presence.setActivity(data.presenceData);
-  else presence.setTrayTitle();
+  if(matchPage(null, "/user/"))
+    updateData("Viewing profile:", window.location.pathname.split("/")[2]);
+  else if(matchPage(null, "/joke/tag/"))
+    updateData("Viewing a tag (Jokes):", window.location.pathname.split("/")[3]);
+  else if(matchPage(null, "/pics/tag/"))
+    updateData("Viewing a tag (Images):", window.location.pathname.split("/")[3]);
+  else if(matchPage(null, "/joke/category/"))
+    updateData("Viewing a category:", `ID: ${window.location.pathname.split("/")[3]}`);
+  else if(matchPage(null, "/joke/subcategory/"))
+    updateData("Viewing a subcategory:", `ID: ${window.location.pathname.split("/")[3]}`);
+  else if(matchPage(null, "/joke/"))
+    updateData("Viewing a joke:", `ID: ${window.location.pathname.split("/")[2]}`);
+  else if(matchPage(null, "/pics/"))
+    updateData("Viewing a picture:", `ID: ${window.location.pathname.split("/")[2]}`);
+  presence.setActivity(data.presenceData);
 });
 
-async function matchPage(href: string) {
-  console.log(href, window.location.pathname);
-
-  if(window.location.pathname === href) return true;
-  else return false;
+function matchPage(href: string, option?: string) {
+  if(window.location.pathname === href || (option && window.location.pathname.startsWith(option) && window.location.pathname.split("/")[2])) 
+    return true;
+  else
+    return false;
 }
 
 function updateData(details?: string, state?: string, buttons: boolean = true) {
-  console.log(details, state, buttons)
   if(buttons && !data.privacyMode) data.presenceData.buttons = [{ label: "View Page", url: window.location.href }];
   if(details && !data.privacyMode) data.presenceData.details = details;
   if(state && !data.privacyMode) data.presenceData.state = state;
@@ -50,6 +77,4 @@ function updateData(details?: string, state?: string, buttons: boolean = true) {
     details: "Privacy mode enabled",
     state: "Content hidden"
   };
-
-  data.dataSent = true;
 }
