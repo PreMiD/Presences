229d95abb9a70d327a42a45ecc1303621394ada4
ririxidev <mail@ririxi.dev>
2021-03-31 18:43:19 +0200

    :bug: Don't return...

--- translator.ts
+++ translator.ts
@@ -278,11 +278,9 @@ const spinnerSettings = {
           await checkCount();
 
           if (!JSON.parse(readFileSync(path).toString()).description[language])
-            return (
               error(
                 `An error occured while saving the file. Please manually add the translation to: ${language}. The version was automatically bumped.`
-              ),
-              process.exit()
+              )
             );
         }
       }
