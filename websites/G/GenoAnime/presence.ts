1eb5be4c4be2eb1a6a75dc33212b0cd6fbd58d67
Genoanime <portugalsensie@gmail.com>
2021-04-03 17:40:11 -0400

    Improved presence with the suggestion given
    fixed spacing
    
    code spacing again
    
    code analytics says spacing....

--- websites/G/GenoAnime/presence.ts
+++ websites/G/GenoAnime/presence.ts
@@ -13,9 +13,11 @@ title = document.title; //title of the page
   if (document.location.pathname == "/" ) {
     presenceData.startTimestamp = browsingStamp;
     presenceData.details = "Exploring Genoanime";
+	presenceData.buttons = [{label:"Visit Genoanime",url: document.location.href}];
   } else if (document.location.pathname.includes("/browse")) {
     presenceData.startTimestamp = browsingStamp;
     presenceData.details = "Exploring Genoanime library";
+	presenceData.buttons = [{label:"See the Library",url: document.location.href}];
   }
   else if (document.location.pathname.includes("/details")) {
     presenceData.startTimestamp = browsingStamp;
@@ -25,7 +27,8 @@ title = document.title; //title of the page
     presenceData.startTimestamp = browsingStamp;
     presenceData.details = title;
     presenceData.state = 'Episode '+ String(document.location.href.split("episode=")[1]);
-  }
+    presenceData.buttons = [{label:"Watch The Same",url: document.location.href}];
+ }
   else if (document.location.pathname.includes("/search")) {
     presenceData.startTimestamp = browsingStamp;
     presenceData.details = "Searching Catalogue";
@@ -33,6 +36,17 @@ title = document.title; //title of the page
   else if (document.location.pathname.includes("/amv")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Watching cool AMV videos";
+	    presenceData.buttons = [{label:"Check It Yourself",url: document.location.href}];
+  }
+  else if (document.location.pathname.includes("/favorite")) {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Browsing Favourites";
+  }
+  
+  else if (document.location.pathname.includes("/schedule")) {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Checking Schedule";
+	presenceData.buttons = [{label:"Get some Dates",url: document.location.href}];
   }
   if (presenceData.details == null) {
     presence.setTrayTitle();
