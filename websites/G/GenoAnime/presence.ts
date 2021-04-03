5cd9f6fe57bc169f23790d4f11f4fdad6fa3cb5b
Genoanime <portugalsensie@gmail.com>
2021-04-03 11:43:36 -0400

    genoanime presense

--- websites/G/GenoAnime/presence.ts
+++ websites/G/GenoAnime/presence.ts
@@ -5,25 +5,35 @@ browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "genoanime_2-lanczos3"
+    largeImageKey: "geno",
+    details: "Genoanime !!!",
   },
 title = document.title; //title of the page
-  if (
-    document.location.pathname == "/" 
-  ) {
+
+  if (document.location.pathname == "/" ) {
     presenceData.startTimestamp = browsingStamp;
-    presenceData.details = "Watching Genoanime";
+    presenceData.details = "Exploring Genoanime";
   } else if (document.location.pathname.includes("/browse")) {
     presenceData.startTimestamp = browsingStamp;
-    presenceData.details = "Browsing Genoanime library";
-  } else if (document.location.pathname.includes("/watch")) {
-    presenceData.startTimestamp = browsingStamp;
-    presenceData.details = title;
-  } 
+    presenceData.details = "Exploring Genoanime library";
+  }
   else if (document.location.pathname.includes("/details")) {
     presenceData.startTimestamp = browsingStamp;
     presenceData.details = "Browsing on Genoanime";
   } 
+  else if (document.location.pathname.includes("/watch")) {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = title;
+    presenceData.state = 'Episode '+ String(document.location.href.split("episode=")[1]);
+  }
+  else if (document.location.pathname.includes("/search")) {
+    presenceData.startTimestamp = browsingStamp;
+    presenceData.details = "Searching Catalogue";
+  }
+  else if (document.location.pathname.includes("/amv")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Watching cool AMV videos";
+  }
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
