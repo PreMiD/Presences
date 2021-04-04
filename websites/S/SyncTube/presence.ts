4a2e4c2576bb3d471a0fefb8fb5acbf86e94163d
N0chteil <53608074+N0chteil@users.noreply.github.com>
2021-04-04 18:54:17 +0200

    Updated

--- websites/S/SyncTube/presence.ts
+++ websites/S/SyncTube/presence.ts
@@ -1,46 +1,45 @@
 const presence = new Presence({
-  clientId: "827892428266274857"
-}),
-browsingStamp = Math.floor(Date.now() / 1000);
+    clientId: "827892428266274857"
+  }),
+  browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async function () {
-const set_timeElapsed = await presence.getSetting("timeElapsed"),
-      set_showButtons = await presence.getSetting("showButtons"),
-      set_privacy = await presence.getSetting("privacy"),
-      set_logo = await presence.getSetting("logo"),
-      presenceData = {
-            largeImageKey: (set_logo === 0) ? "logo" : "logo2"
-          },
-      urlpath = window.location.pathname.split("/");
+  const set_timeElapsed = await presence.getSetting("timeElapsed"),
+        set_showButtons = await presence.getSetting("showButtons"),
+        set_privacy = await presence.getSetting("privacy"),
+        set_logo = await presence.getSetting("logo"),
+        presenceData = {
+              largeImageKey: (set_logo === 0) ? "logo" : "logo2"
+            },
+        urlpath = window.location.pathname.split("/");
 
-if (set_timeElapsed) {
-  presenceData.startTimestamp = browsingStamp;
-}
-if(!urlpath[1]) {
-  presenceData.details = "Home";
-} else if(urlpath[1] === 'rooms') {
-  if(urlpath[2]) {
-    presenceData.details = (set_privacy) ? "In room" : document.querySelector("div.roomName.noselect").textContent;
-    presenceData.state = (set_privacy) ? "" : document.querySelector("div.userCount.noselect").textContent;
+  if(set_timeElapsed) presenceData.startTimestamp = browsingStamp;
 
-    if(set_showButtons) {
-      presenceData.buttons = [
-        {
-          label: 'Join room',
-          url: window.location.href // https://sync-tube.de/rooms/THE_ROOM
-        }
-      ];
+  if(!urlpath[1]) {
+    presenceData.details = "Home";
+  } else if(urlpath[1] === 'rooms') {
+    if(urlpath[2]) {
+      presenceData.details = (set_privacy) ? "In room" : document.querySelector("div.roomName.noselect").textContent;
+      if(!set_privacy) presenceData.state = document.querySelector("div.userCount.noselect").textContent;
+
+      if(set_showButtons) {
+        presenceData.buttons = [
+          {
+            label: 'Join room',
+            url: window.location.href
+          }
+        ];
+      }
+    } else {
+      presenceData.details = "Browsing rooms";
     }
   } else {
-    presenceData.details = "Browsing rooms";
+    presenceData.details = "Other";
   }
-} else {
-  presenceData.details = "Other";
-}
-if (presenceData.details == null) {
-  presence.setTrayTitle();
-  presence.setActivity();
-} else {
-  presence.setActivity(presenceData);
-}
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
 });
