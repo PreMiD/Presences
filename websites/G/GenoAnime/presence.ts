f8a3343685b177d15a33ebf59bca18031bce0fbf
Tharki-God <73281112+Tharki-God@users.noreply.github.com>
2021-04-05 18:42:24 +0530

    you asked your pull request

--- websites/G/GenoAnime/presence.ts
+++ websites/G/GenoAnime/presence.ts
@@ -5,8 +5,7 @@ strings = presence.getStrings({
   play: "presence.playback.playing",
   pause: "presence.playback.paused"
 }),
-browsingStamp = Math.floor(Date.now() / 1000);
-     
+browsingStamp = Math.floor(Date.now() / 1000);     
 let timestamps: number[],
   video:HTMLVideoElement,
   currentTime: number,
@@ -14,8 +13,7 @@ let timestamps: number[],
   paused: boolean,
   iFrameVideo: boolean,
   playback:boolean,
-  synopsis_link: string; 
-
+  senpai: string; 
   presence.on(
     "iFrameData",
     (data: {
@@ -34,31 +32,34 @@ let timestamps: number[],
         paused = data.iframe_video.paused;
       }
     }
-  );
-  
+  );  
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey: "geno",
     startTimestamp: browsingStamp
   },
-
-title = document.title.slice(0, -13); //title of the page
-  
+title = document.title.slice(0, -13); //title of the page  
   if (document.location.pathname == "/" ) {
     presenceData.details = "Exploring Genoanime";
+	 presenceData.smallImageKey = "pause";
+	presenceData.smallImageText = pause
+        ? (await strings).pause
+        : (await strings).play;
   } else if (document.location.pathname.includes("/browse")) {
     presenceData.details = "Exploring Genoanime library";
     presenceData.buttons = [{label:"View Library",url: document.location.href}];
   }
-  else if (document.location.pathname.includes("/details")) {
-    presenceData.details = "Browsing...";
+  else if (document.location.pathname.includes("/details")) {	   
+    presenceData.details = `Checking Synopsis`;
+	presenceData.state = document.querySelector(".anime__details__title h3").textContent;
+	senpai = String(document.querySelector("#container > section > div > div.anime__details__content > div > div.col-lg-9 > div > div.anime__details__btn > a.watch-btn"));
+	presenceData.buttons = [{label:"Watch It",url: String(senpai)},{label:"Check Synopsis",url: document.location.href}];	
   } 
   else if (document.location.pathname.includes("/watch")) {
     presenceData.details = title;
     presenceData.state = 'Episode '+ String(document.location.href.split("episode=")[1]);
-    synopsis_link = String(document.querySelector("#anime_details_breadcrumbs"));
-    presenceData.buttons = [{label:"Watch Episode",url: document.location.href},{label:"Check Synopsis",url: String(synopsis_link)}];
-
+    senpai = String(document.querySelector("#anime_details_breadcrumbs"));
+    presenceData.buttons = [{label:"Watch Episode",url: document.location.href},{label:"Check Synopsis",url: String(senpai)}];
     if (iFrameVideo) {
       timestamps = presence.getTimestamps(
         Math.floor(currentTime),
@@ -69,15 +70,16 @@ title = document.title.slice(0, -13); //title of the page
         "div > div.plyr__video-wrapper > video"
       );
       if (video){
-        currentTime = video.currentTime,
-        duration = video.duration,
-        paused = video.paused,
-        timestamps = presence.getTimestamps(
+        (currentTime = video.currentTime),
+        (duration = video.duration),
+        (paused = video.paused),
+        (timestamps = presence.getTimestamps(
           Math.floor(currentTime),
           Math.floor(duration)
-        );
+        ));
       }
     }
+    
     if (!isNaN(duration)) {
       presenceData.smallImageKey = paused ? "pause" : "play";
       presenceData.smallImageText = paused
@@ -90,9 +92,11 @@ title = document.title.slice(0, -13); //title of the page
         delete presenceData.endTimestamp;
       }
     }
+    else if (isNaN(duration)){presenceData.startTimestamp = browsingStamp}
  }
   else if (document.location.pathname.includes("/search")) {
-    presenceData.details = "Searching Catalogue";
+    presenceData.details = "Searching For...";
+	presenceData.state = (document.getElementById("search-anime") as HTMLInputElement).value;
   }
   else if (document.location.pathname.includes("/amv")) {
     presenceData.details = "Watching AMV videos";
@@ -105,6 +109,7 @@ title = document.title.slice(0, -13); //title of the page
   
   else if (document.location.pathname.includes("/schedule")) {
     presenceData.details = "Checking Schedule";
+	presenceData.state = document.querySelector("#container > section > div > div:nth-child(1) > div > h3").textContent;
     presenceData.buttons = [{label:"View Schedule",url: document.location.href}];
   }
   if (presenceData.details == null) {
