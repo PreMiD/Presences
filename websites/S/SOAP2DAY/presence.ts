d2b07bb42e797d76ec51ab447158ef3ba91928e5
Fuwn <fuwnzy@gmail.com>
2021-04-05 15:14:48 -0700

    feature: Display extra information
    - Time spent doing an action
    - Time left within a viewing session (movie, TV, etc.)
    - Session status (paused, playing, loading, etc.)

--- websites/S/SOAP2DAY/presence.ts
+++ websites/S/SOAP2DAY/presence.ts
@@ -20,8 +20,19 @@ getAction = (): string => {
     return "tv";
   } else { return "home" }
 },
-getTitle = (): string => {
-  return document.getElementsByClassName("player-title-bar")[0].textContent.trim();
+getText = (text: string): string => {
+  return document.getElementsByClassName(text)[0].textContent.trim();
+},
+epochWithOffset = (h?: number, m?: number, s?: number): number => {
+  let now = new Date();
+  if (h) { now.setTime(now.getTime() + (h * 1000 * 60 * 60)); } // Hours
+  if (m) { now.setTime(now.getTime() + (m * 1000 * 60)); }      // Minutes
+  if (s) { now.setTime(now.getTime() + (s * 1000)); }           // Seconds
+  return now.setTime(now.getTime());
+},
+getStatus = (): string => {
+  const element = document.getElementById("t3").textContent.trim();
+  if (element === "") { return "Loading" } else { return element; }
 },
 constructAction: Record<string, string> = {
   "movielist": "Searching for a movie",
@@ -34,18 +45,41 @@ constructAction: Record<string, string> = {
   "faq":       "Reading the FAQ",
   "tv":        "Relaxing to some TV"
 };
+let flag = false,
+watchStamp = 0,
+flag_set = false;
 
 presence.on("UpdateData", async () => {
+
   let presenceData: PresenceData = {
     largeImageKey: "icon",
     details: constructAction[getAction()],
-    startTimestamp: Math.floor(Date.now() / 1000)
   };
+  // If the user is watching something, get the title and set duration.
   if (["movie", "tv", "sport"].includes(getAction())) {
+    // If paused, reset update remaining.
+    if (getStatus() == "Pause") { flag = false; }
+
+    if (!flag) {
+      const duration = getText("jw-text-duration").split(":").map(e => parseInt(e));
+      switch (duration.length) {
+        case 1: watchStamp = epochWithOffset(null, null, duration[2]);               break;
+        case 2: watchStamp = epochWithOffset(null, duration[0], duration[1]);        break;
+        case 3: watchStamp = epochWithOffset(duration[0], duration[1], duration[2]); break;
+      }
+      if (!isNaN(watchStamp)) { flag = true; }
+    }
     presenceData = {
-      state: getTitle(),
+      state: `${getStatus()} | ${getText("player-title-bar")}`,
+      endTimestamp: watchStamp,
       ...presenceData
     };
+  } else { // If the user is not watching something, return how long they have been browsing.
+    flag = false;
+    presenceData = {
+      startTimestamp: Math.floor(Date.now() / 1000),
+      ...presenceData
+    }
   }
 
   if (presenceData.details == null) {
