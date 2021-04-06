222dd106d4a2214ad5121f910d80e114bcf0beb3
Kozou4ever <72155852+Kozou4ever@users.noreply.github.com>
2021-04-06 15:57:04 +0200

    Adding Scantrad presence
    Hello there, here are the proofs that the presence works perfectly:
    - https://i.imgur.com/zDnHRWj.png
    - https://i.imgur.com/E7gKtp2.png

---
+++ websites/S/Scantrad/presence.ts
@@ -0,0 +1,58 @@
+const presence = new Presence({
+    clientId: "817856303719907389"
+});
+
+const browsingStamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+    const presenceData: PresenceData = { 
+        largeImageKey: "logo",
+        startTimestamp: browsingStamp
+    };
+
+    if (document.location.pathname.includes("/")) {
+        
+        presenceData.details = "Viewing Page:";
+        presenceData.state = "Home page";
+        presenceData.startTimestamp = browsingStamp;
+        };
+    if (document.location.pathname.includes("/mangas")) {
+
+        let chapter = document.getElementById("selectCh") as HTMLSelectElement;
+        let page = document.getElementById("selectPg") as HTMLSelectElement;
+
+        presenceData.details = "Reading " + 
+            document.querySelector(".tl-titre").textContent + ":";
+        presenceData.state = chapter.options[chapter.selectedIndex].text.replace('Chapitre', 'Chapter') + " | " + page.options[page.selectedIndex].text;
+        presenceData.smallImageKey = "reading";
+        presenceData.smallImageText = "Reading a scan";
+        presenceData.startTimestamp = browsingStamp;
+        };
+    if (document.location.pathname.includes("/connexion") || document.location.pathname.includes("/inscription")) {
+        presenceData.details = "Viewing Page:";
+        presenceData.state = "Login page";
+        presenceData.startTimestamp = browsingStamp;
+        };
+    if (document.location.pathname.includes("/planning")) {
+        presenceData.details = "Viewing Page:";
+        presenceData.state = "Planning page";
+        presenceData.startTimestamp = browsingStamp;
+        };
+    if (document.location.pathname.includes("/rejoindre")) {
+        presenceData.details = "Viewing Page:";
+        presenceData.state = "Jobs page";
+        presenceData.startTimestamp = browsingStamp;
+        };
+    if (document.location.pathname.includes("/dons")) {
+        presenceData.details = "Viewing Page:";
+        presenceData.state = "Donation page";
+        presenceData.startTimestamp = browsingStamp;
+        };
+
+    if (presenceData.details == null) {
+        presence.setTrayTitle();
+        presence.setActivity();
+    } else {
+        presence.setActivity(presenceData);
+  };
+});
