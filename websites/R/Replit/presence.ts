2b42efa5b9328c24f539cdc54ee094f810b928e3
Ashutosh-3601 <39941042+Ashutosh-3601@users.noreply.github.com>
2021-04-11 19:54:09 +0530

    final fix to pass all checks

--- websites/R/Replit/presence.ts
+++ websites/R/Replit/presence.ts
@@ -11,19 +11,15 @@ presence.on("UpdateData", () => {
   if (Path == "/" || Path == "/~") {
     presenceData.details = "Viewing home page";
   } else if (Path.startsWith("/@")) {
-    if (
-      Path.replace("/@", "")
-        .split("/")
-        .filter((elm) => elm !== "").length === 1
-    ) {
+    if (Path.replace("/@", "").split("/").filter((elm) => elm !== "").length === 1) {
       const user = Path.replace("/@", "");
       presenceData.details = "Viewing a user's profile";
       presenceData.state = user;
     } else {
       const fileType: string = (document.querySelector(
         "#workspace-root > div > div.jsx-132086333.content > div.jsx-77352756.workspace-page-wrapper.desktop > div > div > div:nth-child(1) > header > div > div.jsx-1874997921.left > div > div > div.jsx-2652062152.workspace-header-info > div.jsx-2652062152.language-icon-container > img"
-      ) as HTMLImageElement).alt;
-      const projectName: string =
+      ) as HTMLImageElement).alt,
+       projectName: string =
         Path.split("/").filter((elm) => elm !== "")[1] +
         `${window.location.hash ? ` - ${window.location.hash.substr(1)}` : ""}`;
       presenceData.details = `Editing ${fileType} repl`;
@@ -42,8 +38,8 @@ presence.on("UpdateData", () => {
   } else if (Path.startsWith("/repls")) {
     const repls: HTMLDivElement = document.querySelector(
       "#__next > div > div > div.jsx-2888589246.content > div.jsx-1264267603.repl-content > div.jsx-4064465542 > div:nth-child(3)"
-    );
-    const length: number = repls ? repls.children.length - (Path === "/repls" ? 2 : 1) : 0;
+    ),
+     length: number = repls ? repls.children.length - (Path === "/repls" ? 2 : 1) : 0;
     presenceData.details = `Viewing repls ${length ? `(Total ${length})` : ""}`;
     presenceData.state = `${
       Path == "/repls"
@@ -52,8 +48,8 @@ presence.on("UpdateData", () => {
     }`;
   } else if (Path.startsWith("/talk")) {
     presenceData.details = "Surfing feed";
-    const postType: string = Path.replace("/talk/", "").split("/")[0];
-    const postElement: HTMLDivElement = document.querySelector(
+    const postType: string = Path.replace("/talk/", "").split("/")[0],
+     postElement: HTMLDivElement = document.querySelector(
       "#__next > div > div.jsx-132086333.content > div.jsx-2019133593 > div.jsx-2019133593.post-page-content > div.jsx-347352367 > div.jsx-347352367.board-post-detail-header > div.jsx-347352367.board-post-detail-title"
     );
     switch (Path.replace("/talk/", "")) {
