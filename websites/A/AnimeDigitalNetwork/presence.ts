083c8987e04595523a6f5e6be503dd98f2749dc5
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-04-14 08:37:22 +0200

    Made the requested changes!
    Changed t2 => t
    Remove !video.paused from setActivity

--- websites/A/AnimeDigitalNetwork/presence.ts
+++ websites/A/AnimeDigitalNetwork/presence.ts
@@ -12,7 +12,7 @@ presence.on("UpdateData", async () => {
   if (document.location.pathname.includes("video") && video) {
     if (video && !isNaN(video.duration)) {
       const t = document.querySelector("#root > div > div > div.sc-qXJnB.ijvXfJ > div > div:nth-child(1) > div.sc-jOdeeR.sc-kHIeKe.fmWHUu > div:nth-child(1) > div > div > h1 > a").textContent.toLowerCase(),
-      title = t2.charAt(0).toUpperCase() + t2.slice(1), 
+      title = t.charAt(0).toUpperCase() + t.slice(1), 
       timestamps = presence.getTimestamps(Math.floor(video.currentTime), Math.floor(video.duration)), 
       data: PresenceData = {
           details: title,
@@ -31,12 +31,12 @@ presence.on("UpdateData", async () => {
       }
 
       if (t !== null) {
-        presence.setActivity(data, !video.paused);
+        presence.setActivity(data);
       }
     }
   } else if (document.location.pathname.includes("video") && !video) {
     const t = document.querySelector("#root > div > div > div.sc-qXJnB.ijvXfJ > div > div > div.sc-pAArZ.idjhKu > div:nth-child(2) > div > div > div > div.sc-jwDLgl.jjQkhb > div > h1").textContent.toLowerCase(),
-          title = t2.charAt(0).toUpperCase() + t2.slice(1), data = {
+          title = t.charAt(0).toUpperCase() + t.slice(1), data = {
         details: "Browsing...",
         state: title,
         largeImageKey: "logo"
