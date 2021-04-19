fb2c1563299f1e6c1134c735dc3e1a7f94f4b619
Luna Tsuki <lunax@mlunax.com>
2021-04-19 13:22:20 +0200

    change the method to setting title from watchparty

--- websites/P/Prime Video/presence.ts
+++ websites/P/Prime Video/presence.ts
@@ -24,16 +24,12 @@ function getTimestamps(
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = { largeImageKey: "pvid" };
   presenceData.startTimestamp = browsingStamp;
-  let title: HTMLElement = document.querySelector(
+  const title: HTMLElement = document.querySelector(
       ".webPlayerSDKUiContainer > div > div > div > div:nth-child(2) > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(2) > div > div > div > h1"
-    ),
+    ) || document.querySelector(".atvwebplayersdk-title-text"),
     title2: HTMLElement = document.querySelector(
       ".av-detail-section > div > h1"
     )
-  if (title == null && title2 == null) {
-    title =  document.querySelector(".atvwebplayersdk-title-text");
-    title2 = document.querySelector(".atvwebplayersdk-subtitle-text");
-  }
   if (title !== null || title2 !== null) {
     let video: HTMLVideoElement = document.querySelector(
       ".scalingVideoContainer > div.scalingVideoContainerBottom > div > video"
