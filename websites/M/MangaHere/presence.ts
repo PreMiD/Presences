49922f18ec6e005c5f660692e2b71a14607d1702
MareGraphics <mare.sampbn@gmail.com>
2021-04-24 17:42:30 +0200

    Update websites/M/MangaHere/presence.ts
    Co-authored-by: ririxidev <mail@ririxi.dev>

--- websites/M/MangaHere/presence.ts
+++ websites/M/MangaHere/presence.ts
@@ -60,11 +60,7 @@ presence.on("UpdateData", async () => {
     else if (pathname.endsWith("/on_going/")) {
       const url = pathname,
        splitUrl = url.split('/');
-      if (splitUrl[1] == "on_going") {
-        data.details = "Browsing ongoing manga";
-      } else {
-        data.details = "Browsing ongoing " + splitUrl[1] + " manga";
-      }
+      data.details = splitUrl[1] === "on_going" ? "Browsing ongoing manga" : `Browsing ongoing ${splitUrl[1]} manga`;
     }
     //Manga Viewing
     else if (pathname.startsWith("/manga") && pathname.endsWith("/")) {
