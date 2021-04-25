f532e590d7a234c89bf8c50b7f5250d07e307791
Declan Mould <44209534+TheGamerzs@users.noreply.github.com>
2021-04-25 22:15:09 +0100

    :art: Formatting Codacy

--- websites/W/WoolyPooly/presence.ts
+++ websites/W/WoolyPooly/presence.ts
@@ -10,7 +10,6 @@ presence.on("UpdateData", async () => {
     largeImageKey: "logo"
   };
 
-
   switch (window.location.pathname) {
     case "/":
       presenceData.state =
@@ -18,32 +17,38 @@ presence.on("UpdateData", async () => {
           .childElementCount + " Coins";
       presenceData.details = "Charts Overview";
       break;
+
     case "/faq":
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "FAQ";
       break;
+
     case "/privacy":
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Privacy Policy";
       break;
+
     case "/tos":
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Terms of Service";
       break;
+
     case "/raveos":
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Rave OS Redirect";
       break;
+
     default:
       if (window.location.pathname.includes("coin")) {
         currencyTitle = document.querySelector(
           "div.mainContent > div.typeSelection > div.coinTitle.flexEqual > div:nth-child(2)"
         ).textContent;
+
         if (window.location.pathname.includes("wallet")) {
           wallet24Revenue = document.querySelector(
             "div.mainContent > div.mainContainer.flex.flex-wrap > div.card.cardSpec > div > div:nth-child(4) > div.miningBShortCell.ctr.cbold.tooltip.tooltipx > span:nth-child(1)"
           ).textContent;
-          //currencyTitle = document.querySelector("body > div.layout.darkBg.darkFont.darkScrollbar > div.contentWrapper > div.mainContent > div.typeSelection > div.coinTitle.flexEqual > div:nth-child(2)").textContent
+
           presenceData.state =
             "24h Revenue: " + wallet24Revenue + " " + currencyTitle;
           currencyTitle += " Wallet";
@@ -51,9 +56,11 @@ presence.on("UpdateData", async () => {
           currencyEffort = document.querySelector(
             "div.mainContainer.flex.flex-wrap > div:nth-child(2) > div:nth-child(3) > div.cardValue > span:nth-child(1) > span:nth-child(1)"
           ).textContent;
+
           effortType = document.querySelector(
             "body > div.layout > div.contentWrapper > div.mainContent > div.typeSelection > div.typeSelectionRight.flexEqual > div > div.baseTab.activeTab"
           ).textContent;
+
           presenceData.state =
             "Effort (" + effortType + "): " + currencyEffort + "%";
         }
