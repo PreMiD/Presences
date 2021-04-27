b768dd6911c1a4ab48555f0cc89ed86a33ae38a3
Marko <mare.sampbn@gmail.com>
2021-04-27 21:24:19 +0200

    Fully functional

--- websites/M/MangaSee/presence.ts
+++ websites/M/MangaSee/presence.ts
@@ -3,10 +3,12 @@ const presence = new Presence({
 }),
   browsingStamp = Math.floor(Date.now() / 1000);
 
-presence.on("UpdateData", async () => {
+  presence.on("UpdateData", async () => {
+  const entries = await presence.getSetting("entries"),
+        buttons = await presence.getSetting("buttons");
   const data: PresenceData = {
     largeImageKey: "logo",
-    startTimestamp: browsingStamp
+    startTimestamp: browsingStamp,
   },
     pathname = document.location.pathname;
     if (pathname === "/"){
@@ -14,7 +16,7 @@ presence.on("UpdateData", async () => {
     }
     else if(pathname == "/directory/")
         data.details = "Browsing all manga";
-    else if(pathname == "/search/" && window.location.search.substr(0,5) == "?sort"){
+    else if(pathname == "/search/" && window.location.search.substr(0,1) == "?"){
         const query = document.location.search,
               urlParams = new URLSearchParams(query),
               search = urlParams.get('name');
@@ -22,8 +24,56 @@ presence.on("UpdateData", async () => {
         data.state = search;
         data.smallImageKey = "search";
     }
+    else if(pathname == "/search/")
+      data.details = "Browsing all anime";
     else if(pathname == "/discussion/")
-        data.details = "Viewing discussion";
-    
+        data.details = "Viewing discussion page";
+    else if(pathname.endsWith("post.php")){
+      const title = document.querySelector(".BoxBody > h1").textContent,
+            author = document.querySelector(".Description > span").textContent,
+            link = window.location.href;
+      data.details = "Discussion: " + title;
+      data.state = "by " + author;
+      data.buttons = [{label: "Join discussion", url: link}]
+    }
+    else if(pathname.endsWith("/subscription.php")){
+      data.details = "Viewing subscriptions";
+      if(entries){
+        const number = document.querySelector(".BoxHeader > span").textContent.replace("(", "").replace(")","");
+        data.state = number + " entries";
+      }
+    }
+    else if(pathname.endsWith("/bookmark.php")){
+      data.details = "Viewing bookmark";
+      if(entries){
+        const number = document.querySelector(".BoxHeader > span").textContent.replace("(", "").replace(")","");
+        data.state = number + " entries";
+      }
+    }
+    else if(pathname.endsWith("/settings.php")){
+      data.details = "Viewing settings";
+    }
+    else if(pathname.startsWith("/manga/")){
+      const title = document.querySelector(".list-group-item > h1").textContent;
+      data.details = "Viewing manga:";
+      data.state = title;
+      data.smallImageKey = "view";
+      if(buttons){
+        const link = window.location.href;
+        data.buttons = [{label: "View manga", url: link}]
+      }
+    }
+    else if(pathname.startsWith("/read-online/")){
+      const title = document.querySelector(".col-lg-4 > a").textContent.replace(new RegExp("\\\t","g"),'').replace(new RegExp("\\\n","g"),''),
+            chapter = document.querySelector('button[data-target="#ChapterModal"]').textContent.replace(new RegExp("\\\t","g"),'').replace(new RegExp("\\\n","g"),''),
+            page = document.querySelector('button[data-target="#PageModal"]').textContent.replace(new RegExp("\\\t","g"),'').replace(new RegExp("\\\n","g"),'');
+      data.details = title;
+      data.state = "📖Ch. " + chapter.split(" ")[1] + " 📄 " + page.split(" ")[1];
+      data.smallImageKey = "read";
+      if(buttons){
+        const link = window.location.href;
+        data.buttons = [{label: "View manga", url: link}]
+      }
+    }
     presence.setActivity(data);
   });
