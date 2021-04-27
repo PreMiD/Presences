5b1ba1d22347fb3552a2e59a9207c8cb9eb9af3b
thebriss <kaymak210621@gmail.com>
2021-04-28 01:00:43 +0300

    discord.style added

---
+++ websites/D/discord.style/presence.ts
@@ -0,0 +1,74 @@
+const presenceClient = new Presence({
+    clientId: "836680940301844531"
+});
+
+presenceClient.on("UpdateData", async () => {
+    const presenceData: PresenceData = {
+        largeImageKey: "logo"
+    }
+    presenceData.startTimestamp = Math.floor(Date.now() / 1000);
+    const pathName: string = window.location.pathname;
+    const hostName: string = window.location.hostname;
+    const buttonValue = await presenceClient.getSetting("buttonsData")
+    const getButton = (labelData: string, urlData: string) => {
+        var resultButton;
+        if (labelData && urlData) {
+            resultButton = [
+                {
+                    label: labelData,
+                    url: urlData
+                }
+            ]
+        }
+        return resultButton
+    }
+    if (hostName.toLowerCase() === "www.discord.style") {
+        presenceData.details = "Viewing page:";
+        if (pathName.toLowerCase() === "/") {
+            presenceData.state = "Homepage";
+            if (buttonValue) {
+                presenceData.buttons = getButton("View page", `https://www.discord.style${pathName}`);
+            }
+        } else if (pathName.startsWith("/browse")) {
+            presenceData.state = "Browse templates";
+            if (buttonValue) {
+                presenceData.buttons = getButton("View page", `https://www.discord.style${pathName}`);
+            }
+        } else if (pathName.startsWith("/latest")) {
+            presenceData.state = "Latest templates";
+            if (buttonValue) {
+                presenceData.buttons = getButton("View page", `https://www.discord.style${pathName}`);
+            }
+        } else if (pathName.startsWith("/bot")) {
+            presenceData.state = "dsc.st bot";
+            if (buttonValue) {
+                presenceData.buttons = getButton("View page", `https://www.discord.style${pathName}`);
+            }
+        } else if (pathName.startsWith("/new")) {
+            presenceData.details = "Adding a new template";
+            if (buttonValue) {
+                presenceData.buttons = getButton("View page", `https://www.discord.style${pathName}`);
+            }
+        } else if (pathName.startsWith("/template")) {
+            presenceData.details = "Viewing template:";
+            presenceData.state = document.querySelectorAll("h1")[1].innerHTML.trim() || document.querySelector("head > title").innerHTML.split("by")[0].trim();
+            if (buttonValue) {
+                presenceData.buttons = getButton("View template", `https://www.discord.style${pathName}`);
+            }
+        } else if (pathName.startsWith("/user")) {
+            presenceData.details = "Viewing user profile:";
+            presenceData.state = document.querySelectorAll("h1")[1].innerHTML.trim() + document.querySelector("h5").innerHTML.trim();
+            if (buttonValue) {
+                presenceData.buttons = getButton("View user profile", `https://www.discord.style${pathName}`);
+            }
+        } else if (pathName.startsWith("/results")) {
+            presenceData.details = "Viewing:";
+            presenceData.state = "Results " + document.querySelector("h2").innerText.split("results")[1].replace(/"/g, "").trim();
+        }
+    }
+    if (presenceData.state == null) {
+        presenceClient.setTrayTitle();
+    } else {
+        presenceClient.setActivity(presenceData);
+    }
+});
