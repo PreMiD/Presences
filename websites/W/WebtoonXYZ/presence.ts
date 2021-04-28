40e453812c0c5a91ac2d455dc94eab18aa5be054
Marko <mare.sampbn@gmail.com>
2021-04-28 16:54:08 +0200

    Homepage, Browsing, Searching

--- websites/W/WebtoonXYZ/presence.ts
+++ websites/W/WebtoonXYZ/presence.ts
@@ -4,12 +4,43 @@ const presence = new Presence({
   browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-  const data: PresenceData = {
-    largeImageKey: "logo"
-  },
+  const v2icons = await presence.getSetting("v2icons"),
+        logo = await presence.getSetting("logo"),
+        data: PresenceData = {
+          largeImageKey: logo==0 ? "logo" : "logo-v2",
+          startTimestamp: browsingStamp
+        },
     pathname = document.location.pathname;
-    if(pathname == "/")
+
+    if(pathname == "/" && window.location.search.substr(0,2) == "?s"){
+      const urlParams = new URLSearchParams(window.location.search),
+            nsfw = urlParams.get("adult"),
+            search = nsfw == "1" ? "nsfw" : nsfw == "0" ? "non nsfw" : urlParams.get("s"),
+            results = document.querySelector(".c-blog__heading > .h4").textContent.split(" ")[1];
+      data.details = "Searching:";
+      data.state = search + " 🔸 " + results +" results";
+      data.smallImageKey = v2icons ? "search-v2" : "search";
+    }    
+    else if(pathname == "/")
       data.details = "Viewing the homepage"
+    else if(pathname.endsWith("/webtoons/")){
+      const results = document.querySelector(".c-blog__heading > .h4").textContent;
+      data.details = "Browsing all webtoons";
+      data.state = results;
+    }
+    else if(pathname.startsWith("/webtoon-genre/")){
+      const genre = document.querySelector(".item-title").textContent,
+            results = document.querySelector(".c-blog__heading > .h4").textContent;
+      data.details = "Browsing " + genre + " webtoons";
+      data.state = "📋 " +results; 
+      data.smallImageKey = v2icons ? "search-v2" : "search";
+    }
+    else if(pathname == "/completed-webtoons/"){
+      data.details = "Browsing:";
+      data.state = "Completed webtoons"
+      data.smallImageKey = v2icons ? "search-v2" : "search";
+    } 
+
     presence.setActivity(data);
   });
   
