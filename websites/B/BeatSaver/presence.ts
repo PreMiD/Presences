9596f1eb168566fc5ded8ebbe1f8950effee5cab
joerkig <44418429+joerkig@users.noreply.github.com>
2021-05-01 23:36:13 +0200

    Semicolons

--- websites/B/BeatSaver/presence.ts
+++ websites/B/BeatSaver/presence.ts
@@ -20,16 +20,16 @@ presence.on("UpdateData", async () => {
   } else if (document.location.pathname.includes("/beatmap/")) {
     if (document.querySelector("span.tag.is-expert-plus") != null)
       presenceData.smallImageKey = "expert_",
-        presenceData.smallImageText = "Top Diff: Expert+"
+        presenceData.smallImageText = "Top Diff: Expert+";
     else if (document.querySelector("span.tag.is-expert") != null)
       presenceData.smallImageKey = "expert",
-        presenceData.smallImageText = "Top Diff: Expert"
+        presenceData.smallImageText = "Top Diff: Expert";
     else if (document.querySelector("span.tag.is-hard") != null)
       presenceData.smallImageKey = "hard",
-        presenceData.smallImageText = "Top Diff: Hard"
+        presenceData.smallImageText = "Top Diff: Hard";
     else if (document.querySelector("span.tag.is-normal") != null)
       presenceData.smallImageKey = "normal",
-        presenceData.smallImageText = "Top Diff: Normal"
+        presenceData.smallImageText = "Top Diff: Normal";
     else
       presenceData.smallImageKey = "easy",
         presenceData.smallImageText = "Top Diff: Easy"
@@ -48,47 +48,47 @@ presence.on("UpdateData", async () => {
         label: "View Uploader's Page",
         url: "https://beatsaver.com" + document.querySelector("h2.is-size-4 > a").getAttribute("href")
       }
-    ]
+    ];
   }
   else if (document.location.pathname.includes("/uploader/"))
     presenceData.details = "Browsing By Uploader",
       presenceData.state = document
         .querySelector("h1.is-size-2.has-text-weight-light.has-text-centered")
         .textContent.split(" ")[2],
-  presenceData.buttons = [
-    {
-      label: "View Page",
-      url: document.location.href
-    }
-  ]
+      presenceData.buttons = [
+        {
+          label: "View Page",
+          url: document.location.href
+        }
+      ];
   else if (document.location.pathname.includes("/browse/latest"))
-    presenceData.details = "Browsing By Latest"
+    presenceData.details = "Browsing By Latest";
   else if (document.location.pathname.includes("/browse/plays"))
-    presenceData.details = "Browsing By Plays"
+    presenceData.details = "Browsing By Plays";
   else if (document.location.pathname.includes("/browse/downloads"))
-    presenceData.details = "Browsing By Downloads"
+    presenceData.details = "Browsing By Downloads";
   else if (document.location.pathname.includes("/browse/latest"))
-    presenceData.details = "Browsing By Latest"
+    presenceData.details = "Browsing By Latest";
   else if (document.location.pathname.includes("/browse/rating"))
-    presenceData.details = "Browsing By Rating"
+    presenceData.details = "Browsing By Rating";
   else if (document.location.pathname.includes("/browse/hot"))
-    presenceData.details = "Browsing By Hot"
+    presenceData.details = "Browsing By Hot";
   else if (document.location.pathname.includes("/ranking/requests"))
-    presenceData.details = "Browsing Rank Requests"
+    presenceData.details = "Browsing Rank Requests";
   else if (document.location.pathname.includes("/legal/license"))
-    presenceData.details = "Viewing License"
+    presenceData.details = "Viewing License";
   else if (document.location.pathname.includes("/legal/privacy"))
-    presenceData.details = "Viewing Privacy Policy"
+    presenceData.details = "Viewing Privacy Policy";
   else if (document.location.pathname == ("/legal/dmca"))
-    presenceData.details = "Viewing DMCA Policy"
+    presenceData.details = "Viewing DMCA Policy";
   else if (document.location.pathname == ("/auth/login"))
-    presenceData.details = "Logging In..."
+    presenceData.details = "Logging In...";
   else if (document.location.pathname == ("/auth/register"))
-    presenceData.details = "Registering..."
+    presenceData.details = "Registering...";
   else if (document.location.pathname == ("/user/upload"))
-    presenceData.details = "Uploading..."
+    presenceData.details = "Uploading...";
   else if (document.location.href == ("https://beatsaver.com/"))
-    presenceData.details = "Viewing Home Page"
+    presenceData.details = "Viewing Home Page";
 
   if (!time)
     delete presenceData.startTimestamp;
