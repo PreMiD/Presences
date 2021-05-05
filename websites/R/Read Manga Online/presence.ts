657439b786cd715bebc5def85fe69123db1d8151
Marko <mare.sampbn@gmail.com>
2021-05-05 14:27:10 +0200

    90% finished version

--- websites/R/Read Manga Online/presence.ts
+++ websites/R/Read Manga Online/presence.ts
@@ -5,13 +5,78 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
   const data: PresenceData = {
-    largeImageKey: "logo"
+    largeImageKey: "logo",
+    startTimestamp: browsingStamp
   },
     pathname = document.location.pathname;
-    console.log(pathname);
     if (pathname === "/"){
       data.details = "Viewing the Homepage";
-      data.startTimestamp = browsingStamp;
+    }
+    else if(pathname === "/popular-manga"){
+      data.details = "Viewing: ";
+      data.state = "Popular Manga";
+    }
+    else if(pathname === "/manga-list"){
+      data.details = "Viewing: ";
+      data.state = "All Manga";
+    }
+    else if(pathname.startsWith("/manga-list") && pathname.endsWith('/completed')){
+      data.details = "Viewing: ";
+      data.state = "Completed Manga on " + pathname.split('/')[2].toUpperCase();
+    }
+    else if(pathname.startsWith("/manga-list") && pathname.endsWith('/ongoing')){
+      data.details = "Viewing: ";
+      data.state = "Ongoing Manga on " + pathname.split('/')[2].toUpperCase();
+    }
+    else if(pathname.startsWith("/manga-list")){
+      data.details = "Viewing: ";
+      data.state = "Manga on " + pathname.split('/')[2].toUpperCase();
+    }
+    else if(pathname === "/manga-list/hash/ongoing"){
+      data.details = "Viewing: ";
+      data.state = "Ongoing Manga";
+    }
+    else if(pathname === "/manga-list/hash/completed"){
+      data.details = "Viewing: ";
+      data.state = "Completed Manga";
+    }
+    else if(pathname === "/latest-releases"){
+      data.details = "Viewing: ";
+      data.state = "Latest releases";
+    }
+    else if(pathname === "/collections"){
+      data.details = "Viewing: ";
+      data.state = "Collections";
+    }
+    else if(pathname.startsWith("/collection")){
+      const collection_name = document.querySelector('.page-title').textContent;
+      data.details = "Viewing collection: ";
+      data.state = collection_name;
+    }
+    else if(pathname === "/advanced-search"){
+      const query = (<HTMLInputElement>document.querySelector('[name="manga-name"]')).value;
+      data.details = "Searching: ";
+      data.state = query;
+      data.smallImageKey = "search";
+    }
+    else if(pathname.startsWith('/category')){
+      data.details = "Browsing category: ";
+      data.state = pathname.split('/')[2].replace('-', ' ');
+    }
+    else if(pathname.endsWith('/all-pages')){
+      const title = document.querySelector('.page-title > a').textContent,
+            chapter = document.querySelector('.page-title > span').textContent;
+      data.details = title;
+      data.state = chapter;
+      data.smallImageKey = "read";
+      data.buttons = [{ label: "Read manga", url: document.location.href }];
+    }
+    else if(pathname.startsWith('/manga')){
+      const title = document.querySelector('.page-title').textContent;
+      data.details = "Viewing: ";
+      data.state = title;
+      data.smallImageKey = "view";
+      data.buttons = [{ label: "View manga", url: document.location.href }];
     }
     
     presence.setActivity(data);
