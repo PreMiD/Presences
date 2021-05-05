8606bd5054ac8710e063ac130e0a752a677a788f
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-05-05 09:54:50 +0200

    Resolved Merge Conflicts & Added Requested Changes

--- websites/S/Scantrad/presence.ts
+++ websites/S/Scantrad/presence.ts
@@ -1,53 +1,57 @@
 const presence = new Presence({
-    clientId: "817856303719907389"
-  }),
-  browsingStamp = Math.floor(Date.now() / 1000);
+  clientId: "817856303719907389"
+}),
+browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-  const presenceData: PresenceData = {
-    largeImageKey: "logo",
-    startTimestamp: browsingStamp
-  };
+const presenceData: PresenceData = {
+  largeImageKey: "logo",
+  startTimestamp: browsingStamp
+};
 
-  if (document.location.pathname.includes("/mangas")) {
-    const chapter = document.getElementById("selectCh") as HTMLSelectElement,
-      page = document.getElementById("selectPg") as HTMLSelectElement;
+if (document.location.pathname.includes("/mangas")) {
+  const chapter = document.querySelector("label.chapitre-main"),
+    page = document.getElementById("selectPg") as HTMLSelectElement;
 
-    presenceData.details =
-      "Reading " + document.querySelector(".tl-titre").textContent + ":";
-    presenceData.state =
-      chapter.options[chapter.selectedIndex].text.replace(
-        "Chapitre",
-        "Chapter"
+  presenceData.details =
+    "Reading " +
+    document
+      .querySelector("title")
+      .textContent.replace(
+        /^Scan Chapitre \d+ de (.+) - Scantrad France$/,
+        "$1"
       ) +
-      " | " +
-      page.options[page.selectedIndex].text;
-    presenceData.smallImageKey = "reading";
-    presenceData.smallImageText = "Reading a scan";
-  } else if (
-    document.location.pathname.includes("/connexion") ||
-    document.location.pathname.includes("/inscription")
-  ) {
-    presenceData.details = "Viewing Page:";
-    presenceData.state = "Login page";
-  } else if (document.location.pathname.includes("/planning")) {
-    presenceData.details = "Viewing Page:";
-    presenceData.state = "Planning page";
-  } else if (document.location.pathname.includes("/rejoindre")) {
-    presenceData.details = "Viewing Page:";
-    presenceData.state = "Jobs page";
-  } else if (document.location.pathname.includes("/dons")) {
-    presenceData.details = "Viewing Page:";
-    presenceData.state = "Donation page";
-  } else {
-    presenceData.details = "Viewing Page:";
-    presenceData.state = "Home page";
-  }
+    ":";
+  presenceData.state =
+    chapter.textContent.replace("Chapitre", "Chapter") +
+    " | " +
+    page.options[page.selectedIndex].text;
+  presenceData.smallImageKey = "reading";
+  presenceData.smallImageText = "Reading a scan";
+} else if (
+  document.location.pathname.includes("/connexion") ||
+  document.location.pathname.includes("/inscription")
+) {
+  presenceData.details = "Viewing Page:";
+  presenceData.state = "Login page";
+} else if (document.location.pathname.includes("/planning")) {
+  presenceData.details = "Viewing Page:";
+  presenceData.state = "Planning page";
+} else if (document.location.pathname.includes("/rejoindre")) {
+  presenceData.details = "Viewing Page:";
+  presenceData.state = "Jobs page";
+} else if (document.location.pathname.includes("/dons")) {
+  presenceData.details = "Viewing Page:";
+  presenceData.state = "Donation page";
+} else {
+  presenceData.details = "Viewing Page:";
+  presenceData.state = "Home page";
+}
 
-  if (presenceData.details == null) {
-    presence.setTrayTitle();
-    presence.setActivity();
-  } else {
-    presence.setActivity(presenceData);
-  }
+if (presenceData.details == null) {
+  presence.setTrayTitle();
+  presence.setActivity();
+} else {
+  presence.setActivity(presenceData);
+}
 });
