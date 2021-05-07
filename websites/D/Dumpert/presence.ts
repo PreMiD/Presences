c7186b3ea06cc126fe3665e08b99a7d06594019e
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-05-07 18:06:58 +0200

    Done with the presence

--- websites/D/Dumpert/presence.ts
+++ websites/D/Dumpert/presence.ts
@@ -2,7 +2,7 @@ const presence = new Presence({
   clientId: "840126038205923369"
 }),
 browsingStamp = Math.floor(Date.now() / 1000);
-let search: HTMLInputElement, title: Element;
+let search: HTMLInputElement, title: Element, title2: Element;
 
 presence.on("UpdateData", async () => {
 const presenceData: PresenceData = {
@@ -12,15 +12,76 @@ const presenceData: PresenceData = {
   pageh = document.location.href;
 
 presenceData.startTimestamp = browsingStamp;
-
 if (page == "/") {
-  presenceData.details = "Home Pagina"
-}
-if (pageh.includes("selectedId=")) {
-   title = document.querySelector(
-    '#app > div > div:nth-child(6) > div > section > div.popup--307itj > div > div:nth-child(3) > div > article > div.item--2CpOv4.card--158lmg.detail--1SO6U0.detail-item > div.summary--2zr-I0.detail--1SO6U0.detail_summary > div.desc--qWymgd > h3'
-   )
- presenceData.details = title.textContent;
+  presenceData.details = "Bekijkt:";
+  presenceData.state = "De Home Pagina";
+} else if (pageh.includes("plaatjes")) {
+  var element = document.querySelector('meta[property~="og:title"]');
+  if (!element) {
+    presenceData.details = "Bekijkt:";
+    presenceData.state = "Plaatjes";
+  } else {
+  var content = element && element.getAttribute("content");
+ presenceData.details = content;
+  }
+} else if (pageh.includes("filmpjes")) {
+  var element = document.querySelector('meta[property~="og:title"]');
+  if (!element) {
+    presenceData.details = "Bekijkt:";
+    presenceData.state = "Filmpjes";
+  } else {
+  var content = element && element.getAttribute("content");
+ presenceData.details = content;
+  }
+} else if (pageh.includes("selectedId=") || pageh.includes("/item/")) {
+  delete presenceData.startTimestamp;
+  title2 = document.querySelector("[id*='vjs_video_']");
+  waitForElement()
+  function waitForElement(){
+      if(title2 != undefined){
+        if (title2.className.includes("paused")) {
+          delete presenceData.endTimestamp;
+          presenceData.smallImageKey = "pause";
+        } else if (title2.className.includes("playing")) {
+          const currentTime = presence.timestampFromFormat(
+            document.querySelector(
+              `#vjs_video_${title2.className.slice(40, 55).replace(/[^0-9.]/g, "")} > div.vjs-control-bar.progress-in-menu > div.vjs-current-time.vjs-time-control.vjs-control > span.vjs-current-time-display`
+            ).textContent
+          ),
+          
+          durationss = presence.timestampFromFormat(
+            document.querySelector(
+              `#vjs_video_${title2.className.slice(40, 55).replace(/[^0-9.]/g, "")} > div.vjs-control-bar.progress-in-menu > div.vjs-duration.vjs-time-control.vjs-control > span.vjs-duration-display`
+            ).textContent
+          ),
+          timestamps = presence.getTimestamps(currentTime, durationss);
+          presenceData.endTimestamp = timestamps[1];
+          presenceData.smallImageKey = "play";
+        }
+      }
+      else{
+          setTimeout(waitForElement, 3000);
+      }
+  }
+  
+  var element = document.querySelector('meta[property~="og:title"]');
+  var content = element && element.getAttribute("content");
+ presenceData.details = content;
+} else if (page.includes("toppers")) {
+  presenceData.details = "Bekijkt:";
+  presenceData.state = "De Toppers";
+} else if (page.includes("/zoek/")) {
+  title = document.querySelector("#app > div > div:nth-child(6) > div > div.grid > main > div > div > div > h1");
+  if (!title) {
+    presenceData.details = "Zoekt Voor:";
+    presenceData.state = page.replace("/zoek/", "");
+  } else {
+    presenceData.details = "Zoekt Voor:";
+    presenceData.state = title.textContent.replace("Geen resultaten voor", "");
+  }
+} else if (page.includes("latest")) {
+  presenceData.details = "Bekijkt:";
+  presenceData.state = "The Latest";
 }
 
 if (presenceData.details == null) {
