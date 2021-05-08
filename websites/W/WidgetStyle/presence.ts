32f97821ca4f18e7593e08b28bcbfde0273f64e8
DevAnaxes <anaxesoce@gmail.com>
2021-05-08 16:02:19 +1000

    Added buttons.

--- websites/W/WidgetStyle/presence.ts
+++ websites/W/WidgetStyle/presence.ts
@@ -11,25 +11,67 @@ presence.on("UpdateData", async () => {
     if (document.location.pathname == "/") {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Viewing 🏠 home page";
+      presenceData.buttons = [
+        {
+          label: "🌐 View Page",
+          url: document.location.href
+        }
+      ];
     } else if (document.location.pathname.includes("/widgets")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Viewing ⚙ widgets page";
+      presenceData.buttons = [
+        {
+          label: "🌐 View Page",
+          url: document.location.href
+        }
+      ];
     } else if (document.location.pathname.includes("/partners")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Viewing 🤝 partners page";
+      presenceData.buttons = [
+        {
+          label: "🌐 View Page",
+          url: document.location.href
+        }
+      ];
     } else if (document.location.pathname.includes("/team")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Viewing 😎 team page";
+      presenceData.buttons = [
+        {
+          label: "🌐 View Page",
+          url: document.location.href
+        }
+      ];
     } else if (document.location.pathname.includes("/privacy")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Viewing 🎭 privacy policy page";
+      presenceData.buttons = [
+        {
+          label: "🌐 View Page",
+          url: document.location.href
+        }
+      ];
     } else if (document.location.pathname.includes("/terms")) {
       presenceData.startTimestamp = browsingStamp;
       presenceData.details = "Viewing 📖 tos page";
+      presenceData.buttons = [
+        {
+          label: "🌐 View Page",
+          url: document.location.href
+        }
+      ];
     } else if (document.location.pathname.includes("/user/")) {
       presenceData.startTimestamp = browsingStamp;
       const user = document.getElementById("username");
       presenceData.details = `Viewing 👤 user: ${user.innerText}`;
+      presenceData.buttons = [
+        {
+          label: "🌐 View Page",
+          url: document.location.href
+        }
+      ];
     }
   }
 
