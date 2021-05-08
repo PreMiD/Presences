6e4f988824345f062ef338ec79e32d6d657d1c43
DevAnaxes <anaxesoce@gmail.com>
2021-05-08 14:58:26 +1000

    WidgetStyle presence.

---
+++ websites/W/WidgetStyle/presence.ts
@@ -0,0 +1,43 @@
+var presence = new Presence({
+  clientId: "828789217793409054"
+});
+
+var browsingStamp = Math.floor(Date.now() / 1000);
+
+var user: any;
+var title: any;
+var replace: any;
+var search: any;
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "logo"
+  };
+
+  if (document.location.hostname == "widgetstyle.xyz") {
+    if (document.location.pathname == "/") {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Viewing 🏠 home page";
+    } else if (document.location.pathname.includes("/widgets")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Viewing ⚙ widgets page";
+    } else if (document.location.pathname.includes("/partners")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Viewing 🤝 partners page";
+    } else if (document.location.pathname.includes("/team")) {
+      presenceData.startTimestamp = browsingStamp;
+      presenceData.details = "Viewing 😎 team page";
+    }  else if (document.location.pathname.includes("/user/")) {
+      presenceData.startTimestamp = browsingStamp;
+      user = document.getElementById("username");
+      presenceData.details = `Viewing 👤 user: ${user.innerText}`;
+    }
+  }
+
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
