72a9fb0fb1aaa573e022d1866ab56d66902b7d5f
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-05-13 11:50:11 +0200

    Fixed ADN Once again

--- websites/A/AnimeDigitalNetwork/presence.ts
+++ websites/A/AnimeDigitalNetwork/presence.ts
@@ -1,57 +1,53 @@
 const presence = new Presence({
-    clientId: "808758769424138252"
-  }),
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
+  clientId: "808758769424138252"
+}),
+strings = presence.getStrings({
+  play: "presence.playback.playing",
+  pause: "presence.playback.paused"
+});
 
 presence.on("UpdateData", async () => {
-  const video: HTMLVideoElement = document.querySelector("video.vjs-tech");
+const video: HTMLVideoElement = document.querySelector("video.vjs-tech");
 
-  if (document.location.pathname.includes("video") && video) {
-    if (video && !isNaN(video.duration)) {
-      const title = document.querySelector(
-          "#root > div > div > div.sc-pbWVv.hTvDIL > div > div:nth-child(1) > div.sc-jWJfXU.sc-kbKFCX.ibhpg > div:nth-child(1) > div > div > h1 > a"
-        ).textContent,
-        timestamps = presence.getTimestamps(
-          Math.floor(video.currentTime),
-          Math.floor(video.duration)
-        ),
-        data: PresenceData = {
-          details: title,
-          largeImageKey: "logo",
-          smallImageKey: video.paused ? "pause" : "play",
-          smallImageText: video.paused
-            ? (await strings).pause
-            : (await strings).play,
-          endTimestamp: timestamps[1]
-        };
-
-      if (video.paused) {
-        delete data.startTimestamp;
-        delete data.endTimestamp;
-      }
-
-      if (title !== null) {
-        presence.setActivity(data);
-      }
-    }
-  } else if (document.location.pathname.includes("video") && !video) {
-    const title = document.querySelector(
-        "#root > div > div > div.sc-pbWVv.hTvDIL > div > div:nth-child(1) > div.sc-jWJfXU.sc-kbKFCX.ibhpg > div:nth-child(1) > div > div > h1 > a"
-      ).textContent,
-      data = {
-        details: "Looking at",
-        state: title,
-        largeImageKey: "logo"
+if (document.location.pathname.includes("video") && video) {
+  if (video && !isNaN(video.duration)) {
+    const title = document.querySelector("#root > div > div > div.sc-pbWVv.hTvDIL > div > div:nth-child(1) > div.sc-kbKFCX.sc-kgMcbC.kecmmo > div:nth-child(1) > div > div > h1 > a"),
+      timestamps = presence.getTimestamps(
+        Math.floor(video.currentTime),
+        Math.floor(video.duration)
+      ),
+      data: PresenceData = {
+        details: title.textContent,
+        largeImageKey: "logo",
+        smallImageKey: video.paused ? "pause" : "play",
+        smallImageText: video.paused
+          ? (await strings).pause
+          : (await strings).play,
+        endTimestamp: timestamps[1]
       };
-    presence.setActivity(data);
-  } else {
-    const browsingPresence: PresenceData = {
-      details: "Browsing...",
+
+    if (video.paused) {
+      delete data.startTimestamp;
+      delete data.endTimestamp;
+    }
+
+    if (title !== null) {
+      presence.setActivity(data);
+    }
+  }
+} else if (document.location.pathname.includes("video") && !video) {
+  const title = document.querySelector("#root > div > div > div.sc-pbWVv.hTvDIL > div > div:nth-child(1) > div.sc-kbKFCX.sc-kgMcbC.kecmmo > div:nth-child(1) > div > div > h1 > a"),
+    data = {
+      details: "Looking at",
+      state: title.textContent,
       largeImageKey: "logo"
     };
-    presence.setActivity(browsingPresence);
-  }
+  presence.setActivity(data);
+} else {
+  const browsingPresence: PresenceData = {
+    details: "Browsing...",
+    largeImageKey: "logo"
+  };
+  presence.setActivity(browsingPresence);
+}
 });
