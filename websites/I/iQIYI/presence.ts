6a638f954bfe630adfc3d865db768ec20f19b2ec
SlowLife1661 <slowlife1165@daum.net>
2021-05-13 17:22:53 +0700

    fix(DramaCool, iQIYI): fixed the video page & added fallback for language

--- websites/I/iQIYI/presence.ts
+++ websites/I/iQIYI/presence.ts
@@ -74,7 +74,11 @@ presence.on("UpdateData", async () => {
         JSON.parse(
           document.querySelectorAll('script[type="application/ld+json"]')[1]
             ?.innerHTML || "{}"
-        )[0]?.itemListElement[0]?.item ?? document.URL,
+        )[0]?.itemListElement.map((x: {
+          item: {
+            name: string
+          }
+        }) => x.item.name.toLowerCase()).join(" ") ?? "",
       video: HTMLVideoElement = document.querySelector("video"),
       isMovie = URLItem.includes("movie"),
       isVShow = URLItem.includes("variety-show"),
