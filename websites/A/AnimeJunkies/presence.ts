ca5235fb3d0c6d7bf47d75c2b63d7680c6e9bae8
RedstoneZockt <45714458+RedstoneZockt@users.noreply.github.com>
2021-05-14 19:43:21 +0200

    apply changes

--- websites/A/AnimeJunkies/presence.ts
+++ websites/A/AnimeJunkies/presence.ts
@@ -1,36 +1,27 @@
 const presence = new Presence({ clientId: "842745567930089482" });
 
-let last_path = "",
-  time_stamp: number = Date.now();
-
 presence.on("UpdateData", async () => {
-  const presence_data = {
-    details: "",
-    state: "",
-    smallImageKey: "",
-    smallImageText: ""
+  const presenceData: PresenceData = {
+    largeImageKey: "logo",
+    startTimestamp: Date.now()
   };
-  if (last_path != window.location.pathname) {
-    last_path = window.location.pathname;
-    time_stamp = Date.now();
-  }
   if (window.location.pathname == "/") {
-    presence_data.details = "Erkundet Startseite";
+    presenceData.details = "Erkundet Startseite";
   } else if (window.location.pathname.startsWith("/community/")) {
     const path_name = window.location.pathname;
     if (path_name == "/community/") {
-      presence_data.details = "Erkundet alle Foren";
+      presenceData.details = "Erkundet alle Foren";
     } else if (path_name.startsWith("/community/forum/")) {
       const titles = document.getElementsByClassName("main-title-forum");
       if (titles.length != 0) {
         const title = titles[0].textContent;
-        presence_data.details = "Erkundet Forum";
-        presence_data.state = "» " + title;
+        presenceData.details = "Erkundet Forum";
+        presenceData.state = "» " + title;
       }
     }
   } else if (window.location.pathname.startsWith("/animeliste/")) {
-    presence_data.details = "Durchstöbert";
-    presence_data.state = "» Anime Liste";
+    presenceData.details = "Durchstöbert";
+    presenceData.state = "» Anime Liste";
   } else {
     const x = document.getElementsByClassName("entry-title");
 
@@ -39,22 +30,13 @@ presence.on("UpdateData", async () => {
       document.getElementsByClassName("author-box-header").length != 0
     ) {
       const b = x[0];
-      presence_data.details = "Schaut einen Beitrag an";
-      presence_data.state = b.textContent;
+      presenceData.details = "Schaut einen Beitrag an";
+      presenceData.state = b.textContent;
     } else {
-      presence_data.details = "Erkundet AnimeJunkies";
-      presence_data.state = "";
+      presenceData.details = "Erkundet AnimeJunkies";
+      presenceData.state = "";
     }
   }
-
-  const presenceData: PresenceData = {
-    largeImageKey: "logo",
-    smallImageKey: presence_data.smallImageKey,
-    smallImageText: presence_data.smallImageText,
-    details: presence_data.details,
-    state: presence_data.state,
-    startTimestamp: time_stamp
-  };
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
