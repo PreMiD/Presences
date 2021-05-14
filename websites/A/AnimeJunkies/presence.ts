e913c3c396ad69d4879dd9a233fca0d9159d2cf3
RedstoneZockt <45714458+RedstoneZockt@users.noreply.github.com>
2021-05-14 16:48:21 +0200

    Review Issues

--- websites/A/AnimeJunkies/presence.ts
+++ websites/A/AnimeJunkies/presence.ts
@@ -1,10 +1,10 @@
 const presence = new Presence({clientId: "842745567930089482"});
 
-var last_path = ""
-var time_stamp: number = Date.now();
+let last_path = "",
+    time_stamp: number = Date.now();
 
 presence.on("UpdateData", async () => {
-    var presence_data = {details: "", state: "", smallImageKey: "", smallImageText: ""};
+    let presence_data = {details: "", state: "", smallImageKey: "", smallImageText: ""};
     if(last_path != window.location.pathname) {
 
       last_path = window.location.pathname;
@@ -13,29 +13,29 @@ presence.on("UpdateData", async () => {
     }
     if(window.location.pathname == "/") {
 
-      presence_data.details = "Erkundet Startseite"
+      presence_data.details = "Erkundet Startseite";
 
     }else if(window.location.pathname.startsWith("/community/")) {
 
-      var path_name = window.location.pathname;
+      let path_name = window.location.pathname;
       if(path_name == "/community/") {
         presence_data.details = "Erkundet alle Foren";
       }else if(path_name.startsWith("/community/forum/")) {
-        let titles = document.getElementsByClassName("main-title-forum");
+        const titles = document.getElementsByClassName("main-title-forum");
         if(titles.length != 0) {
-          var title = titles[0].textContent;
-          presence_data.details = "Erkundet Forum"
+          const title = titles[0].textContent;
+          presence_data.details = "Erkundet Forum";
           presence_data.state = "» " + title;
         }
       }
 
     }else {
 
-      var x = document.getElementsByClassName("entry-title");
+      let x = document.getElementsByClassName("entry-title");
       
       if(x.length != 0 && document.getElementsByClassName("author-box-header").length != 0) {
         
-        let b = x[0];
+        const b = x[0];
         presence_data.details = "Schaut einen Beitrag an";
         presence_data.state = b.textContent;
 
@@ -56,14 +56,12 @@ presence.on("UpdateData", async () => {
       details: presence_data.details, 
       state: presence_data.state, 
       startTimestamp: time_stamp
-    }
+    };
   
     if (presenceData.details == null) {
-      //This will fire if you do not set presence details
-      presence.setTrayTitle(); //Clears the tray title for mac users
-      presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+      presence.setTrayTitle(); 
+      presence.setActivity(); 
     } else {
-      //This will fire if you set presence details
-      presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+      presence.setActivity(presenceData); 
     }
 });
