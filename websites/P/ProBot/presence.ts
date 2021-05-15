19fe5a73d0fbe7bcd15c3f6b7f3bfaf378608b85
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-05-15 08:25:44 +0200

    Recoded most of the presence

--- websites/P/ProBot/presence.ts
+++ websites/P/ProBot/presence.ts
@@ -1,47 +1,90 @@
 const presence = new Presence({
   clientId: "655050505726197781"
-});
-
-const browsingStamp = Math.floor(Date.now() / 1000);
+}), browsingStamp = Math.floor(Date.now() / 1000), page = window.location.pathname, page2 = document.title, e1 = document.querySelector("#root > div > div > div.page-wrapper.inside-panel > div > div:nth-child(1) > div > div > div.moduleTitle.txt-light > h4");
 
 presence.on("UpdateData", () => {
   const presenceData: PresenceData = {
-    largeImageKey: "probot"
+    largeImageKey: "probot",
+    startTimestamp: browsingStamp
   };
 
-  if (document.location.hostname == "probot.io") {
-    presenceData.startTimestamp = browsingStamp;
-    if (document.location.pathname.includes("/server/")) {
+  const l = document.querySelector("#root > div > div > div.page-wrapper.inside-panel > div > div:nth-child(1) > div > div > div.moduleTitle.txt-light > h4"),
+  l2 = document.querySelector("#root > div > div > div.page-wrapper.inside-panel > div > div:nth-child(1) > div > div.pt-25.moduleS > div.moduleTitle.txt-light > h4"),
+  l3 = document.querySelector("#root > div > div > div.page-wrapper.inside-panel > div > div:nth-child(1) > div > div.moduleTitle.txt-light > h4");
+
+    if (page.includes("/server")) {
+      if (l3) {
+        if (l3.textContent.includes("Reaction Roles")) {
+          presenceData.details = "Managing:";
+          presenceData.state = "Reaction Roles Settings";
+      }
+    } else {
+      if (page2.includes("settings")) {
       presenceData.details = "Managing the settings of";
-      const title = document.querySelector(
-        "#root > div > div > div.sidebar_root > div.sidebar_main > div > div.dramex2 > div > ul > li:nth-child(1) > div > div > a"
-      );
-      presenceData.state = "server: " + title.innerHTML;
-    } else if (document.location.pathname.includes("/commands")) {
+      const title = document.querySelector("#root > div > div > div.sidebar_root > div.sidebar_main > div > div.dramex2 > div > ul > li:nth-child(1) > div > div.dropdown.mt-5 > a");
+      presenceData.state = "server: " + title.textContent; 
+    } else if (page2.includes("embed")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Embeds";
+    } else if (l.textContent.includes("Utility")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Utility Settings"; 
+    } else if (l2.textContent.includes("Mod")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Moderation Settings";
+    } else if (l2.textContent.includes("Automod")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Automod Settings";
+    } else if (l2.textContent.includes("Welcomer")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Welcomer Settings"; 
+    } else if (l2.textContent.includes("Auto Responder")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Auto Responder Settings";
+    } else if (l2.textContent.includes("Leveling System")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Leveling Settings";
+    } else if (l2.textContent.includes("Logs")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Logging Settings";
+    } else if (l2.textContent.includes("Colors")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Color Settings";
+    } else if (l2.textContent.includes("Auto Roles")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Auto Roles Settings";
+    } else if (l.textContent.includes("Music")) {
+      presenceData.details = "Managing:";
+      presenceData.state = "Music Settings";
+    } else {
+      presenceData.details = "Viewing:";
+      presenceData.state = "Server Overview";
+    }
+  }
+    } else if (page.includes("/commands")) {
       presenceData.details = "Viewing a page:";
       presenceData.state = "Commands";
-    } else if (document.location.pathname.includes("/premium")) {
+    } else if (page.includes("/premium")) {
       presenceData.details = "Viewing Premium";
-    } else if (document.location.pathname.includes("/bg")) {
+    } else if (page.includes("/bg")) {
       presenceData.details = "Managing the";
       presenceData.state = "profile backgrounds";
-    } else if (document.location.pathname.includes("/id")) {
+    } else if (page.includes("/id")) {
       presenceData.details = "Managing the";
       presenceData.state = "ID backgrounds";
-    } else if (document.location.pathname.includes("/badges")) {
+    } else if (page.includes("/badges")) {
       presenceData.details = "Managing the";
       presenceData.state = "profile badges";
-    } else if (document.location.pathname.includes("/xp")) {
+    } else if (page.includes("/xp")) {
       presenceData.details = "Viewing a page:";
       presenceData.state = "Top 100 by XP";
-    } else if (document.location.pathname.includes("/credits")) {
+    } else if (page.includes("/credits")) {
       presenceData.details = "Viewing a page:";
       presenceData.state = "Richest 100 Billionaires";
-    } else if (document.location.pathname.includes("/transactions")) {
+    } else if (page.includes("/transactions")) {
       presenceData.details = "Viewing a page:";
       presenceData.state = "Credits Transactions";
     }
-  }
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
