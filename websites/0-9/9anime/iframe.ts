4b741fa86e6179805514d4abe0b000e863852783
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-05-15 11:22:10 +0200

    Fixed Issue

--- websites/0-9/9anime/iframe.ts
+++ websites/0-9/9anime/iframe.ts
@@ -3,11 +3,12 @@ const iframe = new iFrame();
 iframe.on("UpdateData", async () => {
   if (document.querySelector("video")) {
     const video: HTMLVideoElement = document.querySelector("video");
+    const paused: HTMLElement = document.querySelector("#player");
     if (video != null && !isNaN(video.duration)) {
       iframe.send({
         duration: video.duration,
         currentTime: video.currentTime,
-        paused: video.paused
+        paused: paused.textContent.includes("playing")
       });
     }
   }
