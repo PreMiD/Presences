21da874d92d9ff7d61e1a957d2423c3c505f7c54
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-05-18 09:55:43 +0200

    Reverted Last Changes
    Reverted Last Changes

--- websites/0-9/9anime/iframe.ts
+++ websites/0-9/9anime/iframe.ts
@@ -3,12 +3,11 @@ const iframe = new iFrame();
 iframe.on("UpdateData", async () => {
   if (document.querySelector("video")) {
     const video: HTMLVideoElement = document.querySelector("video");
-    const paused: HTMLElement = document.querySelector("#player");
     if (video != null && !isNaN(video.duration)) {
       iframe.send({
         duration: video.duration,
         currentTime: video.currentTime,
-        paused: paused.textContent.includes("playing")
+        paused: video.paused
       });
     }
   }
