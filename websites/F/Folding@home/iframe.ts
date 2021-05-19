dd02f2e11da03cff6beb20500dbbb4b76223c620
DarkVillager <42322979+DarkVillager@users.noreply.github.com>
2021-05-19 10:47:59 +0200

    Fixed Folding@Home & Bumped Version
    Fixed Folding@Home & Bumped Version

---
+++ websites/F/Folding@home/iframe.ts
@@ -0,0 +1,14 @@
+const iframe = new iFrame();
+
+iframe.on("UpdateData", async () => {
+  const l1 = document.querySelector("#box-points-team > a"),
+  l2 = document.querySelector("#slot-0 > div.pu > div.pu-bar.ui-progressbar.ui-widget.ui-widget-content.ui-corner-all > div")
+  if (l1 && l2 !== undefined) {
+  iframe.send({
+    iframe_d: {
+ points: l1.textContent,
+ progress: l2.textContent,
+    }
+})
+  }
+});
--- websites/F/Folding@home/presence.ts
+++ websites/F/Folding@home/presence.ts
@@ -1,21 +1,28 @@
-var presence = new Presence({
-  clientId: "704385469856612523"
+const presence = new Presence({
+  clientId: "812025934617509949"
 });
+let points: string,
+  progress: string;
+presence.on("iFrameData", (data: IFrameData) => {
+  points = data.iframe_d.points
+  progress = data.iframe_d.progress
 
+});
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey: "logo"
   };
-  setTimeout(() => {
-  if (document.location.hostname == "client.foldingathome.org") {
-      
 
-
+  if (document.location.hostname == "127.0.0.1") {
     presenceData.details = document.querySelector("#box-points-team > a").textContent;
     presenceData.state = document.querySelector("#slot-0 > div.pu > div.pu-bar.ui-progressbar.ui-widget.ui-widget-content.ui-corner-all > div").textContent;
+  } else {
+
+  console.log(`Points: ${points}, Progress: ${progress},`)
+    presenceData.details = points;
+    presenceData.state = progress;
   }
-}, 20000);
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
@@ -23,3 +30,10 @@ presence.on("UpdateData", async () => {
     presence.setActivity(presenceData);
   }
 });
+
+interface IFrameData {
+  iframe_d: {
+    points: string;
+    progress: string;
+  };
+}
