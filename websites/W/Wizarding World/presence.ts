23fb18fab40019a0bfbbb4359838a1d76af48ac1
SeMiD <semid@premid.app>
2021-05-19 03:26:24 +0200

    ♻️ Refactoring (#3772)
    Signed-off-by: SeMiD <contact@premid.app>
    
    Co-authored-by: SeMiD <contact@premid.app>

--- websites/W/Wizarding World/presence.ts
+++ websites/W/Wizarding World/presence.ts
@@ -5,26 +5,27 @@ const presence = new Presence({
 
 presence.on("UpdateData", async function () {
   const setting = {
-          timeElapsed: await presence.getSetting("timeElapsed"),
-          showButtons: await presence.getSetting("showButtons")
-        },
-        urlpath = window.location.pathname.split("/"),
-        presenceData: PresenceData = {
-              largeImageKey: "logo"
-            };
+      timeElapsed: await presence.getSetting("timeElapsed"),
+      showButtons: await presence.getSetting("showButtons")
+    },
+    urlpath = window.location.pathname.split("/"),
+    presenceData: PresenceData = {
+      largeImageKey: "logo"
+    };
 
-  if(setting.timeElapsed) presenceData.startTimestamp = browsingStamp;
+  if (setting.timeElapsed) presenceData.startTimestamp = browsingStamp;
 
-  if(document.location.host === 'www.wizardingworld.com') {
-    if(!urlpath[1])
-      presenceData.details = "Home";
-    else if(urlpath[1] === 'news' || urlpath[1] === 'features') {
-      presenceData.details = (urlpath[1] === 'news') ? 'News' : 'Features';
+  if (document.location.host === "www.wizardingworld.com") {
+    if (!urlpath[1]) presenceData.details = "Home";
+    else if (urlpath[1] === "news" || urlpath[1] === "features") {
+      presenceData.details = urlpath[1] === "news" ? "News" : "Features";
 
-      if(urlpath[2]) {
-        presenceData.state = document.querySelector("h1.ArticleHero_title__cOam6")?.textContent || "Unknown";
+      if (urlpath[2]) {
+        presenceData.state =
+          document.querySelector("h1.ArticleHero_title__cOam6")?.textContent ||
+          "Unknown";
 
-        if(setting.showButtons) {
+        if (setting.showButtons) {
           presenceData.buttons = [
             {
               label: "View Article",
@@ -33,13 +34,15 @@ presence.on("UpdateData", async function () {
           ];
         }
       }
-    } else if(urlpath[1] === 'quiz') {
-      presenceData.details = 'Quiz';
+    } else if (urlpath[1] === "quiz") {
+      presenceData.details = "Quiz";
 
-      if(urlpath[2]) {
-        presenceData.state = document.querySelector("h1.ArticleHero_title__cOam6")?.textContent || "Unknown";
+      if (urlpath[2]) {
+        presenceData.state =
+          document.querySelector("h1.ArticleHero_title__cOam6")?.textContent ||
+          "Unknown";
 
-        if(setting.showButtons) {
+        if (setting.showButtons) {
           presenceData.buttons = [
             {
               label: "View Quiz",
@@ -48,16 +51,19 @@ presence.on("UpdateData", async function () {
           ];
         }
       }
-    } else if(urlpath[1] === 'writing-by-jk-rowling') {
-      presenceData.details = 'J.K. Rowling Originals';
+    } else if (urlpath[1] === "writing-by-jk-rowling") {
+      presenceData.details = "J.K. Rowling Originals";
 
-      if(urlpath[2]) {
+      if (urlpath[2]) {
         const post = document.querySelector("h1.ArticleHero_title__cOam6"),
-            label = (post?.textContent.length >= 15) ? post?.textContent.substring(0, 15) + "..." : post?.textContent;
+          label =
+            post?.textContent.length >= 15
+              ? post?.textContent.substring(0, 15) + "..."
+              : post?.textContent;
 
         presenceData.state = post?.textContent || "Unknown";
 
-        if(setting.showButtons && post) {
+        if (setting.showButtons && post) {
           presenceData.buttons = [
             {
               label: `View: ${label}`,
@@ -66,21 +72,29 @@ presence.on("UpdateData", async function () {
           ];
         }
       }
-    } else if(urlpath[1] === 'discover') {
-      presenceData.details = 'Discover';
+    } else if (urlpath[1] === "discover") {
+      presenceData.details = "Discover";
 
-      if(urlpath[2] === 'books' || urlpath[2] === 'films' || urlpath[2] === 'portkey-games' || urlpath[2] === 'on-stage' || urlpath[2] === 'experiences') {
-        if(urlpath[3]) {
+      if (
+        urlpath[2] === "books" ||
+        urlpath[2] === "films" ||
+        urlpath[2] === "portkey-games" ||
+        urlpath[2] === "on-stage" ||
+        urlpath[2] === "experiences"
+      ) {
+        if (urlpath[3]) {
           let ctopic = "Loading...";
 
-          presenceData.state = document.querySelector("h1.Header_productName__8oV2G")?.textContent || "Unknown";
+          presenceData.state =
+            document.querySelector("h1.Header_productName__8oV2G")
+              ?.textContent || "Unknown";
 
-          if(urlpath[2] === 'books') ctopic = "Book";
-          else if(urlpath[2] === 'films') ctopic = "Film";
-          else if(urlpath[2] === 'portkey-games') ctopic = "Game";
-          else if(urlpath[2] === 'experiences') ctopic = "Experience";
+          if (urlpath[2] === "books") ctopic = "Book";
+          else if (urlpath[2] === "films") ctopic = "Film";
+          else if (urlpath[2] === "portkey-games") ctopic = "Game";
+          else if (urlpath[2] === "experiences") ctopic = "Experience";
 
-          if(setting.showButtons && urlpath[2] !== 'on-stage') {
+          if (setting.showButtons && urlpath[2] !== "on-stage") {
             presenceData.buttons = [
               {
                 label: `View ${ctopic}`,
@@ -89,15 +103,19 @@ presence.on("UpdateData", async function () {
             ];
           }
         } else
-          presenceData.state = document.querySelector("h1.DiscoverListHeader_header__3ivqr")?.textContent || "Unknown";
+          presenceData.state =
+            document.querySelector("h1.DiscoverListHeader_header__3ivqr")
+              ?.textContent || "Unknown";
       }
-    } else if(urlpath[1] === 'collections') {
-      presenceData.details = 'Collections';
+    } else if (urlpath[1] === "collections") {
+      presenceData.details = "Collections";
 
-      if(urlpath[2]) {
-        presenceData.state = document.querySelector("h1.CollectionHero_header__3rDGu")?.textContent || "Unknown";
+      if (urlpath[2]) {
+        presenceData.state =
+          document.querySelector("h1.CollectionHero_header__3rDGu")
+            ?.textContent || "Unknown";
 
-        if(setting.showButtons) {
+        if (setting.showButtons) {
           presenceData.buttons = [
             {
               label: "View Collection",
@@ -106,17 +124,19 @@ presence.on("UpdateData", async function () {
           ];
         }
       }
-    } else if(urlpath[1] === 'harry-potter-fan-club')
-      presenceData.details = 'Fan Club';
-    else
-      presenceData.details = 'Other';
-  } else if(document.location.host === 'my.wizardingworld.com') {
-    if(urlpath[1] === 'passport')
-      presenceData.details = "Passport";
+    } else if (urlpath[1] === "harry-potter-fan-club")
+      presenceData.details = "Fan Club";
+    else presenceData.details = "Other";
+  } else if (document.location.host === "my.wizardingworld.com") {
+    if (urlpath[1] === "passport") presenceData.details = "Passport";
 
-    if(document.querySelector("span.HogwartsHouse_houseName__CykI1")) {
-      presenceData.smallImageKey = document.querySelector("span.HogwartsHouse_houseName__CykI1")?.textContent.toLocaleLowerCase();
-      presenceData.smallImageText = document.querySelector("span.HogwartsHouse_houseName__CykI1")?.textContent;
+    if (document.querySelector("span.HogwartsHouse_houseName__CykI1")) {
+      presenceData.smallImageKey = document
+        .querySelector("span.HogwartsHouse_houseName__CykI1")
+        ?.textContent.toLocaleLowerCase();
+      presenceData.smallImageText = document.querySelector(
+        "span.HogwartsHouse_houseName__CykI1"
+      )?.textContent;
     }
   }
 
