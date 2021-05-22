31a66453705c0611c3478c4743da453d417872af
MrBartusek <bartusekcraft@gmail.com>
2021-05-22 13:07:56 +0200

    `==` => `===`

--- websites/Z/ZombsRoyale.io/presence.ts
+++ websites/Z/ZombsRoyale.io/presence.ts
@@ -10,56 +10,56 @@ presence.on("UpdateData", async () => {
   },
   currentGameState = await getPageletiable("game.currentGameState");
 
-  if (currentGameState == "loading" || currentGameState == "UiLoadingOverlay") {
+  if (currentGameState === "loading" || currentGameState === "UiLoadingOverlay") {
     presenceData.details = "Loading...";
-  } else if (currentGameState == "MainMenu") {
+  } else if (currentGameState === "MainMenu") {
     presenceData.details = "Lurking in main menu";
-  } else if (currentGameState == "Cosmetics" || currentGameState == "UiCosmeticSelectorOverlay") {
+  } else if (currentGameState === "Cosmetics" || currentGameState === "UiCosmeticSelectorOverlay") {
     presenceData.details = "Viewing cosmetics";
-  } else if (currentGameState == "Profile") {
+  } else if (currentGameState === "Profile") {
     presenceData.details = "Viewing their profile";
-  } else if (currentGameState == "Shop" || currentGameState == "UiPreviewPackOverlay") {
+  } else if (currentGameState === "Shop" || currentGameState === "UiPreviewPackOverlay") {
     presenceData.details = "Browsing Shop";
-  } else if (currentGameState == "Friends" || currentGameState == "UiFriendAddOverlay") {
+  } else if (currentGameState === "Friends" || currentGameState === "UiFriendAddOverlay") {
     presenceData.details = "Viewing their friends";
-  } else if (currentGameState == "Leaderboards") {
+  } else if (currentGameState === "Leaderboards") {
     presenceData.details = "Browsing leaderboards";
-  } else if (currentGameState == "UiLoginOverlay") {
+  } else if (currentGameState === "UiLoginOverlay") {
     presenceData.details = "Loging in";
-  } else if (currentGameState == "UiSettingsOverlay") {
+  } else if (currentGameState === "UiSettingsOverlay") {
     presenceData.details = "Changing settings";
-  } else if (currentGameState == "UiSeasonPurchaseOverlay") {
+  } else if (currentGameState === "UiSeasonPurchaseOverlay") {
     presenceData.details = "Buying a Battle Pass";
-  } else if (currentGameState.startsWith("UiSeason") || currentGameState == "UiPreviewItemOverlay") {
+  } else if (currentGameState.startsWith("UiSeason") || currentGameState === "UiPreviewItemOverlay") {
     presenceData.details = "Viewing Battle Pass";
-  } else if (currentGameState == "UiChallengesOverlay") {
+  } else if (currentGameState === "UiChallengesOverlay") {
     presenceData.details = "Viewing challenges";
-  } else if (currentGameState == "UiBuyGemsOverlay") {
+  } else if (currentGameState === "UiBuyGemsOverlay") {
     presenceData.details = "Buying gems";
-  } else if (currentGameState == "VideoAd") {
+  } else if (currentGameState === "VideoAd") {
     presenceData.details = "Watching video ad";
-  } else if (currentGameState == "Countdown" || currentGameState == "Lobby") {
+  } else if (currentGameState === "Countdown" || currentGameState === "Lobby") {
     presenceData.details = "In Lobby";
     presenceData.state = "Waiting for game to start";
-  } else if (currentGameState == "Game" || currentGameState == "UiReportPlayerOverlay" || currentGameState == "UiLeaveOverlay") {
+  } else if (currentGameState === "Game" || currentGameState === "UiReportPlayerOverlay" || currentGameState === "UiLeaveOverlay") {
     presenceData.details = "In Game";
-  } else if (currentGameState == "Plane") {
+  } else if (currentGameState === "Plane") {
     presenceData.details = "In Game";
     presenceData.state = "Flying plane";
-  } else if (currentGameState == "Parachute") {
+  } else if (currentGameState === "Parachute") {
     presenceData.details = "In Game";
     presenceData.state = "Parachuting";
-  } else if (currentGameState == "Dead" || currentGameState == "UiGameOver" || currentGameState == "UiSpectator" || currentGameState == "UiFeedbackOverlay") {
+  } else if (currentGameState === "Dead" || currentGameState === "UiGameOver" || currentGameState === "UiSpectator" || currentGameState === "UiFeedbackOverlay") {
     presenceData.details = "In Game";
     presenceData.state = "Dead";
-  } else if (currentGameState == "UiMapOverlay") {
+  } else if (currentGameState === "UiMapOverlay") {
     presenceData.details = "In Game";
     presenceData.state = "Viewing map";
   }else {
     presence.error("Unknown state: " + currentGameState);
   }
 
-  const playing = presenceData.details == "In Game" || presenceData.details == "In Lobby";
+  const playing = presenceData.details === "In Game" || presenceData.details === "In Lobby";
 
   if(playing && matchStart === null)
   {
@@ -72,7 +72,7 @@ presence.on("UpdateData", async () => {
 
   presenceData.startTimestamp = matchStart;
 
-  if (presenceData.details == null) {
+  if (presenceData.details === null) {
     presence.setTrayTitle();
     presence.setActivity();
   } else {
