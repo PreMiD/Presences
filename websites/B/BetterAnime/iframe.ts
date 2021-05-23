5a4aa6b3de41c8a7b62223f754f4bc8440a90c45
Slowlife <slowlife1165@daum.net>
2021-05-24 01:23:10 +0700

    fix(BetterAnime): fixed the video page (#3793)
    * fix(DramaCool, iQIYI): fixed the video page & added fallback for language
    
    * fallback
    
    * feat: Added Emanate
    
    * ;
    
    * fix(BetterAnime): fixed the video page
    
    They moved video into iframe

---
+++ websites/B/BetterAnime/iframe.ts
@@ -0,0 +1,9 @@
+((iframe) => iframe.on("UpdateData", () => {
+  const video = document.querySelector("video");
+  if (video)
+    iframe.send({
+      duration: video.duration,
+      currentTime: video.currentTime,
+      paused: video.paused
+    });
+}))(new iFrame());
