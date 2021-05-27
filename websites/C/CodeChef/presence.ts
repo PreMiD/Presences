0d576c45d5594caecfc25db92483a574e885d5f7
Avinash <avinashdhillor24@gmail.com>
2021-05-27 23:47:40 +0530

    updated as per suggestion

--- websites/C/CodeChef/presence.ts
+++ websites/C/CodeChef/presence.ts
@@ -1,121 +1,128 @@
 const presence = new Presence({
     clientId: "845716323296083999"
   }),
-  timeElapsed: number = ~~Date.now() / 1000;
+  timeElapsed: number = ~~(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
       largeImageKey: "logo",
       startTimestamp: timeElapsed
     },
-    domain = location.host;
+    domain = location.host.split(".");
 
-  if (domain.includes("blog")) {
-    // ? https://blog.codechef.com
-    presenceData.details = "Viewing Blogs";
-    presenceData.smallImageKey = "reading";
-    presenceData.smallImageText = "Reading";
-    if (location.pathname.split("/")[1] === "all-blogs")
-      presenceData.details = "Viewing All Blogs";
-    else if (location.pathname.split("/")[1] === "author") {
-      presenceData.details = "Viewing Profile: ";
-      presenceData.state = "Author: " + location.pathname.split("/")[2];
-    } else if (location.pathname.split("/")[4]) {
-      presenceData.details =
-        "Reading: " + document.querySelector(".posttitle").textContent;
-      presenceData.state =
-        "Author: " + document.querySelector(".author-username").textContent;
+  switch (domain[0]) {
+    case "blog": {
+      // ? https://blog.codechef.com
+      presenceData.details = "Viewing Blogs";
       presenceData.smallImageKey = "reading";
-      presenceData.smallImageText = "Reading";
+      if (location.pathname.split("/")[1] === "all-blogs")
+        presenceData.details = "Viewing All Blogs";
+      else if (location.pathname.split("/")[1] === "author") {
+        presenceData.details = "Viewing Profile: ";
+        presenceData.state = "Author: " + location.pathname.split("/")[2];
+      } else if (location.pathname.split("/")[4]) {
+        presenceData.details =
+          "Reading: " + document.querySelector(".posttitle").textContent;
+        presenceData.state =
+          "Author: " + document.querySelector(".author-username").textContent;
+        presenceData.smallImageKey = "reading";
+      }
+      break;
     }
-  } else if (domain.includes("discuss")) {
-    // ? https://discuss.codechef.com
-    presenceData.details = "Viewing Discussions";
-    if (location.pathname.split("/")[1] === "t") {
-      const discussionTopicHeading: string =
-        document.querySelector(".fancy-title").textContent;
-      presenceData.details = "Reading Discussions:";
-      presenceData.state = discussionTopicHeading;
-      presenceData.smallImageKey = "reading";
-      presenceData.smallImageText = "Reading";
+
+    case "discuss": {
+      // ? https://discuss.codechef.com
+      presenceData.details = "Viewing Discussions";
+      if (location.pathname.split("/")[1] === "t") {
+        const discussionTopicHeading: string =
+          document.querySelector(".fancy-title").textContent;
+        presenceData.details = "Reading Discussions:";
+        presenceData.state = discussionTopicHeading;
+        presenceData.smallImageKey = "reading";
+      }
+      break;
     }
-  } else if (location.pathname.split("/")[1] == "")
-    presenceData.details = "Viewing Home Page";
-  else if (location.pathname.split("/").includes("tags")) {
-    presenceData.details = "Viewing:";
-    presenceData.state = "Problems By Tag Name";
-  } else if (location.pathname.split("/").includes("problems")) {
-    const contestName: string =
-        document.querySelector(".breadcrumbs").lastElementChild.textContent,
-      problemName: string = document.title.split("|")[0];
-    presenceData.details = contestName;
-    presenceData.state = "Solving: " + problemName;
-    presenceData.smallImageKey = "code";
-    presenceData.smallImageText = "coding";
-  } else {
-    // ? https://codechef.com
-    switch (location.pathname.split("/")[1]) {
-      case "ide": {
-        const compilerName: string =
-          document.querySelector(".chosen-single").childNodes[0].textContent;
-        presenceData.details = "Using IDE";
-        presenceData.state = "Editing " + compilerName + " file";
+
+    default: {
+      if (location.pathname.split("/")[1] == "")
+        presenceData.details = "Viewing Home Page";
+      else if (location.pathname.split("/").includes("tags")) {
+        presenceData.details = "Viewing:";
+        presenceData.state = "Problems By Tag Name";
+      } else if (location.pathname.split("/").includes("problems")) {
+        const contestName: string =
+            document.querySelector(".breadcrumbs").lastElementChild.textContent,
+          problemName: string = document.title.split("|")[0];
+        presenceData.details = contestName;
+        presenceData.state = "Solving: " + problemName;
         presenceData.smallImageKey = "code";
-        presenceData.smallImageText = "coding";
-        break;
-      }
-      case "AIMICPC": {
-        presenceData.details = "Viewing:";
-        presenceData.state = "AIM ICPC - Weekly Training Series";
-        break;
-      }
+      } else {
+        // ? https://codechef.com
+        switch (location.pathname.split("/")[1]) {
+          case "ide": {
+            const compilerName: string =
+              document.querySelector(".chosen-single").childNodes[0]
+                .textContent;
+            presenceData.details = "Using IDE";
+            presenceData.state = "Editing " + compilerName + " file";
+            presenceData.smallImageKey = "code";
+            break;
+          }
 
-      case "LEARNDSA": {
-        presenceData.details = "Viewing:";
-        presenceData.state = "DSA Learning Series";
-        break;
-      }
+          case "AIMICPC": {
+            presenceData.details = "Viewing:";
+            presenceData.state = "AIM ICPC - Weekly Training Series";
+            break;
+          }
 
-      case "contests": {
-        presenceData.details = "Viewing:";
-        presenceData.state = "Contests List";
-        break;
-      }
+          case "LEARNDSA": {
+            presenceData.details = "Viewing:";
+            presenceData.state = "DSA Learning Series";
+            break;
+          }
 
-      case "certification": {
-        const pageHeading: string =
-          document.querySelector(".page-title").childNodes[3].textContent;
-        presenceData.details = "Viewing Certification: ";
-        presenceData.state = pageHeading;
-        break;
-      }
+          case "contests": {
+            presenceData.details = "Viewing:";
+            presenceData.state = "Contests List";
+            break;
+          }
 
-      case "careers": {
-        presenceData.details = "Viewing:";
-        presenceData.state = "Careers Page";
-        break;
-      }
+          case "certification": {
+            const pageHeading: string =
+              document.querySelector(".page-title").childNodes[3].textContent;
+            presenceData.details = "Viewing Certification: ";
+            presenceData.state = pageHeading;
+            break;
+          }
 
-      case "signup": {
-        presenceData.details = "Creating an account";
-        break;
-      }
+          case "careers": {
+            presenceData.details = "Viewing:";
+            presenceData.state = "Careers Page";
+            break;
+          }
 
-      case "wiki": {
-        const wikiTopicHeading: string =
-          document.querySelector(".ns-heading").textContent;
-        presenceData.details = "Viewing Wiki:";
-        presenceData.state = wikiTopicHeading;
-        break;
-      }
+          case "signup": {
+            presenceData.details = "Creating an account";
+            break;
+          }
 
-      case "users": {
-        const displayName: string =
-            document.querySelector("header > h2").textContent,
-          userName: string = document.title.split("|")[0];
-        presenceData.details = "Viewing Profile:";
-        presenceData.state = `${userName} (${displayName})`;
-        break;
+          case "wiki": {
+            const wikiTopicHeading: string =
+              document.querySelector(".ns-heading").textContent;
+            presenceData.details = "Viewing Wiki:";
+            presenceData.state = wikiTopicHeading;
+            break;
+          }
+
+          case "users": {
+            const displayName: string =
+                document.querySelector("header > h2").textContent,
+              userName: string = document.title.split("|")[0];
+            presenceData.details = "Viewing Profile:";
+            presenceData.state = `${userName} (${displayName})`;
+            break;
+          }
+        }
       }
     }
   }
