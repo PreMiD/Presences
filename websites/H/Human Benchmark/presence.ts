c0f5079daf86b5f802e8ca6a3d319500eb2863e9
ririxi <mail@ririxi.dev>
2021-05-27 21:30:01 +0200

    🚨 Human Benchmark

--- websites/H/Human Benchmark/presence.ts
+++ websites/H/Human Benchmark/presence.ts
@@ -7,47 +7,44 @@ let details: string,
   state: string,
   lives: string,
   level: string,
-  strikes: string = "0 of 3",
-  numbers: string = "4";
+  strikes = "0 of 3",
+  numbers = "4";
 
 presence.on("UpdateData", async () => {
-  const presenceData: PresenceData = {
-      largeImageKey: "hb"
-    },
+  const presenceData: PresenceData = { largeImageKey: "hb" },
     showTime: boolean = await presence.getSetting("stamp");
 
-  presenceData.startTimestamp = showTime ? browsingStamp : null;
-  if (presenceData.startTimestamp == null) delete presenceData.startTimestamp;
+  if (showTime) presenceData.startTimestamp = browsingStamp;
 
-  if (document.location.pathname == "/") {
+  if (document.location.pathname === "/") {
     details = "Browsing...";
     state = "On the homepage";
-  } else if (document.location.pathname == "/tests/reactiontime") {
-    if (document.querySelector(".css-0") == null) {
+  } else if (document.location.pathname === "/tests/reactiontime") {
+    if (!document.querySelector(".css-0")) {
       details = "Testing...";
       state = "Reaction Speed";
     } else {
       details = "Viewing Results (Reaction Speed)";
       state = `Time: ${document.querySelector(".css-0").innerHTML}`;
     }
-  } else if (document.location.pathname == "/tests/sequence") {
-    if (document.querySelector(".css-0") == null) {
+  } else if (document.location.pathname === "/tests/sequence") {
+    if (!document.querySelector(".css-0")) {
       details = "Testing...";
       state = "Sequence Memory";
     } else {
       details = "Viewing Results (Sequence Memory)";
       state = `${document.querySelector(".css-0").innerHTML}`;
     }
-  } else if (document.location.pathname == "/tests/aim") {
-    if (document.querySelector(".css-0") == null) {
+  } else if (document.location.pathname === "/tests/aim") {
+    if (!document.querySelector(".css-0")) {
       details = "Playing...";
       state = "Aim Trainer";
     } else {
       details = "Viewing Results (Aim Trainer)";
       state = `Average Time: ${document.querySelector(".css-0").innerHTML}`;
     }
-  } else if (document.location.pathname == "/tests/number-memory") {
-    if (document.querySelector(".level") == null) {
+  } else if (document.location.pathname === "/tests/number-memory") {
+    if (!document.querySelector(".level")) {
       details = "Testing...";
       state = "Number Memory";
     } else {
@@ -56,33 +53,33 @@ presence.on("UpdateData", async () => {
         document.querySelector(".level > .number").innerHTML
       }`;
     }
-  } else if (document.location.pathname == "/tests/chimp") {
+  } else if (document.location.pathname === "/tests/chimp") {
     if (
-      document.querySelector(".css-0") == null &&
-      document.querySelector(".css-1tl77r2") == null
+      !document.querySelector(".css-0") &&
+      !document.querySelector(".css-1tl77r2")
     ) {
       details = "Testing...";
       state = `Chimp Test (${numbers} numbers, ${strikes} strikes)`;
-    } else if (document.querySelector(".css-1tl77r2") != null) {
+    } else if (document.querySelector(".css-1tl77r2")) {
       numbers = document.querySelector(".css-0").innerHTML;
       strikes = document.querySelector(".css-1tl77r2").innerHTML;
       details = "Testing...";
       state = `Chimp Test (${numbers} numbers, ${strikes} strikes)`;
     } else if (
-      document.querySelector(".css-1tl77r2") == null &&
-      document.querySelector(".css-0") != null
+      !document.querySelector(".css-1tl77r2") &&
+      document.querySelector(".css-0")
     ) {
       details = "Viewing Results (Chimp Test)";
       state = `Score: ${document.querySelector(".css-0").innerHTML}`;
     }
-  } else if (document.location.pathname == "/tests/memory") {
+  } else if (document.location.pathname === "/tests/memory") {
     if (
-      document.querySelector(".css-0") == null &&
-      document.querySelector(".big-score") == null
+      !document.querySelector(".css-0") &&
+      !document.querySelector(".big-score")
     ) {
       details = "Testing...";
-      state = `Visual Memory`;
-    } else if (document.querySelector(".big-score") != null) {
+      state = "Visual Memory";
+    } else if (document.querySelector(".big-score")) {
       lives = document
         .querySelector(".big-score")
         .getElementsByClassName("score")
@@ -101,8 +98,8 @@ presence.on("UpdateData", async () => {
       details = "Viewing Results (Visual Memory)";
       state = `${document.querySelector(".css-0").innerHTML}`;
     }
-  } else if (document.location.pathname == "/tests/hearing") {
-    if (document.querySelector(".css-0") == null) {
+  } else if (document.location.pathname === "/tests/hearing") {
+    if (!document.querySelector(".css-0")) {
       details = "Testing...";
       state = "Hearing";
     } else {
@@ -111,8 +108,8 @@ presence.on("UpdateData", async () => {
         document.querySelector(".css-0").innerHTML
       }`;
     }
-  } else if (document.location.pathname == "/tests/typing") {
-    if (document.querySelector(".css-0") == null) {
+  } else if (document.location.pathname === "/tests/typing") {
+    if (!document.querySelector(".css-0")) {
       details = "Testing...";
       state = "Typing";
     } else {
@@ -121,7 +118,7 @@ presence.on("UpdateData", async () => {
         document.querySelector(".css-0").innerHTML.split("wpm")[0]
       }`;
     }
-  } else if (document.location.pathname == "/dashboard") {
+  } else if (document.location.pathname === "/dashboard") {
     details = "Browsing...";
     state = "User's Dashboard";
   } else {
