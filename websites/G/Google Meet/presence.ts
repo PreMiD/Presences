1ca88290506f25cfccde6d42dbaa9ce438105bf3
Slowlife <slowlife1165@daum.net>
2021-06-02 21:44:46 +0700

    Update presence.ts

--- websites/G/Google Meet/presence.ts
+++ websites/G/Google Meet/presence.ts
@@ -1,13 +1,14 @@
 const presence = new Presence({
   clientId: "701914032541794386"
-});
+}),
+  time = Math.floor(Date.now() / 1000);
 
-const time = Math.floor(Date.now() / 1000);
 presence.on("UpdateData", () => {
   const presenceData: PresenceData = {
       largeImageKey: "meetlogo"
     },
     path = document.location.pathname.toLowerCase();
+  
   if (path === "/") {
     presenceData.details = "Initial page";
     presenceData.state = "Just waiting";
@@ -17,7 +18,7 @@ presence.on("UpdateData", () => {
     const users = (document.querySelector(".wnPUne") ?? document.querySelector(".uGOf1d")).innerHTML;
     presenceData.smallImageKey = "vcall";
     presenceData.details = "In a meeting";
-    presenceData.state = users + " users in the room";
+    presenceData.state = `${users} users in the room`;
     presenceData.startTimestamp = time;
     presence.setActivity(presenceData);
   }
