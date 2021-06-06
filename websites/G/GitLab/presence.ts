76390e1404af39dc4e9e9ca505540ffeefc472bc
Gurrman375 <james.gurr375@gmail.com>
2021-06-05 19:55:36 -0700

    Shorten code

--- websites/G/GitLab/presence.ts
+++ websites/G/GitLab/presence.ts
@@ -14,50 +14,40 @@ presence.on("UpdateData", async () => {
       document.location.pathname == "/" ||
       document.location.pathname.includes("/dashboard/projects")
     ) {
-      if (document.location.pathname == "/dashboard/projects/starred") {
-        presenceData.details = "Viewing Starred";
-      } else {
-        presenceData.details = "Viewing Dashboard";
-      }
-    } else if (document.location.pathname == "/projects/new/") {
-      presenceData.details = "Creating a project";
-    } else if (document.location.pathname == "/groups/new") {
-      presenceData.details = "Creating a group";
-    } else if (document.location.pathname.includes("/explore")) {
-      if (document.location.pathname == "/explore/snippets") {
-        presenceData.details = "Exploring Snippets";
-      } else {
-        presenceData.details = "Exploring projects";
-      }
-    } else if (document.location.pathname == "/dashboard/groups") {
-      presenceData.details = "Viewing Groups";
-    } else if (document.location.pathname == "/dashboard/activity") {
-      presenceData.details = "Viewing Activity";
-    } else if (document.location.pathname == "/dashboard/milestones") {
-      presenceData.details = "Viewing Milestones";
-    } else if (document.location.pathname == "/dashboard/snippets") {
-      presenceData.details = "Viewing Snippets";
-    } else if (document.location.pathname == "/-/operations/environments") {
-      presenceData.details = "Viewing Environments";
-    } else if (document.location.pathname == "/-/operations") {
-      presenceData.details = "Viewing Operations";
-    } else if (document.location.pathname == "/-/security") {
-      presenceData.details = "Viewing Security";
-    } else if (document.location.pathname == "/dashboard/issues") {
-      presenceData.details = "Viewing Issuses";
-    } else if (document.location.pathname == "/dashboard/merge_requests") {
-      presenceData.details = "Viewing Merge Requests";
-    } else if (document.location.pathname == "/dashboard/todos") {
-      presenceData.details = "Viewing TODOS";
-    } else if (document.location.pathname == "/help") {
-      presenceData.details = "Viewing Help";
-    } // /users/sign_in
-    else if (document.location.pathname == "/users/sign_in") {
-      presenceData.details = "Signning in or Registering";
-    } else if (document.location.pathname == "/users/sign_in") {
-      presenceData.details = "Signning in";
-    }
-    // document.querySelector("#dashboard_search")
+      if (document.location.pathname == "/dashboard/projects/starred")presenceData.details = "Viewing Starred";
+      else presenceData.details = "Viewing Dashboard";
+    } else if (document.location.pathname == "/projects/new/")presenceData.details = "Creating a project";
+     else if (document.location.pathname == "/groups/new")presenceData.details = "Creating a group";
+     else if (document.location.pathname.includes("/explore")) {
+      if (document.location.pathname == "/explore/snippets") presenceData.details = "Exploring Snippets";
+      else presenceData.details = "Exploring projects";
+    } 
+    else if (document.location.pathname == "/dashboard/groups") presenceData.details = "Viewing Groups";
+
+    else if (document.location.pathname == "/dashboard/activity")presenceData.details = "Viewing Activity";
+
+    else if (document.location.pathname == "/dashboard/milestones") presenceData.details = "Viewing Milestones";
+
+    else if (document.location.pathname == "/dashboard/snippets") presenceData.details = "Viewing Snippets";
+
+    else if (document.location.pathname == "/-/operations/environments") presenceData.details = "Viewing Environments";
+
+    else if (document.location.pathname == "/-/operations") presenceData.details = "Viewing Operations";
+
+    else if (document.location.pathname == "/-/security") presenceData.details = "Viewing Security";
+
+    else if (document.location.pathname == "/dashboard/issues") presenceData.details = "Viewing Issuses";
+
+    else if (document.location.pathname == "/dashboard/merge_requests") presenceData.details = "Viewing Merge Requests";
+
+    else if (document.location.pathname == "/dashboard/todos") presenceData.details = "Viewing TODOS";
+
+    else if (document.location.pathname == "/help") presenceData.details = "Viewing Help";
+
+    else if (document.location.pathname == "/users/sign_in") presenceData.details = "Signning in or Registering";
+
+    else if (document.location.pathname == "/users/sign_in") presenceData.details = "Signning in";
+
     else if (document.location.pathname == "/search") {
       title = document.querySelector(
         "#dashboard_search"
@@ -91,9 +81,7 @@ presence.on("UpdateData", async () => {
           presenceData.details = "Viewing:";
           presenceData.state = owner.innerText;
           
-        } else {
-          presenceData.details = "Viewing Unknown";
-        }
+        } else presenceData.details = "Viewing Unknown";
       }
     }
   } else if (window.location.hostname == "about.gitlab.com") {
@@ -107,57 +95,42 @@ presence.on("UpdateData", async () => {
     title = document.querySelector(
       "body > div.wrapper.gitlab-ee-page > div.image-title > div > div > h1"
     );
-    if (title) {
-      presenceData.details = "Viewing " + title.textContent;
-      
-    } else if (title == null) {
+    if (title) presenceData.details = "Viewing " + title.textContent;
+
+    else if (title == null) {
       title = document.querySelector("body > div.blank-header > div > h1");
       if (title) {
         presenceData.details = "Viewing " + title.textContent;
         
       } else if (title == null) {
         title = document.querySelector("body > div.wrapper > div > h1");
-        if (title) {
-          presenceData.details = "Viewing " + title.textContent;
-          
-        } else if (title == null) {
+        if (title) presenceData.details = "Viewing " + title.textContent;
+        else if (title == null) {
           title = document.querySelector(
             "body > div.wrapper.blog-landing > div.blog-landing-content > div.blog-hero > div > a > h2"
           );
-          if (title) {
-            presenceData.details = "Viewing " + title.textContent;
-            
-          } else if (title == null) {
+          if (title) presenceData.details = "Viewing " + title.textContent;
+            else if (title == null) {
             title = document.querySelector(
               "body > div.blog.article > div.cover > div > div > h1"
             );
-            if (title) {
-              presenceData.details = "Viewing " + title.textContent;
-              
-            } else {
+            if (title) presenceData.details = "Viewing " + title.textContent;
+            else {
               title = document.querySelector(
                 "body > div.reseller-hero.text-center > div > h1"
               );
-              if (title) {
-                presenceData.details = "Viewing " + title.textContent;
-                
-              }
+              if (title) presenceData.details = "Viewing " + title.textContent;
             }
           }
         }
-      } else {
-        presenceData.details = "Viewing Solutions";
-      }
+      } else presenceData.details = "Viewing Solutions";
     } else if (document.location.pathname == "/services/implementation/") {
       title = document.querySelector("body > div.wrapper > div > h1");
       presenceData.details = "Viewing " + title.textContent;
       
-    } else {
-      presenceData.details = "Viewing Unknown";
-    }
+    } else presenceData.details = "Viewing Unknown";
   } else {
     presenceData.details = "Viewing Unknown";
-    console.log("Viewing Unknown");
   }
   if (presenceData.details == null) {
     //This will fire if you do not set presence details
