e36cb4a9124acf4ded545931e7cbedb0e4be4cd2
Oreo <verifiedmodds@gmail.com>
2021-06-08 11:46:48 -0400

    Update presence.ts
    removed comments

--- websites/N/Neverlose/presence.ts
+++ websites/N/Neverlose/presence.ts
@@ -1,56 +1,42 @@
 const presence = new Presence({
-  clientId: "851818492847194172" //The client ID of the Application created at https://discordapp.com/developers/applications
+  clientId: "851818492847194172" 
 }),
 strings = presence.getStrings({
   play: "presence.playback.playing",
   pause: "presence.playback.paused"
-  //You can use this to get translated strings in their browser language
 });
-
-/*
-
-function myOutsideHeavyLiftingFunction(){
-  //Grab and process all your data here
-
-  // element grabs //
-  // api calls //
-  // variable sets //
-}
-
-setInterval(myOutsideHeavyLiftingFunction, 10000);
-//Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
-
-*/
 var browsingStamp = Math.floor(Date.now() / 1000);
 
 var title: any;
 var search: any;
 
 presence.on("UpdateData", async () => {
-/*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
-
-  It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
 
 const presenceData: PresenceData = {
   largeImageKey:
     "neverlose"
 };
-
+//main page
 if (document.location.pathname == "/") {
   presenceData.details = "Browsing The Home Page!";
 }
+//profile settings
 else if (document.location.pathname == "/me"){
   presenceData.details = "Viewing Their Profile!";
 }
+//faq
 else if (document.location.pathname == "/help"){
   presenceData.details = "Viewing The FAQ!";
 }
+//tickets that are open
 else if (document.location.pathname == "/tickets"){
   presenceData.details = "Viewing Their Tickets!";
 }
+//purchases
 else if (document.location.pathname == "/market/history"){
   presenceData.details = "Viewing Their Purchases!";
 }
+//current subscription
 else if (document.location.pathname.includes("/sub")) {
   //csgo product
   if (document.URL.includes("type=csgo")){
@@ -74,13 +60,11 @@ else if (document.location.pathname.includes("/product")) {
     presenceData.details = "Viewing The Apex Legends Product!";
   }
 }
-else if (document.location.pathname == "https://forum.neverlose.cc/"){
-
-}
+//main market
 else if (document.location.pathname.includes("/market")){
   presenceData.details = "Viewing Paid Configs!";
   presenceData.startTimestamp = browsingStamp;
-  //fre scripts
+  //free scripts
   if(document.URL.includes("type=3")){
     presenceData.details = "Viewing Free Scripts!";
   }
@@ -100,12 +84,10 @@ else if (document.location.pathname.includes("/market")){
 }
 
 if (presenceData.details == null) {
-  //This will fire if you do not set presence details
-  presence.setTrayTitle(); //Clears the tray title for mac users
+  presence.setTrayTitle();
   presenceData.startTimestamp = browsingStamp;
-  presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+  presence.setActivity();
 } else {
-  //This will fire if you set presence details
-  presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+  presence.setActivity(presenceData);
 }
 });
