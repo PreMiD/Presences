73ccb0765ffcfaadb5629a34bc0df7df54a7700e
Toki <47860095+toki0179@users.noreply.github.com>
2021-06-12 23:13:17 +0100

    Add files via upload

--- websites/D/Drama/presence.ts
+++ websites/D/Drama/presence.ts
@@ -8,102 +8,68 @@ strings = presence.getStrings({
 
 presence.on("UpdateData", async () => {
 
+  var presenceData: PresenceData = {
+    largeImageKey:
+      "dramagg",
+    smallImageKey:
+      "search",
+    smallImageText: "Looking for a thread",
+    details: "Looking for drama!",
+    state: 'Browsing',
+  };
+
   if(document.URL.includes('/d/')) {
-    var presenceData: PresenceData = {
-      largeImageKey:
-        "dramagg",
-      smallImageKey:
-        "reading",
-      smallImageText: "Reading a thread",
-      details: `Viewing Thread: ${document.querySelector("h2.DiscussionHero-title").textContent}`,
-      state: `${document.querySelector("span.Scrubber-index").textContent} / ${document.querySelector("span.Scrubber-count").textContent} Posts`,
-      buttons: [
+    presenceData.largeImageKey = "dramagg";
+    presenceData.smallImageKey = "reading";
+    presenceData.smallImageText = "Reading a thread";
+    presenceData.details = `Viewing Thread: ${document.querySelector("h2.DiscussionHero-title").textContent}`;
+    presenceData.state = `${document.querySelector("span.Scrubber-index").textContent} / ${document.querySelector("span.Scrubber-count").textContent} Posts`;
+    presenceData.buttons = [
         {
           label: "Go To Post",
           url: `${document.URL}`
         }
-      ]
-    }; 
+      ] 
   } else if(document.URL.includes('/u/')) {
-      var presenceData: PresenceData = {
-        largeImageKey:
-          "dramagg",
-        smallImageKey:
-          "reading",
-        smallImageText: "Viewing a profile",
-        details: `Viewing Profile: ${document.URL.split("/")[4]}`,
-        state: `Posts: ${document.querySelector("span.Button-badge").textContent} `,
-      };
+    presenceData.smallImageKey = "reading";
+    presenceData.smallImageText = "Viewing a profile";
+    presenceData.details = `Viewing Profile: ${document.URL.split("/")[4]}`;
+    presenceData.state = `Posts: ${document.querySelector("span.Button-badge").textContent} `;
   } else if(document.URL.includes('/meta')) {
-    var presenceData: PresenceData = {
-      largeImageKey:
-        "dramagg",
-      smallImageKey:
-        "search",
-      smallImageText: "Looking for a thread",
-      details: "Looking for drama!",
-      state: `Looking at meta threads`,
-    };
+    presenceData.smallImageKey = "search";
+    presenceData.smallImageText = "Looking for a thread";
+    presenceData.details = "Looking for drama!";
+    presenceData.state = 'Looking at meta threads';
   } else if(document.URL.includes('/resolved')) {
-    var presenceData: PresenceData = {
-      largeImageKey:
-        "dramagg",
-      smallImageKey:
-        "search",
-      smallImageText: "Looking for a thread",
-      details: "Looking for drama!",
-      state: `Looking at resolved threads`,
-    }; 
+    presenceData.smallImageKey = "search";
+    presenceData.smallImageText = "Looking for a thread";
+    presenceData.details = "Looking for drama!";
+    presenceData.state = 'Looking at resolved threads'; 
   } else if(document.URL.includes('/confirmed')) {
-    var presenceData: PresenceData = {
-      largeImageKey:
-        "dramagg",
-      smallImageKey:
-        "search",
-      smallImageText: "Looking for a thread",
-      details: "Looking for drama!",
-      state: `Looking at confirmed threads`,
-    }; 
+    presenceData.smallImageKey = "search";
+    presenceData.smallImageText = "Looking for a thread";
+    presenceData.details = "Looking for drama!";
+    presenceData.state = 'Looking at confirmed threads'; 
   } else if(document.URL.includes('/discussion')) {
-    var presenceData: PresenceData = {
-      largeImageKey:
-        "dramagg",
-      smallImageKey:
-        "search",
-      smallImageText: "Looking for a thread",
-      details: "Looking for drama!",
-      state: `Looking at discussions`,
-    }; 
+    presenceData.smallImageKey = "search";
+    presenceData.smallImageText = "Looking for a thread";
+    presenceData.details = "Looking for drama!";
+    presenceData.state = 'Looking at discussions';
   } else if(document.URL.includes('/users')) {
-    var presenceData: PresenceData = {
-      largeImageKey:
-        "dramagg",
-      smallImageKey:
-        "search",
-      smallImageText: "Looking at all users",
-      details: "Looking for drama!",
-      state: `Looking at all users`,
-    };
+    presenceData.smallImageKey = "search";
+    presenceData.smallImageText = "Looking at all users";
+    presenceData.details = "Looking for drama!";
+    presenceData.state = 'Looking at all users';
   } else if(document.URL.includes('/trinkets')) {
-    var presenceData: PresenceData = {
-      largeImageKey:
-        "dramagg",
-      smallImageKey:
-        "search",
-      smallImageText: "Looking at trinkets",
-      details: "Looking for drama!",
-      state: `Looking at trinkets`,
-    };
+    presenceData.smallImageKey = "search";
+    presenceData.smallImageText = "Looking at trinkets";
+    presenceData.details = "Looking for drama!";
+    presenceData.state = 'Looking at trinkets';
   } else {
-    var presenceData: PresenceData = {
-      largeImageKey:
-        "dramagg",
-      smallImageKey:
-        "search",
-      smallImageText: "Looking for a thread",
-      details: "Looking for drama!",
-      state: `Browsing`,
-    };  
+    presenceData.smallImageKey = "search";
+    presenceData.smallImageText = "Looking for a thread";
+    presenceData.details = "Looking for drama!";
+    presenceData.state = 'Browsing';
   } 
 
 
