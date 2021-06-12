825fc3ce54444e640708e625f5cfb3ac6eff524a
vahya <85792429+vahya@users.noreply.github.com>
2021-06-12 20:35:48 +0530

    Added Catalog and log view

--- websites/I/Indiachan/presence.ts
+++ websites/I/Indiachan/presence.ts
@@ -1,33 +1,43 @@
 const presence = new Presence({
-    clientId: "853244463396552704"
-  }),
-  browsingStamp = Date.now();
+  clientId: "853244463396552704"
+}),
+browsingStamp = Date.now();
 
 presence.on("UpdateData", async () => {
-  const presenceData: PresenceData = {
-    largeImageKey: "inch_large_",
-    smallImageKey: "inch_small_",
-    smallImageText: "Indiachan.com",
-    details: "Browsing Indiachan.com ",
-    state: "Having a good time",
-    startTimestamp: browsingStamp
-  };
+const presenceData: PresenceData = {
+  largeImageKey: "inch_large_",
+  smallImageKey: "inch_small_",
+  smallImageText: "Indiachan.com",
+  details: "Browsing Indiachan.com ",
+  state: "Having a good time",
+  startTimestamp: browsingStamp
+};
 
-  if (document.location.pathname !== "/") {
-    const elements: string[] = document.location.pathname.split("/"),
-      boardName = document.getElementById("labelName").innerText,
-      [, , , threadNum = null] = elements,
-      messageLength = parseInt(
-        document.getElementById("labelMessageLength").innerText
-      );
+if (document.location.pathname !== "/") {
+  const elements: string[] = document.location.pathname.split("/"),
+    [, logs = "", catalog = "", threadNum = null] = elements,
+    messageLength = document.getElementById("labelMessageLength") ? parseInt(
+      document.getElementById("labelMessageLength").innerText
+    ) : 4081;
+  if (logs === "logs.js") {
+    presenceData.details = "Looking in the logs";
+    presenceData.state = "Got banned maybe?";
+  } else {
+    let boardName: string = null;
+    if (catalog === "catalog.html")
+      boardName = document.getElementById("catalogId").innerText;
+    else boardName = document.getElementById("labelName").innerText;
     presenceData.details = `Browsing ${boardName}`;
-    presenceData.state = `${
-      messageLength <= 4080 ? "Shitposting furiously" : "Lurking"
-    } ${threadNum !== null ? `in ${threadNum}` : ""}`;
+    if (catalog !== "catalog.html") {
+      presenceData.state = `${
+        messageLength <= 4080 ? "Shitposting furiously" : "Lurking"
+      } ${threadNum !== null ? `in ${threadNum}` : ""}`;
+    } else presenceData.state = "Lurking in the catalog";
   }
+}
 
-  if (presenceData.details === null) {
-    presence.setTrayTitle();
-    presence.setActivity();
-  } else presence.setActivity(presenceData);
+if (presenceData.details === null) {
+  presence.setTrayTitle();
+  presence.setActivity();
+} else presence.setActivity(presenceData);
 });
