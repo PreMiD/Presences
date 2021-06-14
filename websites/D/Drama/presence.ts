68e2a2d4f4b39d0459de6a91aac42a0f485e388e
Toki <47860095+toki0179@users.noreply.github.com>
2021-06-14 15:37:43 +0100

    Update presence.ts

--- websites/D/Drama/presence.ts
+++ websites/D/Drama/presence.ts
@@ -8,17 +8,17 @@ strings = presence.getStrings({
 
 presence.on("UpdateData", async () => {
 
-  var presenceData: PresenceData = {
+  let presenceData: PresenceData = {
     largeImageKey:
       "dramagg",
     smallImageKey:
       "search",
     smallImageText: "Looking for a thread",
     details: "Looking for drama!",
-    state: 'Browsing',
+    state: "Browsing"
   };
 
-  if(document.URL.includes('/d/')) {
+  if(document.URL.includes("/d/")) {
     presenceData.largeImageKey = "dramagg";
     presenceData.smallImageKey = "reading";
     presenceData.smallImageText = "Reading a thread";
@@ -29,52 +29,52 @@ presence.on("UpdateData", async () => {
           label: "Go To Post",
           url: `${document.URL}`
         }
-      ] 
-  } else if(document.URL.includes('/u/')) {
+      ];
+  } else if(document.URL.includes("/u/")) {
     presenceData.smallImageKey = "reading";
     presenceData.smallImageText = "Viewing a profile";
     presenceData.details = `Viewing Profile: ${document.URL.split("/")[4]}`;
     presenceData.state = `Posts: ${document.querySelector("span.Button-badge").textContent} `;
-  } else if(document.URL.includes('/meta')) {
+  } else if(document.URL.includes("/meta")) {
     presenceData.smallImageKey = "search";
     presenceData.smallImageText = "Looking for a thread";
     presenceData.details = "Looking for drama!";
-    presenceData.state = 'Looking at meta threads';
-  } else if(document.URL.includes('/resolved')) {
+    presenceData.state = "Looking at meta threads";
+  } else if(document.URL.includes("/resolved")) {
     presenceData.smallImageKey = "search";
     presenceData.smallImageText = "Looking for a thread";
     presenceData.details = "Looking for drama!";
-    presenceData.state = 'Looking at resolved threads'; 
-  } else if(document.URL.includes('/confirmed')) {
+    presenceData.state = "Looking at resolved threads"; 
+  } else if(document.URL.includes("/confirmed")) {
     presenceData.smallImageKey = "search";
     presenceData.smallImageText = "Looking for a thread";
     presenceData.details = "Looking for drama!";
-    presenceData.state = 'Looking at confirmed threads'; 
-  } else if(document.URL.includes('/discussion')) {
+    presenceData.state = "Looking at confirmed threads"; 
+  } else if(document.URL.includes("/discussion")) {
     presenceData.smallImageKey = "search";
     presenceData.smallImageText = "Looking for a thread";
     presenceData.details = "Looking for drama!";
-    presenceData.state = 'Looking at discussions';
-  } else if(document.URL.includes('/users')) {
+    presenceData.state = "Looking at discussions";
+  } else if(document.URL.includes("/users")) {
     presenceData.smallImageKey = "search";
     presenceData.smallImageText = "Looking at all users";
     presenceData.details = "Looking for drama!";
-    presenceData.state = 'Looking at all users';
-  } else if(document.URL.includes('/trinkets')) {
+    presenceData.state = "Looking at all users";
+  } else if(document.URL.includes("/trinkets")) {
     presenceData.smallImageKey = "search";
     presenceData.smallImageText = "Looking at trinkets";
     presenceData.details = "Looking for drama!";
-    presenceData.state = 'Looking at trinkets';
-  } else if(document.querySelector('div.Composer.visible')) {
+    presenceData.state = "Looking at trinkets";
+  } else if(document.querySelector("div.Composer.visible")) {
     presenceData.smallImageKey = "reading";
     presenceData.smallImageText = "Writing a thread";
     presenceData.details = "Starting some drama!";
-    presenceData.state = "Writing a post"
+    presenceData.state = "Writing a post";
   } else {
     presenceData.smallImageKey = "search";
     presenceData.smallImageText = "Looking for a thread";
     presenceData.details = "Looking for drama!";
-    presenceData.state = 'Browsing';
+    presenceData.state = "Browsing";
   } 
 
 
