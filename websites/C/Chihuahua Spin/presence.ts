f2c50467c697f3e07df3863a4d00940d088ee871
dzlandis <49926644+dzlandis@users.noreply.github.com>
2021-06-14 23:18:18 -0700

    Added ChihuahuaSpin.com Presence
    Proof of working:
    ![Chihuahua Spin Presence](https://i.imgur.com/OG4MFM7.png)

---
+++ websites/C/Chihuahua Spin/presence.ts
@@ -0,0 +1,18 @@
+const presence = new Presence({
+    clientId: "854128240078356480"
+  });
+  
+  const browsingStamp = Math.floor(Date.now() / 1000);
+  
+  presence.on("UpdateData", () => {
+    const spins = document.querySelector("#spin").textContent.split("Jump to")[0].trim();
+    const presenceData: PresenceData = {
+      largeImageKey: "dogspinthumbnail",
+      startTimestamp: browsingStamp,
+      details: `Watching the chihuahua spin!`,
+      state: `${spins}`,
+      buttons: [{label: "Website", url: "http://chihuahuaspin.com/"}]
+    };
+    presence.setActivity(presenceData);
+  });
+  
