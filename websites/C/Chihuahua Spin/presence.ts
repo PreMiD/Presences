80244f1029358b29ae8bcaf1245b95ebb07e5fa9
dzlandis <49926644+dzlandis@users.noreply.github.com>
2021-06-15 00:14:01 -0700

    Fixed the last few code problems.

--- websites/C/Chihuahua Spin/presence.ts
+++ websites/C/Chihuahua Spin/presence.ts
@@ -1,11 +1,11 @@
 const presence = new Presence({
     clientId: "854128240078356480"
-  });
+  }),
+  browsingStamp = Math.floor(Date.now() / 1000);
   
   presence.on("UpdateData", () => {
-    const spins = document.querySelector("#spin").textContent.split("Jump to")[0].trim();
-    const browsingStamp = Math.floor(Date.now() / 1000);
-    const presenceData: PresenceData = {
+    const spins = document.querySelector("#spin").textContent.split("Jump to")[0].trim(),
+    presenceData: PresenceData = {
       largeImageKey: "dogspinthumbnail",
       startTimestamp: browsingStamp,
       details: "Watching the chihuahua spin!",
