2e4f3528738e791c96e7f7c21c356e7090a200df
dzlandis <49926644+dzlandis@users.noreply.github.com>
2021-06-16 16:03:14 -0700

    Fixed comma problems caused by pretify.

--- websites/T/Typer.io/presence.ts
+++ websites/T/Typer.io/presence.ts
@@ -1,12 +1,12 @@
 const presence = new Presence({
-  clientId: "854448403273351202",
+  clientId: "854448403273351202"
 }),
 browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
 const presenceData: PresenceData = {
   largeImageKey: "logo",
-  startTimestamp: browsingStamp,
+  startTimestamp: browsingStamp
 };
 
 // typer.io/play presence (aka quick play)
