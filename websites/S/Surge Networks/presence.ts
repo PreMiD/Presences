08d2c22cd10549c14e740c9ca0e9256dfac740e6
codefactor-io <support@codefactor.io>
2021-06-20 17:20:34 +0000

    [CodeFactor] Apply fixes to commit 4c128e7

--- websites/S/Surge Networks/presence.ts
+++ websites/S/Surge Networks/presence.ts
@@ -8,34 +8,34 @@ presence.on("UpdateData", async () => {
   };
 
   // Main Domain
-  if (window.location.hostname == "surge-networks.co.uk") {
+  if (window.location.hostname == "surge-networks.co.uk") 
     data.details = "Viewing Landing Page";
-  }
+  
 
   // CDN
-  if (window.location.hostname == "cdn.surge-networks.co.uk") {
+  if (window.location.hostname == "cdn.surge-networks.co.uk") 
     data.details = "Viewing CDN";
-  }
+  
 
   // Support
-  if (window.location.hostname == "support.surge-networks.co.uk") {
+  if (window.location.hostname == "support.surge-networks.co.uk") 
     data.details = "Viewing Support Page";
-  }
+  
 
   // Webmail
-  if (window.location.hostname == "webmail.surge-networks.co.uk") {
+  if (window.location.hostname == "webmail.surge-networks.co.uk") 
     data.details = "Viewing Emails";
-  }
+  
 
   // Status
-  if (window.location.hostname == "status.surge-networks.co.uk") {
+  if (window.location.hostname == "status.surge-networks.co.uk") 
     data.details = "Viewing Status Page";
-  }
+  
 
   // Forums
-  if (window.location.hostname == "forums.surge-networks.co.uk") {
+  if (window.location.hostname == "forums.surge-networks.co.uk") 
     data.details = "Browsing Forums";
-  }
+  
 
   presence.setActivity(data);
 });
