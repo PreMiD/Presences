ea8340536c9dfe2bcddddf4a2f1286381ccd09e2
codefactor-io <support@codefactor.io>
2021-06-23 18:46:12 +0000

    [CodeFactor] Apply fixes

--- websites/B/Birdflop/presence.ts
+++ websites/B/Birdflop/presence.ts
@@ -1,6 +1,6 @@
 const presence = new Presence({
     clientId: "857289302948970506" //The client ID of the Application created at https://discordapp.com/developers/applications
-})
+});
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
@@ -8,26 +8,26 @@ presence.on("UpdateData", async () => {
       "birdflop",
     details: "Browsing Hosting",
   };
-  if (window.location.href.includes("www.birdflop.com")) {
-    presenceData.details = "Browsing Hosting"
-  }else if(window.location.href.includes("client.birdflop.com")) {
+  if (window.location.href.includes("www.birdflop.com")) 
+    presenceData.details = "Browsing Hosting";
+  else if(window.location.href.includes("client.birdflop.com")) {
     if (window.location.pathname.toLowerCase().includes("/order")) {
-      presenceData.details = "Browsing Hosting"
+      presenceData.details = "Browsing Hosting";
       presenceData.state = "Looking at plans";
     }
   }else if (window.location.href.includes("panel.birdflop.com")) {
-      if (window.location.pathname.toLowerCase() === "/") {
+      if (window.location.pathname.toLowerCase() === "/") 
         presenceData.details = "Viewing server list";
-      }else if (window.location.pathname.toLowerCase().includes("/server")) {
+      else if (window.location.pathname.toLowerCase().includes("/server")) 
         presenceData.details = `Looking at ${document.title}`;
-      }else if (window.location.pathname.toLowerCase().includes("/phpmyadmin")){
-        presenceData.details = "Edition SQL databases"
-      }
+      else if (window.location.pathname.toLowerCase().includes("/phpmyadmin")) 
+        presenceData.details = "Edition SQL databases";
+      
   }
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else {
+  } else 
     presence.setActivity(presenceData);
-  }
+  
 });
