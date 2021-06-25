439832cdffaebbcb0a232355ea2f5b11b36c8d7c
gunyu1019 <gunyu1019@gmail.com>
2021-06-26 02:56:29 +0900

    Adding a Music Streaming Service(FLO) in South Korea

---
+++ websites/F/FLO/presence.ts
@@ -0,0 +1,138 @@
+const presence = new Presence({
+  clientId: "857504781438681089"
+});
+
+type actionType = {
+  actionId?: string;
+};
+
+
+function DataRake(dataset: DOMStringMap) {
+  const _dataset = JSON.parse(JSON.stringify(dataset));
+  return JSON.parse(_dataset.rake);
+}
+
+function getTimestamp(time: string, deletedKey: string = null) {
+  if (deletedKey !== null) time = time.replace(deletedKey, "");
+  time = time.replace("    ", "").replace("\n", "");
+  return Date.parse(`1970 00:${time} GMT+0000`);
+}
+
+function getQuery() {
+  const search = location.search.substring(1);
+  return JSON.parse(
+    `{"${
+      decodeURI(search)
+        .replace(/"/g, '\\"')
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"')
+    }"}`
+  );
+}
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+      largeImageKey: "flo-logo",
+      buttons: [
+        {
+          label: "FLO 웹사이트",
+          url: "https://www.music-flo.com/"
+        }
+      ]
+    }, player = document.querySelector(".playbar_wrap"),
+    playerBar: HTMLInputElement = player.querySelector("input.progress");
+  if (!playerBar.disabled) { // Use first one-time player
+    const playButton: HTMLButtonElement = player.querySelector("button.icon-player"),
+      playButtonActionId: actionType = DataRake(playButton.dataset),
+      title = player.querySelector("p.title").textContent,
+      artist = player.querySelector("p.artist").textContent,
+      allTime = player.querySelector(".time_all").textContent,
+      allHiddenTime = player.querySelector(".time_all").querySelector("span.hidden").textContent,
+      currentTime = player.querySelector(".time_current").textContent,
+      currentHiddenTime = player.querySelector(".time_current").querySelector("span.hidden").textContent,
+      nowDate = Date.now(),
+      allDate = getTimestamp(allTime, allHiddenTime),
+      currentDate = getTimestamp(currentTime, currentHiddenTime);
+
+    presenceData.endTimestamp = nowDate + allDate - currentDate;
+    presenceData.details = `${title} - ${artist}`;
+    if (playButtonActionId.actionId === "pause") {
+      presenceData.smallImageKey = "pause";
+      presenceData.smallImageText = "일시 정지";
+    } else if (playButtonActionId.actionId === "play") {
+      presenceData.smallImageKey = "playing";
+      presenceData.smallImageText = "재생";
+    }
+  } else { // Works when the player is not in use.
+    const {location} = window;
+    if (location.pathname === "/") 
+      presenceData.details = "메인";
+    else if (location.pathname.indexOf("/search") === 0) {
+        const keyword = getQuery();
+
+        presenceData.smallImageKey = "search";
+        presenceData.smallImageText = "검색";
+        presenceData.details = "검색";
+        presenceData.state = keyword.keyword;
+
+        if (location.pathname === "/search/track")
+          presenceData.details += "(곡)";
+        else if (location.pathname === "/search/album")
+          presenceData.details += "(앨범)";
+        else if (location.pathname === "/search/artist")
+          presenceData.details += "(아티스트)";
+        else if (location.pathname === "/search/theme")
+          presenceData.details += "(테마리스트)";
+        else if (location.pathname === "/search/lyrics")
+          presenceData.details += "(가사)";
+    }else if (location.pathname.indexOf("/new") === 0) {
+      presenceData.smallImageKey = "search";
+      presenceData.smallImageText = "최근 발매 음악";
+      presenceData.details = "최근 발매 음악";
+
+      if (location.pathname === "/new/track")
+        presenceData.details += "(곡)";
+      else if (location.pathname === "/new/album")
+        presenceData.details += "(앨범)";
+    }else if (location.pathname.indexOf("/help") === 0) {
+      presenceData.details = "고객센터";
+
+      if (location.pathname.indexOf("/help/notice") === 0)
+        presenceData.state = "공지사항";
+      else if (location.pathname.indexOf("/help/faq") === 0)
+        presenceData.state = "자주 묻는 문의";
+      else if (location.pathname.indexOf("/help/qna") === 0)
+        presenceData.state = "1:1 문의";
+    }else if (location.pathname.indexOf("/detail/channel") === 0) {
+      const title = document.querySelector("p.title").textContent;
+
+      presenceData.smallImageKey = "search";
+      presenceData.smallImageText = "테마리스트";
+      presenceData.details = "테마리스트";
+      presenceData.state = title;
+    }else if (location.pathname.indexOf("/detail/album") === 0) {
+      const title = document.querySelector("p.title").textContent,
+       artist = document.querySelector("p.artist").textContent;
+
+      presenceData.smallImageKey = "search";
+      presenceData.smallImageText = "앨범";
+      presenceData.details = "앨범";
+      presenceData.state = `${title} - ${artist}`;
+    }else if (location.pathname === "/browse") {
+      const title = document.querySelector(".chart_content_head>h4").textContent;
+
+      presenceData.smallImageKey = "search";
+      presenceData.smallImageText = title;
+      presenceData.details = title;
+    }else if (location.pathname.indexOf("/storage") === 0) {
+      presenceData.smallImageKey = "search";
+      presenceData.smallImageText = "보관함";
+      presenceData.details = "보관함";
+    }else if (location.pathname.indexOf("/purchase") === 0) 
+      presenceData.details = "이용권";
+    else if (location.pathname === "/intro") 
+      presenceData.details = "소개";
+
+  }
+  presence.setActivity(presenceData);
+});
