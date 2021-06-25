a2deec84846d8f1c74d4d570ac1bdd921b050f54
codefactor-io <support@codefactor.io>
2021-06-25 22:14:30 +0000

    [CodeFactor] Apply fixes to commit f78b9e4

--- websites/A/AnimeDigitalNetwork/presence.ts
+++ websites/A/AnimeDigitalNetwork/presence.ts
@@ -8,15 +8,15 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 
-  const stamp = Math.floor(Date.now() / 1000);
+  const stamp = Math.floor(Date.now() / 1000),
 
-  const presenceData: PresenceData = {
+   presenceData: PresenceData = {
     largeImageKey:
       "adn_logo" /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/,
      //If you want to show Time Left instead of Elapsed, this is the unix epoch timestamp at which the timer ends
-  };
+  },
 
-  const nodeValue = document.getElementsByClassName('sc-qPkqk')[0].attributes[0].nodeValue;
+   {nodeValue} = document.getElementsByClassName('sc-qPkqk')[0].attributes[0];
 
   if (nodeValue) {
     presenceData.smallImageKey = nodeValue.replace('https://avatar.animedigitalnetwork.fr/web/ch', '').replace('_bg1_green0_188.jpg', '');
@@ -33,19 +33,19 @@ presence.on("UpdateData", async () => {
 
     presenceData.startTimestamp = stamp;
 
-    let vfOrvostfr;
-    let quality;
+    let vfOrvostfr,
+     quality;
 
-    const title = document.getElementsByClassName('sc-qQwsb')[0].innerHTML;
-    const episode = document.getElementsByClassName('sc-qYRsW')[0].innerHTML;
+    const title = document.getElementsByClassName('sc-qQwsb')[0].innerHTML,
+     episode = document.getElementsByClassName('sc-qYRsW')[0].innerHTML;
 
     if (document.getElementsByClassName('vjs-menu-item vjs-selected').length > 0) {
       vfOrvostfr = ((<HTMLElement>document.getElementsByClassName('vjs-menu-item vjs-selected')[1]).innerText.replace(', selected', ''));
       quality = (<HTMLElement>document.getElementsByClassName('vjs-menu-item vjs-selected')[0].firstElementChild).innerText.replace('(', '').replace(')', '');
-      presenceData.details = title + ' : ' + vfOrvostfr + ' - ' + quality;
+      presenceData.details = `${title} : ${vfOrvostfr} - ${quality}`;
       presenceData.state = episode;
     } else {
-      presenceData.state = 'Cette vidéo sera disponible dans : ' + (<HTMLElement>document.getElementsByClassName('adn-player-countdown-value')[0]).innerText;
+      presenceData.state = `Cette vidéo sera disponible dans : ${(<HTMLElement>document.getElementsByClassName('adn-player-countdown-value')[0]).innerText}`;
       presenceData.details = title;
     }
   } else if (document.getElementsByClassName("sc-AxjAm khAjwj").length > 0) {
@@ -53,9 +53,9 @@ presence.on("UpdateData", async () => {
     // Page de video
     
     presenceData.startTimestamp = stamp;
-    const title = document.getElementsByClassName('sc-pYA-dN sc-kNYnxC giwmIo')[0].innerHTML;
-    const episodes = (<HTMLElement>document.getElementsByClassName('sc-pTGsb iQjwMY')[0]).innerText.replace('vidéos', 'épisodes')
-    presenceData.details = title
+    const title = document.getElementsByClassName('sc-pYA-dN sc-kNYnxC giwmIo')[0].innerHTML,
+     episodes = (<HTMLElement>document.getElementsByClassName('sc-pTGsb iQjwMY')[0]).innerText.replace('vidéos', 'épisodes');
+    presenceData.details = title;
     presenceData.state = episodes;
   } else if (document.location.pathname.includes("/")) {
     presenceData.startTimestamp = stamp;
