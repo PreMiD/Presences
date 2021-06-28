db11116aef86ea226a3d9aff0e51d8d6a940604b
Mulverine <mulverin3@gmail.com>
2021-06-28 17:03:29 -0600

    More stupidity

--- websites/A/animepahe/presence.ts
+++ websites/A/animepahe/presence.ts
@@ -188,11 +188,12 @@ presence.on("UpdateData", async () => {
 
     presenceData.state = title;
 
-    if (!iframeResponse.paused) [, presenceData.endTimestamp] = presence.getTimestamps(
-      Math.floor(iframeResponse.currentTime),
-      Math.floor(iframeResponse.duration)
-    );
-    else {
+    if (!iframeResponse.paused) {
+      [, presenceData.endTimestamp] = presence.getTimestamps(
+        Math.floor(iframeResponse.currentTime),
+        Math.floor(iframeResponse.duration)
+      );
+    } else {
       presenceData.startTimestamp = null;
       presenceData.smallImageText += ` - ${getTimestamp(iframeResponse.currentTime)}`;
     }
