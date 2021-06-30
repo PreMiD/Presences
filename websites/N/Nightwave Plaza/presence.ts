77aa0d6d8cb1c275d382c32905f762dd1add36d0
vahya <85792429+vahya@users.noreply.github.com>
2021-07-01 00:48:25 +0530

    Added play history and ratings

--- websites/N/Nightwave Plaza/presence.ts
+++ websites/N/Nightwave Plaza/presence.ts
@@ -9,35 +9,58 @@ presence.on("UpdateData", async () => {
     playerTime: HTMLDivElement = document.querySelector("div.player-time"),
     playBackStatus: HTMLButtonElement =
       document.querySelector("button.player-play"),
-    listeners: HTMLDivElement = document.querySelector("div.col.cell");
+    listeners: HTMLDivElement = document.querySelector("div.col.cell"),
+    header: NodeListOf<HTMLDivElement> = document.querySelectorAll(
+      ".window > .inner > .header.header-draggable.noselect"
+    ),
+    songInfo: HTMLDivElement = document.querySelector(".p-2.song-info");
 
-  if (playerTitle) presenceData.state = playerTitle.innerText;
-  if (playerArtist) presenceData.details = playerArtist.innerText;
-
-  if (playBackStatus) {
-    switch (playBackStatus.innerText) {
-      case "Play": {
-        presenceData.smallImageKey = "play";
-        if (listeners) presenceData.smallImageText = listeners.innerText;
-        break;
-      }
-      case "Stop": {
-        presenceData.smallImageKey = "pause";
-        if (listeners) presenceData.smallImageText = listeners.innerText;
-        break;
-      }
-      default:
-        break;
+  if (songInfo) {
+    const details: NodeListOf<HTMLDivElement> =
+        songInfo.querySelectorAll(".mb-1"),
+      [artist, album, title] = [...details].map((e) => e.innerText);
+    if (artist && album && title) {
+      presenceData.details = `Looking at ${title.substring(
+        8
+      )} by ${artist.substring(10)}`;
+      presenceData.state = `Album: ${album.substring(8)}`;
     }
-  }
+  } else if (header.length === 2) {
+    let rating: HTMLButtonElement;
+    if (header[1].innerText === "Ratings")
+      rating = document.querySelector("button.active");
+    presenceData.details = `Looking at ${rating ? rating.innerText : ""} ${
+      header[1].innerText
+    }`;
+  } else {
+    if (playerTitle) presenceData.state = playerTitle.innerText;
+    if (playerArtist) presenceData.details = playerArtist.innerText;
 
-  if (playerTime) {
-    const [currentTime, duration] = playerTime.innerText
-        .split("/")
-        .map((time) => presence.timestampFromFormat(time)),
-      timestamps = presence.getTimestamps(currentTime, duration);
+    if (playBackStatus) {
+      switch (playBackStatus.innerText) {
+        case "Play": {
+          presenceData.smallImageKey = "play";
+          if (listeners) presenceData.smallImageText = listeners.innerText;
+          break;
+        }
+        case "Stop": {
+          presenceData.smallImageKey = "pause";
+          if (listeners) presenceData.smallImageText = listeners.innerText;
+          break;
+        }
+        default:
+          break;
+      }
+    }
 
-    [, presenceData.endTimestamp] = timestamps;
+    if (playerTime) {
+      const [currentTime, duration] = playerTime.innerText
+          .split("/")
+          .map((time) => presence.timestampFromFormat(time)),
+        timestamps = presence.getTimestamps(currentTime, duration);
+
+      [, presenceData.endTimestamp] = timestamps;
+    }
   }
 
   presenceData.buttons = [
