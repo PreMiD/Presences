d72e2a62e4212dca780b44d4251b955d334f8001
vahya <85792429+vahya@users.noreply.github.com>
2021-07-01 11:57:43 +0530

    Update websites/N/Nightwave Plaza/presence.ts
    Co-authored-by: Slowlife <slowlife1165@daum.net>

--- websites/N/Nightwave Plaza/presence.ts
+++ websites/N/Nightwave Plaza/presence.ts
@@ -70,7 +70,7 @@ presence.on("UpdateData", async () => {
     }
   ];
 
-  if (presenceData.details === null) {
+  if (!presenceData.details) {
     presence.setTrayTitle();
     presence.setActivity();
   } else presence.setActivity(presenceData);
