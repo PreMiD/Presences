119dbed6d4a89da9864c37f9c68bcc12a74960e8
codefactor-io <support@codefactor.io>
2021-07-02 14:45:43 +0000

    [CodeFactor] Apply fixes

--- websites/A/AnimeDigitalNetwork/presence.ts
+++ websites/A/AnimeDigitalNetwork/presence.ts
@@ -16,15 +16,15 @@ presence.on("UpdateData", async () => {
     if (video && !isNaN(video.duration)) {
       const title = document.querySelector(
         'meta[name="description"]'
-      ) as HTMLMetaElement;
-      const title2 = title.content.replace(/[0-9]/g, '').substr(6),
+      ) as HTMLMetaElement,
+       title2 = title.content.replace(/[0-9]/g, '').substr(6),
       title3 = title2.length - 131;
       if (title3 < 0) {
         const title4 = title2.length - 112;
         presenceData.details = title2.slice(0, title4);
-      } else {
+      } else 
         presenceData.details = title2.slice(0, title3);
-      }
+      
       const timestamps = presence.getTimestamps(
           Math.floor(video.currentTime),
           Math.floor(video.duration)
@@ -39,26 +39,25 @@ presence.on("UpdateData", async () => {
         delete presenceData.startTimestamp;
         delete presenceData.endTimestamp;
       }
-    }
-    else if (video && isNaN(video.duration)) {
+    } else if (video && isNaN(video.duration)) {
       const title = document.querySelector(
         'meta[name="description"]'
-      ) as HTMLMetaElement;
-      const title2 = title.content.replace(/[0-9]/g, '').substr(6),
+      ) as HTMLMetaElement,
+       title2 = title.content.replace(/[0-9]/g, '').substr(6),
       title3 = title2.length - 131;
       if (title3 < 0) {
         const title4 = title2.length - 112;
         presenceData.state = title2.slice(0, title4);
-      } else {
+      } else 
         presenceData.state = title2.slice(0, title3);
-      }
+      
       presenceData.details = "Looking at";
     }
   } else if (document.location.pathname.includes("video") && !video) {
     const title = document.querySelector(
       'meta[name="description"]'
-    ) as HTMLMetaElement;
-      const title2 = title.content.replace(/[0-9]/g, '').substr(31),
+    ) as HTMLMetaElement,
+       title2 = title.content.replace(/[0-9]/g, '').substr(31),
       title3 = title2.length - 101; 
       presenceData.details = "Looking at";
       presenceData.state = title2.slice(0, title3);
@@ -66,7 +65,7 @@ presence.on("UpdateData", async () => {
   if (presenceData.details === null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else {
+  } else 
     presence.setActivity(presenceData);
-  }
+  
 });
