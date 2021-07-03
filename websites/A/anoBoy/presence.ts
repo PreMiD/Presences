4a122c24891845d230402c06e6eeab7754302b82
Dae <notloneth@gmail.com>
2021-07-04 02:22:41 +0700

    ♻️ Refactoring

--- websites/A/anoBoy/presence.ts
+++ websites/A/anoBoy/presence.ts
@@ -1,31 +1,31 @@
 const presence = new Presence({
-  clientId: "860591302092521533"
-}),
-strings = presence.getStrings({
-  play: "general.playing",
-  pause: "general.paused",
-  viewHome: "general.viewHome",
-});
+    clientId: "860591302092521533"
+  }),
+  strings = presence.getStrings({
+    play: "general.playing",
+    pause: "general.paused",
+    viewHome: "general.viewHome",
+  });
 
 presence.on("UpdateData", async () => {
   const video: HTMLVideoElement = document.querySelector(".video-js"),
-  browsingStamp = Math.floor(Date.now() / 1000),
-  presenceData: PresenceData = {
-    largeImageKey: "logo",
-  };
+    browsingStamp = Math.floor(Date.now() / 1000),
+    presenceData: PresenceData = {
+      largeImageKey: "logo",
+    };
 
   if (video !== null && !isNaN(video.duration)) {
     const [, endTimestamp] = presence.getTimestamps(
       Math.floor(video.currentTime),
       Math.floor(video.duration)
     );
-    
+
     presenceData.details = "Watching :";
     presenceData.state = document
       .querySelector(".pagetitle > h1")
       .textContent.replace(/Subtitle Indonesia/gi, "");
-    presenceData.smallImageKey = video.paused 
-      ? "pause" 
+    presenceData.smallImageKey = video.paused
+      ? "pause"
       : "play",
     presenceData.smallImageText = video.paused
       ? (await strings).pause
