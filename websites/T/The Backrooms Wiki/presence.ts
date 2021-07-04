8f4b047ecaee06ef0858df9bf863cd1f1e2ac05a
Samm-Cheese <SammCheese.github@gmail.com>
2021-07-04 07:13:07 +0200

    Fixing CodeFactor Issue

--- websites/T/The Backrooms Wiki/presence.ts
+++ websites/T/The Backrooms Wiki/presence.ts
@@ -83,7 +83,7 @@ presence.on("UpdateData", async () => {
     ];
   }
 
-  if (presenceData.details == null) {
+  if (presenceData.details === null) {
     presence.setTrayTitle();
     presence.setActivity();
   } else 
