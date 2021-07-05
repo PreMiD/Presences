3b20ba787a436f2b78e5268514065ed38488a00d
Jayden Koh Jia Jian <jaydenkohheroku@gmail.com>
2021-07-05 21:27:36 +0800

    Prettier

--- websites/O/osu!track/presence.ts
+++ websites/O/osu!track/presence.ts
@@ -1,82 +1,77 @@
 const presence = new Presence({
-  clientId: "861180231909113866"
+    clientId: "861180231909113866"
   }),
-
   timestampe = Math.floor(Date.now() / 1000);
 
-presence.on("UpdateData", async() => {
+presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "logo",
-    startTimestamp: timestampe
-  },
+      largeImageKey: "logo",
+      startTimestamp: timestampe
+    },
+    pathnames = location.pathname,
+    urlSplit = document.URL.split("/"),
+    decodeURL = decodeURIComponent(urlSplit[5]);
 
-   pathnames = location.pathname,
-	
-	urlSplit = document.URL.split("/"),
-
-	decodeURL = decodeURIComponent(urlSplit[5]);
-
-  if (pathnames === "/osutrack/") 
-    presenceData.details = "Viewing homepage";
-	else if (pathnames === "/osutrack/user/") {
+  if (pathnames === "/osutrack/") presenceData.details = "Viewing homepage";
+  else if (pathnames === "/osutrack/user/") {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = "User not found!";
-	} else if (pathnames === `/osutrack/user/${urlSplit[5]}`) {
+  } else if (pathnames === `/osutrack/user/${urlSplit[5]}`) {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = decodeURL;
     presenceData.smallImageKey = "osu_std_logo";
     presenceData.smallImageText = "osu!standard";
-	} else if (pathnames === `/osutrack/user/${urlSplit[5]}/`) {
+  } else if (pathnames === `/osutrack/user/${urlSplit[5]}/`) {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = decodeURL;
     presenceData.smallImageKey = "osu_std_logo";
     presenceData.smallImageText = "osu!standard";
-	} else if (pathnames === `/osutrack/user/${urlSplit[5]}/taiko`) {
+  } else if (pathnames === `/osutrack/user/${urlSplit[5]}/taiko`) {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = `${decodeURL} in osu!taiko`;
     presenceData.smallImageKey = "taiko";
     presenceData.smallImageText = "Taiko";
-	} else if (pathnames === `/osutrack/user/${urlSplit[5]}/taiko/`) {
+  } else if (pathnames === `/osutrack/user/${urlSplit[5]}/taiko/`) {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = `${decodeURL} in osu!taiko`;
     presenceData.smallImageKey = "taiko";
     presenceData.smallImageText = "Taiko";
-	} else if (pathnames === `/osutrack/user/${urlSplit[5]}/ctb`) {
+  } else if (pathnames === `/osutrack/user/${urlSplit[5]}/ctb`) {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = `${decodeURL} in osu!catch the beat`;
     presenceData.smallImageKey = "ctb";
     presenceData.smallImageText = "Catch The Beat (CTB)";
-	} else if (pathnames === `/osutrack/user/${urlSplit[5]}/ctb/`) {
+  } else if (pathnames === `/osutrack/user/${urlSplit[5]}/ctb/`) {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = `${decodeURL} in osu!catch the beat`;
     presenceData.smallImageKey = "ctb";
     presenceData.smallImageText = "mania";
-	} else if (pathnames === `/osutrack/user/${urlSplit[5]}/mania`) {
+  } else if (pathnames === `/osutrack/user/${urlSplit[5]}/mania`) {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = `${decodeURL} in osu!mania`;
     presenceData.smallImageKey = "mania";
     presenceData.smallImageText = "Mania";
-	} else if (pathnames === `/osutrack/user/${urlSplit[5]}/mania/`) {
+  } else if (pathnames === `/osutrack/user/${urlSplit[5]}/mania/`) {
     presenceData.details = "Viewing a user's statistics";
     presenceData.state = `${decodeURL} in osu!mania`;
     presenceData.smallImageKey = "mania";
     presenceData.smallImageText = "Mania";
-	} else if (pathnames === "/osutrack/bestplays/") {
+  } else if (pathnames === "/osutrack/bestplays/") {
     presenceData.details = "Viewing at the Best Plays";
     presenceData.state = "osu!standard";
     presenceData.smallImageKey = "osu_std_logo";
     presenceData.smallImageText = "osu!standard";
-	} else if (pathnames === "/osutrack/bestplays/taiko/") {
+  } else if (pathnames === "/osutrack/bestplays/taiko/") {
     presenceData.details = "Viewing at the Best Plays";
     presenceData.state = "osu!taiko";
     presenceData.smallImageKey = "taiko";
     presenceData.smallImageText = "osu!taiko";
-	} else if (pathnames === "/osutrack/bestplays/ctb/") {
+  } else if (pathnames === "/osutrack/bestplays/ctb/") {
     presenceData.details = "ViewKing at the Best Plays";
     presenceData.state = "osu!catch the beat (cbt)";
     presenceData.smallImageKey = "ctb";
     presenceData.smallImageText = "osu!catch the beat";
-	} else if (pathnames === "/osutrack/bestplays/mania/") {
+  } else if (pathnames === "/osutrack/bestplays/mania/") {
     presenceData.details = "Viewing at the Best Plays";
     presenceData.state = "osu!mania";
     presenceData.smallImageKey = "mania";
@@ -85,22 +80,21 @@ presence.on("UpdateData", async() => {
     const indexText = document.querySelector("body > h1");
 
     presenceData.details = `Viewing ${indexText.textContent}`;
-  } else if (pathnames === "/osutrack/b/ads/") 
+  } else if (pathnames === "/osutrack/b/ads/")
     presenceData.details = "Viewing About Ads on osu!track";
-  else if (pathnames === "/osutrack/b/discord/") 
+  else if (pathnames === "/osutrack/b/discord/")
     presenceData.details = "Viewing about osu!track's Discord Bot";
-  else if (pathnames === "/osutrack/b/mailer/") 
+  else if (pathnames === "/osutrack/b/mailer/")
     presenceData.details = "Viewing abotu osu!track's Mailerbot Refrence";
   else if (pathnames === "/osutrack/b/pp/") {
     presenceData.details = "Viewing at Top 10 PP Plays of osu!";
     presenceData.state = "As of 10/10/2014";
-  } else if (pathnames === "/osutrack/updater/") 
+  } else if (pathnames === "/osutrack/updater/")
     presenceData.details = "Viewing at the IRC Bot's Documentation";
-  else if (pathnames === "/osutrack/updater/index.php") 
+  else if (pathnames === "/osutrack/updater/index.php")
     presenceData.details = "Viewing at the IRC Bot's Documentation";
-  
 
-	if (presenceData.details === null) {
+  if (!presenceData.details) {
     //This will fire if you do not set presence details
     presence.setTrayTitle(); //Clears the tray title for mac users
     presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
