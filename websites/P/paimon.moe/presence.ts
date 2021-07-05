e6b4c73f177caf1c1bbd9cc6eba5a94e422e9a86
Jayden Koh Jia Jian <jaydenkohheroku@gmail.com>
2021-07-05 08:58:41 +0800

    a

--- websites/P/paimon.moe/presence.ts
+++ websites/P/paimon.moe/presence.ts
@@ -58,71 +58,71 @@ presence.on("UpdateData", async () => {
   if (pathname === "/") {
     presenceData.details = "Viewing homepage";
   }
-    if (pathname.includes("/characters") || pathname.includes("/characters/")) {
-      presenceData.details = "Viewing the character list";
-      if (validCharacter) {
-        const string = document.URL,
-          original = string.match(/(\/\w+)/gi).pop().substr(1);
-        if (original === "traveler_anemo") 
-          stateText = "Anemo Traveler";
-        else if (original === "traveler_geo") 
-          stateText = "Geo Traveler";
-        else if (original === "kaedehara_kazuha") 
-          stateText = "Kaedehara Kazuha";
-        else if (original === "hu_tao") 
-          stateText = "Hu Tao";
-        else 
-          stateText = original.charAt(0).toUpperCase() + original.slice(1);
+  if (pathname.includes("/characters") || pathname.includes("/characters/")) {
+    presenceData.details = "Viewing the character list";
+    if (validCharacter) {
+      const string = document.URL,
+        original = string.match(/(\/\w+)/gi).pop().substr(1);
+      if (original === "traveler_anemo") 
+        stateText = "Anemo Traveler";
+      else if (original === "traveler_geo") 
+        stateText = "Geo Traveler";
+      else if (original === "kaedehara_kazuha") 
+        stateText = "Kaedehara Kazuha";
+      else if (original === "hu_tao") 
+        stateText = "Hu Tao";
+      else 
+        stateText = original.charAt(0).toUpperCase() + original.slice(1);
         
-        presenceData.details = "Viewing the character details:";
-        presenceData.state = stateText;
-      }
+      presenceData.details = "Viewing the character details:";
+      presenceData.state = stateText;
     }
-    if (pathname.includes("/wish/character-event") || pathname.includes("/wish/character-event/")) {
-      presenceData.details = "Viewing at the Wish Counter";
-      presenceData.state = "Wish Counter: Character Event";
-    } else if (pathname.includes("/wish/weapon-event") || pathname.includes("/wish/weapon-event/")) {
-      presenceData.details = "Viewing at the Wish Counter";
-      presenceData.state = "Wish Counter: Weapon Event";
-    } else if (pathname.includes("/wish/standard") || pathname.includes("/wish/standar/")) {
-      presenceData.details = "Viewing at the Wish Counter";
-      presenceData.state = "Wish Counter: Standard Banner";
-    } else if (pathname.includes("/wish/beginners") || pathname.includes("/wish/beginners/")) {
-      presenceData.details = "Viewing at the Wish Counter";
-      presenceData.state = "Wish Counter: Beginners' Wish";
-    } else if (pathname.includes("/wish/tally") || pathname.includes("/wish/tally/")) 
-      presenceData.details = "🌏 Global Wish Tally";
+    }
+  if (pathname.includes("/wish/character-event") || pathname.includes("/wish/character-event/")) {
+    presenceData.details = "Viewing at the Wish Counter";
+    presenceData.state = "Wish Counter: Character Event";
+  } else if (pathname.includes("/wish/weapon-event") || pathname.includes("/wish/weapon-event/")) {
+    presenceData.details = "Viewing at the Wish Counter";
+    presenceData.state = "Wish Counter: Weapon Event";
+  } else if (pathname.includes("/wish/standard") || pathname.includes("/wish/standar/")) {
+    presenceData.details = "Viewing at the Wish Counter";
+    presenceData.state = "Wish Counter: Standard Banner";
+  } else if (pathname.includes("/wish/beginners") || pathname.includes("/wish/beginners/")) {
+    presenceData.details = "Viewing at the Wish Counter";
+    presenceData.state = "Wish Counter: Beginners' Wish";
+  } else if (pathname.includes("/wish/tally") || pathname.includes("/wish/tally/")) 
+    presenceData.details = "🌏 Global Wish Tally";
     
-    else if (pathname.startsWith("/wish") || pathname.startsWith("/wish/")) {
-      const ww = document.querySelector("#sapper > main > div > div.grid.gap-4.grid-cols-1.md\\:grid-cols-2.xl\\:grid-cols-3.max-w-screen-xl > div:nth-child(6) > div.bg-item.rounded-xl.p-4.flex.items-center.w-full.text-white"),
-       [, , , number] = ww.textContent.split(" ");
+  else if (pathname.startsWith("/wish") || pathname.startsWith("/wish/")) {
+    const ww = document.querySelector("#sapper > main > div > div.grid.gap-4.grid-cols-1.md\\:grid-cols-2.xl\\:grid-cols-3.max-w-screen-xl > div:nth-child(6) > div.bg-item.rounded-xl.p-4.flex.items-center.w-full.text-white"),
+      [, , , number] = ww.textContent.split(" ");
 
       presenceData.details = "Viewing at the Wish Counter";
-      presenceData.state = `Wishes Worth: ✧ ${number}`;
-    } else if (pathname.includes("/calculator") || pathname.includes("/calculator/")) 
-      presenceData.details = "Using the calculator";
+    presenceData.state = `Wishes Worth: ✧ ${number}`;
+  } else if (pathname.includes("/calculator") || pathname.includes("/calculator/")) 
+    presenceData.details = "Using the calculator";
       
-    else if (pathname.includes("/todo") || pathname.includes("/todo/")) 
-      presenceData.details = "Viewing at Todo list";
+  else if (pathname.includes("/todo") || pathname.includes("/todo/")) 
+    presenceData.details = "Viewing at Todo list";
     
-    else if (pathname.includes("/items") || pathname.includes("/items/")) {
+  else if (pathname.includes("/items") || pathname.includes("/items/")) {
+    presenceData.details = "Viewing at the Database";
+    presenceData.state = "Items";
+  } else if (pathname.includes("/achievement") || pathname.includes("/achievement/")) {
+    presenceData.details = "Viewing at the Database";
+    presenceData.state = "Achievements";
+  } else if (pathname.includes("/reminder") || pathname.includes("/reminder/")) {
+    presenceData.details = "Viewing at the Database";
+    presenceData.state = "Reminder";
+  } else if (pathname.includes("/furnishing") || pathname.includes("/furnishing/")) {
+    presenceData.details = "Viewing at the Database";
+    presenceData.state = "Furnishing";
+    if (pathname.includes("/furnishing/inventory") || pathname.includes("/furnishing/inventory/")) {
       presenceData.details = "Viewing at the Database";
-      presenceData.state = "Items";
-    } else if (pathname.includes("/achievement") || pathname.includes("/achievement/")) {
+      presenceData.state = "Looking at Furnishing Inventory";
+    } else if (pathname.includes("/furnishing/list") || pathname.includes("/furnishing/list/")) {
       presenceData.details = "Viewing at the Database";
-      presenceData.state = "Achievements";
-    } else if (pathname.includes("/reminder") || pathname.includes("/reminder/")) {
-      presenceData.details = "Viewing at the Database";
-      presenceData.state = "Reminder";
-    } else if (pathname.includes("/furnishing") || pathname.includes("/furnishing/")) {
-      presenceData.details = "Viewing at the Database";
-      presenceData.state = "Furnishing";
-      if (pathname.includes("/furnishing/inventory") || pathname.includes("/furnishing/inventory/")) {
-        presenceData.details = "Viewing at the Database";
-        presenceData.state = "Looking at Furnishing Inventory";
-      } else if (pathname.includes("/furnishing/list") || pathname.includes("/furnishing/list/")) {
-        presenceData.details = "Viewing at the Database";
-        presenceData.state = "Looking at the Furnishing List";
+      presenceData.state = "Looking at the Furnishing List";
       }
     } else if (pathname.includes("/weapons") || pathname.includes("/weapons/")) {
       presenceData.details = "Viewing at the Database";
