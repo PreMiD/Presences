7ba832875f97c3e4764eca071c64f54dbd9ed146
DarkVillager <darkville13@gmail.com>
2021-07-06 14:26:03 +0200

    Prettier :thumbsup:

--- websites/A/AnimeDigitalNetwork/presence.ts
+++ websites/A/AnimeDigitalNetwork/presence.ts
@@ -7,7 +7,9 @@ const presence = new Presence({
   });
 
 presence.on("UpdateData", async () => {
-  const video: HTMLVideoElement = document.querySelector("#adn-video-js_html5_api"),
+  const video: HTMLVideoElement = document.querySelector(
+      "#adn-video-js_html5_api"
+    ),
     presenceData: PresenceData = {
       largeImageKey: "logo"
     };
@@ -15,20 +17,19 @@ presence.on("UpdateData", async () => {
   if (document.location.pathname.includes("video") && video) {
     if (video && !isNaN(video.duration)) {
       const title = document.querySelector(
-        'meta[name="description"]'
-      ) as HTMLMetaElement,
-       title2 = title.content.replace(/[0-9]/g, '').substr(6),
-      title3 = title2.length - 131;
+          'meta[name="description"]'
+        ) as HTMLMetaElement,
+        title2 = title.content.replace(/[0-9]/g, " ").substr(6),
+        title3 = title2.length - 131;
       if (title3 < 0) {
         const title4 = title2.length - 112;
         presenceData.details = title2.slice(0, title4);
-      } else 
-        presenceData.details = title2.slice(0, title3);
-      
+      } else presenceData.details = title2.slice(0, title3);
+
       const timestamps = presence.getTimestamps(
-          Math.floor(video.currentTime),
-          Math.floor(video.duration)
-        );
+        Math.floor(video.currentTime),
+        Math.floor(video.duration)
+      );
       presenceData.smallImageKey = video.paused ? "pause" : "play";
       presenceData.smallImageText = video.paused
         ? (await strings).pause
@@ -41,31 +42,28 @@ presence.on("UpdateData", async () => {
       }
     } else if (video && isNaN(video.duration)) {
       const title = document.querySelector(
-        'meta[name="description"]'
-      ) as HTMLMetaElement,
-       title2 = title.content.replace(/[0-9]/g, '').substr(6),
-      title3 = title2.length - 131;
+          'meta[name="description"]'
+        ) as HTMLMetaElement,
+        title2 = title.content.replace(/[0-9]/g, " ").substr(6),
+        title3 = title2.length - 131;
       if (title3 < 0) {
         const title4 = title2.length - 112;
         presenceData.state = title2.slice(0, title4);
-      } else 
-        presenceData.state = title2.slice(0, title3);
-      
+      } else presenceData.state = title2.slice(0, title3);
+
       presenceData.details = "Looking at";
     }
   } else if (document.location.pathname.includes("video") && !video) {
     const title = document.querySelector(
-      'meta[name="description"]'
-    ) as HTMLMetaElement,
-       title2 = title.content.replace(/[0-9]/g, '').substr(31),
-      title3 = title2.length - 101; 
-      presenceData.details = "Looking at";
-      presenceData.state = title2.slice(0, title3);
+        'meta[name="description"]'
+      ) as HTMLMetaElement,
+      title2 = title.content.replace(/[0-9]/g, " ").substr(31),
+      title3 = title2.length - 101;
+    presenceData.details = "Looking at";
+    presenceData.state = title2.slice(0, title3);
   } else presenceData.details = "Browsing...";
   if (presenceData.details === null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else 
-    presence.setActivity(presenceData);
-  
+  } else presence.setActivity(presenceData);
 });
