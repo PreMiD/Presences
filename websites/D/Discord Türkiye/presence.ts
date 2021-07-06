ab057c1232aa027e25f8baca48fca03ea08e64d7
codefactor-io <support@codefactor.io>
2021-07-06 13:21:03 +0000

    [CodeFactor] Apply fixes

--- websites/D/Discord Türkiye/presence.ts
+++ websites/D/Discord Türkiye/presence.ts
@@ -6,146 +6,145 @@ presence.on("UpdateData", () => {
       largeImageKey: "logo"
   }, page = window.location.pathname, browsingStamp = Math.floor(Date.now() / 1000);
   presenceData.startTimestamp = browsingStamp;
-  if (page.startsWith("/")) {
+  if (page.startsWith("/")) 
       presenceData.details = "Anasayfayı Görüntülüyor.";
-  }
-  if (page.startsWith("/members")) {
+  
+  if (page.startsWith("/members")) 
       presenceData.details = "Bir kullanıcının profilini görüntülüyor.";
-  }
-  else if (page.startsWith("/categories")) {
+  
+  else if (page.startsWith("/categories")) 
       presenceData.details = "Bir kategori görüntülüyor.";
-  }
-  else if (page.startsWith("/threads")) {
+  
+  else if (page.startsWith("/threads")) 
       presenceData.details = "Bir konuyu görüntülüyor.";
-  }
-  else if (page.startsWith("/whats-new")) {
+  
+  else if (page.startsWith("/whats-new")) 
       presenceData.details = "Yeni konuları görüntülüyor.";
-  }
-  else if (page.endsWith("/duyurular/")) {
+  
+  else if (page.endsWith("/duyurular/")) 
       presenceData.details = "Duyurular sayfasını görüntülüyor.";
-  }
-  else if (page.endsWith("/forum-destek/")) {
+  
+  else if (page.endsWith("/forum-destek/")) 
       presenceData.details = "Forum Destek sayfasını görüntülüyor.";
-  }
-  else if (page.endsWith("/cekilis.84/")) {
+  
+  else if (page.endsWith("/cekilis.84/")) 
       presenceData.details = "Çekiliş sayfasını görüntülüyor.";
-  }
-  else if (page.endsWith("/discord-oyunlari.79/")) {
+  
+  else if (page.endsWith("/discord-oyunlari.79/")) 
       presenceData.details = "Discord Oyunları sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/discord-icerikleri.83/")) {
+  
+  else if (page.endsWith("/discord-icerikleri.83/")) 
       presenceData.details = "Discord İçerikleri sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/bot-tanitim/")) {
+  
+  else if (page.endsWith("/bot-tanitim/")) 
       presenceData.details = "Bot Tanıtım sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/altyapi-paylasim/")) {
+  
+  else if (page.endsWith("/altyapi-paylasim/")) 
       presenceData.details = "Bot Altyapı Paylaşım sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/discord-kod-paylasim/")) {
+  
+  else if (page.endsWith("/discord-kod-paylasim/")) 
       presenceData.details = "Kod Paylaşım sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/discord-kod-destek/")) {
+  
+  else if (page.endsWith("/discord-kod-destek/")) 
       presenceData.details = "Kod Destek sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/discord-sunucu/")) {
+  
+  else if (page.endsWith("/discord-sunucu/")) 
       presenceData.details = "Discord Sunucu sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/minecraft-genel/")) {
+  
+  else if (page.endsWith("/minecraft-genel/")) 
       presenceData.details = "Minecraft Genel sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/minecraft-sunucular/")) {
+  
+  else if (page.endsWith("/minecraft-sunucular/")) 
       presenceData.details = "Minecraft Sunucuları sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/html-css.43/")) {
+  
+  else if (page.endsWith("/html-css.43/")) 
       presenceData.details = "Html , Css sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/javascript.46/")) {
+  
+  else if (page.endsWith("/javascript.46/")) 
       presenceData.details = "Javascript sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/php.44/")) {
+  
+  else if (page.endsWith("/php.44/")) 
       presenceData.details = "PHP sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/python.45/")) {
+  
+  else if (page.endsWith("/python.45/")) 
       presenceData.details = "Python sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/java.48/")) {
+  
+  else if (page.endsWith("/java.48/")) 
       presenceData.details = "Java sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/c.49/")) {
+  
+  else if (page.endsWith("/c.49/")) 
       presenceData.details = "C sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("c.50/")) {
+  
+  else if (page.endsWith("c.50/")) 
       presenceData.details = "C++ sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/c.51/")) {
+  
+  else if (page.endsWith("/c.51/")) 
       presenceData.details = "C# sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/bash-shell.52/")) {
+  
+  else if (page.endsWith("/bash-shell.52/")) 
       presenceData.details = "Bash / Shell sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("visual-basic-net.74/")) {
+  
+  else if (page.endsWith("visual-basic-net.74/")) 
       presenceData.details = "Visual Basic .NET sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/steam.58/")) {
+  
+  else if (page.endsWith("/steam.58/")) 
       presenceData.details = "Steam sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/epic-games.59/")) {
+  
+  else if (page.endsWith("/epic-games.59/")) 
       presenceData.details = "Epic Games sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/origin.60/")) {
+  
+  else if (page.endsWith("/origin.60/")) 
       presenceData.details = "Origin sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/uplay.61/")) {
+  
+  else if (page.endsWith("/uplay.61/")) 
       presenceData.details = "UPlay sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/playstation.63/")) {
+  
+  else if (page.endsWith("/playstation.63/")) 
       presenceData.details = "PlayStation sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/xbox.64/")) {
+  
+  else if (page.endsWith("/xbox.64/")) 
       presenceData.details = "Xbox sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("nintendo.65/")) {
+  
+  else if (page.endsWith("nintendo.65/")) 
       presenceData.details = "Nintendo sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/genel.67/")) {
+  
+  else if (page.endsWith("/genel.67/")) 
       presenceData.details = "Genel sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/muezik.69/")) {
+  
+  else if (page.endsWith("/muezik.69/")) 
       presenceData.details = "Müzik sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/film-dizi.70/")) {
+  
+  else if (page.endsWith("/film-dizi.70/")) 
       presenceData.details = "Film & Dizi sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/tarih-kueltuer.71/")) {
+  
+  else if (page.endsWith("/tarih-kueltuer.71/")) 
       presenceData.details = "Tarih & Kültür sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/youtube-twitch.72/")) {
+  
+  else if (page.endsWith("/youtube-twitch.72/")) 
       presenceData.details = "YouTube & Twitch sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/sosyal-medya.73/")) {
+  
+  else if (page.endsWith("/sosyal-medya.73/")) 
       presenceData.details = "Sosyal Medya sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/methodlar.77/")) {
+  
+  else if (page.endsWith("/methodlar.77/")) 
       presenceData.details = "Methodlar sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/hosting.75/")) {
+  
+  else if (page.endsWith("/hosting.75/")) 
       presenceData.details = "Hosting sayfasında dolaşıyor...";
-  }
-  else if (page.endsWith("/account-details")) {
+  
+  else if (page.endsWith("/account-details")) 
       presenceData.details = "Hesap ayarlarına göz atıyor... 👀";
-  }
-  else if (page.endsWith("/signature")) {
+  
+  else if (page.endsWith("/signature")) 
       presenceData.details = "Hesap imzasını düzenliyor.";
-  }
-  else if (page.endsWith("/admin")) {
+  
+  else if (page.endsWith("/admin")) 
       presenceData.details = "Erişemeyeceği bir yere erişmeye çalışıyor... 👀";
-  }
+  
   if (presenceData.details == null) {
       presence.setTrayTitle();
       presence.setActivity();
-  }
-  else {
+  } else 
       presence.setActivity(presenceData);
-  }
+  
 });
