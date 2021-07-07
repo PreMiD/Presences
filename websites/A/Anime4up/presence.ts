34235f163a1438bf74a9ff810a4c5013b130588e
bash-ghaith <bash-ghaith@gmail.com>
2021-07-07 10:01:13 +0300

    improving routing (so we can get out of the else-ifs' hell)

--- websites/A/Anime4up/presence.ts
+++ websites/A/Anime4up/presence.ts
@@ -1,29 +1,134 @@
-interface LangStrings {
-  play: string;
-  pause: string;
-  browse: string;
-  viewingMovie: string;
-  viewingSeries: string;
-  watchingMovie: string;
-  watchingSeries: string;
-  search: string;
-  viewAnime: string;
+type Functionlize<T> = {
+  [P in keyof T]: () => T[P];
+};
+
+interface Route extends Functionlize<Partial<PresenceData>> {
+  path: RegExp;
+  playback?(): boolean;
+  run?(): PresenceData;
 }
+
+enum Icons {
+  PAUSED = "paused",
+  PLAYED = "played",
+  SEARCHING = "searching",
+  LOCATION = "location",
+  DISCOVERY = "discovery"
+}
+
+let video = { duration: 0, currentTime: 0, paused: true };
 
 const presence = new Presence({
     clientId: "770030754356396052"
   }),
-  getTimestamps = (videoTime: number, videoDuration: number): Array<number> => {
+  getTimestamps = (videoTime: number, videoDuration: number): number[] => {
     const startTime = Date.now(),
       endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
     return [Math.floor(startTime / 1000), endTime];
-  };
+  },
+  router = ({ path, data }: { path: string; data: PresenceData }): Route => {
+    const routes: Route[] = [
+      {
+        path: /^\/episode\//,
+        run: () => {
+          const timestamps = getTimestamps(
+            Math.floor(video.currentTime),
+            Math.floor(video.duration)
+          );
 
-let video = {
-  duration: 0,
-  currentTime: 0,
-  paused: true
-};
+          [data.startTimestamp, data.endTimestamp] = timestamps;
+
+          if (video.paused) {
+            delete data.startTimestamp;
+            delete data.endTimestamp;
+          }
+
+          return data;
+        },
+
+        playback: () => !video.paused,
+
+        smallImageKey: () => (video.paused ? Icons.PAUSED : Icons.PLAYED),
+
+        smallImageText: () => (video.paused ? "Paused" : "Played"),
+
+        state: () =>
+          document
+            .querySelectorAll(".container")[1]
+            .textContent.slice(1, -1)
+            .split(" ")
+            .slice(0, -2)
+            .join(" "),
+
+        details: () =>
+          `Episode: ${document
+            .querySelectorAll(".container")[1]
+            .textContent.slice(1, -1)
+            .split(" ")
+            .pop()}`,
+
+        buttons: () => [
+          { label: "Watch Episode", url: location.href },
+          {
+            label: "Anime Page",
+            url: document
+              .querySelector(".anime-page-link")
+              .querySelector("a")
+              .getAttribute("href")
+          }
+        ]
+      },
+      {
+        path: /^\/\?search_param=(.*)/,
+
+        smallImageKey: () => Icons.SEARCHING,
+
+        smallImageText: () => "Searching",
+
+        details: () =>
+          `Results: ${document.querySelectorAll(".col-lg-2").length ?? 0}`,
+
+        state: () =>
+          `Searching: ${document
+            .querySelectorAll(".container")[1]
+            .textContent.split(" ")
+            .slice(4, -1)
+            .join(" ")}`
+      },
+      {
+        path: /^\/anime\/(.*)/,
+
+        smallImageKey: () => Icons.LOCATION,
+
+        smallImageText: () => "Viewing",
+
+        details: () => "Viewing an Anime",
+
+        state: () => document.querySelector(".anime-details-title").textContent
+      },
+      {
+        path: /^\/(%d9%82%d8%a7%d8%a6%d9%85%d8%a9-%d8%a7%d9%84%d8%a7%d9%86%d9%85%d9%8a|anime-(.*))/,
+
+        smallImageKey: () => Icons.DISCOVERY,
+
+        smallImageText: () => "Browsing",
+
+        details: () => "Browsing for Anime"
+      },
+      {
+        path: /^\/%d9%85%d9%88%d8%a7%d8%b9%d9%8a%d8%af-%d8%b9%d8%b1%d8%b6-%d8%ad%d9%84%d9%82%d8%a7%d8%aa-%d8%a7%d9%84%d8%a7%d9%86%d9%85%d9%8a/,
+
+        smallImageKey: () => Icons.DISCOVERY,
+
+        smallImageText: () => "Discovering",
+
+        details: () => "Discovering Episodes Releases"
+      }
+    ];
+
+    presence.info(path);
+    return routes.find((route) => route.path.test(path));
+  };
 
 presence.on(
   "iFrameData",
@@ -33,94 +138,110 @@ presence.on(
 );
 
 presence.on("UpdateData", async () => {
-  const data: PresenceData = {
+  let data: PresenceData = {
     startTimestamp: Math.floor(Date.now() / 1000),
     largeImageKey: "logo"
   };
 
-  if (location.pathname.startsWith("/episode")) {
-    const timestamps = getTimestamps(
-      Math.floor(video.currentTime),
-      Math.floor(video.duration)
-    );
+  const path = location.href.replace("https://" + location.hostname, ""),
+    route = router({ data, path });
+  if (!route) return presence.setActivity(data);
 
-    data.state = document
-      .querySelectorAll(".container")[1]
-      .textContent.slice(1, -1)
-      .split(" ")
-      .slice(0, -2)
-      .join(" ");
-    data.details = `Episode: ${document
-      .querySelectorAll(".container")[1]
-      .textContent.slice(1, -1)
-      .split(" ")
-      .pop()}`;
+  if (route.run) data = route.run();
+  if (route.state) data.state = route.state();
+  if (route.buttons) data.buttons = route.buttons();
+  if (route.details) data.details = route.details();
+  if (route.endTimestamp) data.endTimestamp = route.endTimestamp();
+  if (route.largeImageKey) data.largeImageKey = route.largeImageKey();
+  if (route.smallImageKey) data.smallImageKey = route.smallImageKey();
+  if (route.smallImageText) data.smallImageText = route.smallImageText();
+  if (route.startTimestamp) data.startTimestamp = route.startTimestamp();
 
-    data.smallImageKey = video.paused ? "paused" : "played";
-    data.smallImageText = video.paused ? "Paused" : "Played";
-    [data.startTimestamp, data.endTimestamp] = timestamps;
+  presence.setActivity(data, route.playback ? route.playback() : false);
 
-    if (video.paused) {
-      delete data.startTimestamp;
-      delete data.endTimestamp;
-    }
+  // if (location.pathname.startsWith("/episode")) {
+  //   const timestamps = getTimestamps(
+  //     Math.floor(video.currentTime),
+  //     Math.floor(video.duration)
+  //   );
 
-    const animeURL = document
-      .querySelector(".anime-page-link")
-      .querySelector("a")
-      .getAttribute("href");
+  //   data.state = document
+  //     .querySelectorAll(".container")[1]
+  //     .textContent.slice(1, -1)
+  //     .split(" ")
+  //     .slice(0, -2)
+  //     .join(" ");
+  //   data.details = `Episode: ${document
+  //     .querySelectorAll(".container")[1]
+  //     .textContent.slice(1, -1)
+  //     .split(" ")
+  //     .pop()}`;
 
-    data.buttons = [
-      { label: "Watch Episode", url: location.href },
-      { label: "Anime Page", url: animeURL }
-    ];
+  //   data.smallImageKey = video.paused ? "paused" : "played";
+  //   data.smallImageText = video.paused ? "Paused" : "Played";
+  //   [data.startTimestamp, data.endTimestamp] = timestamps;
 
-    presence.setActivity(data, !video.paused);
-  } else if (
-    location.href.startsWith("https://ww.anime4up.com/?search_param=animes&s=")
-  ) {
-    data.smallImageKey = "searching";
-    data.smallImageText = "Searching";
-    data.state = `Searching: ${document
-      .querySelectorAll(".container")[1]
-      .textContent.split(" ")
-      .slice(4, -1)
-      .join(" ")}`;
-    data.details = `Results: ${
-      document.querySelectorAll(".col-lg-2").length
-        ? document.querySelectorAll(".col-lg-2").length
-        : "Nothing"
-    }`;
-    presence.setActivity(data);
-  } else if (location.pathname.startsWith("/anime/")) {
-    data.smallImageKey = "location";
-    data.smallImageText = "Viewing";
-    data.state = document.querySelector(".anime-details-title").textContent;
-    data.details = "Viewing an Anime";
+  //   if (video.paused) {
+  //     delete data.startTimestamp;
+  //     delete data.endTimestamp;
+  //   }
 
-    presence.setActivity(data);
-  } else if (
-    location.pathname.startsWith(
-      "/%d9%82%d8%a7%d8%a6%d9%85%d8%a9-%d8%a7%d9%84%d8%a7%d9%86%d9%85%d9%8a"
-    ) ||
-    location.pathname.includes("/anime-")
-  ) {
-    data.smallImageKey = "discovery";
-    data.smallImageText = "Browsing";
-    data.details = "Browsing for Anime";
+  //   const animeURL = document
+  //     .querySelector(".anime-page-link")
+  //     .querySelector("a")
+  //     .getAttribute("href");
 
-    presence.setActivity(data);
-  } else if (
-    location.pathname.startsWith(
-      "/%d9%85%d9%88%d8%a7%d8%b9%d9%8a%d8%af-%d8%b9%d8%b1%d8%b6-%d8%ad%d9%84%d9%82%d8%a7%d8%aa-%d8%a7%d9%84%d8%a7%d9%86%d9%85%d9%8a"
-    )
-  ) {
-    data.smallImageKey = "discovery";
-    data.smallImageText = "Discovering";
-    data.details = "Discovering Episodes Dates";
-    presence.setActivity(data);
-  } else if (location.pathname === "/") {
-    data.details = "On Homepage";
-    presence.setActivity(data);
-  }
+  //   data.buttons = [
+  //     { label: "Watch Episode", url: location.href },
+  //     { label: "Anime Page", url: animeURL }
+  //   ];
+
+  //   presence.setActivity(data, !video.paused);
+  // } else if (
+  //   location.href.startsWith("https://ww.anime4up.com/?search_param=animes&s=")
+  // ) {
+  //   data.smallImageKey = "searching";
+  //   data.smallImageText = "Searching";
+  //   data.state = `Searching: ${document
+  //     .querySelectorAll(".container")[1]
+  //     .textContent.split(" ")
+  //     .slice(4, -1)
+  //     .join(" ")}`;
+  //   data.details = `Results: ${
+  //     document.querySelectorAll(".col-lg-2").length
+  //       ? document.querySelectorAll(".col-lg-2").length
+  //       : "Nothing"
+  //   }`;
+  //   presence.setActivity(data);
+  // } else if (location.pathname.startsWith("/anime/")) {
+  //   data.smallImageKey = "location";
+  //   data.smallImageText = "Viewing";
+  //   data.state = document.querySelector(".anime-details-title").textContent;
+  //   data.details = "Viewing an Anime";
+
+  //   presence.setActivity(data);
+  // } else if (
+  //   location.pathname.startsWith(
+  //     "/%d9%82%d8%a7%d8%a6%d9%85%d8%a9-%d8%a7%d9%84%d8%a7%d9%86%d9%85%d9%8a"
+  //   ) ||
+  //   location.pathname.includes("/anime-")
+  // ) {
+  //   data.smallImageKey = "discovery";
+  //   data.smallImageText = "Browsing";
+  //   data.details = "Browsing for Anime";
+
+  //   presence.setActivity(data);
+  // } else if (
+  //   location.pathname.startsWith(
+  //     "/%d9%85%d9%88%d8%a7%d8%b9%d9%8a%d8%af-%d8%b9%d8%b1%d8%b6-%d8%ad%d9%84%d9%82%d8%a7%d8%aa-%d8%a7%d9%84%d8%a7%d9%86%d9%85%d9%8a"
+  //   )
+  // ) {
+  //   data.smallImageKey = "discovery";
+  //   data.smallImageText = "Discovering";
+  //   data.details = "Discovering Episodes Releases";
+  //   presence.setActivity(data);
+  // } else if (location.pathname === "/") {
+  //   data.details = "On Homepage";
+  //   presence.setActivity(data);
+  // }
 });
