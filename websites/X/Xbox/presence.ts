674b68fb3fdece8f8902d038042ed89453c7199a
codefactor-io <support@codefactor.io>
2021-07-08 12:07:23 +0000

    [CodeFactor] Apply fixes

--- websites/X/Xbox/presence.ts
+++ websites/X/Xbox/presence.ts
@@ -13,59 +13,59 @@ presence.on("UpdateData", async () => {
   if (document.location.href.includes("/game-pass")) {
     presenceData.largeImageKey = "gamepass";
     presenceData.details = "Reading about Xbox Game Pass";
-    if (document.location.href.includes("games")) {
+    if (document.location.href.includes("games")) 
       presenceData.details = "Browsing Xbox Game Pass games";
-    }
+    
   }
 
   //Live Gold
   else if (document.location.href.includes("/live/gold")) {
     presenceData.details = "Reading about Xbox Live Gold";
-    if (document.location.href.includes("withgold")) {
+    if (document.location.href.includes("withgold")) 
       presenceData.details = "Viewing Xbox Live Gold benefits";
-    }
+    
   }
 
   //Games
   else if (document.location.href.includes("/games")) {
     presenceData.details = "Browsing Xbox game catalog";
-    if (document.location.href.includes("optimized")) {
+    if (document.location.href.includes("optimized")) 
       presenceData.details = "Reading about Series X|S optimized games";
-    } else if (document.location.href.includes("backward-compatibility")) {
+     else if (document.location.href.includes("backward-compatibility")) 
       presenceData.details = "Reading about backward compatible games";
-    }
+    
   }
 
   //Consoles
   else if (document.location.href.includes("/consoles")) {
     presenceData.details = "Viewing Xbox consoles";
-    if (document.location.href.includes("consoles/all-consoles")) {
+    if (document.location.href.includes("consoles/all-consoles")) 
       presenceData.details = "Browsing all Xbox consoles";
-    } else if (document.location.href.includes("consoles/help-me-choose")) {
+     else if (document.location.href.includes("consoles/help-me-choose")) 
       presenceData.details = "Determining their recommended Xbox console";
-    } else if (document.location.href.includes("consoles/")) {
+     else if (document.location.href.includes("consoles/")) {
       presenceData.details = "Viewing an Xbox console";
       presenceData.state = document.title.split("|")[0];
-      presenceData.buttons = [{ label: "View this console", url: document.location.href }]
-    } else if (document.location.href.includes("backward-compatibility")) {
+      presenceData.buttons = [{ label: "View this console", url: document.location.href }];
+    } else if (document.location.href.includes("backward-compatibility")) 
       presenceData.details = "Reading about backward compatible games";
-    }
+    
   }
 
   //Accessories
   else if (document.location.href.includes("/accessories")) {
     presenceData.details = "Browsing Xbox accessories";
-    if (document.location.href.includes("consoles/all-consoles")) {
+    if (document.location.href.includes("consoles/all-consoles")) 
       presenceData.details = "Browsing all Xbox consoles";
-    } else if (document.location.href.includes("consoles/help-me-choose")) {
+     else if (document.location.href.includes("consoles/help-me-choose")) 
       presenceData.details = "Determining their recommended Xbox console";
-    } else if (document.location.href.includes("accessories/")) {
+     else if (document.location.href.includes("accessories/")) {
       presenceData.details = "Viewing an Xbox accessory";
       presenceData.state = document.title.split("|")[0];
-      presenceData.buttons = [{ label: "View this accessory", url: document.location.href }]
-    } else if (document.location.href.includes("backward-compatibility")) {
+      presenceData.buttons = [{ label: "View this accessory", url: document.location.href }];
+    } else if (document.location.href.includes("backward-compatibility")) 
       presenceData.details = "Reading about backward compatible games";
-    }
+    
   }
 
   //Play
@@ -77,27 +77,27 @@ presence.on("UpdateData", async () => {
     } else if (document.location.href.includes("play/launch")) {
       presenceData.details = "Playing an Xbox Cloud Gaming game";
       presenceData.state = document.title.split("|")[0];
-      presenceData.buttons = [{ label: "Play this game (Game Pass)", url: document.location.href }]
+      presenceData.buttons = [{ label: "Play this game (Game Pass)", url: document.location.href }];
       if (document.querySelector(`[class^="Provisioning"`)) presenceData.details += " (setting up)";
       else if (document.querySelector(`[class^="NotFocused"`)) presenceData.details += " (unfocused)";
     } else {
       presenceData.details = "Browsing Xbox Cloud Gaming games";
-      if (document.location.href.includes("gallery/")) presenceData.state = "Category: " + document.title.split("|")[0];
+      if (document.location.href.includes("gallery/")) presenceData.state = `Category: ${document.title.split("|")[0]}`;
     }
   }
 
   //Community
   else if (document.location.href.includes("/community")) {
-    presenceData.details = "Viewing the Xbox Community"
-    if (document.location.href.includes("esports")) {
+    presenceData.details = "Viewing the Xbox Community";
+    if (document.location.href.includes("esports")) 
       presenceData.details = "Reading about Xbox Esports";
-    }
+    
   }
 
   //My Xbox
   else if (document.location.hostname === "account.xbox.com") {
     presenceData.details = "Viewing their profile";
-    if (document.location.href.includes("gamertag=")) presenceData.details = "Viewing profile: " + document.title.split("-")[0];
+    if (document.location.href.includes("gamertag=")) presenceData.details = `Viewing profile: ${document.title.split("-")[0]}`;
   }
 
   //Support
@@ -114,15 +114,15 @@ presence.on("UpdateData", async () => {
 
   //Other
   else {
-    presenceData.details = "Browsing the website"
-    presenceData.state = "Page: " + document.title
-    if (document.location.pathname.length < 8) presenceData.state = "Homepage"
+    presenceData.details = "Browsing the website";
+    presenceData.state = `Page: ${document.title}`;
+    if (document.location.pathname.length < 8) presenceData.state = "Homepage";
   }
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else {
+  } else 
     presence.setActivity(presenceData);
-  }
+  
 });
