55b3744ff21f2e6309e038d610e164096f457ea2
codefactor-io <support@codefactor.io>
2021-07-09 19:31:06 +0000

    [CodeFactor] Apply fixes to commit 8cfbc9a

--- websites/A/ADKami/presence.ts
+++ websites/A/ADKami/presence.ts
@@ -3,9 +3,9 @@ const presence = new Presence({ clientId: "863104116502429717" });
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey: "logo"
-  };
+  },
 
-  const path: string = document.location.pathname;
+   path: string = document.location.pathname;
   let cat: string = path.split("/")[1];
 
   if (cat === "drama-streaming") cat = "drama";
@@ -13,31 +13,31 @@ presence.on("UpdateData", async () => {
   if (cat !== "") {
     const HtmlTitle: Element = document.querySelector("#body > section > div.col-12.col-l-9.fiche-look > div.row.blocshadow > div > h1");
     if (HtmlTitle) {
-      let titleParts: string[] = HtmlTitle.textContent.trim().split("-");
+      const titleParts: string[] = HtmlTitle.textContent.trim().split("-"),
 
-      let descrParts: string[] = titleParts.pop().split(" ");
-      let lang: string = descrParts.pop().toUpperCase();
-      let descr = descrParts.join(" ");
+       descrParts: string[] = titleParts.pop().split(" "),
+       lang: string = descrParts.pop().toUpperCase(),
+       descr = descrParts.join(" ");
 
       presenceData.details = titleParts.join("-");
       presenceData.state = `${descr} (${lang})`;
 
-      let bigImage: string = document.querySelector("#body > section > div.col-12.col-l-9.fiche-look > div.row.blocshadow > div > img.col-12.m-hidden").getAttribute("src");
+      const bigImage: string = document.querySelector("#body > section > div.col-12.col-l-9.fiche-look > div.row.blocshadow > div > img.col-12.m-hidden").getAttribute("src");
 
-      presenceData.smallImageText = bigImage
+      presenceData.smallImageText = bigImage;
 
       console.log(bigImage);
 
-    } else {
-      presenceData.details = "Parcours les " + cat + "s";
-    }
+    } else 
+      presenceData.details = `Parcours les ${cat}s`;
+    
   }
 
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else {
+  } else 
     presence.setActivity(presenceData);
-  }
+  
 });
