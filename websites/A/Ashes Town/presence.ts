33aa359a490ddda6af86acafc773d098ffe45793
Kirill <s1d3z5cv@gmail.com>
2021-07-12 19:53:24 +0300

    Fix icons

--- websites/A/Ashes Town/presence.ts
+++ websites/A/Ashes Town/presence.ts
@@ -12,27 +12,27 @@ presence.on("UpdateData", async () => {
         if (document.location.pathname == "/" && document.getElementsByClassName("app-game")["app-game"].hidden == true) {
         // Ashes Town Homepage
         presenceData.details = "Viewing At Home Page";
-        presenceData.smallImageKey = "home-solid_white";
+        presenceData.smallImageKey = "home-solid_green";
         } else if (document.location.pathname == "/help") {
           presenceData.details = "Viewing Page:";
           presenceData.state = "Help";
-          presenceData.smallImageKey = "question-circle-solid_white";
+          presenceData.smallImageKey = "question-circle-solid_green";
         } else if (document.location.pathname == "/about") {
           presenceData.details = "Viewing Page:";
           presenceData.state = "About";
-          presenceData.smallImageKey = "info-circle-solid_white";
+          presenceData.smallImageKey = "info-circle-solid_green";
         } else if (document.location.pathname == "/contributors") {
           presenceData.details = "Viewing Page:";
           presenceData.state = "Contributors";
-          presenceData.smallImageKey = "info-circle-solid_white";
+          presenceData.smallImageKey = "info-circle-solid_green";
         } else if (document.location.pathname == "/character") {
           presenceData.details = "Viewing Page:";
           presenceData.state = "Characters";
-          presenceData.smallImageKey = "horse-head-solid_white";
+          presenceData.smallImageKey = "horse-head-solid_green";
         } else if (document.location.pathname == "/account") {
           presenceData.details = "Viewing Page:";
           presenceData.state = "Account";
-          presenceData.smallImageKey = "user-solid_white";
+          presenceData.smallImageKey = "user-solid_green";
         }
 
         if (document.location.pathname == "/" && document.getElementsByClassName("app-game")["app-game"].hidden == false) {
