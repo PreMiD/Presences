b42b2f621c60a20c41c93730f6161a9c21b1d29b
João Duque <woobiewhy@gmail.com>
2021-07-14 14:18:44 -0300

    Add timestamp for video

--- websites/A/Animesuge/presence.ts
+++ websites/A/Animesuge/presence.ts
@@ -24,22 +24,39 @@ const presence = new Presence({
     "/user/import": "Importing their MAL list to Animesuge!"
   };
 
+let endTime: number,
+  currentTime: number;
+
+function getTimestamps(
+  videoTime: number,
+  videoDuration: number
+): Array<number> {
+  const startTime = Date.now(),
+    endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  return [Math.floor(startTime / 1000), endTime];
+}
+
+presence.on("iFrameData", async (data: {endTime: number; currentTime: number;}) => {
+  ({endTime, currentTime} = data);
+});
+
 presence.on("UpdateData", async () => {
   const page = document.location.pathname,
     animeName = document.querySelector(
       "#body > div > div > div > div > section > div > h1"
     ) as HTMLElement,
     data: PresenceData = {
-      largeImageKey: "animesuge",
-      startTimestamp: Math.floor(Date.now() / 1000)
+      largeImageKey: "animesuge"
     },
     search: URLSearchParams = new URLSearchParams(document.location.search.substring(1));
   if (page === "/") {
     data.details = "At the home page";
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page.includes("/anime")) {
     data.details = pages["/anime"];
     data.state = `Watching ${animeName.textContent}`;
+    [data.startTimestamp, data.endTimestamp] = getTimestamps(currentTime, endTime);
     data.buttons = [
       {
         label: "Watch it too!",
@@ -50,51 +67,67 @@ presence.on("UpdateData", async () => {
     const genre = page.slice("/genre/".length);
     data.details = pages["/genre"];
     data.state = `Searching for ${genre.charAt(0).toUpperCase() + genre.slice(1)} Animes`;
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/newest") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/updated") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/ongoing") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/added") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/tv") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/movie") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/ova") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/ona") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/special") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page.includes("/search")) {
     data.details = pages[page];
     data.state = `Searching: "${search.get("keyword")}"`;
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/az-list") {
     data.details = pages[page];
     data.state = "Seaching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/most-watched") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/upcoming") {
     data.details = pages[page];
     data.state = "Searching animes";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/faq") {
     data.details = pages[page];
     data.state = "Reading";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/contact") {
     data.details = pages[page];
     data.state = "Reading";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else if (page === "/user/settings") {
     data.details = pages[page];
     data.state = "Changing";
@@ -102,14 +135,17 @@ presence.on("UpdateData", async () => {
     const list = search.get("folder");
     data.details = pages[page];
     data.state = `At folder: ${list}`;
+    data.startTimestamp = Math.floor(Date.now() / 1000);
     if (list === null)
       data.state = "Looking at all animes in the watch list";
   } else if (page === "/user/import") {
     data.details = pages[page];
     data.state = "Importing!";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   } else {
     data.details = "Looking at an unknown page";
     data.state = "Unknown";
+    data.startTimestamp = Math.floor(Date.now() / 1000);
   }
   if (data.details !== null && data.state !== null)
     presence.setActivity(data);
