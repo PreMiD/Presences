0b1fd2b67e97c8dd883854a8a484bba59bf09b62
codefactor-io <support@codefactor.io>
2021-07-18 19:54:06 +0000

    [CodeFactor] Apply fixes

--- websites/M/megastudy/presence.ts
+++ websites/M/megastudy/presence.ts
@@ -6,14 +6,14 @@ const presence = new Presence({
     pause: "presence.playback.paused"
   });
 
-var browsingStamp = Math.floor(Date.now() / 1000);
-var isTitleChecked = false;
-var title: string, isPlayerPlaying: boolean, currentTime: number, duration: number
+let browsingStamp = Math.floor(Date.now() / 1000),
+ isTitleChecked = false,
+ title: string, isPlayerPlaying: boolean, currentTime: number, duration: number;
 
 function unescapeHtml(str: string) {
-  if (str == null) {
+  if (str == null) 
    return "";
-  }
+  
   return str
     .replace(/&nbsp;/g, ' ')
     .replace(/&amp;/g, '&')
@@ -27,7 +27,7 @@ function unescapeHtml(str: string) {
 presence.on("iFrameData", (data: { isPlayerPlaying: boolean; currentTime: number; duration: number; }) => {
   isPlayerPlaying = data.isPlayerPlaying,
   currentTime = data.currentTime,
-  duration = data.duration
+  duration = data.duration;
 });
 
 presence.on("UpdateData", async () => {
@@ -37,53 +37,53 @@ presence.on("UpdateData", async () => {
   };
 
   if (document.location.pathname == "/megastudy.asp" || document.location.pathname == "/") {
-    presenceData.details = "홈 화면"
-    presenceData.startTimestamp = browsingStamp
+    presenceData.details = "홈 화면";
+    presenceData.startTimestamp = browsingStamp;
 
   } else if (document.location.pathname.includes("Player")) {
 
-    presenceData.details = unescapeHtml(document.querySelector(".txt").innerHTML)
+    presenceData.details = unescapeHtml(document.querySelector(".txt").innerHTML);
 
     if (!isTitleChecked) {
       try {
-        title = unescapeHtml(document.querySelector("tr.on td").innerHTML)
-        isTitleChecked = true
+        title = unescapeHtml(document.querySelector("tr.on td").innerHTML);
+        isTitleChecked = true;
       } catch(e) {
-        title = "[수강정보]를 클릭하여 주세요"
+        title = "[수강정보]를 클릭하여 주세요";
       }
     }
 
-    presenceData.state = title
+    presenceData.state = title;
 
     if (isPlayerPlaying) {
       const [, endTimestamp] = presence.getTimestamps(currentTime, duration);
-      presenceData.endTimestamp = endTimestamp
-      presenceData.smallImageKey = "play"
-      presenceData.smallImageText = (await strings).play
+      presenceData.endTimestamp = endTimestamp;
+      presenceData.smallImageKey = "play";
+      presenceData.smallImageText = (await strings).play;
     } else {
       delete presenceData.endTimestamp;
-      presenceData.smallImageKey = "pause"
-      presenceData.smallImageText = (await strings).pause
+      presenceData.smallImageKey = "pause";
+      presenceData.smallImageText = (await strings).pause;
     }
 
   } else if (document.location.pathname.includes("my_std_room/main.asp")) {
-    presenceData.details = "내 강의실"
-    presenceData.startTimestamp = browsingStamp
+    presenceData.details = "내 강의실";
+    presenceData.startTimestamp = browsingStamp;
     
   } else if (document.location.pathname.includes("my_std_room/detail.asp")) {
-    presenceData.details = unescapeHtml(document.querySelector(".name").innerHTML)
-    presenceData.state = unescapeHtml(document.querySelector("h4").innerHTML)
-    presenceData.startTimestamp = browsingStamp
+    presenceData.details = unescapeHtml(document.querySelector(".name").innerHTML);
+    presenceData.state = unescapeHtml(document.querySelector("h4").innerHTML);
+    presenceData.startTimestamp = browsingStamp;
 
   } else {
-    presenceData.details = "합격 불변의 법칙"
-    presenceData.startTimestamp = browsingStamp
+    presenceData.details = "합격 불변의 법칙";
+    presenceData.startTimestamp = browsingStamp;
   }
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else {
+  } else 
     presence.setActivity(presenceData);
-  }
+  
 });
