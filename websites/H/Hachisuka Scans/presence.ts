71d1cffc2eb4750aa759af57f42c91440d1ce93e
Yuu-San <42509424+Yuujet@users.noreply.github.com>
2021-07-19 03:49:18 -0400

    Update presence.ts

--- websites/H/Hachisuka Scans/presence.ts
+++ websites/H/Hachisuka Scans/presence.ts
@@ -26,14 +26,14 @@ presence.on("UpdateData", async () => {
             url: document.URL
           }
         ];
-    }
+    };
   } else if (
     (document.location.pathname.includes("/user-settings/"))
   ) {
-    const userPageSelect = document.querySelector(
+   const userPageSelect = document.querySelector(
       "#post-5 > div.entry-content > div > div > div.col-md-3.col-sm-3 > div > ul > li.active"
       ),
-     userName = document.querySelector(
+      userName = document.querySelector(
       "body > div.wrap > div > header > div.c-sub-header-nav.with-border > div > div > div.c-modal_item > div > span"
       );
     presenceData.details = `Minha Conta ( ${userName.textContent.replace("Hi, ","")} )`;
@@ -53,7 +53,7 @@ presence.on("UpdateData", async () => {
       } else {
         presenceData.details = `Postando [ ${postMangaType.value} ] ( ${postMangaCap.value} )`; 
         presenceData.state = postMangaName.value;
-      }
+      };
       presenceData.buttons = [
           {
             label: `Página do ${postMangaName.value.slice(0, 15)}...`,
@@ -63,7 +63,7 @@ presence.on("UpdateData", async () => {
     } else {
       presenceData.details = null;
       presenceData.state = null;
-    }
+    };
   } else if (document.location.pathname.includes("/obras/")) {
     const obraNum = document.querySelector(
       "body > div.wrap > div > div > div.c-page-content.style-1 > div > div > div > div > div > div > div.c-page__content > div.tab-wrap > div > div.h4"
@@ -73,7 +73,7 @@ presence.on("UpdateData", async () => {
     const genrTexto = document.querySelector(
       "body > div.wrap > div > div > div.c-page-content.style-1 > div > div > div > div > div.main-col-inner > div > div.entry-header > div > div > h1"
     ),
-     genrNumObra = document.querySelector(
+    genrNumObra = document.querySelector(
       "body > div.wrap > div > div > div.c-page-content.style-1 > div > div > div > div > div > div > div.c-page__content > div.tab-wrap > div > div.h4"
       );
     presenceData.details = `Gênero: ${genrTexto.innerText} ( ${genrNumObra.textContent.replace(" result", "").replace("s", "")} )`;
@@ -82,13 +82,13 @@ presence.on("UpdateData", async () => {
       const mangaNome = document.querySelector(
         "body > div.wrap > div > div > div > div.profile-manga.summary-layout-2 > div > div > div > div.post-title > h1"
         ),
-       mangaType = document.querySelector(
+      mangaType = document.querySelector(
         "body > div.wrap > div > div > div > div.profile-manga.summary-layout-2 > div > div > div > div.tab-summary > div.summary_content_wrap > div > div > div:nth-child(9) > div.summary-content"
-        ), 
-       mangaGenr = document.querySelector(
+        ),
+      mangaGenr = document.querySelector(
         "body > div.wrap > div > div > div > div.profile-manga.summary-layout-2 > div > div > div > div.tab-summary > div.summary_content_wrap > div > div > div:nth-child(8) > div.summary-content > div"
         ),
-       teste = document.querySelector(
+      teste = document.querySelector(
         "body > div.wrap > div > div > div > div.profile-manga.summary-layout-2 > div > div > div > div.tab-summary > div.summary_content_wrap > div > div > div:nth-child(9) > div.summary-content"
         );
       presenceData.buttons = [
@@ -105,13 +105,13 @@ presence.on("UpdateData", async () => {
       const readerMangaNome = document.querySelector(
         "#manga-reading-nav-head > div > div.entry-header_wrap > div > div.c-breadcrumb > ol > li:nth-child(2) > a"
         ),
-       readerMangaCap = document.querySelector(
+      readerMangaCap = document.querySelector(
         "#manga-reading-nav-head > div > div.entry-header_wrap > div > div.c-breadcrumb > ol > li.active"
         ),
-       obraUrl = document.querySelector(
+      obraUrl = document.querySelector(
         "#manga-reading-nav-head > div > div.entry-header_wrap > div > div.c-breadcrumb > ol > li:nth-child(2) > a"
         ),
-       obraCapNumTotal = document.querySelector(
+      obraCapNumTotal = document.querySelector(
         `#manga-reading-nav-head > div > div.select-view > div.chapter-selection.chapters_selectbox_holder > div > label > select > option:nth-child(1)`
         );
       presenceData.buttons = [
