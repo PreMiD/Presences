a7733d04b00226df151eb2b1fa72e52489e99cae
Northernside <zeidlereliasmanuel@protonmail.com>
2021-07-21 19:46:52 +0200

    Initial

---
+++ websites/L/LABY.net/presence.ts
@@ -0,0 +1,43 @@
+const presence = new Presence({
+  clientId: "867452106016161822"
+});
+
+const browsingStamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "large_image",
+    details: "LABY.net"
+  };
+
+  presenceData.startTimestamp = browsingStamp;
+  if(document.location.pathname.startsWith("/skins")) {
+    presenceData.details = "Viewing skins";
+  } else if(document.location.pathname.startsWith("/skin")) {
+    presenceData.details = "Viewing skins";
+  } else if(document.location.pathname.startsWith("/cloaks")) {
+    presenceData.details = "Viewing LabyMod cloaks";
+  } else if(document.location.pathname.startsWith("/capes")) {
+    presenceData.details = "Viewing Minecraft capes";
+  } else if(document.location.pathname.startsWith("/@")) {
+    presenceData.details = `Viewing ${document.location.pathname.split("/@")[1]}'s profile`;
+  } else if(document.location.pathname.startsWith("/settings")) {
+    presenceData.details = "Viewing profile settings";
+  } else if(document.location.pathname.startsWith("/server")) {
+    const item = document.querySelector("div.server-info-wrapper>h1").textContent;
+    presenceData.details = `Viewing server ${item}`;
+  } else if(document.location.pathname.startsWith("/badge")) {
+    const item = document.querySelector("div.mb-1>h1").textContent;
+    presenceData.details = `Viewing badge ${item}`;
+  } else if(document.location.pathname.startsWith("/cape")) {
+    const item = document.querySelector("div.mb-1>h1").textContent;
+    presenceData.details = `Viewing cape ${item}`;
+  }
+
+  if(presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
