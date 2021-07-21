45f3a752f2eb71be20a388e13938b99e610179d2
1LUXION <illusion106@naver.com>
2021-07-22 00:30:04 +0900

    Simplified Code
    unified "const" statement and browsingStamp assignment

--- websites/M/megastudy/presence.ts
+++ websites/M/megastudy/presence.ts
@@ -5,14 +5,14 @@ const presence = new Presence({
     play: "presence.playback.playing",
     pause: "presence.playback.paused"
   }),
+  video = {
+    isPlayerPlaying: false,
+    currentTime: 0,
+    duration: 0
+  },
   browsingStamp = Math.floor(Date.now() / 1000);
 
 let isTitleChecked = false, title: string;
-const video = {
-  isPlayerPlaying: false,
-  currentTime: 0,
-  duration: 0
-};
 
 presence.on("iFrameData", (data: { isPlayerPlaying: boolean; currentTime: number; duration: number; }) => {
   Object.assign(video, data);
@@ -24,11 +24,7 @@ presence.on("UpdateData", async () => {
     largeImageKey: "logo"
   };
 
-  if (document.location.pathname === "/megastudy.asp" || document.location.pathname === "/") {
-    presenceData.details = "홈 화면";
-    presenceData.startTimestamp = browsingStamp;
-
-  } else if (document.location.pathname.includes("Player")) {
+  if (document.location.pathname.includes("Player")) {
 
     presenceData.details = document.querySelector(".txt").textContent;
 
@@ -54,46 +50,43 @@ presence.on("UpdateData", async () => {
       presenceData.smallImageText = (await strings).pause;
     }
 
-  } else if (document.location.pathname.includes("my_std_room/main.asp")) {
-    presenceData.details = "내 강의실";
-    presenceData.startTimestamp = browsingStamp;
-    
-  } else if (document.location.pathname.includes("my_std_room/detail.asp")) {
-    presenceData.details = document.querySelector(".name").textContent;
-    presenceData.state = document.querySelector("h4").textContent;
-    presenceData.startTimestamp = browsingStamp;
-    
-  } else if (document.location.pathname.includes("/teacher_v2/teacher_main.asp")) {
-    presenceData.details = "메가선생님";
-    presenceData.startTimestamp = browsingStamp;
-
-  } else if (document.location.pathname.includes("/teacher_v2/main.asp")) {
-    [presenceData.details, presenceData.state] = (document.querySelector(".lnb_tit") as HTMLElement).innerText.split("\n");
-    presenceData.startTimestamp = browsingStamp;
-
-  } else if (document.location.pathname.includes("/Mypage/mp_2017/main/main.asp")) {
-    presenceData.details = "마이페이지";
-    presenceData.startTimestamp = browsingStamp;
-
-  } else if (document.location.pathname.includes("/mypage/mp_2017/notice/list.asp")) {
-    presenceData.details = "알림";
-    presenceData.startTimestamp = browsingStamp;
-
-  } else if (document.location.pathname.includes("/Mypage/mp_2017/apply/chr_new/main.asp")) {
-    presenceData.details = "수강신청";
-    presenceData.startTimestamp = browsingStamp;
-
-  } else if (document.location.pathname.includes("/mypage/mp_2017/pay/cart/cart_list.asp")) {
-    presenceData.details = "장바구니";
-    presenceData.startTimestamp = browsingStamp;
-
-  } else if (document.location.pathname.includes("/mypage/mp_2017/order/order_list.asp")) {
-    presenceData.details = "주문/배송조회";
-    presenceData.startTimestamp = browsingStamp;
-
   } else {
-    presenceData.details = "합격 불변의 법칙";
     presenceData.startTimestamp = browsingStamp;
+
+    if (document.location.pathname === "/megastudy.asp" || document.location.pathname === "/") {
+      presenceData.details = "홈 화면";
+  
+    } else if (document.location.pathname.includes("my_std_room/main.asp")) {
+      presenceData.details = "내 강의실";
+      
+    } else if (document.location.pathname.includes("my_std_room/detail.asp")) {
+      presenceData.details = document.querySelector(".name").textContent;
+      presenceData.state = document.querySelector("h4").textContent;
+      
+    } else if (document.location.pathname.includes("/teacher_v2/teacher_main.asp")) {
+      presenceData.details = "메가선생님";
+  
+    } else if (document.location.pathname.includes("/teacher_v2/main.asp")) {
+      [presenceData.details, presenceData.state] = (document.querySelector(".lnb_tit") as HTMLElement).innerText.split("\n");
+  
+    } else if (document.location.pathname.includes("/Mypage/mp_2017/main/main.asp")) {
+      presenceData.details = "마이페이지";
+  
+    } else if (document.location.pathname.includes("/mypage/mp_2017/notice/list.asp")) {
+      presenceData.details = "알림";
+  
+    } else if (document.location.pathname.includes("/Mypage/mp_2017/apply/chr_new/main.asp")) {
+      presenceData.details = "수강신청";
+  
+    } else if (document.location.pathname.includes("/mypage/mp_2017/pay/cart/cart_list.asp")) {
+      presenceData.details = "장바구니";
+  
+    } else if (document.location.pathname.includes("/mypage/mp_2017/order/order_list.asp")) {
+      presenceData.details = "주문/배송조회";
+  
+    } else {
+      presenceData.details = "합격 불변의 법칙";
+    }
   }
 
   if (presenceData.details === null) {
