80342163a52deb4af979282ac314a482d89f76e7
Northernside <zeidlereliasmanuel@protonmail.com>
2021-07-22 18:09:41 +0200

    Update websites/L/LABY.net/presence.ts
    Co-authored-by: N0chteil <53608074+N0chteil@users.noreply.github.com>

--- websites/L/LABY.net/presence.ts
+++ websites/L/LABY.net/presence.ts
@@ -24,7 +24,8 @@ presence.on("UpdateData", () => {
     presenceData.details = "Viewing profile settings";
   else if(document.location.pathname.startsWith("/server")) {
     const item = document.querySelector("div.server-info-wrapper>h1").textContent;
-    presenceData.details = `Viewing server: ${item}`;
+    presenceData.details = "Viewing server";
+    presenceData.state = item;
   } else if(document.location.pathname.startsWith("/badge")) {
     const item = document.querySelector("div.mb-1>h1").textContent;
     presenceData.details = "Viewing badge";
