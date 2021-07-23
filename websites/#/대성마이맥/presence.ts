52a097a831b3d637c8d7958fbc7e34f6abeaebb3
1LUXION <32902575+1LUXION@users.noreply.github.com>
2021-07-23 16:12:05 +0900

    Update websites/#/대성마이맥/presence.ts
    Co-authored-by: Slowlife <slowlife1165@daum.net>

--- websites/#/대성마이맥/presence.ts
+++ websites/#/대성마이맥/presence.ts
@@ -131,7 +131,7 @@ presence.on("UpdateData", async () => {
     
   }
 
-  if (presenceData.details === null) {
+  if (!presenceData.details) {
     presence.setTrayTitle();
     presence.setActivity();
   } else 
