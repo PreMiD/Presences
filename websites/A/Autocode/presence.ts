8b169336fa7cebfda9e4802aeba7863ff9d7205a
codefactor-io <support@codefactor.io>
2021-07-24 17:48:54 +0000

    [CodeFactor] Apply fixes to commit 2e70423

--- websites/A/Autocode/presence.ts
+++ websites/A/Autocode/presence.ts
@@ -2,12 +2,10 @@ const presence = new Presence({
 
     clientId: "858292108195921920"
 
-})
+}),
 
 
-
-const supportedLanguages : Array<String> = [ 'js', 'md', 'json', 'gitignore', 'txt', 'html', 'css']
-
+ supportedLanguages : Array<string> = [ 'js', 'md', 'json', 'gitignore', 'txt', 'html', 'css'];
 
 
 presence.on("UpdateData", async () => {
@@ -16,94 +14,80 @@ presence.on("UpdateData", async () => {
 
         largeImageKey: "autocode"
 
-    };
+    },
 
 
+     { pathname } = window.location,
 
-    const { pathname } = window.location
-
-    let path = pathname.split("/").slice(1)
-
+     path = pathname.split("/").slice(1);
 
 
     if (pathname.includes("/snippet")) {
 
-        if(path.length >= 3) {
+        if(path.length >= 3) 
 
-            presenceData.details = document.querySelector("h1.snippet-title.h3").textContent.trim()
+            presenceData.details = document.querySelector("h1.snippet-title.h3").textContent.trim();
 
-        } else {
+         else 
 
            presenceData.details = "Looking for Snippets";
 
-        }
 
+        presenceData.state = `${window.location.hostname}/${path[0]}`;
 
-
-        presenceData.state = `${window.location.hostname}/${path[0]}`
-
-        presenceData.smallImageKey = 'snippet'
+        presenceData.smallImageKey = 'snippet';
 
     } else if(pathname.includes("/app")) {
 
-        if(path.length >= 3) {
+        if(path.length >= 3) 
 
-            presenceData.details = document.querySelector("h1.jumbo").textContent.trim()
+            presenceData.details = document.querySelector("h1.jumbo").textContent.trim();
 
-        } else {
+         else 
 
            presenceData.details = "Looking for Apps";
 
-        }
 
+        presenceData.state = `${window.location.hostname}/${path[0]}`;
 
-
-        presenceData.state = `${window.location.hostname}/${path[0]}`
-
-        presenceData.smallImageKey = 'apps'
-
+        presenceData.smallImageKey = 'apps';
 
 
     } else if(pathname.includes("/lib")) {
 
-        if(path.length >= 3) {
+        if(path.length >= 3) 
 
-            presenceData.details = `Reading ${path[1]} docs`
+            presenceData.details = `Reading ${path[1]} docs`;
 
-        } else {
+         else 
 
            presenceData.details = "Looking for Docs";
 
-        }
 
+        presenceData.state = `${window.location.hostname}/${path[0]}`;
 
-
-        presenceData.state = `${window.location.hostname}/${path[0]}`
-
-        presenceData.smallImageKey = 'lib'
+        presenceData.smallImageKey = 'lib';
 
     } else if(pathname.includes("/p/")) {
 
-        const filename = document.querySelector("div.filename") ? document.querySelector("div.filename").textContent.split("/").pop() : null 
+        const filename = document.querySelector("div.filename") ? document.querySelector("div.filename").textContent.split("/").pop() : null; 
 
         if(!filename) return;
-
         
 
-        const extension = filename.match(/\.\w+/g) ? filename.match(/\.\w+/g)[0].replace(".", "") : false
+        const extension = filename.match(/\.\w+/g) ? filename.match(/\.\w+/g)[0].replace(".", "") : false;
 
-        presenceData.details = `Project: ${path[2]}`
+        presenceData.details = `Project: ${path[2]}`;
 
-        presenceData.state = `Editing: ${filename}`
+        presenceData.state = `Editing: ${filename}`;
 
-        presenceData.smallImageKey = 'autocode'
+        presenceData.smallImageKey = 'autocode';
 
-        if(extension && supportedLanguages.includes(extension)) presenceData.largeImageKey = `lang-${extension}`
+        if(extension && supportedLanguages.includes(extension)) presenceData.largeImageKey = `lang-${extension}`;
 
-        else presenceData.largeImageKey = `autocode`
+        else presenceData.largeImageKey = `autocode`;
 
     }
-
 
 
     if (!presenceData.details) {
@@ -112,12 +96,9 @@ presence.on("UpdateData", async () => {
 
         presence.setActivity();
 
-    }
-
-    else {
+    } else 
 
         presence.setActivity(presenceData);
-
-    }
+    
 
 });
