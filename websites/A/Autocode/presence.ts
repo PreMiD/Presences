912ff755fa70c49216f040303659b29d82b5542b
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2021-07-24 23:16:14 +0530

    Update presence.ts

--- websites/A/Autocode/presence.ts
+++ websites/A/Autocode/presence.ts
@@ -1,61 +1,61 @@
 const presence = new Presence({
     clientId: "858292108195921920"
-}),
-
- supportedLanguages : Array<string> = [ 'js', 'md', 'json', 'gitignore', 'txt', 'html', 'css'];
-
+});
+const supportedLanguages = ['js', 'md', 'json', 'gitignore', 'txt', 'html', 'css'];
 presence.on("UpdateData", async () => {
-    const presenceData : PresenceData = {
+    const presenceData = {
         largeImageKey: "autocode"
-    },
-
-     { pathname } = window.location,
-     path = pathname.split("/").slice(1);
-
+    };
+    const { pathname } = window.location;
+    let path = pathname.split("/").slice(1);
     if (pathname.includes("/snippet")) {
-        if(path.length >= 3) 
+        if (path.length >= 3) {
             presenceData.details = document.querySelector("h1.snippet-title.h3").textContent.trim();
-         else 
-           presenceData.details = "Looking for Snippets";
-        
-
+        }
+        else {
+            presenceData.details = "Looking for Snippets";
+        }
         presenceData.state = `${window.location.hostname}/${path[0]}`;
         presenceData.smallImageKey = 'snippet';
-    } else if(pathname.includes("/app")) {
-        if(path.length >= 3) 
+    }
+    else if (pathname.includes("/app")) {
+        if (path.length >= 3) {
             presenceData.details = document.querySelector("h1.jumbo").textContent.trim();
-         else 
-           presenceData.details = "Looking for Apps";
-        
-
+        }
+        else {
+            presenceData.details = "Looking for Apps";
+        }
         presenceData.state = `${window.location.hostname}/${path[0]}`;
         presenceData.smallImageKey = 'apps';
-
-    } else if(pathname.includes("/lib")) {
-        if(path.length >= 3) 
+    }
+    else if (pathname.includes("/lib")) {
+        if (path.length >= 3) {
             presenceData.details = `Reading ${path[1]} docs`;
-         else 
-           presenceData.details = "Looking for Docs";
-        
-
+        }
+        else {
+            presenceData.details = "Looking for Docs";
+        }
         presenceData.state = `${window.location.hostname}/${path[0]}`;
         presenceData.smallImageKey = 'lib';
-    } else if(pathname.includes("/p/")) {
-        const filename = document.querySelector("div.filename") ? document.querySelector("div.filename").textContent.split("/").pop() : null; 
-        if(!filename) return;
-        
+    }
+    else if (pathname.includes("/p/")) {
+        const filename = document.querySelector("div.filename") ? document.querySelector("div.filename").textContent.split("/").pop() : null;
+        if (!filename)
+            return;
         const extension = filename.match(/\.\w+/g) ? filename.match(/\.\w+/g)[0].replace(".", "") : false;
         presenceData.details = `Project: ${path[2]}`;
         presenceData.state = `Editing: ${filename}`;
         presenceData.smallImageKey = 'autocode';
-        if(extension && supportedLanguages.includes(extension)) presenceData.largeImageKey = `lang-${extension}`;
-        else presenceData.largeImageKey = `autocode`;
+        if (extension && supportedLanguages.includes(extension))
+            presenceData.largeImageKey = `lang-${extension}`;
+        else
+            presenceData.largeImageKey = `autocode`;
     }
-
     if (!presenceData.details) {
         presence.setTrayTitle();
         presence.setActivity();
-    } else 
+    }
+    else {
         presence.setActivity(presenceData);
-    
+    }
 });
