69e681b081825c71ee473ecba323326272fa22da
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2021-07-25 12:31:56 +0530

    Update presence.ts

--- websites/A/Autocode/presence.ts
+++ websites/A/Autocode/presence.ts
@@ -1,6 +1,7 @@
 const presence = new Presence({
   clientId: "858292108195921920"
-}), supportedLanguages: Array<string> = [ "js", "md", "json", "gitignore", "txt", "html", "css"];
+}),
+supportedLanguages: Array<string> = [ "js", "md", "json", "gitignore", "txt", "html", "css"];
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
@@ -8,15 +9,13 @@ presence.on("UpdateData", async () => {
   }, { pathname } = window.location, path = pathname.split("/").slice(1);
   
   if (pathname.includes("/snippet")) {
-    if (path.length >= 3) 
-      presenceData.details = document.querySelector("h1.snippet-title.h3").textContent.trim();
- else presenceData.details = "Looking for Snippets";
+    if (path.length >= 3) presenceData.details = document.querySelector("h1.snippet-title.h3").textContent.trim();
+    else presenceData.details = "Looking for Snippets";
     presenceData.state = `${window.location.hostname}/${path[0]}`;
     presenceData.smallImageKey = "snippet";
   } else if (pathname.includes("/app")) {
-    if (path.length >= 3) 
-      presenceData.details = document.querySelector("h1.jumbo").textContent.trim();
- else presenceData.details = "Looking for Apps";
+    if (path.length >= 3) presenceData.details = document.querySelector("h1.jumbo").textContent.trim();
+    else presenceData.details = "Looking for Apps";
     presenceData.state = `${window.location.hostname}/${path[0]}`;
     presenceData.smallImageKey = "apps";
   } else if (pathname.includes("/lib")) {
@@ -29,15 +28,13 @@ presence.on("UpdateData", async () => {
       ? document.querySelector("div.filename").textContent.split("/").pop()
       : null;
     if (!filename) return;
-    
     const extension = filename.match(/\.\w+/g)
       ? filename.match(/\.\w+/g)[0].replace(".", "")
       : false;
     presenceData.details = `Project: ${path[2]}`;
     presenceData.state = `Editing: ${filename}`;
     presenceData.smallImageKey = "autocode";
-    if (extension && supportedLanguages.includes(extension))
-      presenceData.largeImageKey = `lang-${extension}`;
+    if (extension && supportedLanguages.includes(extension)) presenceData.largeImageKey = `lang-${extension}`;
     else presenceData.largeImageKey = `autocode`;
   }
 
