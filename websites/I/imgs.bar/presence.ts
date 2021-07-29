d1f4479168ded7b2fb019b8af46a5973ee3ab248
Finley <81249080+f7nl4y@users.noreply.github.com>
2021-07-29 16:15:02 +0100

    Update presence.ts

--- websites/I/imgs.bar/presence.ts
+++ websites/I/imgs.bar/presence.ts
@@ -1,84 +1,76 @@
 const presence = new Presence({
-  clientId: "870075637619118131"
+    clientId: "870075637619118131"
 });
 
 presence.on("UpdateData", async () => {
-  const {title} = document,
+  const title = document.title,
     path = document.location.pathname,
 
-    presenceData: PresenceData = {
+   presenceData: PresenceData = {
       largeImageKey: "large",
       details: title,
-      state: null,
-    };
+      state: null
+  };
 
   switch (path) {
     case "/":
-      presenceData.details = "Viewing imgs.bar";
-      presenceData.state = "A private file host";
-      break;
-    case "/dashboard": {
-      presenceData.startTimestamp = Date.now();
-      presenceData.details = "Viewing the gallery";
-      break;
-    }
+        presenceData.details = "A private file host";
+        break;
+    case "/dasboard":
+        presenceData.startTimestamp = Date.now();
+        presenceData.details = "Viewing gallery";
+        break;
     case "/settings": {
-      const intervalEditor = document.getElementsByClassName("ant-modal-wrap ant-modal-centered")[0] as HTMLElement,
-        embedEditor = document.getElementsByClassName("embedEditor")[0] as HTMLElement,
-        domainPicker = document.getElementsByClassName("ant-select-open")[0] as HTMLElement,
-        fakeUrlManager = document.getElementsByClassName("fakeUrlManager")[0] as HTMLElement;
+      const intervalEditor = <HTMLElement>document.getElementsByClassName("ant-modal-wrap ant-modal-centered")[0],
+       embedEditor = <HTMLElement>document.getElementsByClassName("embedEditor")[0],
+       domainPicker = document.getElementsByClassName("ant-select-open")[0];
 
       if (embedEditor && embedEditor.parentElement.style.display !== "none") {
-        presenceData.state = "Editing embed";
-        presenceData.startTimestamp = Date.now();
+          presenceData.details = "";
+          presenceData.state = "Editing embed";
+          presenceData.startTimestamp = Date.now();
       } else if (domainPicker) {
-        presenceData.startTimestamp = Date.now();
-        presenceData.state = "Choosing a domain";
+          presenceData.details = "";
+          presenceData.state = "Choosing a domain";
+          presenceData.startTimestamp = Date.now();
       } else if (intervalEditor && intervalEditor.style.display !== "none") {
-        presenceData.startTimestamp = Date.now();
-        presenceData.state = "Editing auto-wipe interval";
-      } else if (fakeUrlManager && fakeUrlManager.parentElement.style.display !== "none") {
-        presenceData.startTimestamp = Date.now();
-        presenceData.state = "Editing Fake URL";
-      } else 
-        presenceData.startTimestamp = Date.now();
-        // presenceData.state = "Viewing their settings";
-      
-      break;
-    }
-    case "/settings/domains": {
-      const randomDomainPicker = document.getElementsByClassName("ant-select-open")[0] as HTMLElement;
-      if (randomDomainPicker) {
-        presenceData.startTimestamp = Date.now();
-        presenceData.state = `Adding a random domain`;
-      } else if (!document.body.textContent.includes("Loading")) {
-        presenceData.startTimestamp = Date.now();
-        presenceData.state = `Viewing ${document.querySelectorAll('strong')[0].innerHTML} domains`;
+          presenceData.details = "";
+          presenceData.state = "Editing auto-wipe interval";
+          presenceData.startTimestamp = Date.now();
+      } else {
+          presenceData.startTimestamp = Date.now();
+          presenceData.details = "Viewing settings";
       }
+
       break;
     }
+    case "/domains":
+        presenceData.startTimestamp = Date.now();
+        presenceData.details = "Viewing the domain manager";
+        break;
     case "/tools/upload":
-      presenceData.startTimestamp = Date.now();
-      presenceData.details = "Viewing the uploader";
-      break;
+        presenceData.details = "Viewing uploader";
+        presenceData.startTimestamp = Date.now();
+        break;
     case "/tools/shorten":
-      presenceData.startTimestamp = Date.now();
-      presenceData.details = "Viewing the shortener";
-      break;
+        presenceData.details = "Viewing the shortener";
+        presenceData.startTimestamp = Date.now();
+        break;
     case "/account":
-      presenceData.startTimestamp = Date.now();
-      presenceData.details = "Account";
-      break;
-    default:
-      presenceData.state = "Viewing imgs.bar";
+        presenceData.details = "Viewing their account";
+        presenceData.startTimestamp = Date.now();
+        break;
+    case !isNaN(parseInt(path.charAt(path.length - 1))):
+        presenceData.details = "";
+        presenceData.state = `Viewing ${document.getElementsByClassName("title___aqmzM")[0].innerHTML}"s profile`;
+        presenceData.startTimestamp = Date.now();
+        break;
   }
-  if (!isNaN(parseInt(path.charAt(path.length - 1), 10))) 
-    presenceData.state = `Viewing ${document.getElementsByClassName("title___aqmzM")[0].innerHTML}'s profile`;
-  
-  if (presenceData.details === null) {
-    presence.setTrayTitle();
-    presence.setActivity();
-  } else 
-    presence.setActivity(presenceData);
-  
+
+  if (presenceData.details == null) {
+      presence.setTrayTitle();
+      presence.setActivity();
+  } else {
+      presence.setActivity(presenceData);
+  }
 });
