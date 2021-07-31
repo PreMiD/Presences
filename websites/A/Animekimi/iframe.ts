16f9177c6b80053bd671592f6bea01a88d747051
Faelayis <48393914+Faelayis@users.noreply.github.com>
2021-07-31 14:18:20 +0700

    fixes

--- websites/A/Animekimi/iframe.ts
+++ websites/A/Animekimi/iframe.ts
@@ -3,11 +3,10 @@ const iframe = new iFrame();
 iframe.on("UpdateData", async () => {
   let video: HTMLVideoElement;
 
-  if (document.querySelector(".jw-video") !== null) 
+  if (document.querySelector(".jw-video") !== null)
     video = document.querySelector(".jw-video");
-   else if (document.querySelector(".html5-video-container") !== null) 
+  else if (document.querySelector(".html5-video-container") !== null)
     video = document.querySelector(".html5-video-container > video");
-  
 
   if (video !== undefined && !isNaN(video.duration)) {
     iframe.send({
