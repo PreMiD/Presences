f7fd5378526fa2aab4b8ce32c3957c0416009d9c
gunyu1019 <gunyu1019@gmail.com>
2021-08-04 03:10:03 +0900

    Genie TV added

--- websites/G/Genie Music/presence.ts
+++ websites/G/Genie Music/presence.ts
@@ -41,6 +41,48 @@ presence.on("UpdateData", async () => {
       presenceData.smallImageKey = "playing";
       presenceData.smallImageText = "재생";
     }
-  } else if (location.pathname === "/") presenceData.details = "메인";
+  } else if (location.pathname === "/detail/mediaInfo") {
+    const title = document.querySelector("h2.videoTitle").textContent,
+      video: HTMLVideoElement = document.querySelector("div.fp-player").querySelector("video.fp-engine"),
+      currentDate = video.currentTime,
+      allDate = video.duration;
+
+    [, presenceData.endTimestamp] = presence.getTimestamps(
+      currentDate,
+      allDate
+    );
+    presenceData.details = `${title}`;
+    if (video.paused) {
+      presenceData.smallImageKey = "pause";
+      presenceData.smallImageText = "일시 정지";
+    } else {
+      presenceData.smallImageKey = "playing";
+      presenceData.smallImageText = "재생";
+    }
+  } else if (location.pathname === "/")
+    presenceData.details = "메인";
+  else if (location.pathname.indexOf("/search") === 0) {
+    const keyword = getQuery();
+
+    presenceData.smallImageKey = "search";
+    presenceData.details = "검색";
+    presenceData.state = keyword.query;
+    if (location.pathname === "/search/searchMain")
+      presenceData.details += "(통합검색)";
+    else if (location.pathname === "/search/searchArtist")
+      presenceData.details += "(아티스트)";
+    else if (location.pathname === "/search/searchSong")
+      presenceData.details += "(곡)";
+    else if (location.pathname === "/search/searchAlbum")
+      presenceData.details += "(앨범)";
+    else if (location.pathname === "/search/searchPlaylist")
+      presenceData.details += "(플레이리스트)";
+    else if (location.pathname === "/search/searchMv")
+      presenceData.details += "(동영상)";
+    else if (location.pathname === "/search/searchMagazine")
+      presenceData.details += "(매거진)";
+    else if (location.pathname === "/search/searchLyrics")
+      presenceData.details += "(가사)";
+  }
   presence.setActivity(presenceData);
 });
