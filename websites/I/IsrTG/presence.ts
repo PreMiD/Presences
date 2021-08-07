f3b1f4e54514ec8110427675da504c750c68ba9f
Dor <dorzairi@ymail.com>
2021-08-07 10:57:26 -0400

    added description for each page, added icons

--- websites/I/IsrTG/presence.ts
+++ websites/I/IsrTG/presence.ts
@@ -9,7 +9,7 @@ const presence = new Presence({
 console.log("isrtg working ");
 
 var browsingStamp = Math.floor(Date.now() / 1000);
-
+const eventName = document.querySelector("#app > div > div >div > h2");
 //Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
 presence.on("UpdateData", async () => {
   /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
@@ -19,6 +19,7 @@ presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey:
       "logo" /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/,
+    smallImageKey: "",
     smallImageText: "Some hover text", //The text which is displayed when hovering over the small image
     details: "Browsing Page Name", //The upper section of the presence text
     state: "Reading section A" //The lower section of the presence textt
@@ -30,29 +31,20 @@ presence.on("UpdateData", async () => {
     presenceData.details = "Homepage";
     presenceData.state = "";
   } else if (document.location.pathname.startsWith("/events")) {
-    const [, , pathName] = document.location.pathname.split("/");
-    eventName = document.getElementsByClassName("example");
     presenceData.details = "Events";
-    presenceData.state = `looking at`;
+    presenceData.state = `looking at ${eventName.innerHTML} `;
+    presenceData.smallImageKey = "calendar";
   } else if (document.location.pathname.startsWith("/profile")) {
     presenceData.details = "Profile";
     presenceData.state = "Looking at their profile";
-  } else if (document.location.pathname.startsWith("/about"))
+    presenceData.smallImageKey = "pencil";
+  } else if (document.location.pathname.startsWith("/about")) {
     presenceData.details = "Viewing the about page";
-  else if (document.location.pathname.startsWith("/roster"))
+  } else if (document.location.pathname.startsWith("/roster")) {
     presenceData.details = "Looking at the clan members";
-  else if (document.location.pathname.startsWith("/tutorials"))
+  } else if (document.location.pathname.startsWith("/tutorials")) {
     presenceData.details = "Viewing tutorials";
-  else if (
-    document.location.pathname.startsWith("/direct/inbox") ||
-    document.location.pathname.startsWith("/direct/t")
-  )
-    presenceData.details = "Direct Messages";
-  else {
-    // TODO: Check if the page is really a profile
-    const [, pathName] = document.location.pathname.split("/");
-    presenceData.details = "Viewing a profile";
-    presenceData.state = pathName;
   }
+
   presence.setActivity(presenceData);
 });
