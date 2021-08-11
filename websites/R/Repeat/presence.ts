d320fb7f88b61c3cfa45fe4838b563377f2f28bd
SeMiD <semid@premid.app>
2021-08-11 23:23:20 +0200

    ♻️ Refactoring (#4234)
    Signed-off-by: SeMiD <contact@premid.app>
    
    Co-authored-by: SeMiD <contact@premid.app>

--- websites/R/Repeat/presence.ts
+++ websites/R/Repeat/presence.ts
@@ -1,9 +1,9 @@
 const presence = new Presence({ clientId: "729087463452049559" }),
   timestamp = Math.floor(Date.now() / 1000),
   newStats = async () =>
-  (data = await (
-    await window.fetch("https://uk2.radio01.xonos.gg/api/nowplaying/17")
-  ).json());
+    (data = await (
+      await window.fetch("https://uk2.radio01.xonos.gg/api/nowplaying/17")
+    ).json());
 
 let data: {
   now_playing: {
@@ -19,7 +19,7 @@ let data: {
   live: {
     is_live: boolean;
     streamer_name: string;
-  }
+  };
 };
 
 setInterval(newStats, 10000);
@@ -29,21 +29,23 @@ presence.on("UpdateData", async () => {
   if (!data) return;
 
   const settings = {
-    details: (await presence.getSetting("details")).replace(
-      "%listeners%",
-      data.listeners.unique || "Listeners"
-    ),
-    state: (await presence.getSetting("state"))
-      .replace("%artist%", data.now_playing.song.artist || "Artist")
-      .replace("%songText%", data.now_playing.song.text || "Song")
-      .replace("%title%", data.now_playing.song.title || "Title"),
-    timestamp: await presence.getSetting("timestamp")
-  },
+      details: (await presence.getSetting("details")).replace(
+        "%listeners%",
+        data.listeners.unique || "Listeners"
+      ),
+      state: (await presence.getSetting("state"))
+        .replace("%artist%", data.now_playing.song.artist || "Artist")
+        .replace("%songText%", data.now_playing.song.text || "Song")
+        .replace("%title%", data.now_playing.song.title || "Title"),
+      timestamp: await presence.getSetting("timestamp")
+    },
     presenceData: PresenceData = {
       largeImageKey: "logo",
       details: settings.details,
       state: settings.state,
-      smallImageText: `${data.live.is_live ? data.live.streamer_name : "AutoDJ"} is live!`,
+      smallImageText: `${
+        data.live.is_live ? data.live.streamer_name : "AutoDJ"
+      } is live!`,
       buttons: [
         {
           label: "Tune in",
