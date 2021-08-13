fa0c17e37c729f0d60314ce8b7efb989b82f1efa
SeMiD <contact@premid.app>
2021-08-13 00:50:38 +0000

    ♻️ Refactoring
    Signed-off-by: SeMiD <contact@premid.app>

--- websites/M/Maki.gg/presence.ts
+++ websites/M/Maki.gg/presence.ts
@@ -1,7 +1,6 @@
 const presence = new Presence({
-  clientId: "563434444321587202"
+    clientId: "563434444321587202"
   }),
-
   browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
@@ -14,7 +13,9 @@ presence.on("UpdateData", async () => {
   if (document.location.pathname === "/") {
     presenceData.details = "On Homepage";
 
-    const excatPath = document.location.href.substring(document.location.host.length + document.location.protocol.length + 2);
+    const excatPath = document.location.href.substring(
+      document.location.host.length + document.location.protocol.length + 2
+    );
     if (excatPath === "/#features")
       presenceData.state = "Looking at Makis features";
   } else {
@@ -29,13 +30,23 @@ presence.on("UpdateData", async () => {
       presenceData.details = "On Dashboard";
       presenceData.state = "Selecting a server";
     } else if (document.location.pathname.includes("/dashboard/")) {
-      const guildName = document.querySelector("div.app-content.content > div.content-wrapper > div.content-body > section.dashboard > div.row > div.col-md-10 > div.card > div.card-content > div.card-body > div.tab-content > div.media.mb-2 > div.media-body.mt-50 > h4.media-heading").innerHTML;
+      const guildName = document.querySelector(
+        "div.app-content.content > div.content-wrapper > div.content-body > section.dashboard > div.row > div.col-md-10 > div.card > div.card-content > div.card-body > div.tab-content > div.media.mb-2 > div.media-body.mt-50 > h4.media-heading"
+      ).innerHTML;
 
       //var dashboardTab = document.querySelector("body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-2.mb-2.mb-md-0 > ul > li.active > a").innerHTML;
       let dashboardTab;
-      const dashboardTabs = document.querySelector("body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-2.mb-2.mb-md-0 > ul").getElementsByTagName("li");
+      const dashboardTabs = document
+        .querySelector(
+          "body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-2.mb-2.mb-md-0 > ul"
+        )
+        .getElementsByTagName("li");
       for (let i = 0; i < dashboardTabs.length; i++) {
-        if (dashboardTabs[i].getElementsByTagName("a")[0].classList.contains("active")) {
+        if (
+          dashboardTabs[i]
+            .getElementsByTagName("a")[0]
+            .classList.contains("active")
+        ) {
           const [currentTab] = dashboardTabs[i].getElementsByTagName("a");
           currentTab.removeChild(currentTab.getElementsByTagName("i")[0]);
           dashboardTab = currentTab.innerHTML;
@@ -45,15 +56,23 @@ presence.on("UpdateData", async () => {
       presenceData.details = `${guildName}'s Dashboard`;
       presenceData.state = `Editing ${dashboardTab} settings`;
     } else if (document.location.pathname.includes("/statistics/")) {
-      const guildName = document.querySelector("body > div.app-content.content > div.content-wrapper > div.content-body > div:nth-child(1) > div > div > div > div.media.mb-2 > div > h3").innerHTML;
+      const guildName = document.querySelector(
+        "body > div.app-content.content > div.content-wrapper > div.content-body > div:nth-child(1) > div > div > div > div.media.mb-2 > div > h3"
+      ).innerHTML;
 
       presenceData.details = `Analyzing ${guildName}`;
       presenceData.state = "With Makis statistics";
     } else if (document.location.pathname.includes("/backgrounds/")) {
-      const profileBackgrounds = window.getComputedStyle(document.querySelector("#profile")).display === "block" ? true : false;
+      const profileBackgrounds =
+        window.getComputedStyle(document.querySelector("#profile")).display ===
+        "block"
+          ? true
+          : false;
 
       presenceData.details = "Browsing through";
-      presenceData.state = profileBackgrounds ? "Profile backgrounds" : "Rank backgrounds";
+      presenceData.state = profileBackgrounds
+        ? "Profile backgrounds"
+        : "Rank backgrounds";
     } else if (document.location.pathname.includes("/premium")) {
       presenceData.details = "Taking a look at the amazing features of";
       presenceData.state = "Maki premium";
@@ -64,10 +83,15 @@ presence.on("UpdateData", async () => {
       presenceData.details = "Reading";
       presenceData.state = "Status page";
     } else if (document.location.pathname === "/profile") {
-      const
-        userName = document.querySelector("body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-7.col-lg-8.col-xl-9.col-12 > div > div > div > div.d-flex.justify-content-between.flex-column.col-xl-6.col-21 > div.d-flex.justify-content-start > div > div.mb-1 > h4").innerHTML,
-        userLevel = document.querySelector("body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-7.col-lg-8.col-xl-9.col-12 > div > div > div > div.d-flex.justify-content-between.flex-column.col-xl-6.col-21 > div.d-flex.align-items-center.mt-2 > div.d-flex.align-items-center.mr-2 > div.ml-1 > h5").innerHTML,
-        userXp = document.querySelector("body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-7.col-lg-8.col-xl-9.col-12 > div > div > div > div.d-flex.justify-content-between.flex-column.col-xl-6.col-21 > div.d-flex.align-items-center.mt-2 > div:nth-child(2) > div.ml-1 > h5").innerHTML;
+      const userName = document.querySelector(
+          "body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-7.col-lg-8.col-xl-9.col-12 > div > div > div > div.d-flex.justify-content-between.flex-column.col-xl-6.col-21 > div.d-flex.justify-content-start > div > div.mb-1 > h4"
+        ).innerHTML,
+        userLevel = document.querySelector(
+          "body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-7.col-lg-8.col-xl-9.col-12 > div > div > div > div.d-flex.justify-content-between.flex-column.col-xl-6.col-21 > div.d-flex.align-items-center.mt-2 > div.d-flex.align-items-center.mr-2 > div.ml-1 > h5"
+        ).innerHTML,
+        userXp = document.querySelector(
+          "body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div.col-md-7.col-lg-8.col-xl-9.col-12 > div > div > div > div.d-flex.justify-content-between.flex-column.col-xl-6.col-21 > div.d-flex.align-items-center.mt-2 > div:nth-child(2) > div.ml-1 > h5"
+        ).innerHTML;
 
       presenceData.details = `Looking at the profile of ${userName}`;
       presenceData.state = `Level ${userLevel} | ` + `Xp ${userXp}`;
@@ -80,7 +104,9 @@ presence.on("UpdateData", async () => {
         presenceData.details = "Looking at the";
         presenceData.state = "Global leaderboard";
       } else {
-        const guildName = document.querySelector("body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div:nth-child(1) > div > div > div > div.col-12.col-sm-9.col-md-6.col-lg-5 > div.card-title").innerHTML;
+        const guildName = document.querySelector(
+          "body > div.app-content.content > div.content-wrapper > div.content-body > section > div > div:nth-child(1) > div > div > div > div.col-12.col-sm-9.col-md-6.col-lg-5 > div.card-title"
+        ).innerHTML;
         presenceData.details = "Looking at";
         presenceData.state = `${guildName}'s leaderboard`;
       }
@@ -91,7 +117,9 @@ presence.on("UpdateData", async () => {
         presenceData.state = "Knowledge page";
       } else {
         // Specific question
-        const question = document.querySelector("#knowledge-base-question > div > div.col-lg-9.col-md-7.col-12 > div > div > div > h1").innerHTML;
+        const question = document.querySelector(
+          "#knowledge-base-question > div > div.col-lg-9.col-md-7.col-12 > div > div > div > h1"
+        ).innerHTML;
         presenceData.details = "Reading a knowledge page:";
         presenceData.state = question;
       }
@@ -107,6 +135,5 @@ presence.on("UpdateData", async () => {
   if (!presenceData.details) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else
-    presence.setActivity(presenceData);
+  } else presence.setActivity(presenceData);
 });
