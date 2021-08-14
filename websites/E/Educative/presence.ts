3ea6c143184c20ce1a078a2632ea81f7bc12d0cd
sexnine <planz300@gmail.com>
2021-08-15 05:51:59 +1000

    Add Educative Presence

---
+++ websites/E/Educative/presence.ts
@@ -0,0 +1,87 @@
+var presence = new Presence({
+  clientId: "876055665091678228"
+});
+
+var time = Math.floor(Date.now() / 1000);
+var courseName: string;
+var chapterName: string;
+var courseCompletion: string;
+
+const pathStartsWith = (path: string): boolean => {
+  return document.location.pathname.startsWith(path);
+};
+
+const isInCourse = (): boolean => {
+  return !!document.location.pathname.match(
+    /\/courses\/[\w-_]{1,}\/[\w-_]{1,}/g
+  );
+};
+
+const setCourseInfo = () => {
+  // Sets course info from page
+  if (pathStartsWith("/courses")) {
+    if (isInCourse()) {
+      courseName = document.getElementsByTagName("h4")[0].innerText;
+      courseCompletion = document
+        .getElementsByClassName("whitespace-pre-wrap")[0]
+        .getElementsByTagName("span")[0]
+        .innerText.split("%")[0];
+      var lessonEl = document
+        .getElementsByClassName("bePFDW")[0]
+        .getElementsByTagName("span")[0];
+      var lesson = lessonEl.innerText;
+      var chapter = lessonEl
+        .closest(".CollectionSidebarCategory-sc-15b6owa-0")
+        .getElementsByTagName("h5")[0].innerText;
+      var seperator = lesson && chapter ? " - " : "";
+      chapterName = chapter + seperator + lesson;
+    } else {
+      courseName = document.getElementsByTagName("h1")[0].innerText;
+    }
+  }
+};
+
+setInterval(setCourseInfo, 2000);
+
+presence.on("UpdateData", () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "educativelogo",
+    startTimestamp: time
+  };
+
+  if (pathStartsWith("/courses")) {
+    presenceData.details = courseName;
+    if (isInCourse()) {
+      presenceData.details = courseName || "Learning";
+      presenceData.state = chapterName;
+      presenceData.smallImageKey = "number_" + courseCompletion;
+      presenceData.smallImageText = courseCompletion + "% Complete";
+    } else {
+      presenceData.state = "Viewing course";
+    }
+  } else if (pathStartsWith("/explore")) {
+    presenceData.details = "Exploring courses";
+  } else if (pathStartsWith("/mycourses")) {
+    presenceData.details = "My courses";
+  } else if (pathStartsWith("/learn")) {
+    presenceData.details = "Browsing homepage";
+  } else if (pathStartsWith("/paths")) {
+    presenceData.details = "Exploring paths";
+  } else if (pathStartsWith("/certificates")) {
+    presenceData.details = "Browsing certificates";
+  } else {
+    presenceData.details = "Idle";
+  }
+
+  if (!presenceData.details) {
+    presenceData.details = presenceData.state;
+    presenceData.state = null;
+  }
+
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
