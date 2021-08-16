fa47291965226c3c60c608b574966085eeaae6e3
DarkVillager <darkville13@gmail.com>
2021-08-16 11:59:56 +0200

    Finxed 2

--- websites/P/Picarto/presence.ts
+++ websites/P/Picarto/presence.ts
@@ -17,13 +17,14 @@ presence.on("UpdateData", async () => {
   if (video[0] && !isNaN(presence.timestampFromFormat(time))) {
     const title = document.querySelector(
         "#channel-page > section > section > main > div > div.styled__ChannelLeftContent-sf47ty-1.FBOTx > div.scrollbar-container.styled__ChannelContent-sf47ty-2.bYRWNT.ps > div.styled__StreamTabWrapper-sf47ty-27.qmuRk > div.styled__ChannelBottomContent-sf47ty-5.kkyoeB > div.FlexCol-sc-1y959hh-0.styled__ChannelHeader-sf47ty-4.gxSgjV.dbXdlh > div.FlexRow-sc-1j9kiqj-0.styled__ChannelHeaderTop-sf47ty-22.dFXswc.kRGjEe > span"
-      ),
-      uploader = document.getElementsByClassName("mistvideo-streamer")[0],
+      ).textContent,
+      uploader =
+        document.getElementsByClassName("mistvideo-streamer")[0].textContent,
       playorpause = document
         .getElementsByClassName("mistvideo-play")[0]
         .getAttribute("data-state");
-    presenceData.details = title.textContent;
-    presenceData.state = uploader.textContent;
+    presenceData.details = title;
+    presenceData.state = uploader;
     presenceData.largeImageKey = "logo";
     presenceData.smallImageKey = playorpause.includes("paused")
       ? "pause"
@@ -33,16 +34,13 @@ presence.on("UpdateData", async () => {
       : (await strings).play;
     presenceData.startTimestamp = browsingStamp;
 
-    presence.setTrayTitle(
-      playorpause.includes("paused") ? "" : title.textContent
-    );
+    presence.setTrayTitle(playorpause.includes("paused") ? "" : title);
 
     if (playorpause.includes("paused")) {
       delete presenceData.startTimestamp;
       delete presenceData.endTimestamp;
     }
   } else {
-    console.log("Browsing");
     const pageData: PresenceData = {
       details: "Browsing..",
       largeImageKey: "logo"
