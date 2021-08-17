4eed23e7bee1fc71437777afc0b87b62b7c37f0b
un-boxing-man <unboxingman@hotmail.com>
2021-08-16 22:32:15 -0700

    Update presence.ts
    removed spaces

--- websites/D/Discord Services/presence.ts
+++ websites/D/Discord Services/presence.ts
@@ -9,24 +9,18 @@ presence.on("UpdateData", async () => {
     smallImageKey: "fplogo",
     smallImageText: "fluxpoint",
     startTimestamp: browsingStamp
-
   };
   presenceData.details = "Viewing Page:";
-
   //Discord Bot List
   if (window.location.pathname.startsWith("/staff")) {
     presenceData.details = "Viewing Staff section:";
-
-
     if (window.location.pathname === "/staff/stats")
       presenceData.state = "looking at leader board";
-
   } else if (window.location.pathname.startsWith("/bots")) {
     if (window.location.pathname.endsWith("/submit"))
       presenceData.state = "Add a bot";
     else
       presenceData.details = "Looking for a Discord bot";
-
   } else if (window.location.pathname.startsWith("/bot/")) {
     if (window.location.pathname.endsWith("/edit")) {
       presenceData.details = "Editing a Discord bot:";
@@ -54,17 +48,12 @@ presence.on("UpdateData", async () => {
     else
       presenceData.state = "Looking at Documentation";
   }
-
   //Discord Server List
   else if (window.location.pathname.startsWith("/servers"))
     presenceData.state = "Looking for Discord Servers";
-
-
   //If it doesn't fit to anything
   else if (document.querySelectorAll("h3").item(1))
     presenceData.state = document.querySelectorAll("h3").item(1).textContent;
-
-
   //If it really finds nothing
   else
    presenceData.details = "Viewing something...";
