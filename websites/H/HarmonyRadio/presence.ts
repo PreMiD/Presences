4000111f468cdcb3c91802cf19b1be47ab877c8b
codefactor-io <support@codefactor.io>
2021-08-17 14:23:52 +0000

    [CodeFactor] Apply fixes to commit 888186e

--- websites/H/HarmonyRadio/presence.ts
+++ websites/H/HarmonyRadio/presence.ts
@@ -1,31 +1,31 @@
 const presence = new Presence({
     clientId: "877119819328131093"
-})
+}),
 
-const timeofbrowsing = Math.floor(Date.now() / 1000);
+ timeofbrowsing = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
     const presenceData: PresenceData = {
         largeImageKey:
             "default",
     };
-    console.log(window.location.pathname.toLowerCase())
-    presenceData.startTimestamp = timeofbrowsing
+    console.log(window.location.pathname.toLowerCase());
+    presenceData.startTimestamp = timeofbrowsing;
     if (window.location.pathname.toLowerCase() === "/maintenance") {
-        presenceData.largeImageKey = "default"
-        presenceData.details = "Harmony - Maintenance"
-        presenceData.state = "We will be back soon!"
+        presenceData.largeImageKey = "default";
+        presenceData.details = "Harmony - Maintenance";
+        presenceData.state = "We will be back soon!";
     }
     if (window.location.pathname.toLowerCase() === "/") {
-        presenceData.largeImageKey = "default"
-        let title = document.getElementById("titlemarquee").innerText
-        let artist = document.getElementById("artistmarquee").innerText
-        if (artist === "") {
-            artist = document.getElementsByClassName("player-artist")[0].innerHTML
-        }
-        if (title === "") {
-            title = document.getElementsByClassName("player-title")[0].innerHTML
-        }
+        presenceData.largeImageKey = "default";
+        let title = document.getElementById("titlemarquee").innerText,
+         artist = document.getElementById("artistmarquee").innerText;
+        if (artist === "") 
+            artist = document.getElementsByClassName("player-artist")[0].innerHTML;
+        
+        if (title === "") 
+            title = document.getElementsByClassName("player-title")[0].innerHTML;
+        
         /*if (title >= "24") {
             let amountToRemove = title.length - 27
             console.log(amountToRemove)
@@ -38,9 +38,9 @@ presence.on("UpdateData", async () => {
             let newart = artist.slice(0, amountToRemove2);
             artist = newart + "..."
         }*/
-        let onair = document.getElementsByClassName("player-presenter")
-        presenceData.details = artist + " - " + title
-        presenceData.state = "Presenter: " + onair[0].innerHTML
+        const onair = document.getElementsByClassName("player-presenter");
+        presenceData.details = `${artist} - ${title}`;
+        presenceData.state = `Presenter: ${onair[0].innerHTML}`;
     }
     presence.setActivity(presenceData);
 });
