0f7c841656ff9656d05f36b135028ab2fb591ce9
sexnine <planz300@gmail.com>
2021-08-17 19:05:14 +1000

    Prettier linting

--- websites/E/Educative/presence.ts
+++ websites/E/Educative/presence.ts
@@ -1,46 +1,38 @@
 const presence = new Presence({
-  clientId: "876055665091678228"
-}),
+    clientId: "876055665091678228"
+  }),
+  time = Math.floor(Date.now() / 1000),
+  pathStartsWith = (path: string): boolean => {
+    return document.location.pathname.startsWith(path);
+  },
+  isInCourse = (): boolean => {
+    return !!document.location.pathname.match(
+      /\/courses\/[\w-_]{1,}\/[\w-_]{1,}/g
+    );
+  },
+  setCourseInfo = () => {
+    // Sets course info from page
+    if (pathStartsWith("/courses")) {
+      if (isInCourse()) {
+        courseName = document.getElementsByTagName("h4")[0].innerText;
+        courseCompletion = document
+          .getElementsByClassName("whitespace-pre-wrap")[0]
+          .getElementsByTagName("span")[0]
+          .innerText.split("%")[0];
+        const lessonEl = document
+            .getElementsByClassName("bePFDW")[0]
+            .getElementsByTagName("span")[0],
+          lesson = lessonEl.innerText,
+          chapter = lessonEl
+            .closest(".CollectionSidebarCategory-sc-15b6owa-0")
+            .getElementsByTagName("h5")[0].innerText,
+          seperator = lesson && chapter ? " - " : "";
+        chapterName = chapter + seperator + lesson;
+      } else courseName = document.getElementsByTagName("h1")[0].innerText;
+    }
+  };
 
-time = Math.floor(Date.now() / 1000),
-
-pathStartsWith = (path: string): boolean => {
-  return document.location.pathname.startsWith(path);
-},
-
-isInCourse = (): boolean => {
-  return !!document.location.pathname.match(
-    /\/courses\/[\w-_]{1,}\/[\w-_]{1,}/g
-  );
-},
-
-setCourseInfo = () => {
-  // Sets course info from page
-  if (pathStartsWith("/courses")) {
-    if (isInCourse()) {
-      courseName = document.getElementsByTagName("h4")[0].innerText;
-      courseCompletion = document
-        .getElementsByClassName("whitespace-pre-wrap")[0]
-        .getElementsByTagName("span")[0]
-        .innerText.split("%")[0];
-      const lessonEl = document
-        .getElementsByClassName("bePFDW")[0]
-        .getElementsByTagName("span")[0],
-      lesson = lessonEl.innerText,
-      chapter = lessonEl
-        .closest(".CollectionSidebarCategory-sc-15b6owa-0")
-        .getElementsByTagName("h5")[0].innerText,
-      seperator = lesson && chapter ? " - " : "";
-      chapterName = chapter + seperator + lesson;
-    } else 
-      courseName = document.getElementsByTagName("h1")[0].innerText;
-    
-  }
-};
-
-let courseName: string,
-chapterName: string,
-courseCompletion: string;
+let courseName: string, chapterName: string, courseCompletion: string;
 
 setInterval(setCourseInfo, 2000);
 
@@ -57,22 +49,15 @@ presence.on("UpdateData", () => {
       presenceData.state = chapterName;
       presenceData.smallImageKey = `number_${courseCompletion}`;
       presenceData.smallImageText = `${courseCompletion}% Complete`;
-    } else 
-      presenceData.state = "Viewing course";
-    
-  } else if (pathStartsWith("/explore")) 
+    } else presenceData.state = "Viewing course";
+  } else if (pathStartsWith("/explore"))
     presenceData.details = "Exploring courses";
-   else if (pathStartsWith("/mycourses")) 
-    presenceData.details = "My courses";
-   else if (pathStartsWith("/learn")) 
-    presenceData.details = "Browsing homepage";
-   else if (pathStartsWith("/paths")) 
-    presenceData.details = "Exploring paths";
-   else if (pathStartsWith("/certificates")) 
+  else if (pathStartsWith("/mycourses")) presenceData.details = "My courses";
+  else if (pathStartsWith("/learn")) presenceData.details = "Browsing homepage";
+  else if (pathStartsWith("/paths")) presenceData.details = "Exploring paths";
+  else if (pathStartsWith("/certificates"))
     presenceData.details = "Browsing certificates";
-   else 
-    presenceData.details = "Idle";
-  
+  else presenceData.details = "Idle";
 
   if (!presenceData.details) {
     presenceData.details = presenceData.state;
@@ -82,7 +67,5 @@ presence.on("UpdateData", () => {
   if (presenceData.details === null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else 
-    presence.setActivity(presenceData);
-  
+  } else presence.setActivity(presenceData);
 });
