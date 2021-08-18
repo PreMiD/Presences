d728da399ddcf3def073f3998bdb7c8da273ef96
Flumuffel <luca.weber.gelti@gmail.com>
2021-08-18 23:36:11 +0200

    Integration Anistream

--- websites/A/Anistream/presence.ts
+++ websites/A/Anistream/presence.ts
@@ -1,37 +1,6 @@
 const Anipresence = new Presence({
     clientId: "877640464151494676" //The client ID of the Application created at https://discordapp.com/developers/applications
 }),
-    control = Anipresence.getStrings({
-        play: "presence.playback.playing",
-        pause: "presence.playback.paused"
-        //You can use this to get translated strings in their browser language
-    }),
-
-/*
-
-function myOutsideHeavyLiftingFunction(){
-    //Grab and process all your data here
-
-    // element grabs //
-    // api calls //
-    // variable sets //
-}
-
-setInterval(myOutsideHe: string | number: string | numberavyLiftingFunction, 10000);
-//Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
-
-*/
- template: PresenceData = {
-    largeImageKey:
-        "icon" /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/,
-    smallImageKey:
-        "key" /*The key (file name) of the Small Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/,
-    smallImageText: "Some hover text", //The text which is displayed when hovering over the small image
-    details: "Browsing Page Name", //The upper section of the presence text
-    state: "Reading section A", //The lower section of the presence text
-    startTimestamp: 1577232000, //The unix epoch timestamp for when to start counting from
-    endTimestamp: 1577151472000 //If you want to show Time Left instead of Elapsed, this is the unix epoch timestamp at which the timer ends
-}, /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
 
  browsingStamp = Math.floor(Date.now() / 1000);
 
