d1087a94dd92431a4e4e60302607162ff6ac0618
Ali-Joon <48525075+Ali-Joon@users.noreply.github.com>
2021-08-19 18:26:40 +0430

    Massive changes and improvements
    Finally it's working...

--- websites/B/backpack.tf/presence.ts
+++ websites/B/backpack.tf/presence.ts
@@ -1,56 +1,68 @@
-const presence = new Presence({
+var presence = new Presence({
   clientId: "850295838361649153"
-}),
-browsingStamp = Date.now(),
-presenceData: PresenceData = {
-    largeImageKey: "logo",
-    startTimestamp: browsingStamp
-};
+});
+
+var browsingStamp = Math.floor(Date.now() / 1000);
+var title: any;
+var sec: any;
+
 presence.on("UpdateData", async () => {
-  const showButtons = await presence.getSetting("buttons");
+  const presenceData: PresenceData = {
+    largeImageKey: "logo"
+  };
+  presenceData.startTimestamp = browsingStamp;
+
   if (document.location.hostname == "backpack.tf") {
-    if (document.location.pathname.includes("/stats/")) {
-      const title = document.querySelector(
-        "#page-content > div > :nth-child(1) > div.stats-body > div > div > div.stats-header-title"
+      if (document.location.pathname == "/") {
+        presenceData.details = "Viewing Main page";
+      } else if (document.location.pathname.includes("/overview/")) {
+    title = document.querySelector(
+      "#page-content > div:nth-child(1) > div.stats-body > div > h1"
+    );
+    presenceData.details = "Viewing item stats:";
+    presenceData.state = title.innerText;
+    } else if (document.location.pathname.includes("/stats/")) {
+        if (document.location.pathname.includes("Unique/Mann%20Co.%20Supply%20Crate%20Key")){
+          title = document.querySelector(
+            "#page-content > div.row > div.col-md-8.stats-panel.stats-header-panel > div.stats-body > div.stats-subheader > div.price-boxes > a:nth-child(1) > div.text > div.value"
+          );
+          sec = document.querySelector(
+            "#page-content > div.row > div.col-md-8.stats-panel.stats-header-panel > div.stats-body > div.stats-subheader > div.price-boxes > a:nth-child(2) > div.text > div.value"
+          );
+          presenceData.details = "Viewing Key price:";
+          presenceData.state = `${title.innerText} | ${sec.innerText}`;
+        } else if (document.location.pathname.includes("Unique/Earbuds")) {
+          title = document.querySelector(
+            "#page-content > div.row > div.col-md-8.stats-panel.stats-header-panel > div.stats-body > div.stats-subheader > div.price-boxes > a:nth-child(1) > div.text > div.value"
+          );
+          sec = document.querySelector(
+            "#page-content > div.row > div.col-md-8.stats-panel.stats-header-panel > div.stats-body > div.stats-subheader > div.price-boxes > a:nth-child(2) > div.text > div.value"
+          );
+          presenceData.details = "Viewing Earbuds price:";
+          presenceData.state = `${title.innerText} | ${sec.innerText}`;
+        } else { title = document.querySelector(
+          "#page-content > div.row > div.col-md-8.stats-panel.stats-header-panel > div.stats-body > div.stats-header > div.stats-header-item > div.stats-header-title"
         );
-      presenceData.details = "Viewing Item:";
-      presenceData.state = title.textContent;
-      if (showButtons) {
-        presenceData.buttons = [
-          {
-            label: "View Item",
-            url: document.location.toString()
-          }
-        ];
-      }
+        presenceData.details = "Viewing item:";
+        presenceData.state = title.innerText;}
     } else if (document.location.pathname.includes("/u/")) {
-     presenceData.details = "Viewing profile:";
-      const title = document.querySelector(
-        "#page-content > div > div.panel-body > div > div > div.title > span > a"
-        );
-      presenceData.state = title.textContent;
-      if (showButtons) {
-        presenceData.buttons = [
-          {
-            label: "View profile",
-            url: document.location.toString()
-          }
-        ];
-      }
-    } else if (document.location.pathname.includes("/profiles/")) {
+      title = document.querySelector(
+        "#page-content > div.panel.panel-main.user-panel- > div.panel-body > div > div.information > div.title > span > a"
+      );
       presenceData.details = "Viewing profile:";
-       const title = document.querySelector(
-         "#page-content > div > div.panel-body > div > div > div.title > span > a"
-         );
-       presenceData.state = title.textContent;
-       if (showButtons) {
-        presenceData.buttons = [
-          {
-            label: "View profile",
-            url: document.location.toString()
-          }
-        ];
-      }
+      presenceData.state = title.textContent;
+    } else if (document.location.pathname.includes("/profiles/")) {
+      title = document.querySelector(
+        "#page-content > div.panel.panel-main.user-panel- > div.panel-body > div > div.information > div.title > span > a"
+      );
+      presenceData.details = "Viewing profile:";
+      presenceData.state = title.textContent;
+    } else if (document.location.pathname.includes("/unusual/")) {
+      title = document.querySelector(
+        "head > title"
+      );
+      presenceData.details = "Viewing Unusual Pricelist:";
+      presenceData.state = title.textContent;
     } else if (document.location.pathname.includes("/settings")) {
       presenceData.details = "Viewing a page:";
       presenceData.state = "Settings";
@@ -92,30 +104,34 @@ presence.on("UpdateData", async () => {
     } else if (document.location.pathname.includes("/market")) {
       presenceData.details = "Searching through:";
       presenceData.state = "Steam Community Market Pricelist";
-    } else if (document.location.pathname.includes("/classifieds?steamid")) {
-      presenceData.details = "Viewing Cassifieds of:";
-       const title = document.querySelector(
-         "#search-crumbs > a"
-         );
-       presenceData.state = title.textContent;
-       if (showButtons) {
-        presenceData.buttons = [
-          {
-            label: "View Listings",
-            url: document.location.toString()
-          }
-        ];
-      }
+    } else if (document.location.pathname.includes("/classifieds")) {
+      presenceData.details = "Searching through:";
+      presenceData.state = "Classified Listings";
+    } else if (document.location.pathname.includes("/suggestion/")) {
+      presenceData.details = "Searching through:";
+      presenceData.state = "Price Suggestions";
+    } else if (document.location.pathname.includes("/about")) {
+      presenceData.details = "Viewing page:";
+      presenceData.state = "About backpack.tf";
+    } else if (document.location.pathname.includes("/issues")) {
+      presenceData.details = "Viewing page:";
+      presenceData.state = "Issue tracker";
     } else if (document.location.pathname.includes("/calculator")) presenceData.details = "Useing Calculator";
-     else if (document.location.pathname.includes("/top/backpacks")) presenceData.details = "Viewing Top Backpacks";
-     else if (document.location.pathname.includes("/top/donators")) presenceData.details = "Viewing Top Donators";
-     else if (document.location.pathname.includes("/top/generous")) presenceData.details = "Viewing Top Gifters";
-     else if (document.location.pathname.includes("/top/contributors")) presenceData.details = "Viewing Top Contributors";
-     else if (document.location.pathname.includes("/top/accurate")) presenceData.details = "Viewing The Most Accurate Users";
+    else if (document.location.pathname.includes("/top/backpacks")) presenceData.details = "Viewing Top Backpacks";
+    else if (document.location.pathname.includes("/top/donators")) presenceData.details = "Viewing Top Donators";
+    else if (document.location.pathname.includes("/top/generous")) presenceData.details = "Viewing Top Gifters";
+    else if (document.location.pathname.includes("/top/contributors")) presenceData.details = "Viewing Top Contributors";
+    else if (document.location.pathname.includes("/developer")) presenceData.details = "Viewing Develoapers page";
+    else if (document.location.pathname.includes("/developer")) presenceData.details = "Viewing Develoapers page";
+    else if (document.location.pathname.includes("/help")) presenceData.details = "Viewing help center";
+    else if (document.location.pathname.includes("/rules")) presenceData.details = "Reading rules";
+    else if (document.location.pathname.includes("/servers")) presenceData.details = "Viewing servers with backpack.tf plugin";
+    else if (document.location.pathname.includes("/top/accurate")) presenceData.details = "Viewing The Most Accurate Users";
+};
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else 
+  } else {
     presence.setActivity(presenceData);
   }
 });
