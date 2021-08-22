ffd0262c2c3129a97e69ed834fbfb2be9ba7685d
Lukáš 'Radar' Kubíček <kubicek@radarsoft.cz>
2021-08-22 23:33:22 +0200

    remove unnecessary time computing

--- websites/E/Eternal Torsten/presence.ts
+++ websites/E/Eternal Torsten/presence.ts
@@ -7,34 +7,26 @@ let etData: {
   songTitle?: string,
   totalBeats?: string,
   startedVibing?: number
-} | null = null;
+} | null = null,
+  startedVibing: number | null = null;
 
-const getElapsedMiliseconds = (timeFormat: string): number => {
-    const parts = timeFormat.split(":"),
-      hours = parseInt(parts[0]) * 60 * 60 * 1000,
-      minutes = parseInt(parts[1]) * 60 * 1000,
-      seconds = parseInt(parts[2]) * 1000;
-    return hours + minutes + seconds;
-  },
+const getEtData = () => {
+  const songTitle = document.querySelector("#song-title").textContent ?? "",
+    totalBeats = document.querySelector("#beats").textContent ?? "0",
+    address = location.href;
 
-  getEtData = () => {
-    const songTitle = document.querySelector("#song-title").textContent ?? "",
-      totalBeats = document.querySelector("#beats").textContent ?? "0",
-      address = location.href;
+  if (songTitle === "" || parseInt(totalBeats) === 0) {
+    etData = { address };
+    return;
+  }
 
-    if (songTitle === "" || parseInt(totalBeats) === 0) {
-      etData = { address };
-      return;
-    }
-
-    const startedVibing = Date.now() - getElapsedMiliseconds(document.querySelector("#time").textContent ?? "00:00:00");
-    etData = {
-      address,
-      songTitle,
-      totalBeats,
-      startedVibing
-    };
+  startedVibing = startedVibing ?? Date.now();
+  etData = {
+    address,
+    songTitle,
+    totalBeats
   };
+};
 
 setInterval(getEtData, 1000);
 
@@ -43,7 +35,7 @@ etPresence.on("UpdateData", async () => {
     largeImageKey: "icon",
     details: etData.songTitle ?? "Not vibing 😒",
     state: etData.songTitle ? `Already got ${etData.totalBeats} beats in!` : "",
-    startTimestamp: etData.songTitle ? etData.startedVibing : null,
+    startTimestamp: startedVibing,
     buttons: [
       {
         label: "Join on the vibes",
