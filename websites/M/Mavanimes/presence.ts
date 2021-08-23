056501beee021b1316f6bc5a255567ed9eb7181c
codefactor-io <support@codefactor.io>
2021-08-23 21:29:10 +0000

    [CodeFactor] Apply fixes to commit 6dbfa01

--- websites/M/Mavanimes/presence.ts
+++ websites/M/Mavanimes/presence.ts
@@ -63,9 +63,9 @@ mavanimes.on("UpdateData", async () => {
       Math.floor(video.currentTime),
       Math.floor(video.duration)
     );
-	if (!isNaN(timestamps[0]) && !isNaN(timestamps[1])) {     
+	if (!isNaN(timestamps[0]) && !isNaN(timestamps[1]))      
       [data.startTimestamp, data.endTimestamp] = timestamps;
-    }
+    
     if (video.paused) {
       delete data.startTimestamp;
       delete data.endTimestamp;
