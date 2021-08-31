8a0328c261f1649b60008e1462b0ab4eaaeb70a2
SeMiD <semid@premid.app>
2021-08-31 07:08:33 +0200

    ♻️ Refactoring (#4311)
    Signed-off-by: SeMiD <contact@premid.app>
    
    Co-authored-by: SeMiD <contact@premid.app>

--- websites/M/Mavanimes/iframe.ts
+++ websites/M/Mavanimes/iframe.ts
@@ -1,6 +1,8 @@
 const iframe = new iFrame();
 iframe.on("UpdateData", async () => {
-  const video = document.getElementsByClassName("jw-video")[0] as HTMLVideoElement;
+  const video = document.getElementsByClassName(
+    "jw-video"
+  )[0] as HTMLVideoElement;
   iframe.send({
     currentTime: video.currentTime,
     duration: video.duration,
