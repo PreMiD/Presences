60ebeab4ff3c66cdfd90a1918eda8c7fb76455ba
Marian <toni_kukoc@gmx.de>
2021-09-05 11:23:57 +0200

    Removed unnecessary check

--- websites/M/Maki.gg/presence.ts
+++ websites/M/Maki.gg/presence.ts
@@ -147,9 +147,5 @@ presence.on("UpdateData", async () => {
     }
   }
 
-  if (!presenceData.details) {
-    presence.setTrayTitle();
-    presence.setActivity();
-  } else
-    presence.setActivity(presenceData);
+  presence.setActivity(presenceData);
 });
