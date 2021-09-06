424a0ef0deee17cac02615fffed4c58184f68fa2
codefactor-io <support@codefactor.io>
2021-09-06 01:26:39 +0000

    [CodeFactor] Apply fixes to commit 712bcf0

--- websites/O/Ornikar/presence.ts
+++ websites/O/Ornikar/presence.ts
@@ -1,16 +1,16 @@
 const presence = new Presence({
   clientId: "884234034450939974"
-});
-const timeS = Math.floor(Date.now() / 1000);
+}),
+ timeS = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     smallImageKey: 'car',
     smallImageText: 'Ornikar',
     largeImageKey: 'logo',
-  };
+  },
 
-  const webpath = window.location.pathname.toLowerCase();
+   webpath = window.location.pathname.toLowerCase();
 
   // Connexion
   if (webpath.includes("/connexion")) {
@@ -19,31 +19,31 @@ presence.on("UpdateData", async () => {
   }
 
   // Code de la route
-  else if (webpath.includes("/entrainement-au-code") || webpath.includes("/elearning")) {
+  else if (webpath.includes("/entrainement-au-code") || webpath.includes("/elearning")) 
     presenceData.details = "Code de la route";
-  }
+  
 
   // Conduite
-  else if (webpath.includes("/conduite") || webpath.includes("/espace-conduite")) {
+  else if (webpath.includes("/conduite") || webpath.includes("/espace-conduite")) 
     presenceData.details = "Conduite";
-  }
+  
 
   // Boutique
-  else if (webpath.includes("/boutique")) {
+  else if (webpath.includes("/boutique")) 
     presenceData.details = "Boutique";
-  }
+  
 
   // Compte
-  else if (webpath.includes("/compte")) {
+  else if (webpath.includes("/compte")) 
     presenceData.details = "Compte";
-  }
+  
 
   presenceData.startTimestamp = timeS;
 
   if (presenceData.details == null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else {
+  } else 
     presence.setActivity(presenceData);
-  }
+  
 });
