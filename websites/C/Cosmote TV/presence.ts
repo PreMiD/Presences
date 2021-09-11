cc84fdb9a99a7151c8c027c8bd671386a749b5f7
Vasilis <vasilakis.lolipakis@gmail.com>
2021-09-11 14:51:27 +0300

    Change presence img based on channel

--- websites/C/Cosmote TV/presence.ts
+++ websites/C/Cosmote TV/presence.ts
@@ -5,15 +5,15 @@ const presence = new Presence({
     play: "general.playing",
     pause: "general.paused",
     live: "general.live",
-    search: "general.searchFor",
+    search: "general.searchFor"
   });
 
 function getTimestamps(
   videoTime: number,
   videoDuration: number
 ): Array<number> {
-  const startTime = Date.now(),
-   endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
+  var startTime = Date.now();
+  var endTime = Math.floor(startTime / 1000) - videoTime + videoDuration;
   return [Math.floor(startTime / 1000), endTime];
 }
 
@@ -22,40 +22,40 @@ presence.on("UpdateData", async () => {
     largeImageKey: "tv"
   };
   if (document.location.href.includes("search")) {
-    data.details = "Searching...";
-    data.smallImageKey = "search";
-    data.smallImageText = await (await strings).search;
-  } else if (document.location.href.includes("loginSplash")) 
-    data.details = "Viewing login page...";
-   else if (document.location.href.includes("settings")) 
-    data.details = "Viewing settings...";
-   else if (document.location.href.includes("channels")) 
-    data.details = "Browsing Channels...";
-   else if (document.location.href.includes("privacy")) 
-    data.details = "Viewing privacy policy...";
-   else if (document.location.href.includes("livetv/replaytv")) 
-    data.details = "Browsing Replay TV...";
-   else if (document.location.href.includes("livetv/guide")) 
-    data.details = "Browsing Live TV Guide...";
-   else if (document.location.href.includes("livetv")) 
-    data.details = "Browsing Live TV...";
-   else if (document.location.href.includes("onDemand/FILMS")) 
-    data.details = "Browsing Films...";
-   else if (document.location.href.includes("onDemand/SERIES")) 
-    data.details = "Browsing Series...";
-   else if (document.location.href.includes("onDemand/MOVIES_CLUB")) 
-    data.details = "Browsing Movies Club...";
-   else if (document.location.href.includes("onDemand/SPORTS")) 
-    data.details = "Browsing Sports...";
-   else if (document.location.href.includes("onDemand/DOCUMENTARIES")) 
-    data.details = "Browsing Documentaries...";
-   else if (document.location.href.includes("onDemand/KIDS")) 
-    data.details = "Browsing Kids content...";
-   else if (document.location.href.includes("watchlist")) 
-    data.details = "Viewing watchlist...";
-   else 
-    data.details = "Browsing...";
-  
+    data.details = "Searching..."
+    data.smallImageKey = "search"
+    data.smallImageText = await (await strings).search
+  } else if (document.location.href.includes("loginSplash")) {
+    data.details = "Viewing login page..."
+  } else if (document.location.href.includes("settings")) {
+    data.details = "Viewing settings..."
+  } else if (document.location.href.includes("channels")) {
+    data.details = "Browsing Channels..."
+  } else if (document.location.href.includes("privacy")) {
+    data.details = "Viewing privacy policy..."
+  } else if (document.location.href.includes("livetv/replaytv")) {
+    data.details = "Browsing Replay TV..."
+  } else if (document.location.href.includes("livetv/guide")) {
+    data.details = "Browsing Live TV Guide..."
+  } else if (document.location.href.includes("livetv")) {
+    data.details = "Browsing Live TV..."
+  } else if (document.location.href.includes("onDemand/FILMS")) {
+    data.details = "Browsing Films..."
+  } else if (document.location.href.includes("onDemand/SERIES")) {
+    data.details = "Browsing Series..."
+  } else if (document.location.href.includes("onDemand/MOVIES_CLUB")) {
+    data.details = "Browsing Movies Club..."
+  } else if (document.location.href.includes("onDemand/SPORTS")) {
+    data.details = "Browsing Sports..."
+  } else if (document.location.href.includes("onDemand/DOCUMENTARIES")) {
+    data.details = "Browsing Documentaries..."
+  } else if (document.location.href.includes("onDemand/KIDS")) {
+    data.details = "Browsing Kids content..."
+  } else if (document.location.href.includes("watchlist")) {
+    data.details = "Viewing watchlist..."
+  } else {
+    data.details = "Browsing..."
+  }
   data.startTimestamp = Math.floor(Date.now() / 1000);
 
   const playerCheck = document.querySelector(
@@ -65,32 +65,32 @@ presence.on("UpdateData", async () => {
     : false;
 
   if (playerCheck) {
-    const channel = document
+    var channel = document
       .querySelector(
         ".meta-title[ng-bind='details.channel.title']"
       )
-      ?.textContent.trim(),
+      ?.textContent;
 
-     title = document
+    var title = document
       .querySelector(
         ".meta-title[ng-bind='details.title']"
       )
-      ?.textContent,
+      ?.textContent;
 
-     video: HTMLVideoElement = document.querySelector("video#arxPlayer"),
+    var video: HTMLVideoElement = document.querySelector("video#arxPlayer");
 
-     {paused} = video,
-     {currentTime} = video,
-     {duration} = video,
-     timestamps = getTimestamps(Math.floor(currentTime), Math.floor(duration)),
+    var paused = video.paused
+    var currentTime = video.currentTime;
+    var duration = video.duration;
+    var timestamps = getTimestamps(Math.floor(currentTime), Math.floor(duration));
 
-     live = document.querySelector(
+    var live = document.querySelector(
       ".meta-remain"
     )
       ? true
       : false;
 
-    if (live == false) {
+    if (live === false) {
       (data.smallImageKey = paused ? "pause" : "play"),
         (data.smallImageText = paused
           ? (await strings).pause
@@ -110,35 +110,37 @@ presence.on("UpdateData", async () => {
           ? (await strings).pause
           : (await strings).live);
 
-      const watchTime = Math.floor(Date.now() / 1000);
+      var watchTime = Math.floor(Date.now() / 1000);
       data.startTimestamp = watchTime;
     }
 
     if (channel !== '') {
-      if (live == false) 
-        data.state = `Watching on ${channel}`;
-       else 
-        data.state = `Live on ${channel}`;
-      
+      if (live === false) {
+        data.state = 'Watching on ' + channel;
+      } else {
+        data.state = 'Live on ' + channel;
+      }
+      var hashCode = channel.split("").reduce(function (a, b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0);
+      data.largeImageKey = hashCode.toString();
     }
 
     if (series === true) {
       data.details = document.querySelector(
         "span[ng-bind='details.seriesSubs']"
       ).textContent;
-    } else 
+    } else {
       data.details = title;
-    
+    }
 
     if (paused) {
       delete data.startTimestamp;
       delete data.endTimestamp;
-      data.state = 'Paused';
+      data.state = 'Paused'
     }
   }
-  if (data.details == null) 
+  if (data.details === null) {
     presence.setActivity();
-   else 
+  } else {
     presence.setActivity(data);
-  
+  }
 });
