e9e4192c6d7b3c94dd17d8eb113fd9cc2dae8670
Anshroid Nemo <anshroid07@gmail.com>
2021-09-14 19:25:26 +0100

    Changed presence.ts

--- websites/D/Desmos/presence.ts
+++ websites/D/Desmos/presence.ts
@@ -1,7 +1,7 @@
-const presence = new Presence({
+const desmosPresence = new Presence({
     clientId: "886254740399349852" //The client ID of the Application created at https://discordapp.com/developers/applications
   }),
-  strings = presence.getStrings({
+  strings = desmosPresence.getStrings({
     play: "presence.playback.playing",
     pause: "presence.playback.paused"
     //You can use this to get translated strings in their browser language
@@ -14,57 +14,52 @@ var title: string = ""
 var numEquations: number = 0
 var startTime: number = Date.now()
 
-function getDesmosData() {
-	url = document.URL;
-	if (url[url.length-1] == "/") {
-		url = url.substring(0, url.length-1)
-	}
-	var splitUrl = url.split("/")
-	var urlPage = splitUrl[splitUrl.length-1]
-	if (urlPage == "www.desmos.com") { 
-		pageType = "Home Page" 
-	} else { 
-		pageType = urlPage[0].toUpperCase() + urlPage.substring(1) 
-	}
-	if (url.includes("/calculator")) {
-		graphing = 2;	
-		title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
-		numEquations = document.getElementsByClassName("dcg-template-expressioneach")[0].childElementCount;
-	} else if (url.includes("/geometry")) {
-		graphing = 1;	
-		title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
-		pageType = "Geometry"
-		numEquations = 0
-	} 
-	else if (["scientific", "fourfunction", "matrix", "practice"].includes(urlPage)) {
-		graphing = 1;
-		if (pageType == "Scientific" || pageType == "Fourfunction") {
-			numEquations = document.getElementsByClassName("dcg-basic-list")[0].childElementCount
-		} else if (pageType == "Matrix") {
-			numEquations = document.getElementsByClassName("dcg-matrix-list")[0].childElementCount
-		} else {
-			numEquations = 0
-		}
-	} else {
-		graphing = 0;
-	};
-	//console.log(url, graphing.toString(), pageType, title, numEquations.toString());
-};
-
-getDesmosData();
-setInterval(getDesmosData, 1000);
-
-
-presence.on("UpdateData", async () => {
+desmosPresence.on("UpdateData", async () => {
   /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
 
     It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
-
+	
+  // About the comment above: Is this really recommended? It appears to be not allowed according to the Presence Rules
+  
+  url = document.URL;
+  if (url[url.length-1] == "/") {
+  	url = url.substring(0, url.length-1)
+  }
+  var splitUrl = url.split("/")
+  var urlPage = splitUrl[splitUrl.length-1]
+  if (urlPage == "www.desmos.com") { 
+  	pageType = "Home Page" 
+  } else { 
+  	pageType = urlPage[0].toUpperCase() + urlPage.substring(1) 
+  }
+  if (url.includes("/calculator")) {
+  	graphing = 2;	
+  	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
+  	numEquations = document.getElementsByClassName("dcg-template-expressioneach")[0].childElementCount;
+  } else if (url.includes("/geometry")) {
+  	graphing = 1;	
+  	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
+  	pageType = "Geometry"
+  	numEquations = 0
+  } 
+  else if (["scientific", "fourfunction", "matrix", "practice"].includes(urlPage)) {
+  	graphing = 1;
+  	if (pageType == "Scientific" || pageType == "Fourfunction") {
+  		numEquations = document.getElementsByClassName("dcg-basic-list")[0].childElementCount
+  	} else if (pageType == "Matrix") {
+  		numEquations = document.getElementsByClassName("dcg-matrix-list")[0].childElementCount
+  	} else {
+  		numEquations = 0
+  	}
+  } else {
+  	graphing = 0;
+  }
+  
   var presenceData: PresenceData = {
     largeImageKey:
-      "logo" /*The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets*/,
-    startTimestamp: startTime, //The unix epoch timestamp for when to start counting from
-  }; /*Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceSata.type = "blahblah"; type examples: details, state, etc.*/
+      "logo",
+    startTimestamp: startTime,
+  };
   
   if (graphing == 2) {
 	presenceData.smallImageKey = "logo"
@@ -94,10 +89,10 @@ presence.on("UpdateData", async () => {
   
   if (presenceData.details == null) {
     //This will fire if you do not set presence details
-    presence.setTrayTitle(); //Clears the tray title for mac users
-    presence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+    desmosPresence.setTrayTitle(); //Clears the tray title for mac users
+    desmosPresence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
   } else {
     //This will fire if you set presence details
-    presence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+    desmosPresence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
   }
 });
