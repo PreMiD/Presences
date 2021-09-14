0a3b47e12bcb37db8c2e2cd6c7e663323f4314e6
Anshroid Nemo <anshroid07@gmail.com>
2021-09-14 21:09:05 +0100

    Fixed a lot of stuff

--- websites/D/Desmos/presence.ts
+++ websites/D/Desmos/presence.ts
@@ -1,155 +1,84 @@
-const desmosPresence = new Presence({
-	clientId: "886254740399349852" //The client ID of the Application created at https://discordapp.com/developers/applications
-  })
-  
-var url: string
-var graphing: number = 0
-var pageType: string
-var title: string = ""
-var numEquations: number = 0
-const startTime: number = Date.now()
-  strings = desmosPresence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-    //You can use this to get translated strings in their browser language
-  });
-  
-desmosPresence.on("UpdateData", async () => {
-  /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
+const presence = new Presence({clientId: "886254740399349852"})
 
-	It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
-	
-  // About the comment above: Is this really recommended? It appears to be not allowed according to the Presence Rules
-  
+var url: string
+var graphing: number
+var pageType: string
+var title: string
+var numEquations: number
+var startTime: number = Date.now()
+
+presence.on("UpdateData", async () => {
+  // Getting Data
   url = document.URL;
   if (url[url.length-1] === "/") {
-	url = url.substring(0, url.length-1)
+  	url = url.substring(0, url.length-1)
   }
   var splitUrl = url.split("/")
   var urlPage = splitUrl[splitUrl.length-1]
   if (urlPage === "www.desmos.com") { 
-	pageType = "Home Page" 
+  	pageType = "Home Page" 
   } else { 
-	pageType = urlPage[0].toUpperCase() + urlPage.substring(1) 
+  	pageType = urlPage[0].toUpperCase() + urlPage.substring(1) // Capitalize page title
   }
-  if (url[url.length - 1] == "/") 
-  	url = url.substring(0, url.length - 1);
-  
-  const splitUrl = url.split("/"),
-   urlPage = splitUrl[splitUrl.length - 1];
-  if (urlPage == "www.desmos.com")  
-  	pageType = "Home Page"; 
-   else  
-  	pageType = urlPage[0].toUpperCase() + urlPage.substring(1); 
-  
-  if (url.includes("/calculator")) {
-	graphing = 2;	
-	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
-	numEquations = document.getElementsByClassName("dcg-template-expressioneach")[0].childElementCount;
-  } else if (url.includes("/geometry")) {
-	graphing = 1;	
-	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
-	pageType = "Geometry"
-	numEquations = 0
+  if (url.includes("/calculator")) { // Graphing Calculator
+  	graphing = 2;   
+  	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
+  	numEquations = document.getElementsByClassName("dcg-template-expressioneach")[0].childElementCount;
+  } else if (url.includes("/geometry")) { // Geometry Tool
+  	graphing = 1;   
+  	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
+  	pageType = "Geometry"
+  	numEquations = 0
   } 
   else if (["scientific", "fourfunction", "matrix", "practice"].includes(urlPage)) {
-	graphing = 1;
-	if (pageType === "Scientific" || pageType === "Fourfunction") {
-		numEquations = document.getElementsByClassName("dcg-basic-list")[0].childElementCount
-	} else if (pageType === "Matrix") {
-		numEquations = document.getElementsByClassName("dcg-matrix-list")[0].childElementCount
-	} else {
-		numEquations = 0
-	}
+  	graphing = 1;
+  	if (pageType === "Scientific" || pageType === "Fourfunction") { // These three use a different container for equations
+  		numEquations = document.getElementsByClassName("dcg-basic-list")[0].childElementCount
+  	} else if (pageType === "Matrix") {
+  		numEquations = document.getElementsByClassName("dcg-matrix-list")[0].childElementCount
+  	} else {
+  		numEquations = 0
+  	}
   } else {
-	graphing = 0;
-  }
+  	graphing = 0;
+  };
   
+  // Setting Presence
   var presenceData: PresenceData = {
-	largeImageKey:
-	  "logo",
-	startTimestamp: startTime
+    largeImageKey: "logo",
+    startTimestamp: startTime
   };
   
   if (graphing === 2) {
-	presenceData.smallImageKey = "logo"
-	presenceData.smallImageText = "Desmos Graphing Calculator"
-	presenceData.details = "Plotting a Graph: ".concat(title)
-	presenceData.state = numEquations.toString().concat(" Equation")
-	if (numEquations !== 1) { presenceData.state += "s" }
+    presenceData.smallImageKey = "logo"
+    presenceData.smallImageText = "Desmos Graphing Calculator"
+    presenceData.details = "Plotting a Graph: ".concat(title)
+    presenceData.state = numEquations.toString().concat(" Equation")
+    if (numEquations !== 1) { presenceData.state += "s" }
   } else {
-	delete presenceData.smallImageKey
-	delete presenceData.smallImageText
-	if (graphing === 1) {
-		presenceData.details = "Using Desmos ".concat(pageType)
-		if (numEquations > 0) {
-			presenceData.details += " Calculator"
-			presenceData.state = numEquations.toString().concat(" Expression")
-			if (numEquations !== 1) { presenceData.state += "s" }
-		} else {
-			delete presenceData.state
-		}
-		if (title !== "") {
-			presenceData.details += ": ".concat(title)
-		}
-	} else {
-		presenceData.details = "Reading ".concat(pageType)
-	}
-  	graphing = 1;	
-  	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
-  	pageType = "Geometry";
-  	numEquations = 0;
-  } else if (["scientific", "fourfunction", "matrix", "practice"].includes(urlPage)) {
-  	graphing = 1;
-  	if (pageType == "Scientific" || pageType == "Fourfunction") 
-  		numEquations = document.getElementsByClassName("dcg-basic-list")[0].childElementCount;
-  	 else if (pageType == "Matrix") 
-  		numEquations = document.getElementsByClassName("dcg-matrix-list")[0].childElementCount;
-  	 else 
-  		numEquations = 0;
-  	
-  } else 
-  	graphing = 0;
-  
-  
-  const presenceData: PresenceData = {
-    largeImageKey:
-      "logo",
-    startTimestamp: startTime,
-  };
-  
-  if (graphing == 2) {
-	presenceData.smallImageKey = "logo";
-	presenceData.smallImageText = "Desmos Graphing Calculator";
-	presenceData.details = "Plotting a Graph: ".concat(title);
-	presenceData.state = numEquations.toString().concat(" Equation");
-	if (numEquations != 1) presenceData.state += "s"; 
-  } else {
-	delete presenceData.smallImageKey;
-	delete presenceData.smallImageText;
-	if (graphing == 1) {
-		presenceData.details = "Using Desmos ".concat(pageType);
-		if (numEquations > 0) {
-			presenceData.details += " Calculator";
-			presenceData.state = numEquations.toString().concat(" Expression");
-			if (numEquations != 1) presenceData.state += "s"; 
-		} else 
-			delete presenceData.state;
-		
-		if (title != "") 
-			presenceData.details += ": ".concat(title);
-		
-	} else 
-		presenceData.details = "Reading ".concat(pageType);
-	
+    delete presenceData.smallImageKey
+    delete presenceData.smallImageText
+    if (graphing === 1) {
+        presenceData.details = "Using Desmos ".concat(pageType)
+        if (numEquations > 0) {
+            presenceData.details += " Calculator"
+            presenceData.state = numEquations.toString().concat(" Expression")
+            if (numEquations !== 1) { presenceData.state += "s" }
+        } else {
+            delete presenceData.state
+        }
+        if (title !== "") {
+            presenceData.details += ": ".concat(title)
+        }
+    } else {
+        presenceData.details = "Reading ".concat(pageType)
+    }
   }
   
   if (presenceData.details === null) {
-	//This will fire if you do not set presence details
-	desmosPresence.setTrayTitle(); //Clears the tray title for mac users
-	desmosPresence.setActivity(); /*Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name*/
+    presence.setTrayTitle();
+    presence.setActivity();
   } else {
-	//This will fire if you set presence details
-	desmosPresence.setActivity(presenceData); //Update the presence with all the values from the presenceData object
+    presence.setActivity(presenceData);
   }
 });
