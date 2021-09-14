b7ec126b8ddced416264a697c24c48546f94b8b6
Anshroid Nemo <anshroid07@gmail.com>
2021-09-14 21:15:05 +0100

    Convert tab to space

--- websites/D/Desmos/presence.ts
+++ websites/D/Desmos/presence.ts
@@ -5,41 +5,41 @@ let url: string,
  pageType: string,
  title: string,
  numEquations: number,
- startTime: number = Date.now();
+const startTime: number = Date.now();
 
 presence.on("UpdateData", async () => {
   // Getting Data
   url = document.URL;
   if (url[url.length - 1] === "/") 
-  	url = url.substring(0, url.length - 1);
+    url = url.substring(0, url.length - 1);
   
   const splitUrl = url.split("/"),
    urlPage = splitUrl[splitUrl.length - 1];
   if (urlPage === "www.desmos.com")  
-  	pageType = "Home Page"; 
+    pageType = "Home Page"; 
    else  
-  	pageType = urlPage[0].toUpperCase() + urlPage.substring(1); // Capitalize page title
+    pageType = urlPage[0].toUpperCase() + urlPage.substring(1); // Capitalize page title
   
   if (url.includes("/calculator")) { // Graphing Calculator
-  	graphing = 2;   
-  	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
-  	numEquations = document.getElementsByClassName("dcg-template-expressioneach")[0].childElementCount;
+    graphing = 2;   
+    title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
+    numEquations = document.getElementsByClassName("dcg-template-expressioneach")[0].childElementCount;
   } else if (url.includes("/geometry")) { // Geometry Tool
-  	graphing = 1;   
-  	title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
-  	pageType = "Geometry";
-  	numEquations = 0;
+    graphing = 1;   
+    title = document.getElementsByClassName("dcg-variable-title")[0].innerHTML;
+    pageType = "Geometry";
+    numEquations = 0;
   } else if (["scientific", "fourfunction", "matrix", "practice"].includes(urlPage)) {
-  	graphing = 1;
-  	if (pageType === "Scientific" || pageType === "Fourfunction") { // These three use a different container for equations
-  		numEquations = document.getElementsByClassName("dcg-basic-list")[0].childElementCount;
-  	} else if (pageType === "Matrix") 
-  		numEquations = document.getElementsByClassName("dcg-matrix-list")[0].childElementCount;
-  	 else 
-  		numEquations = 0;
-  	
+    graphing = 1;
+    if (pageType === "Scientific" || pageType === "Fourfunction") { // These three use a different container for equations
+        numEquations = document.getElementsByClassName("dcg-basic-list")[0].childElementCount;
+    } else if (pageType === "Matrix") 
+        numEquations = document.getElementsByClassName("dcg-matrix-list")[0].childElementCount;
+     else 
+        numEquations = 0;
+    
   } else 
-  	graphing = 0;
+    graphing = 0;
   
   
   // Setting Presence
