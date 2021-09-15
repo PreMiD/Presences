5e52213ad073722280bd4f998bedf2a1b95a447a
Mihael Nikolov <the.djrider@gmail.com>
2021-09-15 20:33:06 +0300

    Changed default PHPStorm formatting

--- websites/E/EternalNetwork TM/presence.ts
+++ websites/E/EternalNetwork TM/presence.ts
@@ -24,105 +24,103 @@ let strings: Promise<LangStrings> = getStrings(),
   oldLang: string = null;
 
 presence.on("UpdateData", async () => {
-    const
-      newLang = await presence.getSetting("lang"),
-      showTimestamps = await presence.getSetting("timestamp"),
-      showSubdomain = await presence.getSetting("subdomain"),
-      bigicon = await presence.getSetting("bigicon"),
-      {hostname, pathname} = document.location,
-      etrnl = "eternalnetworktm.com",
-      ttl = document.title,
-      logoArr = ["eternalnetworktm_logo", "eternalnetworktm_logo_2", "eternalnetworktm_logo_3"];
+  const
+    newLang = await presence.getSetting("lang"),
+    showTimestamps = await presence.getSetting("timestamp"),
+    showSubdomain = await presence.getSetting("subdomain"),
+    bigicon = await presence.getSetting("bigicon"),
+    {hostname, pathname} = document.location,
+    etrnl = "eternalnetworktm.com",
+    ttl = document.title,
+    logoArr = ["eternalnetworktm_logo", "eternalnetworktm_logo_2", "eternalnetworktm_logo_3"];
 
-    if (!oldLang)
-      oldLang = newLang;
-    else if (oldLang !== newLang) {
-      oldLang = newLang;
-      strings = getStrings();
+  if (!oldLang)
+    oldLang = newLang;
+  else if (oldLang !== newLang) {
+    oldLang = newLang;
+    strings = getStrings();
+  }
+
+  const presenceData: PresenceData = {
+    details: (await strings).viewPage,
+    largeImageKey: logoArr[bigicon] || "eternalnetworktm_logo",
+    smallImageText: hostname + pathname,
+    startTimestamp: browsingStamp
+  };
+
+  if (!showTimestamps)
+    delete presenceData.startTimestamp;
+
+  if (hostname === etrnl || hostname === `www.${etrnl}`) {
+
+    presenceData.smallImageKey = "eternalnetworktm_logo";
+
+    if (pathname.startsWith("/"))
+      presenceData.state = ttl;
+    if (pathname.includes("/wp-admin")) {
+      presenceData.state = "Using administrating power over the website !";
+      presenceData.smallImageText = "Admin Panel";
     }
 
-    const presenceData: PresenceData = {
-      details: (await strings).viewPage,
-      largeImageKey: logoArr[bigicon] || "eternalnetworktm_logo",
-      smallImageText: hostname + pathname,
-      startTimestamp: browsingStamp
-    };
+  } else if (hostname === `forum.${etrnl}` || hostname === `www.forum.${etrnl}`) {
 
-    if (!showTimestamps)
-      delete presenceData.startTimestamp;
+    presenceData.smallImageKey = "eternalnetworktm_logo_v2";
 
-    if (hostname === etrnl || hostname === `www.${etrnl}`) {
+    if (pathname.startsWith("/"))
+      presenceData.state = ttl;
 
-      presenceData.smallImageKey = "eternalnetworktm_logo";
+    if (pathname.includes("/memberlist.php"))
+      presenceData.state = "Sneaking into member list !";
 
-      if (pathname.startsWith("/"))
-        presenceData.state = ttl;
-
-      if (pathname.includes("/wp-admin")) {
-        presenceData.state = "Using administrating power over the website !";
-        presenceData.smallImageText = "Admin Panel";
-      }
-
-    } else if (hostname === `forum.${etrnl}` || hostname === `www.forum.${etrnl}`) {
-
-      presenceData.smallImageKey = "eternalnetworktm_logo_v2";
-
-      if (pathname.startsWith("/"))
-        presenceData.state = ttl;
-
-      if (pathname.includes("/memberlist.php"))
-        presenceData.state = "Sneaking into member list !";
-
-      if (pathname.includes("/viewtopic.php")) {
-        presenceData.details = (await strings).readingPost;
-        presenceData.state = ttl;
-      }
-
-      if (pathname.includes("/adm/")) {
-        presenceData.state = "Using administrating power over the forum !";
-        presenceData.smallImageText = "Admin Panel";
-      }
-
-    } else if (hostname === `radio.${etrnl}` || hostname === `www.radio.${etrnl}`) {
-
-      presenceData.smallImageKey = "eternalradio_logo";
-      presenceData.details = (await strings).listeningMusic;
-
-      if (pathname.startsWith("/"))
-        presenceData.state = ttl;
-
-      if (pathname.includes("page_ABOUT"))
-        presenceData.state = "About info page !";
-
-      if (pathname.includes("page_PROGRAMS"))
-        presenceData.state = "Checking radio program !";
-
-      if (pathname.includes("page_REQUEST"))
-        presenceData.state = "Requesting a song !";
-
-      if (pathname.includes("page_CONTACTS"))
-        presenceData.state = "Contact us page !";
-    } else if (hostname === `status.${etrnl}` || hostname === `www.status.${etrnl}`) {
-
-      presenceData.smallImageKey = "eternalnetworktm_status";
-
-      if (pathname.startsWith("/"))
-        presenceData.state = ttl;
-
-      if (pathname.includes("/admin")) {
-        presenceData.state = "Adding new incident 😥 !";
-        presenceData.smallImageText = "Admin Panel";
-      }
-
-      if (pathname.includes("/?do=settings"))
-        presenceData.state = "Adding new service !";
+    if (pathname.includes("/viewtopic.php")) {
+      presenceData.details = (await strings).readingPost;
+      presenceData.state = ttl;
     }
 
-    if (!showSubdomain) {
-      delete presenceData.smallImageKey;
-      delete presenceData.smallImageText;
+    if (pathname.includes("/adm/")) {
+      presenceData.state = "Using administrating power over the forum !";
+      presenceData.smallImageText = "Admin Panel";
     }
 
-    presence.setActivity(presenceData);
+  } else if (hostname === `radio.${etrnl}` || hostname === `www.radio.${etrnl}`) {
+
+    presenceData.smallImageKey = "eternalradio_logo";
+    presenceData.details = (await strings).listeningMusic;
+
+    if (pathname.startsWith("/"))
+      presenceData.state = ttl;
+
+    if (pathname.includes("page_ABOUT"))
+      presenceData.state = "About info page !";
+
+    if (pathname.includes("page_PROGRAMS"))
+      presenceData.state = "Checking radio program !";
+
+    if (pathname.includes("page_REQUEST"))
+      presenceData.state = "Requesting a song !";
+
+    if (pathname.includes("page_CONTACTS"))
+      presenceData.state = "Contact us page !";
+  } else if (hostname === `status.${etrnl}` || hostname === `www.status.${etrnl}`) {
+
+    presenceData.smallImageKey = "eternalnetworktm_status";
+
+    if (pathname.startsWith("/"))
+      presenceData.state = ttl;
+
+    if (pathname.includes("/admin")) {
+      presenceData.state = "Adding new incident 😥 !";
+      presenceData.smallImageText = "Admin Panel";
+    }
+
+    if (pathname.includes("/?do=settings"))
+      presenceData.state = "Adding new service !";
+  }
+
+  if (!showSubdomain) {
+    delete presenceData.smallImageKey;
+    delete presenceData.smallImageText;
+  }
+  presence.setActivity(presenceData);
   }
 );
