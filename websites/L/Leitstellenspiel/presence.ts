22db29d846c705045250d517f2332136ee6d7e4e
codefactor-io <support@codefactor.io>
2021-09-16 14:43:34 +0000

    [CodeFactor] Apply fixes to commit 2da1bba

--- websites/L/Leitstellenspiel/presence.ts
+++ websites/L/Leitstellenspiel/presence.ts
@@ -1,4 +1,4 @@
-var presence = new Presence({
+let presence = new Presence({
   clientId: "888061073150865469"
 }),
 presenceData: PresenceData = {
@@ -10,13 +10,13 @@ presence.on("UpdateData", async () => {
 if (
   document.location.pathname === "/" ||
   document.location.pathname === ""
-) {
+) 
   presenceData.details = "Manages Emergencies";
-} else if (document.location.pathname.startsWith("/einsaetze")) {
+ else if (document.location.pathname.startsWith("/einsaetze")) 
   presenceData.details = "Viewing all possible scenarios";
-} else if (document.location.pathname.startsWith("/toplist")) {
+ else if (document.location.pathname.startsWith("/toplist")) 
   presenceData.details = "Viewing the Toplist";
-} else {
+ else {
   presenceData = {
     largeImageKey: "logo",
     details: "Homepage"
