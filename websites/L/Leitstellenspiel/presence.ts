2da1bbab4139c004ea054078392f6cdaa3a69735
Bastian Leicht <50463419+bastianleicht@users.noreply.github.com>
2021-09-16 16:42:08 +0200

    Added leitstellenspiel.de support

---
+++ websites/L/Leitstellenspiel/presence.ts
@@ -0,0 +1,26 @@
+var presence = new Presence({
+  clientId: "888061073150865469"
+}),
+presenceData: PresenceData = {
+  largeImageKey: "logo",
+  startTimestamp: Date.now()
+};
+
+presence.on("UpdateData", async () => {
+if (
+  document.location.pathname === "/" ||
+  document.location.pathname === ""
+) {
+  presenceData.details = "Manages Emergencies";
+} else if (document.location.pathname.startsWith("/einsaetze")) {
+  presenceData.details = "Viewing all possible scenarios";
+} else if (document.location.pathname.startsWith("/toplist")) {
+  presenceData.details = "Viewing the Toplist";
+} else {
+  presenceData = {
+    largeImageKey: "logo",
+    details: "Homepage"
+  };
+}
+presence.setActivity(presenceData);
+});
