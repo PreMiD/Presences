ac0b0a4d01eaa2b0be02e6d73735be5cad81e989
Noctuuu <81293192+Noctuuu@users.noreply.github.com>
2021-09-19 22:21:03 +0100

    Update presence.ts

--- websites/P/Pronote/presence.ts
+++ websites/P/Pronote/presence.ts
@@ -1,21 +1,18 @@
 const presence = new Presence({
   clientId: "886576430408929291"
 });
-
 function get_url_extension( url ) {
   return url.split(/[#?]/)[0].split('.').pop().trim();
 }
-
-
 LeTimeMicieu = Math.floor(Date.now() / 1000);
 presence.on("UpdateData", async () => {
-  if (document.getElementById('breadcrumbBandeau') !== null) 
+  if (document.getElementById('breadcrumbBandeau') !== null) {
   eingrosscarott = document.getElementsByClassName("titre-onglet")[0].textContent;
-   else if (document.getElementById('id_14') !== null) 
-    eingrosscarott = `Connection - ${document.querySelector("#id_14 > div.InlineBlock.Texte10 > div.Texte14.Gras").textContent}`;
-   else 
-    eingrosscarott = `Visionne un fichier ${get_url_extension(document.location.pathname)}`;
-  
+  } else if (document.getElementById('id_14') !== null) {
+    eingrosscarott = "Connection - " + document.querySelector("#id_14 > div.InlineBlock.Texte10 > div.Texte14.Gras").textContent;
+  } else {
+    eingrosscarott = "Visionne un fichier " + get_url_extension(document.location.pathname);
+  }
   if (document.location.pathname.includes('FichiersExternes')) {
     ladetay = "Fichiers Externes";
     smallImageKeyy = "nonmaiscprlesfichiers";
@@ -23,10 +20,8 @@ presence.on("UpdateData", async () => {
   }else{
     ladetay = document.getElementsByClassName("ibe_etab_cont")[0].textContent;
     smallImageKeyy = "mini_logo";
-    smallImageSext = `Lieu de l'établissement : ${document.querySelector("head > meta:nth-child(14)").content}`;
+    smallImageSext = "Lieu de l'établissement : " + document.querySelector("head > meta:nth-child(14)").content;
   }
-
-
   const presenceData = {
     largeImageKey: "pronote_2",
     smallImageKey: smallImageKeyy,
@@ -41,11 +36,10 @@ presence.on("UpdateData", async () => {
          }
     ]
   };
-
   if (presenceData.details === null) {
     presence.setTrayTitle();
     presence.setActivity();
-  } else 
+  } else {
     presence.setActivity(presenceData);
-  
+  }
 });
