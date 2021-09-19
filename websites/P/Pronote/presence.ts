6844e454f8403db4f4a90140afe99a0c128d588f
Noctuuu <81293192+Noctuuu@users.noreply.github.com>
2021-09-19 22:41:30 +0100

    Update presence.ts

--- websites/P/Pronote/presence.ts
+++ websites/P/Pronote/presence.ts
@@ -1,33 +1,20 @@
 const presence = new Presence({
   clientId: "886576430408929291"
 });
-function get_url_extension( url ) {
+
+function getUrlExtension( url ) {
   return url.split(/[#?]/)[0].split('.').pop().trim();
 }
+
 LeTimeMicieu = Math.floor(Date.now() / 1000);
+
 presence.on("UpdateData", async () => {
-  if (document.getElementById('breadcrumbBandeau') !== null) 
-  eingrosscarott = document.getElementsByClassName("titre-onglet")[0].textContent;
-   else if (document.getElementById('id_14') !== null) 
-    eingrosscarott = `Connection - ${document.querySelector("#id_14 > div.InlineBlock.Texte10 > div.Texte14.Gras").textContent}`;
-   else 
-    eingrosscarott = `Visionne un fichier ${get_url_extension(document.location.pathname)}`;
-  
-  if (document.location.pathname.includes('FichiersExternes')) {
-    ladetay = "Fichiers Externes";
-    smallImageKeyy = "nonmaiscprlesfichiers";
-    smallImageSext = get_url_extension(document.location.pathname);
-  }else{
-    ladetay = document.getElementsByClassName("ibe_etab_cont")[0].textContent;
-    smallImageKeyy = "mini_logo";
-    smallImageSext = `Lieu de l'établissement : ${document.querySelector("head > meta:nth-child(14)").content}`;
-  }
-  const presenceData = {
+  let presenceData = {
     largeImageKey: "pronote_2",
-    smallImageKey: smallImageKeyy,
-    details: ladetay,
-    state: eingrosscarott,
-    smallImageText: smallImageSext,
+    smallImageKey: null,
+    details: null,
+    state: null,
+    smallImageText: null,
     startTimestamp: LeTimeMicieu,
     buttons: [
      {
@@ -36,10 +23,24 @@ presence.on("UpdateData", async () => {
          }
     ]
   };
-  if (presenceData.details === null) {
-    presence.setTrayTitle();
-    presence.setActivity();
-  } else 
-    presence.setActivity(presenceData);
+  
+  if (document.getElementById('breadcrumbBandeau') !== null) 
+    presenceData.eingrosscarott = document.getElementsByClassName("titre-onglet")[0].textContent;
+  else if (document.getElementById('id_14') !== null) 
+    presenceData.eingrosscarott = `Connection - ${document.querySelector("#id_14 > div.InlineBlock.Texte10 > div.Texte14.Gras").textContent}`;
+  else 
+    presenceData.eingrosscarott = `Visionne un fichier ${get_url_extension(document.location.pathname)}`;
+
+  if (document.location.pathname.includes('FichiersExternes')) {
+    presenceData.ladetay = "Fichiers Externes";
+    presenceData.smallImageKeyy = "nonmaiscprlesfichiers";
+    presenceData.smallImageSext = get_url_extension(document.location.pathname);
+  }else{
+    presenceData.ladetay = document.getElementsByClassName("ibe_etab_cont")[0].textContent;
+    presenceData.smallImageKeyy = "mini_logo";
+    presenceData.smallImageSext = `Lieu de l'établissement : ${document.querySelector("head > meta:nth-child(14)").content}`;
+  }
+
+  presence.setActivity(presenceData);
   
 });
