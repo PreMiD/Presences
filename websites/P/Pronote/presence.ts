05639cc44d5ed80518092f0b8f751e74e963c9f4
Noctuuu <81293192+Noctuuu@users.noreply.github.com>
2021-09-19 22:46:30 +0100

    Update presence.ts

--- websites/P/Pronote/presence.ts
+++ websites/P/Pronote/presence.ts
@@ -9,7 +9,7 @@ function getUrlExtension( url ) {
 LeTimeMicieu = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-  const presenceData = {
+  let presenceData = {
     largeImageKey: "pronote_2",
     smallImageKey: null,
     details: null,
@@ -23,18 +23,18 @@ presence.on("UpdateData", async () => {
          }
     ]
   };
-  
+
   if (document.getElementById('breadcrumbBandeau') !== null) 
     presenceData.eingrosscarott = document.getElementsByClassName("titre-onglet")[0].textContent;
   else if (document.getElementById('id_14') !== null) 
     presenceData.eingrosscarott = `Connection - ${document.querySelector("#id_14 > div.InlineBlock.Texte10 > div.Texte14.Gras").textContent}`;
   else 
-    presenceData.eingrosscarott = `Visionne un fichier ${get_url_extension(document.location.pathname)}`;
+    presenceData.eingrosscarott = `Visionne un fichier ${getUrlExtension(document.location.pathname)}`;
 
   if (document.location.pathname.includes('FichiersExternes')) {
     presenceData.ladetay = "Fichiers Externes";
     presenceData.smallImageKeyy = "nonmaiscprlesfichiers";
-    presenceData.smallImageSext = get_url_extension(document.location.pathname);
+    presenceData.smallImageSext = getUrlExtension(document.location.pathname);
   }else{
     presenceData.ladetay = document.getElementsByClassName("ibe_etab_cont")[0].textContent;
     presenceData.smallImageKeyy = "mini_logo";
