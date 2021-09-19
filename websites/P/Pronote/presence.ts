56e0685ecc90a1fdb2dc20359086bbfd90950932
Noctuuu <81293192+Noctuuu@users.noreply.github.com>
2021-09-19 21:45:35 +0100

    These are the presence files for the website "pronote"
    pronote is the n1 website for school informations like grades classes homeworks messages etc...
    here you can find a link of all schools in "Lille", a region in france https://www.index-education.com/fr/clients-pronote-12.php#liste
    here is a screenshot of most of the functionalities https://i.imgur.com/5fuLKMP.png
    it also works with the parent, teachers, administrators sections but i don't have a teacher/admin account so i couldn't take a screenshot.
    it's my first time on github so let me know if i have to do something else to publish it.

---
+++ websites/P/Pronote/presence.ts
@@ -0,0 +1,55 @@
+var presence = new Presence({
+  clientId: "886576430408929291"
+});
+
+
+
+
+LeTimeMicieu = Math.floor(Date.now() / 1000);
+presence.on("UpdateData", async () => {
+  if (document.getElementById('breadcrumbBandeau') !== null) {
+  eingrosscarott = document.getElementsByClassName("titre-onglet")[0].textContent;
+  } else if (document.getElementById('id_14') !== null) {
+    eingrosscarott = "Connection - " + document.querySelector("#id_14 > div.InlineBlock.Texte10 > div.Texte14.Gras").textContent;
+  } else {
+    function get_url_extension( url ) {
+      return url.split(/[#?]/)[0].split('.').pop().trim();
+    };
+    eingrosscarott = "Visionne un fichier " + get_url_extension(document.location.pathname);
+  }
+  if (document.location.pathname.includes('FichiersExternes')) {
+    ladetay = "Fichiers Externes";
+    smallImageKeyy = "nonmaiscprlesfichiers";
+    smallImageSext = get_url_extension(document.location.pathname);
+  }else{
+    ladetay = document.getElementsByClassName("ibe_etab_cont")[0].textContent;
+    smallImageKeyy = "mini_logo";
+    smallImageSext = "Lieu de l'établissement : " + document.querySelector("head > meta:nth-child(14)").content;
+  }
+
+
+
+
+
+  var presenceData = {
+    largeImageKey: "pronote_2",
+    smallImageKey: smallImageKeyy,
+    details: ladetay,
+    state: eingrosscarott,
+    smallImageText: smallImageSext,
+    startTimestamp: LeTimeMicieu,
+    buttons: [
+     {
+             label: "Accéder à pronote",
+             url: window.location.href
+         }
+    ]
+  };
+
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData);
+  }
+});
