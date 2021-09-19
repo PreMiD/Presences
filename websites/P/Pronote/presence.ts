077fb56c0ad96460985bf6f42005aa3904632c64
Noctuuu <81293192+Noctuuu@users.noreply.github.com>
2021-09-19 23:12:54 +0100

    Update presence.ts

--- websites/P/Pronote/presence.ts
+++ websites/P/Pronote/presence.ts
@@ -1,13 +1,10 @@
 const presence = new Presence({
   clientId: "886576430408929291"
 });
-
 function getUrlExtension( url ) {
   return url.split(/[#?]/)[0].split('.').pop().trim();
 }
-
 LeTimeMicieu = Math.floor(Date.now() / 1000);
-
 presence.on("UpdateData", async () => {
   const presenceData = {
     largeImageKey: "pronote_2",
@@ -23,14 +20,12 @@ presence.on("UpdateData", async () => {
          }
     ]
   };
-
   if (document.getElementById('breadcrumbBandeau') !== null) 
     presenceData.state = document.getElementsByClassName("titre-onglet")[0].textContent;
   else if (document.getElementById('id_14') !== null) 
     presenceData.state = `Connection - ${document.querySelector("#id_14 > div.InlineBlock.Texte10 > div.Texte14.Gras").textContent}`;
   else 
     presenceData.state = `Visionne un fichier ${getUrlExtension(document.location.pathname)}`;
-
   if (document.location.pathname.includes('FichiersExternes')) {
     presenceData.details = "Fichiers Externes";
     presenceData.smallImageKey = "nonmaiscprlesfichiers";
@@ -40,7 +35,5 @@ presence.on("UpdateData", async () => {
     presenceData.smallImageKey = "mini_logo";
     presenceData.smallImageText = `Lieu de l'établissement : ${document.querySelector("head > meta:nth-child(14)").content}`;
   }
-
   presence.setActivity(presenceData);
-  
-});
+})
