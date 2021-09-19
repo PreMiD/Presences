c81c1809b8f926c5ea5cca91236ff0bb0204b629
codefactor-io <support@codefactor.io>
2021-09-19 14:08:37 +0000

    [CodeFactor] Apply fixes

--- websites/D/Discord BotList/presence.ts
+++ websites/D/Discord BotList/presence.ts
@@ -14,11 +14,11 @@ botPresence.on("UpdateData", async () => {
     sSubmit = await botPresence.getSetting("submitP");
 
   if (botHost == "discord-botlist.eu") {
-    if (botPage === "/") {
+    if (botPage === "/") 
 
       botData.details = "Browsing home";
 
-    } else if (botPage === "/search") {
+     else if (botPage === "/search") {
 
       const search: string = document
         .querySelector("[name='q']")
@@ -32,11 +32,11 @@ botPresence.on("UpdateData", async () => {
         }
       ];
 
-    } else if (botPage === "/login_err/") {
+    } else if (botPage === "/login_err/") 
 
       botData.details = "Login page";
 
-    } else if (botPage.includes("/users/")) {
+     else if (botPage.includes("/users/")) {
 
       const username: string = document.querySelector("#username").textContent;
       botData.details = "Viewing profile:";
@@ -48,11 +48,11 @@ botPresence.on("UpdateData", async () => {
         }
       ];
 
-    } else if (botPage.includes("/submit")) {
+    } else if (botPage.includes("/submit")) 
 
-      botData.details = "Submitting a new bot"
+      botData.details = "Submitting a new bot";
 
-    } else if (botPage.includes("/vote")) {
+     else if (botPage.includes("/vote")) {
 
       const voteBotName: string = document
         .querySelector("#NAME")
@@ -61,7 +61,7 @@ botPresence.on("UpdateData", async () => {
       botData.state = voteBotName;
       botData.buttons = [
         {
-          label: "Vote for " + voteBotName,
+          label: `Vote for ${voteBotName}`,
           url: document.URL
         }
       ];
@@ -100,41 +100,41 @@ botPresence.on("UpdateData", async () => {
       const page: string = document
         .querySelector("#content-manage > strong").textContent;
       
-        if (page.includes("Manage your Bots")) {
-        botData.details = "Managing bots"
-      } else if (page.includes("Inventory")) {
-        botData.details = "Viewing Inventory"
-      } else if (page.includes("Resubmit a bot")) {
-        botData.details = "Watching resubmittable bots"
-      } else if (page.includes("Your reports")) {
-        botData.details = "Watching made reports"
-      } else if (page.includes("Queue")) {
-        botData.details = "[Staff] Viewing the queue"
-      } else if (page.includes("Pending Reports")) {
-        botData.details = "[Staff] Watching pending reports"
-      } else {
-        botData.details = "Editing profile"
-      }
+        if (page.includes("Manage your Bots")) 
+        botData.details = "Managing bots";
+       else if (page.includes("Inventory")) 
+        botData.details = "Viewing Inventory";
+       else if (page.includes("Resubmit a bot")) 
+        botData.details = "Watching resubmittable bots";
+       else if (page.includes("Your reports")) 
+        botData.details = "Watching made reports";
+       else if (page.includes("Queue")) 
+        botData.details = "[Staff] Viewing the queue";
+       else if (page.includes("Pending Reports")) 
+        botData.details = "[Staff] Watching pending reports";
+       else 
+        botData.details = "Editing profile";
+      
 
-    } else if (botPage === "/info/certify/") {
+    } else if (botPage === "/info/certify/") 
       botData.details = "Viewing certification Page";
-    } else if (botPage === "/certify/") {
+     else if (botPage === "/certify/") 
       botData.details = "Applying for certification";
-    } else if (botPage === "/partner") {
-      botData.details = "Viewing partner page"
-    } else if (botPage === "/terms/") {
+     else if (botPage === "/partner") 
+      botData.details = "Viewing partner page";
+     else if (botPage === "/terms/") 
       botData.details = "Viewing ToS";
-    } else if (botPage === "/privacy/") {
+     else if (botPage === "/privacy/") 
       botData.details = "Viewing privacy policy";
-    } else if (botPage === "/imprint/") {
+     else if (botPage === "/imprint/") 
       botData.details = "Viewing imprint";
-    }
+    
   } else if (botHost == "docs.discord-botlist.eu") {
     const page = document.querySelector(
       "#__GITBOOK__ROOT__CLIENT__ > div.reset-3c756112--body-68cac36c > div.reset-3c756112--bodyContent-2f98451b > div > div.reset-3c756112--wholeContentBody-554be184 > div.reset-3c756112--wholeContentPage-6c3f1fc5 > div > div.reset-3c756112--pageContainer-544d6e9c > div.reset-3c756112 > div.reset-3c756112--pageHeader-15724735 > div > div > div.reset-3c756112--horizontalFlex-5a0077e0 > div.reset-3c756112--pageHeaderIntro-0c1463da > h1 > span"
     ).textContent;
     botData.details = "Viewing docs";
-    botData.state = "Page: " + page;
+    botData.state = `Page: ${page}`;
   }
 
   if (!sSubmit && botData.details === "Submitting a new bot") delete botData.details;
@@ -144,7 +144,7 @@ botPresence.on("UpdateData", async () => {
   if (botData.details == null) {
     botPresence.setTrayTitle();
     botPresence.setActivity();
-  } else {
+  } else 
     botPresence.setActivity(botData);
-  }
+  
 });
