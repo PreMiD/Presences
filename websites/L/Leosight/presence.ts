d0f2c274b96e4040356f2bea4189a25d2bd10e2e
codefactor-io <support@codefactor.io>
2021-09-21 13:33:22 +0000

    [CodeFactor] Apply fixes to commit 27ff5c6

--- websites/L/Leosight/presence.ts
+++ websites/L/Leosight/presence.ts
@@ -17,27 +17,27 @@ if (
   !window.location.href.includes("ctf") &&
   !window.location.href.includes("eco") &&
   !window.location.href.includes("guard")
-) {
+) 
   presenceData.details = "Prohlíží si hlavní stránku...";
-} else if (path === "/novinky") {
+ else if (path === "/novinky") {
   presenceData.details = "Fórum";
   presenceData.state = "Novinky";
-} else if (path.includes("registrace")) {
+} else if (path.includes("registrace")) 
   presenceData.details = "Registrace";
-} else if (path.includes("podminky-uziti")) {
+ else if (path.includes("podminky-uziti")) 
   presenceData.details = "Podmínky užití";
-} else if (path.includes("ochrana-osobnich-udaju")) {
+ else if (path.includes("ochrana-osobnich-udaju")) 
   presenceData.details = "GDPR";
-} else if (path.includes("clanek")) {
+ else if (path.includes("clanek")) {
   item = document.querySelector("div.panel-body h2") as HTMLElement;
   item2 = document.querySelector(
     "div.col-md-3.pull-left span.authorname"
   ) as HTMLElement;
   presenceData.details = item.innerText;
   presenceData.state = `Autor článku: ${item2.innerText}`;
-} else if (path.includes("editor/novy")) {
+} else if (path.includes("editor/novy")) 
   presenceData.details = "Píše nový článek";
-} else if (path === "/forum") {
+ else if (path === "/forum") {
   presenceData.details = "Fórum";
   presenceData.state = "Hlavní stránka";
 } else if (
@@ -58,96 +58,96 @@ if (
   ) as HTMLElement;
   presenceData.details = item.innerText;
   presenceData.state = `Autor tématu: ${item2.innerText}`;
-} else if (path === "/forum/43") {
+} else if (path === "/forum/43") 
   presenceData.state = "Chystá se způsobit adminům deprese.";
-} else if (path.includes("/newtopic")) {
+ else if (path.includes("/newtopic")) 
   presenceData.details = "Zakládá nové téma";
-} else if (path.includes("turnaje")) {
+ else if (path.includes("turnaje")) 
   presenceData.details = "Turnaje";
-} else if (path === "/donate") {
+ else if (path === "/donate") 
   presenceData.details = "Podpora portálu";
-} else if (path.includes("/shop")) {
+ else if (path.includes("/shop")) {
   presenceData.details = "Obchod";
-  if (path.includes("/shop/web")) {
+  if (path.includes("/shop/web")) 
     presenceData.state = "Web";
-  } else if (path.includes("skimo")) {
+   else if (path.includes("skimo")) 
     presenceData.state = "Skimo";
-  } else if (path.includes("asteria")) {
+   else if (path.includes("asteria")) 
     presenceData.state = "Asteria";
-  } else if (path.includes("vip")) {
+   else if (path.includes("vip")) 
     presenceData.state = "VIP";
-  } else if (path.includes("akce")) {
+   else if (path.includes("akce")) 
     presenceData.state = "Akce";
-  } else if (path.includes("all")) {
+   else if (path.includes("all")) 
     presenceData.state = "Vše";
-  } else if (path.includes("/shop/")) {
+   else if (path.includes("/shop/")) {
     item = document.querySelector("div.panel-body h3") as HTMLElement;
     presenceData.state = item.innerText;
   }
-} else if (path === "/trade") {
+} else if (path === "/trade") 
   presenceData.details = "Nabídky obchodu";
-} else if (path === "/market") {
+ else if (path === "/market") 
   presenceData.details = "Tržiště";
-} else if (path === "/poukaz") {
+ else if (path === "/poukaz") 
   presenceData.details = "Uplatnit poukaz";
-} else if (path === "/vip") {
+ else if (path === "/vip") 
   presenceData.details = "VIP";
-} else if (path === "/premium") {
+ else if (path === "/premium") 
   presenceData.details = "PREMIUM";
-} else if (path === "/team") {
+ else if (path === "/team") 
   presenceData.details = "Praisuje AT";
-} else if (path === "/statistiky") {
+ else if (path === "/statistiky") 
   presenceData.details = "Statistiky";
-} else if (path === "/pravidla") {
+ else if (path === "/pravidla") 
   presenceData.details = "Pravidla portálu";
-} else if (path.includes("/tym")) {
+ else if (path.includes("/tym")) {
   item = document.querySelector("div.obsah.text-center p") as HTMLElement;
   item2 = document.querySelector("div.obsah.text-center h2") as HTMLElement;
-  if (item2.innerText === "Týmy") {
+  if (item2.innerText === "Týmy") 
     presenceData.details = "Prohlíží si týmy";
-  } else {
+   else {
     presenceData.details = "Prohlíží si tým:";
     presenceData.state = `${item.innerText} (${item2.innerText})`;
   }
-} else if (path === "/ucp") {
+} else if (path === "/ucp") 
   presenceData.details = "Uživatelský panel";
-} else if (path === "/prospect") {
+ else if (path === "/prospect") 
   presenceData.details = "Prospect";
-} else if (path === "/akce") {
+ else if (path === "/akce") 
   presenceData.details = "Komunitní akce";
-} else if (path === "/ankety") {
+ else if (path === "/ankety") 
   presenceData.details = "Ankety";
-} else if (path === "/spoluhraci") {
+ else if (path === "/spoluhraci") 
   presenceData.details = "Hledám spoluhráče";
-} else if (path === "/herna") {
+ else if (path === "/herna") 
   presenceData.details = "Jsem gambler";
-} else if (path === "/hlasky") {
+ else if (path === "/hlasky") 
   presenceData.details = "Hlášky";
-} else if (path === "/ticket") {
+ else if (path === "/ticket") 
   presenceData.details = "Tickety";
-} else if (path === "/logs") {
+ else if (path === "/logs") 
   presenceData.details = "Logy";
-} else if (path === "/ukoly") {
+ else if (path === "/ukoly") 
   presenceData.details = "Denní úkoly";
-} else if (path === "/chat") {
+ else if (path === "/chat") 
   presenceData.details = "Chatuje";
-} else if (path === "/upozorneni") {
+ else if (path === "/upozorneni") 
   presenceData.details = "Upozornění";
-} else if (path === "/znamky") {
+ else if (path === "/znamky") 
   presenceData.details = "Katalog známek";
-} else if (path === "/banlist") {
+ else if (path === "/banlist") 
   presenceData.details = "Seznam zabanovaných uživatelů";
-} else if (path === "/faktury") {
+ else if (path === "/faktury") 
   presenceData.details = "Faktury";
-} else if (path === "/nastaveni") {
+ else if (path === "/nastaveni") 
   presenceData.details = "Nastavení účtu";
-} else if (path === "/dashboard") {
+ else if (path === "/dashboard") 
   presenceData.details = "Provádí adminskou magii";
-} else if (path === "/modlog") {
+ else if (path === "/modlog") 
   presenceData.details = "Provádí ještě adminštější magii";
-} else if (path === "/kodex-at") {
+ else if (path === "/kodex-at") 
   presenceData.details = "Studuje adminskou Bibli";
-} else if (path.includes("/skimo")) {
+ else if (path.includes("/skimo")) {
   presenceData.details = "Skimo rozcestník";
 
   if (path.includes("postavy")) presenceData.state = "Moje postavy";
@@ -169,29 +169,29 @@ if (
 } else if (path === "/profil/3773") {
   presenceData.details = "Obtěžuje autora tohoto";
   presenceData.state = "RPC";
-} else if (path === "/profil/1") {
+} else if (path === "/profil/1") 
   presenceData.details = "Obtěžuje Ratáže.";
-} else if (path.includes("uzivatele")) {
+ else if (path.includes("uzivatele")) 
   presenceData.details = "Seznam uživatelů";
-} else if (path.includes("/profil/")) {
+ else if (path.includes("/profil/")) {
   item = document.querySelector(".on-pname") as HTMLElement;
 
   presenceData.details = "Prohlíží si profil uživatele:";
   presenceData.state = item.innerText;
-} else if (path === "/pratele") {
+} else if (path === "/pratele") 
   presenceData.details = "Seznam přátel";
-} else if (path === "/avatar") {
+ else if (path === "/avatar") 
   presenceData.details = "Nastavuje si avatar";
-} else if (path === "/profil") {
+ else if (path === "/profil") 
   presenceData.details = "Prohlíží si svůj profil";
-} else if (window.location.href.includes("artic")) {
+ else if (window.location.href.includes("artic")) {
   presenceData.smallImageKey = "artic";
   presenceData.smallImageText = "artic.leosight.cz";
   presenceData.details = "Artic";
 
-  if (path === "/" || path === "") {
+  if (path === "/" || path === "") 
     presenceData.state = "Hlavní stránka";
-  }
+  
   if (path.includes("mdc.ic")) {
     presenceData.details = "Prohlíží si MDC";
 
@@ -225,46 +225,46 @@ if (
   } else if (path.includes("stoongle")) {
     presenceData.details = "Stoongluje";
 
-    if (path.includes("katalog")) {
+    if (path.includes("katalog")) 
       presenceData.state = "Katalog webů";
-    }
-  } else if (path.includes("burza.ic")) {
+    
+  } else if (path.includes("burza.ic")) 
     presenceData.state = "Burza";
-  } else if (path.includes("invest.ic")) {
+   else if (path.includes("invest.ic")) 
     presenceData.state = "CE Invest";
-  } else if (path.includes("hzs.ic")) {
+   else if (path.includes("hzs.ic")) 
     presenceData.state = "Hasičský záchranný sbor";
-  } else if (path.includes("nic.ic")) {
+   else if (path.includes("nic.ic")) 
     presenceData.state = "IC.NIC";
-  } else if (path.includes("krypta.ic")) {
+   else if (path.includes("krypta.ic")) 
     presenceData.state = "Krypta - online kasíno";
-  } else if (path.includes("katastr.ic")) {
+   else if (path.includes("katastr.ic")) 
     presenceData.state = "Katastr nemovitostí";
-  } else if (path.includes("lemongate.ic")) {
+   else if (path.includes("lemongate.ic")) 
     presenceData.state = "LemonGate - hlavní stránka";
-  } else if (path.includes("e6ftf4.lemon")) {
+   else if (path.includes("e6ftf4.lemon")) 
     presenceData.state = "LemonGate - rozcestník";
-  } else if (path.includes("board4.lemon")) {
+   else if (path.includes("board4.lemon")) 
     presenceData.state = "LemonGate - Board4";
-  } else if (path.includes("b3s4f3.lemon")) {
+   else if (path.includes("b3s4f3.lemon")) 
     presenceData.state = "LemonGate - kryptografie";
-  } else if (path.includes("irc4na.lemon")) {
+   else if (path.includes("irc4na.lemon")) 
     presenceData.state = "LemonGate - Ircana";
-  } else if (path.includes("1trade.lemon")) {
+   else if (path.includes("1trade.lemon")) 
     presenceData.state = "LemonGate - AceTrade";
-  } else if (path.includes("blockchain.ic")) {
+   else if (path.includes("blockchain.ic")) 
     presenceData.state = "NC Blockchain";
-  } else if (path.includes("nomelcoin.ic")) {
+   else if (path.includes("nomelcoin.ic")) 
     presenceData.state = "NomelCoin";
-  } else if (path.includes("oilrig.ic")) {
+   else if (path.includes("oilrig.ic")) 
     presenceData.state = "OilRig";
-  } else if (path.includes("kiwi.ic")) {
+   else if (path.includes("kiwi.ic")) 
     presenceData.state = "Kiwi - internetové reklamy";
-  } else if (path.includes("spay.ic")) {
+   else if (path.includes("spay.ic")) 
     presenceData.state = "SPay";
-  } else if (path.includes("sherwood.ic")) {
+   else if (path.includes("sherwood.ic")) 
     presenceData.state = "Sherwood Corporation";
-  } else if (path.includes("smail.ic")) {
+   else if (path.includes("smail.ic")) {
     presenceData.details = "Smail";
     presenceData.state = "Hlavní stránka";
 
@@ -272,15 +272,15 @@ if (
     else if (path.includes("napsat")) presenceData.state = "Napsat e-mail";
     else if (path.includes("odeslane")) presenceData.state = "Odeslané";
     else if (path.includes("nastaveni")) presenceData.state = "Nastavení";
-  } else if (path.includes("tcu.ic")) {
+  } else if (path.includes("tcu.ic")) 
     presenceData.state = "Tax Compliance Unit";
-  } else if (path.includes("mining.ic")) {
+   else if (path.includes("mining.ic")) 
     presenceData.state = "Těžba kryptoměn";
-  } else if (path.includes("vaos.ic")) {
+   else if (path.includes("vaos.ic")) 
     presenceData.state = "Veterinární a odchytová služba";
-  } else if (path.includes("writer.ic")) {
+   else if (path.includes("writer.ic")) 
     presenceData.state = "Writer";
-  }
+  
 } else if (window.location.href.includes("ctf")) {
   presenceData.smallImageKey = "ctf";
   presenceData.smallImageText = "ctf.leosight.cz";
