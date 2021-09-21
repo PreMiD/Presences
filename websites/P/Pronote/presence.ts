3eb65b30e2167985272963c451b7c56f06e63664
Noctuuu <81293192+Noctuuu@users.noreply.github.com>
2021-09-21 20:11:34 +0200

    Update presence.ts

--- websites/P/Pronote/presence.ts
+++ websites/P/Pronote/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 function getUrlExtension( url ) {
   return url.split(/[#?]/)[0].split('.').pop().trim();
 }
-LeTimeMicieu = Math.floor(Date.now() / 1000);
+let LeTimeMicieu = Math.floor(Date.now() / 1000);
 presence.on("UpdateData", async () => {
   const presenceData = {
     largeImageKey: "pronote_2",
