30a2fd138f9fb325c278fa72b613d6335156a8f4
Noctuuu <81293192+Noctuuu@users.noreply.github.com>
2021-09-23 22:58:16 +0200

    Update presence.ts

--- websites/P/Pronote/presence.ts
+++ websites/P/Pronote/presence.ts
@@ -2,26 +2,26 @@ const presence = new Presence({
   clientId: "886576430408929291"
 });
 function getUrlExtension( url ) {
-  return url.split(/[#?]/)[0].split('.').pop().trim();
+  return url.split(/[#?]/)[0].split(".").pop().trim();
 }
 presence.on("UpdateData", async () => {
   const presenceData = {
     largeImageKey: "pronote_2",
     startTimestamp: Math.floor(Date.now() / 1000),
     buttons: [
-     {
-             label: "Découvrir pronote",
-             url: "https://www.index-education.com/fr/logiciel-gestion-vie-scolaire.php"
-         }
+    {
+        label: "Découvrir pronote",
+        url: "https://www.index-education.com/fr/logiciel-gestion-vie-scolaire.php"
+     }
     ]
   };
-  if (document.getElementById('breadcrumbBandeau')) 
+  if (document.getElementById("breadcrumbBandeau")) 
     presenceData.state = document.getElementsByClassName("titre-onglet")[0].textContent;
-  else if (document.getElementById('id_14')) 
+  else if (document.getElementById("id_14")) 
     presenceData.state = `Connection - ${document.querySelector("#id_14 > div.InlineBlock.Texte10 > div.Texte14.Gras").textContent}`;
   else 
     presenceData.state = `Visionne un fichier ${getUrlExtension(document.location.pathname)}`;
-  if (document.location.pathname.includes('FichiersExternes')) {
+  if (document.location.pathname.includes("FichiersExternes")) {
     presenceData.details = "Fichiers Externes";
     presenceData.smallImageKey = "nonmaiscprlesfichiers";
     presenceData.smallImageText = getUrlExtension(document.location.pathname);
