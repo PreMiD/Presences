178e641af06ebf3496132573f5b9ac335cba58f6
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-09-23 08:39:26 +0200

    Update presence.ts

--- websites/T/TomsHardware/presence.ts
+++ websites/T/TomsHardware/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey: "tomshardware"
-  },
+  };
 
   let { pathname } = document.location;
 
