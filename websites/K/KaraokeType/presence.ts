ec1e61b6694c6c2914b2e84466fef6e67f3a6919
memetrollsXD <paris@memetrolls.net>
2021-09-27 00:50:56 +0200

    Add KaraokeType... Again

---
+++ websites/K/KaraokeType/presence.ts
@@ -0,0 +1,42 @@
+const presence = new Presence({
+  clientId: "801765062997180437"
+});
+
+let currentURL = new URL(document.location.href),
+  currentPath = currentURL.pathname.slice(1).split("/");
+const browsingStamp = Math.floor(Date.now() / 1000);
+let presenceData: PresenceData = {
+ details: "Viewing an unsupported page",
+  largeImageKey: "kt_logo",
+  startTimestamp: browsingStamp
+};
+
+/**
+ * Initialize/reset presenceData.
+ */
+const resetData = (): void => {
+  currentURL = new URL(document.location.href);
+  currentPath = currentURL.pathname.slice(1).split("/");
+  presenceData = {
+    details: "Viewing an unsupported page",
+    largeImageKey: "kt_logo",
+    startTimestamp: browsingStamp
+  };
+};
+
+((): void => {
+
+  if (currentURL.hostname === "karaoketype.com") {
+      if (currentPath[0] === "play") {
+        presenceData.details = "Playing" + document.title;
+
+    } else if (currentPath[0] === "") {
+      presenceData.details = "Choosing a song";
+    }
+  }
+});
+
+  presence.on("UpdateData", async () => {
+    resetData();
+    presence.setActivity(presenceData);
+  });
