d9d769046c7b42a92f3d5382f2d9f118d0ec5009
Allvaa <alvamailindo@gmail.com>
2021-10-06 16:56:33 +0700

    remove www.pramborsfm.com

--- websites/P/Prambors Radio/presence.ts
+++ websites/P/Prambors Radio/presence.ts
@@ -12,33 +12,31 @@ const presence = new Presence({
 let startTimestamp: number;
 
 presence.on("UpdateData", async () => {
-  const { play, pause } = await strings;
-  if (document.location.hostname === "live.pramborsfm.com") {
-    const buttonAction = [
+  const { play, pause } = await strings,
+    buttonAction = [
       ...document.querySelector("button[class*='td-player']").classList
     ]
       .pop()
       .split("--")
       .pop();
-    if (buttonAction === "play") {
-      startTimestamp = undefined;
-      presenceData.smallImageKey = "pause";
-      presenceData.smallImageText = pause;
-      delete presenceData.startTimestamp;
-      delete presenceData.state;
-    } else if (buttonAction === "pause") {
-      if (!startTimestamp) startTimestamp = Date.now();
-      presenceData.smallImageKey = "play";
-      presenceData.smallImageText = play;
-      presenceData.startTimestamp = startTimestamp;
-      presenceData.state = "Listening";
-    }
-    presenceData.buttons = [
-      {
-        label: "Listen",
-        url: "https://live.pramborsfm.com"
-      }
-    ];
+  if (buttonAction === "play") {
+    startTimestamp = undefined;
+    presenceData.smallImageKey = "pause";
+    presenceData.smallImageText = pause;
+    delete presenceData.startTimestamp;
+    delete presenceData.state;
+  } else if (buttonAction === "pause") {
+    if (!startTimestamp) startTimestamp = Date.now();
+    presenceData.smallImageKey = "play";
+    presenceData.smallImageText = play;
+    presenceData.startTimestamp = startTimestamp;
+    presenceData.state = "Listening";
   }
+  presenceData.buttons = [
+    {
+      label: "Listen",
+      url: "https://live.pramborsfm.com"
+    }
+  ];
   presence.setActivity(presenceData);
 });
