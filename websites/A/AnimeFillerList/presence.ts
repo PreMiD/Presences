3b54848f2a0523db123f56a584fe40b98ee91601
u0f <45316362+u0f@users.noreply.github.com>
2021-10-07 20:39:20 +0200

    Update presence.ts

--- websites/A/AnimeFillerList/presence.ts
+++ websites/A/AnimeFillerList/presence.ts
@@ -1,6 +1,6 @@
 const presence = new Presence({
-  clientId: "894342965772820490"
-}),
+    clientId: "894342965772820490"
+  }),
   browsingStamp = Math.floor(Date.now() / 1000);
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
@@ -8,14 +8,14 @@ presence.on("UpdateData", async () => {
     startTimestamp: browsingStamp
   };
 
-  if (document.location.pathname === "/")  presenceData.details = "In home page...";
-  else if (document.location.pathname.includes("/shows/latest-updates"))  presenceData.details = "Viewing Lastest updates...";
+  if (document.location.pathname === "/") presenceData.details = "In home page...";
+  else if (document.location.pathname.includes("/shows/latest-updates")) presenceData.details = "Viewing Lastest updates...";
   else if (document.location.pathname.includes("/shows/")) {
     presenceData.details = "Viewing filler list:";
     presenceData.state = document.querySelector(".Right > h1").textContent.replace("Filler List", " ");
-  } else if (document.location.pathname.includes("/shows"))  presenceData.details = "Browsing for anime filler list...";
+  } else if (document.location.pathname.includes("/shows")) presenceData.details = "Browsing for anime filler list...";
   else if (document.location.pathname.includes("/search/node/")) {
-    const search = document.location.pathname.split("/")
+    const search = document.location.pathname.split("/");
     presenceData.details = "Searching a filler list:";
     presenceData.state = search[search.length - 1].replace(/%20/g, " ");
   } else if (document.location.pathname.includes("/user/password")) presenceData.details = "Requesting a new password...";
