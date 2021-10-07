23e74652106f98968ff69d19b1f57323b8df2cba
u0f <45316362+u0f@users.noreply.github.com>
2021-10-07 20:30:20 +0200

    Update presence.ts

--- websites/A/AnimeFillerList/presence.ts
+++ websites/A/AnimeFillerList/presence.ts
@@ -8,19 +8,19 @@ presence.on("UpdateData", async () => {
     startTimestamp: browsingStamp
   };
 
-  if (document.location.pathname === "/")  presenceData.details = "In home page..."
-  else if (document.location.pathname.includes("/shows/latest-updates"))  presenceData.details = "Viewing Lastest updates..."
+  if (document.location.pathname === "/")  presenceData.details = "In home page...";
+  else if (document.location.pathname.includes("/shows/latest-updates"))  presenceData.details = "Viewing Lastest updates...";
   else if (document.location.pathname.includes("/shows/")) {
     presenceData.details = "Viewing filler list:";
-    presenceData.state = document.querySelector(".Right > h1").textContent.replace("Filler List", " ");;
-  } else if (document.location.pathname.includes("/shows"))  presenceData.details = "Browsing for anime filler list..."
+    presenceData.state = document.querySelector(".Right > h1").textContent.replace("Filler List", " ");
+  } else if (document.location.pathname.includes("/shows"))  presenceData.details = "Browsing for anime filler list...";
   else if (document.location.pathname.includes("/search/node/")) {
     const search = document.location.pathname.split("/")
     presenceData.details = "Searching a filler list:";
     presenceData.state = search[search.length - 1].replace(/%20/g, " ");
-  } else if (document.location.pathname.includes("/user/password")) presenceData.details = "Requesting a new password..."
-  else if (document.location.pathname.includes("/user/register")) presenceData.details = "Creating a account..."
-  else if (document.location.pathname.includes("/user/login")) presenceData.details = "Logging in..."
+  } else if (document.location.pathname.includes("/user/password")) presenceData.details = "Requesting a new password...";
+  else if (document.location.pathname.includes("/user/register")) presenceData.details = "Creating a account...";
+  else if (document.location.pathname.includes("/user/login")) presenceData.details = "Logging in...";
   else if (document.location.pathname.includes("/users/")) {
     const user = document.querySelector(".content > h1");
     presenceData.details = "Viewing a user:";
@@ -31,9 +31,9 @@ presence.on("UpdateData", async () => {
         url: `https://${document.location.hostname}/users/${user.textContent}`
       }
     ];
-  } else if (document.location.pathname.includes("/contact")) presenceData.details = "Contacting with the page..."
-  else if (document.location.pathname.includes("/changelog")) presenceData.details = "Viewing Changelog..."
-  else if (document.location.pathname.includes("/privacy-policy")) presenceData.details = "Viewing Privacy Policy..."
+  } else if (document.location.pathname.includes("/contact")) presenceData.details = "Contacting with the page...";
+  else if (document.location.pathname.includes("/changelog")) presenceData.details = "Viewing Changelog...";
+  else if (document.location.pathname.includes("/privacy-policy")) presenceData.details = "Viewing Privacy Policy...";
 
   if (!presenceData.details) {
     presence.setTrayTitle();
