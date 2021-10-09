576bd836c609597b3c0977d7647d708e9dd1ea2c
u0f <45316362+u0f@users.noreply.github.com>
2021-10-09 13:52:35 +0200

    Update websites/A/AnimeFillerList/presence.ts
    Co-authored-by: Rodry <38259440+ImRodry@users.noreply.github.com>

--- websites/A/AnimeFillerList/presence.ts
+++ websites/A/AnimeFillerList/presence.ts
@@ -32,7 +32,7 @@ presence.on("UpdateData", async () => {
       }
     ];
   } else if (document.location.pathname.includes("/contact")) presenceData.details = "Contacting with the page...";
-  else if (document.location.pathname.includes("/changelog")) presenceData.details = "Viewing Changelog...";
+  else if (document.location.pathname.includes("/changelog")) presenceData.details = "Reading the changelog";
   else if (document.location.pathname.includes("/privacy-policy")) presenceData.details = "Viewing Privacy Policy...";
 
   if (!presenceData.details) {
