7c209cb33f1522efb590f99f70d25783aa4ac884
u0f <45316362+u0f@users.noreply.github.com>
2021-10-09 13:52:20 +0200

    Update websites/A/AnimeFillerList/presence.ts
    Co-authored-by: Rodry <38259440+ImRodry@users.noreply.github.com>

--- websites/A/AnimeFillerList/presence.ts
+++ websites/A/AnimeFillerList/presence.ts
@@ -16,7 +16,7 @@ presence.on("UpdateData", async () => {
   } else if (document.location.pathname.includes("/shows")) presenceData.details = "Browsing the list of shows";
   else if (document.location.pathname.includes("/search/node/")) {
     const search = document.location.pathname.split("/");
-    presenceData.details = "Searching a filler list:";
+    presenceData.details = "Searching the filler list";
     presenceData.state = search[search.length - 1].replace(/%20/g, " ");
   } else if (document.location.pathname.includes("/user/password")) presenceData.details = "Requesting a new password...";
   else if (document.location.pathname.includes("/user/register")) presenceData.details = "Creating a account...";
