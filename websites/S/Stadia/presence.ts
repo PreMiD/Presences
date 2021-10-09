e6d06939a203664f9ee7f43da9c9a7bc75141202
Alexx <cwmills03+1@gmail.com>
2021-10-09 15:27:12 -0500

    Updated Stadia Timestamp Config

--- websites/S/Stadia/presence.ts
+++ websites/S/Stadia/presence.ts
@@ -9,31 +9,27 @@ let gameName: HTMLElement,
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "logo"
+    largeImageKey: "logo",
+    startTimestamp: browsingStamp
   };
 
   if (document.location.pathname === "/home") {
     presenceData.details = "Viewing Stadia Home";
     userName = document.querySelector("span.VY8blf.fSorq");
     presenceData.smallImageText = userName.innerText;
-    presenceData.startTimestamp = browsingStamp;
   } else if (document.location.pathname.includes("/player")) {
     gameName = document.querySelector("div.HDKZKb.LiQ6Hb");
     presenceData.details = gameName.innerText;
-    presenceData.startTimestamp = browsingStamp;
   } else if (document.location.pathname.includes("/store/details")) {
     storeName = document.querySelector("div.UG7HXc");
     gamePrice = document.querySelector("span.rdAlw");
     presenceData.details = `Viewing ${storeName.innerText}`;
     presenceData.state = `${gamePrice.innerText} on the Stadia Store`;
-    presenceData.startTimestamp = browsingStamp;
-  } else if (document.location.pathname.includes("/store")) {
+  } else if (document.location.pathname.includes("/store")) 
     presenceData.details = "Viewing Stadia Store";
-    presenceData.startTimestamp = browsingStamp;
-  } else if (document.location.pathname.includes("/pro")) {
+  else if (document.location.pathname.includes("/pro")) 
     presenceData.details = "Viewing Stadia Pro";
-    presenceData.startTimestamp = browsingStamp;
-  } else
+  else
     presenceData.details = "Can't read page";
   
 
