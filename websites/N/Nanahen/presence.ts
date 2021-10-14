199bc529bd785a3f8e8501cf46fd8e8f7e51689f
Muhammad Hazmi <mhazmiee@gmail.com>
2021-10-15 06:25:41 +0700

    Removed unwanted character
    Co-authored-by: Rhys <64903135+EncryptedDev@users.noreply.github.com>

--- websites/N/Nanahen/presence.ts
+++ websites/N/Nanahen/presence.ts
@@ -69,7 +69,7 @@ switch (
         "body > main > div > div > h2"
       ).textContent;
       presenceData.details = "Searching by parody...";
-      presenceData.state = `${genres}`;
+      presenceData.state = genres;
       presenceData.smallImageKey = "search";
     }
     if (document.location.pathname.includes("/show/authors/")) {
