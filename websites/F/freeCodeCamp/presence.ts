3bac3c695da33068f99e7569d26596b0cdac7c0f
Talal <43641182+Ta1al@users.noreply.github.com>
2021-10-17 14:29:42 +0500

    Improved freeCodeCamp
    - Added a button
    - Added presence state for `settings` and `profile` page
    - Improved presence for `learn` page

--- websites/F/freeCodeCamp/presence.ts
+++ websites/F/freeCodeCamp/presence.ts
@@ -1,26 +1,48 @@
 const presence = new Presence({
-    clientId: "797749214175035412"
+    clientId: "797749214175035412",
   }),
   timeStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
-  const presenceData: PresenceData = {
+  const {
+      title,
+      location: { pathname: page },
+    } = document,
+    presenceData: PresenceData = {
+      buttons: [
+        { label: "Visit Page", url: `https://freecodecamp.org${page}` },
+      ],
       largeImageKey: "logo",
-      startTimestamp: timeStamp
+      startTimestamp: timeStamp,
     },
-    { title, location: { pathname: page } } = document,
-    details = (page) => {
-      return page === "/" ? "Viewing:"
-        : page.startsWith("/learn") ? "Learning:"
-        : page.startsWith("/news") ? "Viewing page:" : "";
+    arr = page.split("/").filter(String),
+    capitalize = (str) =>
+      str
+        .split("-")
+        .map((item) =>
+          item.replace(item.charAt(0), item.charAt(0).toUpperCase())
+        )
+        .join(" "),
+    details = (page, title) => {
+      if (page === "/") return "Viewing:";
+      if (page.match(/^\/news|^\/settings/)) return "Viewing page:";
+      if (page.startsWith("/learn")) {
+        return arr.length == 1 ? "Learning:" : capitalize(arr[1]) + ":";
+      }
+      if (title.startsWith("Profile")) return "Viewing Profile:";
     },
     state = (page, title) => {
-      return page === "/" ? "The Main Page"
-        : page.startsWith("/learn") && !title.startsWith("Learn to Code") ? title.slice(0, title.length - 19) // gets rid of " | freeCodeCamp.org"
-        : page.startsWith("/learn") && title.startsWith("Learn to Code") ? "Selecting Course"
-        : page.startsWith("/news") ? title : "";
+      if (page === "/") return "The Main Page";
+      if (page.match(/^\/news|^\/settings/))
+        return arr[1] ? title : capitalize(arr[0]);
+      if (page.startsWith("/learn")) {
+        return arr[2] ? capitalize(arr[2])
+          : arr[1] ? "Selecting Lesson"
+          : "Selecting Course";
+      }
+      if (title.startsWith("Profile")) return arr[0];
     };
-  presenceData.details = details(page);
+  presenceData.details = details(page, title);
   presenceData.state = state(page, title);
   presence.setActivity(presenceData);
 });
