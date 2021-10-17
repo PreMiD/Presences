b9b263654647a7ca2a30e99922c302fe6dd9dc9b
Talal <43641182+Ta1al@users.noreply.github.com>
2021-10-17 09:28:51 +0500

    Shortened code

--- websites/F/freeCodeCamp/presence.ts
+++ websites/F/freeCodeCamp/presence.ts
@@ -8,20 +8,18 @@ presence.on("UpdateData", () => {
       largeImageKey: "logo",
       startTimestamp: timeStamp
     },
-    page = document.location.pathname,
-    title = document.title;
-  const details = page => {
-    return page === "/" ? "Viewing:"
-      : page.startsWith("/learn") ? "Learning:"
-      : page.startsWith("/news") ? "Viewing page:" : "";
-  };
-
-  const state = (page, title) => {
-    return page === "/" ? "The Main Page"
-      : page.startsWith("/learn") && !title.startsWith("Learn to Code") ? title.slice(0, title.length - 19) // gets rid of " | freeCodeCamp.org"
-      : page.startsWith("/learn") && title.startsWith("Learn to Code") ? "Selecting Course"
-      : page.startsWith("/news") ? title : "";
-  };
+    { title, location: { pathname: page } } = document,
+    details = (page) => {
+      return page === "/" ? "Viewing:"
+        : page.startsWith("/learn") ? "Learning:"
+        : page.startsWith("/news") ? "Viewing page:" : "";
+    },
+    state = (page, title) => {
+      return page === "/" ? "The Main Page"
+        : page.startsWith("/learn") && !title.startsWith("Learn to Code") ? title.slice(0, title.length - 19) // gets rid of " | freeCodeCamp.org"
+        : page.startsWith("/learn") && title.startsWith("Learn to Code") ? "Selecting Course"
+        : page.startsWith("/news") ? title : "";
+    };
   presenceData.details = details(page);
   presenceData.state = state(page, title);
   presence.setActivity(presenceData);
