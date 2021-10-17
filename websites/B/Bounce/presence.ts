ba44bca051a0818b7ef74a70251e80821e556f03
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2021-10-17 23:58:19 +0100

    Update presence.ts

--- websites/B/Bounce/presence.ts
+++ websites/B/Bounce/presence.ts
@@ -1,18 +1,21 @@
 const presence = new Presence({ clientId: "817772461109018664" }),
   timestamp = Math.floor(Date.now() / 1000),
   newStats = async () =>
-    (data = await (await window.fetch("https://api.itsbounce.net")).json());
+    (data = await (await window.fetch("https://azura.itsbeats.net/api/nowplaying/1")).json());
 
 let data: {
   listeners: {
     total: string;
   };
-  nowplaying: {
-    artist: string;
-    title: string;
+  now_playing: {
+    song: {
+      artist: string;
+      title: string;
+    }
   };
-  presenter: {
-    name: string;
+  live: {
+    is_live: boolean;
+    streamer_name: string;
   };
 };
 
@@ -23,34 +26,34 @@ presence.on("UpdateData", async () => {
   const settings = {
       details: (await presence.getSetting("details")).replace(
         "%listeners%",
-        data.listeners?.total ?? "Listeners"
+        data.listeners.total ?? "Listeners"
       ),
       state: (await presence.getSetting("state"))
-        .replace("%artist%", data.nowplaying?.artist || "Artist")
+        .replace("%artist%", data.now_playing.song.artist || "Artist")
         .replace(
           "%songText%",
           data.nowplaying
-            ? `${data.nowplaying.artist} - ${data.nowplaying.title}`
+            ? `${data.now_playing.song.artist} - ${data.now_playing.song.title}`
             : "Song"
         )
-        .replace("%title%", data.nowplaying?.title || "Title"),
+        .replace("%title%", data.now_playing.song.title || "Title"),
       timestamp: await presence.getSetting("timestamp")
     },
     presenceData: PresenceData = {
       largeImageKey: "logo",
       details: settings.details,
       state: settings.state,
-      smallImageText: `${data.presenter?.name || "AutoDJ"} is live!`,
+      smallImageText: `${data.live.is_live ? data.live.streamer_name : "AutoDJ"} is live!`,
       buttons: [
         {
           label: "Tune in",
-          url: "https://live.itsbounce.net"
+          url: "https://radio.itsbeats.net"
         }
       ]
     };
 
   if (settings.timestamp) presenceData.startTimestamp = timestamp;
-  if (data.presenter?.name !== "AutoDJ") presenceData.smallImageKey = "live";
+  if (data.live.is_live) presenceData.smallImageKey = "live";
   else delete presenceData.smallImageText;
 
   presence.setActivity(presenceData);
