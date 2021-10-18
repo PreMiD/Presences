ea92c9264988f4a2f494f05697e34bee9d6c93ce
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-10-18 01:13:02 +0100

    chore(schemaUpdater)

--- util/tools/schemaUpdater.ts
+++ util/tools/schemaUpdater.ts
@@ -30,7 +30,6 @@ const read = (path: string): string => readFile(path, { encoding: "utf8" }),
         await axios.get(
           "https://api.github.com/repos/PreMiD/Schemas/contents/schemas/metadata"
         )
-      ).data as {name: string}[]
       ).data as { name: string }[]
     )
       .filter((c) => c.name.endsWith(".json"))
