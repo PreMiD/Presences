47a589e6f6e783a06f03225e80c31b6798abd989
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2021-10-18 20:47:57 +0200

    hopefully fixed most of the mistakes
    @MastiderMast or @thevalleyy need to retest the premid
    Also I cant run prettifier/eslint, needs to be done by one of them too

--- websites/M/MGServers/presence.ts
+++ websites/M/MGServers/presence.ts
@@ -17,64 +17,71 @@ presence.on("UpdateData", async () => {
     };
 
     if (privacy) {
-        if (getUrl(`${forumurl}`)) {
+        if (getUrl(forumurl)) {
             presenceData.details = "Ist im Forum";
             presenceData.smallImageText = "Forum";
             presenceData.smallImageKey = "mgs-normal";
         }
-        if (getUrl(`${panelurl}`)) {
+        if (getUrl(panelurl)) {
             presenceData.details = "Ist im Webinterface";
             presenceData.smallImageText = "Webinterface";
             presenceData.smallImageKey = "wi-normal";
         }
     } else {
-        if (getUrl(`${forumurl}`)) {
+        if (getUrl(forumurl)) {
             presenceData.details = "Ist im Forum";
             presenceData.smallImageText = "Forum";
             presenceData.smallImageKey = "mgs-normal";
-            presenceData.state = `${document.querySelector("title").textContent.split(' - MGServers')[0]}`
+            [presenceData.state] = document.querySelector("title").textContent.split(' - MGServers');
 
-            if(document.URL.includes("conversation")) presenceData.state = `Liest eine Konversation`, presenceData.buttons = [{label: "Forum",url: `https://${forumurl}/forum`}]
-            if(document.URL.includes("global-jcoins-statement-list")) presenceData.state = `Kontoauszüge`, presenceData.buttons = [{label: "Forum",url: `https://${forumurl}/forum`}]
-            if(document.URL.includes("acp")) presenceData.state = `Administrationsoberfläche`, presenceData.buttons = [{label: "Forum",url: `https://${forumurl}/forum`}]
-            if(document.URL.includes("moderation-list")) presenceData.state = `Moderation`, presenceData.buttons = [{label: "Forum",url: `https://${forumurl}/forum`}]
+            if (document.URL.includes("conversation")) {
+              presenceData.state = `Liest eine Konversation`;
+              presenceData.buttons = [{label: "Forum",url: `https://${forumurl}/forum`}];
+            } else if (document.URL.includes("global-jcoins-statement-list")) {
+              presenceData.state = `Kontoauszüge`;
+              presenceData.buttons = [{label: "Forum",url: `https://${forumurl}/forum`}];
+            } else if (document.URL.includes("acp")) 
+              presenceData.state = `Administrationsoberfläche`;
+              presenceData.buttons = [{label: "Forum",url: `https://${forumurl}/forum`}];
+            } else if (document.URL.includes("moderation-list")) {
+              presenceData.state = `Moderation`;
+              presenceData.buttons = [{label: "Forum",url: `https://${forumurl}/forum`}];
+            }
         }
-        if (getUrl(`${panelurl}`)) {
+        if (getUrl(panelurl)) {
             presenceData.smallImageText = "Webinterface";
             presenceData.smallImageKey = "wi-normal";
             presenceData.details = "Serverliste";
             
             if (getPath("")) presenceData.details = "Serverliste";
-            if (getPath("/")) presenceData.details = "Serverliste";
-            if (getPath("/auth/login")) presenceData.details = "Panel-Login";
-            if (getPath("/auth/password")) presenceData.details = "Hat sein Passwort vergessen...";
-            if (getPath("/account")) presenceData.details = "Schaut seinen Panel-Account an";
-            if (getPath("/account/api")) presenceData.details = "Schaut seine API-Keys an";
-            if (getPath("/staff")) presenceData.details = "Stellt eine Zugriffsanfrage"
+            else if (getPath("/")) presenceData.details = "Serverliste";
+            else if (getPath("/auth/login")) presenceData.details = "Panel-Login";
+            else if (getPath("/auth/password")) presenceData.details = "Hat sein Passwort vergessen...";
+            else if (getPath("/account")) presenceData.details = "Schaut seinen Panel-Account an";
+            else if (getPath("/account/api")) presenceData.details = "Schaut seine API-Keys an";
+            else if (getPath("/staff")) presenceData.details = "Stellt eine Zugriffsanfrage";
 
-            if (getUrl(`${panelserverurl}`)) {
+            if (getUrl(panelserverurl)) {
+                presenceData.state = `${document.querySelector("title").textContent.split(' |')[0]}`;
 
-                presenceData.state = `${document.querySelector("title").textContent.split(' |')[0]}`
-
-                if (getPathServer(``)) presenceData.details = "Server-Konsole"
-                if (getPathServer(`/files`)) presenceData.details = "Ist im Dateimanager"
-                if (getPathServer(`/databases`)) presenceData.details = "Bearbeitet Datenbanken"
-                if (getPathServer(`/schedules`)) presenceData.details = "Verwaltet Aufgaben"
-                if (getPathServer(`/user`)) presenceData.details = "Verwaltet Subuser"
-                if (getPathServer(`/backups`)) presenceData.details = "Verwaltet Backups"
-                if (getPathServer(`/network`)) presenceData.details = "Verwaltet IPs und Ports"
-                if (getPathServer(`/startup`)) presenceData.details = "Verwaltet Startparameter"
-                if (getPathServer(`/subdomain`)) presenceData.details = "Verwaltet Subdomains"
-                if (getPathServer(`/staff`)) presenceData.details = "Verwaltet Zugriffsanfragen"
-                if (getPathServer(`/settings`)) presenceData.details = "Servereinstellungen"
+                if (getPathServer(``)) presenceData.details = "Server-Konsole";
+                else if (getPathServer(`/files`)) presenceData.details = "Ist im Dateimanager";
+                else if (getPathServer(`/databases`)) presenceData.details = "Bearbeitet Datenbanken";
+                else if (getPathServer(`/schedules`)) presenceData.details = "Verwaltet Aufgaben";
+                else if (getPathServer(`/user`)) presenceData.details = "Verwaltet Subuser";
+                else if (getPathServer(`/backups`)) presenceData.details = "Verwaltet Backups";
+                else if (getPathServer(`/network`)) presenceData.details = "Verwaltet IPs und Ports";
+                else if (getPathServer(`/startup`)) presenceData.details = "Verwaltet Startparameter";
+                else if (getPathServer(`/subdomain`)) presenceData.details = "Verwaltet Subdomains";
+                else if (getPathServer(`/staff`)) presenceData.details = "Verwaltet Zugriffsanfragen";
+                else if (getPathServer(`/settings`)) presenceData.details = "Servereinstellungen";
             }
 
             if (getPath("/admin")) {
-                presenceData.details = "Admininterface"
+                presenceData.details = "Admininterface";
                 delete presenceData.state;
-            }
-            if (document.querySelector("title").textContent.toLowerCase() === "forbidden") {
-                presenceData.details = "Wünscht sich, cool zu sein!"
+            } else if (document.querySelector("title").textContent.toLowerCase() === "forbidden") {
+                presenceData.details = "Wünscht sich, cool zu sein!";
                 delete presenceData.state;
             }
         }
@@ -89,13 +96,13 @@ presence.on("UpdateData", async () => {
                 },
                 {
                     label: "Aktuelle Seite",
-                    url: `${document.URL}`
+                    url: document.URL
                 }
             ]
-            if(document.URL.includes("conversation")) presenceData.state = `Liest eine Konversation`
-            if(document.URL.includes("global-jcoins-statement-list")) presenceData.state = `Kontoauszüge`
-            if(document.URL.includes("acp")) presenceData.state = `Administrationsoberfläche`
-            if(document.URL.includes("moderation-list")) presenceData.state = `Moderation`
+            if (document.URL.includes("conversation")) presenceData.state = `Liest eine Konversation`;
+            else if (document.URL.includes("global-jcoins-statement-list")) presenceData.state = `Kontoauszüge`;
+            else if (document.URL.includes("acp")) presenceData.state = `Administrationsoberfläche`;
+            else if (document.URL.includes("moderation-list")) presenceData.state = `Moderation`;
         } else {
             presenceData.buttons = [
                 {
@@ -121,13 +128,13 @@ presence.on("UpdateData", async () => {
 });
 
 function getUrl(url: string) {
-    return window.location.href.toLowerCase().includes(`${url}`)
+    return window.location.href.toLowerCase().includes(url)
 }
 
 function getPath(path: string) {
-    return window.location.pathname.toLowerCase().includes(`${path}`)
+    return window.location.pathname.toLowerCase().includes(path)
 }
 
 function getPathServer(path: string) {
-    return window.location.href.toLocaleLowerCase().replace(`${panelserverurl}`, "").includes(`${path}`)
+    return window.location.href.toLocaleLowerCase().replace(panelserverurl, "").includes(path)
 }
