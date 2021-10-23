5bf21a670c99edcfe229b333dcfdd326cf186acd
Marko Pejanovic <mare.sampbn@gmail.com>
2021-10-23 15:44:27 +0200

    Fixed requested changes and changed logo

--- websites/M/Manganato/presence.ts
+++ websites/M/Manganato/presence.ts
@@ -15,23 +15,20 @@ const presence = new Presence({
       data.state = (document.querySelector("#search-story") as HTMLInputElement).value;
       data.smallImageKey = "search";
     } else {
-  
       if (/^\/$/.test(pathname)) data.details = "Viewing Home Page";
       else if (/^\/genre-all\/?$/.test(pathname)) {
         // Counting manga
         let manga = document.querySelector(".group-qty a").textContent;
         data.details = "Viewing Manga List";
-        data.state = "📋 " + manga.charAt(0).toUpperCase() + manga.slice(1).toLowerCase() + " manga";
-      }
-      else if (/^\/(manga+)-[a-z0-9]+\/?$/i.test(pathname)) {
+        data.state = `📋 ${manga.charAt(0).toUpperCase()}${manga.slice(1).toLowerCase()} manga`;
+      }else if (/^\/(manga+)-[a-z0-9]+\/?$/i.test(pathname)) {
         data.details = "Viewing Manga";
         data.state = document.querySelector(".story-info-right h1").textContent;
         data.smallImageKey = "view";
-        const viewComicButton: ButtonData = {
+        data.buttons = [{
           label: "Visit Comic Page",
-          url: origin + pathname
-        };
-        data.buttons = [viewComicButton];
+          url: document.location.href
+        }];
       } else if (/^\/(manga+)-[a-z0-9]+\/(chapter+)-[a-z0-9]+?$/i.test(pathname)) {
           
         const comicLink = (document.querySelector(".panel-breadcrumb a:nth-child(3)") as HTMLAnchorElement).href,
@@ -47,15 +44,13 @@ const presence = new Presence({
         data.details = document.querySelector(".panel-breadcrumb a:nth-child(3)").textContent;
         data.state = `📖 ${chapter} 🔸 ${progress}%`;
         data.smallImageKey = "read";
-        const viewComicButton: ButtonData = {
+        data.buttons = [{
           label: "Visit Comic Page",
           url: origin + comicLink
-        },
-          visitChapterButton: ButtonData = {
-            label: "Visit Chapter",
-            url: origin + pathname
-          };
-        data.buttons = [viewComicButton, visitChapterButton];
+        }, {
+          label: "Visit Chapter",
+          url: origin + pathname
+        }];
       } else {
         data.details = "Browsing Manganato";
         data.state = document.title;
