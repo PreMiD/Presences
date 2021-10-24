09574fef28d87598592cfdf0e14ef2291858ca3a
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2021-10-24 11:34:41 +0100

    Recode Tidal presence

---
+++ websites/T/Tidal/presence.ts
@@ -0,0 +1,71 @@
+const presence = new Presence({
+  clientId: "901591802342150174"
+});
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "logo"
+  };
+
+  if (!document.querySelector("#footerPlayer"))
+    return presence.setActivity({ largeImageKey: "logo" });
+
+  const songTitle = document.querySelector(
+      'div[data-test="footer-track-title"] > a'
+    ) as HTMLAnchorElement,
+    songArtist = document.querySelector(
+      'div[data-test="left-column-footer-player"] > div:nth-child(2) > div:nth-child(2) > span > span > span'
+    ),
+    currentTime = (
+      document.querySelector("time.current-time") as HTMLElement
+    ).innerText.split(":"),
+    endTime = (
+      document.querySelector("time.duration-time") as HTMLElement
+    ).innerText.split(":"),
+    currentTimeSec =
+      (parseFloat(currentTime[0]) * 60 + parseFloat(currentTime[1])) * 1000,
+    endTimeSec =
+      (parseFloat(endTime[0]) * 60 + parseFloat(endTime[1]) + 1) * 1000,
+    endTimestamp = Date.now() + (endTimeSec - currentTimeSec),
+    paused =
+      document
+        .querySelector('div[data-test="play-controls"] div > button')
+        .getAttribute("data-test") === "play",
+    onRepeat = document
+      .querySelector(
+        'div[data-test="play-controls"] > button[data-test="repeat"]'
+      )
+      .getAttribute("aria-checked"),
+    repeatType = document
+      .querySelector(
+        'div[data-test="play-controls"] > button[data-test="repeat"]'
+      )
+      .getAttribute("aria-label");
+
+  presenceData.details = songTitle.innerText;
+  presenceData.state = songArtist.textContent;
+
+  if (currentTimeSec > 0 || !paused) {
+    presenceData.endTimestamp = endTimestamp;
+    presenceData.smallImageKey = paused ? "pause" : "play";
+    presenceData.smallImageText = paused ? "Paused" : "Playing";
+  }
+
+  if (onRepeat === "true") {
+    presenceData.smallImageKey =
+      repeatType == "Repeat" ? "repeat" : "repeat-one";
+    presenceData.smallImageText =
+      repeatType == "Repeat" ? "Playlist on loop" : "On loop";
+
+    delete presenceData.endTimestamp;
+  }
+
+  presenceData.buttons = [
+    {
+      label: "View Song",
+      url: songTitle.href
+    }
+  ];
+
+  presence.setActivity(presenceData);
+});
