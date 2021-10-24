6e97caa293cfcacb080bd896bb5369a20d6a460c
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-10-24 19:42:47 +0100

    chore(MangaReader): fixed eslint & prettified

--- websites/M/MangaReader/presence.ts
+++ websites/M/MangaReader/presence.ts
@@ -1,32 +1,34 @@
 const presence = new Presence({
-    clientId: "901608545743683674",
+    clientId: "901608545743683674"
   }),
   strings = presence.getStrings({
     homepage: "general.viewHome",
     reading: "general.reading",
     searchSomething: "general.searchSomething",
     searchFor: "general.searchFor",
-    genre: "general.viewGenre",
+    genre: "general.viewGenre"
   }),
   browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const data: PresenceData = {
     largeImageKey: "logo",
-    details: (await strings).searchFor,
+    details: (await strings).searchFor
   };
 
   if (document.location.pathname === "/" || !document.location.pathname)
     data.details = (await strings).searchSomething;
-  else if (document.location.pathname.includes("/search"))
-    data.state =
-      '"' + document.querySelector(".block_area-header i").textContent + '"';
-  else if (document.location.pathname.includes("/home"))
+  else if (document.location.pathname.includes("/search")) {
+    data.state = `"${
+      document.querySelector(".block_area-header i").textContent
+    }"`;
+  } else if (document.location.pathname.includes("/home"))
     data.details = (await strings).homepage;
   else if (document.location.pathname.includes("/genre")) {
     data.details = (await strings).genre;
-    data.state =
-      "📔 " + document.querySelector(".block_area-header").textContent;
+    data.state = `📔 ${
+      document.querySelector(".block_area-header").textContent
+    }`;
   } else if (document.location.pathname.includes("/new-release"))
     data.state = "✌️ New releases";
   else if (document.location.pathname.includes("/completed"))
@@ -37,10 +39,11 @@ presence.on("UpdateData", async () => {
     data.state = "⚡ Latest Updated";
   else if (document.location.pathname.includes("/az-list"))
     data.state = "🔠 A-Z List";
-  else if (document.location.pathname.includes("/type"))
-    data.state =
-      "📖 " + document.querySelector(".block_area-header").textContent + "s";
-  else if (document.location.pathname.includes("/character")) {
+  else if (document.location.pathname.includes("/type")) {
+    data.state = `📖 ${
+      document.querySelector(".block_area-header").textContent
+    }s`;
+  } else if (document.location.pathname.includes("/character")) {
     data.details = "Viewing Character:";
     data.state = document.querySelector(".name").textContent;
   } else if (document.location.pathname.includes("/author")) {
@@ -51,20 +54,17 @@ presence.on("UpdateData", async () => {
     data.buttons = [
       {
         label: "Read Manga",
-        url: document.location.href,
-      },
+        url: document.location.href
+      }
     ];
 
     if (document.location.pathname.includes("/read")) {
       data.details = `${(await strings).reading} ${data.details}`;
-      data.state =
-        document
-          .querySelector("#current-chapter")
-          .textContent.replace(" ", " #️") +
-        " | " +
-        document
-          .querySelector("#c-selected-lang, #v-selected-lang")
-          .textContent.replace("Language: ", "");
+      data.state = `${document
+        .querySelector("#current-chapter")
+        .textContent.replace(" ", " #️")} | ${document
+        .querySelector("#c-selected-lang, #v-selected-lang")
+        .textContent.replace("Language: ", "")}`;
       data.startTimestamp = browsingStamp;
     }
   }
