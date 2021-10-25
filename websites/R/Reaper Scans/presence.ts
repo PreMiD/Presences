a04dd2939fcd431928a2c6e2eba698683e629766
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-10-25 19:33:39 +0100

    chore(Reaper Scans): fix eslint issues & pretty

--- websites/R/Reaper Scans/presence.ts
+++ websites/R/Reaper Scans/presence.ts
@@ -1,6 +1,6 @@
 const presence = new Presence({
-  clientId: "900717839713959967"
-}),
+    clientId: "900717839713959967"
+  }),
   elapsed = Math.floor(Date.now() / 1e3);
 
 presence.on("UpdateData", () => {
@@ -10,48 +10,67 @@ presence.on("UpdateData", () => {
       largeImageKey: "logo"
     };
 
-  if (document.querySelector(".search-main-menu").classList.contains("active")) {
+  let comics: number;
+
+  if (
+    document.querySelector(".search-main-menu").classList.contains("active")
+  ) {
     data.details = "Searching:";
-    data.state = (document.querySelector(".manga-search-field") as HTMLInputElement).value;
+    data.state = (
+      document.querySelector(".manga-search-field") as HTMLInputElement
+    ).value;
     data.smallImageKey = "search";
   } else {
-
     if (/^\/$/.test(pathname)) data.details = "Viewing Home Page";
     else if (/^\/home1\/?$/.test(pathname)) {
       // Counting comics
-      let comics = document.querySelectorAll(".page-listing-item .row .col-4").length;
+      comics = document.querySelectorAll(
+        ".page-listing-item .row .col-4"
+      ).length;
       data.details = "Viewing Comic List";
-      data.state = "📋 " + comics.toString() + " comics found";
-    }
-    else if (/^\/all-series\/novels+\/?$/.test(pathname)) {
+      data.state = `📋 ${comics.toString()} comics found`;
+    } else if (/^\/all-series\/novels+\/?$/.test(pathname)) {
       // Counting novels
-      let comics = document.querySelectorAll(".page-listing-item .row .col-6").length;
+      comics = document.querySelectorAll(
+        ".page-listing-item .row .col-6"
+      ).length;
       data.details = "Viewing Novel List";
-      data.state = "📋 " + comics.toString() + " novels found";
-    }
-    else if (/^\/all-series\/comics\/manhwas\/?$/.test(pathname)) {
+      data.state = `📋 ${comics.toString()} novels found`;
+    } else if (/^\/all-series\/comics\/manhwas\/?$/.test(pathname)) {
       // Counting manhwa
-      let comics = document.querySelectorAll(".page-listing-item .row .col-6").length;
+      comics = document.querySelectorAll(
+        ".page-listing-item .row .col-6"
+      ).length;
       data.details = "Viewing Manhwa List";
-      data.state = "📋 " + comics.toString() + " manhwa found";
-    }
-    else if (/^\/all-series\/comics\/manhuas\/?$/.test(pathname)) {
+      data.state = `📋 ${comics.toString()} manhwa found`;
+    } else if (/^\/all-series\/comics\/manhuas\/?$/.test(pathname)) {
       // Counting manhua
-      let comics = document.querySelectorAll(".page-listing-item .row .col-4").length;
+      comics = document.querySelectorAll(
+        ".page-listing-item .row .col-4"
+      ).length;
       data.details = "Viewing Manhua List";
-      data.state = "📋 " + comics.toString() + " manhua found";
-    }
-    else if (/^\/series\/[0-9a-z-]+\/?$/i.test(pathname)) {
+      data.state = `📋 ${comics.toString()} manhua found`;
+    } else if (/^\/series\/[0-9a-z-]+\/?$/i.test(pathname)) {
       data.details = "Viewing Comic";
       data.state = document.querySelector(".post-title h1").textContent;
       data.smallImageKey = "view";
-      data.buttons = [{
-        label: "Visit Comic Page",
-        url: origin + pathname
-      }];
-    } else if (/^\/series\/[0-9a-z-]+\/+(chapter|ch)-[0-9]+\/?$/i.test(pathname)) {
-      const comicLink = (document.querySelector("ol.breadcrumb li:nth-child(3) a") as HTMLAnchorElement).href,
-        chapter = document.querySelector("ol.breadcrumb li:nth-child(4)").textContent;
+      data.buttons = [
+        {
+          label: "Visit Comic Page",
+          url: origin + pathname
+        }
+      ];
+    } else if (
+      /^\/series\/[0-9a-z-]+\/+(chapter|ch)-[0-9]+\/?$/i.test(pathname)
+    ) {
+      const comicLink = (
+          document.querySelector(
+            "ol.breadcrumb li:nth-child(3) a"
+          ) as HTMLAnchorElement
+        ).href,
+        chapter = document.querySelector(
+          "ol.breadcrumb li:nth-child(4)"
+        ).textContent;
 
       let progress =
         (document.documentElement.scrollTop /
@@ -60,16 +79,21 @@ presence.on("UpdateData", () => {
         100;
       progress = Math.ceil(progress) > 100 ? 100 : Math.ceil(progress);
 
-      data.details = document.querySelector("ol.breadcrumb li:nth-child(3)").textContent;
+      data.details = document.querySelector(
+        "ol.breadcrumb li:nth-child(3)"
+      ).textContent;
       data.state = `📖 ${chapter} 🔸 ${progress}%`;
       data.smallImageKey = "read";
-      data.buttons = [{
-        label: "Visit Comic Page",
-        url: origin + comicLink
-      }, {
-        label: "Visit Chapter",
-        url: origin + pathname
-      }];
+      data.buttons = [
+        {
+          label: "Visit Comic Page",
+          url: origin + comicLink
+        },
+        {
+          label: "Visit Chapter",
+          url: origin + pathname
+        }
+      ];
     } else {
       data.details = "Browsing Reaper Scans";
       data.state = document.title;
