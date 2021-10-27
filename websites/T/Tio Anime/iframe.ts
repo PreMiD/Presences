a08bf92f75fc1a833ebdc4d7ccab51ea56495929
u0f <45316362+u0f@users.noreply.github.com>
2021-10-27 17:58:51 +0200

    Update iframe.ts

--- websites/T/Tio Anime/iframe.ts
+++ websites/T/Tio Anime/iframe.ts
@@ -2,7 +2,7 @@ const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
   const video: HTMLVideoElement = document.querySelector("video");
-  if (video != null && !isNaN(video.duration)) {
+  if (video !== null && !isNaN(video.duration)) {
     iframe.send({
       duration: video.duration,
       currentTime: video.currentTime,
