d150c3dbfbc6f723353072222c9d03f142ef5c94
Declan Mould <44209534+TheGamerzs@users.noreply.github.com>
2021-10-30 22:00:07 +0100

    Add TeamSeas presence + Update TeamTrees (#4850)
    * Add #TeamSeas
    
    * Update TeamTrees
    
    * Update metadata.json

--- websites/T/TeamSeas/presence.ts
+++ websites/T/TeamSeas/presence.ts
@@ -1,26 +1,25 @@
 const presence = new Presence({
-    clientId: "638344004085350400"
+    clientId: "904072771900948570"
   }),
   browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-      largeImageKey: "teamtrees"
+      largeImageKey: "teamseas"
     },
     currentCount = await presence.getSetting("count"),
-    count = document.getElementById("totalTrees")?.textContent;
+    count = document.getElementById("liveCounter")?.textContent;
 
   if (currentCount) {
     presenceData.details = `$${count}`;
     presenceData.state = "Currently Donated";
   } else {
-    presenceData.details = "Helping #TeamTrees plant";
-    presenceData.state = "20million trees by 2020";
+    presenceData.details = "Helping #TeamSeas clean";
+    presenceData.state = "30million pounds by 2022";
   }
-
   presenceData.startTimestamp = browsingStamp;
   presenceData.buttons = [
-    { label: "Visit #TeamSeas", url: "https://teamSeas.org" }
+    { label: "Visit #TeamTrees", url: "https://teamtrees.org" }
   ];
 
   presence.setActivity(presenceData);
