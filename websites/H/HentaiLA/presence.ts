2d330e8059ba212450110e2af6875bfcab67d5cb
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-10-31 12:39:08 +0000

    style(HentaiLA): fix prettier & eslint

--- websites/H/HentaiLA/presence.ts
+++ websites/H/HentaiLA/presence.ts
@@ -8,39 +8,47 @@ let video = {
   currentTime: 0,
   paused: true
 };
-  
+
 presence.on(
   "iFrameData",
   (data: { duration: number; currentTime: number; paused: boolean }) => {
     video = data;
   }
 );
+
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey: "hentaila",
     startTimestamp: browsingStamp
   };
-  if (document.location.pathname === "/") presenceData.details = "En la página de inicio";
-  else if (document.location.pathname.includes("/genero/"))
-    presenceData.details = `Viendo género ${document.querySelector(".section-title").textContent.split(":")[1]}`;
-  else if (document.location.pathname.includes("/ver/")) {
+  if (document.location.pathname === "/")
+    presenceData.details = "En la página de inicio";
+  else if (document.location.pathname.includes("/genero/")) {
+    presenceData.details = `Viendo género ${
+      document.querySelector(".section-title").textContent.split(":")[1]
+    }`;
+  } else if (document.location.pathname.includes("/ver/")) {
     presenceData.details = "Viendo Hentai:";
     presenceData.state = document.querySelector(".section-title").textContent;
-    [presenceData.startTimestamp, presenceData.endTimestamp] = presence.getTimestamps(
-      Math.floor(video.currentTime),
-      Math.floor(video.duration)
-    );
-    presenceData.smallImageKey = (video.paused) ? "pause" : "play";
-    presenceData.smallImageText = (video.paused) ? "Capítulo pausado" : "Reproduciendo capítulo";
-    if(video.paused) {
+    [presenceData.startTimestamp, presenceData.endTimestamp] =
+      presence.getTimestamps(
+        Math.floor(video.currentTime),
+        Math.floor(video.duration)
+      );
+    presenceData.smallImageKey = video.paused ? "pause" : "play";
+    presenceData.smallImageText = video.paused
+      ? "Capítulo pausado"
+      : "Reproduciendo capítulo";
+    if (video.paused) {
       delete presenceData.startTimestamp;
       delete presenceData.endTimestamp;
     }
   }
-  if(document.querySelector(".hentai-single")) {
+  if (document.querySelector(".hentai-single")) {
     presenceData.details = "Viendo lista capítulos:";
     presenceData.state = document.querySelector(".h-title").textContent;
-  } switch (document.location.pathname) {
+  }
+  switch (document.location.pathname) {
     case "/directorio":
       presenceData.details = "Viendo el directorio de Hentai";
       break;
