1b121c149ecec23f5ca92f32b2b15f3662b01017
N0chteil <53608074+N0chteil@users.noreply.github.com>
2021-10-31 19:08:24 +0100

    Update presence.ts

--- websites/F/Facebook/presence.ts
+++ websites/F/Facebook/presence.ts
@@ -280,9 +280,11 @@ presence.on("UpdateData", async () => {
     else if (document.location.pathname.includes("/events/search")) {
       if (!privacyMode) {
         presenceData.details = "Events - Search";
-        presenceData.state = document.querySelectorAll(
-          "label.rq0escxv.a8c37x1j.a5nuqjux.l9j0dhe7.k4urcfbm > input.oajrlxb2.rq0escxv.f1sip0of.hidtqoto[type='search']"
-        )?.[1]?.value;
+        presenceData.state = (<HTMLInputElement>(
+          document.querySelectorAll(
+            "label.rq0escxv.a8c37x1j.a5nuqjux.l9j0dhe7.k4urcfbm > input.oajrlxb2.rq0escxv.f1sip0of.hidtqoto[type='search']"
+          )?.[1]
+        )).value;
       } else presenceData.state = "Search";
     } else if (/events\/[0-9]/g.test(document.location.pathname)) {
       if (!privacyMode) {
