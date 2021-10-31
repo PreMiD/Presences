c4dfea4482129912e319c086d2f4bb009bff4be7
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-10-31 12:38:29 +0000

    refactor(HentaiLA): remove empty line

--- websites/H/HentaiLA/iframe.ts
+++ websites/H/HentaiLA/iframe.ts
@@ -2,12 +2,11 @@ const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
   const video: HTMLVideoElement = document.querySelector("video");
-  if (video !== null && !isNaN(video.duration)) {
+  if (video && !isNaN(video.duration)) {
     iframe.send({
       duration: video.duration,
       currentTime: video.currentTime,
       paused: video.paused
     });
   }
-  
 });
