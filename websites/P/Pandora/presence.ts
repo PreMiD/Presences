091eb955907527c3ff8d4ef0882b57c376ae02c9
FireController#1847 <usfirepilot123@gmail.com>
2021-11-15 15:47:14 -0700

    Make requested changes

--- websites/P/Pandora/presence.ts
+++ websites/P/Pandora/presence.ts
@@ -20,69 +20,67 @@ function stripText(element: HTMLElement, id = "None", log = true) {
 
 presence.on("UpdateData", async () => {
   // Define presence data
-  const data: PresenceData = {};
-
-  // Set default data
-  data.details = "Browsing...";
-  data.largeImageKey = "pandora";
+  const data: PresenceData = {
+    details: "Browsing...",
+    largeImageKey: "pandora"
+  };
 
   // Define whether or not we're currently playing
-  const isPlaying = true,
+  let isPlaying = true;
 
   // Fetch audio bar
-   audioBar: HTMLElement = document.querySelector(".Tuner__Audio__NowPlayingHitArea");
+  const audioBar: HTMLElement = document.querySelector(".Tuner__Audio__NowPlayingHitArea");
   
   // If the audio bar exists, assume we're listening to something
-  if (audioBar !== null) {
+  if (audioBar) {
     // Fetch title and artist
     const title: HTMLElement = document.querySelector(".Tuner__Audio__TrackDetail__title"),
      artist: HTMLElement = document.querySelector(".Tuner__Audio__TrackDetail__artist");
 
-    // Return if either of them are null
-    if (title === null || artist === null) 
-      return;
-
-    // Set them to the presence
-    data.details = stripText(title, "Title");
-    data.state = stripText(artist, "Artist");
-
-    // Get duration control
-    const timeElapsed: HTMLElement = document.querySelector(".VolumeDurationControl__Duration [data-qa=elapsed_time]"),
-     timeRemaining: HTMLElement = document.querySelector(".VolumeDurationControl__Duration [data-qa=remaining_time]");
-
-    // Return if either are null
-    if (timeElapsed === null || timeRemaining === null) 
-      return;
+    // Only apply them to presence if they're not null
+    if (title !== null && artist !== null) {
+      // Set them to the presence
+      data.details = stripText(title, "Title");
+      data.state = stripText(artist, "Artist");
+    } else 
+      presence.error("Title and artist are null!");
+    
 
     // Fetch play button
     const playButton: HTMLElement = document.querySelector(".Tuner__Control__Play__Button");
 
     // Return if null
-    if (playButton === null) 
-      return;
+    if (playButton !== null) {
+      // Check if we're paused or playing
+      isPlaying = playButton.getAttribute("aria-checked") === "true";
 
-    // Check if we're paused or playing
-    const isPlaying = playButton.getAttribute("aria-checked") === "true";
+      // If we're not paused, set the small image to playing and fetch the timestamps
+      // Otherwise, set the small image to paused
+      if (isPlaying) {
+        data.smallImageKey = "play";
+        data.smallImageText = (await strings).play;
 
-    // If we're not paused, set the small image to playing and fetch the timestamps
-    // Otherwise, set the small image to paused
-    if (isPlaying) {
-      data.smallImageKey = "play";
-      data.smallImageText = (await strings).play;
+        // Get duration control
+        const timeElapsed: HTMLElement = document.querySelector(".VolumeDurationControl__Duration [data-qa=elapsed_time]"),
+              timeRemaining: HTMLElement = document.querySelector(".VolumeDurationControl__Duration [data-qa=remaining_time]");
 
-      // Get timestamps
-      const [startTime, endTime] = presence.getTimestamps(
-        presence.timestampFromFormat(stripText(timeElapsed, "Time Elapsed")),
-        presence.timestampFromFormat(stripText(timeRemaining, "Time Remaining"))
-      );
-
-      // Set timestamps
-      data.startTimestamp = startTime;
-      data.endTimestamp = endTime;
-    } else {
-      data.smallImageKey = "pause";
-      data.smallImageText = (await strings).pause;
-    }
+        // Return if either are null
+        if (timeElapsed !== null && timeRemaining !== null) {
+          // Get timestamps
+          [data.startTimestamp, data.endTimestamp] = presence.getTimestamps(
+            presence.timestampFromFormat(stripText(timeElapsed, "Time Elapsed")),
+            presence.timestampFromFormat(stripText(timeRemaining, "Time Remaining"))
+          );
+        } else 
+          presence.error("Timestamps are null!");
+        
+      } else {
+        data.smallImageKey = "pause";
+        data.smallImageText = (await strings).pause;
+      }
+    } else 
+      presence.error("Play button is null!");
+    
   }
 
   presence.setActivity(data, isPlaying);
