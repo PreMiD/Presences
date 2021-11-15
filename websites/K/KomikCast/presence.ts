b465d17f4a506dcd8e21873e5858408699bfc11e
crazykarpenter <aldijpratama@gmail.com>
2021-11-16 00:53:26 +0700

    used prettier to beautify the code

--- websites/K/KomikCast/presence.ts
+++ websites/K/KomikCast/presence.ts
@@ -24,53 +24,55 @@ presence.on("UpdateData", async () => {
     presenceData.details = "Viewing Bookmark";
   else if (document.location.pathname.endsWith("/download-aplikasi-komikcast/"))
     presenceData.details = "Download APK Komikcast";
-    // Added button
+  // Added button
   else if (document.location.pathname.startsWith("/komik/")) {
     const name = document
         .querySelector(".komik_info-content-body-title")
-        .textContent.replace(/\t|\n/g, ""), type = document.querySelector(".komik_info-content-info-type b").textContent.replace("Type:", "Comics");
+        .textContent.replace(/\t|\n/g, ""),
+      type = document
+        .querySelector(".komik_info-content-info-type b")
+        .textContent.replace("Type:", "Comics");
     presenceData.details = `Viewing ${type}`;
     presenceData.state = name;
     if (buttons) {
-        presenceData.buttons = [
-              {
-                label: `View ${type}`,
-                url: document.location.href
-              }
-          ];
-      }
-  }
-  else if (document.location.pathname.startsWith("/chapter/")) {
+      presenceData.buttons = [
+        {
+          label: `View ${type}`,
+          url: document.location.href
+        }
+      ];
+    }
+  } else if (document.location.pathname.startsWith("/chapter/")) {
     const title = document
         .querySelector("div.chapter_headpost h1")
         .textContent.replace(/\t|\n/g, "")
-        .replace(/Chapter \d+/, ""), chapter = document.location.pathname
+        .replace(/Chapter \d+/, ""),
+      chapter = document.location.pathname
         .match(/chapter-\d+/)[0]
         .replace("c", "C")
         .replace("-", " ");
     presenceData.details = title;
     presenceData.state = chapter;
     if (buttons) {
-        presenceData.buttons = [
-            {
-                label: "View Comic",
-                url: document.location.href
-                    .replace("chapter", "komik")
-                    .replace(/-chapter-\d+/, "")
-                    .replace(/#\d+/, "")
-            },
-            {
-                label: chapter,
-                url: document.location.href.replace(/#\d+/, "")
-              }
-          ];
-      }
-  }
-  else if (document.location.href.includes("?s=")) {
+      presenceData.buttons = [
+        {
+          label: "View Comic",
+          url: document.location.href
+            .replace("chapter", "komik")
+            .replace(/-chapter-\d+/, "")
+            .replace(/#\d+/, "")
+        },
+        {
+          label: chapter,
+          url: document.location.href.replace(/#\d+/, "")
+        }
+      ];
+    }
+  } else if (document.location.href.includes("?s=")) {
     const search = document
-        .querySelector("div.list-update-search-header h1")
-        .textContent.replace("SEARCH", "")
-        .replace(/\t|\n/g, "");
+      .querySelector("div.list-update-search-header h1")
+      .textContent.replace("SEARCH", "")
+      .replace(/\t|\n/g, "");
     presenceData.smallImageKey = "search";
     presenceData.details = "Doing:";
     presenceData.state = search;
@@ -80,4 +82,4 @@ presence.on("UpdateData", async () => {
     delete presenceData.endTimestamp;
   }
   presence.setActivity(presenceData);
-  });
+});
