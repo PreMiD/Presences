f823d08b69744c5db8ca787131b44a70a6f45847
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-11-15 22:06:35 +0000

    style(CoinMarketCap)

--- websites/C/CoinMarketCap/presence.ts
+++ websites/C/CoinMarketCap/presence.ts
@@ -1,7 +1,7 @@
 const presence = new Presence({
-  clientId: "907692817604833281"
-}),
-time = Math.floor(Date.now() / 1000);
+    clientId: "907692817604833281"
+  }),
+  time = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
@@ -22,11 +22,13 @@ presence.on("UpdateData", async () => {
     ).textContent;
   } else if (path.includes("/rankings/exchanges/"))
     presenceData.details = "Looking at top cryptocurrency spot exchanges";
-  else if (path.includes("/rankings/exchanges/derivatives/"))
-    presenceData.details = "Looking at top cryptocurrency derivatives exchanges";
-  else if (path.includes("/rankings/exchanges/dex/"))
-    presenceData.details = "Looking at top cryptocurrency decentralized exchanges";
-  else if (path.includes("/how-to-buy-bitcoin/"))
+  else if (path.includes("/rankings/exchanges/derivatives/")) {
+    presenceData.details =
+      "Looking at top cryptocurrency derivatives exchanges";
+  } else if (path.includes("/rankings/exchanges/dex/")) {
+    presenceData.details =
+      "Looking at top cryptocurrency decentralized exchanges";
+  } else if (path.includes("/how-to-buy-bitcoin/"))
     presenceData.details = "Checking out how to buy bitcoin";
   else if (path.includes("/nft/collections/"))
     presenceData.details = "Looking at NFT top collections";
@@ -44,13 +46,12 @@ presence.on("UpdateData", async () => {
     presenceData.state = document.querySelector(
       "#__next > div > div > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div > div > div:nth-child(2) > div > div.sc-1snuar3-1.hyjDAL > div.sc-1snuar3-2.gqljHA > h1 > span"
     ).textContent;
-  } else if (path.includes("/nft/")) 
-    presenceData.details = "Browsing NFTs...";
-   else if (path.includes("/watchlist/")) 
+  } else if (path.includes("/nft/")) presenceData.details = "Browsing NFTs...";
+  else if (path.includes("/watchlist/"))
     presenceData.details = "Browsing watchlist...";
-   else if (path.includes("/airdrop/")) 
+  else if (path.includes("/airdrop/"))
     presenceData.details = "Looking at Airdrop";
-   else if (path.includes("/referral/"))
+  else if (path.includes("/referral/"))
     presenceData.details = "Checking out the referral program";
   else if (path.includes("/api/"))
     presenceData.details = "Browsing API page...";
@@ -62,9 +63,10 @@ presence.on("UpdateData", async () => {
     presenceData.details = "Checking out new Cryptocurrencies";
   else if (path.includes("/charts/"))
     presenceData.details = "Viewing global cryptocurrency charts";
-  else if (path.includes("/best-cryptos/"))
-    presenceData.details = "Looking at the best cryptos based on the latest data";
-  else if (path.includes("/gainers-losers/"))
+  else if (path.includes("/best-cryptos/")) {
+    presenceData.details =
+      "Looking at the best cryptos based on the latest data";
+  } else if (path.includes("/gainers-losers/"))
     presenceData.details = "Checking top crypto gainers and losers today";
   else if (path.includes("/historical/"))
     presenceData.details = "Browsing cryptocurrency historical data snapshots";
@@ -84,8 +86,7 @@ presence.on("UpdateData", async () => {
     presenceData.details = "Reading the privacy and cookie policy";
   else if (path.includes("/disclaimer/"))
     presenceData.details = "Reading the disclaimer";
-  else if (path.includes("/faq/"))
-    presenceData.details = "Reading the FAQ";
+  else if (path.includes("/faq/")) presenceData.details = "Reading the FAQ";
   else if (path.includes("/alexandria/glossary"))
     presenceData.details = "Reading the glossary";
   else if (path.includes("/trending-cryptocurrencies/"))
