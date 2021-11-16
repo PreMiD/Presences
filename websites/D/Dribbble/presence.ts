cf83a76e11543bccd2a72f86f8497bdae8f1f928
Benjamin Vimeux <vimeux.benji@gmail.com>
2021-11-16 22:37:36 +0100

    Formatted code

--- websites/D/Dribbble/presence.ts
+++ websites/D/Dribbble/presence.ts
@@ -9,7 +9,6 @@ const presence = new Presence({
   path = document.location.pathname, //use just to get url profile
   browsingStamp = Math.floor(Date.now() / 1000);
 
-
 presence.on("UpdateData", async () => {
   // Starter datas Informations
   const presenceData: PresenceData = {
@@ -17,7 +16,7 @@ presence.on("UpdateData", async () => {
     largeImageKey: "dribbble",
     startTimestamp: browsingStamp
   };
-  
+
   // show diffent type project
   const shotsTypes = (type: string) => {
     if (document.location.pathname.includes("/animation")) {
@@ -37,61 +36,64 @@ presence.on("UpdateData", async () => {
     } else if (document.location.pathname.includes("/web-design")) {
       presenceData.state = `${type} Shots - Web Design`;
     } else presenceData.state = `All ${type} Shots`;
-  }
+  };
 
   //template of search, get name and type projet
   const search = (type: string) => {
     const searchTitle = document.querySelector(
       "body > div#wrap > div.search-header > div.search-results-details > h1"
-    )?.textContent
-    
+    )?.textContent;
+
     presenceData.details = `Search : "${searchTitle}"`;
     shotsTypes(type);
-  }
+  };
 
   // principal code
-  if (document.location.pathname.includes("/following")) { // if path contain a type...
-    if (document.location.pathname.includes("/search")) { // ...and 'search'
+  if (document.location.pathname.includes("/following")) {
+    // if path contain a type...
+    if (document.location.pathname.includes("/search")) {
+      // ...and 'search'
       search("following");
     } else {
       presenceData.details = "Viewing page :";
-      shotsTypes("following" );
+      shotsTypes("following");
     }
   } else if (document.location.pathname.includes("/popular")) {
     if (document.location.pathname.includes("/search")) {
       search("popular");
     } else {
       presenceData.details = "Viewing page :";
-      shotsTypes("popular" );
+      shotsTypes("popular");
     }
   } else if (document.location.pathname.includes("/recent")) {
     if (document.location.pathname.includes("/search")) {
       search("recent");
     } else {
       presenceData.details = "Viewing page :";
-      shotsTypes("recent" );
+      shotsTypes("recent");
     }
   } else if (document.location.pathname.includes("/search")) {
     search("");
-  } else if (document.body.id === 'profile') { // view a profile
+  } else if (document.body.id === "profile") {
+    // view a profile
     let profilePath = path;
-    let pathArray = profilePath.split('/');
+    let pathArray = profilePath.split("/");
     let profileName = pathArray[1];
     console.log(profileName);
     presenceData.details = "Viewing profile :";
     presenceData.state = `- ${profileName} -`;
   } else presenceData.details = "Homepage";
 
-  if (document.body.id === 'user-profile') { // view personal profile
+  if (document.body.id === "user-profile") {
+    // view personal profile
     let profilePath = path;
-    let pathArray = profilePath.split('/');
+    let pathArray = profilePath.split("/");
     let profileName = pathArray[1];
     console.log(profileName);
     presenceData.details = "Viewing personal profile :";
     presenceData.state = `- ${profileName} -`;
   }
-  
 
-if (!presenceData.details) presence.setActivity();
-else presence.setActivity(presenceData);
+  if (!presenceData.details) presence.setActivity();
+  else presence.setActivity(presenceData);
 });
