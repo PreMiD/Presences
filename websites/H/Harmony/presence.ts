ca9fff9374eb9b4061088ed4f2bee7dadb4a9dc6
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-11-17 22:43:43 +0000

    style(Harmony)

--- websites/H/Harmony/presence.ts
+++ websites/H/Harmony/presence.ts
@@ -3,15 +3,12 @@ const presence = new Presence({
   }),
   browseTimestamp = Math.floor(Date.now() / 1000);
 
-let npTitle: string,
-  npArtist: string,
-  npOnAir: string,
-  npListeners: number;
+let npTitle: string, npArtist: string, npOnAir: string, npListeners: number;
 
 function fetchStats(): void {
   fetch("https://staff.weareharmony.net/api/nowplaying")
-    .then(result => result.json())
-    .then(result => {
+    .then((result) => result.json())
+    .then((result) => {
       npTitle = result.song.title;
       npArtist = result.song.artist;
       npOnAir = result.onAir.name;
@@ -28,31 +25,37 @@ presence.on("UpdateData", async () => {
     smallImageKey: "live"
   };
 
-  if(document.location.hash.startsWith("#/")) {
+  if (document.location.hash.startsWith("#/")) {
     presenceData.startTimestamp = browseTimestamp;
     presenceData.smallImageText = `Listeners: ${npListeners} | Live DJ: ${npOnAir}`;
-    if(document.location.hash === "#/Home") {
+    if (document.location.hash === "#/Home") {
       presenceData.details = `🎶 ➜ ${npTitle}`;
       presenceData.state = `🎤 ➜ ${npArtist}`;
-    }else if(document.location.hash.startsWith("#/User")) {
-      const username = document.querySelector("body > div.page-container > div > div.p-container > p").textContent;
+    } else if (document.location.hash.startsWith("#/User")) {
+      const username = document.querySelector(
+        "body > div.page-container > div > div.p-container > p"
+      ).textContent;
       presenceData.details = `📰 ➜ Viewing user: ${username}`;
       presenceData.state = `💿 ➜ ${npOnAir}`;
-    }else if(document.location.hash.startsWith("#/Song")) {
+    } else if (document.location.hash.startsWith("#/Song")) {
       const songName = document.querySelector(".sp-title").textContent,
         songArtist = document.querySelector(".sp-artist").textContent;
       presenceData.details = `📰 ➜ Viewing song: ${songName}`;
       presenceData.state = `🎤 ➜ ${songArtist}`;
-    }else if(document.location.hash.startsWith("#/Library")) {
-      const libOwner = (document.querySelector(".mlib") || document.querySelector(".olib")).textContent;
+    } else if (document.location.hash.startsWith("#/Library")) {
+      const libOwner = (
+        document.querySelector(".mlib") || document.querySelector(".olib")
+      ).textContent;
       presenceData.details = `📰 ➜ Viewing page: ${libOwner}`;
       presenceData.state = `💿 ➜ ${npOnAir}`;
-    }else if(document.location.hash.startsWith("#/Special.Blank")) {
-      const usernameTop = document.querySelector("#navbar > ul.dropdown.ubox > div > h1").textContent;
+    } else if (document.location.hash.startsWith("#/Special.Blank")) {
+      const usernameTop = document.querySelector(
+        "#navbar > ul.dropdown.ubox > div > h1"
+      ).textContent;
       presenceData.details = `📰 ➜ ${usernameTop} got FUNKY TOWNED!`;
       presenceData.state = `💿 ➜ ${npOnAir}`;
-    }else {
-      const [ location ] = document.location.hash.slice(2).split("?");
+    } else {
+      const [location] = document.location.hash.slice(2).split("?");
       presenceData.details = `📰 ➜ Viewing page: ${location}`;
       presenceData.state = `💿 ➜ ${npOnAir}`;
     }
