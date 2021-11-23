969f6df6ec9ef31ad4513dd4eae10d46fa83eee7
Rhys <64903135+EncryptedDev@users.noreply.github.com>
2021-11-23 21:27:31 +0000

    style(Harmony): appropriate var name

--- websites/H/Harmony/presence.ts
+++ websites/H/Harmony/presence.ts
@@ -1,7 +1,7 @@
 const presence = new Presence({
     clientId: "882003722270572574"
   }),
-  browseTimestamp = Math.floor(Date.now() / 1000);
+  browsingTimestamp = Math.floor(Date.now() / 1000);
 
 let npTitle: string, npArtist: string, npOnAir: string, npListeners: number;
 
@@ -26,25 +26,41 @@ presence.on("UpdateData", async () => {
   };
 
   if (document.location.hash.startsWith("#/")) {
-    presenceData.startTimestamp = browseTimestamp;
+    presenceData.startTimestamp = browsingTimestamp;
     presenceData.smallImageText = `Listeners: ${npListeners} | Live DJ: ${npOnAir}`;
     if (document.location.hash === "#/Home") {
       presenceData.details = `🎶 ➜ ${npTitle}`;
       presenceData.state = `🎤 ➜ ${npArtist}`;
     } else if (document.location.hash.startsWith("#/User")) {
-      presenceData.details = `📰 ➜ Viewing user: ${document.querySelector("body > div.page-container > div > div.p-container > p").textContent}`;
+      presenceData.details = `📰 ➜ Viewing user: ${
+        document.querySelector(
+          "body > div.page-container > div > div.p-container > p"
+        ).textContent
+      }`;
       presenceData.state = `💿 ➜ ${npOnAir}`;
     } else if (document.location.hash.startsWith("#/Song")) {
-      presenceData.details = `📰 ➜ Viewing song: ${document.querySelector(".sp-title").textContent}`;
-      presenceData.state = `🎤 ➜ ${document.querySelector(".sp-artist").textContent}`;
+      presenceData.details = `📰 ➜ Viewing song: ${
+        document.querySelector(".sp-title").textContent
+      }`;
+      presenceData.state = `🎤 ➜ ${
+        document.querySelector(".sp-artist").textContent
+      }`;
     } else if (document.location.hash.startsWith("#/Library")) {
-      presenceData.details = `📰 ➜ Viewing page: ${(document.querySelector(".mlib") || document.querySelector(".olib")).textContent}`;
+      presenceData.details = `📰 ➜ Viewing page: ${
+        (document.querySelector(".mlib") || document.querySelector(".olib"))
+          .textContent
+      }`;
       presenceData.state = `💿 ➜ ${npOnAir}`;
     } else if (document.location.hash.startsWith("#/Special.Blank")) {
-      presenceData.details = `📰 ➜ ${document.querySelector("#navbar > ul.dropdown.ubox > div > h1").textContent} got FUNKY TOWNED!`;
+      presenceData.details = `📰 ➜ ${
+        document.querySelector("#navbar > ul.dropdown.ubox > div > h1")
+          .textContent
+      } got FUNKY TOWNED!`;
       presenceData.state = `💿 ➜ ${npOnAir}`;
     } else {
-      presenceData.details = `📰 ➜ Viewing page: ${document.location.hash.slice(2).split("?")[0]}`;
+      presenceData.details = `📰 ➜ Viewing page: ${
+        document.location.hash.slice(2).split("?")[0]
+      }`;
       presenceData.state = `💿 ➜ ${npOnAir}`;
     }
   }
