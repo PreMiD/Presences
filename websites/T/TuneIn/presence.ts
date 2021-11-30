c5bd6cde9728e839d676d107356613e0104dcce0
DarkVillager <darkville13@gmail.com>
2021-11-30 14:08:41 +0100

    Fixed TuneIn (Long time fix)
    Fixed TuneIn.

--- websites/T/TuneIn/presence.ts
+++ websites/T/TuneIn/presence.ts
@@ -10,87 +10,73 @@ const presence = new Presence({
 let title, author;
 
 presence.on("UpdateData", async () => {
-  const data: PresenceData = {
+  const presenceData: PresenceData = {
       largeImageKey: "logo",
       startTimestamp: Math.floor(Date.now() / 1000)
     },
-    error = document.querySelector(
-      "#innerAppContent > div.player__playerContainer___JEJ2U > div > div.player__leftSection___flqSC > div.player__leftErrorMessageContainer___10rm9 > div > div > div.player__errorMessageContainer___1dw8a"
-    );
-  if (error) presence.clearActivity();
-  else {
-    const playerCheck = document.querySelector(
-      ".player__playerContainer___JEJ2U"
-    )
+    playerCheck = document
+      .querySelector("#innerAppContent")
+      .querySelectorAll('[data-testid="player"]')
       ? true
       : false;
-    if (playerCheck) {
-      const liveCheck =
-        document.querySelector("#scrubberElapsed").textContent === "LIVE"
-          ? true
-          : false;
+  if (playerCheck) {
+    const liveCheck = document.querySelector("#scrubberElapsed");
+    if (!liveCheck) return presence.setActivity();
+    if (liveCheck.textContent === "LIVE") {
+      const playCheck = document
+        .querySelector("#innerAppContent")
+        .querySelectorAll('[data-testid="player-status-stopped"]')
+        ? true
+        : false;
+      if (playCheck) {
+        title = document.querySelector("#playerTitle").textContent;
+        author = document.querySelector("#playerSubtitle").textContent;
 
-      if (liveCheck) {
-        const playCheck = document.querySelector(
-          ".player-play-button__playerPlayButton___1Kc2Y[data-testid='player-status-playing']"
-        )
-          ? true
-          : false;
-        if (playCheck) {
-          title = document.querySelector("#playerTitle").textContent;
-          author = document.querySelector("#playerSubtitle").textContent;
+        presenceData.details = title;
+        if (title.length > 128)
+          presenceData.details = `${title.substring(0, 125)}...`;
 
-          data.details = title;
-          if (title.length > 128)
-            data.details = `${title.substring(0, 125)}...`;
+        presenceData.state = author;
+        if (author.length > 128)
+          presenceData.state = `${author.substring(0, 125)}...`;
 
-          data.state = author;
-          if (author.length > 128)
-            data.state = `${author.substring(0, 125)}...`;
-
-          data.smallImageKey = "live";
-          data.smallImageText = (await strings).live;
-        } else {
-          title = document.querySelector("#playerTitle").textContent;
-          author = document.querySelector("#playerSubtitle").textContent;
-          const audioTime =
-              document.querySelector("#scrubberElapsed").textContent,
-            audioDuration =
-              document.querySelector("#scrubberElapsed").innerHTML,
-            timestamp1 = presence.timestampFromFormat(audioTime),
-            timestamp2 = presence.timestampFromFormat(audioDuration),
-            timestamps = presence.getTimestamps(timestamp1, timestamp2),
-            paused = document.querySelector(
-              ".player-play-button__playerPlayButton___1Kc2Y[data-testid='player-status-stopped']"
-            )
-              ? true
-              : false;
-
-          data.details = title;
-          if (title.length > 128)
-            data.details = `${title.substring(0, 125)}...`;
-
-          data.state = author;
-          if (author.length > 128)
-            data.state = `${author.substring(0, 125)}...`;
-
-          data.smallImageKey = paused ? "pause" : "play";
-          data.smallImageText = paused
-            ? (await strings).pause
-            : (await strings).play;
-          data.endTimestamp = timestamps.pop();
-
-          if (paused) {
-            delete data.startTimestamp;
-            delete data.endTimestamp;
-          }
-
-          if (!data.details) {
-            presence.setTrayTitle();
-            presence.setActivity();
-          } else presence.setActivity(data);
-        }
+        presenceData.smallImageKey = "live";
+        presenceData.smallImageText = (await strings).live;
       }
-    } else presence.clearActivity();
-  }
+    } else {
+      title = document.querySelector("#playerTitle").textContent;
+      author = document.querySelector("#playerSubtitle").textContent;
+      const audioTime = document.querySelector("#scrubberElapsed").textContent,
+        audioDuration = document.querySelector("#scrubberDuration").textContent,
+        timestamp1 = presence.timestampFromFormat(audioTime),
+        timestamp2 = presence.timestampFromFormat(audioDuration),
+        timestamps = presence.getTimestamps(timestamp1, timestamp2),
+        paused = document
+          .querySelector("#innerAppContent")
+          .querySelectorAll('[data-testid="player-status-paused"]')[0]
+          ? true
+          : false;
+
+      presenceData.details = title;
+      if (title.length > 128)
+        presenceData.details = `${title.substring(0, 125)}...`;
+
+      presenceData.state = author;
+      if (author.length > 128)
+        presenceData.state = `${author.substring(0, 125)}...`;
+
+      presenceData.smallImageKey = paused ? "pause" : "play";
+      presenceData.smallImageText = paused
+        ? (await strings).pause
+        : (await strings).play;
+      presenceData.endTimestamp = timestamps.pop();
+
+      if (paused) {
+        delete presenceData.startTimestamp;
+        delete presenceData.endTimestamp;
+      }
+    }
+    if (!presenceData.details) presence.setActivity();
+    else presence.setActivity(presenceData);
+  } else presence.setActivity();
 });
