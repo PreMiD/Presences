c9c6a19203954338496ec999e50540d4fa2a8131
Connor <halil_ismail@yahoo.com>
2021-12-03 13:14:31 +0100

    Add ZeroTwo

---
+++ websites/Z/ZeroTwo/presence.ts
@@ -0,0 +1,31 @@
+const presence = new Presence({
+    clientId: "752067809214857298"
+  });
+  
+  presence.on("UpdateData", async () => {
+    const presenceData: PresenceData = {
+      largeImageKey: "logo"
+    };
+  
+    if ((await presence.getSetting("incognito")) === false) {
+      presenceData.details = document.getElementById("premid").innerText;
+      if ((await presence.getSetting("showTimestamp")) === true) {
+        presenceData.startTimestamp = Math.floor(Date.now() / 1000);
+      }
+    }
+    if ((await presence.getSetting("buttons")) === true) {
+      presenceData.buttons = [
+        {
+          label: 'Invite ZeroTwo',
+          url: 'https://zerotwo.wtf/invite'
+        }
+      ];
+    };
+    if (presenceData.details === null) {
+      presence.setTrayTitle();
+      presence.setActivity();
+    } else {
+      presence.setActivity(presenceData);
+    }
+  });
+  
