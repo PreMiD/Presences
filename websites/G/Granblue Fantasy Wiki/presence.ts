428074126464e83ace343c665d80f4a93ac8d08f
Nam Anh <77577746+kyrie25@users.noreply.github.com>
2021-12-04 07:06:08 +0700

    Add files via upload

--- websites/G/Granblue Fantasy Wiki/presence.ts
+++ websites/G/Granblue Fantasy Wiki/presence.ts
@@ -11,10 +11,37 @@ presence.on("UpdateData", () => {
   
   if (document.location.pathname === "/Main_Page")
     presenceData.details = "Viewing Wiki home page";
-  else if (document.querySelector(".firstHeading") !== null) {
-    presenceData.details = "Viewing page:";
+  else if (document.querySelector("#wpLoginAttempt"))
+    presenceData.details = "Logging in";
+  else if (document.querySelector("#wpCreateaccount"))
+    presenceData.details = "Creating an account";
+  else if (document.location.pathname === "/Character_Tier_List")
+    presenceData.details = "Viewing the character tier list";
+  else if (document.location.pathname === "/Collection_Tracker")
+    presenceData.details = "Making a collection tracker";
+  else if (document.querySelector(".searchresults")) {
+    presenceData.details = "Searching for:";
+    presenceData.state = (
+      document.querySelector("input[type=search]")as HTMLInputElement
+      ).value;
+    }
+  else if (document.location.href.indexOf ("/Special:Preferences") > -1) {
+    presenceData.details = "Editing preferences";
+  }
+  else if (document.location.href.indexOf ("/Special:Watchlist") > -1) {
+    presenceData.details = "Viewing watchlist";
+  }
+  else if (document.location.href.indexOf ("/Special:Contributions") > -1) {
+    presenceData.details = "Looking up contributions";
+  }
+  else if (document.location.href.indexOf("edit") > -1) {
+    presenceData.details = "Viewing revision history/Editing:";
     presenceData.state = document.querySelector(".firstHeading").textContent;
   }
-
+  else if (document.querySelector(".firstHeading") !== null) {
+    presenceData.details = "Viewing page:"; 
+    presenceData.state = document.querySelector(".firstHeading").textContent;
+  }
+  
   presence.setActivity(presenceData);
 });
