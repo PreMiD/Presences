cb78b34f6cf58b04582ccaac6f619bb8e8e9207e
André <39736248+andrefcdias@users.noreply.github.com>
2021-12-04 02:20:45 +0100

    Add video activity functionality Add icons to activity Use translations for icon hover

--- websites/F/FormulaOneTv/presence.ts
+++ websites/F/FormulaOneTv/presence.ts
@@ -1,54 +1,81 @@
-const presence = new Presence({
-  //The client ID of the Application created at https://discordapp.com/developers/applications
-  clientId: "916438450952097834"
-}),
-  //You can use this to get translated strings in their browser language
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
+const presence = new Presence({ clientId: "916438450952097834" })
+const strings = presence.getStrings({
+  play: "presence.playback.playing",
+  pause: "presence.playback.paused",
+  browsing: "general.browsing"
+})
 
-function getEpoch(): number {
+function getEpochInMs(): number {
   return Math.floor(+new Date() / 1000)
 }
 
-function updateActivity() {
-  //Grab and process all your data here
+function parseTimeToMilliseconds(length: string): number {
+  const [seconds, minutes, hours] = length.split(":").reverse().map(val => parseInt(val))
 
-
-  // element grabs //
-  // api calls //
-  // variable sets //
+  return seconds + ((minutes || 0) * 60) + ((hours || 0) * 3600)
 }
 
-setInterval(updateActivity, 10000);
-//Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
+let presenceData: PresenceData = {
+  largeImageKey: "logo_512",
+}
+
+async function setWatchingVideoActivity() {
+  const videoContainer = document.querySelector(".player-container.main");
+  const titleText = videoContainer.querySelector(".bmpui-label-metadata-title")?.textContent
+  presenceData.details = titleText ? `Watching ${titleText}` : "Watching a race"
+
+  // Video is playing
+  if (videoContainer.querySelector(".bmpui-ui-playbacktogglebutton")?.ariaPressed === "true") {
+    delete presenceData.state
+
+    presenceData.startTimestamp = getEpochInMs()
+    presenceData.smallImageKey = "play"
+    presenceData.smallImageText = (await strings).play
+
+    const [currentTime, videoLength] = videoContainer.querySelector(".bmpui-container-wrapper")
+      .getElementsByClassName("bmpui-ui-playbacktimelabel")
+
+    if (videoLength && currentTime) {
+      const lengthInMs = parseTimeToMilliseconds(videoLength?.textContent)
+      const currentTimeInMs = parseTimeToMilliseconds(currentTime?.textContent)
+
+      presenceData.endTimestamp = getEpochInMs() + lengthInMs - currentTimeInMs
+    }
+  }
+  // Video paused
+  else {
+    delete presenceData.startTimestamp
+    delete presenceData.endTimestamp
+
+    presenceData.state = "Video paused"
+    presenceData.smallImageKey = "pause"
+    presenceData.smallImageText = (await strings).pause
+  }
+}
+
+async function setBrowsingActivity() {
+  delete presenceData.state
+  delete presenceData.endTimestamp
+
+  presenceData.details = "Browsing..."
+  presenceData.startTimestamp = getEpochInMs()
+  presenceData.smallImageKey = "search"
+  presenceData.smallImageText = (await strings).browsing
+}
+
 
 presence.on("UpdateData", async () => {
-  /*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
+  // Watching video
+  if (document.location.href.includes("detail")) {
+    await setWatchingVideoActivity()
+  }
+  // Browsing
+  else {
+    await setBrowsingActivity()
+  }
 
-    It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
-
-  const presenceData: PresenceData = {
-    //The key (file name) of the Large Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets
-    largeImageKey: "logo_512",
-    //The key (file name) of the Small Image on the presence. These are uploaded and named in the Rich Presence section of your application, called Art Assets
-    // smallImageKey: "logo_512",
-    //The text which is displayed when hovering over the small image
-    // smallImageText: "Formula 1 TV",
-    //The upper section of the presence text
-    details: "Browsing main page",
-    //The lower section of the presence text
-    state: "",
-    //The unix epoch timestamp for when to start counting from
-    startTimestamp: getEpoch(),
-    //If you want to show Time Left instead of Elapsed, this is the unix epoch timestamp at which the timer ends
-    // endTimestamp: getEpoch()
-    //Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceData.type = "blahblah"; type examples: details, state, etc.
-  };
-  
-  //Update the presence with all the values from the presenceData object
-  if (presenceData.details) presence.setActivity(presenceData);
-  //Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name
-  else presence.setActivity();
-});
+  if (presenceData.details)
+    presence.setActivity(presenceData)
+  else
+    presence.setActivity()
+})
