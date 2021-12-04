35315ff6a6f2ff78b970170faca489a8a3267b6c
Mustafa "Pxsty" KÖK <75846829+pxsty0@users.noreply.github.com>
2021-12-04 11:07:08 +0300

    Create presence.ts

---
+++ websites/T/teknofest/presence.ts
@@ -0,0 +1,30 @@
+const presence = new Presence({
+  clientId: "903957474489548813"
+});
+
+presence.on("UpdateData", () => {
+  const presenceData: PresenceData = {
+      largeImageKey: "logo"
+    },
+    searchURL = new URL(document.location.href),
+    searchResult = searchURL.searchParams.get("q"),
+    searchCategory = searchURL.searchParams.get("k");
+  if (window.location.pathname.toLowerCase() === "/") {
+    presenceData.details = "Bir sayfa görüntülüyor:";
+    presenceData.state = "Anasayfa";
+  } else if (window.location.pathname.toLowerCase() === "/yarismalar.html") {
+    presenceData.details = "Bir sayfa görüntülüyor:";
+    presenceData.state = "Yarışmalar";
+  } else if (window.location.pathname.toLowerCase() === "/hakkimizda.php") {
+    presenceData.details = "Bir sayfa görüntülüyor:";
+    presenceData.state = "Hakkımızda";
+  } else if (window.location.pathname.toLowerCase() === "/v11tov12") {
+    presenceData.details = "Bir sayfa görüntülüyor:";
+    presenceData.state = "v11 To v12";
+  } 
+
+  if (!presenceData.details) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else presence.setActivity(presenceData);
+});
