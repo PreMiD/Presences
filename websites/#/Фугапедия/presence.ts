fdc7acff3ba68e0360e93759ce3eafb9b85dd113
Synbulat Biishev <syjalo.dev@gmail.com>
2021-12-08 13:47:32 +0500

    Add more cases

--- websites/#/Фугапедия/presence.ts
+++ websites/#/Фугапедия/presence.ts
@@ -4,15 +4,19 @@ const presence = new Presence({
   browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-  let presenceData: PresenceData = {
+  const presenceData: PresenceData = {
     largeImageKey: "logo",
     startTimestamp: browsingTimestamp
   }
 
   const path = document.location.pathname;
-  if (path === "/view") {
+  if (path === "/") {
+    presenceData.details = "На главной странице"
+  }
+  else if (path === "/view") {
     const articleTitle = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
-    presenceData.details = `Читает статью: ${articleTitle}`,
+    presenceData.details = "Читает статью";
+    presenceData.state = articleTitle;
     presenceData.buttons = [
       {
         label: "Перейти к статье",
@@ -20,9 +24,48 @@ presence.on("UpdateData", async () => {
       }
     ]
   }
+  else if (path === "/search") {
+    presenceData.details = "В поисках статьи";
+  }
+  else if (path === "/create") {
+    presenceData.details = "Создаёт статью";
+  }
+  else if (path === "/edit") {
+    const pageTitle = document.querySelector("title").textContent;
+    const articleTitle = pageTitle.slice(2, pageTitle.length - 12);
+    presenceData.details = `Редактирует статью`;
+    presenceData.state = articleTitle;
+  }
+  else if (path === "/portal") {
+    const portalName = document.querySelector("p.text-5xl.font-bold.text-black.place-self-center.ml-8").textContent;
+    presenceData.details = "В портале";
+    presenceData.state = portalName;
+    presenceData.buttons = [
+      {
+        label: "Войти в портал",
+        url: document.URL
+      }
+    ]
+  }
+  else if (path === "/books") {
+    presenceData.details = "В поисках книги";
+  }
+  else if (path === "/book") {
+    const bookName = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
+    presenceData.details = "Читает книгу";
+    presenceData.state = bookName;
+    presenceData.buttons = [
+      {
+        label: "Перейти к книге",
+        url: document.URL
+      }
+    ]
+  }
   else if (path === "/user") {
-    const username = document.querySelector("p.text-2xl.text-black.font-medium.ml-4.place-self-center").firstChild.textContent;
-    presenceData.details = `Смотрит профиль: ${username}`,
+    const nickname = document.querySelector("p.text-2xl.text-black.font-medium.ml-4.place-self-center").firstChild.textContent;
+    const name = document.querySelector("p.text-lg.text-gray-600.ml-4.place-self-center").textContent;
+    presenceData.details = "Смотрит профиль";
+    presenceData.state = `${nickname} (${name})`;
     presenceData.buttons = [
       {
         label: "Перейти к пользователю",
