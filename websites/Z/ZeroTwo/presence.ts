2c7005ab2b282cee5b15eb18a7124535caa38d97
Rhys <me@encrypteddev.com>
2021-12-17 22:29:09 +0000

    prettier

--- websites/Z/ZeroTwo/presence.ts
+++ websites/Z/ZeroTwo/presence.ts
@@ -2,18 +2,17 @@ const presence = new Presence({
     clientId: "752067809214857298"
   }),
   browsingTimestamp = Math.floor(Date.now() / 1000);
-  
-  presence.on("UpdateData", async () => {
-    const presenceData: PresenceData = {
-      largeImageKey: "logo"
-    };
-  
-    if ((await presence.getSetting("incognito")) === false) {
-      presenceData.details = document.getElementById("premid").innerText;
-      if ((await presence.getSetting("showTimestamp")) === true)
-        presenceData.startTimestamp = browsingTimestamp;
-    }
-    if (!presenceData.details) presence.setActivity();
-      else presence.setActivity(presenceData);
-    
-  });
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "logo"
+  };
+
+  if ((await presence.getSetting("incognito")) === false) {
+    presenceData.details = document.getElementById("premid").innerText;
+    if ((await presence.getSetting("showTimestamp")) === true)
+      presenceData.startTimestamp = browsingTimestamp;
+  }
+  if (!presenceData.details) presence.setActivity();
+  else presence.setActivity(presenceData);
+});
