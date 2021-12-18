e3879a9d1b3aff74158ef83db58cfa3901d8624e
Synbulat Biishev <syjalo.dev@gmail.com>
2021-12-18 19:42:40 +0500

    Update Фугапедия presence (#5038)
    * Add `/view_c` case
    
    * Add `/edits` case
    
    * Update `/portal` case
    
    * Add `#admintools` to `/account` case
    
    * Formatting
    
    * Add settings
    
    * Bump version
    
    * Optimization
    
    * Remove variables used only once

--- websites/#/Фугапедия/presence.ts
+++ websites/#/Фугапедия/presence.ts
@@ -3,79 +3,133 @@ const presence = new Presence({
   }),
   browsingTimestamp = Math.floor(Date.now() / 1000);
 
-presence.on("UpdateData", () => {
-  const presenceData: PresenceData = {
+presence.on("UpdateData", async () => {
+  const [
+      privacyMode,
+      showTime,
+      showButtons
+    ] = await Promise.all([
+      presence.getSetting("privacyMode"),
+      presence.getSetting("showTime"),
+      presence.getSetting("showButtons")
+    ]),
+    presenceData: PresenceData = {
       largeImageKey: "logo",
-      startTimestamp: browsingTimestamp
+      startTimestamp: showTime ? browsingTimestamp : null
     },
     path = document.location.pathname;
 
   if (path === "/") presenceData.details = "На главной странице";
   else if (path === "/view") {
-    const articleTitle = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
     presenceData.details = "Читает статью";
-    presenceData.state = articleTitle;
-    presenceData.buttons = [
-      {
-        label: "Перейти к статье",
-        url: document.URL
+    if (!privacyMode) {
+      // An article title
+      presenceData.state = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
+      if (showButtons) {
+        presenceData.buttons = [
+          {
+            label: "Перейти к статье",
+            url: document.URL
+          }
+        ];
       }
-    ];
+    }
+  } else if (path === "/view_c") {
+    presenceData.details = "Смотрит правку";
+    if (!privacyMode) {
+      // An edit title
+      presenceData.state = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
+      if (showButtons) {
+        presenceData.buttons = [
+          {
+            label: "Перейти к правке",
+            url: document.URL
+          }
+        ];
+      }
+    }
   } else if (path === "/category") {
-    const categoryName = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
     presenceData.details = "Смотрит категорию";
-    presenceData.state = categoryName;
-    presenceData.buttons = [
-      {
-        label: "Перейти в категорию",
-        url: document.URL
+    if (!privacyMode) {
+      // A category name
+      presenceData.state = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
+      if (showButtons) {
+        presenceData.buttons = [
+          {
+            label: "Перейти в категорию",
+            url: document.URL
+          }
+        ];
       }
-    ];
+    }
   } else if (path === "/search") presenceData.details = "В поисках статьи";
   else if (path === "/create") presenceData.details = "Создаёт статью";
   else if (path === "/edit") {
-    const pageTitle = document.querySelector("title").textContent,
-      articleTitle = pageTitle.slice(2, pageTitle.length - 12);
     presenceData.details = "Редактирует статью";
-    presenceData.state = articleTitle;
+    if (!privacyMode) {
+      const pageTitle = document.querySelector("title").textContent;
+      // An article title
+      presenceData.state = pageTitle.slice(2, pageTitle.length - 12);
+    }
+  } else if (path === "/edits") {
+    presenceData.details = "Смотрит список правок";
+    presenceData.state = "на всей вики";
   } else if (path === "/history") {
-    const pageTitle = document.querySelector("title").textContent,
-      articleTitle = pageTitle.slice(2, pageTitle.length - 12);
     presenceData.details = "Смотрит историю статьи";
-    presenceData.state = articleTitle;
+    if (!privacyMode) {
+      const pageTitle = document.querySelector("title").textContent;
+      // An article title
+      presenceData.state = pageTitle.slice(2, pageTitle.length - 12);
+    }
   } else if (path === "/portal") {
-    const portalName = document.querySelector("p.text-5xl.font-bold.text-black.place-self-center.ml-8").textContent;
     presenceData.details = "В портале";
-    presenceData.state = portalName;
-    presenceData.buttons = [
-      {
-        label: "Войти в портал",
-        url: document.URL
+    if (!privacyMode) {
+      // A portal name
+      presenceData.state = document.querySelector("p.text-4xl.font-semibold.text-white").textContent;
+      if (showButtons) {
+        presenceData.buttons = [
+          {
+            label: "Войти в портал",
+            url: document.URL
+          }
+        ];
       }
-    ];
+    }
   } else if (path === "/books") presenceData.details = "В поисках книги";
   else if (path === "/book") {
-    const bookName = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
     presenceData.details = "Читает книгу";
-    presenceData.state = bookName;
-    presenceData.buttons = [
-      {
-        label: "Перейти к книге",
-        url: document.URL
+    if (!privacyMode) {
+      // A book name
+      presenceData.state = document.querySelector("p.text-4xl.font-bold.font-3").textContent;
+      if (showButtons) {
+        presenceData.buttons = [
+          {
+            label: "Перейти к книге",
+            url: document.URL
+          }
+        ];
       }
-    ];
+    }
   } else if (path === "/book-create") presenceData.details = "Создаёт книгу";
   else if (path === "/user") {
-    const nickname = document.querySelector("p.text-2xl.text-black.font-medium.ml-4.place-self-center").firstChild.textContent,
-      name = document.querySelector("p.text-lg.text-gray-600.ml-4.place-self-center").textContent;
     presenceData.details = "Смотрит профиль";
-    presenceData.state = `${nickname} (${name})`;
-    presenceData.buttons = [
-      {
-        label: "Перейти к пользователю",
-        url: document.URL
+    if (!privacyMode) {
+      presenceData.state = `${
+        // A user nickname
+        document.querySelector("p.text-2xl.text-black.font-medium.ml-4.place-self-center").firstChild.textContent
+      } (${
+        // A user name
+        document.querySelector("p.text-lg.text-gray-600.ml-4.place-self-center").textContent
+      })`;
+      if (showButtons) {
+        presenceData.buttons = [
+          {
+            label: "Перейти к пользователю",
+            url: document.URL
+          }
+        ];
       }
-    ];
+    }
   } else if (path === "/privacypolicy") {
     presenceData.details = "Читает политику";
     presenceData.state = "конфиденциальности";
@@ -85,8 +139,10 @@ presence.on("UpdateData", () => {
   } else if (path === "/ad") {
     presenceData.details = "Читает условия";
     presenceData.state = "размещения рекламы";
-  } else if (path === "/account") presenceData.details = "Редактирует свой аккаунт";
-  else if (path === "/sub") presenceData.details = "Собирается купить подписку";
+  } else if (path === "/account") {
+    if (document.querySelector("#admintools")) presenceData.details = "Что-то делает в админ-панели";
+    else presenceData.details = "Редактирует свой аккаунт";
+  } else if (path === "/sub") presenceData.details = "Собирается купить подписку";
   else if (path === "/upload") presenceData.details = "Загружает изображение";
   else if (path === "/api.php") presenceData.details = "Читает документацию API";
   else presenceData.details = "Что-то делает на Фугапедии";
