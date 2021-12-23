d10d414d1844baf3ea670fe4a36a91c20784d0a1
joerkig <joeri@uitdelft.nl>
2021-12-23 16:12:20 +0100

    Cover as largeImageKey

--- websites/S/Score Saber/presence.ts
+++ websites/S/Score Saber/presence.ts
@@ -7,6 +7,7 @@ presence.on("UpdateData", async () => {
   const time = await presence.getSetting("time"),
     privacy = await presence.getSetting("privacy"),
     buttons = await presence.getSetting("buttons"),
+    cover = await presence.getSetting("cover"),
     presenceData: PresenceData = {
       largeImageKey: "logo",
       startTimestamp: browsingStamp
@@ -73,6 +74,7 @@ presence.on("UpdateData", async () => {
           url: document.location.href
         }
       ];
+      if (cover) presenceData.largeImageKey = document.querySelector<HTMLImageElement>("[class^='map-cover']").src
     } else presenceData.details = "Viewing Leaderboard";
   } else if (document.location.pathname.includes("/ranking/requests")) {
     presenceData.details = "Browsing Rank Requests";
