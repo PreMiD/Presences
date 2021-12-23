0119f4c882bf50a1d2709036ff15cb23a338f057
Bas950 <me@bas950.com>
2021-12-23 20:14:47 +0100

    fix(Qobuz): remove unneeded function

--- websites/Q/Qobuz/presence.ts
+++ websites/Q/Qobuz/presence.ts
@@ -14,107 +14,103 @@ async function getStrings() {
   );
 }
 
-(async () => {
-  let strings = await getStrings(),
-    oldLang: string = null;
+let strings: Awaited<ReturnType<typeof getStrings>> = null,
+  oldLang: string = null;
 
-  presence.on("UpdateData", async () => {
-    if (!document.querySelector("#root"))
-      return presence.setActivity({ largeImageKey: "logo" });
+presence.on("UpdateData", async () => {
+  if (!document.querySelector("#root"))
+    return presence.setActivity({ largeImageKey: "logo" });
 
-    const [newLang, timestamps, cover] = await Promise.all([
-      presence.getSetting("lang").catch(() => "en"),
-      presence.getSetting("timestamps"),
-      presence.getSetting("cover")
-    ]);
+  const [newLang, timestamps, cover] = await Promise.all([
+    presence.getSetting("lang").catch(() => "en"),
+    presence.getSetting("timestamps"),
+    presence.getSetting("cover")
+  ]);
 
-    oldLang ??= newLang;
-    if (oldLang !== newLang) {
-      oldLang = newLang;
-      strings = await getStrings();
-    }
+  if (oldLang !== newLang) {
+    oldLang = newLang;
+    strings = await getStrings();
+  }
 
-    const presenceData: PresenceData = {
-        largeImageKey: cover
-          ? document
-              .querySelector<HTMLImageElement>(
-                'div[class="player__track-cover"] img'
-              )
-              .src.replaceAll("230", "600")
-          : "logo"
-      },
-      songTitle = document.querySelector<HTMLAnchorElement>(
-        'a[class="player__track-name"]'
-      ),
-      fromPlaylist = !!document.querySelectorAll(
+  const presenceData: PresenceData = {
+      largeImageKey: cover
+        ? document
+            .querySelector<HTMLImageElement>(
+              'div[class="player__track-cover"] img'
+            )
+            .src.replaceAll("230", "600")
+        : "logo"
+    },
+    songTitle = document.querySelector<HTMLAnchorElement>(
+      'a[class="player__track-name"]'
+    ),
+    fromPlaylist = !!document.querySelectorAll(
+      'div[class="player__track-album"] a'
+    )[2],
+    currentTimeSec =
+      presence.timestampFromFormat(
+        document.querySelector<HTMLElement>(
+          'span[class="player__track-time-text"]'
+        ).textContent
+      ) * 1000,
+    endTimeSec =
+      presence.timestampFromFormat(
+        document.querySelectorAll<HTMLElement>(
+          'span[class="player__track-time-text"]'
+        )[1].textContent
+      ) * 1000,
+    paused = !!document.querySelector(
+      'span[class="player__action-play pct pct-player-play "] '
+    ),
+    elm = document.querySelector(".player__action-repeat.pct"),
+    obj = {
+      repeatType: elm.classList.contains("pct-repeat-once")
+        ? "loopTrack"
+        : elm.classList.contains("player__action-repeat--active")
+        ? "loopQueue"
+        : "deactivated",
+      songPlaylist: document.querySelectorAll(
         'div[class="player__track-album"] a'
-      )[2],
-      currentTimeSec =
-        presence.timestampFromFormat(
-          document.querySelector<HTMLElement>(
-            'span[class="player__track-time-text"]'
-          ).textContent
-        ) * 1000,
-      endTimeSec =
-        presence.timestampFromFormat(
-          document.querySelectorAll<HTMLElement>(
-            'span[class="player__track-time-text"]'
-          )[1].textContent
-        ) * 1000,
-      paused = !!document.querySelector(
-        'span[class="player__action-play pct pct-player-play "] '
-      ),
-      elm = document.querySelector(".player__action-repeat.pct"),
-      obj = {
-        repeatType: elm.classList.contains("pct-repeat-once")
-          ? "loopTrack"
-          : elm.classList.contains("player__action-repeat--active")
-          ? "loopQueue"
-          : "deactivated",
-        songPlaylist: document.querySelectorAll(
-          'div[class="player__track-album"] a'
-        )[2] as HTMLAnchorElement
-      };
+      )[2] as HTMLAnchorElement
+    };
 
-    let playliststring = "";
-    if (fromPlaylist)
-      playliststring = ` | From: ${obj.songPlaylist.textContent}`;
+  let playliststring = "";
+  if (fromPlaylist) playliststring = ` | From: ${obj.songPlaylist.textContent}`;
 
-    presenceData.details = songTitle.textContent;
-    presenceData.state =
-      document.querySelector('div[class="player__track-album"] > a')
-        .textContent + playliststring;
+  presenceData.details = songTitle.textContent;
+  presenceData.state =
+    document.querySelector('div[class="player__track-album"] > a').textContent +
+    playliststring;
 
-    if (currentTimeSec > 0 || !paused) {
-      presenceData.endTimestamp = Date.now() + (endTimeSec - currentTimeSec);
-      presenceData.smallImageKey = paused ? "pause" : "play";
-      presenceData.smallImageText = paused ? strings.pause : strings.play;
+  if (currentTimeSec > 0 || !paused) {
+    presenceData.endTimestamp = Date.now() + (endTimeSec - currentTimeSec);
+    presenceData.smallImageKey = paused ? "pause" : "play";
+    presenceData.smallImageText = paused ? strings.pause : strings.play;
+  }
+
+  if (paused || !timestamps) {
+    delete presenceData.startTimestamp;
+    delete presenceData.endTimestamp;
+  }
+
+  if (obj.repeatType !== "deactivated" && !paused) {
+    presenceData.smallImageKey =
+      obj.repeatType === "loopQueue" ? "repeat" : "repeat-one";
+    presenceData.smallImageText =
+      obj.repeatType === "loopQueue" ? "Repeat" : "Repeat Once";
+  }
+
+  presenceData.buttons = [
+    {
+      label: strings.viewAlbum,
+      url: songTitle.href
     }
-
-    if (paused || !timestamps) {
-      delete presenceData.startTimestamp;
-      delete presenceData.endTimestamp;
-    }
-
-    if (obj.repeatType !== "deactivated" && !paused) {
-      presenceData.smallImageKey =
-        obj.repeatType === "loopQueue" ? "repeat" : "repeat-one";
-      presenceData.smallImageText =
-        obj.repeatType === "loopQueue" ? "Repeat" : "Repeat Once";
-    }
-
-    presenceData.buttons = [
-      {
-        label: strings.viewAlbum,
-        url: songTitle.href
-      }
-    ];
-    if (fromPlaylist) {
-      presenceData.buttons.push({
-        label: strings.viewPlaylist,
-        url: obj.songPlaylist.href
-      });
-    }
-    presence.setActivity(presenceData);
-  });
-})();
+  ];
+  if (fromPlaylist) {
+    presenceData.buttons.push({
+      label: strings.viewPlaylist,
+      url: obj.songPlaylist.href
+    });
+  }
+  presence.setActivity(presenceData);
+});
