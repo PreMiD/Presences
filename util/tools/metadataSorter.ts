fa0f175aa3c27f0978e13cab00ee02c57eab55db
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-12-25 12:31:54 +0000

    fix(MS): correctly fetch latest metadata version

--- util/tools/metadataSorter.ts
+++ util/tools/metadataSorter.ts
@@ -6,6 +6,7 @@ import {
   writeFileSync as writeFile
 } from "node:fs";
 import { sync as glob } from "glob";
+import { latestMetadataSchema } from "./schemaUpdater";
 
 export function isValidJSON(text: string): boolean {
   try {
@@ -45,38 +46,41 @@ if (missingMetadata?.length > 0)
     } include a metadata file :\n${missingMetadata.join(", ")}\n`
   );
 
-for (const metadata of allmeta) {
-  if (metadata) {
-    const newData: Metadata = {
-      $schema: "https://schemas.premid.app/metadata/1.5",
-      author: metadata[0].author,
-      contributors: metadata[0].contributors,
-      service: metadata[0].service,
-      altnames: metadata[0].altnames,
-      description: metadata[0].description,
-      url: metadata[0].url,
-      regExp: metadata[0].regExp,
-      version: metadata[0].version,
-      logo: metadata[0].logo,
-      thumbnail: metadata[0].thumbnail,
-      color: metadata[0].color,
-      category: metadata[0].category,
-      tags: metadata[0].tags,
-      iframe: metadata[0].iframe,
-      iFrameRegExp: metadata[0].iFrameRegExp,
-      readLogs: metadata[0].readLogs,
-      settings: metadata[0].settings
-    };
+(async function () {
+  const latestSchema = await latestMetadataSchema()
+  console.log(latestSchema)
+  for (const metadata of allmeta) {
+    if (metadata) {
+      const newData: Metadata = {
+        $schema: latestSchema,
+        author: metadata[0].author,
+        contributors: metadata[0].contributors,
+        service: metadata[0].service,
+        altnames: metadata[0].altnames,
+        description: metadata[0].description,
+        url: metadata[0].url,
+        regExp: metadata[0].regExp,
+        version: metadata[0].version,
+        logo: metadata[0].logo,
+        thumbnail: metadata[0].thumbnail,
+        color: metadata[0].color,
+        category: metadata[0].category,
+        tags: metadata[0].tags,
+        iframe: metadata[0].iframe,
+        iFrameRegExp: metadata[0].iFrameRegExp,
+        readLogs: metadata[0].readLogs,
+        settings: metadata[0].settings
+      };
 
-    for (const key in newData) {
-      //@ts-ignore
-      if (typeof newData[key] === "undefined") delete newData[key];
+      for (const key in newData) {
+        if (typeof newData[key] === "undefined") delete newData[key];
+      }
+      write(metadata[1], newData);
     }
-    write(metadata[1], newData);
   }
-}
+})();
 
-export interface Metadata {
+export interface Metadata extends Record<string, any> {
   $schema: `https://schemas.premid.app/metadata/${number}.${number}`;
   author: Contributor;
   contributors?: Contributor[];
--- util/tools/schemaUpdater.ts
+++ util/tools/schemaUpdater.ts
@@ -23,9 +23,9 @@ function isValidJSON(text: string): boolean {
   }
 }
 
-const read = (path: string): string => readFile(path, { encoding: "utf8" }),
-  latestMetadataSchema = async (): Promise<string> => {
-    const versions = (
+export const read = (path: string): string => readFile(path, { encoding: "utf8" }),
+  latestMetadataSchema = async () => {
+    const latestVersion = (
       (
         await axios.get(
           "https://api.github.com/repos/PreMiD/Schemas/contents/schemas/metadata"
@@ -33,60 +33,57 @@ const read = (path: string): string => readFile(path, { encoding: "utf8" }),
       ).data as { name: string }[]
     )
       .filter(c => c.name.endsWith(".json"))
-      .map(c => c.name.match(/\d.\d/g)[0]);
-    return `https://schemas.premid.app/metadata/${versions.pop()}`;
+      .map(c => c.name.match(/\d.\d/g)[0]).pop() as `${number}.${number}`;
+    return `https://schemas.premid.app/metadata/${latestVersion}` as const;
   },
   write = (path: string, code: Metadata): void =>
     writeFile(path, JSON.stringify(code, null, 2), {
       encoding: "utf8",
       flag: "w"
+    });
+
+(async function () {
+  const missingMetadata: string[] = glob("./{websites,programs}/*/*/").filter(
+      pF => !exists(`${pF}/dist/metadata.json`)
+    ),
+    allmeta: Array<[Metadata, string]> = glob(
+      "./{websites,programs}/*/*/*/metadata.json"
+    ).map(pF => {
+      const file = read(pF);
+      if (isValidJSON(file)) return [JSON.parse(file), pF];
+      else {
+        console.error(`Error. ${pF} is not a valid metadata file, skipping...`);
+        return null;
+      }
     }),
-  main = async () => {
-    const missingMetadata: string[] = glob("./{websites,programs}/*/*/").filter(
-        pF => !exists(`${pF}/dist/metadata.json`)
-      ),
-      allmeta: Array<[Metadata, string]> = glob(
-        "./{websites,programs}/*/*/*/metadata.json"
-      ).map(pF => {
-        const file = read(pF);
-        if (isValidJSON(file)) return [JSON.parse(file), pF];
-        else {
-          console.error(
-            `Error. ${pF} is not a valid metadata file, skipping...`
-          );
-          return null;
-        }
-      }),
-      latestSchema = await latestMetadataSchema();
+    latestSchema = await latestMetadataSchema();
 
-    if (missingMetadata && missingMetadata.length > 0) {
-      console.log(
-        `\nThe following presence${
-          missingMetadata.length > 1 ? "s don't" : "doesn't"
-        } include a metadata file :\n${missingMetadata.join(", ")}\n`
-      );
-    }
+  if (missingMetadata && missingMetadata.length > 0) {
+    console.log(
+      `\nThe following presence${
+        missingMetadata.length > 1 ? "s don't" : "doesn't"
+      } include a metadata file :\n${missingMetadata.join(", ")}\n`
+    );
+  }
 
-    for (const metadata of allmeta) {
-      if (metadata) {
-        let newData = metadata[0];
-        if (newData.$schema) {
-          newData.$schema = latestSchema;
+  for (const metadata of allmeta) {
+    if (metadata) {
+      let newData = metadata[0];
+      if (newData.$schema) {
+        newData.$schema = latestSchema;
+        write(metadata[1], newData);
+      } else {
+        try {
+          newData = { ...{ $schema: latestSchema }, ...newData };
           write(metadata[1], newData);
-        } else {
-          try {
-            newData = { ...{ $schema: latestSchema }, ...newData };
-            write(metadata[1], newData);
-          } catch (err) {
-            console.log(err);
-            continue;
-          }
+        } catch (err) {
+          console.log(err);
+          continue;
         }
       }
     }
-  };
-
-main();
+  }
+});
 
 interface Metadata {
   $schema: string;
