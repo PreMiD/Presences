d2b360deccd11b9e601613cca9fa03c151d195e0
ACertainCoder <47316898+ACertainCoder@users.noreply.github.com>
2021-12-26 16:31:21 +0100

    feat(IFTTT): Complete recode

--- websites/I/IFTTT/presence.ts
+++ websites/I/IFTTT/presence.ts
@@ -8,270 +8,353 @@ const presence = new Presence({
   });
 
 presence.on("UpdateData", async () => {
-  const path = window.location.pathname.split("/").slice(1),
-    presenceData: PresenceData = {
-      details: "IFTTT",
-      largeImageKey: "logo_big"
-    };
+  const host = window.location.hostname.split("."),
+    path = window.location.pathname.split("/").slice(1);
 
-  switch (window.location.hostname.replace("www.", "")) {
-    //IFTTT URL Shortener (for the Help Center)
-    case "ift.tt":
-      presence.setActivity();
-      return;
-
-    //IFTTT for Business
-    case "platform.ifttt.com":
-      presenceData.details = "for Business";
-      if (path.length > 0) {
-        switch (path[0]) {
-          //Plans
-          case "plans":
-            presenceData.state = "Plans";
-            break;
-          //Documentation
-          case "docs":
-            presenceData.state = "Documentation";
-
-            if (document.querySelector("h1")) {
-              presenceData.smallImageKey = "reading";
-              presenceData.smallImageText = (await strings).reading;
-
-              presenceData.details = "Documentation";
-              presenceData.state = document.querySelector("h1").textContent;
-            } else {
-              presenceData.smallImageKey = "reading";
-              presenceData.smallImageText = (await strings).browsing;
-            }
-            break;
-          //Library
-          case "blog":
-            presenceData.state = "Library";
-
-            if (document.getElementsByClassName("story-title").length > 0) {
-              presenceData.smallImageKey = "reading";
-              presenceData.smallImageText = (await strings).reading;
-
-              presenceData.details = "Library";
-              presenceData.state =
-                document.getElementsByClassName("story-title")[0].textContent;
-            }
-            break;
-          //Case studies
-          case "case_studies":
-            presenceData.state = "Case studies";
-
-            if (document.getElementsByClassName("story-title").length > 0) {
-              presenceData.smallImageKey = "reading";
-              presenceData.smallImageText = (await strings).reading;
-
-              presenceData.details = "Case studies";
-              presenceData.state =
-                document.getElementsByClassName("story-title")[0].textContent;
-            }
-            break;
-          //Testimonials
-          case "testimonials":
-            presenceData.state = "Testimonials";
-            break;
-          //Contact
-          case "contact_sales":
-            presenceData.state = "Contact";
-            break;
-          //Terms of Use
-          case "terms":
-            presenceData.state = "Terms of Use";
-            break;
+  if (host[0] === "ift") {
+    // IFTTT URL Shortener (for the Help Center)
+    presence.setActivity();
+  } else if (host[0] === "help") {
+    // IFTTT Help Center
+    switch (path[0]) {
+      // Startpage
+      case "hc":
+        await handleHelpCenter(path);
+        break;
+      // Unknown
+      default:
+        presence.setActivity();
+        break;
+    }
+  } else if (host[0] === "platform") {
+    // IFTTT for Businesses / Developers
+    switch (path[0]) {
+      // Documentation
+      case "docs":
+        await handleDocs();
+        break;
+      // Developer spotlight
+      case "blog":
+        await handleCustom(
+          "Developer spotlight",
+          path[1]?.length > 0
+            ? document.querySelector<HTMLHeadingElement>("h1").textContent
+            : null
+        );
+        break;
+      // Solutions
+      case "solutions":
+        await handleCustom(
+          "Solutions",
+          path[1]?.length > 0
+            ? document.querySelector<HTMLHeadingElement>("h3").textContent
+            : null
+        );
+        break;
+      // Case studies
+      case "case_studies":
+        await handleCustom(
+          "Case studies",
+          path[1]?.length > 0
+            ? document.querySelector<HTMLHeadingElement>("h1").textContent
+            : null
+        );
+        break;
+      // Testimonials
+      case "testimonials":
+        await handleCustom("Testimonials");
+        break;
+      // Contact sales
+      case "contact_sales":
+        await handleCustom("Contact sales");
+        break;
+      // Startpage
+      // Unknown
+      default:
+        presence.setActivity();
+        break;
+    }
+  } else if (host[0] === "status") {
+    // IFTTT Status
+    switch (path[0]) {
+      case "incidents":
+        await handleCustom(
+          "IFTTT Status - Incident Report",
+          document.querySelector<HTMLDivElement>(".incident-name").textContent
+        );
+        break;
+      default:
+        await handleStatusPage();
+        break;
+    }
+  } else {
+    // Main page
+    switch (path[0]) {
+      // Applets
+      case "applets":
+        await handleApplet();
+        break;
+      // Account settings
+      case "settings":
+        await handleCustom("Account settings");
+        break;
+      // Billing
+      case "billing":
+        await handleCustom("Billing");
+        break;
+      // My Applets
+      case "home":
+      case "my_applets":
+        await handleCustom("My Applets");
+        break;
+      // Creating an Applet
+      case "create":
+        await handleAppletCreation();
+        break;
+      // Activity
+      case "activity":
+        await handleCustom("Activity");
+        break;
+      // My Services
+      case "date_and_time":
+      case "email":
+      case "email_digest":
+      case "ifttt":
+      case "feed":
+      case "space":
+      case "weather":
+      case "maker_webhooks":
+      case "my_services":
+        await handleMyServices(
+          document.querySelector<HTMLHeadingElement>("h1")?.textContent
+        );
+        break;
+      // Explore
+      // Blog entry
+      // Search
+      case "explore":
+      case "search":
+        if (
+          document.querySelector<HTMLDivElement>(".story-title")?.textContent
+        ) {
+          return await handleBlog(
+            document.querySelector<HTMLHeadingElement>("h1").textContent
+          );
         }
-      }
-      break;
 
-    //IFTTT Help Center
-    case "help.ifttt.com":
-      presenceData.details = "Help Center";
-      if (path.length > 2) {
-        switch (path[2]) {
-          //Articles
-          case "articles":
-            presenceData.smallImageKey = "reading";
-            presenceData.smallImageText = (await strings).reading;
-
-            presenceData.state = document.querySelector("h1").textContent;
-            break;
-          //Categories
-          case "categories":
-          case "sections":
-            presenceData.smallImageKey = "reading";
-            presenceData.smallImageText = (await strings).browsing;
-
-            presenceData.state = document.querySelector("h1").textContent;
-            break;
-          //Search
-          case "search":
-            presenceData.smallImageKey = "search";
-            presenceData.smallImageText = (await strings).search;
-
-            presenceData.state = `Searching for "${new URLSearchParams(
-              window.location.search
-            ).get("query")}"`;
-            break;
+        if (
+          document.querySelector<HTMLInputElement>("#search")?.value?.length > 0
+        ) {
+          return await handleSearch(
+            document.querySelector<HTMLInputElement>("#search").value
+          );
         }
-      }
-      break;
 
-    //IFTTT
-    default:
-    case "ifttt.com":
-      switch (path[0]) {
-        //Applets / Connections
-        case "applets":
-        case "connections":
-          presenceData.smallImageKey = "reading";
-          presenceData.smallImageText = (await strings).browsing;
+        await handleCustom("Exploring Applets & Services");
+        break;
+      // Plans
+      case "plans":
+        await handleCustom("Plans");
+        break;
+      // Blog
+      case "blog":
+        await handleBlog();
+        break;
+      // Developers
+      case "developers":
+        await handleCustom("Developers");
+        break;
+      // Contact
+      case "contact":
+        await handleCustom("Contact");
+        break;
+      // Trust & Privacy
+      case "terms":
+        await handleCustom("Privacy Policy & Terms of Use");
+        break;
+      // Careers
+      case "careers":
+        await handleCustom("Careers");
+        break;
+      // Startpage
+      // Services
+      // Unknown
+      default:
+        if (document.querySelector<HTMLDivElement>(".brand-section"))
+          return await handleSerivce();
 
-          presenceData.details =
-            document.getElementsByClassName("connection-title")[0].textContent;
-          presenceData.state = document
-            .getElementsByClassName("owner by-and-author-link")[0]
-            .textContent.replace("\n", " ");
-          break;
-        //Sign up
-        case "join":
-          presenceData.smallImageKey = "writing";
+        presence.setActivity();
+        break;
+    }
+  }
+});
 
-          presenceData.details = "Sign up";
-          break;
-        //Sign in
-        case "login":
-          presenceData.smallImageKey = "writing";
+/**
+ * Handle status page
+ */
+async function handleStatusPage(): Promise<void> {
+  let incidents: HTMLDivElement[];
 
-          presenceData.details = "Sign in";
-          break;
-        //Session
-        case "session":
-          if (path.length > 1) {
-            switch (path[1]) {
-              case "logout":
-                presenceData.smallImageKey = "writing";
+  try {
+    incidents = [
+      ...(document.querySelector<HTMLDivElement>(".unresolved-incidents")
+        .children as unknown as HTMLDivElement[])
+    ].filter(e => e.style.display !== "none");
+  } catch (e) {
+    incidents = [];
+  }
 
-                presenceData.details = "Sign out";
-                break;
-            }
-          } else {
-            presence.setActivity();
-            return;
-          }
-          break;
-        //Account settings
-        case "settings":
-        case "profile":
-          presenceData.details = "Settings";
-          if (path.length > 1) {
-            switch (path[1]) {
-              case "edit":
-                presenceData.state = "Edit profile";
-                break;
-              case "change_password":
-                presenceData.state = "Change password";
-                break;
-              case "export_my_data":
-                presenceData.state = "Export data";
-                break;
-              case "confirm_deletion":
-                presenceData.state = "Delete account";
-                break;
-            }
-          }
-          break;
-        //My Applets
-        case "my_applets":
-          presenceData.details = "My Applets";
-          break;
-        //Creating an Applet
-        case "create":
-          presenceData.details = "Creating an Applet";
-          if (document.getElementsByClassName("header").length > 0) {
-            presenceData.smallImageKey = "writing";
-            presenceData.smallImageText =
-              document.getElementsByClassName("user-step")[0].textContent;
+  presence.setActivity({
+    details: "IFTTT Status",
+    state:
+      incidents.length > 0 ? `Unresolved incidents: ${incidents.length}` : null,
+    largeImageKey: "logo_big"
+  });
+}
 
-            presenceData.state =
-              document.getElementsByClassName("header")[0].textContent;
-          }
-          break;
-        //Activity
-        case "activity":
-          presenceData.details = "Activity";
-          if (path.length > 1) {
-            switch (path[1]) {
-              case "service":
-                presenceData.state = document.querySelector("h1").textContent;
-                break;
-            }
-          }
-          break;
-        //My services
-        case "date_and_time":
-        case "email":
-        case "email_digest":
-        case "ifttt":
-        case "feed":
-        case "space":
-        case "weather":
-        case "maker_webhooks":
-          presenceData.details = "My Services";
-          presenceData.state = document.querySelector("h1").textContent;
-          break;
-        case "my_services":
-          presenceData.details = "My Services";
-          break;
-        //Connect services
-        case "connect":
-          presenceData.details = "My Services";
-          presenceData.state = `Connect ${
-            (document.querySelector("h1").firstElementChild as HTMLTitleElement)
-              .title
-          } to ${
-            (document.querySelector("h1").lastElementChild as HTMLTitleElement)
-              .title
-          }`;
-          break;
-        //Explore
-        case "discover":
-          presenceData.details = "Explore";
-          break;
-        //Search
-        case "search":
-          presenceData.smallImageKey = "search";
-          presenceData.smallImageText = (await strings).search;
+/**
+ * Handle help center pages
+ * @param path URL path
+ */
+async function handleHelpCenter(path: string[]): Promise<void> {
+  const presenceData: PresenceData = {
+    details: "Help Center",
+    largeImageKey: "logo_big"
+  };
 
-          presenceData.details = `Searching for "${
-            (document.getElementById("search") as HTMLInputElement).value
-          }"`;
-          presenceData.state = `Tab: ${
-            document.getElementsByClassName("active")[1].textContent
-          }`;
-          break;
-        //Trust & Privacy
-        case "terms":
-          presenceData.details = "Privacy Policy & Terms of Use";
-          break;
-        //Careers
-        case "careers":
-          presenceData.details = "Careers";
-          break;
-        //Unknown
-        default:
-          if (document.getElementsByClassName("brand-section").length > 0) {
-            presenceData.details = "Services";
-            presenceData.state = document.querySelector("h1").textContent;
-          } else {
-            presence.setActivity();
-            return;
-          }
-      }
-      break;
+  if (path.length > 2) {
+    switch (path[2]) {
+      // Articles
+      case "articles":
+        presenceData.state = "Article: ";
+        break;
+      // Categories
+      case "categories":
+        presenceData.state = "Category: ";
+        break;
+      // Sections
+      case "sections":
+        presenceData.state = "Section: ";
+        break;
+      // Unknown
+      default:
+        presenceData.state = "";
+        break;
+    }
+
+    const heading = document.querySelector<HTMLHeadingElement>("h1");
+    if (heading) presenceData.state += heading.textContent;
   }
 
   presence.setActivity(presenceData);
-});
+}
+
+/**
+ * Handle documentation
+ */
+async function handleDocs(): Promise<void> {
+  const chapter = document.querySelector<HTMLHeadingElement>("h1")?.textContent,
+    section =
+      document.querySelector<HTMLAnchorElement>("a.active")?.textContent;
+
+  presence.setActivity({
+    details: "Documentation",
+    state: chapter ? `${chapter}${section ? ` - ${section}` : ""}` : null,
+    largeImageKey: "logo_big",
+    smallImageText: (await strings).reading,
+    smallImageKey: "reading"
+  });
+}
+
+/**
+ * Handle applet page
+ */
+async function handleApplet(): Promise<void> {
+  presence.setActivity({
+    details: document.querySelector<HTMLHeadingElement>("h1").textContent,
+    state: `by ${
+      document.querySelector<HTMLSpanElement>(".author").textContent
+    }`,
+    largeImageKey: "logo_big",
+    smallImageText: (await strings).browsing,
+    smallImageKey: "reading"
+  });
+}
+
+/**
+ * Handle applet creation
+ */
+async function handleAppletCreation(): Promise<void> {
+  presence.setActivity({
+    details: "Creating an Applet",
+    state: document.querySelector<HTMLHeadingElement>("h1").textContent,
+    largeImageKey: "logo_big"
+  });
+}
+
+/**
+ * Handle service page
+ */
+async function handleSerivce(): Promise<void> {
+  presence.setActivity({
+    details: document.querySelector<HTMLHeadingElement>("h1").textContent,
+    state: document.querySelector<HTMLImageElement>(".large-service-logo")
+      .title,
+    largeImageKey: "logo_big",
+    smallImageText: (await strings).browsing,
+    smallImageKey: "reading"
+  });
+}
+
+/**
+ * Handle search page
+ * @param value Search value
+ */
+async function handleSearch(value: string): Promise<void> {
+  presence.setActivity({
+    details: "Searching for Applets & Services",
+    state: value,
+    largeImageKey: "logo_big",
+    smallImageText: (await strings).search,
+    smallImageKey: "search"
+  });
+}
+
+/**
+ * Handle blog pages
+ * @param entry Blog entry name
+ */
+async function handleBlog(entry?: string): Promise<void> {
+  presence.setActivity({
+    details: "Blog",
+    state: entry,
+    largeImageKey: "logo_big"
+  });
+}
+
+/**
+ * Handle my services pages
+ * @param category Service category
+ */
+async function handleMyServices(category?: string): Promise<void> {
+  presence.setActivity({
+    details: "My Services",
+    state: category,
+    largeImageKey: "logo_big",
+    smallImageText: (await strings).browsing,
+    smallImageKey: "reading"
+  });
+}
+
+/**
+ * Handle custom pages
+ * @param details Presence details text
+ * @param state Presence state text
+ */
+async function handleCustom(details: string, state?: string): Promise<void> {
+  presence.setActivity({
+    details,
+    state,
+    largeImageKey: "logo_big"
+  });
+}
