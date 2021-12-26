066564a455ea5937ff19423829533fb517616fa4
ACertainCoder <47316898+ACertainCoder@users.noreply.github.com>
2021-12-26 16:29:04 +0100

    feat(Radio.net): Complete recode

--- websites/R/Radio.net/presence.ts
+++ websites/R/Radio.net/presence.ts
@@ -8,180 +8,234 @@ const presence = new Presence({
     browsing: "presence.activity.browsing"
   });
 
-let lastPath = "", //Last played radio station or podcast
-  browsingTimestamp = 0; //Timestamp when started listening to a radio station
+let timestamp: number;
 
 presence.on("UpdateData", async () => {
-  //Current path
-  const path = window.location.pathname.split("/").slice(1),
-    //Presence data
-    presenceData: PresenceData = {
-      details: "Radio.net",
-      largeImageKey: "logo_big"
-    },
-    //Document title
-    { title } = document,
-    //Document header
-    header = document.querySelector("h1") as HTMLElement;
+  const host = window.location.hostname.split("."),
+    path = window.location.pathname.split("/").slice(1);
 
-  switch (path[0]) {
-    //Radio
-    //Podcast
-    case "s":
-    case "p": {
-      if (path[1] !== lastPath || browsingTimestamp === 0) {
-        browsingTimestamp = Math.round(Date.now() / 1000);
-        [, lastPath] = path;
-      }
-
-      //Player State
-      const status = document.querySelector<HTMLElement>(".player__info-wrap");
-
-      if (
-        document.querySelector<HTMLElement>(".player__animate-icon").style
-          .display !== "none"
-      ) {
-        //Playing
-        presenceData.details = header.textContent;
-        presenceData.state =
-          document.querySelector<HTMLElement>("div.player__song").textContent;
-
-        presenceData.smallImageText = (await strings).play;
-        presenceData.smallImageKey = "play";
-
-        presenceData.startTimestamp = browsingTimestamp;
-
-        //Get the podcast play position
-        if (path[0] === "p") {
-          presenceData.endTimestamp = browsingTimestamp;
-
-          //Get the start / current position
-          const [start, end] = document
-            .querySelector<HTMLElement>(".player__timing-wrap")
-            .textContent.split("|")
-            .map(e => e.split(":").reverse());
-
-          //Add the amount of time the podcast has been playing
-          if (start.length > 0)
-            presenceData.startTimestamp += parseInt(start[0]);
-
-          if (start.length > 1)
-            presenceData.startTimestamp += parseInt(start[1]) * 60;
-
-          if (start.length > 2)
-            presenceData.startTimestamp += parseInt(start[2]) * 60 * 24;
-
-          //Add the length of the podcast
-          if (end.length > 0) presenceData.endTimestamp += parseInt(end[0]);
-
-          if (end.length > 1)
-            presenceData.endTimestamp += parseInt(end[1]) * 60;
-
-          if (end.length > 2)
-            presenceData.endTimestamp += parseInt(end[2]) * 60 * 24;
-        }
-      } else {
-        //Paused
-        browsingTimestamp = 0;
-
-        presenceData.details = header.textContent;
-
-        presenceData.smallImageText = (await strings).pause;
-        presenceData.smallImageKey = "pause";
-
-        if (status.style.display !== "none") {
-          //Player status is being displayed (example: BUFFERING)
-          const adlength = status.textContent.match(/\d+/g)
-            ? parseInt(status.textContent.match(/\d+/g)[0])
-            : 0;
-
-          if (adlength > 0) {
-            presenceData.state = "Currently watching an ad";
-
-            presenceData.startTimestamp = Math.floor(Date.now() / 1000);
-            presenceData.endTimestamp = presenceData.startTimestamp + adlength;
-          } else presenceData.state = status.textContent;
-        } else {
-          //Player is inactive (no status is being displayed)
-          presenceData.state =
-            document.querySelector<HTMLElement>(".z7kxsz-11").textContent;
-        }
-      }
-      break;
+  if (host[0] === "corporate") {
+    // Corporate page
+    switch (path[0]) {
+      // About us
+      // Broadcasters
+      // Advertising
+      // Press
+      // Jobs
+      // Contact
+      case "about-us":
+      case "ueber-uns":
+      case "broadcasters":
+      case "sender":
+      case "advertising":
+      case "werbung":
+      case "press":
+      case "presse":
+      case "jobs":
+      case "contact":
+      case "kontakt":
+        await handleCorporate(host.slice(1).join("."));
+        break;
+      default:
+        presence.setActivity();
+        break;
     }
-    //Search
-    case "search": {
-      browsingTimestamp = 0;
-
-      presenceData.details = new URLSearchParams(window.location.search).get(
-        "q"
-      );
-      presenceData.state = `${header.textContent.match(/\d+/g)[0]} results`;
-
-      presenceData.smallImageKey = "search";
-      presenceData.smallImageText = (await strings).search;
-      break;
+  } else {
+    // Main page
+    switch (path[0]) {
+      // Radio station
+      case "s":
+        await handleStation();
+        break;
+      // Podcast
+      case "p":
+        await handlePodcast();
+        break;
+      // Search
+      case "search":
+        await handleSearch();
+        break;
+      // Genre
+      // Topic
+      // Country
+      // City
+      // Local stations
+      // Top stations
+      case "genre":
+      case "topic":
+      case "country":
+      case "city":
+      case "local-stations":
+      case "top-stations":
+        await handleListing();
+        break;
+      // Choose your country
+      // Contact
+      // App
+      // Terms and conditions
+      // Privacy policy
+      // Imprint
+      case "country-selector":
+      case "contact":
+      case "app":
+      case "terms-and-conditions":
+      case "privacy-policy":
+      case "imprint":
+        await handleGeneric(true);
+        break;
+      // Startpage
+      // Unknown
+      default:
+        presence.setActivity();
+        break;
     }
-    //Genre
-    //Topic
-    //Country
-    //City
-    //Local Stations
-    //Top 100 Stations
-    case "genre":
-    case "topic":
-    case "country":
-    case "city":
-    case "local-stations":
-    case "top-stations": {
-      browsingTimestamp = 0;
+  }
+});
 
-      presenceData.details = header.textContent;
+/**
+ * Handle radio station
+ */
+async function handleStation(): Promise<void> {
+  let presenceData: PresenceData;
 
-      presenceData.smallImageKey = "reading";
-      presenceData.smallImageText = (await strings).browsing;
-      break;
-    }
-    //My Profile
-    //Recently Played
-    //My Favorites
-    //Terms and Conditions
-    //Privacy Policy
-    //Imprint
-    //Contact
-    case "profile":
-    case "recents":
-    case "favorites":
-    case "terms-and-conditions":
-    case "privacy-policy":
-    case "imprint":
-    case "contact": {
-      browsingTimestamp = 0;
+  // Check if the animation icon is shown
+  if (
+    document.querySelector<HTMLElement>(".player__animate-icon").style
+      .display !== "none"
+  ) {
+    // Radio is playing / buffering
+    if (timestamp === 0) timestamp = Date.now();
 
-      presenceData.details = title;
-      break;
-    }
-    //iPhone App
-    //iPad App
-    //Android App
-    //Windows App
-    //Blackberry App
-    case "iphone":
-    case "ipad":
-    case "android":
-    case "windowsphone":
-    case "blackberry": {
-      browsingTimestamp = 0;
+    presenceData = {
+      details: document.querySelector("h1").textContent,
+      state: document.querySelector<HTMLElement>(".player__song").textContent,
+      largeImageKey: (
+        document.querySelector<HTMLElement>("#station").children[3].children[1]
+          .firstChild.firstChild.firstChild as HTMLImageElement
+      ).src,
+      smallImageText: (await strings).play,
+      smallImageKey: "play",
+      startTimestamp: timestamp
+    };
+  } else {
+    // Radio is paused
+    timestamp = 0;
 
-      presenceData.details = title;
-      break;
-    }
-    //Unknown
-    default: {
-      presence.setActivity();
-      return;
-    }
+    presenceData = {
+      details: document.querySelector("h1").textContent,
+      largeImageKey: (
+        document.querySelector<HTMLElement>("#station").children[3].children[1]
+          .firstChild.firstChild.firstChild as HTMLImageElement
+      ).src,
+      smallImageText: (await strings).pause,
+      smallImageKey: "pause"
+    };
   }
 
   presence.setActivity(presenceData);
-});
+}
+
+/**
+ * Handle podcast
+ */
+async function handlePodcast(): Promise<void> {
+  let presenceData: PresenceData;
+
+  // Check if the animation icon is shown
+  if (
+    document.querySelector<HTMLElement>(".player__animate-icon").style
+      .display !== "none"
+  ) {
+    // Podcast is playing / buffering
+    const times = document
+        .querySelector<HTMLElement>(".player__timing-wrap")
+        .textContent.split("|"),
+      timestamps = presence.getTimestamps(
+        presence.timestampFromFormat(times[0]),
+        presence.timestampFromFormat(times[1])
+      );
+
+    presenceData = {
+      details: document.querySelector("h1").textContent,
+      state: document.querySelector<HTMLElement>(".player__song").textContent,
+      largeImageKey: (
+        document.querySelector<HTMLElement>("#podcast").children[1].children[1]
+          .firstChild.firstChild.firstChild as HTMLImageElement
+      ).src,
+      smallImageText: (await strings).play,
+      smallImageKey: "play",
+      startTimestamp: timestamps[0],
+      endTimestamp: timestamps[1]
+    };
+  } else {
+    // Podcast is paused
+    presenceData = {
+      details: document.querySelector("h1").textContent,
+      state: document.querySelector<HTMLElement>(".player__song").textContent,
+      largeImageKey: (
+        document.querySelector<HTMLElement>("#podcast").children[1].children[1]
+          .firstChild.firstChild.firstChild as HTMLImageElement
+      ).src,
+      smallImageText: (await strings).pause,
+      smallImageKey: "pause"
+    };
+  }
+
+  presence.setActivity(presenceData);
+}
+
+/**
+ * Handle search
+ */
+async function handleSearch(): Promise<void> {
+  const presenceData: PresenceData = {
+    details: new URLSearchParams(window.location.search).get("q"),
+    state: document.querySelector<HTMLElement>("h1").textContent,
+    largeImageKey: "logo_big",
+    smallImageText: (await strings).search,
+    smallImageKey: "search"
+  };
+
+  presence.setActivity(presenceData);
+}
+
+/**
+ * Handle radio station or podcast listing
+ */
+async function handleListing(): Promise<void> {
+  presence.setActivity({
+    details: document.querySelector<HTMLElement>("h1").textContent,
+    largeImageKey: "logo_big",
+    smallImageText: (await strings).browsing,
+    smallImageKey: "reading"
+  });
+}
+
+/**
+ * Handle corporate page
+ * @param hostname Hostname of the corporate page
+ */
+async function handleCorporate(hostname: string): Promise<void> {
+  const item: string =
+    document.querySelector<HTMLElement>(".current_page_item").textContent;
+
+  presence.setActivity({
+    details: `${hostname} corporate`,
+    state: item.charAt(0).toUpperCase() + item.slice(1).toLowerCase(),
+    largeImageKey: "logo_big"
+  });
+}
+
+/**
+ * Handle generic pages
+ * @param preferTitle Prefer document title over h1 text content
+ */
+async function handleGeneric(preferTitle = false): Promise<void> {
+  presence.setActivity({
+    details: preferTitle
+      ? document.title
+      : document.querySelector<HTMLHeadingElement>("h1")?.textContent ||
+        document.title,
+    largeImageKey: "logo_big"
+  });
+}
