84969efb14c96c25f315af3f3f0debb9168e0761
Dustin Lontzek <18153513+CrypticXT@users.noreply.github.com>
2021-12-27 21:43:37 +0100

    Added paypal presence

--- websites/P/PayPal/presence.ts
+++ websites/P/PayPal/presence.ts
@@ -11,40 +11,44 @@ presence.on("UpdateData", async () => {
 
   if (document.location.pathname === "/myaccount/summary") {
     presenceData.details = "Viewing:";
-    presenceData.state = "Dashboard";}
-
-  else if (document.location.pathname === ("/myaccount/transfer/homepage")) {
+    presenceData.state = "Dashboard";
+  } else if (document.location.pathname === "/myaccount/transfer/homepage") {
     presenceData.details = "Viewing:";
-    presenceData.state = "Send Money";}
-  else if (document.location.pathname.includes("/myaccount/money/")) {
+    presenceData.state = "Send Money";
+  } else if (document.location.pathname.includes("/myaccount/money/")) {
     presenceData.details = "Viewing:";
-    presenceData.state = "Wallet";}
-  else if (document.location.pathname === ("/myaccount/transfer/homepage/request")) {
+    presenceData.state = "Wallet";
+  } else if (
+    document.location.pathname === "/myaccount/transfer/homepage/request"
+  ) {
     presenceData.details = "About to:";
-    presenceData.state = "Request money";}
-  else if (document.location.pathname === ("/myaccount/transactions/")){
+    presenceData.state = "Request money";
+  } else if (document.location.pathname === "/myaccount/transactions/") {
     presenceData.details = "Viewing:";
     presenceData.state = "Activity";
-    if (document.querySelector(".chevron-rotation")){
+    if (document.querySelector(".chevron-rotation")) {
       presenceData.details = "Inspecting:";
-      presenceData.state = "Transaction";}
-      }
-  else if (document.location.pathname.includes("/smarthelp/home")) {
+      presenceData.state = "Transaction";
+    }
+  } else if (document.location.pathname.includes("/smarthelp/home")) {
     presenceData.details = "Viewing:";
-    presenceData.state = "Help";}
-  else if (document.location.pathname === "/myaccount/transfer/homepage/send/preview" || document.location.pathname === ("/myaccount/transfer/homepage/buy/preview"))
-  {
+    presenceData.state = "Help";
+  } else if (
+    document.location.pathname ===
+      "/myaccount/transfer/homepage/send/preview" ||
+    document.location.pathname === "/myaccount/transfer/homepage/buy/preview"
+  ) {
     presenceData.details = "About to:";
     presenceData.state = "Send money";
-  }
-  else if (document.location.pathname.includes("/myaccount/settings/")) {
+  } else if (document.location.pathname.includes("/myaccount/settings/")) {
     presenceData.details = "Viewing:";
-    presenceData.state = "Settings"}
-  else if (document.location.pathname.includes("/myaccount/transactions/details")){
+    presenceData.state = "Settings";
+  } else if (
+    document.location.pathname.includes("/myaccount/transactions/details")
+  ) {
     presenceData.details = "Viewing:";
-    presenceData.state = "Transaction"}
-  else presenceData.details = "Browsing..."
-
+    presenceData.state = "Transaction";
+  } else presenceData.details = "Browsing...";
 
   if (presenceData.details) presence.setActivity(presenceData);
   else presence.setActivity();
