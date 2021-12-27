eb5d3721170221661d34fde466399d6976ddbcd6
ACertainCoder <47316898+ACertainCoder@users.noreply.github.com>
2021-12-27 18:01:18 +0100

    Handle all pages in one function

--- websites/M/My Nintendo/presence.ts
+++ websites/M/My Nintendo/presence.ts
@@ -5,45 +5,104 @@ const presence = new Presence({
 let strings: Awaited<ReturnType<typeof getStrings>>;
 
 presence.on("UpdateData", async () => {
-  const path = window.location.pathname.split("/").slice(1);
+  const path = window.location.pathname.split("/").slice(1),
+    presenceData: PresenceData = {};
 
   strings = await getStrings();
 
   switch (path[0]) {
     // Reward Categories
     case "reward_categories":
-      handleRewardCategories(path);
+      presenceData.details =
+        document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
+        document.title;
+      presenceData.largeImageKey = "logo_big";
+      presenceData.smallImageText = strings.browsing;
+      presenceData.smallImageKey = "reading";
+
+      if (path.length > 1) {
+        presenceData.state = document.querySelector<HTMLHeadingElement>(
+          ".PageSubHeader_title"
+        )?.textContent;
+      }
       break;
     // Rewards
     case "rewards":
-      handleRewards(path);
+      presenceData.details =
+        document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
+        document.title;
+      presenceData.largeImageKey = "logo_big";
+      presenceData.smallImageText = strings.browsing;
+      presenceData.smallImageKey = "reading";
+
+      if (path.length > 1) {
+        presenceData.state = document.querySelector<HTMLHeadingElement>(
+          ".RewardHeader_title"
+        ).textContent;
+      }
       break;
     // Missions
     case "missions":
-      handleGeneric(false, "reading");
+      presenceData.details =
+        document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
+        document.title;
+      presenceData.largeImageKey = "logo_big";
+
+      presenceData.smallImageKey = strings.reading;
+      presenceData.smallImageText = "reading";
       break;
     // Points
     case "point":
-      handlePoints(path);
+      if (path.length < 2) return presence.setActivity();
+
+      presenceData.details =
+        document.querySelector<HTMLHeadingElement>("h1").textContent;
+      presenceData.state =
+        document.querySelector<HTMLHeadingElement>("h2")?.textContent;
+      presenceData.largeImageKey = "logo_big";
       break;
     // News
     case "news":
-      handleNews(path);
+      presenceData.details =
+        document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
+        document.title;
+      presenceData.largeImageKey = "logo_big";
+      presenceData.smallImageText = strings.reading;
+      presenceData.smallImageKey = "reading";
+
+      if (path.length > 1) {
+        presenceData.state =
+          document.querySelector<HTMLHeadingElement>(
+            ".NewsDetail_title"
+          )?.textContent;
+      } else {
+        presenceData.state = document.querySelector<HTMLHeadingElement>(
+          ".PageSubHeader_title"
+        )?.textContent;
+      }
       break;
     // Redeem Point Codes
     case "serial_number":
-      handleGeneric();
+      presenceData.details =
+        document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
+        document.title;
+      presenceData.largeImageKey = "logo_big";
       break;
     // Getting Started, About Points, About Gold Points
     case "getting_started":
     case "about_point":
     case "about_gold_point":
-      handleGeneric(false, "reading");
+      presenceData.details =
+        document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
+        document.title;
+      presenceData.largeImageKey = "logo_big";
+
+      presenceData.smallImageKey = strings.reading;
+      presenceData.smallImageText = "reading";
       break;
     // Startpage, Unknown
     default:
-      presence.setActivity();
-      break;
+      return presence.setActivity();
   }
 });
 
@@ -55,116 +114,4 @@ async function getStrings() {
     },
     await presence.getSetting<string>("lang").catch(() => "en")
   );
-}
-
-/**
- * Handle reward category pages
- * @param path URL path
- */
-function handleRewardCategories(path: string[]): void {
-  const presenceData: PresenceData = {
-    details:
-      document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
-      document.title,
-    largeImageKey: "logo_big",
-    smallImageText: strings.browsing,
-    smallImageKey: "reading"
-  };
-
-  if (path.length > 1) {
-    presenceData.state = document.querySelector<HTMLHeadingElement>(
-      ".PageSubHeader_title"
-    )?.textContent;
-  }
-
-  presence.setActivity(presenceData);
-}
-
-/**
- * Handle reward page
- * @param path URL path
- */
-function handleRewards(path: string[]): void {
-  const presenceData: PresenceData = {
-    details:
-      document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
-      document.title,
-    largeImageKey: "logo_big",
-    smallImageText: strings.browsing,
-    smallImageKey: "reading"
-  };
-
-  if (path.length > 1) {
-    presenceData.state = document.querySelector<HTMLHeadingElement>(
-      ".RewardHeader_title"
-    ).textContent;
-  }
-
-  presence.setActivity(presenceData);
-}
-
-/**
- * Handle points page
- * @param path URL path
- */
-function handlePoints(path: string[]): void {
-  if (path.length < 2) return presence.setActivity();
-
-  presence.setActivity({
-    details: document.querySelector<HTMLHeadingElement>("h1").textContent,
-    state: document.querySelector<HTMLHeadingElement>("h2")?.textContent,
-    largeImageKey: "logo_big"
-  });
-}
-
-/**
- * Handle news pages
- * @param path URL path
- */
-function handleNews(path: string[]): void {
-  const presenceData: PresenceData = {
-    details:
-      document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
-      document.title,
-    largeImageKey: "logo_big",
-    smallImageText: strings.reading,
-    smallImageKey: "reading"
-  };
-
-  if (path.length > 1) {
-    presenceData.state =
-      document.querySelector<HTMLHeadingElement>(
-        ".NewsDetail_title"
-      )?.textContent;
-  } else {
-    presenceData.state = document.querySelector<HTMLHeadingElement>(
-      ".PageSubHeader_title"
-    )?.textContent;
-  }
-
-  presence.setActivity(presenceData);
-}
-
-/**
- * Handle generic pages
- * @param preferTitle Prefer document title over h1 text content
- */
-function handleGeneric(
-  preferTitle = false,
-  action: "none" | "reading" | "browsing" = "none"
-): void {
-  const presenceData: PresenceData = {
-    details: preferTitle
-      ? document.title
-      : document.querySelector<HTMLHeadingElement>("h1")?.textContent ??
-        document.title,
-    largeImageKey: "logo_big"
-  };
-
-  if (action === "reading" || action === "browsing") {
-    presenceData.smallImageKey = strings[action];
-    presenceData.smallImageText = action;
-  }
-
-  presence.setActivity(presenceData);
 }
