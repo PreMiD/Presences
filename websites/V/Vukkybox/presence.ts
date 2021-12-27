e129115bdc613fd47ba71ebd272937af7e052058
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-12-27 22:23:40 +0100

    Update presence.ts

--- websites/V/Vukkybox/presence.ts
+++ websites/V/Vukkybox/presence.ts
@@ -20,10 +20,11 @@ presence.on("UpdateData", async () => {
     presenceData.state = `Unlocked ${document
       .getElementById("total_text")
       .childNodes[3].textContent.substring(14, 21)} Vukkies!`;
-  } else if (document.location.pathname === "/") {
-    presenceData.details = "Browsing the homepage";
   } else if (document.location.pathname === "/store") {
     presenceData.details = "Browsing the store";
+    if(document.getElementById("balance")) presenceData.state = `${
+      document.getElementById("balance").innerText
+    } Vukkybux to spend!`;
   } else if (document.location.pathname === "/buyBox/classic") {
     presenceData.details = "Opening a Classic Box";
     presenceData.state = `${
@@ -54,8 +55,11 @@ presence.on("UpdateData", async () => {
     presenceData.state = `${
       document.getElementById("balance").innerText
     } Vukkybux left!`;
-  } else if (document.location.pathname === "/profile") {
-    presenceData.details = "Changing profile settings";
+  } else if (document.location.pathname === "/buyBox/beggars") {
+    presenceData.details = "Opening a Beggar's Box";
+    presenceData.state = `${
+      document.getElementById("balance").innerText
+    } Vukkybux left!`;
   } else if (document.location.pathname.includes("/redeem")) {
     presenceData.details = "Redeeming a coupon!";
     if (
@@ -64,9 +68,9 @@ presence.on("UpdateData", async () => {
     )
       presenceData.state = "Yay, it's a valid coupon!";
     else presenceData.state = "Uh oh, the coupon is invalid!";
-  } else if (document.location.pathname === "/login") {
-    presenceData.details = "Logging in";
-  }
+  } else if (document.location.pathname === "/") presenceData.details = "Browsing the homepage";
+  else if (document.location.pathname === "/login") presenceData.details = "Logging in";
+  else if (document.location.pathname === "/profile") presenceData.details = "Changing profile settings";
   if (presenceData.details) presence.setActivity(presenceData);
   else presence.setActivity();
 });
