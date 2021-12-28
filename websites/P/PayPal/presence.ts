91f45801a017270ffef730f30234724bf48f58a2
Dustin Lontzek <18153513+CrypticXT@users.noreply.github.com>
2021-12-28 15:24:03 +0100

    Add presence

---
+++ websites/P/PayPal/presence.ts
@@ -0,0 +1,55 @@
+const presence = new Presence({
+    clientId: "924713441464295424"
+  }),
+  browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "logo",
+    startTimestamp: browsingTimestamp
+  };
+
+  if (document.location.pathname === "/myaccount/summary") {
+    presenceData.details = "Viewing:";
+    presenceData.state = "Dashboard";
+  } else if (document.location.pathname === "/myaccount/transfer/homepage") {
+    presenceData.details = "Viewing:";
+    presenceData.state = "Send Money";
+  } else if (document.location.pathname.includes("/myaccount/money/")) {
+    presenceData.details = "Viewing:";
+    presenceData.state = "Wallet";
+  } else if (
+    document.location.pathname === "/myaccount/transfer/homepage/request"
+  ) {
+    presenceData.details = "About to:";
+    presenceData.state = "Request money";
+  } else if (document.location.pathname === "/myaccount/transactions/") {
+    presenceData.details = "Viewing:";
+    presenceData.state = "Activity";
+    if (document.querySelector(".chevron-rotation")) {
+      presenceData.details = "Inspecting:";
+      presenceData.state = "Transaction";
+    }
+  } else if (document.location.pathname.includes("/smarthelp/home")) {
+    presenceData.details = "Viewing:";
+    presenceData.state = "Help";
+  } else if (
+    document.location.pathname ===
+      "/myaccount/transfer/homepage/send/preview" ||
+    document.location.pathname === "/myaccount/transfer/homepage/buy/preview"
+  ) {
+    presenceData.details = "About to:";
+    presenceData.state = "Send money";
+  } else if (document.location.pathname.includes("/myaccount/settings/")) {
+    presenceData.details = "Viewing:";
+    presenceData.state = "Settings";
+  } else if (
+    document.location.pathname.includes("/myaccount/transactions/details")
+  ) {
+    presenceData.details = "Viewing:";
+    presenceData.state = "Transaction";
+  } else presenceData.details = "Browsing...";
+
+  if (presenceData.details) presence.setActivity(presenceData);
+  else presence.setActivity();
+});
