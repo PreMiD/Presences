0d2d866857208924614b523a620e28ed3900f892
Nam Anh <phamnamanh25@gmail.com>
2021-12-28 10:08:45 +0700

    feat(Google Photos); add presence

---
+++ websites/G/Google Photos/presence.ts
@@ -0,0 +1,29 @@
+const presence = new Presence({
+    clientId: "925204937225416704"
+  }),
+  browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", () => {
+  const presenceData: PresenceData = {
+      largeImageKey: "logo",
+      startTimestamp: browsingTimestamp
+    },
+    path = document.location.pathname;
+  if (path === "" || path === "/") presenceData.details = "Browsing photos";
+  else if (path.indexOf("/photo") > -1)
+    presenceData.details = "Viewing a photo";
+  else if (path.startsWith("/memory")) {
+    presenceData.details = "Playing back a memory:";
+    [presenceData.state] = document.title.split(/-/, 1);
+  } else if (path.startsWith("/search")) {
+    presenceData.details = "Searching for:";
+    [presenceData.state] = document.title.split(/-/, 1);
+  } else if (path === "/albums") presenceData.details = "Browsing albums";
+  else if (path.startsWith("/album")) {
+    presenceData.details = "Viewing an album:";
+    [presenceData.state] = document.title.split(/-/, 1);
+  } else if (path.startsWith("/archive"))
+    presenceData.details = "Viewing the archive";
+  else presenceData;
+  presence.setActivity(presenceData);
+});
