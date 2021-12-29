16e9307b271acf2eb83cad98702d8d659da0214a
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2021-12-29 03:00:48 +0200

    feat(eslint warnings): Step 2

--- websites/A/Animes Portal/iframe.ts
+++ websites/A/Animes Portal/iframe.ts
@@ -1,10 +1,11 @@
 const iframe = new iFrame();
 iframe.on("UpdateData", async () => {
-  const video: HTMLVideoElement = document.querySelector("#html5player");
-  if (video && video.duration && !isNaN(video.duration))
+  const video = document.querySelector<HTMLVideoElement>("#html5player");
+  if (video && !isNaN(video.duration)) {
     iframe.send({
       duration: video.duration,
       currentTime: video.currentTime,
       paused: video.paused
     });
+  }
 });
