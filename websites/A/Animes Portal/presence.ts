943490e2758b3fbbeae464419f8ad0790d4fcd0b
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2021-12-29 02:32:29 +0200

    feat(eslint warnings): Step 1

--- websites/A/Animes Portal/presence.ts
+++ websites/A/Animes Portal/presence.ts
@@ -1,59 +1,55 @@
 const presence = new Presence({
-  clientId: "924791712944099389"
-});
-
-const filterCodes: Record<number, string> = {
-  366: "BG sub"
-};
+    clientId: "924791712944099389"
+  }),
+  filterCodes: Record<number, string> = {
+    366: "BG sub"
+  };
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "logo"
-  };
-
-  const pathname: string = document.location.pathname;
-  const paths = pathname.split("/");
+      largeImageKey: "logo"
+    },
+    { pathname } = document.location,
+    paths = pathname.split("/");
   if (!paths[0]) paths.shift();
 
   if (pathname === "/") {
     presenceData.details = "Home";
     presenceData.state = "Viewing the home page";
   } else if (pathname.startsWith("/messages")) {
-    if (!paths[1]) {
-      presenceData.details = "Viewing messages";
-    } else if (paths[1].startsWith("pm-")) {
+    if (!paths[1]) presenceData.details = "Viewing messages";
+    else if (paths[1].startsWith("pm-")) {
       const body = document.querySelector(
-        "body > main.animated > div.wrapper > div.dialogPadding"
-      );
-      const username = body.querySelector("span.username").textContent;
-      const avatar = parseAvatarFromAttr(
-        body.querySelector("a.avatar").attributes.getNamedItem("style")
-          .textContent
-      );
+          "body > main.animated > div.wrapper > div.dialogPadding"
+        ),
+        username = body.querySelector("span.username").textContent,
+        avatar = parseAvatarFromAttr(
+          body.querySelector("a.avatar").attributes.getNamedItem("style")
+            .textContent
+        );
 
       presenceData.details = `Messaging ${username}`;
       presenceData.smallImageKey = avatar;
       presenceData.smallImageText = `Messaging ${username}`;
     }
-  } else if (pathname === "/chat") {
+  } else if (pathname === "/chat")
     presenceData.details = "Chatting in the chat";
-  } else if (pathname.startsWith("/otakus")) {
+  else if (pathname.startsWith("/otakus")) {
     if (!paths[1] || paths[1].startsWith("page-")) {
       presenceData.details = "Viewing otakus";
       if (paths[1])
         presenceData.state = `Page ${paths[1].replace("page-", "")}`;
     } else if (paths[1]) {
       const body = document.querySelector(
-        "body > main.animated > section.wrapper > div.header"
-      );
-      const username = body.querySelector(
-        "div.content > div.holder > span.heading"
-      ).textContent;
-
-      const avatar = parseAvatarFromAttr(
-        body.querySelector("a.avatar").attributes.getNamedItem("style")
-          .textContent
-      );
+          "body > main.animated > section.wrapper > div.header"
+        ),
+        username = body.querySelector(
+          "div.content > div.holder > span.heading"
+        ).textContent,
+        avatar = parseAvatarFromAttr(
+          body.querySelector("a.avatar").attributes.getNamedItem("style")
+            .textContent
+        );
 
       presenceData.details = `Viewing otaku ${username}`;
       presenceData.largeImageKey = avatar;
@@ -62,9 +58,9 @@ presence.on("UpdateData", async () => {
     }
   } else if (pathname.startsWith("/animes")) {
     const filter: string = new URL(document.location.href).searchParams.get(
-      "filters"
-    );
-    const filters: string[] = [];
+        "filters"
+      ),
+      filters: string[] = [];
 
     if (filter) {
       filter
@@ -77,27 +73,27 @@ presence.on("UpdateData", async () => {
 
     if (paths[1] === "search" && paths[2]) {
       presenceData.details = `Searching for ${paths[2].replaceAll("-", " ")} ${
-        filters.length ? "With " + filters.join(", ") : ""
+        filters.length ? `With ${filters.join(", ")}` : ""
       }`;
-    } else if (paths[1]) {
+    } else if (paths[1])
       presenceData.details = `Viewing animes that starts with the keyboard ${paths[1].toUpperCase()}`;
-    } else
+    else {
       presenceData.details = `Viewing animes ${
-        filters.length ? "With " + filters.join(", ") : ""
+        filters.length ? `With ${filters.join(", ")}` : ""
       }`;
+    }
   } else if (pathname.startsWith("/anime/")) {
-    const uid = paths[1];
-    const eid = paths[2];
+    const [uid, eid] = paths;
 
     if (uid && !eid) {
       const name = document.querySelector(
-        "body > main.animated > div.wrapper > article.rowView > header.rowView-head > h1.heading"
-      ).innerHTML;
-      const image = document
-        .querySelector(
-          "body > main.animated > div.wrapper > article.rowView > aside.aside > div.cover-holder > img.abs"
-        )
-        ?.getAttribute("src");
+          "body > main.animated > div.wrapper > article.rowView > header.rowView-head > h1.heading"
+        ).innerHTML,
+        image = document
+          .querySelector(
+            "body > main.animated > div.wrapper > article.rowView > aside.aside > div.cover-holder > img.abs"
+          )
+          ?.getAttribute("src");
 
       if (image) {
         presenceData.smallImageKey = "logo";
@@ -113,13 +109,12 @@ presence.on("UpdateData", async () => {
         }
       ];
     } else if (uid && eid) {
-      const { name, episode, part } = getInfo();
-
-      const playing = document
-        .querySelector(
-          "body > main.animated > div.wrapper > section.holder > div.player > div.holder > div.vplayer"
-        )
-        ?.classList.contains("playing");
+      const { name, episode, part } = getInfo(),
+        playing = document
+          .querySelector(
+            "body > main.animated > div.wrapper > section.holder > div.player > div.holder > div.vplayer"
+          )
+          ?.classList.contains("playing");
 
       if (!playing) {
         presenceData.details = `Watching ${name}`;
@@ -136,9 +131,9 @@ presence.on("UpdateData", async () => {
     }
   } else if (pathname.startsWith("/movies")) {
     const filter: string = new URL(document.location.href).searchParams.get(
-      "filters"
-    );
-    const filters: string[] = [];
+        "filters"
+      ),
+      filters: string[] = [];
 
     if (filter) {
       filter
@@ -153,12 +148,13 @@ presence.on("UpdateData", async () => {
       presenceData.details = `Searching for ${paths[2].replaceAll("-", " ")} ${
         filters.length ? "With " + filters.join(", ") : ""
       }`;
-    } else if (paths[1]) {
+    } else if (paths[1])
       presenceData.details = `Viewing movies that starts with the keyboard ${paths[1].toUpperCase()}`;
-    } else
+    else {
       presenceData.details = `Viewing movies ${
-        filters.length ? "With " + filters.join(", ") : ""
+        filters.length ? `With ${filters.join(", ")}` : ""
       }`;
+    }
   } else if (pathname.startsWith("/movie/")) {
     const name = document.querySelector(
       "body > main.animated > div.wrapper > article.rowView > header.rowView-head > h1.heading"
@@ -178,9 +174,9 @@ presence.on("UpdateData", async () => {
     presenceData.largeImageKey = image ?? "logo";
   } else if (pathname.startsWith("/manga")) {
     const filter: string = new URL(document.location.href).searchParams.get(
-      "filters"
-    );
-    const filters: string[] = [];
+        "filters"
+      ),
+      filters: string[] = [];
 
     if (filter) {
       filter
@@ -193,19 +189,19 @@ presence.on("UpdateData", async () => {
 
     if (paths[1] === "search" && paths[2]) {
       presenceData.details = `Searching for ${paths[2].replaceAll("-", " ")} ${
-        filters.length ? "With " + filters.join(", ") : ""
+        filters.length ? `With ${filters.join(", ")}` : ""
       }`;
     } else if (paths[1] && paths[2]?.startsWith("vol-")) {
       const tom = paths[2].replace("vol-", "");
 
       if (paths[3].startsWith("chapter-")) {
         const list = document.querySelectorAll(
-          "body > main.animated > ul#path > li > a > span"
-        );
-        const name = list[1].textContent;
-        const page = document.querySelector(
-          "body > main.animated > div.wrapper > div.heading > b#num"
-        ).textContent;
+            "body > main.animated > ul#path > li > a > span"
+          ),
+          name = list[1].textContent,
+          page = document.querySelector(
+            "body > main.animated > div.wrapper > div.heading > b#num"
+          ).textContent;
 
         presenceData.details = `Reading manga ${name}`;
         presenceData.state = `Tom: ${tom}, Chapter: ${between(
@@ -236,13 +232,13 @@ presence.on("UpdateData", async () => {
       }
     } else if (hasNumber(paths[1])) {
       const name = document.querySelector(
-        "body > main.animated > div.wrapper > article.rowView > header.rowView-head > h1.heading"
-      ).textContent;
-      const image = document
-        .querySelector(
-          "body > main.animated > div.wrapper > article.rowView > aside.aside > div.cover-holder > img.abs"
-        )
-        ?.getAttribute("src");
+          "body > main.animated > div.wrapper > article.rowView > header.rowView-head > h1.heading"
+        ).textContent,
+        image = document
+          .querySelector(
+            "body > main.animated > div.wrapper > article.rowView > aside.aside > div.cover-holder > img.abs"
+          )
+          ?.getAttribute("src");
 
       if (name) presenceData.details = `Viewing manga ${name}`;
 
@@ -259,11 +255,11 @@ presence.on("UpdateData", async () => {
           url: document.location.href
         }
       ];
-    } else if (paths[1]) {
+    } else if (paths[1])
       presenceData.details = `Viewing manga that starts with the keyboard ${paths[1].toUpperCase()}`;
-    } else if (!paths[1])
+    else if (!paths[1])
       presenceData.details = `Viewing manga ${
-        filters.length ? "With " + filters.join(", ") : ""
+        filters.length ? `With ${filters.join(", ")}` : ""
       }`;
   }
 
@@ -281,18 +277,17 @@ interface iframeData {
 presence.on("iFrameData", async (data: iframeData) => {
   if (!data.currentTime || !data.duration) return;
   const presenceData: PresenceData = {
-    largeImageKey: "logo"
-  };
+      largeImageKey: "logo"
+    },
+    epInfo = getInfo();
 
-  const epInfo = getInfo();
-
-  if (!data.paused)
+  if (!data.paused) {
     [presenceData.startTimestamp, presenceData.endTimestamp] =
       presence.getTimestamps(
         Math.floor(data.currentTime),
         Math.floor(data.duration)
       );
-  else {
+  } else {
     presenceData.smallImageKey = "start";
     presenceData.smallImageText = "Paused";
   }
@@ -309,9 +304,8 @@ presence.on("iFrameData", async (data: iframeData) => {
   if (epInfo.part)
     presenceData.state = presenceData.state += ` (part ${epInfo.part})`;
 
-  if (presenceData.details) {
-    presence.setActivity(presenceData);
-  } else presence.setActivity();
+  if (presenceData.details) presence.setActivity(presenceData);
+  else presence.setActivity();
 });
 
 function getInfo(): {
@@ -320,13 +314,13 @@ function getInfo(): {
   part: number;
 } {
   const ep = document.querySelector(
-    "body > main.animated > div.wrapper > section.holder > h1.heading > b#num"
-  ).innerHTML;
-  const name = document.querySelector(
-    "body > main.animated > div.wrapper > section.holder > h2.heading > a"
-  ).innerHTML;
-  let part = between(ep, "(", ")");
-  let rep = ep;
+      "body > main.animated > div.wrapper > section.holder > h1.heading > b#num"
+    ).innerHTML,
+    name = document.querySelector(
+      "body > main.animated > div.wrapper > section.holder > h2.heading > a"
+    ).innerHTML;
+  let part = between(ep, "(", ")"),
+    rep = ep;
 
   if (ep.includes("(") && ep.includes(")")) {
     rep = ep.replace(` (${part})`, "");
