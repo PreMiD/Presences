2190f71624051e312a78322b3742718b7d949a2f
Dustin Lontzek <18153513+CrypticXT@users.noreply.github.com>
2021-12-30 21:51:38 +0100

    Updated presence

--- websites/P/PayPal/presence.ts
+++ websites/P/PayPal/presence.ts
@@ -11,23 +11,15 @@ presence.on("UpdateData", async () => {
   };
 
   if (document.location.pathname === "/myaccount/summary") {
-    // Dashboard
     presenceData.details = "Viewing";
     presenceData.state = "Dashboard";
   } else if (document.location.pathname.includes("/webapps")) {
-    // Most info pages
     title = document.querySelector("head > title");
     presenceData.details = "Reading about";
     presenceData.state = title.textContent;
   } else if (document.location.pathname === "/myaccount/transfer/homepage") {
-    // Send money
     presenceData.details = "About to";
     presenceData.state = "Send Money";
-    // eslint-disable-next-line brace-style
-  } // WALLET
-  else if (document.location.pathname.includes("/myaccount/money/")) {
-    presenceData.details = "Viewing";
-    presenceData.state = "Wallet";
   } else if (
     document.location.pathname.includes("/myaccount/money/cards/new")
   ) {
@@ -38,16 +30,50 @@ presence.on("UpdateData", async () => {
   ) {
     presenceData.details = "About to";
     presenceData.state = "Link a bank account";
+  } else if (document.location.pathname === "/myaccount/money/") {
+    presenceData.details = "Viewing";
+    presenceData.state = "Wallet";
+  } else if (document.location.pathname === "/myaccount/profile/seller-tools") {
+    presenceData.details = "Editing settings";
+    presenceData.state = "Seller Tools";
   } else if (
     document.location.pathname === "/myaccount/transfer/homepage/request"
   ) {
     presenceData.details = "About to";
     presenceData.state = "Request money";
+  } else if (document.location.pathname === "/signin") {
+    presenceData.details = "About to";
+    presenceData.state = "Log In";
   } else if (
     document.location.pathname === "/myaccount/transfer/homepage/contact-list"
   ) {
     presenceData.details = "Looking through";
     presenceData.state = "Contacts";
+  } else if (
+    document.location.pathname === "/myaccount/transfer/homepage/more"
+  ) {
+    presenceData.details = "Viewing";
+    presenceData.state = "More";
+  } else if (document.location.pathname === "/myaccount/transfer/gift") {
+    presenceData.details = "About to";
+    presenceData.state = "Send a gift";
+  } else if (document.location.pathname === "/fundraiser/hub") {
+    presenceData.details = "About to";
+    presenceData.state = "Donate";
+  } else if (
+    document.location.pathname.includes("/myaccount/transfer/split/preview")
+  ) {
+    presenceData.details = "About to";
+    presenceData.state = "Split a bill";
+  } else if (document.location.pathname === "/invoice/s/create") {
+    presenceData.details = "About to";
+    presenceData.state = "Create an Invoice";
+  } else if (document.location.pathname === "/smc/async/") {
+    presenceData.details = "Viewing";
+    presenceData.state = "Message Center";
+  } else if (document.location.pathname === "/disputes/") {
+    presenceData.details = "Viewing";
+    presenceData.state = "Resolution Center";
   } else if (document.location.pathname.includes("/smarthelp/home")) {
     presenceData.details = "Viewing";
     presenceData.state = "Help";
@@ -58,10 +84,7 @@ presence.on("UpdateData", async () => {
   ) {
     presenceData.details = "About to";
     presenceData.state = "Send money";
-    // eslint-disable-next-line brace-style
-  }
-  // SETTINGS
-  else if (document.location.pathname === "/myaccount/settings/") {
+  } else if (document.location.pathname === "/myaccount/settings/") {
     presenceData.details = "Editing settings";
     presenceData.state = "Account";
   } else if (document.location.pathname === "/myaccount/security/") {
@@ -78,27 +101,25 @@ presence.on("UpdateData", async () => {
   ) {
     presenceData.details = "Editing settings";
     presenceData.state = "Notifications";
-    // eslint-disable-next-line brace-style
-  }
-  // TRANSACTIONS
-  else if (
+  } else if (
     document.location.pathname.includes("/myaccount/transactions/details")
   ) {
     presenceData.details = "Viewing";
     presenceData.state = "Transaction";
+  } else if (document.location.pathname === "/myaccount/crypto/") {
+    presenceData.details = "Viewing";
+    presenceData.state = "Finances";
+  } else if (document.location.pathname === "/shopping") {
+    presenceData.details = "Looking for";
+    presenceData.state = "Deals";
   } else if (document.location.pathname === "/myaccount/transactions/") {
-    if (document.querySelector(".filter-search-hasValue")) {
-      presenceData.details = "Searching";
-      presenceData.state = "Activities";
-    } else {
-      presenceData.details = "Viewing:";
-      presenceData.state = "Activity";
-    }
-    if (document.querySelector(".chevron-rotation")) {
-      presenceData.details = "Inspecting";
-      presenceData.state = "Transaction";
-    }
-  } else presenceData.details = "Browsing...";
+    presenceData.details = "Viewing:";
+    presenceData.state = "Activity";
+  }
+  if (document.querySelector(".chevron-rotation")) {
+    presenceData.details = "Inspecting";
+    presenceData.state = "Transaction";
+  }
 
   if (presenceData.details) presence.setActivity(presenceData);
   else presence.setActivity();
