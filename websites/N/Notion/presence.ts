fdd7b4cf20501aa9d92c193f377cba86deb34143
Nam Anh <phamnamanh25@gmail.com>
2021-12-31 18:00:53 +0700

    feat: Return overlay title for `.state`

--- websites/N/Notion/presence.ts
+++ websites/N/Notion/presence.ts
@@ -34,7 +34,10 @@ presence.on("UpdateData", async () => {
     )
   ) {
     presenceData.details = "Editing a page:";
-    presenceData.state = document.title;
+    presenceData.state =
+      document.querySelector(
+        "#notion-app > div > div.notion-overlay-container.notion-default-overlay-container > div:nth-child(2) > div > div:nth-child(2) > div.notion-scroller.vertical > div:nth-child(2) > div > div:nth-child(1) > div > div:nth-child(2) > div > div"
+      ).textContent ?? document.title;
     presenceData.smallImageKey = "edit";
     presenceData.smallImageText = "Editing";
   } else if (
