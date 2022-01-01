7e567fdde3920f412ba156ca9519431451529e19
Kozou <louisfloquey@gmail.com>
2022-01-01 18:24:12 +0100

    feat(Vostfree): add presence

---
+++ websites/V/Vostfree/iframe.ts
@@ -0,0 +1,14 @@
+const iframe = new iFrame();
+
+iframe.on("UpdateData", async () => {
+  if (document.querySelector("#video_html5_wrapper_html5_api") !== null) {
+    const video: HTMLVideoElement = document.querySelector("#video_html5_wrapper_html5_api");
+    if (video !== null && !isNaN(video.duration)) {
+      iframe.send({
+        duration: video.duration,
+        currentTime: video.currentTime,
+        paused: video.paused
+      });
+    }
+  }
+});
