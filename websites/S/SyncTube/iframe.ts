a955c9c85a8dcfe85117860ed13a3457c3e12752
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-01-03 06:18:56 +0100

    Update iframe.ts

--- websites/S/SyncTube/iframe.ts
+++ websites/S/SyncTube/iframe.ts
@@ -8,9 +8,16 @@ iframe.on("UpdateData", async () => {
       current: video.currentTime,
       duration: video.duration,
       paused: video.paused,
-      title: document.querySelector("a.ytp-title-link.yt-uix-sessionlink").textContent || "Unknown",
-      channel: document.querySelector("div.ytp-title-channel h2.ytp-title-expanded-title a").textContent || "Unknown",
-      url: document.querySelector<HTMLAnchorElement>("a.ytp-youtube-button.ytp-button.yt-uix-sessionlink")?.href
+      title:
+        document.querySelector("a.ytp-title-link.yt-uix-sessionlink")
+          .textContent || "Unknown",
+      channel:
+        document.querySelector(
+          "div.ytp-title-channel h2.ytp-title-expanded-title a"
+        ).textContent || "Unknown",
+      url: document.querySelector<HTMLAnchorElement>(
+        "a.ytp-youtube-button.ytp-button.yt-uix-sessionlink"
+      )?.href
     });
   }
 });
