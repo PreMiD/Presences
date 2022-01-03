252eeabdc9e448ddd91968f7daf536bf0b96de89
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-01-03 06:12:35 +0100

    Update

--- websites/S/SyncTube/presence.ts
+++ websites/S/SyncTube/presence.ts
@@ -3,39 +3,67 @@ const presence = new Presence({
   }),
   browsingTimestamp = Math.floor(Date.now() / 1000);
 
+let video = {
+  current: 0,
+  duration: 0,
+  paused: true,
+  title: "Unknown",
+  channel: "Unknown",
+  url: <string>null
+};
+
 presence.on("UpdateData", async function () {
-  const setTimeElapsed = await presence.getSetting<boolean>("timeElapsed"),
-    setShowButtons = await presence.getSetting<boolean>("showButtons"),
-    setPrivacy = await presence.getSetting<boolean>("privacy"),
-    setLogo = await presence.getSetting<number>("logo"),
+  const setting = {
+      timeElapsed: await presence.getSetting<boolean>("timeElapsed"),
+      moreDetails: await presence.getSetting<boolean>("moreDetails"),
+      showButtons: await presence.getSetting<boolean>("showButtons"),
+      privacy: await presence.getSetting<boolean>("privacy"),
+      logo: await presence.getSetting<number>("logo")
+    },
     presenceData: PresenceData = {
-      largeImageKey: setLogo === 0 ? "logo" : "logo2"
+      largeImageKey: setting.logo === 0 ? "logo" : "logo2"
     },
     urlpath = window.location.pathname.split("/");
 
-  if (setTimeElapsed) presenceData.startTimestamp = browsingTimestamp;
+  if (setting.timeElapsed) presenceData.startTimestamp = browsingTimestamp;
 
   if (!urlpath[1]) presenceData.details = "Home";
   else if (urlpath[1] === "rooms") {
     if (urlpath[2]) {
-      presenceData.details = setPrivacy
-        ? "In room"
+      presenceData.details = setting.privacy
+        ? "In Room"
         : document.querySelector("div.roomName.noselect").textContent;
-      if (!setPrivacy) {
-        presenceData.state = document.querySelector(
-          "div.userCount.noselect"
-        ).textContent;
+      if (!setting.privacy) {
+        if (setting.moreDetails && video) {
+          presenceData.details = video.title;
+          presenceData.state = video.channel;
+
+          presenceData.endTimestamp = presence.getTimestamps(
+            Math.floor(video.current),
+            Math.floor(video.duration)
+          )[1];
+        } else
+          presenceData.state = document.querySelector(
+            "div.userCount.noselect"
+          ).textContent;
       }
 
-      if (setShowButtons) {
+      if (setting.showButtons) {
         presenceData.buttons = [
           {
-            label: "Join room",
+            label: "Join Room",
             url: window.location.href
           }
         ];
+
+        if (!setting.privacy && video.url) {
+          presenceData.buttons.push({
+            label: "Watch Video",
+            url: video.url
+          });
+        }
       }
-    } else presenceData.details = "Browsing rooms";
+    } else presenceData.details = "Browsing Rooms";
   } else presenceData.details = "Other";
 
   if (presenceData.details) presence.setActivity(presenceData);
