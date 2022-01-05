96bfbc6bc4a132a27bde793c57c5dea70efc50b5
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2022-01-06 01:24:55 +0200

    feat(AppGallery): add presence

---
+++ websites/A/App Gallery/presence.ts
@@ -0,0 +1,52 @@
+const presence = new Presence({
+  clientId: "928404633209143336"
+});
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+      largeImageKey: "logo"
+    },
+    paths: string[] = document.location.hash
+      ? document.location.hash.replace("#", "").split("/")
+      : document.location.pathname.split("/");
+  if (!paths[0]) paths.shift();
+
+  if (paths.length === 1) {
+    switch (paths[0]) {
+      case "Featured":
+        presenceData.details = "Browsing the featured apps";
+        break;
+      case "Categories":
+        presenceData.details = "Browsing through the categories";
+        break;
+      case "Top":
+        presenceData.details = "Browsing the TOP apps";
+        break;
+      default:
+        presenceData.details = "Browsing the featured apps";
+        break;
+    }
+  } else if (paths[0] === "app" && paths[1]) {
+    const container = document.querySelector<HTMLDivElement>(
+        "body > div.page > div.box > div.appDetailsContainer > div.componentContainer > div > div.detailheadcard"
+      ),
+      logo = container?.querySelector<HTMLImageElement>("img.left_logo")?.src,
+      name = container?.querySelector<HTMLDivElement>(
+        "div.center_info > div.title"
+      )?.textContent;
+
+    if (logo) presenceData.largeImageKey = logo;
+
+    presenceData.details = `Viewing ${name ? name : "app"}`;
+    presenceData.buttons = [
+      {
+        label: "View app",
+        url: document.location.href
+      }
+    ];
+  } else if (paths[0] === "search" && paths[1])
+    presenceData.details = `Searching for ${decodeURIComponent(paths[1])}`;
+
+  if (presenceData.details) presence.setActivity(presenceData);
+  else presence.setActivity();
+});
