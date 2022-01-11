e5f4b6c607d6639c434431ca3dfb29eac76c7f6a
Anaxes <68460474+DiscordAnaxes@users.noreply.github.com>
2022-01-11 21:52:47 +1000

    feat(DscJobs): remove emojis and fixed minor issues.

--- websites/D/DscJobs/presence.ts
+++ websites/D/DscJobs/presence.ts
@@ -1,5 +1,5 @@
 const presence = new Presence({
-    clientId: "846309777508007946"
+    clientId: "846309777508007946",
   }),
   browsingStamp = Math.floor(Date.now() / 1000),
   [page] = document.location.href.split("page=")[1].split("&"),
@@ -11,117 +11,115 @@ presence.on("UpdateData", async () => {
   const [showTimestamp, showButtons, showCvButton] = await Promise.all([
       presence.getSetting<boolean>("timestamp"),
       presence.getSetting<boolean>("buttons"),
-      presence.getSetting<boolean>("cvButton")
+      presence.getSetting<boolean>("cvButton"),
     ]),
     presenceData: PresenceData = {
-      largeImageKey: "dscjobs_logo"
+      largeImageKey: "dscjobs_logo",
     };
 
   if (document.location.pathname === "/")
     presenceData.details = "Viewing home page";
   else if (document.location.pathname.includes("/search")) {
-    presenceData.details = `🔍 Searching for: ${search || "Nothing"}`;
-    presenceData.state = `📖 Results Page ${page}`;
+    presenceData.details = `Searching for: ${search || "Nothing"}`;
+    presenceData.state = `Results Page: ${page}`;
   } else if (document.location.pathname === "/moderators") {
-    presenceData.details = "Viewing 🔨 hireable moderators";
-    presenceData.state = `${filters ? "💿 Filters: True" : `📖 Page ${page}`}`;
+    presenceData.details = "Viewing hireable moderators";
+    presenceData.state = filters ? "Filters: True" : `Page: ${page}`;
     presenceData.buttons = [
       {
         label: "View Page",
-        url: document.location.href
-      }
+        url: document.location.href,
+      },
     ];
   } else if (document.location.pathname === "/developers") {
-    presenceData.details = "Viewing 💻 hireable developers";
-    presenceData.state = `${filters ? "💿 Filters: True" : `📖 Page ${page}`}`;
+    presenceData.details = "Viewing hireable developers";
+    presenceData.state = filters ? "Filters: True" : `Page: ${page}`;
     presenceData.buttons = [
       {
         label: "View Page",
-        url: document.location.href
-      }
+        url: document.location.href,
+      },
     ];
   } else if (document.location.pathname.includes("/premium")) {
-    presenceData.details = "Viewing 💎 premium plan";
+    presenceData.details = "Viewing premium plan";
     presenceData.buttons = [
       {
         label: "View Premium Plan",
-        url: document.location.href
-      }
+        url: document.location.href,
+      },
     ];
   } else if (document.location.pathname === "/partners") {
-    presenceData.details = "Viewing 🤝 partners";
+    presenceData.details = "Viewing partners";
     presenceData.buttons = [
       {
         label: "View Partners",
-        url: document.location.href
-      }
+        url: document.location.href,
+      },
     ];
   } else if (document.location.pathname === "/profile")
-    presenceData.details = "Viewing 👤 profile";
+    presenceData.details = "Viewing profile";
   else if (document.location.pathname.includes("/u/")) {
-    presenceData.details = `Viewing 💳 ${
+    presenceData.details = `Viewing ${
       document.querySelector("h1")?.textContent.split("#")[0]
     } profile`;
   } else if (document.location.pathname.includes("/vote")) {
     const usernameVote = document.querySelector("h1")?.textContent;
-    presenceData.details = `Voting 🗳️ ${usernameVote || "N/A"} `;
+    presenceData.details = `Voting ${usernameVote || "N/A"} `;
     if (usernameVote) {
       presenceData.buttons = [
         {
           label: `Vote ${usernameVote}`,
-          url: document.location.href
-        }
+          url: document.location.href,
+        },
       ];
     }
   } else if (document.location.pathname.includes("/cv/")) {
-    presenceData.details = `Viewing 📖 ${document
+    presenceData.details = `Viewing ${document
       .querySelector("h2.cursor")
       ?.getAttribute("data-title")} resume`;
-    presenceData.state = `❤️ ${document
+    presenceData.state = `Likes: ${document
       .getElementById("likes_amount")
-      ?.getAttribute("data-title")} & 👀 ${document
+      ?.getAttribute("data-title")} & Views: ${document
       .getElementById("views_amount")
       ?.getAttribute("data-title")}`;
-    if (showButtons) {
-      if (showCvButton) {
-        presenceData.buttons = [
-          {
-            label: "View Resume",
-            url: document.location.href
-          }
-        ];
-      }
+    if (showButtons && showCvButton) {
+      presenceData.buttons = [
+        {
+          label: "View Resume",
+          url: document.location.href,
+        },
+      ];
     }
   } else if (document.location.pathname.includes("/settings"))
-    presenceData.details = "Editing 📜 curriculum vitae/resume";
+    presenceData.details = "Editing curriculum vitae/resume";
   else if (document.location.pathname.includes("/privacy")) {
-    presenceData.details = "Viewing 🔑 Privacy Policy";
+    presenceData.details = "Viewing Privacy Policy";
     presenceData.buttons = [
       {
         label: "View Privacy Policy",
-        url: document.location.href
-      }
+        url: document.location.href,
+      },
     ];
   } else if (document.location.pathname.includes("/terms")) {
-    presenceData.details = "Viewing 👩‍⚖️ Terms of Service";
+    presenceData.details = "Viewing Terms of Service";
     presenceData.buttons = [
       {
         label: "View Page",
-        url: document.location.href
-      }
+        url: document.location.href,
+      },
     ];
     // staff panel
   } else if (document.location.pathname.includes("/reviews")) {
-    presenceData.details = "Viewing ⭐ Review Panel";
-    presenceData.state = `📖 Page ${staffPage}`;
+    presenceData.details = "Viewing Review Panel";
+    presenceData.state = `Page: ${staffPage}`;
   } else if (document.location.pathname.includes("/reports")) {
-    presenceData.details = "Viewing 🛑 Report Panel";
-    presenceData.state = `📖 Page ${staffPage}`;
+    presenceData.details = "Viewing Report Panel";
+    presenceData.state = `Page: ${staffPage}`;
   } else if (document.location.pathname.includes("/users")) {
-    presenceData.details = "Viewing 👥 Members Panel";
-    presenceData.state = `📖 Page ${staffPage}`;
+    presenceData.details = "Viewing Members Panel";
+    presenceData.state = `Page: ${staffPage}`;
   } else if (document.location.pathname.includes("/panel"))
-    presenceData.details = "Viewing ⚙️ Staff Panel";
+    presenceData.details = "Viewing Staff Panel";
 
   if (!showButtons) delete presenceData.buttons;
   if (showTimestamp) presenceData.startTimestamp = browsingStamp;
