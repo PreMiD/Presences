d52ff5b30c4e9f2319e94e0bb88cc9673fa6c7e3
Northernside <northernside@pm.me>
2022-01-18 21:12:29 +0100

    v1.1.0 (for real for real)

--- websites/L/LABY.net/presence.ts
+++ websites/L/LABY.net/presence.ts
@@ -18,24 +18,27 @@ presence.on("UpdateData", () => {
   else if (document.location.pathname.startsWith("/badges"))
     presenceData.details = "Viewing LABY.net badges";
   else if (document.location.pathname.startsWith("/settings"))
-      presenceData.details = "Viewing profile settings";
+    presenceData.details = "Viewing profile settings";
   else if (document.location.pathname.startsWith("/@")) {
     presenceData.details = "Viewing profile of:";
-    presenceData.state = document.querySelector(
-      "div.col-12>h1"
-    )?.textContent ?? "an invalid account";
+    presenceData.state =
+      document.querySelector("div.col-12>h1")?.textContent ??
+      "an invalid account";
     presenceData.buttons = [
       {
-          label: "View " + document.querySelector("div.col-12>h1")?.textContent + "'s profile",
-          url: window.location.href
+        label:
+          "View " +
+          document.querySelector("div.col-12>h1")?.textContent +
+          "'s profile",
+        url: window.location.href
       }
     ];
   } else if (document.location.pathname.startsWith("/skin")) {
     presenceData.details = "Viewing a Minecraft skin";
     presenceData.buttons = [
       {
-          label: "View this Minecraft skin",
-          url: window.location.href
+        label: "View this Minecraft skin",
+        url: window.location.href
       }
     ];
   } else if (document.location.pathname.startsWith("/cape")) {
@@ -43,16 +46,19 @@ presence.on("UpdateData", () => {
     presenceData.state = document.querySelector("div.mb-1>h1").textContent;
     presenceData.buttons = [
       {
-          label: "View " + document.querySelector("div.mb-1>h1")?.textContent + " cape",
-          url: window.location.href
+        label:
+          "View " +
+          document.querySelector("div.mb-1>h1")?.textContent +
+          " cape",
+        url: window.location.href
       }
     ];
   } else if (document.location.pathname.startsWith("/cloak")) {
     presenceData.details = "Viewing a LabyMod cloak";
     presenceData.buttons = [
       {
-          label: "View this cloak",
-          url: window.location.href
+        label: "View this cloak",
+        url: window.location.href
       }
     ];
   } else if (document.location.pathname.startsWith("/badge")) {
@@ -60,8 +66,11 @@ presence.on("UpdateData", () => {
     presenceData.state = document.querySelector("div.mb-1>h1").textContent;
     presenceData.buttons = [
       {
-          label: "View " + document.querySelector("div.mb-1>h1")?.textContent + " badge",
-          url: window.location.href
+        label:
+          "View " +
+          document.querySelector("div.mb-1>h1")?.textContent +
+          " badge",
+        url: window.location.href
       }
     ];
   } else if (document.location.pathname.startsWith("/server")) {
@@ -71,8 +80,10 @@ presence.on("UpdateData", () => {
     ).textContent;
     presenceData.buttons = [
       {
-          label: "View " + document.querySelector("div.server-info-wrapper>h1")?.textContent,
-          url: window.location.href
+        label:
+          "View " +
+          document.querySelector("div.server-info-wrapper>h1")?.textContent,
+        url: window.location.href
       }
     ];
   }
