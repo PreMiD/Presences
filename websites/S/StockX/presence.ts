30df91d77526c6de2b8fded6f4025bba68a907cf
Nam Anh <phamnamanh25@gmail.com>
2022-01-23 13:29:30 +0700

    refactor: trim `querySelector`s

--- websites/S/StockX/presence.ts
+++ websites/S/StockX/presence.ts
@@ -10,7 +10,7 @@ presence.on("UpdateData", async () => {
 			startTimestamp: browsingTimestamp
 		},
 		productName = document.querySelector<HTMLHeadingElement>(
-			"#main-content > div > section:nth-child(3) > div.css-j7qwjs > div.css-0 > h1"
+			"#main-content > div > section:nth-child(3) > div > div.css-0 > h1"
 		),
 		{ pathname } = document.location,
 		[price, image, buttons] = await Promise.all([
@@ -21,7 +21,7 @@ presence.on("UpdateData", async () => {
 	if (pathname.startsWith("/search")) {
 		presenceData.details = "Searching For:";
 		presenceData.state = document.querySelector<HTMLParagraphElement>(
-			"#browse-wrapper > div > div > div.css-c8gdzb > div.css-b1ilzc > div > div:nth-child(1) > div > div.css-0 > nav > ol > li:nth-child(3) > p"
+			"#browse-wrapper nav > ol > li:nth-child(3) > p"
 		).textContent;
 		presenceData.smallImageKey = "search";
 	} else if (pathname.startsWith("/buy") || pathname.startsWith("/sell")) {
@@ -31,11 +31,11 @@ presence.on("UpdateData", async () => {
 				presenceData.details = "Buying a Product/Placing a Bid:";
 			else presenceData.details = "Selling an Item:";
 			presenceData.state = document.querySelector<HTMLHeadingElement>(
-				"#main-content > div > div > div.pane.sixty.css-12aduxl > div > div.css-b0dyos > h1"
+				"#main-content > div > div > div > div > div> h1"
 			).textContent;
 			presenceData.largeImageKey =
 				document.querySelector<HTMLImageElement>(
-					"#main-content > div > div > div.pane.sixty.css-12aduxl > div > div.css-1ru79ae > img"
+					"#main-content > div > div > div > div > div > img"
 				)?.src ?? "logo";
 		}
 	} else if (pathname.startsWith("/news/")) {
@@ -54,26 +54,24 @@ presence.on("UpdateData", async () => {
 			presenceData.details = `Viewing: ${productName.textContent}`;
 			presenceData.state = `${
 				document.querySelector<HTMLLIElement>(
-					"#main-content > div > section:nth-child(3) > div.css-gg4vpm > div.css-0 > div.css-qt7qal > div.chakra-stack.css-12vwdz3 > div:nth-child(1) > div > dl > dd"
+					"#main-content div:nth-child(1) > div > dl > dd"
 				).textContent
 			} / ${
 				document.querySelector<HTMLLIElement>(
-					"#main-content > div > section:nth-child(3) > div.css-gg4vpm > div.css-0 > div.css-qt7qal > div.chakra-stack.css-12vwdz3 > div:nth-child(3) > div > dl > dd"
+					"#main-content div:nth-child(3) > div > dl > dd"
 				).textContent
 			}`;
 			presenceData.largeImageKey =
-				document.querySelector<HTMLImageElement>(
-					":is(div.css-sn7afr > div > div > img, div > div > div > img)"
-				)?.src ?? "logo";
+				document.querySelector<HTMLImageElement>("div > div > div > img")
+					?.src ?? "logo";
 			presenceData.smallImageKey = "search";
 			presenceData.buttons = [{ label: "View Product", url: document.URL }];
 		} else {
 			presenceData.details = "Viewing Product:";
 			presenceData.state = productName.textContent;
 			presenceData.largeImageKey =
-				document.querySelector<HTMLImageElement>(
-					":is(div.css-sn7afr > div > div > img, div > div > div > img)"
-				)?.src ?? "logo";
+				document.querySelector<HTMLImageElement>("div > div > div > img")
+					?.src ?? "logo";
 			presenceData.smallImageKey = "search";
 		}
 	} else if (
