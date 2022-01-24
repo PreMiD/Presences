4679de155639f7c0244e0df9aba3052b1ad9109a
critamine <cstempho@live.com>
2022-01-24 13:27:55 -0500

    refactor(ArchLinux): added support for states

--- websites/A/ArchLinux/presence.ts
+++ websites/A/ArchLinux/presence.ts
@@ -2,9 +2,12 @@ const presence = new Presence({
     clientId: "929881116679237653"
   }),
   browsingTimestamp = Math.floor(Date.now() / 1000),
-  hostName = document.location.hostname,
-  webPath = document.location.pathname;
-
+  hostName = location.hostname,
+  webPath = location.pathname,
+  pathArr = webPath.split("/"),
+  path0 = pathArr[0],
+  path1 = pathArr[1],
+  data = "";
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
     largeImageKey: "arch",
@@ -13,80 +16,147 @@ presence.on("UpdateData", async () => {
 
   type dt = {
     [hostName: string]: {
-      [webPath: string]: string;
+      [webPath: string]: {
+        [data: string]: string;
+      };
     };
   };
 
   const archData: dt = {
     "archlinux.org": {
-      root: "Browsing the home page",
-      news: "Browsing the news archives",
-      donate: "Browsing the donation page",
-      mirrorlist: "Browsing the Mirrorlist",
-      mirrors: "Browsing the Mirrorlist",
-      groups: "Browsing the package groups",
-      todo: "Browsing the todo lists"
+      "": {
+        details: "Browsing the home page",
+        state: "h2"
+      },
+      packages: {
+        details: "Browsing the official repo",
+        state: "h2"
+      },
+      news: {
+        details: "Browsing the news archives",
+        state: "h2"
+      },
+      donate: {
+        details: "Browsing the donation page"
+      },
+      mirrorlist: {
+        details: "Browsing the Mirrorlist"
+      },
+      mirrors: {
+        details: "Browsing the Mirrorlist"
+      },
+      groups: {
+        details: "Browsing the package groups",
+        state: "h2"
+      },
+      todo: {
+        details: "Browsing the todo lists",
+        state: "h2"
+      },
+      people: {
+        details: "Browsing the people of Arch",
+        state: "h2"
+      }
     },
     "bbs.archlinux.org": {
-      root: "Browsing the forums"
+      "": {
+        details: "Browsing the forums"
+      }
     },
     "wiki.archlinux.org": {
-      root: "Browsing the Wiki"
+      "": {
+        details: "Browsing the Wiki"
+      }
     },
     "bugs.archlinux.org": {
-      root: "Browsing the Bugtracker"
+      "": {
+        details: "Browsing the Bugtracker"
+      }
     },
     "security.archlinux.org": {
-      root: "Browsing Security Issues"
+      "": {
+        details: "Browsing Security Issues"
+      }
     },
     "aur.archlinux.org": {
-      root: "Browsing the user repository"
+      "": {
+        details: "Browsing the user repository"
+      }
     },
     "status.archlinux.org": {
-      root: "Browsing the status dashboard"
+      "": {
+        details: "Browsing the status dashboard"
+      }
     },
     "lists.archlinux.org": {
-      root: "Browsing the mailing lists"
+      "": {
+        details: "Browsing the mailing lists"
+      }
     },
     "archive.archlinux.org": {
-      root: "Browsing the archive"
+      "": {
+        details: "Browsing the archive"
+      }
     },
     "matrix.archlinux.org": {
-      root: "Browsing nothing of importance..."
+      "": {
+        details: "Browsing nothing of importance..."
+      }
     },
     "dashboards.archlinux.org": {
-      root: "Browsing the Grafana dashboards"
+      "": {
+        details: "Browsing the Grafana dashboards"
+      }
     },
     "conf.archlinux.org": {
-      root: "Browsing the Arch Conferences"
+      "": {
+        details: "Browsing the Arch Conferences"
+      }
     },
     "whatcanidofor.archlinux.org": {
-      root: "Learning how to help Arch Linux"
+      "": {
+        details: "Learning how to help Arch Linux"
+      }
     },
     "reproducible.archlinux.org": {
-      root: "Browsing the Reproducible Status"
+      "": {
+        details: "Browsing the Reproducible Status"
+      }
     },
     "aur-dev.archlinux.org": {
-      root: "Browsing the AUR-DEV Repository"
+      "": {
+        details: "Browsing the AUR-DEV Repository"
+      }
     },
     "terms.archlinux.org": {
-      root: "Browsing the terms of service"
+      "": {
+        details: "Browsing the terms of service"
+      }
     },
     "gitlab.archlinux.org": {
-      root: "Browsing the GitLab repository"
+      "": {
+        details: "Browsing the GitLab repository"
+      }
     },
     "patchwork.archlinux.org": {
-      root: "Browsing the patch tracking system"
+      "": {
+        details: "Browsing the patch tracking system"
+      }
     },
     "man.archlinux.org": {
-      root: "Browsing the manual pages"
+      "": {
+        details: "Browsing the manual pages"
+      }
     }
   };
 
-  if (hostName in archData) {
-    if (webPath.split("/")[1] in archData[hostName])
-      presenceData.details = archData[hostName][webPath.split("/")[1]];
-    else presenceData.details = archData[hostName].root;
+  if (hostName in archData && path1 in archData[hostName]) {
+    presenceData.details = archData[hostName][path1].details;
+    if (archData[hostName][path1].state) {
+      presenceData.state = document
+        .getElementsByTagName(archData[hostName][path1].state)
+        .item(0).innerHTML;
+    }
   }
 
   if (presenceData.details) presence.setActivity(presenceData);
