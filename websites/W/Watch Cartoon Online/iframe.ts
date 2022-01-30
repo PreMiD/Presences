22d28d11729466d98a352760b18a792b2af1979e
Nam Anh <phamnamanh25@gmail.com>
2022-01-30 11:43:49 +0700

    feat(Watch Cartoon Online): add presence

---
+++ websites/W/Watch Cartoon Online/iframe.ts
@@ -0,0 +1,15 @@
+const iframe = new iFrame();
+
+iframe.on("UpdateData", async () => {
+	if (document.querySelector<HTMLVideoElement>("#video-js_html5_api")) {
+		iframe.send({
+			paused: document
+				.querySelector<HTMLButtonElement>(
+					"#video-js > div.vjs-control-bar > button"
+				)
+				.title.includes("Play"),
+			timeLeft: document.querySelector("span.vjs-remaining-time-display")
+				.textContent
+		});
+	}
+});
