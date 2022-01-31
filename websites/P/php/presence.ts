263526652059008946d3b25dc3a52de62bcaa2c8
Kirihito <satowa.network@gmail.com>
2022-01-31 13:39:44 +0100

    add Presence
    Presence for the php web page.

---
+++ websites/P/php/presence.ts
@@ -0,0 +1,27 @@
+const presence = new Presence({
+		clientId: "937645461467197470"
+	}),
+	presenceData: PresenceData = {
+		largeImageKey: "logo"
+	},
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+// eslint-disable-next-line no-console
+console.log("Presence Started...");
+presence.on("UpdateData", async () => {
+	if (document.location.hostname === "www.php.net" || "php.net") {
+		presenceData.startTimestamp = browsingTimestamp;
+		presenceData.details = "Viewing Page:";
+		presenceData.state = document.title;
+		//<===================================================>
+		//				Startpage
+		//<===================================================>
+		if (document.location.pathname === "/") {
+			presenceData.startTimestamp = browsingTimestamp;
+			presenceData.details = "Viewing Page:";
+			presenceData.state = document.title;
+		}
+	}
+	presenceData.largeImageKey = "php_standalone";
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
