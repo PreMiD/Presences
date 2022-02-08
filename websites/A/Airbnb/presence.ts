67c003b635e967210afe72c3d102cb891b2f16e0
Benjamin Vimeux <vimeux.benji@gmail.com>
2022-02-08 22:32:04 +0100

    feat(Airbnb): show searching city

--- websites/A/Airbnb/presence.ts
+++ websites/A/Airbnb/presence.ts
@@ -2,48 +2,32 @@ const presence = new Presence({
 		//The client ID of the Application created at https://discordapp.com/developers/applications
 		clientId: "940342152494452806"
 	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000),
-	//You can use this to get translated strings in their browser language
-	strings = presence.getStrings({
-		play: "presence.playback.playing",
-		pause: "presence.playback.paused"
-	});
-
-/*
-function myOutsideHeavyLiftingFunction(){
-    //Grab and process all your data here
-
-    // element grabs //
-    // api calls //
-    // variable sets //
-}
-
-setInterval(myOutsideHeavyLiftingFunction, 10000);
-//Run the function separate from the UpdateData event every 10 seconds to get and set the variables which UpdateData picks up
-*/
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-	/*UpdateData is always firing, and therefore should be used as your refresh cycle, or `tick`. This is called several times a second where possible.
-
-    It is recommended to set up another function outside of this event function which will change variable values and do the heavy lifting if you call data from an API.*/
-
 	const presenceData: PresenceData = {
-		//The large image on the presence. This can be a key of an image uploaded on the Discord Developer Portal - Rich Presence - Art Assets, or a URL to an image
+		details: "Search host",
 		largeImageKey: "airbnb-big",
-		//The text which is displayed when hovering over the small image
-		smallImageText: "Some hover text",
-		//The upper section of the presence text
-		details: "Browsing Page Name",
-		//The lower section of the presence text
-		state: "Reading section A",
-		//The unix epoch timestamp for when to start counting from
-		startTimestamp: 3133657200000,
-		//If you want to show Time Left instead of Elapsed, this is the unix epoch timestamp at which the timer ends
-		endTimestamp: 3133700400000
-		//Optionally you can set a largeImageKey here and change the rest as variable subproperties, for example presenceData.type = "blahblah"; type examples: details, state, etc.
-	};
-	//Update the presence with all the values from the presenceData object
+		startTimestamp: browsingTimestamp
+	}
+
+	if (document.location.pathname.includes("/s/")) {
+		// search
+		if (document.querySelector("button._b2fxuo > span.a8jt5op")?.textContent) {
+			// split the url to retrieve only the name of the city or region searched
+			const destination = location.pathname.split("/")[2].split("-")[0];
+			// decodes the url to display the accents correctly
+			const decodedDestination = decodeURI(destination);
+			presenceData.details = "Search hosting in :"
+			presenceData.state = `${decodedDestination}`;
+		}
+		if (document.location.pathname.includes("/experiences")) {
+			presenceData.details = "Search an experience"
+		}
+	} else {
+		presenceData.details = "Viewing homepage"
+	}
+
 	if (presenceData.details) presence.setActivity(presenceData);
-	//Update the presence with no data, therefore clearing it and making the large image the Discord Application icon, and the text the Discord Application name
 	else presence.setActivity();
 });
