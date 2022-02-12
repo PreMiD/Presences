a0d027d5c32cf91553f39392cf658df3b754d5c4
立花 瀧 <59746573+ndbiaw@users.noreply.github.com>
2022-02-13 03:52:37 +0700

    Update presence.ts

--- websites/H/HVNC/presence.ts
+++ websites/H/HVNC/presence.ts
@@ -2,7 +2,7 @@ const presence = new Presence({
 		clientId: "941317056589086730"
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
-let title, searchstr, page, edit;
+let title, searchstr, page, edit, progress;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
@@ -57,8 +57,12 @@ presence.on("UpdateData", async () => {
 		presenceData.smallImageKey = "sett";
 		presenceData.details = "Configuring...";
 	} else if (document.location.pathname.includes("/batch")) {
+		progress = document
+			.querySelector("div[id='progress']")
+			.textContent.trim();
 		presenceData.smallImageKey = "sett";
 		presenceData.details = "Running Batch Operations...";
+		presenceData.state = progress;
 	} else if (document.location.pathname.startsWith("/edit")) {
 		presenceData.smallImageKey = "sett";
 		edit = document.querySelector("h2").textContent.trim();
