9ae989247fbe1031c6decf5d55af8e79d8304802
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-02-12 08:12:44 +0100

    viewing category

--- websites/C/CDKeys/presence.ts
+++ websites/C/CDKeys/presence.ts
@@ -26,15 +26,32 @@ presence.on("UpdateData", async () => {
 		presenceData.details = document.querySelector<HTMLMetaElement>(
 			"meta[property='og:title']"
 		).content;
-	} else if (page.includes("/pc")) presenceData.details = "Pc Games";
-	else if (page.includes("-psn")) presenceData.details = "Playstation Games";
-	else if (page.includes("/xbox-live")) presenceData.details = "Xbox Games";
-	else if (page.includes("/nintendo")) presenceData.details = "Nintendo Games";
-	else if (page.includes("/top-up-cards")) presenceData.details = "Top up card";
-	else if (page.includes("/sale")) presenceData.details = "Sale";
-	else if (page.includes("/daily-deals")) presenceData.details = "Daily Deals";
-	else if (page.includes("/new")) presenceData.details = "New Products";
-	else if (page.includes("/coming-soon")) presenceData.details = "Coming Soon";
+	} else if (page.includes("/pc")) {
+		presenceData.details = "Viewing Category:";
+		presenceData.state = "Pc Games";
+	} else if (page.includes("-psn")) {
+		presenceData.details = "Viewing Category:";
+		presenceData.state = "Playstation Games";
+	} else if (page.includes("/xbox-live")) {
+		presenceData.details = "Viewing Category:";
+		presenceData.state = "Xbox Games";
+	} else if (page.includes("/nintendo")) {
+		presenceData.details = "Viewing Category:";
+		presenceData.state = "Nintendo Games";
+	} else if (page.includes("/top-up-cards")) {
+		presenceData.details = "Viewing Category:";
+		presenceData.state = "Top Up Cards";
+	} else if (page.includes("/sale")) {
+		presenceData.details = "Viewing Category:";
+		presenceData.state = "Sale";
+	} else if (page.includes("/daily-deals")) {
+		presenceData.details = "Viewing Category:";
+		presenceData.state = "Daily Deals";
+	} else if (page.includes("/new")) {
+		presenceData.details = "Viewing Category:";
+		presenceData.state = "New Products";
+	} else if (page.includes("/coming-soon"))
+		presenceData.details = "Coming Soon";
 	else if (page.includes("/wishlist")) presenceData.details = "Wishlist";
 	else if (page.includes("/cart")) presenceData.details = "Cart";
 	else if (page.includes("order/history"))
