2303dcb18b9ce4417cf86bf56fd9a50cc3f06d86
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-02-12 08:10:11 +0100

    general strings

--- websites/L/LookMovie/presence.ts
+++ websites/L/LookMovie/presence.ts
@@ -2,8 +2,8 @@ const presence = new Presence({
 		clientId: "934789855962083359"
 	}),
 	strings = presence.getStrings({
-		play: "presence.playback.playing",
-		pause: "presence.playback.paused",
+		play: "general.playing",
+		pause: "general.paused",
 		browsing: "general.browsing"
 	});
 
