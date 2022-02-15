7aca888f01de711458d83aa483158b149bed9073
立花 瀧 <59746573+ndbiaw@users.noreply.github.com>
2022-02-16 05:59:50 +0700

    feat(HVNC): add presence

--- websites/H/HVNC/presence.ts
+++ websites/H/HVNC/presence.ts
@@ -4,64 +4,69 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-	const inPath = document.location.pathname,
-		select = document.querySelector,
-		presenceData: PresenceData = {
-			largeImageKey: "logo",
-			startTimestamp: browsingTimestamp
-		};
+	const presenceData: PresenceData = {
+		largeImageKey: "logo",
+		startTimestamp: browsingTimestamp
+	};
 
 	if (new URLSearchParams(window.location.search).has("q")) {
 		presenceData.details = "Searching For:";
-		presenceData.state = `${select("input").value} - Page ${select(
-			"a[class='paginate_button current']"
-		).textContent.trim()}`;
+		presenceData.state = `${
+			document.querySelector("input").value
+		} - Page ${document
+			.querySelector("a[class='paginate_button current']")
+			.textContent.trim()}`;
 		presenceData.smallImageKey = "search";
-	} else if (inPath === "/") {
+	} else if (document.location.pathname === "/") {
 		presenceData.details = "Viewing Homepage";
-		presenceData.state = `Page ${select(
-			"a[class='paginate_button current']"
-		).textContent.trim()}`;
-	} else if (inPath.includes("/reader")) {
+		presenceData.state = `Page ${document
+			.querySelector("a[class='paginate_button current']")
+			.textContent.trim()}`;
+	} else if (document.location.pathname.includes("/reader")) {
 		presenceData.smallImageKey = "read";
 		presenceData.details = "Reading:";
-		presenceData.state = `${select("title").textContent.trim()} - Page ${select(
-			"span[class='current-page']"
-		).textContent.trim()}`;
+		presenceData.state = `${document
+			.querySelector("title")
+			.textContent.trim()} - Page ${document
+			.querySelector("span[class='current-page']")
+			.textContent.trim()}`;
 		presenceData.buttons = [
 			{
 				label: "Read along",
-				url: `${document.URL}&p=${select(
-					"span[class='current-page']"
-				).textContent.trim()}`
+				url: `${document.URL}&p=${document
+					.querySelector("span[class='current-page']")
+					.textContent.trim()}`
 			}
 		];
-	} else if (inPath.includes("/stats")) {
+	} else if (document.location.pathname.includes("/stats")) {
 		presenceData.smallImageKey = "read";
 		presenceData.details = "Viewing Statistics";
-	} else if (inPath.includes("/login")) {
+	} else if (document.location.pathname.includes("/login")) {
 		presenceData.smallImageKey = "sett";
 		presenceData.details = "Logging In...";
-	} else if (inPath.includes("/logs")) {
+	} else if (document.location.pathname.includes("/logs")) {
 		presenceData.smallImageKey = "sett";
 		presenceData.details = "Viewing Logs";
-	} else if (inPath.includes("/upload")) {
+	} else if (document.location.pathname.includes("/upload")) {
 		presenceData.smallImageKey = "sett";
 		presenceData.details = "Adding Archives...";
-		presenceData.state = select<HTMLDivElement>(".bar").style.width;
-	} else if (inPath.startsWith("/config/categories/")) {
+		presenceData.state =
+			document.querySelector<HTMLDivElement>(".bar").style.width;
+	} else if (document.location.pathname.startsWith("/config/categories/")) {
 		presenceData.smallImageKey = "sett";
 		presenceData.details = "Editing Category...";
-	} else if (inPath.includes("/config")) {
+	} else if (document.location.pathname.includes("/config")) {
 		presenceData.smallImageKey = "sett";
 		presenceData.details = "Configuring...";
-	} else if (inPath.includes("/batch")) {
+	} else if (document.location.pathname.includes("/batch")) {
 		presenceData.smallImageKey = "sett";
 		presenceData.details = "Running Batch Operations...";
-		presenceData.state = select("div[id='progress']").textContent.trim();
-	} else if (inPath.startsWith("/edit")) {
+		presenceData.state = document
+			.querySelector("div[id='progress']")
+			.textContent.trim();
+	} else if (document.location.pathname.startsWith("/edit")) {
 		presenceData.smallImageKey = "sett";
-		presenceData.details = select("h2").textContent.trim();
+		presenceData.details = document.querySelector("h2").textContent.trim();
 	}
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
