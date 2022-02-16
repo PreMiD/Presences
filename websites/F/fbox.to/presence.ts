d2453f056f69c08d280aa84dc195c7beca3a2095
Nam Anh <phamnamanh25@gmail.com>
2022-02-16 16:59:15 +0700

    feat(Fbox.to): fix presence & add poster image

--- websites/F/fbox.to/presence.ts
+++ websites/F/fbox.to/presence.ts
@@ -22,27 +22,27 @@ presence.on("UpdateData", async () => {
 			largeImageKey: "fbox_logo"
 		},
 		{ pathname } = document.location,
-		buttons = await presence.getSetting<boolean>("buttons");
+		[buttons, image] = await Promise.all([
+			presence.getSetting<boolean>("buttons"),
+			presence.getSetting<boolean>("image")
+		]);
 
-	if (pathname === "/") presenceData.details = "Browsing";
+	if (pathname === "/home") presenceData.details = "Browsing";
 	else if (pathname.startsWith("/series/")) {
 		const title: HTMLHeadingElement = document.querySelector(
 				"#watch > div.container > div.watch-extra > div.bl-1 > section.info > div.info > h1"
 			),
-			season: HTMLDataListElement = document.querySelector(
-				"#episodes > div.bl-seasons > ul > li.active"
-			),
-			date: HTMLSpanElement = season ? season.querySelector("span") : null,
-			episode: HTMLAnchorElement = document.querySelector(
-				".episodes > li > a.active"
-			);
+			season: HTMLSpanElement = document.querySelector(".value"),
+			episode: HTMLAnchorElement = document.querySelector("a.active");
 		if (title) presenceData.details = title.textContent;
-		if (season && date) {
-			presenceData.state = season.textContent.substring(
-				0,
-				season.textContent.indexOf(date.textContent)
-			);
-			if (episode) presenceData.state += episode.textContent;
+		if (season) {
+			presenceData.state = season.textContent.split("-")[0].trim();
+			if (episode) presenceData.state += ` - ${episode.textContent.trim()}`;
+		}
+		if (image) {
+			presenceData.largeImageKey = document
+				.querySelector("meta[property='og:image']")
+				.getAttribute("content");
 		}
 		if (iFrameData && !iFrameData.paused) {
 			[, presenceData.endTimestamp] = presence.getTimestamps(
@@ -64,6 +64,11 @@ presence.on("UpdateData", async () => {
 			"#watch > div.container > div.watch-extra > div.bl-1 > section.info > div.info > h1"
 		);
 		if (title) presenceData.details = title.textContent;
+		if (image) {
+			presenceData.largeImageKey = document
+				.querySelector("meta[property='og:image']")
+				.getAttribute("content");
+		}
 		if (iFrameData && !iFrameData.paused) {
 			[, presenceData.endTimestamp] = presence.getTimestamps(
 				iFrameData.currTime,
@@ -84,15 +89,12 @@ presence.on("UpdateData", async () => {
 	else if (pathname === "/user/watchlist")
 		presenceData.details = "Checking Watchlist";
 	else {
-		const genre: HTMLHeadingElement = document.querySelector(
-			"#body > div > div.col-left > section > div.heading > h1"
-		);
+		const genre: HTMLHeadingElement = document.querySelector("section.bl h1");
 		if (genre) {
 			presenceData.details = genre.textContent;
 			presenceData.smallImageKey = "search";
 		}
 	}
 
-	if (presenceData.details) presence.setActivity(presenceData);
-	else presence.setActivity();
+	presence.setActivity(presenceData);
 });
