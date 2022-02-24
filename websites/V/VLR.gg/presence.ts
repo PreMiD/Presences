257675f4170d355f22b0d6cadb3d4354333ff2cd
Madson Mendes <50425778+MadsonMendes@users.noreply.github.com>
2022-02-23 22:56:41 -0300

    Update presence.ts

--- websites/V/VLR.gg/presence.ts
+++ websites/V/VLR.gg/presence.ts
@@ -2,11 +2,14 @@ const presence = new Presence({
 		clientId: "939236497985536070"
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
-let title: HTMLElement,
-	rankinginfo: HTMLElement,
-	teamicon: HTMLImageElement,
-	eventicon: HTMLImageElement,
-	playername: HTMLElement;
+let title: HTMLElement;
+
+let rankinginfo: HTMLElement;
+
+let teamicon: HTMLImageElement;
+let eventicon: HTMLImageElement;
+
+let playername: HTMLElement;
 
 presence.on("UpdateData", async () => {
 	title = document.querySelector("head > title");
@@ -26,7 +29,8 @@ presence.on("UpdateData", async () => {
 		presenceData.details = "On home";
 		delete presenceData.state;
 	} else if (document.location.pathname.includes("/threads")) {
-		/*Main pages*/
+
+	/*Main pages*/
 		presenceData.details = "Browsing threads";
 		delete presenceData.state;
 	} else if (document.location.pathname.includes("/matches")) {
@@ -45,14 +49,17 @@ presence.on("UpdateData", async () => {
 		presenceData.details = "Viewing statistics";
 		delete presenceData.state;
 	} else if (document.location.pathname.includes("/user")) {
-		presenceData.details =
-			"Viewing" + title.textContent.replace("Profile | VLR.gg", "") + "profile";
+		presenceData.details = `Viewing ${title.textContent.replace(
+			"Profile | VLR.gg",
+			""
+		)} profile`;
 		delete presenceData.state;
 	} else if (document.location.pathname.includes("/settings")) {
 		presenceData.details = "Viewing settings";
 		delete presenceData.state;
 	} else if (document.location.pathname.includes("/event")) {
-		/*Show event name and logo*/
+
+	/*Show event name and logo*/
 		presenceData.details = "Viewing event";
 		presenceData.state = title.textContent;
 		presenceData.state = title.textContent.replace(
@@ -61,7 +68,8 @@ presence.on("UpdateData", async () => {
 		);
 		presenceData.largeImageKey = eventicon.src;
 	} else if (document.location.pathname.includes("/team")) {
-		/*Show team logo*/
+
+	/*Show team logo*/
 		presenceData.details = "Viewing team page";
 		presenceData.state = title.textContent.replace(
 			": Valorant Team Profile | VLR.gg",
@@ -69,13 +77,13 @@ presence.on("UpdateData", async () => {
 		);
 		presenceData.largeImageKey = teamicon.src;
 	} else if (document.location.pathname.includes("/player")) {
-		/*Viewing player's page*/
+
+	/*Viewing player's page*/
 		presenceData.details = "Viewing player's page";
-		presenceData.state =
-			title.textContent.replace(": Valorant Player Profile | VLR.gg", "") +
-			"(" +
-			playername.textContent +
-			")";
+		presenceData.state = `${title.textContent.replace(
+			": Valorant Player Profile | VLR.gg",
+			""
+		)}(${playername.textContent})`;
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
