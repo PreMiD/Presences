c39e5608364931ffcf05ee52272ed9777dd2cde5
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-03-11 16:03:18 +0100

    feat(Q-dance): add cover
    feat(Q-dance): add cover

--- websites/Q/Q-dance/presence.ts
+++ websites/Q/Q-dance/presence.ts
@@ -1,9 +1,6 @@
 const presence = new Presence({
 		clientId: "844107169205190686"
 	}),
-	strings = presence.getStrings({
-		live: "presence.activity.live"
-	}),
 	elapsed = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
@@ -11,15 +8,27 @@ presence.on("UpdateData", async () => {
 		largeImageKey: "logo"
 	};
 	if (document.querySelector("svg.audioplayer-controls__icon--play")) {
-		(presenceData.details = document.querySelector(
+		presenceData.details = document.querySelector(
 			".audioplayer-nowplaying__track"
-		).textContent),
-			(presenceData.state = document.querySelector(
-				".audioplayer-nowplaying__artist"
-			).textContent),
-			(presenceData.smallImageKey = "live"),
-			(presenceData.smallImageText = (await strings).live);
+		).textContent;
+		presenceData.state = document.querySelector(
+			".audioplayer-nowplaying__artist"
+		).textContent;
+		presenceData.smallImageKey = "live";
+		presenceData.smallImageText = `Volume: ${document
+			.querySelector(
+				"#app > footer > div > div.audioplayer__wrapper > div.audioplayer-controls.audioplayer__controls > div.audioplayer-controls__right > div > div > div"
+			)
+			.getAttribute("style")
+			.replace("width:", "")
+			.replace(/;/, "")}`;
+		presenceData.largeImageKey =
+			document
+				.querySelector(
+					"#app > footer > div > div.audioplayer__wrapper > div.audioplayer-nowplaying.audioplayer__nowplaying > div.audioplayer-nowplaying__image > img"
+				)
+				?.getAttribute("src") ?? "logo";
 		presenceData.startTimestamp = elapsed;
 		presence.setActivity(presenceData);
-	} else presence.clearActivity();
+	} else presence.setActivity();
 });
