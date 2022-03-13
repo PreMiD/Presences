2e98374736ee16b16c30d5e927e5dc5683f1c4c1
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-03-13 14:38:28 +0100

    feat: add more URLs

--- websites/V/VanMoof/presence.ts
+++ websites/V/VanMoof/presence.ts
@@ -189,10 +189,24 @@ presence.on("UpdateData", async function () {
 			switch (urlpath[2]) {
 				case "electric-bikes":
 				case "ebikes":
-					presenceData.details = "Viewing E-Bikes";
+				case "velos-electriques":
+				case "elektrische-fietsen":
+					switch (urlpath[3]) {
+						case "powerbank":
+							presenceData.details = "Viewing PowerBank";
 
-					if (urlpath[3] === "powerbank")
-						presenceData.details = "Viewing PowerBank";
+							break;
+
+						case "app":
+							presenceData.details = "Viewing App";
+
+							break;
+
+						default:
+							presenceData.details = "Viewing E-Bikes";
+
+							break;
+					}
 
 					break;
 
@@ -245,12 +259,28 @@ presence.on("UpdateData", async function () {
 					break;
 
 				case "company-bikes":
+				case "firmenfahrräder":
 					presenceData.details = "Viewing Company Bikes";
 
 					break;
 
 				case "certified-partner":
 					presenceData.details = "About Certified Partnerships";
+
+					break;
+
+				case "shipping":
+					presenceData.details = "Viewing Shipping Information";
+
+					break;
+
+				case "returns":
+					presenceData.details = "Viewing Return Policy";
+
+					break;
+
+				case "warranty":
+					presenceData.details = "Viewing Warranty Information";
 
 					break;
 			}
