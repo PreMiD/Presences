d615b00fe1a0091229d61f605607d9b0f7698ab3
iLazlow <karr54@hotmail.de>
2022-03-13 17:14:42 +0100

    fixed some code issues

--- websites/P/PHP.net/presence.ts
+++ websites/P/PHP.net/presence.ts
@@ -1,17 +1,17 @@
 const presence = new Presence({
-    clientId: "952575137180442626"
-  }),
+  clientId: "952575137180442626"
+}),
   browsingStamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
   const data: PresenceData = {
-      largeImageKey: "php_logo",
-      startTimestamp: browsingStamp
-    },
+    largeImageKey: "php_logo",
+    startTimestamp: browsingStamp
+  },
     route = document.location.pathname.split("/");
   if (route.length === 1 || route[1] === "")
     data.details = "Browsing the main page...";
-  else if (route[1] === "downloads" || route[1] === "downloads.php") 
+  else if (route[1] === "downloads" || route[1] === "downloads.php")
     data.details = "Browsing the download section";
   else if (route[1] === "docs.php")
     data.details = "Browsing the documentation";
@@ -21,15 +21,15 @@ presence.on("UpdateData", () => {
     data.details = "Reading the changelogs";
   else if (route[1] === "download-logos.php")
     data.details = "Viewing the official logos";
-  else if (route[1] === "manual"){
+  else if (route[1] === "manual") {
     data.details = "Viewing the documentation: ";
 
     let manualTitle = document.title.replaceAll("PHP: ", "").replaceAll(" - Manual", "");
-    if (route[3].includes("function.")){
+    if (route[3].includes("function.")) {
       data.state = `Function: ${manualTitle}`;
     } else if (route[3].includes("language.")) {
       let c = route[3].split(".")[1];
-      data.state = `${c.charAt(0).toUpperCase()+c.slice(1)}: ${manualTitle}`;
+      data.state = `${c.charAt(0).toUpperCase() + c.slice(1)}: ${manualTitle}`;
     } else {
       data.state = manualTitle;
     }
