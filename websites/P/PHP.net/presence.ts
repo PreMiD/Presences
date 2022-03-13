5707c736abbe9ccac3a649d0ecb3dc72bd717c21
iLazlow <karr54@hotmail.de>
2022-03-13 18:09:12 +0100

    Updated presence.ts

--- websites/P/PHP.net/presence.ts
+++ websites/P/PHP.net/presence.ts
@@ -9,33 +9,50 @@ presence.on("UpdateData", () => {
 			startTimestamp: browsingStamp
 		},
 		route = document.location.pathname.split("/");
-	if (route.length === 1 || route[1] === "")
-		data.details = "Browsing the main page...";
-	else if (route[1] === "downloads" || route[1] === "downloads.php")
-		data.details = "Browsing the download section";
-	else if (route[1] === "docs.php") data.details = "Browsing the documentation";
-	else if (route[1] === "download-docs.php")
-		data.details = "Downloading the documentation";
-	else if (route[1].toLowerCase().includes("changelog"))
-		data.details = "Reading the changelogs";
-	else if (route[1] === "download-logos.php")
-		data.details = "Viewing the official logos";
-	else if (route[1] === "manual") {
-		data.details = "Viewing the documentation: ";
 
-		const manualTitle = document.title
-			.replaceAll("PHP: ", "")
-			.replaceAll(" - Manual", "");
-		if (route[3].includes("function.")) data.state = `Function: ${manualTitle}`;
-		else if (route[3].includes("language.")) {
-			const c = route[3].split(".")[1];
-			data.state = `${c.charAt(0).toUpperCase() + c.slice(1)}: ${manualTitle}`;
-		} else data.state = manualTitle;
-	} else if (route[1] === "releases") {
-		if (route[2] != "") {
-			data.details = "Viewing an release version:";
-			data.state = `PHP v${route[2]}`;
-		} else data.details = "Viewing the releases";
-	} else data.details = "Browsing the website";
+	switch (route[1]) {
+		case "":
+			data.details = "Browsing the main page...";
+			break;
+		case "downloads":
+		case "downloads.php":
+			data.details = "Browsing the download section";
+			break;
+		case "docs.php":
+			data.details = "Browsing the documentation";
+			break;
+		case "download-docs.php":
+			data.details = "Downloading the documentation";
+			break;
+		case "download-logos.php":
+			data.details = "Viewing the official logos";
+			break;
+		case "manual":
+			data.details = "Viewing the documentation: ";
+			const manualTitle = document.title
+				.replaceAll("PHP: ", "")
+				.replaceAll(" - Manual", "");
+			if (route[3].includes("function."))
+				data.state = `Function: ${manualTitle}`;
+			else if (route[3].includes("language.")) {
+				const c = route[3].split(".")[1];
+				data.state = `${
+					c.charAt(0).toUpperCase() + c.slice(1)
+				}: ${manualTitle}`;
+			} else data.state = manualTitle;
+			break;
+		case "releases":
+			if (route[2] != "") {
+				data.details = "Viewing an release version:";
+				data.state = `PHP v${route[2]}`;
+			} else data.details = "Viewing the releases";
+			break;
+		default:
+			data.details = "Browsing the website";
+	}
+
+	if (route[1].toLowerCase().includes("changelog"))
+		data.details = "Reading the changelogs";
+
 	presence.setActivity(data);
 });
