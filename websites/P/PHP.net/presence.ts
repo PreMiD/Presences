0a2b3fc97c2c72c91b17b50d73d1697c96291926
iLazlow <karr54@hotmail.de>
2022-03-14 23:24:06 +0100

    Made some changes as requested

--- websites/P/PHP.net/presence.ts
+++ websites/P/PHP.net/presence.ts
@@ -1,66 +1,68 @@
 const presence = new Presence({
 		clientId: "952575137180442626"
 	}),
-	browsingStamp = Math.floor(Date.now() / 1000);
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
-	const data: PresenceData = {
+	const presenceData: PresenceData = {
 			largeImageKey: "php_logo",
-			startTimestamp: browsingStamp
+			startTimestamp: browsingTimestamp
 		},
 		route = document.location.pathname.split("/");
-	data.buttons = [{ label: "View Website", url: document.location.href }];
+	presenceData.buttons = [
+		{ label: "View Website", url: document.location.href }
+	];
 
 	switch (route[1]) {
 		case "":
-			data.details = "Browsing the main page...";
+			presenceData.details = "Browsing the main page...";
 			break;
 		case "downloads":
 		case "downloads.php":
-			data.details = "Browsing the download section";
+			presenceData.details = "Browsing the download section";
 			break;
 		case "docs.php":
-			data.details = "Browsing the documentation";
+			presenceData.details = "Browsing the documentation";
 			break;
 		case "download-docs.php":
-			data.details = "Downloading the documentation";
+			presenceData.details = "Downloading the documentation";
 			break;
 		case "download-logos.php":
-			data.details = "Viewing the official logos";
+			presenceData.details = "Viewing the official logos";
 			break;
 		case "manual-lookup.php":
-			data.details = "Searching the documentation";
+			presenceData.details = "Searching the documentation";
 			break;
 		case "manual": {
-			data.details = "Viewing the documentation: ";
+			presenceData.details = "Viewing the documentation: ";
 			const manualTitle = document.title
 				.replaceAll("PHP: ", "")
 				.replaceAll(" - Manual", "");
 			if (route[3].includes("function.")) {
-				data.state = `Function: ${manualTitle}`;
-				data.buttons = [
+				presenceData.state = `Function: ${manualTitle}`;
+				presenceData.buttons = [
 					{ label: "View Function", url: document.location.href }
 				];
 			} else if (route[3].includes("language.")) {
 				const c = route[3].split(".")[1];
-				data.state = `${
+				presenceData.state = `${
 					c.charAt(0).toUpperCase() + c.slice(1)
 				}: ${manualTitle}`;
-			} else data.state = manualTitle;
+			} else presenceData.state = manualTitle;
 			break;
 		}
 		case "releases":
 			if (route[2] != "") {
-				data.details = "Viewing an release version:";
-				data.state = `PHP v${route[2]}`;
-			} else data.details = "Viewing the releases";
+				presenceData.details = "Viewing an release version:";
+				presenceData.state = `PHP v${route[2]}`;
+			} else presenceData.details = "Viewing the releases";
 			break;
 		default:
-			data.details = "Browsing the website";
+			presenceData.details = "Browsing the website";
 	}
 
 	if (route[1].toLowerCase().includes("changelog"))
-		data.details = "Reading the changelogs";
+		presenceData.details = "Reading the changelogs";
 
-	presence.setActivity(data);
+	presence.setActivity(presenceData);
 });
