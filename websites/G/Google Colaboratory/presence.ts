364ed86e01af28c45b33a6b7d84de0a7752e6f24
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2022-03-21 18:55:30 +0530

    Fix prettier

--- websites/G/Google Colaboratory/presence.ts
+++ websites/G/Google Colaboratory/presence.ts
@@ -1,22 +1,22 @@
 const presence = new Presence({
-	clientId: "955447491858432000"
-}),
-time = Math.floor(Date.now() / 1000);
+		clientId: "955447491858432000"
+	}),
+	time = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
-const presenceData: PresenceData = {
-	largeImageKey: "meetlogo",
-	startTimestamp: time
-};
+	const presenceData: PresenceData = {
+		largeImageKey: "meetlogo",
+		startTimestamp: time
+	};
 
-if (document.location.pathname.toLowerCase() === "/") {
-	presenceData.details = "Choosing a file";
-	presenceData.state = "Idling";
-} else {
-	presenceData.smallImageKey = "vcall";
-	presenceData.details = "Editing a notebook";
-	presenceData.state = `${document.title}`;
-}
+	if (document.location.pathname.toLowerCase() === "/") {
+		presenceData.details = "Choosing a file";
+		presenceData.state = "Idling";
+	} else {
+		presenceData.smallImageKey = "vcall";
+		presenceData.details = "Editing a notebook";
+		presenceData.state = `${document.title}`;
+	}
 
-presence.setActivity(presenceData);
+	presence.setActivity(presenceData);
 });
