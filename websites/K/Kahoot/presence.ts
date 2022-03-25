9cd739aaaef11ddae353edb9f633ec72b03647f5
theusaf <theusafyt@gmail.com>
2022-03-25 16:29:33 -0700

    feat(kahoot.it): Add new paths
    - Also fixes some issues with certain messages

--- websites/K/Kahoot/presence.ts
+++ websites/K/Kahoot/presence.ts
@@ -1,10 +1,9 @@
-// TODO: Update for new Kahoot ranking system
 async function findRanking(rankingSelector: HTMLElement) {
 	if (
 		rankingSelector.textContent === strings.stString ||
 		rankingSelector.textContent === strings.ndString ||
 		rankingSelector.textContent === strings.rdString ||
-		rankingSelector.textContent === strings.topX
+		rankingSelector.textContent.replace(/\d+/, "{0}") === strings.topX
 	)
 		return true;
 	else return false;
@@ -17,41 +16,44 @@ const presence = new Presence({
 async function getStrings() {
 	return presence.getStrings(
 		{
-			buttonJoinGame: "kahoot.buttonJoinGame",
-			joiningGame: "kahoot.joiningGame", // Joining game...
-			waiting: "kahoot.waiting", // Waiting to start...
-			gameStarting: "kahoot.gameStarting", // Game starting!
-			playing: "kahoot.playing", // Playing:
-			questionLoading: "kahoot.questionLoading", // Question: {0} | Loading...
-			incorrectAnswer: "kahoot.incorrectAnswer", // Incorrect answer!
-			correctAnswer: "kahoot.correctAnswer", // Correct answer!
-			pollAnswer: "kahoot.pollAnswer", // Answered poll
-			resultsQuestion: "kahoot.resultsQuestion", // Looking at results:
-			slideViewing: "kahoot.slideViewing", // Viewing a slide with content
-			gameOver: "kahoot.gameOver", // Game over!
-			gameCreate: "kahoot.gameCreate",
-			loadingPage: "kahoot.loadingPage",
-			firstPlace: "kahoot.firstPlace",
-			points: "kahoot.points", // {0} points
-			questionsCorrect: "kahoot.questionsCorrect",
-			slideShowing: "kahoot.slideShowing",
-			questionShowing: "kahoot.questionShowing",
-			stString: "kahoot.stString",
-			ndString: "kahoot.ndString",
-			rdString: "kahoot.rdString",
-			topX: "kahoot.topX",
-			onPodium: "kahoot.onPodium",
-			of: "kahoot.of", // of
-			questionNumber: "kahoot.questionNumber",
-			feedback: "kahoot.feedback", // Providing feedback...
-			waitingAnswer: "kahoot.waitingAnswer", // Waiting for question results...
-			drumRoll: "kahoot.drumRoll" // Waiting for ranking results...
+			buttonJoinGame: "kahoot.buttonJoinGame", // Join Game: {0}
+			joiningGame: "kahoot.joiningGame", // Joining Game...
+			waiting: "kahoot.waiting", // Waiting to Start...
+			gameStarting: "kahoot.gameStarting", // Game Starting!
+			playing: "kahoot.playing", // Playing...
+			questionLoading: "kahoot.questionLoading", // Next Question Loading...
+			incorrectAnswer: "kahoot.incorrectAnswer", // Incorrect Answer
+			correctAnswer: "kahoot.correctAnswer", // Correct Answer
+			pollAnswer: "kahoot.pollAnswer", // + Answered Poll
+			resultsQuestion: "kahoot.resultsQuestion", // Looking at Results:
+			slideViewing: "kahoot.slideViewing", // - Looking a Slide with Content...
+			gameOver: "kahoot.gameOver", // Game Over
+			gameCreate: "kahoot.gameCreate", // Creating a Game...
+			loadingPage: "kahoot.loadingPage", // Loading Page...
+			firstPlace: "kahoot.firstPlace", // First Place: {0}
+			points: "kahoot.points", // Points: {0}
+			questionsCorrect: "kahoot.questionsCorrect", // Questions Correct: {0}
+			slideShowing: "kahoot.slideShowing", // - Showing Slide with Content...
+			questionShowing: "kahoot.questionShowing", // Showing Question:
+			stString: "kahoot.stString", // 1st place
+			ndString: "kahoot.ndString", // 2nd place
+			rdString: "kahoot.rdString", // 3rd place
+			topX: "kahoot.topX", // Top {0}
+			onPodium: "kahoot.onPodium", // + On the Podium
+			of: "kahoot.of", // {0} of {1}
+			questionNumber: "kahoot.questionNumber", // Question: {0}
+			feedback: "kahoot.feedback", // Providing Feedback...
+			waitingAnswer: "kahoot.waitingAnswer", // Waiting for Results...
+			drumRoll: "kahoot.drumRoll", // + Waiting for Final Ranking...
+			position: "kahoot.position", // + Position: {0},
+			teamTalk: "kahoot.teamTalk" // + Discussing with Team...
 		},
 		await presence.getSetting<string>("lang")
 	);
 }
 
 let currentQuestion: string,
+	totalQuestions: string,
 	score: string,
 	ranking: string,
 	top5: Promise<boolean>,
@@ -74,54 +76,83 @@ presence.on("UpdateData", async () => {
 		case "kahoot.it": {
 			const path = document.location.pathname;
 
-			if (path === "/" || path.includes("/join") || path === "/v2/")
+			if (path === "/" || path.includes("/join") || path === "/v2/") {
+				// Home/Join screen
 				presenceData.details = strings.joiningGame;
-			else if (path.includes("/instructions"))
+			} else if (path.includes("/instructions")) {
+				// Waiting for game to start
 				presenceData.details = strings.waiting;
-			else if (path.includes("/start"))
+			} else if (path.includes("/start")) {
+				// Game is starting
 				presenceData.details = strings.gameStarting;
-			else if (path.includes("/gameblock")) {
-				currentQuestion = document.querySelector(
-					'[data-functional-selector="question-index-counter"]'
-				).textContent;
+			} else if (path.includes("/gameblock")) {
+				// Playing/Answering a question
+				[currentQuestion, totalQuestions] = document
+					.querySelector('[data-functional-selector="question-index-counter"]')
+					.textContent.match(/\d+/g);
 				score = document.querySelector(
 					'[data-functional-selector="bottom-bar-score"]'
 				).textContent;
 				presenceData.details = strings.playing;
 				presenceData.state = `${strings.questionNumber.replace(
 					"{0}",
-					""
-				)} ${currentQuestion} | ${strings.points.replace("{0}", "")} ${score}`;
+					strings.of
+						.replace("{0}", currentQuestion)
+						.replace("{1}", totalQuestions)
+				)} | ${strings.points.replace("{0}", score)}`;
 			} else if (path.includes("/getready")) {
-				currentQuestion = document.querySelector(
-					'[data-functional-selector="question-index-counter"]'
-				).textContent;
+				// Next question is loading
+				[currentQuestion, totalQuestions] = document
+					.querySelector('[data-functional-selector="question-index-counter"]')
+					.textContent.match(/\d+/g);
 				presenceData.details = strings.questionLoading;
 				presenceData.state = `${strings.questionNumber.replace(
 					"{0}",
-					""
-				)} ${currentQuestion}`;
-			} else if (path === "/answer")
+					strings.of
+						.replace("{0}", currentQuestion)
+						.replace("{1}", totalQuestions)
+				)}`;
+			} else if (path.includes("/teamtalk")) {
+				// Team discussion time
+				presenceData.details = strings.teamTalk;
+			} else if (path.includes("/answer") && !path.includes("/result")) {
+				// Waiting for question to end
 				presenceData.details = strings.waitingAnswer;
-			else if (path.includes("/result")) {
-				score = document.querySelector(
-					'[data-functional-selector="bottom-bar-score"]'
-				).textContent;
-				presenceData.details = strings.resultsQuestion;
-				presenceData.state = `${
-					document.querySelector('[data-functional-selector="correct-answer"]')
-						? strings.correctAnswer
-						: document.querySelector(
-								'[data-functional-selector="survey-answer"]'
-						  )
-						? strings.pollAnswer
-						: strings.incorrectAnswer
-				} | ${strings.points.replace("{0}", "")} ${score}`;
-			} else if (path.includes("/contentblock"))
-				presenceData.details = strings.slideViewing;
-			else if (path.includes("/ranking")) {
+			} else if (path.includes("/result")) {
+				// Answer result screen
 				rankingSelector = document.querySelector(
-					'[data-functional-selector="ranking-header-winners"]'
+					'[data-functional-selector="player-rank"]'
+				);
+				if (!rankingSelector) {
+					presenceData.details = strings.resultsQuestion;
+					presenceData.state = strings.pollAnswer;
+				} else {
+					score = document.querySelector(
+						'[data-functional-selector="bottom-bar-score"]'
+					).textContent;
+					presenceData.details = strings.resultsQuestion;
+					presenceData.state = `${
+						document.querySelector(
+							'[data-functional-selector="correct-answer"]'
+						)
+							? strings.correctAnswer
+							: strings.incorrectAnswer
+					} | ${
+						/\d+/.test(rankingSelector.textContent)
+							? strings.position.replace(
+									"{0}",
+									rankingSelector.textContent.match(/\d+/)[0]
+							  )
+							: strings.onPodium
+					} | ${strings.points.replace("{0}", score)}`;
+				}
+			} else if (path.includes("/contentblock")) {
+				// Viewing a slide with content
+				presenceData.details = strings.slideViewing;
+			} else if (path.includes("/ranking")) {
+				// Viewing the final ranking
+				rankingSelector = document.querySelector(
+					'[data-functional-selector="ranking-header"],[data-functional-selector="ranking-header-winners"]'
 				);
 				if (!rankingSelector) presenceData.details = strings.drumRoll;
 				else {
@@ -133,16 +164,14 @@ presence.on("UpdateData", async () => {
 					presenceData.details = top5
 						? `${strings.gameOver} | ${ranking} | ${strings.points.replace(
 								"{0}",
-								""
-						  )} ${score}`
-						: `${strings.gameOver} | ${strings.points.replace(
-								"{0}",
-								""
-						  )} ${score}`;
+								score
+						  )}`
+						: `${strings.gameOver} | ${strings.points.replace("{0}", score)}`;
 				}
-			} else if (path.includes("/feedback"))
+			} else if (path.includes("/feedback")) {
+				// Providing feedback
 				presenceData.details = strings.feedback;
-			else presenceData.details = strings.loadingPage;
+			} else presenceData.details = strings.loadingPage;
 
 			presence.setActivity(presenceData);
 			break;
