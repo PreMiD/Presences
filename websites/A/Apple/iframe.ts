f86401fac1adf377a47c50e8409141538c1e4aca
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-03-29 19:21:39 +0200

    style: prettier

--- websites/A/Apple/iframe.ts
+++ websites/A/Apple/iframe.ts
@@ -9,13 +9,12 @@ iframe.on("UpdateData", async () => {
 				document.querySelector<HTMLParagraphElement>(
 					"ul.mailbox-list li.mailbox-list-item[aria-selected=true] p"
 				)?.textContent ?? "Unknown",
-			currentMailSubject:
-				document.querySelector<HTMLHeadingElement>(
-					"div.ic-neqjca h2.ic-1ngj2ga"
-				)?.textContent,
-			currentMailSender:
-				document.querySelector<HTMLSpanElement>("div.ic-gg4vpm span.ic-x1z554")
-					?.textContent
+			currentMailSubject: document.querySelector<HTMLHeadingElement>(
+				"div.ic-neqjca h2.ic-1ngj2ga"
+			)?.textContent,
+			currentMailSender: document.querySelector<HTMLSpanElement>(
+				"div.ic-gg4vpm span.ic-x1z554"
+			)?.textContent
 		});
 	}
 });
