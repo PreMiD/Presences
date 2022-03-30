1c5a046d36898875a32861a0b281d7235b4ea634
Kanna <mail@kannadev.com>
2022-03-30 17:43:54 +0100

    feat(audiomack.com): Add presence

---
+++ websites/A/Audiomack/presence.ts
@@ -0,0 +1,23 @@
+const presence = new Presence({
+    clientId: "958483511310974977"
+    })
+
+  presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+  largeImageKey: "https://i.imgur.com/c0y4XDa.png"
+  };
+
+const song = document.querySelector(
+  "div.player.player--wide.player--active > div > div.PlayerArtistInfo-module__container--1r4wG > p > a > span:nth-child(2)"
+  ).textContent,
+
+  title = document.querySelector(
+      "div.player.player--wide.player--active > div > div.PlayerArtistInfo-module__container--1r4wG > p > a > span:nth-child(1)"
+      ).textContent;
+
+if (song && title)
+presenceData.details = `${song} - ${title}`
+
+if (presenceData.details) presence.setActivity(presenceData);
+else presence.clearActivity();
+});
