defd5238a96293a5a39cdf3005741e85a50790cc
Kanna <mail@kannadev.com>
2022-03-30 18:24:12 +0100

    used prettier

--- websites/A/Audiomack/presence.ts
+++ websites/A/Audiomack/presence.ts
@@ -1,23 +1,21 @@
 const presence = new Presence({
-    clientId: "958483511310974977"
-    })
+	clientId: "958483511310974977"
+});
 
-  presence.on("UpdateData", async () => {
+presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-  largeImageKey: "https://i.imgur.com/c0y4XDa.png"
-  };
+		largeImageKey: "https://i.imgur.com/c0y4XDa.png"
+	};
 
-const song = document.querySelector(
-  "div.player.player--wide.player--active > div > div.PlayerArtistInfo-module__container--1r4wG > p > a > span:nth-child(2)"
-  ).textContent,
+	const song = document.querySelector(
+			"div.player.player--wide.player--active > div > div.PlayerArtistInfo-module__container--1r4wG > p > a > span:nth-child(2)"
+		).textContent,
+		title = document.querySelector(
+			"div.player.player--wide.player--active > div > div.PlayerArtistInfo-module__container--1r4wG > p > a > span:nth-child(1)"
+		).textContent;
 
-  title = document.querySelector(
-      "div.player.player--wide.player--active > div > div.PlayerArtistInfo-module__container--1r4wG > p > a > span:nth-child(1)"
-      ).textContent;
+	if (song && title) presenceData.details = `${song} - ${title}`;
 
-if (song && title)
-presenceData.details = `${song} - ${title}`
-
-if (presenceData.details) presence.setActivity(presenceData);
-else presence.clearActivity();
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.clearActivity();
 });
