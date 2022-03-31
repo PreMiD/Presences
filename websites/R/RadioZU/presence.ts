f914a3c60546310fe96680159acf046a596c7bc5
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2022-03-31 18:54:37 +0300

    Add files via upload

--- websites/R/RadioZU/presence.ts
+++ websites/R/RadioZU/presence.ts
@@ -1,35 +1,34 @@
 const presence = new Presence({
-	clientId: "959069119012028467"
-}),
-
-strings = presence.getStrings({
-	play: "presence.playback.playing",
-	pause: "presence.playback.paused"
-});
+		clientId: "959069119012028467"
+	}),
+	strings = presence.getStrings({
+		play: "presence.playback.playing",
+		pause: "presence.playback.paused"
+	});
 
 let startTimestamp = 0;
 
 presence.on("UpdateData", async () => {
 	const path = window.location.pathname.split("/").slice(1);
-	switch(path[0]) {
+	switch (path[0]) {
 		case "live": {
 			const isPlaying = document.getElementsByClassName("jp-state-playing")[0];
-			if(!isPlaying) {
+			if (!isPlaying) {
 				startTimestamp = 0;
 				presence.setActivity();
 			} else {
-				if(!startTimestamp) startTimestamp = Date.now();
+				if (!startTimestamp) startTimestamp = Date.now();
 				const jpTitle = document.getElementsByClassName("jp-title")[0];
 				let titleName = "Unknown title";
 				const title = jpTitle.getElementsByTagName("strong")[0];
-				if(title) titleName = title.innerText;
+				if (title) titleName = title.innerText;
 				presence.setActivity({
 					largeImageKey: "logo",
 					smallImageKey: "live",
 					smallImageText: "Live",
 					details: "LIVE",
 					state: titleName,
-					buttons: [{label: "Play", url: "https://radiozu.ro/live"}],
+					buttons: [{ label: "Play", url: "https://radiozu.ro/live" }],
 					startTimestamp: startTimestamp
 				});
 			}
@@ -38,7 +37,7 @@ presence.on("UpdateData", async () => {
 		default: {
 			presence.setActivity({
 				largeImageKey: "logo",
-				details: "Browsing homepage",
+				details: "Browsing homepage"
 			});
 		}
 	}
