5da447eeb9b9e162a3d41d910e3ae4628cadbff7
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2022-03-31 19:15:17 +0300

    Add files via upload

--- websites/R/RadioZU/presence.ts
+++ websites/R/RadioZU/presence.ts
@@ -1,27 +1,18 @@
-const presence = new Presence({
-		clientId: "959069119012028467"
-	}),
-	strings = presence.getStrings({
-		play: "presence.playback.playing",
-		pause: "presence.playback.paused"
-	});
+const presence = new Presence({ clientId: "959069119012028467" });
 
-let startTimestamp = 0;
+let startTime = 0;
 
 presence.on("UpdateData", async () => {
-	const path = window.location.pathname.split("/").slice(1);
-	switch (path[0]) {
-		case "live": {
-			const isPlaying = document.getElementsByClassName("jp-state-playing")[0];
-			if (!isPlaying) {
-				startTimestamp = 0;
+	switch (window.location.pathname.split("/").slice(1)) {
+		case ["live"]: {
+			if (!document.querySelector(".jp-state-playing")) {
+				startTime = 0;
 				presence.setActivity();
 			} else {
-				if (!startTimestamp) startTimestamp = Date.now();
-				const jpTitle = document.getElementsByClassName("jp-title")[0];
+				if (!startTime) startTime = Number(Date.now());
 				let titleName = "Unknown title";
-				const title = jpTitle.getElementsByTagName("strong")[0];
-				if (title) titleName = title.innerText;
+				const title = document.querySelector(".jp-title>strong");
+				if (title) titleName = title.textContent;
 				presence.setActivity({
 					largeImageKey: "logo",
 					smallImageKey: "live",
@@ -29,7 +20,7 @@ presence.on("UpdateData", async () => {
 					details: "LIVE",
 					state: titleName,
 					buttons: [{ label: "Play", url: "https://radiozu.ro/live" }],
-					startTimestamp: startTimestamp
+					startTimestamp: startTime
 				});
 			}
 			break;
