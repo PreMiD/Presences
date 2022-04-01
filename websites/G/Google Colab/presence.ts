e6c36b775cfe3514703c32ceed5acdc24eef0ae4
theusaf <theusafyt@gmail.com>
2022-04-01 10:15:53 -0700

    feat(Google Colab): add presence

---
+++ websites/G/Google Colab/presence.ts
@@ -0,0 +1,31 @@
+const presence = new Presence({
+		clientId: "959487033963843594"
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", () => {
+	const presenceData: PresenceData = {
+			largeImageKey: "logo",
+			startTimestamp: browsingTimestamp
+		},
+		notebookTitle = document.querySelector("#doc-name").textContent,
+		selected = document.querySelector(".cell.focused");
+
+	if (
+		document
+			.querySelector("colab-last-saved-indicator")
+			.getAttribute("command") === "save"
+	)
+		presenceData.details = `Viewing ${notebookTitle}`;
+	else presenceData.details = `Editing ${notebookTitle}`;
+
+	if (selected.classList.contains("code"))
+		presenceData.state = "Editing code block";
+	else if (selected.classList.contains("text")) {
+		if (selected.classList.contains("edit"))
+			presenceData.state = "Editing text block";
+		else presenceData.state = "Viewing text block";
+	}
+
+	presence.setActivity(presenceData);
+});
