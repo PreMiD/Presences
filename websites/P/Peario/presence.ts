c99c770d4b039f1a39553b4b9aa528f434443946
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-05-01 14:50:29 +0200

    feat(Peario): added presence
    feat(Peario): added presence:
    - Added Rooms
    - Added Presence Settings
    - Added Settings & Addons Tab

--- websites/P/Peario/presence.ts
+++ websites/P/Peario/presence.ts
@@ -2,21 +2,101 @@ const presence = new Presence({
 		clientId: "969204609845428234"
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
-let title: HTMLElement, title2: string;
+let cached: JSON;
 
+async function fetchWithoutVideo() {
+	if (
+		!cached ||
+		!JSON.stringify(cached).includes(window.location.href.split("/")[5])
+	) {
+		const fetched = await fetch(
+			`https://v3-cinemeta.strem.io/meta/${
+				window.location.href.split("/")[4]
+			}/${window.location.href.split("/")[5]}.json`
+		).then(x => x.json());
+		cached = fetched;
+		return fetched;
+	} else return cached;
+}
+async function fetchWithVideo(video: HTMLMediaElement) {
+	if (
+		!cached ||
+		!JSON.stringify(cached).includes(window.location.href.split("/")[5])
+	) {
+		const fetched = await fetch(
+			`https://v3-cinemeta.strem.io/meta/movie/${
+				video.getAttribute("poster").split("/")[5]
+			}.json`
+		).then(x => x.json());
+		cached = fetched;
+		return fetched;
+	} else return cached;
+}
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			largeImageKey: "logo",
 			startTimestamp: browsingTimestamp
 		},
+		video = document.querySelectorAll(
+			"[data-v-aacedae0='']"
+		)[2] as HTMLVideoElement,
 		page = window.location.pathname,
 		[privacy, buttons] = await Promise.all([
 			presence.getSetting<boolean>("privacy"),
 			presence.getSetting<boolean>("buttons")
 		]);
-	if (privacy) presenceData.details = "Browsing...";
-	else if (page === "/") presenceData.details = "Bekijkt de homepagina";
-
+	if (privacy && !video) presenceData.details = "Browsing...";
+	else if (privacy && video) presenceData.details = "Watching...";
+	else if (document.querySelector("#addons")) presenceData.details = "Addons";
+	else if (
+		document.querySelector(
+			"#app > div.modal > div.inner > div.title.secondary > ion-icon"
+		)
+	)
+		presenceData.details = "Settings";
+	else if (page.includes("search")) {
+		const search = document.querySelector<HTMLInputElement>(
+			"#app > div > div > div > div > input"
+		);
+		if (search && search.value) {
+			presenceData.details = "Searching for:";
+			presenceData.state = search.value;
+		} else presenceData.details = "Browsing...";
+	} else if (page.includes("stream") || page.includes("room")) {
+		if (
+			document.querySelector(
+				"#app > div > div > div > div.player > div.lock-screen > div > button"
+			)
+		)
+			presenceData.details = "Waiting...";
+		else if (video) {
+			delete presenceData.startTimestamp;
+			const fetched = await fetchWithVideo(video);
+			if (fetched) presenceData.details = fetched.meta.name;
+			presenceData.smallImageText = `${
+				document.querySelector("#app > div > div > div > div.users.show > div")
+					.textContent
+			} Viewers`;
+			if (video.paused || isNaN(video.duration)) {
+				delete presenceData.endTimestamp;
+				presenceData.smallImageKey = "pause";
+			} else {
+				presenceData.smallImageKey = "play";
+				presenceData.endTimestamp = presence.getTimestampsfromMedia(video)[1];
+			}
+		} else {
+			const fetched = await fetchWithoutVideo();
+			if (fetched) presenceData.details = fetched.meta.name;
+		}
+	}
+	if (buttons && !privacy && page !== "/") {
+		presenceData.buttons = [
+			{
+				label: `View ${presenceData.details}`,
+				url: document.location.href
+			}
+		];
+	}
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
