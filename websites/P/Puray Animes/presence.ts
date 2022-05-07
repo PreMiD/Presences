648f0ed8de0c9a7df28c868574d7d4c245180cc8
Luscarvalho <themeros999@gmail.com>
2022-05-07 00:29:52 -0400

    fix Lins

--- websites/P/Puray Animes/presence.ts
+++ websites/P/Puray Animes/presence.ts
@@ -7,39 +7,40 @@ presence.on("UpdateData", () => {
 			largeImageKey: "logo",
 			startTimestamp: browsingTimestamp
 		},
-		{ pathname } = window.location;
-	let data: PresenceData = defaultData;
-	if (document.querySelector("input[id^=headlessui]")) {
+		{ pathname } = window.location,
+		data: PresenceData = defaultData;
+	if (document.querySelector("input[id^=headlessui]"))
 		data.details = "🔍・Pesquisando Animes";
-	} else if (pathname.startsWith("/home")) {
+	else if (pathname.startsWith("/home")) {
 		data.details = "Início:";
 		data.state = "Visualizando animes.";
 		data.buttons = [{ label: "💻・Puray Animes", url: location.href }];
 	} else if (pathname.startsWith("/profile/")) {
-		const username = document.querySelector("h3").childNodes[0]?.textContent,
-			section = document.querySelector("button[class*=blue] span")?.textContent;
+		const username = document.querySelector("h3").childNodes[0]?.textContent;
 		data.details = "Visualizando Perfil:";
-		data.state = `${username} - ${section}`;
+		data.state = `${username} - ${
+			document.querySelector("button[class*=blue] span")?.textContent
+		}`;
 		data.startTimestamp = browsingTimestamp;
 		data.buttons = [{ label: `🐒・Perfil de ${username}`, url: location.href }];
 	} else if (pathname.startsWith("/anime/")) {
-		const animename = document.querySelector(
+		const genders: string[] = [];
+		for (const _ of document.querySelectorAll("div[class^=sm]>div[class^=mb]"))
+			genders.push(_.textContent);
+
+		data.details = document.querySelector(
 			"section div[class^=text-3xl]"
 		)?.textContent;
-		const genders: string[] = [];
-		document.querySelectorAll("div[class^=sm]>div[class^=mb]").forEach(_ => {
-			genders.push(_.textContent);
-		});
-		data.details = animename;
 		if (genders.length) data.state = genders.join(", ");
 		data.startTimestamp = browsingTimestamp;
 		data.buttons = [{ label: "📺・Assistir Anime", url: location.href }];
 	} else if (pathname.startsWith("/watch/")) {
-		const animename = document.querySelector(
-				"span.text-sm.font-bold.underline"
-			)?.textContent,
-			episode = document.querySelector("span.text-lg.font-bold")?.textContent;
-		data.details = animename;
+		const episode = document.querySelector(
+			"span.text-lg.font-bold"
+		)?.textContent;
+		data.details = document.querySelector(
+			"span.text-sm.font-bold.underline"
+		)?.textContent;
 		data.state = episode;
 		data.buttons = [
 			{
@@ -48,11 +49,12 @@ presence.on("UpdateData", () => {
 			}
 		];
 		const video = document.querySelector("video");
-		if (!video.paused && video.readyState >= 1)
+		if (!video.paused && video.readyState >= 1) {
 			[data.startTimestamp, data.endTimestamp] = presence.getTimestamps(
 				video.currentTime,
 				video.duration
 			);
+		}
 	}
 	if (!data.details) presence.setActivity(defaultData);
 	else presence.setActivity(data);
