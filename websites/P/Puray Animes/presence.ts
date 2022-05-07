18d51c2ea27996ae830a90e65e4c2b903f4e8a5a
Luscarvalho <themeros999@gmail.com>
2022-05-06 20:39:43 -0400

    Funções Adicionadas

--- websites/P/Puray Animes/presence.ts
+++ websites/P/Puray Animes/presence.ts
@@ -3,10 +3,42 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
+enum PathNames {
+	home = "/home",
+	watch = "/watch/",
+	profile = "/profile/",
+	anime = "/anime/"
+}
+
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://imgur.com/8MP205p.png",
-		startTimestamp: browsingTimestamp
-	};
-	presence.setActivity(presenceData);
+			largeImageKey: "https://imgur.com/8MP205p.png",
+			startTimestamp: browsingTimestamp
+		},
+		pathName = window.location.pathname
+	function DefaultPresence(): void {
+		if (!presenceData.details) presence.setActivity();
+		else presence.setActivity(presenceData);
+	}
+	if (pathName.startsWith(PathNames.home)) {
+		presenceData.details = "Início:";
+		presenceData.state = `Visualizando Animes.`;
+		presenceData.startTimestamp = browsingTimestamp;
+	} else if (pathName.startsWith(PathNames.profile)) {
+		const username = document.querySelector("h3").childNodes[0].textContent;
+		presenceData.details = "Visualizando Perfil:";
+		presenceData.state = `${username}`;
+		presenceData.startTimestamp = browsingTimestamp;
+	} else if (pathName.startsWith(PathNames.anime)) {
+		const animename = document.querySelector("section div[class^=text-3xl]");
+		presenceData.details = "Visualizando Anime:";
+		presenceData.state = `${animename.textContent}`;
+		presenceData.startTimestamp = browsingTimestamp;
+	} else if (pathName.startsWith(PathNames.watch)) {
+		const animename = document.querySelector("section div[class^=text-3xl]");
+		presenceData.details = "Assistindo Anime:";
+		presenceData.state = `${animename.textContent}`;
+		presenceData.startTimestamp = browsingTimestamp;
+	}
+	DefaultPresence();
 });
