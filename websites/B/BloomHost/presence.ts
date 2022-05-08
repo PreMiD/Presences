fe5f75acb51db0332f8ce66f0e92f47983702385
BossDaily <comparatorcraft2020@gmail.com>
2022-05-08 14:31:17 -0500

    removed document.location

--- websites/B/BloomHost/presence.ts
+++ websites/B/BloomHost/presence.ts
@@ -9,129 +9,87 @@ presence.on("UpdateData", async () => {
 		largeImageKey: "bloom_logo",
 		startTimestamp: browsingTimestamp
 	};
-	if (document.location.hostname === "bloom.host") {
+	if (hostname === "bloom.host") {
 		presenceData.smallImageKey = "globe-solid";
 		presenceData.details = "Viewing the main website";
-		if (document.location.pathname.includes("minecraft")) {
+		if (pathname.includes("minecraft")) {
 			presenceData.details = "Viewing the main website";
 			presenceData.state = "Reading: Minecraft Plans";
-		} else if (document.location.pathname.includes("vps")) {
+		} else if (pathname.includes("vps")) {
 			presenceData.details = "Viewing the main website";
 			presenceData.state = "Reading: VPS Plans";
-		} else if (document.location.pathname.includes("duck-panel")) {
+		} else if (pathname.includes("duck-panel")) {
 			presenceData.details = "Viewing the main website";
 			presenceData.state = "Reading: About the Duck Panel";
-		} else if (document.location.pathname.includes("about-bloom")) {
+		} else if (pathname.includes("about-bloom")) {
 			presenceData.details = "Viewing the main website";
 			presenceData.state = "Reading: About Bloom";
 		}
 	}
-	if (document.location.hostname === "docs.bloom.host") {
+	if (hostname === "docs.bloom.host") {
 		presenceData.details = "Viewing the docs";
 		presenceData.state = `Reading: ${
 			document.querySelector("div.text--center > header > h1").textContent
 		}`;
 		presenceData.smallImageKey = "book-open-solid";
 	}
-	if (
-		document.location.hostname === "mc.bloom.host" ||
-		document.location.hostname === "demo.bloom.host"
-	) {
+	if (hostname === "mc.bloom.host" || hostname === "demo.bloom.host") {
 		presenceData.details = "Using the panel";
 		presenceData.smallImageKey = "terminal-solid";
-		if (document.location.pathname.includes("account")) {
+		if (pathname.includes("account")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Editing: Account Details";
-		} else if (document.location.pathname.includes("console")) {
+		} else if (pathname.includes("console")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: The console";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("logs")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("logs")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: Audit Logs";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("import")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("import")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Using: The Server Importer ";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("startup")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("startup")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: Startup";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("settings")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("settings")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: Settings";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("subdomain")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("subdomain")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: Subdomains";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("network")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("network")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: Ports & Proxies";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("backups")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("backups")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Creating: Backups";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("users")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("users")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: SubUsers";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("schedules")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("schedules")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: Schedules";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("databases")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("databases")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Viewing: Databases";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("minecraft")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("minecraft")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Managing: Minecraft players";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("plugins")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("plugins")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Installing: Plugins";
-		} else if (
-			document.location.pathname.includes("server") &&
-			document.location.pathname.includes("modpacks")
-		) {
+		} else if (pathname.includes("server") && pathname.includes("modpacks")) {
 			presenceData.details = "Using the Panel";
 			presenceData.state = "Installing: Modpacks";
-		} else if (document.location.pathname.includes("files")) {
+		} else if (pathname.includes("files")) {
 			presenceData.details = "Using the panel";
 			presenceData.state = "Editing: Files";
 		}
 	}
-	if (document.location.hostname === "status.bloom.host") {
+	if (hostname === "status.bloom.host") {
 		presenceData.details = "Viewing the Status page";
 		presenceData.smallImageKey = "stream-solid";
-		if (document.location.pathname.includes("report")) {
+		if (pathname.includes("report")) {
 			presenceData.details = "Viewing the Status Page";
 			presenceData.state = `Viewing the status of: ${document
 				.querySelector(" div.portlet-body > div.row > div.col-xs-12 > span")
@@ -140,11 +98,11 @@ presence.on("UpdateData", async () => {
 			presenceData.smallImageKey = "stream-solid";
 		}
 	}
-	if (document.location.hostname === "vps.bloom.host")
+	if (hostname === "vps.bloom.host")
 		presenceData.details = "Using the VPS panel";
 	presenceData.smallImageKey = "server-solid";
 
-	if (document.location.hostname === "billing.bloom.host")
+	if (hostname === "billing.bloom.host")
 		presenceData.details = "Using the Billing Panel";
 	presenceData.smallImageKey = "shopping-cart-solid";
 
