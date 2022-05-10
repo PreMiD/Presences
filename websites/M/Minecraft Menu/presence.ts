ed861fa598b3960d362f52c38a1fa9f249ee2ca7
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-05-10 09:13:39 +0200

    feat(Minecraft Menu): add presence
    feat(Minecraft Menu): add presence:
    - Added Full Site Support

---
+++ websites/M/Minecraft Menu/presence.ts
@@ -0,0 +1,82 @@
+const presence = new Presence({
+		clientId: "971311477514444800"
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+let title: HTMLElement, title2: string;
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+			largeImageKey: "logo",
+			startTimestamp: browsingTimestamp
+		},
+		search = document.querySelector<HTMLInputElement>(
+			"body > div.pusher > div.site-nav.clearfix > div > header > nav > div.right.menu > div > div.ui.right.action.left.icon.input > input"
+		),
+		page = window.location.pathname,
+		[privacy, buttons] = await Promise.all([
+			presence.getSetting<boolean>("privacy"),
+			presence.getSetting<boolean>("buttons")
+		]),
+		regex = new RegExp(/Minecraft .* Servers/gm);
+	if (privacy) presenceData.details = "Browsing...";
+	else if (search.value) {
+		presenceData.details = "Searching for:";
+		presenceData.state = search.value;
+		presenceData.smallImageKey = "searching";
+	} else if (page === "/") presenceData.details = "Homepage";
+	else if (page.includes("/server")) {
+		title = document.querySelector(
+			"body > div.pusher > section > div.ui.stackable.grid > div.six.wide.column > table > thead > tr > th"
+		);
+		presenceData.details = title.textContent.trim();
+		presenceData.state = document.querySelector(
+			"[class='active item']"
+		).textContent;
+		presenceData.buttons = [
+			{
+				label: "View Server",
+				url: document.location.href
+			}
+		];
+	} else if (page.includes("/cp")) {
+		presenceData.buttons = [
+			{
+				label: "Open Control Panel",
+				url: document.location.href
+			}
+		];
+		presenceData.details = "Control Panel";
+		presenceData.state =
+			document.querySelector("[class='active item']")?.textContent ??
+			document.querySelector("[class=' active item']")?.textContent;
+	} else if (page.includes("/partners")) {
+		presenceData.buttons = [
+			{
+				label: "View Partners",
+				url: document.location.href
+			}
+		];
+		presenceData.details = "Partners";
+	} else if (page.includes("/sponsored")) {
+		presenceData.buttons = [
+			{
+				label: "View Sponsored Servers",
+				url: document.location.href
+			}
+		];
+		presenceData.details = "Sponsored";
+	} else if (page.search(regex)) {
+		presenceData.buttons = [
+			{
+				label: "View Category",
+				url: document.location.href
+			}
+		];
+		presenceData.details = document.querySelector(
+			"body > div.pusher > section > div.header-wrappy > h2 > span"
+		).textContent;
+	}
+	if (!buttons) delete presenceData.buttons;
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
