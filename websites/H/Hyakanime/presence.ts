e42c749926ee6de26a64ead8a966ef25253d977b
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2022-05-12 00:55:58 +0200

    Update presence.ts

--- websites/H/Hyakanime/presence.ts
+++ websites/H/Hyakanime/presence.ts
@@ -48,7 +48,8 @@ presence.on("UpdateData", async () => {
 		];
 	}
 
-	if (presenceData.details) presence.setActivity(presenceData);
+	if (presenceData.details) 
+		presence.setActivity(presenceData);
 	else {
 		presenceData.details = "Sur la page d'accueil";
 		presence.setActivity(presenceData);
