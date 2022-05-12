8e11164b06ad0f6dbe7faf3bec3b3c7a312243be
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-05-12 23:56:18 +0100

    feat: replace massCompile with compileChanged

---
+++ util/tools/compileChanged.ts
@@ -0,0 +1,74 @@
+import "source-map-support/register";
+
+import { green, red } from "chalk";
+import {
+	createAnnotation,
+	execShellCommand,
+	getChangedFolders,
+	validateArg
+} from "./util";
+
+async function main() {
+	const errors: string[] = [],
+		changedFolders = await getChangedFolders(validateArg(process.argv[2]));
+	if (!changedFolders.length) return;
+
+	await compileFile(0);
+	const presenceErrors = Object.keys(errors).length;
+	if (presenceErrors) console.log(errors.join("\n"));
+	else console.log(green("✔ All presences compiled successfully!"));
+
+	async function compileFile(i: number): Promise<void> {
+		if (!changedFolders[i]) return;
+		try {
+			await execShellCommand("tsc --noEmit", { cwd: changedFolders[i] });
+
+			console.log(
+				green(`✔ Successfully compiled ${changedFolders[i].split("/").at(-1)}`)
+			);
+		} catch (err) {
+			const error = (
+				err.stderr ||
+				err.stdout ||
+				"Couldn't find error"
+			).replaceAll("\r", "").trim() as string;
+
+			error
+				.split("\n")
+				.filter(Boolean)
+				.forEach(e => {
+					const regexResult = e.match(/\w+\.\w+\((\d+),\d+\): (.+)/);
+
+					if (regexResult)
+						errors.push(
+							createAnnotation({
+								type: "error",
+								file: changedFolders[i] + "/presence.ts",
+								title: "Failed to compile",
+								line: regexResult[1],
+								message: regexResult[2]
+							})
+						);
+					else
+						errors.push(
+							createAnnotation({
+								type: "error",
+								file: changedFolders[i] + "/presence.ts",
+								title: "Failed to compile",
+								message: e
+							})
+						);
+				});
+
+			console.error(
+				red(`✘ Error on ${changedFolders[i].split("/").at(-1)}:\n`),
+				error
+			);
+		} finally {
+			return await compileFile(i + 1);
+		}
+	}
+	process.exit(errors.length ? 1 : 0);
+}
+
+main();
