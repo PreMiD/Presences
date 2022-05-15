77f3b407ea23f3db07cadf863e4680a83044b522
Faelayis <48393914+Faelayis@users.noreply.github.com>
2022-05-15 08:11:12 +0700

    refactor: update presence

--- websites/G/Genshin Impact Map/presence.ts
+++ websites/G/Genshin Impact Map/presence.ts
@@ -1,8 +1,25 @@
 const presence = new Presence({
 		clientId: "973729201104511007"
 	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000),
-	maps = [
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+interface Maps {
+	city: boolean;
+	hylid: number;
+	map: string;
+	largeImageKey: string;
+	pvlargeImageKey: string | null;
+	smallImageKey: string;
+}
+
+interface City {
+	position?: number;
+	map: string;
+	largeImageKey: string;
+	smallImageKey: string;
+}
+
+const map: Maps[] = [
 		{
 			city: true,
 			hylid: 2,
@@ -36,43 +53,28 @@ const presence = new Presence({
 			smallImageKey: "emblem_unknown"
 		}
 	],
-	city = [
+	city: City[] = [
 		{
-			position: [1200],
+			position: 1200,
 			map: "Mondstadt",
 			largeImageKey: "preview_mondstadt",
 			smallImageKey: "emblem_mondstadt"
 		},
 		{
-			position: [4000],
+			position: 4000,
 			map: "Liyue",
 			largeImageKey: "preview_liyue",
 			smallImageKey: "emblem_liyue"
 		},
 		{
-			position: [9000],
+			position: 9000,
 			map: "Inazuma",
 			largeImageKey: "preview_tenshukaku",
 			smallImageKey: "emblem_inazuma"
 		}
-		// { position: [], map: "Sumeru", largeImageKey: "", smallImageKey: "" }
 	];
 
-let getpos: number,
-	current: {
-		city: boolean;
-		hylid: number;
-		map: string;
-		largeImageKey: string;
-		pvlargeImageKey: string | null;
-		smallImageKey: string;
-	},
-	currentCity: {
-		position?: number[];
-		map: string;
-		largeImageKey: string;
-		smallImageKey: string;
-	};
+let getpos: number, current: Maps, currentCity: City;
 
 presence.on("UpdateData", async () => {
 	const [showPreview, timestamps] = await Promise.all([
@@ -85,10 +87,10 @@ presence.on("UpdateData", async () => {
 			smallImageKey: "search",
 			startTimestamp: browsingTimestamp
 		},
-		path = document.location.toString().split("/");
+		path = document.location.href.split("/");
 	switch (document.location.hostname) {
 		case "genshin-impact-map.appsample.com":
-			current = maps.find(
+			current = map.find(
 				i =>
 					i.map
 						.toLowerCase()
@@ -106,9 +108,9 @@ presence.on("UpdateData", async () => {
 				document.location.href.split("/maps/")[1].toLowerCase() ===
 				"the-chasm-underground"
 			)
-				current = maps.find(i => i.hylid === 9);
+				current = map.find(i => i.hylid === 9);
 			else {
-				current = maps.find(i =>
+				current = map.find(i =>
 					i.map
 						.toLowerCase()
 						.includes(document.location.href.split("/maps/")[1].toLowerCase())
@@ -121,27 +123,20 @@ presence.on("UpdateData", async () => {
 				!document.location.href.includes("&zoom=")
 			)
 				return;
-			// pos[0].split(",").map(i => Number(i))
 			getpos = parseInt(
 				document.location.href
 					.split("&center=")[1]
 					.split("&zoom=")[0]
 					.split(",")[0]
 			);
-			current = maps.find(
+			current = map.find(
 				i =>
 					i.hylid ===
 						parseInt(document.location.href.split("/map/")[1].split("?")[0]) ||
 					0
 			);
-			if (current.city) {
-				currentCity = city.find(
-					i =>
-						i.position.reduce((prev, curr) =>
-							Math.abs(curr - getpos) < Math.abs(prev - getpos) ? curr : prev
-						) > getpos
-				);
-			} else currentCity = null;
+			if (current.city) currentCity = city.find(i => i.position > getpos);
+			else currentCity = null;
 			break;
 	}
 	if (!current) return;
