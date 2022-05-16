db2999c8fdfa2e49351ee81c6d11ec2da9b96a5e
Tenebrosful <hugobernard39@gmail.com>
2022-05-16 09:59:29 +0200

    fix(Wolfy): lang path offset and handle events

--- websites/W/Wolfy/presence.ts
+++ websites/W/Wolfy/presence.ts
@@ -9,6 +9,11 @@ let path,
 	cp: number,
 	currTime: string;
 
+const waitingString: Record<string, string> = {
+	en: "WAITING",
+	fr: "EN ATTENTE"
+};
+
 function getTime(list: string[]): number {
 	let ret = 0;
 	for (let index = list.length - 1; index >= 0; index--)
@@ -48,6 +53,10 @@ function addConsultCategoryButton(presenceData: PresenceData, url: string) {
 	addButton(presenceData, "Consulter la catégorie", url);
 }
 
+function addConsultEventButton(presenceData: PresenceData, href: string) {
+	addButton(presenceData, "Consulter l'évènement", href);
+}
+
 async function addJoinGameButton(presenceData: PresenceData, gameId: string) {
 	if (!(await presence.getSetting("joinGameButton"))) return;
 	addButton(
@@ -55,7 +64,7 @@ async function addJoinGameButton(presenceData: PresenceData, gameId: string) {
 		`(${
 			document.querySelector("div.Header_timer__36MsP")?.textContent
 		}) Rejoindre la partie`,
-		`https://wolfy.fr/game/${gameId}`
+		`https://wolfy.net/game/${gameId}`
 	);
 }
 
@@ -63,7 +72,7 @@ function addVisitHelpCenterButton(presenceData: PresenceData) {
 	addButton(
 		presenceData,
 		"Consulter le centre d'aide",
-		"https://help.wolfy.fr"
+		"https://help.wolfy.net"
 	);
 }
 
@@ -77,11 +86,11 @@ async function addVisitProfilButton(
 	let label = `Visiter le profil de ${username}`;
 	if (label.length > 32) label = `${label.slice(0, 31)}…`;
 
-	addButton(presenceData, label, `https://wolfy.fr/leaderboard/${username}`);
+	addButton(presenceData, label, `https://wolfy.net/leaderboard/${username}`);
 }
 
 function addVisitWolfyButton(presenceData: PresenceData) {
-	addButton(presenceData, "Visiter le site de Wolfy", "https://wolfy.fr");
+	addButton(presenceData, "Visiter le site de Wolfy", "https://wolfy.net");
 }
 
 async function handleCheckingLeaderboard(
@@ -132,6 +141,9 @@ presence.on("UpdateData", async () => {
 
 	path = document.location.pathname;
 
+	const pathOffset =
+		path.split("/")[1] === document.querySelector("html")?.lang ? 1 : 0; // If the language is the first path element, we need to add an offset
+
 	if (window.location.href !== prev && !path.includes("/game/")) {
 		delete presenceData.startTimestamp;
 		delete presenceData.endTimestamp;
@@ -141,7 +153,7 @@ presence.on("UpdateData", async () => {
 
 	presenceData.startTimestamp = elapsed;
 
-	if (document.location.hostname === "help.wolfy.fr") {
+	if (document.location.hostname.includes("help.wolfy")) {
 		if (path.includes("/article") && path.split("/")[3]) {
 			presenceData.details = "Lit l'article ⤵️";
 			presenceData.state = document.querySelector(
@@ -160,12 +172,12 @@ presence.on("UpdateData", async () => {
 			presenceData.state = "Page d'accueil";
 			addVisitHelpCenterButton(presenceData);
 		}
-	} else if (path.includes("/articles/") && path.split("/")[2]) {
+	} else if (path.includes("/articles/") && path.split("/")[2 + pathOffset]) {
 		presenceData.details = "Lis l'article ⤵️";
 		presenceData.smallImageKey = "reading";
 		presenceData.smallImageText = "Lis un article";
 		presenceData.state = document.querySelector("body h1").textContent;
-	} else if (path.includes("/game/") && path.split("/")[2]) {
+	} else if (path.includes("/game/") && path.split("/")[2 + pathOffset]) {
 		presenceData.state = document
 			.querySelector("div.Header_nameState__3u5uu")
 			.textContent.toUpperCase();
@@ -178,35 +190,59 @@ presence.on("UpdateData", async () => {
 			currTime = document.querySelector("div.Header_timer__36MsP").textContent;
 		}
 
+		presenceData.details = "En jeu";
+
+		presenceData.smallImageKey = "live";
+		presenceData.smallImageText = "En jeu";
+		if (currTime?.includes(":")) {
+			[presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(
+				cp,
+				currTime
+			);
+		} else presenceData.startTimestamp = cp;
+
 		await addVisitProfilButton(
 			presenceData,
 			document.querySelector("span.ChatMain_username__2C_7z")?.textContent // Will be the anonymous username if used
 		);
 
-		if (presenceData.state === "EN ATTENTE") {
+		if (
+			presenceData.state === waitingString[document.querySelector("html").lang]
+		) {
 			presenceData.state += ` (${
 				document.querySelector("div.Header_timer__36MsP")?.textContent
 			})`;
-			await addJoinGameButton(presenceData, path.split("/")[2]);
+			await addJoinGameButton(presenceData, path.split("/")[2 + pathOffset]);
 		}
-
-		const [startTimestamp, endTimestamp] = getTimestamps(cp, currTime);
-
-		presenceData.details = "En jeu";
-
-		presenceData.smallImageKey = "live";
-		presenceData.smallImageText = "En jeu";
-		if (currTime && currTime.includes(":")) {
-			presenceData.startTimestamp = startTimestamp;
-			presenceData.endTimestamp = endTimestamp;
-		} else presenceData.startTimestamp = cp;
 	} else if (path.includes("/leaderboard")) {
 		await addVisitProfilButton(
 			presenceData,
 			document.querySelector("p.Social_username__KhUdM")?.textContent
 		);
 
-		await handleCheckingLeaderboard(presenceData, path.split("/")[2]);
+		await handleCheckingLeaderboard(
+			presenceData,
+			path.split("/")[2 + pathOffset]
+		);
+	} else if (path.includes("/event") && path.split("/")[2 + pathOffset]) {
+		if (document.querySelector("div.Event_eventIntroduction__1HrZz")) {
+			presenceData.details = "Participe à un évènement";
+			presenceData.state = `Top ${parseInt(
+				document.querySelector(
+					"div.Event_lineLeaderboard__hxgUV.Event_me__2Lm3n > div.Event_rank__Id586"
+				).textContent
+			).toLocaleString()} - ${parseInt(
+				document.querySelector(
+					"div.Event_lineLeaderboard__hxgUV.Event_me__2Lm3n > div.Event_points__3K_vr"
+				).textContent
+			).toLocaleString()} points`;
+		} else {
+			presenceData.details = "Consulte un évènement à venir ⤵️";
+			presenceData.state = document.querySelector(
+				"div.Event_title__hCZ5r"
+			)?.textContent;
+		}
+		addConsultEventButton(presenceData, document.location.href);
 	} else {
 		await addVisitProfilButton(
 			presenceData,
@@ -215,27 +251,25 @@ presence.on("UpdateData", async () => {
 
 		presenceData.details = "Dans un menu";
 
-		switch (path) {
-			case "/skin":
+		switch (path.split("/")[1 + pathOffset]) {
+			case "skin":
 				presenceData.smallImageKey = "skin";
 				presenceData.smallImageText = "Choisis ton skin";
 				presenceData.state = "Consulte ses Skins";
 				break;
-			case "/settings":
+			case "settings":
 				presenceData.state = "Change ses paramètres";
 				break;
-			case "/shop":
+			case "shop":
 				presenceData.smallImageKey = "shop";
-				presenceData.smallImageText = "Achete des skins";
+				presenceData.smallImageText = "Achète des skins";
 				presenceData.state = "Consulte la Boutique";
 				break;
-			case "/articles":
+			case "articles":
 				presenceData.state = "Consulte les dernières actualités";
 				presenceData.smallImageKey = "reading";
 				presenceData.smallImageText = "En train de lire";
 				break;
-			case "/play":
-			case "/":
 			default:
 				presenceData.state = "Page d'accueil";
 		}
