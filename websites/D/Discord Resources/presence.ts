530ec802de99d8e7bbbd65f3ad1a4624462befef
2Lost4GitHub <2lost4email@gmail.com>
2022-05-20 20:35:19 -0400

    Changing /wiki -> /resources
    They updated the link so i have to update the /wiki as well

--- websites/D/Discord Resources/presence.ts
+++ websites/D/Discord Resources/presence.ts
@@ -1,71 +1,81 @@
 const presence = new Presence({
-		clientId: "973710731549745152"
-	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+    clientId: "973710731549745152"
+}), browsingTimestamp = Math.floor(Date.now() / 1000);
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-			largeImageKey: "drw",
-			startTimestamp: browsingTimestamp
-		},
-		{ pathname, search } = window.location;
-	switch (true) {
-		case pathname.includes("/wiki"): {
-			presenceData.smallImageKey = "wiki";
-			presenceData.smallImageText = "Wiki";
-			if (pathname === "/wiki" || pathname === "/wiki/")
-				presenceData.details = "Viewing the main page";
-			else {
-				presenceData.details = "Reading a wiki page";
-				presenceData.state = document.title.split("|")[0];
-				presenceData.buttons = [
-					{
-						label: "Read Page",
-						url: location.href
-					}
-				];
-			}
-			break;
-		}
-		case pathname.includes("/blog"): {
-			presenceData.smallImageKey = "blog";
-			presenceData.smallImageText = "Blog";
-			if (document.title === "Blog | Discord Resources")
-				presenceData.details = "Viewing the main page";
-			else if (pathname.includes("/tags/")) {
-				const pSplit = pathname.split("/");
-				presenceData.details = "Looking for posts tagged with:";
-				presenceData.state = `"${pSplit[pSplit.length - 1]}"`;
-			} else {
-				presenceData.details = "Reading a Post";
-				presenceData.state = document.title.split("|")[0];
-				presenceData.buttons = [
-					{
-						label: "Read Post",
-						url: location.href
-					}
-				];
-			}
-			break;
-		}
-		case pathname === "/search": {
-			presenceData.smallImageKey = "searching";
-			presenceData.smallImageText = "Searching...";
-			if (search) {
-				presenceData.details = "Searching for:";
-				presenceData.state = document
-					.querySelector<HTMLHeadElement>(
-						"#__docusaurus > div.main-wrapper > div > h1"
-					)
-					.textContent.split("Search results for ")[1];
-			} else presenceData.details = "Searching for something...";
-			break;
-		}
-		default: {
-			presenceData.smallImageKey = "unsupported";
-			presenceData.smallImageText = "Unknown";
-			presenceData.details = "Viewing an unsupported page";
-		}
-	}
-	if (presenceData.details) presence.setActivity(presenceData);
-	else presence.setActivity();
+    const presenceData = {
+        largeImageKey: "drw",
+        startTimestamp: browsingTimestamp
+    }, { pathname, search } = window.location;
+    switch (true) {
+        case pathname.includes("/resources"): {
+            presenceData.smallImageKey = "wiki";
+            presenceData.smallImageText = "Wiki";
+                presenceData.details = "Reading a wiki page";
+                presenceData.state = document.title.split("|")[0];
+                presenceData.buttons = [
+                    {
+                        label: "Read Page",
+                        url: location.href
+                    }
+                ];
+            break;
+        }
+        case pathname.includes("/contribution"): {
+            presenceData.smallImageKey = "wiki";
+            presenceData.smallImageText = "Wiki";
+            presenceData.details = "Reading a page";
+            presenceData.state = document.title.split("|")[0];
+                presenceData.buttons = [
+                    {
+                        label: "Read Page",
+                        url: location.href
+                    }
+                ];
+            break;
+        }
+        case pathname.includes("/blog"): {
+            presenceData.smallImageKey = "blog";
+            presenceData.smallImageText = "Blog";
+            if (document.title === "Blog | Discord Resources")
+                presenceData.details = "Viewing the main page";
+            else if (pathname.includes("/tags/")) {
+                const pSplit = pathname.split("/");
+                presenceData.details = "Looking for posts tagged with:";
+                presenceData.state = `"${pSplit[pSplit.length - 1]}"`;
+            }
+            else {
+                presenceData.details = "Reading a Post";
+                presenceData.state = document.title.split("|")[0];
+                presenceData.buttons = [
+                    {
+                        label: "Read Post",
+                        url: location.href
+                    }
+                ];
+            }
+            break;
+        }
+        case pathname === "/search": {
+            presenceData.smallImageKey = "searching";
+            presenceData.smallImageText = "Searching...";
+            if (search) {
+                presenceData.details = "Searching for:";
+                presenceData.state = document
+                    .querySelector("#__docusaurus > div.main-wrapper > div > h1")
+                    .textContent.split("Search results for ")[1];
+            }
+            else
+                presenceData.details = "Searching for something...";
+            break;
+        }
+        default: {
+            presenceData.smallImageKey = "unsupported";
+            presenceData.smallImageText = "Unknown";
+            presenceData.details = "Viewing an unsupported page";
+        }
+    }
+    if (presenceData.details)
+        presence.setActivity(presenceData);
+    else
+        presence.setActivity();
 });
