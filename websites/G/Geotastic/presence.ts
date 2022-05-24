b3ab2197c2c4c676b783316908aa4fe804168fe9
AzNate <nathanldblulogin@hotmail.com>
2022-05-24 12:21:44 -0700

    Update presence.ts
    Addressing Lint issues

--- websites/G/Geotastic/presence.ts
+++ websites/G/Geotastic/presence.ts
@@ -22,74 +22,128 @@ presence.on("UpdateData", async () => {
 			presenceData.details = "Preparing for a round";
 		else if (document.querySelector(".active-component-play")) {
 			if (document.querySelector(".side-bar")) {
-				let info = document
-					.querySelector(".meta-infos")
-					.querySelectorAll("span");
 				presenceData.details = "In an online battle";
-				presenceData.state = "Playing map " + info[1].textContent;
+				presenceData.state = `Playing map ${
+					document.querySelector(".meta-infos").querySelectorAll("span")[1]
+						.textContent
+				}`;
 			} else {
-				let info = document
+				const info = document
 					.querySelector(".meta-infos")
 					.querySelectorAll("span");
-				presenceData.details = "Playing map " + info[1].textContent;
-				presenceData.state = info[2].textContent + " " + info[3].textContent;
+				presenceData.details = `Playing map ${info[1].textContent}`;
+				presenceData.state = `${info[2].textContent} ${info[3].textContent}`;
 			}
 		} else if (document.querySelector(".active-component-result")) {
 			presenceData.details = document
 				.querySelector(".round-info")
 				.querySelector("span").textContent;
-			presenceData.state =
-				"Score " + document.querySelector(".own-score").textContent;
+			presenceData.state = `Score ${
+				document.querySelector(".own-score").textContent
+			}`;
 		} else if (document.querySelector(".active-component-finished")) {
 			presenceData.details = "Round finished";
-			presenceData.state =
-				"Final score " + document.querySelector(".score").textContent;
+			presenceData.state = `Final score ${
+				document.querySelector(".score").textContent
+			}`;
 		} else presenceData.details = "Playing a game";
-	} else if (document.location.pathname === "/user-challenges/find")
-		presenceData.details = "Browsing unplayed user challenges";
-	else if (document.location.pathname === "/user-challenges/played")
-		presenceData.details = "Browsing played user challenges";
-	else if (document.location.pathname === "/user-challenges/own")
-		presenceData.details = "Browing created user challenges";
-	else if (document.location.pathname === "/user-challenges/create")
-		presenceData.details = "Creating a challenge";
-	else if (document.location.pathname.includes("/challenge-details/"))
-		presenceData.details = "Viewing a challenge";
-	else if (document.location.pathname.includes("/online-lobby/"))
-		presenceData.details = "In an online lobby";
-	else if (document.location.pathname === "/matchmaking-lobby")
-		presenceData.details = "In a matchmaking lobby";
-	else if (document.location.pathname === "/community-map")
-		presenceData.details = "Viewing the community map";
-	else if (document.location.pathname === "/squad/list")
-		presenceData.details = "Browsing the list of squads";
-	else if (document.location.pathname === "/squad/management")
-		presenceData.details = "Viewing the squad management page";
-	else if (document.location.pathname === "/map-editor/maps")
-		presenceData.details = "Creating a map";
-	else if (document.location.pathname === "/map-editor/drop-groups")
-		presenceData.details = "Viewing their drop groups";
-	else if (document.location.pathname === "/events")
-		presenceData.details = "Browsing past and future events";
-	else if (document.location.pathname.includes("/events/")) {
-		presenceData.details = "Viewing an event";
-		presenceData.state = document.querySelector("h1").textContent;
-	} else if (document.location.pathname.includes("/help-out/"))
-		presenceData.details = "Viewing ways to support Geotastic";
-	else if (document.location.pathname === "/account/profile")
-		presenceData.details = "Viewing their profile settings";
-	else if (document.location.pathname === "/account/statistics")
-		presenceData.details = "Viewing their statistics";
-	else if (document.location.pathname === "/account/trophies")
-		presenceData.details = "Viewing their trophies";
-	else if (document.location.pathname === "/account/friends")
-		presenceData.details = "Viewing their friends";
-	else if (document.location.pathname === "/account/supporter-status")
-		presenceData.details = "Viewing their supporter status";
-	else if (document.location.pathname === "/account/quota-consumption")
-		presenceData.details = "Viewing their quota consumption";
-	else if (document.location.pathname === "/account/account-settings")
-		presenceData.details = "Viewing their account settings";
+	} else {
+		switch (document.location.pathname) {
+			case "/user-challenges/find": {
+				presenceData.details = "Browsing unplayed user challenges";
+				break;
+			}
+			case "/user-challenges/played": {
+				presenceData.details = "Browsing played user challenges";
+				break;
+			}
+			case "/user-challenges/own": {
+				presenceData.details = "Browing created user challenges";
+				break;
+			}
+			case "/user-challenges/create": {
+				presenceData.details = "Creating a challenge";
+				break;
+			}
+			default:
+				if (document.location.pathname.includes("/challenge-details/"))
+					presenceData.details = "Viewing a challenge";
+				else if (document.location.pathname.includes("/online-lobby/"))
+					presenceData.details = "In an online lobby";
+				else {
+					switch (document.location.pathname) {
+						case "/matchmaking-lobby": {
+							presenceData.details = "In a matchmaking lobby";
+							break;
+						}
+						case "/community-map": {
+							presenceData.details = "Viewing the community map";
+							break;
+						}
+						case "/squad/list": {
+							presenceData.details = "Browsing the list of squads";
+							break;
+						}
+						case "/squad/management": {
+							presenceData.details = "Viewing the squad management page";
+							break;
+						}
+						case "/map-editor/maps": {
+							presenceData.details = "Creating a map";
+							break;
+						}
+						case "/map-editor/drop-groups": {
+							presenceData.details = "Viewing their drop groups";
+							break;
+						}
+						case "/events": {
+							presenceData.details = "Browsing past and future events";
+							break;
+						}
+						default:
+							if (document.location.pathname.includes("/events/")) {
+								presenceData.details = "Viewing an event";
+								presenceData.state = document.querySelector("h1").textContent;
+							} else if (document.location.pathname.includes("/help-out/"))
+								presenceData.details = "Viewing ways to support Geotastic";
+							else {
+								switch (document.location.pathname) {
+									case "/account/profile": {
+										presenceData.details = "Viewing their profile settings";
+										break;
+									}
+									case "/account/statistics": {
+										presenceData.details = "Viewing their statistics";
+										break;
+									}
+									case "/account/trophies": {
+										presenceData.details = "Viewing their trophies";
+										break;
+									}
+									case "/account/friends": {
+										presenceData.details = "Viewing their friends";
+										break;
+									}
+									case "/account/supporter-status": {
+										presenceData.details = "Viewing their supporter status";
+										break;
+									}
+									case "/account/quota-consumption": {
+										presenceData.details = "Viewing their quota consumption";
+										break;
+									}
+									case "/account/account-settings":
+										{
+											presenceData.details = "Viewing their account settings";
+											// No default
+										}
+										break;
+								}
+							}
+					}
+				}
+		}
+	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
