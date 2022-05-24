56f2e5935157b05de0a8f8abb92b58f17a9000d0
dEN5 <54153548+dEN5-tech@users.noreply.github.com>
2022-05-24 18:24:51 +0300

    Add files via upload

---
+++ websites/A/Animevost/presence.ts
@@ -0,0 +1,76 @@
+const presence = new Presence({
+		clientId: "978634361266110534"
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+let title: HTMLElement, search: HTMLElement;
+
+function DifIndient(st: string) {
+	const state = document.location.pathname.split(st);
+	if (state.length == 2 && state[1] != "") return "iframe";
+	else return "genre";
+}
+
+function GetSeria() {
+	const all: string[] = [];
+	document.querySelectorAll("div[id='items']>div").forEach((e: Element) => {
+		if (e.className.includes("active")) all.push(e.textContent);
+	});
+	return all.join("");
+}
+
+function SetCategory(tag: string) {
+	const data = Array.from(
+		document.querySelectorAll("span[id='blok_4']>a")
+	).reduce(
+		(obj, rule: HTMLElement) => {
+			if (rule.getAttribute("href").includes(tag)) {
+				obj.tag = rule.textContent;
+				return obj;
+			}
+			return obj;
+		},
+		{ tag: null }
+	);
+	return data;
+}
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey: "animevost",
+		startTimestamp: browsingTimestamp
+	};
+
+	if (document.location.pathname === "/")
+		presenceData.details = "Просмотр главной страницы сайта";
+	else if (
+		document.location.pathname.includes("/tip/") &&
+		!document.location.pathname.endsWith(".html")
+	) {
+		presenceData.details = "Просмативает категорию";
+		presenceData.state = SetCategory(document.location.pathname).tag
+			? `${SetCategory(document.location.pathname).tag}`
+			: "";
+	} else if (document.location.pathname.includes("/ongoing"))
+		presenceData.details = "Просмативает категорию Онгоинги";
+	else if (
+		document.location.pathname.endsWith(".html") &&
+		document.location.pathname.includes("/tip/")
+	) {
+
+		presenceData.state = `смотрит\n ${GetSeria()}`;
+		presenceData.details = `${
+			document
+				.querySelector("meta[name='description']")
+				.getAttribute("content")
+				.split(" / ")[0]
+		}`;
+	} else if (document.location.pathname.includes("zhanr")) {
+		presenceData.state = `жанр ${
+			document.querySelector("div[class='present']>h1").textContent
+		}`;
+		presenceData.details = "Просмативает категорию";
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
