862fae0c92a661ee40bdb8ad3220bf36028a44f5
dzlandis <49926644+dzlandis@users.noreply.github.com>
2022-06-02 15:53:56 -0700

    style(Brain.fm): fixed punctuation and removed Brain.fm from presence details

--- websites/B/Brain.fm/presence.ts
+++ websites/B/Brain.fm/presence.ts
@@ -12,31 +12,31 @@ presence.on("UpdateData", () => {
 
 	if (["www.brain.fm", "try.brain.fm"].includes(hostname)) {
 		if (["/", "/home"].includes(pathname))
-			presenceData.details = "Viewing the home page.";
+			presenceData.details = "Viewing the home page";
 		else if (pathname.startsWith("/about"))
-			presenceData.details = "Viewing Brain.fm's about page.";
+			presenceData.details = "Viewing the about page";
 		else if (pathname.startsWith("/pdfs"))
-			presenceData.details = "Reading a Brain.fm PDF.";
+			presenceData.details = "Reading a PDF";
 		else if (pathname.startsWith("/science"))
-			presenceData.details = "Viewing Brain.fm's science page.";
+			presenceData.details = "Viewing the science page";
 		else if (pathname.startsWith("/press"))
-			presenceData.details = "Viewing Brain.fm's press page.";
+			presenceData.details = "Viewing the press page";
 		else if (pathname.startsWith("/pricing"))
-			presenceData.details = "Viewing pricing for Brain.fm.";
+			presenceData.details = "Viewing pricing options";
 		else if (pathname.startsWith("/jobs"))
-			presenceData.details = "Viewing jobs at Brain.fm.";
+			presenceData.details = "Viewing the jobs page";
 		else if (pathname.startsWith("/faq"))
-			presenceData.details = "Viewing Brain.fm's FAQ page.";
+			presenceData.details = "Viewing the FAQ page";
 		else if (pathname.startsWith("/contact"))
-			presenceData.details = "Viewing Brain.fm's contact page.";
+			presenceData.details = "Viewing the contact page";
 		else if (pathname.startsWith("/terms"))
-			presenceData.details = "Viewing Brain.fm's terms page.";
+			presenceData.details = "Viewing the terms page";
 		else if (pathname.startsWith("/privacy"))
-			presenceData.details = "Viewing Brain.fm's privacy page.";
+			presenceData.details = "Viewing the privacy page";
 		else if (pathname.startsWith("/legal"))
-			presenceData.details = "Viewing Brain.fm's legal page.";
+			presenceData.details = "Viewing the legal page";
 		else if (pathname.startsWith("/forgot-password"))
-			presenceData.details = "Resetting Password...";
+			presenceData.details = "Resetting their password";
 	} else if (hostname.startsWith("my")) {
 		if (pathname === "/") {
 			if (
@@ -44,15 +44,15 @@ presence.on("UpdateData", () => {
 					"#root > div > div.sc-gKsewC.sc-iBPRYJ.sc-gGiJkG.efCouF.cOWINi.jzdkxv"
 				)
 			)
-				presenceData.details = "Logging into Brain.fm";
+				presenceData.details = "Logging in";
 			else if (
 				document.querySelector("#root > div > div > div > div.sc-jsNsAp.byZiWf")
 			)
-				presenceData.details = "Choosing Mode...";
+				presenceData.details = "Choosing mode";
 		} else if (pathname.startsWith("/signup"))
-			presenceData.details = "Signing Up for Brain.fm";
+			presenceData.details = "Signing up";
 		else if (pathname.startsWith("/welcome"))
-			presenceData.details = "Getting started with Brain.fm";
+			presenceData.details = "Getting started";
 		else if (
 			["/focus", "/relax", "/sleep"].includes(pathname)
 		) {
@@ -101,12 +101,12 @@ presence.on("UpdateData", () => {
 			presenceData.smallImageKey = "logo";
 			presenceData.smallImageText = songTypeInfo[1];
 		} else if (pathname.startsWith("/user"))
-			presenceData.details = "Viewing user information...";
+			presenceData.details = "Viewing user information";
 		else if (pathname.startsWith("/payment"))
-			presenceData.details = "Viewing payment information...";
+			presenceData.details = "Viewing payment information";
 		else if (pathname.startsWith("/forgot-password"))
-			presenceData.details = "Resetting Password...";
-	} else presenceData.details = "Browsing...";
+			presenceData.details = "Resetting their password";
+	} else presenceData.details = "Browsing";
 
 	presence.setActivity(presenceData);
 });
