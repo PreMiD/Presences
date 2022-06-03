abe792b087466a9c7c27de216813f3c6d66fea20
Sub2Rhys <59174048+Sub2Rhys@users.noreply.github.com>
2022-06-03 02:09:28 +0100

    prettier

--- websites/R/RhysBOT/presence.ts
+++ websites/R/RhysBOT/presence.ts
@@ -1,39 +1,45 @@
 const presence = new Presence({
-	clientId: "658765364439810048"
-}),
-strings = presence.getStrings({
-	view: "presence.viewing",
-});
+		clientId: "658765364439810048"
+	}),
+	strings = presence.getStrings({
+		view: "presence.viewing"
+	});
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey: "https://i.imgur.com/yAhhPzj.png",
 		details: `Exploring`,
-		startTimestamp: + new Date(),
+		startTimestamp: +new Date(),
 		buttons: [
 			{
 				label: "View Page",
 				url: location.href
-			},
+			}
 		]
 	};
 
-	var page = location.href.split("#")[0].split("?")[0].split("/").slice(-1)
+	var page = location.href.split("#")[0].split("?")[0].split("/").slice(-1);
 
-	if(document.getElementById('itemName')) {
-		presenceData.largeImageKey = (<HTMLImageElement>document.getElementById("itemImage")).src
+	if (document.getElementById("itemName")) {
+		presenceData.largeImageKey = (<HTMLImageElement>(
+			document.getElementById("itemImage")
+		)).src;
 
-		presenceData.details = `Viewing ${document.getElementById('itemName').innerHTML}`
+		presenceData.details = `Viewing ${
+			document.getElementById("itemName").innerHTML
+		}`;
 
-		if(document.getElementById('itemDescription')) {
-			presenceData.state = `${document.getElementById('itemDescription').innerHTML}`
+		if (document.getElementById("itemDescription")) {
+			presenceData.state = `${
+				document.getElementById("itemDescription").innerHTML
+			}`;
 		}
-	} else if(page[0] && !page[0].includes('_')) {
-		presenceData.details = `Exploring the ${page} page`
+	} else if (page[0] && !page[0].includes("_")) {
+		presenceData.details = `Exploring the ${page} page`;
 	}
 
-	if(location.href.includes('#google_vignette')) {
-		presenceData.details = `Closing a pop-up ad`
+	if (location.href.includes("#google_vignette")) {
+		presenceData.details = `Closing a pop-up ad`;
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
