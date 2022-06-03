65ba5e0c4fa9d3e0354d8d185534f06542e5abfd
Sub2Rhys <59174048+Sub2Rhys@users.noreply.github.com>
2022-06-03 02:18:39 +0100

    Update presence.ts

--- websites/R/RhysBOT/presence.ts
+++ websites/R/RhysBOT/presence.ts
@@ -1,46 +1,40 @@
 const presence = new Presence({
-		clientId: "658765364439810048"
-	}),
-	strings = presence.getStrings({
-		view: "presence.viewing"
-	});
+	clientId: "658765364439810048"
+});
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/yAhhPzj.png",
-		details: `Exploring`,
-		startTimestamp: +new Date(),
-		buttons: [
-			{
-				label: "View Page",
-				url: location.href
-			}
-		]
-	};
+			largeImageKey: "https://i.imgur.com/yAhhPzj.png",
+			details: "Exploring",
+			startTimestamp: Date.now(),
+			buttons: [
+				{
+					label: "View Page",
+					url: location.href
+				}
+			]
+		},
+		page = location.href.split("#")[0].split("?")[0].split("/").slice(-1);
 
-	var page = location.href.split("#")[0].split("?")[0].split("/").slice(-1);
-
-	if (document.getElementById("itemName")) {
+	if (document.querySelector("#itemName")) {
 		presenceData.largeImageKey = (<HTMLImageElement>(
-			document.getElementById("itemImage")
+			document.querySelector("#itemImage")
 		)).src;
 
 		presenceData.details = `Viewing ${
-			document.getElementById("itemName").innerHTML
+			document.querySelector("#itemName").innerHTML
 		}`;
 
-		if (document.getElementById("itemDescription")) {
+		if (document.querySelector("#itemDescription")) {
 			presenceData.state = `${
-				document.getElementById("itemDescription").innerHTML
+				document.querySelector("#itemDescription").innerHTML
 			}`;
 		}
-	} else if (page[0] && !page[0].includes("_")) {
+	} else if (page[0] && !page[0].includes("_"))
 		presenceData.details = `Exploring the ${page} page`;
-	}
 
-	if (location.href.includes("#google_vignette")) {
-		presenceData.details = `Closing a pop-up ad`;
-	}
+	if (location.href.includes("#google_vignette"))
+		presenceData.details = "Closing a pop-up ad";
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
