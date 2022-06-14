3156c91d7cee4a928f03375d4324ef60c9243600
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-06-14 13:09:01 +0100

    feat(Trakt): new presence
    Co-authored-by: Slowlife <slowlife1165@gmail.com>
    Co-authored-by: Rhys <64903135+EncryptedDev@users.noreply.github.com>

---
+++ websites/T/Trakt/presence.ts
@@ -0,0 +1,169 @@
+const presence = new Presence({
+		clientId: "968880591003082783"
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+	let presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/7V0kxEb.png",
+		startTimestamp: browsingTimestamp
+	};
+
+	const path = document.location.pathname.toLowerCase(),
+		pages: Record<string, PresenceData> = {
+			"/": { details: "Browsing the home page" },
+			"/dashboard": { details: "Browsing dashboard" },
+			"/shows/trending": { state: "Trending shows" },
+			"/shows/popular": { state: "Popular shows" },
+			"/shows/recommended": { state: "Recommended shows" },
+			"/shows/anticipated": { state: "Anticipated shows" },
+			"/shows/watched": { state: "The most watched shows" },
+			"/shows/collected": { state: "The most collected shows" },
+			"/movies/trending": { state: "Trending movies" },
+			"/movies/popular": { state: "Popular movies" },
+			"/movies/recommended": { state: "Recommended movies" },
+			"/movies/anticipated": { state: "Anticipated movies" },
+			"/movies/watched": { state: "The most watched movies" },
+			"/movies/collected": { state: "The most collected movies" },
+			"/movies/boxoffice": { state: "Top 10 grossing movies" },
+			"/calendars/my": { details: "Browsing my calendar" },
+			"/calendars/my/shows": {
+				details: "Browsing my calendar",
+				state: "Shows"
+			},
+			"/calendars/my/premieres": {
+				details: "Browsing my calendar",
+				state: "Premieres"
+			},
+			"/calendars/my/new-shows": {
+				details: "Browsing my calendar",
+				state: "New shows"
+			},
+			"/calendars/my/movies": {
+				details: "Browsing my calendar",
+				state: "Movies"
+			},
+			"/calendars/my/dvd": {
+				details: "Browsing my calendar",
+				state: "DVD & Blu-ray"
+			},
+			"/calendars/shows": {
+				details: "Browsing calendar",
+				state: "Shows"
+			},
+			"/calendars/premieres": {
+				details: "Browsing calendar",
+				state: "Premieres"
+			},
+			"/calendars/new-shows": {
+				details: "Browsing calendar",
+				state: "New shows"
+			},
+			"/calendars/movies": {
+				details: "Browsing calendar",
+				state: "Movies"
+			},
+			"/calendars/dvd": {
+				details: "Browsing calendar",
+				state: "DVD & Blu-ray"
+			},
+			"/discover": { details: "Finding new shows/movies" },
+			"/apps": { details: "Browsing apps" },
+			"/vip": { details: "Browsing VIP features" },
+			"/settings": { details: "Browsing settings" },
+			"/auth/signin": { details: "Registering on Trakt" },
+			"/auth/login": { details: "Logging in to Trakt" },
+			"/widgets": { details: "Browsing widgets" }
+		};
+
+	for (const [pathname, data] of Object.entries(pages))
+		if (path.includes(pathname)) presenceData = { ...presenceData, ...data };
+
+	if (path.includes("/users")) {
+		const username = document.querySelector(
+			"#avatar-wrapper >  .emojis-supported > a"
+		).textContent;
+		if (path.includes("/history"))
+			presenceData.details = `Browsing ${username}'s history`;
+		else if (path.includes("/progress"))
+			presenceData.details = `Browsing ${username}'s progress`;
+		else if (path.includes("/collection"))
+			presenceData.details = `Browsing ${username}'s collection`;
+		else if (path.includes("/ratings"))
+			presenceData.details = `Browsing ${username}'s ratings`;
+		else if (path.includes("/lists"))
+			presenceData.details = `Browsing ${username}'s lists`;
+		else if (path.includes("/comments"))
+			presenceData.details = `Browsing ${username}'s comments`;
+		else if (path.includes("/network" || "/network/friends"))
+			presenceData.details = `Browsing ${username}'s friends`;
+		else presenceData.details = `Browsing ${username}'s profile`;
+	} else if (path.includes("/shows/")) {
+		if (path.includes("/episodes/")) {
+			presenceData.details = "Browsing episode";
+			presenceData.state = `${
+				document
+					.querySelector(
+						"#summary-wrapper > div.container.summary > div > div > div.col-md-10.col-md-offset-2.col-sm-9.col-sm-offset-3.mobile-title > h2 > a:nth-child(1)"
+					)
+					.textContent.split(":")[0]
+			} - 
+		${
+			document.querySelector(
+				"#summary-wrapper > div.container.summary > div > div > div.col-md-10.col-md-offset-2.col-sm-9.col-sm-offset-3.mobile-title > h1 > span.main-title-sxe"
+			).textContent
+		}`;
+			presenceData.buttons = [{ label: "Check episode", url: document.URL }];
+		} else if (path.includes("/seasons/")) {
+			presenceData.details = "Browsing season";
+			presenceData.state = `${
+				document.querySelector("#level-up-link").textContent
+			}: ${
+				document.querySelector(
+					"#summary-wrapper > div.container.summary > div > div > div.col-md-10.col-md-offset-2.col-sm-9.col-sm-offset-3.mobile-title > h1"
+				).firstChild.textContent
+			}`;
+			presenceData.buttons = [{ label: "Check season", url: document.URL }];
+		} else {
+			try {
+				presenceData.details = "Browsing TV show";
+				presenceData.state = document.querySelector(
+					"#summary-wrapper > div.container.summary > div > div > div.col-md-10.col-md-offset-2.col-sm-9.col-sm-offset-3.mobile-title > h1"
+				).firstChild.textContent;
+				presenceData.buttons = [{ label: "Check TV show", url: document.URL }];
+			} catch (e) {
+				presenceData.details = "Browsing";
+			}
+		}
+	} else if (path.includes("/movies")) {
+		try {
+			presenceData.details = "Browsing movie";
+			presenceData.state = document
+				.querySelector(
+					"#summary-wrapper > div.container.summary > div > div > div.col-md-10.col-md-offset-2.col-sm-9.col-sm-offset-3.mobile-title > h1"
+				)
+				.textContent.split(
+					document.querySelector(
+						"#summary-wrapper > div.container.summary > div > div > div.col-md-10.col-md-offset-2.col-sm-9.col-sm-offset-3.mobile-title > h1 > span"
+					).textContent
+				)[0];
+			presenceData.buttons = [{ label: "Check movie", url: document.URL }];
+		} catch (e) {
+			presenceData.details = "Browsing";
+		}
+	} else if (path.includes("/people")) {
+		presenceData.details = "Browsing actor";
+		presenceData.state = document.querySelector(
+			"#summary-wrapper > div.container.summary > div > div > div.col-md-10.col-md-offset-2.col-sm-9.col-sm-offset-3.mobile-title > h1"
+		).textContent;
+		presenceData.buttons = [{ label: "Check actor", url: document.URL }];
+	} else if (path.includes("/search")) {
+		presenceData.details = "Searching for";
+		presenceData.state = new URLSearchParams(window.location.search).get(
+			"query"
+		);
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
