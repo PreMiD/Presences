5e689419b2d3b5f9667b75b23c610f4c8c422459
nut1414 <nut.pannapat@gmail.com>
2022-06-21 22:10:20 +0700

    feat(jpdb): new presence

---
+++ websites/J/jpdb/presence.ts
@@ -0,0 +1,97 @@
+const presence = new Presence({
+		clientId: "988699263775154176",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+let duecount: string, decktitle: string, query: string, meaning: string;
+
+const decklist: string[] = [
+	"/novel",
+	"/vocabulary-list",
+	"/textbook",
+	"/anime",
+	"/youtube-video",
+	"/live-action",
+	"/video-game",
+	"/web-novel",
+	"/visual-novel",
+	"/audio",
+];
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey: "jpdb",
+		smallImageText: "jpdb.io",
+		startTimestamp: browsingTimestamp,
+	};
+	if (document.location.hostname === "jpdb.io") {
+		if (document.location.pathname.includes("/learn")) {
+			presenceData.details = "Viewing learn page";
+			duecount =
+				document.querySelector('a[href="/learn"]').firstElementChild
+					.textContent;
+			presenceData.state = `Due : ${duecount}\n item(s)`;
+		} else if (document.location.pathname.includes("/review")) {
+			presenceData.details = "Reviewing cards";
+			duecount =
+				document.querySelector('a[href="/learn"]').firstElementChild
+					.textContent;
+			presenceData.state = `Due : ${duecount}\n item(s)`;
+		} else if (document.location.pathname.includes("/search")) {
+			if (document.querySelector("div.results.search")) {
+				presenceData.details = "Searching:";
+				query = decodeURI(
+					new URLSearchParams(document.location.search).get("q")
+				);
+				presenceData.state = query;
+			} else {
+				if (document.querySelector("div.result.kanji")) {
+					presenceData.details = "Viewing a kanji:";
+					query = new URLSearchParams(document.location.search).get("q");
+					meaning = decodeURI(
+						document.querySelector(
+							"div.result.kanji > div.vbox.gap > div.hbox > div.vbox.gap > div > div"
+						).textContent
+					);
+					presenceData.state = `${query} - ${meaning}`;
+				}
+				if (document.querySelector("div.result.vocabulary")) {
+					presenceData.details = "Viewing a word:";
+					query = decodeURI(
+						new URLSearchParams(document.location.search).get("q")
+					);
+					presenceData.state = query;
+				}
+			}
+		} else if (document.location.pathname.includes("/vocabulary")) {
+			presenceData.details = "Viewing a word:";
+			query = decodeURI(document.location.pathname.split("/")[3]);
+			presenceData.state = query;
+		} else if (
+			decklist.some(deck => document.location.pathname.includes(deck))
+		) {
+			presenceData.details = "Viewing pre-built deck";
+			decktitle = document.querySelector("h3").textContent;
+			presenceData.state = decktitle;
+		} else if (document.location.pathname.includes("/deck")) {
+			presenceData.details = "Viewing a deck";
+			decktitle = document.querySelector(
+				"div.container.bugfix > div:nth-child(2)"
+			).textContent;
+			presenceData.state = decktitle;
+		} else if (document.location.pathname.includes("/prebuilt_decks"))
+			presenceData.details = "Finding pre-built deck";
+		else if (document.location.pathname === "/")
+			presenceData.details = "Viewing home page";
+		else if (document.location.pathname.includes("/stats"))
+			presenceData.details = "Viewing stats";
+		else if (document.location.pathname.includes("/quiz"))
+			presenceData.details = "Taking quizes";
+		else if (document.location.pathname.includes("/leaderboard"))
+			presenceData.details = "Viewing leaderboard";
+		else if (document.location.pathname.includes("/settings"))
+			presenceData.details = "Viewing settings page";
+	}
+	if (!presenceData.details) presence.setActivity();
+	else presence.setActivity(presenceData);
+});
