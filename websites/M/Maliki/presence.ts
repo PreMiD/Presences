4131226c0cb7dc004fdfc1a181b832cfb5720de0
RisingSunLight <risingsunlight5776@gmail.com>
2022-06-26 22:13:42 +0200

    wip(Maliki): add all pages

--- websites/M/Maliki/presence.ts
+++ websites/M/Maliki/presence.ts
@@ -11,17 +11,31 @@ const presenceData: PresenceData = {
 };
 const path_array = document.location.pathname.split("/");
 const page_name = path_array.length === 0 ? path_array[0] : path_array[1];
-console.log(page_name+ " " + path_array);
 
 switch(page_name) {
 	case "":
-		presenceData.details = "Regarde la page principale"
+		presenceData.details = "Regarde la page principale";
 		break;
 	case "strips":
-		presenceData.details = path_array[2] == "" ? "Parcourt les strips" : "Lit un strip"
+		presenceData.details = path_array[2] == "" ? "Parcourt les strips" : "Lit un strip";
 		break;
 	case "actualites":
-		presenceData.details = "Parcourt les actualités"
+		presenceData.details = "Parcourt les actualités";
+		break;
+	case "bonus":
+		presenceData.details = "Parcours les bonus";
+		break;
+	case "radio":
+		presenceData.details = "Écoute la radio";
+		break;
+	case "shop":
+		presenceData.details = "Parcours le shop";
+		break;
+	case "events":
+		presenceData.details = "Se renseigne sur les futurs événements";
+		break;
+	case "faq":
+		presenceData.details = "Lire la foire aux questions";
 		break;
 }
 
