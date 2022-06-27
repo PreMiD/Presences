a829de0e1ea424912bb918043d13fa5363caecc2
Ghost0159 <contact.ghost01@gmail.com>
2022-06-27 18:55:07 +0200

    Update presence.ts

--- websites/G/Ghost eShop/presence.ts
+++ websites/G/Ghost eShop/presence.ts
@@ -1,35 +1,35 @@
 const presence = new Presence({
-        clientId: "990900646301544468",
-    }),
-    elapsed = Math.floor(Date.now() / 1000);
+	clientId: "990900646301544468",
+}),
+elapsed = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", () => {
-    const presenceData: PresenceData = {
-            largeImageKey: "https://ghosteshop.com/icon.png",
-            startTimestamp: elapsed,
-        },
-        type = window.location.hostname.split(".");
+const presenceData: PresenceData = {
+		largeImageKey: "https://ghosteshop.com/icon.png",
+		startTimestamp: elapsed,
+	},
+	type = window.location.hostname.split(".");
 
-    if (type.length === 3) {
-        presenceData.details = `Reading the ${type[0].toLowerCase()}`;
-        presenceData.state = `Reading ${document.title}`;
-    } else {
-        switch (window.location.pathname.replace(/\//g, "")) {
-            case "": {
-                presenceData.details = "Reading the Ghost eShop website";
-                presenceData.state = "Home menu";
-                break;
-                        }
-            case "games": {
-                presenceData.details = "Looking for a game";
-                const titles = document.querySelectorAll(".v-card__title");
-                if (titles.length === 2) presenceData.state = titles[1].textContent;
-                else presenceData.state = "Viewing all games";
-                break;
-                       }
-        }
-    }
+if (type.length === 3) {
+	presenceData.details = `Reading the ${type[0].toLowerCase()}`;
+	presenceData.state = `Reading ${document.title}`;
+} else {
+	switch (window.location.pathname.replace(/\//g, "")) {
+		case "": {
+			presenceData.details = "Reading the Ghost eShop website";
+			presenceData.state = "Home menu";
+			break;
+		}
+		case "games": {
+			presenceData.details = "Looking for a game";
+			const titles = document.querySelectorAll(".v-card__title");
+			if (titles.length === 2) presenceData.state = titles[1].textContent;
+			else presenceData.state = "Viewing all games";
+			break;
+		}
+	}
+}
 
-    if (presenceData.details) presence.setActivity(presenceData);
-    else presence.setActivity();
+if (presenceData.details) presence.setActivity(presenceData);
+else presence.setActivity();
 });
