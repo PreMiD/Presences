de4efd62eb89b8dab2ae983b68054dd67f376b91
Ghost0159 <contact.ghost01@gmail.com>
2022-06-27 17:57:51 +0200

    Add files via upload

---
+++ websites/G/Ghost eShop/dist/presence.js
@@ -0,0 +1,39 @@
+const presence = new Presence({
+    clientId: '990900646301544468'
+});
+const elapsed = Math.floor(Date.now() / 1000);
+presence.on('UpdateData', () => {
+    const presenceData = {
+        largeImageKey: 'https://ghosteshop.com/icon.png',
+        startTimestamp: elapsed
+    };
+    const path = window.location.hostname;
+    const page = window.location.pathname.replace(/\//g, '');
+    const type = path.split('.');
+    if (type.length === 3) {
+        presenceData.details = `Reading the ${type[0].toLowerCase()}`;
+        presenceData.state = `Reading ${document.title}`;
+    }
+    else {
+        switch (page) {
+            case '':
+                presenceData.details = 'Reading the Ghost eShop website';
+                presenceData.state = 'Home menu';
+                break;
+            case 'games':
+                presenceData.details = 'Looking for a game';
+                const titles = document.querySelectorAll('.v-card__title');
+                if (titles.length === 2)
+                    presenceData.state = titles[1].textContent;
+                else
+                    presenceData.state = 'Viewing all games';
+                break;
+        }
+        ;
+    }
+    ;
+    if (presenceData.details)
+        presence.setActivity(presenceData);
+    else
+        presence.setActivity();
+});
