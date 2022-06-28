dc929516f452fb02520d0b656ca6cb088c947967
rezarg <88119183+rezarg@users.noreply.github.com>
2022-06-27 23:05:40 -0700

    Fixed Presence
    Formatting and Linting gave me a bunch of errors and broke the presence for some reason, but I managed to fix it.

---
+++ websites/F/Flowlab/dist/presence.js
@@ -0,0 +1,55 @@
+const presence = new Presence({ clientId: "991160367629750372" }), flowlabTimestamp = Math.floor(Date.now() / 1000);
+presence.on("UpdateData", async () => {
+    const presenceData = {
+        largeImageKey: "icon",
+        startTimestamp: flowlabTimestamp,
+    };
+    switch (document.location.hostname) {
+        case "flowlab.io": {
+            switch (document.location.pathname) {
+                case "/": {
+                    presenceData.details = "Viewing Home Page";
+                    break;
+                }
+                case "/game/list": {
+                    presenceData.details = "Viewing Their Games";
+                    break;
+                }
+                case "/game/browse": {
+                    presenceData.details = "Viewing Games Page";
+                    break;
+                }
+            }
+        }
+    }
+    if (document.location.pathname.includes("/profile")) {
+        presenceData.details = `Viewing ${document
+            .getElementsByClassName("username")
+            .item(0)
+            .textContent.trim()}'s Profile`;
+    }
+    else if (document.location.pathname.includes("/view/")) {
+        presenceData.details = `Editing "${document.getElementById("game_title").textContent}"`;
+        presenceData.state = document.getElementById("game_author").textContent;
+    }
+    else if (document.location.pathname.includes("/play/")) {
+        (presenceData.details = `Playing "${document.getElementById("game_title").textContent}"`),
+            (presenceData.state = `${document.getElementById("game_author").textContent}`);
+    }
+    else if (document.location.pathname === "/resources")
+        presenceData.details = "Viewing Flowlab Examples";
+    else if (document.location.pathname === "/lab/blog/")
+        presenceData.details = "Viewing Flowlab Blog";
+    else if (document.location.pathname.includes("/blog/")) {
+        presenceData.state = "Reading Blog Entry";
+        presenceData.details = document
+            .getElementsByClassName("display-2 text-white")
+            .item(0).textContent;
+    }
+    else
+        presenceData.state = "Viewing Flowlab";
+    if (presenceData.details)
+        presence.setActivity(presenceData);
+    else
+        presence.setActivity();
+});
--- websites/F/Flowlab/presence.ts
+++ websites/F/Flowlab/presence.ts
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prefer-query-selector */
 const presence = new Presence({ clientId: "991160367629750372" }),
 	flowlabTimestamp = Math.floor(Date.now() / 1000);
 
@@ -22,38 +23,36 @@ presence.on("UpdateData", async () => {
 					presenceData.details = "Viewing Games Page";
 					break;
 				}
-				default:
-					if (document.location.pathname.includes("/profile")) {
-						presenceData.details = `Viewing ${document
-							.querySelectorAll("username")
-							.item(0)
-							.textContent.trim()}'s Profile`;
-					} else if (document.location.pathname.includes("/view")) {
-						(presenceData.details = `Editing "${
-							document.querySelector("game_title").textContent
-						}"`),
-							(presenceData.state = `${
-								document.querySelector("game_author").textContent
-							}`);
-					} else if (document.location.pathname.includes("/play")) {
-						(presenceData.details = `Playing "${
-							document.querySelector("game_title").textContent
-						}"`),
-							(presenceData.state = `${
-								document.querySelector("game_author").textContent
-							}`);
-					} else if (document.location.pathname === "/resources")
-						presenceData.details = "Viewing Flowlab Examples";
-					else if (document.location.pathname === "/lab/blog/")
-						presenceData.details = "Viewing Flowlab Blog";
-					else if (document.location.pathname.includes("/blog/"))
-						presenceData.state = "Reading Blog Entry";
 			}
-			presenceData.details = `${
-				document.querySelectorAll("display-2 text-white").item(0).textContent
-			}`;
 		}
 	}
+	if (document.location.pathname.includes("/profile")) {
+		presenceData.details = `Viewing ${document
+			.getElementsByClassName("username")
+			.item(0)
+			.textContent.trim()}'s Profile`;
+	} else if (document.location.pathname.includes("/view/")) {
+		presenceData.details = `Editing "${
+			document.getElementById("game_title").textContent
+		}"`;
+		presenceData.state = document.getElementById("game_author").textContent;
+	} else if (document.location.pathname.includes("/play/")) {
+		(presenceData.details = `Playing "${
+			document.getElementById("game_title").textContent
+		}"`),
+			(presenceData.state = `${
+				document.getElementById("game_author").textContent
+			}`);
+	} else if (document.location.pathname === "/resources")
+		presenceData.details = "Viewing Flowlab Examples";
+	else if (document.location.pathname === "/lab/blog/")
+		presenceData.details = "Viewing Flowlab Blog";
+	else if (document.location.pathname.includes("/blog/")) {
+		presenceData.state = "Reading Blog Entry";
+		presenceData.details = document
+			.getElementsByClassName("display-2 text-white")
+			.item(0).textContent;
+	} else presenceData.state = "Viewing Flowlab";
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
