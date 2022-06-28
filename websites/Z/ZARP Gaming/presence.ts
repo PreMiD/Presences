7c92437a220799377d157e8ea985b253986622a4
callumok2004 <callumokane123@gmail.com>
2022-06-28 19:58:16 +0100

    feat(ZARPGaming): new presence

--- websites/Z/ZARP Gaming/presence.ts
+++ websites/Z/ZARP Gaming/presence.ts
@@ -9,78 +9,69 @@ presence.on("UpdateData", () => {
 	switch (window.location.host) {
 		case "zarpgaming.com":
 			if (
-				window.location.pathname == "/index.php" ||
-				window.location.pathname == "/index.php/forum"
+				window.location.pathname === "/index.php" ||
+				window.location.pathname === "/index.php/forum"
 			)
 				presenceData.details = "Viewing the home page";
-
-			if (window.location.pathname.startsWith("/index.php/forum/")) {
+			else if (window.location.pathname.startsWith("/index.php/forum/")) {
 				presenceData.details = "Viewing a section:";
-				presenceData.state = (document.querySelector(
-					"#Kunena > form > div > div.kheader > h2 > span"
-				) as HTMLElement)?.innerText || document.title;
+				presenceData.state =
+					document.querySelector<HTMLElement>(
+						"#Kunena > form > div > div.kheader > h2 > span"
+					)?.textContent ?? document.title;
 				presenceData.buttons = [
 					{
 						label: "Visit Section",
 						url: window.location.href,
 					},
 				];
-			}
-
-			if (window.location.pathname == "/index.php/servers")
+			} else if (window.location.pathname === "/index.php/servers")
 				presenceData.details = "Viewing servers";
-
-			if (window.location.pathname == "/index.php/user-list")
+			else if (window.location.pathname === "/index.php/user-list")
 				presenceData.details = "Viewing the list of users";
-
-			if (window.location.pathname == "/index.php/zarp-vip")
+			else if (window.location.pathname === "/index.php/zarp-vip")
 				presenceData.details = "Viewing the VIP page";
-
-			if (window.location.pathname == "/index.php/zarp-vip/booster-pack")
+			else if (window.location.pathname === "/index.php/zarp-vip/booster-pack")
 				presenceData.details = "Viewing the booster packs";
-
-			if (window.location.pathname == "/index.php/help")
+			else if (window.location.pathname === "/index.php/help")
 				presenceData.details = "Viewing the help page";
-
-			if (window.location.pathname.startsWith("/index.php/help/faq")) {
+			else if (window.location.pathname.startsWith("/index.php/help/faq")) {
 				presenceData.details = "Viewing the FAQ";
 				if (window.location.pathname.includes("faq/"))
 					presenceData.state = document.title;
-			}
-
-			if (
+			} else if (
 				document.querySelector(
 					"#Kunena > div:nth-child(5) > div.kheader > h1 > span"
 				)
 			) {
 				presenceData.details = "Viewing a thead:";
-				presenceData.state = (document.querySelector(
-					"#Kunena > div:nth-child(5) > div.kheader > h1 > span"
-				) as HTMLElement).innerText.replace("TOPIC: ", "");
+				presenceData.state = document
+					.querySelector<HTMLElement>(
+						"#Kunena > div:nth-child(5) > div.kheader > h1 > span"
+					)
+					.textContent.replace("TOPIC: ", "");
 				presenceData.buttons = [
 					{
 						label: "Visit Thread",
 						url: window.location.href,
 					},
 				];
-			}
-
-			if (
+			} else if (
 				window.location.pathname.startsWith("/index.php/2013-11-13-21-35-14/")
 			) {
 				presenceData.details = "Viewing a section:";
-				presenceData.state = (document.querySelector(
+				presenceData.state = document.querySelector<HTMLElement>(
 					"#Kunena > div.kblock.kpathway.breadcrumbs-1 > div > div > div > div.path-element > a"
-				) as HTMLElement).innerText;
+				).textContent;
 				presenceData.buttons = [
 					{
 						label: "Visit Section",
 						url: window.location.href,
 					},
 				];
-			}
-
-			if (window.location.pathname.startsWith("/index.php/forum/profile/")) {
+			} else if (
+				window.location.pathname.startsWith("/index.php/forum/profile/")
+			) {
 				presenceData.details = "Viewing a profile:";
 				presenceData.state = document.title.replace("Profile for ", "");
 				presenceData.buttons = [
@@ -89,44 +80,34 @@ presence.on("UpdateData", () => {
 						url: window.location.href,
 					},
 				];
-			}
-
-			if (
+			} else if (
 				window.location.pathname.startsWith("/index.php/forum/announcement/")
 			) {
 				if (window.location.pathname.includes("list"))
 					presenceData.details = "Viewing the announcement list";
 				else {
 					presenceData.details = "Viewing an announcement:";
-					presenceData.state = (document.querySelector(
+					presenceData.state = document.querySelector<HTMLElement>(
 						"#Kunena > div.kblock.kannouncement > div.kheader > h2 > span"
-					) as HTMLElement).innerText;
+					).textContent;
 				}
-			}
-
-			if (window.location.pathname.includes("/topic/create")) {
+			} else if (window.location.pathname.includes("/topic/create")) {
 				presenceData.details = "Creating a topic in section:";
-				presenceData.state = (document.querySelector(
+				presenceData.state = document.querySelector<HTMLElement>(
 					"#Kunena > div.kblock.kpathway.breadcrumbs-1 > div > div > div > div:nth-child(3) > a"
-				) as HTMLElement).innerText;
-			}
-
-			if (window.location.pathname.includes("/new-topic")) {
+				).textContent;
+			} else if (window.location.pathname.includes("/new-topic")) {
 				presenceData.details = "Creating a topic in section:";
-				const topics = document.querySelector(
-					"#postcatid"
-				) as HTMLSelectElement;
-				for (let i = 0; i < topics.options.length; i++) {
-					if (topics.options[i].selected) {
-						presenceData.state = topics.options[i].innerText
+
+				for (const option of document.querySelector<HTMLSelectElement>("#postcatid")) {
+					if (option.selected) {
+						presenceData.state = option.textContent
 							.replace(/^[-\s]+/, "")
 							.replace(/[-\s]+/g, " ");
 						break;
 					}
 				}
-			}
-
-			if (!presenceData.details) {
+			} else if (!presenceData.details) {
 				presenceData.details = "Viewing a page:";
 				presenceData.state = document.title;
 			}
