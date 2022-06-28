aaf8d2f60aaa671e55a800b6e778bb44d620e714
rezarg <88119183+rezarg@users.noreply.github.com>
2022-06-27 21:55:07 -0700

    Linted

--- websites/F/Flowlab/presence.ts
+++ websites/F/Flowlab/presence.ts
@@ -9,45 +9,49 @@ presence.on("UpdateData", async () => {
 
 	switch (document.location.hostname) {
 		case "flowlab.io": {
-			if (document.location.pathname == "/") {
-				presenceData.details = "Viewing Home Page";
-			} else if (document.location.pathname == "/game/list") {
-				presenceData.details = "Viewing Their Games";
-			} else if (document.location.pathname == "/game/browse") {
-				presenceData.details = "Viewing Games Page";
-			} else if (document.location.pathname.includes("/profile")) {
-				presenceData.details = `Viewing ${document
-					.getElementsByClassName("username")
-					.item(0)
-					.textContent.trim()}'s Profile`;
-			} else if (document.location.pathname.includes("/view")) {
-				presenceData.details = `Editing "${
-					document.getElementById("game_title").textContent
-				}"`;
-				presenceData.state = `${
-					document.getElementById("game_author").textContent
-				}`;
-			} else if (document.location.pathname.includes("/play")) {
-				presenceData.details = `Playing "${
-					document.getElementById("game_title").textContent
-				}"`;
-				presenceData.state = `${
-					document.getElementById("game_author").textContent
-				}`;
-			} else if (document.location.pathname == "/resources") {
-				presenceData.details = "Viewing Flowlab Examples";
-			} else if (document.location.pathname == "/lab/blog/") {
-				presenceData.details = "Viewing Flowlab Blog";
-			} else if (document.location.pathname.includes("/blog/")) {
-				presenceData.state = "Reading Blog Entry";
-				presenceData.details = `${
-					document.getElementsByClassName("display-2 text-white").item(0)
-						.textContent
-				}`;
-			} else {
-				presenceData.state = "";
-				presenceData.details = "";
+			switch (document.location.pathname) {
+				case "/": {
+					presenceData.details = "Viewing Home Page";
+					break;
+				}
+				case "/game/list": {
+					presenceData.details = "Viewing Their Games";
+					break;
+				}
+				case "/game/browse": {
+					presenceData.details = "Viewing Games Page";
+					break;
+				}
+				default:
+					if (document.location.pathname.includes("/profile")) {
+						presenceData.details = `Viewing ${document
+							.querySelectorAll("username")
+							.item(0)
+							.textContent.trim()}'s Profile`;
+					} else if (document.location.pathname.includes("/view")) {
+						(presenceData.details = `Editing "${
+							document.querySelector("game_title").textContent
+						}"`),
+							(presenceData.state = `${
+								document.querySelector("game_author").textContent
+							}`);
+					} else if (document.location.pathname.includes("/play")) {
+						(presenceData.details = `Playing "${
+							document.querySelector("game_title").textContent
+						}"`),
+							(presenceData.state = `${
+								document.querySelector("game_author").textContent
+							}`);
+					} else if (document.location.pathname === "/resources")
+						presenceData.details = "Viewing Flowlab Examples";
+					else if (document.location.pathname === "/lab/blog/")
+						presenceData.details = "Viewing Flowlab Blog";
+					else if (document.location.pathname.includes("/blog/"))
+						presenceData.state = "Reading Blog Entry";
 			}
+			presenceData.details = `${
+				document.querySelectorAll("display-2 text-white").item(0).textContent
+			}`;
 		}
 	}
 
