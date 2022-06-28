643388c4645166f4d5db43cfda402b3853a25116
Tofuine <73127480+realtofuine@users.noreply.github.com>
2022-06-28 09:57:36 -0500

    movies now work for cuevana.pro

--- websites/C/Cuevana 3/presence.ts
+++ websites/C/Cuevana 3/presence.ts
@@ -30,24 +30,88 @@ presence.on(
 );
 
 presence.on("UpdateData", async () => {
+	if(document.location.hostname === "ww1.cuevana3.me") {
+		const presenceData: PresenceData = {
+			largeImageKey: "logo",
+		};
+
+		if (
+			document.location.pathname.match("^/[0-9]") ||
+			document.location.pathname.includes("/episodio")
+		) {
+			const titulo = document.querySelector("h1.Title").textContent,
+				subtitulo = document.querySelector("h2.SubTitle").textContent,
+				cover = document
+					.querySelector("div.backdrop > article > div.Image > figure > img")
+					.getAttribute("data-src");
+
+			if (!document.location.pathname.includes("/episodio")) {
+				presenceData.details = titulo;
+				if (subtitulo === titulo) delete presenceData.state;
+				else presenceData.state = subtitulo;
+				presenceData.buttons = [
+					{ label: "Ver Película", url: window.location.href },
+				];
+				presenceData.largeImageKey = cover;
+			} else {
+				presenceData.details = titulo
+					.replace(document.querySelector("h1.Title > span").textContent, "")
+					.replace(/\s+/g, " ")
+					.trim();
+				presenceData.state = `${
+					document.querySelector("h1.Title > span").textContent
+				} ${subtitulo}`;
+				presenceData.buttons = [
+					{ label: "Ver Episodio", url: window.location.href },
+				];
+				presenceData.largeImageKey = cover;
+			}
+
+			if (iFrameVideo) {
+				(presenceData.smallImageKey = videoPaused ? "pause" : "play"),
+					(presenceData.smallImageText = videoPaused
+						? (await strings).pause
+						: (await strings).play),
+					(presenceData.startTimestamp = startTimestamp),
+					(presenceData.endTimestamp = endTimestamp);
+
+				if (videoPaused) {
+					delete presenceData.startTimestamp;
+					delete presenceData.endTimestamp;
+				}
+			}
+
+			presence.setActivity(presenceData, !videoPaused);
+		} else if (document.location.pathname.includes("/serie/")) {
+			presenceData.details = document.querySelector("h1.Title").textContent;
+			presenceData.smallImageKey = "browsing";
+			presenceData.smallImageText = (await strings).browsing;
+			presenceData.largeImageKey = document
+				.querySelector("div.backdrop > article > div.Image > figure > img")
+				.getAttribute("data-src");
+		} else {
+			presenceData.details = (await strings).browsing;
+			presenceData.smallImageKey = "browsing";
+			presenceData.smallImageText = (await strings).browsing;
+		}
+	}
+		
 	const presenceData: PresenceData = {
 		largeImageKey: "logo",
 	};
 
 	if (
-		document.location.pathname.match("^/[0-9]") ||
+		document.location.pathname.includes("/pelicula") ||
 		document.location.pathname.includes("/episodio")
 	) {
-		const titulo = document.querySelector("h1.Title").textContent,
-			subtitulo = document.querySelector("h2.SubTitle").textContent,
-			cover = document
-				.querySelector("div.backdrop > article > div.Image > figure > img")
-				.getAttribute("data-src");
+		const titulo = document.querySelector("h1.title > span").textContent,
+			cover = `https://ww3.cuevana.pro${String(document
+				.querySelector("figure.poster > img")
+				.getAttribute("src"))}`;
 
 		if (!document.location.pathname.includes("/episodio")) {
 			presenceData.details = titulo;
-			if (subtitulo === titulo) delete presenceData.state;
-			else presenceData.state = subtitulo;
+			delete presenceData.state;
 			presenceData.buttons = [
 				{ label: "Ver Película", url: window.location.href },
 			];
@@ -57,13 +121,11 @@ presence.on("UpdateData", async () => {
 				.replace(document.querySelector("h1.Title > span").textContent, "")
 				.replace(/\s+/g, " ")
 				.trim();
-			presenceData.state = `${
-				document.querySelector("h1.Title > span").textContent
-			} ${subtitulo}`;
+			delete presenceData.state;
 			presenceData.buttons = [
 				{ label: "Ver Episodio", url: window.location.href },
 			];
-			presenceData.largeImageKey = cover;
+			// presenceData.largeImageKey = cover;
 		}
 
 		if (iFrameVideo) {
@@ -93,5 +155,6 @@ presence.on("UpdateData", async () => {
 		presenceData.smallImageKey = "browsing";
 		presenceData.smallImageText = (await strings).browsing;
 	}
+
 	presence.setActivity(presenceData);
 });
