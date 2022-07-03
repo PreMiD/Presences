cfeb0583407f498d9209951c8acf06971dd4673c
NeroPako <neropako.web@outlook.com>
2022-07-03 12:37:36 +0200

    fix(BookWalker): implement required changes

--- websites/B/BookWalker/presence.ts
+++ websites/B/BookWalker/presence.ts
@@ -1,74 +1,77 @@
-const presence = new Presence({ clientId: "991729654757064774" });
+const presence = new Presence({ 
+		clientId: "991729654757064774" 
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey:
-			"https://pool.img.aptoide.com/split-store/85a754bbbfb92ee17701c03ad287ea68_icon.png",
-		startTimestamp: Math.floor(Date.now() / 1000),
+		largeImageKey: "https://i.imgur.com/MixQOh3.png",
+		startTimestamp: browsingTimestamp,
 	};
 
 	switch (document.location.hostname) {
-		case "bookwalker.jp": {
-			if (document.location.pathname.includes("/category")) {
-				presenceData.details = `Browsing category: ${
-					document.querySelector(".o-contents-section__title").textContent
-				}`;
-			} else if (document.location.pathname.includes("/series")) {
-				presenceData.details = `Viewing series: ${
-					document.querySelector(".o-contents-section__title").textContent
-				}`;
-			} else if (document.location.pathname.includes("/search")) {
-				presenceData.details = `Browsing search results for: ${
-					document.querySelector(".o-contents-section__title").textContent
-				}`;
-			} else if (document.location.pathname.includes("/new"))
-				presenceData.details = "Browsing newly added";
-			else if (document.location.pathname.includes("/schedule"))
-				presenceData.details = "Viewing release schedule";
-			else if (document.location.pathname.includes("/holdBooks"))
-				presenceData.details = "Viewing library";
-			else if (document.querySelector(".p-main__title") !== null) {
-				presenceData.details = `Viewing: ${
-					document.querySelector(".p-main__title").textContent
-				}`;
-			} else presenceData.details = "Browsing";
-
-			break;
-		}
-		case "global.bookwalker.jp": {
-			if (document.location.pathname.includes("/categories")) {
-				presenceData.details = `Browsing category: ${
-					document.querySelector(".title-main-inner").textContent
-				}`;
-			} else if (document.location.pathname.includes("/series")) {
-				presenceData.details = `Viewing series: ${
-					document.querySelector(".title-main-inner").textContent
-				}`;
-			} else if (document.location.pathname.includes("/search")) {
-				presenceData.details = `Browsing search results for: ${
-					document.querySelector(".title-main-inner").textContent
-				}`;
-			} else if (document.location.pathname.includes("/holdBooks"))
-				presenceData.details = "Viewing library";
-			else if (document.location.pathname.includes("/release-schedule"))
-				presenceData.details = "Viewing release schedule";
-			else if (document.location.pathname.includes("/new"))
-				presenceData.details = "Browsing newly added";
-			else if (document.location.pathname.includes("/pre-order"))
-				presenceData.details = "Browsing pre-orders";
-			else if (document.querySelector("h1[itemProp='name']") !== null) {
-				presenceData.details = `Viewing: ${
-					document.querySelector("h1[itemProp='name']").textContent
-				}`;
-			} else presenceData.details = "Browsing";
-
-			break;
-		}
-		case "viewer.bookwalker.jp": {
-			presenceData.details = `Reading: ${document.title}`;
-			break;
-		}
-	}
+        case "bookwalker.jp": {
+            if (document.location.pathname.includes("/category")) {
+                presenceData.details = `Browsing category:`
+                presenceData.state = `${document.querySelector(".o-contents-section__title").textContent}`
+            }
+            else if (document.location.pathname.includes("/series")) {
+                presenceData.details = `Viewing series:`;
+                presenceData.state = `${document.querySelector(".o-contents-section__title").textContent}`
+            }
+            else if (document.location.pathname.includes("/search")) {
+                presenceData.details = `Browsing search results for:`
+                presenceData.state = `${document.querySelector(".o-contents-section__title").textContent}`
+            }
+            else if (document.location.pathname.includes("/new"))
+                presenceData.details = `Browsing newly added`
+            else if (document.location.pathname.includes("/schedule"))
+                presenceData.details = `Viewing release schedule`
+            else if (document.location.pathname.includes("/holdBooks"))
+                presenceData.details = `Viewing library`
+            else if (document.querySelector(".p-main__title")) {
+                presenceData.details = `Viewing:`
+                presenceData.state = `${document.querySelector(".p-main__title").textContent}`
+            }
+            else
+                presenceData.details = "Browsing"
+            break;
+        }
+        case "global.bookwalker.jp": {
+            if (document.location.pathname.includes("/categories")) {
+                presenceData.details = `Browsing category:`
+                presenceData.state = `${document.querySelector(".title-main-inner").textContent}`
+            }
+            else if (document.location.pathname.includes("/series")) {
+                presenceData.details = `Viewing series:`
+                presenceData.state = `${document.querySelector(".title-main-inner").textContent}`
+            }
+            else if (document.location.pathname.includes("/search")) {
+                presenceData.details = `Browsing search results for:`
+                presenceData.state = `${document.querySelector(".title-main-inner").textContent}`
+            }
+            else if (document.location.pathname.includes("/holdBooks"))
+                presenceData.details = `Viewing library`
+            else if (document.location.pathname.includes("/release-schedule"))
+                presenceData.details = `Viewing release schedule`
+            else if (document.location.pathname.includes("/new"))
+                presenceData.details = `Browsing newly added`
+            else if (document.location.pathname.includes("/pre-order"))
+                presenceData.details = `Browsing pre-orders`
+            else if (document.querySelector("h1[itemProp='name']")) {
+                presenceData.details = `Viewing:`
+                presenceData.state = `${document.querySelector("h1[itemProp='name']").textContent}`
+            }
+            else
+                presenceData.details = `Browsing`
+            break;
+        }
+        case "viewer.bookwalker.jp": {
+            presenceData.details = `Reading:`
+            presenceData.state = `${document.title}`
+            break;
+        }
+    }
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
