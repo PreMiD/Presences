95b36471a3d1b542c020796698fc762aafab978d
Tofuine <73127480+realtofuine@users.noreply.github.com>
2022-07-02 19:09:05 -0500

    fixed suggestions, added more pages

--- websites/A/Audible/presence.ts
+++ websites/A/Audible/presence.ts
@@ -8,19 +8,18 @@ presence.on("UpdateData", async () => {
 		largeImageKey: "logo",
 	};
 
-	if (document.location.pathname.includes("/account")) {
+	presenceData.startTimestamp = browsingTimestamp;
+	if (document.location.pathname.includes("/account")) 
 		presenceData.details = "Viewing account";
-		delete presenceData.state;
-		presenceData.startTimestamp = browsingTimestamp;
-	} else if (document.location.pathname.includes("/wl")) {
+	 else if (document.location.pathname.includes("/wl")) 
 		presenceData.details = "Viewing wishlist";
-		delete presenceData.state;
-		presenceData.startTimestamp = browsingTimestamp;
-	} else if (document.location.pathname.includes("/library")) {
+	 else if (document.location.pathname.includes("/library")) 
 		presenceData.details = "Viewing library";
-		delete presenceData.state;
-		presenceData.startTimestamp = browsingTimestamp;
-	} else if (document.location.pathname.includes("/pd")) {
+	 else if(document.location.pathname.includes("plus")) 
+		presenceData.details = "Viewing plus catalog";
+	else if(document.location.pathname.includes("gift")) 
+		presenceData.details = "Viewing gift center";
+	 else if (document.location.pathname.includes("/pd")) {
 		const title = document.querySelector(
 				"h1.bc-heading.bc-color-base.bc-pub-break-word.bc-text-bold"
 			).textContent,
@@ -32,8 +31,6 @@ presence.on("UpdateData", async () => {
 				"li.bc-list-item.authorLabel > a.bc-link.bc-color-link"
 			).textContent
 		}`;
-		delete presenceData.state;
-		presenceData.startTimestamp = browsingTimestamp;
 		presenceData.largeImageKey = cover;
 	} else if (document.location.pathname.includes("/webplayer")) {
 		presenceData.details = `Listening to ${document
@@ -55,7 +52,6 @@ presence.on("UpdateData", async () => {
 	} else {
 		presenceData.details = "Browsing";
 		delete presenceData.state;
-		presenceData.startTimestamp = browsingTimestamp;
 	}
 	presence.setActivity(presenceData);
 });
