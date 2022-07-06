a9e3eb0495e35bf95fb315b00c4fc3e394a4345e
Tofuine <73127480+realtofuine@users.noreply.github.com>
2022-07-06 18:42:18 -0500

    add privacy mode

--- websites/G/Google Maps/presence.ts
+++ websites/G/Google Maps/presence.ts
@@ -8,53 +8,67 @@ function lettersOnly(str: string) {
 }
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
+	
+	const privacy = await presence.getSetting("privacy"),
+	 presenceData: PresenceData = {
 		largeImageKey: "logo",
 		startTimestamp: browsingTimestamp,
-	};
+		};
 	if (document.location.pathname.includes("/place")) {
-		const place = document.location.href,
-			indexes = [];
-		for (const [index, element] of Array.from(place).entries())
-			if (element === "/") indexes.push(index);
-
-		presenceData.details = `Viewing ${lettersOnly(
-			place.substring(indexes[4] + 1, indexes[5])
-		)}`;
-		presenceData.buttons = [
-			{ label: "View Place", url: document.location.href },
-		];
-	} else if (document.location.pathname.includes("/dir")) {
-		presenceData.details = "Viewing directions";
-		let from, destination;
-		if (document.querySelector("#sb_ifc50 > input") === null) {
-			(from = document
-				.querySelector("#sb_ifc51 > input")
-				.getAttribute("aria-label")),
-				(destination = document
-					.querySelector("#sb_ifc52 > input")
-					.getAttribute("aria-label"));
+		if (privacy) {
+			presenceData.details = "Viewing a place";
 		} else {
-			(from = document
-				.querySelector("#sb_ifc50 > input")
-				.getAttribute("aria-label")),
-				(destination = document
-					.querySelector("#sb_ifc51 > input")
-					.getAttribute("aria-label"));
-		}
-		presenceData.state = `${from}, ${destination}`;
-		presenceData.buttons = [
-			{ label: "View Directions", url: document.location.href },
-		];
-	} else if (document.location.pathname.includes("/search")) {
-		const search = document.location.href,
-			indexes = [];
-		for (const [index, element] of Array.from(search).entries())
-			if (element === "/") indexes.push(index);
+			const place = document.location.href,
+				indexes = [];
+			for (const [index, element] of Array.from(place).entries())
+				if (element === "/") indexes.push(index);
 
-		presenceData.details = `Searching for ${lettersOnly(
-			search.substring(indexes[4] + 1, indexes[5])
-		)}`;
+			presenceData.details = `Viewing ${lettersOnly(
+				place.substring(indexes[4] + 1, indexes[5])
+			)}`;
+			presenceData.buttons = [
+				{ label: "View Place", url: document.location.href },
+			];
+		}
+	} else if (document.location.pathname.includes("/dir")) {
+		if (privacy) {
+			presenceData.details = "Viewing directions from one place to another";
+		} else {
+			presenceData.details = "Viewing directions";
+			let from, destination;
+			if (document.querySelector("#sb_ifc50 > input") === null) {
+				(from = document
+					.querySelector("#sb_ifc51 > input")
+					.getAttribute("aria-label")),
+					(destination = document
+						.querySelector("#sb_ifc52 > input")
+						.getAttribute("aria-label"));
+			} else {
+				(from = document
+					.querySelector("#sb_ifc50 > input")
+					.getAttribute("aria-label")),
+					(destination = document
+						.querySelector("#sb_ifc51 > input")
+						.getAttribute("aria-label"));
+			}
+			presenceData.state = `${from}, ${destination}`;
+			presenceData.buttons = [
+				{ label: "View Directions", url: document.location.href },
+			];
+		}
+	} else if (document.location.pathname.includes("/search")) {
+		if (privacy) {
+			presenceData.details = "Searching for a place";
+		} else {
+			const search = document.location.href,
+				indexes = [];
+			for (const [index, element] of Array.from(search).entries())
+				if (element === "/") indexes.push(index);
+
+			presenceData.details = `Searching for ${lettersOnly(
+				search.substring(indexes[4] + 1, indexes[5])
+			)}`;
+		}
 	} else presenceData.details = "Viewing map";
 
 	presence.setActivity(presenceData);
