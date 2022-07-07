8dc8c7d0d8201e80c300c01eb94f4a1e163d79d2
sybe <54631194+sybetjeh@users.noreply.github.com>
2022-07-07 19:27:40 +0200

    Things asked to change

--- websites/R/Roblox Forum/presence.ts
+++ websites/R/Roblox Forum/presence.ts
@@ -2,58 +2,50 @@ const presence = new Presence({
 	clientId: "993929577099448481",
 });
 
+const browsingTimestamp = Math.floor(Date.now() / 1000);
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			largeImageKey: "ig-mal",
 			smallImageKey: "small",
 			smallImageText: "Swooosh!",
-		},
-		date = Math.floor(Date.now() / 1000);
+			startTimestamp: browsingTimestamp
+		}
 
 	if (document.location.pathname === "/") {
 		presenceData.details = "Viewing the homepage";
-		presenceData.startTimestamp = date;
 	} else if (document.location.pathname.startsWith("/threads/")) {
 		presenceData.details = "Browsing a thread...";
 		presenceData.state = `Thread: ${document
 			.querySelector("meta[property='og:title']")
 			.getAttribute("content")}`;
-		presenceData.startTimestamp = date;
 	} else if (document.location.pathname.startsWith("/forums/")) {
 		presenceData.details = "Browsing a category...";
 		presenceData.state = `Category: ${document
 			.querySelector("meta[property='og:title']")
 			.getAttribute("content")}`;
-		presenceData.startTimestamp = date;
 	} else if (document.location.pathname.startsWith("/members/")) {
 		presenceData.details = "Viewing a profile...";
 		presenceData.state = `Profile: ${document
 			.querySelector("meta[property='og:title']")
 			.getAttribute("content")}`;
-		presenceData.startTimestamp = date;
 	} else if (document.location.pathname.startsWith("/whats-new/")) {
 		presenceData.details = "Browsing latest content...";
 		presenceData.state = document
 			.querySelector("meta[property='og:title']")
 			.getAttribute("content");
-		presenceData.startTimestamp = date;
 	} else {
 		switch (document.location.pathname) {
 			case "/pages/premium/":
 				presenceData.details = "Browsing premium packages";
-				presenceData.startTimestamp = date;
 				break;
 			case "/account/account-details/":
 				presenceData.details = "Editing my account details";
-				presenceData.startTimestamp = date;
 				break;
 			case "/account/alerts":
 				presenceData.details = "Viewing my alerts in full";
-				presenceData.startTimestamp = date;
 				break;
 			case "/account/privacy":
 				presenceData.details = "Editing my privacy settings";
-				presenceData.startTimestamp = date;
 				break;
 		}
 	}
