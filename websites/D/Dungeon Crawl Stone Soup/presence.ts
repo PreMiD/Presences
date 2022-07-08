a46bc53a31a9311f4f2d1f4ed06c98835c960d6f
impsyutgard <impsyutgard@gmail.com>
2022-07-08 01:07:16 -0600

    Add files via upload

---
+++ websites/D/Dungeon Crawl Stone Soup/presence.ts
@@ -0,0 +1,44 @@
+const presence = new Presence({
+		clientId: "994723983415062548",
+	}),
+	strings = presence.getStrings({
+		play: "presence.playback.playing",
+		pause: "presence.playback.paused",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey: "dcsslogo",
+		startTimestamp: browsingTimestamp,
+	};
+
+	if (window.location.hash === "#lobby")
+  presenceData.details = "Browsing lobby.."
+else if (window.location.hash.startsWith("#watch")) {
+        presenceData.details = `Spectating: ${document.querySelector<HTMLSpanElement>("#stats_titleline").textContent}`
+        presenceData.state = `XL: ${document.querySelector<HTMLSpanElement>("#stats_xl").textContent} | Location: ${document.querySelector<HTMLSpanElement>("#stats_place").textContent}`
+        presenceData.buttons =
+              [
+                {
+                  label: "Spectate",
+                  url: document.location.href
+                }
+              ];
+}
+else if (window.location.hash.startsWith("#play-dcss-")) {
+	presenceData.details = `Playing as: ${document.querySelector<HTMLSpanElement>("#stats_titleline").textContent}`
+	presenceData.state = `XL: ${document.querySelector<HTMLSpanElement>("#stats_xl").textContent} | Location: ${document.querySelector<HTMLSpanElement>("#stats_place").textContent}`
+	presenceData.buttons =
+		  [
+			{
+			  label: "Spectate",
+			  url: `http://crawl.akrasiac.org:8080/#watch-${document.querySelector<HTMLSpanElement>("#stats_titleline").textContent.split("the")[0].trim()}`
+			}
+		  ];
+}
+	
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
+
