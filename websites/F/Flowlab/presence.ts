a9a1340de1c3fcebd9ffd5c07248e82d24be04ed
rezarg <88119183+rezarg@users.noreply.github.com>
2022-07-09 03:11:36 -0700

    Update presence.ts

--- websites/F/Flowlab/presence.ts
+++ websites/F/Flowlab/presence.ts
@@ -6,37 +6,38 @@ presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey: "icon",
 		startTimestamp: flowlabTimestamp,
+      		{ pathname } = document.location
 	};
 
 	switch (document.location.hostname) {
 		case "flowlab.io": {
-			switch (document.location.pathname) {
+			switch (pathname) {
 				case "/": {
-					presenceData.details = "Viewing Home Page";
+					presenceData.details = "Viewing home page";
 					break;
 				}
 				case "/game/list": {
-					presenceData.details = "Viewing Their Games";
+					presenceData.details = "Viewing their games";
 					break;
 				}
 				case "/game/browse": {
-					presenceData.details = "Viewing Games Page";
+					presenceData.details = "Viewing games page";
 					break;
 				}
 			}
 		}
 	}
-	if (document.location.pathname.includes("/profile")) {
+	if (pathname.includes("/profile")) {
 		presenceData.details = `Viewing ${document
 			.getElementsByClassName("username")
 			.item(0)
-			.textContent.trim()}'s Profile`;
-	} else if (document.location.pathname.includes("/view/")) {
+			.textContent.trim()}'s profile`;
+	} else if (pathname.includes("/view/")) {
 		presenceData.details = `Editing "${
 			document.getElementById("game_title").textContent
 		}"`;
 		presenceData.state = document.getElementById("game_author").textContent;
-	} else if (document.location.pathname.includes("/play/")) {
+	} else if (pathname.includes("/play/")) {
 		(presenceData.details = `Playing "${
 			document.getElementById("game_title").textContent
 		}"`),
@@ -44,15 +45,15 @@ presence.on("UpdateData", async () => {
 				document.getElementById("game_author").textContent
 			}`);
 	} else if (document.location.pathname === "/resources")
-		presenceData.details = "Viewing Flowlab Examples";
+		presenceData.details = "Viewing Flowlab examples";
 	else if (document.location.pathname === "/lab/blog/")
-		presenceData.details = "Viewing Flowlab Blog";
+		presenceData.details = "Viewing Flowlab blog";
 	else if (document.location.pathname.includes("/blog/")) {
-		presenceData.state = "Reading Blog Entry";
+		presenceData.state = "Reading blog entry";
 		presenceData.details = document
 			.getElementsByClassName("display-2 text-white")
 			.item(0).textContent;
-	} else presenceData.state = "Viewing Flowlab";
+	} else presenceData.details = "Viewing Flowlab";
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
