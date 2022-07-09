eaff13b1fccc524355646495da78d88b100cc7df
sybe <54631194+sybetjeh@users.noreply.github.com>
2022-07-09 23:37:27 +0200

    Update presence.ts

--- websites/R/Roblox Forum/presence.ts
+++ websites/R/Roblox Forum/presence.ts
@@ -12,28 +12,22 @@ presence.on("UpdateData", async () => {
 	if (document.location.pathname === "/")
 		presenceData.details = "Viewing the homepage";
 	else if (document.location.pathname.startsWith("/threads/")) {
-		presenceData.details = "Browsing a thread...";
-		presenceData.state = `Thread: ${document
-			.querySelector("meta[property='og:title']")
-			.getAttribute("content")}`;
+		presenceData.details = "Browsing a thread";
 		presenceData.state = `Thread: ${document
 			.querySelector("meta[property='og:title']")
 			.getAttribute("content")}`;
 	} else if (document.location.pathname.startsWith("/forums/")) {
-		presenceData.details = "Browsing a category...";
+		presenceData.details = "Browsing a category";
 		presenceData.state = `Category: ${document
 			.querySelector("meta[property='og:title']")
 			.getAttribute("content")}`;
 	} else if (document.location.pathname.startsWith("/members/")) {
-		presenceData.details = "Viewing a profile...";
+		presenceData.details = "Viewing a profile";
 		presenceData.state = `Profile: ${document
 			.querySelector("meta[property='og:title']")
 			.getAttribute("content")}`;
 	} else if (document.location.pathname.startsWith("/whats-new/")) {
-		presenceData.details = "Browsing latest content...";
-		presenceData.state = document
-			.querySelector("meta[property='og:title']")
-			.getAttribute("content");
+		presenceData.details = "Browsing latest content";
 	} else {
 		switch (document.location.pathname) {
 			case "/pages/premium/":
