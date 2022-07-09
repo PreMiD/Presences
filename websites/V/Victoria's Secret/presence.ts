a86ffe0571b04f30b956d1976f3de77c9ffba8b1
Skeptator <edmundassilanskas2016@gmail.com>
2022-07-09 05:30:56 +0100

    Hello World!

--- websites/V/Victoria's Secret/presence.ts
+++ websites/V/Victoria's Secret/presence.ts
@@ -2,10 +2,6 @@ const presence = new Presence({
 		clientId: "994556598414479370",
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
-let user: HTMLElement,
-	title: HTMLElement,
-	replace: HTMLElement,
-	search: HTMLElement;
 
 presence.on("UpdateData", async () => {
 	const sections = [
@@ -43,25 +39,22 @@ presence.on("UpdateData", async () => {
 					document.querySelector("h1").textContent
 				}`;
 			} else if (document.location.pathname.includes("/offers")) {
-				presenceData.details = 'Viewing Offers'
-					document.querySelector("h2").textContent
-
+				presenceData.details = "Viewing Offers";
+				document.querySelector("h2").textContent;
 			} else if (document.location.pathname.includes("/favourites")) {
-				presenceData.details = 'Viewing Favourites'
-					document.querySelector("h2").textContent
-
+				presenceData.details = "Viewing Favourites";
+				document.querySelector("h2").textContent;
 			} else if (document.location.pathname.includes("/Login")) {
-				presenceData.details = 'Logging In'
-					document.querySelector("h2").textContent
-
+				presenceData.details = "Logging In";
+				document.querySelector("h2").textContent;
 			} else if (document.location.pathname.includes("/search")) {
-				presenceData.details = 'Searching:'
+				presenceData.details = "Searching:";
 				presenceData.state = `${
 					document.querySelector("h1").textContent.split("vs")[0]
 				}`;
 			} else if (document.location.pathname.includes("/bras")) {
-				presenceData.details = 'Viewing: Selection of Bras'
-				document.querySelector("h2").textContent
+				presenceData.details = "Viewing: Selection of Bras";
+				document.querySelector("h2").textContent;
 			}
 			break;
 		}
