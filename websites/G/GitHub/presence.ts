d1012345133f2e947026ace104eadf4953c87472
Tofuine <73127480+realtofuine@users.noreply.github.com>
2022-07-11 18:09:55 -0500

    add github gists

--- websites/G/GitHub/presence.ts
+++ websites/G/GitHub/presence.ts
@@ -50,7 +50,7 @@ presence.on("UpdateData", async () => {
 				details: "Viewing subscriptions",
 			},
 		},
-		{ pathname, search, href } = document.location,
+		{ pathname, search, href, hostname } = document.location,
 		[cover, timestamp, privacy] = await Promise.all([
 			presence.getSetting<boolean>("cover"),
 			presence.getSetting<boolean>("timestamp"),
@@ -61,235 +61,270 @@ presence.on("UpdateData", async () => {
 		if (pathname.includes(`/${path}`))
 			presenceData = { ...presenceData, ...data };
 	}
-
-	switch (true) {
-		// * For Profiles
-		case !!document.querySelector<HTMLBodyElement>("body.page-profile"):
-			if (privacy) {
-				presenceData.details = "Viewing a profile";
+	if (hostname === "github.com") {
+		switch (true) {
+			// * For Profiles
+			case !!document.querySelector<HTMLBodyElement>("body.page-profile"):
+				if (privacy) {
+					presenceData.details = "Viewing a profile";
+					break;
+				}
+				const searchParam = new URLSearchParams(search).get("tab"),
+					profileName = document
+						.querySelector("span.p-nickname")
+						.textContent.trim();
+				presenceData.buttons = [{ label: "View Profile", url: href }];
+				if (cover) {
+					presenceData.largeImageKey = `${document.querySelector<HTMLImageElement>("img.avatar").src
+						}.png`;
+				}
+				if (searchParam)
+					presenceData.details = `Viewing ${profileName}'s ${searchParam}`;
+				else presenceData.details = `Viewing ${profileName}'s profile`;
 				break;
-			}
-			const searchParam = new URLSearchParams(search).get("tab"),
-				profileName = document
-					.querySelector("span.p-nickname")
-					.textContent.trim();
-			presenceData.buttons = [{ label: "View Profile", url: href }];
-			if (cover) {
-				presenceData.largeImageKey = `${
-					document.querySelector<HTMLImageElement>("img.avatar").src
-				}.png`;
-			}
-			if (searchParam)
-				presenceData.details = `Viewing ${profileName}'s ${searchParam}`;
-			else presenceData.details = `Viewing ${profileName}'s profile`;
-			break;
-		// * For repositories
-		case !!document.querySelector<HTMLDivElement>(
-			"div#repository-container-header"
-		):
-			const repository = {
-				owner: pathname.split("/")[1],
-				name: pathname.split("/")[2],
-				target: pathname.split("/")[4],
-				id: pathname.split("/")[4],
-			};
-			presenceData.buttons = [{ label: "View Repository", url: href }];
-			if (cover && !privacy) {
-				presenceData.largeImageKey = `https://avatars.githubusercontent.com/u/${
-					document.querySelector<HTMLMetaElement>(
+			// * For repositories
+			case !!document.querySelector<HTMLDivElement>(
+				"div#repository-container-header"
+			):
+				const repository = {
+					owner: pathname.split("/")[1],
+					name: pathname.split("/")[2],
+					target: pathname.split("/")[4],
+					id: pathname.split("/")[4],
+				};
+				presenceData.buttons = [{ label: "View Repository", url: href }];
+				if (cover && !privacy) {
+					presenceData.largeImageKey = `https://avatars.githubusercontent.com/u/${document.querySelector<HTMLMetaElement>(
 						'meta[name~="octolytics-dimension-user_id"]'
 					).content
-				}`;
-			}
-
-			if (pathname.includes("/tree/")) {
-				if (privacy) {
-					presenceData.details = "Viewing a repository";
-					presenceData.state = "In a folder";
-					delete presenceData.buttons;
-					break;
+						}`;
 				}
-				presenceData.details = `Browsing repository ${repository.owner}/${repository.name}`;
 
-				presenceData.state = `In folder ${pathname
-					.split("/")
-					.slice(4)
-					.join("/")}`;
-			} else if (pathname.includes("/blob/")) {
-				if (privacy) {
-					presenceData.details = "Browsing a repository";
-					presenceData.state = "Viewing a file";
-					delete presenceData.buttons;
-					break;
-				}
-				presenceData.details = `Browsing repository ${repository.owner}/${repository.name}`;
-				presenceData.state = `Viewing file ${document
-					.querySelector("h2#blob-path > strong")
-					.textContent.trim()} at ${repository.target}`;
-			} else if (pathname.includes("/issues")) {
-				if (pathname.includes("/issues/")) {
-					if (pathname.includes("new")) {
-						if (privacy) {
-							presenceData.details = "Creating an issue";
-							delete presenceData.state;
-							delete presenceData.buttons;
-							break;
+				if (pathname.includes("/tree/")) {
+					if (privacy) {
+						presenceData.details = "Viewing a repository";
+						presenceData.state = "In a folder";
+						delete presenceData.buttons;
+						break;
+					}
+					presenceData.details = `Browsing repository ${repository.owner}/${repository.name}`;
+
+					presenceData.state = `In folder ${pathname
+						.split("/")
+						.slice(4)
+						.join("/")}`;
+				} else if (pathname.includes("/blob/")) {
+					if (privacy) {
+						presenceData.details = "Browsing a repository";
+						presenceData.state = "Viewing a file";
+						delete presenceData.buttons;
+						break;
+					}
+					presenceData.details = `Browsing repository ${repository.owner}/${repository.name}`;
+					presenceData.state = `Viewing file ${document
+						.querySelector("h2#blob-path > strong")
+						.textContent.trim()} at ${repository.target}`;
+				} else if (pathname.includes("/issues")) {
+					if (pathname.includes("/issues/")) {
+						if (pathname.includes("new")) {
+							if (privacy) {
+								presenceData.details = "Creating an issue";
+								delete presenceData.state;
+								delete presenceData.buttons;
+								break;
+							}
+							presenceData.details = `Creating an issue in ${repository.owner}/${repository.name}`;
+						} else {
+							if (privacy) {
+								presenceData.details = "Looking at an issue";
+								delete presenceData.state;
+								delete presenceData.buttons;
+								break;
+							}
+							presenceData.details = `Looking at issue #${repository.id}`;
+							presenceData.state = `${document.querySelectorAll<HTMLAnchorElement>("a.author")[0]
+								.textContent
+								} - ${document.querySelector<HTMLHeadingElement>("h1.gh-header-title")
+									.textContent
+								}`;
+							presenceData.buttons = [{ label: "View Issue", url: href }];
 						}
-						presenceData.details = `Creating an issue in ${repository.owner}/${repository.name}`;
 					} else {
 						if (privacy) {
-							presenceData.details = "Looking at an issue";
+							presenceData.details = "Browsing issues";
 							delete presenceData.state;
 							delete presenceData.buttons;
 							break;
 						}
-						presenceData.details = `Looking at issue #${repository.id}`;
-						presenceData.state = `${
-							document.querySelectorAll<HTMLAnchorElement>("a.author")[0]
-								.textContent
-						} - ${
-							document.querySelector<HTMLHeadingElement>("h1.gh-header-title")
-								.textContent
-						}`;
-						presenceData.buttons = [{ label: "View Issue", url: href }];
-					}
-				} else {
-					if (privacy) {
 						presenceData.details = "Browsing issues";
+						presenceData.state = `${repository.owner}/${repository.name}`;
+					}
+				} else if (pathname.includes("/pulls")) {
+					if (privacy) {
+						presenceData.details = "Browsing pull requests";
 						delete presenceData.state;
 						delete presenceData.buttons;
 						break;
 					}
-					presenceData.details = "Browsing issues";
-					presenceData.state = `${repository.owner}/${repository.name}`;
-				}
-			} else if (pathname.includes("/pulls")) {
-				if (privacy) {
 					presenceData.details = "Browsing pull requests";
-					delete presenceData.state;
-					delete presenceData.buttons;
-					break;
-				}
-				presenceData.details = "Browsing pull requests";
-				presenceData.state = `${repository.owner}/${repository.name}`;
-			} else if (pathname.includes("/pull")) {
-				if (privacy) {
-					presenceData.details = "Looking at a pull request";
-					delete presenceData.state;
-					delete presenceData.buttons;
-					break;
-				}
-				presenceData.details = `Looking at pull request #${repository.id}`;
-				presenceData.state = `${
-					document.querySelectorAll<HTMLAnchorElement>(
+					presenceData.state = `${repository.owner}/${repository.name}`;
+				} else if (pathname.includes("/pull")) {
+					if (privacy) {
+						presenceData.details = "Looking at a pull request";
+						delete presenceData.state;
+						delete presenceData.buttons;
+						break;
+					}
+					presenceData.details = `Looking at pull request #${repository.id}`;
+					presenceData.state = `${document.querySelectorAll<HTMLAnchorElement>(
 						"a.author.Link--primary"
 					)[0].textContent
-				} - ${
-					document.querySelector<HTMLHeadingElement>("h1.gh-header-title")
+						} - ${document.querySelector<HTMLHeadingElement>("h1.gh-header-title")
+							.textContent
+						}`;
+					presenceData.buttons = [{ label: "View Pull Request", url: href }];
+				} else if (pathname.endsWith("/discussions")) {
+					if (privacy) {
+						presenceData.details = "Browsing discussions";
+						delete presenceData.state;
+						delete presenceData.buttons;
+						break;
+					}
+					presenceData.details = "Browsing discussions in";
+					presenceData.state = `${repository.owner}/${repository.name}`;
+				} else if (pathname.includes("/discussions/")) {
+					if (privacy) {
+						presenceData.details = "Looking at a discussion";
+						delete presenceData.state;
+						delete presenceData.buttons;
+						break;
+					}
+					presenceData.details = `Looking at discussion #${repository.id}`;
+					presenceData.state = `${document.querySelectorAll<HTMLAnchorElement>("a.author")[0]
 						.textContent
-				}`;
-				presenceData.buttons = [{ label: "View Pull Request", url: href }];
-			} else if (pathname.endsWith("/discussions")) {
-				if (privacy) {
-					presenceData.details = "Browsing discussions";
-					delete presenceData.state;
-					delete presenceData.buttons;
-					break;
-				}
-				presenceData.details = "Browsing discussions in";
-				presenceData.state = `${repository.owner}/${repository.name}`;
-			} else if (pathname.includes("/discussions/")) {
-				if (privacy) {
-					presenceData.details = "Looking at a discussion";
-					delete presenceData.state;
-					delete presenceData.buttons;
-					break;
-				}
-				presenceData.details = `Looking at discussion #${repository.id}`;
-				presenceData.state = `${
-					document.querySelectorAll<HTMLAnchorElement>("a.author")[0]
-						.textContent
-				} - ${
-					document.querySelector<HTMLHeadingElement>("h1.gh-header-title")
-						.textContent
-				}`;
-				presenceData.buttons = [{ label: "View Discussion", url: href }];
-			} else if (
-				pathname.includes("/pulse") ||
-				pathname.includes("/graphs/contributors") ||
-				pathname.includes("/community") ||
-				pathname.includes("/graphs/commit-activity") ||
-				pathname.includes("/graphs/code-frequency") ||
-				pathname.includes("/network/dependencies") ||
-				pathname.includes("/graphs/commit-activity") ||
-				pathname.includes("/network") ||
-				pathname.includes("/network/members")
-			) {
-				if (privacy) {
-					presenceData.details = "Browsing insights";
-					delete presenceData.state;
-					delete presenceData.buttons;
-					break;
-				}
-				presenceData.details = `Browsing insights of ${repository.owner} / ${repository.name}`;
+						} - ${document.querySelector<HTMLHeadingElement>("h1.gh-header-title")
+							.textContent
+						}`;
+					presenceData.buttons = [{ label: "View Discussion", url: href }];
+				} else if (
+					pathname.includes("/pulse") ||
+					pathname.includes("/graphs/contributors") ||
+					pathname.includes("/community") ||
+					pathname.includes("/graphs/commit-activity") ||
+					pathname.includes("/graphs/code-frequency") ||
+					pathname.includes("/network/dependencies") ||
+					pathname.includes("/graphs/commit-activity") ||
+					pathname.includes("/network") ||
+					pathname.includes("/network/members")
+				) {
+					if (privacy) {
+						presenceData.details = "Browsing insights";
+						delete presenceData.state;
+						delete presenceData.buttons;
+						break;
+					}
+					presenceData.details = `Browsing insights of ${repository.owner} / ${repository.name}`;
 
-				presenceData.state = document.querySelector<HTMLAnchorElement>(
-					"nav a.js-selected-navigation-item.selected.menu-item"
-				).textContent;
-			} else {
+					presenceData.state = document.querySelector<HTMLAnchorElement>(
+						"nav a.js-selected-navigation-item.selected.menu-item"
+					).textContent;
+				} else {
+					if (privacy) {
+						presenceData.details = "Browsing a repository";
+						delete presenceData.state;
+						delete presenceData.buttons;
+						break;
+					}
+					presenceData.details = "Browsing repository";
+					presenceData.state = `${repository.owner}/${repository.name}`;
+				}
+				break;
+			case !!document.querySelector<HTMLHeadingElement>(
+				"#js-pjax-container > div > header > div.container-xl.pt-4.pt-lg-0.p-responsive.clearfix > div > div.flex-1 > h1"
+			):
 				if (privacy) {
-					presenceData.details = "Browsing a repository";
+					presenceData.details = "Viewing an organization";
 					delete presenceData.state;
 					delete presenceData.buttons;
 					break;
 				}
-				presenceData.details = "Browsing repository";
-				presenceData.state = `${repository.owner}/${repository.name}`;
-			}
-			break;
-		case !!document.querySelector<HTMLHeadingElement>(
-			"#js-pjax-container > div > header > div.container-xl.pt-4.pt-lg-0.p-responsive.clearfix > div > div.flex-1 > h1"
-		):
-			if (privacy) {
 				presenceData.details = "Viewing an organization";
-				delete presenceData.state;
-				delete presenceData.buttons;
+				presenceData.state =
+					document.querySelector<HTMLHeadingElement>("h1").textContent;
+				if (cover) {
+					presenceData.largeImageKey = `${document.querySelector<HTMLImageElement>("div > img").src
+						}`;
+				}
 				break;
-			}
-			presenceData.details = "Viewing an organization";
-			presenceData.state =
-				document.querySelector<HTMLHeadingElement>("h1").textContent;
-			if (cover) {
-				presenceData.largeImageKey = `${
-					document.querySelector<HTMLImageElement>("div > img").src
-				}`;
-			}
-			break;
-		case pathname.includes("/features"):
-			presenceData.details = "Browsing features";
-			if (pathname.includes("copilot"))
-				presenceData.state = "Looking at Github Copilot";
+			case pathname.includes("/features"):
+				presenceData.details = "Browsing features";
+				if (pathname.includes("copilot"))
+					presenceData.state = "Looking at Github Copilot";
 
-			break;
-		case pathname.includes("/orgs/"):
-			if (privacy) {
-				presenceData.details = "Viewing the people in an organization";
-				delete presenceData.state;
-				delete presenceData.buttons;
 				break;
+			case pathname.includes("/orgs/"):
+				if (privacy) {
+					presenceData.details = "Viewing the people in an organization";
+					delete presenceData.state;
+					delete presenceData.buttons;
+					break;
+				}
+				presenceData.details = `Viewing ${pathname.split("/")[2]}'s ${pathname.split("/")[3]
+					}`;
+				if (cover) {
+					presenceData.largeImageKey = `${document.querySelector<HTMLImageElement>("h1 > a > img").src
+						}`;
+				}
+				break;
+			case pathname === "/" || !pathname:
+				presenceData.details = "Viewing the home page";
+				break;
+		}
+	} else {
+		if (document.location.pathname.includes("/discover")) {
+			presenceData.details = "Browsing GitHub gists";
+		} else if (document.location.pathname.includes("/forked")) {
+			presenceData.details = "Browsing forked GitHub gists";
+		} else if (document.location.pathname.includes("/starred")) {
+			presenceData.details = "Browsing starred GitHub gists";
+		} else {
+			switch (true) {
+				// * For Profiles
+				case !!document.querySelector("div.js-profile-editable-replace"):
+					console.log(1);
+					if (privacy) {
+						presenceData.details = "Viewing a profile";
+						break;
+					}
+					const searchParam = new URLSearchParams(search).get("tab"),
+						profileName = document
+							.querySelector("span.p-nickname")
+							.textContent.trim();
+					presenceData.buttons = [{ label: "View Profile", url: href }];
+					if (cover) {
+						presenceData.largeImageKey = `${document.querySelector<HTMLImageElement>("img.avatar.avatar-user.width-full").src
+							}.png`;
+					}
+					console.log(presenceData.largeImageKey);
+					if (searchParam)
+						presenceData.details = `Viewing ${profileName}'s ${searchParam}`;
+					else presenceData.details = `Viewing ${profileName}'s profile`;
+					break;
+				// * For repositories
+				case !!document.querySelector("div.repository-content.gist-content"):
+					if (privacy) {
+						console.log("privacy");
+						presenceData.details = "Viewing a gist";
+						break;
+					}
+					const repository = {
+						owner: pathname.split("/")[1],
+						name: document.querySelector("[itemprop = 'name'] > a").innerHTML,
+					};
+					presenceData.buttons = [{ label: "View Repository", url: href }];
+					presenceData.details = `Browsing gist ${repository.name} by ${repository.owner}`;
 			}
-			presenceData.details = `Viewing ${pathname.split("/")[2]}'s ${
-				pathname.split("/")[3]
-			}`;
-			if (cover) {
-				presenceData.largeImageKey = `${
-					document.querySelector<HTMLImageElement>("h1 > a > img").src
-				}`;
-			}
-			break;
-		case pathname === "/" || !pathname:
-			presenceData.details = "Viewing the home page";
-			break;
+		}
 	}
 	if (timestamp) presenceData.startTimestamp = browsingTimestamp;
 	presence.setActivity(presenceData);
