07821f81feae1d265d651a277e1e8c792bf11571
NimaWoods <96501660+NimaWood@users.noreply.github.com>
2022-07-14 03:59:54 +0200

    Add files via upload

---
+++ websites/M/MEGAKino/presence.ts
@@ -0,0 +1,27 @@
+const presence = new Presence({
+  clientId: "996940670667333742",
+}),
+browsingTimestamp = Math.floor(Date.now() / 1000);
+let user: HTMLElement, title: HTMLElement, search: HTMLElement, image: HTMLElement, simage: HTMLElement, stitle: user: HTMLElement;
+
+presence.on("UpdateData", async () => {
+const presenceData: PresenceData = {
+};
+
+if (document.location.hostname === "megakino.co") {
+  presenceData.startTimestamp = browsingTimestamp;
+  presenceData.largeImageKey = "https://i.ibb.co/mCtbnS1/Opera-Momentaufnahme-2022-07-14-021950-megakino-co.png"
+  if (document.location.pathname === "/")
+    presenceData.details = "Browsing...";
+
+  else if (document.location.pathname.includes("/films/")) {
+    user = document.querySelector(".page__subcol-main > h1");
+    image = document.querySelector(".pmovie__poster > img")
+    presenceData.details = "Schaut Film:";
+    presenceData.state = user.textContent;
+    presenceData.largeImageKey = "https://megakino.co" + image.getAttribute("src");
+  } 
+
+if (presenceData.details) presence.setActivity(presenceData);
+else presence.setActivity();
+});
