c5225230eac0282ccecd8df96c0b4ae80b9a2e16
sybe <54631194+sybetjeh@users.noreply.github.com>
2022-07-14 22:43:21 +0200

    Update presence.ts

---
+++ websites/R/Roblox Forum/presence.ts
@@ -0,0 +1,53 @@
+const presence = new Presence({
+	clientId: "993929577099448481",
+});
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey: "ig-mal",
+		smallImageKey: "small",
+		startTimestamp: Math.floor(Date.now() / 1000),
+	};
+	if (document.location.pathname === "/")
+		presenceData.details = "Viewing the homepage";
+	else if (document.location.pathname.startsWith("/threads/")) {
+		presenceData.details = "Browsing a thread";
+		presenceData.state = `Thread: ${document
+			.querySelector("meta[property='og:title']")
+			.getAttribute("content")}`;
+	} else if (document.location.pathname.startsWith("/forums/")) {
+		presenceData.details = "Browsing a category";
+		presenceData.state = `Category: ${document
+			.querySelector("meta[property='og:title']")
+			.getAttribute("content")}`;
+	} else if (document.location.pathname.startsWith("/members/")) {
+		presenceData.details = "Viewing a profile";
+		presenceData.state = `Profile: ${document
+			.querySelector("meta[property='og:title']")
+			.getAttribute("content")}`;
+	} else if (document.location.pathname.startsWith("/admin.php")) {
+		presenceData.details = "Performing moderation duties";
+	} else if (document.location.pathname.startsWith("/approval-queue/")) {
+		presenceData.details = "Performing moderation duties";
+	} else if (document.location.pathname.startsWith("/reports/")) {
+		presenceData.details = "Performing moderation duties";
+	} else if (document.location.pathname.startsWith("/whats-new/"))
+		presenceData.details = "Browsing latest content";
+	else {
+		switch (document.location.pathname) {
+			case "/pages/premium/":
+				presenceData.details = "Browsing premium packages";
+				break;
+			case "/account/account-details/":
+				presenceData.details = "Editing my account details";
+				break;
+			case "/account/alerts":
+				presenceData.details = "Viewing my alerts in full";
+				break;
+			case "/account/privacy":
+				presenceData.details = "Editing my privacy settings";
+				break;
+		}
+	}
+	presence.setActivity(presenceData);
+});
