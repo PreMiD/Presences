d63f67178592f4222e55a0e5ef4a89a1dd06778b
sybe <54631194+sybetjeh@users.noreply.github.com>
2022-07-15 05:50:01 +0200

    Update presence.ts

--- websites/R/Roblox Forum/presence.ts
+++ websites/R/Roblox Forum/presence.ts
@@ -36,7 +36,11 @@ presence.on("UpdateData", async () => {
 				url: document.location.href,
 			},
 		];
-	} else if (document.location.pathname.startsWith("/admin.php" && "/approval-queue/" && "/reports/"))
+	} else if (
+		document.location.pathname == "/admin.php" ||
+		"/approval-queue/" ||
+		"/reports/"
+	)
 		presenceData.details = "Performing moderation actions";
 	else if (document.location.pathname.startsWith("/whats-new/"))
 		presenceData.details = "Browsing the latest content";
