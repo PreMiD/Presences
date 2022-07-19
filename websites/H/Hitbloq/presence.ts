6440a12b35747fc1448a2e3ead8e8102fdab5146
joerkig <joeri@uitdelft.nl>
2022-07-19 12:32:27 +0200

    feat(Hitbloq): new presence

---
+++ websites/H/Hitbloq/presence.ts
@@ -0,0 +1,168 @@
+const presence = new Presence({
+		clientId: "939893060672827402",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+	const time = await presence.getSetting<boolean>("time"),
+		buttons = await presence.getSetting<boolean>("buttons"),
+		cover = await presence.getSetting<boolean>("cover"),
+		presenceData: PresenceData = {
+			largeImageKey: "logo",
+			startTimestamp: browsingTimestamp,
+		};
+	presenceData.details = document.location.pathname.split("/")[1];
+	switch (document.location.pathname.split("/")[1]) {
+		case "map_pool": {
+			presenceData.details = "Viewing Map Pool";
+			presenceData.state = document
+				.querySelector<HTMLMetaElement>("[property='og:title']")
+				.content.replace(" Map Pool", "");
+			presenceData.buttons = [
+				{
+					label: "View Page",
+					url: document.location.href,
+				},
+			];
+			if (cover) {
+				presenceData.largeImageKey = document.querySelector<HTMLMetaElement>(
+					"[property='og:image']"
+				).content;
+			}
+			break;
+		}
+		case "ladder": {
+			presenceData.details = "Viewing Ladder";
+			presenceData.state = document
+				.querySelector<HTMLMetaElement>("[property='og:title']")
+				.content.replace(" Ladder", "");
+			break;
+		}
+		case "ranked_list": {
+			presenceData.details = "Viewing Ranked List";
+			presenceData.state = document
+				.querySelector<HTMLMetaElement>("[property='og:title']")
+				.content.replace(" Ranked List", "");
+			break;
+		}
+		case "user": {
+			presenceData.details = `User: ${
+				document.querySelector(".player-profile-username > b").textContent
+			}`;
+			presenceData.state = `Pool: ${
+				document.querySelector("title").textContent.split("'s Profile - ")[1]
+			}`;
+			presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
+				".player-profile-image"
+			).src;
+			presenceData.buttons = [
+				{
+					label: "View Profile",
+					url: document.location.href,
+				},
+			];
+			break;
+		}
+		case "leaderboard": {
+			presenceData.details =
+				document.querySelector(".leaderboard-title").textContent;
+			presenceData.state =
+				document.querySelector(".leaderboard-data").childNodes[2].textContent;
+			presenceData.largeImageKey =
+				document.querySelector<HTMLImageElement>(".leaderboard-cover").src;
+			presenceData.buttons = [
+				{
+					label: "View Page",
+					url: document.location.href,
+				},
+			];
+			presenceData.smallImageKey = `${
+				document.location.pathname.split("_")[2]
+			}${document.location.pathname.split("_")[1]}`;
+			let characteristic;
+			switch (document.location.pathname.split("_")[2]) {
+				case "s": {
+					characteristic = "Standard";
+					break;
+				}
+				case "sll": {
+					characteristic = "Lawless";
+					break;
+				}
+				case "s90": {
+					characteristic = "90 Degree";
+					break;
+				}
+				case "s360": {
+					characteristic = "360 Degree";
+					break;
+				}
+				case "s1s": {
+					characteristic = "One Saber";
+					break;
+				}
+				case "sna": {
+					characteristic = "No Arrows";
+					break;
+				}
+			}
+			let difficulty;
+			switch (document.location.pathname.split("_")[1]) {
+				case "e": {
+					difficulty = "Easy";
+					break;
+				}
+				case "n": {
+					difficulty = "Normal";
+					break;
+				}
+				case "h": {
+					difficulty = "Hard";
+					break;
+				}
+				case "ex": {
+					difficulty = "Expert";
+					break;
+				}
+				case "ep": {
+					difficulty = "Expert+";
+					break;
+				}
+			}
+			presenceData.smallImageText = `${characteristic} ${difficulty}`;
+			break;
+		}
+		case "map_pools": {
+			presenceData.details = "Browsing Map Pools";
+			break;
+		}
+		case "about": {
+			presenceData.details = "Viewing About Us";
+			break;
+		}
+		case "contact": {
+			presenceData.details = "Viewing Contact Us";
+			break;
+		}
+		case "actions": {
+			presenceData.details = "Viewing Action Queue";
+			break;
+		}
+		case "add_user": {
+			presenceData.details = "Viewing Add User";
+			break;
+		}
+		case "": {
+			presenceData.details = "Viewing Homepage";
+			break;
+		}
+	}
+
+	cover;
+
+	if (!time) delete presenceData.startTimestamp;
+
+	if (!buttons) delete presenceData.buttons;
+
+	presence.setActivity(presenceData);
+});
