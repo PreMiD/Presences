30641321b1c1c925889703dbe980e309d4595241
Madson Mendes <50425778+MadsonMendes@users.noreply.github.com>
2022-07-21 15:08:07 -0300

    Update presence.ts
    Signed-off-by: Madson Mendes <50425778+MadsonMendes@users.noreply.github.com>

--- websites/A/Alura/presence.ts
+++ websites/A/Alura/presence.ts
@@ -1,31 +1,16 @@
 const presence = new Presence({
-		clientId: "996859957813198990",
+		clientId: "999738497546059907",
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-async function getStrings() {
-	return presence.getStrings(
-		{
-			user: "general.viewUser",
-			readindArticle: "general.readingArticle",
-		},
-		await presence.getSetting<string>("lang").catch(() => "en")
-	);
-}
-let strings: Awaited<ReturnType<typeof getStrings>>,
-	oldLang: string = null;
+let title: HTMLElement;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/mJC39Kz.png",
-			details: "Vendo a Alura",
-			startTimestamp: browsingTimestamp,
-		},
-		newLang = await presence.getSetting<string>("lang").catch(() => "en");
-	if (oldLang !== newLang || !strings) {
-		oldLang = newLang;
-		strings = await getStrings();
-	}
+		largeImageKey: "https://i.imgur.com/mJC39Kz.png",
+		details: "Vendo a Alura",
+		startTimestamp: browsingTimestamp,
+	};
 
 	if (
 		document.location.pathname.includes("/course") &&
@@ -46,48 +31,41 @@ presence.on("UpdateData", async () => {
 			).textContent;
 		}
 
-		if (
+		switch (
 			document
 				.querySelector("a.course-header-banner-breadcrumb__category")
-				.getAttribute("aria-label") === "Front-end"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/D42AEvv.png";
-		else if (
-			document
-				.querySelector("a.course-header-banner-breadcrumb__category")
-				.getAttribute("aria-label") === "Programação"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/p57IsEa.png";
-		else if (
-			document
-				.querySelector("a.course-header-banner-breadcrumb__category")
-				.getAttribute("aria-label") === "DevOps"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/RRyEvvR.png";
-		else if (
-			document
-				.querySelector("a.course-header-banner-breadcrumb__category")
-				.getAttribute("aria-label") === "UX & Design"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/dIKn6Wz.png";
-		else if (
-			document
-				.querySelector("a.course-header-banner-breadcrumb__category")
-				.getAttribute("aria-label") === "Data Science"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/4AQNRB1.png";
-		else if (
-			document
-				.querySelector("a.course-header-banner-breadcrumb__category")
-				.getAttribute("aria-label") === "Mobile"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/mLpf7Yz.png";
-		else if (
-			document
-				.querySelector("a.course-header-banner-breadcrumb__category")
-				.getAttribute("aria-label") === "Inovação & Gestão"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/RrC0z8A.png";
+				.getAttribute("aria-label")
+		) {
+			case "Front-end": {
+				presenceData.largeImageKey = "https://i.imgur.com/D42AEvv.png";
+				break;
+			}
+			case "Programação": {
+				presenceData.largeImageKey = "https://i.imgur.com/p57IsEa.png";
+				break;
+			}
+			case "DevOps": {
+				presenceData.largeImageKey = "https://i.imgur.com/RRyEvvR.png";
+				break;
+			}
+			case "UX & Design": {
+				presenceData.largeImageKey = "https://i.imgur.com/dIKn6Wz.png";
+				break;
+			}
+			case "Data Science": {
+				presenceData.largeImageKey = "https://i.imgur.com/4AQNRB1.png";
+				break;
+			}
+			case "Mobile": {
+				presenceData.largeImageKey = "https://i.imgur.com/mLpf7Yz.png";
+				break;
+			}
+			case "Inovação & Gestão":
+				{
+					presenceData.largeImageKey = "https://i.imgur.com/RrC0z8A.png";
+				}
+				break;
+		}
 	} else if (document.location.pathname.includes("/dashboard"))
 		presenceData.details = "Vendo a dashboard";
 	else if (document.location.pathname.includes("/formacao")) {
@@ -96,46 +74,44 @@ presence.on("UpdateData", async () => {
 			"div.formacao-header-headline > h1"
 		).textContent;
 
-		if (
-			document.querySelector("div.formacao__info-categoria > a").textContent ===
-			"Front-end"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/D42AEvv.png";
-		else if (
-			document.querySelector("div.formacao__info-categoria > a").textContent ===
-			"Programação"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/p57IsEa.png";
-		else if (
-			document.querySelector("div.formacao__info-categoria > a").textContent ===
-			"DevOps"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/RRyEvvR.png";
-		else if (
-			document.querySelector("div.formacao__info-categoria > a").textContent ===
-			"UX & Design"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/dIKn6Wz.png";
-		else if (
-			document.querySelector("div.formacao__info-categoria > a").textContent ===
-			"Data Science"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/4AQNRB1.png";
-		else if (
-			document.querySelector("div.formacao__info-categoria > a").textContent ===
-			"Mobile"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/mLpf7Yz.png";
-		else if (
-			document.querySelector("div.formacao__info-categoria > a").textContent ===
-			"Inovação & Gestão"
-		)
-			presenceData.largeImageKey = "https://i.imgur.com/RrC0z8A.png";
+		switch (
+			document.querySelector("div.formacao__info-categoria > a").textContent
+		) {
+			case "Front-end": {
+				presenceData.largeImageKey = "https://i.imgur.com/D42AEvv.png";
+				break;
+			}
+			case "Programação": {
+				presenceData.largeImageKey = "https://i.imgur.com/p57IsEa.png";
+				break;
+			}
+			case "DevOps": {
+				presenceData.largeImageKey = "https://i.imgur.com/RRyEvvR.png";
+				break;
+			}
+			case "UX & Design": {
+				presenceData.largeImageKey = "https://i.imgur.com/dIKn6Wz.png";
+				break;
+			}
+			case "Data Science": {
+				presenceData.largeImageKey = "https://i.imgur.com/4AQNRB1.png";
+				break;
+			}
+			case "Mobile": {
+				presenceData.largeImageKey = "https://i.imgur.com/mLpf7Yz.png";
+				break;
+			}
+			case "Inovação & Gestão":
+				{
+					presenceData.largeImageKey = "https://i.imgur.com/RrC0z8A.png";
+				}
+				break;
+		}
 	} else if (document.location.pathname.includes("/courses"))
 		presenceData.details = "Vendo meus cursos";
 	else if (document.location.pathname.includes("/user")) {
 		if (document.querySelector(".profile-header-name")) {
-			presenceData.details = strings.user;
+			presenceData.details = "Vendo o perfil de um usuário";
 			presenceData.state = document.querySelector(
 				".profile-header-name"
 			).textContent;
@@ -143,79 +119,49 @@ presence.on("UpdateData", async () => {
 			if (document.location.pathname.includes("/planos-de-estudo"))
 				presenceData.details = "Vendo planos de estudo";
 			else if (document.location.pathname.includes("/payments"))
-				presenceData.details = "Vendo pegamentos";
+				presenceData.details = "Vendo pagamentos";
 			else document.location.pathname.includes("/rank");
-			presenceData.details = "Vendo o ranking";
+			presenceData.details = "Vendo o ranking da comunidade";
 		}
 	} else if (document.location.pathname.includes("/aluraflix")) {
 		presenceData.largeImageKey = "https://i.imgur.com/43iG62U.png";
 		presenceData.details = "Vendo o Aluraflix";
 	} else if (document.location.pathname.includes("/carreirasemfronteiras")) {
+		title = document.querySelector(".podcast-header-title");
 		presenceData.largeImageKey = "https://i.imgur.com/XbsVEFV.png";
 		presenceData.details = 'Ouvindo o "Carreiras sem Fronteiras"';
-		if (
-			document.querySelector(".podcast-header-title") &&
-			!document.querySelector(".--margin-left-image")
-		) {
-			presenceData.state = `"${
-				document.querySelector(".podcast-header-title").textContent
-			}"`;
-		}
-	} else if (document.location.pathname.includes("/cases")) {
-		presenceData.largeImageKey = "https://i.imgur.com/rtevoAV.png";
-		presenceData.details = "Assistindo um Case";
-		if (
-			document.querySelector(".intro__title") &&
-			document.location.pathname.includes("/extra")
-		) {
-			presenceData.state = `"${
-				document.querySelector(".intro__title").textContent
-			}"`;
-		}
+		if (title && !document.querySelector(".--margin-left-image"))
+			presenceData.state = `"${title.textContent}"`;
 	} else if (document.location.pathname.includes("/scubadev")) {
+		title = document.querySelector(".podcast-header-title");
 		presenceData.largeImageKey = "https://i.imgur.com/SMQfe3Y.png";
 		presenceData.details = 'Ouvindo o "ScubaDev"';
-		if (
-			document.querySelector(".podcast-header-title") &&
-			!document.querySelector(".--margin-left-image")
-		) {
-			presenceData.state = `"${
-				document.querySelector(".podcast-header-title").textContent
-			}"`;
-		}
+		if (title && !document.querySelector(".--margin-left-image"))
+			presenceData.state = `"${title.textContent}"`;
 	} else if (document.location.pathname.includes("/layerstech")) {
+		title = document.querySelector(".podcast-header-title");
 		presenceData.largeImageKey = "https://i.imgur.com/xpUDQWR.png";
 		presenceData.details = 'Ouvindo o "Layers.Tech"';
-		if (
-			document.querySelector(".podcast-header-title") &&
-			!document.querySelector(".--margin-left-image")
-		) {
-			presenceData.state = `"${
-				document.querySelector(".podcast-header-title").textContent
-			}"`;
-		}
+		if (title && !document.querySelector(".--margin-left-image"))
+			presenceData.state = `"${title.textContent}"`;
 	} else if (document.location.pathname.includes("/likeaboss")) {
+		title = document.querySelector(".podcast-header-title");
 		presenceData.largeImageKey = "https://i.imgur.com/96xQTpM.png";
 		presenceData.details = 'Ouvindo o "Like a Boss"';
-		if (
-			document.querySelector(".podcast-header-title") &&
-			!document.querySelector(".--margin-left-image")
-		) {
-			presenceData.state = `"${
-				document.querySelector(".podcast-header-title").textContent
-			}"`;
-		}
+		if (title && !document.querySelector(".--margin-left-image"))
+			presenceData.state = `"${title.textContent}"`;
 	} else if (document.location.pathname.includes("/hipsterstech")) {
+		title = document.querySelector(".podcast-header-title");
 		presenceData.largeImageKey = "https://i.imgur.com/T7SMsQU.png";
-		presenceData.details = 'Ouvindo o "Hipsters.tech"';
-		if (
-			document.querySelector(".podcast-header-title") &&
-			!document.querySelector(".--margin-left-image")
-		) {
-			presenceData.state = `"${
-				document.querySelector(".podcast-header-title").textContent
-			}"`;
-		}
+		presenceData.details = 'Ouvindo o "Hipsters.Tech"';
+		if (title && !document.querySelector(".--margin-left-image"))
+			presenceData.state = `"${title.textContent}"`;
+	} else if (document.location.pathname.includes("/cases")) {
+		title = document.querySelector(".intro__title");
+		presenceData.largeImageKey = "https://i.imgur.com/rtevoAV.png";
+		presenceData.details = "Assistindo um case";
+		if (title && document.location.pathname.includes("/extra"))
+			presenceData.state = `"${title.textContent}"`;
 	} else if (document.location.pathname.includes("/alura-mais")) {
 		presenceData.largeImageKey = "https://i.imgur.com/KrviSfF.png";
 		presenceData.details = "Vendo Alura+";
