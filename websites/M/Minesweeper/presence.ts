97d9ecfa685f451e8e124eb583f463332416f6d7
Josh <jjlum@frontier.com>
2022-07-26 23:30:41 -0700

    Ran prettier

--- websites/M/Minesweeper/presence.ts
+++ websites/M/Minesweeper/presence.ts
@@ -1,7 +1,7 @@
 const presence = new Presence({
-	clientId: "989759189394030613",
-}),
-browsingTimestamp = Math.floor(Date.now() / 1000);
+		clientId: "989759189394030613",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
@@ -9,100 +9,103 @@ presence.on("UpdateData", async () => {
 			startTimestamp: browsingTimestamp,
 		},
 		{ pathname } = window.location;
-	
+
 	if (document.location.pathname === "/")
 		presenceData.details = "Viewing the homepage";
 	else if (document.location.pathname.startsWith("/game/")) {
 		presenceData.details = "Clicking cells";
-		presenceData.state = `Difficulty: ${document
-			.querySelector<HTMLSpanElement>("a.level-select-link.active > span").textContent}`;
-			presenceData.buttons = [
-				{
-				  label: "Spectate Game",
-				  url: location.href
-				}
-			  ];
+		presenceData.state = `Difficulty: ${
+			document.querySelector<HTMLSpanElement>(
+				"a.level-select-link.active > span"
+			).textContent
+		}`;
+		presenceData.buttons = [
+			{
+				label: "Spectate Game",
+				url: location.href,
+			},
+		];
 	} else if (document.location.pathname.startsWith("/player/")) {
 		presenceData.details = "Viewing profile";
 		presenceData.state = `${document
 			.querySelector("meta[property='og:title']")
-			.getAttribute("content").replace(' - Minesweeper Online', '')}`;
-			presenceData.buttons = [
-				{
-					label: "View Profile",
-					url: location.href
-				}
-			];
+			.getAttribute("content")
+			.replace(" - Minesweeper Online", "")}`;
+		presenceData.buttons = [
+			{
+				label: "View Profile",
+				url: location.href,
+			},
+		];
 	} else if (document.location.pathname.startsWith("/help/")) {
-		presenceData.details = "Viewing the help page"
+		presenceData.details = "Viewing the help page";
 		presenceData.state = `${document
 			.querySelector("meta[property='og:title']")
-			.getAttribute("content").replace(' - Minesweeper Online', '')}`; 	
-		}
-	else {	
-			switch (pathname) {
-			  
+			.getAttribute("content")
+			.replace(" - Minesweeper Online", "")}`;
+	} else {
+		switch (pathname) {
 			case "/ranking": {
 				presenceData.details = "Viewing the rankings";
 				break;
-	}
+			}
 			case "/my-games": {
 				presenceData.details = "Viewing their games history";
 				break;
-	}			
+			}
 			case "/best-players": {
 				presenceData.details = "Viewing the best players";
 				break;
-	}			
+			}
 			case "/season-leaders": {
 				presenceData.details = "Viewing this seasons's leaders";
 				break;
-	}				
+			}
 			case "/quests": {
 				presenceData.details = "Viewing their quests";
 				break;
-	}		
+			}
 			case "/arena": {
 				presenceData.details = "In the arena";
-				break;	
-	}			
+				break;
+			}
 			case "/equipment": {
 				presenceData.details = "Viewing their equipment";
-				break;		
-	}			
+				break;
+			}
 			case "/marketplace": {
 				presenceData.details = "Viewing the marketplace";
-				break;		
-	}			
+				break;
+			}
 			case "/events": {
 				presenceData.details = "Viewing the event";
-				break;	
-	}				
+				break;
+			}
 			case "/players-online": {
 				presenceData.details = "Viewing players online";
-				break;		
-	}			
+				break;
+			}
 			case "/news": {
 				presenceData.details = "Viewing the news";
-				break;	
-	}				
+				break;
+			}
 			case "/statistics": {
 				presenceData.details = "Viewing statistics";
-				break;		
-	}			
+				break;
+			}
 			case "/chat": {
 				presenceData.details = "Chatting";
-				break;	
-	}					
+				break;
+			}
 			case "/premium": {
 				presenceData.details = "Viewing premium perks";
-				break;	
-	}			
+				break;
+			}
 			case "/shop": {
 				presenceData.details = "Viewing the shop";
-				break;			
-			}		
-		}		
-	}	
+				break;
+			}
+		}
+	}
 	presence.setActivity(presenceData);
 });
