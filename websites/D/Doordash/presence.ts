0a7c15c37b68a017113894bc7c6615328b5191ab
astrid <k1ndled11@gmail.com>
2022-07-27 13:22:09 -0400

    refactor(Doordash): lint code

--- websites/D/Doordash/presence.ts
+++ websites/D/Doordash/presence.ts
@@ -1,36 +1,43 @@
 const presence = new Presence({
-	clientId: "1001831947611426877",
-});
-
-const started = Date.now();
+		clientId: "1001831947611426877",
+	}),
+	started = Date.now();
 
 presence.on("UpdateData", async () => {
-	const path: string = document.location.pathname;
-
-	const presenceData: PresenceData = {
-		largeImageKey: "logo",
-		startTimestamp: started,
-	};
+	const path: string = document.location.pathname,
+		presenceData: PresenceData = {
+			largeImageKey: "logo",
+			startTimestamp: started,
+		};
 
 	// If they're using includes statements, it means the page is not a static page (like home, orders, etc.)
 
 	// Store page
 	if (path.includes("/store/") && !path.includes("/search/")) {
-		const name = document.querySelector<HTMLSpanElement>("h1.styles__TextElement-sc-1jrzcv7-0").textContent;
-		const image = document.querySelector<HTMLImageElement>(".menu-header-styles__LogoInner-sc-1kgi6s8-6 > picture:nth-child(1) > img:nth-child(5)");
+		const name = document.querySelector<HTMLSpanElement>(
+				"h1.styles__TextElement-sc-1jrzcv7-0"
+			).textContent,
+			image = document.querySelector<HTMLImageElement>(
+				".menu-header-styles__LogoInner-sc-1kgi6s8-6 > picture:nth-child(1) > img:nth-child(5)"
+			),
+			hideFood = await presence.getSetting<boolean>("hideFood");
 
-		const menuItemModal = document.querySelector<HTMLImageElement>(".styles__ModalContainer-sc-1r4qbfh-0")
-		const hideFood = await presence.getSetting<boolean>("hideFood");
-
-		if (menuItemModal === null) {
+		if (
+			document.querySelector<HTMLImageElement>(
+				".styles__ModalContainer-sc-1r4qbfh-0"
+			) === null
+		) {
 			presenceData.largeImageKey = image.src;
 			presenceData.smallImageKey = "logo";
 			presenceData.details = `Viewing the menu of ${name}`;
 		} else {
 			if (!hideFood) {
-				const menuItemName = document.querySelector<HTMLSpanElement>(".bbOfWk").textContent;
-				const menuItemImage = document.querySelector<HTMLImageElement>(".BaseItemModalStyles__PictureFrame-sc-mpq8va-1 > picture:nth-child(1) > img:nth-child(5)");
-				presenceData.state = `Looking at ${menuItemName}`;
+				const menuItemImage = document.querySelector<HTMLImageElement>(
+					".BaseItemModalStyles__PictureFrame-sc-mpq8va-1 > picture:nth-child(1) > img:nth-child(5)"
+				);
+				presenceData.state = `Looking at ${
+					document.querySelector<HTMLSpanElement>(".bbOfWk").textContent
+				}`;
 				presenceData.largeImageKey = menuItemImage.src;
 			}
 
@@ -42,9 +49,11 @@ presence.on("UpdateData", async () => {
 	} else if (path.includes("/search/store/")) {
 		const hideSearch = await presence.getSetting<boolean>("hideSearch");
 		if (!hideSearch) {
-			const search = document.querySelector<HTMLInputElement>("#FieldWrapper-0");
-			const searchItem = search ? search.value : "something";
-			presenceData.details = `Searching for ${searchItem}`;
+			const search =
+				document.querySelector<HTMLInputElement>("#FieldWrapper-0");
+			presenceData.details = `Searching for ${
+				search ? search.value : "something"
+			}`;
 		}
 	}
 
@@ -54,23 +63,20 @@ presence.on("UpdateData", async () => {
 			presenceData.details = "Browsing Doordash";
 			let activeSpan;
 
-			// Get active menu type (Fast Food, Pizza, etc.)
+			const spanArray = Array.from(document.querySelectorAll(".sc-hFLmAl"));
 
-			const menuTypes = document.getElementsByClassName('sc-hFLmAl');
-			// Loop through all spans to find the active one
-			Array.from(menuTypes).forEach((item: HTMLSpanElement) => {
-				// Get the color of the span
-				const color = item.attributes[1].value;
+			for (const span of spanArray) {
 				// If the color is red, it's the active one, so set it
-				if (color === "#EB1700")
-					activeSpan = item;
-			})
+				if (span.attributes[1].value === "#EB1700") activeSpan = span;
+			}
 
 			// If there is an active span, set the rpc state to the text of the span
 			if (activeSpan) {
-				let span = activeSpan as HTMLSpanElement;
-				presenceData.state = `Looking for ${span.textContent}`;
-			}
+presenceData.state = `Looking for ${
+					(activeSpan as HTMLSpanElement).textContent
+				}`;
+}
+
 			break;
 		}
 		// Orders page
