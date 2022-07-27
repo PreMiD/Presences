19d033b2100fbf858aec83d95eceee03902e9f42
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-07-27 10:39:57 +0200

    fix(Discord): dm username

--- websites/D/Discord/presence.ts
+++ websites/D/Discord/presence.ts
@@ -133,7 +133,10 @@ presence.on("UpdateData", async () => {
 								"input[name=channel_name]"
 							) as HTMLInputElement
 					  )?.value || "Undefined"
-					: `@${document.querySelector("h3")?.textContent || "Undefined"}`,
+					: `@${
+							document.querySelector("[aria-label='Channel header']")
+								?.firstElementChild?.children[2]?.textContent || "Undefined"
+					  }`,
 				serverTyping =
 					Array.from(
 						document.querySelectorAll("div[contenteditable=true]")
