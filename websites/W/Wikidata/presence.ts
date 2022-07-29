6d6b378b5d70321bbf9eedb5d1ce7e7cfe5c246e
Hans5958 <hans5958@outlook.com>
2022-07-29 23:12:56 +0700

    feat(Wikidata): update presence (1.1.0)

--- websites/W/Wikidata/presence.ts
+++ websites/W/Wikidata/presence.ts
@@ -1,25 +1,20 @@
 const presence = new Presence({
-	clientId: "733216821758525460",
-});
-
+		clientId: "733216821758525460",
+	}),
+	browsingStamp = Math.floor(Date.now() / 1000);
 let currentURL = new URL(document.location.href),
-	currentPath = currentURL.pathname.replace(/^\/|\/$/g, "").split("/");
-const browsingTimestamp = Math.floor(Date.now() / 1000);
-let presenceData: PresenceData = {
-	details: "Viewing an unsupported page",
-	largeImageKey: "lg",
-	startTimestamp: browsingTimestamp,
-};
+	presenceData: PresenceData = {
+		details: "Viewing an unsupported page",
+		largeImageKey: "lg",
+		startTimestamp: browsingStamp,
+	};
 const updateCallback = {
-		_function: null as () => void,
+		_function: () => void {} as () => void,
 		get function(): () => void {
 			return this._function;
 		},
 		set function(parameter) {
 			this._function = parameter;
-		},
-		get present(): boolean {
-			return this._function !== null;
 		},
 	},
 	/**
@@ -29,11 +24,10 @@ const updateCallback = {
 		defaultData: PresenceData = {
 			details: "Viewing an unsupported page",
 			largeImageKey: "lg",
-			startTimestamp: browsingTimestamp,
+			startTimestamp: browsingStamp,
 		}
 	): void => {
 		currentURL = new URL(document.location.href);
-		currentPath = currentURL.pathname.replace(/^\/|\/$/g, "").split("/");
 		presenceData = { ...defaultData };
 	},
 	/**
@@ -44,83 +38,97 @@ const updateCallback = {
 		return currentURL.searchParams.get(urlParam);
 	};
 
-((): void => {
-	let title: string;
-	const actionResult = (): string =>
-			getURLParam("action") || getURLParam("veaction"),
-		titleFromURL = (): string => {
-			return decodeURI(
-				(currentPath[1] === "index.php"
-					? getURLParam("title")
-					: currentPath.slice(1).join("/")
-				).replaceAll("_", " ")
+/**
+ * An object obtained from on `mw.config.values`.
+ * (Unused values on this type has been removed. Refer to the link for the full object.)
+ * @link https://github.com/wikimedia-gadgets/types-mediawiki/blob/main/mw/config.d.ts
+ */
+type mwConfigValues = {
+	wgContentLanguage: string;
+	wgSiteName: string;
+	wgAction: string;
+	wgCanonicalNamespace: string;
+	wgCurRevisionId: number;
+	wgNamespaceNumber: number;
+	wgPageName: string;
+	wgRevisionId: number;
+	wgIsMainPage: boolean;
+	wgDiffOldId: number | false;
+	wgDiffNewId: number;
+};
+
+const prepare = async (): Promise<void> => {
+	const mwConfig: mwConfigValues = await presence.getPageletiable(
+			'mw"]["config"]["values'
+		),
+		action: string = mwConfig.wgAction,
+		actionFromURL = (): string =>
+			getURLParam("action") || getURLParam("veaction") || "view",
+		titleFromConfig: string = decodeURIComponent(
+			mwConfig.wgPageName.replaceAll("_", " ")
+		),
+		title =
+			document.querySelector(".wikibase-title-label")?.textContent.trim() ||
+			document.querySelector("h1")?.textContent.trim() ||
+			titleFromConfig,
+		/**
+		 * Returns details based on the namespace.
+		 * @link https://www.wikidata.org/wiki/Help:Namespaces
+		 */
+		namespaceDetails = (): string => {
+			const details: { [index: string]: string } = {
+				"-2": "Viewing a media",
+				"-1": "Viewing a special page",
+				0: "Viewing an item",
+				1: "Viewing a talk page",
+				2: "Viewing a user page",
+				3: "Viewing a user talk page",
+				4: "Viewing a project page",
+				5: "Viewing a project talk page",
+				6: "Viewing a file",
+				7: "Viewing a file talk page",
+				8: "Viewing an interface page",
+				9: "Viewing an interface talk page",
+				10: "Viewing a template",
+				11: "Viewing a template talk page",
+				12: "Viewing a help page",
+				13: "Viewing a help talk page",
+				14: "Viewing a category",
+				15: "Viewing a category talk page",
+				120: "Viewing a propery",
+				121: "Viewing a propery talk page",
+				122: "Viewing a query",
+				123: "Viewing a query talk page",
+				146: "Viewing a lexeme",
+				147: "Viewing a lexeme talk page",
+				640: "Viewing a schema",
+				641: "Viewing a schema talk page",
+				828: "Viewing a module",
+				829: "Viewing a module talk page",
+				2300: "Viewing a gadget",
+				2301: "Viewing a gadget talk page",
+				2302: "Viewing a gadget definition page",
+				2303: "Viewing a gadget definition talk page",
+				2600: "Viewing a topic",
+			};
+			return (
+				details[mwConfig.wgNamespaceNumber] ||
+				`Viewing a/an ${mwConfig.wgCanonicalNamespace.replaceAll(
+					"_",
+					" "
+				)} page`
 			);
 		};
 
-	try {
-		title = document.querySelector(".wikibase-title")
-			? document.querySelector(".wikibase-title-label").textContent.trim()
-			: document.querySelector("h1").textContent;
-	} catch (e) {
-		title = titleFromURL();
-	}
+	//
+	// Important note:
+	//
+	// When checking for the current location, avoid using the URL.
+	// The URL is going to be different in other languages.
+	// Use the elements on the page or the config instead.
+	//
 
-	/**
-	 * Returns details based on the namespace.
-	 * @link https://www.wikidata.org/wiki/Help:Namespaces
-	 */
-	const namespaceDetails = (): string => {
-		const details: { [index: string]: string } = {
-			"-2": "Viewing a media",
-			"-1": "Viewing a special page",
-			0: "Reading an article",
-			1: "Viewing a talk page",
-			2: "Viewing a user page",
-			3: "Viewing a user talk page",
-			4: "Viewing a project page",
-			5: "Viewing a project talk page",
-			6: "Viewing a file",
-			7: "Viewing a file talk page",
-			8: "Viewing an interface page",
-			9: "Viewing an interface talk page",
-			10: "Viewing a template",
-			11: "Viewing a template talk page",
-			12: "Viewing a help page",
-			13: "Viewing a help talk page",
-			14: "Viewing a category",
-			15: "Viewing a category talk page",
-			120: "Viewing a propery",
-			121: "Viewing a propery talk page",
-			122: "Viewing a query",
-			123: "Viewing a query talk page",
-			146: "Viewing a lexeme",
-			147: "Viewing a lexeme talk page",
-			640: "Viewing a schema",
-			641: "Viewing a schema talk page",
-			828: "Viewing a module",
-			829: "Viewing a module talk page",
-			2300: "Viewing a gadget",
-			2301: "Viewing a gadget talk page",
-			2302: "Viewing a gadget definition page",
-			2303: "Viewing a gadget definition talk page",
-			2600: "Viewing a topic",
-		};
-		return (
-			details[
-				[...document.querySelector("body").classList]
-					.find(v => /ns--?\d/.test(v))
-					.slice(3)
-			] || "Viewing a page"
-		);
-	};
-
-	if (
-		(
-			document.querySelector<HTMLAnchorElement>("#n-mainpage a") ||
-			document.querySelector<HTMLAnchorElement>("#p-navigation a") ||
-			document.querySelector<HTMLAnchorElement>(".mw-wiki-logo")
-		).href === currentURL.href
-	)
+	if (mwConfig.wgIsMainPage && action === "view")
 		presenceData.details = "On the main page";
 	else if (document.querySelector("#wpLoginAttempt"))
 		presenceData.details = "Logging in";
@@ -131,50 +139,58 @@ const updateCallback = {
 		presenceData.state = (
 			document.querySelector("input[type=search]") as HTMLInputElement
 		).value;
-	} else if (actionResult() === "history") {
+	} else if (action === "history") {
 		presenceData.details = "Viewing revision history";
-		presenceData.state = titleFromURL();
-	} else if (getURLParam("diff")) {
+		presenceData.state = titleFromConfig;
+	} else if (mwConfig.wgDiffOldId) {
 		presenceData.details = "Viewing difference between revisions";
-		presenceData.state = titleFromURL();
-	} else if (getURLParam("oldid")) {
+		presenceData.state = titleFromConfig;
+	} else if (mwConfig.wgCurRevisionId !== mwConfig.wgRevisionId) {
 		presenceData.details = "Viewing an old revision of a page";
-		presenceData.state = titleFromURL();
+		presenceData.state = titleFromConfig;
 	} else if (document.querySelector("#ca-ve-edit") || getURLParam("veaction")) {
-		presenceData.state = `${
-			title.toLowerCase() === titleFromURL().toLowerCase()
-				? `${title}`
-				: `${title} (${titleFromURL()})`
-		}`;
+		presenceData.state =
+			title +
+			(title.toLowerCase() === titleFromConfig.toLowerCase()
+				? ""
+				: ` (${titleFromConfig})`);
 		updateCallback.function = (): void => {
-			if (actionResult() === "edit" || actionResult() === "editsource")
+			if (actionFromURL().startsWith("edit"))
 				presenceData.details = "Editing a page";
 			else presenceData.details = namespaceDetails();
 		};
-	} else if (actionResult() === "edit") {
+	} else if (action === "edit") {
 		presenceData.details = document.querySelector("#ca-edit")
 			? "Editing a page"
 			: "Viewing source";
-		presenceData.state = titleFromURL();
+		presenceData.state = titleFromConfig;
 	} else {
 		presenceData.details = namespaceDetails();
-		presenceData.state = `${
-			title.toLowerCase() === titleFromURL().toLowerCase()
-				? `${title}`
-				: `${title} (${titleFromURL()})`
-		}`;
+		presenceData.state =
+			title +
+			(title.toLowerCase() === titleFromConfig.toLowerCase()
+				? ""
+				: ` (${titleFromConfig})`);
 	}
-})();
 
-if (updateCallback.present) {
+	presenceData.buttons = [
+		{
+			label: "View Page",
+			url: window.location.href,
+		},
+	];
+};
+
+(async (): Promise<void> => {
+	await prepare();
+
 	const defaultData = { ...presenceData };
 	presence.on("UpdateData", async () => {
 		resetData(defaultData);
 		updateCallback.function();
+		if (!(await presence.getSetting("time")))
+			delete presenceData.startTimestamp;
+		if (!(await presence.getSetting("buttons"))) delete presenceData.buttons;
 		presence.setActivity(presenceData);
 	});
-} else {
-	presence.on("UpdateData", async () => {
-		presence.setActivity(presenceData);
-	});
-}
+})();
