bbee169deb3c74b0fb2175e08b85a4aaee76044d
Josh <65370077+unicornbetrayal@users.noreply.github.com>
2022-08-01 14:08:50 -0700

    Fixed line

--- websites/T/Tanks.gg/presence.ts
+++ websites/T/Tanks.gg/presence.ts
@@ -37,7 +37,7 @@ presence.on("UpdateData", async () => {
 		];
 	} else if (document.location.pathname.startsWith("/techtree/")) {
 		presenceData.details = "Viewing the tech tree";
-		presenceData.state = `${document.querySelector(".current").innerHTML}`;
+		presenceData.state = document.querySelector(".current").textContent;
 		presenceData.buttons = [
 			{
 				label: "View Tech Tree",
