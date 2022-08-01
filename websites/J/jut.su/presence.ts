262e19e62732d104d73a4b7eb647b8560c9ef368
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-08-01 11:53:29 +0200

    fix(Jit.su): add rest of the site

--- websites/J/jut.su/presence.ts
+++ websites/J/jut.su/presence.ts
@@ -1,103 +1,50 @@
 const presence = new Presence({
 		clientId: "1003589140241186918",
 	}),
-	strings = presence.getStrings({
-		play: "general.playing",
-		pause: "general.paused",
-	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/f3zxOuL.png",
-		startTimestamp: browsingTimestamp,
-	};
-	switch (document.location.pathname) {
-		case "/": {
-			presenceData.details = "Смотрит домашнюю страницу";
-			break;
+			largeImageKey: "https://i.imgur.com/f3zxOuL.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname } = document.location,
+		title =
+			document.querySelector('[class="b-b-title center"]') ??
+			document.querySelector('[itemprop="name"]'),
+		search = document.querySelector<HTMLInputElement>('[name="ystext"]'),
+		mangaTitle = document
+			.querySelector("#the_manga_title")
+			?.textContent.split(":"),
+		video = document.querySelector<HTMLVideoElement>(
+			'[id="my-player_html5_api"]'
+		),
+		name = document.querySelector("#dle-content > div > h1 > span");
+	if (search?.value) {
+		presenceData.details = "идет поиск";
+		presenceData.state = search.value;
+	} else if (pathname === "/")
+		presenceData.details = "Смотрит домашнюю страницу";
+	else if (mangaTitle) {
+		presenceData.details = mangaTitle[0];
+		presenceData.state = mangaTitle[1];
+	} else if (video) {
+		delete presenceData.startTimestamp;
+
+		if (!video.paused && !isNaN(Number(video.duration))) {
+			presenceData.endTimestamp = presence.getTimestampsfromMedia(video)[1];
+			presenceData.smallImageKey = "play";
+			presenceData.smallImageText = "Игра";
+		} else {
+			delete presenceData.endTimestamp;
+			presenceData.smallImageKey = "pause";
+			presenceData.smallImageText = "Приостановлено";
 		}
-		case "/anime": {
-			presenceData.details = "Выбирает аниме";
-			break;
-		}
-		case "/manga": {
-			presenceData.details = "Выбирает мангу";
-			break;
-		}
-		default: {
-			const mangaTitle = document.querySelector("#the_manga_title");
+		const titles = name.textContent.replace(/[0-9]* сезон [0-9]* серия/g, "");
+		presenceData.details = titles;
+		presenceData.state = name.textContent.replace(titles, "");
+	} else if (title) presenceData.details = `Просмотр ${title.textContent}`;
 
-			if (mangaTitle) {
-				const nameArray = mangaTitle.childNodes[0].nodeValue
-					.trim()
-					.substring(0, mangaTitle.childNodes[0].nodeValue.trim().length - 1)
-					.split(" ");
-				presenceData.state = `${
-					nameArray[nameArray.indexOf("глава") - 1]
-				} глава`;
-				presenceData.details = nameArray
-					.splice(nameArray.indexOf("манги") + 1)
-					.join(" ");
-			} else {
-				const video = document.querySelector<HTMLVideoElement>(
-					"#my-player_html5_api"
-				);
-
-				if (video) {
-					const timestamps = presence.getTimestampsfromMedia(video);
-
-					if (
-						video.currentTime > 0 &&
-						!video.paused &&
-						!video.ended &&
-						video.readyState > 2
-					) {
-						presenceData.endTimestamp = Number.isNaN(timestamps[1])
-							? null
-							: timestamps[1];
-						presenceData.smallImageKey = "play";
-						presenceData.smallImageText = (await strings).play;
-					} else {
-						delete presenceData.endTimestamp;
-						presenceData.smallImageKey = "pause";
-						presenceData.smallImageText = (await strings).pause;
-					}
-				}
-
-				const name = document.querySelector('[itemprop="name"]');
-
-				if (name) {
-					const nameArray = name.textContent.trim().split(" ");
-					nameArray.shift();
-
-					const episode = nameArray[nameArray.indexOf("серия") - 1],
-						season = nameArray[nameArray.indexOf("сезон") - 1];
-
-					nameArray.splice(nameArray.indexOf("сезон") - 1, season ? 4 : 2);
-					presenceData.details = nameArray.join(" ");
-
-					if (episode) {
-						presenceData.state = `${
-							season ? `${season} сезон ` : ""
-						}${episode} серия`;
-					}
-				}
-
-				const namePreview = document.querySelector(
-					".header_video.allanimevideo.anime_padding_for_title"
-				);
-
-				if (namePreview) {
-					const namePreviewArray = namePreview.textContent.trim().split(" ");
-					namePreviewArray.shift();
-
-					namePreviewArray.splice(namePreviewArray.indexOf("все"));
-					presenceData.details = namePreviewArray.join(" ");
-				}
-			}
-		}
-	}
-
-	presence.setActivity(presenceData);
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
 });
