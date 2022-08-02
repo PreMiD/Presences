ba2fbc1e58c0794c15f38c0673a1a23c6b5e820a
Rhys <rhyss.rakoff@gmail.com>
2022-08-02 22:35:20 +0100

    chore: apply suggestions

--- websites/D/Doordash/presence.ts
+++ websites/D/Doordash/presence.ts
@@ -4,37 +4,41 @@ const presence = new Presence({
 	browsingTimestamp = Date.now();
 
 presence.on("UpdateData", async () => {
-	const { pathname: path } = document.location;
+	const { pathname } = document.location,
+		presenceData: PresenceData = {
+			largeImageKey: "logo",
+			startTimestamp: browsingTimestamp,
+		},
+		[hideSearch, hideFood] = await Promise.all([
+			presence.getSetting<boolean>("hideSearch"),
+			presence.getSetting<boolean>("hideFood"),
+		]);
 
-	const presenceData: PresenceData = {
-		largeImageKey: "logo",
-		startTimestamp: browsingTimestamp,
-	};
-
-	const hideSearch = await presence.getSetting<boolean>("hideSearch");
-	const hideFood = await presence.getSetting<boolean>("hideFood");
-
-	if (path.includes("/store/") && !path.includes("/search/")) {
+	if (pathname.includes("/store/") && !pathname.includes("/search/")) {
 		const name = document.querySelector<HTMLSpanElement>(
 				"h1.styles__TextElement-sc-1jrzcv7-0"
 			).textContent,
-			image = document.querySelector<HTMLImageElement>(
-				".menu-header-styles__LogoInner-sc-1kgi6s8-6 > picture:nth-child(1) > img:nth-child(5)"
-			).src;
+			image = document
+				.querySelector<HTMLImageElement>(
+					".menu-header-styles__LogoInner-sc-1kgi6s8-6 > picture:nth-child(1) > img:nth-child(5)"
+				)
+				.getAttribute("src");
 
 		if (
-			!document.querySelector<HTMLImageElement>(
-				".styles__ModalContainer-sc-1r4qbfh-0"
-			)
+			!document
+				.querySelector<HTMLImageElement>(".styles__ModalContainer-sc-1r4qbfh-0")
+				.getAttribute("src")
 		) {
 			presenceData.largeImageKey = image;
 			presenceData.smallImageKey = "logo";
 			presenceData.details = `Viewing the menu of ${name}`;
 		} else {
 			if (!hideFood) {
-				const menuItemImage = document.querySelector<HTMLImageElement>(
-					".BaseItemModalStyles__PictureFrame-sc-mpq8va-1 > picture:nth-child(1) > img:nth-child(5)"
-				).src;
+				const menuItemImage = document
+					.querySelector<HTMLImageElement>(
+						".BaseItemModalStyles__PictureFrame-sc-mpq8va-1 > picture:nth-child(1) > img:nth-child(5)"
+					)
+					.getAttribute("src");
 				presenceData.state = `Looking at ${
 					document.querySelector<HTMLSpanElement>(".bbOfWk").textContent
 				}`;
@@ -44,30 +48,27 @@ presence.on("UpdateData", async () => {
 			presenceData.details = `Viewing the menu of ${name}`;
 			presenceData.smallImageKey = image;
 		}
-	} else if (path.includes("/search/store/")) {
-		if (!hideSearch) {
-			const search =
-				document.querySelector<HTMLInputElement>("#FieldWrapper-0");
-			presenceData.details = `Searching for ${
-				search ? search.value : "something"
-			}`;
-		}
+	} else if (pathname.includes("/search/store/") && !hideSearch) {
+		const search = document.querySelector<HTMLInputElement>("#FieldWrapper-0");
+		presenceData.details = `Searching for ${
+			search ? search.value : "something"
+		}`;
 	}
 
-	switch (path) {
+	switch (pathname) {
 		case "/": {
 			presenceData.details = "Browsing Doordash";
 			let activeSpan: HTMLSpanElement;
 
 			const spanArray = Array.from(document.querySelectorAll(".sc-hFLmAl"));
 
-			for (const span of spanArray)
+			for (const span of spanArray) {
 				if (span.attributes[1].value === "#EB1700")
 					activeSpan = span as HTMLSpanElement;
-
-			if (activeSpan) {
-				presenceData.state = `Looking for ${activeSpan.textContent}`;
 			}
+
+			if (activeSpan)
+				presenceData.state = `Looking for ${activeSpan.textContent}`;
 
 			break;
 		}
