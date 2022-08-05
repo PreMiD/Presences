cdce4b23491c285c1cd4a52fab444897d49ad041
xKubsoneQ <kubson462kontakt@gmail.com>
2022-08-05 21:11:16 +0200

    fix

--- websites/M/Musixmatch/presence.ts
+++ websites/M/Musixmatch/presence.ts
@@ -8,62 +8,100 @@ presence.on("UpdateData", () => {
 			largeImageKey: "https://i.imgur.com/Yb4CWnm.png",
 			startTimestamp: browsingTimestamp,
 		},
-	{pathname: path, hostname: domain} = document.location;
+		{ pathname: path, hostname: domain } = document.location;
 
-	if(domain === "musixmatch.com" || domain === "www.musixmatch.com") {
-		if(path === "/") 
-			presenceData.details = "Viewing the home page";
-		 else if(path.includes("/explore")) 
+	if (domain === "musixmatch.com" || domain === "www.musixmatch.com") {
+		if (path === "/") presenceData.details = "Viewing the home page";
+		else if (path.includes("/explore"))
 			presenceData.details = "Exploring lyrics";
-		 else if(path.includes("/community")) 
+		else if (path.includes("/community"))
 			presenceData.details = "Viewing the community page";
-		 else if(path.includes("/contribute")) 
+		else if (path.includes("/contribute"))
 			presenceData.details = "Viewing the contribute page";
-		 else if(path.includes("/profile/me")) {
+		else if (path.includes("/profile/me")) {
 			//if user status = curator show curator badge
-			if(document.querySelector("#site > div > div > div > main > div > div.mxm-user-header.mxm-user-header--profile > div > div:nth-child(1) > div:nth-child(2) > div > div > span")) {
+			if (
+				document.querySelector(
+					"#site > div > div > div > main > div > div.mxm-user-header.mxm-user-header--profile > div > div:nth-child(1) > div:nth-child(2) > div > div > span"
+				)
+			) {
 				presenceData.smallImageKey = "https://i.imgur.com/Bl7BSIW.png";
 				presenceData.smallImageText = "Curator";
 			}
 			presenceData.details = "Viewing their profile";
-		 } else if(path.includes("/profile/")) {
+		} else if (path.includes("/profile/")) {
 			//if user status = curator show curator badge
-			if(document.querySelector("#site > div > div > div > main > div > div.mxm-user-header.mxm-user-header--profile > div > div:nth-child(1) > div:nth-child(2) > div > div > span")) {
+			if (
+				document.querySelector(
+					"#site > div > div > div > main > div > div.mxm-user-header.mxm-user-header--profile > div > div:nth-child(1) > div:nth-child(2) > div > div > span"
+				)
+			) {
 				presenceData.smallImageKey = "https://i.imgur.com/Bl7BSIW.png";
 				presenceData.smallImageText = "Curator";
 			}
-			presenceData.details = `Viewing ${document.querySelector("#site > div > div > div > main > div > div.mxm-user-header.mxm-user-header--profile > div > div:nth-child(1) > div:nth-child(2) > div > div > h1").textContent}'s profile`;
-		 } else if(path.includes("/search")) {
+			presenceData.details = `Viewing ${
+				document.querySelector(
+					"#site > div > div > div > main > div > div.mxm-user-header.mxm-user-header--profile > div > div:nth-child(1) > div:nth-child(2) > div > div > h1"
+				).textContent
+			}'s profile`;
+		} else if (path.includes("/search")) {
 			presenceData.details = "Searching";
 			presenceData.state = path.split("/")[2].replaceAll("%20", " ");
-		 } else if (path.includes("/lyrics") && path.includes("/translation/")) {
-				presenceData.largeImageKey = document.querySelector<HTMLImageElement>("#site > div > div > div > main > div > div > div.mxm-track-banner.top > div > div > div > div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div > img").src;
-				presenceData.smallImageKey = "https://i.imgur.com/Yb4CWnm.png";
-				presenceData.details = `Reading ${path.split("/")[5]} translation`;
-				presenceData.state = `${document.querySelector("#site h2 > span > a")?.textContent || document.querySelector("#site h2 > span > span > a").textContent} - ${document.querySelector("#site h1").childNodes[1].textContent}`;
+		} else if (path.includes("/lyrics") && path.includes("/translation/")) {
+			presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
+				"#site > div > div > div > main > div > div > div.mxm-track-banner.top > div > div > div > div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div > img"
+			).src;
+			presenceData.smallImageKey = "https://i.imgur.com/Yb4CWnm.png";
+			presenceData.details = `Reading ${path.split("/")[5]} translation`;
+			presenceData.state = `${
+				document.querySelector("#site h2 > span > a")?.textContent ||
+				document.querySelector("#site h2 > span > span > a").textContent
+			} - ${document.querySelector("#site h1").childNodes[1].textContent}`;
 		} else if (path.includes("/lyrics")) {
-				presenceData.largeImageKey = document.querySelector<HTMLImageElement>("#site > div > div > div > main > div > div > div.mxm-track-banner.top > div > div > div > div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div > img").src;
-				presenceData.smallImageKey = "https://i.imgur.com/Yb4CWnm.png";
-				presenceData.details = "Reading lyrics";
-				presenceData.state = `${document.querySelector("#site h2 > span > a")?.textContent || document.querySelector("#site h2 > span > span > a").textContent} - ${document.querySelector("#site h1").childNodes[1].textContent}`;
-		 } else if(path.includes("/artist")) {
-			const avatar = document.querySelector<HTMLImageElement>("#content > div > div.profile-header.artist-header > div > div.profile-info > div > div.profile-image > img").src;
-			if(!avatar.includes("avatar-placeholder.png")) {
+			presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
+				"#site > div > div > div > main > div > div > div.mxm-track-banner.top > div > div > div > div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div > img"
+			).src;
+			presenceData.smallImageKey = "https://i.imgur.com/Yb4CWnm.png";
+			presenceData.details = "Reading lyrics";
+			presenceData.state = `${
+				document.querySelector("#site h2 > span > a")?.textContent ||
+				document.querySelector("#site h2 > span > span > a").textContent
+			} - ${document.querySelector("#site h1").childNodes[1].textContent}`;
+		} else if (path.includes("/artist")) {
+			const avatar = document.querySelector<HTMLImageElement>(
+				"#content > div > div.profile-header.artist-header > div > div.profile-info > div > div.profile-image > img"
+			).src;
+			if (!avatar.includes("avatar-placeholder.png")) {
 				presenceData.largeImageKey = avatar;
 				presenceData.smallImageKey = "https://i.imgur.com/Yb4CWnm.png";
 			}
 			presenceData.details = "Browsing artist";
-			presenceData.state = document.querySelector("#content > div > div.profile-header.artist-header > div > div.profile-info > div > div.profile-data > h1").textContent;
-		 }
-	} else if(domain === "curators.musixmatch.com") {
-		if(path === "/" || path === "/missions") presenceData.details = "Browsing curator's missions";
-		else if(path.includes("/tasks/")) 
-			presenceData.details = `Browsing ${document.querySelector("#__next > div > div > div > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-13awgt0.r-12vffkv > div > div > div > div > div > div.css-1dbjc4n.r-1mdbw0j.r-1guathk > div > div.css-1dbjc4n.r-obd0qt.r-18u37iz.r-1wtj0ep.r-1wzrnnt > div.css-901oao.r-jwli3a.r-1grxjyw.r-uho16t.r-146t82q.r-tskmnb.r-fdjqy7").textContent} tasks`;
-		 else if(path.includes("/tool")) {
+			presenceData.state = document.querySelector(
+				"#content > div > div.profile-header.artist-header > div > div.profile-info > div > div.profile-data > h1"
+			).textContent;
+		}
+	} else if (domain === "curators.musixmatch.com") {
+		if (path === "/" || path === "/missions")
+			presenceData.details = "Browsing curator's missions";
+		else if (path.includes("/tasks/")) {
+			presenceData.details = `Browsing ${
+				document.querySelector(
+					"#__next > div > div > div > div > div > div > div:nth-child(2) > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-13awgt0.r-12vffkv > div > div > div > div > div > div.css-1dbjc4n.r-1mdbw0j.r-1guathk > div > div.css-1dbjc4n.r-obd0qt.r-18u37iz.r-1wtj0ep.r-1wzrnnt > div.css-901oao.r-jwli3a.r-1grxjyw.r-uho16t.r-146t82q.r-tskmnb.r-fdjqy7"
+				).textContent
+			} tasks`;
+		} else if (path.includes("/tool")) {
 			presenceData.details = "Editing lyrics";
-			presenceData.state = `${document.querySelector("#__next > div > div > div > div > div > div > div > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-13awgt0.r-12vffkv > div > div > div > div > div > div > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div.css-1dbjc4n.r-13awgt0.r-1jkjb > div.css-901oao.css-bfa6kz.r-a023e6.r-rjixqe.r-fdjqy7").textContent} - ${document.querySelector("#__next > div > div > div > div > div > div > div > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-13awgt0.r-12vffkv > div > div > div > div > div > div > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div.css-1dbjc4n.r-13awgt0.r-1jkjb > div.css-901oao.css-bfa6kz.r-jwli3a.r-1inkyih.r-1kfrs79.r-rjixqe.r-fdjqy7").textContent}`;
+			presenceData.state = `${
+				document.querySelector(
+					"#__next > div > div > div > div > div > div > div > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-13awgt0.r-12vffkv > div > div > div > div > div > div > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div.css-1dbjc4n.r-13awgt0.r-1jkjb > div.css-901oao.css-bfa6kz.r-a023e6.r-rjixqe.r-fdjqy7"
+				).textContent
+			} - ${
+				document.querySelector(
+					"#__next > div > div > div > div > div > div > div > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af > div.css-1dbjc4n.r-13awgt0.r-12vffkv > div > div > div > div > div > div > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div.css-1dbjc4n.r-13awgt0.r-1jkjb > div.css-901oao.css-bfa6kz.r-jwli3a.r-1inkyih.r-1kfrs79.r-rjixqe.r-fdjqy7"
+				).textContent
+			}`;
 		}
 	}
 
 	presence.setActivity(presenceData);
-}); 
+});
