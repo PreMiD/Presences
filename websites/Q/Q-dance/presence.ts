c7da070c9cb2261ee2f3b419b62a0636411a2c13
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-08-06 14:04:09 +0200

    feat(Q-Dance): add buttons & settings & perf

--- websites/Q/Q-dance/presence.ts
+++ websites/Q/Q-dance/presence.ts
@@ -5,26 +5,54 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "logo",
-	};
-	if (document.querySelector("svg.audioplayer-controls__icon--play")) {
+			largeImageKey: "https://i.imgur.com/wkzP4tG.png",
+		},
+		[privacy, buttons, covers] = await Promise.all([
+			presence.getSetting<boolean>("privacy"),
+			presence.getSetting<boolean>("buttons"),
+			presence.getSetting<boolean>("covers"),
+		]),
+		{ href } = document.location;
+	if (privacy) presenceData.details = "Browsing";
+	else if (!document.querySelector("svg.audioplayer-controls__icon--play")) {
 		presenceData.details = document.querySelector(
 			".audioplayer-nowplaying__track"
 		).textContent;
 		presenceData.state = document.querySelector(
 			".audioplayer-nowplaying__artist"
 		).textContent;
-		(presenceData.smallImageKey = "live"),
-			(presenceData.smallImageText = (
-				await presence.getStrings({
-					live: "general.live",
-				})
-			).live);
+		presenceData.smallImageKey = "live";
+		presenceData.smallImageText = (
+			await presence.getStrings({
+				live: "general.live",
+			})
+		).live;
 		presenceData.largeImageKey =
 			document.querySelector<HTMLImageElement>(
 				"div.audioplayer-nowplaying__image > img"
-			)?.src ?? "logo";
+			)?.src ?? "https://i.imgur.com/wkzP4tG.png";
 		presenceData.startTimestamp = elapsed;
-		presence.setActivity(presenceData);
-	} else presence.setActivity();
+		presenceData.buttons = [
+			{
+				label: "Tune In Live",
+				url: href,
+			},
+		];
+	} else {
+		presenceData.buttons = [
+			{
+				label: "Browse",
+				url: href,
+			},
+		];
+		presenceData.details = `Browsing ${
+			document.querySelector(
+				'[class="nav-item__link router-link-exact-active router-link-active active"]'
+			)?.textContent ?? ""
+		}`;
+	}
+	if (!buttons) delete presenceData.buttons;
+	if (!covers) presenceData.largeImageKey = "https://i.imgur.com/wkzP4tG.png";
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
 });
