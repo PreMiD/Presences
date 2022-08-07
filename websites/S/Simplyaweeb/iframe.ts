d01b7360d13e6c164c496bb4d3436fe2e61529f7
RisingSunLight <85577959+RisingSunLight42@users.noreply.github.com>
2022-08-08 00:02:27 +0200

    fix(Simplyaweeb): change condition format
    Co-authored-by: Rhys Rakoff <64903135+EncryptedDev@users.noreply.github.com>
    Signed-off-by: RisingSunLight <85577959+RisingSunLight42@users.noreply.github.com>

--- websites/S/Simplyaweeb/iframe.ts
+++ websites/S/Simplyaweeb/iframe.ts
@@ -3,7 +3,7 @@ const iframe = new iFrame();
 iframe.on("UpdateData", async () => {
 	const video: HTMLVideoElement = document.querySelector("video");
 
-	if (video && !isNaN(video.duration)) {
+	if (!isNaN(video?.duration)) {
 		iframe.send({
 			current: video.currentTime,
 			duration: video.duration,
