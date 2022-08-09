74ec16f351f0a87a44e5c4a6c8cc25fe32ee8b89
Nolo001 <56518932+Nolo001-Aha@users.noreply.github.com>
2022-08-09 13:16:38 +0200

    Start using switch instead of if/else if
    Signed-off-by: Nolo001 <56518932+Nolo001-Aha@users.noreply.github.com>

--- websites/Y/Yandex/presence.ts
+++ websites/Y/Yandex/presence.ts
@@ -2,7 +2,7 @@ const presence = new Presence({
 		clientId: "1006328788147245086",
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000),
-	currentPath = window.location.pathname,
+	currentPath = document.location.pathname.split("/"),
 	textQuery = new URLSearchParams(window.location.search).get("text");
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
@@ -10,17 +10,25 @@ presence.on("UpdateData", async () => {
 		startTimestamp: browsingTimestamp,
 	};
 
-	if (currentPath === "/") presenceData.details = "Home Page";
-	else if (currentPath.startsWith("/search") && currentPath !== "/")
-		presenceData.details = `Searching for ${textQuery}`;
-	else if (currentPath.startsWith("/images/search") && currentPath !== "/")
-		presenceData.details = `Looking for Images of ${textQuery}`;
-	else if (currentPath.startsWith("/video/search") && currentPath !== "/")
-		presenceData.details = `Looking for Videos of ${textQuery}`;
-	else if (currentPath.startsWith("/news") && currentPath !== "/")
-		presenceData.details = `Looking for News on ${textQuery}`;
-	else if (currentPath.startsWith("/maps") && currentPath !== "/")
-		presenceData.details = "Using Maps";
-
+	switch (currentPath[1]) {
+		case "/": {
+			presenceData.details = "Home Page";
+			break;
+		}
+		case "search": {
+			presenceData.details = `Searching for ${textQuery}`;
+			break;
+		}
+		case "image": {
+			presenceData.details = `Looking for Images of ${textQuery}`;
+			break;
+		}
+		default:
+			if (currentPath[1] === "video" && currentPath[2] === "search")
+				presenceData.details = `Looking for Videos of ${textQuery}`;
+			else if (currentPath[1] === "news")
+				presenceData.details = `Looking for News on ${textQuery}`;
+			else if (currentPath[1] === "maps") presenceData.details = "Using Maps";
+	}
 	presence.setActivity(presenceData);
 });
