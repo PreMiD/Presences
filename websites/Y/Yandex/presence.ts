162c1eeda8f34195a3d013888f4b0f609678e211
Nolo001 <56518932+Nolo001-Aha@users.noreply.github.com>
2022-08-09 12:20:52 +0200

    Using variables for current location and query, lint update
    Signed-off-by: Nolo001 <56518932+Nolo001-Aha@users.noreply.github.com>

--- websites/Y/Yandex/presence.ts
+++ websites/Y/Yandex/presence.ts
@@ -2,38 +2,24 @@ const presence = new Presence({
 		clientId: "1006328788147245086",
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000),
-	urlParams = new URLSearchParams(window.location.search);
+	currentPath = window.location.pathname,
+	textQuery = new URLSearchParams(window.location.search).get("text");
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey: "main",
 		startTimestamp: browsingTimestamp,
 	};
 
-	if (window.location.pathname === "/") presenceData.details = "Home Page";
-	else if (
-		document.location.pathname.startsWith("/search") &&
-		window.location.pathname !== "/"
-	)
-		presenceData.details = `Searching for ${urlParams.get("text")}`;
-	else if (
-		document.location.pathname.startsWith("/images/search") &&
-		window.location.pathname !== "/"
-	)
-		presenceData.details = `Looking for Images of ${urlParams.get("text")}`;
-	else if (
-		document.location.pathname.startsWith("/video/search") &&
-		window.location.pathname !== "/"
-	)
-		presenceData.details = `Looking for Videos of ${urlParams.get("text")}`;
-	else if (
-		document.location.pathname.startsWith("/news") &&
-		window.location.pathname !== "/"
-	)
-		presenceData.details = `Looking for News on ${urlParams.get("text")}`;
-	else if (
-		document.location.pathname.startsWith("/maps") &&
-		window.location.pathname !== "/"
-	)
+	if (currentPath === "/") presenceData.details = "Home Page";
+	else if (currentPath.startsWith("/search") && currentPath !== "/")
+		presenceData.details = `Searching for ${textQuery}`;
+	else if (currentPath.startsWith("/images/search") && currentPath !== "/")
+		presenceData.details = `Looking for Images of ${textQuery}`;
+	else if (currentPath.startsWith("/video/search") && currentPath !== "/")
+		presenceData.details = `Looking for Videos of ${textQuery}`;
+	else if (currentPath.startsWith("/news") && currentPath !== "/")
+		presenceData.details = `Looking for News on ${textQuery}`;
+	else if (currentPath.startsWith("/maps") && currentPath !== "/")
 		presenceData.details = "Using Maps";
 
 	presence.setActivity(presenceData);
