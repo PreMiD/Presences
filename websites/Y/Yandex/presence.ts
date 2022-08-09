1366ab896695237f80e95f9546de0ada3a1b4a5d
Nolo001 <56518932+Nolo001-Aha@users.noreply.github.com>
2022-08-09 03:25:00 +0200

    Yarn format update
    Signed-off-by: Nolo001 <56518932+Nolo001-Aha@users.noreply.github.com>

--- websites/Y/Yandex/presence.ts
+++ websites/Y/Yandex/presence.ts
@@ -1,29 +1,40 @@
 const presence = new Presence({
-  clientId: "1006328788147245086",
-}),
-browsingTimestamp = Math.floor(Date.now() / 1000),
- 
- urlParams = new URLSearchParams(window.location.search);
+		clientId: "1006328788147245086",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000),
+	urlParams = new URLSearchParams(window.location.search);
 presence.on("UpdateData", async () => {
-const presenceData: PresenceData = {
-    largeImageKey: "main",
-    startTimestamp: browsingTimestamp,
-  };
- 
-if(window.location.pathname === "/") 
-  presenceData.details = "Home Page";
- else if (document.location.pathname.startsWith("/search") && window.location.pathname !== "/") 
+	const presenceData: PresenceData = {
+		largeImageKey: "main",
+		startTimestamp: browsingTimestamp,
+	};
 
-      presenceData.details = `Searching for ${urlParams.get("text")}`;
- else if (document.location.pathname.startsWith("/images/search") && window.location.pathname !== "/") 
-      presenceData.details = `Looking for Images of ${urlParams.get("text")}`;
- else if (document.location.pathname.startsWith("/video/search") && window.location.pathname !== "/") 
-      presenceData.details = `Looking for Videos of ${urlParams.get("text")}`;
- else if (document.location.pathname.startsWith("/news") && window.location.pathname !== "/") 
-      presenceData.details = `Looking for News on ${urlParams.get("text")}`;
- else if (document.location.pathname.startsWith("/maps") && window.location.pathname !== "/") 
-      presenceData.details = `Using Maps to find ${urlParams.get("text")}`;
+	if (window.location.pathname === "/") presenceData.details = "Home Page";
+	else if (
+		document.location.pathname.startsWith("/search") &&
+		window.location.pathname !== "/"
+	)
+		presenceData.details = `Searching for ${urlParams.get("text")}`;
+	else if (
+		document.location.pathname.startsWith("/images/search") &&
+		window.location.pathname !== "/"
+	)
+		presenceData.details = `Looking for Images of ${urlParams.get("text")}`;
+	else if (
+		document.location.pathname.startsWith("/video/search") &&
+		window.location.pathname !== "/"
+	)
+		presenceData.details = `Looking for Videos of ${urlParams.get("text")}`;
+	else if (
+		document.location.pathname.startsWith("/news") &&
+		window.location.pathname !== "/"
+	)
+		presenceData.details = `Looking for News on ${urlParams.get("text")}`;
+	else if (
+		document.location.pathname.startsWith("/maps") &&
+		window.location.pathname !== "/"
+	)
+		presenceData.details = "Using Maps";
 
-
-presence.setActivity(presenceData);
+	presence.setActivity(presenceData);
 });
