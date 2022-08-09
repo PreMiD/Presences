6234dc7f25b99ae8aa3cb41640fdc0b17c84e7d0
Nolo001 <56518932+Nolo001-Aha@users.noreply.github.com>
2022-08-09 02:40:27 +0200

    First upload of Yandex integration

---
+++ websites/Y/Yandex/dist/presence.js
@@ -0,0 +1,30 @@
+const presence = new Presence({
+    clientId: "1006328788147245086",
+}), browsingTimestamp = Math.floor(Date.now() / 1000);
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+presence.on("UpdateData", async () => {
+    const presenceData = {
+        largeImageKey: "main",
+        startTimestamp: browsingTimestamp,
+    };
+    if (window.location.pathname == '/') {
+        presenceData.details = `Home Page`;
+    }
+    else if (document.location.pathname.startsWith("/search") && window.location.pathname != '/') {
+        presenceData.details = `Searching for ${urlParams.get('text')}`;
+    }
+    else if (document.location.pathname.startsWith("/images/search") && window.location.pathname != '/') {
+        presenceData.details = `Looking for Images of ${urlParams.get('text')}`;
+    }
+    else if (document.location.pathname.startsWith("/video/search") && window.location.pathname != '/') {
+        presenceData.details = `Looking for Videos of ${urlParams.get('text')}`;
+    }
+    else if (document.location.pathname.startsWith("/news") && window.location.pathname != '/') {
+        presenceData.details = `Looking for News on ${urlParams.get('text')}`;
+    }
+    else if (document.location.pathname.startsWith("/maps") && window.location.pathname != '/') {
+        presenceData.details = `Using Maps to find ${urlParams.get('text')}`;
+    }
+    presence.setActivity(presenceData);
+});
