eb012fa107b9d2ee7fef20649e0ae364ae203180
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2022-08-12 16:32:05 +0200

    feat(NationStates): if–else replaced with a switch

--- websites/N/NationStates/presence.ts
+++ websites/N/NationStates/presence.ts
@@ -136,6 +136,7 @@ async function updatePresenceData(): Promise<void> {
 		const nationname: string = await fetchSelfNationName();
 		if (nationname && nationname.length > 0) presenceData.details = nationname;
 		else delete presenceData.details;
+
 		// Set presence button, if enabled
 		if (nationname && (await presence.getSetting("buttons"))) {
 			presenceData.buttons = [
@@ -147,156 +148,272 @@ async function updatePresenceData(): Promise<void> {
 				},
 			];
 		} else delete presenceData.buttons;
-		// Set presence state to the current page
-		const page = window.location.pathname.toLowerCase();
-		if (page.startsWith("/page=dilemmas")) {
-			// Browsing issues
-			presenceData.state = "Addressing Issues";
-			presenceData.smallImageKey = "https://i.imgur.com/8pTlzJw.png";
-			presenceData.smallImageText = "Issues";
-		} else if (page.startsWith("/page=show_dilemma")) {
-			// Issue (viewing)
-			const issuename = document
-				.querySelector("#content")
-				?.querySelector(".dpaper4")?.textContent;
-			if (issuename) {
-				presenceData.state = `Issue: ${
-					issuename.length > 128 - 7
-						? `${issuename.substring(0, 128 - 7)}…'`
-						: issuename
-				}`;
-			} else presenceData.state = "Addressing Issues";
-			presenceData.smallImageKey = "https://i.imgur.com/8pTlzJw.png";
-			presenceData.smallImageText = "Issues";
-		} else if (page.startsWith("/page=enact_dilemma")) {
-			// Issue (resolved)
-			const issuename = document
-				.querySelector("#dlegislationtext")
-				?.querySelector("p.dtitle")?.textContent;
-			if (issuename) {
-				presenceData.state = `Issue: ${
-					issuename.length > 128 - 7
-						? `${issuename.substring(0, 128 - 7)}…'`
-						: issuename
-				}`;
-			} else presenceData.state = "Addressing Issues";
-			presenceData.smallImageKey = "https://i.imgur.com/8pTlzJw.png";
-			presenceData.smallImageText = "Issues";
-		} else if (
-			page.startsWith("/page=telegram") ||
-			page.startsWith("/page=tg") ||
-			page.startsWith("/page=compose_telegram") ||
-			page.startsWith("/page=notice") ||
-			page.startsWith("/page=subscriptions")
-		) {
-			// Telegrams
-			presenceData.state = "Reading Telegrams";
-			presenceData.smallImageKey = "https://i.imgur.com/6Hp3f7k.png";
-			presenceData.smallImageText = "Telegrams";
-		} else if (
-			page.startsWith("/page=settings") ||
-			page.startsWith("/page=banners") ||
-			page.startsWith("/page=upload_flag")
-		) {
-			// Settings
-			presenceData.state = "Tweaking the Nation";
-			delete presenceData.smallImageKey;
-		} else if (page.startsWith("/page=create_nation")) {
-			// Nation creation
-			presenceData.state = "Declaring a New Nation";
-			presenceData.smallImageKey = "https://i.imgur.com/BAzbs0d.png";
-			presenceData.smallImageText = "My Nation";
-		} else if (
-			page.startsWith("/page=ga") ||
-			page.startsWith("/page=un_proposal/council=1") ||
-			page.startsWith("/page=wa_past_resolutions/council=1")
-		) {
-			// General Assembly
-			presenceData.state = "Attending the General Assembly";
-			presenceData.smallImageKey = "https://i.imgur.com/A09MAL2.png";
-			presenceData.smallImageText = "World Assembly";
-		} else if (
-			page.startsWith("/page=sa") ||
-			page.startsWith("/page=un_proposal/council=2") ||
-			page.startsWith("/page=wa_past_resolutions/council=2")
-		) {
-			// Security Council
-			presenceData.state = "Attending the Security Council";
-			presenceData.smallImageKey = "https://i.imgur.com/A09MAL2.png";
-			presenceData.smallImageText = "World Assembly";
-		} else if (
-			page.startsWith("/page=wa") ||
-			page.startsWith("/page=un") ||
-			page.startsWith("/page=list_un")
-		) {
-			// World Assembly
-			presenceData.state = "Attending the World Assembly";
-			presenceData.smallImageKey = "https://i.imgur.com/A09MAL2.png";
-			presenceData.smallImageText = "World Assembly";
-		} else if (page.startsWith("/nation=")) {
-			// Nation page
 
-			const nationid: string = page.match(/(?<=\/nation=)\w+/gi)[0];
-			if (nationid === document.body.getAttribute("data-nname")) {
-				// Self nation page
-				presenceData.state = "Viewing Nation";
-				presenceData.smallImageKey = "https://i.imgur.com/BAzbs0d.png";
-				presenceData.smallImageText = "My Nation";
-			} else {
-				// Other nation page
-				presenceData.state = `Viewing Nation: ${await fetchNationName(
-					nationid
-				)}`;
-				presenceData.smallImageKey = "https://i.imgur.com/hE7TN9d.png";
-				presenceData.smallImageText = "World";
-			}
-		} else if (page.startsWith("/region=")) {
-			// Region page
-			presenceData.state = `Viewing Region: ${await fetchRegionName(
-				page.match(/(?<=\/region=)\w+/gi)[0]
-			)}`;
-			presenceData.smallImageKey = "https://i.imgur.com/hE7TN9d.png";
-			presenceData.smallImageText = "World";
-		} else if (page.startsWith("/page=dispatches")) {
-			// Dispatches
-			presenceData.state = "Browsing Dispatches";
-			presenceData.smallImageKey = "https://i.imgur.com/pArUzoy.png";
-			presenceData.smallImageText = "Dispatches";
-		} else if (page.startsWith("/page=news")) {
-			// News
-			presenceData.state = "Reading News";
-			presenceData.smallImageKey = "https://i.imgur.com/pArUzoy.png";
-			presenceData.smallImageText = "News";
-		} else if (page.startsWith("/page=dispatch")) {
-			// Reading a dispatch
-			presenceData.smallImageKey = "https://i.imgur.com/pArUzoy.png";
-			presenceData.smallImageText = "Dispatches";
-			let title = document
-				.querySelector("#content")
-				?.querySelector(".dispatch")
-				?.querySelector("h2")?.textContent;
-			if (title && title.length > 1) {
-				if (title.length > 128 - 18) title = `${title.substring(0, 128 - 19)}…`;
-				presenceData.state = `Reading Dispatch: ${title}`;
-			} else presenceData.state = "Reading a Dispatch";
-		} else if (page.startsWith("/page=create_dispatch")) {
-			// Writing a dispatch
-			presenceData.state = "Writing Dispatch";
-			presenceData.smallImageKey = "https://i.imgur.com/pArUzoy.png";
-			presenceData.smallImageText = "Dispatches";
-		} else if (
-			page.startsWith("/page=store") ||
-			page.startsWith("/page=cart") ||
-			page.startsWith("/page=orders")
+		// Determine the current page
+		const path = window.location.pathname.toLowerCase(),
+			page = path
+				.substring(path.indexOf("=") + 1)
+				.replace(/\/.+/iu, "")
+				.replace(/\?.+/iu, "")
+				.toLowerCase();
+
+		// Set presence state according to the current page (if applicable)
+		switch (
+			path.substring(path.indexOf("/") + 1, path.indexOf("=")).toLowerCase()
 		) {
+			// Nation page
+			case "nation": {
+				if (page === document.body.getAttribute("data-nname")) {
+					// Visiting own nation profile
+					presenceData.state = "Viewing Nation";
+					presenceData.smallImageKey = images.flag;
+					presenceData.smallImageText = "My Nation";
+				} else {
+					// Other nation profile
+					const nationname: string = await fetchNationName(page);
+					presenceData.state = nationname
+						? `Viewing Nation: ${nationname}`
+						: "Viewing a Nation";
+					presenceData.smallImageKey = images.globe;
+					presenceData.smallImageText = "World";
+				}
+				return;
+			}
+
+			// Region page
+			case "region": {
+				const regionname = await fetchRegionName(page);
+				presenceData.state = regionname
+					? `Viewing Region: ${regionname}`
+					: "Viewing a Region";
+				presenceData.smallImageKey = images.globe;
+				presenceData.smallImageText = "World";
+				return;
+			}
+
+			// Regular page
+			case "page":
+				// Continue to subpage check
+				break;
+
+			// Index page
+			default:
+				presenceData.state = "Browsing";
+				delete presenceData.smallImageKey;
+				return;
+		}
+
+		// Set presence state according to the current subpage
+		switch (page) {
+			// Nation creation
+			case "create_nation":
+				presenceData.state = "Declaring a New Nation";
+				presenceData.smallImageKey = images.flag;
+				presenceData.smallImageText = "My Nation";
+				break;
+
+			// Region activities
+			case "display_region_rmb":
+			case "region_control":
+			case "region_history":
+			case "region_rank":
+				presenceData.state = "Attending Regional Activities";
+				presenceData.smallImageKey = images.flag;
+				presenceData.smallImageText = "Region";
+				break;
+
+			// Telegrams
+			case "telegram":
+			case "telegrams":
+			case "tg":
+			case "notices":
+			case "notice":
+				presenceData.state = "Reading Telegrams";
+				presenceData.smallImageKey = images.envelope;
+				presenceData.smallImageText = "Telegrams";
+				break;
+
+			// Writing a telegram
+			case "write_telegram":
+			case "compose_telegram":
+				presenceData.state = "Writing a Telegram";
+				presenceData.smallImageKey = images.envelope;
+				presenceData.smallImageText = "Telegrams";
+				break;
+
+			// Browsing nations and regions
+			case "world":
+			case "dossier":
+			case "change_region":
+			case "list_entities":
+			case "list_nations":
+			case "list_regions":
+			case "tag_search":
+			case "activity":
+				presenceData.state = "Observing the World";
+				presenceData.smallImageKey = images.globe;
+				presenceData.smallImageText = "World";
+				break;
+
+			// Cards
+			case "deck":
+				presenceData.state = "Playing Cards";
+				delete presenceData.smallImageKey;
+				break;
+
 			// Store
-			presenceData.state = "Browsing the Store";
-			presenceData.smallImageKey = "https://i.imgur.com/28eyl2A.png";
-			presenceData.smallImageText = "Store";
-		} else {
-			// Other
-			presenceData.state = "Browsing";
-			delete presenceData.smallImageKey;
+			case "store":
+			case "cart":
+			case "order":
+				presenceData.state = "Browsing the Store";
+				presenceData.smallImageKey = images.gift;
+				presenceData.smallImageText = "Store";
+				break;
+
+			// Challange
+			case "challenge": {
+				const opponent = [
+					...document
+						.querySelector(".trumps-challenger")
+						.querySelectorAll(".nname"),
+				].find(
+					challanger =>
+						challanger.textContent.toLowerCase().split(" ").join("_") !==
+						document.body.getAttribute("data-nname")
+				);
+				if (opponent && opponent.textContent)
+					presenceData.state = `Challenging ${opponent.textContent}`;
+				else presenceData.state = "Challenging";
+				delete presenceData.smallImageKey;
+				break;
+			}
+
+			// Settings
+			case "settings":
+			case "banners":
+			case "upload_flag":
+			case "tgsettings":
+			case "subscriptions":
+				presenceData.state = "Configuring NationStates";
+				delete presenceData.smallImageKey;
+				break;
+
+			// Dispatches - browsing
+			case "dispatches":
+				presenceData.state = "Browsing Dispatches";
+				presenceData.smallImageKey = images.page;
+				presenceData.smallImageText = "Dispatches";
+				break;
+
+			// Dispatches - reading
+			case "dispatch": {
+				const dispatchname = document
+					.querySelector("#content")
+					?.querySelector(".dispatch")
+					?.querySelector("h2")?.textContent;
+				if (dispatchname) {
+					presenceData.state = `Dispatch: ${
+						dispatchname.length > 128 - 10
+							? `${dispatchname.substring(0, 128 - 11)}…'`
+							: dispatchname
+					}`;
+				} else presenceData.state = "Browsing Dispatches";
+				presenceData.smallImageKey = images.page;
+				presenceData.smallImageText = "Dispatches";
+				break;
+			}
+
+			// Issues - browsing
+			case "issues":
+			case "dilemmas":
+				presenceData.state = "Browsing Issues";
+				presenceData.smallImageKey = images.person;
+				presenceData.smallImageText = "Issues";
+				break;
+
+			// Issues - resolving
+			case "show_dilemma": {
+				const issuename = document
+					.querySelector("#content")
+					?.querySelector(".dpaper4")?.textContent;
+				if (issuename) {
+					presenceData.state = `Issue: ${
+						issuename.length > 128 - 7
+							? `${issuename.substring(0, 128 - 7)}…'`
+							: issuename
+					}`;
+				} else presenceData.state = "Browsing Issues";
+				presenceData.smallImageKey = images.person;
+				presenceData.smallImageText = "Issues";
+				break;
+			}
+
+			// Issues - enacting
+			case "enact_dilemma": {
+				const issuename = document
+					.querySelector("#dlegislationtext")
+					?.querySelector("p.dtitle")?.textContent;
+				if (issuename) {
+					presenceData.state = `Issue: ${
+						issuename.length > 128 - 7
+							? `${issuename.substring(0, 128 - 7)}…'`
+							: issuename
+					}`;
+				} else presenceData.state = "Browsing Issues";
+				presenceData.smallImageKey = images.person;
+				presenceData.smallImageText = "Issues";
+				break;
+			}
+
+			// World Assembly - browsing
+			case "wa":
+			case "un":
+			case "list_un":
+			case "list_wa":
+			case "un_proposal":
+			case "wa_proposal":
+				presenceData.state = "Attending the World Assembly";
+				presenceData.smallImageKey = images.worldassembly;
+				presenceData.smallImageText = "World Assembly";
+				break;
+
+			// World Assembly - new proposal
+			case "un_repeal":
+			case "wa_repeal":
+			case "un_new_proposal":
+			case "wa_new_proposal":
+				presenceData.state = "Writing a new WA proposal";
+				presenceData.smallImageKey = images.worldassembly;
+				presenceData.smallImageText = "World Assembly";
+				break;
+
+			// World Assembly - resolution
+			case "ga":
+			case "sc":
+			case "wa_past_resolution":
+			case "wa_past_resolutions":
+			case "un_past_resolution":
+			case "un_past_resolutions": {
+				const resolution = document
+					.querySelector(".WA_thing")
+					?.querySelector("h2")?.textContent;
+				if (resolution && document.querySelectorAll(".WA_thing").length === 1) {
+					presenceData.state = `Resolution: ${
+						resolution.length > 128 - 7
+							? `${resolution.substring(0, 128 - 7)}…'`
+							: resolution
+					}`;
+				} else presenceData.state = "Attending the World Assembly";
+				presenceData.smallImageKey = images.worldassembly;
+				presenceData.smallImageText = "World Assembly";
+				break;
+			}
+
+			// Other (main page, hopefully)
+			default:
+				presenceData.state = "Browsing";
+				delete presenceData.smallImageKey;
+				break;
 		}
 	} else if (document.location.hostname === "forum.nationstates.net") {
 		// Forums (forum.nationstates.net)
