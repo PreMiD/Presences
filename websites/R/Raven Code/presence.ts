729f90bc5f9266a63f60863d904ee9072848f056
Gökhan Can <55600314+chimpdev@users.noreply.github.com>
2022-09-06 00:59:39 +0300

    Add files via upload
    - Adding buttons
    - Support for docs subdomain
    - Bumping version

--- websites/R/Raven Code/presence.ts
+++ websites/R/Raven Code/presence.ts
@@ -11,77 +11,85 @@ setInterval(
 );
 
 presence.on("UpdateData", async () => {
+	console.log(document.location)
 	const presenceData: PresenceData = {
 			largeImageKey: "logo",
 			details: "Geziniyor",
+			buttons: [{ label: "Sayfaya git", url: document.location.href }],
 			startTimestamp,
 		},
-		{ pathname, search } = document.location,
+		{ pathname, search, host } = document.location,
 		searchParams = new URLSearchParams(search);
 
-	switch (pathname) {
-		case "/":
-			presenceData.details = "Anasayfayı görüntülüyor";
-			if (searchParams.has("user_id")) {
-				const image = document.querySelector<HTMLImageElement>(
-						"[data-premid-useravatar]"
-					)?.src,
-					userTag = document.querySelector<HTMLElement>(
-						"[data-premid-usertag]"
-					)?.innerText;
-				if (image && userTag) {
-					presenceData.details = "Kullanıcıyı görüntülüyor:";
-					presenceData.state = userTag;
-					presenceData.largeImageKey = image;
+	if (host === "docs.ravencode.live") {
+		presenceData.details = "docs.ravencode.live";
+		presenceData.state = document.title.replaceAll("-", "—");
+	} else if (host === "ravencode.live") {
+		switch (pathname) {
+			case "/":
+				presenceData.details = "Anasayfayı görüntülüyor";
+				if (searchParams.has("user_id")) {
+					const image = document.querySelector<HTMLImageElement>(
+							"[data-premid-useravatar]"
+						)?.src,
+						userTag = document.querySelector<HTMLElement>(
+							"[data-premid-usertag]"
+						)?.innerText;
+					if (image && userTag) {
+						presenceData.details = "Kullanıcıyı görüntülüyor:";
+						presenceData.state = userTag;
+						presenceData.largeImageKey = image;
+					}
+				} else if (searchParams.has("invite_code")) {
+					const image = document.querySelector<HTMLImageElement>(
+							"[data-premid-guildicon]"
+						)?.src,
+						guildName = document.querySelector<HTMLElement>(
+							"[data-premid-guildname]"
+						)?.innerText;
+					if (image && guildName) {
+						presenceData.details = "Daveti görüntülüyor:";
+						presenceData.state = guildName;
+						presenceData.largeImageKey = image;
+					}
 				}
-			} else if (searchParams.has("invite_code")) {
-				const image = document.querySelector<HTMLImageElement>(
-						"[data-premid-guildicon]"
-					)?.src,
-					guildName = document.querySelector<HTMLElement>(
-						"[data-premid-guildname]"
-					)?.innerText;
-				if (image && guildName) {
-					presenceData.details = "Daveti görüntülüyor:";
-					presenceData.state = guildName;
-					presenceData.largeImageKey = image;
-				}
-			}
-			break;
-		case "/share":
-			presenceData.details = "Kod paylaşıyor";
-			break;
+				break;
+			case "/share":
+				presenceData.details = "Kod paylaşıyor";
+				break;
 
-		case "/profile":
-			presenceData.details = "Profil görüntüleniyor:";
-			presenceData.state = document.title.split("— ")[1];
-			presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
-				"[data-premid-avatar]"
-			).src;
-			break;
+			case "/profile":
+				presenceData.details = "Profil görüntüleniyor:";
+				presenceData.state = document.title.split("— ")[1];
 
-		case "/codes":
-			presenceData.details = "Kodlar sayfasını görüntülüyor";
-			if (searchParams.get("search"))
-				presenceData.state = `Arama: ${searchParams.get("search")}`;
-			break;
-	}
+				presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
+					"[data-premid-avatar]"
+				).src;
+				break;
 
-	if (pathname.startsWith("/code/")) {
-		presenceData.details = "Kod görüntüleniyor:";
-		presenceData.state = document.title.split("— ")[1];
-
-		for (const language of languages) {
-			if (pathname.includes(language.page)) {
-				presenceData.smallImageKey = language.key;
-				presenceData.smallImageText = `Kategori — ${language.text}`;
-			}
+			case "/codes":
+				presenceData.details = "Kodlar sayfasını görüntülüyor";
+				if (searchParams.get("search"))
+					presenceData.state = `Arama: ${searchParams.get("search")}`;
+				break;
 		}
 
-		if (
-			document.activeElement === document.querySelector('input[id="comment"]')
-		)
-			presenceData.details = "Yorum yapılıyor:";
+		if (pathname.startsWith("/code/")) {
+			presenceData.details = "Kod görüntüleniyor:";
+			presenceData.state = document.title.split("— ")[1];
+
+			for (const language of languages) {
+				if (pathname.includes(language.page)) {
+					presenceData.smallImageKey = language.key;
+					presenceData.smallImageText = `Kategori — ${language.text}`;
+				}
+			}
+
+			if (
+				document.activeElement === document.querySelector('input[id="comment"]')
+			)
+				presenceData.details = "Yorum yapılıyor:";
+		}
 	}
 
 	presence.setActivity(presenceData);
