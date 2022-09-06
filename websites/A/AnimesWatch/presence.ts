c291326bdb01c862c2eb63e9878af8a9c7b9cab9
NullMan <73245847+iamnullman@users.noreply.github.com>
2022-09-06 05:18:45 +0300

    Update presence.ts
    Signed-off-by: NullMan <73245847+iamnullman@users.noreply.github.com>

--- websites/A/AnimesWatch/presence.ts
+++ websites/A/AnimesWatch/presence.ts
@@ -4,11 +4,10 @@ const presence = new Presence({
 	strings = presence.getStrings({
 		playing: "general.playing",
 		paused: "general.paused",
-		browsing: "general.browsing",
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-let video: HTMLVideoElement;
+var video: HTMLVideoElement;
 
 presence.on("iFrameData", async (msg: HTMLVideoElement) => {
 	if (!msg) return;
@@ -19,10 +18,10 @@ presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			largeImageKey: "animeswatch",
 			details: "Ana Sayfada Göz Gezdiriyor",
-			startTimestamp: browsingTimestamp,
 		},
 		{ pathname } = document.location,
 		title = document.title.slice(0, document.title.length - 14);
+
 	if (pathname === "/") {
 		presenceData.details = "Ana Sayfada";
 		presenceData.state = "Anime Arıyor";
@@ -36,24 +35,35 @@ presence.on("UpdateData", async () => {
 		presenceData.details = title;
 		presenceData.state = "Animenin Detaylarını İnceliyor";
 	} else if (pathname.includes("/users")) {
+		presenceData.details = "Profil Sayfasında";
+		presenceData.state = title + " kullanıcısının profilinde";
+	} else if (pathname.includes("/watch")) {
 		presenceData.details = title;
-		presenceData.state = "Bir kullanıcı profiline bakıyor";
-	}
-	if (video) {
-		presenceData.smallImageKey = video.paused ? "pause" : "play";
-		presenceData.smallImageText = video.paused
-			? (await strings).paused
-			: (await strings).playing;
-		presenceData.state = "Anime İzliyor";
-		presenceData.details = title;
-		if (!video.paused && video.duration) {
-			[presenceData.startTimestamp, presenceData.endTimestamp] =
-				presence.getTimestamps(
-					Math.floor(video.currentTime),
-					Math.floor(video.duration)
-				);
+		presenceData.state = "Anime izleme sayfasında";
+
+		if (video) {
+			presenceData.smallImageKey = video.paused ? "pause" : "play";
+			presenceData.smallImageText = video.paused
+				? (await strings).paused
+				: (await strings).playing;
+			presenceData.state = "Anime İzliyor";
+			if (!video.paused && video.duration) {
+				[presenceData.startTimestamp, presenceData.endTimestamp] =
+					presence.getTimestamps(
+						Math.floor(video.currentTime),
+						Math.floor(video.duration)
+					);
+			}
 		}
 	}
+
+	if (await presence.getSetting("timestamp"))
+		presenceData.startTimestamp = browsingTimestamp;
+	if (await presence.getSetting("button"))
+		presenceData.buttons = [
+			{ label: "Sayfaya Git", url: document.location.href },
+			{ label: "Discord", url: "https://animeswatch.com/discord" },
+		];
 
 	presence.setActivity(presenceData);
 });
