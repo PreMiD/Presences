3d67c6e904c3d2b8d5032806f22ff6574de98ee3
theusaf <theusafyt@gmail.com>
2022-09-08 13:46:31 -0700

    feat(MusicBrainz): add more paths

--- websites/M/MusicBrainz/presence.ts
+++ websites/M/MusicBrainz/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 // TODO:
-// edit history, open edits, creations, etc.
+// edit history, open edits, creations, collections, annotations, etc.
 
 presence.on("UpdateData", () => {
 	const presenceData: PresenceData = {
@@ -59,7 +59,7 @@ presence.on("UpdateData", () => {
 		}
 		case "artist": {
 			const artistName = document.querySelector("bdi").textContent,
-				[, artistPath] = pathname.match(/^\/user\/.*?\/(.*?)\/?$/) || [];
+				[, artistPath] = pathname.match(/^\/artist\/.*?\/(.*?)\/?$/) || [];
 			presenceData.details = "Viewing artist";
 			presenceData.buttons = [
 				{
@@ -96,7 +96,8 @@ presence.on("UpdateData", () => {
 		}
 		case "release": {
 			const releaseName = document.querySelector("bdi").textContent,
-				[, releasePath] = pathname.match(/^\/user\/.*?\/(.*?)(\/.*?)?$/) || [],
+				[, releasePath] =
+					pathname.match(/^\/release\/.*?\/(.*?)(\/.*?)?$/) || [],
 				coverArtImage = document.querySelector<HTMLImageElement>(
 					".cover-art-image > img"
 				);
@@ -153,7 +154,8 @@ presence.on("UpdateData", () => {
 		}
 		case "recording": {
 			const recordingName = document.querySelector("bdi").textContent,
-				[, recordingPath] = pathname.match(/^\/user\/.*?\/(.*?)\/?$/) || [];
+				[, recordingPath] =
+					pathname.match(/^\/recording\/.*?\/(.*?)\/?$/) || [];
 			presenceData.details = "Viewing recording";
 			presenceData.smallImageKey = "https://i.imgur.com/ybpozZ6.png";
 			presenceData.smallImageText = `Artist: '${
@@ -202,6 +204,364 @@ presence.on("UpdateData", () => {
 				presenceData.details = "Viewing URL";
 			}
 			presenceData.state = document.querySelector("bdi").textContent;
+			break;
+		}
+		case "work": {
+			const workName = document.querySelector("bdi").textContent,
+				[, workPath] = pathname.match(/^\/work\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing work";
+			presenceData.buttons = [
+				{
+					label: "View Work",
+					url: href,
+				},
+			];
+			switch (workPath) {
+				case "aliases":
+				case "tags":
+				case "details": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${workName}`;
+					break;
+				}
+				case "edit": {
+					presenceData.details = "Editing work";
+					presenceData.state = workName;
+					delete presenceData.buttons;
+					break;
+				}
+				case "edit_annotation": {
+					presenceData.details = "Editing work annotation";
+					presenceData.state = workName;
+					delete presenceData.buttons;
+					break;
+				}
+				default: {
+					presenceData.state = workName;
+				}
+			}
+			break;
+		}
+		case "event": {
+			const eventName = document.querySelector("bdi").textContent,
+				[, eventPath] = pathname.match(/^\/event\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing event";
+			presenceData.buttons = [
+				{
+					label: "View Event",
+					url: href,
+				},
+			];
+			switch (eventPath) {
+				case "aliases":
+				case "tags":
+				case "ratings":
+				case "details": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${eventName}`;
+					break;
+				}
+				case "edit": {
+					presenceData.details = "Editing event";
+					presenceData.state = eventName;
+					delete presenceData.buttons;
+					break;
+				}
+				case "edit_annotation": {
+					presenceData.details = "Editing event annotation";
+					presenceData.state = eventName;
+					delete presenceData.buttons;
+					break;
+				}
+				default: {
+					presenceData.state = eventName;
+				}
+			}
+			break;
+		}
+		case "label": {
+			const labelName = document.querySelector("bdi").textContent,
+				[, labelPath] = pathname.match(/^\/label\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing label";
+			presenceData.buttons = [
+				{
+					label: "View Label",
+					url: href,
+				},
+			];
+			switch (labelPath) {
+				case "aliases":
+				case "tags":
+				case "relationships":
+				case "ratings":
+				case "details": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${labelName}`;
+					break;
+				}
+				case "edit": {
+					presenceData.details = "Editing label";
+					presenceData.state = labelName;
+					delete presenceData.buttons;
+					break;
+				}
+				case "annotations": {
+					presenceData.details = "Viewing annotation history";
+					presenceData.state = labelName;
+					delete presenceData.buttons;
+					break;
+				}
+				case "edit_annotation": {
+					presenceData.details = "Editing label annotation";
+					presenceData.state = labelName;
+					delete presenceData.buttons;
+					break;
+				}
+				default: {
+					presenceData.state = labelName;
+				}
+			}
+			break;
+		}
+		case "tag": {
+			const tagName =
+					document.querySelector<HTMLAnchorElement>("h1 > a").textContent,
+				[, tagPath] = pathname.match(/^\/tag\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing tag";
+			presenceData.buttons = [
+				{
+					label: "View Tag",
+					url: href,
+				},
+			];
+			switch (tagPath) {
+				case "artist":
+				case "release-group":
+				case "release":
+				case "recording":
+				case "work":
+				case "label":
+				case "place":
+				case "area":
+				case "instrument":
+				case "series":
+				case "event": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${tagName}`;
+					break;
+				}
+				default: {
+					presenceData.state = tagName;
+				}
+			}
+			break;
+		}
+		case "collection": {
+			const collectionName = document.querySelector("bdi").textContent,
+				[, collectionPath] =
+					pathname.match(/^\/collection\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing label";
+			if (
+				!document
+					.querySelector<HTMLParagraphElement>(".subheader")
+					.textContent.includes("Private collection")
+			) {
+				presenceData.buttons = [
+					{
+						label: "View Collection",
+						url: href,
+					},
+				];
+			}
+			switch (collectionPath) {
+				case "edit": {
+					presenceData.details = "Editing collection";
+					presenceData.state = collectionName;
+					delete presenceData.buttons;
+					break;
+				}
+				default: {
+					presenceData.state = collectionName;
+				}
+			}
+			break;
+		}
+		case "instrument": {
+			const instrumentName = document.querySelector("bdi").textContent,
+				[, instrumentPath] =
+					pathname.match(/^\/instrument\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing instrument";
+			presenceData.buttons = [
+				{
+					label: "View Instrument",
+					url: href,
+				},
+			];
+			switch (instrumentPath) {
+				case "aliases":
+				case "artists":
+				case "releases":
+				case "recordings":
+				case "tags":
+				case "details": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${instrumentName}`;
+					break;
+				}
+				default: {
+					presenceData.state = instrumentName;
+				}
+			}
+			break;
+		}
+		case "area": {
+			const areaName = document.querySelector("bdi").textContent,
+				[, areaPath] = pathname.match(/^\/area\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing area";
+			presenceData.buttons = [
+				{
+					label: "View Area",
+					url: href,
+				},
+			];
+			switch (areaPath) {
+				case "aliases":
+				case "events":
+				case "labels":
+				case "users":
+				case "works":
+				case "places":
+				case "artists":
+				case "releases":
+				case "recordings":
+				case "tags":
+				case "details": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${areaName}`;
+					break;
+				}
+				default: {
+					presenceData.state = areaName;
+				}
+			}
+			break;
+		}
+		case "series": {
+			const seriesName = document.querySelector("bdi").textContent,
+				[, seriesPath] = pathname.match(/^\/area\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing series";
+			presenceData.buttons = [
+				{
+					label: "View Series",
+					url: href,
+				},
+			];
+			switch (seriesPath) {
+				case "aliases":
+				case "tags":
+				case "details": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${seriesName}`;
+					break;
+				}
+				case "edit": {
+					presenceData.details = "Editing series";
+					presenceData.state = seriesName;
+					delete presenceData.buttons;
+					break;
+				}
+				case "edit_annotation": {
+					presenceData.details = "Editing series annotation";
+					presenceData.state = seriesName;
+					delete presenceData.buttons;
+					break;
+				}
+				default: {
+					presenceData.state = seriesName;
+				}
+			}
+			break;
+		}
+		case "release-group": {
+			const releaseGroupName = document.querySelector("bdi").textContent,
+				[, releaseGroupPath] =
+					pathname.match(/^\/release-group\/.*?\/(.*?)\/?$/) || [],
+				coverArtImage = document.querySelector<HTMLImageElement>(
+					".cover-art-image > img"
+				);
+			presenceData.details = "Viewing release group";
+			presenceData.buttons = [
+				{
+					label: "View Release Group",
+					url: href,
+				},
+			];
+			if (coverArtImage) {
+				presenceData.largeImageKey = coverArtImage.src;
+			}
+			switch (releaseGroupPath) {
+				case "aliases":
+				case "tags":
+				case "ratings":
+				case "details": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${releaseGroupName}`;
+					break;
+				}
+				case "set-cover-art": {
+					presenceData.details = "Setting release group cover art";
+					presenceData.state = releaseGroupName;
+					delete presenceData.buttons;
+					break;
+				}
+				case "edit": {
+					presenceData.details = "Editing release group";
+					presenceData.state = releaseGroupName;
+					delete presenceData.buttons;
+					break;
+				}
+				case "edit_annotation": {
+					presenceData.details = "Editing release group annotation";
+					presenceData.state = releaseGroupName;
+					delete presenceData.buttons;
+					break;
+				}
+				default: {
+					presenceData.state = releaseGroupName;
+				}
+			}
+			break;
+		}
+		case "genre": {
+			const genreName = document.querySelector("bdi").textContent,
+				[, genrePath] = pathname.match(/^\/genre\/.*?\/(.*?)\/?$/) || [];
+			presenceData.details = "Viewing genre";
+			presenceData.buttons = [
+				{
+					label: "View Genre",
+					url: href,
+				},
+			];
+			switch (genrePath) {
+				case "aliases":
+				case "details": {
+					presenceData.state = `${document.querySelector(
+						".sel bdi"
+					)} for ${genreName}`;
+					break;
+				}
+				default: {
+					presenceData.state = genreName;
+				}
+			}
 			break;
 		}
 		default: {
