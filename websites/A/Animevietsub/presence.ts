4d47770d089a0b930ef333df0f843e611a827434
Minh <54742686+MinhYz@users.noreply.github.com>
2022-09-08 13:54:34 +0700

    Update presence.ts
    Signed-off-by: Minh <54742686+MinhYz@users.noreply.github.com>

--- websites/A/Animevietsub/presence.ts
+++ websites/A/Animevietsub/presence.ts
@@ -1,10 +1,10 @@
 const presence = new Presence({
-	clientId: "1016991973531451502",
-}),
-strings = presence.getStrings({
-	play: "general.playing",
-	pause: "general.paused",
-});
+		clientId: "1016991973531451502",
+	}),
+	strings = presence.getStrings({
+		play: "general.playing",
+		pause: "general.paused",
+	});
 
 let lastPlaybackState: boolean,
 	lastPath: string,
@@ -27,9 +27,17 @@ presence.on("UpdateData", async () => {
 	}
 
 	if (!playback) {
-		if (["/anime-bo/", "/anime-sap-chieu/", "/anime-le/", "/danh-sach/", "/hoat-hinh-trung-quoc/"].some(e => curPath.startsWith(e)))
+		if (
+			[
+				"/anime-bo/",
+				"/anime-sap-chieu/",
+				"/anime-le/",
+				"/danh-sach/",
+				"/hoat-hinh-trung-quoc/",
+			].some(e => curPath.startsWith(e))
+		)
 			presenceData.details = "Đang chọn phim";
-		else if (curPath.startsWith("/account/info/")) 
+		else if (curPath.startsWith("/account/info/"))
 			presenceData.details = "Đang xem profile...";
 		else if (curPath.startsWith("/tim-kiem/"))
 			presenceData.details = "Đang tìm kiếm...";
@@ -37,7 +45,7 @@ presence.on("UpdateData", async () => {
 			presenceData.details = "Đang đăng nhập...";
 		else if (curPath.startsWith("/account/register"))
 			presenceData.details = "Đang đăng ký...";
-		else if (curPath.startsWith("/the-loai/")) 
+		else if (curPath.startsWith("/the-loai/"))
 			presenceData.details = "Đang chọn thể loại phim";
 		else if (curPath.startsWith("/anime/library/"))
 			presenceData.details = "Đang xem thử viện alime ";
@@ -46,18 +54,18 @@ presence.on("UpdateData", async () => {
 		else if (curPath.startsWith("/season/"))
 			presenceData.details = "Đang chọn mùa phim";
 		else if (curPath.startsWith("/phim/")) {
-			const phim = document.querySelector<HTMLAnchorElement>(".Title").textContent
-			presenceData.details = `Định xem phim ${phim}`;
-		}
-		else if (curPath.startsWith("/quen-mat-khau.html"))
+			presenceData.details = `Định xem phim ${
+				document.querySelector<HTMLAnchorElement>(".Title").textContent
+			}`;
+		} else if (curPath.startsWith("/quen-mat-khau.html"))
 			presenceData.details = "Đang bị quên mật khẩu kek.";
 		else if (curPath.startsWith("/lich-chieu-phim.html"))
 			presenceData.details = "Đang xem lịch chiếu anime";
 		else presenceData.details = "Đang xem trang chủ...";
-			presenceData.startTimestamp = browsingTimestamp
+		presenceData.startTimestamp = browsingTimestamp;
 		presence.setActivity(presenceData, true);
 		return;
-}
+	}
 	const [video] = document.querySelectorAll("video");
 
 	if (video && !isNaN(video.duration)) {
@@ -76,10 +84,11 @@ presence.on("UpdateData", async () => {
 			: (await strings).play;
 		presenceData.startTimestamp = startTimestamp;
 		presenceData.endTimestamp = endTimestamp;
-		
-		const chap = document.querySelector<HTMLAnchorElement>(".episode.playing").textContent
+
 		presenceData.details = `Đang xem: ${titleArrOne} `;
-		presenceData.state = `Tập: ${chap}`;
+		presenceData.state = `Tập: ${
+			document.querySelector<HTMLAnchorElement>(".episode.playing").textContent
+		}`;
 
 		if (video.paused) {
 			delete presenceData.startTimestamp;
