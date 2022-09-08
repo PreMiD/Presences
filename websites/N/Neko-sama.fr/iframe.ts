0f2d8f67f25bc17a9894c2d80ae583c67a7d47f6
Mokocchibi <arnaudleboeuf3@gmail.com>
2022-09-08 15:59:19 +0200

    feat(Neko-sama): new presence

--- websites/N/Neko-sama.fr/iframe.ts
+++ websites/N/Neko-sama.fr/iframe.ts
@@ -1,12 +1,10 @@
 const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
-	if (document.querySelector("video") === null) return;
-
 	const video = document.querySelector("video");
 	iframe.send({
-		time: video.currentTime,
-		duration: video.duration,
-		paused: video.paused,
+		time: video?.currentTime,
+		duration: video?.duration,
+		paused: video?.paused,
 	});
 });
