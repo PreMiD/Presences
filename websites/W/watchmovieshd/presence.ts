0b9ec11bd30d5562c07805a6bdde889a33b712b7
Toxicitydev <88631894+Aarondevp@users.noreply.github.com>
2022-09-10 05:27:58 -0500

    Update websites/W/watchmovieshd/presence.ts
    Co-authored-by: RisingSunLight <85577959+RisingSunLight42@users.noreply.github.com>
    Signed-off-by: Toxicitydev <88631894+Aarondevp@users.noreply.github.com>

--- websites/W/watchmovieshd/presence.ts
+++ websites/W/watchmovieshd/presence.ts
@@ -89,7 +89,8 @@ presence.on("UpdateData", async () => {
 			presenceData.smallImageKey = "search";
 		}
 	}
-
+if (!buttons) delete presenceData.buttons
+if (!image) delete presenceData.largeImageKey
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
