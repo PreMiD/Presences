c9744988f1ea8d8552f504e551a19d01f5db13c3
theusaf <theusafyt@gmail.com>
2022-09-24 18:43:41 -0700

    feat: add www.canvas.net details

--- websites/C/Canvas/presence.ts
+++ websites/C/Canvas/presence.ts
@@ -5,9 +5,46 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/DRlJvzX.png",
-		startTimestamp: browsingTimestamp
-	};
+			largeImageKey: "https://i.imgur.com/DRlJvzX.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname, hostname } = window.location,
+		pathSplit = pathname.split("/").filter(val => val);
 
-	presence.setActivity(presenceData);
+	if (hostname === "www.canvas.net") {
+		switch (pathSplit[0] ?? "") {
+			case "": {
+				presenceData.details = "Browsing catalog";
+				break;
+			}
+			case "browse": {
+				presenceData.details = "Browsing course details";
+				presenceData.state =
+					document.querySelector<HTMLHeadingElement>(".h1").textContent;
+				break;
+			}
+			case "courses": {
+				presenceData.details = "Enrolling in a course";
+				presenceData.state =
+					document.querySelector<HTMLSpanElement>(
+						"h2 > span"
+					).lastChild.textContent;
+				break;
+			}
+			case "dashboard": {
+				presenceData.details = "Viewing their dashboard";
+				break;
+			}
+			case "order_items": {
+				presenceData.details = "Viewing their purchase history";
+				break;
+			}
+		}
+	} else {
+		switch (pathSplit[0]) {
+		}
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
 });
