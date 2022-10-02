2d02fc90c0700267ff497ae04b50cdf47c39fe8e
theusaf <theusafyt@gmail.com>
2022-10-02 09:55:01 -0700

    feat: add books and otehr details

--- websites/0-9/5etools/presence.ts
+++ websites/0-9/5etools/presence.ts
@@ -17,20 +17,58 @@ presence.on("UpdateData", async () => {
 			presenceData.state = "Home page";
 			break;
 		}
+		case "adventure.html": {
+			presenceData.details = `Browsing adventure: ${
+				document.querySelector<HTMLHeadingElement>(".page__title").textContent
+			}`;
+			presenceData.state =
+				document.querySelector<HTMLSpanElement>(
+					".entry-title-inner"
+				).textContent;
+			break;
+		}
+		case "adventures.html": {
+			presenceData.details = "Browsing list of adventures";
+			break;
+		}
+		case "book.html": {
+			presenceData.details = `Browsing book: ${
+				document.querySelector<HTMLHeadingElement>(".page__title").textContent
+			}`;
+			presenceData.state =
+				document.querySelector<HTMLSpanElement>(
+					".entry-title-inner"
+				).textContent;
+			break;
+		}
+		case "books.html": {
+			presenceData.details = "Browsing list of books";
+			break;
+		}
 		case "classes.html": {
 			presenceData.details = "Browsing classes";
 			presenceData.state = document.querySelector<HTMLTableCellElement>(
 				".cls-tbl__disp-name"
 			).textContent;
+			presenceData.buttons = [
+				{
+					label: "View Class",
+					url: href,
+				},
+			];
 			break;
 		}
 		case "backgrounds.html":
+		case "charcreationoptions.html":
+		case "conditionsdiseases.html":
 		case "feats.html":
 		case "items.html":
 		case "optionalfeatures.html":
 		// TODO: fix this
 		case "rces.html":
-		case "spells.html": {
+		case "spells.html":
+		case "tables.html":
+		case "variantrules.html": {
 			const type =
 				document.querySelector<HTMLHeadingElement>(".page__title").textContent;
 			presenceData.details = `Browsing ${type}`;
@@ -42,6 +80,21 @@ presence.on("UpdateData", async () => {
 					url: href,
 				},
 			];
+			break;
+		}
+		case "statgen.html": {
+			presenceData.details = "Using stat generator";
+			presenceData.details = document.querySelector<HTMLDivElement>(
+				".ui-tab-side__disp-active-tab-name"
+			).textContent;
+			break;
+		}
+		case "quickreference.html": {
+			presenceData.details = "Browsing quick reference";
+			presenceData.state =
+				document.querySelector<HTMLSpanElement>(
+					".entry-title-inner"
+				).textContent;
 			break;
 		}
 	}
