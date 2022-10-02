76c5a514f0e8ef1b4e23ca0fe29c679bbab99705
theusaf <theusafyt@gmail.com>
2022-10-02 14:40:05 -0700

    refactor: remove code injection, use provided readLogs

--- websites/T/Town of Salem/presence.ts
+++ websites/T/Town of Salem/presence.ts
@@ -2,122 +2,28 @@ const presence = new Presence({
 	clientId: "754771926857285782",
 });
 
+enum Assets {
+	idle = "https://i.imgur.com/m7dbikM.png",
+	day = "https://i.imgur.com/cYUWU5G.png",
+	night = "https://i.imgur.com/DLiJ9VV.png",
+	voting = "https://i.imgur.com/FfmS4Ko.png",
+	covenLogo = "https://i.imgur.com/weE0OjD.png",
+	regularLogo = "https://i.imgur.com/y7VYTQK.jpg",
+}
+
+enum GameEvent {
+	day = 1,
+	night = 0,
+	voting = 2,
+	end = 3,
+}
+
 interface GameState {
 	page: string;
 	day: number;
 	type: string;
-	state: number;
+	state: GameEvent;
 }
-
-// This will be how the site talks to the extension.
-// JSON data will be written to the output.textContent
-const output = document.createElement("textarea");
-output.id = "PreMiD_Salem_Out";
-output.style.display = "none";
-document.body.append(output);
-
-// Overwrites console.log to get data from Town of Salem
-// This is because the game information is not global and everything is rendered on a canvas.
-// However, it seems that every event is logged to the console.
-const s = document.createElement("script");
-s.textContent = `(()=>{
-  const mainObject = {
-    page: "Login",
-    day: 1,
-    type: "Classic",
-    state: 1 // 1 = day, 0 = night, 2 = vote, 3 = end
-  };
-
-  /**
-   * decodeHex - Converts a string of hex values to utf8
-   *
-   * @param  {string} hex The hex string
-   * @returns {string} The utf8 string
-   */
-  function decodeHex(hex){
-    const input = hex.split(" ");
-    let out = "";
-    for(let i = 0;i<input.length;i++){
-      const encoded = input[i].replace(/0x/g,"").replace(/[0-9a-f]{2}/gi,"%$&");
-      try{
-        out += decodeURIComponent(encoded);
-      }catch(err){
-        out += "§"+input[i]+"§";
-      }
-    }
-    return out;
-  }
-
-  const output = document.getElementById("PreMiD_Salem_Out");
-  output.textContent = JSON.stringify(mainObject);
-  // messages that can be ignored by this program
-  const ignoreRegex = /(Submitting chat)|(SocketSend\\.)|(Message received)|(> Potion ID)|(Number of)|(Adding game type)|(Clearing any)|(Initializing)|(Entering)|(Unloading)|(Preloading)|(Login Scene)|\\[ApplicationController]|\\[UnityCache]|\\[Subsystems]|\\[CachedXMLHttpRequest]/gm,
-    oldLog = console.log;
-
-  console.log = function(...args){
-    try{
-      if(args[0].search(ignoreRegex) !== -1){
-        // return;
-      }else if(args[0].search(/^Switched to /gm) === 0){
-        const page = args[0].split(" \\n")[0].split("Switched to ")[1];
-        mainObject.page = page;
-        if(page === "BigHome Scene"){
-          mainObject.day = 1;
-          mainObject.state = 1;
-          mainObject.type = "Classic";
-        }
-        output.textContent = JSON.stringify(mainObject);
-      }else if(args[0].search(/^Creating lobby:/gm) === 0){
-        const type = args[0].split(" |")[0].split(": ")[1];
-        mainObject.type = type;
-        output.textContent = JSON.stringify(mainObject);
-      }else if(args[0].search(/\\[Network]/gm) !== -1 && args[0].search(/Bytes:/gm) !== -1){
-        const hex = args[0].split("Bytes: ")[1].split("</color>")[0],
-          out = decodeHex(hex);
-        // console.warn("Network Log: " + out);
-        if(out.includes("§")){
-          const code = out.match(/§.*?§/g)[0];
-          switch (code.toLowerCase()) {
-            case "§0x91§":{ // night start
-              mainObject.state = 0;
-              break;
-            }
-            case "§0x92§":{ // day start
-              mainObject.state = 1;
-              mainObject.day++;
-              break;
-            }
-            case "§0x93§":{ // voting end
-              mainObject.state = 1;
-              break;
-            }
-            case "§0x9b§":{ // voting starts
-              mainObject.state = 2;
-              break;
-            }
-            case "§0xc2§":{ // end game
-              mainObject.state = 3;
-              break;
-            }
-          }
-          output.textContent = JSON.stringify(mainObject);
-        }
-      }else if(args[0].search(/Entered HandleStartRanked/gm) === 0){
-        mainObject.type = "Ranked";
-        output.textContent = JSON.stringify(mainObject);
-      }else if(args[0].search(/Entered HandleOnLeaveRankedQueue/gm) === 0){
-        mainObject.type = "Classic";
-        output.textContent = JSON.stringify(mainObject);
-      }
-    }catch(err){
-      // console.error(err);
-    }
-
-    // Log original message
-    oldLog(...args);
-  };
-})();`;
-document.body.append(s);
 
 let elapsed = Math.round(Date.now() / 1000),
 	oldState: GameState = {
@@ -125,31 +31,108 @@ let elapsed = Math.round(Date.now() / 1000),
 		day: 1,
 		type: "Classic",
 		state: 1,
-	};
+	},
+	currentState = oldState,
+	logs: string[] = [];
+
+function decodeHex(hex: string): string {
+	const input = hex.split(" ");
+	let out = "";
+	for (let i = 0; i < input.length; i++) {
+		const encoded = input[i].replace(/0x/g, "").replace(/[0-9a-f]{2}/gi, "%$&");
+		try {
+			out += decodeURIComponent(encoded);
+		} catch (err) {
+			out += "§" + input[i] + "§";
+		}
+	}
+	return out;
+}
+
+function handleLog(log: string) {
+	if (log.startsWith("Switched to ")) {
+		const pageName = log.split(" \\n")[0].split("Switched to ")[1];
+		currentState.page = pageName;
+		if (pageName === "BigHome Scene") {
+			currentState.day = 1;
+			currentState.state = GameEvent.day;
+			currentState.type = "Classic";
+		}
+	} else if (log.startsWith("Creating lobby:")) {
+		const type = log.split(" |")[0].split(": ")[1];
+		currentState.type = type;
+	} else if (log.includes("[Network]") && log.includes("Bytes:")) {
+		const hex = log.split("Bytes: ")[1].split("</color>")[0],
+			out = decodeHex(hex);
+		if (out.includes("§")) {
+			const code = out.match(/§.*?§/g)[0];
+			switch (code.toLowerCase()) {
+				case "§0x91§": {
+					// night start
+					currentState.state = GameEvent.night;
+					break;
+				}
+				case "§0x92§": {
+					// day start
+					currentState.state = GameEvent.day;
+					currentState.day++;
+					break;
+				}
+				case "§0x93§": {
+					// voting end
+					currentState.state = GameEvent.day;
+					break;
+				}
+				case "§0x9b§": {
+					// voting starts
+					currentState.state = GameEvent.voting;
+					break;
+				}
+				case "§0xc2§": {
+					// end game
+					currentState.state = GameEvent.end;
+					break;
+				}
+			}
+		}
+	} else if (log.startsWith("Entered HandleStartRanked")) {
+		currentState.type = "Ranked";
+	} else if (log.startsWith("Entered HandleOnLeaveRankedQueue")) {
+		currentState.type = "Classic";
+	}
+}
+
+setInterval(async () => {
+	const latestLogs: string[] = await presence.getLogs(
+		/(Submitting chat)|(SocketSend\\.)|(Message received)|(> Potion ID)|(Number of)|(Adding game type)|(Clearing any)|(Initializing)|(Entering)|(Unloading)|(Preloading)|(Login Scene)|\\[ApplicationController]|\\[UnityCache]|\\[Subsystems]|\\[CachedXMLHttpRequest]/gm
+	);
+	for (let i = latestLogs.length - 1; i >= 0; i--) {
+		if (logs.includes(latestLogs[i])) continue;
+		handleLog(latestLogs[i]);
+	}
+	logs = latestLogs;
+}, 500);
 
 presence.on("UpdateData", () => {
-	let data = {} as PresenceData;
+	let data: PresenceData = {};
 
 	if (window.location.pathname !== "/TownOfSalem/") {
 		data = {
 			details: "Browsing BlankMediaGames",
 			state: document.title,
 			startTimestamp: elapsed,
-			largeImageKey: "regular",
+			largeImageKey: Assets.regularLogo,
 		};
 	} else {
 		try {
-			const info = JSON.parse(
-				document.querySelector<HTMLTextAreaElement>("#PreMiD_Salem_Out")
-					.textContent
-			);
-			if (oldState.page !== info.page) elapsed = Math.round(Date.now() / 1000);
+			if (oldState.page !== currentState.page)
+				elapsed = Math.round(Date.now() / 1000);
 
-			let key = "regular";
-			if (info.type.search(/Coven/g) !== -1) key = "coven";
+			let key = Assets.regularLogo;
+			if (currentState.type.search(/Coven/g) !== -1) key = Assets.covenLogo;
 
 			let gameType;
-			switch (info.type) {
+			switch (currentState.type) {
 				case "ClassicTownTraitor": {
 					gameType = "Town Traitor";
 					break;
@@ -195,34 +178,34 @@ presence.on("UpdateData", () => {
 					break;
 				}
 				default: {
-					gameType = info.type;
+					gameType = currentState.type;
 					break;
 				}
 			}
-			switch (info.page) {
+			switch (currentState.page) {
 				case "Login": {
 					data = {
 						details: "Logging in",
-						largeImageKey: "regular",
-						smallImageKey: "idle",
+						largeImageKey: Assets.regularLogo,
+						smallImageKey: Assets.idle,
 						startTimestamp: elapsed,
 					};
 					break;
 				}
 				case "BigHome Scene": {
-					if (info.type === "Ranked") {
+					if (currentState.type === "Ranked") {
 						data = {
 							details: "In a Ranked match",
 							state: "Waiting in queue",
-							largeImageKey: "regular",
-							smallImageKey: "idle",
+							largeImageKey: Assets.regularLogo,
+							smallImageKey: Assets.idle,
 							startTimestamp: elapsed,
 						};
 					} else {
 						data = {
 							details: "Browsing Home Screen",
-							largeImageKey: "regular",
-							smallImageKey: "idle",
+							largeImageKey: Assets.regularLogo,
+							smallImageKey: Assets.idle,
 							startTimestamp: elapsed,
 						};
 					}
@@ -234,7 +217,7 @@ presence.on("UpdateData", () => {
 						state: "Waiting in lobby",
 						elapsed,
 						largeImageKey: key,
-						smallImageKey: "idle",
+						smallImageKey: Assets.idle,
 					});
 					break;
 				}
@@ -244,25 +227,25 @@ presence.on("UpdateData", () => {
 						largeImageKey: key,
 						startTimestamp: elapsed,
 					});
-					switch (info.state) {
-						case 0: {
-							data.state = `Night ${info.day}`;
-							data.smallImageKey = "night";
+					switch (currentState.state) {
+						case GameEvent.night: {
+							data.state = `Night ${currentState.day}`;
+							data.smallImageKey = Assets.night;
 							break;
 						}
-						case 1: {
-							data.state = `Day ${info.day}`;
-							data.smallImageKey = "day";
+						case GameEvent.day: {
+							data.state = `Day ${currentState.day}`;
+							data.smallImageKey = Assets.day;
 							break;
 						}
-						case 2: {
-							data.state = `Day ${info.day} | Judgement`;
-							data.smallImageKey = "voting";
+						case GameEvent.voting: {
+							data.state = `Day ${currentState.day} | Judgement`;
+							data.smallImageKey = Assets.voting;
 							break;
 						}
-						case 3: {
-							data.state = `Day ${info.day} | Game End`;
-							data.smallImageKey = "idle";
+						case GameEvent.end: {
+							data.state = `Day ${currentState.day} | Game End`;
+							data.smallImageKey = Assets.idle;
 							break;
 						}
 					}
@@ -272,7 +255,7 @@ presence.on("UpdateData", () => {
 					Object.assign(data, {
 						details: "Browsing End-Game Screen",
 						largeImageKey: key,
-						smallImageKey: "idle",
+						smallImageKey: Assets.idle,
 						startTimestamp: elapsed,
 					});
 					break;
@@ -282,12 +265,12 @@ presence.on("UpdateData", () => {
 					break;
 				}
 			}
-			oldState = info;
+			oldState = currentState;
 		} catch (e) {
 			Object.assign(data, {
-				details: "Logging in.",
-				largeImageKey: "regular",
-				smallImageKey: "idle",
+				details: "Logging in",
+				largeImageKey: Assets.regularLogo,
+				smallImageKey: Assets.idle,
 				startTimestamp: elapsed,
 			});
 		}
