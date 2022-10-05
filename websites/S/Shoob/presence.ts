8aef6eec3577607a37b2189175dd8cf9afe5358a
theusaf <theusafyt@gmail.com>
2022-10-05 15:05:33 -0700

    refactor: use switch statement

--- websites/S/Shoob/presence.ts
+++ websites/S/Shoob/presence.ts
@@ -9,88 +9,162 @@ presence.on("UpdateData", async () => {
 			startTimestamp: browsingTimestamp,
 		},
 		{ pathname, href } = window.location,
+		pathSplit = pathname.split("/").slice(1),
 		pageTitle = document.querySelector(
 			"[itemprop='breadcrumb'] > li:last-child"
 		)?.textContent;
 
-	if (pathname === "/" || pathname === "/home/")
-		presenceData.details = "Viewing home page";
-	else if (pathname.includes("/user")) {
-		presenceData.details = "Viewing a profile";
-		presenceData.state = pageTitle;
-	} else if (pathname.includes("/dashboard"))
-		presenceData.details = "Viewing the Dashboard";
-	else if (pathname.includes("/premium"))
-		presenceData.details = "Viewing Premium";
-	else if (pathname.includes("/giveaway"))
-		presenceData.details = "Viewing Giveaways";
-	else if (pathname.includes("/settings"))
-		presenceData.details = "Viewing Settings";
-	else if (pathname.includes("/market"))
-		presenceData.details = "Viewing the Market";
-	else if (pathname.includes("/notifications"))
-		presenceData.details = "Viewing Notifications";
-	else if (pathname.includes("/events"))
-		presenceData.details = "Viewing Events";
-	else if (pathname.includes("/anime")) presenceData.details = "Viewing Anime";
-	else if (pathname.includes("/shop"))
-		presenceData.details = "Viewing the Shop";
-	else if (pathname.includes("/bank"))
-		presenceData.details = "Viewing the Bank";
-	else if (pathname.includes("/cards")) {
-		if (pathname.includes("/info")) {
-			presenceData.details = "Viewing a Card";
+	switch (pathSplit[0] ?? "") {
+		case "":
+		case "home": {
+			presenceData.details = "Viewing home page";
+			break;
+		}
+		case "user": {
+			presenceData.details = "Viewing a profile";
 			presenceData.state = pageTitle;
-			presenceData.largeImageKey =
-				document.querySelector<HTMLImageElement>(".cardData > img").src;
-			presenceData.buttons = [
-				{
-					label: "View Card",
-					url: href,
-				},
-			];
-		} else presenceData.details = "Viewing the Cards";
-	} else if (pathname.includes("/card-abilities"))
-		presenceData.details = "Viewing Card Abilities";
-	else if (pathname.includes("/card-events"))
-		presenceData.details = "Viewing Card Events";
-	else if (pathname.includes("/inventory"))
-		presenceData.details = "Viewing Inventory";
-	else if (pathname.includes("/fusion")) presenceData.details = "Fusing Cards";
-	else if (pathname.includes("/auction"))
-		presenceData.details = "Viewing the Auction";
-	else if (pathname.includes("/trades"))
-		presenceData.details = "Viewing Trades";
-	/// Anime Soul games section ///
-	else if (pathname.includes("/this-or-that"))
-		presenceData.details = "Playing This or That";
-	else if (pathname.includes("/mini-games"))
-		presenceData.details = "Playing Mini Games";
-	/// Anime Soul community section ///
-	else if (pathname.includes("/creators"))
-		presenceData.details = "Viewing Creators";
-	else if (pathname.includes("/medals"))
-		presenceData.details = "Viewing Medals";
-	else if (pathname.includes("/friends"))
-		presenceData.details = "Viewing Friends";
-	else if (pathname.includes("/leaderboards"))
-		presenceData.details = "Viewing Leaderboards";
-	else if (pathname.includes("/cardmakers/leaderboard"))
-		presenceData.details = "Viewing CardMaker Leaderboards";
-	else if (pathname.includes("/servers"))
-		presenceData.details = "Viewing Servers";
-	else if (pathname.includes("/appeals"))
-		presenceData.details = "Viewing Appeals";
-	else if (pathname.includes("/updates"))
-		presenceData.details = "Viewing Updates";
-	else if (pathname.includes("/articles"))
-		presenceData.details = "Viewing Guides";
-	else if (pathname.includes("/rules"))
-		presenceData.details = "Reading the Rules";
-	else if (pathname.includes("/staff-list"))
-		presenceData.details = "Viewing Staff List";
-	else if (pathname.includes("/staff"))
-		presenceData.details = "Viewing Staff Pages";
+			break;
+		}
+		case "dashboard": {
+			presenceData.details = "Viewing the Dashboard";
+			break;
+		}
+		case "premium": {
+			presenceData.details = "Viewing Premium";
+			break;
+		}
+		case "giveaway": {
+			presenceData.details = "Viewing Giveaways";
+			break;
+		}
+		case "settings": {
+			presenceData.details = "Managing Settings";
+			break;
+		}
+		case "market": {
+			presenceData.details = "Viewing the Market";
+			break;
+		}
+		case "notifications": {
+			presenceData.details = "Viewing Notifications";
+			break;
+		}
+		case "events": {
+			presenceData.details = "Viewing Events";
+			break;
+		}
+		case "anime": {
+			presenceData.details = "Viewing Anime";
+			break;
+		}
+		case "shop": {
+			presenceData.details = "Viewing the Shop";
+			break;
+		}
+		case "bank": {
+			presenceData.details = "Viewing the Bank";
+			break;
+		}
+		case "cards": {
+			if (pathSplit[1] === "info") {
+				presenceData.details = "Viewing a Card";
+				presenceData.state = pageTitle;
+				presenceData.largeImageKey =
+					document.querySelector<HTMLImageElement>(".cardData > img").src;
+				presenceData.buttons = [
+					{
+						label: "View Card",
+						url: href,
+					},
+				];
+			} else presenceData.details = "Viewing the Cards";
+			break;
+		}
+		case "card-abilities": {
+			presenceData.details = "Viewing Card Abilities";
+			break;
+		}
+		case "card-events": {
+			presenceData.details = "Viewing Card Events";
+			break;
+		}
+		case "inventory": {
+			presenceData.details = "Viewing Inventory";
+			break;
+		}
+		case "fusion": {
+			presenceData.details = "Fusing Cards";
+			break;
+		}
+		case "auction": {
+			presenceData.details = "Viewing the Auction";
+			break;
+		}
+		case "trades": {
+			presenceData.details = "Viewing Trades";
+			break;
+		}
+		// Shoob games section
+		case "this-or-that": {
+			presenceData.details = "Playing This or That";
+			break;
+		}
+		case "mini-games": {
+			presenceData.details = "Playing Mini Games";
+			break;
+		}
+		// Shoob community section
+		case "creators": {
+			presenceData.details = "Viewing Creators";
+			break;
+		}
+		case "medals": {
+			presenceData.details = "Viewing Medals";
+			break;
+		}
+		case "friends": {
+			presenceData.details = "Viewing Friends";
+			break;
+		}
+		case "leaderboards": {
+			presenceData.details = "Viewing Leaderboards";
+			break;
+		}
+		case "cardmakers": {
+			if (pathSplit[1] === "leaderboard") {
+				presenceData.details = "Viewing CardMaker Leaderboards";
+			}
+			break;
+		}
+		case "servers": {
+			presenceData.details = "Viewing Servers";
+			break;
+		}
+		case "appeals": {
+			presenceData.details = "Viewing Appeals";
+			break;
+		}
+		case "updates": {
+			presenceData.details = "Viewing Updates";
+			break;
+		}
+		case "articles": {
+			presenceData.details = "Viewing Guides";
+			break;
+		}
+		case "rules": {
+			presenceData.details = "Reading the Rules";
+			break;
+		}
+		case "staff-list": {
+			presenceData.details = "Viewing Staff List";
+			break;
+		}
+		case "staff": {
+			presenceData.details = "Viewing Staff Pages";
+			break;
+		}
+	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
