87a3333e1c780fcd17e11263b9a2c5b2798082cb
theusaf <theusafyt@gmail.com>
2022-10-05 13:06:38 -0700

    feat: add features.weather.com

--- websites/T/The Weather Channel/presence.ts
+++ websites/T/The Weather Channel/presence.ts
@@ -1,59 +1,62 @@
 const presence = new Presence({
 		clientId: "1027249400738750625",
 	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000)
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/Zzh3YNq.png",
-		startTimestamp: browsingTimestamp
-	},
-	{ pathname } = window.location,
-	pathSplit = pathname.split("/").slice(1);
+			largeImageKey: "https://i.imgur.com/Zzh3YNq.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname, hostname } = window.location,
+		pathSplit = pathname.split("/").filter(x => x);
 
-	switch (pathSplit[0] ?? "") {
-		case "": {
-			presenceData.details = "Browsing";
-			presenceData.state = "Home page";
-			break;
+	if (hostname === "weather.com") {
+		switch (pathSplit[0] ?? "") {
+			case "": {
+				presenceData.details = "Browsing";
+				presenceData.state = "Home page";
+				break;
+			}
+			case "deals": {
+				break;
+			}
+			case "forecast": {
+				break;
+			}
+			case "photos": {
+				break;
+			}
+			case "login":
+			case "signup": {
+				break;
+			}
+			case "maps": {
+				break;
+			}
+			case "member": {
+				break;
+			}
+			case "news": {
+				break;
+			}
+			case "safety": {
+				break;
+			}
+			case "slideshows": {
+				break;
+			}
+			case "storms": {
+				break;
+			}
+			case "weather": {
+				break;
+			}
+			default: {
+				break;
+			}
 		}
-		case "deals": {
-			break;
-		}
-		case "forecast": {
-			break;
-		}
-		case "photos": {
-			break;
-		}
-		case "login":
-		case "signup": {
-			break;
-		}
-		case "maps": {
-			break;
-		}
-		case "member": {
-			break;
-		}
-		case "news": {
-			break;
-		}
-		case "safety": {
-			break;
-		}
-		case "slideshows": {
-			break;
-		}
-		case "storms": {
-			break;
-		}
-		case "weather": {
-			break;
-		}
-		default: {
-			break;
-		}
+	} else {
 	}
 
 	presence.setActivity(presenceData);
