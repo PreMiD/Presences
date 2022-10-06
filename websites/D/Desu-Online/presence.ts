2baadbb8fd62327042ba7315426d4f43b0a327a5
Mefuuu <42370647+Mefuuu@users.noreply.github.com>
2022-10-06 15:49:33 +0200

    forgot to run yarn format
    Signed-off-by: Mefuuu <42370647+Mefuuu@users.noreply.github.com>

--- websites/D/Desu-Online/presence.ts
+++ websites/D/Desu-Online/presence.ts
@@ -130,7 +130,8 @@ presence.on("UpdateData", async () => {
 			)
 			.textContent.split("-");
 		presenceData.state = `Odcinek:${playinfo[0].replace("Odc", "")}`;
-		if (playinfo.length > 2) presenceData.state += ` - "${playinfo[1].slice(1, -1)}"`;
+		if (playinfo.length > 2)
+			presenceData.state += ` - "${playinfo[1].slice(1, -1)}"`;
 		presenceData.buttons = [
 			{ label: "Oglądaj", url: document.URL },
 			{
