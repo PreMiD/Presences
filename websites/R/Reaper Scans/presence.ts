66e24f1575e773ece13690810d61db612ff1b057
theusaf <theusafyt@gmail.com>
2022-10-09 08:57:12 -0700

    feat: add search and blog paths

--- websites/R/Reaper Scans/presence.ts
+++ websites/R/Reaper Scans/presence.ts
@@ -18,63 +18,87 @@ presence.on("UpdateData", () => {
 		},
 		pathSplit = pathname.split("/").slice(1);
 
-	if ((pathSplit[0] ?? "") === "") presenceData.details = "Viewing Home Page";
-	else if (pathSplit[0] === "comics" || pathSplit[0] === "novels") {
-		const captitalized = `${pathSplit[0][0].toUpperCase()}${pathSplit[0].slice(
-			1
-		)}`;
-		if (pathSplit[1]) {
-			if (pathSplit[2] === "chapters") {
-				let progress =
-					(document.documentElement.scrollTop /
-						(document.querySelector(
-							pathSplit[0] === "comics"
-								? "main > div:not([id*='Ads'])"
-								: "article"
-						).scrollHeight -
-							window.innerHeight)) *
-					100;
-				progress = Math.ceil(progress) > 100 ? 100 : Math.ceil(progress);
-
-				presenceData.details = `Reading ${
-					document.querySelector("h2").textContent
-				}`;
-				presenceData.state = `📖 ${document
-					.querySelector("h1")
-					.textContent.trim()} 🔸 ${progress}%`;
-				presenceData.smallImageKey = Assets.Read;
-				presenceData.buttons = [
-					{
-						label: `Visit ${captitalized.slice(captitalized.length - 1)} Page`,
-						url: document.querySelector<HTMLAnchorElement>("h2 + div > a").href,
-					},
-					{
-						label: "Visit Chapter",
-						url: href,
-					},
-				];
-			} else {
-				presenceData.details = `Viewing ${captitalized} Page`;
-				presenceData.state = document.querySelector("h1").textContent;
-				presenceData.smallImageKey = Assets.View;
-				presenceData.buttons = [
-					{
-						label: `Visit ${captitalized} Page`,
-						url: href,
-					},
-				];
-			}
-		} else {
-			presenceData.details = `Viewing ${captitalized} List`;
-			presenceData.state = `📋 ${
-				document.querySelectorAll<HTMLLIElement>("h2 + div li").length
-			} ${pathSplit[0]}s found`;
+	switch (pathSplit[0] ?? "") {
+		case "": {
+			const searchInput = document.querySelector<HTMLInputElement>("#search");
+			if (searchInput === document.activeElement) {
+				presenceData.details = "Searching";
+				presenceData.state = searchInput.value;
+				presenceData.smallImageKey = Assets.Search;
+			} else presenceData.details = "Viewing Home Page";
+			break;
 		}
-	} else if (pathSplit[0] === "account")
-		presenceData.details = "Managing Account Settings";
-	else {
-		presenceData.details = "Browsing Reaper Scans";
-		presenceData.state = document.title;
+		case "comics":
+		case "novels": {
+			const captitalized = `${pathSplit[0][0].toUpperCase()}${pathSplit[0].slice(
+				1
+			)}`;
+			if (pathSplit[1]) {
+				if (pathSplit[2] === "chapters") {
+					let progress =
+						(document.documentElement.scrollTop /
+							(document.querySelector(
+								pathSplit[0] === "comics"
+									? "main > div:not([id*='Ads'])"
+									: "article"
+							).scrollHeight -
+								window.innerHeight)) *
+						100;
+					progress = Math.ceil(progress) > 100 ? 100 : Math.ceil(progress);
+
+					presenceData.details = `Reading ${
+						document.querySelector("h2").textContent
+					}`;
+					presenceData.state = `📖 ${document
+						.querySelector("h1")
+						.textContent.trim()} 🔸 ${progress}%`;
+					presenceData.smallImageKey = Assets.Read;
+					presenceData.buttons = [
+						{
+							label: `Visit ${captitalized.slice(
+								captitalized.length - 1
+							)} Page`,
+							url: document.querySelector<HTMLAnchorElement>("h2 + div > a")
+								.href,
+						},
+						{
+							label: "Visit Chapter",
+							url: href,
+						},
+					];
+				} else {
+					presenceData.details = `Viewing ${captitalized} Page`;
+					presenceData.state = document.querySelector("h1").textContent;
+					presenceData.smallImageKey = Assets.View;
+					presenceData.buttons = [
+						{
+							label: `Visit ${captitalized} Page`,
+							url: href,
+						},
+					];
+				}
+			} else {
+				presenceData.details = `Viewing ${captitalized} List`;
+				presenceData.state = `📋 ${
+					document.querySelectorAll<HTMLLIElement>("h2 + div li").length
+				} ${pathSplit[0]}s found`;
+			}
+
+			break;
+		}
+		case "account": {
+			presenceData.details = "Managing Account Settings";
+			break;
+		}
+		case "blog": {
+			presenceData.details = "Reading a Blog Post";
+			presenceData.state = document.querySelector("h1").textContent.trim();
+			break;
+		}
+		default: {
+			presenceData.details = "Browsing Reaper Scans";
+			presenceData.state = document.title;
+		}
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
