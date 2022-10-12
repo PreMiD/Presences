a6bc8056c76699b39d352974bdfbe0b09114d725
sarcasternub <sarcasteryt@gmail.com>
2022-10-12 15:55:25 +0530

    refactor(Sofi): remove commented code

--- websites/S/Sofi/presence.ts
+++ websites/S/Sofi/presence.ts
@@ -14,12 +14,7 @@ presence.on("UpdateData", async () => {
 		presenceData.details = "Viewing Homepage";
 		presenceData.smallImageKey = "https://i.imgur.com/9WPWqvT.png";
 		presenceData.smallImageText = "Viewing...";
-		presenceData.buttons = [
-			{
-				label: "View Homepage",
-				url: href,
-			},
-		];
+
 		switch (pathname) {
 			case "/backgrounds": {
 				presenceData.details = "Viewing Backgrounds";
@@ -133,122 +128,7 @@ presence.on("UpdateData", async () => {
 		delete presenceData.buttons;
 	}
 
-	// else if (search?.value) {
-	// 	presenceData.details = "Searching for";
-	// 	presenceData.state = search.value;
-	// 	presenceData.smallImageKey = "https://i.imgur.com/0tmziN8.pngg";
-	// } else if (hostname === "store.epicgames.com") {
-	// 	if (
-	// 		pathname ===
-	// 		`/${document
-	// 			.querySelector('[data-baseurl="https://store.epicgames.com"]')
-	// 			.getAttribute("lang")}/`
-	// 	)
-	// 		presenceData.details = "Viewing homepage";
-	// 	else {
-	// 		switch (pathname.split("/")[2]) {
-	// 			case "p": {
-	// 				if (video?.muted || !video) presenceData.details = "Viewing game";
-	// 				else {
-	// 					delete presenceData.startTimestamp;
-	// 					presenceData.details = "Viewing trailer of";
-	// 					if (!video.paused) {
-	// 						presenceData.smallImageKey = "https://i.imgur.com/OLaz6JN.png";
-	// 						[, presenceData.endTimestamp] =
-	// 							presence.getTimestampsfromMedia(video);
-	// 					} else
-	// 						presenceData.smallImageKey = "https://i.imgur.com/4iyMINk.png";
-	// 				}
-	// 				presenceData.state =
-	// 					JSON.parse(
-	// 						document.querySelector('[type="application/ld+json"]')?.innerHTML
-	// 					)?.name ??
-	// 					document
-	// 						.querySelector<HTMLMetaElement>('[property="og:title"]')
-	// 						.content.split("|")[0];
-	// 				presenceData.buttons = [
-	// 					{
-	// 						label: "View Game",
-	// 						url: href,
-	// 					},
-	// 				];
-	// 				presenceData.largeImageKey = document
-	// 					.querySelector('[class="css-7i770w"]')
-	// 					.getAttribute("src");
-
-	// 				break;
-	// 			}
-	// 			case "news": {
-	// 				if (document.querySelector('[id="storeNews"]')) {
-	// 					presenceData.details = "Reading about";
-	// 					presenceData.smallImageKey = "https://i.imgur.com/nese1O7.png";
-	// 					presenceData.state = document
-	// 						.querySelector("title")
-	// 						.textContent.slice(0, 128);
-	// 				} else presenceData.details = "Browsing all news";
-	// 				presenceData.buttons = [
-	// 					{
-	// 						label: "Read The News",
-	// 						url: href,
-	// 					},
-	// 				];
-	// 				break;
-	// 			}
-	// 			case "browse": {
-	// 				presenceData.buttons = [
-	// 					{
-	// 						label: "Browse Games",
-	// 						url: href,
-	// 					},
-	// 				];
-	// 				if (document.querySelector('[aria-checked="true"]')) {
-	// 					presenceData.details = "Viewing results for";
-	// 					presenceData.state = document.querySelector(
-	// 						'[aria-checked="true"]'
-	// 					).textContent;
-	// 				} else if (document.querySelector('[class="css-71iht3"]')) {
-	// 					delete presenceData.buttons;
-	// 					presenceData.details = "Viewing results for";
-	// 					presenceData.state = href?.split("?q=")?.at(0)?.split("&")?.at(0);
-	// 				} else presenceData.details = "Browsing games";
-	// 				break;
-	// 			}
-	// 			default: {
-	// 				presenceData.details = "Browsing";
-	// 				break;
-	// 			}
-	// 		}
-	// 	}
-	// } else if (hostname === "www.epicgames.com") {
-	// 	if (
-	// 		pathname ===
-	// 		`/site/${document
-	// 			.querySelector('[data-baseurl="https://www.epicgames.com/site"]')
-	// 			?.getAttribute("lang")}/`
-	// 	)
-	// 		presenceData.details = "Viewing homepage";
-	// 	else if (pathname.includes("/help/")) {
-	// 		presenceData.details = "Reading about";
-	// 		presenceData.state = document.querySelector(
-	// 			'[class*="ArticleAnchorLink--active"]'
-	// 		).textContent;
-	// 		presenceData.smallImageKey = "https://i.imgur.com/nese1O7.png";
-	// 	} else {
-	// 		switch (pathname.split("/")[3]) {
-	// 			case "epic-games-store-faq": {
-	// 				presenceData.details = "Reading the FAQ";
-	// 				presenceData.smallImageKey = "https://i.imgur.com/nese1O7.png";
-	// 				break;
-	// 			}
-	// 			default: {
-	// 				presenceData.details = "Browsing";
-	// 				break;
-	// 			}
-	// 		}
-	// 	}
-	// }
 	if (!buttons) delete presenceData.buttons;
-	// if (!covers) presenceData.largeImageKey = "https://i.imgur.com/CAsgHOc.png";
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
