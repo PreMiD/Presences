15e262a00f21243f5b179089695153e7919e2fa3
theusaf <theusafyt@gmail.com>
2022-10-11 23:07:36 -0700

    refactor: create constants and remove long if-else chain

--- websites/J/Jackbox/presence.ts
+++ websites/J/Jackbox/presence.ts
@@ -3,141 +3,324 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.round(Date.now() / 1000);
 
-let layout: string;
+enum LayoutVersion {
+	New = "new",
+	Dictionarium = "dict",
+	Legacy = "legacy",
+	Guesspionage = "guesp",
+}
+
+interface Game {
+	name: string;
+	layout?: LayoutVersion;
+	selector: string;
+	logo: string;
+}
+
+const Games: Game[] = [
+	// Party Pack 1
+	{
+		name: "You Don't Know Jack 2015",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-ydkj2015",
+		logo: "https://i.imgur.com/EGnX1E5.png",
+	},
+	{
+		name: "Drawful 1/2",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-drawful",
+		logo: "https://i.imgur.com/TOaYCE3.png",
+	},
+	{
+		name: "Word Spud",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-wordspud",
+		logo: "https://i.imgur.com/gFUB4EX.png",
+	},
+	{
+		name: "Lie Swatter",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-lieswatter",
+		logo: "https://i.imgur.com/PavDjzP.png",
+	},
+	// Party Pack 2
+	{
+		name: "Bidiots",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-auction",
+		logo: "https://i.imgur.com/CMcGDrM.png",
+	},
+	{
+		name: "Bomb Corp",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-bombintern",
+		logo: "https://i.imgur.com/XkqOv6o.png",
+	},
+	{
+		name: "Earwax",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-earwax",
+		logo: "https://i.imgur.com/oEidNn6.png",
+	},
+	{
+		name: "Fibbage XL/2",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-fibbage",
+		logo: "https://i.imgur.com/LxAtHuy.png",
+	},
+	// Party Pack 3
+	{
+		name: "Tee K.O.",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-awshirt",
+		logo: "https://i.imgur.com/wGbJhoR.png",
+	},
+	{
+		name: "Fakin' It",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-fakinit",
+		logo: "https://i.imgur.com/nDqnyqV.png",
+	},
+	{
+		name: "Guesspionage",
+		layout: LayoutVersion.Guesspionage,
+		selector: "#page-pollposition",
+		logo: "https://i.imgur.com/nDqnyqV.png",
+	},
+	{
+		name: "Quiplash 1/2/XL",
+		layout: LayoutVersion.Legacy,
+		selector: "#page-quiplash",
+		logo: "https://i.imgur.com/NAySr0E.png",
+	},
+	{
+		name: "Trivia M Party", // TODO: Fix name
+		layout: LayoutVersion.Legacy,
+		selector: "#page-triviadeath",
+		logo: "https://i.imgur.com/zHOvymB.png",
+	},
+	// Party Pack 4
+	{
+		name: "Civic Doodle",
+		selector: ".Overdrawn",
+		logo: "https://i.imgur.com/6CBskbM.png",
+	},
+	{
+		name: "Bracketeering",
+		selector: ".bracketeering",
+		logo: "https://i.imgur.com/oJJpQqi.png",
+	},
+	{
+		name: "Monster Seeking Monster",
+		selector: ".MonsterMingle",
+		logo: "https://i.imgur.com/CPkaxX3.png",
+	},
+	{
+		name: "Survive the Internet",
+		selector: ".SurviveTheInternet",
+		logo: "https://i.imgur.com/eXCzZ2e.png",
+	},
+	{
+		name: "Fibbage 3",
+		selector: ".Fibbage3",
+		logo: "https://i.imgur.com/eTsm2zC.png",
+	},
+	// Party Pack 5
+	{
+		name: "Split the Room",
+		selector: ".SplitTheRoom",
+		logo: "https://i.imgur.com/YyhOPAp.png",
+	},
+	{
+		name: "Zeeple Dome",
+		selector: ".SlingShoot",
+		logo: "https://i.imgur.com/QqEKHgG.png",
+	},
+	{
+		name: "Patently Stupid",
+		selector: ".PatentlyStupid",
+		logo: "https://i.imgur.com/yGEE0Aw.png",
+	},
+	{
+		name: "Mad Verse City",
+		selector: ".RapBattle",
+		logo: "https://i.imgur.com/Up8Paw8.png",
+	},
+	{
+		name: "You Don't Know Jack: Full Stream",
+		selector: ".YDKJ2018",
+		logo: "https://i.imgur.com/Li8TLXI.png",
+	},
+	// Party Pack 6
+	{
+		name: "Push the Button",
+		selector: ".Push\\.The\\.Button", // TODO: Verify
+		logo: "https://i.imgur.com/OhgGmMQ.png",
+	},
+	{
+		name: "Joke Boat",
+		selector: ".Jokeboat",
+		logo: "https://i.imgur.com/Cih6bqA.png",
+	},
+	{
+		name: "Role Models",
+		selector: ".RoleModels",
+		logo: "https://i.imgur.com/sETBCgc.png",
+	},
+	{
+		name: "Trivia M Party 2", // TODO: Fix name
+		selector: ".TriviaDeath2",
+		logo: "https://i.imgur.com/9MmGVGD.png",
+	},
+	{
+		name: "Dictionarium",
+		selector: ".Ridictionarium",
+		logo: "https://i.imgur.com/8S0aSTw.png",
+		layout: LayoutVersion.Dictionarium,
+	},
+	// Party Pack 7
+	{
+		name: "Quiplash 3",
+		selector: "TODO",
+		logo: "https://i.imgur.com/Jds4R56.png",
+	},
+	{
+		name: "Talking Points",
+		selector: "TODO",
+		logo: "https://i.imgur.com/8Amy9Di.png",
+	},
+	{
+		name: "Blather 'Round",
+		selector: "TODO",
+		logo: "https://i.imgur.com/qLW2Fft.png",
+	},
+	{
+		name: "The Devils and the Details",
+		selector: "TODO",
+		logo: "https://i.imgur.com/tXkh58c.png",
+	},
+	{
+		name: "Champ'd Up",
+		selector: "TODO",
+		logo: "https://i.imgur.com/KnAS7yD.png",
+	},
+	// Party Pack 8
+	{
+		name: "Wheel of Enormous Proportions",
+		selector: "TODO",
+		logo: "https://i.imgur.com/KnAS7yD.png",
+	},
+	{
+		name: "Weapons Drawn",
+		selector: "TODO",
+		logo: "https://i.imgur.com/yRXmXbf.png",
+	},
+	{
+		name: "Job Job",
+		selector: "TODO",
+		logo: "https://i.imgur.com/FfZYRGL.png",
+	},
+	{
+		name: "Drawful Animate",
+		selector: "TODO",
+		logo: "https://i.imgur.com/NtRvc93.png",
+	},
+	{
+		name: "The Poll Mine",
+		selector: "TODO",
+		logo: "https://i.imgur.com/o4aaUox.png",
+	},
+	// Party Pack 9
+	{
+		name: "Fibbage 4",
+		selector: "TODO",
+		logo: "TODO",
+	},
+	{
+		name: "Quixort",
+		selector: "TODO",
+		logo: "TODO",
+	},
+	{
+		name: "Nonsensory",
+		selector: "TODO",
+		logo: "TODO",
+	},
+	{
+		name: "Junktopia",
+		selector: "TODO",
+		logo: "TODO",
+	},
+	{
+		name: "Roomerang",
+		selector: "TODO",
+		logo: "TODO",
+	},
+];
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "jackbox",
+			largeImageKey: "https://i.imgur.com/SXfEdnL.png",
 		},
-		// Check for presence setting
 		useName = await presence.getSetting<boolean>("useName"),
-		useTime = await presence.getSetting<boolean>("useTime");
+		useTime = await presence.getSetting<boolean>("useTime"),
+		useDetails = await presence.getSetting<boolean>("useDetails");
 
 	if (useTime) presenceData.startTimestamp = browsingTimestamp;
 
-	// PP6
-	if (document.querySelectorAll(".Ridictionary").length > 0) {
-		presenceData.details = "Playing Dictionarium";
-		layout = "dict";
-	} else if (document.querySelectorAll(".TriviaDeath2").length > 0) {
-		presenceData.details = "Playing Trivia Murder Party 2";
-		layout = "new";
-	} else if (document.querySelectorAll(".RoleModels").length > 0) {
-		presenceData.details = "Playing Role Models";
-		layout = "new";
-	} else if (document.querySelectorAll(".Jokeboat").length > 0) {
-		presenceData.details = "Playing Joke Boat";
-		layout = "new";
-	} else if (document.querySelectorAll(".Push.The.Button").length > 0) {
-		presenceData.details = "Playing Push The Button";
-		layout = "new";
-
-		// PP5
-	} else if (document.querySelectorAll(".YDKJ2018").length > 0) {
-		presenceData.details = "Playing You Don't Know Jack: Full Stream";
-		layout = "new";
-	} else if (document.querySelectorAll(".RapBattle").length > 0) {
-		presenceData.details = "Playing Mad Verse City";
-		layout = "new";
-	} else if (document.querySelectorAll(".PatentlyStupid").length > 0) {
-		presenceData.details = "Playing Patently Stupid";
-		layout = "new";
-	} else if (document.querySelectorAll(".SlingShoot").length > 0) {
-		presenceData.details = "Playing Zeeple Dome";
-		layout = "new";
-	} else if (document.querySelectorAll(".SplitTheRoom").length > 0) {
-		presenceData.details = "Playing Split The Room";
-		layout = "new";
-
-		// PP4
-	} else if (document.querySelectorAll(".Fibbage3").length > 0) {
-		presenceData.details = "Playing Fibbage 3";
-		layout = "new";
-	} else if (document.querySelectorAll(".SurviveTheInternet").length > 0) {
-		presenceData.details = "Playing Survive The Internet";
-		layout = "new";
-	} else if (document.querySelectorAll(".MonsterMingle").length > 0) {
-		presenceData.details = "Playing Monster Seeking Monster";
-		layout = "new";
-	} else if (document.querySelectorAll(".bracketeering").length > 0) {
-		presenceData.details = "Playing Bracketeering";
-		layout = "new";
-	} else if (document.querySelectorAll(".Overdrawn").length > 0) {
-		presenceData.details = "Playing Civic Doodle";
-		layout = "new";
-
-		// PP3
-	} else if (document.querySelector("#page-quiplash")) {
-		presenceData.details = "Playing Quiplash 1/XL/2";
-		layout = "legacy";
-	} else if (document.querySelector("#page-triviadeath")) {
-		presenceData.details = "Playing Trivia Murder Party";
-		layout = "legacy";
-	} else if (document.querySelector("#page-pollposition")) {
-		presenceData.details = "Playing Guesspionage";
-		layout = "guessp";
-	} else if (document.querySelector("#page-fakinit")) {
-		presenceData.details = "Playing Fakin' It";
-		layout = "legacy";
-	} else if (document.querySelector("#page-awshirt")) {
-		presenceData.details = "Playing Tee K.O.";
-		layout = "legacy";
-
-		// PP2
-	} else if (document.querySelector("#page-fibbage")) {
-		presenceData.details = "Playing Fibbage XL/2";
-		layout = "legacy";
-	} else if (document.querySelector("#page-earwax")) {
-		presenceData.details = "Playing Earwax";
-		layout = "legacy";
-	} else if (document.querySelector("#page-auction")) {
-		presenceData.details = "Playing Bidiots";
-		layout = "legacy";
-	} else if (document.querySelector("#page-bombintern")) {
-		presenceData.details = "Playing Bomb Corp";
-		layout = "legacy";
-
-		// PP1
-	} else if (document.querySelector("#page-ydkj2015")) {
-		presenceData.details = "Playing You Don't Know Jack 2015";
-		layout = "legacy";
-	} else if (document.querySelector("#page-drawful")) {
-		presenceData.details = "Playing Drawful 1/2";
-		layout = "legacy";
-	} else if (document.querySelector("#page-wordspud")) {
-		presenceData.details = "Playing Word Spud";
-		layout = "legacy";
-	} else if (document.querySelector("#page-lieswatter")) {
-		presenceData.details = "Playing Lie Swatter";
-		layout = "legacy";
-
-		// Other
-	} else if (window.location.href.includes("games.jackbox.tv"))
-		presenceData.details = "Looking at a past game";
-	else presenceData.details = "Idle";
-
-	if (useName && layout === "new") {
-		presenceData.state = `as ${
-			document.querySelector("#playername").textContent
-		}`;
-	}
-
-	if (useName && layout === "legacy") {
-		presenceData.state = `as ${
-			document.querySelector("#player").children[0].textContent
-		}`;
-	}
-
-	if (useName && layout === "dict") {
-		presenceData.state = `as ${
-			document.querySelector("#playericon").className.split("_")[1]
-		}${document.querySelector("#playername").textContent.toLowerCase()}`;
-	}
-
-	if (useName && layout === "guessp") {
-		presenceData.state = `as ${
-			document.querySelector("#player").children[1].textContent
-		}`;
+	switch (window.location.hostname) {
+		case "jackbox.tv": {
+			let layout: LayoutVersion = LayoutVersion.New;
+			const {
+				layout: gamelayout,
+				name,
+				logo,
+			} = Games.find(game => document.querySelector(game.selector) !== null);
+			if (gamelayout) layout = gamelayout;
+			presenceData.details = `Playing ${name}`;
+			presenceData.largeImageKey = logo;
+			if (useName) {
+				switch (layout) {
+					case LayoutVersion.New: {
+						presenceData.state = `as ${
+							document.querySelector("#playername").textContent
+						}`;
+						break;
+					}
+					case LayoutVersion.Legacy: {
+						presenceData.state = `as ${
+							document.querySelector("#player").children[0].textContent
+						}`;
+						break;
+					}
+					case LayoutVersion.Dictionarium: {
+						presenceData.state = `as ${
+							document.querySelector("#playericon").className.split("_")[1]
+						}${document
+							.querySelector("#playername")
+							.textContent.toLowerCase()}`;
+						break;
+					}
+					case LayoutVersion.Guesspionage: {
+						presenceData.state = `as ${
+							document.querySelector("#player").children[1].textContent
+						}`;
+						break;
+					}
+				}
+			}
+			if (useDetails) {
+				// TODO: Add game details...
+			}
+			break;
+		}
+		case "games.jackbox.tv": {
+			presenceData.details = "Looking at a past game";
+			break;
+		}
+		case "www.jackboxgames.com": {
+			break;
+		}
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
