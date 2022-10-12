3796a960147c5497e97870a94b89b45a749667f2
João Fonsaka <joaofonsaka@outlook.com>
2022-10-12 20:14:12 -0300

    feat: add chat and modals

--- websites/Y/YNOproject/presence.ts
+++ websites/Y/YNOproject/presence.ts
@@ -10,7 +10,52 @@ presence.on("UpdateData", async () => {
 		location = document.querySelector("#locationText");
 
 	if (titleInfo.length > 1 && titleInfo[1] === "YNOproject" ? true : false) {
-		presenceData.details = `Dreaming on ${titleInfo[0]}`;
+		switch (
+			document.querySelector("#modalContainer .modal:not(.hidden) .modalTitle")
+				?.textContent
+		) {
+			case "Settings":
+				presenceData.details = `Changing settings for ${titleInfo[0]}`;
+				break;
+			case "Rankings":
+				presenceData.details = `Viewing rankings for ${titleInfo[0]}`;
+				break;
+			case "Badges":
+				presenceData.details = `Viewing badges for ${titleInfo[0]}`;
+				break;
+			case "Expeditions":
+				presenceData.details = `Viewing expeditions for ${titleInfo[0]}`;
+				break;
+			case "UI Theme":
+				presenceData.details = `Changing UI theme for ${titleInfo[0]}`;
+				break;
+			case "Log In":
+				presenceData.details = `Logging in to ${titleInfo[0]}`;
+				break;
+			default:
+				presenceData.details = `Dreaming on ${titleInfo[0]}`;
+				break;
+		}
+
+		if (
+			document.querySelector("#chatInput:focus") &&
+			document.querySelector("#playerCountLabel")
+		) {
+			const playerCount = Math.max(
+				0,
+				parseInt(
+					document.querySelector("#playerCountLabel").textContent.split(" ")[0]
+				) - 1
+			);
+			presenceData.details = `Chatting ${
+				playerCount === 0
+					? "alone"
+					: playerCount === 1
+					? "with 1 player"
+					: `with ${playerCount} players`
+			} on ${titleInfo[0]}`;
+		}
+
 		presenceData.smallImageKey = `https://${
 			document.location.hostname
 		}/images/door_${document.location.href.split("/")[3]}.gif`;
