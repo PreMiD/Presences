6ca5e084a727d93971ad1ff364566bb97370cb6f
theusaf <theusafyt@gmail.com>
2022-10-12 18:14:34 -0700

    feat: add fakin' it

--- websites/J/Jackbox/presence.ts
+++ websites/J/Jackbox/presence.ts
@@ -622,6 +622,30 @@ presence.on("UpdateData", async () => {
 							break;
 						}
 						case Games.FakinIt: {
+							const currentGamePage = document.querySelector<HTMLDivElement>(
+									".fakinit-page:not(.pt-page-off)"
+								),
+								{ classList } = currentGamePage;
+							if (classList.contains("state-lobby")) {
+								presenceData.state = "Waiting in lobby";
+							} else if (classList.contains("state-nothing")) {
+								presenceData.state = "Waiting";
+							} else if (classList.contains("state-gameplay")) {
+								presenceData.state = "Following instructions";
+							} else if (classList.contains("state-skip-instructions")) {
+								presenceData.state = "Watching the tutorial";
+							} else if (classList.contains("state-categories")) {
+								presenceData.state = "Choosing a category";
+							} else if (classList.contains("state-notchoosing")) {
+								presenceData.state = "Waiting for a category to be chosen";
+							} else if (classList.contains("state-round")) {
+								presenceData.state =
+									currentGamePage.querySelector("p").textContent;
+							} else if (classList.contains("state-vote")) {
+								presenceData.state = "Voting on a player";
+							} else if (classList.contains("state-vote-locked")) {
+								presenceData.state = "Viewing vote results";
+							}
 							break;
 						}
 						case Games.Guesspionage: {
