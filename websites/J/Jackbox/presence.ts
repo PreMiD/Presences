6405689a923fd98f12b5d2aad6d55be2e8402c08
theusaf <theusafyt@gmail.com>
2022-10-13 16:35:29 -0700

    feat(Patently Stupid): add details

--- websites/J/Jackbox/presence.ts
+++ websites/J/Jackbox/presence.ts
@@ -1047,6 +1047,72 @@ presence.on("UpdateData", async () => {
 							break;
 						}
 						case Games.patentlystupid: {
+							switch (gamePlayerState.state) {
+								case "Lobby": {
+									presenceData.state = "Waiting in lobby";
+									break;
+								}
+								case "Logo": {
+									presenceData.state = "Waiting";
+									break;
+								}
+								case "MakeSingleChoice": {
+									const { html } = gamePlayerState.prompt as {
+										html: string;
+									};
+									if (html === "") {
+										presenceData.state = "Watching the tutorial";
+									} else if (
+										html ===
+										"<div>Choose an Issue</div><div>to base your invention on.</div>"
+									) {
+										presenceData.state = "Choosing an issue to work on";
+									} else if (
+										html ===
+										"<div>Present your idea!</div><div>It's your turn to show off your invention!<br /><br />Would you like to present your invention, or would you like the game to do it for you?</div>"
+									) {
+										presenceData.state = "Choosing presentation options";
+									} else if (
+										html ===
+										"<div>Present your idea!</div><div>You can control the timing!<br />(Show elements in any order.)</div>"
+									) {
+										presenceData.state = "Presenting their invention";
+									} else if (
+										html.startsWith("<div>Invest in the best!</div>")
+									) {
+										presenceData.state = "Investing in an invention";
+									} else if (
+										/choose an issue.*?to base your invention on/is.test(html)
+									) {
+										presenceData.state = "Choosing the final issue to work on";
+									}
+									break;
+								}
+								case "EnterSingleText": {
+									const { html } = gamePlayerState.prompt as {
+										html: string;
+									};
+									if (html.startsWith("<div>Fill in the Blank!</div>")) {
+										presenceData.state = "Creating a problem";
+									} else if (
+										html ===
+										"<div>Write a title</div><div>for your invention</div>"
+									) {
+										presenceData.state = "Naming their invention";
+									} else if (
+										html ===
+										"<div>Write a tagline</div><div>for your invention</div>"
+									) {
+										presenceData.state =
+											"Creating a tagline for their invention";
+									}
+									break;
+								}
+								case "Draw": {
+									presenceData.state = "Drawing their invention";
+									break;
+								}
+							}
 							break;
 						}
 						case Games.rapbattle: {
