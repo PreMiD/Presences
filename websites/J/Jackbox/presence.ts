3f2ed53cc11448f85a4586fd2876b8a75a19d116
theusaf <theusafyt@gmail.com>
2022-10-14 16:30:53 -0700

    feat(Job Job): add details

--- websites/J/Jackbox/presence.ts
+++ websites/J/Jackbox/presence.ts
@@ -1961,6 +1961,40 @@ presence.on("UpdateData", async () => {
 							break;
 						}
 						case Games["apply-yourself"]: {
+							switch (gamePlayerState.kind) {
+								case "lobby": {
+									presenceData.state = "Waiting in lobby";
+									break;
+								}
+								case "Logo": {
+									presenceData.state = "Waiting";
+									break;
+								}
+								case "writing": {
+									presenceData.state = "Writing a response";
+									break;
+								}
+								case "voting": {
+									presenceData.state = "Voting for a response";
+									break;
+								}
+								case "magnets": {
+									presenceData.state = "Answering a prompt using magnets";
+									break;
+								}
+								case "resumagnets": {
+									presenceData.state = "Filling out their resumé using magnets";
+									break;
+								}
+								case "done": {
+									presenceData.state = "Waiting for others to finish";
+									break;
+								}
+								case "postGame": {
+									presenceData.state = "Viewing the results";
+									break;
+								}
+							}
 							break;
 						}
 						case Games["drawful-animate"]: {
