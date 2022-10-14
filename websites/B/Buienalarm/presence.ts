1733bd4106b1584f4faf3f5690427320f114257c
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-10-14 09:51:01 +0200

    chore(Buienalarm): remove console statement

--- websites/B/Buienalarm/presence.ts
+++ websites/B/Buienalarm/presence.ts
@@ -81,8 +81,6 @@ presence.on("UpdateData", async () => {
 		else presenceData.details = "Bekijkt de weersgrafiek voor";
 	}
 
-	console.log(presenceData);
-
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
