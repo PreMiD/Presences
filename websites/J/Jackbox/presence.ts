a777052900059734ed0e3daf40f6945706f95473
theusaf <theusafyt@gmail.com>
2022-10-14 09:22:57 -0700

    feat(Devils and the Details): add details

--- websites/J/Jackbox/presence.ts
+++ websites/J/Jackbox/presence.ts
@@ -1743,6 +1743,95 @@ presence.on("UpdateData", async () => {
 									presenceData.state = "Waiting";
 									break;
 								}
+								case "MakeSingleChoice": {
+									switch (gamePlayerState.choiceId) {
+										case "ChangeCharacter": {
+											presenceData.state = "Choosing a character";
+											break;
+										}
+										default: {
+											if (gamePlayerState.choiceType === "SkipIntro") {
+												presenceData.state = "Watching the intro";
+											} else if (
+												gamePlayerState.choiceType === "ShowTutorial"
+											) {
+												presenceData.state =
+													"Deciding if they want to watch the tutorial";
+											}
+										}
+									}
+									break;
+								}
+								case "CancelerMechanic": {
+									presenceData.state =
+										"Blocking a member from their selfish task";
+									break;
+								}
+								case "TaskList": {
+									presenceData.state = "Choosing a task";
+									break;
+								}
+								case "NothingMechanic": {
+									presenceData.state = "Doing nothing";
+									break;
+								}
+								case "ScrubMechanic": {
+									presenceData.state = "Scrubbing something";
+									break;
+								}
+								case "RotateMechanic": {
+									presenceData.state = "Rotating something";
+									break;
+								}
+								case "TapMechanic": {
+									presenceData.state = "Tapping something";
+									break;
+								}
+								case "SwipeMechanic": {
+									presenceData.state = "Swiping something";
+									break;
+								}
+								case "TravelMechanicParticipant": {
+									presenceData.state = "Travelling as a passenger";
+									break;
+								}
+								case "TravelMechanicCaptain": {
+									presenceData.state = "Travelling as the driver";
+									break;
+								}
+								case "PhoneMechanic": {
+									presenceData.state = "Calling someone";
+									break;
+								}
+								case "DialogMechanic": {
+									presenceData.state = "Talking to someone";
+									break;
+								}
+								case "SearchMechanicCaptain":
+								case "SearchMechanicParticipant": {
+									presenceData.state = "Searching for something";
+									break;
+								}
+								case "TextMechanicParticipant": {
+									presenceData.state = "Writing a text";
+									break;
+								}
+								case "TextMechanicCaptain": {
+									presenceData.state = "Reading a text";
+									break;
+								}
+								case "InstructionsMechanicCaptain": {
+									presenceData.state = "Following instructions";
+									break;
+								}
+								case "InstructionsMechanicParticipant": {
+									presenceData.state = "Giving instructions";
+									break;
+								}
+								case "HoldMechanic": {
+									presenceData.state = "Holding something";
+									break;
+								}
 							}
 							break;
 						}
