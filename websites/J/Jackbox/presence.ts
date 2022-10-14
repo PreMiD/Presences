7a2fc601c3b8fbccbc23608cd4120fe10f412885
theusaf <theusafyt@gmail.com>
2022-10-13 21:20:04 -0700

    feat(Role Models): add details

--- websites/J/Jackbox/presence.ts
+++ websites/J/Jackbox/presence.ts
@@ -1237,6 +1237,53 @@ presence.on("UpdateData", async () => {
 							break;
 						}
 						case Games.rolemodels: {
+							switch (gamePlayerState.state) {
+								case "Lobby": {
+									presenceData.state = "Waiting in lobby";
+									break;
+								}
+								case "Logo": {
+									presenceData.state = "Waiting";
+									break;
+								}
+								case "Camera": {
+									presenceData.state = "Taking a photo of themselves";
+									break;
+								}
+								case "Draw": {
+									presenceData.state = "Drawing a portrait of themselves";
+									break;
+								}
+								case "MakeSingleChoice": {
+									switch (gamePlayerState.choiceType) {
+										case "SkipTutorial": {
+											presenceData.state = "Watching the tutorial";
+											break;
+										}
+										case "Prompt": {
+											presenceData.state = "Choosing a category";
+											break;
+										}
+										case "TagResolution": {
+											presenceData.state = "Resolving a role conflict";
+											break;
+										}
+										case "RoleModelsChoice": {
+											presenceData.state = "Choosing the role that fits best";
+											break;
+										}
+									}
+									break;
+								}
+								case "Sortable": {
+									presenceData.state = "Assigning roles to players";
+									break;
+								}
+								case "EnterSingleText": {
+									presenceData.state = "Answering a prompt";
+									break;
+								}
+							}
 							break;
 						}
 						case Games.triviadeath2: {
