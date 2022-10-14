144199d110660bd8bce1c6a15360d8fc415c0725
theusaf <theusafyt@gmail.com>
2022-10-13 22:20:32 -0700

    feat(Push The Button): add details

--- websites/J/Jackbox/presence.ts
+++ websites/J/Jackbox/presence.ts
@@ -1183,6 +1183,89 @@ presence.on("UpdateData", async () => {
 						}
 						// Party Pack 6
 						case Games.pushthebutton: {
+							switch (gamePlayerState.state) {
+								case "Lobby": {
+									presenceData.state = "Waiting in lobby";
+									break;
+								}
+								case "Logo": {
+									presenceData.state = "Waiting";
+									break;
+								}
+								case "SuspicionVote": {
+									presenceData.state = "Voting on who seems suspicious";
+									break;
+								}
+								case "Probe": {
+									presenceData.state = "Probing their fellow players";
+									break;
+								}
+								case "Draw": {
+									presenceData.state = "In the Drawing Quarters";
+									break;
+								}
+								case "AnalyzePlayer": {
+									presenceData.state =
+										"Using the bioscanner - describing glyphs";
+									break;
+								}
+								case "AnalysisReport": {
+									presenceData.state = "Using the bioscanner - reading report";
+									break;
+								}
+								case "MakeSingleChoice": {
+									const choiceType = gamePlayerState.choiceType,
+										activeTest = (
+											gamePlayerState.gameInfo as { activeTest: string }
+										).activeTest;
+									if (choiceType) {
+										switch (gamePlayerState.choiceType) {
+											case "TutorialOptOut": {
+												presenceData.state =
+													"Choosing whether to skip the tutorial";
+												break;
+											}
+											case "ConfirmedIdentity": {
+												presenceData.state = "Confirming their identity";
+												break;
+											}
+										}
+									} else if (activeTest) {
+										if (activeTest === "Push The Button") {
+											presenceData.state = "Accusing players";
+										} else {
+											presenceData.state = `Taking the ${activeTest} test`;
+										}
+									} else {
+										const html = gamePlayerState.prompt?.html;
+										if (
+											html ===
+											"You are the Captain.  What test would you like to perform?"
+										) {
+											presenceData.state = "Choosing a test to perform";
+											break;
+										} else if (
+											/^Select <strong>.*?<\/strong> to Test!$/.test(html)
+										) {
+											presenceData.state = "Choosing players to test";
+											break;
+										}
+									}
+									break;
+								}
+								case "EnterSingleText": {
+									const activeTest = (
+										gamePlayerState.gameInfo as { activeTest: string }
+									).activeTest;
+									if (activeTest) {
+										presenceData.state = `Taking the ${activeTest} test`;
+									}
+									break;
+								}
+								case "GameOver": {
+									presenceData.state = `Viewing results - ${gamePlayerState.winningRole}s won`;
+								}
+							}
 							break;
 						}
 						case Games.jokeboat: {
