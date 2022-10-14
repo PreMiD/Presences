9f441b39f95b7b3d4aa6559e72def51e4a24fddb
sarcasternub <sarcasteryt@gmail.com>
2022-10-15 00:18:32 +0530

    fix(Sofi): change the switch-case to a record

--- websites/S/Sofi/presence.ts
+++ websites/S/Sofi/presence.ts
@@ -1,113 +1,130 @@
 const presence = new Presence({
 		clientId: "1028679580027977839",
 	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+	browsingTimestamp = Math.floor(Date.now() / 1000),
+	content = {
+		viewingImage: "https://i.imgur.com/9WPWqvT.png",
+		buyingImage: "https://i.imgur.com/JKLNlvT.png",
+		viewingImageText: "Viewing...",
+		buyingImageText: "Buying...",
+	};
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/IlUpO2s.png",
-			startTimestamp: browsingTimestamp,
-		},
-		{ host, pathname, href } = document.location;
-
-	if (host === "sofi.gg") {
-		presenceData.details = "Viewing Homepage";
-		presenceData.smallImageKey = "https://i.imgur.com/9WPWqvT.png";
-		presenceData.smallImageText = "Viewing...";
-
-		switch (pathname) {
-			case "/backgrounds": {
-				presenceData.details = "Viewing Backgrounds";
-				presenceData.smallImageKey = "https://i.imgur.com/9WPWqvT.png";
-				presenceData.smallImageText = "Viewing...";
-				presenceData.buttons = [
+	let presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/IlUpO2s.png",
+		details: "Viewing Homepage",
+		smallImageKey: content.viewingImage,
+		smallImageText: content.viewingImageText,
+		startTimestamp: browsingTimestamp,
+	};
+	const { host, pathname, href } = document.location,
+		pages: Record<string, PresenceData> = {
+			"/backgrounds": {
+				details: "Viewing Backgrounds",
+				smallImageKey: content.viewingImage,
+				smallImageText: content.viewingImageText,
+				buttons: [
 					{
 						label: "View Backgrounds",
 						url: href,
 					},
-				];
-				break;
-			}
-			case "/profilebg": {
-				presenceData.details = "Viewing Profile Backgrounds";
-				presenceData.smallImageKey = "https://i.imgur.com/9WPWqvT.png";
-				presenceData.smallImageText = "Viewing...";
-				presenceData.buttons = [
+				],
+			},
+			"/profilebg": {
+				details: "Viewing Profile Backgrounds",
+				smallImageKey: content.viewingImage,
+				smallImageText: content.viewingImageText,
+				buttons: [
 					{
 						label: "View Profile Backgrounds",
 						url: href,
 					},
-				];
-				break;
-			}
-			case "/frames": {
-				presenceData.details = "Viewing Frames";
-				presenceData.smallImageKey = "https://i.imgur.com/9WPWqvT.png";
-				presenceData.smallImageText = "Viewing...";
-				presenceData.buttons = [
+				],
+			},
+			"/frames": {
+				details: "Viewing Frames",
+				smallImageKey: content.viewingImage,
+				smallImageText: content.viewingImageText,
+				buttons: [
 					{
 						label: "View Frames",
 						url: href,
 					},
-				];
-				break;
-			}
-			case "/items": {
-				presenceData.details = "Viewing Items";
-				presenceData.smallImageKey = "https://i.imgur.com/9WPWqvT.png";
-				presenceData.smallImageText = "Viewing...";
-				presenceData.buttons = [
+				],
+			},
+			"/items": {
+				details: "Viewing Items",
+				smallImageKey: content.viewingImage,
+				smallImageText: content.viewingImageText,
+				buttons: [
 					{
 						label: "View Items",
 						url: href,
 					},
-				];
-				break;
-			}
-			case "/bump": {
-				presenceData.details = "Bumping";
-				presenceData.buttons = [
+				],
+			},
+			"/bump": {
+				details: "Bumping",
+				smallImageKey: null,
+				smallImageText: null,
+				buttons: [
 					{
 						label: "Wanna Bump?",
 						url: href,
 					},
-				];
-				break;
-			}
-			case "/team": {
-				presenceData.details = "Viewing Team";
-				presenceData.smallImageKey = "https://i.imgur.com/9WPWqvT.png";
-				presenceData.smallImageText = "Viewing...";
-				presenceData.buttons = [
+				],
+			},
+			"/team": {
+				details: "Viewing Team",
+				smallImageKey: content.viewingImage,
+				smallImageText: content.viewingImageText,
+				buttons: [
 					{
 						label: "View Sofi Team",
 						url: href,
 					},
-				];
-				break;
+				],
+			},
+			"/profile/settings": { details: "Viewing profile settings" },
+			"/vtc": { details: "Viewing the VTC center" },
+			"/vtc/search": { details: "Searching for a VTC" },
+			"/vtc/create": { details: "Creating a VTC" },
+			"/blog": { details: "Browsing the blog" },
+			"/events": { details: "Viewing the events system" },
+			"/events/manage": { details: "Managing their events" },
+			"/events/manage/past": { details: "Viewing their past events" },
+			"/events/create": { details: "Creating an event" },
+			"/events/search": { details: "Searching for an event" },
+			"/api": { details: "Viewing the API" },
+			"/live": { details: "Viewing the live stats" },
+			"/history": { details: "Viewing the history" },
+			"/settings": { details: "Viewing the settings" },
+		};
+
+	if (host === "sofi.gg") {
+		if (pathname.startsWith("/glows")) {
+			const data = document.querySelector("div.flex.text-xl.lg\\:text-2xl");
+
+			presenceData.details = "Viewing Glows";
+			presenceData.buttons = [
+				{
+					label: "View Glows",
+					url: href,
+				},
+			];
+
+			if (data) {
+				const userTag = data.textContent?.match(/.*#[0-9]+/gi);
+				userTag ? (presenceData.state = `Of ${userTag[0]}`) : null;
 			}
-			default:
-				if (pathname.startsWith("/glows")) {
-					const data = document.querySelector("div.flex.text-xl.lg\\:text-2xl");
-
-					presenceData.details = "Viewing Glows";
-					presenceData.buttons = [
-						{
-							label: "View Glows",
-							url: href,
-						},
-					];
-
-					if (data) {
-						const userTag = data.textContent?.match(/.*#[0-9]+/gi);
-						userTag ? (presenceData.state = `Of ${userTag[0]}`) : null;
-					}
-				}
 		}
+
+		for (const [path, data] of Object.entries(pages))
+			if (pathname.includes(path)) presenceData = { ...presenceData, ...data };
 	} else if (host === "gems.sofi.gg") {
 		presenceData.details = "Buying Gems";
-		presenceData.smallImageKey = "https://i.imgur.com/JKLNlvT.png";
-		presenceData.smallImageText = "Buying...";
+		presenceData.smallImageKey = content.buyingImage;
+		presenceData.smallImageText = content.buyingImageText;
 		presenceData.buttons = [
 			{
 				label: "Buy Gems",
