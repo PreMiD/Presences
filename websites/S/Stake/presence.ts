b38243be13d413d318e08148e9a08ffa2b682af2
RootK1d <irgendjemand31@gmail.com>
2022-10-15 16:06:27 +0200

    test(Stake): fixed lint issues

--- websites/S/Stake/presence.ts
+++ websites/S/Stake/presence.ts
@@ -1,6 +1,6 @@
 const presence = new Presence({
-		clientId: "1030599970551771266",
-	}),
+	clientId: "1030599970551771266",
+}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
@@ -11,14 +11,11 @@ presence.on("UpdateData", async () => {
 		presence.getSetting<boolean>("showTimestamp"),
 		presence.getSetting<boolean>("showCurrentGame"),
 	]),
+		presenceData: PresenceData = {
+			largeImageKey: "logo",
+		};
 
-	 presenceData: PresenceData = {
-		largeImageKey: "logo",
-	};
-
-	if (showTimestamp) 
-		presenceData.startTimestamp = browsingTimestamp;
-	
+	if (showTimestamp) presenceData.startTimestamp = browsingTimestamp;
 
 	if (
 		window.location.pathname.includes("casino") &&
@@ -27,21 +24,16 @@ presence.on("UpdateData", async () => {
 		//Browsing Casino / Games
 
 		if (window.location.pathname.includes("group")) {
-			
-			
-			presenceData.state = `Browsing ${window.location.pathname.split("/group/").pop().replaceAll("-", " ").replace(
-				/(^\w{1})|(\s+\w{1})/g,
-				letter => letter.toUpperCase()
-			)}...`;
-		} else 
-			presenceData.state = "Browsing Casino...";
-		
+			presenceData.state = `Browsing ${window.location.pathname
+				.split("/group/")
+				.pop()
+				.replaceAll("-", " ")
+				.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase())}...`;
+		} else presenceData.state = "Browsing Casino...";
 	} else if (window.location.pathname.includes("games")) {
 		//Playing Casino Game
 
 		if (showCurrentGame) {
-			
-
 			presenceData.state = `Playing ${document
 				.querySelector("div.title-wrap > h1")
 				.textContent.trim()}`;
@@ -57,9 +49,7 @@ presence.on("UpdateData", async () => {
 		//Browsing Sports
 
 		presenceData.state = "Browsing Sports...";
-	} else 
-		presenceData.state = "Browsing...";
-	
+	} else presenceData.state = "Browsing...";
 
 	if (showBal) {
 		//Display Balance
@@ -67,14 +57,13 @@ presence.on("UpdateData", async () => {
 		const balance = document
 			.querySelector("div.currency > span.content > span")
 			.textContent.trim(),
+			//Get Currency
 
-		//Get Currency
-
-		 currency = document
-			.querySelector("div.currency > span.variant-subtle > svg > use")
-			.getAttributeNS("http://www.w3.org/1999/xlink", "href")
-			.replace("#icon-currency-", "")
-			.toUpperCase();
+			currency = document
+				.querySelector("div.currency > span.variant-subtle > svg > use")
+				.getAttributeNS("http://www.w3.org/1999/xlink", "href")
+				.replace("#icon-currency-", "")
+				.toUpperCase();
 
 		if (window.location.pathname.includes("games")) {
 			//Balance In Game (Subject To Change)
@@ -83,28 +72,25 @@ presence.on("UpdateData", async () => {
 		} else {
 			//Stake Account Balance
 
-			if (balance.includes(",")) 
+			if (balance.includes(","))
 				presenceData.details = `Balance: ${balance} (${currency})`;
-			 else 
-				presenceData.details = `Balance: ${balance} ${currency}`;
-			
+			else presenceData.details = `Balance: ${balance} ${currency}`;
 		}
 
 		//Browsing Modals and Subsites
 
-		if (window.location.search.includes("modal=wallet")) 
+		if (window.location.search.includes("modal=wallet"))
 			presenceData.state = "Checking Wallet...";
-		 else if (window.location.search.includes("modal=vault")) 
+		else if (window.location.search.includes("modal=vault"))
 			presenceData.state = "Checking Vault...";
-		 else if (window.location.search.includes("modal=vip")) 
+		else if (window.location.search.includes("modal=vip"))
 			presenceData.state = "Checking VIP Progress...";
-		 else if (window.location.search.includes("modal=user")) 
+		else if (window.location.search.includes("modal=user"))
 			presenceData.state = "Checking Statistics...";
-		 else if (window.location.pathname.includes("/transactions/")) 
+		else if (window.location.pathname.includes("/transactions/"))
 			presenceData.state = "Viewing Transactions...";
-		 else if (window.location.pathname.includes("/settings/")) 
+		else if (window.location.pathname.includes("/settings/"))
 			presenceData.state = "Adjusting Settings...";
-		
 	}
 
 	//Update Data
