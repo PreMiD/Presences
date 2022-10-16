81a397b30bcc430321bec0fee97a0bd008060228
Kozou <louisfloquey@gmail.com>
2022-10-16 16:06:18 +0200

    feat(MyCanal): complete presence recode

--- websites/M/myCANAL/presence.ts
+++ websites/M/myCANAL/presence.ts
@@ -6,62 +6,111 @@ const presence = new Presence({
 		pause: "general.paused",
 		live: "general.live",
 	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
-let title;
+	browsingTimestamp = Math.floor(Date.now() / 1000),
+	containsTerm = (term: string) => {
+		if (document.location.pathname.includes(term))
+			return document.location.pathname;
+	};
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			largeImageKey: "logo",
 		},
+		video = document.querySelector<HTMLVideoElement>(".iIZX3IGkM2eBzzWle1QQ"),
 		showCover = await presence.getSetting<boolean>("cover"),
-		video: HTMLVideoElement = document.querySelector(".aPWk0-TaQEzvggxIT6qvP");
-	if (video && !isNaN(video.duration)) {
-		if (!document.querySelector("._3uUpH58Juk_Qbizq6j5ThG")) {
-			if (document.location.pathname.includes("/live/")) {
-				title = document.querySelector("._3tdt8zwgvMCJ6v_sElXneQ").textContent;
-				presenceData.smallImageKey = "live";
-				presenceData.smallImageText = (await strings).live;
-				presenceData.startTimestamp = browsingTimestamp;
-			} else {
-				title = `${
-					document.querySelector("._3tdt8zwgvMCJ6v_sElXneQ")?.textContent
-				} ${document.querySelector("._3pyJlyeeH9KBKeFd4nFAmt")?.textContent}`;
+		mainTitle = document.querySelector(".bodyTitle___HwRP2");
 
+	switch (document.location.pathname) {
+		case "/mes-videos/":
+			presenceData.state = "Mes Vidéos";
+			break;
+		case "/chaines/":
+			presenceData.state = "Chaînes";
+			break;
+		case "/programme-tv/":
+			presenceData.state = "Programme TV";
+			break;
+		case "/cinema/":
+			presenceData.state = "Films";
+			break;
+		case "/series/":
+			presenceData.state = "Séries";
+			break;
+		case "/live/":
+			presenceData.state = "Chaînes en direct";
+			break;
+	}
+
+	if (video && !isNaN(video.duration)) {
+		const titleTvShows = document.querySelectorAll(".MGrm26svmXpUhj6dfbGN"),
+			params = new URLSearchParams(window.location.search);
+		let channelID = params.get("channel");
+		switch (document.location.pathname) {
+			case containsTerm("live"):
+				channelID = `${channelID.charAt(0)} ${channelID.substring(1)}`;
+				presenceData.details = presenceData.details = document.querySelector(
+					".A6AH2oNkXUuOKJN5IYrL"
+				).textContent;
+				presenceData.state = `sur ${
+					document.querySelector<HTMLImageElement>(
+						`#\\3${channelID} > a > div > div > div > div > div > img`
+					).alt
+				}`;
+				[presenceData.startTimestamp, presenceData.endTimestamp] =
+					presence.getTimestamps(video.currentTime, video.duration);
+				presenceData.largeImageKey = showCover
+					? document.querySelector<HTMLImageElement>(
+							`#\\3${channelID} > a > div > div > div > div > div > img`
+					  ).src
+					: "logo";
+				presenceData.smallImageKey = "live";
+				presenceData.smallImageText = "En direct";
+				delete presenceData.startTimestamp;
+				delete presenceData.endTimestamp;
+				presenceData.startTimestamp = browsingTimestamp;
+				break;
+			case containsTerm("cinema"):
+				presenceData.details = document.querySelector(
+					".A6AH2oNkXUuOKJN5IYrL"
+				).textContent;
+				[presenceData.startTimestamp, presenceData.endTimestamp] =
+					presence.getTimestamps(video.currentTime, video.duration);
+				presenceData.largeImageKey = showCover
+					? (presenceData.largeImageKey =
+							document.querySelector<HTMLMetaElement>(
+								"[property='og:image']"
+							)?.content)
+					: "logo";
 				presenceData.smallImageKey = video.paused ? "pause" : "play";
 				presenceData.smallImageText = video.paused
 					? (await strings).pause
 					: (await strings).play;
-				[, presenceData.endTimestamp] = presence.getTimestamps(
-					Math.floor(video.currentTime),
-					Math.floor(video.duration)
-				);
-
-				if (showCover) {
-					presenceData.largeImageKey =
-						document.querySelector<HTMLImageElement>(
-							"#binder_mediaCardHeader > div > img"
-						)?.src ||
-						document.querySelector<HTMLImageElement>(
-							"#episodeGrid-Épisode\\ 1\\ -\\ Les\\ Héritiers\\ du\\ Dragon_onclick_toFocus > div.poster___Cbuef.card__poster___cb98P.poster--dark___UyYiC > img"
-						)?.src;
-				}
-			}
-			const subtitle = document.querySelector(
-				"._39WJKEhrSYo7ftwMlFjZtA  ._3tdt8zwgvMCJ6v_sElXneQ"
-			).textContent;
-			presenceData.details = title;
-			presenceData.state = subtitle;
-
-			if (video.paused) {
-				delete presenceData.startTimestamp;
-				delete presenceData.endTimestamp;
-			}
-
-			if (title && subtitle) presence.setActivity(presenceData, !video.paused);
-		} else {
-			(presenceData.details = "Watching an Ad"),
-				presence.setActivity(presenceData);
+				break;
+			case containsTerm("series"):
+				presenceData.details = titleTvShows[0].textContent.trim();
+				presenceData.state = titleTvShows[1].textContent.trim();
+				[presenceData.startTimestamp, presenceData.endTimestamp] =
+					presence.getTimestamps(video.currentTime, video.duration);
+				presenceData.largeImageKey = showCover
+					? (presenceData.largeImageKey =
+							document.querySelector<HTMLMetaElement>(
+								"[property='og:image']"
+							)?.content)
+					: "logo";
+				presenceData.smallImageKey = video.paused ? "pause" : "play";
+				presenceData.smallImageText = video.paused
+					? (await strings).pause
+					: (await strings).play;
+				break;
 		}
-	} else
-		(presenceData.details = "Browsing..."), presence.setActivity(presenceData);
+		if (video.paused) {
+			delete presenceData.startTimestamp;
+			delete presenceData.endTimestamp;
+		}
+	} else if (mainTitle) {
+		presenceData.details = "Regarde...";
+		presenceData.state = mainTitle.textContent;
+	} else presenceData.details = "Navigue...";
+
+	presence.setActivity(presenceData);
 });
