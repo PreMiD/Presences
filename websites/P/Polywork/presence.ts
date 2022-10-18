acabeb3b13160579d8e5c317435b44c10b064abd
Rhys <rhyss.rakoff@gmail.com>
2022-10-19 00:43:04 +0100

    style: fix format

--- websites/P/Polywork/presence.ts
+++ websites/P/Polywork/presence.ts
@@ -1,45 +1,41 @@
 const presence = new Presence({
 		clientId: "1021098794894499952",
 	}),
-
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-	presence.on("UpdateData", async () => {
-		const presenceData: PresenceData = {
-				largeImageKey: "https://i.imgur.com/8KLMTMQ.png",
-				details: "Browsing..",
-				startTimestamp: browsingTimestamp,
-			},
-			{ pathname, hostname, href } = document.location;
-		
-			if (hostname === "polywork.com") {
-				presenceData.details = "Home page";
-				presenceData.largeImageKey = "polywork";
-			if (pathname.includes("/feeds/")) {
-					presenceData.details = "Read are news feeds";
-					presenceData.state = document.title.split("|")[0];
-					presenceData.buttons = [
-						{
-							label: "Feed",
-							url: href,
-						},
-					];
-				}
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+			largeImageKey: "https://i.imgur.com/8KLMTMQ.png",
+			details: "Browsing..",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname, hostname, href } = document.location,
+		{ title } = document;
 
-				else if (document.location.href.includes("/collaborate/")) {
-				presenceData.details = "Looking for collaborators";
-				presenceData.state = document.title.split("|")[0];
-				presenceData.buttons = [
-					{
-						label: "Collaborate",
-						url: href,
-					},
-				];
-			}
-		
-			else if (href.includes("/clubs/")) {
+	if (hostname === "polywork.com") {
+		presenceData.details = "Home page";
+		presenceData.largeImageKey = "polywork";
+		if (pathname.includes("/feeds/")) {
+			presenceData.details = "Read are news feeds";
+			presenceData.state = title.split("|")[0];
+			presenceData.buttons = [
+				{
+					label: "Feed",
+					url: href,
+				},
+			];
+		} else if (href.includes("/collaborate/")) {
+			presenceData.details = "Looking for collaborators";
+			presenceData.state = title.split("|")[0];
+			presenceData.buttons = [
+				{
+					label: "Collaborate",
+					url: href,
+				},
+			];
+		} else if (href.includes("/clubs/")) {
 			presenceData.details = "Looking for clubs";
-			presenceData.state = document.title.split("|")[0];
+			presenceData.state = title.split("|")[0];
 			presenceData.buttons = [
 				{
 					label: "Clubs",
@@ -50,8 +46,5 @@ const presence = new Presence({
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
-	else {
-		presence.setTrayTitle();
-		presence.setActivity();
-	}
+	else presence.setActivity();
 });
