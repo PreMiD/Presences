6448f21ffe2de48ca6aa5e14ef9ddafa36d1e519
Valley152 <53654126+Valley152@users.noreply.github.com>
2022-10-18 20:57:39 -0600

    Replace `pagee` with `page`
    Signed-off-by: Valley152 <53654126+Valley152@users.noreply.github.com>

--- websites/L/LambdaGeneration/presence.ts
+++ websites/L/LambdaGeneration/presence.ts
@@ -17,69 +17,69 @@ presence.on("UpdateData", async () => {
 			["csgo"]: "CS:GO",
 			["l4d"]: "Left 4 Dead",
 		},
-		pagee: string[] = ["Viewing", "trending"];
+		page: string[] = ["Viewing", "trending"];
 	if (window.location.hostname.includes("lambdabuilds")) {
 		if (splitted[0]) {
-			pagee[0] += " competition:";
-			pagee[1] = document.querySelectorAll("h1.Title")[0].innerHTML;
+			page[0] += " competition:";
+			page[1] = document.querySelectorAll("h1.Title")[0].innerHTML;
 		} else {
-			pagee[0] += ":";
-			pagee[1] = "competitions";
+			page[0] += ":";
+			page[1] = "competitions";
 		}
 	} else {
 		switch (splitted.length) {
 			case 1: {
-				pagee[0] += ":";
-				if (window.location.hostname.includes("lft")) pagee[1] = "LFTs";
+				page[0] += ":";
+				if (window.location.hostname.includes("lft")) page[1] = "LFTs";
 				else if (subComms[splitted[0]])
-					pagee[1] += ` in ${subComms[splitted[0]]}`;
-				else if (splitted[0]) pagee[1] = splitted[0];
+					page[1] += ` in ${subComms[splitted[0]]}`;
+				else if (splitted[0]) page[1] = splitted[0];
 				break;
 			}
 
 			case 2:
 			case 3: {
 				if (splitted[0] === "user") {
-					pagee[0] += " user:";
-					pagee[1] = document.querySelectorAll(
+					page[0] += " user:";
+					page[1] = document.querySelectorAll(
 						".PROFILE_NAME.Title"
 					)[0].innerHTML;
 					break;
 				} else if (splitted[1] === "post") {
-					pagee[0] += " post by:";
-					pagee[1] = document.querySelectorAll(
+					page[0] += " post by:";
+					page[1] = document.querySelectorAll(
 						".TEXT_CLIP.POST_DISPLAY_NAME.AuthorName"
 					)[0].innerHTML;
 					break;
 				} else if (splitted[0] === "search") {
-					pagee[0] = "Searching for";
-					if (splitted[2] === "users") pagee[0] += " user";
+					page[0] = "Searching for";
+					if (splitted[2] === "users") page[0] += " user";
 
-					pagee[0] += ":";
-					pagee[1] = document.querySelectorAll(".Title")[0].innerHTML;
+					page[0] += ":";
+					page[1] = document.querySelectorAll(".Title")[0].innerHTML;
 					break;
 				} else if (
 					splitted[0].includes("image") ||
 					splitted[0].includes("video") ||
 					splitted[1].includes("img")
 				) {
-					pagee[0] += " directly:";
-					pagee[1] = "media";
+					page[0] += " directly:";
+					page[1] = "media";
 					break;
 				} else if (subComms[splitted[0]]) {
-					pagee[0] += ":";
+					page[0] += ":";
 					if (!splitted[2])
-						pagee[1] = `${splitted[1]} in ${subComms[splitted[0]]}`;
+						page[1] = `${splitted[1]} in ${subComms[splitted[0]]}`;
 					else {
-						pagee[1] = `${splitted[2]} ${splitted[1]} in ${
+						page[1] = `${splitted[2]} ${splitted[1]} in ${
 							subComms[splitted[0]]
 						}`;
 					}
 
 					break;
 				}
-				pagee[0] += ":";
-				pagee[1] = splitted[0];
+				page[0] += ":";
+				page[1] = splitted[0];
 				break;
 			}
 		}
@@ -87,8 +87,8 @@ presence.on("UpdateData", async () => {
 
 	const presenceData: PresenceData = {
 		largeImageKey: "https://i.imgur.com/XSPtxro.png",
-		details: pagee[0],
-		state: pagee[1],
+		details: page[0],
+		state: page[1],
 	};
 
 	if (presenceData.details) presence.setActivity(presenceData);
