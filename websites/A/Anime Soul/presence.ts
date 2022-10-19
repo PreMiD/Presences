9cfdc775b36838f8c295ed6b806c54c37b03df64
theusaf <theusafyt@gmail.com>
2022-10-18 21:50:38 -0700

    refactor: use static page convention

--- websites/A/Anime Soul/presence.ts
+++ websites/A/Anime Soul/presence.ts
@@ -2,25 +2,25 @@ const presence = new Presence({
 		clientId: "672156210627084328",
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000),
-	staticPages: Record<string, string> = {
-		"": "Viewing home page",
-		"staff-list": "Viewing Staff List",
-		rules: "Viewing the rules",
-		appeals: "Viewing Appeals",
-		creators: "Viewing AS Creators",
-		leaderboards: "Viewing Leaderboards",
-		premium: "Viewing Premium",
+	staticPages: Record<string, PresenceData> = {
+		"/": { details: "Viewing home page" },
+		"/staff-list": { details: "Viewing Staff List" },
+		rules: { details: "Viewing the rules" },
+		appeals: { details: "Viewing Appeals" },
+		creators: { details: "Viewing AS Creators" },
+		leaderboards: { details: "Viewing Leaderboards" },
+		premium: { details: "Viewing Premium" },
 	};
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/fd4DHxK.png",
-			startTimestamp: browsingTimestamp,
-		},
-		{ pathname } = window.location,
-		path = pathname.split("/")[1] ?? "";
+	let presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/fd4DHxK.png",
+		startTimestamp: browsingTimestamp,
+	};
+	const { pathname } = window.location;
 
-	if (staticPages[path]) presenceData.details = staticPages[path];
+	for (const [path, data] of Object.entries(staticPages))
+		if (pathname.startsWith(path)) presenceData = { ...presenceData, ...data };
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
