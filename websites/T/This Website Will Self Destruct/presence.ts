f955baa4ffdecbce64cb3f1fed12d204c63062b5
theusaf <theusafyt@gmail.com>
2022-10-19 12:13:52 -0700

    refactor: remove unneeded text trimmer
    The extension already does this

--- websites/T/This Website Will Self Destruct/presence.ts
+++ websites/T/This Website Will Self Destruct/presence.ts
@@ -2,11 +2,6 @@ const presence = new Presence({
 	clientId: "1031628776209141892",
 });
 
-function formatText(message: string) {
-	if (message.length > 127) message = `${message.substring(0, 124)}...`;
-	return message;
-}
-
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			largeImageKey: "https://i.imgur.com/fpqx5Mn.png",
@@ -23,27 +18,26 @@ presence.on("UpdateData", async () => {
 				document.querySelector<HTMLTextAreaElement>("#letter-textarea"),
 			messageContainer = document.querySelector<HTMLDivElement>("#message");
 
-		if (!isNaN(remainingSeconds))
+		if (!isNaN(remainingSeconds)) {
 			presenceData.endTimestamp =
 				Math.floor(Date.now() / 1000) + remainingSeconds;
+		}
 
 		if (document.querySelector<HTMLDivElement>("#report"))
 			presenceData.details = "Reporting a letter";
 		else if (letterInput === document.activeElement) {
 			presenceData.details = "Writing a letter";
-			if (!privacyMode) presenceData.state = formatText(letterInput.value);
+			if (!privacyMode) presenceData.state = letterInput.value;
 		} else if (
 			document
 				.querySelector<HTMLAnchorElement>("#message > div > p:last-child > a")
 				?.href.startsWith("mailto:F")
 		) {
 			presenceData.details = "Reading main letter";
-			if (!privacyMode)
-				presenceData.state = formatText(messageContainer.textContent);
+			if (!privacyMode) presenceData.state = messageContainer.textContent;
 		} else {
 			presenceData.details = "Reading a letter";
-			if (!privacyMode)
-				presenceData.state = formatText(messageContainer.textContent);
+			if (!privacyMode) presenceData.state = messageContainer.textContent;
 		}
 	} else if (pathname.startsWith("/privacy"))
 		presenceData.details = "Reading the privacy policy";
