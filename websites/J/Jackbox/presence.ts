9c7d954accba26774976f859b8a570727e708952
theusaf <theusafyt@gmail.com>
2022-10-23 11:34:46 -0700

    refactor: create type definition file

--- websites/J/Jackbox/presence.ts
+++ websites/J/Jackbox/presence.ts
@@ -4,48 +4,6 @@ const presence = new Presence({
 	clientId: "638118757453004820",
 });
 
-interface Game {
-	name: string;
-	logo: string;
-	getPresenceData: (
-		params: GameCallbackParams
-	) => Promise<PresenceData> | PresenceData;
-}
-
-interface GamePlayerState {
-	playerName?: string;
-	username?: string;
-	playerInfo?: {
-		username?: string;
-	};
-	state?: string;
-	status?: string;
-	kind?: string;
-	category?: string;
-	prompt?: {
-		text?: string;
-		html?: string;
-	};
-	entryId?: string;
-	choiceId?: string;
-	responseKey?: string;
-	placeholder?: string;
-	choiceType?: string;
-	classes?: string[];
-	[x: string]: unknown;
-}
-
-interface GameInfoState {
-	name?: string;
-}
-
-interface GameCallbackParams {
-	playerState: GamePlayerState;
-	infoState: GameInfoState;
-	tag: string;
-	presence: Presence;
-}
-
 let gamePlayerState: GamePlayerState = {
 		playerName: null,
 		state: null,
