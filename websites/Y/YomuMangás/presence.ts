1dda707966215d1163ab7abdf688d2b2b63e8e1c
Vitor Lach <vitu@yomumangas.com>
2022-10-24 08:31:38 +0200

    Add presence

---
+++ websites/Y/YomuMangás/presence.ts
@@ -0,0 +1,122 @@
+const presence = new Presence({ clientId: "869377195682983957" }), readingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+	const { pathname } = window.location,
+		presenceData: PresenceData = { largeImageKey: "logo" },
+		[images, manga, chapter, user, timestamp, adult, privacy] = await Promise.all([
+			presence.getSetting<boolean>("images"),
+			presence.getSetting<boolean>("manga"),
+			presence.getSetting<boolean>("chapter"),
+			presence.getSetting<boolean>("user"),
+			presence.getSetting<boolean>("timestamp"),
+			presence.getSetting<boolean>("adult"),
+			presence.getSetting<boolean>("privacy")
+		]);
+	
+	if (privacy) {
+		presenceData.details = "Criando um novo mundo!";
+		presenceData.state = "yomumangas.com"
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/manga")) {
+		const isReader = pathname.includes("/chapter");
+		const isAdult = document.getElementById("premid-adult")?.innerText === "true";
+		const mangaTitle = document.getElementById("premid-manga-title")?.innerText;
+		const mangaSlug = document.getElementById("premid-manga-slug")?.innerText;
+		const mangaId = document.getElementById("premid-manga-id")?.innerText;
+		const mangaImage = (document.getElementById("premid-manga-image") as HTMLAnchorElement)?.href;
+		const chapterNumber = document.getElementById("premid-chapter-number")?.innerText;
+		const chapterTitle = document.getElementById("premid-chapter-title")?.innerText;
+
+		if (images && !isAdult) presenceData.largeImageKey = mangaImage;
+		if (!manga || isAdult && !adult) presenceData.details = isReader ? "Lendo um Mangá" : "Página de um Mangá";
+		if (manga && (isAdult && adult || !isAdult)) presenceData.details = isReader ? `Lendo ${mangaTitle}` : `Página de ${mangaTitle}`;
+		if (chapter && isReader) presenceData.state = `Cap. ${chapterNumber}${chapterTitle ? ` - ${chapterTitle}` : ""}`;
+		if (!chapter && isReader) presenceData.state = "Lendo Capítulo";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+		if (manga && (isAdult && adult || !isAdult) && !(chapter && isReader)) presenceData.buttons = [{ label: "Ver Mangá", url: `https://yomumangas.com/manga/${mangaId}/${mangaSlug}` }];
+		if (manga && (isAdult && adult || !isAdult) && (chapter && isReader)) presenceData.buttons = [{ label: "Ver Mangá", url: `https://yomumangas.com/manga/${mangaId}/${mangaSlug}` }, { label: "Ver Capítulo", url: `https://yomumangas.com/manga/${mangaId}/${mangaSlug}/chapter/${chapterNumber}` }]
+	} else if (pathname.includes("/user")) {
+		const userName = document.getElementById("premid-user-name")?.innerText;
+		const userImage = (document.getElementById("premid-user-image") as HTMLAnchorElement)?.href;
+
+		presenceData.details = user ? `Perfil de ${userName}` : "Stalkeando Alguém";
+		presenceData.buttons = [{ label: "Ver Perfil", url: `https://yomumangas.com/user/${userName}` }];
+		if (images && user) presenceData.largeImageKey = userImage;
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/signup")) {
+		presenceData.details = "Registrando-se";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/recovery")) {
+		presenceData.details = "Recuperando a Conta";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/login")) {
+		presenceData.details = "Fazendo Login";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/404")) {
+		presenceData.details = "404";
+		presenceData.state = "Perdido no Mato"
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/tutorial")) {
+		presenceData.details = "Lendo o Tutorial";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/faq")) {
+		presenceData.details = "Lendo o FAQ";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/terms-of-use")) {
+		presenceData.details = "Lendo os Termos de Uso";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/privacy")) {
+		presenceData.details = "Lendo as Políticas de Privacidade";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/dmca")) {
+		presenceData.details = "Lendo o DMCA";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/search")) {
+		presenceData.details = "Busca Avançada";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/about")) {
+		presenceData.details = "Sobra a Yomu";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/donate")) {
+		presenceData.details = "Página de Doação";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/contact")) {
+		presenceData.details = "Página de Contato";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/ranks")) {
+		presenceData.details = "Página de Ranks";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/minigames")) {
+		presenceData.details = "Página de Minigames";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/donators")) {
+		presenceData.details = "Página de Doadores";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/staffs")) {
+		presenceData.details = "Página da Equipe";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/partners")) {
+		presenceData.details = "Página de Parceiros";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/settings")) {
+		presenceData.details = "Página de Configurações";
+		if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	} else if (pathname.includes("/admin")) { 
+		const isStaff = document.getElementById("premid-admin-panel")?.innerText;
+
+		if (!isStaff) {
+			presenceData.details = "404";
+			presenceData.state = "Perdido no Mato"
+			if (timestamp) presenceData.startTimestamp = readingTimestamp;
+		} else {
+			presenceData.details = "Painel Admin";
+			presenceData.state = isStaff;
+            if (timestamp) presenceData.startTimestamp = readingTimestamp;
+		}
+	} else {
+		presenceData.details = "Página Inicial";
+        if (timestamp) presenceData.startTimestamp = readingTimestamp;
+	}
+
+	presence.setActivity(presenceData);
+});
