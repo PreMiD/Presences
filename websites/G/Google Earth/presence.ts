12b440571cc36b9894b79dfec4d2ce126ad2723e
theusaf <theusafyt@gmail.com>
2022-10-24 09:31:54 -0700

    feat: add basic location and card information

---
+++ websites/G/Google Earth/presence.ts
@@ -0,0 +1,48 @@
+const presence = new Presence({
+		clientId: "1034133591938060298",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+			largeImageKey: "https://i.imgur.com/bsN9g4H.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname } = window.location;
+
+	if (pathname === "/web/") {
+		presenceData.details = "Loading";
+	} else {
+		const appRoot = document.querySelector("earth-app").shadowRoot,
+			knowledgeCard = appRoot.querySelector("earth-knowledge-card");
+
+		if (knowledgeCard.getAttribute("hidden") !== "true") {
+			const mainCardRoot =
+				knowledgeCard.shadowRoot.querySelector("#top-card").shadowRoot;
+			presenceData.details = "Looking at a location";
+			presenceData.state = `${
+				mainCardRoot.querySelector<HTMLDivElement>("#title").textContent
+			} - ${
+				mainCardRoot.querySelector<HTMLDivElement>("#known-for").textContent
+			}`;
+		} else if (knowledgeCard.getAttribute("expanded") === "") {
+			const expandedCartRoot = document
+				.querySelector("earth-expanded-card")
+				.shadowRoot.querySelector("app-header-layout")
+				.querySelector<HTMLDivElement>("#card-content");
+			presenceData.details = "Viewing more information about a location";
+			presenceData.state = `${
+				expandedCartRoot.querySelector<HTMLDivElement>("#title").textContent
+			} - ${
+				expandedCartRoot.querySelector<HTMLDivElement>("#known-for").textContent
+			}`;
+		} else{
+			presenceData.details = "Looking at a point on the map";
+			presenceData.state = document
+				.querySelector<HTMLSpanElement>("#pointer-coordinates")
+				.textContent.trim();
+		}
+	}
+
+	presence.setActivity(presenceData);
+});
