3098026ca64dcc85234efbc866b9842004637c93
Vitor Lach <vitu@yomumangas.com>
2022-10-25 21:49:38 +0200

    fix: using Record and privacy on top of config

--- websites/Y/YomuMangás/presence.ts
+++ websites/Y/YomuMangás/presence.ts
@@ -2,8 +2,8 @@ const presence = new Presence({ clientId: "869377195682983957" }),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = { largeImageKey: "logo" },
-		{ pathname } = window.location,
+	let presenceData: PresenceData = { largeImageKey: "logo" };
+	const { pathname, href } = window.location,
 		[images, manga, chapter, user, timestamp, adult, privacy] =
 			await Promise.all([
 				presence.getSetting<boolean>("images"),
@@ -13,7 +13,30 @@ presence.on("UpdateData", async () => {
 				presence.getSetting<boolean>("timestamp"),
 				presence.getSetting<boolean>("adult"),
 				presence.getSetting<boolean>("privacy"),
-			]);
+			]),
+		pages: Record<string, PresenceData> = {
+			"/": { details: "Página Inicial" },
+			"/signup": { details: "Registrando-se" },
+			"/login": { details: "Fazendo Login" },
+			"/recovery": { details: "Recuperando a Conta" },
+			"/404": { details: "Erro 404", state: "Perdido no Mato" },
+			"/500": { details: "Erro 500", state: "O barco afundou" },
+			"/tutorial": { details: "Lendo o Tutorial" },
+			"/faq": { details: "Lendo o FAQ" },
+			"/tos": { details: "Lendo os Termos de Uso" },
+			"/privacy": { details: "Lendo as Políticas de Privacidade" },
+			"/dmca": { details: "Lendo DMCA" },
+			"/search": { details: "Fazendo uma Busca Avançada" },
+			"/about": { details: "Lendo Sobre a Yomu" },
+			"/contact": { details: "Página de Contato" },
+			"/donate": { details: "Página de Doação" },
+			"/donators": { details: "Lista de Doadores" },
+			"/partners": { details: "Lista de Parceiros" },
+			"/staffs": { details: "Lista da Equipe" },
+			"/ranks": { details: "Página de Ranks" },
+			"/settings": { details: "Configurações" },
+			"/minigames": { details: "Página de Minigames" },
+		};
 
 	if (privacy) {
 		presenceData.details = "Criando um novo mundo!";
@@ -43,81 +66,27 @@ presence.on("UpdateData", async () => {
 				document.querySelector("#premid-chapter-number")?.textContent
 			}${chapterTitle ? ` - ${chapterTitle}` : ""}`;
 		}
+		if (manga && ((isAdult && adult) || !isAdult)) {
+			presenceData.buttons = [
+				{
+					label: "Ver Mangá",
+					url: !(chapter && isReader) ? href : href.split("/chapter")[0],
+				},
+				{ label: "Ver Capítulo", url: href },
+			];
+		}
 		if (!chapter && isReader) presenceData.state = "Lendo Capítulo";
-		if (manga && ((isAdult && adult) || !isAdult) && !(chapter && isReader)) {
-			presenceData.buttons = [
-				{
-					label: "Ver Mangá",
-					url: document.querySelector<HTMLAnchorElement>("#premid-manga-url")
-						?.href,
-				},
-			];
-		}
-		if (manga && ((isAdult && adult) || !isAdult) && chapter && isReader) {
-			presenceData.buttons = [
-				{
-					label: "Ver Mangá",
-					url: document.querySelector<HTMLAnchorElement>("#premid-manga-url")
-						?.href,
-				},
-				{
-					label: "Ver Capítulo",
-					url: document.querySelector<HTMLAnchorElement>("#premid-chapter-url")
-						?.href,
-				},
-			];
-		}
+		if (!(chapter && isReader)) delete presenceData.buttons[1];
 	} else if (pathname.includes("/user")) {
 		presenceData.details = user
 			? `Perfil de ${document.querySelector("#premid-user-name")?.textContent}`
 			: "Stalkeando Alguém";
-		presenceData.buttons = [
-			{
-				label: "Ver Perfil",
-				url: document.querySelector<HTMLAnchorElement>("#premid-user-url")
-					?.href,
-			},
-		];
+		presenceData.buttons = [{ label: "Ver Perfil", url: href }];
 		if (images && user) {
 			presenceData.largeImageKey =
 				document.querySelector<HTMLAnchorElement>("#premid-user-image")?.href;
 		}
-	} else if (pathname.includes("/signup"))
-		presenceData.details = "Registrando-se";
-	else if (pathname.includes("/recovery"))
-		presenceData.details = "Recuperando a Conta";
-	else if (pathname.includes("/login")) presenceData.details = "Fazendo Login";
-	else if (pathname.includes("/404")) {
-		presenceData.details = "404";
-		presenceData.state = "Perdido no Mato";
-	} else if (pathname.includes("/tutorial"))
-		presenceData.details = "Lendo o Tutorial";
-	else if (pathname.includes("/faq")) presenceData.details = "Lendo o FAQ";
-	else if (pathname.includes("/terms-of-use"))
-		presenceData.details = "Lendo os Termos de Uso";
-	else if (pathname.includes("/privacy"))
-		presenceData.details = "Lendo as Políticas de Privacidade";
-	else if (pathname.includes("/dmca")) presenceData.details = "Lendo o DMCA";
-	else if (pathname.includes("/search"))
-		presenceData.details = "Busca Avançada";
-	else if (pathname.includes("/about")) presenceData.details = "Sobra a Yomu";
-	else if (pathname.includes("/donate"))
-		presenceData.details = "Página de Doação";
-	else if (pathname.includes("/contact"))
-		presenceData.details = "Página de Contato";
-	else if (pathname.includes("/ranks"))
-		presenceData.details = "Página de Ranks";
-	else if (pathname.includes("/minigames"))
-		presenceData.details = "Página de Minigames";
-	else if (pathname.includes("/donators"))
-		presenceData.details = "Página de Doadores";
-	else if (pathname.includes("/staffs"))
-		presenceData.details = "Página da Equipe";
-	else if (pathname.includes("/partners"))
-		presenceData.details = "Página de Parceiros";
-	else if (pathname.includes("/settings"))
-		presenceData.details = "Página de Configurações";
-	else if (pathname.includes("/admin")) {
+	} else if (pathname.includes("/admin")) {
 		const isStaff = document.querySelector("#premid-admin-panel")?.textContent;
 
 		if (!isStaff) {
@@ -127,7 +96,10 @@ presence.on("UpdateData", async () => {
 			presenceData.details = "Painel Admin";
 			presenceData.state = isStaff;
 		}
-	} else presenceData.details = "Página Inicial";
+	} else {
+		for (const [path, data] of Object.entries(pages))
+			if (pathname.includes(path)) presenceData = { ...presenceData, ...data };
+	}
 
 	if (timestamp) presenceData.startTimestamp = browsingTimestamp;
 	presence.setActivity(presenceData);
