72a6257cea5aa35c3b05652f9da1748d6105caf6
theusaf <theusafyt@gmail.com>
2022-10-31 20:21:23 -0700

    chore: prepare domains and paths

--- websites/F/Fly.io/presence.ts
+++ websites/F/Fly.io/presence.ts
@@ -5,9 +5,38 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/1rey7rc.png",
-		startTimestamp: browsingTimestamp,
-	};
+			largeImageKey: "https://i.imgur.com/1rey7rc.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname, href, hostname } = window.location,
+		pathSplit = pathname.split("/").slice(1);
+
+	switch (hostname) {
+		case "fly.io":
+		case "www.fly.io": {
+			switch (pathSplit[0]) {
+				case "": {
+					presenceData.details = "Browsing the homepage";
+					break;
+				}
+				case "blog": {
+					break;
+				}
+				case "docs": {
+					break;
+				}
+				default: {
+					presenceData.details = "Browsing";
+					presenceData.state = document.title.match(/^(.*)( · .*?)?$/)[1];
+					break;
+				}
+			}
+			break;
+		}
+		case "community.fly.io": {
+			break;
+		}
+	}
 
 	presence.setActivity(presenceData);
 });
