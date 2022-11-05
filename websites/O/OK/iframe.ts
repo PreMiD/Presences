365218919fad52b88fbf36283f7e890376b78266
SHISEN <alex-ench@yandex.ru>
2022-11-05 18:39:58 +0400

    chore: removed unnecessary conditional

--- websites/O/OK/iframe.ts
+++ websites/O/OK/iframe.ts
@@ -1,14 +1,12 @@
 const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
-	if (document.querySelector("video")) {
-		const video = document.querySelector<HTMLVideoElement>("video");
-		if (!isNaN(video?.duration)) {
-			iframe.send({
-				duration: video.duration,
-				currentTime: video.currentTime,
-				paused: video.paused,
-			});
-		}
+	const video = document.querySelector<HTMLVideoElement>("video");
+	if (!isNaN(video?.duration)) {
+		iframe.send({
+			duration: video.duration,
+			currentTime: video.currentTime,
+			paused: video.paused,
+		});
 	}
 });
