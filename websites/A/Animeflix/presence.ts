e12dd96369840219fe06833e8135ca89afc3319c
Micotsu <41237200+Micotsu@users.noreply.github.com>
2022-11-08 16:31:58 +0200

    Add files via upload
    Signed-off-by: Micotsu <41237200+Micotsu@users.noreply.github.com>

--- websites/A/Animeflix/presence.ts
+++ websites/A/Animeflix/presence.ts
@@ -1,29 +1,62 @@
 const presence = new Presence({
-		clientId: "1039178085922250873",
-	}),
-	strings = presence.getStrings({
-		play: "presence.playback.playing",
-		pause: "presence.playback.paused",
-	});
+	clientId: "1039178085922250873",
+});
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-    largeImageKey: "https://i.imgur.com/4hiyxuW.png"
-  }, { pathname } = document.location;
+			largeImageKey: "https://i.imgur.com/4hiyxuW.png",
+			buttons: [
+				{
+					label: "Visit Website",
+					url: "https://animeflix.live/",
+				},
+				{
+					label: "Join Discord",
+					url: "https://discord.gg/SKcb2C7HjH",
+				},
+			],
+		},
+		{ pathname } = document.location;
 
-  console.log(pathname)
-  if (pathname === "/") presenceData.details = "Exploring Main Page";
-  else if(pathname === "/genres") presenceData.details = "Exploring Genres";
-  else if(pathname === "/series") presenceData.details = "Exploring Series";
-  else if(pathname === "/movies") presenceData.details = "Exploring Movies";
-  else if(pathname === "/mylist") presenceData.details = "Going through My List";
-  else if(pathname.includes("/watch")) {
-    const showName = <HTMLMetaElement>document.querySelector('meta[name="anime-skip.show.name"]');
-    const episodeName = <HTMLMetaElement>document.querySelector('meta[name="anime-skip.episode.name"]');
-    const epNum = <HTMLMetaElement>document.querySelector('meta[name="anime-skip.episode.number"]');
-    presenceData.details = `Watching ${showName.content}`
-    presenceData.state = episodeName.content.includes("Episode") ? episodeName.content : `${epNum.content}. ${episodeName.content}`
-  }
-  else presenceData.details = "Exploring Animeflix";
+	switch (pathname) {
+		case "/": {
+			presenceData.details = "Exploring Main Page";
+			break;
+		}
+		case "/genres": {
+			presenceData.details = "Exploring Genres";
+			break;
+		}
+		case "/series": {
+			presenceData.details = "Exploring Series";
+			break;
+		}
+		case "/movies": {
+			presenceData.details = "Exploring Movies";
+			break;
+		}
+		case "/mylist": {
+			presenceData.details = "Going through My List";
+			break;
+		}
+		default:
+			if (pathname.includes("/watch")) {
+				const episodeName = document.querySelector<HTMLMetaElement>(
+					'meta[name="anime-skip.episode.name"]'
+				);
+				presenceData.details = `Watching ${
+					document.querySelector<HTMLMetaElement>(
+						'meta[name="anime-skip.show.name"]'
+					).content
+				}`;
+				presenceData.state = episodeName.content.includes("Episode")
+					? episodeName.content
+					: `${
+							document.querySelector<HTMLMetaElement>(
+								'meta[name="anime-skip.episode.number"]'
+							).content
+					  }. ${episodeName.content}`;
+			} else presenceData.details = "Exploring Animeflix";
+	}
 	presence.setActivity(presenceData);
 });
