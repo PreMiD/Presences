744c6c23a875fc6272284e819b7ad6fc633a56dc
theusaf <theusafyt@gmail.com>
2022-11-16 10:13:09 -0800

    style(Party Pack 2): use switch(true)

--- websites/J/Jackbox/games/Party Pack 2/bombintern.ts
+++ websites/J/Jackbox/games/Party Pack 2/bombintern.ts
@@ -5,23 +5,42 @@ export function getPresenceData(): PresenceData {
 	const { classList } = document.querySelector<HTMLDivElement>(
 		".bombintern-page:not(.pt-page-off)"
 	);
-	if (classList.contains("state-lobby")) return { state: "Waiting in lobby" };
-	else if (classList.contains("state-dayend")) return { state: "Day end" };
-	else if (classList.contains("state-gameover")) return { state: "Game over" };
-	else if (classList.contains("state-nothing")) return { state: "Waiting" };
-	else if (classList.contains("state-message"))
-		return { state: "Reading a message" };
-	else if (classList.contains("state-smashpuzzle"))
-		return { state: "Defusing a smash puzzle bomb" };
-	else if (classList.contains("state-wiredbomb"))
-		return { state: "Defusing a wired bomb" };
-	else if (classList.contains("state-coffeebomb"))
-		return { state: "Defusing a coffee bomb" };
-	else if (classList.contains("state-filingbomb"))
-		return { state: "Defusing a filing bomb" };
-	else if (classList.contains("state-keypadbomb"))
-		return { state: "Defusing a keypad bomb" };
-	else if (classList.contains("state-copierbomb"))
-		return { state: "Defusing a copier bomb" };
-	return {};
+	switch (true) {
+		case classList.contains("state-lobby"): {
+			return { state: "Waiting in lobby" };
+		}
+		case classList.contains("state-dayend"): {
+			return { state: "Day end" };
+		}
+		case classList.contains("state-gameover"): {
+			return { state: "Game over" };
+		}
+		case classList.contains("state-nothing"): {
+			return { state: "Waiting" };
+		}
+		case classList.contains("state-message"): {
+			return { state: "Reading a message" };
+		}
+		case classList.contains("state-smashpuzzle"): {
+			return { state: "Defusing a smash puzzle bomb" };
+		}
+		case classList.contains("state-wiredbomb"): {
+			return { state: "Defusing a wired bomb" };
+		}
+		case classList.contains("state-coffeebomb"): {
+			return { state: "Defusing a coffee bomb" };
+		}
+		case classList.contains("state-filingbomb"): {
+			return { state: "Defusing a filing bomb" };
+		}
+		case classList.contains("state-keypadbomb"): {
+			return { state: "Defusing a keypad bomb" };
+		}
+		case classList.contains("state-copierbomb"): {
+			return { state: "Defusing a copier bomb" };
+		}
+		default: {
+			return {};
+		}
+	}
 }
