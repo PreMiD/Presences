7f10338d5d3f11630e2cdfcdf94942e843b0dbd4
Dwifte <67983181+DwifteJB@users.noreply.github.com>
2022-11-17 17:05:52 +0000

    style(asset.party): lint

--- websites/A/asset.party/presence.ts
+++ websites/A/asset.party/presence.ts
@@ -9,55 +9,56 @@ presence.on("UpdateData", async () => {
 		{ pathname, href } = document.location,
 		pathSplit = pathname.split(/^.(.*)\/([^/]*)$/).filter(String);
 	switch (pathname) {
-	case "/": {
-	presenceData.details = "Exploring asset.party";
-	break;
-	}
-	case "/get/developer/preview": {
-		const keyDetails = document.querySelectorAll(".tag");
-		presenceData.details = "Trying to find the key...";
-		presenceData.state = `🔑${keyDetails[0].innerHTML.match(/\d+/)[0]} 🧍 ${
-			keyDetails[2].innerHTML.match(/\d+/)[0]
-		} 👀 ${keyDetails[3].innerHTML.match(/\d+/)[0]} `;
-		presenceData.smallImageKey = "https://i.imgur.com/mflNxaO.png";
-		presenceData.smallImageText = "You found the key!";
-	
-	break;
-	}
-	case "/api-history/": {
-	presenceData.details = "Viewing API History";
-	break;
-	}
-	default: if (
-		document.querySelectorAll(".nav-thumb").length !== 0 &&
-		document.querySelectorAll(".page-header-block").length !== 0
-	) {
-		const Details = document.querySelectorAll(".page-header-block");
-		if (
-			Details.length !== 0 &&
-			document.querySelectorAll(".nav-thumb").length !== 0
-		) {
-			presenceData.details = `Viewing ${Details[2].innerHTML} `;
-			presenceData.state = `Developed by ${Details[1].innerHTML}`;
-			presenceData.buttons = [
-				{
-					label: "Open Project",
-					url: href,
-				},
-			];
+		case "/": {
+			presenceData.details = "Exploring asset.party";
+			break;
 		}
-	} else if (pathSplit.length !== 0 && pathSplit[0] === "api") {
-		presenceData.details = "Viewing API documentation";
-		if (pathSplit[1]) {
-			presenceData.state = `Learning ${pathSplit[1]}`;
-			presenceData.buttons = [
-				{
-					label: "View documentation",
-					url: href,
-				},
-			];
+		case "/get/developer/preview": {
+			const keyDetails = document.querySelectorAll(".tag");
+			presenceData.details = "Trying to find the key...";
+			presenceData.state = `🔑${keyDetails[0].innerHTML.match(/\d+/)[0]} 🧍 ${
+				keyDetails[2].innerHTML.match(/\d+/)[0]
+			} 👀 ${keyDetails[3].innerHTML.match(/\d+/)[0]} `;
+			presenceData.smallImageKey = "https://i.imgur.com/mflNxaO.png";
+			presenceData.smallImageText = "You found the key!";
+
+			break;
 		}
-	}
+		case "/api-history/": {
+			presenceData.details = "Viewing API History";
+			break;
+		}
+		default:
+			if (
+				document.querySelectorAll(".nav-thumb").length !== 0 &&
+				document.querySelectorAll(".page-header-block").length !== 0
+			) {
+				const Details = document.querySelectorAll(".page-header-block");
+				if (
+					Details.length !== 0 &&
+					document.querySelectorAll(".nav-thumb").length !== 0
+				) {
+					presenceData.details = `Viewing ${Details[2].innerHTML} `;
+					presenceData.state = `Developed by ${Details[1].innerHTML}`;
+					presenceData.buttons = [
+						{
+							label: "Open Project",
+							url: href,
+						},
+					];
+				}
+			} else if (pathSplit.length !== 0 && pathSplit[0] === "api") {
+				presenceData.details = "Viewing API documentation";
+				if (pathSplit[1]) {
+					presenceData.state = `Learning ${pathSplit[1]}`;
+					presenceData.buttons = [
+						{
+							label: "View documentation",
+							url: href,
+						},
+					];
+				}
+			}
 	}
 
 	presence.setActivity(presenceData);
