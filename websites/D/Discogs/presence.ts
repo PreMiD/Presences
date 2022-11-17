62d67866c2410d0d8eb6fd6bcb24ec1c99273595
theusaf <theusafyt@gmail.com>
2022-11-17 10:58:59 -0800

    feat: add release/label paths

--- websites/D/Discogs/presence.ts
+++ websites/D/Discogs/presence.ts
@@ -1,13 +1,97 @@
 const presence = new Presence({
 		clientId: "1042567470738837534",
 	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+	browsingTimestamp = Math.floor(Date.now() / 1000),
+	slideshow = presence.createSlideshow();
+
+const shortenedURLs: Record<string, string> = {};
+async function getShortURL(url: string) {
+	if (url.length < 256) return url;
+	if (shortenedURLs[url]) return shortenedURLs[url];
+	try {
+		const pdURL = await (
+			await fetch(`https://pd.premid.app/create/${url}`)
+		).text();
+		shortenedURLs[url] = pdURL;
+		return pdURL;
+	} catch (err) {
+		presence.error(err);
+		return url;
+	}
+}
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/2LZSDR9.png",
-		startTimestamp: browsingTimestamp
-	};
+			largeImageKey: "https://i.imgur.com/2LZSDR9.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ href, pathname } = window.location,
+		pathSplit = pathname.split("/").filter(Boolean),
+		lastPath = pathSplit[pathSplit.length - 1];
 
-	presence.setActivity(presenceData);
+	switch (pathSplit[0] ?? "") {
+		case "": {
+			presenceData.details = "Browsing home page";
+			break;
+		}
+		case "label": {
+			if (lastPath === "images") {
+				presenceData.details = "Viewing label images";
+				presenceData.state = document.querySelector("h2 > a").textContent;
+				const images =
+					document.querySelectorAll<HTMLImageElement>("#view_images img");
+				for (let i = 0; i < images.length; i++) {
+					slideshow.addSlide(
+						i.toString(),
+						{
+							...presenceData,
+							largeImageKey: await getShortURL(images[i].src),
+						},
+						5000
+					);
+				}
+			} else {
+				presenceData.details = "Viewing a label";
+				presenceData.state = document.querySelector("h1").textContent;
+				presenceData.largeImageKey = await getShortURL(
+					document.querySelector<HTMLImageElement>(".thumbnail_link img").src
+				);
+				presenceData.buttons = [{ label: "View Label", url: href }];
+			}
+			break;
+		}
+		case "release": {
+			if (pathSplit[1] === "stats") {
+				presenceData.details = "Viewing stats for release";
+				presenceData.state = document.querySelector("h1 > a").textContent;
+			} else {
+				presenceData.details = "Viewing a release";
+				presenceData.state = document.querySelector("h1").textContent;
+				presenceData.largeImageKey = await getShortURL(
+					document.querySelector<HTMLImageElement>("picture > img").src
+				);
+			}
+			presenceData.buttons = [{ label: "View Release", url: href }];
+			break;
+		}
+		case "user": {
+			if (lastPath === "reviews") {
+				presenceData.details = "Viewing reviews by user";
+			} else {
+				presenceData.details = "Viewing a user";
+			}
+			presenceData.state = document.querySelector("h1").textContent.trim();
+			presenceData.smallImageKey = await getShortURL(
+				document.querySelector<HTMLImageElement>(".user_avatar > img").src
+			);
+			presenceData.buttons = [{ label: "View User", url: href }];
+			break;
+		}
+	}
+
+	if (slideshow.getSlides().length > 0) {
+		presence.setActivity(slideshow);
+	} else {
+		presence.setActivity(presenceData);
+	}
 });
