d6fe9b2340fc8ad13e38136bf6324716d4ffef63
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2022-11-25 07:10:48 -0800

    feat(Shoob): add card and user details (#7033)
    * feat: add buttons and more details
    
    * fix: fix profile image and currency amounts
    
    * feat: add cart details
    
    * chore: reomve card image
    
    * chore: remove image from user profile
    
    * feat: add card amounts
    
    * feat: add wallet to combined
    
    - also changes the 絵文字

--- websites/S/Shoob/presence.ts
+++ websites/S/Shoob/presence.ts
@@ -19,19 +19,16 @@ const presence = new Presence({
 		"/friends": { details: "Viewing Friends" },
 		"/fusion": { details: "Fusing Cards" },
 		"/giveaway": { details: "Viewing Giveaways" },
-		"/inventory": { details: "Viewing Inventory" },
 		"/keysgiveaways": { details: "Viewing Key Giveaways" },
 		"/leaderboards": { details: "Viewing Leaderboards" },
 		"/market": { details: "Viewing the Market" },
 		"/medals": { details: "Viewing Medals" },
 		"/messages": { details: "Viewing Private Messages" },
-		"/mini-games": { details: "Viewing Mini-Games" },
 		"/notifications": { details: "Viewing Notifications" },
 		"/premium": { details: "Viewing Premium" },
 		"/privacy-policy": { details: "Viewing Privacy Policy" },
 		"/rules": { details: "Reading the Rules" },
 		"/settings": { details: "Managing Settings" },
-		"/shop": { details: "Viewing the Shop" },
 		"/stacks": { details: "Viewing Stacks" },
 		"/staff": { details: "Viewing Staff Pages" },
 		"/staff-list": { details: "Viewing Staff List" },
@@ -42,6 +39,34 @@ const presence = new Presence({
 		"/updates": { details: "Viewing Updates" },
 	};
 
+function convertSuffixedToNumber(amount: string): number {
+	const [, number, suffix] = amount.match(/([\d.]+)\s*([A-Z])?/);
+	return (
+		parseFloat(number) *
+		({
+			K: 1e3,
+			M: 1e6,
+			B: 1e9,
+			T: 1e12,
+		}[suffix] ?? 1)
+	);
+}
+
+function convertNumberToSuffixed(amount: number): string {
+	const amounts: Record<string, number> = {
+			K: 1e3,
+			M: 1e6,
+			B: 1e9,
+			T: 1e12,
+		},
+		suffixes = Object.keys(amounts).reverse();
+	for (const suffix of suffixes) {
+		if (amount >= amounts[suffix])
+			return `${(amount / amounts[suffix]).toFixed(1)}${suffix}`;
+	}
+	return `${amount}`;
+}
+
 presence.on("UpdateData", async () => {
 	let presenceData: PresenceData = {
 		largeImageKey: "https://i.imgur.com/aI1Qn8s.png",
@@ -49,27 +74,43 @@ presence.on("UpdateData", async () => {
 	};
 	const { pathname, href } = window.location,
 		pathSplit = pathname.split("/").slice(1),
-		pageTitle = document.querySelector(
+		pageTitle = document.querySelector<HTMLLIElement>(
 			"[itemprop='breadcrumb'] > li:last-child"
 		)?.textContent,
-		profileImage = document.querySelector<HTMLImageElement>(".header-avatar");
+		profileImage = document.querySelector<HTMLAnchorElement>(".header-avatar"),
+		currencyFormat = await presence.getSetting<number>("currencyFormat");
 
 	if (profileImage) {
-		presenceData.smallImageKey = profileImage.src;
-		presenceData.smallImageText = `Wallet: ${
-			document.querySelector<HTMLSpanElement>(".header-wallet").textContent
-		} | Bank: ${
-			document.querySelector<HTMLSpanElement>(".header-bank:not(.orange)")
-				.textContent
-		} | Sōru: ${document
-			.querySelector<HTMLSpanElement>(".header-bank.orange")
-			.textContent.substring(1)}`;
+		presenceData.smallImageKey =
+			getComputedStyle(profileImage).backgroundImage.match(/url\("(.*)"\)/)[1];
+		if (currencyFormat === 0) {
+			presenceData.smallImageText = `💴 ${
+				document.querySelector<HTMLSpanElement>(".header-wallet").textContent
+			}`;
+		} else {
+			presenceData.smallImageText = `💴 ${
+				document.querySelector<HTMLSpanElement>(".header-wallet").textContent
+			} 💱 ${convertNumberToSuffixed(
+				convertSuffixedToNumber(
+					document.querySelector<HTMLSpanElement>(".header-wallet").textContent
+				) +
+					convertSuffixedToNumber(
+						document.querySelector<HTMLSpanElement>(".header-bank:not(.orange)")
+							.textContent
+					) +
+					4.2 *
+						convertSuffixedToNumber(
+							document.querySelector<HTMLSpanElement>(".header-bank.orange")
+								.textContent
+						)
+			)}`;
+		}
 	}
 
 	for (const [path, data] of Object.entries(staticPages))
 		if (pathname.startsWith(path)) presenceData = { ...presenceData, ...data };
 
-	switch (pathSplit[0] ?? "") {
+	switch (pathSplit[0]) {
 		case "articles":
 			if (pathSplit[1]) {
 				presenceData.details = "Reading an Article";
@@ -109,12 +150,52 @@ presence.on("UpdateData", async () => {
 				}
 			}
 			break;
+		case "inventory":
+			if (pathSplit[1]) {
+				presenceData.details = "Viewing a card in their inventory";
+				presenceData.state = `${pageTitle}#${
+					document.querySelector(".user_purchased + div + div b:nth-child(2)")
+						.textContent
+				}`;
+			} else {
+				presenceData.details = "Viewing Inventory";
+				presenceData.state = `${
+					document.querySelector<HTMLSpanElement>(".inventory-percent + span")
+						.textContent
+				} cards collected`;
+			}
+			break;
+		case "market":
+			presenceData.details = "Viewing the Market";
+			presenceData.buttons = [{ label: "View Market", url: href }];
+			break;
+		case "mini-games":
+			if (pathSplit[1]) {
+				presenceData.details = "Playing a Mini Game";
+				presenceData.buttons = [{ label: "View Mini-Game", url: href }];
+			} else {
+				presenceData.details = "Viewing Mini-Games";
+				presenceData.buttons = [{ label: "View Mini-Games", url: href }];
+			}
+			break;
 		case "servers":
 			if (pathSplit[1]) {
 				presenceData.details = "Viewing a Server";
 				presenceData.state = pageTitle;
 				presenceData.buttons = [{ label: "View Server", url: href }];
 			} else presenceData.details = "Viewing Servers";
+			break;
+		case "shop":
+			if (pathSplit[1] === "category") {
+				presenceData.details = "Browsing a Shop Category";
+				presenceData.state = pageTitle;
+			} else if (pathSplit[1] === "item") {
+				presenceData.details = "Viewing a Shop Item";
+				presenceData.state = pageTitle;
+				presenceData.largeImageKey =
+					document.querySelector<HTMLImageElement>(".item-pic-img").src;
+				presenceData.buttons = [{ label: "View Item", url: href }];
+			} else presenceData.details = "Browsing the Shop";
 			break;
 		case "support":
 			switch (pathSplit[1]) {
@@ -134,14 +215,28 @@ presence.on("UpdateData", async () => {
 				}
 			}
 			break;
-		case "user":
-			presenceData.details = "Viewing a profile";
-			presenceData.state = pageTitle;
-			presenceData.smallImageKey = document.querySelector<HTMLImageElement>(
-				".profile-avatar-pic img"
-			).src;
+		case "user": {
+			const tab = document.querySelector<HTMLButtonElement>(
+				"button.Mui-selected"
+			).textContent;
+			presenceData.details = `Viewing ${pageTitle}'s Profile`;
+			if (tab === "Info") {
+				const [, level, xp] = document
+					.querySelector<HTMLSpanElement>(".detailedStats > span")
+					.textContent.match(/(\d+)\s*level\s*\((\d+)\s*XP\)/);
+				presenceData.state = `🎚 Level ${level} ⚡${xp} XP`;
+			} else if (tab === "Cards") {
+				presenceData.state = `Their Cards (Total: ${
+					document
+						.querySelector<HTMLSpanElement>(
+							".tabpanel-content > div > div:nth-child(2) .MuiTypography-root"
+						)
+						.textContent.match(/\d+/)[0]
+				})`;
+			} else presenceData.state = `Their ${tab}`;
 			presenceData.buttons = [{ label: "View Profile", url: href }];
 			break;
+		}
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
