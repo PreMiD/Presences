231d2de2030b9de97f955f11fa41c7ae7e28da09
Optischa <fabian.evers2602@gmail.com>
2022-12-06 08:59:36 +0100

    feat(BreathFM): fix comment errors and run yarn format

--- websites/B/BreathFM/presence.ts
+++ websites/B/BreathFM/presence.ts
@@ -11,27 +11,22 @@ presence.on("UpdateData", async () => {
 			details: navigator.mediaSession.metadata.title,
 			state: navigator.mediaSession.metadata.artist,
 		};
+		presence.setActivity(presenceData);
+		return;
 	} else if (location.pathname === "/history") {
 		presenceData = {
 			details: "Schaut die Historie an",
-			largeImageKey:
-				"https://pbs.twimg.com/profile_images/1436408219904135171/8xqklEOk_400x400.jpg",
-			startTimestamp: browsingTimestamp,
 		};
 	} else if (location.pathname === "/bots") {
 		presenceData = {
 			details: "Interresiert sich für die Bots",
-			largeImageKey:
-				"https://pbs.twimg.com/profile_images/1436408219904135171/8xqklEOk_400x400.jpg",
-			startTimestamp: browsingTimestamp,
 		};
 	} else {
 		presenceData = {
 			details: "Durch Stöbert die Webseite",
-			largeImageKey:
-				"https://pbs.twimg.com/profile_images/1436408219904135171/8xqklEOk_400x400.jpg",
-			startTimestamp: browsingTimestamp,
 		};
 	}
+	presenceData.startTimestamp = browsingTimestamp;
+	presenceData.largeImageKey = "https://i.imgur.com/C8eRVDU.jpg";
 	presence.setActivity(presenceData);
 });
