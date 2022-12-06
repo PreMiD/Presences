99e266e700a61db333c8fff7afe03a85c2e9e5d2
Optischa <fabian.evers2602@gmail.com>
2022-12-06 23:40:41 +0100

    feat(BreathFM): added more sites to presence and cleanup code

--- websites/B/BreathFM/presence.ts
+++ websites/B/BreathFM/presence.ts
@@ -12,21 +12,49 @@ presence.on("UpdateData", async () => {
 			state: navigator.mediaSession.metadata.artist,
 		};
 		presence.setActivity(presenceData);
-		return;
-	} else if (location.pathname === "/history") {
-		presenceData = {
-			details: "Schaut die Historie an",
-		};
-	} else if (location.pathname === "/bots") {
-		presenceData = {
-			details: "Interresiert sich für die Bots",
-		};
 	} else {
-		presenceData = {
-			details: "Durch Stöbert die Webseite",
+		const presenceData: PresenceData = {
+			startTimestamp: browsingTimestamp,
+			largeImageKey: "https://i.imgur.com/C8eRVDU.jpg",
 		};
+		let onlineService = 0;
+		const children =
+			document.querySelectorAll(".monitors").length !== 0
+				? document.querySelectorAll(".monitors")[0].children
+				: null;
+
+		switch (location.pathname.toString().replace("/", "")) {
+			case "history":
+				presenceData.details = "Schaut die Historie an";
+				break;
+			case "bots":
+				presenceData.details = "Interresiert sich für die Bots";
+				break;
+			case "status":
+				for (let i = 0; i < children.length; i++) {
+					if (children.item(i).children.item(1).textContent === "Online") 
+						onlineService++;
+					
+				}
+				presenceData.details = "Schaut sich den Status an...";
+				presenceData.state = `${onlineService} von ${children.length} Services Online`;
+				break;
+			case "impressum":
+				presenceData.details = "Schaut sich das Impressum an...";
+				break;
+			case "changelog":
+				presenceData.details = "Schaut sich die Änderungen an...";
+				break;
+			case "dashboard":
+				presenceData.details = "Schaut sich im Dashboard um...";
+				break;
+			case "datenschutz":
+				presenceData.details = "Schaut sich den Datenschutz an...";
+				break;
+			default:
+				presenceData.details = "Durch Stöbert die Webseite";
+				break;
+		}
+		presence.setActivity(presenceData);
 	}
-	presenceData.startTimestamp = browsingTimestamp;
-	presenceData.largeImageKey = "https://i.imgur.com/C8eRVDU.jpg";
-	presence.setActivity(presenceData);
 });
