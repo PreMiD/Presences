cad592ba343b61b5477f498d960ce969576fd32f
Edwin Hernandez <45644797+EdwinH7@users.noreply.github.com>
2022-12-06 18:01:52 -0800

    feat(Baseball-Reference): resolve more lint

--- websites/B/Baseball-Reference/presence.ts
+++ websites/B/Baseball-Reference/presence.ts
@@ -3,7 +3,7 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-let team: any = null,
+let team: string = null,
 	image = document.images.item(1).src;
 
 presence.on("UpdateData", async () => {
@@ -17,21 +17,22 @@ presence.on("UpdateData", async () => {
 		}
 	} else if (document.location.pathname.includes("/players")) {
 		presenceData.details = "Viewing a Player:";
-		presenceData.state =
-			`${document.querySelector("div#meta div h1 span").textContent
-			} | ${
-				document
-					.querySelector("div#meta div p strong")
-					.nextSibling.textContent.replace(/\s/g, " ")
-					.trim()}`;
+		presenceData.state = `${
+			document.querySelector("div#meta div h1 span").textContent
+		} | ${document
+			.querySelector("div#meta div p strong")
+			.nextSibling.textContent.replace(/\s/g, " ")
+			.trim()}`;
 		if (!presence.getActivity().largeImageKey) {
 			presenceData.largeImageKey = image;
 		}
-		presenceData.buttons = [{label: "View Player", url: document.URL}];
+
+		presenceData.buttons = [{ label: "View Player", url: document.URL }];
 
 		if (
-			document.querySelector("#meta > div:nth-child(2) > p:nth-child(5) > a") !==
-			null
+			document.querySelector(
+				"#meta > div:nth-child(2) > p:nth-child(5) > a"
+			) !== null
 		) {
 			team = document.querySelector(
 				"#meta > div:nth-child(2) > p:nth-child(5) > a"
@@ -46,31 +47,34 @@ presence.on("UpdateData", async () => {
 		if (!presence.getActivity().largeImageKey) {
 			presenceData.largeImageKey = image;
 		}
-		presenceData.buttons = [{label: "View Manager", url: document.URL}];
+
+		presenceData.buttons = [{ label: "View Manager", url: document.URL }];
 	} else if (document.location.pathname.includes("/teams")) {
 		presenceData.details = "Viewing a Team:";
-		presenceData.state =
-			`${document.querySelector("div#meta div h1 span").textContent
-			} ${
-				document.querySelector("div#meta div h1 span").nextElementSibling
-					.textContent}`;
+		presenceData.state = `${
+			document.querySelector("div#meta div h1 span").textContent
+		} ${
+			document.querySelector("div#meta div h1 span").nextElementSibling
+				.textContent
+		}`;
 		if (!presence.getActivity().largeImageKey) {
 			presenceData.largeImageKey = image;
 		}
-		presenceData.buttons = [{label: "View Team", url: document.URL}];
+
+		presenceData.buttons = [{ label: "View Team", url: document.URL }];
 	} else if (document.location.pathname.includes("/leagues")) {
 		presenceData.details = "Viewing Stats:";
-		presenceData.state =
-			`${document.querySelector("div#meta div h1 span").textContent
-			} ${
-				document.querySelector("div#meta div h1 span").nextElementSibling
-					.textContent
-			} ${
-				document.querySelector("div#meta div h1 span.header_end").textContent}`;
+		presenceData.state = `${
+			document.querySelector("div#meta div h1 span").textContent
+		} ${
+			document.querySelector("div#meta div h1 span").nextElementSibling
+				.textContent
+		} ${document.querySelector("div#meta div h1 span.header_end").textContent}`;
 		if (!presence.getActivity().largeImageKey) {
 			presenceData.largeImageKey = image;
 		}
-		presenceData.buttons = [{label: "View Page", url: document.URL}];
+
+		presenceData.buttons = [{ label: "View Page", url: document.URL }];
 	} else if (document.location.pathname.includes("/boxes")) {
 		presenceData.details = "Viewing Box Score:";
 		presenceData.state = document
@@ -79,16 +83,17 @@ presence.on("UpdateData", async () => {
 		if (!presence.getActivity().largeImageKey) {
 			image = document.images.item(2).src;
 		}
+
 		presenceData.largeImageKey = image;
-		presenceData.buttons = [{label: "View Box Score", url: document.URL}];
+		presenceData.buttons = [{ label: "View Box Score", url: document.URL }];
 	} else if (document.location.pathname.includes("/friv")) {
 		presenceData.details = "Viewing Misc:";
 		presenceData.state = document.querySelector("div h1").textContent;
-		presenceData.buttons = [{label: "View Page", url: document.URL}];
+		presenceData.buttons = [{ label: "View Page", url: document.URL }];
 	} else if (document.location.pathname.includes("/oracle")) {
 		presenceData.details = "Consulting the Oracle of Baseball";
 		presenceData.state = document.querySelector("div h1").textContent;
-		presenceData.buttons = [{label: "View Page", url: document.URL}];
+		presenceData.buttons = [{ label: "View Page", url: document.URL }];
 	}
 	if (presenceData.details) {
 		presence.setActivity(presenceData);
