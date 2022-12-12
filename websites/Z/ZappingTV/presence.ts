8bb7ebba3a0773052695650f4f08324ce64da0ce
Álvaro Sebastián <sebalvaro@users.noreply.github.com>
2022-12-12 19:23:41 -0300

    fix(Zapping TV): suggested changes

--- websites/Z/ZappingTV/presence.ts
+++ websites/Z/ZappingTV/presence.ts
@@ -30,56 +30,52 @@ const presence = new Presence({
 				"li.btn-channel.active > div.channel-data > div.name"
 			).textContent.trim(),
 		};
-	},
-	assets: {
-		logo: string;
-		sleep: string;
-	} = {
-		logo: "https://i.imgur.com/5gyFfcd.png",
-		sleep: "https://i.imgur.com/gWBjy43.png",
 	};
+enum Assets {
+	zappingLogo = "https://i.imgur.com/5gyFfcd.png",
+	zappingSleep = "https://i.imgur.com/gWBjy43.png",
+}
 
 presence.on("UpdateData", async () => {
-	const { logo: zappingLogo, sleep: zappingSleep } = assets,
-		presenceData: PresenceData = {
+	const presenceData: PresenceData = {
 			smallImageText: "Zapping TV",
-			largeImageKey: zappingLogo,
+			largeImageKey: Assets.zappingLogo,
 			startTimestamp: elapsed,
 		},
-		coloredLogos: boolean = await presence.getSetting("coloredLogos");
+		coloredLogos: boolean = await presence.getSetting("coloredLogos"),
+		{ pathname } = document.location;
 
-	switch (document.location.pathname.split("/")[1]) {
+	switch (pathname.split("/")[1]) {
 		case "player": {
 			const { logo, title, channel } = await getChannelData({ coloredLogos });
-			if (channel !== "" && title !== "" && logo !== "") {
+			if (channel && title && logo) {
 				presenceData.details = `${title}`;
 				presenceData.state = `En ${channel}`;
 				presenceData.largeImageKey = logo;
-				presenceData.smallImageKey = zappingLogo;
+				presenceData.smallImageKey = Assets.zappingLogo;
 				presenceData.endTimestamp = getEndTime();
-				presence.setActivity(presenceData);
-			} else presence.clearActivity();
+			}
 			break;
 		}
 
 		case "dashboard": {
 			presenceData.details = "En el Dashboard";
-			presenceData.smallImageKey = zappingSleep;
+			presenceData.smallImageKey = Assets.zappingSleep;
 			presenceData.smallImageText = "AFK";
-			presence.setActivity(presenceData);
 			break;
 		}
 
 		case "login": {
 			presenceData.details = "Iniciando sesión";
-			presence.setActivity(presenceData);
 			break;
 		}
 
 		case "": {
 			presenceData.details = "En la página principal";
-			presence.setActivity(presenceData);
 			break;
 		}
 	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
 });
