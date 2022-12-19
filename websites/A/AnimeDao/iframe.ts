5676c0ebb4812c8133f0f6a990b1e8ce342faf28
BySuspect <omerbayraktar43@gmail.com>
2022-12-19 14:43:48 +0300

    Revert "feat(AnimeDao): add settings & fix bugs (#7066)"
    This reverts commit aedd51de1dd6a8aab6da66319d31d9129a8e3fbf.

--- websites/A/AnimeDao/iframe.ts
+++ websites/A/AnimeDao/iframe.ts
@@ -1,13 +1,20 @@
 const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
-	const video = document.querySelector("video");
-	if (video) {
-		iframe.send({
-			current: video.currentTime,
-			duration: video.duration,
-			isVideo: true,
-			paused: video.paused,
-		});
+	const { hostname } = window.location;
+	if (
+		hostname === "vcdn.space" ||
+		hostname === "streamtape.com" ||
+		hostname === "mixdrop.to"
+	) {
+		const video = document.querySelector("video");
+		if (video) {
+			iframe.send({
+				current: video.currentTime,
+				duration: video.duration,
+				paused: video.paused,
+				played: video.currentTime !== 0,
+			});
+		}
 	}
 });
