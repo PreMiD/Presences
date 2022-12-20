91491afba28d3c5788d01964369460e4b5aa589b
Anaxes <68460474+DiscordAnaxes@users.noreply.github.com>
2022-12-20 20:03:58 +0530

    feat(India Today News): implement suggested changes

--- websites/I/India Today News/presence.ts
+++ websites/I/India Today News/presence.ts
@@ -8,22 +8,33 @@ async function getStrings() {
 		{
 			play: "general.playing",
 			pause: "general.paused",
+			home: "general.viewHome",
+			search: "general.searchFor",
+			browse: "general.browsing",
+			watchingLive: "general.watchingLive",
+			live: "general.live",
+			watchingVid: "general.watchingVid",
 		},
 		await presence.getSetting<string>("lang").catch(() => "en")
 	);
 }
 
-interface iFrameData {
-	duration: number;
-	currentTime: number;
-	paused: boolean;
+function capitaliseFirstLetter(string: string) {
+	if (string) {
+		return (
+			string.trim().charAt(0).toUpperCase() +
+			string.trim().slice(1).toLowerCase()
+		);
+	}
 }
 
-let video: iFrameData = {
-	duration: 0,
-	currentTime: 0,
-	paused: true,
-};
+let video = {
+		duration: 0,
+		currentTime: 0,
+		paused: true,
+	},
+	strings: Awaited<ReturnType<typeof getStrings>>,
+	oldLang: string = null;
 
 presence.on(
 	"iFrameData",
@@ -36,268 +47,44 @@ enum Assets {
 	Logo = "https://i.imgur.com/JJHnTn3.png",
 	Reading = "https://i.imgur.com/8vMPNni.png",
 	Searching = "https://i.imgur.com/oGQtnIY.png",
-	Pause = "https://i.imgur.com/0A75vqT.png",
-	Play = "https://i.imgur.com/Dj5dekr.png",
-	Live = "https://i.imgur.com/wtweZhK.png",
+	Pause = "https://i.imgur.com/NyZsbVO.png",
+	Play = "https://i.imgur.com/Y1m0KVP.png",
+	Live = "https://i.imgur.com/a1VVpnH.png",
 }
 
 presence.on("UpdateData", async () => {
 	let presenceData: PresenceData = {
-			largeImageKey: Assets.Logo,
-			details: "Unsupported page",
-			smallImageKey: Assets.Logo,
-			smallImageText: "India Today News",
-		},
-		strings: Awaited<ReturnType<typeof getStrings>>,
-		oldLang: string = null;
+		largeImageKey: Assets.Logo,
+		details: "Unsupported page",
+	};
 
 	const { href, pathname } = document.location,
-		[showTimestamp, showButtons, showAuthor, newLang] = await Promise.all([
-			presence.getSetting<boolean>("timestamp"),
-			presence.getSetting<boolean>("buttons"),
-			presence.getSetting<boolean>("author"),
-			presence.getSetting<string>("lang").catch(() => "en"),
-		]),
-		pages: Record<string, PresenceData> = {
-			"/elections": {
-				details: "Viewing election news",
+		[showTimestamp, showButtons, showAuthor, newLang, privacy] =
+			await Promise.all([
+				presence.getSetting<boolean>("timestamp"),
+				presence.getSetting<boolean>("buttons"),
+				presence.getSetting<boolean>("author"),
+				presence.getSetting<string>("lang").catch(() => "en"),
+				presence.getSetting<boolean>("privacy"),
+			]);
+
+	/* deepscan-disable-next-line CONSTANT_CONDITION */
+	if (oldLang !== newLang || !strings) {
+		oldLang = newLang; /* deepscan-disable-line UNUSED_VAR_ASSIGN */
+		strings = await getStrings();
+	}
+
+	if (privacy) {
+		presenceData.details = strings.browse;
+		presence.setActivity(presenceData);
+		return;
+	}
+	const pages: Record<string, PresenceData> = {
+			"/photo": {
+				details: "Viewing Photographical news",
 				buttons: [
 					{
-						label: "View election news",
-						url: href,
-					},
-				],
-			},
-			"/india": {
-				details: "Viewing Indian news",
-				buttons: [
-					{
-						label: "View Indian news",
-						url: href,
-					},
-				],
-			},
-			"/world": {
-				details: "Viewing world news",
-				buttons: [
-					{
-						label: "View world news",
-						url: href,
-					},
-				],
-			},
-			"/business": {
-				details: "Viewing business news",
-				buttons: [
-					{
-						label: "View business news",
-						url: href,
-					},
-				],
-			},
-			"/technology": {
-				details: "Viewing technology news",
-				buttons: [
-					{
-						label: "View technology news",
-						url: href,
-					},
-				],
-			},
-			"/showbuzz": {
-				details: "Viewing showbuzz news",
-				buttons: [
-					{
-						label: "View showbuzz news",
-						url: href,
-					},
-				],
-			},
-			"/movies": {
-				details: "Viewing movie news",
-				buttons: [
-					{
-						label: "View movie news",
-						url: href,
-					},
-				],
-			},
-			"/sports": {
-				details: "Viewing sports news",
-				buttons: [
-					{
-						label: "View sports news",
-						url: href,
-					},
-				],
-			},
-			"/health": {
-				details: "Viewing health news",
-				buttons: [
-					{
-						label: "View health news",
-						url: href,
-					},
-				],
-			},
-			"/news-analysis": {
-				details: "Viewing news analysis news",
-				buttons: [
-					{
-						label: "View news analysis news",
-						url: href,
-					},
-				],
-			},
-			"/magazine": {
-				details: "Viewing magazine news",
-				buttons: [
-					{
-						label: "View magazine news",
-						url: href,
-					},
-				],
-			},
-			"/trending-news": {
-				details: "Viewing trending news news",
-				buttons: [
-					{
-						label: "View trending news news",
-						url: href,
-					},
-				],
-			},
-			"/newsmo": {
-				details: "Viewing newsmo news",
-				buttons: [
-					{
-						label: "View newsmo news",
-						url: href,
-					},
-				],
-			},
-			"/lifestyle": {
-				details: "Viewing lifestyle news",
-				buttons: [
-					{
-						label: "View lifestyle news",
-						url: href,
-					},
-				],
-			},
-			"/environment": {
-				details: "Viewing environment news",
-				buttons: [
-					{
-						label: "View environment news",
-						url: href,
-					},
-				],
-			},
-			"/education-today": {
-				details: "Viewing education news",
-				buttons: [
-					{
-						label: "View education news",
-						url: href,
-					},
-				],
-			},
-			"/law-today": {
-				details: "Viewing law news",
-				buttons: [
-					{
-						label: "View law news",
-						url: href,
-					},
-				],
-			},
-			"/auto": {
-				details: "Viewing auto news",
-				buttons: [
-					{
-						label: "View auto news",
-						url: href,
-					},
-				],
-			},
-			"/cities": {
-				details: "Viewing cities news",
-				buttons: [
-					{
-						label: "View cities news",
-						url: href,
-					},
-				],
-			},
-			"/crime": {
-				details: "Viewing crime news",
-				buttons: [
-					{
-						label: "View crime news",
-						url: href,
-					},
-				],
-			},
-			"/podcasts": {
-				details: "Viewing podcasts news",
-				buttons: [
-					{
-						label: "View podcasts news",
-						url: href,
-					},
-				],
-			},
-			"/opinion-columns": {
-				details: "Viewing opinion columns news",
-				buttons: [
-					{
-						label: "View opinion columns news",
-						url: href,
-					},
-				],
-			},
-			"/horoscopes": {
-				details: "Viewing horoscopes news",
-				buttons: [
-					{
-						label: "View horoscopes news",
-						url: href,
-					},
-				],
-			},
-			"/interactive": {
-				details: "Viewing interactive news",
-				buttons: [
-					{
-						label: "View interactive news",
-						url: href,
-					},
-				],
-			},
-			"/visualstories": {
-				details: "Viewing visual stories",
-				buttons: [
-					{
-						label: "View visual stories",
-						url: href,
-					},
-				],
-			},
-			"/cryptocurrency": {
-				details: "Viewing cryptocurrency news",
-				buttons: [
-					{
-						label: "View cryptocurrency news",
-						url: href,
-					},
-				],
-			},
-			"/fact-check": {
-				details: "Viewing fact check news",
-				buttons: [
-					{
-						label: "View fact check news",
+						label: "View page",
 						url: href,
 					},
 				],
@@ -306,16 +93,7 @@ presence.on("UpdateData", async () => {
 				details: "Viewing DIU news",
 				buttons: [
 					{
-						label: "View DIU news",
-						url: href,
-					},
-				],
-			},
-			"/photo": {
-				details: "Viewing photo news",
-				buttons: [
-					{
-						label: "View photo news",
+						label: "View page",
 						url: href,
 					},
 				],
@@ -324,34 +102,7 @@ presence.on("UpdateData", async () => {
 				details: "Viewing news",
 				buttons: [
 					{
-						label: "View news",
-						url: href,
-					},
-				],
-			},
-			"/foryou": {
-				details: "Viewing for you page",
-				buttons: [
-					{
-						label: "View for you page",
-						url: href,
-					},
-				],
-			},
-			"/cover-story": {
-				details: "Viewing cover story",
-				buttons: [
-					{
-						label: "View cover story",
-						url: href,
-					},
-				],
-			},
-			"/programmes": {
-				details: "Viewing programmes",
-				buttons: [
-					{
-						label: "View programmes",
+						label: "View page",
 						url: href,
 					},
 				],
@@ -361,22 +112,29 @@ presence.on("UpdateData", async () => {
 		authorTitle = document
 			.querySelector("div.Story_story__author__cJoes")
 			?.querySelector("a.jsx-99cc083358cc2e2d")?.textContent,
-		multipleAuthors = document.querySelector("div.Story_authors__OqkJZ");
-	/* deepscan-disable-next-line CONSTANT_CONDITION */
-	if (oldLang !== newLang || !strings) {
-		oldLang = newLang; /* deepscan-disable-line UNUSED_VAR_ASSIGN */
-		strings = await getStrings();
-	}
+		multipleAuthors = document.querySelector("div.Story_authors__OqkJZ"),
+		topic = document
+			.querySelector('[class="jsx-559305e56e2f7896 active"]')
+			?.textContent.toLowerCase()
+			.trim();
 
 	for (const [path, data] of Object.entries(pages))
 		if (pathname.includes(path)) presenceData = { ...presenceData, ...data };
 
-	if (pathname === "/") presenceData.details = "Viewing the homepage";
+	if (pathname === "/") presenceData.details = strings.home;
 	else if (pathname.includes("/search/")) {
-		presenceData.details = "Searching for";
+		presenceData.details = strings.search;
 		presenceData.state = pathname.split("/search/")[1];
 		presenceData.smallImageKey = Assets.Searching;
 		presenceData.smallImageText = "Searching";
+	} else if (pathname.includes(`/${topic}`)) {
+		presenceData.details = `Viewing ${capitaliseFirstLetter(topic)} news`;
+		presenceData.buttons = [
+			{
+				label: "View page",
+				url: href,
+			},
+		];
 	} else if (pathname.includes("/photo")) {
 		presenceData.details = document.querySelector(
 			"h1.Photo_maintitle__G_cdj"
@@ -418,10 +176,10 @@ presence.on("UpdateData", async () => {
 			Math.floor(video.currentTime),
 			Math.floor(video.duration)
 		);
-		presenceData.details = document.querySelector(
+		presenceData.details = strings.watchingVid;
+		presenceData.state = document.querySelector(
 			"h1.Videoplayer_title__3Vk_I"
 		)?.textContent;
-		presenceData.state = storyDate;
 		presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
 		presenceData.smallImageText = video.paused ? strings.pause : strings.play;
 		presenceData.buttons = [
@@ -454,23 +212,21 @@ presence.on("UpdateData", async () => {
 			];
 		}
 	} else if (pathname.includes("/weather/")) {
-		presenceData.details = document
+		const weather = document
 			.querySelector("div.wtr_ttl_lhs")
 			?.querySelector("h1")
-			?.textContent.split("Today")[0];
-		presenceData.state = document
-			.querySelector("div.wtr_ttl_lhs")
-			?.querySelector("h1")
-			?.textContent.split("Today")[1];
+			?.textContent.split("Today");
+		presenceData.details = weather?.[0].trim();
+		presenceData.state = weather?.[1].trim();
 		presenceData.smallImageKey = document
 			.querySelector("span.wkl_li_icn")
 			?.querySelector("img")?.src;
-		presenceData.smallImageText = `${
-			document.querySelector("span.wtr_tmp_txt")?.textContent
-		} - ${
-			document.querySelector("div.wtr_tmp_rhs")?.querySelector("strong")
-				?.textContent
-		}°C`;
+		presenceData.smallImageText = `${document
+			.querySelector("span.wtr_tmp_txt")
+			?.textContent.trim()} - ${document
+			.querySelector("div.wtr_tmp_rhs")
+			?.querySelector("strong")
+			?.textContent.trim()}°C`;
 		presenceData.buttons = [
 			{
 				label: "View Weather",
@@ -478,6 +234,7 @@ presence.on("UpdateData", async () => {
 			},
 		];
 	} else if (pathname.includes("/story")) {
+		const liveBlog = document.querySelector("h1.LiveBlog_blogtitile__WB5Az");
 		presenceData.details = document.querySelector(
 			"h1.Story_strytitle__MYXmR"
 		)?.textContent;
@@ -489,6 +246,19 @@ presence.on("UpdateData", async () => {
 			.querySelector("a.jsx-99cc083358cc2e2d")
 			?.querySelector("img")?.src;
 		presenceData.smallImageText = authorTitle;
+		if (liveBlog) {
+			presenceData.details = liveBlog.textContent;
+			presenceData.state = document.querySelector("span.catdate")?.textContent;
+			presenceData.largeImageKey = document
+				.querySelector("div.LiveBlog_blogmos__UcOVK > img")
+				?.getAttribute("src");
+			presenceData.smallImageKey = document
+				.querySelector("div.Byline_profile__pic__ijD9v > a > img")
+				?.getAttribute("src");
+			presenceData.smallImageText = document.querySelector(
+				"div.Byline_category__author__UEnOW > a"
+			)?.textContent;
+		}
 		presenceData.buttons = [
 			{
 				label: "Read Article",
@@ -504,13 +274,13 @@ presence.on("UpdateData", async () => {
 			presenceData.smallImageText = "Multiple Authors";
 		}
 	} else if (video && !isNaN(video.duration) && pathname.includes("livetv")) {
-		presenceData.details = "Watching Live TV";
+		presenceData.details = strings.watchingLive;
 		presenceData.state = document
 			.querySelector("h1.Livetv_titlewithicon__Z2jnL")
 			?.textContent.toUpperCase()
 			?.replace("LIVE TV", "");
 		presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Live;
-		presenceData.smallImageText = video.paused ? strings.pause : "Live";
+		presenceData.smallImageText = video.paused ? strings.pause : strings.live;
 		presenceData.buttons = [
 			{
 				label: "Watch Live TV",
@@ -519,8 +289,12 @@ presence.on("UpdateData", async () => {
 		];
 	}
 
-	if (!showButtons) delete presenceData.buttons;
-	if (showTimestamp) presenceData.startTimestamp = browsingTimestamp;
+	if (!showButtons && presenceData.buttons) delete presenceData.buttons;
+	if (
+		(showTimestamp && !presenceData.startTimestamp) ||
+		!presenceData.endTimestamp
+	)
+		presenceData.startTimestamp = browsingTimestamp;
 	if (showAuthor) {
 		if (pathname.includes("/story")) {
 			presenceData.smallImageKey = Assets.Reading;
