ae7d34fb281bca74451faaf0c712291df3791256
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2022-12-29 11:25:23 +0100

    fix(Wiki.gg): rewrite/fix all issues

--- websites/W/Wiki.gg/presence.ts
+++ websites/W/Wiki.gg/presence.ts
@@ -2,75 +2,125 @@ const presence = new Presence({
 		clientId: "978186598669758504",
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
-let title: string, wikiTitle: string;
+let title: string;
+enum Assets {
+	Logo = "https://i.imgur.com/4YN40As.png",
+	Search = "https://i.imgur.com/oGQtnIY.png",
+	Read = "https://i.imgur.com/wPUmqu5.png",
+}
+
+async function getStrings() {
+	return presence.getStrings(
+		{
+			browse: "general.browsing",
+			search: "general.searchFor",
+			viewHome: "general.viewHome",
+			buttonViewPage: "general.buttonViewPage",
+			buttonViewProfile: "general.buttonViewProfile",
+		},
+		await presence.getSetting<string>("lang").catch(() => "en")
+	);
+}
+
+function capitalizeFirstLetter(string: string) {
+	if (string) {
+		return (
+			string.trim().charAt(0).toUpperCase() +
+			string.trim().slice(1).toLowerCase()
+		);
+	} else return "Undefined";
+}
+let strings: Awaited<ReturnType<typeof getStrings>>,
+	oldLang: string = null;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "logo",
+			largeImageKey: Assets.Logo,
 			startTimestamp: browsingTimestamp,
 		},
 		{ pathname, hostname, href } = document.location,
-		[privacy, buttons] = await Promise.all([
+		[newLang, privacy, buttons] = await Promise.all([
+			presence.getSetting<string>("lang").catch(() => "en"),
 			presence.getSetting<boolean>("privacy"),
 			presence.getSetting<boolean>("buttons"),
-		]);
-	if (!privacy) {
-		if (hostname === "wiki.gg") {
-			if (pathname === "/") presenceData.details = "Viewing the home page";
-			else {
-				presenceData.details = document
-					.querySelector("head > title")
-					.textContent.slice(0, -10);
-			}
-		} else {
-			const search = document.querySelectorAll<HTMLInputElement>(
-				"input[name='search']"
-			);
-			wikiTitle = hostname.split(".")[0];
-			title = wikiTitle[0].toUpperCase() + wikiTitle.slice(1);
-			switch (title) {
-				case "Terraria": {
-					presenceData.largeImageKey = "https://i.imgur.com/fNW9Glp.png";
-					break;
-				}
-				case "Darkdeity": {
-					presenceData.largeImageKey = "https://i.imgur.com/r1CDG80.png";
-					break;
-				}
-				case "Coromon": {
-					presenceData.largeImageKey = "https://i.imgur.com/0V4Onno.png";
-				}
-			}
-			if (search[0]?.value || search[1]?.value) {
-				if (search[0]?.value) presenceData.state = search[0].value;
-				else if (
-					document.querySelector("#mw-search-top-table > div.results-info") &&
-					search[1]?.value
-				)
-					presenceData.state = search[1].value;
-				title = `Searching ${title}'s wiki for:`;
-			} else if (pathname.includes("wiki") || pathname.includes("index.php")) {
-				title = `Viewing ${title} Wiki about:`;
-				presenceData.state = document
-					.querySelector("#firstHeading")
-					.textContent.replace(":", " ");
-				presenceData.buttons = [
-					{
-						label: "View Page",
-						url: href,
-					},
-				];
-			} else if (
-				pathname === "/" ||
-				document.querySelector(
-					"#mw-content-text > div.mw-parser-output > div > div:nth-child(1) > div > div:nth-child(1)"
-				)
-			)
-				presenceData.state = "Viewing home page";
+		]),
+		main =
+			document
+				.querySelector('[id="ca-nstab-main"]')
+				?.textContent?.toLowerCase() ?? "";
+	if (oldLang !== newLang || !strings) {
+		oldLang = newLang;
+		strings = await getStrings();
+	}
+	if (privacy) {
+		presenceData.details = strings.browse;
+		presence.setActivity(presenceData);
+		return;
+	}
+	if (hostname === "wiki.gg") {
+		if (pathname === "/") presenceData.details = strings.viewHome;
+		else {
+			presenceData.details = document
+				.querySelector("head > title")
+				.textContent.slice(0, -10);
+		}
+	} else {
+		title = capitalizeFirstLetter(
+			document
+				.querySelector('[property="og:site_name"]')
+				?.getAttribute("content")
+				?.toLowerCase() ??
+				document
+					.querySelector('[property="og:title"]')
+					?.getAttribute("content")
+					?.toLowerCase() ??
+				document
+					.querySelector('[id="footer-places-about"]')
+					?.textContent?.replace(/About /gm, "")
+					?.toLowerCase()
+		);
+		let search: HTMLInputElement | NodeListOf<HTMLInputElement> =
+			document.querySelectorAll<HTMLInputElement>("input[id='searchInput']") ??
+			document.querySelectorAll<HTMLInputElement>("input[name='search']");
+		if (search.length === 1) search = search[0];
+		else search = search[1];
+		presenceData.largeImageKey = `https://${hostname}/images/e/e6/Site-logo.png`;
+		if (search?.value) {
+			presenceData.state = search.value;
+			title = `${title} | ${strings.search?.toLowerCase()}`;
+			presenceData.details = title;
+			presenceData.smallImageKey = Assets.Search;
+		} else if (pathname.includes("wiki") && main !== "main page") {
+			presenceData.smallImageKey = Assets.Read;
+			const firstHeading = document
+				.querySelector("#firstHeading")
+				.textContent?.toLowerCase();
+			presenceData.details = title;
+			if (firstHeading?.includes(":")) {
+				presenceData.state = `${capitalizeFirstLetter(
+					document
+						.querySelector("#firstHeading")
+						.textContent?.toLowerCase()
+						.split(":")[0]
+				)} | ${
+					document
+						.querySelector("#firstHeading")
+						.textContent?.toLowerCase()
+						.split(":")[1]
+				}`;
+			} else presenceData.state = capitalizeFirstLetter(firstHeading);
+			presenceData.buttons = [
+				{
+					label: strings.buttonViewPage,
+					url: href,
+				},
+			];
+		} else if (pathname === "/" || main === "main" || main === "main page") {
+			presenceData.state = strings.viewHome;
 			presenceData.details = title;
 		}
-	} else presenceData.details = "Browsing";
-	if (privacy || !buttons) delete presenceData.buttons;
+	}
+	if (!buttons && presenceData.buttons) delete presenceData.buttons;
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
