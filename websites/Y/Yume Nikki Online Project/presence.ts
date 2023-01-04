8da14acc6a1de05a9d65c1ed8c330981a6719554
Ｐｎｆｒｌ　Ｅｎｍ <35457994+PnfrlEnm@users.noreply.github.com>
2023-01-04 15:13:28 -0600

    Add files via upload
    Signed-off-by: Ｐｎｆｒｌ　Ｅｎｍ <35457994+PnfrlEnm@users.noreply.github.com>

---
+++ websites/Y/Yume Nikki Online Project/presence.ts
@@ -0,0 +1,30 @@
+const presence = new Presence({
+		clientId: "1060269808383430696",
+	}),
+	elapsed = Math.floor(Date.now() / 1000),
+	strings = presence.getStrings({
+		play: "presence.playback.playing",
+		pause: "presence.playback.paused",
+	});
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey: "yume",
+		startTimestamp: elapsed,
+	};
+
+	if (location.pathname === "/" || location.href.split(location.host)[1].toLowerCase() === "/index") {
+		presenceData.details = "The Nexus";
+		presenceData.state = "(Selecting Game)";
+
+	} else if (document.title.split(" - ")[0].toString() != "YNOproject") {
+		presenceData.details = document.title.split(" - ")[0].toString();
+		if (document.getElementById("locationText").innerText) {presenceData.state = document.getElementById("locationText").innerText;}
+		else {presenceData.state = "In Menu"}
+
+	} else {
+		presenceData.details = "Loading..."
+	}
+
+	presence.setActivity(presenceData);
+});
