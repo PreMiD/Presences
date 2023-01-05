28d6ca911e0cd6ce3fd3082b566468ccc4cc5edc
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2023-01-05 10:58:44 +0100

    style(Wiki.gg): fix style

--- websites/W/Wiki.gg/presence.ts
+++ websites/W/Wiki.gg/presence.ts
@@ -25,8 +25,7 @@ async function getStrings() {
 function capitalizeFirstLetter(string: string) {
 	if (!string) return "Undefined";
 	return (
-		string.trim().charAt(0).toUpperCase() +
-		string.trim().slice(1).toLowerCase()
+		string.trim().charAt(0).toUpperCase() + string.trim().slice(1).toLowerCase()
 	);
 }
 let strings: Awaited<ReturnType<typeof getStrings>>,
