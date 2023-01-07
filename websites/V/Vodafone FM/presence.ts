f106bc5d357f46dacac04347760cda7985845ee7
André <39736248+andrefcdias@users.noreply.github.com>
2023-01-08 00:32:37 +0100

    feat: add basic functionality

--- websites/V/Vodafone FM/presence.ts
+++ websites/V/Vodafone FM/presence.ts
@@ -9,9 +9,23 @@ const presence = new Presence({
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey: "logo_512",
-		details: "Jamming fr fr...",
-		startTimestamp: Math.floor(Date.now() / 1000),
+		details: "Listening to music",
 	};
+
+	const playButton = document.getElementById("play");
+	if (playButton.classList.contains("hidden")) {
+		presenceData.smallImageKey = "play";
+		presenceData.smallImageText = (await strings).play
+	} else {
+		presenceData.smallImageKey = "pause";
+		presenceData.smallImageText = (await strings).pause;
+	}
+
+	const artistElement = document.getElementById("artist_name");
+	if (artistElement) {
+		const songElement = document.getElementById("song_name");
+		presenceData.state = `${artistElement.title} - ${songElement.title}`;
+	}
 
 	presence.setActivity(presenceData);
 });
