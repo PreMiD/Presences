e7e8f78bfffab0102b6e4fc8cc1d6f2417409091
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2023-01-14 13:30:00 +0100

    chore(Cracked.io): format w/ prettier

--- websites/C/Cracked.io/presence.ts
+++ websites/C/Cracked.io/presence.ts
@@ -19,7 +19,9 @@ async function getStrings() {
 }
 function capitalizeFirstLetter(string: string) {
 	if (!string) return "Undefined";
-	return string.trim().charAt(0).toUpperCase() + string.trim().slice(1).toLowerCase();
+	return (
+		string.trim().charAt(0).toUpperCase() + string.trim().slice(1).toLowerCase()
+	);
 }
 
 enum Assets {
