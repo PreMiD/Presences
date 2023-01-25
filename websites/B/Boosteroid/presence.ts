dd43ef78af964127ca3ac84610375a1f84f6a021
Saith <39977285+Shironep@users.noreply.github.com>
2023-01-25 07:13:40 -0300

    fix(Boosteroid): code formated with lint

--- websites/B/Boosteroid/presence.ts
+++ websites/B/Boosteroid/presence.ts
@@ -1,6 +1,6 @@
 const presence = new Presence({
-	clientId: "1066889761928777848",
-}),
+		clientId: "1066889761928777848",
+	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
@@ -11,49 +11,63 @@ presence.on("UpdateData", async () => {
 	}
 
 	async function getGame(id: number): Promise<Game | null> {
-		const response = await fetch('https://github.saith.dev/games.json')
+		const response = await fetch("https://github.saith.dev/games.json");
 		if (!response.ok) return null;
 
-		let games = await response.json()
+		const games = await response.json();
 
 		if (!games[id]) return null;
-		return games[id]
+		return games[id];
 	}
 
 	const presenceData: PresenceData = {
-		largeImageKey: "icon",
-		startTimestamp: browsingTimestamp,
-	},
-		{ pathname } = document.location
+			largeImageKey: "icon",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname } = document.location;
 
 	switch (true) {
-		case pathname === "/dashboard":
-			presenceData.details = "Browsing"
-			presenceData.state = `In: ${(document.querySelector('button.tab-button--active')).innerHTML}`
-			presenceData.largeImageKey = "https://i.imgur.com/Ldqj50W.png"
+		case pathname === "/dashboard": {
+			presenceData.details = "Browsing";
+			presenceData.state = `In: ${
+				(document.querySelector("button.tab-button--active") as HTMLElement)
+					.innerHTML
+			}`;
+			presenceData.largeImageKey = "https://i.imgur.com/Ldqj50W.png";
 			presence.setActivity(presenceData);
 			break;
-
-		case pathname.startsWith('/application/'):
-			presenceData.details = "Looking at Game information"
-			presenceData.state = document.querySelector('h1').innerHTML
-			presenceData.largeImageKey = "https://i.imgur.com/Ldqj50W.png"
+		}
+		case pathname.startsWith("/application/"): {
+			presenceData.details = "Looking at Game information";
+			presenceData.state = (
+				document.querySelector("h1") as HTMLElement
+			).innerHTML;
+			presenceData.largeImageKey = "https://i.imgur.com/Ldqj50W.png";
 			presence.setActivity(presenceData);
 			break;
-
-		case pathname === "/static/streaming/streaming.html":
-			let gameid = await getGame(Number(window.localStorage.getItem('appId')))
+		}
+		case pathname === "/profile/account/main": {
+			presenceData.details = "Browsing";
+			break;
+		}
+		case pathname === "/static/streaming/streaming.html": {
+			const gameid = await getGame(
+				Number(window.localStorage.getItem("appId"))
+			);
 			if (!gameid) {
-				presenceData.details = "Unknown Game"
-				presenceData.largeImageKey = "https://i.imgur.com/cnczdmM.png"
+				presenceData.details = "Unknown Game";
+				presenceData.largeImageKey = "https://i.imgur.com/cnczdmM.png";
 				presence.setActivity(presenceData);
 			} else {
-				let gameid = await getGame(Number(window.localStorage.getItem('appId')))
-				presenceData.details = gameid.name
-				presenceData.state = `Platform: ${gameid.platform}`
-				presenceData.largeImageKey = gameid.icon
+				const gameid = await getGame(
+					Number(window.localStorage.getItem("appId"))
+				);
+				presenceData.details = gameid.name;
+				presenceData.state = `Platform: ${gameid.platform}`;
+				presenceData.largeImageKey = gameid.icon;
 				presence.setActivity(presenceData);
 			}
 			break;
+		}
 	}
-})
+});
