70716226d55f55bae1bc5553b4c2f92d065cd27d
André <39736248+andrefcdias@users.noreply.github.com>
2023-01-28 10:46:52 +0100

    feat: add multi lang support

--- websites/V/Vodafone FM/presence.ts
+++ websites/V/Vodafone FM/presence.ts
@@ -1,24 +1,30 @@
 const presence = new Presence({
 		clientId: "1061398473368412210",
 	}),
-	strings = presence.getStrings({
-		play: "general.playing",
-		pause: "general.paused",
-		listeningMusic: "general.listeningMusic",
-	});
+	getStrings = async () =>
+		presence.getStrings(
+			{
+				play: "general.playing",
+				pause: "general.paused",
+				listeningMusic: "general.listeningMusic",
+			},
+			await presence.getSetting<string>("lang").catch(() => "en")
+		);
+let strings: Awaited<ReturnType<typeof getStrings>>;
 
 presence.on("UpdateData", async () => {
+	strings = await getStrings();
 	const presenceData: PresenceData = {
 		largeImageKey: "https://i.imgur.com/6IUf3O6.png",
-		details: (await strings).listeningMusic,
+		details: strings.listeningMusic,
 	};
 
 	if (document.querySelector("#play").classList.contains("hidden")) {
 		presenceData.smallImageKey = "https://i.imgur.com/xiZprpt.png";
-		presenceData.smallImageText = (await strings).play;
+		presenceData.smallImageText = strings.play;
 	} else {
 		presenceData.smallImageKey = "https://i.imgur.com/w1jwJTh.png";
-		presenceData.smallImageText = (await strings).pause;
+		presenceData.smallImageText = strings.pause;
 	}
 
 	const artist = document.querySelector("#artist_name")?.textContent,
