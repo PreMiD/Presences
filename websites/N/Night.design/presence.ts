2d3d0cd5522c185d4ba21b6f83f2b70a87369992
Nightcorelena <59476216+Nightcorelena@users.noreply.github.com>
2023-02-01 23:10:13 +0100

    Updated again
    Removed from buttons and change the name from "Domain" to "pathname"
    
    Signed-off-by: Nightcorelena <59476216+Nightcorelena@users.noreply.github.com>

--- websites/N/Night.design/presence.ts
+++ websites/N/Night.design/presence.ts
@@ -8,73 +8,67 @@ presence.on("UpdateData", async () => {
 	};
 
 	presenceData.startTimestamp = Math.floor(Date.now() / 1000);
-	presenceData.buttons = [
-		{
-			label: "View Website",
-			url: "https://night.design/",
-		},
-	];
 
-	const domain = document.location.pathname.slice(3);
+	const pathname = document.location.pathname.slice(3);
 
-	if (domain === "/home") {
+	if (pathname === "/home") {
 		presenceData.details = "View the Homepage";
-	} else if (domain === "/resources") {
+	} else if (pathname === "/resources") {
 		if (!document.location.search) {
 			presenceData.details = "Search...";
 		}
-	} else if (domain === "/settings/account") {
+	} else if (pathname === "/settings/account") {
 		presenceData.details = "Change my Account Settings";
-	} else if (domain === "/settings/notifications") {
+	} else if (pathname === "/settings/notifications") {
 		presenceData.details = "Change my notifications Settings";
-	} else if (domain === "/settings/connected-accounts") {
+	} else if (pathname === "/settings/connected-accounts") {
 		presenceData.details = "Add a user connection to my account";
-	} else if (domain === "/settings/account-security") {
+	} else if (pathname === "/settings/account-security") {
 		presenceData.details = "Change my Account Security Settings";
-	} else if (domain === "/password-forgotten") {
+	} else if (pathname === "/password-forgotten") {
 		presenceData.details = "Change my Password";
-	} else if (domain === "/login") {
+	} else if (pathname === "/login") {
 		presenceData.details = "Sign in with my Account";
-	} else if (domain === "/register") {
+	} else if (pathname === "/register") {
 		presenceData.details = "Create a new Account";
-	} else if (domain === "/dashboard") {
+	} else if (pathname === "/dashboard") {
 		presenceData.details = "View my Dashboard";
-	} else if (domain === "/creator") {
+	} else if (pathname === "/creator") {
 		presenceData.details = "View Creator page";
-	} else if (domain === "/modder") {
+	} else if (pathname === "/modder") {
 		presenceData.details = "View Modder Page";
-	} else if (domain === "/modder/publisher") {
+	} else if (pathname === "/modder/publisher") {
 		presenceData.details = "Apply to become a Modder";
-	} else if (domain === "/legal/terms-of-service") {
+	} else if (pathname === "/legal/terms-of-service") {
 		presenceData.details = "Read the Terms of Service from Night.design";
-	} else if (domain === "/legal/privacy-policy") {
+	} else if (pathname === "/legal/privacy-policy") {
 		presenceData.details = "Read the Privacy Policy from Night.design";
-	} else if (domain === "/legal/imprint") {
+	} else if (pathname === "/legal/imprint") {
 		presenceData.details = "Read the Imprint from Night.design";
-	} else if (domain === `/resources/${document.location.pathname.split("/")[3]}`) {
+	} else if (pathname === `/resources/${document.location.pathname.split("/")[3]}`) {
 		presenceData.details = document.getElementsByClassName("m-b-10")[0].innerHTML;
 		let pic = document.getElementById("prev") as HTMLImageElement;
 		presenceData.largeImageKey = pic.src;
-	} else if (domain === `/u/${document.location.pathname.split("/")[3]}/activities`) {
+	} else if (pathname === `/u/${document.location.pathname.split("/")[3]}/activities`) {
 		let image = document.getElementsByClassName("rounded-circle  ng-lazyloaded")[0] as HTMLImageElement;
 		presenceData.largeImageKey = image.src;
 		presenceData.details = `View ${document.location.pathname.split("/")[3]}'s activitys`;
-	} else if (domain === `/u/${document.location.pathname.split("/")[3]}/resources`) {
+	} else if (pathname === `/u/${document.location.pathname.split("/")[3]}/resources`) {
 		let image = document.getElementsByClassName("rounded-circle  ng-lazyloaded")[0] as HTMLImageElement;
 		presenceData.largeImageKey = image.src;
 		presenceData.details = `View ${document.location.pathname.split("/")[3]}'s designs`;
-	} else if (domain ===`/u/${document.location.pathname.split("/")[3]}/connected-accounts`) {
+	} else if (pathname === `/u/${document.location.pathname.split("/")[3]}/connected-accounts`) {
 		let image = document.getElementsByClassName("rounded-circle  ng-lazyloaded")[0] as HTMLImageElement;
 		presenceData.largeImageKey = image.src;
 		presenceData.details = `View ${document.location.pathname.split("/")[3]}'s connections`;
-	} else if(domain === `/modder/${document.location.pathname.split("/")[3]}/statistics`) {
+	} else if(pathname === `/modder/${document.location.pathname.split("/")[3]}/statistics`) {
 		presenceData.details = "View my Application Statistics";
-	} else if(domain === `/modder/${document.location.pathname.split("/")[3]}/moderation/queue`) {
+	} else if(pathname === `/modder/${document.location.pathname.split("/")[3]}/moderation/queue`) {
 		presenceData.details = "Manage moderations for my Application";
-	} else if(domain ===`/modder/${document.location.pathname.split("/")[3]}/settings/basic`) {
+	} else if(pathname ===`/modder/${document.location.pathname.split("/")[3]}/settings/basic`) {
 		presenceData.details = "Manage Settings for my Application";
-	} else if(domain === `/modder/${document.location.pathname.split("/")[3]}/settings/members`) {
-		presenceData.details = "Manage members for my Application";
+	} else if(pathname === `/modder/${document.location.pathname.split("/")[3]}/settings/members`) {
+		presenceData.details = "Manage Members for my Application";
 	} else {
 		presenceData.details = "Nothing Found";
 	}
