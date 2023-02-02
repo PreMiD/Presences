3cfdca4a1ef4b735674cef853bbf21e7816710ff
Lel Illumina <dragonbilal938@outlook.com>
2023-02-02 22:01:28 +0500

    fix: change logo link

--- websites/K/Kemono.Party/presence.ts
+++ websites/K/Kemono.Party/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	let presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/mwpEkf3.png",
+		largeImageKey: "https://i.imgur.com/jdXZ8kV.png",
 		startTimestamp: browsingTimestamp,
 	};
 
