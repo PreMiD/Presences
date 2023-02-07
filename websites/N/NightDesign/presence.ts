0622ba4485209e1813105e672d61df100592c67c
Fabio S <bccs.team@gmail.com>
2023-02-07 19:12:54 +0100

    feat(NightDesign): init night.design presence with all requested fixes

--- websites/N/NightDesign/presence.ts
+++ websites/N/NightDesign/presence.ts
@@ -18,17 +18,20 @@ const presence = new Presence({
 
 		stateResources: "Type: $0 | Creator: $1",
 	},
-	start = Math.round(Date.now() / 1000);
+	browsingTimestamp = Math.round(Date.now() / 1000);
+let lastPath: string | undefined;
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/A9934OD.png",
-		startTimestamp: start,
-	};
-
 	let path: string = window.location.pathname;
 	const maybeSupportedPageLanguage: string = path.split("/")[1];
 	if (maybeSupportedPageLanguage.length === 2)
 		path = path.replace(`/${maybeSupportedPageLanguage}`, "");
+
+	if (lastPath === path) return;
+
+	const presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/cspBuGx.png",
+		startTimestamp: browsingTimestamp,
+	};
 
 	if (path !== "/home") {
 		presenceData.buttons = [
@@ -41,14 +44,14 @@ presence.on("UpdateData", async () => {
 
 	const pathSegments: string[] = path.split("/").filter(segment => segment);
 	for (let i = 0; i < pathSegments.length; i++) {
-		const pathSegement: string = pathSegments
+		const pathSegment: string = pathSegments
 				.slice(0, pathSegments.length - i)
 				.join("/"),
-			data = strings[pathSegement];
+			data = strings[pathSegment];
 
 		if (!data) continue;
 
-		switch (pathSegement) {
+		switch (pathSegment) {
 			case "resources":
 				if (pathSegments.length !== 2) {
 					presenceData.details = strings.resources;
@@ -58,7 +61,7 @@ presence.on("UpdateData", async () => {
 				presenceData.details = strings.resource.replace(
 					"$0",
 					textContent(
-						"body > app-root > div.router-content > app-resource > div.page-header > div > div > div.col-md-4 > ul > li:nth-child(4) > a"
+						"div.page-header > div > div > div.col-md-4 > ul > li:nth-child(4) > a"
 					)
 				);
 				presenceData.state = strings.stateResources
@@ -66,15 +69,13 @@ presence.on("UpdateData", async () => {
 						"$0",
 						toTitleCase(
 							textContent(
-								"body > app-root > div.router-content > app-resource > div.page-content > div.row > div.col-md-7 > div:nth-child(2) > div.card-block > p > code:nth-child(1)"
+								"div.page-content > div.row > div.col-md-7 > div:nth-child(2) > div.card-block > p > code:nth-child(1)"
 							)
 						)
 					)
 					.replace(
 						"$1",
-						textContent(
-							"body > app-root > div.router-content > app-resource > div.page-content > div.row > div.col-md-7 > div.card.mini-profile > div > div > h6 > span > span"
-						)
+						textContent("div.card.mini-profile > div > div > h6 > span > span")
 					);
 				break;
 
@@ -82,7 +83,7 @@ presence.on("UpdateData", async () => {
 				presenceData.details = data.replace(
 					"$0",
 					textContent(
-						"body > app-root > div.router-content > app-server-viewer > div.page-header > div > div > div.col-md-4 > ul > li:nth-child(3)"
+						"div.page-header > div > div > div.col-md-4 > ul > li:nth-child(3)"
 					)
 				);
 				presenceData.state = toTitleCase(pathSegments.slice(-1)[0]);
@@ -91,9 +92,7 @@ presence.on("UpdateData", async () => {
 			case "u":
 				presenceData.details = data.replace(
 					"$0",
-					textContent(
-						"body > app-root > div.router-content > app-user > div.page-header > div > div > div.col-md-4 > ul > li:nth-child(3) > a"
-					)
+					textContent("div.page-header > div > div > div.col-md-8 > div > h5")
 				);
 				presenceData.state = toTitleCase(pathSegments.slice(-1)[0]);
 				break;
@@ -110,6 +109,7 @@ presence.on("UpdateData", async () => {
 			strings.prefix + presenceData.details + strings.suffix;
 	}
 
+	lastPath = path;
 	presence.setActivity(presenceData);
 });
 function toTitleCase(str: string) {
