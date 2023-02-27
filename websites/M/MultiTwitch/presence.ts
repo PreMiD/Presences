a20b121a1779cda6c13ff2d8f8f6737881da46d7
Kenneth Sweezy <KennethSweezy@gmail.com>
2023-02-27 18:43:07 -0500

    style(multitwitch): ran yarn format
    Linted code

--- websites/M/MultiTwitch/presence.ts
+++ websites/M/MultiTwitch/presence.ts
@@ -1,23 +1,26 @@
 const presence = new Presence({
-	clientId: "1077743931460177972",
-}),
-watchingTimestamp = Math.floor(Date.now() / 1000);
+		clientId: "1077743931460177972",
+	}),
+	watchingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-const presenceData: PresenceData = {
-	largeImageKey: "https://i.imgur.com/gJBDsCE.png",
-	startTimestamp: watchingTimestamp,
-};
+	const presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/gJBDsCE.png",
+		startTimestamp: watchingTimestamp,
+	},
 
-let currentStreams = document.location.pathname.split("/").filter(function(str) {
-	return str !== "";
-});
-if(currentStreams.length === 0) 
-	presenceData.details = "On the home screen";
- else 
-	presenceData.details = `Watching ${currentStreams.length} ${currentStreams.length > 1 ? "streams" : "stream"}`;
+	 currentStreams = document.location.pathname
+		.split("/")
+		.filter(function (str) {
+			return str !== "";
+		});
+	if (currentStreams.length === 0) presenceData.details = "On the home screen";
+	else {
+presenceData.details = `Watching ${currentStreams.length} ${
+			currentStreams.length > 1 ? "streams" : "stream"
+		}`;
+}
 
-
-if (presenceData.details) presence.setActivity(presenceData);
-else presence.setActivity();
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
 });
