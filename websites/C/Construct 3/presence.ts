8509c8e2e99144f28ca2ea44e721e3f3b4b47454
Muratcan <muratcan9999@hotmail.com>
2023-02-28 21:43:25 +0900

    fixed linting
    Signed-off-by: Muratcan <muratcan9999@hotmail.com>

--- websites/C/Construct 3/presence.ts
+++ websites/C/Construct 3/presence.ts
@@ -1,35 +1,27 @@
 const presence = new Presence({
 		clientId: "1080079619367108729",
 	}),
-	strings = presence.getStrings({
-		play: "presence.playback.playing",
-		pause: "presence.playback.paused",
-	});
-
-var timestamp = Math.floor(Date.now() / 1000);
+	timestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-	var projectName = document.title.replace(" - Construct 3", "");
-	const element = document.querySelector("#propid2");
-	var value = "";
+	const projectName = document.title.replace(" - Construct 3", ""),
+		element = document.querySelector("#propid2");
+	let version = "";
 	if (element instanceof HTMLInputElement) {
-		value = element.value;
+		const { value: elementValue } = element;
+		version = elementValue;
 	}
-	var projectVersion = value;
-	if (projectVersion != "" && projectVersion != undefined) {
-		projectVersion = " | v:" + projectVersion;
-	}
+	if (version !== "") version = ` | v:${version}`;
+
 	const presenceData: PresenceData = {
 		largeImageKey: "editor",
 		startTimestamp: timestamp,
 		details:
-			projectName == "Game Making Software"
+			projectName === "Game Making Software"
 				? "No Project"
 				: "Editing a Project",
 		state:
-			projectName == "Game Making Software"
-				? "Idle"
-				: projectName + projectVersion,
+			projectName === "Game Making Software" ? "Idle" : projectName + version,
 	};
 
 	presence.setActivity(presenceData);
