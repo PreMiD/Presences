aa88256b3af67973fc5fed3fc09e646b5d682fca
Kenneth Sweezy <KennethSweezy@gmail.com>
2023-02-27 19:15:13 -0500

    style(multitwitch): rerun prettier

--- websites/M/MultiTwitch/presence.ts
+++ websites/M/MultiTwitch/presence.ts
@@ -5,21 +5,20 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/gJBDsCE.png",
-		startTimestamp: watchingTimestamp,
-	},
-
-	 currentStreams = document.location.pathname
-		.split("/")
-		.filter(function (str) {
-			return str !== "";
-		});
+			largeImageKey: "https://i.imgur.com/gJBDsCE.png",
+			startTimestamp: watchingTimestamp,
+		},
+		currentStreams = document.location.pathname
+			.split("/")
+			.filter(function (str) {
+				return str !== "";
+			});
 	if (currentStreams.length === 0) presenceData.details = "On the home screen";
 	else {
-presenceData.details = `Watching ${currentStreams.length} ${
+		presenceData.details = `Watching ${currentStreams.length} ${
 			currentStreams.length > 1 ? "streams" : "stream"
 		}`;
-}
+	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
