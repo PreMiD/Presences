72c95698f33e715ae86f20f331bb27c1a4d3072b
BySuspect <omerbayraktar43@gmail.com>
2023-03-06 16:42:19 +0300

    fix(TempestFansub): domain update and some changes

--- websites/T/Tempest Fansub/presence.ts
+++ websites/T/Tempest Fansub/presence.ts
@@ -9,131 +9,119 @@ presence.on("UpdateData", async () => {
 			largeImageKey: "https://i.imgur.com/ppTaAAl.png",
 		},
 		searchParams = new URLSearchParams(location.search);
-	//Dino
-	if (pathname === "/dinozor-oyunu/") presenceData.details = "Dino oynuyor.";
-	//arama
-	else if (searchParams.get("s")) {
-		if (hostname === "tempestfansub.com")
-			presenceData.details = "Bir anime arıyor.";
-		if (hostname === "manga.tempestfansub.com")
+	if (hostname === "tempestscans.com") {
+		//Manga----------------------------------------------------------------------------------------------------------
+		if (searchParams.get("s")) {
 			presenceData.details = "Bir manga arıyor.";
-		presenceData.state = searchParams.get("s");
-		//Animeler
-	} else if (
-		(hostname === "tempestfansub.com" && pathname === "/") ||
-		pathname.endsWith("/anime/")
-	)
-		presenceData.details = "Animelere göz atıyor.";
-	//Bir Anime
-	else if (!pathname.endsWith("/anime/") && pathname.includes("/anime/")) {
-		presenceData.details = "Bir animeye bakıyor.";
-		presenceData.state = document
-			.querySelector("h1.entry-title")
-			?.textContent.trim();
-		const imgs = document.querySelectorAll(
-			"#content > div > div > article > div:nth-child(2) > div > div.thumbook > div > img"
-		);
-		for (const element of imgs) {
-			if (element.getAttribute("src"))
-				presenceData.largeImageKey = element.getAttribute("src");
-		}
-		//Mangalar
-	} else if (
-		hostname.startsWith("manga.") &&
-		(pathname.endsWith("/manga/") || pathname.endsWith("/anasayfa/"))
-	)
-		presenceData.details = "Mangalara göz atıyor.";
-	//Bir Manga
-	else if (pathname.includes("/manga/")) {
-		presenceData.details = "Bir mangaya bakıyor.";
-		presenceData.state = document
-			.querySelector("h1.entry-title")
-			?.textContent.trim();
-		presenceData.buttons = [
-			{
-				label: "Mangayı Aç",
-				url: href,
-			},
-		];
-		const imgs = document.querySelectorAll(
-			"#content > div > div > article > div:nth-child(2) > div > div.thumbook > div > img"
-		);
-		for (const element of imgs) {
-			if (element.getAttribute("src"))
-				presenceData.largeImageKey = element.getAttribute("src");
-		}
-		//manga a-z listesi
-	} else if (pathname.includes("/a-z/"))
-		presenceData.details = "Manga A-Z Listesine bakıyor.";
-	//Anime a-z listesi
-	else if (pathname.includes("/az-listesi/"))
-		presenceData.details = "Anime A-Z Listesine bakıyor.";
-	//Anime tür listesi
-	else if (
-		pathname.includes("/turler/") ||
-		pathname.includes("/genres/") ||
-		pathname.includes("/siniflandirma/")
-	) {
-		presenceData.details = "Anime Türler Listesine bakıyor.";
-		if (pathname.includes("/genres/")) {
+			presenceData.state = searchParams.get("s");
+			//mangalar
+		} else if (pathname.endsWith("/manga/") || pathname === "/")
+			presenceData.details = "Mangalara göz atıyor.";
+		//Bir Manga
+		else if (pathname.includes("/manga/")) {
+			presenceData.details = "Bir mangaya bakıyor.";
 			presenceData.state = document
-				.querySelector("#content > div > div > div > div > h1")
+				.querySelector("h1.entry-title")
 				?.textContent.trim();
-		} //Kitaplık
-	} else if (pathname.includes("/kitaplik/"))
-		presenceData.details = "Kitaplığa bakıyor.";
-	//Takvim
-	else if (pathname.includes("/takvim/"))
-		presenceData.details = "Takvime bakıyor.";
-	//Anime EkipBaşvuru
-	else if (pathname.includes("/basvuru/"))
-		presenceData.details = "Anime ekip başvurusuna bakıyor.";
-	//Manga EkipBaşvuru
-	else if (pathname.includes("/manga-cevirmen-editor-basvuru/"))
-		presenceData.details = "Manga ekip başvurusuna bakıyor.";
-	//Manga okuyor
-	else if (
-		!pathname.includes("/manga/") &&
-		hostname.startsWith("manga.") &&
-		!pathname.endsWith("/anasayfa/")
-	) {
-		try {
-			const title = document
-					.querySelector("h1.entry-title")
-					?.textContent.trim(),
-				tsplited = title.split(" "),
-				_manga = document.querySelectorAll("div.allc > a");
-			for (const element of _manga) {
-				if (element.getAttribute("href")) {
-					presenceData.buttons = [
-						{
-							label: "Bölümü Aç",
-							url: href,
-						},
-						{
-							label: "Mangayı Aç",
-							url: element.getAttribute("href"),
-						},
-					];
-					break;
-				}
-			}
-			presenceData.details = title.replace(tsplited[tsplited.length - 1], "");
-			presenceData.state = `Bölüm ${tsplited[tsplited.length - 1]} Okuyor.`;
-			const imgs = document.querySelectorAll("#readerarea > img");
+			presenceData.buttons = [
+				{
+					label: "Mangayı Aç",
+					url: href,
+				},
+			];
+			const imgs = document.querySelectorAll(
+				"#content > div > div > article > div:nth-child(2) > div > div.thumbook > div > img"
+			);
 			for (const element of imgs) {
-				if (
-					element.getAttribute("src") &&
-					element.getAttribute("data-index") === "0"
-				) {
+				if (element.getAttribute("src"))
 					presenceData.largeImageKey = element.getAttribute("src");
-					break;
-				}
 			}
-		} catch {
-			presenceData.details = "Bilinmeyen Adress.";
-		}
+			//manga a-z listesi
+		} else if (pathname.includes("/a-z/"))
+			presenceData.details = "Manga A-Z Listesine bakıyor.";
+		//Takvim
+		else if (pathname.includes("/takvim/"))
+			presenceData.details = "Takvime bakıyor.";
+		//kitaplik
+		else if (pathname.includes("/kitaplik/"))
+			presenceData.details = "Manga kitaplığına bakıyor.";
+		//Manga EkipBaşvuru
+		else if (pathname.includes("/manga-cevirmen-editor-basvuru-2/"))
+			presenceData.details = "Manga ekip başvurusuna bakıyor.";
+		//Manga Destek
+		else if (pathname.includes("/destek-ol/"))
+			presenceData.details = "Fansuba bağış yapıyor.";
+		//Manga okuyor
+		else if (document.querySelector("div.chapterbody")) {
+			try {
+				const title = document
+						.querySelector("h1.entry-title")
+						?.textContent.trim(),
+					tsplited = title.split(" "),
+					_manga = document.querySelectorAll("div.allc > a");
+				for (const element of _manga) {
+					if (element.getAttribute("href")) {
+						presenceData.buttons = [
+							{
+								label: "Bölümü Aç",
+								url: href,
+							},
+							{
+								label: "Mangayı Aç",
+								url: element.getAttribute("href"),
+							},
+						];
+						break;
+					}
+				}
+				presenceData.details = title.replace(tsplited[tsplited.length - 1], "");
+				presenceData.state = `Bölüm ${tsplited[tsplited.length - 1]} Okuyor.`;
+			} catch {
+				presenceData.details = "Bilinmeyen Adress.";
+			}
+		} else presenceData.details = "Bilinmeyen Adress.";
+	} else if (hostname === "tempestfansub.com") {
+		//Anime----------------------------------------------------------------------------------------------------------
+		if (searchParams.get("s")) {
+			presenceData.details = "Bir anime arıyor.";
+			presenceData.state = searchParams.get("s");
+			//animeler
+		} else if (pathname === "/" || pathname.endsWith("/anime/"))
+			presenceData.details = "Animelere göz atıyor.";
+		//bir anime
+		else if (!pathname.endsWith("/anime/") && pathname.includes("/anime/")) {
+			presenceData.details = "Bir animeye bakıyor.";
+			presenceData.state = document
+				.querySelector("h1.entry-title")
+				?.textContent.trim();
+			const imgs = document.querySelectorAll(
+				"#content > div > div > article > div:nth-child(2) > div > div.thumbook > div > img"
+			);
+			for (const element of imgs) {
+				if (element.getAttribute("src"))
+					presenceData.largeImageKey = element.getAttribute("src");
+			}
+			//Anime a-z listesi
+		} else if (pathname.includes("/az-listesi/"))
+			presenceData.details = "Anime A-Z Listesine bakıyor.";
+		//Anime tür listesi
+		else if (
+			pathname.includes("/turler/") ||
+			pathname.includes("/genres/") ||
+			pathname.includes("/siniflandirma/")
+		) {
+			presenceData.details = "Anime Türler Listesine bakıyor.";
+			if (pathname.includes("/genres/")) {
+				presenceData.state = document
+					.querySelector("#content > div > div > div > div > h1")
+					?.textContent.trim();
+			}
+		} else if (pathname.includes("/kitaplik/"))
+			presenceData.details = "Anime kitaplığına bakıyor.";
+		//Anime EkipBaşvuru
+		else if (pathname.includes("/basvuru/"))
+			presenceData.details = "Anime ekip başvurusuna bakıyor.";
+		else presenceData.details = "Bilinmeyen Adress.";
 	} else presenceData.details = "Bilinmeyen Adress.";
-
 	presence.setActivity(presenceData);
 });
