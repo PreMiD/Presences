fe30f8dd62e02c05417b31e893fd34e0950d1013
Kuriel <37979213+ipaddysz@users.noreply.github.com>
2023-03-08 21:58:24 +0000

    feat: add presence

--- websites/T/TabNews/presence.ts
+++ websites/T/TabNews/presence.ts
@@ -1,13 +1,94 @@
 const presence = new Presence({
-		clientId: "",
+		clientId: "1083051669996187748",
 	}),
-	strings = presence.getStrings({
-		play: "presence.playback.playing",
-		pause: "presence.playback.paused",
-	});
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {};
+	const presenceData: PresenceData = {
+			details: "Outros",
+			largeImageKey: "https://i.imgur.com/pQeO9sn.png",
+			startTimestamp: browsingTimestamp,
+			buttons: [
+				{
+					label: "Ver conteúdo",
+					url: document.location.href,
+				},
+			],
+		},
+		urlpath = document.location.pathname.split("/"),
+		currentCount = await presence.getSetting<boolean>("count");
 
-	presence.setActivity(presenceData);
+	switch (true) {
+		case !urlpath[1] || urlpath[1] === "pagina":
+			presenceData.details = "Vendo conteúdos relevantes";
+			if (urlpath[1] === "pagina") presenceData.state = `Página ${urlpath[2]}`;
+			break;
+		case urlpath[1].startsWith("recentes"):
+			presenceData.details = "Vendo conteúdos recentes";
+			if (urlpath[3]) presenceData.state = `Página ${urlpath[3]}`;
+			break;
+		default:
+			if (urlpath[1] && !urlpath[2]) {
+				switch (urlpath[1]) {
+					case "publicar":
+						presenceData.details = "Publicando novo conteúdo";
+						break;
+					case "perfil":
+						presenceData.details = "Editando perfil";
+						break;
+					case "contato":
+						presenceData.details = "Vendo os contatos";
+						break;
+					case "termos-de-uso":
+						presenceData.details = "Lendo os termos de uso";
+						break;
+					case "status":
+						presenceData.details = "Vendo estatísticas do site";
+						break;
+					case "museu":
+						presenceData.details = "Admirando o museu";
+						break;
+					case "cadastro":
+						presenceData.details = "Fazendo o cadastro";
+						break;
+					case "login":
+						presenceData.details = "Fazendo o login";
+						break;
+					default:
+						if (document.querySelector<HTMLHeadingElement>("h1")) {
+							presenceData.details = "Vendo perfil de:";
+							presenceData.state =
+								document.querySelector<HTMLHeadingElement>("h1")?.textContent;
+						} else presenceData.details = "Vendo outros conteúdos";
+						break;
+				}
+			}
+			if (urlpath[2]) {
+				switch (urlpath[2]) {
+					case "pagina": {
+						presenceData.details = `Vendo perfil de ${urlpath[1]}`;
+						presenceData.state = `Página ${urlpath[3]}`;
+						break;
+					}
+					case "rss":
+						presenceData.details = "Vendo o Feed RSS";
+						break;
+					default:
+						if (!document.querySelector<HTMLHeadingElement>("h1")) {
+							presenceData.details = "Lendo resposta a:";
+							presenceData.state =
+								document.querySelector("strong")?.textContent;
+						} else {
+							presenceData.details = "Lendo o conteúdo:";
+							presenceData.state =
+								document.querySelector<HTMLHeadingElement>("h1")?.textContent;
+						}
+						break;
+				}
+				break;
+			}
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
 });
