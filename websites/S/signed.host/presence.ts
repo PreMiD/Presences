accaa9efe81e68b540644c45e4fb9da27eaba104
Panagiotis Kalos <42353172+malwarepad@users.noreply.github.com>
2023-03-11 11:14:40 +0200

    Fixes that were asked

--- websites/S/signed.host/presence.ts
+++ websites/S/signed.host/presence.ts
@@ -2,13 +2,13 @@ const presence = new Presence({
 	clientId: "1000664778404016230",
 });
 
-let lastUnique: string | undefined,
-	lastTimestamp: number | undefined = Date.now();
+let lastUnique: string,
+	lastTimestamp = Date.now();
 
 presence.on("UpdateData", async () => {
 	if (document.querySelector(".next-error-h1")) return presence.clearActivity();
 
-	const { pathname, hostname } = document.location;
+	const { pathname, hostname, href } = document.location;
 
 	if (lastUnique !== pathname) {
 		lastUnique = pathname;
@@ -16,106 +16,81 @@ presence.on("UpdateData", async () => {
 	}
 
 	let details = `Currently on ${pathname}`,
-		buttonLabel = "Visit page",
-		buttonURL = document.location.toString(),
+		buttonLabel = null,
+		buttonURL = null,
 		state = null;
 
-	const loggedUsername = document.querySelector("#premid-username")?.innerHTML;
+	const loggedUsername =
+			document.querySelector("#premid-username")?.textContent,
+		pages: Record<string, PresenceData> = {
+			"/": { details: "Viewing the front page" },
+			"/terms": { details: "Reading the terms of the service" },
+			"/login": { details: "Logging into their dashboard" },
+
+			"/dash": { details: "Watching their general statistics" },
+			"/dash/service/account": { details: "Editing account information" },
+			"/dash/service/domains": { details: "Changing their domain" },
+			"/dash/service/settings": {
+				details: "Configuring their upload preferences",
+			},
+			"/dash/service/subscription": { details: "Viewing subscription options" },
+			"/dash/service/gallery": { details: "Viewing their image gallery" },
+			"/dash/service/warns": { details: "Viewing their warns" },
+			"/dash/service/invites": { details: "Viewing their invite inventory" },
+			"/dash/service/upload": {
+				details: "Uploading file(s) with the web uploader",
+			},
+			"/dash/service/email": { details: "Configuring their email preferences" },
+			"/dash/connections/discord": {
+				details: "Modifying their Discord connection",
+			},
+			"/dash/bio/customize": { details: "Customizing their bio page" },
+			"/dash/bio/css": { details: "Editing their custom CSS" },
+		};
 
 	switch (hostname) {
 		case "signed.host":
 			if (pathname.includes("/dash")) {
-				loggedUsername
-					? (state = `Logged in as ${loggedUsername}`)
-					: (state = "Not logged in!");
-				buttonLabel = "Join signed.host";
-				buttonURL = "https://signed.host/";
+				if (loggedUsername) state = `Logged in as ${loggedUsername}`;
+				else state = "Not logged in!";
 			}
-			switch (pathname) {
-				// Design/Frontend routes
-				case "/":
-					details = "Viewing the front page";
-					break;
-				case "/terms":
-					details = "Reading the terms of the service";
-					break;
 
-				case "/login":
-					details = "Logging into their dashboard";
-					buttonLabel = "Join signed.host";
-					buttonURL = "https://signed.host/";
-					break;
+			details = pages[pathname]?.details;
 
-				// Dashboard routes
-				case "/dash":
-					details = "Watching their general statistics";
-					break;
-				case "/dash/service/account":
-					details = "Editing account information";
-					break;
-				case "/dash/service/domains":
-					details = "Changing their domain";
-					break;
-				case "/dash/service/settings":
-					details = "Configuring their upload preferances";
-					break;
-				case "/dash/service/subscription":
-					details = "Viewing subscription options";
-					break;
-				case "/dash/service/gallery":
-					details = "Viewing their image gallery";
-					break;
-				case "/dash/service/warns":
-					details = "Viewing their warns";
-					break;
-				case "/dash/service/invites":
-					details = "Viewing their invite inventory";
-					break;
-				case "/dash/service/upload":
-					details = "Uploading file(s) with the web uploader";
-					break;
-				case "/dash/service/email":
-					details = "Configuring their email preferances";
-					break;
-				case "/dash/connections/discord":
-					details = "Modifying their Discord connection";
-					break;
-				case "/dash/bio/customize":
-					details = "Customizing their bio page";
-					break;
-				case "/dash/bio/css":
-					details = "Editing their custom CSS";
-					break;
-			}
 			break;
 		case "bio.signed.host":
-			if (pathname === "/") details = "Viewing the front bio page";
-			else {
-				details = `Viewing ${
-					document
-						.querySelector("#premid-username")
-						?.innerHTML?.replace(/#.*/g, "") ?? "someone"
-				}'s bio page`;
-				state = `${
-					document.querySelector("#premid-status")?.innerHTML ?? "unknown"
-				} | ${
-					parseInt(document.querySelector("#premid-bioviews")?.innerHTML) ||
-					0 > 0
-						? document.querySelector("#premid-bioviews")?.innerHTML
-						: "no"
-				} views | uid ${
-					document.querySelector("#premid-uid")?.innerHTML ?? "no"
-				}`;
-				buttonLabel = "Visit bio";
+			if (pathname === "/") {
+				details = "Viewing the front bio page";
+				return;
 			}
+			details = `Viewing ${
+				document
+					.querySelector("#premid-username")
+					?.textContent?.replace(/#.*/g, "") ?? "someone"
+			}'s bio page`;
+			state = `${
+				document.querySelector("#premid-status")?.textContent ?? "unknown"
+			} | ${
+				parseInt(document.querySelector("#premid-bioviews")?.textContent) ||
+				0 > 0
+					? document.querySelector("#premid-bioviews")?.textContent
+					: "no"
+			} views | uid ${
+				document.querySelector("#premid-uid")?.textContent ?? "no"
+			}`;
+			buttonLabel = "Visit Bio";
+			buttonURL = href;
 			break;
 	}
 
 	presence.setActivity({
-		largeImageKey: "logo",
+		largeImageKey: "https://i.imgur.com/lpLWPa4.png",
 		state,
 		startTimestamp: lastTimestamp,
 		details,
-		buttons: [{ label: buttonLabel, url: buttonURL }],
+		buttons:
+			buttonLabel && buttonURL
+				? [{ label: buttonLabel, url: buttonURL }]
+				: null,
 	});
 });
