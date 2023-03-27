6736a3c3954d84aafbcbb95427782debb57f2a32
theusaf <theusafyt@gmail.com>
2023-03-27 09:38:35 -0700

    feat: add details for nav links

--- websites/H/Hacker News/presence.ts
+++ websites/H/Hacker News/presence.ts
@@ -5,9 +5,63 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/xWtIX5I.png",
-		startTimestamp: browsingTimestamp,
-	};
+			largeImageKey: "https://i.imgur.com/xWtIX5I.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname, href, search } = document.location,
+		searchParams = new URLSearchParams(search);
 
-	presence.setActivity(presenceData);
+	switch (pathname) {
+		case "/":
+		case "/news": {
+			presenceData.details = "Browsing the main feed";
+			break;
+		}
+		case "/newest": {
+			presenceData.details = "Browsing the newest threads";
+			break;
+		}
+		case "/threads": {
+			presenceData.details = "Browsing threads by a user";
+			presenceData.state = searchParams.get("id");
+			break;
+		}
+		case "/front": {
+			presenceData.details = "Browsing past threads";
+			presenceData.state = document.querySelector<HTMLDivElement>(
+				"#pagespace + tr div"
+			).firstChild.textContent;
+			break;
+		}
+		case "newcomments": {
+			presenceData.details = "Browsing new comments";
+			break;
+		}
+		case "ask": {
+			presenceData.details = "Browsing Ask HN threads";
+			break;
+		}
+		case "show": {
+			presenceData.details = "Browsing Show HN threads";
+			break;
+		}
+		case "jobs": {
+			presenceData.details = "Browsing job threads";
+			break;
+		}
+		case "submit": {
+			presenceData.details = "Creating a new thread";
+			presenceData.state =
+				document.querySelector<HTMLInputElement>("[name='title']").value;
+			break;
+		}
+		case "user": {
+			presenceData.details = "Viewing a user's profile";
+			presenceData.state = searchParams.get("id");
+			break;
+		}
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
 });
