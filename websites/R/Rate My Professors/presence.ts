79ebef134f4b113831249d18c00ec32db9af0b70
theusaf <theusafyt@gmail.com>
2023-03-27 08:44:38 -0700

    feat: add school page

--- websites/R/Rate My Professors/presence.ts
+++ websites/R/Rate My Professors/presence.ts
@@ -3,11 +3,40 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
+enum Assets {
+	Info = "https://i.imgur.com/eIFVRHe.png",
+	Logo = "https://i.imgur.com/zN7rsOi.png",
+}
+
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/zN7rsOi.png",
-		startTimestamp: browsingTimestamp,
-	};
+			largeImageKey: Assets.Logo,
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname, hostname } = document.location,
+		pathList = pathname.split("/").filter(x => x !== "");
+
+	if (hostname === "www.ratemyprofessors.com") {
+		switch (pathList[0]) {
+			case "school": {
+				presenceData.details = "Viewing a school";
+				presenceData.state = document.querySelector<HTMLDivElement>(
+					"[class*='StyledTitleName']"
+				).textContent;
+				presenceData.smallImageKey = Assets.Info;
+				presenceData.smallImageText = `Quality: ${
+					document.querySelector<HTMLDivElement>(
+						"[class*='OverallRating__Number']"
+					).textContent
+				}`;
+				break;
+			}
+			default: {
+				presenceData.details = "Browsing";
+			}
+		}
+	} else if (hostname === "help.ratemyprofessors.com") {
+	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
