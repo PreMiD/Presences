37f19d8d8f6b9443cb10dc82cbc93e728b8765c8
Mustafa Pxsty KÖK <mustafakok2020@mail.com.tr>
2023-04-03 18:33:03 +0300

    Update presence.ts

--- websites/T/Teknofest/presence.ts
+++ websites/T/Teknofest/presence.ts
@@ -1,20 +1,18 @@
 const presence = new Presence({
-    clientId: "903957474489548813"
-  });
-  
-  presence.on("UpdateData", () => {
-    const presenceData: PresenceData = {
-        largeImageKey: "logo"
-      },
-      searchURL = new URL(document.location.href),
-      searchResult = searchURL.searchParams.get("q"),
-      searchCategory = searchURL.searchParams.get("k");
-    if (window.location.pathname.startsWith("/")) {
-      presenceData.details = "Bir sayfa görüntülüyor:";
-      presenceData.state = "Teknofest";
-    } 
-    if (!presenceData.details) {
-      presence.setTrayTitle();
-      presence.setActivity();
-    } else presence.setActivity(presenceData);
-  });
+	clientId: "903957474489548813",
+});
+
+presence.on("UpdateData", () => {
+	const presenceData: PresenceData = {
+		largeImageKey: "logo",
+	};
+
+	if (window.location.pathname.startsWith("/")) {
+		presenceData.details = "Bir sayfa görüntülüyor:";
+		presenceData.state = "Teknofest";
+	}
+	if (!presenceData.details) {
+		presence.setTrayTitle();
+		presence.setActivity();
+	} else presence.setActivity(presenceData);
+});
