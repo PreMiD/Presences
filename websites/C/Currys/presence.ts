29ddc8af374e3d8ba8608e9c01cb5d9465224bd0
xFutuxe <65718499+xFutuxe@users.noreply.github.com>
2023-04-04 12:49:09 +0100

    Resolved all suggested changes
    added imgur links to both key images
    'pathname' is now a const in 'updateData'
    removed 'if host statement'
    added record and switch statements
    
    
    Signed-off-by: xFutuxe <65718499+xFutuxe@users.noreply.github.com>

--- websites/C/Currys/presence.ts
+++ websites/C/Currys/presence.ts
@@ -1,80 +1,60 @@
 const presence = new Presence({
 		clientId: "996516483150663721",
 	}),
-	{ pathname, host } = document.location,
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-	const presenceData = {
-		smallImageKey: "search",
+	let presenceData = {
+		smallImageKey: "https://i.imgur.com/B5iUj20.png",
 		largeImageKey: "https://i.imgur.com/yOolF4f.png",
 		largeImageText: "Browsing Currys",
 		startTimestamp: browsingTimestamp,
 		details: "Browsing",
 		state: "Currys Website",
 	};
-	if (host === "www.currys.co.uk") {
-		presenceData.startTimestamp = browsingTimestamp;
-		presenceData.state = "Homepage";
+	const { pathname } = document.location;
+
+	switch (true) {
+		case !!document.querySelector(".amp-herobanner-headline"):
+			presenceData.state = document.querySelector(
+				".amp-herobanner-headline"
+			).textContent;
+			break;
+		case !!document.querySelector(".plp-heading"):
+			presenceData.state = document.querySelector(".plp-heading").textContent;
+			break;
+		case !!document.querySelector("#clp-row-1 > div > div > h1"):
+			presenceData.state = document.querySelector(
+				"#clp-row-1 > div > div > h1"
+			).textContent;
+			break;
+		case !!document.querySelector("#clp-row-1 > div > h1"):
+			presenceData.state = document.querySelector(
+				"#clp-row-1 > div > h1"
+			).textContent;
+			break;
+		case pathname.includes("/products/") &&
+			!!document.querySelector(".product-name"):
+			presenceData.state = document.querySelector(".product-name").textContent;
+			break;
+		default:
+			break;
 	}
 
-	// Product categories / Sub-categories
-	if (document.querySelector(".amp-herobanner-headline")) {
-		presenceData.state = document.querySelector(
-			".amp-herobanner-headline"
-		).textContent;
-	}
-	if (document.querySelector(".plp-heading"))
-		presenceData.state = document.querySelector(".plp-heading").textContent;
+	const pages: Record<string, PresenceData> = {
+		"/account": { details: "Viewing", state: "My Account" },
+		"/wishlist-show": { details: "Viewing", state: "Wishlist" },
+		"/order": { details: "Viewing", state: "Orders" },
+		"/manage-your-details": { details: "Managing", state: "Account Details" },
+		"/cart": { details: "Viewing", state: "Basket" },
+		"/store-finder": { details: "Finding", state: "Stores" },
+		"/services/": { details: "Viewing", state: "Services" },
+	};
 
-	if (document.querySelector("#clp-row-1 > div > div > h1")) {
-		presenceData.state = document.querySelector(
-			"#clp-row-1 > div > div > h1"
-		).textContent;
-	} else if (document.querySelector("#clp-row-1 > div > h1")) {
-		presenceData.state = document.querySelector(
-			"#clp-row-1 > div > h1"
-		).textContent;
-	}
+	for (const [path, data] of Object.entries(pages))
+		if (pathname.includes(path)) presenceData = { ...presenceData, ...data };
 
-	// Product page
-	if (
-		pathname.includes("/products/") &&
-		document.querySelector(".product-name")
-	)
-		presenceData.state = document.querySelector(".product-name").textContent;
-
-	// Account related pages
-	if (pathname.includes("/my-account")) {
-		if (pathname.includes("/saved-for-later")) {
-			presenceData.details = "Viewing";
-			presenceData.state = "Wishlist";
-		} else if (pathname.includes("/orders")) {
-			presenceData.details = "Viewing";
-			presenceData.state = "Orders";
-		} else if (pathname.includes("/manage-your-details")) {
-			presenceData.details = "Managing";
-			presenceData.state = "Account details";
-		} else if (pathname.includes("/login")) {
-			presenceData.details = "Viewing";
-			presenceData.state = "Sign in page";
-		} else if (pathname.includes("/app/basket")) {
-			presenceData.details = "Viewing";
-			presenceData.state = "Basket";
-		}
-	}
-
-	if (pathname.includes("/store-finder")) {
-		presenceData.details = "Finding";
-		presenceData.state = "Stores";
-	} else if (pathname.includes("/gbuk/techtalk")) {
-		// TechTalk articles
-		presenceData.state = "TechTalk Articles";
-	}
-
-	// Overall services
-	if (pathname.includes("/services/")) presenceData.state = "Services";
-
+	// Search Term
 	if (location.href.includes("search")) {
 		// Searching product
 		const replaced = document
