4078f4d1d734cb7b5768eef1816850eae63cd792
Alex Gokhale <hello@alexgokhale.com>
2023-04-10 13:00:27 +0100

    feat(3388): add watch functionality

--- websites/0-9/3388/presence.ts
+++ websites/0-9/3388/presence.ts
@@ -4,16 +4,52 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "logo",
-		smallImageKey: "search",
-	};
+			largeImageKey: "https://i.imgur.com/FhGHKTn.png",
+			smallImageKey: "search",
+			smallImageText: "Browsing..."
+		},
+		{ pathname } = document.location;
 
-	if (document.location.pathname.includes("/detail/")) {
+	if (pathname.includes("/detail/")) {
+		const filmItems = document.querySelectorAll(".fd-item"),
+			coverElement = document.querySelector<HTMLElement>(".detail-film-cover");
+
+		let name = document.querySelector(".film-name").textContent;
+
+		if (filmItems.length >= 3 && !Number.isNaN(parseInt(filmItems[2].textContent)))
+			name += ` (${filmItems[2].textContent})`;
+
+		if (coverElement.style.backgroundImage.startsWith("url(\"") && coverElement.style.backgroundImage.endsWith("\")"))
+			presenceData.largeImageKey = coverElement.style.backgroundImage.slice(5, coverElement.style.backgroundImage.length - 2);
+
 		presenceData.details = `Viewing ${!document.querySelector("#season-list") ? "a Movie..." : "a TV Show..."}`;
-		presenceData.state = document.querySelector(".film-name").textContent;
-	} else if (document.location.pathname === "/movies")
+		presenceData.state = name;
+	} else if (pathname.includes("/watch/")) {
+		const video = document.querySelector<HTMLVideoElement>("#player"),
+			[startTimestamp, endTimestamp] = presence.getTimestamps(
+				Math.floor(video.currentTime),
+				Math.floor(video.duration)
+			);
+
+		if (document.querySelector("#menu-eps")) {
+			presenceData.details = `${document.querySelector(".film-name").textContent} - ${document.querySelector(".cmecb-seasons ul li.active a").textContent}`;
+			presenceData.state = `E${Array.from(document.querySelector(".cmecb-epslist ul").children).findIndex((e) => e.classList.contains("active")) + 1}: ${document.querySelector(".cmecb-epslist ul li.active a").getAttribute("title")}`;
+		} else {
+			presenceData.details = document.querySelector("span.large").textContent;
+			presenceData.state = "Movie";
+		}
+
+		presenceData.largeImageKey = document.querySelector<HTMLImageElement>(".mbu-bg.blur-small img").src;
+		presenceData.smallImageKey = video.paused ? "pause" : "play";
+		presenceData.smallImageText = video.paused ? "Paused" : "Playing";
+
+		if (!video.paused) {
+			presenceData.startTimestamp = startTimestamp;
+			presenceData.endTimestamp = endTimestamp;
+		}
+	} else if (pathname === "/movies")
 		presenceData.details = "Browsing Movies...";
-	else if (document.location.pathname === "/tv-shows")
+	else if (pathname === "/tv-shows")
 		presenceData.details = "Browsing TV Shows...";
 	else
 		presenceData.details = "Browsing...";
