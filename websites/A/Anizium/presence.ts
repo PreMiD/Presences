6ea575d121fb807625ecf05801ae139d3e692786
Mert <89777525+DevJustForever@users.noreply.github.com>
2023-04-11 05:20:06 -0700

    update presence.
    Signed-off-by: Mert <89777525+DevJustForever@users.noreply.github.com>

--- websites/A/Anizium/presence.ts
+++ websites/A/Anizium/presence.ts
@@ -5,13 +5,11 @@ interface Video {
 }
 
 const presence = new Presence({
-	clientId: "1094616557763710976",
-}),
+		clientId: "1094616557763710976",
+	}),
 	strings = presence.getStrings({
 		viewHome: "general.viewHome",
-
 	});
-
 
 let video: Video;
 
@@ -22,16 +20,39 @@ presence.on("iFrameData", (msg: Video) => {
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey: "https://i.imgur.com/Z3G1Ubm.png",
-	};
+	},
 
-	let haberAd: string = document.querySelector("body > div.haberler > div > div > div:nth-child(7) > div > div > div.kart-header > h2")?.textContent.trim() || null;
-	let haberDetay: string = document.querySelector("body > div.haberler > div > div > div:nth-child(7) > div > div > div.kart-body > p:nth-child(4)")?.textContent.trim() || null;
+	 haberAd: string =
+		document
+			.querySelector(
+				"body > div.haberler > div > div > div:nth-child(7) > div > div > div.kart-header > h2"
+			)
+			?.textContent.trim() || null,
+	 haberDetay: string =
+		document
+			.querySelector(
+				"body > div.haberler > div > div > div:nth-child(7) > div > div > div.kart-body > p:nth-child(4)"
+			)
+			?.textContent.trim() || null,
 
-	let animeInfo: string = document.querySelector("body > div.container-fluid > div > div > div:nth-child(2) > div:nth-child(1) > span")?.textContent.trim() || null;
+	 animeInfo: string =
+		document
+			.querySelector(
+				"body > div.container-fluid > div > div > div:nth-child(2) > div:nth-child(1) > span"
+			)
+			?.textContent.trim() || null,
 
-	let profilAd: string = document.querySelector("body > div.container-fluid > div.row > div.col-md-10 > div:nth-child(6) > div:nth-child(1) > label")?.textContent.trim() || null;
+	 profilAd: string =
+		document
+			.querySelector(
+				"body > div.container-fluid > div.row > div.col-md-10 > div:nth-child(6) > div:nth-child(1) > label"
+			)
+			?.textContent.trim() || null;
 
-	if (window.location.pathname === "/" || window.location.pathname.startsWith("/index")) {
+	if (
+		window.location.pathname === "/" ||
+		window.location.pathname.startsWith("/index")
+	) {
 		presenceData.details = (await strings).viewHome;
 		presenceData.startTimestamp = Math.floor(Date.now() / 1000);
 	} else if (window.location.pathname.startsWith("/animeler")) {
@@ -42,33 +63,33 @@ presence.on("UpdateData", async () => {
 		presenceData.startTimestamp = Math.floor(Date.now() / 1000);
 	} else if (window.location.pathname.startsWith("/haberoku") && haberAd) {
 		presenceData.details = `Haber başlığı: ${haberAd}`;
-		if (haberDetay) {
+		if (haberDetay) 
 			presenceData.state = haberDetay;
-		};
+		
 		presenceData.buttons = [
 			{
 				label: "Haber",
 				url: document.URL,
 			},
-		]
+		];
 	} else if (window.location.pathname.startsWith("/paketler")) {
 		presenceData.details = "Paketlere bakıyor";
 		presenceData.startTimestamp = Math.floor(Date.now() / 1000);
 	} else if (window.location.pathname.startsWith("/hemenizle") && animeInfo) {
-		const epNum = animeInfo.match(/[0-9]+\.Bölüm/g) || animeInfo.match(/[0-9]+\. Bölüm/g);
-
+		const epNum =
+			animeInfo.match(/[0-9]+\.Bölüm/g) || animeInfo.match(/[0-9]+\. Bölüm/g);
 
 		presenceData.details = `${animeInfo.split("->")[2]} izliyor`;
 
-		if (epNum) {
+		if (epNum) 
 			presenceData.state = `Bölüm ${epNum[0].split(".")[0]}`;
-		};
+		
 		presenceData.buttons = [
 			{
 				label: "Anime",
 				url: document.URL,
 			},
-		]
+		];
 	} else if (window.location.pathname.startsWith("/profile") && profilAd) {
 		presenceData.state = `${profilAd} profilinde`;
 		presenceData.buttons = [
@@ -76,26 +97,23 @@ presence.on("UpdateData", async () => {
 				label: "Profil",
 				url: document.URL,
 			},
-		]
+		];
 	} else {
-		presenceData.details = "Göz atıyor..."
+		presenceData.details = "Göz atıyor...";
 		presenceData.startTimestamp = Math.floor(Date.now() / 1000);
-	};
-
+	}
 
 	if (video) {
 		presenceData.smallImageKey = video.paused ? "pause" : "play";
-		presenceData.smallImageText = video.paused
-			? "Duraklatıldı"
-			: "Oynatılıyor";
+		presenceData.smallImageText = video.paused ? "Duraklatıldı" : "Oynatılıyor";
 
 		if (!video.paused && video.duration) {
 			[, presenceData.endTimestamp] = presence.getTimestamps(
 				Math.floor(video.currentTime),
 				Math.floor(video.duration)
 			);
-		};
-	};
+		}
+	}
 
 	presence.setActivity(presenceData);
 });
