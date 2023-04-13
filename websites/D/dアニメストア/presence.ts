906c4c1e170a5b2c1d2289d3c0f42490851aa9a7
neco222 <107422195+neco222@users.noreply.github.com>
2023-04-13 18:43:11 +0900

    Update presence.ts
    I tried changing the code.
    I don't know if it works, but could you please check?
    
    Signed-off-by: neco222 <107422195+neco222@users.noreply.github.com>

--- websites/D/dアニメストア/presence.ts
+++ websites/D/dアニメストア/presence.ts
@@ -1,36 +1,29 @@
-{
-	const presence = new Presence({
-			clientId: "611012705306017792",
-		}),
-		strings = presence.getStrings({
-			play: "general.playing",
-			pause: "general.paused",
-		});
+const presence = new Presence({
+  clientId: "611012705306017792"
+});
 
-	presence.on("UpdateData", async () => {
-		if (
-			location.pathname.startsWith("/animestore/sc_d_pc") &&
-			document.querySelector("#video")
-		) {
-			const video: HTMLVideoElement = document.querySelector("#video"),
-				isPlaying = !video.paused,
-				presenceData: PresenceData = {
-					details: `${document.querySelector(".backInfoTxt1").textContent} - ${
-						document.querySelector(".backInfoTxt2").textContent
-					}`,
-					state: document.querySelector(".backInfoTxt3").textContent,
-					largeImageKey: "https://i.imgur.com/6sp3k8m.png",
-					smallImageKey: isPlaying ? "play" : "pause",
-					smallImageText: isPlaying
-						? (await strings).play
-						: (await strings).pause,
-					startTimestamp:
-						Math.floor(Date.now() / 1000) - Math.floor(video.currentTime),
-				};
+const strings = await presence.getStrings({
+  play: "general.playing",
+  pause: "general.paused",
+});
 
-			if (!isPlaying) delete presenceData.startTimestamp;
-
-			presence.setActivity(presenceData);
-		}
-	});
-}
+presence.on("UpdateData", async () => {
+  if (
+    location.pathname.startsWith("/animestore/sc_d_pc") &&
+    document.querySelector("#dplayer_video_box video")
+  ) {
+    const video = document.querySelector("#dplayer_video_box video"),
+          isPlaying = !video.paused,
+          presenceData = {
+            details: `${document.querySelector(".backInfoTxt1").textContent} - ${document.querySelector(".backInfoTxt2").textContent}`,
+            state: document.querySelector(".backInfoTxt3").textContent,
+            largeImageKey: "https://i.imgur.com/6sp3k8m.png",
+            smallImageKey: isPlaying ? "play" : "pause",
+            smallImageText: isPlaying ? (await strings).play : (await strings).pause,
+            startTimestamp: isPlaying ? Math.floor(Date.now() / 1000) - Math.floor(video.currentTime) : undefined
+          };
+    presence.setActivity(presenceData);
+  } else {
+    presence.setActivity();
+  }
+});
