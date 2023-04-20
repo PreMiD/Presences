d7366bea0a90d13e2dd74651a36f4c5df65bd0c6
Türker <63150613+turkwr@users.noreply.github.com>
2023-04-20 11:46:54 +0300

    DiscordGO

--- websites/D/DCGO/presence.ts
+++ websites/D/DCGO/presence.ts
@@ -1,7 +1,11 @@
-const presence = new Presence({ clientId: "1075115687888044072" });
+const presence = new Presence({ 
+	clientId: "1075115687888044072" 
+});
 
 presence.on("UpdateData", () => {
-	const presenceData: PresenceData = { largeImageKey: "https://i.imgur.com/81z9xFR.png" };
+	const presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/81z9xFR.png",
+	};
 	if (window.location.pathname.toLowerCase() === "/") {
 		presenceData.details = "Bir sayfa görüntülüyor:";
 		presenceData.state = "Anasayfa";
@@ -23,16 +27,26 @@ presence.on("UpdateData", () => {
 	} else if (window.location.pathname.toLowerCase() === "/profile-settings") {
 		presenceData.details = "Bir ayar görüntülüyor:";
 		presenceData.state = "Bağlantılar";
-	}else if (window.location.pathname.toLowerCase() === "/profile-settings") {
+	} else if (window.location.pathname.toLowerCase() === "/profile-settings") {
 		presenceData.details = "Bir sayfa görüntülüyor:";
 		presenceData.state = "Yardım";
 	} else if (window.location.pathname.toLowerCase().includes("/profile")) {
 		presenceData.details = "Bir kullanıcı profili görüntülüyor:";
-		presenceData.state =`${document.querySelector("body > div.main-wrapper > div.main-content > div > div > div > div.col-lg-12 > div > div.card-body.d-block.pt-4.text-center.position-relative > h4").textContent}`};
-  if (window.location.pathname.toLowerCase().includes("/server-details")) {
+		presenceData.state = `${
+			document.querySelector(
+				"body > div.main-wrapper > div.main-content > div > div > div > div.col-lg-12 > div > div.card-body.d-block.pt-4.text-center.position-relative > h4"
+			).textContent
+		}`;
+	}
+	if (window.location.pathname.toLowerCase().includes("/server-details")) {
 		presenceData.details = "Bir sunucu görüntülüyor:";
-		presenceData.state = `${document.querySelector("body > div.main-wrapper > div.main-content > div > div > div > div.col-lg-12 > div > div.card-body.p-0.position-relative > h4").textContent}`
-	} 
-	
- presence.setActivity(presenceData);
-})
+		presenceData.state = `${
+			document.querySelector(
+				"body > div.main-wrapper > div.main-content > div > div > div > div.col-lg-12 > div > div.card-body.p-0.position-relative > h4"
+			).textContent
+		}`;
+	}
+
+	presence.setActivity(presenceData);
+	//presence.setActivity()
+});
