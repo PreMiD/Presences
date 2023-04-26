bda362dcadf8eb8158d88257613781f4ef36b260
theusaf <theusafyt@gmail.com>
2023-04-26 11:05:58 -0700

    feat: add utilities

---
+++ websites/B/Board Game Arena/util.ts
@@ -0,0 +1,59 @@
+export interface GameMetadata extends Record<string, any> {
+  bRealtime: number;
+  bTutorial: boolean;
+  channel: string;
+  chatDetached: boolean;
+  current_player_is_active: boolean;
+  current_player_name: string;
+  game_group: string;
+  game_id: number;
+  game_name: string;
+  game_status: string;
+  isSpectator: boolean;
+  is_coop: boolean;
+  is_sandbox: boolean;
+  is_solo: boolean;
+  metasiteurl: string;
+  player_id: number;
+  gamedatas: Record<string, any>;
+}
+
+
+let dataCache: GameMetadata,
+  dataCacheTime: Record<string, number> = {}
+
+export function getGameTag() {
+  return getMetadata<string>("game_name");
+}
+
+function getCachedItem(key: string) {
+  return dataCache[key];
+}
+
+function setCachedItem(key: string, value: any) {
+  dataCache[key] = value;
+}
+
+function getCacheTime(key: string) {
+  return dataCacheTime[key] ?? 0;
+}
+
+function setCacheTime(key: string, time: number) {
+  dataCacheTime[key] = time;
+}
+
+export async function getMetadata<E>(key: string) {
+  const now = Date.now();
+  if (now - getCacheTime(key) > 1000) {
+    setCacheTime(key, now);
+    const data = await presence.getPageVariable(`gameui.${key}`);
+    setCachedItem(key, data);
+    return data as E;
+  } else {
+    return getCachedItem(key) as E;
+  }
+}
+
+export function getGameData<E>(key: string) {
+  return getMetadata<E>(`gamedatas.${key}`);
+}
