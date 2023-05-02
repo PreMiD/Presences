a8c5022adebe10e44c9d0414a4fa5c19c745b3b2
Giang <463189@student.fontys.nl>
2023-05-02 16:04:11 +0200

    feat(ChatGPT): add presence

---
+++ websites/C/ChatGPT/presence.ts
@@ -0,0 +1,12 @@
+const presence = new Presence({ clientId: "1102935778570547282", });
+
+presence.on("UpdateData", async () => {
+	const showTitle = await presence.getSetting<boolean>("showTitle");
+
+	presence.setActivity({
+		largeImageKey: "https://i.imgur.com/qw4f6EN.png",
+		startTimestamp: Math.floor(Date.now() / 1000),
+		details: showTitle ? "Talking with AI about:" : "Talking with AI",
+		state: showTitle ? document.querySelector("div.flex-col > div > div .bg-gray-800")?.textContent : ""
+	});
+});
