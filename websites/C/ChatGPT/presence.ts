ab9b964f35e8a1c9f3306f1c8b2a32f5d751d1fa
Giang <463189@student.fontys.nl>
2023-05-02 16:06:57 +0200

    refactor: lint

--- websites/C/ChatGPT/presence.ts
+++ websites/C/ChatGPT/presence.ts
@@ -1,4 +1,4 @@
-const presence = new Presence({ clientId: "1102935778570547282", });
+const presence = new Presence({ clientId: "1102935778570547282" });
 
 presence.on("UpdateData", async () => {
 	const showTitle = await presence.getSetting<boolean>("showTitle");
@@ -7,6 +7,9 @@ presence.on("UpdateData", async () => {
 		largeImageKey: "https://i.imgur.com/qw4f6EN.png",
 		startTimestamp: Math.floor(Date.now() / 1000),
 		details: showTitle ? "Talking with AI about:" : "Talking with AI",
-		state: showTitle ? document.querySelector("div.flex-col > div > div .bg-gray-800")?.textContent : ""
+		state: showTitle
+			? document.querySelector("div.flex-col > div > div .bg-gray-800")
+					?.textContent
+			: "",
 	});
 });
