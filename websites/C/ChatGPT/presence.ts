a124fb8072bc5cc43eaf17c1bc52c07e52422c5c
Giang <463189@student.fontys.nl>
2023-05-05 20:50:58 +0200

    feat: add word & question counter + talking state

--- websites/C/ChatGPT/presence.ts
+++ websites/C/ChatGPT/presence.ts
@@ -1,15 +1,39 @@
 const presence = new Presence({ clientId: "1102935778570547282" });
-
+enum Assets {
+	Logo = "https://i.imgur.com/qw4f6EN.png",
+	Talking = "https://i.imgur.com/hSv055V.png",
+}
 presence.on("UpdateData", async () => {
-	const showTitle = await presence.getSetting<boolean>("showTitle");
+	const showTitle = await presence.getSetting<boolean>("showTitle"),
+		talking = document.querySelector('[class*="text-2xl"] > span:nth-child(3)');
+
+	let words = 0;
+	for (const element of document.querySelectorAll(
+		'[class*="flex flex-col items-center"] > *'
+	)) {
+		const text = element.textContent.replace(
+			/([.])|(\n)|([0-9]*)|(\/)|()/gm,
+			""
+		);
+		words += text.split(" ").slice(2, text.split(" ").length).length;
+	}
 
 	presence.setActivity({
-		largeImageKey: "https://i.imgur.com/qw4f6EN.png",
+		largeImageKey: Assets.Logo,
 		startTimestamp: Math.floor(Date.now() / 1000),
-		details: showTitle ? "Talking with AI about:" : "Talking with AI",
-		state: showTitle
-			? document.querySelector("div.flex-col > div > div .bg-gray-800")
-					?.textContent
-			: "",
+		details: showTitle
+			? `${
+					document.querySelector("li a button > svg")?.closest("li")
+						?.textContent
+			  }` ??
+			  document.querySelector('[class*="visible"]')?.parentElement?.textContent
+			: "Talking with AI about something",
+		state: talking
+			? "AI is responding..."
+			: `asked (${
+					Number(document.querySelectorAll('[class*="group w-full"]').length) /
+					2
+			  }) times | (${words}) words`,
+		smallImageKey: talking ? Assets.Talking : null,
 	});
 });
