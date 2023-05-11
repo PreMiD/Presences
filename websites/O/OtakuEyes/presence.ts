3c28b286491a06843a640094feeb2d7bf93c1470
Yù <63105226+YuToutCourt@users.noreply.github.com>
2023-05-11 12:19:14 +0200

    add search icon :p
    Signed-off-by: Yù <63105226+YuToutCourt@users.noreply.github.com>

--- websites/O/OtakuEyes/presence.ts
+++ websites/O/OtakuEyes/presence.ts
@@ -1,33 +1,35 @@
 const presence = new Presence({
 		clientId: "784067980086607952",
 	}),
-	startTimestamp: number = Math.floor(Date.now() / 1000);
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
 	const [page, , episode] = document.location.pathname.split("/").slice(1, 4),
 		[showCover] = await Promise.all([presence.getSetting<boolean>("cover")]),
 		presenceData: PresenceData = {
 			largeImageKey: "https://i.imgur.com/b3H1vRU.png",
-			startTimestamp,
+			startTimestamp: browsingTimestamp,
 		};
 
 	switch (page) {
 		case "anime": {
 			presenceData.details = "Watching an anime";
-			presenceData.state = document
-				.querySelector("h3.titleA")
-				.textContent.trim();
+			presenceData.state = episode
+				? `${document
+						.querySelector("h3.titleA")
+						.textContent.trim()}  - Episode ${episode}`
+				: document.querySelector("h3.titleA").textContent.trim();
 			if (showCover) {
 				presenceData.largeImageKey = document
 					.querySelector(".cover")
 					.getAttribute("src");
 			}
-			if (episode) presenceData.state += ` - Episode ${episode}`;
 			break;
 		}
 		default:
 			presenceData.details = "Looking for an anime to watch";
 			presenceData.state = "Home";
+			presenceData.smallImageKey = "https://i.imgur.com/poTatnI.png";
 	}
 
 	presence.setActivity(presenceData, true);
