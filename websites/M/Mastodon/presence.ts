0ff1ca81d04f7b6a7f39dac3d77fb1e29613eee2
Hikki <buscarinoantonino@gmail.com>
2023-05-15 14:08:17 +0200

    chore: run prettier

--- websites/M/Mastodon/presence.ts
+++ websites/M/Mastodon/presence.ts
@@ -1,13 +1,12 @@
 const presence = new Presence({
-		clientId: "1107321415243415574",
-	});
+	clientId: "1107321415243415574",
+});
 
 enum Assets {
 	Logo = "https://i.imgur.com/njcb4Wp.png",
 	FediverseLogo = "https://i.imgur.com/B0HPMOQ.png",
-	Search = "https://i.imgur.com/WhzGXp1.png"
+	Search = "https://i.imgur.com/WhzGXp1.png",
 }
-
 
 // checks if user is logged from textarea
 function checkPositionAccountAvatar() {
@@ -18,33 +17,27 @@ function checkPositionAccountAvatar() {
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: Assets.Logo,
-	},
+			largeImageKey: Assets.Logo,
+		},
+		{ pathname } = document.location;
 
-	{ pathname } = document.location;
-	
 	if (pathname === "/home") {
 		presenceData.smallImageText = "Home";
 		presenceData.details = "On the homepage";
-
 	} else if (pathname === "/notifications") {
 		presenceData.smallImageText = "Notifications";
 		presenceData.details = "Checking notifications";
-
 	} else if (pathname.startsWith("/explore")) {
 		presenceData.smallImageKey = Assets.Search;
 		presenceData.smallImageText = "Explore";
 		presenceData.details = "On the explore";
-
 	} else if (pathname === "/public/local") {
 		presenceData.smallImageText = "Local timeline";
 		presenceData.details = "Looking in local timeline";
-
 	} else if (pathname === "/public") {
 		presenceData.largeImageKey = Assets.FediverseLogo;
 		presenceData.smallImageText = "Fediverse";
 		presenceData.details = "Looking in fediverse";
-
 	} else if (
 		pathname === "/bookmarks" ||
 		pathname === "/favourites" ||
@@ -53,47 +46,49 @@ presence.on("UpdateData", async () => {
 		const pathName = pathname.split("/")[1];
 		presenceData.smallImageText = pathName;
 		presenceData.details = `Looking their ${pathName}`;
-
 	} else if (pathname === "/search") {
-		const searchInput = document.querySelector(".search__input").getAttribute("value");
+		const searchInput = document
+			.querySelector(".search__input")
+			.getAttribute("value");
 		presenceData.smallImageText = "Search";
 		presenceData.details = `Searching ${searchInput}`;
-
 	} else if (pathname.startsWith("/@")) {
 		const userName = pathname.split(/[@,/]+/)[1];
-		presenceData.smallImageKey = document.querySelectorAll(".account__avatar")[checkPositionAccountAvatar()].querySelector("img").getAttribute("src");
+		presenceData.smallImageKey = document
+			.querySelectorAll(".account__avatar")
+			[checkPositionAccountAvatar()].querySelector("img")
+			.getAttribute("src");
 		presenceData.smallImageText = `${document.location}`;
-		
-		presenceData.details = `Viewing ${userName}'s profile`;
 
+		presenceData.details = `Viewing ${userName}'s profile`;
 	} else if (pathname.startsWith("/settings")) {
 		presenceData.smallImageText = "Settings";
 		presenceData.details = "Viewing their settings";
-
 	} else if (pathname === "/relationships") {
 		presenceData.smallImageText = "Relationships";
 		presenceData.details = "Viewing their followers";
-
 	} else if (pathname.startsWith("/tags")) {
 		const tag = pathname.split("/")[2];
 		presenceData.smallImageText = "tags";
 		presenceData.details = `Searching ${tag} tag`;
-
-	} else if (pathname === ("/about")) {
+	} else if (pathname === "/about") {
 		const instanceName = document.location.hostname;
-		presenceData.smallImageKey = document.querySelectorAll(".account__avatar")[checkPositionAccountAvatar()].querySelector("img").getAttribute("src");
-		presenceData.largeImageKey = document.querySelector(".about__header__hero").querySelector("img").getAttribute("src");
+		presenceData.smallImageKey = document
+			.querySelectorAll(".account__avatar")
+			[checkPositionAccountAvatar()].querySelector("img")
+			.getAttribute("src");
+		presenceData.largeImageKey = document
+			.querySelector(".about__header__hero")
+			.querySelector("img")
+			.getAttribute("src");
 		presenceData.smallImageText = `About ${instanceName}`;
 		presenceData.details = `About ${instanceName}`;
-
-	} else if (pathname === ("/directory")) {
+	} else if (pathname === "/directory") {
 		presenceData.smallImageText = "Directory";
 		presenceData.details = "On the directory";
-
-	} else if (pathname === ("/privacy-policy")) {
+	} else if (pathname === "/privacy-policy") {
 		presenceData.smallImageText = "Privacy and Policy";
 		presenceData.details = "Reading Privacy and Policy";
-
 	}
 
 	presence.setActivity(presenceData);
