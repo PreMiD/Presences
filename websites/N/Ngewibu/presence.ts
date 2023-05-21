ae3f5a9ddc7c632f99cc75c022ec3c463a0030df
sinkaroid <bibsindra@gmail.com>
2023-05-21 07:46:52 +0700

    fix: adjust latest site changes

--- websites/N/Ngewibu/presence.ts
+++ websites/N/Ngewibu/presence.ts
@@ -2,64 +2,89 @@ const presence = new Presence({
 		clientId: "1104875968134397963",
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
-enum Assets {
-	Logo = "https://i.imgur.com/ZmylEPM.png",
-	Play = "https://i.imgur.com/Am7HASZ.png",
-	Pause = "https://i.imgur.com/OKNf81I.png",
-	Search = "https://i.imgur.com/HNy8ilN.png",
-}
+
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/ZmylEPM.png",
+			largeImageKey: "https://i.imgur.com/UMRqZDd.png",
 			startTimestamp: browsingTimestamp,
 		},
-		{ href, pathname, hostname } = document.location,
-		video = document.querySelector<HTMLVideoElement>("video"),
-		search = document.querySelector<HTMLInputElement>("[id='search-input']");
-	if (search?.value) {
+		{ href, pathname, search } = document.location,
+		video = document.querySelector<HTMLVideoElement>("video");
+	if (pathname.startsWith("/search")) {
 		presenceData.details = "Searching for";
-		presenceData.state = search?.value;
+		//get value after ?q=
+		presenceData.state = search.split("?q=")[1];
 		presenceData.smallImageKey = Assets.Search;
 	} else if (pathname.startsWith("/play")) {
 		const thumbnail = document
-			.querySelector('[class="img-fluid"]')
-			?.getAttribute("src")
-			?.replace(".webp", ".png");
-		presenceData.details = document.querySelector(
-			'[class="mb-1  text-truncate"]'
-		)?.textContent;
-		presenceData.state = document
-			.querySelector('[class*="currentPlay bg-success"]')
-			?.textContent.replace("E", "Episode");
-		presenceData.largeImageKey = thumbnail
-			? `https://${hostname}${thumbnail}`
-			: Assets.Logo;
+				.querySelector<HTMLMetaElement>('meta[property="og:image"]')
+				.getAttribute("content")
+				.replace("webp", "png"),
+			title = document
+				.querySelector<HTMLMetaElement>('meta[property="og:title"]')
+				.getAttribute("content")
+				.replace(" - NGEWIBU.TV", "");
+
+		presenceData.largeImageKey = thumbnail || presenceData.largeImageKey;
+		presenceData.state = `Episode ${title
+			.match(/E \d+/g)[0]
+			.replace("E ", "")}`;
+		presenceData.details = `Watching ${title}`;
 		if (video) {
 			presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
 			presenceData.smallImageText = video.paused ? "Paused" : "Playing";
 			presenceData.buttons = [{ label: "Watch Video", url: href }];
 		} else presenceData.buttons = [{ label: "View Anime", url: href }];
-	} else if (pathname === "/index") {
-		presenceData.details = "Browsing all animes";
-		presenceData.buttons = [{ label: "Browse All Animes", url: href }];
-	} else if (pathname.includes("/index")) {
-		presenceData.details = `Viewing ${pathname
-			.split("/")[2]
-			.toLowerCase()} animes`;
-		presenceData.state = `Page ${
-			document.querySelector('[class="page-item active"]')?.textContent
-		}/${
-			document.querySelector(
-				'[class="pagination pagination-sm justify-content-center"]'
-			).children[
-				Number(
-					document.querySelector(
-						'[class="pagination pagination-sm justify-content-center"]'
-					).children.length
-				) - 2
-			].textContent
+	} else if (pathname.startsWith("/video/")) {
+		const thumbnail = document
+			.querySelector<HTMLMetaElement>('meta[property="og:image"]')
+			.getAttribute("content")
+			.replace("webp", "png");
+
+		presenceData.largeImageKey = thumbnail || presenceData.largeImageKey;
+		presenceData.state = "Shorts Video";
+		presenceData.details = `Watching ${document
+			.querySelector<HTMLMetaElement>('meta[property="og:title"]')
+			.getAttribute("content")
+			.replace(" - NGEWIBU.TV", "")}`;
+		if (video) {
+			presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
+			presenceData.smallImageText = video.paused ? "Paused" : "Playing";
+			presenceData.buttons = [{ label: "Watch Video", url: href }];
+		} else presenceData.buttons = [{ label: "View Videos", url: href }];
+	} else if (pathname === "/") {
+		presenceData.details = "Viewing home page";
+		presenceData.smallImageKey = Assets.Viewing;
+		presenceData.buttons = [{ label: "Viewing home page", url: href }];
+	} else if (pathname.includes("/anime")) {
+		presenceData.details = `Viewing Anime category ${
+			pathname.split("/")[2] || "semua"
 		}`;
+		presenceData.state = `Page ${pathname.split("/")[3] || 1}`;
+		presenceData.smallImageKey = Assets.Viewing;
 		presenceData.buttons = [{ label: "View Animes", url: href }];
+	} else if (pathname.includes("/tv-shows")) {
+		presenceData.details = "Viewing TV Shows";
+		presenceData.state = `Page ${pathname.split("/")[2] || 1}`;
+		presenceData.smallImageKey = Assets.Viewing;
+		presenceData.buttons = [{ label: "View TV Shows", url: href }];
+	} else if (pathname.includes("/trending")) {
+		presenceData.details = "Viewing trending";
+		presenceData.state = `${pathname.split("/")[2] || "umum"}`;
+		presenceData.smallImageKey = Assets.Viewing;
+		presenceData.buttons = [{ label: "View Trending", url: href }];
+	} else if (pathname.includes("/videos")) {
+		presenceData.details = "Viewing shorts videos";
+		presenceData.state = `Page ${pathname.split("/")[2] || 1}`;
+		presenceData.smallImageKey = Assets.Viewing;
+		presenceData.buttons = [{ label: "View page", url: href }];
+	} else if (pathname.includes("/jadwal-tayang")) {
+		presenceData.details = "Viewing jadwal tayang";
+		presenceData.state = document.querySelector(
+			"h6.mb-3.text-white.px-2"
+		).textContent;
+		presenceData.smallImageKey = Assets.Viewing;
+		presenceData.buttons = [{ label: "View Jadwal Tayang", url: href }];
 	}
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
