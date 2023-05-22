2e5f80b42b2de23b229a739a690ec8ef19a02c86
sino <55715737+SINO87@users.noreply.github.com>
2023-05-22 15:37:20 +0900

    Update presence.ts
    Signed-off-by: sino <55715737+SINO87@users.noreply.github.com>

--- websites/#/アイドルマスターシャイニーカラーズ/presence.ts
+++ websites/#/アイドルマスターシャイニーカラーズ/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-    largeImageKey: "https://i.imgur.com/ue8EatG.jpg",
+    largeImageKey: "https://i.imgur.com/YfaIAPE.png",
   };
 
   const { pathname } = document.location;
