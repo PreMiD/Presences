e3aba2f7e944ca34236cc7361ca6801480421a3b
Yù <63105226+YuToutCourt@users.noreply.github.com>
2023-05-24 11:13:53 +0200

    Update presence.ts
    Signed-off-by: Yù <63105226+YuToutCourt@users.noreply.github.com>

--- websites/O/OtakuEyes/presence.ts
+++ websites/O/OtakuEyes/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const [page, , episode] = document.location.pathname.split("/").slice(1, 4),
-		[showCover] = await Promise.all([presence.getSetting<boolean>("cover")]),
+		showCover = await presence.getSetting<boolean>("cover"),
 		presenceData: PresenceData = {
 			largeImageKey: "https://i.imgur.com/b3H1vRU.png",
 			startTimestamp: browsingTimestamp,
