0c9eb9b4e87420831059b29426ed08f09556f6c7
theusaf <theusafyt@gmail.com>
2023-05-25 06:41:05 -0700

    fix(games/dotsandboxes): fix incorrect logo

--- websites/B/Board Game Arena/games/D/dotsandboxes.ts
+++ websites/B/Board Game Arena/games/D/dotsandboxes.ts
@@ -10,7 +10,7 @@ import {
 } from "../../util";
 
 const dotsandboxes: GamePresence = {
-	logo: "https://i.imgur.com/wpsDpO7.png",
+	logo: "https://i.imgur.com/lSOhWTe.png",
 	async getData(presence: Presence) {
 		const gameState = await getCurrentGameState(presence),
 			activePlayer = await getActivePlayerId(presence),
