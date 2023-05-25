f3661a5ce4285bdadd8f362f35229c97fbd3d66f
theusaf <theusafyt@gmail.com>
2023-05-25 06:58:30 -0700

    fix(games/flume): fix incorrect logo

--- websites/B/Board Game Arena/games/F/flume.ts
+++ websites/B/Board Game Arena/games/F/flume.ts
@@ -10,7 +10,7 @@ import {
 } from "../../util";
 
 const flume: GamePresence = {
-	logo: "https://i.imgur.com/nXCXyLO.png",
+	logo: "https://i.imgur.com/FZRLXAO.png",
 	async getData(presence: Presence) {
 		const gameState = await getCurrentGameState(presence),
 			activePlayer = await getActivePlayerId(presence),
