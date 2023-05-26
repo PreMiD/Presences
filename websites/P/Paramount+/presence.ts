12bf9f949b5394697490d1e05f7c415fdef034e6
tenKatsu <62923115+calicokatsu@users.noreply.github.com>
2023-05-26 01:04:34 -0400

    chore: refactor brand path detection

--- websites/P/Paramount+/presence.ts
+++ websites/P/Paramount+/presence.ts
@@ -139,33 +139,33 @@ presence.on("UpdateData", async () => {
 			break;
 
 		case pathIncludes(path, "/brands"):
-			if (path.includes("/cbs/")) {
-				presenceData.details = "Browsing Brand:";
+			presenceData.details = "Browsing Brands";
+			presenceData.state = "Viewing Brands";
+
+			if (pathIncludes(path, "/cbs/")) {
+				presenceData.details = "Browsing Brand";
 				presenceData.state = "CBS";
 				presenceData.largeImageKey = Logos.CBS;
-			} else if (path.includes("/bet/")) {
-				presenceData.details = "Browsing Brand:";
+			} else if (pathIncludes(path, "/bet/")) {
+				presenceData.details = "Browsing Brand";
 				presenceData.state = "BET";
-				presenceData.largeImageKey = Logos.CBS;
-			} else if (path.includes("/comedy-central/")) {
-				presenceData.details = "Browsing Brand:";
+				presenceData.largeImageKey = Logos.BET;
+			} else if (pathIncludes(path, "/comedy-central/")) {
+				presenceData.details = "Browsing Brand";
 				presenceData.state = "Comedy Central";
 				presenceData.largeImageKey = Logos.ComedyCentral;
-			} else if (path.includes("/mtv/")) {
-				presenceData.details = "Browsing Brand:";
+			} else if (pathIncludes(path, "/mtv/")) {
+				presenceData.details = "Browsing Brand";
 				presenceData.state = "MTV";
 				presenceData.largeImageKey = Logos.MTV;
-			} else if (path.includes("/nickelodeon/")) {
-				presenceData.details = "Browsing Brand:";
+			} else if (pathIncludes(path, "/nickelodeon/")) {
+				presenceData.details = "Browsing Brand";
 				presenceData.state = "Nickelodeon";
 				presenceData.largeImageKey = Logos.Nickelodeon;
-			} else if (path.includes("/smithsonian-channel/")) {
-				presenceData.details = "Browsing Brand:";
+			} else if (pathIncludes(path, "/smithsonian-channel/")) {
+				presenceData.details = "Browsing Brand";
 				presenceData.state = "Smithsonian Channel";
 				presenceData.largeImageKey = Logos.Smithsonian;
-			} else {
-				presenceData.details = "Browsing Brands";
-				presenceData.state = "Viewing Brands";
 			}
 
 			break;
