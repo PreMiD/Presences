7bd295a78323f47c66e4804408ccffb9f864f656
theusaf <theusafyt@gmail.com>
2023-05-27 12:02:43 -0700

    fix(games/spiritsoftheforest): improve light mode visibility

--- websites/B/Board Game Arena/games/S/spiritsoftheforest.ts
+++ websites/B/Board Game Arena/games/S/spiritsoftheforest.ts
@@ -10,7 +10,7 @@ import {
 } from "../../util";
 
 const spiritsoftheforest: GamePresence = {
-	logo: "https://i.imgur.com/pXGb1hO.png",
+	logo: "https://i.imgur.com/XIhmJIg.png",
 	async getData(presence: Presence) {
 		const gameState = await getCurrentGameState(presence),
 			activePlayer = await getActivePlayerId(presence),
