41d587649cf892b8cf6b9ad24d5dd656e859658d
theusaf <theusafyt@gmail.com>
2023-05-27 11:58:24 -0700

    fix(games/regidice): improve light mode visibility

--- websites/B/Board Game Arena/games/R/regidice.ts
+++ websites/B/Board Game Arena/games/R/regidice.ts
@@ -10,7 +10,7 @@ import {
 } from "../../util";
 
 const regidice: GamePresence = {
-	logo: "https://i.imgur.com/EK0QUsf.png",
+	logo: "https://i.imgur.com/19zBMmV.png",
 	async getData(presence: Presence) {
 		const gameState = await getCurrentGameState(presence),
 			activePlayer = await getActivePlayerId(presence),
