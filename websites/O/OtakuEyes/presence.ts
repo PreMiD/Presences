3ccaf9385de1bf35ae3f1c740326103f65d0cf83
Yù <63105226+YuToutCourt@users.noreply.github.com>
2023-05-27 12:09:33 +0200

    Check if the footer is OtakuEyes
    Signed-off-by: Yù <63105226+YuToutCourt@users.noreply.github.com>

--- websites/O/OtakuEyes/presence.ts
+++ websites/O/OtakuEyes/presence.ts
@@ -3,34 +3,42 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
+function checkFooterContent() {
+	const footer = document.querySelector("footer");
+	const pElement = footer.querySelector("p");
+	return pElement && pElement.textContent.trim() === "OtakuEyes";
+}
+
 presence.on("UpdateData", async () => {
-	const [page, , episode] = document.location.pathname.split("/").slice(1, 4),
-		showCover = await presence.getSetting<boolean>("cover"),
-		presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/b3H1vRU.png",
-			startTimestamp: browsingTimestamp,
-		};
+	if (checkFooterContent()) {
+		const [page, , episode] = document.location.pathname.split("/").slice(1, 4),
+			showCover = await presence.getSetting<boolean>("cover"),
+			presenceData: PresenceData = {
+				largeImageKey: "https://i.imgur.com/b3H1vRU.png",
+				startTimestamp: browsingTimestamp,
+			};
 
-	switch (page) {
-		case "anime": {
-			presenceData.details = "Watching an anime";
-			presenceData.state = episode
-				? `${document
-						.querySelector("h3.titleA")
-						.textContent.trim()}  - Episode ${episode}`
-				: document.querySelector("h3.titleA").textContent.trim();
-			if (showCover) {
-				presenceData.largeImageKey = document
-					.querySelector(".cover")
-					.getAttribute("src");
+		switch (page) {
+			case "anime": {
+				presenceData.details = "Watching an anime";
+				presenceData.state = episode
+					? `${document
+							.querySelector("h3.titleA")
+							.textContent.trim()}  - Episode ${episode}`
+					: document.querySelector("h3.titleA").textContent.trim();
+				if (showCover) {
+					presenceData.largeImageKey = document
+						.querySelector(".cover")
+						.getAttribute("src");
+				}
+				break;
 			}
-			break;
+			default:
+				presenceData.details = "Looking for an anime to watch";
+				presenceData.state = "Home";
+				presenceData.smallImageKey = Assets.Search;
 		}
-		default:
-			presenceData.details = "Looking for an anime to watch";
-			presenceData.state = "Home";
-			presenceData.smallImageKey = Assets.Search;
-	}
 
-	presence.setActivity(presenceData, true);
+		presence.setActivity(presenceData, true);
+	}
 });
