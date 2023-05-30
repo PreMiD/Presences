f2d656c939f4e5b1acdaa1050012977c265e7601
Dawid Andrzejewski <60063669+Weemizo@users.noreply.github.com>
2023-05-30 18:02:53 +0200

    Forgot to run yarn format
    Signed-off-by: Dawid Andrzejewski <60063669+Weemizo@users.noreply.github.com>

--- websites/S/Songsterr/presence.ts
+++ websites/S/Songsterr/presence.ts
@@ -3,57 +3,54 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/vKeC7jt.png",
-		startTimestamp: browsingTimestamp,
-},
+			largeImageKey: "https://i.imgur.com/vKeC7jt.png",
+			startTimestamp: browsingTimestamp,
+		}
+		
+		;
 
-revisionscheck = (<HTMLInputElement>document.querySelector("#revisions-toggle")).title,
-
-revisionsubmit = (<HTMLInputElement>document.querySelector("#submitRevisionButton"))?.textContent;
-
-if (document.location.search.startsWith("?pattern=")) {
-		const searchvar = (<HTMLInputElement>document.querySelector("#sticky-list-header > div.Ccm27v > div > input")).value;
+	if (document.location.search.startsWith("?pattern=")) {
+		const searchvar = (<HTMLInputElement>(
+			document.querySelector("#sticky-list-header > div.Ccm27v > div > input")
+		)).value;
 		presenceData.details = `Searching tabs for '${searchvar}'`;
-} else if (document.location.pathname.endsWith("plus")) 
+	} else if (document.location.pathname.endsWith("plus"))
 		presenceData.details = "Considering buying Songsterr Plus";
-
-		else if (document.location.pathname.endsWith("mytabs")) 
+	else if (document.location.pathname.endsWith("mytabs"))
 		presenceData.details = "Checking submitted tabs";
-
-		else if (document.location.pathname.endsWith("submit"))
+	else if (document.location.pathname.endsWith("submit"))
 		presenceData.details = "Submitting tabs";
-
-		else if (document.location.pathname.endsWith("help"))
+	else if (document.location.pathname.endsWith("help"))
 		presenceData.details = "Reading FAQ";
-
-		else if (document.location.pathname.endsWith("howtoreadtab"))
+	else if (document.location.pathname.endsWith("howtoreadtab"))
 		presenceData.details = "Learning how to read a tab";
-
-		else if (document.location.pathname.endsWith("account"))
+	else if (document.location.pathname.endsWith("account"))
 		presenceData.details = "Typing in account details";
-
-		else if (document.location.pathname.endsWith("favorites"))
+	else if (document.location.pathname.endsWith("favorites"))
 		presenceData.details = "Checking saved favorite songs";
-
-		else if (document.location.pathname.endsWith("payment"))
+	else if (document.location.pathname.endsWith("payment"))
 		presenceData.details = "Buying Songsterr Plus";
-
-		else if (revisionscheck === "Hide revisions") {
+	else if ((<HTMLInputElement>(
+			document.querySelector("#revisions-toggle")
+		)).title === "Hide revisions") {
 		presenceData.details = "Checking Revisions";
-		if (revisionsubmit === "Submit a new revision") {
+		if ((<HTMLInputElement>(
+			document.querySelector("#submitRevisionButton")
+		))?.textContent === "Submit a new revision") {
 			presenceData.details = "Checking Revisions";
 			presenceData.state = "Submitting a new revision";
+		}
+	} else {
+		presenceData.state = `Author: ${
+			document.querySelector('[aria-label="artist"]').textContent
+		}`;
+		presenceData.details = `Title: ${
+			document.querySelector('[aria-label="title"]').textContent
+		}`;
 	}
-} else {
-	
-	presenceData.state = `Author: ${document.querySelector('[aria-label="artist"]').textContent}`;
-	presenceData.details = `Title: ${document.querySelector('[aria-label="title"]').textContent}`;
-}
 
-
-if (presenceData) presence.setActivity(presenceData);
-else presence.setActivity(presenceData);
+	if (presenceData) presence.setActivity(presenceData);
+	else presence.setActivity(presenceData);
 });
