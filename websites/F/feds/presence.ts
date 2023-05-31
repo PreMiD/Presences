a22779bb75beb0be2aab0a35314052177b6e9593
addi00000 <81730200+addi00000@users.noreply.github.com>
2023-05-31 08:38:03 -0400

    fix(feds): fix image to correct size
    Signed-off-by: addi00000 <81730200+addi00000@users.noreply.github.com>

--- websites/F/feds/presence.ts
+++ websites/F/feds/presence.ts
@@ -14,7 +14,7 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/nBWzgJs.png",
+			largeImageKey: "https://i.imgur.com/TzIxLtq.png",
 		},
 		{ pathname, href } = document.location;
 
