8100f9f195ed15371d685f6992c998b6b5bd873a
Bas950 <me@bas950.com>
2023-06-01 22:57:51 +0200

    feat(AssetsManager): automatically upload assets to Recodive's CDN (#7388)
    * wip(AssetsManager): initial class and updater
    
    * wip(AssetManager): get cdn assets
    
    * wip(AssetManager): should be done now
    
    * wip(AssetManager): fetch assetBaseUrl once
    
    * chore: add workflow
    
    * chore: update comment
    
    * wip(AssetsManager): change upload
    
    * chore: change fileLocation
    
    * feat(AssetsManager): auto resize to 512x512
    
    * chore(AssetsManager): lil improvements

---
+++ tools/auto/assetsUpdater.ts
@@ -0,0 +1,88 @@
+import actions from "@actions/core";
+import chalk from "chalk";
+import { getDiff } from "../util.js";
+import AssetsManager from "../classes/AssetsManager.js";
+
+if (!getDiff().length && !getDiff("removed").length) {
+	actions.info(chalk.green("No Presences changed, exiting..."));
+	process.exit();
+}
+
+const changedPresenceFolders = getDiff(),
+	deletedPresenceFolders = getDiff("removed");
+
+if (changedPresenceFolders.length) {
+	for (const folder of changedPresenceFolders) {
+		const assetsManager = new AssetsManager(folder),
+			{ toBeUploaded, toBeMoved, toBeDeleted } =
+				await assetsManager.getAssetsChanges();
+
+		if (!toBeUploaded.size && !toBeMoved.size && !toBeDeleted.size) {
+			actions.info(chalk.green(`No assets changes for ${folder}, skipping...`));
+			continue;
+		}
+
+		if (toBeDeleted.size) {
+			actions.info(
+				chalk.green(`Deleting ${toBeDeleted.size} asset(s) for ${folder}...`)
+			);
+			const errors = await assetsManager.deleteAssets(toBeDeleted);
+			for (const error of errors) actions.error(error);
+			if (errors.length) actions.setFailed("Failed to delete assets");
+		}
+
+		if (toBeMoved.size) {
+			actions.info(
+				chalk.green(`Moving ${toBeMoved.size} asset(s) for ${folder}...`)
+			);
+			const errors: string[] = [];
+			errors.push(
+				...(await assetsManager.uploadAssets(toBeMoved)),
+				...(await assetsManager.deleteAssets([...toBeMoved.keys()]))
+			);
+			for (const error of errors) actions.error(error);
+			if (errors.length) actions.setFailed("Failed to move assets");
+
+			assetsManager.replaceInFiles(toBeMoved);
+		}
+
+		if (toBeUploaded.size) {
+			actions.info(
+				chalk.green(`Uploading ${toBeUploaded.size} asset(s) for ${folder}...`)
+			);
+			const errors = await assetsManager.uploadAssets(toBeUploaded);
+			for (const error of errors) actions.error(error);
+			if (errors.length) actions.setFailed("Failed to upload assets");
+
+			assetsManager.replaceInFiles(toBeUploaded);
+		}
+	}
+}
+
+if (deletedPresenceFolders.length) {
+	for (const folder of deletedPresenceFolders) {
+		const assetsManager = new AssetsManager(folder),
+			allAssets = assetsManager.allAssets,
+			assets = [
+				allAssets.logo,
+				allAssets.thumbnail,
+				...allAssets.assets,
+			].filter(asset => asset.startsWith(assetsManager.assetBaseUrl));
+
+		if (!assets.length) {
+			actions.info(
+				chalk.green(`No assets to delete for ${folder}, skipping...`)
+			);
+			continue;
+		}
+
+		actions.info(
+			chalk.green(`Deleting ${assets.length} asset(s) for ${folder}...`)
+		);
+		const errors = await assetsManager.deleteAssets(assets);
+		for (const error of errors) actions.error(error);
+		if (errors.length) actions.setFailed("Failed to delete assets");
+	}
+}
+
+actions.info(chalk.green("Done!"));
