434e44fd6e6ee231f32b5a8af684fb13de045b0f
theusaf <theusafyt@gmail.com>
2023-06-06 20:01:22 -0700

    style: format code

--- websites/P/PlayScrabble/presence.ts
+++ websites/P/PlayScrabble/presence.ts
@@ -9,10 +9,10 @@ const enum Assets {
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: Assets.Logo,
-		startTimestamp: browsingTimestamp,
-	};
-	const { pathname } = document.location,
+			largeImageKey: Assets.Logo,
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname } = document.location,
 		pathList = pathname.split("/").filter(Boolean);
 
 	switch (pathList[0]) {
@@ -21,29 +21,30 @@ presence.on("UpdateData", async () => {
 				"[class*='ScrabbleGame_rightGamePanel'] > div > div:nth-child(2)"
 			);
 			if (gameElement) {
-				const activePlayerName = document.querySelector<HTMLSpanElement>(
-						".game_player_info.switched_active_text"
-					).textContent,
-					gameType = gameElement.textContent.replace(/\s{2}/, ""),
-					score = document.querySelector<HTMLDivElement>(
-						".game_player_score_block"
-					).textContent,
-					time = document.querySelector<HTMLDivElement>(
-						".game_player_timer_block"
-					).textContent;
 				if (
 					document.querySelector<HTMLImageElement>(
 						"[src*='roundSummaryBackground']"
 					)
-				) {
+				)
 					presenceData.details = "Viewing round summary";
-				} else {
-					presenceData.details = `Playing ${gameType}`;
-					presenceData.state = `My score: ${score} | My time remaining: ${time}`;
+				else {
+					presenceData.details = `Playing ${gameElement.textContent.replace(
+						/\s{2}/,
+						""
+					)}`;
+					presenceData.state = `My score: ${
+						document.querySelector<HTMLDivElement>(".game_player_score_block")
+							.textContent
+					} | My time remaining: ${
+						document.querySelector<HTMLDivElement>(".game_player_timer_block")
+							.textContent
+					}`;
 					presenceData.smallImageKey = document.querySelector<HTMLImageElement>(
 						"[class*='switched_active'] [class*='Avatar'] img"
 					).src;
-					presenceData.smallImageText = activePlayerName;
+					presenceData.smallImageText = document.querySelector<HTMLSpanElement>(
+						".game_player_info.switched_active_text"
+					).textContent;
 				}
 			} else {
 				presenceData.details = "Setting up a game";
