bddc3f044e07eff079138eb304f0ce97fe183984
Nico <67909777+ipexadev@users.noreply.github.com>
2023-06-18 11:05:32 +0200

    Add files via upload
    Signed-off-by: Nico <67909777+ipexadev@users.noreply.github.com>

--- websites/K/Kick/presence.ts
+++ websites/K/Kick/presence.ts
@@ -1,36 +1,47 @@
 const presence1 = new Presence({ clientId: "1112901248421732462" });
 
 presence1.on("UpdateData", () => {
-const presenceData: PresenceData = {
-	largeImageKey: "app_icon",
-	startTimestamp: Math.floor(Date.now() / 1000)
-	};
+  const presenceData: PresenceData = {
+    largeImageKey: "app_icon",
+    startTimestamp: Math.floor(Date.now() / 1000)
+    };
 
-	const username = document.location.pathname.split("/").pop();
-  
-  if (document.location.pathname === "/") {
-    presenceData.details = "Viewing:";
-    presenceData.state = "Startpage";
-  } else if (document.location.pathname.includes("/terms-of-service")) {
-    presenceData.details = "Viewing:";
-    presenceData.state = "Terms of Service";
-  } else if (document.location.pathname.includes("/privacy-policy")) {
-    presenceData.details = "Viewing:";
-    presenceData.state = "Privacy Policy";
-  } else if (document.location.pathname.includes("/dmca-policy")) {
-    presenceData.details = "Viewing:";
-    presenceData.state = "DMCA Policy";
-  } else if (document.location.pathname.includes("/community-guidelines")) {
-    presenceData.details = "Viewing:";
-    presenceData.state = "Community Guidelines";
-  } else if (username) {
-	  presenceData.details = `Watching: ${username}`;
-	  //presenceData.state = "On Kick";
-	  presenceData.largeImageKey = "app_icon";
-	  presenceData.buttons = [{ label: "Watch Stream", url: `https://kick.com/${username}` }];
-  } else {
-    presenceData.details = "Viewing:";
-    presenceData.state = "Unknown";
+  const username = document.location.pathname.split("/").pop();
+  const pagePath = document.location.pathname;
+
+  switch (pagePath) {
+    case "/":
+      presenceData.details = "Viewing";
+      presenceData.state = "Startpage";
+      break;
+    case "/terms-of-service":
+      presenceData.details = "Viewing";
+      presenceData.state = "Terms of Service";
+      break;
+    case "/privacy-policy":
+      presenceData.details = "Viewing";
+      presenceData.state = "Privacy Policy";
+      break;
+    case "/dmca-policy":
+      presenceData.details = "Viewing";
+      presenceData.state = "DMCA Policy";
+      break;
+    case "/community-guidelines":
+      presenceData.details = "Viewing";
+      presenceData.state = "Community Guidelines";
+      break;
+    default:
+      if (username) {
+        presenceData.details = "Watching";
+        presenceData.state = username;
+        presenceData.buttons = [
+          { label: "Watch Stream", url: `https://kick.com/${username}` }
+        ];
+      } else {
+        presenceData.details = "Viewing";
+        presenceData.state = "Unknown";
+      }
+      break;
   }
 
   presence1.setActivity(presenceData);
