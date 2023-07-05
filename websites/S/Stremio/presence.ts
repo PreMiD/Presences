33d2ec38d22c42020ef76c40f14c156d4d594232
Sleeyax <yourd3veloper@gmail.com>
2023-07-05 22:44:34 +0200

    refactor(Stremio): improve control flow
    Replace nested if/else statements with switch statements. This makes the code easier to read and maintain going forward.

--- websites/S/Stremio/presence.ts
+++ websites/S/Stremio/presence.ts
@@ -3,10 +3,7 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-let timestamp: [number, number],
-	pauseCheck: boolean,
-	search: HTMLInputElement,
-	title: string;
+let	title: string;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
@@ -14,7 +11,7 @@ presence.on("UpdateData", async () => {
 				"https://cdn.rcd.gg/PreMiD/websites/S/Stremio/assets/logo.png",
 			startTimestamp: browsingTimestamp,
 		},
-		{ hash, hostname, href } = document.location,
+		{ hash, hostname, pathname, href } = document.location,
 		[privacy, thumbnails, buttons] = await Promise.all([
 			presence.getSetting<boolean>("privacy"),
 			presence.getSetting<boolean>("thumbnails"),
@@ -24,151 +21,186 @@ presence.on("UpdateData", async () => {
 			"[class='ng-binding ng-scope selected']"
 		)?.textContent;
 
-	if (hostname.includes("app.strem.io")) {
-		search = document.querySelector("#global-search-field");
-		const video = document.querySelector<HTMLMediaElement>("#videoPlayer");
-		if (privacy && !video) presenceData.details = "Browsing...";
-		else if (privacy && video) presenceData.details = "Watching...";
-		else if (search?.value) {
-			presenceData.details = "Searching For:";
-			presenceData.state = search.value;
-		} else if (hash.includes("/detail/")) {
-			title = document.querySelector(
-				"#detail > div:nth-child(3) > div > div.sidebar-info-container > div > div.logo > div"
-			).textContent;
-			presenceData.details = title;
-			presenceData.buttons = [
-				{
-					label: "Watch Video",
-					url: href,
-				},
-			];
-			if (thumbnails) {
-				presenceData.largeImageKey =
-					document
-						.querySelector(
-							"#detail > div.details-less-info > div.details-top > div:nth-child(1)"
-						)
-						?.firstElementChild.getAttribute("src") ?? "logo";
-			}
-		} else if (hash.includes("addons")) {
-			search = document.querySelector(
-				"#addons > div.filter > form > div.addon-search-input > input"
-			);
-			if (search?.value) {
-				presenceData.details = "Searching addons for:";
-				presenceData.state = search.value;
-			} else {
-				presenceData.state = active ?? "All";
-				title = document.querySelector(
-					"[class='ng-scope selected']"
-				)?.textContent;
+	switch (hostname) {
+		case "app.strem.io": {			
+			const video = document.querySelector<HTMLMediaElement>("#videoPlayer");
 
-				presenceData.buttons = [
-					{
-						label: "Browse Addons",
-						url: href,
-					},
-				];
-				presenceData.details = `Browsing ${title}`;
+			if (privacy) {
+				presenceData.details = !video ? "Browsing..." : "Watching...";
+				break;
 			}
-		} else if (hash.includes("settings")) {
-			presenceData.details = `${
-				document.querySelector("[class='ng-scope ng-binding active']")
-					?.textContent ?? "General"
-			} settings`;
-		} else if (hash.includes("/discover/")) {
-			if (active) {
-				presenceData.buttons = [
-					{
-						label: "Browse",
-						url: href,
-					},
-				];
-				presenceData.details = `Browsing: ${active}`;
-			} else presenceData.state = "Browsing Movies";
-		} else if (hash.includes("/library")) {
-			if (active) presenceData.details = `${active} Library`;
-			else presenceData.details = "Library";
 
-			presenceData.buttons = [
-				{
-					label: "View Library",
-					url: href,
-				},
-			];
-		} else if (hash.includes("/calendar")) {
-			presenceData.buttons = [
-				{
-					label: "View Calendar",
-					url: href,
-				},
-			];
-			presenceData.details = "Calendar";
-		} else if (hash.includes("player")) {
-			if (video?.duration) {
-				timestamp = presence.getTimestampsfromMedia(video);
-				pauseCheck = video?.paused ?? true;
-			} else if (!video.duration && document.querySelector("#controlbar-top")) {
-				let split = document
-					.querySelector("#play-progress-text")
-					?.textContent.split("/");
-				if (split?.[0]) {
-					split = split.map(s => s.trim());
-					timestamp = presence.getTimestamps(
-						presence.timestampFromFormat(split[0]),
-						presence.timestampFromFormat(split[1])
-					);
+			switch (hash.replace("#/", "").split("/").shift()) {
+				case "":
+					presenceData.details = "Board";
+					break;
+				case "detail":
+					title = document.querySelector(
+						"#detail > div:nth-child(3) > div > div.sidebar-info-container > div > div.logo > div"
+					)?.textContent;
+					presenceData.details = title;
+					presenceData.state = "Viewing Metadata";
+					presenceData.buttons = [
+						{
+							label: "View Metadata",
+							url: href,
+						},
+					];
+
+					if (thumbnails) {
+						presenceData.largeImageKey =
+							document
+								.querySelector(
+									"#detail > div.details-less-info > div.details-top > div:nth-child(1)"
+								)
+								?.firstElementChild.getAttribute("src") ?? "logo";
+					}
+
+					break;
+				case "addons":
+					title = document.querySelector(
+						"[class='ng-scope selected']"
+						)?.textContent;
+						
+					presenceData.state = active ?? "All";
+					presenceData.buttons = [
+						{
+							label: "Browse Addons",
+							url: href,
+						},
+					];
+					presenceData.details = `Browsing ${title}`;
+					break;
+				case "settings":
+					// eslint-disable-next-line no-case-declarations, no-one-time-vars/no-one-time-vars
+					const section = document.querySelector("[class='ng-scope ng-binding active']")?.textContent ?? "General";
+					presenceData.details = `${section} settings`;
+					break;
+				case "discover":
+					if (active) {
+						presenceData.buttons = [
+							{
+								label: "Browse",
+								url: href,
+							},
+						];
+					}
+					presenceData.details = `Browsing ${active ?? "Content"}`;
+					break;
+				case "library":
+					presenceData.details = "Library";
+					if (active) presenceData.details = `${active} ${presenceData.details}`;
+					presenceData.buttons = [
+						{
+							label: "View Library",
+							url: href,
+						},
+					];
+					break;
+				case "calendar":
+					presenceData.buttons = [
+						{
+							label: "View Calendar",
+							url: href,
+						},
+					];
+					presenceData.details = "Calendar";
+					break;
+				case "player": {
+					let timestamp: [number, number],
+					 pauseCheck: boolean;
+
+					if (video?.duration) {
+						timestamp = presence.getTimestampsfromMedia(video);
+						pauseCheck = video.paused ?? true;
+					} else if (document.querySelector("#controlbar-top")) {
+						let split = document
+							.querySelector("#play-progress-text")
+							?.textContent.split("/");
+						if (split?.[0]) {
+							split = split.map(s => s.trim());
+							timestamp = presence.getTimestamps(
+								presence.timestampFromFormat(split[0]),
+								presence.timestampFromFormat(split[1])
+							);
+						}
+	
+						pauseCheck = !document
+							.querySelector("#controlbar-top")
+							?.firstElementChild.className.includes("pause");
+					}
+
+					delete presenceData.startTimestamp;
+
+					if (
+						!pauseCheck &&
+						!document.querySelector("#loading-logo").className.includes("flashing")
+					) {
+						presenceData.endTimestamp = timestamp[1];
+						presenceData.smallImageKey = Assets.Play;
+					} else {
+						delete presenceData.endTimestamp;
+						presenceData.smallImageKey = Assets.Pause;
+					}
+
+					title = document
+						.querySelector("head > title")
+						?.textContent.replace("Stremio -", "");
+
+					presenceData.details = title;
+					presenceData.state = pauseCheck ? "Paused" : "Watching";
+					presenceData.buttons = [
+						{
+							label: "Watch",
+							url: href,
+						},
+					];
+					break;
 				}
-
-				pauseCheck = !document
-					.querySelector("#controlbar-top")
-					?.firstElementChild.className.includes("pause");
 			}
-			delete presenceData.startTimestamp;
-			if (
-				!pauseCheck &&
-				!document.querySelector("#loading-logo").className.includes("flashing")
-			) {
-				presenceData.endTimestamp = timestamp[1];
-				presenceData.smallImageKey = Assets.Play;
-			} else {
-				delete presenceData.endTimestamp;
-				presenceData.smallImageKey = Assets.Pause;
-			}
-			title = document
-				.querySelector("head > title")
-				?.textContent.replace("Stremio -", "");
-			presenceData.details = title;
-			presenceData.buttons = [
-				{
-					label: "Join View Party",
-					url: href,
-				},
-			];
-		} else if (hash === "#/") presenceData.details = "Viewing the homepage";
-	} else if (hostname === "stremio.com") {
-		if (hash.includes("addon-sdk")) presenceData.details = "Viewing Addon SDK";
-		else if (hash.includes("contribute"))
-			presenceData.details = "Contributing page";
-		else if (hash.includes("community"))
-			presenceData.details = "Viewing the Community";
-		else if (hash.includes("technology"))
-			presenceData.details = "Viewing the technology";
-		else if (document.querySelector("#tos-container > h1 > strong")) {
-			presenceData.details = `Reading ${
-				document.querySelector("#tos-container > h1 > strong").textContent
-			}`;
-		} else if (
-			document.querySelector("[class='active']")?.textContent !== "EN"
-		) {
-			presenceData.details =
-				document.querySelector("[class='active']")?.textContent ??
-				"Browsing...";
+			break;
 		}
+		case "www.stremio.com":
+		case "stremio.com":
+			presenceData.details = "Visiting stremio.com";
+
+			switch (pathname) {
+				case "/addon-sdk":
+					presenceData.state = "Addon SDK";
+					break;
+				
+				case "/contribute":
+					presenceData.state = "Contribute";
+					break;
+			
+				case "/community":
+					presenceData.state = " Community";
+					break;
+
+				case "/technology":
+					presenceData.state = "Technology";
+					break;
+			
+				case "/competition":
+					presenceData.state = "Competition";
+					break;
+
+				case "/careers":
+					presenceData.state = "Careers";
+					break;
+
+				default: 
+					// eslint-disable-next-line no-case-declarations
+					const activeTab = document.querySelector("[class='active']");
+					if (activeTab === null || activeTab.parentElement?.className === "langs") break;
+					presenceData.state = activeTab.textContent;
+			}
+			
+			break;
 	}
 
 	if (!buttons) delete presenceData.buttons;
+	
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
