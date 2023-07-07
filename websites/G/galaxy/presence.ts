11837f137c3c1983762ea85e4ebeb7e79c5d7b66
SeMiD <semid@premid.app>
2023-07-07 02:14:56 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/G/galaxy/presence.ts
+++ websites/G/galaxy/presence.ts
@@ -5,7 +5,8 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/KQvYGoh.png",
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/G/galaxy/assets/logo.png",
 			startTimestamp: browsingTimestamp,
 		},
 		{ href, pathname } = document.location,
