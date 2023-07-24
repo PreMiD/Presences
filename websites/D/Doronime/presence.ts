2c0dfdc25cef8094af1b69f45438b0981ec3131f
noxzym <orchitiadi@gmail.com>
2023-07-25 00:09:12 +0700

    chore(Doronime): update code

--- websites/D/Doronime/presence.ts
+++ websites/D/Doronime/presence.ts
@@ -8,96 +8,119 @@ const presence = new Presence({
 	};
 
 presence.on("UpdateData", async () => {
-	const [base, ...data] = document.location.pathname.slice(1).split("/");
-	if (!base.length) presenceData.details = "Viewing Homepage";
-	if (base === "anime") {
-		if (data.length) {
-			const animeInfo = [
-					...document.querySelectorAll(".Content__breadcrumb > li").values(),
-				]
-					.slice(1)
-					.map(x => {
-						if (x.querySelector("a")) return x.querySelector("a").textContent;
-						return x.querySelector("span").textContent;
-					}),
-				parseDetails = animeInfo[0].includes("List")
-					? "Viewing Anime List"
-					: animeInfo[1].includes("Batch")
-					? "Viewing Anime Batch"
-					: animeInfo[0],
-				parseState = animeInfo[0].includes("List")
-					? animeInfo[1]
-					: animeInfo[1].includes("Batch")
-					? animeInfo[0]
-					: animeInfo[1];
-			presenceData.details = parseDetails;
-			presenceData.state = parseState;
-			presenceData.buttons = [
-				{
-					label: "View Anime",
-					url: document.location.href,
-				},
-			];
-		} else presenceData.details = "Viewing Anime List";
-	}
-	if (base === "batch") presenceData.details = "Viewing Anime Batch List";
-	if (base === "movie") presenceData.details = "Viewing Anime Movie List";
-	if (base === "ost") {
-		if (data.length) {
-			const [title, artist] = [
-				...document.querySelectorAll(
-					".Content__description-caption > div > div > div"
-				),
-			].map(x => x.textContent);
-			presenceData.details = artist;
-			presenceData.state = title;
-			presenceData.buttons = [
-				{
-					label: "View OST",
-					url: document.location.href,
-				},
-			];
-		} else presenceData.details = "Viewing Anime OST List";
-	}
-	if (base === "genre") {
-		if (data.length) {
-			presenceData.details = `Viewing Anime Genre ${
-				document.querySelector(".Content__tabs > div > span").textContent
-			}`;
-		} else presenceData.details = "Viewing Anime Genre List";
-	}
-	if (base === "season") {
-		if (data.length) {
-			presenceData.details = `Viewing Anime Season ${
-				document.querySelector(".Content__tabs > div > span").textContent
-			}`;
-		} else presenceData.details = "Viewing Anime Season List";
-	}
-	if (base === "schedule") presenceData.details = "Viewing Anime Schedule List";
-	if (base === "artist") {
-		if (data.length) {
-			presenceData.details = `Viewing OST Artist ${document
-				.querySelector(".Content__tabs > div > span")
-				.textContent.split(" ")
-				.slice(2)
-				.join(" ")}`;
-		} else presenceData.details = "Viewing Artist List";
-	}
-	if (base === "producer") {
-		if (data.length) {
-			presenceData.details = `Viewing Anime Producer ${
-				document.querySelector(".Content__tabs > div > span").textContent
-			}`;
-		} else presenceData.details = "Viewing Anime Producer List";
-	}
-	if (base === "studio") {
-		if (data.length) {
-			presenceData.details = `Viewing Anime Studio ${
-				document.querySelector(".Content__tabs > div > span").textContent
-			}`;
-		} else presenceData.details = "Viewing Anime Studio List";
-	}
+	const capitalize = (str: string[]) =>
+		str.map(x => x[0].toUpperCase() + x.slice(1)).join(" ");
+
 	if (document.location.search.startsWith("?s"))
 		presenceData.details = "Searching Anime";
+
+	const [base, ...data] = document.location.pathname.slice(1).split("/");
+	switch (base) {
+		case "batch":
+			presenceData.details = "Viewing Anime Batch List";
+			break;
+		case "movie":
+			presenceData.details = "Viewing Anime Movie List";
+			break;
+		case "schedule":
+			presenceData.details = "Viewing Anime Schedule List";
+			break;
+		case "anime":
+			if (data.length) {
+				const title = document
+					.querySelector(".Content__title")
+					.textContent.replace(/\[Batch\]|Subtitle Indonesia/g, "")
+					.split("Episode")[0]
+					.trim();
+				presenceData.details =
+					document.location.pathname.slice(1).split("/").length === 2
+						? "Viewing Anime Info"
+						: title;
+				presenceData.state =
+					document.location.pathname.slice(1).split("/").length === 2
+						? title
+						: capitalize(
+								document.location.pathname
+									.slice(1)
+									.split("/")[2]
+									.replace("bd-", "")
+									.split("-")
+						  );
+			} else presenceData.details = "Viewing Anime List";
+			break;
+		case "ost":
+			if (data.length) {
+				const [title, artist] = [
+					...document.querySelectorAll(
+						".Content__description-caption > .form-row > div"
+					),
+				]
+					.map((x, i) => (i % 2 === 1 ? x : null))
+					.filter(Boolean)
+					.splice(0, 2)
+					.map(x =>
+						x.textContent.replace(/ +?/g, "").split("\n").join(" ").trim()
+					);
+				presenceData.details =
+					title.split(" ").length > 1 ? title.split(" ")[1] : title;
+				presenceData.state = artist;
+			} else presenceData.details = "Viewing Anime OST List";
+			break;
+		case "genre":
+			if (data.length) {
+				presenceData.details = "Viewing Anime Genre";
+				presenceData.state = document.querySelector(
+					".Content__tabs > div > span"
+				).textContent;
+			} else presenceData.details = "Viewing Anime Genre List";
+			break;
+		case "season":
+			if (data.length) {
+				presenceData.details = "Viewing Anime Season";
+				presenceData.state = document.querySelector(
+					".Content__tabs > div > span"
+				).textContent;
+			} else presenceData.details = "Viewing Anime Season List";
+			break;
+		case "artist":
+			if (data.length) {
+				presenceData.details = "Viewing OST Artist";
+				presenceData.state = document.querySelector(
+					".Content__tabs > div > span"
+				).textContent;
+			} else presenceData.details = "Viewing Artist List";
+			break;
+		case "producer":
+			if (data.length) {
+				presenceData.details = "Viewing Anime Producer";
+				presenceData.state = document.querySelector(
+					".Content__tabs > div > span"
+				).textContent;
+			} else presenceData.details = "Viewing Anime Producer List";
+			break;
+		case "studio":
+			if (data.length) {
+				presenceData.details = "Viewing Anime Studio";
+				presenceData.state = document.querySelector(
+					".Content__tabs > div > span"
+				).textContent;
+			} else presenceData.details = "Viewing Anime Studio List";
+			break;
+		default:
+			presenceData.details = "Viewing Homepage";
+	}
+
+	presenceData.buttons = [
+		{
+			label: data.length
+				? base === "anime"
+					? "View Anime"
+					: base === "ost"
+					? "View OST"
+					: "Visit Doronime"
+				: "Visit Doronime",
+			url: document.location.href,
+		},
+	];
 	presence.setActivity(presenceData);
 });
