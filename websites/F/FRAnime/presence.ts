da101c2f0aecd85a3a53ca8107f145fa2ba9b5ac
Noah Schmidt <48517965+Wys2@users.noreply.github.com>
2023-07-24 17:07:47 -0700

    Update presence.ts
    Signed-off-by: Noah Schmidt <48517965+Wys2@users.noreply.github.com>

--- websites/F/FRAnime/presence.ts
+++ websites/F/FRAnime/presence.ts
@@ -1,55 +1,42 @@
 const presence = new Presence({
-	clientId: "1102272266278027296",
-});
-
-const enum playerAssets {
-	logo = "https://i.imgur.com/YKxUCs3.png",
-	logoBg = "https://i.imgur.com/byFtXWd.png",
-	pause = "https://i.imgur.com/0A75vqT.png",
-	play = "https://i.imgur.com/Dj5dekr.png",
-	search = "https://i.imgur.com/C3CetGw.png",
-}
-
-const enum Selectors {
-	watching = 'div[class="aspect-video border-[1.5px] border-gray-700 rounded-lg flex w-full justify-center align-items-center"]',
-	vk = "#video_player > div > div.videoplayer_media > video",
-	sibnetSendvid = "#video_html5_wrapper_html5_api",
-	myvi = "#player > video.player-video",
-	FRAnime = 'video[class="art-video"]',
-}
-
-const enum presenceStrings {
-	browsing = "Parcours le catalogue",
-	branding = "Animes gratuit & sans pub",
-	websiteName = "FRAnime.fr",
-	onPage = "Sur la page",
-	watchOn = "Regarder sur FRAnime.fr",
-}
-
-const animeBrowsingTimestamp = Math.floor(Date.now() / 1000);
+		clientId: "1102272266278027296",
+	}),
+	playerAssets = {
+		logo: "https://i.imgur.com/YKxUCs3.png",
+		logoBg: "https://i.imgur.com/byFtXWd.png",
+		pause: "https://i.imgur.com/0A75vqT.png",
+		play: "https://i.imgur.com/Dj5dekr.png",
+		search: "https://i.imgur.com/C3CetGw.png",
+	},
+	presenceStrings = {
+		browsing: "Parcours le catalogue",
+		branding: "Animes gratuit & sans pub",
+		websiteName: "FRAnime.fr",
+		onPage: "Sur la page",
+		watchOn: "Regarder sur FRAnime.fr",
+	},
+	animeBrowsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {},
-		{ href } = document.location;
+		{ href, pathname } = document.location,
+		params = new URLSearchParams(new URL(href).search.slice(1)),
+		scriptElement = document.querySelector<HTMLScriptElement>(
+			'script[type="application/ld+json"]'
+		),
+		jsonLD = scriptElement ? JSON.parse(scriptElement.textContent || "") : null;
 
 	// Home page
-	presenceData.details = presenceStrings.browsing;
-	presenceData.state = presenceStrings.branding;
-	presenceData.smallImageText = presenceStrings.websiteName;
-	presenceData.largeImageKey = playerAssets.logoBg;
-	presenceData.smallImageKey = playerAssets.search;
-	presenceData.startTimestamp = animeBrowsingTimestamp;
+	if (pathname === "/") {
+		presenceData.details = presenceStrings.browsing;
+		presenceData.state = presenceStrings.branding;
+		presenceData.smallImageText = presenceStrings.websiteName;
+		presenceData.largeImageKey = playerAssets.logoBg;
+		presenceData.smallImageKey = playerAssets.search;
+		presenceData.startTimestamp = animeBrowsingTimestamp;
 
-	// Anime page
-	if (href.split("/").includes("anime")) {
-		// Get the scriptElement that contains the episode info
-		const scriptElement = document.querySelector<HTMLScriptElement>(
-				'script[type="application/ld+json"]'
-			),
-			jsonLD = scriptElement
-				? JSON.parse(scriptElement.textContent || "")
-				: null;
-
+		// Anime page
+	} else if (params.get("ep") === "") {
 		presenceData.details = `${presenceStrings.onPage} ${jsonLD.name}`;
 		presenceData.state = presenceStrings.branding;
 		presenceData.smallImageText = presenceStrings.websiteName;
@@ -63,114 +50,16 @@ presence.on("UpdateData", async () => {
 			},
 		];
 
-		// watching page
-		if (document.querySelector(Selectors.watching)) {
-			presenceData.details = `${jsonLD.name} - S${
-				href.split("=")[1].split("&")[0]
-			}, EP${href.split("=")[2].split("&")[0]}`;
-			presenceData.state = presenceStrings.branding;
-			presenceData.smallImageText = presenceStrings.websiteName;
-			presenceData.smallImageKey = playerAssets.pause;
-			presenceData.buttons = [
-				{
-					label: presenceStrings.watchOn,
-					url: href,
-				},
-			];
+		// Watching page
+	} else if (document.querySelector("#play_button")) {
+		presenceData.details = `${jsonLD.name} - S${params.getAll(
+			"s"
+		)}, EP${params.getAll("ep")}`;
 
-			// The anime is loading, so we delete the timestamp
-			delete presenceData.startTimestamp;
-			delete presenceData.endTimestamp;
-
-			// player is loaded
-		} else if (document.querySelectorAll("video")) {
-			const video = document.querySelector("video");
-
-			presenceData.details = `${jsonLD.name} - S${
-				href.split("=")[1].split("&")[0]
-			}, EP${href.split("=")[2].split("&")[0]}`;
-			presenceData.state = presenceStrings.branding;
-			presenceData.smallImageText = presenceStrings.websiteName;
-
-			presenceData.startTimestamp = presence.getTimestampsfromMedia(video)[0];
-
-			presenceData.endTimestamp = presence.getTimestampsfromMedia(video)[1];
-			presenceData.buttons = [
-				{
-					label: presenceStrings.watchOn,
-					url: href,
-				},
-			];
-
-			// If the video is playing, display the "Play" small Image. Else, display the pause one and delete the timestamp
-			if (!video.paused) presenceData.smallImageKey = playerAssets.play;
-			else {
-				presenceData.smallImageKey = playerAssets.pause;
-				delete presenceData.startTimestamp;
-				delete presenceData.endTimestamp;
-			}
-		}
-	}
-	presence.setActivity(presenceData);
-});
-const presence = new Presence({
-	clientId: "1102272266278027296",
-});
-
-const enum playerAssets {
-	logo = "https://i.imgur.com/YKxUCs3.png",
-	logoBg = "https://i.imgur.com/byFtXWd.png",
-	pause = "https://i.imgur.com/0A75vqT.png",
-	play = "https://i.imgur.com/Dj5dekr.png",
-	search = "https://i.imgur.com/C3CetGw.png",
-}
-
-const enum Selectors {
-	watching = 'div[class="aspect-video border-[1.5px] border-gray-700 rounded-lg flex w-full justify-center align-items-center"]',
-	vk = "#video_player > div > div.videoplayer_media > video",
-	sibnetSendvid = "#video_html5_wrapper_html5_api",
-	myvi = "#player > video.player-video",
-	FRAnime = 'video[class="art-video"]',
-}
-
-const enum presenceStrings {
-	browsing = "Parcours le catalogue",
-	branding = "Animes gratuit & sans pub",
-	websiteName = "FRAnime.fr",
-	onPage = "Sur la page",
-	watchOn = "Regarder sur FRAnime.fr",
-}
-
-const animeBrowsingTimestamp = Math.floor(Date.now() / 1000);
-
-presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {},
-		{ href } = document.location;
-
-	// Home page
-	presenceData.details = presenceStrings.browsing;
-	presenceData.state = presenceStrings.branding;
-	presenceData.smallImageText = presenceStrings.websiteName;
-	presenceData.largeImageKey = playerAssets.logoBg;
-	presenceData.smallImageKey = playerAssets.search;
-	presenceData.startTimestamp = animeBrowsingTimestamp;
-
-	// Anime page
-	if (href.split("/").includes("anime")) {
-		// Get the scriptElement that contains the episode info
-		const scriptElement = document.querySelector<HTMLScriptElement>(
-				'script[type="application/ld+json"]'
-			),
-			jsonLD = scriptElement
-				? JSON.parse(scriptElement.textContent || "")
-				: null;
-
-		presenceData.details = `${presenceStrings.onPage} ${jsonLD.name}`;
 		presenceData.state = presenceStrings.branding;
 		presenceData.smallImageText = presenceStrings.websiteName;
+		presenceData.smallImageKey = playerAssets.pause;
 		presenceData.largeImageKey = jsonLD.thumbnailUrl;
-		presenceData.smallImageKey = playerAssets.search;
-		presenceData.startTimestamp = animeBrowsingTimestamp;
 		presenceData.buttons = [
 			{
 				label: presenceStrings.watchOn,
@@ -178,52 +67,37 @@ presence.on("UpdateData", async () => {
 			},
 		];
 
-		// watching page
-		if (document.querySelector(Selectors.watching)) {
-			presenceData.details = `${jsonLD.name} - S${
-				href.split("=")[1].split("&")[0]
-			}, EP${href.split("=")[2].split("&")[0]}`;
-			presenceData.state = presenceStrings.branding;
-			presenceData.smallImageText = presenceStrings.websiteName;
-			presenceData.smallImageKey = playerAssets.pause;
-			presenceData.buttons = [
-				{
-					label: presenceStrings.watchOn,
-					url: href,
-				},
-			];
+		// The anime is loading, so we delete the timestamp
+		delete presenceData.startTimestamp;
+		delete presenceData.endTimestamp;
 
-			// The anime is loading, so we delete the timestamp
+		// player is loaded
+	} else if (document.querySelector("video")) {
+		const video = document.querySelector("video"),
+			[startTimestamp, endTimestamp] = presence.getTimestampsfromMedia(video);
+
+		presenceData.details = `${jsonLD.name} - S${params.getAll(
+			"s"
+		)}, EP${params.getAll("ep")}`;
+
+		presenceData.state = presenceStrings.branding;
+		presenceData.smallImageText = presenceStrings.websiteName;
+		presenceData.startTimestamp = startTimestamp;
+		presenceData.endTimestamp = endTimestamp;
+		presenceData.largeImageKey = jsonLD.thumbnailUrl;
+		presenceData.buttons = [
+			{
+				label: presenceStrings.watchOn,
+				url: href,
+			},
+		];
+
+		// If the video is playing, display the "Play" small Image. Else, display the pause one and delete the timestamp
+		if (!video.paused) presenceData.smallImageKey = playerAssets.play;
+		else {
+			presenceData.smallImageKey = playerAssets.pause;
 			delete presenceData.startTimestamp;
 			delete presenceData.endTimestamp;
-
-			// player is loaded
-		} else if (document.querySelectorAll("video")) {
-			const video = document.querySelector("video");
-
-			presenceData.details = `${jsonLD.name} - S${
-				href.split("=")[1].split("&")[0]
-			}, EP${href.split("=")[2].split("&")[0]}`;
-			presenceData.state = presenceStrings.branding;
-			presenceData.smallImageText = presenceStrings.websiteName;
-
-			presenceData.startTimestamp = presence.getTimestampsfromMedia(video)[0];
-
-			presenceData.endTimestamp = presence.getTimestampsfromMedia(video)[1];
-			presenceData.buttons = [
-				{
-					label: presenceStrings.watchOn,
-					url: href,
-				},
-			];
-
-			// If the video is playing, display the "Play" small Image. Else, display the pause one and delete the timestamp
-			if (!video.paused) presenceData.smallImageKey = playerAssets.play;
-			else {
-				presenceData.smallImageKey = playerAssets.pause;
-				delete presenceData.startTimestamp;
-				delete presenceData.endTimestamp;
-			}
 		}
 	}
 	presence.setActivity(presenceData);
