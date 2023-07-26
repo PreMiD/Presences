9fd75733ebd282a99d42e7b94f04ad4e79058f88
theusaf <theusafyt@gmail.com>
2023-07-26 08:05:40 -0700

    chore: format files

--- websites/F/Fire Emblem Heroes/presence.ts
+++ websites/F/Fire Emblem Heroes/presence.ts
@@ -67,9 +67,8 @@ function applyMainHostDetails(
 				presenceData.buttons = [
 					{ label: "Read Manga", url: document.location.href },
 				];
-			} else {
-				presenceData.details = "Browsing manga";
-			}
+			} else presenceData.details = "Browsing manga";
+
 			break;
 		}
 		case "system": {
@@ -81,20 +80,17 @@ function applyMainHostDetails(
 			presenceData.details = "Browsing articles";
 			if (section) {
 				const articleContainer = document.querySelector<HTMLDivElement>(
-						`${section}`
-					),
-					articleDate =
-						articleContainer.querySelector<HTMLSpanElement>(
-							".date"
-						).textContent,
-					articleTitle =
-						articleContainer.querySelector<HTMLParagraphElement>(
-							".heading"
-						).textContent,
-					articleImage =
-						articleContainer.querySelector<HTMLImageElement>(".img > img").src;
-				presenceData.state = `${articleTitle} - ${articleDate}`;
-				presenceData.largeImageKey = articleImage;
+					`${section}`
+				);
+
+				presenceData.state = `${
+					articleContainer.querySelector<HTMLParagraphElement>(".heading")
+						.textContent
+				} - ${
+					articleContainer.querySelector<HTMLSpanElement>(".date").textContent
+				}`;
+				presenceData.largeImageKey =
+					articleContainer.querySelector<HTMLImageElement>(".img > img").src;
 				presenceData.buttons = [
 					{
 						label: "Read Article",
@@ -128,23 +124,23 @@ const observer = new IntersectionObserver(
 function activateMainIntersectionObservers(pathList: string[]): void {
 	if (intersectionObserversActivated) return;
 	switch (pathList[0] ?? "") {
-		case "":
+		case "": {
 			observer.observe(document.querySelector("#cont2"));
 			observer.observe(document.querySelector("#cont4"));
 			break;
-		case "topics":
+		}
+		case "topics": {
 			const articles = [...document.querySelectorAll(".article")];
 			for (const article of articles) observer.observe(article);
 			break;
+		}
 	}
 	intersectionObserversActivated = true;
 }
 
 function applyCYLDetails(presenceData: PresenceData, pathList: string[]): void {
 	const campaignTitle =
-			document.querySelector<HTMLDivElement>(".campaigns-title").textContent,
-		search = new URLSearchParams(document.location.search);
-
+		document.querySelector<HTMLDivElement>(".campaigns-title").textContent;
 	presenceData.buttons = [
 		{ label: "View Campaign", url: document.location.href },
 	];
@@ -172,63 +168,63 @@ function applyCYLDetails(presenceData: PresenceData, pathList: string[]): void {
 		case "result_detail":
 		case "results": {
 			if (
-				search.get("overall") ||
+				new URLSearchParams(document.location.search).get("overall") ||
 				pathList[0] !== "result" ||
 				(pathList[0] === "result" && pathList[1])
-			) {
+			)
 				presenceData.details = `Viewing ${campaignTitle} results`;
-			} else {
+			else {
 				// Note to future maintainers: The pages for each event look the same, but seem to keep changing
 				// the class names for the elements between events.
 				// I've tried to make this as future-proof as possible, but if it breaks, this is probably why.
-				const winners = [
-						...document.querySelectorAll<HTMLDivElement>(
-							".result-special-character [class*=result-special-character-wrap], .HeroItem.-golden"
-						),
-					],
-					winnerData = winners.map<PresenceData>(winner => {
-						let mainImage = winner.querySelector<HTMLImageElement>(
-							".result-stand > img, .hero-image, .HeroItem__Image"
-						)?.src;
-						if (!mainImage)
-							mainImage = getComputedStyle(
-								winner.querySelector<HTMLParagraphElement>(
-									"[class*=result-stand]"
-								)
-							)?.backgroundImage.match(/url\((.+)\)/)[1];
-						let rankImage = winner.querySelector<HTMLImageElement>(
-							".star > img, .icon-rank, .HeroItem__Rank"
-						).src;
-						if (!rankImage)
-							rankImage = getComputedStyle(
-								winner,
-								"::after"
-							).backgroundImage.match(/url\((.+)\)/)[1];
-						return {
-							...presenceData,
-							details: `Viewing winners of ${campaignTitle}`,
-							state: `${
-								winner.querySelector(
-									".hero-result-hero-name, .hero-name, .HeroItem__Name"
-								).textContent
-							} - ${
-								winner.querySelector(
-									".hero-result-series-name, .series-name, .HeroItem__SeriesName"
-								).textContent
-							}`,
-							largeImageKey: mainImage,
-							smallImageKey: rankImage,
-							smallImageText: `${
-								winner.querySelector(
-									".hero-result-vote-count, .HeroItem__VoteCount, .vote-count"
-								).textContent
-							} votes}`,
-							buttons: [{ label: "View Results", url: document.location.href }],
-						};
-					});
-				for (const winner of winnerData) {
+				const winnerData = [
+					...document.querySelectorAll<HTMLDivElement>(
+						".result-special-character [class*=result-special-character-wrap], .HeroItem.-golden"
+					),
+				].map<PresenceData>(winner => {
+					let mainImage = winner.querySelector<HTMLImageElement>(
+						".result-stand > img, .hero-image, .HeroItem__Image"
+					)?.src;
+					if (!mainImage) {
+						mainImage = getComputedStyle(
+							winner.querySelector<HTMLParagraphElement>(
+								"[class*=result-stand]"
+							)
+						)?.backgroundImage.match(/url\((.+)\)/)[1];
+					}
+					let rankImage = winner.querySelector<HTMLImageElement>(
+						".star > img, .icon-rank, .HeroItem__Rank"
+					).src;
+					if (!rankImage) {
+						rankImage = getComputedStyle(
+							winner,
+							"::after"
+						).backgroundImage.match(/url\((.+)\)/)[1];
+					}
+					return {
+						...presenceData,
+						details: `Viewing winners of ${campaignTitle}`,
+						state: `${
+							winner.querySelector(
+								".hero-result-hero-name, .hero-name, .HeroItem__Name"
+							).textContent
+						} - ${
+							winner.querySelector(
+								".hero-result-series-name, .series-name, .HeroItem__SeriesName"
+							).textContent
+						}`,
+						largeImageKey: mainImage,
+						smallImageKey: rankImage,
+						smallImageText: `${
+							winner.querySelector(
+								".hero-result-vote-count, .HeroItem__VoteCount, .vote-count"
+							).textContent
+						} votes}`,
+						buttons: [{ label: "View Results", url: document.location.href }],
+					};
+				});
+				for (const winner of winnerData)
 					slideshow.addSlide(winner.largeImageKey, winner, 5e3);
-				}
 			}
 			break;
 		}
@@ -255,9 +251,7 @@ function applyCYLDetails(presenceData: PresenceData, pathList: string[]): void {
 						{ label: "Vote Character", url: document.location.href },
 					];
 				}
-			} else {
-				presenceData.state = "Selecting a series";
-			}
+			} else presenceData.state = "Selecting a series";
 			break;
 		}
 	}
@@ -295,16 +289,12 @@ presence.on("UpdateData", async () => {
 		case /vote\d+[.]campaigns[.]fire-emblem-heroes[.]com/.test(hostname):
 		case hostname === "support.fire-emblem-heroes.com": {
 			if (hostname.startsWith("support")) {
-				if (pathList[0] === "vote" || pathList[0] === "vote2") {
+				if (pathList[0] === "vote" || pathList[0] === "vote2")
 					applyCYLDetails(presenceData, pathList.slice(1));
-				} else {
-					applySupportDetails(presenceData, pathList);
-				}
-			} else if (hostname.startsWith("vote3")) {
+				else applySupportDetails(presenceData, pathList);
+			} else if (hostname.startsWith("vote3"))
 				applyCYLDetails(presenceData, pathList);
-			} else {
-				applyCYLDetails(presenceData, pathList.slice(1));
-			}
+			else applyCYLDetails(presenceData, pathList.slice(1));
 			break;
 		}
 	}
