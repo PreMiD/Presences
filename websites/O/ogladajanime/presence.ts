639a5c5f3f32f0436baf1c7cd45d0675fe2ee226
_eki <ekigamesplay6@gmail.com>
2023-08-08 11:38:36 +0200

    discord rpc

---
+++ websites/O/ogladajanime/presence.ts
@@ -0,0 +1,50 @@
+const presence = new Presence({
+    clientId: "1137362720254074972", // Client ID
+});
+const browsingTimestamp = Math.floor(Date.now() / 1000);
+
+let anime: HTMLElement,
+    animeicon = document.querySelector(".img-fluid.lozad");
+
+presence.on("UpdateData", async () => {
+    const presenceData: PresenceData = {
+        startTimestamp: browsingTimestamp,
+		largeImageKey: "https://i.imgur.com/TKVukXW.png"
+    };
+
+    if (document.location.pathname === "/main2") {
+        presenceData.details = "Viewing the home page";
+    } else if (document.location.pathname.includes("/search/name/")) {
+        presenceData.details = "Searching anime";
+    } else if (document.location.pathname.includes("/chat")) {
+        presenceData.details = "Writing in the chat";
+    } else if (document.location.pathname.includes("/anime_list/")) {
+        presenceData.details = "Browsing the anime list";
+        const listlink = document.location.href;
+        presenceData.buttons = [
+            { label: "Anime List", url: listlink },
+        ];
+    } else if (document.location.pathname.includes("/anime")) {
+        anime = document.querySelector("#anime_name_id");
+        if (anime) {
+            presenceData.details = anime.innerText;
+            presenceData.smallImageKey = "https://i.imgur.com/TKVukXW.png";
+            const animeLink = document.location.href;
+            presenceData.buttons = [
+                { label: "Watch Now", url: animeLink },
+            ];
+
+            const episodeNumber = parseInt(animeLink.split('/').pop()) || 1;
+            presenceData.state = `Episode ${episodeNumber}`;
+        }
+
+        if (animeicon) {
+            presenceData.largeImageKey = animeicon
+                .getAttribute("data-srcset")
+                .split(" ")[0];
+        }
+    }
+    
+    // Set the activity
+    presence.setActivity(presenceData);
+});
