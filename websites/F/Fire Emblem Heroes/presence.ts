2b4b3eeff9a4a266158b9a71bf1acbabde07c70c
theusaf <theusafyt@gmail.com>
2023-08-09 00:38:48 -0700

    feat(guide): add support

--- websites/F/Fire Emblem Heroes/presence.ts
+++ websites/F/Fire Emblem Heroes/presence.ts
@@ -407,6 +407,64 @@ function applyFehPassDetails(presenceData: PresenceData, pathList: string[]) {
 	}
 }
 
+function applyGuideDetails(presenceData: PresenceData, pathList: string[]) {
+	switch (pathList[0] ?? "") {
+		case "": {
+			presenceData.details = "Browsing the Guide";
+			break;
+		}
+		case "category": {
+			if (pathList[1] === "character") {
+				presenceData.details = "Browsing the Guide: Character List";
+				presenceData.state = document.querySelector<HTMLSelectElement>(".select_character").selectedOptions[0].textContent;
+			} else if (pathList[1] === "comeback") {
+				switch (pathList[2] ?? "") {
+					case "": {
+						presenceData.details = "Browsing the Guide: Returner Tips";
+						break;
+					}
+					case "elementary":
+					case "intermediate":
+					case "advanced": {
+						presenceData.details = "Browsing the Guide: Advanced Training Tips";
+						presenceData.details = `Browsing the Guide: ${pathList[2][0].toUpperCase()}${pathList[2].slice(1)} Training Tips`;
+						break;
+					}
+					case "weapon": {
+						presenceData.details = "Browsing the Guide: List of Weapons";
+						presenceData.state = document.querySelector<HTMLSelectElement>(".select_weapon").selectedOptions[0].textContent;
+						break;
+					}
+				}
+			}
+			break;
+		}
+		default: {
+			const characterSubName = document.querySelector<HTMLParagraphElement>(".sec_charanick").textContent,
+				characterName = document.querySelector<HTMLHeadingElement>(".sec_charaname").textContent,
+				characterImage = document.querySelector<HTMLImageElement>(".sec_gif > img").src,
+				largeCharacterImage = document.querySelector<HTMLImageElement>(".slick-slide.slick-current img").src,
+				characterDescriptions = [...document.querySelectorAll("dl dt")].map(dt => ({
+					text: dt.textContent,
+					image: dt.nextElementSibling.querySelector("img").src,
+				}));
+			presenceData.details = `Reading about ${characterName}: ${characterSubName}`;
+			presenceData.largeImageKey = largeCharacterImage;
+			presenceData.smallImageKey = characterImage;
+			presenceData.buttons = [{ label: "View Character", url: document.location.href }];
+			slideshow.addSlide(characterImage, presenceData, 5e3);
+			for (const description of characterDescriptions) {
+				const tmpData: PresenceData = Object.assign({}, presenceData);
+				tmpData.state = truncateText(description.text);
+				tmpData.smallImageKey = description.image;
+				slideshow.addSlide(description.image, tmpData, 5e3);
+			}
+		}
+	}
+}
+
+function applyNewGuideDetails(presenceData: PresenceData, pathList: string[]) {}
+
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			largeImageKey: Assets.Logo,
@@ -444,6 +502,13 @@ presence.on("UpdateData", async () => {
 		}
 		case hostname === "fehpass.fire-emblem-heroes.com": {
 			applyFehPassDetails(presenceData, pathList.slice(1));
+			break;
+		}
+		case hostname === "guide.fire-emblem-heroes.com": {
+			applyGuideDetails(presenceData, pathList.slice(1));
+			break;
+		}
+		case hostname === "new-guide.fire-emblem-heroes.com": {
 			break;
 		}
 	}
