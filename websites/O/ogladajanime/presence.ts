2ba1368126a1e73810df28a4a903a5b75f01d308
_eki <ekigamesplay6@gmail.com>
2023-08-09 19:39:14 +0200

    Update presence.ts

--- websites/O/ogladajanime/presence.ts
+++ websites/O/ogladajanime/presence.ts
@@ -1,8 +1,7 @@
 const presence = new Presence({ clientId: "1137362720254074972" }),
 	browsingTimestamp = Math.floor(Date.now() / 1000),
 	anime: HTMLElement = document.querySelector("#anime_name_id"),
-	animeicon = document.querySelector(".img-fluid.lozad"),
-	{ pathname, href } = document.location;
+	animeicon = document.querySelector(".img-fluid.lozad");
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
@@ -18,14 +17,19 @@ presence.on("UpdateData", async () => {
 		presenceData.details = "Rozmawia na chacie";
 	else if (document.location.pathname.includes("/anime_list/")) {
 		presenceData.details = "Przegląda listę Anime";
-		presenceData.buttons = [{ label: "Lista Anime", url: href }];
+		presenceData.buttons = [
+			{ label: "Lista Anime", url: document.location.href },
+		];
 	} else if (document.location.pathname.includes("/anime")) {
 		if (anime) {
 			presenceData.details = anime.textContent;
 			presenceData.smallImageKey = "https://i.imgur.com/xHce23t.png";
-			presenceData.buttons = [{ label: "Obejrzyj teraz", url: href }];
+			presenceData.buttons = [
+				{ label: "Obejrzyj teraz", url: document.location.href },
+			];
 
-			const episodeNumber = parseInt(href.split("/").pop() || "1", 10) || 1;
+			const episodeNumber =
+				parseInt(document.location.href.split("/").pop() || "1", 10) || 1;
 			presenceData.state = `Odcinek : ${episodeNumber}`;
 		}
 
