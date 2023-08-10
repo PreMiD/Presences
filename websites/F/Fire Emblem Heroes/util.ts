6d627e3c6571c23eb51e28e5b0416dc9324e64ad
SeMiD <semid@premid.app>
2023-08-10 19:55:10 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/F/Fire Emblem Heroes/util.ts
+++ websites/F/Fire Emblem Heroes/util.ts
@@ -1,10 +1,9 @@
-export const presence = new Presence({
-		clientId: "1133602327476047873",
-	}),
-	slideshow = presence.createSlideshow();
+export const slideshow = new Presence({
+	clientId: "1133602327476047873",
+}).createSlideshow();
 
 export const enum Assets {
-	Logo = "https://i.imgur.com/Wrd78TA.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/F/Fire%20Emblem%20Heroes/assets/logo.png",
 }
 
 export function truncateText(text: string): string {
