261fd3f24c16283b3cd4e4a8eda0e1870b590c35
SeMiD <semid@premid.app>
2023-08-16 23:04:18 +0100

    chore(Moopa): update assets (#7583)
    chore: update assets
    
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/M/Moopa/presence.ts
+++ websites/M/Moopa/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/lelaLhp.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/M/Moopa/assets/logo.png",
 }
 
 function getTitle(titleLang: number) {
