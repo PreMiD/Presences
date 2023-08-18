c775f5ae2b722da18f170e5bc08bbb780bcb9b0e
Mathe2005 <62605025+Mathe2005@users.noreply.github.com>
2023-08-18 15:47:58 +0400

    Update presence.ts
    Signed-off-by: Mathe2005 <62605025+Mathe2005@users.noreply.github.com>

--- websites/A/ANIMETV7/presence.ts
+++ websites/A/ANIMETV7/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/CtyAaoq.jpg",
+	Logo = "https://i.imgur.com/hwnprna.jpg",
 }
 
 function getTitle(titleLang: number) {
