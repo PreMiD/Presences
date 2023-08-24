6e1aa722853741e8150e8aaf2fd52669e1ea4542
SeMiD <semid@premid.app>
2023-08-24 18:16:22 +0100

    chore(ANIMETV7): update assets (#7595)
    chore: update assets
    
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/A/ANIMETV7/presence.ts
+++ websites/A/ANIMETV7/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/WYI9kyx.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/A/ANIMETV7/assets/logo.png",
 }
 
 function getTitle(titleLang: number) {
