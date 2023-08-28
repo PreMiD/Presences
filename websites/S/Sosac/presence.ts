7478a2e156563dcd8e3bbd6d1cc5a053a7ed3f90
Jakub Knot <jakearts@email.cz>
2023-08-28 15:25:58 +0200

    feat(Sosac): fix CI issues

--- websites/S/Sosac/presence.ts
+++ websites/S/Sosac/presence.ts
@@ -1,73 +1,75 @@
 const presence = new Presence({
 	clientId: "1079537235076071524",
 }),
-strings = presence.getStrings({
-	play: "general.playing",
-	pause: "general.paused",
-	browsing: "general.browsing",
-});
+	strings = presence.getStrings({
+		play: "general.playing",
+		pause: "general.paused",
+		browsing: "general.browsing",
+	});
 let video = {
-duration: 0,
-currentTime: 0,
-paused: true,
+	duration: 0,
+	currentTime: 0,
+	paused: true,
 };
 
 const enum Assets {
-Logo = "https://i.imgur.com/iOYRQjm.png",
+	Logo = "https://i.imgur.com/iOYRQjm.png",
 }
 
 presence.on(
-"iFrameData",
-(data: { duration: number; currentTime: number; paused: boolean }) => {
-	video = data;
-}
+	"iFrameData",
+	(data: { duration: number; currentTime: number; paused: boolean }) => {
+		video = data;
+	}
 );
 
 presence.on("UpdateData", async () => {
-const presenceData: PresenceData = {
-	largeImageKey: Assets.Logo,
-};
+	let strs = await strings;
+	const presenceData: PresenceData = {
+		largeImageKey: Assets.Logo,
+	};
 
-if (
-	video &&
-	!isNaN(video.duration) &&
-	document.location.pathname.includes("/player")
-) {
-	[presenceData.startTimestamp, presenceData.endTimestamp] =
-		presence.getTimestamps(
-			Math.floor(video.currentTime),
-			Math.floor(video.duration)
-		);
+	if (
+		video &&
+		!isNaN(video.duration) &&
+		document.location.pathname.includes("/player")
+	) {
+		[presenceData.startTimestamp, presenceData.endTimestamp] =
+			presence.getTimestamps(
+				Math.floor(video.currentTime),
+				Math.floor(video.duration)
+			);
 
-		const firstH3Title = document.querySelector('h3');
+		const firstH3Title = document.querySelector("h3");
 
 		if (firstH3Title) {
-		  const originalText = firstH3Title.textContent;
-		  
-		  const movpattern = /^([^\n]+)/; // Movie/Series name
-		  const movmatch = originalText.match(movpattern)[1];
-		  
-		  const epipattern = /\d+x\d+/; // Episode number
-		  const epimatch = originalText.match(epipattern);
-		  
-		  const result = epimatch ? epimatch[0] : ' ';
-  		  presenceData.details = ''+movmatch+' '+result;}
-	  
-		  presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
-	presenceData.smallImageText = video.paused
-		? (await strings).pause
-		: (await strings).play;
+			const originalText = firstH3Title.textContent;
 
-	if (video.paused) {
-		delete presenceData.startTimestamp;
-		delete presenceData.endTimestamp;
+			const movpattern = /^([^\n]+)/; // Movie/Series name
+			const movmatch = originalText.match(movpattern)[1];
+
+			const epipattern = /\d+x\d+/; // Episode number
+			const epimatch = originalText.match(epipattern);
+
+			const result = epimatch ? epimatch[0] : " ";
+			presenceData.details = "" + movmatch + " " + result;
+		}
+
+		presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
+		presenceData.smallImageText = video.paused
+			? strs.pause
+			: strs.play;
+
+		if (video.paused) {
+			delete presenceData.startTimestamp;
+			delete presenceData.endTimestamp;
+		}
+
+		presence.setActivity(presenceData, !video.paused);
+	} else {
+		presenceData.details = strs.browsing;
+		presenceData.smallImageKey = Assets.Search;
+		presenceData.smallImageText = strs.browsing;
+		presence.setActivity(presenceData);
 	}
-
-	presence.setActivity(presenceData, !video.paused);
-} else {
-	presenceData.details = (await strings).browsing;
-	presenceData.smallImageKey = Assets.Search;
-	presenceData.smallImageText = (await strings).browsing;
-	presence.setActivity(presenceData);
-}
 });
