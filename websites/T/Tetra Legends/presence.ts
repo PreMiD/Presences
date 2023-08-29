b66b14448c96e8875364a41fddf786e798eab5aa
DXL44 <28364459+DXL44@users.noreply.github.com>
2023-08-29 13:39:08 -0400

    style(Tetra Legends): formatted code

--- websites/T/Tetra Legends/presence.ts
+++ websites/T/Tetra Legends/presence.ts
@@ -3,19 +3,19 @@ const presence = new Presence({
 	}),
 	gameTime = Math.floor(Date.now() / 1000);
 
-
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey:
-				"https://i.imgur.com/UV7X7U4.jpg",
+		largeImageKey: "https://i.imgur.com/UV7X7U4.jpg",
 		details: "In Menus",
 		startTimestamp: gameTime,
 	};
 
-	if (document.querySelector("#game-container").classList[0] === "hidden") { // 
+	if (document.querySelector("#game-container").classList[0] === "hidden")
 		presenceData.details = "In Menus";
-	} else 
-		presenceData.details = `Mode: ${document.querySelector("header.header").innerHTML}`;
-
+	else {
+		presenceData.details = `Mode: ${
+			document.querySelector("header.header").innerHTML
+		}`;
+	}
 	presence.setActivity(presenceData);
 });
