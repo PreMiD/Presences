d23407ebf7a2de195a69b5bfe89f39acfb594f92
DXL44 <28364459+DXL44@users.noreply.github.com>
2023-08-29 13:23:42 -0400

    feat(Tetra Legends): add presence

---
+++ websites/T/Tetra Legends/presence.ts
@@ -0,0 +1,21 @@
+const presence = new Presence({
+		clientId: "1145899076760637460",
+	}),
+	gameTime = Math.floor(Date.now() / 1000);
+
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey:
+				"https://i.imgur.com/UV7X7U4.jpg",
+		details: "In Menus",
+		startTimestamp: gameTime,
+	};
+
+	if (document.querySelector("#game-container").classList[0] === "hidden") { // 
+		presenceData.details = "In Menus";
+	} else 
+		presenceData.details = `Mode: ${document.querySelector("header.header").innerHTML}`;
+
+	presence.setActivity(presenceData);
+});
