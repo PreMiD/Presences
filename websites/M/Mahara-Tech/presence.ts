67d31cd7d64bf1b31fa87f98cea7a56dacbe6224
Youssef Ayman <62572632+ELJoOker2004@users.noreply.github.com>
2023-08-30 23:32:00 +0300

    Update presence.ts
    Signed-off-by: Youssef Ayman <62572632+ELJoOker2004@users.noreply.github.com>

--- websites/M/Mahara-Tech/presence.ts
+++ websites/M/Mahara-Tech/presence.ts
@@ -8,13 +8,9 @@ interface Route extends Functionlize<Partial<PresenceData>> {
 	run?(): PresenceData;
 }
 
-const enum Assets {
-	Logo = "https://i.imgur.com/SjtqzAF.png",
-	PAUSED = "https://raw.githubusercontent.com/PreMiD/Presences/main/.resources/pause.png",
-	PLAYED = "https://raw.githubusercontent.com/PreMiD/Presences/main/.resources/play.png",
-	SEARCHING = "https://raw.githubusercontent.com/PreMiD/Presences/main/.resources/search.png",
-	Discover = "https://raw.githubusercontent.com/PreMiD/Presences/main/.resources/reading.png",
-	VIEWING = "https://raw.githubusercontent.com/PreMiD/Presences/main/.resources/viewing.png",
+const enum LoAssets {
+	Logo = "https://i.imgur.com/QRBVWsG.png",
+
 }
 
 enum Settings {
@@ -36,114 +32,122 @@ const presence = new Presence({
 		presenceData: PresenceData;
 	}): Route => {
 		const routes: Route[] = [
-				{
-					path: /^\/my\//,
-					details: () => "On Homepage",
-					state: () => {
-						return "Dashboard";
-					},
-					smallImageKey: () => Assets.Discover,
-					smallImageText: () => "Browsing Courses",
+
+			
+			{
+				path: /^\/my\//,
+				details: () => "On Homepage",
+				state: () => {
+					return "Dashboard";
 				},
-				{
-					path: /^\/user\//,
-					details: () => "Viewing a profile",
-					state: () => {
-						return `${
-							document.querySelector("h4.breadcrumb_title").textContent
-						}'s profile`;
-					},
-					smallImageKey: () => Assets.Discover,
-
-					buttons: () => [
-						{
-							label: "Visit profile",
-							url: (() => {
-								return window.location.href;
-							})(),
-						},
-					],
+				smallImageKey: () => Assets.Reading,
+				smallImageText: () => "Browsing Courses",
+			},
+			{
+				path: /^\/user\//,
+				details: () => "Viewing a profile",
+				state: () => {
+					return document.querySelector("h4.breadcrumb_title").textContent + "'s profile";
 				},
-				{
-					path: /^\/mod\/hvp\/*/,
-
-					run: () => {
-						[, presenceData.endTimestamp] = presence.getTimestamps(
-							Math.floor(video.currentTime),
-							Math.floor(video.duration)
-						);
-
-						if (video.paused) presence.getTimestamps;
-
-						return presenceData;
+				smallImageKey: () => Assets.Reading,
+				
+				buttons: () => [
+					{
+						label: "Visit profile",
+						url: (() => {
+							return window.location.href;
+						})(),
 					},
-					playback: () => !video.paused,
-					smallImageKey: () => (video.paused ? Assets.PAUSED : Assets.PLAYED),
-					smallImageText: () => (video.paused ? "Paused" : "Playing"),
+				],
+			},
+			{
+				path: /^\/mod\/hvp\/*/,
 
-					details: () => document.title,
-					state: () => {
-						return document.querySelector("h4.title.float-left").textContent;
-					},
-					buttons: () => [
-						{
-							label: "View Course",
-							url: (() => {
-								const href = document
+				run: () => {
+					[, presenceData.endTimestamp] = presence.getTimestamps(
+						Math.floor(video.currentTime),
+						Math.floor(video.duration)
+					);
+
+					if (video.paused) presence.getTimestamps;
+
+					return presenceData;
+				},
+				playback: () => !video.paused,
+				smallImageKey: () => (video.paused ? Assets.Pause : Assets.Play),
+				smallImageText: () => (video.paused ? "Paused" : "Playing"),
+
+				details: () => document.title,
+				state: () => {
+					return document.querySelector("h4.title.float-left").textContent;
+				},
+				buttons: () => [
+					{
+						label: "View Course",
+						url: (() => {
+							const 
+								href = document
 									.querySelectorAll(
 										".btn.btn-primary.ccn-btn-backtocourse.float-left"
 									)[0]
 									.getAttribute("href");
-								return href;
-							})(),
-						},
-					],
-				},
-
-				{
-					path: /^\/course\/(.*)/,
-
-					smallImageKey: () => Assets.VIEWING,
-					smallImageText: () => "Viewing",
-					details: () => "Viewing course",
-					state: () => {
-						return document.querySelector("h4.breadcrumb_title").textContent;
+							return href;
+						})(),
 					},
-					buttons: () => [
-						{
-							label: "View Course",
-							url: (() => {
-								return window.location.href;
-							})(),
-						},
-					],
-				},
-				{
-					path: /^\/mod\/*/,
-					details: () => "Browsing Courses",
-					state: () => "Choosing a Course",
-					smallImageKey: () => Assets.SEARCHING,
-					buttons: () => [
-						{
-							label: "Search with me?",
-							url: (() => {
-								return window.location.href;
-							})(),
-						},
-					],
-				},
-				{
-					path: /.*$/,
-					details: () => "On Homepage",
+				],
+			},
 
-					smallImageKey: () => Assets.SEARCHING,
-					smallImageText: () => "Browsing website",
-				},
-			],
-			matchedRoute = routes.find(route => route.path?.test(path));
-		if (matchedRoute) return matchedRoute;
+			{
+				path: /^\/course\/(.*)/,
 
-		return routes[routes.length - 1];
+				smallImageKey: () => Assets.Viewing,
+				smallImageText: () => "Viewing",
+				details: () => "Viewing course",
+				state: () => {
+					return document.querySelector("h4.breadcrumb_title").textContent;
+				},
+				buttons: () => [
+					{
+						label: "View Course",
+						url: (() => {
+							return window.location.href;
+						})(),
+					},
+				],
+			},
+			{
+				path: /^\/mod\/*/, 
+				details: () => "Browsing Courses",
+				state:()=>"Choosing a Course",
+				smallImageKey: () => Assets.Search,
+				buttons: () => [
+					{
+						label: "Search with me?",
+						url: (() => {
+							return window.location.href;
+						})(),
+					},
+				],
+			  },
+			{
+				path: /.*$/, 
+				details: () => "On Homepage",
+				
+				smallImageKey: () => Assets.Search,
+				smallImageText: () => "Browsing website",
+			  },
+			 
+			
+		];
+
+		const matchedRoute = routes.find(route => route.path?.test(path));
+  if (matchedRoute) {
+    return matchedRoute;
+  }
+
+  
+  const defaultRoute = routes[routes.length - 1];
+  return defaultRoute;
 	};
 
 presence.on(
@@ -160,7 +164,7 @@ presence.on("UpdateData", async () => {
 	]);
 
 	let presenceData: PresenceData = {
-		largeImageKey: Assets.Logo,
+		largeImageKey: LoAssets.Logo,
 	};
 
 	if (showTimestamp) presenceData.startTimestamp = startTimestamp;
