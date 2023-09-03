46be19ed334dc1ffa238b612a5d12dfa7767c784
Jakub Knot <68172465+jayjakee@users.noreply.github.com>
2023-09-03 12:44:41 +0200

    feat(Sosac): add presence (#7596)
    * Sosac
    
    Another movie & series platform
    
    Signed-off-by: Jakub Knot <68172465+jayjakee@users.noreply.github.com>
    
    * Update websites/S/Sosac/metadata.json
    
    1.7 to 1.9
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Jakub Knot <68172465+jayjakee@users.noreply.github.com>
    
    * feat(Sosac): fix CI issues
    
    * feat(Sosac): fix CI issues 2
    
    * feat(Sosac): fix CI issues 3
    
    * feat(Sosac): narrow down iFrameRegExp
    
    ---------
    
    Signed-off-by: Jakub Knot <68172465+jayjakee@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

---
+++ websites/S/Sosac/presence.ts
@@ -0,0 +1,72 @@
+const presence = new Presence({
+		clientId: "1079537235076071524",
+	}),
+	strings = presence.getStrings({
+		play: "general.playing",
+		pause: "general.paused",
+		browsing: "general.browsing",
+	});
+let video = {
+	duration: 0,
+	currentTime: 0,
+	paused: true,
+};
+
+const enum Assets {
+	Logo = "https://i.imgur.com/iOYRQjm.png",
+}
+
+presence.on(
+	"iFrameData",
+	(data: { duration: number; currentTime: number; paused: boolean }) => {
+		video = data;
+	}
+);
+
+presence.on("UpdateData", async () => {
+	const strs = await strings,
+		presenceData: PresenceData = {
+			largeImageKey: Assets.Logo,
+		};
+
+	if (
+		video &&
+		!isNaN(video.duration) &&
+		document.location.pathname.includes("/player")
+	) {
+		[presenceData.startTimestamp, presenceData.endTimestamp] =
+			presence.getTimestamps(
+				Math.floor(video.currentTime),
+				Math.floor(video.duration)
+			);
+
+		const firstH3Title = document.querySelector("h3");
+
+		if (firstH3Title) {
+			const originalText = firstH3Title.textContent,
+				// Movie/Series name
+
+				// Episode number
+				epimatch = originalText.match(/\d+x\d+/);
+
+			presenceData.details = `${originalText.match(/^([^\n]+)/)[1]} ${
+				epimatch ? epimatch[0] : " "
+			}`;
+		}
+
+		presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
+		presenceData.smallImageText = video.paused ? strs.pause : strs.play;
+
+		if (video.paused) {
+			delete presenceData.startTimestamp;
+			delete presenceData.endTimestamp;
+		}
+
+		presence.setActivity(presenceData, !video.paused);
+	} else {
+		presenceData.details = strs.browsing;
+		presenceData.smallImageKey = Assets.Search;
+		presenceData.smallImageText = strs.browsing;
+		presence.setActivity(presenceData);
+	}
+});
