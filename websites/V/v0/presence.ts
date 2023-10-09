22083139fa4cc940d041fcb9442ae28612d757bf
Gaëtan H <steellgold0@gmail.com>
2023-10-09 20:56:03 +0200

    fix(presence.ts): fix formatting and indentation in presence.ts file

--- websites/V/v0/presence.ts
+++ websites/V/v0/presence.ts
@@ -1,14 +1,18 @@
-const presence = new Presence({ clientId: "1160993221854380132" }), presenceData: PresenceData = {
-	largeImageKey: "https://i.imgur.com/UhCCRnQ.png",
-	startTimestamp: Math.floor(Date.now() / 1000),
-};
+const presence = new Presence({ clientId: "1160993221854380132" }),
+	presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/UhCCRnQ.png",
+		startTimestamp: Math.floor(Date.now() / 1000),
+	};
 
 presence.on("UpdateData", async () => {
 	switch (
 		document.location.pathname.endsWith("/") &&
-		document.location.pathname.length > 1 ?
-			document.location.pathname.slice(0, document.location.pathname.length - 1) :
-			document.location.pathname
+		document.location.pathname.length > 1
+			? document.location.pathname.slice(
+					0,
+					document.location.pathname.length - 1
+			  )
+			: document.location.pathname
 	) {
 		case "/":
 			presenceData.details = "Viewing homepage";
@@ -17,12 +21,22 @@ presence.on("UpdateData", async () => {
 			break;
 		case `/r/${document.location.pathname.split("/")[2]}`:
 			presenceData.details = "Prompting a template";
-			presenceData.state = `User prompt: "${document.querySelector("span.text-sm.line-clamp-1.text-ellipsis.text-left").textContent.slice(0, 50)}"`;
+			presenceData.state = `User prompt: "${document
+				.querySelector("span.text-sm.line-clamp-1.text-ellipsis.text-left")
+				.textContent.slice(0, 50)}"`;
 			presenceData.buttons = null;
 			break;
 		case `/t/${document.location.pathname.split("/")[2]}`:
-			presenceData.details = `Viewing a template by @${(document.querySelector("a.flex-none") as HTMLAnchorElement).href.split("/")[3]}`;
-			presenceData.state = `"${document.querySelector("h1.md\\:max-w-xs.lg\\:max-w-sm.text-sm.line-clamp-1.text-ellipsis.animate-in.fade-in-5.text-gray-1000").textContent.slice(0, 50)}"`;
+			presenceData.details = `Viewing a template by @${
+				(document.querySelector("a.flex-none") as HTMLAnchorElement).href.split(
+					"/"
+				)[3]
+			}`;
+			presenceData.state = `"${document
+				.querySelector(
+					"h1.md\\:max-w-xs.lg\\:max-w-sm.text-sm.line-clamp-1.text-ellipsis.animate-in.fade-in-5.text-gray-1000"
+				)
+				.textContent.slice(0, 50)}"`;
 			presenceData.buttons = [
 				{
 					label: "View template",
@@ -32,9 +46,12 @@ presence.on("UpdateData", async () => {
 			break;
 		// v0.dev/{username}
 		case `/${document.location.pathname.split("/")[1]}`:
-			presenceData.details = `Viewing @${document.location.pathname.split("/")[1]} profile`;
-			// get the img "rounded-full h-12 w-12"
-			presenceData.largeImageKey = (document.querySelector("img.rounded-full.h-12.w-12") as HTMLImageElement).src;
+			presenceData.details = `Viewing @${
+				document.location.pathname.split("/")[1]
+			} profile`;
+			presenceData.largeImageKey = (
+				document.querySelector("img.rounded-full.h-12.w-12") as HTMLImageElement
+			).src;
 			presenceData.smallImageKey = "https://i.imgur.com/UhCCRnQ.png";
 			presenceData.buttons = [
 				{
