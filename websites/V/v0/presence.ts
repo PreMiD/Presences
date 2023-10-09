d38daf11fc54d2728eec7d4eb48171f79b1853c7
Gaëtan H <steellgold0@gmail.com>
2023-10-09 20:42:52 +0200

    feat(V/v0): add metadata.json and presence.ts for v0 website
    Add metadata.json file for the v0 website, which includes information about the author, service, description, URL, version, logo, thumbnail, color, category, and tags.
    
    Add presence.ts file for the v0 website, which handles the presence data for different pages of the website. It sets the details, state, buttons, and images based on the current page being viewed.

---
+++ websites/V/v0/presence.ts
@@ -0,0 +1,48 @@
+const presence = new Presence({ clientId: "1160993221854380132" }), presenceData: PresenceData = {
+	largeImageKey: "https://i.imgur.com/UhCCRnQ.png",
+	startTimestamp: Math.floor(Date.now() / 1000),
+};
+
+presence.on("UpdateData", async () => {
+	switch (
+		document.location.pathname.endsWith("/") &&
+		document.location.pathname.length > 1 ?
+			document.location.pathname.slice(0, document.location.pathname.length - 1) :
+			document.location.pathname
+	) {
+		case "/":
+			presenceData.details = "Viewing homepage";
+			presenceData.largeImageKey = "https://i.imgur.com/UhCCRnQ.png";
+			presenceData.smallImageKey = "";
+			break;
+		case `/r/${document.location.pathname.split("/")[2]}`:
+			presenceData.details = "Prompting a template";
+			presenceData.state = `User prompt: "${document.querySelector("span.text-sm.line-clamp-1.text-ellipsis.text-left").textContent.slice(0, 50)}"`;
+			presenceData.buttons = null;
+			break;
+		case `/t/${document.location.pathname.split("/")[2]}`:
+			presenceData.details = `Viewing a template by @${(document.querySelector("a.flex-none") as HTMLAnchorElement).href.split("/")[3]}`;
+			presenceData.state = `"${document.querySelector("h1.md\\:max-w-xs.lg\\:max-w-sm.text-sm.line-clamp-1.text-ellipsis.animate-in.fade-in-5.text-gray-1000").textContent.slice(0, 50)}"`;
+			presenceData.buttons = [
+				{
+					label: "View template",
+					url: document.location.href,
+				},
+			];
+			break;
+		// v0.dev/{username}
+		case `/${document.location.pathname.split("/")[1]}`:
+			presenceData.details = `Viewing @${document.location.pathname.split("/")[1]} profile`;
+			// get the img "rounded-full h-12 w-12"
+			presenceData.largeImageKey = (document.querySelector("img.rounded-full.h-12.w-12") as HTMLImageElement).src;
+			presenceData.smallImageKey = "https://i.imgur.com/UhCCRnQ.png";
+			presenceData.buttons = [
+				{
+					label: "View profile",
+					url: document.location.href,
+				},
+			];
+			break;
+	}
+	presence.setActivity(presenceData);
+});
