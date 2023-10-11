d34e631d08c76be6115f12b6ce5265e8e8574d4b
Misakiix <73598057+Misakiix@users.noreply.github.com>
2023-10-11 10:37:29 -0300

    Update iframe.ts
    Signed-off-by: Misakiix <73598057+Misakiix@users.noreply.github.com>

--- websites/G/GoAnimes/iframe.ts
+++ websites/G/GoAnimes/iframe.ts
@@ -1,15 +1,15 @@
 const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
-		const video = document.querySelector<HTMLVideoElement>(
-			"video.jw-video.jw-reset"
-		);
-		if (video) {
-			iframe.send({
-				currentTime: video.currentTime,
-				duration: video.duration,
-				played: video.duration !== 0,
-				paused: video.paused,
-			});
-		}
+	const video = document.querySelector<HTMLVideoElement>(
+		"video.jw-video.jw-reset"
+	);
+	if (video) {
+		iframe.send({
+			currentTime: video.currentTime,
+			duration: video.duration,
+			played: video.duration !== 0,
+			paused: video.paused,
+		});
+	}
 });
