16031e6cec9b0f5ea5660764bc13e659fb46e222
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2023-10-14 13:14:52 +0200

    chore(OneTouchTV): change iframe & add regexp

--- websites/O/OneTouchTV/iframe.ts
+++ websites/O/OneTouchTV/iframe.ts
@@ -1,16 +1,13 @@
 const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
-	const video: HTMLVideoElement = document.querySelector("video");
+	const video = document.querySelector<HTMLVideoElement>("video");
 
-	switch (true) {
-		case !!video: {
-			iframe.send({
-				paused: video.paused,
-				duration: video.duration,
-				currentTime: video.currentTime,
-			});
-			break;
-		}
+	if (video) {
+		iframe.send({
+			paused: video.paused,
+			duration: video.duration,
+			currentTime: video.currentTime,
+		});
 	}
 });
