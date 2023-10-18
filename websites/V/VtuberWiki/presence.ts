17bf9b3536688a2426e041d0ecb15156c60c6e7a
ステラ <live.hylia@outlook.com>
2023-10-18 11:51:59 -0400

    fix(VtuberWiki): linted code

--- websites/V/VtuberWiki/presence.ts
+++ websites/V/VtuberWiki/presence.ts
@@ -1,18 +1,27 @@
 type StaticPagesType = Record<string, PresenceData>;
 
-let presence = new Presence({
-	clientId: "1164189020922843257",
-	appMode: true
-}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
-const staticPages: StaticPagesType = {
-	"/": { details: "Viewing the homepage", buttons: [{ label: "View Website", url: "https://wiki.hylia.dev" }] },
-	"/faq": { details: "Viewing the FAQ", buttons: [{ label: "View Website", url: "https://wiki.hylia.dev/faq" }] },
-	"/api": { details: "Viewing the API", buttons: [{ label: "View Website", url: "https://wiki.hylia.dev/api" }] },
-}; // Took this from websites/S/Shoob but I rewrote some of it. :heart:
+const presence = new Presence({
+		clientId: "1164189020922843257",
+		appMode: true,
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000),
+	staticPages: StaticPagesType = {
+		"/": {
+			details: "Viewing the homepage",
+			buttons: [{ label: "View Website", url: "https://wiki.hylia.dev" }],
+		},
+		"/faq": {
+			details: "Viewing the FAQ",
+			buttons: [{ label: "View Website", url: "https://wiki.hylia.dev/faq" }],
+		},
+		"/api": {
+			details: "Viewing the API",
+			buttons: [{ label: "View Website", url: "https://wiki.hylia.dev/api" }],
+		},
+	}; // Took this from websites/S/Shoob but I rewrote some of it. :heart:
 
 function fNumber(number: number): string {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 presence.on("UpdateData", async () => {
@@ -21,12 +30,13 @@ presence.on("UpdateData", async () => {
 		pageTitle = document.title.split(" | ")[0]?.trim();
 
 	let presenceData: PresenceData = {
-		largeImageKey: "https://pbs.twimg.com/profile_images/1713923311858593792/doH2HOXp_400x400.png",
+		largeImageKey:
+			"https://pbs.twimg.com/profile_images/1713923311858593792/doH2HOXp_400x400.png",
 		startTimestamp: browsingTimestamp,
 	};
 
-	for (const [path, data] of Object.entries(staticPages)) if (pathname.startsWith(path)) presenceData = { ...presenceData, ...data };
-
+	for (const [path, data] of Object.entries(staticPages))
+		if (pathname.startsWith(path)) presenceData = { ...presenceData, ...data };
 
 	switch (pathSplit[0]) {
 		case "wiki":
@@ -35,40 +45,68 @@ presence.on("UpdateData", async () => {
 					if (pathSplit[2]) {
 						presenceData.largeImageKey = `https://wiki.hylia.dev/vtubers/${pathSplit[2]}/photo.jpg`;
 						presenceData.details = "Viewing a Vuber";
-						presenceData.state = `${pageTitle} • ${document.getElementById("vtuber-desc")?.textContent}`;
-						presenceData.buttons = [{ label: `View ${pathSplit[2]}`, url: href }];
-						presenceData.smallImageKey = document.querySelector<HTMLImageElement>(".author-avatar")?.src;
-						presenceData.smallImageText = `Written by github.com/${document.querySelector<HTMLImageElement>(".author-avatar")?.alt}`;
-					} else presenceData.details = `Viewing ${fNumber(document.querySelector(".amoumt-count")?.textContent as unknown as number)} Vtubers`;
+						presenceData.state = `${pageTitle} • ${
+							document.querySelector("#vtuber-desc")?.textContent
+						}`;
+						presenceData.buttons = [
+							{ label: `View ${pathSplit[2]}`, url: href },
+						];
+						presenceData.smallImageKey =
+							document.querySelector<HTMLImageElement>(".author-avatar")?.src;
+						presenceData.smallImageText = `Written by github.com/${
+							document.querySelector<HTMLImageElement>(".author-avatar")?.alt
+						}`;
+					} else
+						presenceData.details = `Viewing ${fNumber(
+							document.querySelector(".amoumt-count")
+								?.textContent as unknown as number
+						)} Vtubers`;
 
 					break;
 				case "software":
 					if (pathSplit[2]) {
-						presenceData.largeImageKey = "https://wiki.hylia.dev/images/premid/9081.png";
+						presenceData.largeImageKey =
+							"https://wiki.hylia.dev/images/premid/9081.png";
 						presenceData.details = "Viewing Software";
 						presenceData.state = `${pageTitle}`;
-						presenceData.buttons = [{ label: `View ${pathSplit[2]}`, url: href }];
-						presenceData.smallImageKey = document.querySelector<HTMLImageElement>(".author-avatar")?.src;
-						presenceData.smallImageText = `Written by github.com/${document.querySelector<HTMLImageElement>(".author-avatar")?.alt}`;
-					} else presenceData.details = `Viewing ${fNumber(document.querySelector(".amoumt-count")?.textContent as unknown as number)} Software`;
+						presenceData.buttons = [
+							{ label: `View ${pathSplit[2]}`, url: href },
+						];
+						presenceData.smallImageKey =
+							document.querySelector<HTMLImageElement>(".author-avatar")?.src;
+						presenceData.smallImageText = `Written by github.com/${
+							document.querySelector<HTMLImageElement>(".author-avatar")?.alt
+						}`;
+					} else
+						presenceData.details = `Viewing ${fNumber(
+							document.querySelector(".amoumt-count")
+								?.textContent as unknown as number
+						)} Software`;
 					break;
 				case "guides":
 					if (pathSplit[2]) {
-						presenceData.largeImageKey = "https://wiki.hylia.dev/images/premid/8243.png";
+						presenceData.largeImageKey =
+							"https://wiki.hylia.dev/images/premid/8243.png";
 						presenceData.details = "Viewing Guides";
 						presenceData.state = `${pageTitle}`;
 						presenceData.buttons = [{ label: "View Guide", url: href }];
-						presenceData.smallImageKey = document.querySelector<HTMLImageElement>(".author-avatar")?.src;
-						presenceData.smallImageText = `Written by github.com/${document.querySelector<HTMLImageElement>(".author-avatar")?.alt}`;
-					} else presenceData.details = `Viewing ${fNumber(document.querySelector(".amoumt-count")?.textContent as unknown as number)} Guides`;
+						presenceData.smallImageKey =
+							document.querySelector<HTMLImageElement>(".author-avatar")?.src;
+						presenceData.smallImageText = `Written by github.com/${
+							document.querySelector<HTMLImageElement>(".author-avatar")?.alt
+						}`;
+					} else
+						presenceData.details = `Viewing ${fNumber(
+							document.querySelector(".amoumt-count")
+								?.textContent as unknown as number
+						)} Guides`;
 					break;
 				default:
 					presenceData.details = "Viewing The wiki";
 					presenceData.state = pageTitle;
 					presenceData.buttons = [{ label: "View Wiki", url: href }];
 			}
-		break;
-
+			break;
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
