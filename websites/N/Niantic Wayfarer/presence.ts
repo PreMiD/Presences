59fee532ed155372b78b9d171580a92624ed2039
theusaf <theusafyt@gmail.com>
2023-10-21 10:48:58 -0700

    fix: update help center selectors
    ... and add criteria

--- websites/N/Niantic Wayfarer/presence.ts
+++ websites/N/Niantic Wayfarer/presence.ts
@@ -5,15 +5,15 @@ const presence = new Presence({
 
 const enum Assets {
 	Logo = "https://cdn.rcd.gg/PreMiD/websites/N/Niantic%20Wayfarer/assets/logo.png",
-	Pin = "https://cdn.discordapp.com/app-assets/684174415415476240/684175146973790208.png?size=512"
+	Pin = "https://cdn.discordapp.com/app-assets/684174415415476240/684175146973790208.png?size=512",
 }
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: Assets.Logo,
-		startTimestamp: browsingTimestamp,
-	},
-	{ pathname } = document.location;
+			largeImageKey: Assets.Logo,
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname } = document.location;
 
 	if (pathname.includes("/review")) {
 		const title = document.querySelector(
@@ -22,7 +22,9 @@ presence.on("UpdateData", async () => {
 			description = document.querySelector(
 				"app-title-and-description .wf-review-card__body a+div"
 			),
-			location = document.querySelector("app-should-be-wayspot .wf-review-card__body > div > div:last-child");
+			location = document.querySelector(
+				"app-should-be-wayspot .wf-review-card__body > div > div:last-child"
+			);
 		if (title && description && location) {
 			presenceData.smallImageKey = Assets.Pin;
 			presenceData.details = `Reviewing: ${title.textContent.trim()}`;
@@ -37,16 +39,21 @@ presence.on("UpdateData", async () => {
 	} else if (pathname.includes("/settings"))
 		presenceData.details = "Managing settings";
 	else if (pathname.includes("/help")) {
-		const article = document.querySelector(
-			"#help-section-breadcrumbs > span.ng-binding"
-		).textContent;
+		const article = document
+			.querySelector("wf-page-header h2 a + span")
+			?.textContent.split(">")[1]
+			.trim();
 		presenceData.smallImageKey = Assets.Reading;
-		if (article !== "") {
+		if (article) {
 			presenceData.details = "Reading article:";
 			presenceData.state = article;
 		} else presenceData.details = "Browsing the Help Center";
-	} else if (pathname.includes("/login"))
-		presenceData.details = "Logging in";
+	} else if (pathname.includes("/criteria")) {
+		presenceData.details = "Reading criteria";
+		presenceData.state = document
+			.querySelector("wf-subnavigation li li[class*=selected]")
+			.textContent.trim();
+	} else if (pathname.includes("/login")) presenceData.details = "Logging in";
 	else if (pathname.includes("/profile"))
 		presenceData.details = "Viewing their profile";
 	else if (pathname.includes("/nominations"))
