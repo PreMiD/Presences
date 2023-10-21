e0767fc2a8948392eeba6bf6d9f06513a8e1e9c9
theusaf <theusafyt@gmail.com>
2023-10-21 10:40:46 -0700

    feat: add assets and update selectors

--- websites/N/Niantic Wayfarer/presence.ts
+++ websites/N/Niantic Wayfarer/presence.ts
@@ -3,36 +3,40 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
+const enum Assets {
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/N/Niantic%20Wayfarer/assets/logo.png",
+	Pin = "https://cdn.discordapp.com/app-assets/684174415415476240/684175146973790208.png?size=512"
+}
+
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey:
-			"https://cdn.rcd.gg/PreMiD/websites/N/Niantic%20Wayfarer/assets/logo.png",
+		largeImageKey: Assets.Logo,
 		startTimestamp: browsingTimestamp,
-	};
+	},
+	{ pathname } = document.location;
 
-	if (document.location.pathname.includes("/review")) {
+	if (pathname.includes("/review")) {
 		const title = document.querySelector(
-				"#descriptionDiv > div.card__body > div > a > h1"
+				"app-title-and-description .wf-review-card__body a"
 			),
 			description = document.querySelector(
-				"#descriptionDiv > div.card__body > div > h4"
+				"app-title-and-description .wf-review-card__body a+div"
 			),
-			location = document.querySelector(".flex-map-row > span:nth-child(2)");
+			location = document.querySelector("app-should-be-wayspot .wf-review-card__body > div > div:last-child");
 		if (title && description && location) {
-			presenceData.largeImageKey = "wayfarer";
-			presenceData.smallImageKey = "nw";
-			presenceData.details = `Reviewing: ${title.textContent}`;
-			presenceData.state = `Description: ${description.textContent}`;
+			presenceData.smallImageKey = Assets.Pin;
+			presenceData.details = `Reviewing: ${title.textContent.trim()}`;
+			presenceData.state = `Description: ${description.textContent.trim()}`;
 			presenceData.smallImageText = `Address: ${location.textContent
 				.split(":")[1]
 				.trim()}`;
 		} else {
 			presenceData.details = "Getting ready to";
-			presenceData.state = "review a location...";
+			presenceData.state = "review a location";
 		}
-	} else if (document.location.pathname.includes("/settings"))
-		presenceData.details = "Changing some settings...";
-	else if (document.location.pathname.includes("/help")) {
+	} else if (pathname.includes("/settings"))
+		presenceData.details = "Managing settings";
+	else if (pathname.includes("/help")) {
 		const article = document.querySelector(
 			"#help-section-breadcrumbs > span.ng-binding"
 		).textContent;
@@ -40,15 +44,15 @@ presence.on("UpdateData", async () => {
 		if (article !== "") {
 			presenceData.details = "Reading article:";
 			presenceData.state = article;
-		} else presenceData.details = "Browsing the Help Center...";
-	} else if (document.location.pathname.includes("/login"))
-		presenceData.details = "Logging in...";
-	else if (document.location.pathname.includes("/profile"))
-		presenceData.details = "Viewing their own profile...";
-	else if (document.location.pathname.includes("/nominations"))
-		presenceData.details = "Viewing their nominations...";
-	else if (document.location.pathname === "/")
-		presenceData.details = "Viewing the showcased wayspots...";
+		} else presenceData.details = "Browsing the Help Center";
+	} else if (pathname.includes("/login"))
+		presenceData.details = "Logging in";
+	else if (pathname.includes("/profile"))
+		presenceData.details = "Viewing their profile";
+	else if (pathname.includes("/nominations"))
+		presenceData.details = "Viewing their nominations";
+	else if (pathname.includes("/showcase"))
+		presenceData.details = "Viewing the showcased wayspots";
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
