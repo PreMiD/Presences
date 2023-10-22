8db694aa957d25075d9f4732e1c48d54b2041414
DarkVillager <darkville13@gmail.com>
2023-10-22 14:05:47 +0200

    feat(KimCartoon): rewrite presence with multiple domain support
    - Add Domain regex/n- Add buttons/n- Add cover support w/ setting

--- websites/K/KimCartoon/presence.ts
+++ websites/K/KimCartoon/presence.ts
@@ -1,21 +1,17 @@
 const presence = new Presence({
-		clientId: "640253556078673951",
-	}),
-	strings = presence.getStrings({
-		play: "general.playing",
-		pause: "general.paused",
-	});
+	clientId: "640253556078673951",
+});
 
 let browsingTimestamp = Math.floor(Date.now() / 1000),
-	title: HTMLElement,
-	air: HTMLInputElement,
-	iFrameVideo: boolean,
-	currentTime: number,
-	duration: number,
-	paused: boolean,
-	lastPlaybackState: boolean,
-	playback: boolean;
+	title: string,
+	iFrameVideo = false,
+	currentTime = 0,
+	duration = 0,
+	paused = true;
 
+const enum Assets {
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/K/KimCartoon/assets/logo.png",
+}
 interface IFrameData {
 	iframeVideo: {
 		dur: number;
@@ -24,77 +20,127 @@ interface IFrameData {
 		currTime: number;
 	};
 }
+function capitalizeFirstLetter(string: string) {
+	if (!string) return "Undefined";
+	return (
+		string.trim().charAt(0).toUpperCase() + string.trim().slice(1).toLowerCase()
+	);
+}
+
+function fullURL(covers: string, covers2: string, hostname: string) {
+	if (covers?.includes("https")) return covers;
+	else if (covers2?.includes("https")) return covers2;
+	else if (covers?.includes("Uploads") || covers2?.includes("Uploads"))
+		return `https://${hostname}${covers ?? covers2}`;
+	else return Assets.Logo;
+}
 
 presence.on("iFrameData", (data: IFrameData) => {
-	if (lastPlaybackState !== playback) {
-		lastPlaybackState = playback;
-		browsingTimestamp = Math.floor(Date.now() / 1000);
-	}
-	playback = data.iframeVideo.dur !== null ? true : false;
-
-	if (playback) {
-		({ iFrameVideo, paused } = data.iframeVideo);
-		currentTime = data.iframeVideo.currTime;
-		duration = data.iframeVideo.dur;
-	}
+	({ iFrameVideo, paused } = data.iframeVideo);
+	currentTime = data.iframeVideo.currTime;
+	duration = data.iframeVideo.dur;
 });
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey:
-			"https://cdn.rcd.gg/PreMiD/websites/K/KimCartoon/assets/logo.png",
-	};
+			largeImageKey: Assets.Logo,
+			startTimestamp: browsingTimestamp,
+		},
+		cover = await presence.getSetting<boolean>("cover"),
+		{ hostname, pathname, href } = document.location,
+		backupImg = document
+			.querySelector('[class="barContent"] > div > img')
+			?.getAttribute("src");
 
-	if (
-		document.querySelector("#adsIfrme > div > div > div > h1 > strong") !== null
-	) {
-		if (iFrameVideo === true && !isNaN(duration)) {
-			presenceData.smallImageKey = paused ? Assets.Pause : Assets.Play;
-			presenceData.smallImageText = paused
-				? (await strings).pause
-				: (await strings).play;
-			[presenceData.startTimestamp, presenceData.endTimestamp] =
-				presence.getTimestamps(Math.floor(currentTime), Math.floor(duration));
+	switch (true) {
+		case document.querySelector('[id="player_container"]') ??
+			document.querySelector('[id="myContainer"]') !== null: {
+			const epsOrMovie =
+					document.querySelector('option[selected="selected"]')?.textContent ??
+					document.querySelector('[id="selectEpisode"] > [selected]')
+						?.textContent,
+				titleSplit =
+					document
+						.querySelector('[class="watch_title a_center"]')
+						?.textContent.toLowerCase()
+						?.replace("watch", "")
+						?.replace(" online free", "")
+						?.replace(" information", "")
+						?.split(" season") ??
+					document
+						.querySelector('[id="navsubbar"]')
+						?.textContent.toLowerCase()
+						?.replace("watch", "")
+						?.replace(" online free", "")
+						?.replace(" information", "")
+						?.split(" season");
 
-			title = document.querySelector(
-				"#adsIfrme > div > div > div > h1 > strong"
+			title = capitalizeFirstLetter(titleSplit?.[0]);
+			presenceData.details = title;
+			presenceData.state =
+				epsOrMovie?.includes("Episode") && titleSplit?.length > 1
+					? `Season ${titleSplit[1]} ${epsOrMovie}`
+					: epsOrMovie?.includes("Episode") && titleSplit?.length === 1
+					? epsOrMovie
+					: epsOrMovie === "Movie"
+					? "Movie"
+					: `Episode ${epsOrMovie}`;
+			presenceData.largeImageKey = fullURL(
+				document
+					.querySelector('[property="og:image"]')
+					?.getAttribute("content"),
+				"",
+				hostname
 			);
-			presenceData.details = title.textContent;
-
-			air = document.querySelector("#selectServer");
-
-			presenceData.state = `Server: ${air.value}`;
-
-			if (paused) {
+			if (iFrameVideo === true && !isNaN(duration)) {
 				delete presenceData.startTimestamp;
-				delete presenceData.endTimestamp;
-			}
-		} else if (iFrameVideo === null && isNaN(duration)) {
-			presenceData.startTimestamp = browsingTimestamp;
-			presenceData.details = "Looking at: ";
-			title = document.querySelector(
-				"#adsIfrme > div > div > div > h1 > strong"
-			);
+				presenceData.smallImageKey = paused ? Assets.Pause : Assets.Play;
+				presenceData.smallImageText = paused ? "Paused" : "Playing";
+				if (!paused) {
+					[, presenceData.endTimestamp] = presence.getTimestamps(
+						currentTime,
+						duration
+					);
+				}
 
-			presenceData.state = title.textContent;
-			presenceData.smallImageKey = Assets.Reading;
+				presenceData.buttons = [{ label: "Watch Cartoon", url: href }];
+			} else presenceData.buttons = [{ label: "View Cartoon", url: href }];
+			break;
 		}
-	} else if (document.location.pathname === "/") {
-		presenceData.details = "Viewing main page";
-		presenceData.startTimestamp = browsingTimestamp;
-	} else if (document.location.pathname.includes("/CartoonList")) {
-		presenceData.details = "Viewing cartoonlist";
-		presenceData.startTimestamp = browsingTimestamp;
-	} else if (document.location.pathname.includes("/Genre")) {
-		presenceData.details = "Viewing genres";
-		presenceData.startTimestamp = browsingTimestamp;
-	} else if (document.location.pathname.includes("/Cartoon")) {
-		presenceData.details = "Viewing cartoon:";
-		presenceData.state = document.querySelector(
-			"#leftside > div:nth-child(1) > div.barContent.full > div > div.right_movie > h1 > a"
-		).textContent;
-		presenceData.startTimestamp = browsingTimestamp;
+		case pathname === "/": {
+			presenceData.details = "Viewing the homepage";
+			break;
+		}
+		case pathname.includes("/Genre"): {
+			presenceData.details = "Viewing all genres";
+			break;
+		}
+		case pathname.includes("/CartoonList"): {
+			presenceData.details = "Viewing the cartoonlist";
+			presenceData.buttons = [{ label: "View The Cartoonlist", url: href }];
+			break;
+		}
+		case pathname.includes("/Cartoon/"): {
+			presenceData.details = "Viewing cartoon:";
+			presenceData.state =
+				document.querySelector('[class="bigChar"]')?.textContent;
+			presenceData.largeImageKey =
+				document
+					.querySelector('[property="og:image"]')
+					?.getAttribute("content") ?? backupImg
+					? `https://${hostname}${backupImg}`
+					: Assets.Logo;
+			presenceData.buttons = [{ label: "View Cartoon", url: href }];
+			break;
+		}
+		case pathname.includes("/kimcartoon.me"): {
+			presenceData.details = "Viewing the old homepage";
+			break;
+		}
 	}
+
+	if (!cover && presenceData.largeImageKey !== Assets.Logo)
+		presenceData.largeImageKey = Assets.Logo;
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
