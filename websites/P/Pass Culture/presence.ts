4efe2c3515f7160fa631736094d434559af18350
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-10-23 11:50:10 +0200

    feat(pass culture): add "pass culture" presence

---
+++ websites/P/Pass Culture/presence.ts
@@ -0,0 +1,140 @@
+const presence = new Presence({
+	clientId: "1165384499987742800",
+});
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+			largeImageKey: "https://i.imgur.com/ES9Hy84.png",
+		},
+		path = document.location.pathname.split("/"),
+		showCounts = await presence.getSetting<boolean>("show_counts");
+
+	switch (path[1]) {
+		case "questionnaire-pratiques-initiales":
+			presenceData.details = "En train de faire le questionnaire";
+			break;
+		case "accueil":
+			presenceData.details = "Regarde la page d'accueil";
+			break;
+		case "accueil-thematique": {
+			presenceData.details = "Regarde la page d'accueil";
+
+			const offersType = (<HTMLHeadingElement>(
+				document.querySelector(".css-175oi2r.r-43z982.r-1udh08x")
+			))?.textContent;
+			if (offersType) presenceData.state = offersType;
+
+			break;
+		}
+		case "profil":
+			presenceData.details = "Regarde son profil";
+
+			break;
+		case "recherche": {
+			presenceData.details = "Recherche :";
+
+			const query = (<HTMLInputElement>(
+					document.querySelector("input[type=search]")
+				))?.value,
+				results = parseInt(document.title);
+			if (query && !isNaN(results))
+				presenceData.state = `${query} (${results} résultats)`;
+
+			break;
+		}
+		case "reservations": {
+			const reservationsCount = (<HTMLHeadingElement>(
+				document.querySelector(
+					'[data-testid="OnGoingBookingsList"] h2.css-1rynq56'
+				)
+			))?.textContent;
+
+			presenceData.details = "Regarde ses réservations";
+			if (reservationsCount && showCounts)
+				presenceData.state = reservationsCount;
+
+			break;
+		}
+		case "reservations-terminees": {
+			const doneReservationsCount = (<HTMLHeadingElement>(
+				document.querySelector('[data-testid="EndedBookings"] h2.css-1rynq56')
+			))?.textContent;
+
+			presenceData.details = "Regarde ses réservations terminées";
+			if (doneReservationsCount && showCounts)
+				presenceData.state = doneReservationsCount;
+
+			break;
+		}
+		case "reservation": {
+			presenceData.details = "Regarde une réservation";
+
+			const reservation = (<HTMLHeadingElement>(
+					document.querySelector(
+						".css-175oi2r.r-1777fci.r-cn4s0y h1.css-1rynq56.r-1rmcaf9.r-1gknse6"
+					)
+				))?.textContent,
+				reservationPrice = (<HTMLSpanElement>(
+					document.querySelector("span.css-1qaijid.r-1rmcaf9.r-yv33h5")
+				))?.textContent.trim(),
+				offerLink = (<HTMLAnchorElement>(
+					document.querySelector(
+						"a.AppButtonweb__Link-sc-1veyq7-1.hTlNnp.sc-iGgWBj.kxRSZa"
+					)
+				))?.getAttribute("href");
+			if (reservation && reservationPrice) {
+				presenceData.state = `${reservation} (${reservationPrice})`;
+				presenceData.buttons = [
+					{
+						label: "Voir l'offre",
+						url: `${location.protocol}//${location.hostname}${
+							offerLink.split("?")[0]
+						}`,
+					},
+				];
+			}
+			break;
+		}
+		case "favoris": {
+			presenceData.details = "Regarde ses favoris";
+
+			const favoritesCount = document.querySelector(
+				'[data-testid="favoritesResultsFlatlist"] h2.css-1rynq56'
+			)?.textContent;
+			if (favoritesCount && showCounts) presenceData.state = favoritesCount;
+
+			break;
+		}
+		case "offre": {
+			if (path[3] && path[3] === "description")
+				presenceData.details = "Regarde les détails d'une offre";
+			else {
+				presenceData.details = "Regarde une offre";
+
+				const offer = (<HTMLHeadingElement>(
+						document.querySelector(
+							'.css-175oi2r.r-1hy1u7s h1[data-testid^="Nom de l’offre"]'
+						)
+					))?.textContent,
+					price = (<HTMLDivElement>(
+						document.querySelector("div[data-testid=caption-iconPrice]")
+					))?.textContent;
+				if (offer && price) {
+					presenceData.state = `${offer} (${price})`;
+					presenceData.buttons = [
+						{
+							label: "Voir l'offre",
+							url: window.location.href.split("?")[0],
+						},
+					];
+				}
+			}
+			break;
+		}
+		default:
+			presence.clearActivity();
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
