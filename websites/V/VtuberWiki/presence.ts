91daeeb89b3c4901445fc554f8b742e01676d355
ステラ <withervt@outlook.com>
2023-10-23 02:26:53 -0400

    feat(VtuberWiki): add presence (#7673)
    * feat(VtuberWiki): add presence
    
    * fix(VtuberWiki): linted code
    
    * fix(VtuberWiki): added `{...}` to else
    
    * fix(VtuberWiki): ran lint, due to pc crashing
    
    * feat(VtuberWiki): fixed reviewer's items
    
    * feat(VtuberWiki): make requested changes
    
    * feat(VtuberWiki): this should pass checks
    
    * fix(Sosac): fixed broken metadata
    
    * feat(VtuberWiki): remove dup items
    
    * feat(VtuberWiki): remove author due to a bug

---
+++ websites/V/VtuberWiki/presence.ts
@@ -0,0 +1,85 @@
+type StaticPagesType = Record<string, PresenceData>;
+
+const presence = new Presence({
+		clientId: "1164189020922843257",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000),
+	staticPages: StaticPagesType = {
+		"/": {
+			details: "Viewing the homepage",
+		},
+		"/faq": {
+			details: "Viewing the FAQ",
+			buttons: [{ label: "View The FAQ", url: "https://wiki.hylia.dev/faq" }],
+		},
+		"/api": {
+			details: "Viewing the API",
+			buttons: [{ label: "Browse the API", url: "https://wiki.hylia.dev/api" }],
+		},
+	};
+
+const enum Assets {
+	Cog = "https://wiki.hylia.dev/images/premid/9081.png",
+	Book = "https://wiki.hylia.dev/images/premid/8243.png",
+	Logo = "https://i.imgur.com/svbsNmR.png",
+}
+
+presence.on("UpdateData", async () => {
+	const { pathname, href } = document.location,
+		pathSplit = pathname.split("/").slice(1),
+		pageTitle = document.title.split(" | ")[0]?.trim();
+
+	let presenceData: PresenceData = {
+		largeImageKey: Assets.Logo,
+		startTimestamp: browsingTimestamp,
+	};
+
+	for (const [path, data] of Object.entries(staticPages))
+		if (pathname.startsWith(path)) presenceData = { ...presenceData, ...data };
+
+	switch (pathSplit[0]) {
+		case "wiki":
+			switch (pathSplit[1]) {
+				case "vtubers":
+					if (pathSplit[2]) {
+						presenceData.largeImageKey = `https://wiki.hylia.dev/vtubers/${pathSplit[2]}/photo.jpg`;
+						presenceData.details = "Viewing a Vuber";
+						presenceData.state = `${pageTitle} • ${
+							document.querySelector("#vtuber-desc")?.textContent
+						}`;
+						presenceData.buttons = [
+							{ label: `View ${pathSplit[2]}`, url: href },
+						];
+					} else presenceData.details = "Viewing All The Vtubers";
+
+					break;
+				case "software":
+					if (pathSplit[2]) {
+						presenceData.largeImageKey = Assets.Cog;
+						presenceData.details = "Viewing Software";
+						presenceData.state = `${pageTitle}`;
+						presenceData.buttons = [
+							{ label: `View ${pathSplit[2]}`, url: href },
+						];
+					} else presenceData.details = "Viewing All The Software";
+
+					break;
+				case "guides":
+					if (pathSplit[2]) {
+						presenceData.largeImageKey = Assets.Book;
+						presenceData.details = "Viewing Guides";
+						presenceData.state = `${pageTitle}`;
+						presenceData.buttons = [{ label: "View Guide", url: href }];
+					} else presenceData.details = "Viewing All The Guides";
+					break;
+				default:
+					presenceData.details = "Viewing The wiki";
+					presenceData.state = pageTitle;
+					presenceData.buttons = [{ label: "View Wiki", url: href }];
+			}
+			break;
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
