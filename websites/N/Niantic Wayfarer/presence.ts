1855b442b5496eb522ba40ec7c62395151cf8c90
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2023-10-22 23:31:19 -0700

    feat(Niantic Wayfarer): update selectors and add details (#7686)
    * feat: add assets and update selectors
    
    * fix: update help center selectors
    
    ... and add criteria
    
    * feat: add details to profile page
    
    * feat: display waypoint image when reviewing
    
    * feat: add other review types

--- websites/N/Niantic Wayfarer/presence.ts
+++ websites/N/Niantic Wayfarer/presence.ts
@@ -3,52 +3,122 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
+const enum Assets {
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/N/Niantic%20Wayfarer/assets/logo.png",
+	Pin = "https://cdn.discordapp.com/app-assets/684174415415476240/684175146973790208.png?size=512",
+}
+
+const shortenedURLs: Record<string, string> = {};
+async function getShortURL(url: string) {
+	if (url.length < 256) return url;
+	if (shortenedURLs[url]) return shortenedURLs[url];
+	try {
+		shortenedURLs[url] = Assets.Logo;
+		const pdURL = await (
+			await fetch(`https://pd.premid.app/create/${url}`)
+		).text();
+		shortenedURLs[url] = pdURL;
+		return pdURL;
+	} catch (err) {
+		presence.error(err);
+		return url;
+	}
+}
+
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey:
-			"https://cdn.rcd.gg/PreMiD/websites/N/Niantic%20Wayfarer/assets/logo.png",
-		startTimestamp: browsingTimestamp,
-	};
+			largeImageKey: Assets.Logo,
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname } = document.location;
 
-	if (document.location.pathname.includes("/review")) {
+	if (pathname.includes("/review")) {
 		const title = document.querySelector(
-				"#descriptionDiv > div.card__body > div > a > h1"
+				"app-title-and-description .wf-review-card__body a"
 			),
 			description = document.querySelector(
-				"#descriptionDiv > div.card__body > div > h4"
+				"app-title-and-description .wf-review-card__body a+div"
 			),
-			location = document.querySelector(".flex-map-row > span:nth-child(2)");
+			location = document.querySelector(
+				"app-should-be-wayspot .wf-review-card__body > div > div:last-child"
+			);
 		if (title && description && location) {
-			presenceData.largeImageKey = "wayfarer";
-			presenceData.smallImageKey = "nw";
-			presenceData.details = `Reviewing: ${title.textContent}`;
-			presenceData.state = `Description: ${description.textContent}`;
+			presenceData.largeImageKey = await getShortURL(
+				document
+					.querySelector<HTMLDivElement>(
+						"app-should-be-wayspot .wf-image-modal"
+					)
+					.style.backgroundImage.match(/url\("(.+)"\)/)[1]
+			);
+			presenceData.smallImageKey = Assets.Pin;
+			presenceData.details = `Reviewing: ${title.textContent.trim()}`;
+			presenceData.state = `Description: ${description.textContent.trim()}`;
 			presenceData.smallImageText = `Address: ${location.textContent
 				.split(":")[1]
 				.trim()}`;
+		} else if (document.querySelector("app-review-photo")) {
+			presenceData.details = "Reviewing photos";
+			presenceData.state = `for ${
+				document.querySelector(
+					"app-review-photo .review-photo__info > div > div:first-child"
+				).textContent
+			}`;
+		} else if (document.querySelector("app-review-edit")) {
+			presenceData.details = "Reviewing a Wayspot edit";
+			presenceData.state = `for ${document
+				.querySelector("app-review-edit-info .review-edit-info__info")
+				.textContent.trim()}`;
 		} else {
 			presenceData.details = "Getting ready to";
-			presenceData.state = "review a location...";
+			presenceData.state = "review a location";
 		}
-	} else if (document.location.pathname.includes("/settings"))
-		presenceData.details = "Changing some settings...";
-	else if (document.location.pathname.includes("/help")) {
-		const article = document.querySelector(
-			"#help-section-breadcrumbs > span.ng-binding"
-		).textContent;
+	} else if (pathname.includes("/settings"))
+		presenceData.details = "Managing settings";
+	else if (pathname.includes("/help")) {
+		const article = document
+			.querySelector("wf-page-header h2 a + span")
+			?.textContent.split(">")[1]
+			.trim();
 		presenceData.smallImageKey = Assets.Reading;
-		if (article !== "") {
+		if (article) {
 			presenceData.details = "Reading article:";
 			presenceData.state = article;
-		} else presenceData.details = "Browsing the Help Center...";
-	} else if (document.location.pathname.includes("/login"))
-		presenceData.details = "Logging in...";
-	else if (document.location.pathname.includes("/profile"))
-		presenceData.details = "Viewing their own profile...";
-	else if (document.location.pathname.includes("/nominations"))
-		presenceData.details = "Viewing their nominations...";
-	else if (document.location.pathname === "/")
-		presenceData.details = "Viewing the showcased wayspots...";
+		} else presenceData.details = "Browsing the Help Center";
+	} else if (pathname.includes("/criteria")) {
+		presenceData.details = "Reading criteria";
+		presenceData.state = document
+			.querySelector("wf-subnavigation li li[class*=selected]")
+			.textContent.trim();
+	} else if (pathname.includes("/login")) presenceData.details = "Logging in";
+	else if (pathname.includes("/profile")) {
+		presenceData.details = "Viewing their profile";
+		presenceData.state = `Rating: ${document
+			.querySelector("wf-rating-bar section[class*=active]")
+			.textContent.trim()}`;
+		presenceData.smallImageKey = await getShortURL(
+			document.querySelector<SVGImageElement>("wf-upgrade-visualization image")
+				.href.baseVal
+		);
+		const agreements = [
+				...document.querySelectorAll(
+					"wf-profile-stats > div > div:not([class]):not(:last-child) .wf-profile-stats__stat"
+				),
+			].reduce((value, element) => {
+				return value + +element.children[1].textContent.trim();
+			}, 0),
+			total = +document
+				.querySelector(
+					"wf-profile-stats > div > .wf-profile-stats__stat > div:last-child"
+				)
+				.textContent.trim();
+		presenceData.smallImageText = `Total: ${total}, Agreements: ${agreements} (${(
+			(agreements / total) *
+			100
+		).toFixed(1)}%)`;
+	} else if (pathname.includes("/nominations"))
+		presenceData.details = "Viewing their nominations";
+	else if (pathname.includes("/showcase"))
+		presenceData.details = "Viewing the showcased wayspots";
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
