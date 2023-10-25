eaa55978924829b6b4961b3374c9b290ad9b01d6
Kacper Mura <20924469+VocalZero@users.noreply.github.com>
2023-10-25 19:58:43 +0200

    feat(atomicradio): add atomicradio presence (#7689)
    * feat(atomicradio): add atomicradio presence
    
    * fix(atomicradio): linted code
    
    * fix(atomicradio): replaced discord asset names
    
    * fix(atomicradio): resize logo
    
    * fix(atomicradio): shortened query selectors
    
    * fix(atomicradio): shortened location pathname

---
+++ websites/A/atomicradio/presence.ts
@@ -0,0 +1,58 @@
+const presence = new Presence({
+		clientId: "904084297831571518",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", () => {
+	const presenceData: PresenceData = {
+			largeImageKey: "https://i.imgur.com/vPI68Ny.png",
+			smallImageKey: Assets.Search,
+			startTimestamp: browsingTimestamp,
+		},
+		player = document.querySelector<HTMLDivElement>("div.player"),
+		{ pathname } = document.location;
+
+	if (player) {
+		const spaceId = player.querySelector("#spaceId").textContent;
+		presenceData.largeImageKey = player.querySelector("#artwork").textContent;
+		presenceData.smallImageKey =
+			player.querySelector<HTMLButtonElement>('[id*="-button"]').id ===
+			"play-button"
+				? Assets.Pause
+				: Assets.Play;
+		presenceData.details =
+			player.querySelector<HTMLAnchorElement>("a.track-title").textContent;
+		presenceData.state =
+			player.querySelector<HTMLDivElement>("div.track-artist").textContent;
+		presenceData.endTimestamp = new Date(
+			player.querySelector("#endingAt").textContent
+		).getTime();
+		presenceData.smallImageText = spaceId.toUpperCase();
+		presenceData.buttons = [
+			{ label: "Listen", url: `https://atomic.radio/${spaceId}` },
+		];
+	} else {
+		presenceData.details = "Browsing...";
+		if (pathname.includes("/statistics"))
+			presenceData.details = "Viewing statistics";
+		else if (pathname.includes("/streams"))
+			presenceData.details = "Viewing stream urls";
+		else if (pathname.includes("/about"))
+			presenceData.details = "Viewing about us";
+		else if (pathname.includes("/apply"))
+			presenceData.details = "Viewing apply";
+		else if (pathname.includes("/contributors"))
+			presenceData.details = "Viewing contributors";
+		else if (pathname.includes("/partners"))
+			presenceData.details = "Viewing partners";
+		else if (pathname.includes("/legal/imprint"))
+			presenceData.details = "Viewing imprint";
+		else if (pathname.includes("/legal/privacy"))
+			presenceData.details = "Viewing privacy";
+		else if (pathname.includes("/account"))
+			presenceData.details = "Viewing account";
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
