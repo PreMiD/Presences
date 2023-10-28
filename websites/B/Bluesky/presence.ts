b290f1e5b693f4ef7561ec3dacdc83dfcb70fc01
oriionn <xemilien67@gmail.com>
2023-10-28 23:52:48 +0200

    feat(Bluesky): add Bluesky presence

---
+++ websites/B/Bluesky/presence.ts
@@ -0,0 +1,116 @@
+const presence = new Presence({
+	clientId: "1167920926076190861",
+});
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+			largeImageKey: "https://i.imgur.com/uBFoZMP.jpg",
+		},
+		path = document.location.pathname.split("/"),
+		showUsername = await presence.getSetting<boolean>("show_profile"),
+		showPost = await presence.getSetting<boolean>("show_post"),
+		showFeed = await presence.getSetting<boolean>("show_feed");
+
+	switch (path[1]) {
+		case "profile":
+			switch (path[3]) {
+				case "feed":
+					presenceData.details = "Viewing a feed...";
+					if (showFeed) {
+						presenceData.state = document.querySelector(
+							"a.css-1jxf684.r-1loqt21:not(.r-13awgt0)"
+						)?.textContent;
+						presenceData.buttons = [
+							{
+								label: "View Feed",
+								url: document.location.href,
+							},
+						];
+					}
+					break;
+				case "post":
+					presenceData.details = "Viewing a post...";
+					if (showUsername)
+						presenceData.state = `Posted by ${
+							document.querySelector(
+								".css-175oi2r.r-1loqt21.r-1otgn73.r-11isodc.r-19gegkz"
+							)?.textContent
+						}`;
+
+					if (showPost) {
+						presenceData.buttons = [
+							{
+								label: "View Post",
+								url: document.location.href,
+							},
+						];
+					}
+					break;
+				default:
+					presenceData.details = "Viewing a profile...";
+					if (showUsername) {
+						presenceData.state = document.querySelector(
+							'div[data-testid="profileHeaderDisplayName"]'
+						)?.textContent;
+						presenceData.buttons = [
+							{
+								label: "View Profile",
+								url: document.location.href,
+							},
+						];
+					}
+			}
+			break;
+		case "feeds":
+			presenceData.details = "Browsing...";
+			presenceData.state = "Feeds";
+			break;
+		case "search":
+			presenceData.details = "Searching...";
+			break;
+		case "notifications":
+			presenceData.details = "Browsing...";
+			presenceData.state = "Notifications";
+			break;
+		case "moderation":
+			presenceData.details = "Managing moderation settings...";
+			switch (path[2]) {
+				case "mute-lists":
+					presenceData.state = "Mute Lists";
+					break;
+				case "muted-accounts":
+					presenceData.state = "Muted Accounts";
+					break;
+				case "blocked-accounts":
+					presenceData.state = "Blocked Accounts";
+					break;
+			}
+			break;
+		case "settings":
+			presenceData.details = "Managing settings...";
+			switch (path[2]) {
+				case "home-feed":
+					presenceData.state = "Home Feed Preferences";
+					break;
+				case "threads":
+					presenceData.state = "Threads Preferences";
+					break;
+				case "saved-feeds":
+					presenceData.state = "Feeds";
+					break;
+				case "language":
+					presenceData.state = "Language Settings";
+					break;
+				case "app-passwords":
+					presenceData.state = "Applications";
+					break;
+			}
+			break;
+		default:
+			presenceData.details = "Browsing...";
+			presenceData.state = "Home";
+			break;
+	}
+
+	presence.setActivity(presenceData);
+});
