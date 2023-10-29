4ade65544a60caff1d7a7acbe764bdeb500f510b
iamhv6 <100041033+iamhv6@users.noreply.github.com>
2023-10-29 17:43:31 +0530

    document location, homepage button
    Signed-off-by: iamhv6 <100041033+iamhv6@users.noreply.github.com>

--- websites/S/Spyke/presence.ts
+++ websites/S/Spyke/presence.ts
@@ -29,7 +29,7 @@ async function postGQLAPI(
 let url = "https://spyke.social/";
 
 presence.on("UpdateData", () => {
-	const nurl = document.URL;
+	const nurl = document.location.href;
 	if (nurl !== url) {
 		const presenceData: PresenceData = {
 			largeImageKey: "https://i.imgur.com/v10pkZA.png",
@@ -38,57 +38,54 @@ presence.on("UpdateData", () => {
 			state: "On Homepage",
 			startTimestamp: browsingTimestamp,
 		};
-		presenceData.buttons = [
-			{ label: "View Spyke", url: "https://spyke.social/" },
-		];
 
-		if (document.URL.includes("/p/")) {
+		if (document.location.href.includes("/p/")) {
 			postGQLAPI(
 				"presencepst",
 				"query presencepst($id: ID!) { comments(ids: [$id]) { ... on Post { title content { id data __typename } communities { name dp } } } }",
-				{ id: document.URL.split("/p/")[1] }
+				{ id: document.location.href.split("/p/")[1] }
 			).then(res => {
 				if (res.data.comments[0].content[0].__typename === "Image") {
 					presenceData.largeImageKey = res.data.comments[0].content[0].data;
 					presenceData.smallImageKey = res.data.comments[0].communities[0].dp;
 					presenceData.details = `Reading post in ${res.data.comments[0].communities[0].name}`;
 					presenceData.state = res.data.comments[0].title;
-					presenceData.buttons = [{ url: document.URL, label: "View Post" }];
+					presenceData.buttons = [{ url: document.location.href, label: "View Post" }];
 					presence.setActivity(presenceData);
 				} else {
 					presenceData.largeImageKey = res.data.comments[0].communities[0].dp;
 					presenceData.smallImageKey = "https://i.imgur.com/v10pkZA.png";
 					presenceData.details = `Reading post in ${res.data.comments[0].communities[0].name}`;
 					presenceData.state = res.data.comments[0].title;
-					presenceData.buttons = [{ url: document.URL, label: "View Post" }];
+					presenceData.buttons = [{ url: document.location.href, label: "View Post" }];
 					presence.setActivity(presenceData);
 				}
 			});
-		} else if (document.URL.includes("/g/")) {
+		} else if (document.location.href.includes("/g/")) {
 			postGQLAPI(
 				"presencegrp",
 				"query presencegrp($name:String!){ communityByName(name:$name) { name dp cover}}",
-				{ name: document.URL.split("/g/")[1] }
+				{ name: document.location.href.split("/g/")[1] }
 			).then(res => {
 				presenceData.largeImageKey = res.data.communityByName.cover;
 				presenceData.smallImageKey = res.data.communityByName.dp;
 				presenceData.state = `${res.data.communityByName.name}`;
 				presenceData.details = "Viewing a Clan";
-				presenceData.buttons = [{ url: document.URL, label: "View Clan" }];
+				presenceData.buttons = [{ url: document.location.href, label: "View Clan" }];
 				presence.setActivity(presenceData);
 			});
-		} else if (document.URL.includes("/upload")) {
+		} else if (document.location.href.includes("/upload")) {
 			presenceData.largeImageKey = "https://i.imgur.com/v10pkZA.png";
 			presenceData.smallImageKey = "https://i.imgur.com/55dCzB1.png";
 			presenceData.details = "Uploading a post";
 			presenceData.state = "";
 			presence.setActivity(presenceData);
-		} else if (document.URL.includes("/u/")) {
+		} else if (document.location.href.includes("/u/")) {
 			presenceData.largeImageKey = "https://api.dicebear.com/7.x/avataaars/png"; //`https://i.imgur.com/v10pkZA.png`;
 			presenceData.smallImageKey = "https://i.imgur.com/v10pkZA.png";
 			presenceData.details = "Viewing a user profile";
-			presenceData.state = `@${document.URL.split("/u/")[1]}`;
-			presenceData.buttons = [{ url: document.URL, label: "View User" }];
+			presenceData.state = `@${document.location.href.split("/u/")[1]}`;
+			presenceData.buttons = [{ url: document.location.href, label: "View User" }];
 			presence.setActivity(presenceData);
 		} else presence.setActivity(presenceData);
 		url = nurl;
