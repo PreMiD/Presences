f707d3d4c2c95d1125e42e77a3439fc6ed7b7992
DEMON <musicbyblakk@gmail.com>
2023-11-23 16:13:05 +0100

    feat(DXVIL): add  presence

---
+++ websites/D/DXVIL/presence.ts
@@ -0,0 +1,22 @@
+const presence = new Presence({
+	clientId: "1177239603535675412",
+}),
+	largeImage = "https://i.imgur.com/IgrY0YE.gif" || "dxvil_lg";
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey: largeImage,
+	};
+
+	presenceData.details = "⠎⠕⠇⠧⠊⠝⠛ ⠍⠽⠎⠞⠑⠗⠊⠑⠎";
+	presenceData.state = "⠋⠊⠝⠙ ⠞⠓⠑ ⠑⠉⠓⠕⠑⠎";
+	presenceData.buttons = [
+		{
+			label: "⠋⠊⠝⠙ ⠞⠓⠑ ⠞⠗⠥⠞⠓",
+			url: "https://dxvil.com",
+		},
+	];
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
