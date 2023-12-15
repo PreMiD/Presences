2e2219a17f3a5836487588e935c4c17bf4eacf99
SeMiD <semid@premid.app>
2023-12-15 12:20:30 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/W/wumpus.store/presence.ts
+++ websites/W/wumpus.store/presence.ts
@@ -5,7 +5,8 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/KaW8pr2.png",
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/W/wumpus.store/assets/logo.png",
 			details: "Viewing Home Page",
 			startTimestamp: browsingUnix,
 		},
