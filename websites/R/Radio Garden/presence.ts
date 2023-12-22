df75acfc2e6758308b8a8fc2cf3c2d01f3da6b70
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2023-12-22 12:49:52 +0100

    fix(Radio Garden): fix it not displaying radio names & location
    fix(Radio Garden): fix it not displaying radio names & location

--- websites/R/Radio Garden/presence.ts
+++ websites/R/Radio Garden/presence.ts
@@ -1,58 +1,62 @@
 const presence = new Presence({
-	clientId: "687070418804408445", //The client ID of the Application created at https://discordapp.com/developers/applications
-});
+		clientId: "687070418804408445", //The client ID of the Application created at https://discordapp.com/developers/applications
+	}),
+	elapsed = Math.floor(Date.now() / 1000);
 
 let details: string, state: string, currentState: string;
 
 presence.on("UpdateData", async () => {
 	if (!document.title.includes("Explore live radio by rotating")) {
 		details =
-			document.querySelector("._title_165lg_36")?.textContent ??
-			"Unknown Radio"; // which radio we're tuning right now
+			document
+				.querySelector('[class*="_channel_"]')
+				?.querySelector('[class*="_title_"]')?.textContent ?? "Unknown Radio"; // which radio we're tuning right now
 		state =
-			document.querySelector("._subtitle_165lg_43")?.textContent ??
+			document.querySelector('[class*="_subtitle_"]')?.textContent ??
 			"Unknown State/Country"; // the state where the radio serves
 
-		const elapsed = Math.floor(Date.now() / 1000),
-			presenceData: PresenceData = {
-				details,
-				state,
-				largeImageKey:
-					"https://cdn.rcd.gg/PreMiD/websites/R/Radio%20Garden/assets/logo.png",
-				startTimestamp: elapsed,
-			};
+		const presenceData: PresenceData = {
+			details,
+			state,
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/R/Radio%20Garden/assets/logo.png",
+			startTimestamp: elapsed,
+		};
 		currentState = document
-			.querySelectorAll("._control_oyndo_11._modPlay_oyndo_53")[0]
-			.getAttribute("aria-label"); // get current state of the "button", not the "player", except when loading
-		if (currentState.length > 0) {
+			.querySelector("[class*='_modPlay_']")
+			?.getAttribute("aria-label"); // get current state of the "button", not the "player", except when loading
+		if (currentState) {
 			switch (currentState) {
 				case "stop": {
 					// currently playing
-					presenceData.smallImageKey = "statusplay";
+					presenceData.smallImageKey = Assets.Play;
 					presenceData.smallImageText = "Playing";
 					presenceData.details = details;
 					presenceData.state = state;
-					presenceData.startTimestamp = elapsed;
-
+					presenceData.buttons = [
+						{ label: "Listen To The Radio", url: document.location.href },
+					];
 					break;
 				}
 				case "loading": {
 					// currently loading
-					presenceData.smallImageKey = "statusplay";
+					presenceData.smallImageKey = "https://i.imgur.com/2hmWns3.gif";
 					presenceData.smallImageText = "Tuning";
 					presenceData.details = "Tuning";
 					delete presenceData.startTimestamp;
 					delete presenceData.state;
+					delete presenceData.buttons;
 
 					break;
 				}
 				case "play": {
 					// currently stopped
-					presenceData.smallImageKey = "statusstop";
+					presenceData.smallImageKey = Assets.Stop;
 					presenceData.smallImageText = "Stopped";
-					delete presenceData.state;
 					presenceData.details = "Stopped";
 					delete presenceData.startTimestamp;
+					delete presenceData.state;
+					delete presenceData.buttons;
 
 					break;
 				}
