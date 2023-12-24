66d191c9f5d796fa6b8703acae1d89798433f255
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2023-12-24 21:32:26 +0100

    fix(index.d.ts): readd metadata interface (#7833)
    * fix(index.d.ts): readd metadata interface
    
    Bas: 'Ah can you pr that the metadata stuff is added back to the index.d.ts'
    
    * fix(index.d.ts): readd metadata interface
    
    Bas: 'Ah can you pr that the metadata stuff is added back to the index.d.ts'

--- @types/premid/index.d.ts
+++ @types/premid/index.d.ts
@@ -148,6 +148,185 @@ const enum Assets {
 	Viewing = "https://cdn.rcd.gg/PreMiD/resources/viewing.png",
 }
 
+/**
+ * Contains basic information about the presece
+ * @link https://docs.premid.app/dev/presence/metadata
+ */
+interface Metadata {
+	/**
+	 * Should contain Object with name and id of the presence developer.
+	 *
+	 * Name is your Discord username without the identifier(#0000).
+	 *
+	 * User id can be copied from Discord by enabling developer mode and right-clicking on your profile.
+	 */
+	author: Contributor;
+	/**
+	 * Should contain an Array of Objects with each Object having the name and id of the contributor.
+	 *
+	 * Name is your Discord username without the identifier(#0000).
+	 *
+	 * User id can be copied from Discord by enabling developer mode and right-clicking on your profile.
+	 */
+	contributors?: Contributor[];
+	/**
+	 * The title of the service that this presence supports. The folder name and service name should also be the same.
+	 */
+	service: string;
+	/**
+	 * Alternative titles for the service which can be used for searching in the store.
+	 *
+	 * Useful for services that have different names in different countries or for services which have spaces in them, you can remove the space in the alternative name for easier searching.
+	 *
+	 * Note: This is **NOT** used for tags! Only for alternative names!
+	 */
+	altnames?: string[];
+	/**
+	 * Small description of the service.
+	 *
+	 * Your description must have key pair values which indicate the language, and the description in that specific language.
+	 *
+	 * Make descriptions with the languages that you know, our translators will make changes to your metadata file.
+	 *
+	 * Visit the link for all the language IDs.
+	 * @link https://api.premid.app/v2/langFile/list
+	 */
+	description: Record<string, string>;
+	/**
+	 * URL of the service.
+	 *
+	 * Example: `vk.com`
+	 *
+	 * This url must match the url of the website as it will be used to detect wherever or not this is the website to inject the script to.
+	 *
+	 * This may only be used as an array when there are more than one urls.
+	 *
+	 * Note: Do **NOT** add `http://` or `https://` in the url or it will not work.
+	 */
+	url: string | string[];
+	/**
+	 * Version of your presence.
+	 *
+	 * Use Sematic Versioning; <MAJOR>.<MINOR>.<PATCH>
+	 *
+	 * @link https://semver.org/
+	 */
+	version: string;
+	/**
+	 * Link to service's logo.
+	 *
+	 * Must be an imgur link ending with .png/.jpg/.jpeg/.gif.
+	 */
+	logo: string;
+	/**
+	 * Link to service's thumbnail or picture of the website.
+	 *
+	 * Must end with .png/.jpg/etc.
+	 */
+	thumbnail: string;
+	/**
+	 * `#HEX` value.
+	 *
+	 * We recommend using a color that resembles the service the most.
+	 */
+	color: string;
+	/**
+	 * Array with tags, they will help users to search your presence on the website.
+	 */
+	tags: string[];
+	/**
+	 * A string used to represent the category the presence falls under.
+	 *
+	 * @link https://docs.premid.app/dev/presence/metadata#presence-categories
+	 */
+	category: "anime" | "games" | "music" | "socials" | "videos" | "other";
+	/**
+	 * Defines whether `iFrames` are used.
+	 */
+	iframe?: boolean;
+	/**
+	 * A regular expression string used to match urls.
+	 * @link https://docs.premid.app/dev/presence/metadata#regular-expressions
+	 */
+	regExp?: string;
+	/**
+	 * A regular expression selector that selects iframes to inject into.
+	 * @link https://docs.premid.app/dev/presence/metadata#regular-expressions
+	 */
+	iFrameRegExp?: string;
+	/**
+	 * Defines whether `getLogs()` is used.
+	 */
+	readLogs?: boolean;
+	/**
+	 * Whether to include a "add presence" button on the store. Only available for partnered presences.
+	 *
+	 * @private
+	 */
+	button?: boolean;
+	/**
+	 * Whether to display a warning on the presence installation page.
+	 */
+	warning?: boolean;
+	/**
+	 * An array of settings the user can change.
+	 * @link https://docs.premid.app/dev/presence/metadata#presence-settings
+	 */
+	settings?: {
+		/**
+		 * Identifier of the setting, used to obtain its value through presence.getSetting()
+		 */
+		id: string;
+		/**
+		 * Needed for every setting except if you use `multiLanguage`.
+		 */
+		title?: string;
+		/**
+		 * Needed for every setting except if you use `multiLanguage`.
+		 */
+		icon?: string;
+		/**
+		 * Record of conditions that need to be matched for the setting to appear.
+		 * The keys should be ids of other settings and the values should be the value they need to match.
+		 */
+		if?: Record<string, string | number | boolean>;
+		/**
+		 * The text that appears in the background of string settings when nothing is in the input.
+		 */
+		placeholder?: string;
+		/**
+		 * The default value of the setting.
+		 */
+		value?: string | number | boolean;
+		/**
+		 * An array of values to be used as choices for the setting.
+		 * The returned value will be a number representing the index of the chosen option
+		 */
+		values?: (string | number | boolean)[];
+		/**
+		 * `false`: default, it disables multi-localization.
+		 *
+		 * `true`: use this if you are only going to use strings from the [`general.json`](https://github.com/PreMiD/Localization/blob/main/src/Presence/general.json) file.
+		 *
+		 * `string`: name of the file, excluding the extension (.json), inside the [localization github repo](https://github.com/PreMiD/Localization/tree/master/src/Presence).
+		 *
+		 * `string[]`: if you are using more than one file, from inside of the [localization github repo](https://github.com/PreMiD/Localization/tree/master/src/Presence), you can specify all the values in an array. Only common languages of all the files will be listed.
+		 */
+		multiLanguage?: boolean | string | string[];
+	}[];
+}
+
+interface Contributor {
+	/**
+	 * Name of the contributor on Discord.
+	 */
+	name: string;
+	/**
+	 * Discord ID of the contributor.
+	 */
+	id: string;
+}
+
 type PresenceClassSetActivityEvent = {
 	event: "setActivity";
 	data: { clientId: string; presenceData: PresenceDataFinal };
