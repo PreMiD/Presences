ffb1acc5aa47765685de9b617aaab7dabbedb5b9
SeMiD <semid@premid.app>
2023-12-25 14:48:19 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/T/Tetris/presence.ts
+++ websites/T/Tetris/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/cdEP9LJ.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/T/Tetris/assets/logo.png",
 }
 
 interface TetrisFrameData {
