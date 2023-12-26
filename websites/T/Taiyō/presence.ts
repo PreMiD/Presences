fb38655c0c359ef9c540f0c2bde507bca2044d4e
Lusca Carvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-12-26 06:09:39 -0400

    feat(Taiyō): Add presence (#7845)
    * feat: add presence
    
    * feat: update presence state to include chapter number and current page
    
    * feat: update metadata and presence details for Taiyō website
    
    * refactor: yarn format
    
    * refactor: presence.ts to improve readability and remove unnecessary code
    
    * refactor: update presence.ts with more descriptive details
    
    * refactor: add subdomain
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Lusca Carvalho <71954875+Luscarvalho@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Lusca Carvalho <71954875+Luscarvalho@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

---
+++ websites/T/Taiyō/presence.ts
@@ -0,0 +1,68 @@
+const presence = new Presence({
+		clientId: "1187606265829019689",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+			largeImageKey: "https://i.imgur.com/HwdiE6p.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ href, pathname } = document.location;
+
+	if (pathname === "/") presenceData.details = "Na página inicial...";
+	else if (pathname.endsWith("/sign-in"))
+		presenceData.details = "Fazendo login";
+	else if (pathname.startsWith("/dashboard")) {
+		if (pathname.includes("/medias/import"))
+			presenceData.details = "Importando mangá";
+		else if (pathname.includes("/medias/edit"))
+			presenceData.details = "Editando mangá";
+		else if (pathname.includes("/chapters/upload")) {
+			const chapterNumber = document.querySelector<HTMLInputElement>(
+					'.chapter-number input[type="number"]'
+				)?.value,
+				chapterVolume = document.querySelector<HTMLInputElement>(
+					'.chapter-volume input[type="number"]'
+				)?.value,
+				chapterTitle = document.querySelectorAll<HTMLInputElement>(
+					'input[data-slot="input"]'
+				)[1]?.value;
+			if (chapterTitle) {
+				presenceData.details = `Upando: ${chapterTitle}`;
+				if (chapterNumber && chapterVolume)
+					presenceData.state = `Cap. ${chapterNumber} - Vol. ${chapterVolume}`;
+			} else presenceData.details = "Upando mangá...";
+		} else if (pathname.includes("/scans/add"))
+			presenceData.details = "Adicionando scan";
+		else presenceData.details = "No dashboard...";
+	} else if (pathname.startsWith("/chapter/")) {
+		presenceData.details = `${
+			document.querySelector(".media-title")?.textContent
+		}`;
+		presenceData.state = `${
+			document.querySelector(".chapter-number")?.textContent
+		} - Página ${document.querySelector(".chapter-currentPage")?.textContent}`;
+		presenceData.buttons = [
+			{
+				label: "Ler capítulo",
+				url: href,
+			},
+		];
+	} else if (pathname.startsWith("/media/")) {
+		presenceData.largeImageKey =
+			document.querySelector<HTMLImageElement>(".cover-url").src;
+		presenceData.details = `${
+			document.querySelector(".media-title")?.textContent
+		}`;
+		presenceData.buttons = [
+			{
+				label: "Ler mangá",
+				url: href,
+			},
+		];
+	}
+
+	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
+});
