c92b245227eec63bc45b00612affd670e833c239
SeMiD <semid@premid.app>
2023-12-26 17:19:55 +0000

    chore(Taiyō): update assets (#7850)
    chore: update assets
    
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/T/Taiyō/presence.ts
+++ websites/T/Taiyō/presence.ts
@@ -5,7 +5,8 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/HwdiE6p.png",
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/T/Taiy%C5%8D/assets/logo.png",
 			startTimestamp: browsingTimestamp,
 		},
 		{ href, pathname } = document.location;
