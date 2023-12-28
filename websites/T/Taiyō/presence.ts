8ccde5c055be7e68495f2e6ec5b2eaa994f916d7
Luscarvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-12-28 19:24:07 -0400

    refactor: presence.ts to use switch statement for pathname

--- websites/T/Taiyō/presence.ts
+++ websites/T/Taiyō/presence.ts
@@ -11,60 +11,64 @@ presence.on("UpdateData", async () => {
 		},
 		{ href, pathname } = document.location;
 
-	if (pathname === "/") presenceData.details = "Na página inicial...";
-	else if (pathname.endsWith("/sign-in"))
-		presenceData.details = "Fazendo login";
-	else if (pathname.startsWith("/dashboard")) {
-		if (pathname.includes("/medias/import"))
-			presenceData.details = "Importando mangá";
-		else if (pathname.includes("/medias/edit"))
-			presenceData.details = "Editando mangá";
-		else if (
-			pathname.includes("/chapters/upload") ||
-			pathname.includes("/chapters/bulk-upload")
-		) {
-			const chapterNumber = document.querySelector<HTMLInputElement>(
-					'.chapter-number input[type="number"]'
-				)?.value,
-				chapterVolume = document.querySelector<HTMLInputElement>(
-					'.chapter-volume input[type="number"]'
-				)?.value,
-				chapterTitle = document.querySelectorAll<HTMLInputElement>(
-					'input[data-slot="input"]'
-				)[1]?.value;
-			if (chapterTitle) {
-				presenceData.details = `Upando: ${chapterTitle}`;
-				if (chapterNumber && chapterVolume)
-					presenceData.state = `Cap. ${chapterNumber} - Vol. ${chapterVolume}`;
-			} else presenceData.details = "Upando mangá...";
-		} else if (pathname.includes("/scans/add"))
-			presenceData.details = "Adicionando scan";
-		else presenceData.details = "No dashboard...";
-	} else if (pathname.startsWith("/chapter/")) {
-		presenceData.details = `${
-			document.querySelector(".media-title")?.textContent
-		}`;
-		presenceData.state = `${
-			document.querySelector(".chapter-number")?.textContent
-		} - Página ${document.querySelector(".chapter-currentPage")?.textContent}`;
-		presenceData.buttons = [
-			{
-				label: "Ler capítulo",
-				url: href,
-			},
-		];
-	} else if (pathname.startsWith("/media/")) {
-		presenceData.largeImageKey =
-			document.querySelector<HTMLImageElement>(".cover-url").src;
-		presenceData.details = `${
-			document.querySelector(".media-title")?.textContent
-		}`;
-		presenceData.buttons = [
-			{
-				label: "Ler mangá",
-				url: href,
-			},
-		];
+	switch (pathname.split("/")[1]) {
+		case "auth":
+			presenceData.details = "Fazendo login";
+			break;
+
+		case "dashboard": {
+			if (pathname.includes("/medias/import"))
+				presenceData.details = "Importando mangá";
+			else if (pathname.includes("/medias/edit"))
+				presenceData.details = "Editando mangá";
+			else if (
+				pathname.includes("/chapters/upload") ||
+				pathname.includes("/chapters/bulk-upload")
+			) {
+				const chapterNumber = document.querySelector<HTMLInputElement>(
+						'.chapter-number input[type="number"]'
+					)?.value,
+					chapterVolume = document.querySelector<HTMLInputElement>(
+						'.chapter-volume input[type="number"]'
+					)?.value,
+					chapterTitle = document.querySelectorAll<HTMLInputElement>(
+						'input[data-slot="input"]'
+					)[1]?.value;
+				if (chapterTitle) {
+					presenceData.details = `Upando: ${chapterTitle}`;
+					if (chapterNumber && chapterVolume)
+						presenceData.state = `Cap. ${chapterNumber} - Vol. ${chapterVolume}`;
+				} else presenceData.details = "Upando mangá...";
+			} else if (pathname.includes("/scans/add"))
+				presenceData.details = "Adicionando scan";
+			else presenceData.details = "No dashboard...";
+			break;
+		}
+
+		case "chapter": {
+			presenceData.details =
+				document.querySelector(".media-title")?.textContent;
+			presenceData.state = `${
+				document.querySelector(".chapter-number")?.textContent
+			} - Página ${
+				document.querySelector(".chapter-currentPage")?.textContent
+			}`;
+			presenceData.buttons = [{ label: "Ler capítulo", url: href }];
+			break;
+		}
+
+		case "media": {
+			presenceData.largeImageKey =
+				document.querySelector<HTMLImageElement>(".cover-url")?.src;
+			presenceData.details =
+				document.querySelector(".media-title")?.textContent;
+			presenceData.buttons = [{ label: "Ler mangá", url: href }];
+			break;
+		}
+
+		default:
+			presenceData.details = "Na página inicial...";
+			break;
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
