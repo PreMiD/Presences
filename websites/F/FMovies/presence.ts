fc2990d6cc019f12f1eb44da2cb14346e4768614
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2023-12-29 14:41:13 +0100

    feat(FMovies): add regex & support more versions

--- websites/F/FMovies/presence.ts
+++ websites/F/FMovies/presence.ts
@@ -3,11 +3,11 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-let iFrameData: {
-	currTime: number;
-	duration: number;
-	paused: boolean;
-} = null;
+let iFrameData = {
+	currTime: -1,
+	duration: -1,
+	paused: true,
+};
 
 presence.on(
 	"iFrameData",
@@ -19,19 +19,57 @@ presence.on(
 function setCommonData(
 	presenceData: PresenceData,
 	document: Document,
-	iFrameData: { currTime: number; duration: number; paused: boolean },
+	iFrameData: {
+		currTime: number;
+		duration: number;
+		paused: boolean;
+	},
 	href: string
 ) {
 	if (document) {
 		delete presenceData.startTimestamp;
-		presenceData.details =
-			document.querySelector('[itemprop="image"]')?.getAttribute("alt") ??
-			document.querySelector('[itemprop="name"]')?.textContent ??
-			"Unknown";
+		const isMovie =
+				href.includes("-movie") ||
+				href.includes("/movie") ||
+				href.includes("-film") ||
+				href.includes("/film"),
+			title =
+				document.querySelector(
+					'[aria-current="page"],[itemprop="name"],.card-title fs-4,.Title'
+				)?.textContent ??
+				document.querySelector('[itemprop="image"]')?.getAttribute("alt") ??
+				document.querySelector(".film-poster-img")?.getAttribute("title") ??
+				document
+					.querySelector(".lazy img-fluid rounded")
+					?.getAttribute("alt") ??
+				"Unknown";
+
+		if (!iFrameData?.currTime || iFrameData?.currTime === -1) {
+			presenceData.details = isMovie ? "Viewing movie:" : "Viewing series:";
+			presenceData.state = title;
+
+			presenceData.buttons = [
+				{
+					label: isMovie ? "View Movie" : "View Series",
+					url: href,
+				},
+			];
+			return presenceData;
+		}
+		if (!isMovie) {
+			if (title.toLowerCase().includes("season")) {
+				const splitEl = title.split("-");
+				presenceData.details = splitEl?.[0];
+				presenceData.state = `${splitEl?.[1] ?? "Unknown season"} - ${
+					splitEl?.[2]?.split(":")?.[0] ?? "unknown episode"
+				}`;
+			}
+		} else presenceData.details = title;
+
 		presenceData.largeImageKey =
 			document.querySelector('[itemprop="image"]')?.getAttribute("src") ??
 			Assets.Logo;
-		if (iFrameData && !iFrameData.paused) {
+		if (!iFrameData?.paused) {
 			[, presenceData.endTimestamp] = presence.getTimestamps(
 				iFrameData.currTime,
 				iFrameData.duration
@@ -40,11 +78,12 @@ function setCommonData(
 		} else presenceData.smallImageKey = Assets.Pause;
 		presenceData.buttons = [
 			{
-				label: href.includes("/movie") ? "Watch Movie" : "Watch Series",
+				label: isMovie ? "Watch Movie" : "Watch Series",
 				url: href,
 			},
 		];
 	}
+
 	return presenceData;
 }
 
@@ -63,16 +102,13 @@ presence.on("UpdateData", async () => {
 			presence.getSetting<boolean>("image"),
 		]);
 
-	if (pathname.includes("/series/") || pathname.includes("/tv/")) {
-		const matches = href.match(/\/(\d+-\d+)/);
-
-		if (matches) {
-			const [season, episode] = matches[1].split("-");
-			presenceData.state = `S${season}:E${episode}`;
-		} else presenceData.state = "";
-
-		setCommonData(presenceData, document, iFrameData, href);
-	} else if (pathname.startsWith("/movie/"))
+	if (
+		pathname.includes("/series/") ||
+		pathname.includes("/tv/") ||
+		pathname.includes("/watch") ||
+		pathname.includes("/film/") ||
+		pathname.includes("/movie/")
+	)
 		setCommonData(presenceData, document, iFrameData, href);
 	else if (pathname === "/user/profile")
 		presenceData.details = "Checking Profile";
