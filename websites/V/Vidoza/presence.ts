1366f8e57d2628b3b9a29de880fd27df20f38575
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2023-12-29 22:14:29 +0100

    feat(Vidoza): add presence

---
+++ websites/V/Vidoza/presence.ts
@@ -0,0 +1,42 @@
+const presence = new Presence({
+		clientId: "1191396494381694976",
+	}),
+	browsingTimestamp = Math.round(Date.now() / 1000),
+	strings = presence.getStrings({
+		play: "general.playing",
+		pause: "general.paused",
+	});
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+			startTimestamp: browsingTimestamp,
+			largeImageKey: "https://i.imgur.com/y23NjvR.png",
+		},
+		{ pathname } = document.location,
+		video = document.querySelector<HTMLVideoElement>("video");
+
+	if (pathname.includes("embed-")) {
+		delete presenceData.startTimestamp;
+		const el = document
+				.querySelector(".body-container")
+				.querySelectorAll('[type="text/javascript"]'),
+			title = el[el.length - 1].innerHTML
+				.split("var curFileName =")?.[1]
+				?.replace(/([.])/gm, " ")
+				.replace('"', "");
+
+		presenceData.details = title?.split(" S0")?.[0] ?? title;
+		presenceData.state = title?.match(/S[0-9]*E[0-9]* /gm)?.[0];
+
+		if (!isNaN(video?.duration)) {
+			if (!video.paused)
+				[, presenceData.endTimestamp] = presence.getTimestampsfromMedia(video);
+			presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
+			presenceData.smallImageText = video.paused
+				? (await strings).pause
+				: (await strings).play;
+		}
+	}
+
+	presence.setActivity(presenceData);
+});
