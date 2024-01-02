dfe6d4f16952c8ce9ee7cadce2f4cd0a56518814
joerkig <joeri@uitdelft.nl>
2024-01-02 23:14:13 +0100

    fix(2048Verse): logo 512x512 Forgot to turn off "maintain aspect ratio" when cropping 😔

--- websites/0-9/2048Verse/presence.ts
+++ websites/0-9/2048Verse/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/wqRX0c7.png",
+	Logo = "https://i.imgur.com/aigOGhI.png",
 }
 
 presence.on("UpdateData", async () => {
