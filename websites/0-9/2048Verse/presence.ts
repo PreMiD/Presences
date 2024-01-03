a64fe6a768328caad387d60437626ee899aed24b
SeMiD <semid@premid.app>
2024-01-03 11:28:19 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/0-9/2048Verse/presence.ts
+++ websites/0-9/2048Verse/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/aigOGhI.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/0-9/2048Verse/assets/logo.png",
 }
 
 presence.on("UpdateData", async () => {
