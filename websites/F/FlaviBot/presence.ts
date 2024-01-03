5c8bf05ecd7fca22745fc359576b5df36a58eb3f
SeMiD <semid@premid.app>
2024-01-03 19:19:14 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/F/FlaviBot/presence.ts
+++ websites/F/FlaviBot/presence.ts
@@ -5,7 +5,8 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/R9S9p94.png",
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/F/FlaviBot/assets/logo.png",
 			startTimestamp: browsingTimestamp,
 		},
 		{ pathname, href } = document.location;
