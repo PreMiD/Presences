5627b3dafa844a8eb72739d007b60f170169c4e8
joerkig <44418429+joerkig@users.noreply.github.com>
2024-01-03 12:26:32 +0100

    feat(2048Verse): add presence (#7874)
    * feat(2048Verse): add presence
    
    * fix(2048Verse): bump schema
    
    * fix(2048Verse): logo 512x512
    Forgot to turn off "maintain aspect ratio" when cropping 😔
    
    * fix(2048Verse): metadata logo
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: joerkig <44418429+joerkig@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: joerkig <44418429+joerkig@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

---
+++ websites/0-9/2048Verse/presence.ts
@@ -0,0 +1,77 @@
+const presence = new Presence({
+		clientId: "939893158777618432",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+const enum Assets {
+	Logo = "https://i.imgur.com/aigOGhI.png",
+}
+
+presence.on("UpdateData", async () => {
+	const [time, buttons] = await Promise.all([
+			presence.getSetting<boolean>("time"),
+			presence.getSetting<boolean>("buttons"),
+		]),
+		presenceData: PresenceData = {
+			largeImageKey: Assets.Logo,
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname, href } = document.location,
+		gridSize = `${document.querySelectorAll(".grid-row").length.toString()}x${(
+			document.querySelectorAll(".grid-cell").length /
+			document.querySelectorAll(".grid-row").length
+		).toString()}`;
+
+	if (pathname.split("/")[1] === "leaderboard") {
+		let leaderboardType;
+		switch (document.querySelector<HTMLDivElement>("#marker").style.left) {
+			case "-280px": {
+				leaderboardType = "All Time";
+				break;
+			}
+			case "-157px": {
+				leaderboardType = "Today";
+				break;
+			}
+			case "-44px": {
+				leaderboardType = "Week";
+				break;
+			}
+		}
+		presenceData.details = "Viewing leaderboard";
+		presenceData.state = `${
+			document.querySelector<HTMLSelectElement>("#variant").value
+		} ${leaderboardType}`;
+	} else if (pathname.split("/")[1] === "user") {
+		presenceData.details = "Viewing profile";
+		presenceData.state =
+			document.querySelector("h1.title").firstChild.textContent;
+		presenceData.buttons = [{ label: "View Profile", url: href }];
+	} else if (pathname.split("/")[1] === "replay") {
+		presenceData.details = `Watching replay ${gridSize}`;
+		presenceData.state = `Score ${
+			document.querySelector(".score-container")?.firstChild?.textContent
+		}`;
+	} else if (pathname.split("/")[1] === "p") {
+		presenceData.details = `Practising ${gridSize}`;
+		presenceData.state = `Score ${
+			document.querySelector(".score-container")?.firstChild?.textContent
+		} Best ${document.querySelector(".best-container")?.textContent}`;
+	} else if (pathname.split("/")[1] === "lessons")
+		presenceData.details = "Browsing lessons";
+	else if (document.querySelectorAll(".grid-row").length) {
+		presenceData.details = `Playing ${gridSize}`;
+
+		presenceData.state = `Score ${
+			document.querySelector(".score-container")?.firstChild?.textContent
+		} Best ${document.querySelector(".best-container")?.textContent}`;
+	}
+
+	if (!time) {
+		delete presenceData.startTimestamp;
+		delete presenceData.endTimestamp;
+	}
+	if (!buttons && presenceData.buttons) delete presenceData.buttons;
+
+	presence.setActivity(presenceData);
+});
