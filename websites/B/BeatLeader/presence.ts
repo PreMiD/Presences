736373051bd701d5479d0fc49f74b1c02368c17c
joerkig <44418429+joerkig@users.noreply.github.com>
2024-01-03 14:06:11 +0100

    fix(Beatleader): gif weirdness
    Signed-off-by: joerkig <44418429+joerkig@users.noreply.github.com>

--- websites/B/BeatLeader/presence.ts
+++ websites/B/BeatLeader/presence.ts
@@ -13,7 +13,7 @@ const presence = new Presence({
 		"https://cdn.rcd.gg/PreMiD/websites/B/BeatLeader/assets/80.png", //GladdePaling
 		"https://cdn.rcd.gg/PreMiD/websites/B/BeatLeader/assets/81.gif", //EarthDay
 		`https://i.imgur.com${"/Kf8bXNc.gif"}`, //Christmas 2022
-		"https://i.imgur.com/4dpoXYa.gif", //Christmas 2023
+		`https://i.imgur.com${"/4dpoXYa.gif"}`, //Christmas 2023
 	];
 
 const enum Assets {
