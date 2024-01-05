7b87b772fc7a35b250f45b2cfc3314e21ee7eb57
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2024-01-05 12:29:15 +0100

    Update websites/R/Reddit/presence.ts
    Signed-off-by: Dark_Ville <42322979+DarkVillager@users.noreply.github.com>

--- websites/R/Reddit/presence.ts
+++ websites/R/Reddit/presence.ts
@@ -40,7 +40,7 @@ const presences: { [key in PresenceClients]?: Presence } = {
 		[PresenceClients.Reddit]: presence,
 	},
 	startTimestamp = Math.floor(Date.now() / 1000),
-	oldReddit = document.querySelector("#default-header") === null;
+	oldReddit = document.querySelector(".default-header") === null;
 
 function setClient(clientId: PresenceClients) {
 	presence.clearActivity();
