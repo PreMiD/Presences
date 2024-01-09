5db6dca81de46a1c14fdd5d2800446035748829a
Mateusz Starkiewicz <84156177+ItsMateo20@users.noreply.github.com>
2024-01-09 01:30:36 +0100

    feat(Notespace): add presence (#7921)
    * new
    
    Signed-off-by: Mateusz Starkiewicz <84156177+ItsMateo20@users.noreply.github.com>
    
    * Update websites/N/Notespace/metadata.json
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Mateusz Starkiewicz <84156177+ItsMateo20@users.noreply.github.com>
    
    * Update websites/N/Notespace/presence.ts
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Mateusz Starkiewicz <84156177+ItsMateo20@users.noreply.github.com>
    
    * feat(Notespace): add new image keys and buttons
    
    * feat(Notespace): browsing start timestamp in presence.ts
    
    * feat(Notespace): forgot to yarn format
    
    ---------
    
    Signed-off-by: Mateusz Starkiewicz <84156177+ItsMateo20@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

---
+++ websites/N/Notespace/presence.ts
@@ -0,0 +1,89 @@
+const presence = new Presence({
+		clientId: "1193651901221306378",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+const enum Assets {
+	Icon = "https://i.imgur.com/W7Ybot8.png",
+}
+
+presence.on("UpdateData", async () => {
+	const currentPath = document.location.pathname,
+		presenceData: PresenceData = {
+			details: "Twoja wiedza jest tu",
+			largeImageKey: Assets.Icon,
+			smallImageKey: Assets.Viewing,
+			smallImageText: "Przegląda...",
+			startTimestamp: browsingTimestamp,
+			buttons: [
+				{
+					label: "Odwiedź stronę o nas",
+					url: "https://notespace.edu.pl/o-nas",
+				},
+			],
+		};
+
+	if (currentPath.endsWith("/") || currentPath.includes("strona-glowna"))
+		presenceData.details = "Przegląda stronę główną";
+	else if (currentPath.includes("kursy")) {
+		presenceData.details = "Przegląda kursy";
+		presenceData.smallImageKey = Assets.Reading;
+		presenceData.smallImageText = "Czyta...";
+		presenceData.state = document.querySelector("h1").textContent;
+	} else if (currentPath.includes("kurs")) {
+		presenceData.details = "Przegląda kurs";
+		presenceData.smallImageKey = Assets.Reading;
+		presenceData.smallImageText = "Czyta...";
+		presenceData.state = document.querySelector("h1").textContent;
+	} else if (currentPath.includes("ankieta")) {
+		presenceData.details = "Przegląda strone ankiety";
+		presenceData.state = "Wypełnia ankietę";
+		presenceData.smallImageKey = Assets.Writing;
+		presenceData.smallImageText = "Pisze...";
+		presenceData.buttons = [
+			{ label: "Odwiedź stronę o nas", url: "https://notespace.edu.pl/o-nas" },
+			{ label: "Wypełnij ankietę", url: "https://notespace.edu.pl/ankieta" },
+		];
+	} else if (currentPath.includes("o-nas"))
+		presenceData.details = "Przegląda strone o nas";
+	else if (currentPath.includes("kontakt")) {
+		presenceData.details = "Przegląda strone kontaktów";
+		presenceData.smallImageKey = Assets.Call;
+		presenceData.smallImageText = "Kontaktuje się z nami...";
+		presenceData.buttons = [
+			{ label: "Odwiedź stronę o nas", url: "https://notespace.edu.pl/o-nas" },
+			{
+				label: "Skontaktuj się z nami",
+				url: "https://notespace.edu.pl/kontakt",
+			},
+		];
+	} else if (currentPath.includes("dokumenty"))
+		presenceData.details = "Przegląda stronę dokumentów";
+	else if (currentPath.includes("polityka-prywatnosci")) {
+		presenceData.details = "Przegląda dokument";
+		presenceData.state = "Polityka prywatności";
+		presenceData.smallImageKey = Assets.Reading;
+		presenceData.smallImageText = "Czyta...";
+		presenceData.buttons = [
+			{ label: "Odwiedź stronę o nas", url: "https://notespace.edu.pl/o-nas" },
+			{
+				label: "Dowiedz sie o naszej polityce",
+				url: "https://notespace.edu.pl/polityka-prywatnosci",
+			},
+		];
+	} else if (currentPath.includes("polityka-cookies")) {
+		presenceData.details = "Przegląda dokument";
+		presenceData.state = "Polityka cookies";
+		presenceData.smallImageKey = Assets.Reading;
+		presenceData.smallImageText = "Czyta...";
+		presenceData.buttons = [
+			{ label: "Odwiedź stronę o nas", url: "https://notespace.edu.pl/o-nas" },
+			{
+				label: "Dowiedz sie o naszej polityce",
+				url: "https://notespace.edu.pl/polityka-cookies",
+			},
+		];
+	}
+
+	presence.setActivity(presenceData);
+});
