bf6071a2b9cc5b074584c6d2b3fce1babe36ae49
Cardinal System <cardinalsystemofficial@gmail.com>
2024-01-09 21:28:39 -0500

    fix(Planet Minecraft): linted and corrected version number
    Ran prettier (again). Fixed version number. I can't believe I forgot this one!

--- websites/P/Planet Minecraft/presence.ts
+++ websites/P/Planet Minecraft/presence.ts
@@ -1,6 +1,6 @@
 const presence = new Presence({
-	clientId: "1193411747898458132",
-}),
+		clientId: "1193411747898458132",
+	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000),
 	stateless = "stateless";
 
@@ -141,8 +141,8 @@ presence.on("UpdateData", async () => {
 							presenceData.state = stateless;
 						} else {
 							const parentElement = document.querySelector(
-								"#content_graphic_header > *:first-child"
-							),
+									"#content_graphic_header > *:first-child"
+								),
 								{ length } = parentElement.children;
 
 							if (parentElement.lastChild.nodeType === Node.TEXT_NODE) {
