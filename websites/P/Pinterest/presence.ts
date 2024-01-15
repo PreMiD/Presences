42d6b06aadd506c7701f6045123e83cf12c8686a
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2024-01-15 18:42:46 +0100

    Update websites/P/Pinterest/presence.ts
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Dark_Ville <42322979+DarkVillager@users.noreply.github.com>

--- websites/P/Pinterest/presence.ts
+++ websites/P/Pinterest/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const enum Assets {
 	Likes = "https://i.imgur.com/394dVMI.png",
 	Logo = "https://cdn.rcd.gg/PreMiD/websites/P/Pinterest/assets/logo.png",
-	Loading = `https://i.imgur.com/${"6s5f2TA"}.gif`,
+	Loading = "https://i.imgur.com/\u002e.gif",
 }
 
 presence.on("UpdateData", async () => {
