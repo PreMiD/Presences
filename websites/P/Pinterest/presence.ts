a65e321dd6af5d713deb36d814280d7205cbff47
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2024-01-17 10:05:59 -0800

    fix: remove extra dot from Loading link
    Signed-off-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

--- websites/P/Pinterest/presence.ts
+++ websites/P/Pinterest/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const enum Assets {
 	Likes = "https://i.imgur.com/394dVMI.png",
 	Logo = "https://cdn.rcd.gg/PreMiD/websites/P/Pinterest/assets/logo.png",
-	Loading = "https://i.imgur.com/\u002e.gif",
+	Loading = "https://i.imgur.com/\u002egif",
 }
 
 presence.on("UpdateData", async () => {
