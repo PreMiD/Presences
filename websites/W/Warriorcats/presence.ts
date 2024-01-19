8ad068e46dad395b93acc72481f2193b839840fc
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2024-01-19 23:02:03 +0100

    feat: update logo URL in metadata.json

--- websites/W/Warriorcats/presence.ts
+++ websites/W/Warriorcats/presence.ts
@@ -1,18 +1,14 @@
 const presence = new Presence({
-	clientId: "936630095035113522",
-});
-const enum Assets {
-	Logo = "",
+		clientId: "936630095035113522",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+async function getPostNumber() {
+	// find <article></article> and get the #post- number
+	let postNumber = document.querySelector("article").getAttribute("id");
+	postNumber = postNumber.replace(/post-/, "");
+	return postNumber;
 }
-
-var browsingTimestamp = Math.floor(Date.now() / 1000);
-
-	async function getPostNumber() {
-		// find <article></article> and get the #post- number
-		let postNumber = document.querySelector("article").getAttribute("id");
-		postNumber = postNumber.replace(/post-/, "");
-		return postNumber;
-	}
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
@@ -27,143 +23,170 @@ presence.on("UpdateData", async () => {
 		],
 	};
 
-	if (document.location.pathname === "/") {
-		presenceData.details = "Viewing";
-        presenceData.state = "Warrior Cats Home";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-	} else if (document.location.pathname === "/beutehaufen-uebersicht/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "Warrior Cats Book Overview";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg"
-	} else if (document.location.pathname === "/nachrichten-vom-baumgeviert/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "Warrior Cats News";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-	} else if (document.location.pathname === "/nutzerkonto/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "Warrior Cats Account";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-	} else if (document.location.pathname === "/dein-clanlager/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "Warrior Cats Clan Camp";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-	} else if (document.location.pathname === "/deine-clankatze/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "own Warrior Cats Clan Cat";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-	} else if (document.location.pathname === "/graphic-novel/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "Warrior Cats Graphic Novels";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-		presence.setActivity(presenceData);
-	} else if (document.location.pathname === "/alle-baende/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "all Warrior Cats Books";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-		presence.setActivity(presenceData);
-	} else if (document.location.pathname === "/erin-hunter/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "stuff about Erin Hunter";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-		presence.setActivity(presenceData);
-	} else if (document.location.pathname === "/welt-der-warrior-cats/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.state = "the Warrior Cats World";
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-		presence.setActivity(presenceData);
-	} else if (document.location.pathname === "/zeitstrahl/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.state = "the Warrior Cats LORE";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-	} else if (document.location.pathname === "/wiki/") {
-  
-		presenceData.details = "Viewing";
-		presenceData.state = "the Warrior Cats Wiki";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
-	} else {
-		presenceData.details = "Viewing an unsupported page";
-		presenceData.buttons = [
-			{ label: "Current Page", url: document.location.href },
-			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
-		];
-		presenceData.largeImageKey =
-			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+	switch (document.location.pathname) {
+		case "/": {
+			presenceData.details = "Viewing";
+			presenceData.state = "Warrior Cats Home";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+
+			break;
+		}
+		case "/beutehaufen-uebersicht/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "Warrior Cats Book Overview";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+
+			break;
+		}
+		case "/nachrichten-vom-baumgeviert/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "Warrior Cats News";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+
+			break;
+		}
+		case "/nutzerkonto/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "Warrior Cats Account";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+
+			break;
+		}
+		case "/dein-clanlager/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "Warrior Cats Clan Camp";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+
+			break;
+		}
+		case "/deine-clankatze/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "own Warrior Cats Clan Cat";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+
+			break;
+		}
+		case "/graphic-novel/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "Warrior Cats Graphic Novels";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+			presence.setActivity(presenceData);
+
+			break;
+		}
+		case "/alle-baende/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "all Warrior Cats Books";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+			presence.setActivity(presenceData);
+
+			break;
+		}
+		case "/erin-hunter/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "stuff about Erin Hunter";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+			presence.setActivity(presenceData);
+
+			break;
+		}
+		case "/welt-der-warrior-cats/": {
+			presenceData.details = "Viewing";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.state = "the Warrior Cats World";
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+			presence.setActivity(presenceData);
+
+			break;
+		}
+		case "/zeitstrahl/": {
+			presenceData.details = "Viewing";
+			presenceData.state = "the Warrior Cats LORE";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+
+			break;
+		}
+		case "/wiki/": {
+			presenceData.details = "Viewing";
+			presenceData.state = "the Warrior Cats Wiki";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+
+			break;
+		}
+		default: {
+			presenceData.details = "Viewing an unsupported page";
+			presenceData.buttons = [
+				{ label: "Current Page", url: document.location.href },
+				{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
+			];
+			presenceData.largeImageKey =
+				"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
+		}
 	}
 
 	if (document.location.pathname.startsWith("/staffel-")) {
 		presenceData.details = `Viewing Staffel ${
 			document.location.pathname.split("/")[1].split("-")[1]
-		}`; 
+		}`;
 
 		presenceData.largeImageKey =
 			"https://www.warriorcats.de/wp-content/uploads/9783407759085-web.jpg";
@@ -178,25 +201,26 @@ presence.on("UpdateData", async () => {
 	// like this Staffel 8 Band 2 Wolken
 	// and then set the presence state to Currently Staffel 8 Band 2 Wolken
 
-
 	async function getStaffelName(postNumber: string) {
-		let staffelName = document.querySelector(
-			`#post-${postNumber} > div > div.entry-content > div > div.wc-biblio > div.wc-staffel`
-		).textContent;
-		// Remove "Warrior Cats |"
-		let result1 = staffelName.replace(/^Warrior Cats \| /, "");
-		// remove Staffel
-		let result2 = result1.replace(/Staffel /, "");
-		// get staffel number from url e.g
-		let replacethis = document.location.pathname
-			.split("/")[2]
-			.split("-")[1]
-			.toUpperCase();
-		// for ressult3 match replacethis with result2 and replacce it with nothing
-		let result3 = result2.replace(replacethis, "");
+		const staffelName = document.querySelector(
+				`#post-${postNumber} > div > div.entry-content > div > div.wc-biblio > div.wc-staffel`
+			).textContent,
+			// Remove "Warrior Cats |"
+
+			// remove Staffel
+
+			// get staffel number from url e.g
+
+			// for ressult3 match replacethis with result2 and replacce it with nothing
+			result3 = staffelName
+				.replace(/^Warrior Cats \| /, "")
+				.replace(/Staffel /, "")
+				.replace(
+					document.location.pathname.split("/")[2].split("-")[1].toUpperCase(),
+					""
+				);
 		// now remove everything that is not a letter or a space and trim the start and end
-		let result4 = result3.replace(/[^a-zA-Z ]/g, "").trim();
-		return result4;
+		return result3.replace(/[^a-zA-Z ]/g, "").trim();
 	}
 
 	// async function getStaffelNumber(postNumber: string) {
@@ -216,7 +240,7 @@ presence.on("UpdateData", async () => {
 	}
 
 	async function getBuchImage(postNumber: string) {
-		let buchImage = document
+		const buchImage = document
 			.querySelector(
 				`#post-${postNumber} > div > div.entry-content > div > div.wc-article-thumbnail > a > img`
 			)
@@ -228,11 +252,10 @@ presence.on("UpdateData", async () => {
 		document.location.pathname.startsWith("/beute/staffel-") ||
 		document.location.pathname.startsWith("/beute/")
 	) {
-  
-		let postNumber = await getPostNumber();
-		let staffelName = await getStaffelName(postNumber);
-		let buchName = await getBuchName(postNumber);
-		let buchImage = await getBuchImage(postNumber);
+		const postNumber = await getPostNumber(),
+			staffelName = await getStaffelName(postNumber),
+			buchName = await getBuchName(postNumber),
+			buchImage = await getBuchImage(postNumber);
 
 		presenceData.state = `Viewing ${buchName}`;
 		presenceData.details = `${staffelName}`;
@@ -242,6 +265,6 @@ presence.on("UpdateData", async () => {
 			{ label: "Warrior Cats Germany", url: "https://www.warriorcats.de/" },
 		];
 	}
-    presence.clearActivity();
-    presence.setActivity(presenceData);
+	presence.clearActivity();
+	presence.setActivity(presenceData);
 });
