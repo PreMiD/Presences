6f1d13b54affbc0646a938d152c6d0b12a585cef
bear_ <captainkelvinvu@gmail.com>
2024-01-20 16:24:57 -0800

    chore(Soundgasm): change getStrings implementation

--- websites/S/Soundgasm/presence.ts
+++ websites/S/Soundgasm/presence.ts
@@ -1,28 +1,45 @@
 const presence = new Presence({
 		clientId: "1195258708368822312",
 	}),
-	strings = presence.getStrings({
-		play: "general.playing",
-		pause: "general.paused",
-	}),
+	getStrings = async () => {
+		return presence.getStrings(
+			{
+				play: "general.playing",
+				pause: "general.paused",
+				upload: "youtube.upload",
+			},
+			await presence.getSetting<string>("lang").catch(() => "en")
+		);
+	},
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
 	Logo = "https://i.imgur.com/QIxjVEX.png",
 }
 
-let data, audioTimestamps, audioElement;
+let data,
+	audioTimestamps,
+	audioElement,
+	strings: Awaited<ReturnType<typeof getStrings>>,
+	oldLang: string = null;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: Assets.Logo,
-		startTimestamp: browsingTimestamp,
-	};
+			largeImageKey: Assets.Logo,
+			startTimestamp: browsingTimestamp,
+		},
+		newLang = await presence.getSetting<string>("ID").catch(() => "en");
+
+	if (oldLang !== newLang || !strings) {
+		oldLang = newLang;
+		strings = await getStrings();
+	}
 
 	switch (document.location.pathname) {
 		case "/upload": {
 			presenceData.details = "Uploading an audio";
 			presenceData.smallImageKey = Assets.Uploading;
+			presenceData.smallImageText = strings.upload;
 			break;
 		}
 		case "/signup": {
@@ -56,10 +73,10 @@ presence.on("UpdateData", async () => {
 						document.querySelector<HTMLAudioElement>("#jp_audio_0");
 					if (audioElement.paused) {
 						presenceData.smallImageKey = Assets.Pause;
-						presenceData.smallImageText = (await strings).pause;
+						presenceData.smallImageText = strings.pause;
 					} else {
 						presenceData.smallImageKey = Assets.Play;
-						presenceData.smallImageText = (await strings).play;
+						presenceData.smallImageText = strings.play;
 						audioTimestamps = presence.getTimestampsfromMedia(audioElement);
 						presenceData.startTimestamp = audioTimestamps[0];
 						presenceData.endTimestamp = audioTimestamps[1];
