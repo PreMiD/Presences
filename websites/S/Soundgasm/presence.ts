c2acd0ec2c27f4d498f5618defe408a916076e4f
SeMiD <semid@premid.app>
2024-01-23 21:28:29 +0000

    chore(Soundgasm): update assets (#7976)
    chore: update assets
    
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/S/Soundgasm/presence.ts
+++ websites/S/Soundgasm/presence.ts
@@ -14,7 +14,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/QIxjVEX.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/S/Soundgasm/assets/logo.png",
 }
 
 let data,
