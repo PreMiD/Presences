21cd90fb2a4f1ea4ae7c5363723839c18d21ac3f
KaduKessler <kadukessler@gmail.com>
2024-01-24 15:59:34 -0300

    fix(TuneIn): refactor & requested changes

--- websites/T/TuneIn/presence.ts
+++ websites/T/TuneIn/presence.ts
@@ -2,20 +2,24 @@ const presence = new Presence({
 	clientId: "844108776793178122",
 });
 
+async function getStrings() {
+	return presence.getStrings({
+		play: "general.playing",
+		pause: "general.paused",
+		browse: "general.browsing",
+		live: "general.live",
+		listening: "general.listeningMusic",
+	});
+}
+
+let strings: Awaited<ReturnType<typeof getStrings>>,
+	oldLang: string = null;
+
 presence.on("UpdateData", async () => {
-	const settings = {
-			lang: await presence.getSetting("lang"),
-			timestamps: await presence.getSetting("timestamps"),
-			cover: await presence.getSetting("cover"),
-			private: await presence.getSetting("privacy"),
+	const presenceData: PresenceData = {
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/T/TuneIn/assets/logo.png",
 		},
-		strings = await presence.getStrings({
-			play: "general.playing",
-			pause: "general.paused",
-			browse: "general.browsing",
-			live: "general.live",
-			listening: "general.listeningMusic",
-		}),
 		conditions = {
 			isPaused: document.querySelector('[data-testid="player-status-paused"]'),
 			isStopped: document.querySelector(
@@ -26,94 +30,88 @@ presence.on("UpdateData", async () => {
 			),
 			isLive: document.querySelector("#scrubberElapsed"),
 		},
-		presenceData: PresenceData = {
-			largeImageKey:
-				"https://cdn.rcd.gg/PreMiD/websites/T/TuneIn/assets/logo.png",
-			details: strings.browse,
-		};
+		[newLang, timestamps, cover, private] = await Promise.all([
+			presence.getSetting<string>("lang").catch(() => "en"),
+			presence.getSetting<boolean>("timestamps"),
+			presence.getSetting<boolean>("cover"),
+			presence.getSetting<boolean>("privacy"),
+		]);
 
-	if (
-		document.querySelector('[data-testid="player"]') &&
-		!conditions.isConnecting
-	) {
-		if (conditions.isLive) {
-			if (settings.private) presenceData.details = strings.listening;
-			else {
-				const title = document.querySelector("#playerTitle").textContent,
-					author = document.querySelector("#playerSubtitle").textContent,
-					artwork = document
-						.querySelector("#playerArtwork")
-						.getAttribute("src");
-
-				if (title) {
-					if (title.length > 128)
-						presenceData.details = `${title.substring(0, 125)}...`;
-					else presenceData.details = title;
-				}
-
-				if (author) {
-					if (author.length > 128)
-						presenceData.state = `${author.substring(0, 125)}...`;
-					else presenceData.state = author;
-				}
-
-				if (artwork && settings.cover) presenceData.largeImageKey = artwork;
-			}
-
-			if (conditions.isLive.textContent === "") {
-				if (conditions.isStopped) {
-					delete presenceData.startTimestamp;
-					presenceData.smallImageKey = Assets.Pause;
-					presenceData.smallImageText = strings.pause;
-				} else {
-					if (!settings.private && settings.timestamps) {
-						const timeElapsed = presence.timestampFromFormat(
-							document
-								.querySelector("#scrubberElapsed")
-								.getAttribute("data-elapsedtime")
-						);
-						presenceData.startTimestamp =
-							Math.floor(Date.now() / 1000) - timeElapsed;
-					}
-					presenceData.smallImageKey = Assets.Live;
-					presenceData.smallImageText = strings.live;
-				}
-			} else {
-				if (!settings.private && settings.timestamps) {
-					const elapsed =
-							document.querySelector("#scrubberElapsed").textContent,
-						duration = document.querySelector("#scrubberDuration").textContent;
-
-					if (elapsed !== "00:00" || duration !== "") {
-						const timestamps = presence.getTimestamps(
-							presence.timestampFromFormat(elapsed),
-							presence.timestampFromFormat(duration)
-						);
-
-						presenceData.startTimestamp = timestamps[0];
-						presenceData.endTimestamp = timestamps[1];
-					}
-				} else {
-					delete presenceData.startTimestamp;
-					delete presenceData.endTimestamp;
-				}
-
-				if (conditions.isPaused) {
-					delete presenceData.startTimestamp;
-					delete presenceData.endTimestamp;
-					presenceData.smallImageKey = Assets.Pause;
-					presenceData.smallImageText = strings.pause;
-				} else if (conditions.isStopped) {
-					delete presenceData.startTimestamp;
-					delete presenceData.endTimestamp;
-					presenceData.smallImageKey = Assets.Stop;
-					presenceData.smallImageText = strings.pause;
-				} else {
-					presenceData.smallImageKey = Assets.Play;
-					presenceData.smallImageText = strings.play;
-				}
-			}
-		} else return presence.setActivity(presenceData);
+	if (oldLang !== newLang) {
+		oldLang = newLang;
+		strings = await getStrings();
 	}
+
+	if (!conditions.isConnecting && conditions.isLive) {
+		if (private) presenceData.details = (await strings).listening;
+		else {
+			const title = document.querySelector("#playerTitle").textContent,
+				author = document.querySelector("#playerSubtitle").textContent,
+				artwork = document.querySelector("#playerArtwork").getAttribute("src");
+
+			if (title) {
+				if (title.length > 128)
+					presenceData.details = `${title.substring(0, 125)}...`;
+				else presenceData.details = title;
+			}
+
+			if (author) {
+				if (author.length > 128)
+					presenceData.state = `${author.substring(0, 125)}...`;
+				else presenceData.state = author;
+			}
+
+			if (artwork && cover) presenceData.largeImageKey = artwork;
+		}
+
+		if (conditions.isLive.textContent === "") {
+			if (conditions.isStopped) {
+				delete presenceData.startTimestamp;
+				presenceData.smallImageKey = Assets.Pause;
+				presenceData.smallImageText = (await strings).pause;
+			} else {
+				if (!private && timestamps) {
+					const timeElapsed = presence.timestampFromFormat(
+						document
+							.querySelector("#scrubberElapsed")
+							.getAttribute("data-elapsedtime")
+					);
+					presenceData.startTimestamp =
+						Math.floor(Date.now() / 1000) - timeElapsed;
+				}
+				presenceData.smallImageKey = Assets.Live;
+				presenceData.smallImageText = (await strings).live;
+			}
+		} else {
+			if (!private && timestamps) {
+				const elapsed = document.querySelector("#scrubberElapsed").textContent,
+					duration = document.querySelector("#scrubberDuration").textContent;
+
+				if (elapsed !== "00:00" || duration !== "") {
+					const timestamps = presence.getTimestamps(
+						presence.timestampFromFormat(elapsed),
+						presence.timestampFromFormat(duration)
+					);
+
+					presenceData.startTimestamp = timestamps[0];
+					presenceData.endTimestamp = timestamps[1];
+				}
+			}
+
+			if (conditions.isPaused) {
+				delete presenceData.startTimestamp;
+				delete presenceData.endTimestamp;
+				presenceData.smallImageKey = Assets.Pause;
+				presenceData.smallImageText = (await strings).pause;
+			} else if (conditions.isStopped) {
+				presenceData.smallImageKey = Assets.Stop;
+				presenceData.smallImageText = (await strings).pause;
+			} else {
+				presenceData.smallImageKey = Assets.Play;
+				presenceData.smallImageText = (await strings).play;
+			}
+		}
+	} else presenceData.details = (await strings).browse;
+
 	presence.setActivity(presenceData);
 });
