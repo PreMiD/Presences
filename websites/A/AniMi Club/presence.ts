5ea1f0393ff4b85a3aff4990a45a275ea9558cf5
Artur Dibrova <Gleshaportal255@gmail.com>
2024-01-25 03:26:57 +0100

    feat(AniMi Club): added AniMi Club

---
+++ websites/A/AniMi Club/presence.ts
@@ -0,0 +1,121 @@
+// Создаем объект Presence с указанием clientId
+const presence = new Presence({
+	clientId: "1127599137374871563",
+});
+
+// Перечисление URL-адресов для использования в коде
+const enum Assets {
+	Logo = "https://i.imgur.com/BZtDh0g.png",
+}
+
+// Интерфейс для данных видео
+interface VideoData {
+	duration: number;
+	currentTime: number;
+	paused: boolean;
+}
+
+// Функция для получения строковых значений
+async function getStrings() {
+	return presence.getStrings({
+		play: "general.playing",
+		pause: "general.paused",
+		browse: "general.browsing",
+		viewPage: "general.viewPage",
+		page: "general.page",
+	});
+}
+
+// Асинхронно получаем строковые значения и сохраняем их в переменной
+let strings: Awaited<ReturnType<typeof getStrings>>;
+
+// Объект для хранения данных о видео
+let video: VideoData = {
+	duration: 0,
+	currentTime: 0,
+	paused: true,
+};
+
+// Обработчик события "iFrameData", обновляющий данные о видео
+presence.on("iFrameData", (data: VideoData) => {
+	video = data;
+});
+
+// Обработчик события "UpdateData", обновляющий активность Presence
+presence.on("UpdateData", async () => {
+	// Асинхронно получаем настройки пользователя
+	const [privacy, time] = await Promise.all([
+		presence.getSetting<boolean>("privacy"),
+		presence.getSetting<boolean>("time"),
+	]);
+
+	// Объект для хранения данных для Presence
+	const presenceData: PresenceData = {
+		details: "Где-то на сайте",
+		largeImageKey: Assets.Logo,
+		smallImageText: "AniMi Club",
+	};
+
+	// Если строки не загружены, загружаем их
+	if (!strings) strings = await getStrings();
+
+	// Определяем тип контента
+	const typeContent = document
+		.querySelector("meta[property='og:url']")
+		.getAttribute("content")
+		.split("/")[3];
+	const typeCurrent = typeContent === "anime" ? "аниме" : "манга";
+
+	// Определяем активность в зависимости от текущего URL
+	if (document.location.pathname === "/") {
+		presenceData.details = "На главной странице";
+		presenceData.buttons = [
+			{
+				label: "Смотри аниме тут ☕",
+				url: "https://animi.club",
+			},
+		];
+	} else if (document.location.pathname.match(/\/(anime|manga)\//)) {
+		const title =
+			document
+				.querySelector("meta[property='og:title']")
+				?.getAttribute("content") ?? "Неизвестное название";
+
+		presenceData.details = `Смотрит страницу ${typeCurrent}`;
+
+		// Если нет приватности, добавляем состояние и кнопки
+		if (!privacy) {
+			presenceData.state = `«${title}»`;
+
+			presenceData.buttons = [
+				{
+					label: "Смотреть страницу",
+					url:
+						document
+							.querySelector("meta[property='og:url']")
+							?.getAttribute("content") || "",
+				},
+			];
+		}
+
+		// Если есть данные о видео, добавляем их в PresenceData
+		if (video.duration) {
+			presenceData.smallImageKey = video.paused ? Assets.Play : Assets.Pause;
+			presenceData.smallImageText = video.paused ? strings.play : strings.pause;
+
+			// Если включено отображение времени, добавляем таймстампы
+			if (time) {
+				if (video.paused) {
+					delete presenceData.startTimestamp;
+					delete presenceData.endTimestamp;
+				} else {
+					[presenceData.startTimestamp, presenceData.endTimestamp] =
+						presence.getTimestamps(video.currentTime, video.duration);
+				}
+			}
+		}
+	}
+
+	// Устанавливаем активность Presence
+	presence.setActivity(presenceData);
+});
