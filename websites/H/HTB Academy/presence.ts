c57d47f078bf27ce263a371cb5d085cee5737aa4
Aren Manukyan <aren.manukyan9@gmail.com>
2024-01-28 14:52:46 +0400

    fix(HTB Academy): fixed some things commented

--- websites/H/HTB Academy/presence.ts
+++ websites/H/HTB Academy/presence.ts
@@ -1,15 +1,10 @@
+/* eslint-disable no-one-time-vars/no-one-time-vars */
 const presence = new Presence({
 		clientId: "1200517025383075840",
 	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+	browsingTimestamp = Math.floor(Date.now() / 1000),
 
-let persred: HTMLElement,
-	module: HTMLElement,
-	persblue: HTMLElement,
-	persgrn: HTMLElement,
-	section: HTMLElement,
-	iterminal: HTMLElement,
-	module2: HTMLElement;
+ path = document.location.pathname;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
@@ -19,35 +14,36 @@ presence.on("UpdateData", async () => {
 	};
 
 	if (
-		document.location.pathname === "/" ||
-		document.location.pathname === "/login"
+		path === "/" ||
+		path === "/login"
 	)
 		presenceData.details = "Breaching into the Academy";
-	else if (document.location.pathname.includes("/dashboard")) {
+	else if (path.includes("/dashboard")) {
 		presenceData.details = "Browsing the dashboard";
-		persred = document.querySelector(
-			"#app > div:nth-child(2) > div.col-md-12.col-xl-8 > div.card.userProgress > div > div > div:nth-child(1) > div > div.red > div > div > div.percent > span"
-		);
-		persblue = document.querySelector(
-			"#app > div:nth-child(2) > div.col-md-12.col-xl-8 > div.card.userProgress > div > div > div:nth-child(2) > div > div.blue > div > div > div.percent > span"
-		);
-		persgrn = document.querySelector(
-			"#app > div:nth-child(2) > div.col-md-12.col-xl-8 > div.card.userProgress > div > div > div:nth-child(3) > div > div.green > div > div > div.percent > span"
+	const persred: HTMLElement = document.querySelector(
+			".red .progress"
+		),
+	 persblue: HTMLElement = document.querySelector(
+			".blue .progress"
+		),
+	 persgrn: HTMLElement = document.querySelector(
+			".green .progress"
 		);
 		presenceData.state =
-			`Off: ${persred.textContent}% ` +
-			`Def: ${persblue.textContent}% ` +
-			`Gen: ${persgrn.textContent}%`;
-	} else if (document.location.pathname.includes("/exams"))
+			`Off: ${persred.textContent} ` +
+			`Def: ${persblue.textContent} ` +
+			`Gen: ${persgrn.textContent}`;
+	} else if (path.includes("/exams"))
 		presenceData.details = "Browsing the exams";
-	else if (document.location.pathname.includes("/paths"))
+	else if (path.includes("/paths"))
 		presenceData.details = "Browsing paths";
-	else if (document.location.pathname.includes("/modules"))
+	else if (path.includes("/modules"))
 		presenceData.details = "Browsing modules";
-	else if (document.location.pathname.includes("/section")) {
-		iterminal = document.querySelector(".iterminal");
+	else if (path.includes("/section")) {
+		let module: HTMLElement;
+		const iterminal: HTMLElement = document.querySelector(".iterminal");
 		if (iterminal) {
-			module = document.querySelector(
+		 module = document.querySelector(
 				"#layout-wrapper > div.main-content > div > div:nth-child(2) > div > div > h4"
 			);
 		} else {
@@ -56,18 +52,18 @@ presence.on("UpdateData", async () => {
 			);
 		}
 
-		section = document.querySelector(".training-module h1");
-		presenceData.details = `Reading Module: ${module?.textContent}`;
+		const section: HTMLElement = document.querySelector(".training-module h1");
+		presenceData.details = `Reading Module: ${module.textContent}`;
 		presenceData.state = `Section: ${section?.textContent}`;
-	} else if (document.location.pathname.includes("/details")) {
-		module2 = document.querySelector(
-			"#app > div:nth-child(1) > div > div > h4"
+	} else if (path.includes("/details")) {
+		const module2: HTMLElement = document.querySelector(
+			".page-title-box .page-title"
 		);
 		presenceData.details = "Reading details about module:";
-		presenceData.state = module2.textContent;
-	} else if (document.location.pathname.includes("/my-certificates"))
+		presenceData.state = `"${module2.textContent}"`;
+	} else if (path.includes("/my-certificates"))
 		presenceData.details = "Looking at their certificates";
-	else if (document.location.pathname.includes("/my-badges"))
+	else if (path.includes("/my-badges"))
 		presenceData.details = "Looking at their badges";
 
 	presence.setActivity(presenceData);
