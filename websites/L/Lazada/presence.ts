e2cf54ce9504b2dc62a20e92d8b77366078d1f07
Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>
2024-01-31 01:35:56 +0800

    fix(Lazada): forgot to use prettier
    Signed-off-by: Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>

--- websites/L/Lazada/presence.ts
+++ websites/L/Lazada/presence.ts
@@ -7,59 +7,80 @@ let item: HTMLElement;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: 
+		largeImageKey:
 			"https://lzd-img-global.slatic.net/g/tps/tfs/TB1PApewFT7gK0jSZFpXXaTkpXa-200-200.png",
-		startTimestamp: browsingTimestamp,	
+		startTimestamp: browsingTimestamp,
 	};
 
-	if (document.querySelector(".pdp-mod-product-badge-title")) { // PRODUCT
+	if (document.querySelector(".pdp-mod-product-badge-title")) {
+		// PRODUCT
 		item = document.querySelector(".pdp-mod-product-badge-title");
 		presenceData.details = "Viewing Product:";
 		presenceData.state = item.textContent;
-		presenceData.largeImageKey = document.querySelector('meta[name="og:image"]').getAttribute("content") || "";
-	} else if (document.location.pathname.includes("/tag")) { // TAGS
+		presenceData.largeImageKey =
+			document.querySelector('meta[name="og:image"]').getAttribute("content") ||
+			"";
+	} else if (document.location.pathname.includes("/tag")) {
+		// TAGS
 		item = document.querySelector(".JrAyI");
 		presenceData.details = "Tags:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/catalog")) { // CATALOG
+	} else if (document.location.pathname.includes("/catalog")) {
+		// CATALOG
 		item = document.querySelector(".JrAyI");
 		presenceData.details = "Catalog:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/customer/order")) { // ORDER
+	} else if (document.location.pathname.includes("/customer/order")) {
+		// ORDER
 		presenceData.details = "My Orders";
-	} else if (document.location.pathname.includes("/user/login")) { // LOGIN
+	} else if (document.location.pathname.includes("/user/login")) {
+		// LOGIN
 		presenceData.details = "User:";
 		presenceData.state = "Login";
-	} else if (document.location.pathname.includes("/user/profile")) { // PROFILE
+	} else if (document.location.pathname.includes("/user/profile")) {
+		// PROFILE
 		item = document.querySelector(".my-profile-item-info");
 		presenceData.details = "My Profile:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/address")) { // ADDRESS
+	} else if (document.location.pathname.includes("/address")) {
+		// ADDRESS
 		presenceData.details = "Address Book";
-	} else if (document.location.pathname.includes("/cart")) { // CART
+	} else if (document.location.pathname.includes("/cart")) {
+		// CART
 		item = document.querySelector(".checkout-summary-label");
 		presenceData.details = "Cart:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/customer/cancellations/index")) { // CANCALLATIONS
+	} else if (
+		document.location.pathname.includes("/customer/cancellations/index")
+	) {
+		// CANCALLATIONS
 		presenceData.details = "My Cancellations";
-	} else if (document.location.pathname.includes("customer/cancellations/view")) { // CANCELLATION DETAILS
+	} else if (
+		document.location.pathname.includes("customer/cancellations/view")
+	) {
+		// CANCELLATION DETAILS
 		item = document.querySelector(".info-unit");
 		presenceData.details = "Cancellation Details:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/wallet")) { // WALLET
+	} else if (document.location.pathname.includes("/wallet")) {
+		// WALLET
 		presenceData.details = "Lazada Wallet";
-	} else if (document.location.pathname.includes("/customer/myReview")) { // CANCELLATION DETAILS
+	} else if (document.location.pathname.includes("/customer/myReview")) {
+		// CANCELLATION DETAILS
 		presenceData.details = "My Reviews";
-	} else if (document.location.pathname.includes("/wishlist")) { // WISHLIST
+	} else if (document.location.pathname.includes("/wishlist")) {
+		// WISHLIST
 		item = document.querySelector(".first ");
 		presenceData.details = item.textContent;
-	} else if (document.location.pathname.includes("/customer/returns")) { // RETURNS
+	} else if (document.location.pathname.includes("/customer/returns")) {
+		// RETURNS
 		presenceData.details = "My Returns";
-	} else (presenceData.details = "Home");
+	} else presenceData.details = "Home";
 
 	if (await presence.getSetting<boolean>("incognito")) {
 		presenceData.details = "Incognito";
-		presenceData.largeImageKey = "https://lzd-img-global.slatic.net/g/tps/tfs/TB1PApewFT7gK0jSZFpXXaTkpXa-200-200.png";
+		presenceData.largeImageKey =
+			"https://lzd-img-global.slatic.net/g/tps/tfs/TB1PApewFT7gK0jSZFpXXaTkpXa-200-200.png";
 		presenceData.state = null;
 		presenceData.startTimestamp = null;
 	}
