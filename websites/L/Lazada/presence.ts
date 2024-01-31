de581f2b4414413960a4e625535aeb00b9e558a4
Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>
2024-01-31 11:38:44 +0800

    Merge branch to main
    Signed-off-by: Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>

--- websites/L/Lazada/presence.ts
+++ websites/L/Lazada/presence.ts
@@ -3,86 +3,68 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-let item: HTMLElement;
-
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey:
-			"https://lzd-img-global.slatic.net/g/tps/tfs/TB1PApewFT7gK0jSZFpXXaTkpXa-200-200.png",
-		startTimestamp: browsingTimestamp,
-	};
+			largeImageKey: "https://i.ibb.co/LpxnMfj/512x512pngoflazada.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname } = document.location;
+
+	let item: HTMLElement;
 
 	if (document.querySelector(".pdp-mod-product-badge-title")) {
-		// PRODUCT
 		item = document.querySelector(".pdp-mod-product-badge-title");
 		presenceData.details = "Viewing Product:";
 		presenceData.state = item.textContent;
 		presenceData.largeImageKey =
 			document.querySelector('meta[name="og:image"]').getAttribute("content") ||
 			"";
-	} else if (document.location.pathname.includes("/tag")) {
-		// TAGS
+	} else if (pathname.includes("/tag")) {
 		item = document.querySelector(".JrAyI");
-		presenceData.details = "Tags:";
+		presenceData.details = "Viewing Tags:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/catalog")) {
-		// CATALOG
+	} else if (pathname.includes("/catalog")) {
 		item = document.querySelector(".JrAyI");
-		presenceData.details = "Catalog:";
+		presenceData.details = "Viewing Catalog:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/customer/order")) {
-		// ORDER
+	} else if (pathname.includes("/customer/order"))
 		presenceData.details = "My Orders";
-	} else if (document.location.pathname.includes("/user/login")) {
-		// LOGIN
+	else if (pathname.includes("/user/login")) {
 		presenceData.details = "User:";
 		presenceData.state = "Login";
-	} else if (document.location.pathname.includes("/user/profile")) {
-		// PROFILE
+	} else if (pathname.includes("/user/profile")) {
 		item = document.querySelector(".my-profile-item-info");
 		presenceData.details = "My Profile:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/address")) {
-		// ADDRESS
+	} else if (pathname.includes("/address"))
 		presenceData.details = "Address Book";
-	} else if (document.location.pathname.includes("/cart")) {
-		// CART
+	else if (pathname.includes("/cart")) {
 		item = document.querySelector(".checkout-summary-label");
 		presenceData.details = "Cart:";
 		presenceData.state = item.textContent;
-	} else if (
-		document.location.pathname.includes("/customer/cancellations/index")
-	) {
-		// CANCALLATIONS
+	} else if (pathname.includes("/customer/cancellations/index"))
 		presenceData.details = "My Cancellations";
-	} else if (
-		document.location.pathname.includes("customer/cancellations/view")
-	) {
-		// CANCELLATION DETAILS
+	else if (pathname.includes("customer/cancellations/view")) {
 		item = document.querySelector(".info-unit");
 		presenceData.details = "Cancellation Details:";
 		presenceData.state = item.textContent;
-	} else if (document.location.pathname.includes("/wallet")) {
-		// WALLET
+	} else if (pathname.includes("/wallet"))
 		presenceData.details = "Lazada Wallet";
-	} else if (document.location.pathname.includes("/customer/myReview")) {
-		// CANCELLATION DETAILS
+	else if (pathname.includes("/customer/myReview"))
 		presenceData.details = "My Reviews";
-	} else if (document.location.pathname.includes("/wishlist")) {
-		// WISHLIST
+	else if (pathname.includes("/wishlist")) {
 		item = document.querySelector(".first ");
 		presenceData.details = item.textContent;
-	} else if (document.location.pathname.includes("/customer/returns")) {
-		// RETURNS
+	} else if (pathname.includes("/customer/returns"))
 		presenceData.details = "My Returns";
-	} else presenceData.details = "Home";
+	else presenceData.details = "Home";
 
 	if (await presence.getSetting<boolean>("incognito")) {
 		presenceData.details = "Incognito";
 		presenceData.largeImageKey =
 			"https://lzd-img-global.slatic.net/g/tps/tfs/TB1PApewFT7gK0jSZFpXXaTkpXa-200-200.png";
-		presenceData.state = null;
-		presenceData.startTimestamp = null;
+		delete presenceData.state;
+		delete presenceData.startTimestamp;
 	}
 	if (!presenceData.details) presence.setActivity();
 	else presence.setActivity(presenceData);
