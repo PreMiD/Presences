7729f3c23de742982a8c3aecf4452c8f9b300647
Bas950 <me@bas950.com>
2024-02-03 19:21:26 +0100

    chore: update example

--- presences/Y/YouTube/src/watch.ts
+++ presences/Y/YouTube/src/watch.ts
@@ -2,9 +2,9 @@ import { AnyCallback, PresenceType, useDom } from "premid";
 
 import { YouTubeCallback } from "../config.js";
 
-export const watch: AnyCallback = (({ presence, useSettingsVisibility, config: { name } }) => {
+export const watch: AnyCallback = (({ presence, useSettingsVisibility, config: { name, logo } }) => {
 	//* Set default presence data
-	presence.set("name", name).set("type", PresenceType.Watching);
+	presence.set("name", name).set("largeImageKey", logo).set("type", PresenceType.Watching);
 
 	const { showTimestampVisibility } = useSettingsVisibility();
 	showTimestampVisibility.value = false;
