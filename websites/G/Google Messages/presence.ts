c878cbdc9961535db88d7c7f1e683f9df06cb9a1
theusaf <theusafyt@gmail.com>
2024-02-07 14:33:23 -0800

    refactor(Google Messages): use url assets

--- websites/G/Google Messages/presence.ts
+++ websites/G/Google Messages/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			smallImageKey: "google",
+			smallImageKey: "https://cdn.discordapp.com/app-assets/809898713996066827/810144767467716629.png?size=512",
 			smallImageText: "Google",
 			startTimestamp: tmb,
 		},
