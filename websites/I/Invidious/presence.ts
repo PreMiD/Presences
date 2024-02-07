37e0704ccbe9c27b7d3a11cb170b92b17547db80
theusaf <theusafyt@gmail.com>
2024-02-07 14:35:20 -0800

    refactor(Invidious): use url assets

--- websites/I/Invidious/presence.ts
+++ websites/I/Invidious/presence.ts
@@ -16,7 +16,7 @@ presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey:
 			"https://cdn.rcd.gg/PreMiD/websites/I/Invidious/assets/logo.png",
-		smallImageKey: "more",
+		smallImageKey: 'https://cdn.discordapp.com/app-assets/761617743593209869/761857793908932619.png?size=512',
 	};
 	let clear = false;
 
