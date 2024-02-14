fcd1b743d7c4ecca83162b0891b9b9e1c597570b
darkitxu84 <76633266+darkitxu84@users.noreply.github.com>
2024-02-14 18:33:58 -0300

    feat(BitView): added missing pages
        modified:   websites/B/BitView/metadata.json
    	modified:   websites/B/BitView/presence.ts

--- websites/B/BitView/presence.ts
+++ websites/B/BitView/presence.ts
@@ -4,16 +4,16 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets { // Other default assets can be found at index.d.ts
-	Logo = "https://cdn.discordapp.com/app-icons/899181356372860958/5393df9f35f992394c3e6a3c4b5166a0.png",
+	Logo = "https://i.imgur.com/x7XnGcX.png",
 }
 
-// Utils functions.
+// Utils functions and consts.
 const getChannelURL = () => {
 	return `https://${document.location.hostname}${document.querySelector("#watch-channel-stats > a").getAttribute("href")}`;
 },
 
 getChannelAvatar = () => {
-	return `https://${document.location.host}${document.querySelector("#main-channel-content > div > div > div > div > div > a > img").getAttribute("src")}`;
+	return `https://${document.location.host}${document.querySelector(".user-thumb-semismall > div > img").getAttribute("src")}`;
 },
 
 getChannelSuscribers = () => {
@@ -21,21 +21,34 @@ getChannelSuscribers = () => {
 },
 
 VIEW_PAGES = new Map<string, string>([
-	["playlists", "Viewing the playlists"],
-	["videos", "Viewing uploaded videos"],
-	["subscriptions", "Viewing subscriptions videos"],
-	["favorites", "Viewing favorite videos"],
-	["quicklist", "Viewing the quicklist"],
-	["account", "Viewing account settings"],
-	["history", "Viewing the history"]
+	["playlists", "Viewing their playlists"],
+	["videos", "Viewing their uploaded videos"],
+	["subscriptions", "Viewing their subscriptions videos"],
+	["favorites", "Viewing their favorite videos"],
+	["quicklist", "Viewing their quicklist"],
+	["account", "Viewing their account settings"],
+	["history", "Viewing their history"],
+	["groups", "Viewing their joined groups"],
+	["channel", "Viewing a channel"],
+	["inbox", "Viewing their inbox"],
+	["guidelines", "Viewing the community guidelines"],
+	["terms", "Viewing the terms of use"],
+	["privacy", "Viewing the privacy policy"]
 ]);
 
 presence.on("UpdateData", async () => {
+	// Functions and consts.
 	const path: string = document.location.pathname,
 
 		  presenceData: PresenceData = {
 			name: "Bitview",
 			largeImageKey: Assets.Logo,
+		  },
+		
+		  baseViewingPresence = () => {
+			presenceData.smallImageKey = Assets.Viewing;
+			presenceData.smallImageText = "Viewing";
+			presenceData.startTimestamp = browsingTimestamp;
 		  },
 
 		  baseSearchPresence = () => {
@@ -46,15 +59,16 @@ presence.on("UpdateData", async () => {
 
 		  baseBrowsing = (browsingIn: string) => {
 			baseSearchPresence();
-			presenceData.details = `Browsing in ${browsingIn} list`;
+			presenceData.details = "Browsing through";
+			presenceData.state = `${browsingIn}`;
 		  };
 	
-	if (path === "/") { // MAIN PAGE
+	if (path === "/") { // Home Page
 		presenceData.details = "Viewing home page";
 		presenceData.startTimestamp = browsingTimestamp;
 	}
 
-	if (path.includes("watch") && document.querySelector("video")) { // VIDEO
+	if (path.includes("watch") && document.querySelector("video")) { //  Watching a video
 		const video = document.querySelector("video");
 
 		presenceData.details = document.querySelector("#watch-vid-title").children[1]; // Video name
@@ -79,19 +93,20 @@ presence.on("UpdateData", async () => {
 		} else {
 			const isLooping = document.querySelector("#watch-player-div")
 							 .children[4]
-							 .textContent
+							 .getAttribute("class")
 							 .includes("loop"); // video.loop doesn't work.
 
 			presenceData.smallImageKey = isLooping
 			? Assets.Repeat
 			: Assets.Play;
+
 			presenceData.smallImageText = isLooping
 			? "On loop"
 			: "Playing";
 		}
 
-	} else if (path.includes("user")) { // CHANNEL
-		presenceData.details = "Viewing a channel";
+	} else if (path.includes("user")) { // Viewing a Channel.
+		presenceData.details = VIEW_PAGES.get("channel");
 		presenceData.state = document.querySelector("#main-channel-left > div")
 							.children[1]
 							.querySelector("div"); // Channel name
@@ -102,25 +117,37 @@ presence.on("UpdateData", async () => {
 		presenceData.buttons = [ { label: "View Channel", url: document.location.href} ];
 		presenceData.startTimestamp = browsingTimestamp;
 
-	} else if (path.includes("results")) { // SEARCH
+	} else if (path.includes("results")) { // Searching.
 		presenceData.details = "Searching for:";
-		presenceData.state = Object.fromEntries(new URLSearchParams(document.location.search).entries()).search;
+		presenceData.state = Object.fromEntries(new URLSearchParams(document.location.search).entries()).search; // Query String "search"
 		baseSearchPresence();
  
-	} else if (path.includes("my") || path.includes("viewing")) {
-		presenceData.smallImageKey = Assets.Viewing;
-		presenceData.smallImageText = "Viewing";
-		presenceData.startTimestamp = browsingTimestamp;
+	} else if (path.includes("my") || path.includes("viewing")) { // my_videos, my_subscriptons... and viewing_historial
+		baseViewingPresence();
 		presenceData.details = VIEW_PAGES.get(path.split("_")[1]) || "Viewing a page";
 
+	} else if (path.includes("inbox")) {
+		baseViewingPresence();
+		presenceData.details = VIEW_PAGES.get("inbox");
+
+	} else if ( ["guidelines", "terms", "privacy"].includes(path.slice(1))) {
+		baseViewingPresence();
+		presenceData.details = VIEW_PAGES.get(path.slice(1));
+
 	} else if (path.includes("browse"))
-		baseBrowsing("videos");
+		baseBrowsing("all videos list");
 
 	else if (path.includes("groups"))
-		baseBrowsing("groups");
+		baseBrowsing("all groups list");
 
 	 else if (path.includes("channels")) 
-		baseBrowsing("channels");
+		baseBrowsing("all channels list");
+
+	else if (path.includes("partners"))
+		baseBrowsing("partners list");
+
+	else if (path.includes("blog"))
+		baseBrowsing("the blog");
 
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
