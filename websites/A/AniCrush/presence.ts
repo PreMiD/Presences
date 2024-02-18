1d1e92b8c22dd190c9352ee6e89b5a6e4c9f7552
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2024-02-18 13:34:15 +0530

    fix(AniCrush): move logo url to imgur

--- websites/A/AniCrush/presence.ts
+++ websites/A/AniCrush/presence.ts
@@ -2,7 +2,7 @@ const presence = new Presence({
 	clientId: "1208440916461887488",
 });
 const enum Assets {
-	Logo = "https://cdn.discordapp.com/attachments/1188145024492380174/1208641322811138148/QYco5JN.png",
+	Logo = "https://i.imgur.com/cwFUt40.png",
 }
 let video = {
 	exists: false,
@@ -25,7 +25,7 @@ presence.on(
 presence.on("UpdateData", async () => {
 	let presenceData: PresenceData = {
 		largeImageKey:
-			"https://cdn.discordapp.com/attachments/1188145024492380174/1208641322811138148/QYco5JN.png",
+			Assets.Logo,
 	};
 	const { pathname, href, search } = document.location,
 		showButtons = await presence.getSetting<boolean>("buttons"),
