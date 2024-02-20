5094c88635fcf9ed586739d20d71e6cbfe859d75
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2024-02-20 13:48:03 +0530

    style(AniCrush): format presence.ts

--- websites/A/AniCrush/presence.ts
+++ websites/A/AniCrush/presence.ts
@@ -27,11 +27,11 @@ presence.on("UpdateData", async () => {
 		largeImageKey: Assets.Logo,
 	};
 	const { pathname, href, search } = document.location,
-	[showButtons, showW2Button, showTimestamps] = await Promise.all([
-		presence.getSetting<boolean>("buttons"),
-		presence.getSetting<boolean>("watch2getherJoinRoomButton"),
-		presence.getSetting<boolean>("timestamps")
-	]);
+		[showButtons, showW2Button, showTimestamps] = await Promise.all([
+			presence.getSetting<boolean>("buttons"),
+			presence.getSetting<boolean>("watch2getherJoinRoomButton"),
+			presence.getSetting<boolean>("timestamps"),
+		]);
 	switch (true) {
 		case pathname.startsWith("/watch/"): {
 			const streamingType = Array.from(
