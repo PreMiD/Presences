2af0cbb1f27f864f6b51aec4fe3def5853e17e63
Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
2024-02-20 20:05:02 +0100

    feat(Amazon Kindle): add presence (#8077)
    * feat(Amazon Kindle): add presence
    
    * chore(Amazon Kindle): formatting
    
    * feat(Amazon Kindle): fix icon
    
    * chore(Amazon Kindle): formatting
    
    * Update websites/A/Amazon Kindle/metadata.json
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
    
    * Update presence.ts
    
    Signed-off-by: Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sarah Engel <45071533+PrincessAkira@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

---
+++ websites/A/Amazon Kindle/presence.ts
@@ -0,0 +1,31 @@
+const presence = new Presence({
+		clientId: "1208798725116395592",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+			details: "Amazon Kindle Home",
+			largeImageKey: "https://i.imgur.com/LnSgbsr.png",
+			startTimestamp: browsingTimestamp,
+		},
+		{ pathname } = document.location;
+	switch (pathname) {
+		case "/": {
+			presenceData.details = document.querySelector(
+				"#top-menu-bar > ion-toolbar > ion-title"
+			).textContent;
+			presenceData.state = document
+				.querySelector(
+					"#main-content > div:nth-child(7) > ion-footer > ion-toolbar > ion-title > div"
+				)
+				.textContent.split("●")[0]
+				.replace("Page", " ")
+				.replace("of", "/")
+				.replace("Location", " ")
+				.trim();
+		}
+	}
+
+	presence.setActivity(presenceData);
+});
