8f122f6b764a402fa92fda38b357fb0bf7a8137d
theusaf <theusafyt@gmail.com>
2024-02-21 13:03:53 -0800

    refactor: update static pages

--- websites/F/Font Awesome/presence.ts
+++ websites/F/Font Awesome/presence.ts
@@ -1,81 +1,50 @@
 const presence = new Presence({
 		clientId: "820023496934817804",
 	}),
-	pBrowsing = Math.floor(Date.now() / 1000);
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			largeImageKey:
 				"https://cdn.rcd.gg/PreMiD/websites/F/Font%20Awesome/assets/logo.png",
-			startTimestamp: pBrowsing,
+			startTimestamp: browsingTimestamp,
+			type: ActivityType.Playing,
+			name: "Font Awesome",
 		},
-		pPage = window.location.pathname;
+		{ pathname, search, href } = document.location,
+		pathList = pathname.split("/").filter(Boolean);
 
-	if (pPage === "/") {
-		presenceData.details = "Browsing Page:";
-		presenceData.state = "Main";
-	} else if (pPage.includes("/icons")) {
-		const myParam: string = new URLSearchParams(window.location.search).get(
-			"q"
-		);
-
-		if (myParam === null) {
-			const icon: HTMLElement = document.querySelector(
-				"#page-top > div.view.flex.flex-column.min-vh-100.db-pr > div.flex-grow-1.flex-shrink-0.flex-basis-auto > div > div.ph6-l > div > section > header > div.flex.flex-column.flex-row-xl.items-center-xl.justify-between-xl.mb2.mb4-l > h1 > span"
-			);
-			if (icon === null) {
-				presenceData.details = "Browsing Page:";
-				presenceData.state = "Icons";
-			} else {
-				presenceData.details = "Viewing Icon:";
-				presenceData.state = icon.textContent;
-				presenceData.buttons = [
-					{
-						label: "View Icon",
-						url: document.URL,
-					},
-				];
-			}
-		} else {
-			presenceData.details = "Searching:";
-			presenceData.state = myParam;
+	switch (pathList[0] ?? "/") {
+		case "/": {
+			presenceData.details = "Browsing Home Page";
+			break;
 		}
-	} else {
-		switch (pPage) {
-			case "/start": {
-				presenceData.details = "Browsing Page:";
-				presenceData.state = "Start";
-
-				break;
+		case "icons": {
+			break;
+		}
+		case "start": {
+			presenceData.details = "Getting Started";
+			break;
+		}
+		case "support": {
+			presenceData.details = "Browsing Support";
+			break;
+		}
+		case "plans": {
+			if (pathList[1]) {
+				presenceData.details = "Viewing Plan";
+				presenceData.state = document.querySelector("h3");
+			} else {
+				presenceData.details = "Browsing Plans";
 			}
-			case "/support": {
-				presenceData.details = "Browsing Page:";
-				presenceData.state = "Support";
-
-				break;
-			}
-			case "/plans": {
-				presenceData.details = "Browsing Page:";
-				presenceData.state = "Plans";
-
-				break;
-			}
-			case "/plans/standard": {
-				presenceData.details = "Browsing Page:";
-				presenceData.state = "Plan Standard";
-
-				break;
-			}
-			case "/sessions/sign-in": {
-				presenceData.details = "Browsing Page:";
-				presenceData.state = "Sign In";
-
-				break;
-			}
-			// No default
+			break;
+		}
+		case "sessions": {
+			presenceData.details = "Signing In";
+			break;
 		}
 	}
 
-	if (!presenceData.details) presence.setActivity();
+	if (!presenceData.details) presence.clearActivity();
 	else presence.setActivity(presenceData);
 });
