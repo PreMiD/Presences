ec99bb63b925387d4a9931b43dac07c9165f9787
Dongwan Seo <kr.ihah@kakao.com>
2024-02-21 23:58:29 +0900

    feat(Sorryfield): add small images, new path
    Signed-off-by: Dongwan Seo <kr.ihah@kakao.com>

--- websites/S/Sorryfield/presence.ts
+++ websites/S/Sorryfield/presence.ts
@@ -3,35 +3,27 @@ const presence = new Presence({
 });
 
 const enum Assets {
-	LOGO = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/logo.png",
-	CHART_SEARCH = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/0.jpg",
-	CHART_HYPORANKED = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/1.jpg",
-	CHART_EXPONENTIAL = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/2.jpg",
-	CHART_HARD = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/3.jpg",
-	CHART_INTERMEDIATE = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/4.jpg",
-	CHART_EASY = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/5.jpg",
-	CHART_NEW = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/6.jpg",
-	CHART_HISTORY = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/7.jpg",
-	CHART_POSSESSION = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/8.jpg",
-	CHART_HOT = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/9.jpg",
-	CHART_COLD = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/10.jpg",
-	CHART_MULTIPLAYER = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/11.jpg",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/logo.png",
+	ChartSearch = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/0.jpg",
+	ChartHypoRanked = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/1.jpg",
+	ChartExponential = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/2.jpg",
+	ChartHard = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/3.jpg",
+	ChartIntermediate = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/4.jpg",
+	ChartEasy = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/5.jpg",
+	ChartNew = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/6.jpg",
+	ChartHistory = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/7.jpg",
+	ChartPossession = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/8.jpg",
+	ChartHot = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/9.jpg",
+	ChartCold = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/10.jpg",
+	ChartMultiPlayer = "https://cdn.rcd.gg/PreMiD/websites/S/Sorryfield/assets/11.jpg",
 }
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			details: "Sorryfield",
-			smallImageKey: Assets.LOGO,
+			largeImageKey: Assets.Logo,
 		},
 		{ pathname, href } = document.location;
-	if (
-		pathname === "/" ||
-		pathname === "/sorrygle" ||
-		pathname.startsWith("/song/")
-	) {
-		presenceData.largeImageKey = Assets.LOGO;
-		delete presenceData.smallImageKey;
-	}
 	if (pathname === "/") {
 		presenceData.details = "곡 선택 중";
 		presenceData.state = document.querySelector(".menu>.desc").textContent;
@@ -50,20 +42,31 @@ presence.on("UpdateData", async () => {
 			} else presenceData.details = "노래 추가 중";
 		} else {
 			presenceData.details = "노래 듣는 중";
+			presenceData.type = ActivityType.Listening;
 			presenceData.state = `${
 				document.querySelector(".song").children[2].childNodes[0].textContent
 			} - ${document.querySelector(".title").textContent.trim()}`;
+			presenceData.buttons = [{ label: "듣기", url: href }];
+			const playing = document
+				.querySelector<HTMLElement>(".control>button>i")
+				.classList.contains("fa-pause");
+			presenceData.smallImageKey = playing ? Assets.Play : Assets.Pause;
+			presenceData.smallImageText = playing ? "재생 중" : "일시 정지";
 			const duration = document
 				.querySelector("time>.desc")
 				?.textContent.split(":");
-			if (duration) {
-				presenceData.endTimestamp =
+			if (duration && playing) {
+				const nowTime =
 					Math.floor(Date.now() / 1000) +
 					(parseInt(duration[0].replace("(-", "")) * 60 +
 						parseInt(duration[1].replace(")", ""))) +
 					1;
+				presenceData.endTimestamp = nowTime;
+				if (nowTime <= Math.floor(Date.now() / 1000)) {
+					delete presenceData.endTimestamp;
+					presenceData.smallImageKey = Assets.Stop;
+				}
 			} else delete presenceData.endTimestamp;
-			presenceData.buttons = [{ label: "듣기", url: href }];
 		}
 	}
 	if (pathname === "/playlist") presenceData.details = "재생 목록 확인 중";
@@ -88,17 +91,28 @@ presence.on("UpdateData", async () => {
 				document.querySelector(".current").children[0].children[7].childNodes[0]
 					.textContent
 			}`;
+			presenceData.buttons = [{ label: "듣기", url: href }];
+			presenceData.type = ActivityType.Listening;
+			const playing = document
+				.querySelector<HTMLElement>(".control>button>i")
+				.classList.contains("fa-pause");
+			presenceData.smallImageKey = playing ? Assets.Play : Assets.Pause;
+			presenceData.smallImageText = playing ? "재생 중" : "일시 정지";
 			const duration = document
 				.querySelector("time>.desc")
 				?.textContent.split(":");
-			if (duration) {
-				presenceData.endTimestamp =
+			if (duration && playing) {
+				const nowTime =
 					Math.floor(Date.now() / 1000) +
 					(parseInt(duration[0].replace("(-", "")) * 60 +
 						parseInt(duration[1].replace(")", ""))) +
 					1;
+				presenceData.endTimestamp = nowTime;
+				if (nowTime <= Math.floor(Date.now() / 1000)) {
+					delete presenceData.endTimestamp;
+					presenceData.smallImageKey = Assets.Stop;
+				}
 			} else delete presenceData.endTimestamp;
-			presenceData.buttons = [{ label: "듣기", url: href }];
 		}
 	}
 	if (pathname === "/karaoke") presenceData.details = "노래방: 로비";
@@ -112,7 +126,7 @@ presence.on("UpdateData", async () => {
 	}
 	if (pathname.startsWith("/java")) {
 		presenceData.details = "자바!";
-		presenceData.largeImageKey = Assets.LOGO;
+		presenceData.largeImageKey = Assets.Logo;
 		delete presenceData.smallImageKey;
 		if (pathname === "/java" && !href.includes("?")) {
 			presenceData.details = "자바! 싱글플레이어";
@@ -123,57 +137,57 @@ presence.on("UpdateData", async () => {
 				.toUpperCase();
 			let imageKey = "",
 				menuName = "";
-			presenceData.smallImageKey = Assets.LOGO;
+			presenceData.smallImageKey = Assets.Logo;
 
 			switch (image) {
 				case "SEARCH":
-					imageKey = Assets.CHART_SEARCH;
+					imageKey = Assets.ChartSearch;
 					presenceData.state = `채보 검색 중: ${
 						document.querySelector<HTMLInputElement>("#search").value
 					}`;
 					break;
 				case "HYPORANKED":
-					imageKey = Assets.CHART_HYPORANKED;
+					imageKey = Assets.ChartHypoRanked;
 					menuName = "내 순위가 아래인 채보";
 					break;
 				case "EXPONENTIAL":
-					imageKey = Assets.CHART_EXPONENTIAL;
+					imageKey = Assets.ChartExponential;
 					menuName = "경지에 다다른 채보";
 					break;
 				case "HARD":
-					imageKey = Assets.CHART_HARD;
+					imageKey = Assets.ChartHard;
 					menuName = "어려운 채보";
 					break;
 				case "INTERMEDIATE":
-					imageKey = Assets.CHART_INTERMEDIATE;
+					imageKey = Assets.ChartIntermediate;
 					menuName = "적당한 채보";
 					break;
 				case "EASY":
-					imageKey = Assets.CHART_EASY;
+					imageKey = Assets.ChartEasy;
 					menuName = "쉬운 채보";
 					break;
 				case "NEW":
-					imageKey = Assets.CHART_NEW;
+					imageKey = Assets.ChartNew;
 					menuName = "신상 채보";
 					break;
 				case "HISTORY":
-					imageKey = Assets.CHART_HISTORY;
+					imageKey = Assets.ChartHistory;
 					menuName = "내가 최근 완주한 채보";
 					break;
 				case "POSSESSION":
-					imageKey = Assets.CHART_POSSESSION;
+					imageKey = Assets.ChartPossession;
 					menuName = "내가 소장한 곡의 채보";
 					break;
 				case "HOT":
-					imageKey = Assets.CHART_HOT;
+					imageKey = Assets.ChartHot;
 					menuName = "요즘 북적이는 채보";
 					break;
 				case "COLD":
-					imageKey = Assets.CHART_COLD;
+					imageKey = Assets.ChartCold;
 					menuName = "요즘 안 북적이는 채보";
 					break;
 				case "MULTIPLAYER":
-					imageKey = Assets.CHART_MULTIPLAYER;
+					imageKey = Assets.ChartMultiPlayer;
 					presenceData.details = "자바! 멀티플레이어";
 					presenceData.state = "방 선택 중";
 					break;
@@ -190,11 +204,32 @@ presence.on("UpdateData", async () => {
 			presenceData.state = document
 				.querySelector("title")
 				.textContent.replace(" - 자바! - 쏘리들", "");
+		} else if (pathname.includes("/record/")) {
+			presenceData.details = `${
+				document.querySelector<HTMLElement>(".header-profile>.left>a")
+					.textContent
+			}님의 리플레이: ${`${
+				document
+					.querySelector<HTMLElement>(".header-profile>.right")
+					.textContent.split("%")[0]
+			}%`}`;
+			presenceData.state = `Lv.${
+				document.querySelector<HTMLDivElement>(".chart-header>.level")
+					.textContent
+			} / ${document
+				.querySelector<HTMLTitleElement>("title")
+				.textContent.split(" by ")[0]
+				.slice(2)} ${
+				document.querySelector<HTMLDivElement>(".chart-header>.tail>div")
+					.textContent
+			}`;
+			presenceData.type = ActivityType.Watching;
+			presenceData.buttons = [{ label: "리플레이 보기", url: href }];
 		} else if (pathname.includes("/shop")) presenceData.state = "상점";
 		else if (pathname.includes("/sync")) presenceData.state = "환경 설정";
 		else if (pathname === "/java/multiplayer") {
 			presenceData.details = "자바! 멀티플레이어";
-			presenceData.largeImageKey = Assets.CHART_MULTIPLAYER;
+			presenceData.largeImageKey = Assets.ChartMultiPlayer;
 			if (document.querySelector(".room-header")) {
 				presenceData.state = `${
 					document.querySelector(".room-header").children[0].childNodes[1]
