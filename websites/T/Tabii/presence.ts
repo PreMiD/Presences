0e7c13088522bea51460735e1375f8d5e1e7fe71
Süleyman <128982325+magoa-bot@users.noreply.github.com>
2024-02-27 18:23:52 +0300

    fix(tabii.com): edit presence

--- websites/T/Tabii/presence.ts
+++ websites/T/Tabii/presence.ts
@@ -4,7 +4,8 @@ const presence = new Presence({
 strings = presence.getStrings({
 	play: "general.playing",
 	pause: "general.paused",
-})
+	view: "general.watching"
+});
 
 const enum Assets { // Other default assets can be found at index.d.ts
 	Logo = "https://cms-tabii-public-image.tabii.com/int/webp/26087.jpeg",
@@ -18,29 +19,26 @@ presence.on("UpdateData", async () => {
 	};
 
 
-	if (document.location.pathname == "/" || document.location.pathname.includes("/browse/")) {
+	if (document.location.pathname === "/" || document.location.pathname.includes("/browse/")) {
 		
-		let pageIds = [
+		const pageIds = [
 			{ id: "154830_134666", name: "Anasayfa" },
 			{ id: "149106_149112", name: "Dizi" },
 			{ id: "149265_149266", name: "Film" },
 			{ id: "149301_149418", name: "Belgesel" },
 			{ id: "149332_149333", name: "Çocuk" },
 			{ id: "233080_242470", name: "Tekrar İzle" },
-		]
-
-		let pageName = pageIds.find((x) => document.location.pathname.includes(x.id))?.name;
-
-		presenceData.details = pageName + " Sayfasına Göz Atıyor";
+		];
 		
-	}
-	else if (document.location.pathname.includes("/settings/account")) presenceData.details = "Hesap Ayarlarına Göz Atıyor";
+
+		presenceData.details = `${pageIds.find((x) => document.location.pathname.includes(x.id))?.name} Sayfasına Göz Atıyor`;
+		
+	} else if (document.location.pathname.includes("/settings/account")) presenceData.details = "Hesap Ayarlarına Göz Atıyor";
 	else if (document.location.pathname.includes("/settings/profile")) presenceData.details = "Profil Ayarlarına Göz Atıyor";
 	else if (document.location.pathname.includes("/search")) {
 		if (document.querySelector("input").value) presenceData.details = "Arama Yapıyor";
 		else presenceData.details = "Arama Sayfasına Göz Atıyor";
-	}
-	else if (document.location.pathname.includes("/watch")) {
+	} else if (document.location.pathname.includes("/watch")) {
 		
 		const video = document.querySelector("video");
 
@@ -50,7 +48,7 @@ presence.on("UpdateData", async () => {
 			const stringsData = await strings;
 			presenceData.buttons = [
 				{
-					label: "İzle",
+					label: `${stringsData.view} ${presenceData.state}`,
 					url: document.location.href,
 				},
 			];
@@ -67,13 +65,11 @@ presence.on("UpdateData", async () => {
 			presenceData.smallImageText = video.paused ? stringsData.pause : stringsData.play;
 
 		}
-	}
-	else if (document.location.pathname.includes("/genre")) {
-		let catName = (document.querySelector("#Main > div > div:nth-child(1) > main > div > div.flex.pt-8 > div > div > span:nth-child(2)") as HTMLElement).innerText;
-		if (catName) presenceData.details = catName + " Kategorisine Göz Atıyor";
+	} else if (document.location.pathname.includes("/genre")) {
+		const catName = (document.querySelector("#Main > div > div:nth-child(1) > main > div > div.flex.pt-8 > div > div > span:nth-child(2)") as HTMLElement).textContent;
+		if (catName) presenceData.details = `${catName} Kategorisine Göz Atıyor`;
 		else presenceData.details = "Kategori Sayfasına Göz Atıyor";
-	}
-	else if (document.location.pathname.includes("/my-stuff")) presenceData.details = "Kütüphanesine Göz Atıyor";
+	} else if (document.location.pathname.includes("/my-stuff")) presenceData.details = "Kütüphanesine Göz Atıyor";
 	else if (document.location.pathname.includes("/faq")) presenceData.details = "SSS'ye Göz Atıyor";
 
 	else presenceData.details = "Bilinmeyen Sayfada";
