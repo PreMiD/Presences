13a8234237b8012a67cbe67b97cfdb2490e55531
angelolz <angel.lugia98@gmail.com>
2024-02-29 03:50:18 -0700

    feat(Nebula): added video/podcast/class support with timestamps

--- websites/N/Nebula/presence.ts
+++ websites/N/Nebula/presence.ts
@@ -14,62 +14,160 @@ const enum Assets { // Other default assets can be found at index.d.ts
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey: Assets.Logo,
-		startTimestamp: browsingTimestamp,
+		startTimestamp: browsingTimestamp
 	},
 	{ pathname } = document.location,
 	path = pathname.split("/");
 	path.shift(); 
 	if (pathname.endsWith("/")) path.pop();
 
-	presenceData.details = getPageDetails(path); 
+	getDetails(presenceData, path);
 
 	presence.setActivity(presenceData);
 });
 
-function getPageDetails(path: string[]): string {
-	let val = "";
-	if(path.length === 0)
-		val = "Viewing home page";
+function getDetails(presenceData: PresenceData, path: string[]): void {
+	// console.log(path);
+	// console.log(path.length);
+	if(path.length === 0) {
+		presenceData.details = "Viewing home page";
+		return;
+	}
 
 	switch(path[0].toLowerCase()) {
 		case "library":
-			val = "Viewing library page";
+			if(path.length === 1)
+				presenceData.details = "Viewing library page";
+			else
+				presenceData.details = getLibraryCategory(path[1].toLowerCase());
 			break;
 		case "explore":
-			val = "Viewing explore page";
+			if(path.length === 1)
+				presenceData.details = "Viewing explore page";
+			else
+				presenceData.details = getExploreCategory(path[1].toLowerCase());
 			break;
 		case "classes":
-			val = "Viewing classes page";
+			presenceData.details = "Viewing classes page";
 			break;
-
 		case "settings":
-			val = "Viewing account settings";
+			presenceData.details = "Viewing account settings";
 			break;
-
 		case "faq":
-			val = "Frequently Asked Questions";
+			presenceData.details = "Viewing Frequently Asked Questions";
 			break;
-
 		case "terms":
-			val = "Viewing Terms of Service";
+			presenceData.details = "Viewing Terms of Service";
 			break;
-
 		case "privacy":
-			val = "Viewing Privacy Policy";
+			presenceData.details = "Viewing Privacy Policy";
 			break;
-
 		case "beta":
-			val = "Viewing beta apps page";
+			presenceData.details = "Viewing beta apps page";
 			break;
-
 		case "videos":
-			val = "Viewing a video"; //todo
+			getVideoDetails(presenceData);
 			break;
+		default: //todo make it work for podcasts and classes
+			getOtherDetails(presenceData);
+			break;
+	}
+}
 
-		default:
-			val = "Listening to a podcast"; // i think?
+function getExploreCategory(category: string): string {
+	switch(category) {
+		case "videos":
+			return "Exploring videos";
+		case "channels":
+			return "Exploring channels";
+		case "podcasts":
+			return "Exploring podcasts";
+		case "episodes":
+			return "Exploring episodes";
+	}
+}
+
+function getLibraryCategory(category: string): string {
+	switch(category) {
+		case "latest-videos":
+			return "Viewing latest videos";
+		case "followed-channels":
+			return "Viewing followed channels";
+		case "watch-later":
+			return "Viewing Watch Later";
+		case "watch-history":
+			return "Viewing watch history";
+		case "latest-episodes":
+			return "Viewing latest episodes";
+		case "followed-shows":
+			return "Viewing followed shows";
+		case "saved-episodes":
+			return "Viewing saved episodes";
+		case "listen-history":
+			return "Viewing listening history";
+		case "classes-in-progress":
+			return "Viewing classes in progress";
+		case "saved-classes":
+			return "Viewing saved classes";
+		case "lesson-history":
+			return "Viewing lesson history";
+	}
+}
+
+function getVideoDetails(presenceData: PresenceData): void {
+	const videoElement = document.querySelector("video");
+
+	presenceData.details = document.querySelector(".css-11wyb0j").innerHTML;
+	presenceData.state = document.querySelector(".css-r06ha9").innerHTML;
+
+	if(videoElement === null) return;
+	setTimestamps(videoElement, presenceData);
+}
+
+function getOtherDetails(presenceData: PresenceData): void {
+	const videoElement = document.querySelector("video"),
+	audioElement = document.querySelector("audio");
+
+	if(videoElement === null && audioElement === null) { // viewing a channel?
+		const channelName = document.querySelector(".css-dv828b"),
+		podcastName = document.querySelector(".css-xdl2kn");
+
+		if(channelName === null && podcastName === null)
+			return;
+	
+		if(channelName === null) {
+			presenceData.details = "Viewing a podcast";
+			presenceData.state = podcastName.innerHTML;
+		} else {
+			presenceData.details = "Viewing a channel";
+			presenceData.state = channelName.innerHTML;
+		}
+	} else if(videoElement === null) { //its a podcast
+		presenceData.details = document.querySelector(".css-l7qxoj").innerHTML;
+		presenceData.state = document.querySelector(".css-2m8aus").innerHTML;
+		setTimestamps(audioElement, presenceData);
+	} else { //its an episode
+		const episodeName = document.querySelector(".css-13igzay"),
+		className = document.querySelector(".css-1ls4t7r");
+
+		if(episodeName !== null) 
+			presenceData.details = `${episodeName.innerHTML} | ${className.innerHTML}`;
+		else
+			presenceData.details = className.innerHTML;
+
+		presenceData.state = document.querySelector(".css-p7br9k").innerHTML;
+		setTimestamps(videoElement, presenceData);
 	}
 
-	console.log("val: ", val);
-	return val;
+}
+
+function setTimestamps(element: HTMLAudioElement | HTMLVideoElement, presenceData: PresenceData): void {
+	delete presenceData.startTimestamp;
+	presenceData.endTimestamp = presence.getTimestampsfromMedia(element)[1];
+	if (element.paused) {
+		delete presenceData.endTimestamp;
+		presenceData.smallImageKey = Assets.Pause;
+	} else 
+		presenceData.smallImageKey = Assets.Play;
+	
 }
