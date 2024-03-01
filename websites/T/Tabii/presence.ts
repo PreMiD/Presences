6a75f71afa3329df60fc0ff6fd6ead20bbc56094
gerigel <148766079+gerigel@users.noreply.github.com>
2024-03-01 03:19:10 +0300

    style(tabii.com): req fix
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: gerigel <148766079+gerigel@users.noreply.github.com>

--- websites/T/Tabii/presence.ts
+++ websites/T/Tabii/presence.ts
@@ -8,9 +8,7 @@ const presence = new Presence({
 	});
 
 const enum Assets {
-	Logo = "https://cms-tabii-public-image.tabii.com/int/webp/26087.jpeg",
-	pause = "pause",
-	play = "play",
+	Logo = "https://i.imgur.com/ZfDhD5n.png",
 }
 
 presence.on("UpdateData", async () => {
