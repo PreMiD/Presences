d8b13a0ec60ff35bba52973aca5698d84daac87b
kitten99 <141530947+kitten99@users.noreply.github.com>
2024-03-02 22:15:09 -0600

    Update presence.ts
    Signed-off-by: kitten99 <141530947+kitten99@users.noreply.github.com>

--- websites/R/Rate Your Music/presence.ts
+++ websites/R/Rate Your Music/presence.ts
@@ -1,301 +1,289 @@
-const presence = new Presence({
-    clientId: "1213365863470866462",
-});
-
 const enum Assets {
-    Logo = "https://i.imgur.com/4hrNGBS.png",
+	Logo = "https://i.imgur.com/4hrNGBS.png",
 }
 
 let browsingTimestamp: number;
 
 function updatePresence() {
-    if (!browsingTimestamp) {
-        browsingTimestamp = Math.floor(Date.now() / 1000);
-    }
-    
-    let presenceData: PresenceData = {
-        largeImageKey: Assets.Logo,
-        startTimestamp: browsingTimestamp
-    };
+	if (!browsingTimestamp) browsingTimestamp = Math.floor(Date.now() / 1000);
 
-    const pages: Record<string, PresenceData> = {
-        "/charts/": {
-            details: "Viewing Chart:",
-        },
-        "/new-music/": {
-            details: "New Releases",
-        },
-        "/genres/": {
-            details: "Genres",
-        },
-        "/lists/": {
-            details: "Lists",
-        },
-        "/subscribe/": {
-            details: "Subscribe",
-        },
-        "/": {
-            details: "Home",
-        },
-    };
+	let presenceData: PresenceData = {
+		largeImageKey: Assets.Logo,
+		startTimestamp: browsingTimestamp,
+	};
 
-    //chart page
-    if (location.pathname.startsWith("/charts/")) {
-        const chartNameElement = document.getElementById("page_charts_section_charts_header_chart_name");
-        if (chartNameElement) {
-            const chartName = chartNameElement.textContent?.trim();
-            if (chartName) {
-                presenceData.details = "Viewing Chart:";
-                presenceData.state = chartName;
-            }
-        }
-    }
+	const pages: Record<string, PresenceData> = {
+		"/charts/": {
+			details: "Viewing Chart:",
+		},
+		"/new-music/": {
+			details: "New Releases",
+		},
+		"/genres/": {
+			details: "Genres",
+		},
+		"/lists/": {
+			details: "Lists",
+		},
+		"/subscribe/": {
+			details: "Subscribe",
+		},
+		"/": {
+			details: "Home",
+		},
+	};
 
-    //account page
-    if (location.pathname.startsWith("/account/")) {
-        presenceData.details = "Account";
-    }
+	//chart page
+	if (location.pathname.startsWith("/charts/")) {
+		const chartNameElement = document.querySelector(
+			"#page_charts_section_charts_header_chart_name"
+		);
+		if (chartNameElement) {
+			const chartName = chartNameElement.textContent?.trim();
+			if (chartName) {
+				presenceData.details = "Viewing Chart:";
+				presenceData.state = chartName;
+			}
+		}
+	}
 
-    //recs page
-    if (location.pathname.startsWith("/recs/to/") || location.pathname.startsWith("/recs/from/")) {
-        const username = location.pathname.split("/")[3];
-        presenceData.details = "Viewing User Recs:";
-        presenceData.state = username;
-    }
+	//account page
+	if (location.pathname.startsWith("/account/"))
+		presenceData.details = "Account";
 
-    //play history page
-    if (location.pathname.startsWith("/play-history/")) {
-        const username = location.pathname.split("/")[2];
-        presenceData.details = "Viewing Play History:";
-        presenceData.state = `${username}`;
-    }
+	//recs page
+	if (
+		location.pathname.startsWith("/recs/to/") ||
+		location.pathname.startsWith("/recs/from/")
+	) {
+		presenceData.details = "Viewing User Recs:";
+		presenceData.state = location.pathname.split("/")[3];
+	}
 
-    //user stats page
-    if (location.pathname.startsWith("/stats/userstats")) {
-        const username = new URLSearchParams(location.search).get("user");
-        presenceData.details = "Viewing User Stats:";
-        presenceData.state = username;
-    }
+	//play history page
+	if (location.pathname.startsWith("/play-history/")) {
+		presenceData.details = "Viewing Play History:";
+		presenceData.state = `${location.pathname.split("/")[2]}`;
+	}
 
-    //messages page
-    if (location.pathname.startsWith("/messages/")) {
-        presenceData.details = "Messages";
-    }
+	//user stats page
+	if (location.pathname.startsWith("/stats/userstats")) {
+		presenceData.details = "Viewing User Stats:";
+		presenceData.state = new URLSearchParams(location.search).get("user");
+	}
 
-    //development page
-    if (location.pathname.startsWith("/development/")) {
-        presenceData.details = "Development";
-    }
+	//messages page
+	if (location.pathname.startsWith("/messages/"))
+		presenceData.details = "Messages";
 
-    //RYMzilla page
-    if (location.pathname.startsWith("/rymzilla/")) {
-        presenceData.details = "RYMzilla";
-    }
+	//development page
+	if (location.pathname.startsWith("/development/"))
+		presenceData.details = "Development";
 
-    //wiki page
-    if (location.pathname.startsWith("/wiki/")) {
-        presenceData.details = "Wiki";
-    }
+	//RYMzilla page
+	if (location.pathname.startsWith("/rymzilla/"))
+		presenceData.details = "RYMzilla";
 
-    //privacy page
-    if (location.pathname.startsWith("/privacy/")) {
-        presenceData.details = "Privacy Policy";
-    }
+	//wiki page
+	if (location.pathname.startsWith("/wiki/")) presenceData.details = "Wiki";
 
-    //tos page
-    if (location.pathname.startsWith("/tos/")) {
-        presenceData.details = "Terms of Service";
-    }
+	//privacy page
+	if (location.pathname.startsWith("/privacy/"))
+		presenceData.details = "Privacy Policy";
 
-    //contact page
-    if (location.pathname.startsWith("/contact/")) {
-        presenceData.details = "Support / Feedback";
-    }
+	//tos page
+	if (location.pathname.startsWith("/tos/"))
+		presenceData.details = "Terms of Service";
 
-    //latest reviews page
-    if (location.pathname.startsWith("/latest")) {
-        const dateElement = document.querySelector("h4");
-        if (dateElement) {
-            const date = dateElement.textContent?.trim();
-            presenceData.details = "Latest Reviews";
-            presenceData.state = date;
-        }
-    }
+	//contact page
+	if (location.pathname.startsWith("/contact/"))
+		presenceData.details = "Support / Feedback";
 
-    //feature page
-    if (location.pathname.startsWith("/feature/")) {
-        const featureTitleElement = document.querySelector("h1");
-        if (featureTitleElement) {
-            const featureTitle = featureTitleElement.textContent?.trim();
-            presenceData.details = "Viewing Feature:";
-            presenceData.state = featureTitle;
-        }
-    }
+	//latest reviews page
+	if (location.pathname.startsWith("/latest")) {
+		const dateElement = document.querySelector("h4");
+		if (dateElement) {
+			presenceData.details = "Latest Reviews";
+			presenceData.state = dateElement.textContent?.trim();
+		}
+	}
 
-    //recommendations page
-    if (location.pathname.startsWith("/recommendations/")) {
-        const username = location.pathname.split("/")[2];
-        presenceData.details = "Viewing Recommendations:";
-        presenceData.state = `${username}`;
-    }
+	//feature page
+	if (location.pathname.startsWith("/feature/")) {
+		const featureTitleElement = document.querySelector("h1");
+		if (featureTitleElement) {
+			presenceData.details = "Viewing Feature:";
+			presenceData.state = featureTitleElement.textContent?.trim();
+		}
+	}
 
-    //friends page
-    if (location.pathname.startsWith("/friends/")) {
-        const username = location.pathname.split("/")[2];
-        presenceData.details = "Viewing Friends:";
-        presenceData.state = username;
-    }
+	//recommendations page
+	if (location.pathname.startsWith("/recommendations/")) {
+		presenceData.details = "Viewing Recommendations:";
+		presenceData.state = `${location.pathname.split("/")[2]}`;
+	}
 
-    //label page
-    if (location.pathname.startsWith("/label/")) {
-        const labelNameElement = document.querySelector(".page_company_music_section_name_inner h1");
-        if (labelNameElement) {
-            const labelName = labelNameElement.textContent?.trim();
-            presenceData.details = "Viewing Label:";
-            presenceData.state = labelName;
-        }
-    }
+	//friends page
+	if (location.pathname.startsWith("/friends/")) {
+		presenceData.details = "Viewing Friends:";
+		presenceData.state = location.pathname.split("/")[2];
+	}
 
-    //work page
-    if (location.pathname.startsWith("/work/")) {
-        const workNameElement = document.querySelector("li#ui_breadcrumb_item_page_breadcrumb");
-        if (workNameElement) {
-            const workName = workNameElement.textContent?.trim();
-            presenceData.details = "Viewing Work:";
-            presenceData.state = workName;
-        }
-    }
+	//label page
+	if (location.pathname.startsWith("/label/")) {
+		const labelNameElement = document.querySelector(
+			".page_company_music_section_name_inner h1"
+		);
+		if (labelNameElement) {
+			presenceData.details = "Viewing Label:";
+			presenceData.state = labelNameElement.textContent?.trim();
+		}
+	}
 
-    //review page
-    if (location.pathname.startsWith("/music-review/")) {
-        const albumNameElement = document.querySelector("a.album");
-        const artistElement = document.querySelector("a.artist");
-        const userElement = document.querySelector("a.user");
-        if (albumNameElement && artistElement && userElement) {
-            const albumName = albumNameElement.textContent?.trim();
-            const artist = artistElement.textContent?.trim();
-            const user = userElement.textContent?.trim();
-            presenceData.details = "Viewing Review:";
-            presenceData.state = `${albumName} by ${artist} | Review by ${user}`;
-        }
-    }
+	//work page
+	if (location.pathname.startsWith("/work/")) {
+		const workNameElement = document.querySelector(
+			"li#ui_breadcrumb_item_page_breadcrumb"
+		);
+		if (workNameElement) {
+			presenceData.details = "Viewing Work:";
+			presenceData.state = workNameElement.textContent?.trim();
+		}
+	}
 
-    //submissions page
-    if (location.pathname.startsWith("/submissions/") || location.pathname.startsWith("/admin/") || location.pathname.startsWith("/artist/profile_history?scope=") || location.pathname.startsWith("/rgenre/")) {
-        presenceData.details = "Submissions";
-    }
+	//review page
+	if (location.pathname.startsWith("/music-review/")) {
+		const albumNameElement = document.querySelector("a.album"),
+			artistElement = document.querySelector("a.artist"),
+			userElement = document.querySelector("a.user");
+		if (albumNameElement && artistElement && userElement) {
+			presenceData.details = "Viewing Review:";
+			presenceData.state = `${albumNameElement.textContent?.trim()} by ${artistElement.textContent?.trim()} | Review by ${userElement.textContent?.trim()}`;
+		}
+	}
 
-    //genre page
-    if (location.pathname.startsWith("/genre/")) {
-        const genreNameElement = document.querySelector("#page_genre_section_name h1");
-        if (genreNameElement) {
-            const genreName = genreNameElement.textContent?.trim();
-            if (genreName) {
-                presenceData.details = "Viewing Genre:";
-                presenceData.state = genreName;
-            }
-        }
-    }
+	//submissions page
+	if (
+		location.pathname.startsWith("/submissions/") ||
+		location.pathname.startsWith("/admin/") ||
+		location.pathname.startsWith("/artist/profile_history?scope=") ||
+		location.pathname.startsWith("/rgenre/")
+	)
+		presenceData.details = "Submissions";
 
-    //artist page
-    if (location.pathname.startsWith("/artist/")) {
-        const artistNameElement = document.querySelector("h1.artist_name_hdr");
-        if (artistNameElement) {
-            const artistName = artistNameElement.textContent?.trim();
-            if (artistName) {
-                presenceData.details = "Viewing Artist:";
-                presenceData.state = artistName;
-            }
-        }
-    }
+	//genre page
+	if (location.pathname.startsWith("/genre/")) {
+		const genreNameElement = document.querySelector(
+			"#page_genre_section_name h1"
+		);
+		if (genreNameElement) {
+			const genreName = genreNameElement.textContent?.trim();
+			if (genreName) {
+				presenceData.details = "Viewing Genre:";
+				presenceData.state = genreName;
+			}
+		}
+	}
 
-    //user page
-    if (location.pathname.startsWith("/~")) {
-        const usernameElement = document.querySelector("#profilename");
-        if (usernameElement) {
-            const username = usernameElement.textContent?.trim();
-            if (username) {
-                presenceData.details = "Viewing User:";
-                presenceData.state = username;
-            }
-        }
-    }
+	//artist page
+	if (location.pathname.startsWith("/artist/")) {
+		const artistNameElement = document.querySelector("h1.artist_name_hdr");
+		if (artistNameElement) {
+			const artistName = artistNameElement.textContent?.trim();
+			if (artistName) {
+				presenceData.details = "Viewing Artist:";
+				presenceData.state = artistName;
+			}
+		}
+	}
 
-    //list page
-    if (location.pathname.startsWith("/list/")) {
-        const listTitleElement = document.querySelector("h1[style='font-size:2.1em']");
-        const listOwner = location.pathname.split("/")[2];
-        if (listTitleElement) {
-            const listTitle = listTitleElement.textContent?.trim();
-            if (listTitle && listOwner) {
-                presenceData.details = "Viewing List:";
-                presenceData.state = `${listTitle} by ${listOwner}`;
-            }
-        }
-    }
+	//user page
+	if (location.pathname.startsWith("/~")) {
+		const usernameElement = document.querySelector("#profilename");
+		if (usernameElement) {
+			const username = usernameElement.textContent?.trim();
+			if (username) {
+				presenceData.details = "Viewing User:";
+				presenceData.state = username;
+			}
+		}
+	}
 
-    //voting on descriptors page
-    if (location.pathname.startsWith("/rdescriptor/set")) {
-        const albumElement = document.querySelector("a.album");
-        const artistElement = document.querySelector("a.artist");
-        if (albumElement && artistElement) {
-            const album = albumElement.textContent?.trim();
-            const artist = artistElement.textContent?.trim();
-            presenceData.details = "Voting on Descriptors";
-            presenceData.state = `${album} by ${artist}`;
-        }
-    }
+	//list page
+	if (location.pathname.startsWith("/list/")) {
+		const listTitleElement = document.querySelector(
+				"h1[style='font-size:2.1em']"
+			),
+			listOwner = location.pathname.split("/")[2];
+		if (listTitleElement) {
+			const listTitle = listTitleElement.textContent?.trim();
+			if (listTitle && listOwner) {
+				presenceData.details = "Viewing List:";
+				presenceData.state = `${listTitle} by ${listOwner}`;
+			}
+		}
+	}
 
-    //editing list page
-    if (location.pathname.startsWith("/lists/edit")) {
-        const listNameElement = document.querySelector("span");
-        if (listNameElement) {
-            presenceData.details = "Editing List:";
-        }
-    }
+	//voting on descriptors page
+	if (location.pathname.startsWith("/rdescriptor/set")) {
+		const albumElement = document.querySelector("a.album"),
+			artistElement = document.querySelector("a.artist");
+		if (albumElement && artistElement) {
+			presenceData.details = "Voting on Descriptors";
+			presenceData.state = `${albumElement.textContent?.trim()} by ${artistElement.textContent?.trim()}`;
+		}
+	}
 
-    //search page
-    if (location.pathname.startsWith("/search")) {
-        const searchTerm = new URLSearchParams(location.search).get("searchterm");
-        if (searchTerm) {
-            presenceData.details = "Searching for:";
-            presenceData.state = searchTerm;
-        }
-    }
+	//editing list page
+	if (
+		location.pathname.startsWith("/lists/edit") &&
+		document.querySelector("span")
+	)
+		presenceData.details = "Editing List:";
 
-    //release page
-    if (location.pathname.startsWith("/release/")) {
-        const albumTitleElement = document.querySelector(".album_title");
-        const artistElement = document.querySelector(".album_artist_small a.artist");
-        if (albumTitleElement && artistElement) {
-            const albumTitle = albumTitleElement.textContent?.trim();
-            presenceData.details = `Viewing Release:`;
-            presenceData.state = `${albumTitle}`;
-        }
-    }
+	//search page
+	if (location.pathname.startsWith("/search")) {
+		const searchTerm = new URLSearchParams(location.search).get("searchterm");
+		if (searchTerm) {
+			presenceData.details = "Searching for:";
+			presenceData.state = searchTerm;
+		}
+	}
 
-    //collection page
-    if (location.pathname.startsWith("/collection/")) {
-        const collectionOwner = location.pathname.split("/")[2];
-        presenceData.details = "Viewing Collection:";
-        presenceData.state = `${collectionOwner}`;
-    }
+	//release page
+	if (location.pathname.startsWith("/release/")) {
+		const albumTitleElement = document.querySelector(".album_title");
+		if (
+			albumTitleElement &&
+			document.querySelector(".album_artist_small a.artist")
+		) {
+			presenceData.details = "Viewing Release:";
+			presenceData.state = `${albumTitleElement.textContent?.trim()}`;
+		}
+	}
 
-    //compatibility page
-    if (location.pathname.startsWith("/find_similar_users")) {
-        const username = new URLSearchParams(location.search).get("user");
-        presenceData.details = "Viewing Compatibility:";
-        presenceData.state = username;
-    }
+	//collection page
+	if (location.pathname.startsWith("/collection/")) {
+		presenceData.details = "Viewing Collection:";
+		presenceData.state = `${location.pathname.split("/")[2]}`;
+	}
 
-    for (const [path, data] of Object.entries(pages)) {
-        if (location.pathname === path) {
-            presenceData = { ...presenceData, ...data };
-            break;
-        }
-    }
+	//compatibility page
+	if (location.pathname.startsWith("/find_similar_users")) {
+		presenceData.details = "Viewing Compatibility:";
+		presenceData.state = new URLSearchParams(location.search).get("user");
+	}
 
-    presence.setActivity(presenceData);
+	for (const [path, data] of Object.entries(pages)) {
+		if (location.pathname === path) {
+			presenceData = { ...presenceData, ...data };
+			break;
+		}
+	}
+
+	new Presence({
+		clientId: "1213365863470866462",
+	}).setActivity(presenceData);
 }
 
 updatePresence();
