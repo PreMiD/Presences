88c2b0783946c460914a850abb65572951228180
Süleyman <128982325+magoa-bot@users.noreply.github.com>
2024-03-03 16:20:57 +0300

    feat(dizipal): fix

--- websites/D/dizipal/presence.ts
+++ websites/D/dizipal/presence.ts
@@ -47,99 +47,92 @@ presence.on("UpdateData", async () => {
 			largeImageKey: Assets.Logo,
 			startTimestamp: browsingTimestamp,
 		},
-		stringsData = await strings,
-		{ pathname, href } = document.location,
-		seriesPageCheck =
-			!!document.querySelector('.cover-actions > [data-request]')
+		{ pathname, href } = document.location;
 
-	for (const [key, value] of Object.entries(pages)) {
-		if (pathname.includes("/dizi/") && seriesPageCheck) {
-			const stringsData = await strings,
-				seriesTitle = document
-					.querySelector("#container")
-					.querySelector("h5")?.textContent,
-				episode = document.querySelector(
-					"#container > div.episode-head > div:nth-child(1) > header > h2 > a"
-				)?.textContent,
-				seriesUrl = document.querySelector<HTMLAnchorElement>(
-					"#container > div.episode-head > div:nth-child(1) > header > h2 > a"
-				)?.href;
+	if (
+		pathname.includes("/dizi/") &&
+		!!document.querySelector(".cover-actions > [data-request]")
+	) {
+		const stringsData = await strings,
+			seriesTitle = document
+				.querySelector("#container")
+				.querySelector("h5")?.textContent,
+			episode = document.querySelector(
+				"#container > div.episode-head > div:nth-child(1) > header > h2 > a"
+			)?.textContent,
+			seriesUrl = document.querySelector<HTMLAnchorElement>(
+				"#container > div.episode-head > div:nth-child(1) > header > h2 > a"
+			)?.href;
 
-			if (seriesTitle && !episode) {
-				presenceData.details = `${seriesTitle} Adlı Diziye Göz Atıyor.`;
-
-				presenceData.buttons = [
-					{
-						label: `${stringsData.viewShow} ${seriesTitle}`,
-						url: href,
-					},
-				];
-			} else if (episode) {
-				delete presenceData.startTimestamp;
-
-				if (video) {
-					presenceData.details = `${episode} İzliyor.`;
-
-					presenceData.state = `${
-						document.querySelector(
-							".episode-head").querySelector("h6"
-						)?.textContent
-					} ${video.paused ? stringsData.pause : stringsData.play}`;
-
-					[presenceData.startTimestamp, presenceData.endTimestamp] =
-						presence.getTimestamps(video.currentTime, video.duration);
-				} else presenceData.details = `${episode} Adlı Bölüme Göz Atıyor.`;
-
-				presenceData.buttons = [
-					{
-						label: stringsData.viewEpisode,
-						url: href,
-					},
-					{
-						label: "Diziye Git",
-						url: seriesUrl,
-					},
-				];
-			}
-		} else if (
-			document.querySelector(".g-title > div > span")
-				?.textContent
-		) {
-			const stringsData = await strings;
-
-			presenceData.details = document.querySelector(
-				".g-title > div > span"
-			)?.textContent;
-
-			if (video?.currentTime && video?.duration) {
-				presenceData.state = video.paused
-					? stringsData.pause
-					: stringsData.play;
-
-				[presenceData.startTimestamp, presenceData.endTimestamp] =
-					presence.getTimestamps(video.currentTime, video.duration);
-
-				presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
-				presenceData.smallImageText = video.paused
-					? stringsData.pause
-					: stringsData.play;
-			}
+		if (seriesTitle && !episode) {
+			presenceData.details = `${seriesTitle} Adlı Diziye Göz Atıyor.`;
 
 			presenceData.buttons = [
 				{
-					label: stringsData.viewMovie,
+					label: `${stringsData.buttonViewShow} ${seriesTitle}`,
 					url: href,
 				},
 			];
-		} else if (pathname.includes("/koleksiyon/")) {
-			presenceData.details = `${
-				document.querySelector(".g-title > div")?.textContent
-			} Koleksiyonuna Göz Atıyor`;
-		} else if (pathname.includes("@")) {
-			presenceData.details = `${
-				document.querySelector(".title")?.textContent
-			} Adlı Kullanıcının Profiline Göz Atıyor`;
-} else {
+		} else if (episode) {
+			delete presenceData.startTimestamp;
+
+			if (video) {
+				presenceData.details = `${episode} İzliyor.`;
+
+				presenceData.state = `${
+					document.querySelector(".episode-head").querySelector("h6")
+						?.textContent
+				} ${video.paused ? stringsData.pause : stringsData.play}`;
+
+				[presenceData.startTimestamp, presenceData.endTimestamp] =
+					presence.getTimestamps(video.currentTime, video.duration);
+			} else presenceData.details = `${episode} Adlı Bölüme Göz Atıyor.`;
+
+			presenceData.buttons = [
+				{
+					label: stringsData.viewEpisode,
+					url: href,
+				},
+				{
+					label: "Diziye Git",
+					url: seriesUrl,
+				},
+			];
+		}
+	} else if (document.querySelector(".g-title > div > span")?.textContent) {
+		const stringsData = await strings;
+
+		presenceData.details = document.querySelector(
+			".g-title > div > span"
+		)?.textContent;
+
+		if (video?.currentTime && video?.duration) {
+			presenceData.state = video.paused ? stringsData.pause : stringsData.play;
+
+			[presenceData.startTimestamp, presenceData.endTimestamp] =
+				presence.getTimestamps(video.currentTime, video.duration);
+
+			presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
+			presenceData.smallImageText = video.paused
+				? stringsData.pause
+				: stringsData.play;
+		}
+
+		presenceData.buttons = [
+			{
+				label: stringsData.viewMovie,
+				url: href,
+			},
+		];
+	} else if (pathname.includes("/koleksiyon/")) {
+		presenceData.details = `${
+			document.querySelector(".g-title > div")?.textContent
+		} Koleksiyonuna Göz Atıyor`;
+	} else if (pathname.includes("@")) {
+		presenceData.details = `${
+			document.querySelector(".title")?.textContent
+		} Adlı Kullanıcının Profiline Göz Atıyor`;
+	} else {
 		for (const [key, value] of Object.entries(pages))
 			if (pathname === key) presenceData.details = value;
 	}
