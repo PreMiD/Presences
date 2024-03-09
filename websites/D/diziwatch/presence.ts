0aa857ff3bea20a4d248b5762c5730bde8006dec
Emre <85563550+emrovsky@users.noreply.github.com>
2024-03-09 15:56:10 +0300

    Update presence.ts
    Signed-off-by: Emre <85563550+emrovsky@users.noreply.github.com>

--- websites/D/diziwatch/presence.ts
+++ websites/D/diziwatch/presence.ts
@@ -1,70 +1,97 @@
 const presence = new Presence({
-		clientId: "1214974615202299994",
-	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+        clientId: "1214974615202299994",
+    }),
+    browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/p9ktkQw.png",
+    Logo = "https://i.imgur.com/RMrW6j3.png",
 }
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-		largeImageKey: Assets.Logo,
-		startTimestamp: browsingTimestamp,
-	};
-	switch (document.URL) {
-		case "https://diziwatch.net/": {
-			presenceData.details = "Ana sayfa görüntüleniyor";
+    const presenceData: PresenceData = {
+        largeImageKey: Assets.Logo,
+    };
+    switch (document.location.pathname) {
+        case "/": {
+            presenceData.details = "Ana sayfa görüntüleniyor";
+            presenceData.startTimestamp = browsingTimestamp;
 
-			break;
-		}
-		case "https://diziwatch.net/calendar/": {
-			presenceData.details = "Takvim sayfası görüntüleniyor";
+            break;
+        }
+        case "/calendar/": {
+            presenceData.details = "Takvim sayfası görüntüleniyor";
+            presenceData.startTimestamp = browsingTimestamp;
 
-			break;
-		}
-		case "https://diziwatch.net/anime-arsivi/": {
-			presenceData.details = "Anime arşivi görüntüleniyor";
+            break;
+        }
+        case "/anime-arsivi/": {
+            presenceData.details = "Anime arşivi görüntüleniyor";
+            presenceData.startTimestamp = browsingTimestamp;
 
-			break;
-		}
-		default:
-			if (document.URL.startsWith("https://diziwatch.net/dizi/")) {
-				presenceData.details = `${
-					document.querySelector("#content > div.incontentx > div.title > h1")
-						.textContent
-				} inceleniyor`;
-				presenceData.startTimestamp = browsingTimestamp;
-			} else {
-				const video = document.querySelector('video'),
-					videoTitle = document.querySelector(
-						".title"
-					).textContent,
-					animeImg = document
-						.querySelector(
-							".jw-preview.jw-reset"
-						)
-						.getAttribute("style")
-						.split('"')[1],
-					playStatus = document
-						.querySelector(
-							"#player > div.jw-wrapper.jw-reset > div.jw-controls.jw-reset > div.jw-display.jw-reset > div > div > div.jw-display-icon-container.jw-display-icon-display.jw-reset > div"
-						)
-						.getAttribute("aria-label");
+            break;
+        }
+        case "/dizi-arsivi/": {
+            presenceData.details = "Dizi arşivi görüntüleniyor";
+            presenceData.startTimestamp = browsingTimestamp;
 
-				presenceData.details = videoTitle;
+            break;
+        }
+        case "/episodes/": {
+            presenceData.details = "Bölümler inceleniyor";
+            presenceData.startTimestamp = browsingTimestamp;
 
-				if (playStatus) [, presenceData.endTimestamp] = presence.getTimestampsfromMedia(video)
-				presenceData.smallImageKey = playStatus ? Assets.Paused : Assets.Play
-				
+            break;
+        }
+        case "/contact-us/": {
+            presenceData.details = "İletişim bilgileri inceleniyor 💀";
+            presenceData.startTimestamp = browsingTimestamp;
 
-				presenceData.largeImageKey = animeImg;
-				presenceData.largeImageText = document.querySelector(
-					"#benzerli > div > span"
-				).textContent;
+            break;
+        }
+        default:
+            if (document.location.href.startsWith("https://diziwatch.net/dizi/")) {
+                presenceData.details = `${
+                    document.querySelector("#content div.incontentx div.title h1")
+                        .textContent
+                } inceleniyor`;
+                presenceData.startTimestamp = browsingTimestamp;
+            } else {
+                const video = document.querySelector('video'),
+                    videoTitle = document.querySelector(
+                        "#player .jw-title-primary"
+                    ).textContent,
 
-			}
-	}
+                    animeImg = document
+                        .querySelector(
+                            "#player .jw-preview"
+                        )
+                        .getAttribute("style")
+                        .split('"')[1],
+                    playStatus = document
+                        .querySelector(
+                            ".jw-icon.jw-icon-display.jw-button-color.jw-reset"
+                        )
+                        .getAttribute("aria-label"),
+								 duration = new Date(video.duration * 1000).toISOString().substr(14, 5),
+								 currentTime = new Date(video.currentTime * 1000).toISOString().substr(14, 5);
 
-	presence.setActivity(presenceData);
+                presenceData.details = videoTitle;
+
+                if (playStatus === "Oynat")
+                    presenceData.state = `Duraklatıldı, ${currentTime} / ${duration}`;
+
+                if (playStatus === "Duraklat")
+                    presenceData.state = `İzleniyor, ${currentTime} / ${duration}`;
+
+                presenceData.largeImageKey = animeImg;
+                presenceData.largeImageText = document.querySelector(
+                    "#benzerli > div > span"
+                ).textContent;
+
+                presenceData.smallImageKey = Assets.Logo;
+                presenceData.smallImageText = "Diziwatch";
+            }
+    }
+
+    presence.setActivity(presenceData);
 });
