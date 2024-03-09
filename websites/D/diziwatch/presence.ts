e878ba90d765282863ddc7660696ad189f9e9dea
Emre <85563550+emrovsky@users.noreply.github.com>
2024-03-09 16:38:25 +0300

    Update presence.ts
    Signed-off-by: Emre <85563550+emrovsky@users.noreply.github.com>

--- websites/D/diziwatch/presence.ts
+++ websites/D/diziwatch/presence.ts
@@ -11,64 +11,69 @@ presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey: Assets.Logo,
 	};
-	presenceData.startTimestamp = browsingTimestamp;
 	switch (document.location.pathname) {
 		case "/": {
 			presenceData.details = "Ana sayfa görüntüleniyor";
+			presenceData.startTimestamp = browsingTimestamp;
 
 			break;
 		}
 		case "/calendar/": {
 			presenceData.details = "Takvim sayfası görüntüleniyor";
+			presenceData.startTimestamp = browsingTimestamp;
 
 			break;
 		}
 		case "/anime-arsivi/": {
 			presenceData.details = "Anime arşivi görüntüleniyor";
+			presenceData.startTimestamp = browsingTimestamp;
 
 			break;
 		}
 		case "/dizi-arsivi/": {
 			presenceData.details = "Dizi arşivi görüntüleniyor";
+			presenceData.startTimestamp = browsingTimestamp;
 
 			break;
 		}
 		case "/episodes/": {
 			presenceData.details = "Bölümler inceleniyor";
+			presenceData.startTimestamp = browsingTimestamp;
 
 			break;
 		}
 		case "/contact-us/": {
 			presenceData.details = "İletişim bilgileri inceleniyor 💀";
+			presenceData.startTimestamp = browsingTimestamp;
 
 			break;
 		}
 		default:
 			if (document.location.href.startsWith("https://diziwatch.net/dizi/")) {
 				presenceData.details = `${
-					document.querySelector("#content div.incontentx div.title h1")
+					document.querySelector("#content > div.incontentx > div.title > h1")
 						.textContent
 				} inceleniyor`;
+				presenceData.startTimestamp = browsingTimestamp;
 			} else {
-				presenceData.startTimestamp = null;
-				const video = document.querySelector("video"),
+				const currentTime = document.querySelector(
+						"#player .jw-icon.jw-text-elapsed"
+					).textContent,
+					duration = document.querySelector(
+						"#player .jw-icon.jw-text-duration"
+					).textContent,
+					videoTitle = document.querySelector(
+						"#player .jw-title-primary"
+					).textContent,
 					animeImg = document
 						.querySelector("#player .jw-preview")
 						.getAttribute("style")
 						.split('"')[1],
 					playStatus = document
 						.querySelector(".jw-icon.jw-icon-display.jw-button-color.jw-reset")
-						.getAttribute("aria-label"),
-					duration = new Date(video.duration * 1000)
-						.toISOString()
-						.substr(14, 5),
-					currentTime = new Date(video.currentTime * 1000)
-						.toISOString()
-						.substr(14, 5);
+						.getAttribute("aria-label");
 
-				presenceData.details = document.querySelector(
-					"#player .jw-title-primary"
-				).textContent;
+				presenceData.details = videoTitle;
 
 				if (playStatus === "Oynat")
 					presenceData.state = `Duraklatıldı, ${currentTime} / ${duration}`;
