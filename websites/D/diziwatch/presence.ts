f894758967efa9e794ec23714e113fbf62f60ba3
Emre <85563550+emrovsky@users.noreply.github.com>
2024-03-09 15:26:34 +0300

    Update websites/D/diziwatch/presence.ts
    Co-authored-by: Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
    Signed-off-by: Emre <85563550+emrovsky@users.noreply.github.com>

--- websites/D/diziwatch/presence.ts
+++ websites/D/diziwatch/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/45R11f9.png",
+	Logo = "https://i.imgur.com/p9ktkQw.png",
 }
 
 presence.on("UpdateData", async () => {
