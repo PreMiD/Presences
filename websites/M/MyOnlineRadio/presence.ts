a1498bca1e68f90d5449e2de5c422a3deb3fc8c7
ION606 <ion606@protonmail.com>
2024-03-16 12:18:08 -0700

    Update websites/M/MyOnlineRadio/presence.ts
    Co-authored-by: Bas van Zanten <bas.van.zanten007@gmail.com>
    Signed-off-by: ION606 <ion606@protonmail.com>

--- websites/M/MyOnlineRadio/presence.ts
+++ websites/M/MyOnlineRadio/presence.ts
@@ -9,7 +9,6 @@ const presence = new Presence({
 // Other default assets can be found at index.d.ts
 const enum Assets {
 	Logo = "https://i.ibb.co/5LsDstd/mic2.png",
-	smallImageKey = "",
 }
 
 const enum Pages {
