e1b2dea820c359c8eee0cf59322f4494745d0d73
leriaz <leriaz@live.com>
2024-03-16 16:13:43 +0200

    Updated audible selectors

--- websites/A/Audible/presence.ts
+++ websites/A/Audible/presence.ts
@@ -1,57 +1,71 @@
 const presence = new Presence({
-		clientId: "991933219719086080",
-	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+	clientId: "991933219719086080",
+}),
+browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-		largeImageKey:
-			"https://cdn.rcd.gg/PreMiD/websites/A/Audible/assets/logo.png",
-		startTimestamp: browsingTimestamp,
-	};
-	if (document.location.pathname.includes("/account"))
-		presenceData.details = "Viewing account";
-	else if (document.location.pathname.includes("/wl"))
-		presenceData.details = "Viewing wishlist";
-	else if (document.location.pathname.includes("/library"))
-		presenceData.details = "Viewing library";
-	else if (document.location.pathname.includes("plus"))
-		presenceData.details = "Viewing plus catalog";
-	else if (document.location.pathname.includes("gift"))
-		presenceData.details = "Viewing gift center";
-	else if (document.location.pathname.includes("/pd")) {
-		presenceData.details = `Viewing ${
-			document.querySelector(
-				"h1.bc-heading.bc-color-base.bc-pub-break-word.bc-text-bold"
-			).textContent
-		} by ${
-			document.querySelector(
-				"li.bc-list-item.authorLabel > a.bc-link.bc-color-link"
-			).textContent
-		}`;
-		presenceData.largeImageKey = document
-			.querySelector("img.bc-pub-block.bc-image-inset-border.js-only-element")
-			.getAttribute("src");
-	} else if (document.location.pathname.includes("/webplayer")) {
-		presenceData.details = `Listening to ${document
-			.querySelector("input[name=title]")
-			.getAttribute("value")}`;
-		presenceData.state = document.querySelector(
-			"span.bc-text.timeLeft"
-		).textContent;
-		presenceData.largeImageKey = document
-			.querySelector("img[id=adbl-cloudBook]")
-			.getAttribute("src");
-		if (
-			document
-				.querySelector("img[title='Play/Pause']")
-				.className.includes("bc-hidden")
-		)
-			presenceData.smallImageKey = Assets.Pause;
-		else presenceData.smallImageKey = Assets.Play;
+const presenceData: PresenceData = {
+	largeImageKey:
+		"https://cdn.rcd.gg/PreMiD/websites/A/Audible/assets/logo.png",
+	startTimestamp: browsingTimestamp,
+};
+if (document.location.pathname.includes("/account"))
+	presenceData.details = "Viewing account";
+else if (document.location.pathname.includes("/wl"))
+	presenceData.details = "Viewing wishlist";
+else if (document.location.pathname.includes("/library"))
+	presenceData.details = "Viewing library";
+else if (document.location.pathname.includes("plus"))
+	presenceData.details = "Viewing plus catalog";
+else if (document.location.pathname.includes("gift"))
+	presenceData.details = "Viewing gift center";
+else if (document.location.pathname.includes("/pd")) {
+	presenceData.details = `Viewing ${
+		document.querySelector(
+			"h1.bc-heading.bc-color-base.bc-pub-break-word.bc-text-bold",
+		).textContent
+	} by ${
+		document.querySelector(
+			"li.bc-list-item.authorLabel > a.bc-link.bc-color-link",
+		).textContent
+	}`;
+	presenceData.largeImageKey = document
+		.querySelector("img.bc-pub-block.bc-image-inset-border.js-only-element")
+		.getAttribute("src");
+} else if (document.location.pathname.includes("/webplayer")) {
+	presenceData.details = `Listenting to ${
+		document.getElementById("adbl-cloud-player-container-data").dataset
+			.parenttitle
+	}`;
+	presenceData.state = document.getElementById(
+		"cp-Top-chapter-display",
+	).innerText;
+	const chapterTimeLeft = document
+		.getElementById("adblMediaBarTimeLeft")
+		.innerText.replace("– ", "");
+	const chapterTimeSpent = document.getElementById(
+		"adblMediaBarTimeSpent",
+	).innerText;
+	const chapterRemaining = presence.getTimestamps(
+		presence.timestampFromFormat(chapterTimeSpent),
+		presence.timestampFromFormat(chapterTimeLeft) +
+			presence.timestampFromFormat(chapterTimeSpent),
+	);
+	presenceData.startTimestamp = chapterRemaining[0];
+	presenceData.endTimestamp = chapterRemaining[1];
+	presenceData.largeImageKey = document
+		.getElementById("adbl-cloudBook")
+		.getAttribute("src");
+	if (
+		document.querySelector(".adblPlayButton").classList.contains("bc-hidden")
+	) {
+		presenceData.smallImageKey = Assets.Pause;
 	} else {
-		presenceData.details = "Browsing";
-		delete presenceData.state;
+		presenceData.smallImageKey = Assets.Play;
 	}
-	presence.setActivity(presenceData);
+} else {
+	presenceData.details = "Browsing";
+	delete presenceData.state;
+}
+presence.setActivity(presenceData);
 });
