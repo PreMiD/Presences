5b0e78693adc8de3cebd6a0d99180cfe084604db
Ashley :3 <oxp7tbr0@duck.com>
2024-03-27 00:29:49 +0300

    feat(Poke) : add presence (#8201)
    * add poke
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * add poke
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * Rename premid.ts to presence.ts
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * fix silly comma
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * fix stuff
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * feat: Add more instances
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * update schema ver
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * make version 1.0.0
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * lint code :3
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * add the matches field :3 
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * rename dir
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * add comma :3
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * fix commas again
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * fix logo
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * use assets. instead
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Ashley :3 <oxp7tbr0@duck.com>
    
    * change logo url
    
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Signed-off-by: Ashley :3 <oxp7tbr0@duck.com>
    
    * fix lint
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    
    * style: format code with prettier
    
    ---------
    
    Signed-off-by: Ashley :3 <iamashley@tuta.io>
    Signed-off-by: Ashley :3 <oxp7tbr0@duck.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Co-authored-by: theusaf <theusafyt@gmail.com>

---
+++ websites/P/Poke/presence.ts
@@ -0,0 +1,74 @@
+const presence = new Presence({
+	clientId: "1188371319776100463",
+});
+
+function getTimestamps() {
+	const timeElement = document.querySelector("video"),
+		[startTimestamp, endTimestamp] = presence.getTimestamps(
+			Math.floor(timeElement.currentTime),
+			Math.floor(timeElement.duration)
+		);
+
+	return { startTimestamp, endTimestamp };
+}
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey: "https://poketube.fun/css/yt-ukraine-512.png",
+	};
+	let clear = false;
+
+	switch (document.location.pathname.replace("/feed", "").split("/")[1]) {
+		case "":
+		case "app":
+			presenceData.details = "Browsing trending videos";
+			break;
+
+		case "my-acc":
+			presenceData.details = "Browsing subscriptions";
+			break;
+
+		case "license":
+		case "privacy":
+			presenceData.details = "Managing preferences";
+			break;
+
+		case "watch": {
+			presenceData.smallImageKey =
+				document.querySelector("video") &&
+				!document.querySelector("video").paused
+					? Assets.Play
+					: Assets.Pause;
+			const videoTitleElement = document.querySelector(".video-title.t");
+			presenceData.details = videoTitleElement
+				? videoTitleElement.textContent.trim()
+				: "No Title Available";
+
+			const channelNameElement = document.querySelector(
+				".video-info-pill-channelname"
+			);
+			presenceData.state = channelNameElement
+				? channelNameElement.textContent.trim()
+				: "No Channel Name Available";
+			const videoPlayer = document.querySelector("video");
+
+			if (videoPlayer && !videoPlayer.paused)
+				presenceData.endTimestamp = getTimestamps().endTimestamp;
+			break;
+		}
+		case "channel": {
+			const nameElement = document.querySelector(".name p");
+			presenceData.details = "Viewing channel";
+			presenceData.state = nameElement
+				? nameElement.textContent.trim()
+				: "No Name Available";
+			break;
+		}
+		default:
+			clear = true;
+			break;
+	}
+
+	if (clear) presence.setActivity();
+	else presence.setActivity(presenceData);
+});
