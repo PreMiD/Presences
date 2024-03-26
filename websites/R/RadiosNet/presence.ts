001335be7514f5cc19085074d6a5b112bb6e2246
ntn-ss <nathandossantosdasilva@gmail.com>
2024-03-26 10:44:00 -0300

    feat(RadiosNet): add presence

--- websites/R/RadiosNet/presence.ts
+++ websites/R/RadiosNet/presence.ts
@@ -105,9 +105,6 @@ presence.on("UpdateData", async () => {
 		else {
 			presenceData.smallImageText =
 				"Desde 1997 trazendo o melhor compilado de emissoras de rádio!";
-		} else {
-			presenceData.smallImageText = slogan;
-		}
 
 		presenceData.state = document
 			.querySelector(".info")
