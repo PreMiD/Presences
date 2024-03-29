ba83c666174518ab7be70fae92f153ae1db02f10
SeMiD <semid@premid.app>
2024-03-29 21:29:46 +0000

    chore(Mahjong Soul): update assets (#8220)
    chore: update assets
    
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/M/Mahjong Soul/presence.ts
+++ websites/M/Mahjong Soul/presence.ts
@@ -25,7 +25,7 @@ import {
 } from "./util";
 
 const enum Asset {
-	Logo = "https://i.imgur.com/X0BvMqW.jpeg",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/M/Mahjong%20Soul/assets/logo.jpeg",
 }
 
 presence.on("UpdateData", async () => {
