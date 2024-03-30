9bd8bda139c8b043ae3a348715c516674e2b146d
ION606 <ion606@protonmail.com>
2024-03-29 20:36:42 -0700

    feat(Daily Mail): add presence

---
+++ websites/D/Daily Mail/newsPage.ts
@@ -0,0 +1,34 @@
+import { findPage } from "./findPage";
+import { Assets } from "./presence";
+
+function handleArticle(presenceData: PresenceData) {
+	const aBody = document.querySelector("#js-article-text"),
+		titleEl = aBody.querySelector("h1");
+	presenceData.buttons = [{ label: "Read Article", url: window.location.href }];
+
+	if (titleEl.querySelector(".is-exclusive")) {
+		presenceData.details = "Reading Article";
+		presenceData.state = titleEl.textContent.replace("EXCLUSIVE", "");
+		presenceData.smallImageKey = Assets.exclIco;
+		presenceData.smallImageText = "Exclusive!";
+	} else {
+		presenceData.details = "Reading Article";
+		presenceData.state = titleEl.textContent;
+		presenceData.smallImageKey = "";
+	}
+
+	// authors
+	const authors = Array.from(
+		aBody.querySelector(".author-section").querySelectorAll("a")
+	).map(el => el.textContent);
+	presenceData.state = `By ${authors.join(", ") || "unknown"}`;
+}
+
+export function handleNewsPage(presenceData: PresenceData) {
+	const newsPaths = window.location.pathname
+		.split("/")
+		.find(e => e && e !== "news");
+
+	if (newsPaths.includes("article-")) handleArticle(presenceData);
+	else findPage(window.location.pathname.substring(1), presenceData);
+}
---
+++ websites/D/Daily Mail/presence.ts
@@ -0,0 +1,68 @@
+import { findPage } from "./findPage";
+import { handleNewsPage } from "./newsPage";
+
+const presence = new Presence({
+	clientId: "1217153856665026580",
+});
+
+export const enum Assets {
+	Logo = "https://i.imgur.com/glm7on6.png",
+	exclIco = "https://www.pngmart.com/files/15/Red-Exclamation-Mark-PNG-Transparent-Image-279x279.png",
+	play = "https://static.vecteezy.com/system/resources/previews/000/426/027/original/play-icon-vector-illustration.jpg",
+	pause = "https://icons.iconarchive.com/icons/graphicloads/100-flat/256/pause-icon.png",
+}
+
+const enum Pages {
+	homepage = "home",
+	news = "news",
+	wellness = "wellness-us", // apparently there's....no uk page?
+}
+
+// possible expansion for other countries, leave as helper function
+const catToCountry = (cat: string) => {
+	if (cat.startsWith("us")) return [cat.replace("us", ""), "Daily Mail (US)"];
+	else if (cat.startsWith("au"))
+		return [cat.replace("au", ""), "Daily Mail (US)"];
+	return [cat.replace("us", ""), "Daily Mail"];
+};
+
+presence.on("UpdateData", async () => {
+	if (!document.location.toString()) return;
+
+	// all urls are formatted "category/pagename"
+	const [catNoCountry, titleWithCountry] = catToCountry(
+			document.location.pathname.split("/")[1]
+		),
+		presenceData: PresenceData = {
+			largeImageKey: Assets.Logo,
+			name: titleWithCountry,
+			type: ActivityType.Playing,
+		};
+
+	presenceData.startTimestamp = Math.floor(Date.now() / 1000);
+
+	switch (catNoCountry) {
+		case Pages.homepage:
+			{
+				const subcat = document
+					.querySelector("[data-mol-fe-latest-headlines--active]")
+					?.querySelector("a")
+					?.textContent?.toLowerCase();
+
+				if (subcat && ["all", "news", "home"].includes(subcat))
+					presenceData.details = `Browsing the latest ${subcat} news`;
+				else presenceData.details = "Browsing the latest news";
+			}
+			break;
+
+		case Pages.news:
+			handleNewsPage(presenceData);
+			break;
+
+		default:
+			findPage(window.location.pathname.substring(1), presenceData);
+			break;
+	}
+
+	presence.setActivity(presenceData);
+});
