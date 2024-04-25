fa4723fab70ad70de05fd70a09b90442a2e3322c
Slowlife01 <slowlife1165@gmail.com>
2024-04-25 19:25:35 +0700

    fix(Naver): no chzzk pls

--- websites/N/Naver/presence.ts
+++ websites/N/Naver/presence.ts
@@ -75,6 +75,8 @@ presence.on("UpdateData", async () => {
 		data.isChecked = true;
 	}
 
+	if (location.hostname.includes("chzzk")) return;
+
 	const presenceData: PresenceData = {
 		name: data.service[0],
 		details: "Browsing...",
