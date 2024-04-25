ae0937a1a8e6e789732f45c14f08499fe4c19e8a
theusaf <theusafyt@gmail.com>
2024-04-25 15:25:58 -0700

    feat: improve fetching of permissions and titles

--- websites/M/Minecraft Wiki/presence.ts
+++ websites/M/Minecraft Wiki/presence.ts
@@ -38,37 +38,54 @@ presence.on("UpdateData", async () => {
 			viewAThread: "general.viewAThread",
 			buttonViewProfile: "general.buttonViewProfile",
 			editing: "general.editing",
-			settings: "x.settings",
-			advancedSettings: "gmail.advancedSettings",
+			advancedSettings: "google classroom.settings",
 			viewAPage: "general.viewAPage",
 			buttonViewPage: "general.buttonViewPage",
 			readingAbout: "general.readingAbout",
+			moving: "minecraft wiki.moving",
+			viewSourceOf: "minecraft wiki.viewSourceOf",
+			viewHistory: "minecraft wiki.viewHistory",
+			changeProtection: "minecraft wiki.changeProtection",
+			viewProtection: "minecraft wiki.viewProtection",
 		}),
-		mainPath = pathname.split("/").filter(Boolean)[1] ?? "/";
+		mainPath = pathname.split("/").filter(Boolean)[1] ?? "/",
+		pageTitle = document.querySelector<HTMLMetaElement>(
+			"meta[property='og:title']"
+		)?.content;
 
 	presenceData.largeImageKey =
 		locationSpecificLogos[hostname.split(".")[0]] ?? Assets.Logo;
 
 	if (mainPath === "/") presenceData.details = strings.viewHome;
-	else if (mainPath.startsWith("User:")) {
+	else if (
+		searchParams.get("action") === "edit" ||
+		searchParams.get("veaction") === "edit"
+	) {
+		if (document.querySelector(".permissions-errors")) {
+			presenceData.details = strings.viewSourceOf;
+		} else {
+			presenceData.details = strings.editing;
+		}
+		presenceData.state = pageTitle;
+	} else if (searchParams.get("action") === "history") {
+		presenceData.details = strings.viewHistory;
+		presenceData.state = pageTitle;
+	} else if (searchParams.get("action") === "protect") {
+		if (document.querySelector(".permissions-errors")) {
+			presenceData.details = strings.viewProtection;
+		} else {
+			presenceData.details = strings.changeProtection;
+		}
+		presenceData.state = pageTitle;
+	} else if (mainPath.startsWith("User:")) {
 		presenceData.details = strings.viewUser;
-		presenceData.state = document.querySelector<HTMLSpanElement>(
-			".mw-page-title-main"
-		);
+		presenceData.state = pageTitle.slice(5);
 		presenceData.buttons = [{ label: strings.buttonViewProfile, url: href }];
 	} else if (/^[^:]*talk:/i.test(mainPath)) {
 		presenceData.details = strings.viewAThread;
-		presenceData.state =
-			document.querySelector<HTMLHeadingElement>("#firstHeading");
-	} else if (searchParams.get("action") === "edit") {
-		presenceData.details = strings.editing;
-		presenceData.state =
-			document.querySelector<HTMLHeadingElement>("#firstHeadingTitle") ??
-			document.querySelector<HTMLAnchorElement>("#mw-content-subtitle a");
-	} else if (["history", "protect"].includes(searchParams.get("action"))) {
-		presenceData.details = strings.settings;
-		presenceData.state =
-			document.querySelector<HTMLHeadingElement>("#firstHeading");
+		presenceData.state = document.querySelector<HTMLSpanElement>(
+			".mw-page-title-main"
+		);
 	} else if (mainPath.startsWith("Special:")) {
 		presenceData.details = strings.advancedSettings;
 		presenceData.state =
