db0c2ecdbe27cb9c7fc0823830a101e481c01c2b
theusaf <theusafyt@gmail.com>
2024-04-25 13:26:43 -0700

    feat(Kotobade Asobou): add presence

---
+++ websites/K/Kotobade Asobou/strings.ts
@@ -0,0 +1,14 @@
+const en = {
+		victory: "Victory",
+		failure: "Failure",
+		guessing: "Guessing",
+		play: "Play Game",
+	},
+	ja: Record<keyof typeof en, string> = {
+		victory: "しょうり",
+		failure: "しっぱい",
+		guessing: "すいそく",
+		play: "あそぼう",
+	};
+
+export default { en, ja };
