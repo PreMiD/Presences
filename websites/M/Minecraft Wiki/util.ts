6637eab0e8ee7d0cdd50533dbed43f5196ad7e72
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2024-04-27 20:31:54 -0700

    feat(Minecraft Wiki): add presence (#8300)

---
+++ websites/M/Minecraft Wiki/util.ts
@@ -0,0 +1,41 @@
+const promiseCache = new Map<string, Promise<Response>>();
+
+export async function getUserNamespace(): Promise<string> {
+	const cached = sessionStorage.getItem("PMD_UserNamespace");
+	if (cached) return cached;
+	let response: Response;
+	const promise = promiseCache.get("getUserNamespace");
+	if (promise) response = await promise;
+	else {
+		const promise = fetch(
+			`https://${document.location.hostname}/w/User:Example`
+		);
+		promiseCache.set("getUserNamespace", promise);
+		response = await promise;
+	}
+	sessionStorage.setItem(
+		"PMD_UserNamespace",
+		new URL(response.url).pathname.split("/").filter(Boolean)[1].split(":")[0]
+	);
+	return sessionStorage.getItem("PMD_UserNamespace");
+}
+
+export async function getTalkNamespace(): Promise<string> {
+	const cached = sessionStorage.getItem("PMD_TalkNamespace");
+	if (cached) return cached;
+	let response: Response;
+	const promise = promiseCache.get("getTalkNamespace");
+	if (promise) response = await promise;
+	else {
+		const promise = fetch(
+			`https://${document.location.hostname}/w/Talk:Example`
+		);
+		promiseCache.set("getTalkNamespace", promise);
+		response = await promise;
+	}
+	sessionStorage.setItem(
+		"PMD_TalkNamespace",
+		new URL(response.url).pathname.split("/").filter(Boolean)[1].split(":")[0]
+	);
+	return sessionStorage.getItem("PMD_TalkNamespace");
+}
