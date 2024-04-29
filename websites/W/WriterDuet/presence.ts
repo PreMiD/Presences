019cc634c481c27eebc1b3bc5b93b9a3c83e9da5
ION606 <ion606@protonmail.com>
2024-04-28 20:55:35 -0700

    feat(Writers Duet): add presence

--- websites/W/WriterDuet/presence.ts
+++ websites/W/WriterDuet/presence.ts
@@ -13,51 +13,60 @@ const enum Pages {
 	blog = "blog",
 	help = "category",
 	pricing = "pricing",
-	article = "article"
+	article = "article",
 }
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: Assets.Logo,
-		startTimestamp: browsingTimestamp,
-	},
-	[,page, subpage] = document.location.pathname.split("/");
+			largeImageKey: Assets.Logo,
+			startTimestamp: browsingTimestamp,
+		},
+		[, page, subpage] = document.location.pathname.split("/");
 
-	switch(page.split("#")[0]) {
-		case Pages.script: {
-			presenceData.details = document.title;
-			presenceData.state = document
-				.querySelector(".Mui-selected")
-				.querySelector("[data-tip]").textContent;
-			presence.setActivity(presenceData);
-		}
-		break;
+	switch (page.split("#")[0]) {
+		case Pages.script:
+			{
+				presenceData.details = document.title;
+				presenceData.state = document
+					.querySelector(".Mui-selected")
+					.querySelector("[data-tip]").textContent;
+				presence.setActivity(presenceData);
+			}
+			break;
 
-		case Pages.home: {
-			presenceData.details = "Browsing Home";
-		}
-		break;
+		case Pages.home:
+			{
+				presenceData.details = "Browsing Home";
+			}
+			break;
 
 		case Pages.help:
-		case Pages.article: {
-			presenceData.details = "Browsing Help",
-			presenceData.state = document.querySelector("#categoryHead > h1")?.textContent
-								|| document.querySelector(".title").textContent;
-		}
-		break;
+		case Pages.article:
+			{
+				(presenceData.details = "Browsing Help"),
+					(presenceData.state =
+						document.querySelector("#categoryHead > h1")?.textContent ||
+						document.querySelector(".title").textContent);
+			}
+			break;
 
-		case Pages.pricing: presenceData.details = "Reviewing Payement Plans";
-		break;
+		case Pages.pricing:
+			presenceData.details = "Reviewing Payement Plans";
+			break;
 
-		case Pages.blog: {
-			if (subpage) {
-				presenceData.details = "Reading Blog Article";
-				presenceData.state = document.querySelector(".jupiterx-post-title").textContent;
-			} else presenceData.details = "Reading the Blog";
-		}
-		break;
+		case Pages.blog:
+			{
+				if (subpage) {
+					presenceData.details = "Reading Blog Article";
+					presenceData.state = document.querySelector(
+						".jupiterx-post-title"
+					).textContent;
+				} else presenceData.details = "Reading the Blog";
+			}
+			break;
 
-		default: presenceData.details = "Browsing the Site";
+		default:
+			presenceData.details = "Browsing the Site";
 	}
 
 	presence.setActivity(presenceData);
