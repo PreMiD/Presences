266bde95888e64910d811774f6d4139d22d71ae5
SeMiD <semid@premid.app>
2024-04-29 23:35:13 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/P/Podurama/presence.ts
+++ websites/P/Podurama/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/tetKhbd.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/P/Podurama/assets/logo.png",
 }
 
 const enum Pages {
