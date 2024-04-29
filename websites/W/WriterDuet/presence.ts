75b704460993d562b7a0572b696fb654b54b35ec
ION606 <ion606@protonmail.com>
2024-04-28 20:46:17 -0700

    feat(Writers Duet): add presence

--- websites/W/WriterDuet/presence.ts
+++ websites/W/WriterDuet/presence.ts
@@ -7,15 +7,62 @@ const enum Assets {
 	Logo = "https://www.writerduet.com/script/wd-logo-square.png",
 }
 
+const enum Pages {
+	home = "",
+	script = "script",
+	blog = "blog",
+	help = "category",
+	pricing = "pricing",
+	article = "article"
+}
+
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 		largeImageKey: Assets.Logo,
 		startTimestamp: browsingTimestamp,
 	};
+	
+	let [,page, subpage] = document.location.pathname.split("/");
 
-	presenceData.details = document.title;
-	presenceData.state = document
-		.querySelector(".Mui-selected")
-		.querySelector("[data-tip]").textContent;
+	// /#home exists for some reason
+	if (page.startsWith("#")) page = page.split("#")[0];
+
+	switch(page) {
+		case Pages.script: {
+			presenceData.details = document.title;
+			presenceData.state = document
+				.querySelector(".Mui-selected")
+				.querySelector("[data-tip]").textContent;
+			presence.setActivity(presenceData);
+		}
+		break;
+
+		case Pages.home: {
+			presenceData.details = "Browsing Home";
+		}
+		break;
+
+		case Pages.help:
+		case Pages.article: {
+			presenceData.details = "Browsing Help",
+			presenceData.state = document.querySelector("#categoryHead > h1")?.textContent
+								|| document.querySelector(".title").textContent;
+		}
+		break;
+
+		case Pages.pricing: presenceData.details = "Reviewing Payement Plans";
+		break;
+
+		case Pages.blog: {
+			if (subpage) {
+				presenceData.details = "Reading Blog Article";
+				presenceData.state = document.querySelector(".jupiterx-post-title").textContent;
+			} else presenceData.details = "Reading the Blog";
+		}
+		break;
+
+		default: presenceData.details = "Browsing the Site";
+	}
+
 	presence.setActivity(presenceData);
 });
