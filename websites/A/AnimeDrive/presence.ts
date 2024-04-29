58d185272414b302149dc4b26e457d829e824da5
SeMiD <semid@premid.app>
2024-04-30 00:36:03 +0100

    chore(AnimeDrive): update assets (#8338)

--- websites/A/AnimeDrive/presence.ts
+++ websites/A/AnimeDrive/presence.ts
@@ -5,7 +5,8 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/VlBtvJo.png",
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/A/AnimeDrive/assets/logo.png",
 			startTimestamp: browsingTimestamp,
 		},
 		{ pathname, search } = document.location;
