09b06c5d3a10abf53fb24a2abc890f5d6d72c361
SeMiD <semid@premid.app>
2024-04-29 23:27:46 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/W/WriterDuet/presence.ts
+++ websites/W/WriterDuet/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://www.writerduet.com/script/wd-logo-square.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/W/WriterDuet/assets/logo.png",
 }
 
 const enum Pages {
