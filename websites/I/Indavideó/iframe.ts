343d6c464163e40008a1eb38dd43855f570ca408
techyyyt <132097847+techyyyt@users.noreply.github.com>
2024-05-02 20:50:31 +0200

    feat(Indavideó): add presence

--- websites/I/Indavideó/iframe.ts
+++ websites/I/Indavideó/iframe.ts
@@ -1,12 +1,14 @@
 const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
-    const video: HTMLMediaElement = document.querySelector("div#mainContainer > div#player > video#html5video.video-player");
+	const video: HTMLMediaElement = document.querySelector(
+		"div#mainContainer > div#player > video#html5video.video-player"
+	);
 	if (video) {
 		iframe.send({
 			currentTime: video.currentTime,
 			paused: video.paused,
 			duration: video.duration,
 		});
-    }
+	}
 });
