d2511bb656e41224e94723d36c8fa3732116738d
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2024-05-10 21:56:08 -0700

    refactor: simplify channel name text check
    Co-authored-by: Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
    Signed-off-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

--- websites/Y/YouTube/presence.ts
+++ websites/Y/YouTube/presence.ts
@@ -327,8 +327,7 @@ presence.on("UpdateData", async () => {
 					).textContent;
 					// Get channel name when viewing a channel
 				} else if (
-					document.querySelector("#text.ytd-channel-name")?.textContent !==
-						"" &&
+					!document.querySelector("#text.ytd-channel-name")?.textContent &&
 					documentTitle.includes(
 						document.querySelector("#text.ytd-channel-name")?.textContent
 					)
