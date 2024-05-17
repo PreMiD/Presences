b0fc9da0071338d2eee7adac59f25a0f0fe34708
SeMiD <semid@premid.app>
2024-05-18 00:41:48 +0100

    chore(Game8): update assets (#8379)

--- websites/G/Game8/presence.ts
+++ websites/G/Game8/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/NPFtbsz.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/G/Game8/assets/logo.png",
 }
 
 presence.on("UpdateData", async () => {
