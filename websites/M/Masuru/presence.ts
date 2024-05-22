a2cc1ef310984151fc9aaa9861a361a9782be588
Aomsin <kittikorn.g@masuru.in.th>
2024-05-23 05:53:34 +0700

    feat: update

--- websites/M/Masuru/presence.ts
+++ websites/M/Masuru/presence.ts
@@ -1,31 +1,30 @@
 const presence = new Presence({
-		clientId: "861582205028794418",
-	}),
+	clientId: "861582205028794418",
+}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets { // Other default assets can be found at index.d.ts
-	Logo = "masuru",
-	Bot = "logo",
+	Logo = "https://cdn.masuru.in.th/storage/masuru/7f8731ab214db1aeecb6d2c23956ac72.webp",
+	Bot = "https://cdn.masuru.in.th/storage/masuru/3648d2c34ecbbaa12ece7ed230a4b3d3.webp"
 }
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			startTimestamp: browsingTimestamp,
-		},
+		startTimestamp: browsingTimestamp,
+	},
 		{ pathname, hostname, href } = document.location;
 	switch (hostname.replace("www.", "")) {
 		case "masuru.in.th": {
 			const title = document.querySelector("title").textContent;
 			if (title) {
 				presenceData.details = title;
-				(presenceData.largeImageKey = Assets.Logo),
-					// Add Button View Page
-					(presenceData.buttons = [
-						{
-							label: "View Page",
-							url: href,
-						},
-					]);
+				presenceData.largeImageKey = Assets.Logo;
+				presenceData.buttons = [
+					{
+						label: "View Page",
+						url: href,
+					},
+				];
 				presence.setActivity(presenceData);
 			}
 			break;
