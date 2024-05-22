bef90d716c9f701ac878abe80b64b182d7291755
Rag404 <tt.favnec.yt@gmail.com>
2024-05-22 17:51:09 +0200

    feat(Panzoid): add rendering progress detection for Clipmaker 2 and 3

--- websites/P/Panzoid/iframe.ts
+++ websites/P/Panzoid/iframe.ts
@@ -3,6 +3,12 @@
 const iframe = new iFrame();
 
 
+function getLegacyRenderingProgress() : number {
+	const element = document.querySelector<HTMLElement>("div.pz-progress > span");
+	return element ? Number(element.style.width.slice(0, -1)) : null;
+}
+
+
 iframe.on("UpdateData", async () => {
 	let details = "",
 		state = "";
@@ -10,18 +16,28 @@ iframe.on("UpdateData", async () => {
 	// - - - - - - - - - - - - - - - - - - - - - Clipmaker 3 - - - - - - - - - - - - - - - - - - - - -
 	if (document.location.pathname.startsWith("/legacy/gen3/clipmaker.html")) {
 		details = "Editing in Clipmaker 3";
-		state = document.querySelectorAll("span.noselect").length
-			+ " tracks | "
-			+ document.querySelectorAll(".clip").length
-			+ " clips";
+
+		const renderingProgress = getLegacyRenderingProgress();
+		if (renderingProgress === null) {
+			state = document.querySelectorAll("span.noselect").length
+				+ " tracks | "
+				+ document.querySelectorAll(".clip").length
+				+ " clips";
+		} else
+			state = `Rendering ${renderingProgress.toFixed(2)}%`;
 
 	// - - - - - - - - - - - - - - - - - - - - - Clipmaker 2 - - - - - - - - - - - - - - - - - - - - -
 	} else if (document.location.pathname.startsWith("/legacy/gen2/clipmaker.html")) {
 		details = "Editing in Clipmaker 2";
-		state = document.querySelectorAll("#controls > div:nth-child(4) > ul.pz-listbox > li").length
-			+ " objects | "
-			+ document.querySelectorAll("#controls > div:nth-child(5) > ul.pz-listbox > li").length
-			+ " effects";
+
+		const renderingProgress = getLegacyRenderingProgress();
+		if (renderingProgress === null) {
+			state = document.querySelectorAll("#controls > div:nth-child(4) > ul.pz-listbox > li").length
+				+ " objects | "
+				+ document.querySelectorAll("#controls > div:nth-child(5) > ul.pz-listbox > li").length
+				+ " effects";
+		} else
+			state = `Rendering ${renderingProgress.toFixed(2)}%`;
 	}
 
 	iframe.send({
