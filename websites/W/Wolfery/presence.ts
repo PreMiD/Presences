9e5cc77c51b869deb401b319f55c52a7b9c42b89
Shawn McHenry <drraccoony@gmail.com>
2024-05-26 11:08:32 -0500

    chore:ran prettier

--- websites/W/Wolfery/presence.ts
+++ websites/W/Wolfery/presence.ts
@@ -1,14 +1,13 @@
-	const presence = new Presence({ 
+const presence = new Presence({
 		clientId: "1244143703660953651",
 	}),
-
-browsingTimestamp = Math.floor(Date.now() / 1000);
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets { // Other default assets can be found at index.d.ts
 	Logo = "https://forum.wolfery.com/uploads/default/original/1X/96ef8d56ab7b19e7f69cf42fc9109c98bf93e588.png",
 }
 
-// TODO: Add LFRP Flag (v1.1.0) 
+// TODO: Add LFRP Flag (v1.1.0)
 // TODO: Find potential links for adding presence button to open character sheet. (v1.1.0)
 
 const detailMsg = "Roleplaying on Wolfery.com";
@@ -16,7 +15,7 @@ let stateMsg = "";
 
 presence.on("UpdateData", async () => {
 	const [characterPrivacy] = await Promise.all([
-		presence.getSetting<boolean>("characterPrivacy")
+		presence.getSetting<boolean>("characterPrivacy"),
 	]);
 
 	let characterName = document.querySelector(".namesection--title").textContent;
@@ -24,13 +23,12 @@ presence.on("UpdateData", async () => {
 	if (characterPrivacy) {
 		stateMsg = "";
 		characterName = "";
-	} else 
-		stateMsg = `Playing As: ${characterName}`;
+	} else stateMsg = `Playing As: ${characterName}`;
 
 	presence.setActivity({
 		details: detailMsg,
 		state: stateMsg,
 		largeImageKey: Assets.Logo,
 		startTimestamp: browsingTimestamp,
-	}); 
+	});
 });
