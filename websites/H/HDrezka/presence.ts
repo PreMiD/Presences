7ef4b3d6347f407cfaf13c60f3a3d8caa5306611
envelope <42vddg@gmail.com>
2024-06-05 20:19:25 +0300

    add presence

---
+++ websites/H/HDrezka/presence.ts
@@ -0,0 +1,48 @@
+const presence = new Presence({
+	clientId: "1191450515670843533",
+});
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		details: "Где-то на сайте",
+		largeImageKey: "https://cdn.discordapp.com/app-assets/1191450515670843533/1247862870276833292.png"
+	};
+
+	const contentType = document
+		.querySelector("meta[property='og:url']")
+		.getAttribute("content")
+		.split("/")[3];
+
+	const currentType = contentType === "films" ? "фильм"
+		: contentType === "series" ? "сериал"
+			: contentType === "cartoons" ? "мультфильм" : "чего-то"
+
+	if (document.location.pathname === "/") {
+		presenceData.details = "На главной странице"
+	}
+	if (
+		document.location.pathname === "/films" ||
+		document.location.pathname === "/series" ||
+		document.location.pathname === "/cartoons" ||
+		document.location.pathname.match(/\/(films|series|cartoons)\//)
+	) {
+		if (document.location.pathname.match(/\/(films|series|cartoons)\/.+/)) {
+			const contentTitle = document.querySelector(".b-post__title h1").textContent;
+			const image = document.querySelector<HTMLImageElement>(".b-sidecover a img").src;
+			presenceData.details = `Смотрит ${currentType}`;
+			presenceData.state = `${contentTitle}`
+			presenceData.largeImageKey = image;
+			presenceData.smallImageKey = "https://cdn.discordapp.com/app-assets/1191450515670843533/1247862870276833292.png";
+			presenceData.buttons = [{
+				label: "Открыть страницу",
+				url: document.querySelector("meta[property='og:url']").getAttribute("content")
+			}];
+		} else {
+			presenceData.details = `В поиске ${currentType + "a"}`;
+			presenceData.smallImageKey = "https://cdn.discordapp.com/app-assets/1191450515670843533/1191452075100479498.png";
+		}
+	}
+	presence.setActivity(presenceData);
+});
+
+
