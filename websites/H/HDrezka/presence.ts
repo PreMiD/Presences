73a03b819ed2db0b46228b418b37e9260edd3832
envelope <42vddg@gmail.com>
2024-06-05 21:28:29 +0300

    format code with prettier

--- websites/H/HDrezka/presence.ts
+++ websites/H/HDrezka/presence.ts
@@ -4,18 +4,22 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		details: "Где-то на сайте",
-		largeImageKey: "https://cdn.discordapp.com/app-assets/1191450515670843533/1247862870276833292.png"
-	},
-
+			details: "Где-то на сайте",
+			largeImageKey:
+				"https://cdn.discordapp.com/app-assets/1191450515670843533/1247862870276833292.png",
+		},
 		contentType = document
 			.querySelector("meta[property='og:url']")
 			.getAttribute("content")
 			.split("/")[3],
-
-		currentType = contentType === "films" ? "фильм"
-			: contentType === "series" ? "сериал"
-				: contentType === "cartoons" ? "мультфильм" : "чего-то";
+		currentType =
+			contentType === "films"
+				? "фильм"
+				: contentType === "series"
+				? "сериал"
+				: contentType === "cartoons"
+				? "мультфильм"
+				: "чего-то";
 
 	if (document.location.pathname === "/")
 		presenceData.details = "На главной странице";
@@ -28,16 +32,25 @@ presence.on("UpdateData", async () => {
 	) {
 		if (document.location.pathname.match(/\/(films|series|cartoons)\/.+/)) {
 			presenceData.details = `Смотрит ${currentType}`;
-			presenceData.state = `${document.querySelector(".b-post__title h1").textContent}`;
-			presenceData.largeImageKey = document.querySelector<HTMLImageElement>(".b-sidecover a img").src;
-			presenceData.smallImageKey = "https://cdn.discordapp.com/app-assets/1191450515670843533/1247862870276833292.png";
-			presenceData.buttons = [{
-				label: "Открыть страницу",
-				url: document.querySelector("meta[property='og:url']").getAttribute("content")
-			}];
+			presenceData.state = `${
+				document.querySelector(".b-post__title h1").textContent
+			}`;
+			presenceData.largeImageKey =
+				document.querySelector<HTMLImageElement>(".b-sidecover a img").src;
+			presenceData.smallImageKey =
+				"https://cdn.discordapp.com/app-assets/1191450515670843533/1247862870276833292.png";
+			presenceData.buttons = [
+				{
+					label: "Открыть страницу",
+					url: document
+						.querySelector("meta[property='og:url']")
+						.getAttribute("content"),
+				},
+			];
 		} else {
 			presenceData.details = `В поиске ${currentType}a`;
-			presenceData.smallImageKey = "https://cdn.discordapp.com/app-assets/1191450515670843533/1191452075100479498.png";
+			presenceData.smallImageKey =
+				"https://cdn.discordapp.com/app-assets/1191450515670843533/1191452075100479498.png";
 		}
 	}
 	presence.setActivity(presenceData);
