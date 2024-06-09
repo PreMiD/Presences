765d797dbd915fffe4c98be8ae90aceed3bbe182
kuba <dornicakkuba@gmail.com>
2024-06-09 16:19:02 +0200

    style: 💄 Lint

--- websites/D/daily.dev/presence.ts
+++ websites/D/daily.dev/presence.ts
@@ -1,6 +1,6 @@
 const presence = new Presence({
-	clientId: "1248697988331864084",
-}),
+		clientId: "1248697988331864084",
+	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
@@ -8,96 +8,95 @@ const enum Assets {
 }
 
 const processPostSite = (): Partial<PresenceData> => {
-	return {
-		details: "Reading a post",
-		state: document.querySelector("h1")?.textContent,
-		buttons: [
-			{
-				label: "Read Post",
-				url: document.location.href
-			}
-		]
-	};
-},
-
+		return {
+			details: "Reading a post",
+			state: document.querySelector("h1")?.textContent,
+			buttons: [
+				{
+					label: "Read Post",
+					url: document.location.href,
+				},
+			],
+		};
+	},
 	processSourceSite = () => {
 		return {
 			details: "Reading a source",
-			state: `Source: ${document.querySelector("h1")?.textContent}`
+			state: `Source: ${document.querySelector("h1")?.textContent}`,
 		};
 	},
-
 	processSearchSite = () => {
 		return {
 			details: "Searching",
-			state: `Searching for ${new URLSearchParams(document.location.search).get("search")}`
+			state: `Searching for ${new URLSearchParams(document.location.search).get(
+				"search"
+			)}`,
 		};
 	},
-
 	siteDataMap = [
 		{
 			path: /^\/$/,
 			process: () => ({
 				details: "Homepage",
-				state: "Discovering new posts"
-			})
+				state: "Discovering new posts",
+			}),
 		},
 		{
 			path: /^\/posts\/[a-z0-9-]+$/,
-			process: processPostSite
+			process: processPostSite,
 		},
 		{
 			path: /^\/squads$/,
 			process: () => ({
 				details: "Squads page",
-				state: "Discovering new squads"
-			})
+				state: "Discovering new squads",
+			}),
 		},
 		{
 			path: /^\/discussed$/,
 			process: () => ({
 				details: "Discussion homepage",
-				state: "Discovering new discussions"
-			})
+				state: "Discovering new discussions",
+			}),
 		},
 		{
 			path: /^\/bookmarks$/,
 			process: () => ({
 				details: "Bookmarks",
-				state: "Discovering saved posts"
-			})
+				state: "Discovering saved posts",
+			}),
 		},
 		{
 			path: /^\/popular$/,
 			process: () => ({
 				details: "Popular posts",
-				state: "Discovering popular posts"
-			})
+				state: "Discovering popular posts",
+			}),
 		},
 		{
 			path: /^\/sources\/.*$/,
-			process: processSourceSite
+			process: processSourceSite,
 		},
 		{
 			path: /^\/upvoted\/.*$/,
 			process: () => ({
 				details: "Upvoted posts",
-				state: "Discovering upvoted posts"
-			})
+				state: "Discovering upvoted posts",
+			}),
 		},
 		{
 			path: /^\/search$/,
 			process: () => ({
 				details: "Searching",
-				process: processSearchSite
-			})
+				process: processSearchSite,
+			}),
 		},
 		{
 			path: /^\/history\/.*$/,
 			process: () => ({
 				details: "History",
-				state: "Browsing personal history"
-			})
+				state: "Browsing personal history",
+			}),
 		},
 	];
 
@@ -107,14 +106,14 @@ presence.on("UpdateData", async () => {
 		startTimestamp: browsingTimestamp,
 	};
 
-	const siteData = siteDataMap.find(
-		(siteData) => siteData.path.test(document.location.pathname)
+	const siteData = siteDataMap.find(siteData =>
+		siteData.path.test(document.location.pathname)
 	);
 
 	if (siteData) {
 		presenceData = {
 			...presenceData,
-			...siteData.process()
+			...siteData.process(),
 		};
 	}
 
