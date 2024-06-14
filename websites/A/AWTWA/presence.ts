009226a7ca0cfbf62a7591a3c3e69c33978642d6
git <128653732+emberantz@users.noreply.github.com>
2024-06-15 01:30:41 +0200

    fix(some files): why there is commit linting

--- websites/A/AWTWA/presence.ts
+++ websites/A/AWTWA/presence.ts
@@ -6,16 +6,15 @@ const enum Assets {
 	Logo = "awtwa_logo",
 }
 
-const animeBrowsingTimestamp = Math.floor(Date.now() / 1000);
-
-const presenceStrings = {
-	home: "Se balade sur la page d'accueil",
-	browsing: "Utilise son haki de l'observation sur le catalogue",
-	branding: "Tous vos animes gratuits et en haute qualité sur AWTWA.site",
-	websiteName: "AWTWA.site",
-	onPage: "En train de regarder ",
-	watchOn: "Regarder sur AWTWA.site",
-};
+const animeBrowsingTimestamp = Math.floor(Date.now() / 1000),
+	presenceStrings = {
+		home: "Se balade sur la page d'accueil",
+		browsing: "Utilise son haki de l'observation sur le catalogue",
+		branding: "Tous vos animes gratuits et en haute qualité sur AWTWA.site",
+		websiteName: "AWTWA.site",
+		onPage: "En train de regarder ",
+		watchOn: "Regarder sur AWTWA.site",
+	};
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {},
@@ -35,13 +34,20 @@ presence.on("UpdateData", async () => {
 		presenceData.smallImageKey = Assets.Search;
 		presenceData.startTimestamp = animeBrowsingTimestamp;
 	} else if (pathname.includes("/animes")) {
-		const animeName = document.querySelector("h1")?.textContent;
-		const poster = document.querySelector("div[role='img']")?.getAttribute("style").split("url(")[1].split(")")[0].replace(/['"]/g, "");
-		const episode = (document.getElementById("episode") as HTMLSelectElement).value;
-		const season = (document.getElementById("season") as HTMLSelectElement).value;
+		const poster = document
+			.querySelector("div[role='img']")
+			?.getAttribute("style")
+			.split("url(")[1]
+			.split(")")[0]
+			.replace(/['"]/g, "");
 
-		presenceData.details = presenceStrings.onPage + animeName;
-		presenceData.state = "Saison " + season + " - Épisode " + episode;
+		presenceData.details =
+			presenceStrings.onPage + document.querySelector("h1")?.textContent;
+		presenceData.state = `Saison ${
+			(document.querySelector("#season") as HTMLSelectElement).value
+		} - Épisode ${
+			(document.querySelector("#episode") as HTMLSelectElement).value
+		}`;
 		presenceData.smallImageText = presenceStrings.watchOn;
 		presenceData.largeImageKey = poster;
 		presenceData.smallImageKey = Assets.Play;
@@ -53,7 +59,7 @@ presence.on("UpdateData", async () => {
 			},
 		];
 	}
-	
+
 	presenceData.type = ActivityType.Watching;
 	presence.setActivity(presenceData);
 });
