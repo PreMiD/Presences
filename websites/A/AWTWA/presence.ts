2bbb48ad6fd5eb6678c0f388d1b05f837a12df80
git <128653732+emberantz@users.noreply.github.com>
2024-06-15 00:51:28 +0200

    Add AWTWA presence

---
+++ websites/A/AWTWA/presence.ts
@@ -0,0 +1,59 @@
+const presence = new Presence({
+	clientId: "1249362206072635483",
+});
+
+const enum Assets {
+	Logo = "awtwa_logo",
+}
+
+const animeBrowsingTimestamp = Math.floor(Date.now() / 1000);
+
+const presenceStrings = {
+	home: "Se balade sur la page d'accueil",
+	browsing: "Utilise son haki de l'observation sur le catalogue",
+	branding: "Tous vos animes gratuits et en haute qualité sur AWTWA.site",
+	websiteName: "AWTWA.site",
+	onPage: "En train de regarder ",
+	watchOn: "Regarder sur AWTWA.site",
+};
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {},
+		{ pathname, href } = document.location;
+
+	if (pathname === "/") {
+		presenceData.details = presenceStrings.home;
+		presenceData.state = presenceStrings.branding;
+		presenceData.smallImageText = presenceStrings.websiteName;
+		presenceData.largeImageKey = Assets.Logo;
+		presenceData.smallImageKey = Assets.Viewing;
+	} else if (pathname === "/catalog") {
+		presenceData.details = presenceStrings.browsing;
+		presenceData.state = presenceStrings.branding;
+		presenceData.smallImageText = presenceStrings.websiteName;
+		presenceData.largeImageKey = Assets.Logo;
+		presenceData.smallImageKey = Assets.Search;
+		presenceData.startTimestamp = animeBrowsingTimestamp;
+	} else if (pathname.includes("/animes")) {
+		const animeName = document.querySelector("h1")?.textContent;
+		const poster = document.querySelector("div[role='img']")?.getAttribute("style").split("url(")[1].split(")")[0].replace(/['"]/g, "");
+		const episode = (document.getElementById("episode") as HTMLSelectElement).value;
+		const season = (document.getElementById("season") as HTMLSelectElement).value;
+
+		presenceData.details = presenceStrings.onPage + animeName;
+		presenceData.state = "Saison " + season + " - Épisode " + episode;
+		presenceData.smallImageText = presenceStrings.watchOn;
+		presenceData.largeImageKey = poster;
+		presenceData.smallImageKey = Assets.Play;
+
+		presenceData.buttons = [
+			{
+				label: presenceStrings.watchOn,
+				url: href,
+			},
+		];
+	}
+	
+	presenceData.type = ActivityType.Watching;
+	presence.setActivity(presenceData);
+});
