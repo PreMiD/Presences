907a6be8f12d3fa20612d7446d74122e5e914479
git <128653732+emberantz@users.noreply.github.com>
2024-06-15 03:03:10 +0200

    fix(iframe): fixed iframe not finding video

--- websites/A/AWTWA/presence.ts
+++ websites/A/AWTWA/presence.ts
@@ -6,6 +6,12 @@ const enum Assets {
 	Logo = "awtwa_logo",
 }
 
+let iFrameData = {
+	currentTime: -1,
+	duration: -1,
+	paused: true,
+};
+
 const animeBrowsingTimestamp = Math.floor(Date.now() / 1000),
 	presenceStrings = {
 		home: "Se balade sur la page d'accueil",
@@ -15,6 +21,13 @@ const animeBrowsingTimestamp = Math.floor(Date.now() / 1000),
 		onPage: "En train de regarder ",
 		watchOn: "Regarder sur AWTWA.site",
 	};
+
+presence.on(
+	"iFrameData",
+	(data: { currentTime: number; duration: number; paused: boolean }) => {
+		iFrameData = data;
+	}
+);
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {},
@@ -35,29 +48,60 @@ presence.on("UpdateData", async () => {
 		presenceData.startTimestamp = animeBrowsingTimestamp;
 	} else if (pathname.includes("/animes")) {
 		const poster = document
-			.querySelector("div[role='img']")
-			?.getAttribute("style")
-			.split("url(")[1]
-			.split(")")[0]
-			.replace(/['"]/g, "");
+				.querySelector("div[role='img']")
+				?.getAttribute("style")
+				.split("url(")[1]
+				.split(")")[0]
+				.replace(/['"]/g, ""),
+			isPrivacyMode = await presence.getSetting("privacy");
 
-		presenceData.details =
-			presenceStrings.onPage + document.querySelector("h1")?.textContent;
-		presenceData.state = `Saison ${
-			(document.querySelector("#season") as HTMLSelectElement).value
-		} - Épisode ${
-			(document.querySelector("#episode") as HTMLSelectElement).value
-		}`;
-		presenceData.smallImageText = presenceStrings.watchOn;
-		presenceData.largeImageKey = poster;
-		presenceData.smallImageKey = Assets.Play;
+		if (isPrivacyMode) {
+			presenceData.details = `${presenceStrings.onPage}un anime`;
+			presenceData.state = presenceStrings.branding;
+			presenceData.smallImageText = presenceStrings.watchOn;
+			presenceData.largeImageKey = Assets.Logo;
+			presenceData.smallImageKey = Assets.Play;
+		} else {
+			presenceData.details =
+				presenceStrings.onPage + document.querySelector("h1")?.textContent;
+			presenceData.state = `Saison ${
+				(document.querySelector("#season") as HTMLSelectElement).value
+			} - Épisode ${
+				(document.querySelector("#episode") as HTMLSelectElement).value
+			}`;
+			presenceData.smallImageText = presenceStrings.watchOn;
+			presenceData.largeImageKey = poster;
+			presenceData.smallImageKey = Assets.Play;
 
-		presenceData.buttons = [
-			{
-				label: presenceStrings.watchOn,
-				url: href,
-			},
-		];
+			presenceData.details =
+				presenceStrings.onPage + document.querySelector("h1")?.textContent;
+			presenceData.state = `Saison ${
+				(document.querySelector("#season") as HTMLSelectElement).value
+			} - Épisode ${
+				(document.querySelector("#episode") as HTMLSelectElement).value
+			}`;
+			presenceData.smallImageText = presenceStrings.watchOn;
+			presenceData.largeImageKey = poster;
+			presenceData.smallImageKey = Assets.Play;
+
+			if (!iFrameData?.paused) {
+				[, presenceData.endTimestamp] = presence.getTimestamps(
+					iFrameData.currentTime,
+					iFrameData.duration
+				);
+				presenceData.smallImageKey = Assets.Play;
+			} else {
+				presenceData.smallImageKey = Assets.Pause;
+				delete presenceData.endTimestamp;
+			}
+
+			presenceData.buttons = [
+				{
+					label: presenceStrings.watchOn,
+					url: href,
+				},
+			];
+		}
 	}
 
 	presenceData.type = ActivityType.Watching;
