d000546279f5dfa1e8a4b6a100299be41984c376
SHISEN <alex-ench@yandex.ru>
2024-06-23 04:31:52 +0400

    chore: adjust logo

--- websites/0-9/7tv/presence.ts
+++ websites/0-9/7tv/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/CKgSFXQ.png",
+	Logo = "https://i.imgur.com/gSIeY1b.png",
 }
 
 async function getStrings() {
