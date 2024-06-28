12bdf1773d97b50a5d1b36da02795e1188d6e752
SeMiD <semid@premid.app>
2024-06-28 14:22:49 +0100

    chore(7tv): update assets (#8536)
    chore: update assets
    
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/0-9/7tv/presence.ts
+++ websites/0-9/7tv/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/gSIeY1b.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/0-9/7tv/assets/logo.png",
 }
 
 async function getStrings() {
