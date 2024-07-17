a7dde8f10c50dbbeeb6e3ade416ea332867c635c
Abdulbaki Dursun <eggsydev@gmail.com>
2024-07-17 21:47:05 +0300

    chore: 512x512 logo

--- websites/P/Papara Topluluk/presence.ts
+++ websites/P/Papara Topluluk/presence.ts
@@ -30,7 +30,7 @@ const presence = new Presence({
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
 			largeImageKey:
-				"https://i.imgur.com/3L9gPyD.png",
+				"https://i.imgur.com/3zExiGz.png",
 			startTimestamp: dateNow,
 		},
 		page = document.location.pathname,
