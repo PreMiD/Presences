f3956e98a1c3aef8f1651f9ea94f8fc33f2daa29
1intan99 <41572020+1intan99@users.noreply.github.com>
2024-08-14 14:50:18 +0700

    feat(KomikIndo): improve presence functionality and update logo url

--- websites/K/Komik Indo/presence.ts
+++ websites/K/Komik Indo/presence.ts
@@ -4,7 +4,12 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/HccQ60t.png",
+	Logo = "https://i.imgur.com/YY8ZLqd.png",
+}
+
+function getTitleText(selector: string): string {
+	const element = document.querySelector(selector);
+	return element ? element.textContent.replace(/\t|\n/g, "").trim() : "Unknown";
 }
 
 presence.on("UpdateData", async () => {
@@ -13,115 +18,85 @@ presence.on("UpdateData", async () => {
 			largeImageKey: Assets.Logo,
 			startTimestamp: browsingTimestamp,
 		},
-		{ pathname, href, hostname } = document.location,
+		{ pathname, href } = document.location,
 		buttons = await presence.getSetting<boolean>("buttons");
-	switch (true) {
-		case hostname === "komikindo.tv":
-			presenceData.details = "Viewing Homepage";
-			break;
-		case pathname.endsWith("/daftar-komik/"):
-			presenceData.details = "Viewing Popular Comics List";
-			break;
-		case pathname.endsWith("/komik-terbaru/"):
-			presenceData.details = "Viewing Latest Comics List";
-			break;
-		case pathname.endsWith("/komik-berwarna/"):
-			presenceData.details = "Viewing Colored Comics List";
-			break;
-		case pathname.endsWith("/daftar-manga"):
-			presenceData.details = "Viewing Manga List";
-			break;
-		case pathname.endsWith("/manga/"):
-			presenceData.details = "Viewing Manga";
-			break;
-		case pathname.endsWith("/manhwa/"):
-			presenceData.details = "Viewing Manhwa";
-			break;
-		case pathname.endsWith("/manhua/"):
-			presenceData.details = "Viewing Manhua";
-			break;
-		case pathname.endsWith("/akun/"):
-			presenceData.details = "Viewing Account";
-			break;
-		case pathname.endsWith("/iklan/"):
-			presenceData.details = "Contact Komikindo";
-			break;
-		case pathname.endsWith("/konten/ecchi/"):
-			presenceData.details = "Viewing Ecchi Content";
-			break;
-		case href.includes("?s="): {
-			const searchElement = document
-				.querySelector("div.widget-title h1")
-				.textContent.replace("Komik Hasil Pencarian ", "")
-				.replace(/\t|\n/g, "");
-			presenceData.state = `Searching: ${searchElement}`;
-			presenceData.smallImageKey = Assets.Search;
-			presenceData.details = "Searching for Comics";
-			break;
+
+	if (href.includes("?s=")) {
+		presenceData.state = `Searching: ${getTitleText(
+			"div.widget-title h1"
+		).replace("Komik Hasil Pencarian ", "")}`;
+		presenceData.smallImageKey = Assets.Search;
+		presenceData.details = "Searching for Comics";
+	} else if (pathname === "/") presenceData.details = "Viewing Homepage";
+	else if (pathname.endsWith("/daftar-komik/"))
+		presenceData.details = "Viewing Popular Comics List";
+	else if (pathname.endsWith("/komik-terbaru/"))
+		presenceData.details = "Viewing Latest Comics List";
+	else if (pathname.endsWith("/komik-berwarna/"))
+		presenceData.details = "Viewing Colored Comics List";
+	else if (pathname.endsWith("/daftar-manga"))
+		presenceData.details = "Viewing Manga List";
+	else if (pathname.endsWith("/manga/")) presenceData.details = "Viewing Manga";
+	else if (pathname.endsWith("/manhwa/"))
+		presenceData.details = "Viewing Manhwa";
+	else if (pathname.endsWith("/manhua/"))
+		presenceData.details = "Viewing Manhua";
+	else if (pathname.endsWith("/akun/"))
+		presenceData.details = "Viewing Account";
+	else if (pathname.endsWith("/iklan/"))
+		presenceData.details = "Contact Komikindo";
+	else if (pathname.endsWith("/konten/ecchi/"))
+		presenceData.details = "Viewing Ecchi Content";
+	else if (pathname.startsWith("/komik/")) {
+		const titleElement = getTitleText("h1.entry-title").replace("Komik", "");
+		presenceData.details = `Viewing: ${titleElement}`;
+
+		let author = "Unknown",
+			comicType = "Unknown";
+
+		for (const element of document.querySelectorAll(
+			"div.infox > div.spe > span > b"
+		)) {
+			if (element.textContent.includes("Pengarang:"))
+				author = element.nextSibling.textContent.trim();
+			if (element.textContent.includes("Jenis Komik:"))
+				comicType = element.nextElementSibling.textContent.trim();
 		}
-		case pathname.startsWith("/komik/"): {
-			const titleElement = document
-				.querySelector("h1.entry-title")
-				.textContent.replace("Komik", "")
-				.replace(/\t|\n/g, "");
-			presenceData.details = `Viewing: ${titleElement}`;
 
-			const infoElements = document.querySelectorAll(
-				"div.infox > div.spe > span > b"
-			);
-			let author = "Unknown",
-				comicType = "Unknown";
-
-			for (const element of infoElements) {
-				if (element.textContent.includes("Pengarang:"))
-					author = element.nextSibling.textContent.trim();
-
-				if (element.textContent.includes("Jenis Komik:"))
-					comicType = element.nextElementSibling.textContent.trim();
-			}
-
-			presenceData.state = `Author: ${author} | Type: ${comicType}`;
-			presenceData.smallImageText = titleElement;
-			if (buttons) {
-				presenceData.buttons = [
-					{
-						label: "View Comic",
-						url: href,
-					},
-				];
-			}
-			break;
+		presenceData.state = `Author: ${author} | Type: ${comicType}`;
+		presenceData.smallImageText = titleElement;
+		if (buttons) {
+			presenceData.buttons = [
+				{
+					label: "View Comic",
+					url: href,
+				},
+			];
 		}
-		case href.includes("chapter-"): {
-			const titleElement = document
-				.querySelector("div.dtlx > h1.entry-title")
-				.textContent.replace("Komik", "")
-				.replace(/\t|\n/g, "");
+	} else if (href.includes("chapter-")) {
+		const titleElement = getTitleText("div.dtlx > h1.entry-title").replace(
+			"Komik",
+			""
+		);
+		presenceData.details = titleElement;
+		presenceData.state = "Reading Chapter";
+		presenceData.smallImageText = titleElement;
 
-			presenceData.details = titleElement;
-			presenceData.state = "Reading Chapter";
-			presenceData.smallImageText = titleElement;
-
-			if (buttons) {
-				presenceData.buttons = [
-					{
-						label: "Read Chapter",
-						url: href,
-					},
-					{
-						label: "View Comic",
-						url: document
-							.querySelectorAll("div.nextprev > a")[1]
-							.getAttribute("href"),
-					},
-				];
-			}
-			break;
+		if (buttons) {
+			presenceData.buttons = [
+				{
+					label: "Read Chapter",
+					url: href,
+				},
+				{
+					label: "View Comic",
+					url: document
+						.querySelectorAll("div.nextprev > a")[1]
+						.getAttribute("href"),
+				},
+			];
 		}
-		default:
-			presenceData.details = "Browsing...";
-			break;
-	}
+	} else presenceData.details = "Browsing...";
 
 	presence.setActivity(presenceData);
 });
