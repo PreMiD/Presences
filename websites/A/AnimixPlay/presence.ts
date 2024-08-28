5dd515d884c3c8d5e0d8c9f568e2743ebfe03aa7
safu.69 <Safwan55.ah@gmail.com>
2024-08-29 00:05:23 +0600

    chore(AnimixPlay): formatted files with Prettier

--- websites/A/AnimixPlay/presence.ts
+++ websites/A/AnimixPlay/presence.ts
@@ -6,7 +6,6 @@ const presence = new Presence({
 		play: "presence.playback.playing",
 		pause: "presence.playback.paused",
 	}),
-
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets { // Other default assets can be found at index.d.ts
@@ -18,46 +17,45 @@ let video = {
 	currentTime: 0,
 };
 
-presence.on(
-	"iFrameData",
-	(data: {
-		duration: number;
-		currentTime: number;
-	}) => {
-		video = data;
-	}
-);
+presence.on("iFrameData", (data: { duration: number; currentTime: number }) => {
+	video = data;
+});
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = { 
-		largeImageKey: Assets.Logo,
-		buttons: [
-			{
+	const presenceData: PresenceData = {
+			largeImageKey: Assets.Logo,
+			buttons: [
+				{
 					label: "Visit AnimixPlay",
-					url: "https://animixplay.fun/"
-			}
-			]
-	},
+					url: "https://animixplay.fun/",
+				},
+			],
+		},
+		{ href } = document.location;
 
-	{href} = document.location;
-
-	if (href.startsWith("https://animixplay.fun/") || href.startsWith("https://animixplay.best/")) 
+	if (
+		href.startsWith("https://animixplay.fun/") ||
+		href.startsWith("https://animixplay.best/")
+	)
 		presenceData.details = "Browsing Anime";
-		presenceData.startTimestamp = browsingTimestamp;
+	presenceData.startTimestamp = browsingTimestamp;
 
 	if (href.startsWith("https://www1.animixplayer.top/")) {
 		const [startTimestamp, endTimestamp] = presence.getTimestamps(
-			Math.floor(video.currentTime),
-			Math.floor(video.duration)
-		),
-
-		// eslint-disable-next-line unicorn/prefer-query-selector
-		TitleAndEpisode = document.getElementsByClassName("animetitle")[0].innerHTML;
+				Math.floor(video.currentTime),
+				Math.floor(video.duration)
+			),
+			TitleAndEpisode =
+				// eslint-disable-next-line unicorn/prefer-query-selector
+				document.getElementsByClassName("animetitle")[0].innerHTML;
 
 		presenceData.startTimestamp = startTimestamp;
 		presenceData.endTimestamp = endTimestamp;
 
-		presenceData.details = `Watching ${TitleAndEpisode.replace(/Episode [0-9]+/, "")}`;
+		presenceData.details = `Watching ${TitleAndEpisode.replace(
+			/Episode [0-9]+/,
+			""
+		)}`;
 		presenceData.state = `On ${TitleAndEpisode.match(/Episode [0-9]+/)}`;
 
 		presenceData.smallImageKey = Assets.Play;
