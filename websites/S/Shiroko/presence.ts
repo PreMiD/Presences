da630f4e4a3d49798f229ac051454d3f86a8c62b
kaisei-kto <skaisei@aol.com>
2024-09-20 17:37:34 -0400

    yarn formatted

--- websites/S/Shiroko/presence.ts
+++ websites/S/Shiroko/presence.ts
@@ -1,30 +1,29 @@
 import { getEpisode, getId, getImage, getTitle } from "./utils";
 
 const presence = new Presence({
-	clientId: "1286500193881161804"
-}),
+		clientId: "1286500193881161804",
+	}),
 	DOMAIN = "https://shiroko.co";
 
 let lastPlaybackState = false,
 	playback = false,
 	startTimestamp = Math.floor(Date.now() / 1e3),
-	lastHref : string,
+	lastHref: string,
 	lastUpdate = 0;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		buttons: [ { label: "Visit Shiroko", url: "https://shiroko.co" } ],
-		startTimestamp
-	},
-	video = document.body.querySelectorAll("video")[0];
+			buttons: [{ label: "Visit Shiroko", url: "https://shiroko.co" }],
+			startTimestamp,
+		},
+		video = document.body.querySelectorAll("video")[0];
 
 	if (video) {
 		presenceData.type = ActivityType.Watching;
 		playback = !video.paused && !video.ended;
 
-		if (lastPlaybackState !== playback) 
+		if (lastPlaybackState !== playback)
 			startTimestamp = Math.floor(Date.now() / 1000);
-		
 
 		presenceData.smallImageKey = playback ? Assets.Play : Assets.Pause;
 		presenceData.smallImageText = playback ? "Playing" : "Paused";
@@ -36,34 +35,55 @@ presence.on("UpdateData", async () => {
 	presenceData.endTimestamp = null;
 
 	const { pathname, href } = location;
-	
-	if (pathname.startsWith("/en/schedule")) presenceData.details = "Viewing Schedule...", presenceData.buttons.unshift({ label: "View Schedule", url: `${DOMAIN}/en/schedule` });
-	if (pathname.includes("/search/manga")) presenceData.details = "Searching Manga...";
-	if (pathname.includes("/search/anime")) presenceData.details = "Searching Anime...";
-	if (href.includes("/search/anime?season=")) presenceData.details = "Viewing Season...";
-	if (pathname.startsWith("/en/anime/") && !pathname.startsWith("/en/anime/watch")) {
+
+	if (pathname.startsWith("/en/schedule")) {
+		presenceData.details = "Viewing Schedule...";
+		presenceData.buttons.unshift({
+			label: "View Schedule",
+			url: `${DOMAIN}/en/schedule`,
+		});
+	}
+	if (pathname.includes("/search/manga"))
+		presenceData.details = "Searching Manga...";
+	if (pathname.includes("/search/anime"))
+		presenceData.details = "Searching Anime...";
+	if (href.includes("/search/anime?season="))
+		presenceData.details = "Viewing Season...";
+	if (
+		pathname.startsWith("/en/anime/") &&
+		!pathname.startsWith("/en/anime/watch")
+	) {
 		presenceData.details = `${getTitle()}`;
 		presenceData.state = "Viewing Anime...";
 
-		presenceData.buttons.unshift({ label: "View Anime", url: `${DOMAIN}/en/anime/${getId()}` });
-	} else
-	if (pathname.startsWith("/en/manga/")) {
+		presenceData.buttons.unshift({
+			label: "View Anime",
+			url: `${DOMAIN}/en/anime/${getId()}`,
+		});
+	} else if (pathname.startsWith("/en/manga/")) {
 		presenceData.details = `${getTitle()}`;
 		presenceData.state = "Viewing Manga...";
 
-		presenceData.buttons.unshift({ label: "View Manga", url: `${DOMAIN}/en/manga/${getId()}` });
-	} else
-	if (typeof presenceData.details !== "string") presenceData.details = "Viewing Home...";
+		presenceData.buttons.unshift({
+			label: "View Manga",
+			url: `${DOMAIN}/en/manga/${getId()}`,
+		});
+	} else if (typeof presenceData.details !== "string")
+		presenceData.details = "Viewing Home...";
 
 	if (pathname.startsWith("/en/anime/watch")) {
-		
 		const episode = getEpisode();
-		
+
 		presenceData.details = `${getTitle()}`;
-		presenceData.state = !isNaN(episode) ? `Episode ${episode}` : "Unable to retrieve episode";
+		presenceData.state = !isNaN(episode)
+			? `Episode ${episode}`
+			: "Unable to retrieve episode";
 		presenceData.type = ActivityType.Watching;
-		
-		presenceData.buttons.unshift({ label: "Watch Anime", url: `${DOMAIN}/en/anime/watch?id=${getId()}&n=${episode}` });
+
+		presenceData.buttons.unshift({
+			label: "Watch Anime",
+			url: `${DOMAIN}/en/anime/watch?id=${getId()}&n=${episode}`,
+		});
 	}
 
 	if (pathname.startsWith("/en/profile/")) {
@@ -72,10 +92,12 @@ presence.on("UpdateData", async () => {
 			const username = split.pop();
 
 			presenceData.details = `Viewing ${username}'s Profile`;
-			presenceData.buttons.unshift({ label: "View Profile", url: `${DOMAIN}/en/profile/${username}` });
+			presenceData.buttons.unshift({
+				label: "View Profile",
+				url: `${DOMAIN}/en/profile/${username}`,
+			});
 		}
 	}
-
 
 	const now = Date.now();
 	if (lastHref !== href || (now - lastUpdate) / 1e3 >= 1) {
--- websites/S/Shiroko/utils.ts
+++ websites/S/Shiroko/utils.ts
@@ -1,45 +1,49 @@
-export function getImage() : string | undefined {
-	for (const img of document.images) 
-		if (["poster anime", "Poster Anime", "Anime Cover"].includes(String(img.attributes.getNamedItem("alt")?.value))) return String(img.src);
+export function getImage(): string | undefined {
+	for (const img of document.images) {
+		if (["poster anime", "Poster Anime", "Anime Cover"].includes(String(img.attributes.getNamedItem("alt")?.value)))
+			return String(img.src);
+	}
 }
 
 export function getTitle() {
 	const nt = document.querySelectorAll(".title-nt")[0],
-	 	  ro = document.querySelectorAll(".title-rm")[0],
-		  en = document.querySelectorAll(".title-en")[0];
+		ro = document.querySelectorAll(".title-rm")[0],
+		en = document.querySelectorAll(".title-en")[0];
 
 	if (en) return en.innerHTML;
 	if (ro) return ro.innerHTML;
 	if (nt) return nt.innerHTML;
 
 	for (const div of document.querySelectorAll("div")) {
-		if (div.className === "flex flex-col gap-1 text-center md:text-start w-full") {
-			for (const element of div.children) {
-				if (element.tagName.toLowerCase() === "h1") 
-					return element.innerHTML;
-				
-			}
+		if (
+			div.className === "flex flex-col gap-1 text-center md:text-start w-full"
+		) {
+			for (const element of div.children) 
+				if (element.tagName.toLowerCase() === "h1") return element.innerHTML;
+			
 		}
 	}
 }
 
-export function getEpisode() : number {
+export function getEpisode(): number {
 	let episode = 1;
-	const query = document.querySelector("#primary > div.flex.flex-col.px-3.lg\\:px-0.mt-2 > div.flex.flex-col.gap-2.font-karla.pt-2.pb-3.w-full > div.flex.justify-between.items-center > div:nth-child(1) > div");
+	const query = document.querySelector(
+		"#primary > div.flex.flex-col.px-3.lg\\:px-0.mt-2 > div.flex.flex-col.gap-2.font-karla.pt-2.pb-3.w-full > div.flex.justify-between.items-center > div:nth-child(1) > div"
+	);
 
 	if (query) episode = Number(query.children[0].innerHTML.split(" ").pop());
 
 	return episode;
 }
 
-export function getId() : number {
-	for (const [key, value] of location.search.substr(1).split("&").map(s => s.split("="))) 
+export function getId(): number {
+	for (const [key, value] of location.search
+		.substr(1)
+		.split("&")
+		.map(s => s.split("=")))
 		if (key === "id") return Number(value);
-	
 
-	for (const path of location.pathname.split("/")) {
-		if (path.match(/^(\d+)$/)) 
-			return Number(path);
-		
-	}
+	for (const path of location.pathname.split("/")) 
+		if (path.match(/^(\d+)$/)) return Number(path);
+	
 }
