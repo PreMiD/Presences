cb1624cf5ca000cfb5f6c747626fee8ad862b491
SeMiD <semid@premid.app>
2024-09-28 10:43:48 +0100

    chore(Miruro): update assets (#8763)

--- websites/M/Miruro/presence.ts
+++ websites/M/Miruro/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/Wwd5S5f.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/M/Miruro/assets/logo.png",
 }
 
 presence.on("UpdateData", async () => {
