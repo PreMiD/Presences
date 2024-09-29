b8c22f7afbdf30698b1c5d135fc7dc3cc93f4186
Arthur Augis <rainbowyyyoshi@gmail.com>
2024-09-29 15:44:12 +0200

    Second Fix Errors Recover

--- websites/W/Watch-Anime/presence.ts
+++ websites/W/Watch-Anime/presence.ts
@@ -14,58 +14,29 @@ async function getInformationAnime(nameAnime: string): Promise<AnimeInfo | null>
 				img: data[0].affiche_anime,
 				name: data[0].nom_anime
 			};
-		} else {
-			throw new Error("Aucun anime trouvé");
 		}
 	} catch (error) {
-		console.error("Erreur lors de la récupération de l'image de l'anime:", error);
+		console.error("Erreur lors de la récupération de l\'image de l\'anime:", error);
 		return null;
 	}
 }
-
-function getTimestampsFromMedia(mediaElement: HTMLMediaElement) {
-	if (!mediaElement) {
-		throw new Error("L'élément média est invalide.");
-	}
-
-	const { currentTime, duration } = mediaElement;
-	const startTimestamp = Date.now() - currentTime * 1000;
-	const endTimestamp = startTimestamp + duration * 1000;
-
-	return {
-		currentTime,
-		duration,
-		startTimestamp,
-		endTimestamp
-	};
-}
-
-function debugMode(word: unknown) {
-	console.log("-------------------");
-	console.log(word);
-	console.log("-------------------");
-}
 //#endregion FUNCTIONS
 
-//#region PRESENCE DECLARATION
+//#region PRESENCEDECLARATION
 const presence = new Presence({
-	clientId: "1146930741570187385"
+	clientId: "1146930741570187385",
 });
 
-const strings = presence.getStrings({
-	play: "presence.playback.playing",
-	pause: "presence.playback.paused"
-});
-
-const browsingTimestamp = Math.floor(Date.now() / 1000);
+let browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://watch-anime.fr/favicon.png"
+	Logo = "https://watch-anime.fr/favicon.png",
 }
-//#endregion PRESENCE DECLARATION
+//#endregion PRESENCEDECLARATION
 
 //#region PRESENCE CALL
 presence.on("UpdateData", async () => {
+
 	let details: string | undefined;
 	let state: string | undefined;
 	let presenceData: PresenceData;
@@ -83,13 +54,12 @@ presence.on("UpdateData", async () => {
 	} else {
 		const pathParts = window.location.pathname.split("/");
 		if (pathParts[1] === "player" && pathParts.length >= 6) {
-			const animeName = decodeURIComponent(pathParts[2]);
+			let animeName = decodeURIComponent(pathParts[2]);
 			animeInfo = await getInformationAnime(animeName);
-			const language = pathParts[3];
-			const season = pathParts[4].split("-")[1]; // Saison
-			const episode = pathParts[5].split("-")[1]; // Episode
+			let language = pathParts[3];
+			let season = pathParts[4].split("-")[1]; // Saison
+			let episode = pathParts[5].split("-")[1]; // Episode
 			urlAnime = `https://watch-anime.fr/${pathParts[1]}/${pathParts[2]}`;
-
 			if (animeInfo) {
 				details = `Visite la page de l'animé : ${animeInfo.name}`;
 				state = `Saison ${season} • Épisode ${episode} • ${language.toUpperCase()}`;
@@ -101,7 +71,7 @@ presence.on("UpdateData", async () => {
 		presenceData = {
 			details: details!,
 			largeImageKey: Assets.Logo,
-			startTimestamp: browsingTimestamp
+			startTimestamp: browsingTimestamp,
 		};
 	} else if (animeInfo) {
 		presenceData = {
@@ -119,7 +89,7 @@ presence.on("UpdateData", async () => {
 					url: urlAnime!
 				}
 			] as [ButtonData, ButtonData?]
-		};
+		}
 	}
 
 	presence.setActivity(presenceData);
