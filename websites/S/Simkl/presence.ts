1bd54929b9bf6580b26ab3ca758295a8acde7b29
SeMiD <semid@premid.app>
2024-10-10 07:52:29 +0100

    chore(Simkl): update assets (#8800)
    Signed-off-by: SeMiD <semid@premid.app>
    Signed-off-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

--- websites/S/Simkl/presence.ts
+++ websites/S/Simkl/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/1m6HokC.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/S/Simkl/assets/logo.png",
 }
 function textContent(tags: string) {
 	return document.querySelector(tags)?.textContent?.trim();
