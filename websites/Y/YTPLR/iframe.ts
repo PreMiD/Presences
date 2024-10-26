790bfbd4dc245c7944c2d9215c4aa3916fc20cc4
Peter Strick <60312421+PeterStrick@users.noreply.github.com>
2024-10-26 12:16:58 +0200

    fix(YTPLR): fixes iframe has already been declared in Premid 2.6.*
    Signed-off-by: Peter Strick <60312421+PeterStrick@users.noreply.github.com>

--- websites/Y/YTPLR/iframe.ts
+++ websites/Y/YTPLR/iframe.ts
@@ -1,10 +1,10 @@
-const iframe = new iFrame();
-iframe.on("UpdateData", async () => {
+const ytplr_iframe = new iFrame();
+ytplr_iframe.on("UpdateData", async () => {
 	const videoElement: HTMLVideoElement =
 		document.querySelector(".video-stream");
 	if (!videoElement) return;
 
-	iframe.send({
+	ytplr_iframe.send({
 		title: document.querySelector<HTMLAnchorElement>("div.ytp-title-text > a")
 			.textContent,
 		duration: videoElement.duration,
