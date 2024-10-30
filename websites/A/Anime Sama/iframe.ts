a79de0201a4e17962888a37dcffbff347f52ea4d
Skully <117316808+skullysmods@users.noreply.github.com>
2024-10-30 12:03:40 +0100

    feat(Anime Sama): add privacy mode and update timestamps (#8839)
    Signed-off-by: Skully <117316808+skullysmods@users.noreply.github.com>

--- websites/A/Anime Sama/iframe.ts
+++ websites/A/Anime Sama/iframe.ts
@@ -2,7 +2,7 @@ const iframe = new iFrame();
 
 iframe.on("UpdateData", async () => {
 	const video = document.querySelector<HTMLVideoElement>("video");
-	if (!isNaN(video?.duration)) {
+	if (video && !isNaN(video.duration)) {
 		iframe.send({
 			currentTime: video.currentTime,
 			duration: video.duration,
