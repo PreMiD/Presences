aba31f625aa9f6c89be68032365463d420eccbe9
Slowlife <slowlife1165@gmail.com>
2024-11-14 15:35:44 +0000

    feat(HBO GO): use watching type & fix only show browsing

--- websites/H/HBO GO/presence.ts
+++ websites/H/HBO GO/presence.ts
@@ -1,6 +1,6 @@
 const presence = new Presence({
-		clientId: "605437254776651786",
-	}),
+	clientId: "605437254776651786",
+}),
 	strings = presence.getStrings({
 		play: "general.playing",
 		pause: "general.paused",
@@ -8,33 +8,28 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey:
-				"https://cdn.rcd.gg/PreMiD/websites/H/HBO%20GO/assets/logo.png",
-		},
-		video: HTMLVideoElement = document.querySelector(
-			"#hbo-sdk--controller-container #hbo-sdk--controller-osd #hbo-sdk--vid #hbo-sdk--vid_Clpp_html5_mse_smooth_api"
-		);
-	if (
-		(document.querySelector(
-			"#hbo-sdk--controller-container #hbo-sdk--controller-osd #hbo-sdk--vid #hbo-sdk--vid_Clpp_html5_mse_smooth_api"
-		) === null &&
-			document.querySelector("#hbo-sdk--player-title > div.content-title") ===
-				null) ||
-		!video
-	) {
-		presenceData.details = "Browsing...";
-		presenceData.startTimestamp = Math.floor(Date.now() / 1000);
+		details: "Browsing...",
+		largeImageKey:
+			"https://cdn.rcd.gg/PreMiD/websites/H/HBO%20GO/assets/logo.png",
+		type: ActivityType.Watching
+	},
+		video: HTMLVideoElement = document.querySelector("video");
 
-		return presence.setActivity(presenceData);
-	}
 
-	if (!isNaN(video.duration)) {
-		//* Get required tags
-		const title: HTMLElement = document.querySelector(
-			"#hbo-sdk--player-title > div.content-title"
-		);
-		presenceData.details = "Watching:";
-		presenceData.state = title.textContent;
+	if (!isNaN(video?.duration) && !!document.querySelector("div.movie-title")) {
+		const title = document.querySelector("div.movie-title").textContent;
+
+		if (location.pathname.includes("/series/")) {
+			const lastColonIndex = title.lastIndexOf(":"),
+				episode = title.substring(0, lastColonIndex).split(" ");
+
+			presenceData.details = `Season ${episode[episode.length - 2]}, Episode ${episode[episode.length - 1]}`;
+			presenceData.state = title.substring(lastColonIndex + 1).trim();
+		} else {
+			presenceData.details = title;
+			presenceData.state = location.pathname.includes("/movies/") ? "Movie" : "Extra";
+		}
+
 		presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
 		presenceData.smallImageText = video.paused
 			? (await strings).pause
@@ -45,13 +40,11 @@ presence.on("UpdateData", async () => {
 				Math.floor(video.duration)
 			);
 
-		//* Remove timestamps if paused
 		if (video.paused) {
 			delete presenceData.startTimestamp;
 			delete presenceData.endTimestamp;
 		}
+	}
 
-		//* If tags are not "null"
-		if (title.textContent) presence.setActivity(presenceData, !video.paused);
-	} else presence.setActivity();
+	presence.setActivity(presenceData);
 });
