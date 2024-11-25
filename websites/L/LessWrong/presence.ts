4d039684f9f3e470a19ce9840e5283648c11ca42
tonybolivar <71916060+tonybolivar@users.noreply.github.com>
2024-11-25 02:58:35 -0500

    Add files via upload
    Signed-off-by: tonybolivar <71916060+tonybolivar@users.noreply.github.com>

--- websites/L/LessWrong/presence.ts
+++ websites/L/LessWrong/presence.ts
@@ -4,28 +4,27 @@ const presence = new Presence({
 
 function capitalize(string: string): string {
 	return string.charAt(0).toUpperCase() + string.substring(1);
-}	
+}
 
 const browsingTimestamp = Math.floor(Date.now() / 1000);
 let data;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey:
-			"https://i.imgur.com/GnuhkCJ.png",
+		largeImageKey: "https://i.imgur.com/GnuhkCJ.png",
 		startTimestamp: browsingTimestamp,
 	};
 
 	if (document.location.pathname === "/") {
 		presenceData.details = "Browsing LessWrong";
 		presenceData.state = "at Homepage";
-	} else if (
-		document.location.pathname.startsWith("/posts/") 
-	) {
+	} else if (document.location.pathname.startsWith("/posts/")) {
 		data = document.location.pathname.split("/");
 		presenceData.details = `Reading ${data[1]}`;
-		presenceData.state = `${capitalize(data[3].replace(/-\d+$/, "").split("-").join(" "))}`;
-	} else{
+		presenceData.state = `${capitalize(
+			data[3].replace(/-\d+$/, "").split("-").join(" ")
+		)}`;
+	} else {
 		presenceData.details = "Browsing LessWrong";
 		presenceData.state = " ";
 	}
