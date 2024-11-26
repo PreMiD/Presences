be9dc9d2b8db1282eca34e5602ddf33abe288b83
armup31 <armup31@gmail.com>
2024-11-26 17:29:29 +0530

    fix and format

--- websites/P/Physics Wallah/presence.ts
+++ websites/P/Physics Wallah/presence.ts
@@ -76,7 +76,8 @@ presence.on("UpdateData", async () => {
 	} else if (pathname.startsWith("/watch")) {
 		const deta = JSON.parse(localStorage.getItem("SCHEDULE_DETAILS"));
 		let detal = "";
-		if (deta.subject === null) detal = ""; else detal = ` | ${deta.subject.name}`;
+		if (deta.subject === null) detal = "";
+		else detal = ` | ${deta.subject.name}`;
 		if (!privacyMode) {
 			presenceData.details = `Watching Lecture${detal}`;
 
