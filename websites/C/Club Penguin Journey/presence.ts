b7d1d7db30076bcf33d77445147cb4fa14caefb1
moyaimoment <51049688+moyaimoment@users.noreply.github.com>
2024-11-29 22:04:25 -0500

    ok
    Signed-off-by: moyaimoment <51049688+moyaimoment@users.noreply.github.com>

--- websites/C/Club Penguin Journey/presence.ts
+++ websites/C/Club Penguin Journey/presence.ts
@@ -4,35 +4,35 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
-  const presenceData: PresenceData = {
-    largeImageKey: "https://i.imgur.com/2MUm0oA.jpeg",
-    startTimestamp: browsingTimestamp,
-  };
+	const presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/2MUm0oA.jpeg",
+		startTimestamp: browsingTimestamp,
+	};
 
-  switch (document.location.hostname) {
-    case "cpjourney.net": {
-      if (document.location.pathname === "/") {
-        presenceData.details = "Waddling around";
-        presenceData.state = "Viewing Home";
-      } else if (document.location.pathname.startsWith("/news/")) {
-        presenceData.details = "Reading post:";
-        presenceData.state =
-          document.querySelector(".post_title__MNhEJ").textContent;
-      } else if (document.location.pathname.startsWith("/news")) {
-        presenceData.details = "Waddling around";
-        presenceData.state = "Scrolling through the news";
-      } else presenceData.details = "Waddling around";
-      break;
-    }
-    case "play.cpjourney.net":
-      {
-        if (document.location.pathname === "/") {
-          presenceData.details = "Waddling around";
-          presenceData.state = "Playing Club Penguin Journey";
-        }
-      }
-      break;
-  }
+	switch (document.location.hostname) {
+		case "cpjourney.net": {
+			if (document.location.pathname === "/") {
+				presenceData.details = "Waddling around";
+				presenceData.state = "Viewing Home";
+			} else if (document.location.pathname.startsWith("/news/")) {
+				presenceData.details = "Reading post:";
+				presenceData.state =
+					document.querySelector(".post_title__MNhEJ").textContent;
+			} else if (document.location.pathname.startsWith("/news")) {
+				presenceData.details = "Waddling around";
+				presenceData.state = "Scrolling through the news";
+			} else presenceData.details = "Waddling around";
+			break;
+		}
+		case "play.cpjourney.net":
+			{
+				if (document.location.pathname === "/") {
+					presenceData.details = "Waddling around";
+					presenceData.state = "Playing Club Penguin Journey";
+				}
+			}
+			break;
+	}
 
-  presence.setActivity(presenceData);
+	presence.setActivity(presenceData);
 });
