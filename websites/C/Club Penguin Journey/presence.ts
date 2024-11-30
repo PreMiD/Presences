b60afa9681a299f82365dbce6e876e7273ed0a37
moyaimoment <51049688+moyaimoment@users.noreply.github.com>
2024-11-29 21:39:35 -0500

    Club Penguin Journey
    Signed-off-by: moyaimoment <51049688+moyaimoment@users.noreply.github.com>

---
+++ websites/C/Club Penguin Journey/presence.ts
@@ -0,0 +1,43 @@
+const presence = new Presence({
+  clientId: "1310452380915077170",
+});
+
+let user: HTMLElement, search: HTMLElement, title: HTMLElement;
+
+const browsingTimestamp = Math.floor(Date.now() / 1000);
+let data;
+
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "https://i.imgur.com/2MUm0oA.jpeg",
+    startTimestamp: browsingTimestamp,
+  };
+
+  switch (document.location.hostname) {
+    case "cpjourney.net": {
+      if (document.location.pathname === "/") {
+        presenceData.details = "Waddling around";
+        presenceData.state = "Viewing Home";
+      } else if (document.location.pathname.startsWith("/news/")) {
+        presenceData.details = "Reading post:";
+        presenceData.state =
+          document.querySelector(".post_title__MNhEJ").textContent;
+      } else if (document.location.pathname.startsWith("/news")) {
+        presenceData.details = "Waddling around";
+        presenceData.state = "Scrolling through the news";
+      } else presenceData.details = "Waddling around";
+      break;
+    }
+    case "play.cpjourney.net":
+      {
+        if (document.location.pathname === "/") {
+          presenceData.details = "Waddling around";
+          presenceData.state = "Playing Club Penguin Journey";
+        }
+      }
+      break;
+  }
+
+  if (presenceData.details) presence.setActivity(presenceData);
+  else presence.setActivity();
+});
