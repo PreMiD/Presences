1723e0c910d1d3f599120e001e033a942e394b2a
moyaimoment <51049688+moyaimoment@users.noreply.github.com>
2024-11-29 21:58:50 -0500

    💀
    Signed-off-by: moyaimoment <51049688+moyaimoment@users.noreply.github.com>

--- websites/C/Club Penguin Journey/presence.ts
+++ websites/C/Club Penguin Journey/presence.ts
@@ -2,7 +2,7 @@ const presence = new Presence({
   clientId: "1310452380915077170",
 });
 
-const browsingTimestamp = Math.floor(Date.now() / 1000);
+browsingTimestamp = Math.floor(Date.now() / 1000);
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
