eff956fd6126b66234fda3a2afa50afa3d563af7
faetalize <faetalize@pm.me>
2024-12-05 11:43:27 +0100

    fix(1Anime): fix broken if clause and set logo properly

--- websites/0-9/1anime/presence.ts
+++ websites/0-9/1anime/presence.ts
@@ -1,14 +1,14 @@
 const presence = new Presence({
-	clientId: "",
-}),
-strings = presence.getStrings({
-	play: "presence.playback.playing",
-	pause: "presence.playback.paused",
-}),
-browsingTimestamp = Math.floor(Date.now() / 1000);
+		clientId: "1314062632419852309",
+	}),
+	strings = presence.getStrings({
+		play: "presence.playback.playing",
+		pause: "presence.playback.paused",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets { // Other default assets can be found at index.d.ts
-Logo = ""
+	Logo = "https://1anime.co/android-chrome-512x512.png",
 }
 
 //state
@@ -22,31 +22,35 @@ presence.on("UpdateData", async () => {
 
 	switch (document.location.hostname) {
 		case "1anime.one": {
-			if (document.location.pathname === "/") 
-				presenceData.details = "Checking out 1anime's homepage!";
-			else if (document.location.pathname.includes("/anime/watch/"))
+			if (document.location.pathname === "/")
+				(presenceData.details = "Checking out 1anime's homepage!");
+			else if (document.location.pathname.includes("/anime/watch/")) {
 				//player state ig
 				playing = !document.querySelector("video").paused;
 
 				//anime info ig
 				//title is in "Anime Title - Episode x" format
-				const 
-				title = document
-					.querySelector("div#details h3.font-Archivo")
-					.textContent, 
-				splitIndex = title.lastIndexOf(" - "), 
-				animeTitle = title.slice(0, splitIndex).trim(), //anime title
-				episodeLiteral = title.slice(splitIndex + 3).trim(); //episode number
+				const title = document.querySelector(
+						"div#details h3.font-Archivo"
+					).textContent,
+					splitIndex = title.lastIndexOf(" - "),
+					animeTitle = title.slice(0, splitIndex).trim(), //anime title
+					episodeLiteral = title.slice(splitIndex + 3).trim(); //episode number
 
 				//not needed for now
 				// const episodeName : string = document
 				// 	.querySelector("div#details div.flex.font-Archivo.font-bold")
 				// 	.textContent;
-				
+
 				presenceData.details = `Watching ${animeTitle || "something.."}.`;
-				presenceData.state = `Currently on ${episodeLiteral || "an episode.."}.`;
+				presenceData.state = `Currently on ${
+					episodeLiteral || "an episode.."
+				}.`;
 				presenceData.smallImageKey = playing ? Assets.Play : Assets.Pause;
-				presenceData.smallImageText = playing ? (await strings).play : (await strings).pause;
+				presenceData.smallImageText = playing
+					? (await strings).play
+					: (await strings).pause;
+			}
 			break;
 		}
 	}
