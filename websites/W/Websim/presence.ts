84f17fa3a3c0090180e65ffc860b5fa7db2b30ac
gwashark <150717581+gwashark@users.noreply.github.com>
2024-12-09 13:10:56 -0500

    feat(Websim): add presence

--- websites/W/Websim/presence.ts
+++ websites/W/Websim/presence.ts
@@ -1,39 +1,30 @@
 const presence = new Presence({
 		clientId: "1315383978878046411",
 	}),
-	strings = presence.getStrings({
-		play: "presence.playback.playing",
-	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
-
-const enum Assets { // Other default assets can be found at index.d.ts
-	Generating = "generating",
-}
 
 presence.on("UpdateData", async () => {
 	const showButtons = await presence.getSetting("showButtons"),
-	 btnPrivacy = await presence.getSetting("btnPrivacy"),
-	 presenceData: PresenceData = {
-		startTimestamp: browsingTimestamp,
-	},
+		btnPrivacy = await presence.getSetting("btnPrivacy"),
+		presenceData: PresenceData = {
+			startTimestamp: browsingTimestamp,
+		},
+		path = window.location.pathname;
 
-	 path = window.location.pathname;
-
-	if (path === "/") 
-		presenceData.details = "Browsing the homepage.";
-	 else if (path == "/fyp") {
+	if (path === "/") presenceData.details = "Browsing the homepage.";
+	else if (path === "/fyp") {
 		presenceData.details = "Browsing the FYP.";
 		if (showButtons) {
 			presenceData.buttons = [
 				{
 					label: "View FYP",
 					url: "https://websim.ai/fyp",
-				}
+				},
 			];
 		}
 	} else if (path.startsWith("/@")) {
 		const upath = path.substring(2).split("/"),
-		 username = upath[0];
+			username = upath[0];
 		if (upath.length === 2) {
 			presenceData.details = `Viewing ${document.title}`;
 			presenceData.state = `By ${username}`;
@@ -59,7 +50,7 @@ presence.on("UpdateData", async () => {
 	} else if (path.startsWith("/p/")) {
 		const projPath = path.substring(3).split("/");
 		presenceData.details = `Viewing ${document.title}`;
-		presenceData.state = `${(projPath[1]) ? `Revision #${projPath[1]}` : ""}`;
+		presenceData.state = `${projPath[1] ? `Revision #${projPath[1]}` : ""}`;
 		if (showButtons) {
 			presenceData.buttons = [
 				{
