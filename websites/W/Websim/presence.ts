132e3a02d11ed30b36e118fbc4d8de60329ae784
gwashark <150717581+gwashark@users.noreply.github.com>
2024-12-08 21:49:31 -0500

    Linting

--- websites/W/Websim/presence.ts
+++ websites/W/Websim/presence.ts
@@ -11,17 +11,17 @@ const enum Assets { // Other default assets can be found at index.d.ts
 }
 
 presence.on("UpdateData", async () => {
-	const showButtons = await presence.getSetting("showButtons");
-	const btnPrivacy = await presence.getSetting("btnPrivacy");
-	const presenceData: PresenceData = {
+	const showButtons = await presence.getSetting("showButtons"),
+	 btnPrivacy = await presence.getSetting("btnPrivacy"),
+	 presenceData: PresenceData = {
 		startTimestamp: browsingTimestamp,
-	};
+	},
 
-	const path = window.location.pathname;
+	 path = window.location.pathname;
 
-	if (path === "/") {
+	if (path === "/") 
 		presenceData.details = "Browsing the homepage.";
-	} else if (path == "/fyp") {
+	 else if (path == "/fyp") {
 		presenceData.details = "Browsing the FYP.";
 		if (showButtons) {
 			presenceData.buttons = [
@@ -29,11 +29,11 @@ presence.on("UpdateData", async () => {
 					label: "View FYP",
 					url: "https://websim.ai/fyp",
 				}
-			]
+			];
 		}
 	} else if (path.startsWith("/@")) {
-		const upath = path.substring(2).split("/");
-		const username = upath[0];
+		const upath = path.substring(2).split("/"),
+		 username = upath[0];
 		if (upath.length === 2) {
 			presenceData.details = `Viewing ${document.title}`;
 			presenceData.state = `By ${username}`;
@@ -57,9 +57,9 @@ presence.on("UpdateData", async () => {
 			}
 		}
 	} else if (path.startsWith("/p/")) {
-		const projPath = path.substring(3).split("/")
+		const projPath = path.substring(3).split("/");
 		presenceData.details = `Viewing ${document.title}`;
-		presenceData.state = `${(projPath[1]) ? `Revision #${projPath[1]}` : ""}`
+		presenceData.state = `${(projPath[1]) ? `Revision #${projPath[1]}` : ""}`;
 		if (showButtons) {
 			presenceData.buttons = [
 				{
