af41c6a9cb9fbce0d9e8e1f278f034e3a8c67311
moyaimoment <51049688+moyaimoment@users.noreply.github.com>
2024-12-12 21:24:21 -0500

    Fixes 2 & 3
    Signed-off-by: moyaimoment <51049688+moyaimoment@users.noreply.github.com>

--- websites/K/KBHGames/presence.ts
+++ websites/K/KBHGames/presence.ts
@@ -14,47 +14,44 @@ const presence = new Presence({
 		.replace(/^url\(["']?|["']?\)$/g, "");
 
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/c61Rzcw.png",
-		startTimestamp: browsingTimestamp,
-	};
-
-	if (window.location.hostname.includes("kbhgames")) {
-		if (document.location.pathname.startsWith("/new"))
-			presenceData.details = "Viewing new games";
-		else if (document.location.pathname.startsWith("/best-new-games"))
-			presenceData.details = "Viewing Best of New Games";
-		else if (document.location.pathname.startsWith("/best-games"))
-			presenceData.details = "Viewing Top Games";
-		else if (document.location.pathname.startsWith("/about"))
-			presenceData.details = "Viewing about KBHGames";
-		else if (document.location.pathname.startsWith("/terms-of-use"))
-			presenceData.details = "Viewing the Terms of Use";
-		else if (document.location.pathname.startsWith("/privacy"))
-			presenceData.details = "Viewing the Privacy Policy";
-		else if (document.location.pathname.startsWith("/favorite"))
-			presenceData.details = "Viewing favorited games";
-		else if (document.location.search.startsWith("?s=")) {
-			presenceData.details = "Searching";
-			presenceData.state = document.querySelector("#taginfo h1").textContent;
-		} else if (document.location.pathname.startsWith("/tag/")) {
-			presenceData.details = "Viewing category:";
-			presenceData.state = document.querySelector("#taginfo h1").textContent;
-		} else if (document.location.pathname.startsWith("/engine/")) {
-			presenceData.details = "Viewing engine:";
-			presenceData.state = document.querySelector("#taginfo h1").textContent;
-		} else if (document.location.pathname.startsWith("/game/")) {
-			presenceData.details = "Playing game:";
-			presenceData.state = document.querySelector("#info-box h1").textContent;
-			presenceData.largeImageKey = gameIcon;
-			presenceData.buttons = [
-				{
-					label: "View Game",
-					url: window.location.href,
-				},
-			];
-		} else presenceData.details = "Viewing KBHGames";
-	}
+	const { href, search, pathname } = document.location,
+		presenceData: PresenceData = {
+			largeImageKey: "https://i.imgur.com/c61Rzcw.png",
+			startTimestamp: browsingTimestamp,
+		};
+	if (pathname.startsWith("/new")) presenceData.details = "Viewing new games";
+	else if (pathname.startsWith("/best-new-games"))
+		presenceData.details = "Viewing Best of New Games";
+	else if (pathname.startsWith("/best-games"))
+		presenceData.details = "Viewing Top Games";
+	else if (pathname.startsWith("/about"))
+		presenceData.details = "Viewing about KBHGames";
+	else if (pathname.startsWith("/terms-of-use"))
+		presenceData.details = "Viewing the Terms of Use";
+	else if (pathname.startsWith("/privacy"))
+		presenceData.details = "Viewing the Privacy Policy";
+	else if (pathname.startsWith("/favorite"))
+		presenceData.details = "Viewing favorited games";
+	else if (search.startsWith("?s=")) {
+		presenceData.details = "Searching";
+		presenceData.state = document.querySelector("#taginfo h1").textContent;
+	} else if (pathname.startsWith("/tag/")) {
+		presenceData.details = "Viewing category:";
+		presenceData.state = document.querySelector("#taginfo h1").textContent;
+	} else if (pathname.startsWith("/engine/")) {
+		presenceData.details = "Viewing engine:";
+		presenceData.state = document.querySelector("#taginfo h1").textContent;
+	} else if (pathname.startsWith("/game/")) {
+		presenceData.details = "Playing game:";
+		presenceData.state = document.querySelector("#info-box h1").textContent;
+		presenceData.largeImageKey = gameIcon;
+		presenceData.buttons = [
+			{
+				label: "View Game",
+				url: href,
+			},
+		];
+	} else presenceData.details = "Viewing KBHGames";
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
