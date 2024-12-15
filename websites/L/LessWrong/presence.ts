ef05aac8bc5c8abb7e6217066ab6674b21c664ad
Anthony Bolivar <71916060+tonybolivar@users.noreply.github.com>
2024-12-14 22:18:13 -0500

    Update presence.ts
    Signed-off-by: Anthony Bolivar <71916060+tonybolivar@users.noreply.github.com>

--- websites/L/LessWrong/presence.ts
+++ websites/L/LessWrong/presence.ts
@@ -1,18 +1,12 @@
 const presence = new Presence({
-	clientId: "1310452380915077170",
-});
-
-function capitalize(string: string): string {
-	return string.charAt(0).toUpperCase() + string.substring(1);
-}
-
-const browsingTimestamp = Math.floor(Date.now() / 1000);
+		clientId: "1310452380915077170",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
 let data;
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey:
-			"https://cdn.rcd.gg/PreMiD/websites/L/LessWrong/assets/0.png",
+		largeImageKey: "https://i.imgur.com/GnuhkCJ.png",
 		startTimestamp: browsingTimestamp,
 	};
 
@@ -21,10 +15,35 @@ presence.on("UpdateData", async () => {
 		presenceData.state = "at Homepage";
 	} else if (document.location.pathname.startsWith("/posts/")) {
 		data = document.location.pathname.split("/");
+		const postTitle =
+			document.querySelector(".PostsPageTitle-link")?.textContent ||
+			document.querySelector(".PostsPage-secondSplashPageHeader")
+				?.textContent ||
+			"Unknown Post";
 		presenceData.details = `Reading ${data[1]}`;
-		presenceData.state = `${capitalize(
-			data[3].replace(/-\d+$/, "").split("-").join(" ")
-		)}`;
+		presenceData.state = postTitle;
+
+		presenceData.buttons = [
+			{
+				label: "View Post",
+				url: new URL(document.location.pathname, window.location.origin).href,
+			},
+		];
+	} else if (document.location.pathname.startsWith("/users/")) {
+		data = document.location.pathname.split("/");
+
+		presenceData.details = "Viewing User";
+		presenceData.state = `${
+			document.querySelector(".UsersProfile-usernameTitle")?.textContent ||
+			"Loading User"
+		}'s profile`;
+
+		presenceData.buttons = [
+			{
+				label: "View Profile",
+				url: new URL(document.location.pathname, window.location.origin).href,
+			},
+		];
 	} else presenceData.details = "Browsing LessWrong";
 
 	if (presenceData.details) presence.setActivity(presenceData);
