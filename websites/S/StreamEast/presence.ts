b191a1393eb0d27e6c1021a11ed6bf4e22a1040c
Daniel Lun <daniel@dlun.tk>
2024-12-20 19:09:42 -0800

    feat(StreamEast): add details for more pages

--- websites/S/StreamEast/presence.ts
+++ websites/S/StreamEast/presence.ts
@@ -9,13 +9,58 @@ presence.on("UpdateData", async () => {
 		startTimestamp: browsingTimestamp,
 	};
 
-	if (document.title.includes("vs")) {
-		presenceData.details = `[${window.location.pathname
-			.match(/^\/([^/]+)\//)[1]
-			.toUpperCase()}] ${document.title.replace(
-			" Live Stream - StreamEast",
-			""
-		)}`;
+	switch (document.location.pathname) {
+		case "/v85": {
+			presenceData.details = "Viewing home page";
+			break;
+		}
+		case "/nflstreams5": {
+			presenceData.details = "Viewing NFL streams";
+			break;
+		}
+		case "/nba/streams4": {
+			presenceData.details = "Viewing NBA streams";
+			break;
+		}
+		case "/nhl/streams2": {
+			presenceData.details = "Viewing NHL streams";
+			break;
+		}
+		case "/cfb/streams2": {
+			presenceData.details = "Viewing CFB streams";
+			break;
+		}
+		case "/ncaab/streams": {
+			presenceData.details = "Viewing NCAAB streams";
+			break;
+		}
+		case "/boxingstreams13": {
+			presenceData.details = "Viewing Boxing streams";
+			break;
+		}
+		case "/mmastreams10": {
+			presenceData.details = "Viewing MMA streams";
+			break;
+		}
+		case "/f1/streams": {
+			presenceData.details = "Viewing F1 streams";
+			break;
+		}
+		case "/multi-stream": {
+			presenceData.details = "Watching multiple streams";
+			break;
+		}
+		default:
+			if (document.location.pathname.includes("/blog/"))
+				presenceData.details = `Viewing blog: ${document.title}`;
+			else if (document.title.includes("vs")) {
+				presenceData.details = `Watching: [${window.location.pathname
+					.match(/^\/([^/]+)\//)[1]
+					.toUpperCase()}] ${document.title.replace(
+					" Live Stream - StreamEast",
+					""
+				)}`;
+			}
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
