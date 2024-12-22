31ab9ce9934cf919194e6aeae618a57fe8d7960c
Dutsakorn Tubsang <66200082@kmitl.ac.th>
2024-12-22 20:39:20 +0700

    fix: change the logo size to 512x512

--- websites/D/Dek-d/presence.ts
+++ websites/D/Dek-d/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/eq3Nmvz.png",
+	Logo = "https://i.imgur.com/9wQMfAA.png",
 }
 
 presence.on("UpdateData", async () => {
