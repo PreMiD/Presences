373fda2a05fce777d7fcf8581fc760d467e1a697
SeMiD <semid@premid.app>
2024-12-22 09:05:55 +0000

    chore(StudyStream): update assets (#8985)
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/S/StudyStream/presence.ts
+++ websites/S/StudyStream/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/f4ehLFP.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/S/StudyStream/assets/logo.png",
 }
 
 presence.on("UpdateData", async () => {
