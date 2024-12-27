b934af6a70c65cb81d50bd5f97012a9531d4ce88
Slowlife <slowlife1165@gmail.com>
2024-12-27 04:17:10 +0000

    feat(Yandex Music): hide when paused

--- websites/Y/Yandex Music/presence.ts
+++ websites/Y/Yandex Music/presence.ts
@@ -1,17 +1,15 @@
 const presence = new Presence({
-		clientId: "745261937092198532",
-	}),
-	strings = presence.getStrings({
-		playing: "general.playing",
-		pause: "general.paused",
-	});
+	clientId: "745261937092198532",
+});
 
 presence.on("UpdateData", async () => {
-	if (!navigator.mediaSession.metadata) return presence.setActivity();
+	if (
+		document.querySelectorAll(".player-controls__btn_pause").length !== 2 ||
+		!navigator.mediaSession.metadata
+	)
+		return presence.clearActivity();
 
-	const playing =
-			document.querySelectorAll(".player-controls__btn_pause").length === 2,
-		largeImageKey = navigator.mediaSession.metadata.artwork
+	const largeImageKey = navigator.mediaSession.metadata.artwork
 			? navigator.mediaSession.metadata.artwork.at(-1).src
 			: "https://cdn.rcd.gg/PreMiD/websites/Y/Yandex%20Music/assets/logo.png",
 		timePassed = document.querySelector(".progress__left").textContent,
@@ -32,8 +30,6 @@ presence.on("UpdateData", async () => {
 		presenceData = {
 			type: ActivityType.Listening,
 			largeImageKey,
-			smallImageKey: playing ? Assets.Play : Assets.Pause,
-			smallImageText: playing ? (await strings).playing : (await strings).pause,
 			details: navigator.mediaSession.metadata.title,
 			state:
 				navigator.mediaSession.metadata.artist ||
@@ -41,11 +37,6 @@ presence.on("UpdateData", async () => {
 			startTimestamp,
 			endTimestamp,
 		};
-
-	if (!playing) {
-		delete presenceData.startTimestamp;
-		delete presenceData.endTimestamp;
-	}
 
 	presence.setActivity(presenceData);
 });
