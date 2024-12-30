13ce1898407bb72e65339ecb22972e6d35beb0c1
SeMiD <semid@premid.app>
2024-12-30 06:34:37 +0000

    chore: update assets
    Signed-off-by: SeMiD <semid@premid.app>

--- websites/R/Royal Road/presence.ts
+++ websites/R/Royal Road/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/7LzTPVj.png",
+	Logo = "https://cdn.rcd.gg/PreMiD/websites/R/Royal%20Road/assets/logo.png",
 }
 
 // Todo - Multilanguage functionality as hard to test atm due to bugs.
