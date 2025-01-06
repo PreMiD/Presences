de2fb3531a6141b44e294627ead07521d6788800
arut-11 <arutselvan.p@gmail.com>
2025-01-06 12:18:02 +0530

    Refactor(JioCinema): rewrite activity
    Signed-off-by: arut-11 <arutselvan.p@gmail.com>

--- websites/J/JioCinema/presence.ts
+++ websites/J/JioCinema/presence.ts
@@ -14,39 +14,39 @@ presence.on("UpdateData", async () => {
 			startTimestamp,
 		},
 		url = window.location.href;
-	if (url.includes("/watch/")) {
-		const [video] = document.querySelectorAll("video");
-		presenceData.details =
-			document.querySelectorAll(".meta-data-title")[0].textContent;
-		presenceData.largeImageKey =
-			"https://cdn.rcd.gg/PreMiD/websites/J/JioCinema/assets/logo.png";
-		presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
-		presenceData.smallImageText = video.paused
-			? (await strings).pause
-			: (await strings).play;
-		[presenceData.startTimestamp, presenceData.endTimestamp] =
-			presence.getTimestamps(
-				Math.floor(video.currentTime),
-				Math.floor(video.duration)
-			);
-		if (url.includes("/tv/")) {
-			presenceData.state = (
-				document.querySelectorAll("div.now-playing") as NodeListOf<HTMLElement>
-			)[0].offsetParent
-				.querySelectorAll("span.jioTitle")[1]
-				.textContent.replace("| ", "");
-		} else if (url.includes("/movies/")) presenceData.state = "Movie";
-		else if (url.includes("/playlist/")) presenceData.state = "Music Video";
-		else presenceData.state = "Video";
-
-		if (video.paused) {
-			delete presenceData.startTimestamp;
-			delete presenceData.endTimestamp;
-		}
-	} else if (url.includes("/search/")) {
-		presenceData.details = "Searching...";
-		presenceData.smallImageKey = Assets.Search;
-	} else presenceData.details = "Browsing";
-
-	presence.setActivity(presenceData, true);
-});
+		if (url.includes("/movies") || url.includes("/tv-shows")) {
+			presenceData.type = ActivityType.Watching;
+			const [video] = document.querySelectorAll("video"),
+			title =
+				document.querySelector('meta[property="og:title"]')?.getAttribute("content");
+			presenceData.largeImageKey =
+				"https://cdn.rcd.gg/PreMiD/websites/J/JioCinema/assets/logo.png";
+			presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play;
+			presenceData.smallImageText = video.paused
+				? (await strings).pause
+				: (await strings).play;
+			[presenceData.startTimestamp, presenceData.endTimestamp] =
+				presence.getTimestamps(
+					Math.floor(video.currentTime),
+					Math.floor(video.duration)
+				);
+			if (url.includes("/watch")) {
+				presenceData.details = title.match(/^(.+?)\s*(\((\d{4})\))/)[1].trim();
+				presenceData.state = "Movie";
+			} else if (title) {
+				const tvshowtitle = title.match(/Watch\s+(.+?)\s+Season\s+(\d+)\s+Episode\s+(\d+)\s*:\s*(.+?)\s*-/);
+				presenceData.details = tvshowtitle[1].trim();
+				presenceData.state = `S${parseInt(tvshowtitle[2], 10)} E${parseInt(tvshowtitle[3], 10)} · ${tvshowtitle[4].trim()}`;
+			} else presenceData.state = "Video";
+	
+			if (video.paused) {
+				delete presenceData.startTimestamp;
+				delete presenceData.endTimestamp;
+			}
+		} else if (url.includes("/search")) {
+			presenceData.details = "Searching...";
+			presenceData.smallImageKey = Assets.Search;
+		} else presenceData.details = "Browsing";
+	
+		presence.setActivity(presenceData, true);
+   });
