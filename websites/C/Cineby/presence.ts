580fcbe4dd9a3302c37ec0596f801b416aef6b06
SeMiD <semid@premid.app>
2025-01-06 21:11:42 +0000

    chore(Cineby): update assets (#9116)
    Signed-off-by: SeMiD <semid@premid.app>
    Signed-off-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

--- websites/C/Cineby/presence.ts
+++ websites/C/Cineby/presence.ts
@@ -7,7 +7,8 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/zAKeytL.png",
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/C/Cineby/assets/logo.png",
 			details: "Browsing",
 			type: ActivityType.Watching,
 			startTimestamp,
