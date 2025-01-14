736da74163e8f0e31673895d20a926ef7d6f43e7
Lordreider <robinsteidle0303@gmail.com>
2025-01-15 00:45:39 +0100

    .

---
+++ websites/A/Averge/presence.ts
@@ -0,0 +1,63 @@
+const presence1 = new Presence({
+  clientId: "910193698644586536"
+});
+const browsingStamp2 = Math.floor(Date.now() / 1000);
+
+presence1.on("UpdateData", () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "averge.eu",
+    startTimestamp: browsingStamp2
+  };
+
+   if (window.location.href.includes("averge.eu"))
+    presenceData.details = "https://averge.eu";
+  else if (window.location.href.includes("averge.eu")) {
+    if (window.location.pathname.toLowerCase() === "/") {
+      presenceData.details = "Schaut auf die Startseite";
+      presenceData.state = "Dashboard";
+    } else if (window.location.pathname.toLowerCase() === "/raidprotect") {
+      presenceData.details = "Schaut auf RaidProtect";
+      presenceData.state = "RaidProtect";
+    } else if (window.location.pathname.toLowerCase() === "/globalsecure") {
+      presenceData.details = "Schaut auf GlobalSecure";
+      presenceData.state = "GlobalSecure";
+    } else if (window.location.pathname.toLowerCase() === "/about") {
+      presenceData.details = "Schaut über uns an";
+      presenceData.state = "Über uns";
+    } else if (window.location.pathname.toLowerCase() === "/projectserver") {
+      presenceData.details = "Schaut auf ProjektServer";
+      presenceData.state = "ProjektServer";
+    } else if (window.location.pathname.toLowerCase() === "/kontakt") {
+      presenceData.details = "Schaut Kontakt an";
+      presenceData.state = "Kontakt";
+    }  else if (window.location.pathname.toLowerCase().includes("/404")) {
+      presenceData.details = "Schaut sich die Seite an:";
+      presenceData.state = "404 Seite";
+    } else if (window.location.pathname.toLowerCase().includes("/impressum")) {
+      presenceData.details = "Schaut sich die Seite an:";
+      presenceData.state = "Impressum";
+    } else if (window.location.pathname.toLowerCase().includes("/datenschutz")) {
+      presenceData.details = "Schaut sich die Seite an:";
+      presenceData.state = "Datenschutz";
+    } 
+  } else {
+    presenceData.details = "Schaut sich die Seite an:";
+    if (window.location.pathname.toLowerCase() === "/")
+      presenceData.state = "Homepage";
+     else if (window.location.pathname.toLowerCase().includes("/404")) {
+      presenceData.details = "Viewing a page:";
+      presenceData.state = "404 Seite";
+    } else if (window.location.pathname.toLowerCase().includes("/impressum")) {
+      presenceData.details = "Schaut sich die Seite an:";
+      presenceData.state = "Impressum";
+    } else if (window.location.pathname.toLowerCase().includes("/legal/privacy")) {
+      presenceData.details = "Schaut sich die Seite an:";
+      presenceData.state = "Datenschutz";
+    }  else delete presenceData.details;
+  }
+
+  if (!presenceData.details) {
+    presence1.setTrayTitle();
+    presence1.setActivity();
+  } else presence1.setActivity(presenceData);
+});
