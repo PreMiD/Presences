60cb4b22e06e8480bff613b802e6639a0dbbb6e7
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2025-01-14 19:23:57 +0000

    feat(Eurogamer): add activity

--- websites/E/Eurogamer/presence.ts
+++ websites/E/Eurogamer/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
 const enum Assets {
-	Logo = "https://i.imgur.com/W7S7h1m.png",
+	Logo = "https://i.imgur.com/L2r2n5e.png",
 }
 
 async function getStrings() {
