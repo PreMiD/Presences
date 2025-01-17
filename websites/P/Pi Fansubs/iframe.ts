406b0c27ed64663f44938ccea512cb15d4d434ed
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2025-01-17 20:52:58 +0000

    feat(Pi Fansubs): add activity

---
+++ websites/P/Pi Fansubs/iframe.ts
@@ -0,0 +1,22 @@
+const iframe = new iFrame();
+
+iframe.on("UpdateData", async () => {
+	let video: HTMLVideoElement;
+
+	if (document.querySelector(".jw-video"))
+		video = document.querySelector(".jw-video");
+	else if (document.querySelector(".html5-video-container"))
+		video = document.querySelector(".html5-video-container > video");
+
+	if (video && !isNaN(video.duration)) {
+		iframe.send({
+			iframeVideo: {
+				iFrameVideo: true,
+				currTime: video.currentTime,
+				dur: video.duration,
+				paused: video.paused,
+			},
+		});
+	}
+});
+
