2a2ec339b1f690f1dc5580ee7ebe507297ec68d0
Bas950 <me@bas950.com>
2025-01-17 21:22:20 +0100

    chore: lint # folder

--- websites/#/ッツ Ebook Reader/presence.ts
+++ websites/#/ッツ Ebook Reader/presence.ts
@@ -1,44 +1,45 @@
 const presence = new Presence({
-		clientId: "1167164781426380931",
-	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+  clientId: '1167164781426380931',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
 const enum Assets {
-	Logo = "https://cdn.rcd.gg/PreMiD/websites/%23/%E3%83%83%E3%83%84%20Ebook%20Reader/assets/logo.png",
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/%23/%E3%83%83%E3%83%84%20Ebook%20Reader/assets/logo.png',
 }
 
-presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-			details: "Browsing",
-			largeImageKey: Assets.Logo,
-			startTimestamp: browsingTimestamp,
-		},
-		{ pathname } = document.location;
-	switch (pathname.replace("_", "").split("/")[1]) {
-		case "manage": {
-			presenceData.details = "Managing library";
-			break;
-		}
-		case "settings": {
-			presenceData.details = "Setting things up";
-			break;
-		}
-		case "b": {
-			const titleElement =
-					document.querySelector<HTMLTitleElement>("head title"),
-				progressElement = document.querySelector<HTMLDivElement>(
-					".writing-horizontal-tb.fixed.bottom-2.right-2"
-				);
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    details: 'Browsing',
+    largeImageKey: Assets.Logo,
+    startTimestamp: browsingTimestamp,
+  }
+  const { pathname } = document.location
+  switch (pathname.replace('_', '').split('/')[1]) {
+    case 'manage': {
+      presenceData.details = 'Managing library'
+      break
+    }
+    case 'settings': {
+      presenceData.details = 'Setting things up'
+      break
+    }
+    case 'b': {
+      const titleElement = document.querySelector<HTMLTitleElement>('head title')
+      const progressElement = document.querySelector<HTMLDivElement>(
+        '.writing-horizontal-tb.fixed.bottom-2.right-2',
+      )
 
-			let title = " a book",
-				progress = "";
+      let title = ' a book'
+      let progress = ''
 
-			if (titleElement) title = titleElement.text.split("|")[0];
-			if (progressElement) progress = progressElement.textContent;
+      if (titleElement)
+        title = titleElement.text.split('|')[0]!
+      if (progressElement)
+        progress = progressElement.textContent!
 
-			presenceData.details = `Reading ${title}`;
-			presenceData.state = progress;
-		}
-	}
+      presenceData.details = `Reading ${title}`
+      presenceData.state = progress
+    }
+  }
 
-	presence.setActivity(presenceData);
-});
+  presence.setActivity(presenceData)
+})
