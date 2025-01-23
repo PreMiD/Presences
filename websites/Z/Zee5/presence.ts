10f8fe0433c18c67f42a32a6a576f71d8e42d10a
Bas950 <me@bas950.com>
2025-01-23 15:38:41 +0100

    chore: lint Z folder

--- websites/Z/Zee5/presence.ts
+++ websites/Z/Zee5/presence.ts
@@ -1,79 +1,89 @@
 const presence = new Presence({
-		clientId: "1103408220921397349",
-	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+  clientId: '1103408220921397349',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 const enum Assets {
-	Logo = "https://cdn.rcd.gg/PreMiD/websites/Z/Zee5/assets/logo.jpg",
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/Z/Zee5/assets/logo.jpg',
 }
-const getVideoStatus = (
-	presenceData: PresenceData,
-	video: HTMLVideoElement
-) => {
-	if (video.paused) {
-		presenceData.smallImageKey = Assets.Pause;
-		presenceData.smallImageText = "paused";
-		delete presenceData.startTimestamp;
-	} else {
-		[presenceData.startTimestamp, presenceData.endTimestamp] =
-			presence.getTimestampsfromMedia(video);
-		presenceData.smallImageKey = Assets.Play;
-		presenceData.smallImageText = "playing";
-	}
-	return presenceData;
-};
+function getVideoStatus(presenceData: PresenceData, video: HTMLVideoElement) {
+  if (video.paused) {
+    presenceData.smallImageKey = Assets.Pause
+    presenceData.smallImageText = 'paused'
+    delete presenceData.startTimestamp
+  }
+  else {
+    [presenceData.startTimestamp, presenceData.endTimestamp] = presence.getTimestampsfromMedia(video)
+    presenceData.smallImageKey = Assets.Play
+    presenceData.smallImageText = 'playing'
+  }
+  return presenceData
+}
 
-presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-			largeImageKey: Assets.Logo,
-			startTimestamp: browsingTimestamp,
-		},
-		{ pathname } = document.location;
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: Assets.Logo,
+    startTimestamp: browsingTimestamp,
+  }
+  const { pathname } = document.location
 
-	if (pathname === "/") presenceData.details = "Browsing home page";
-	else if (pathname === "/movies") presenceData.details = "Browsing movies";
-	else if (pathname.includes("/movies/details")) {
-		const currentPresenceData = getVideoStatus(
-			presenceData,
-			document.querySelector<HTMLVideoElement>("video")
-		);
-		currentPresenceData.details = "Watching";
-		currentPresenceData.state = document.querySelector(
-			"div.consumptionMetaDiv > div > h1"
-		).textContent;
-	} else if (pathname.includes("tv-shows/details")) {
-		const currentPresenceData = getVideoStatus(
-			presenceData,
-			document.querySelector<HTMLVideoElement>("video")
-		);
-		currentPresenceData.details = `Watching ${
-			document.querySelector("div.consumptionMetaDiv >  a  > h2").textContent
-		}`;
-		currentPresenceData.state = `${
-			document.querySelector("div.consumptionMetaDiv >  div  > h1").textContent
-		} ${
-			document.querySelector("div.consumptionMetaDiv >  div  > p").textContent
-		}`;
-	} else if (pathname.includes("web-series/details")) {
-		const currentPresenceData = getVideoStatus(
-			presenceData,
-			document.querySelector<HTMLVideoElement>("video")
-		);
-		currentPresenceData.details = `Watching ${
-			document.querySelector("div.consumptionMetaDiv >  a  > h2").textContent
-		}`;
-		currentPresenceData.state = `${
-			document.querySelector("div.consumptionMetaDiv >  div  > h1").textContent
-		} ${
-			document.querySelector("div.consumptionMetaDiv >  div  > p").textContent
-		}`;
-	} else if (pathname === "tv-shows")
-		presenceData.details = "Browsing TV shows";
-	else if (pathname.includes("/premium"))
-		presenceData.details = "Browsing premium";
-	else if (pathname.includes("/web-series"))
-		presenceData.details = "Browsing web series";
-	else if (pathname.includes("/news")) presenceData.details = "Browsing news";
-	if (presenceData.details) presence.setActivity(presenceData);
-	else presence.setActivity();
-});
+  if (pathname === '/') {
+    presenceData.details = 'Browsing home page'
+  }
+  else if (pathname === '/movies') {
+    presenceData.details = 'Browsing movies'
+  }
+  else if (pathname.includes('/movies/details')) {
+    const currentPresenceData = getVideoStatus(
+      presenceData,
+      document.querySelector<HTMLVideoElement>('video')!,
+    )
+    currentPresenceData.details = 'Watching'
+    currentPresenceData.state = document.querySelector(
+      'div.consumptionMetaDiv > div > h1',
+    )?.textContent
+  }
+  else if (pathname.includes('tv-shows/details')) {
+    const currentPresenceData = getVideoStatus(
+      presenceData,
+      document.querySelector<HTMLVideoElement>('video')!,
+    )
+    currentPresenceData.details = `Watching ${
+      document.querySelector('div.consumptionMetaDiv >  a  > h2')?.textContent
+    }`
+    currentPresenceData.state = `${
+      document.querySelector('div.consumptionMetaDiv >  div  > h1')?.textContent
+    } ${
+      document.querySelector('div.consumptionMetaDiv >  div  > p')?.textContent
+    }`
+  }
+  else if (pathname.includes('web-series/details')) {
+    const currentPresenceData = getVideoStatus(
+      presenceData,
+      document.querySelector<HTMLVideoElement>('video')!,
+    )
+    currentPresenceData.details = `Watching ${
+      document.querySelector('div.consumptionMetaDiv >  a  > h2')?.textContent
+    }`
+    currentPresenceData.state = `${
+      document.querySelector('div.consumptionMetaDiv >  div  > h1')?.textContent
+    } ${
+      document.querySelector('div.consumptionMetaDiv >  div  > p')?.textContent
+    }`
+  }
+  else if (pathname === 'tv-shows') {
+    presenceData.details = 'Browsing TV shows'
+  }
+  else if (pathname.includes('/premium')) {
+    presenceData.details = 'Browsing premium'
+  }
+  else if (pathname.includes('/web-series')) {
+    presenceData.details = 'Browsing web series'
+  }
+  else if (pathname.includes('/news')) {
+    presenceData.details = 'Browsing news'
+  }
+  if (presenceData.details)
+    presence.setActivity(presenceData)
+  else presence.setActivity()
+})
--- websites/Z/Zoom/presence.ts
+++ websites/Z/Zoom/presence.ts
@@ -1,100 +1,103 @@
 const presence = new Presence({
-		clientId: "765876503161733140",
-	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+  clientId: '765876503161733140',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
 
-presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-			largeImageKey:
-				"https://cdn.rcd.gg/PreMiD/websites/Z/Zoom/assets/logo.png",
-		},
-		{ pathname } = document.location;
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/Z/Zoom/assets/logo.png',
+  }
+  const { pathname } = document.location
 
-	switch (true) {
-		case pathname === "/myhome":
-		case pathname === "/": {
-			presenceData.details = "Viewing home page";
-			break;
-		}
+  switch (true) {
+    case pathname === '/myhome':
+    case pathname === '/': {
+      presenceData.details = 'Viewing home page'
+      break
+    }
 
-		case pathname.includes("signin"): {
-			presenceData.details = "Singing in";
-			break;
-		}
-		case pathname.includes("signup"): {
-			presenceData.details = "Creating an account";
-			break;
-		}
-		case pathname.includes("profile"): {
-			presenceData.details = "Viewing their profile";
-			break;
-		}
-		case pathname.includes("webinars"): {
-			presenceData.details = "Viewing webinars";
-			break;
-		}
-		case pathname.includes("recording"): {
-			presenceData.details = "Viewing recordings";
-			break;
-		}
-		case pathname.includes("settings"): {
-			presenceData.details = "Editing their settings";
-			break;
-		}
-		case pathname.includes("meeting/schedule"): {
-			presenceData.details = "Scheduling a meeting";
-			break;
-		}
-		case pathname.includes("meetings"): {
-			presenceData.details = "Viewing meetings page";
-			break;
-		}
-		case pathname.includes("/s/"): {
-			presenceData.details = "Joining a meeting";
-			break;
-		}
-		case pathname.includes("/wc/"): {
-			if (document.querySelector("#prompt > h4") && pathname.endsWith("start"))
-				presenceData.details = "Joining a meeting";
-			else if (pathname.endsWith("leave"))
-				presenceData.details = "Leaving an meeting";
-			else {
-				if (videoEnabled()) {
-					presenceData.details = "In a video meeting";
-					presenceData.smallImageKey = Assets.VideoCall;
-				} else {
-					presenceData.details = "In a meeting";
-					presenceData.smallImageKey = Assets.Call;
-				}
-				if (memberCount()) {
-					presenceData.state = `${memberCount()} user${
-						memberCount() > 1 ? "s" : ""
-					} in room`;
-				}
-				presenceData.startTimestamp = browsingTimestamp;
-			}
+    case pathname.includes('signin'): {
+      presenceData.details = 'Singing in'
+      break
+    }
+    case pathname.includes('signup'): {
+      presenceData.details = 'Creating an account'
+      break
+    }
+    case pathname.includes('profile'): {
+      presenceData.details = 'Viewing their profile'
+      break
+    }
+    case pathname.includes('webinars'): {
+      presenceData.details = 'Viewing webinars'
+      break
+    }
+    case pathname.includes('recording'): {
+      presenceData.details = 'Viewing recordings'
+      break
+    }
+    case pathname.includes('settings'): {
+      presenceData.details = 'Editing their settings'
+      break
+    }
+    case pathname.includes('meeting/schedule'): {
+      presenceData.details = 'Scheduling a meeting'
+      break
+    }
+    case pathname.includes('meetings'): {
+      presenceData.details = 'Viewing meetings page'
+      break
+    }
+    case pathname.includes('/s/'): {
+      presenceData.details = 'Joining a meeting'
+      break
+    }
+    case pathname.includes('/wc/'): {
+      if (document.querySelector('#prompt > h4') && pathname.endsWith('start')) {
+        presenceData.details = 'Joining a meeting'
+      }
+      else if (pathname.endsWith('leave')) {
+        presenceData.details = 'Leaving an meeting'
+      }
+      else {
+        if (videoEnabled()) {
+          presenceData.details = 'In a video meeting'
+          presenceData.smallImageKey = Assets.VideoCall
+        }
+        else {
+          presenceData.details = 'In a meeting'
+          presenceData.smallImageKey = Assets.Call
+        }
+        if (memberCount()) {
+          presenceData.state = `${memberCount()} user${
+            memberCount()! > 1 ? 's' : ''
+          } in room`
+        }
+        presenceData.startTimestamp = browsingTimestamp
+      }
 
-			break;
-		}
-	}
-	if (presenceData.details) presence.setActivity(presenceData);
-	else presence.setActivity();
-});
+      break
+    }
+  }
+  if (presenceData.details)
+    presence.setActivity(presenceData)
+  else presence.setActivity()
+})
 
 function videoEnabled() {
-	return (
-		document
-			.querySelector<HTMLButtonElement>(".send-video-container > button")
-			.getAttribute("aria-label") &&
-		document
-			.querySelector<HTMLButtonElement>(".send-video-container > button")
-			.getAttribute("aria-label") !== "start sending my video"
-	);
+  return (
+    document
+      .querySelector<HTMLButtonElement>('.send-video-container > button')
+      ?.getAttribute('aria-label')
+      && document
+        .querySelector<HTMLButtonElement>('.send-video-container > button')
+        ?.getAttribute('aria-label') !== 'start sending my video'
+  )
 }
 
 function memberCount() {
-	const counter = document.querySelector<HTMLSpanElement>(
-		".footer-button__number-counter"
-	)?.textContent;
-	return counter === null ? null : Number(counter);
+  const counter = document.querySelector<HTMLSpanElement>(
+    '.footer-button__number-counter',
+  )?.textContent
+  return counter === null ? null : Number(counter)
 }
