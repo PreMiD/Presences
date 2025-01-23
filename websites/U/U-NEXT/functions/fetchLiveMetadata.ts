cf7203ea1816d609e2cd6d31f8cc9e8468929f4f
Bas950 <me@bas950.com>
2025-01-23 11:08:10 +0100

    chore: lint U folder

--- websites/U/U-NEXT/functions/fetchLiveMetadata.ts
+++ websites/U/U-NEXT/functions/fetchLiveMetadata.ts
@@ -1,47 +1,47 @@
-import pLimit from "p-limit";
-import { LiveRoot } from "../types";
+import type { LiveRoot } from '../types'
+import pLimit from 'p-limit'
 
-const limit = pLimit(1);
+const limit = pLimit(1)
 
+// eslint-disable-next-line import/no-mutable-exports
 export let liveMetadata: {
-	url: string;
-	data?: LiveRoot;
-} | null = null;
+  url: string
+  data?: LiveRoot
+} | null = null
 
 export async function fetchLiveMetadata(id: string): Promise<void> {
-	await limit(async () => {
-		if (liveMetadata?.url === document.location.href) return;
+  await limit(async () => {
+    if (liveMetadata?.url === document.location.href)
+      return
 
-		const getLiveDetailResponse = await fetch("https://cc.unext.jp/", {
-			method: "POST",
-			headers: {
-				"content-type": "application/json",
-			},
-			body: JSON.stringify({
-				operationName: "cosmo_getLiveDetail",
-				variables: {
-					liveCode: id,
-				},
-				extensions: {
-					persistedQuery: {
-						version: 1,
-						sha256Hash:
-							"02f3512167abdfa1ebc3ba0fba04f54d2ccf5203e3e5639cbe36e5b844cbee38",
-					},
-				},
-			}),
-		});
+    const getLiveDetailResponse = await fetch('https://cc.unext.jp/', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        operationName: 'cosmo_getLiveDetail',
+        variables: {
+          liveCode: id,
+        },
+        extensions: {
+          persistedQuery: {
+            version: 1,
+            sha256Hash: '02f3512167abdfa1ebc3ba0fba04f54d2ccf5203e3e5639cbe36e5b844cbee38',
+          },
+        },
+      }),
+    })
 
-		liveMetadata = {
-			url: document.location.href,
-			data: {
-				webfrontGetLive: (await getLiveDetailResponse.json()).data
-					.webfront_getLive,
-			},
-		};
-	});
+    liveMetadata = {
+      url: document.location.href,
+      data: {
+        webfrontGetLive: (await getLiveDetailResponse.json()).data.webfront_getLive,
+      },
+    }
+  })
 }
 
 export function clearLiveMetadata(): void {
-	liveMetadata = null;
+  liveMetadata = null
 }
