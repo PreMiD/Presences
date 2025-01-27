0948c044dba1c61d4b7d91e8c21cc11cf593f626
SeMiD <semid@premid.app>
2025-01-27 21:33:06 +0000

    chore(kisskh): update assets (#9210)

--- websites/K/kisskh/presence.ts
+++ websites/K/kisskh/presence.ts
@@ -5,7 +5,8 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-			largeImageKey: "https://i.imgur.com/zu43jYo.png",
+			largeImageKey:
+				"https://cdn.rcd.gg/PreMiD/websites/K/kisskh/assets/logo.png",
 			details: "Browsing",
 			type: ActivityType.Watching,
 			startTimestamp,
