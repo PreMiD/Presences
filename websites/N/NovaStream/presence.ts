1c91e992fee2506177b5a030e1c983dc3597e251
Lenoch <81lennoch@gmail.com>
2025-01-30 21:20:36 +0100

    feat(NovaStream): add activity

---
+++ websites/N/NovaStream/presence.ts
@@ -0,0 +1,100 @@
+const presence = new Presence({
+		clientId: "1334576902081351761",
+	}),
+	browsingTimestamp = Math.floor(Date.now() / 1000);
+
+presence.on("UpdateData", async () => {
+	const presenceData: PresenceData = {
+		largeImageKey: "https://i.imgur.com/sASc3GV.png",
+		smallImageKey: Assets.Play,
+		smallImageText: "Watching free content",
+		startTimestamp: browsingTimestamp,
+		type: ActivityType.Watching,
+	};
+
+	if (document.location.pathname === "/") {
+		const searchInput = (
+			document.querySelector(".search-input") as HTMLInputElement
+		)?.value;
+		if (searchInput.length > 0)
+			presenceData.state = `Searching for: "${searchInput}"`;
+		else presenceData.state = "Browsing content to watch";
+	} else if (document.location.pathname.includes("/watch")) {
+		let title = document.querySelector("h1.text-2xl")?.textContent,
+			releaseDate,
+			rating,
+			season,
+			episode,
+			runtime,
+			seasonEpisode;
+
+		if (document.location.search.includes("type=movie")) {
+			releaseDate = document.querySelector(
+				"div.space-y-2:nth-child(3) > p:nth-child(2) > span:nth-child(2)"
+			)?.textContent;
+			rating = document
+				.querySelector(
+					"div.space-y-2:nth-child(3) > p:nth-child(4) > span:nth-child(1) > span:nth-child(1)"
+				)
+				?.textContent?.split(" (")[0];
+			runtime = document.querySelector(
+				"p.text-gray-300:nth-child(8) > span:nth-child(2)"
+			)?.textContent;
+		} else {
+			releaseDate = document.querySelector(
+				"div.bg-gray-800:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > p:nth-child(2) > span:nth-child(2)"
+			)?.textContent;
+			rating = document
+				.querySelector(
+					"p.text-gray-300:nth-child(5) > span:nth-child(1) > span:nth-child(1)"
+				)
+				?.textContent.split(" (")[0];
+			season = (
+				document.querySelector(
+					"select.bg-gray-700:nth-child(1)"
+				) as HTMLSelectElement
+			)?.selectedOptions[0].textContent.replace("Season ", "");
+			episode = (
+				document.querySelector(
+					"select.rounded-lg:nth-child(2)"
+				) as HTMLSelectElement
+			)?.selectedOptions[0].textContent.replace("Episode ", "");
+			if (season && episode) seasonEpisode = `S${season}E${episode}  •  `;
+		}
+		const releaseYear = releaseDate?.substring(0, 4);
+		if (releaseYear) title += ` (${releaseYear})`;
+
+		presenceData.buttons = [
+			{ label: "Watch now", url: document.location.href },
+		];
+		if (title) presenceData.details = `Watching ${title}`;
+		presenceData.state = [
+			runtime ? `${runtime}  •  ` : "",
+			seasonEpisode ?? "",
+			rating ? `⭐${rating}` : "",
+		].join("");
+	}
+
+	// Watchlist modal
+	const watchListElements =
+		(document.querySelector(
+			"div.fixed:nth-child(9) .grid-cols-1"
+		) as HTMLInputElement) ||
+		(document.querySelector(
+			"div.grid-cols-1:nth-child(2)"
+		) as HTMLInputElement);
+	if (
+		watchListElements &&
+		window.getComputedStyle(
+			watchListElements.parentElement.parentElement.parentElement,
+			null
+		).display === "flex"
+	) {
+		presenceData.state = `Browsing Watchlist: ${
+			watchListElements.children.length - 1
+		} items`;
+		presenceData.details = "";
+	}
+
+	if (presenceData.state) presence.setActivity(presenceData);
+});
