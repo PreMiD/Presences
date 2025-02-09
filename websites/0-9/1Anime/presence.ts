f530bff988dc7356443b7884b31174824e83c4df
Bas950 <me@bas950.com>
2025-02-09 17:22:18 +0100

    fix(1Anime): reupload assets

--- websites/0-9/1Anime/presence.ts
+++ websites/0-9/1Anime/presence.ts
@@ -10,7 +10,7 @@ const strings = presence.getStrings({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://cdn.rcd.gg/PreMiD/websites/0-9/1anime/assets/logo.png',
+  Logo = 'https://i.imgur.com/MIOf3Wj.png',
 }
 
 // state
