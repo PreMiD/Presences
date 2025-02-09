bf472cf37146bfc15d642a98e1f2ffb701a69b25
hakanxd <hakobabo2002@gmail.com>
2025-02-09 16:05:25 +0300

    ♻️ feat(AniTilky): add AniTilky presence

---
+++ websites/A/AniTilky/iframe.ts
@@ -0,0 +1,18 @@
+declare class iFrame {
+	on(event: string, callback: Function): void;
+	send(data: any): void;
+}
+
+const iframe = new iFrame();
+
+iframe.on("UpdateData", async () => {
+	const video = document.querySelector<HTMLVideoElement>("video");
+
+	if (video) {
+		iframe.send({
+			paused: video.paused,
+			duration: video.duration,
+			currentTime: video.currentTime
+		});
+	}
+}); 
