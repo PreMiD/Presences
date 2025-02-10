62e571692a39a5615ba6ab4e2d26a4e3fc63969b
hakanxd <hakobabo2002@gmail.com>
2025-02-10 14:38:36 +0300

    Update presence.ts

--- websites/A/AniTilky/presence.ts
+++ websites/A/AniTilky/presence.ts
@@ -137,11 +137,10 @@ presence.on("UpdateData", async () => {
 			animeInfo?.title.native ||
 			"Yükleniyor...";
 		presenceData.largeImageKey = animeInfo?.coverImage || "logo";
-		if (animeInfo) {
+		if (animeInfo)
 			presenceData.smallImageText = `${animeInfo.type || "TV"} • ${
 				animeInfo.status || "Devam Ediyor"
 			}`;
-		}
 		presenceData.startTimestamp = time;
 
 		presenceData.buttons = [
@@ -164,25 +163,20 @@ presence.on("UpdateData", async () => {
 			"Yükleniyor...";
 		presenceData.state = `Sezon ${season} Bölüm ${episode}`;
 		presenceData.largeImageKey = animeInfo?.coverImage || "logo";
-		if (animeInfo) {
+		if (animeInfo)
 			presenceData.smallImageText = `${animeInfo.type || "TV"} • ${
 				animeInfo.status || "Devam Ediyor"
 			}`;
-		}
 
 		if (typeof videoData.paused === "boolean") {
 			presenceData.smallImageKey = videoData.paused ? "pause" : "play";
-			presenceData.smallImageText = videoData.paused
-				? "Duraklatıldı"
-				: "Oynatılıyor";
+			presenceData.smallImageText = videoData.paused ? "Duraklatıldı" : "Oynatılıyor";
 
-			if (!videoData.paused && videoData.duration > 0) {
-				[presenceData.startTimestamp, presenceData.endTimestamp] =
-					presence.getTimestamps(
-						Math.floor(videoData.current),
-						Math.floor(videoData.duration)
-					);
-			}
+			if (!videoData.paused && videoData.duration > 0)
+				[presenceData.startTimestamp, presenceData.endTimestamp] = presence.getTimestamps(
+					Math.floor(videoData.current),
+					Math.floor(videoData.duration)
+				);
 		}
 
 		presenceData.buttons = [
