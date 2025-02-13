98e1ae366eea03dea786692fdc276c51e95b82b0
hyqanxd <hakobabo2002@gmail.com>
2025-02-13 09:29:02 +0300

    .

--- websites/A/AniTilky/iframe.ts
+++ websites/A/AniTilky/iframe.ts
@@ -1,15 +1,15 @@
 /// <reference types="premid" />
 
-const iframeProvider = new iFrame();
+const iframeProvider = new iFrame()
 
-iframeProvider.on("UpdateData", async () => {
-	const video: HTMLVideoElement = document.querySelector("video");
+iframeProvider.on('UpdateData', async () => {
+  const video: HTMLVideoElement = document.querySelector('video')
 
-	if (video) {
-		iframeProvider.send({
-			current: video.currentTime,
-			duration: video.duration,
-			paused: video.paused
-		});
-	}
-});
+  if (video) {
+    iframeProvider.send({
+      current: video.currentTime,
+      duration: video.duration,
+      paused: video.paused,
+    })
+  }
+})
