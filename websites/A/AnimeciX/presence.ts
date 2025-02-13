9abaa2d76cf27f45d794f9d466e3908fdaca565e
iSwesia <124099376+iSwesia@users.noreply.github.com>
2025-02-13 08:17:34 +0300

    Update presence.ts
    Signed-off-by: iSwesia <124099376+iSwesia@users.noreply.github.com>

--- websites/A/AnimeciX/presence.ts
+++ websites/A/AnimeciX/presence.ts
@@ -1,183 +1,193 @@
-const presence = new Presence({
-		clientId: "1336362292622655569",
-	}),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+import { ActivityType, Assets } from 'premid'
 
-const enum Assets {
-	Logo = "https://i.imgur.com/ExLxNbB.png",
-	SettingsICO = "https://i.imgur.com/pUucFeP.png",
+const presence = new Presence({
+  clientId: '1336362292622655569',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum Images {
+  Logo = 'https://i.imgur.com/ExLxNbB.png',
+  SettingsICO = 'https://i.imgur.com/pUucFeP.png',
 }
 
-const observeDOMChanges = (callback: () => void) => {
-		new MutationObserver(() => {
-			callback();
-		}).observe(document.body, { childList: true, subtree: true });
-	},
-	updatePresence = () => {
-		const presenceData: PresenceData = {
-			largeImageKey: Assets.Logo,
-		} as PresenceData;
+function observeDOMChanges(callback: () => void) {
+  new MutationObserver(() => {
+    callback()
+  }).observe(document.body, { childList: true, subtree: true })
+}
+function updatePresence() {
+  const presenceData: PresenceData = {
+    largeImageKey: Images.Logo,
+  } as PresenceData
 
-		presenceData.startTimestamp = browsingTimestamp;
-		presenceData.type = ActivityType.Watching;
-		if (document.location.pathname === "/") {
-			presenceData.details = "AnimeciX";
-			presenceData.state = "Ana Sayfa görüntüleniyor";
+  presenceData.startTimestamp = browsingTimestamp
+  presenceData.type = ActivityType.Watching
+  if (document.location.pathname === '/') {
+    presenceData.details = 'AnimeciX'
+    presenceData.state = 'Ana Sayfa görüntüleniyor'
 
-			//WATCHİNG PAGE
-		} else if (
-			document.location.pathname.includes("/titles/") &&
-			document.location.pathname.includes("/episode/")
-		) {
-			// Thumbnail
-			// const animeImgElement = document.querySelector("img.media-image-el");
-			const animeImg = document
-				.querySelector<HTMLElement>("media-item-header")
-				?.style.backgroundImage.match(/url\(["']?(.*?)["']?\)/)?.[1];
+    // WATCHİNG PAGE
+  }
+  else if (
+    document.location.pathname.includes('/titles/')
+    && document.location.pathname.includes('/episode/')
+  ) {
+    // Thumbnail
+    // const animeImgElement = document.querySelector("img.media-image-el");
+    const animeImg = document
+      .querySelector<HTMLElement>('media-item-header')
+      ?.style
+      .backgroundImage
+      .match(/url\(["']?(.*?)["']?\)/)?.[1]
 
-			presenceData.largeImageKey = animeImg;
-			presenceData.details =
-				document.querySelectorAll(".t-title > .ng-star-inserted")[1]
-					?.textContent || "Loading";
-			presenceData.state =
-				document.querySelector(".episode-number")?.textContent || "Loading";
+    presenceData.largeImageKey = animeImg
+    presenceData.details = document.querySelectorAll('.t-title > .ng-star-inserted')[1]
+      ?.textContent || 'Loading'
+    presenceData.state = document.querySelector('.episode-number')?.textContent || 'Loading'
 
-			//EPİSODES PAGE
-		} else if (
-			document.location.pathname.includes("/titles/") &&
-			!document.location.pathname.includes("/episode/")
-		) {
-			// Thumbnail
-			// const animeImgElement = document.querySelector("img.media-image-el");
-			const animeImg = document
-				.querySelector<HTMLElement>("media-item-header")
-				?.style.backgroundImage.match(/url\(["']?(.*?)["']?\)/)?.[1];
+    // EPİSODES PAGE
+  }
+  else if (
+    document.location.pathname.includes('/titles/')
+    && !document.location.pathname.includes('/episode/')
+  ) {
+    // Thumbnail
+    // const animeImgElement = document.querySelector("img.media-image-el");
+    const animeImg = document
+      .querySelector<HTMLElement>('media-item-header')
+      ?.style
+      .backgroundImage
+      .match(/url\(["']?(.*?)["']?\)/)?.[1]
 
-			presenceData.largeImageKey = animeImg;
-			presenceData.details =
-				document.querySelector(".t-title a")?.textContent || "Loading";
-			presenceData.state = "Bölümler görüntüleniyor";
+    presenceData.largeImageKey = animeImg
+    presenceData.details = document.querySelector('.t-title a')?.textContent || 'Loading'
+    presenceData.state = 'Bölümler görüntüleniyor'
 
-			//BROWSE
-		} else if (
-			document.location.pathname === "/browse" &&
-			!document.location.pathname.includes("/lists/")
-		) {
-			presenceData.largeImageKey = Assets.Logo;
-			presenceData.smallImageKey = Assets.Reading;
-			presenceData.smallImageText = "Göz atılıyor..";
-			presenceData.details = "AnimeciX";
-			presenceData.state = "Göz atılıyor..";
-		} else if (document.location.pathname === "/calendar") {
-			presenceData.largeImageKey = Assets.Logo;
-			presenceData.details = "AnimeciX";
-			presenceData.state = "Takvim görüntüleniyor..";
-		} else if (document.location.pathname === "/news") {
-			presenceData.largeImageKey = Assets.Logo;
-			presenceData.details = "AnimeciX";
-			presenceData.state = "Haberler görüntüleniyor..";
-		} else if (document.location.pathname.includes("/users/")) {
-			const userName = document.querySelector(".header > h1.name")?.textContent;
-			presenceData.largeImageKey = Assets.Logo;
-			presenceData.smallImageKey = document.querySelector<HTMLImageElement>(
-				".header > .user-avatar.real"
-			)?.src;
-			presenceData.smallImageText = userName;
-			presenceData.details = "Kullanıcı görüntüleniyor:";
-			presenceData.state = userName || "Loading..";
-		} else {
-			switch (document.location.pathname) {
-				case "/watchlist": {
-					presenceData.largeImageKey = Assets.Logo;
-					presenceData.smallImageKey = Assets.Viewing;
-					presenceData.smallImageText = "Watchlist";
-					presenceData.details = "AnimeciX";
-					presenceData.state = "İzlenenler görüntüleniyor..";
+    // BROWSE
+  }
+  else if (
+    document.location.pathname === '/browse'
+    && !document.location.pathname.includes('/lists/')
+  ) {
+    presenceData.largeImageKey = Images.Logo
+    presenceData.smallImageKey = Assets.Reading
+    presenceData.smallImageText = 'Göz atılıyor..'
+    presenceData.details = 'AnimeciX'
+    presenceData.state = 'Göz atılıyor..'
+  }
+  else if (document.location.pathname === '/calendar') {
+    presenceData.largeImageKey = Images.Logo
+    presenceData.details = 'AnimeciX'
+    presenceData.state = 'Takvim görüntüleniyor..'
+  }
+  else if (document.location.pathname === '/news') {
+    presenceData.largeImageKey = Images.Logo
+    presenceData.details = 'AnimeciX'
+    presenceData.state = 'Haberler görüntüleniyor..'
+  }
+  else if (document.location.pathname.includes('/users/')) {
+    const userName = document.querySelector('.header > h1.name')?.textContent
+    presenceData.largeImageKey = Images.Logo
+    presenceData.smallImageKey = document.querySelector<HTMLImageElement>(
+      '.header > .user-avatar.real',
+    )?.src
+    presenceData.smallImageText = userName
+    presenceData.details = 'Kullanıcı görüntüleniyor:'
+    presenceData.state = userName || 'Loading..'
+  }
+  else {
+    switch (document.location.pathname) {
+      case '/watchlist': {
+        presenceData.largeImageKey = Images.Logo
+        presenceData.smallImageKey = Assets.Viewing
+        presenceData.smallImageText = 'Watchlist'
+        presenceData.details = 'AnimeciX'
+        presenceData.state = 'İzlenenler görüntüleniyor..'
 
-					break;
-				}
-				case "/lists": {
-					presenceData.largeImageKey = Assets.Logo;
-					presenceData.smallImageKey = Assets.Viewing;
-					presenceData.smallImageText = "İzlenecek Listesi";
-					presenceData.details = "AnimeciX";
-					presenceData.state = "İzleme listeleri görüntüleniyor..";
+        break
+      }
+      case '/lists': {
+        presenceData.largeImageKey = Images.Logo
+        presenceData.smallImageKey = Assets.Viewing
+        presenceData.smallImageText = 'İzlenecek Listesi'
+        presenceData.details = 'AnimeciX'
+        presenceData.state = 'İzleme listeleri görüntüleniyor..'
 
-					break;
-				}
-				case "/lists/liked": {
-					presenceData.largeImageKey = Assets.Logo;
-					presenceData.smallImageKey = Assets.Viewing;
-					presenceData.smallImageText = "Favoriler";
-					presenceData.details = "AnimeciX";
-					presenceData.state = "Favoriler görüntüleniyor..";
+        break
+      }
+      case '/lists/liked': {
+        presenceData.largeImageKey = Images.Logo
+        presenceData.smallImageKey = Assets.Viewing
+        presenceData.smallImageText = 'Favoriler'
+        presenceData.details = 'AnimeciX'
+        presenceData.state = 'Favoriler görüntüleniyor..'
 
-					break;
-				}
-				case "/lists/browse": {
-					presenceData.largeImageKey = Assets.Logo;
-					presenceData.smallImageKey = Assets.Reading;
-					presenceData.smallImageText = "Browsing..";
-					presenceData.details = "AnimeciX";
-					presenceData.state = "Oynatma listeleri görüntüleniyor..";
+        break
+      }
+      case '/lists/browse': {
+        presenceData.largeImageKey = Images.Logo
+        presenceData.smallImageKey = Assets.Reading
+        presenceData.smallImageText = 'Browsing..'
+        presenceData.details = 'AnimeciX'
+        presenceData.state = 'Oynatma listeleri görüntüleniyor..'
 
-					break;
-				}
-				default:
-					if (
-						document.location.pathname.includes("/lists/") &&
-						/\d/.test(document.location.pathname)
-					) {
-						const listname = document.querySelector(".list-name")?.textContent;
-						presenceData.largeImageKey = Assets.Logo;
-						presenceData.smallImageKey = Assets.Reading;
-						presenceData.smallImageText = listname || "Loading..";
-						presenceData.details = "Liste görüntüleniyor:";
-						presenceData.state = listname || "Loading..";
-					} else {
-						switch (document.location.pathname) {
-							case "/account/settings": {
-								presenceData.largeImageKey = Assets.Logo;
-								presenceData.smallImageKey = Assets.SettingsICO;
-								presenceData.smallImageText = "Ayarlar";
-								presenceData.details = "AnimeciX";
-								presenceData.state = "Ayarlar";
+        break
+      }
+      default:
+        if (
+          document.location.pathname.includes('/lists/')
+          && /\d/.test(document.location.pathname)
+        ) {
+          const listname = document.querySelector('.list-name')?.textContent
+          presenceData.largeImageKey = Images.Logo
+          presenceData.smallImageKey = Assets.Reading
+          presenceData.smallImageText = listname || 'Loading..'
+          presenceData.details = 'Liste görüntüleniyor:'
+          presenceData.state = listname || 'Loading..'
+        }
+        else {
+          switch (document.location.pathname) {
+            case '/account/settings': {
+              presenceData.largeImageKey = Images.Logo
+              presenceData.smallImageKey = Images.SettingsICO
+              presenceData.smallImageText = 'Ayarlar'
+              presenceData.details = 'AnimeciX'
+              presenceData.state = 'Ayarlar'
 
-								break;
-							}
-							case "/gw-rooms": {
-								presenceData.largeImageKey = Assets.Logo;
-								presenceData.smallImageKey = Assets.Premiere;
-								presenceData.smallImageText = "Group Watch";
-								presenceData.details = "AnimeciX";
-								presenceData.state = "İzleme odaları görüntüleniyor..";
+              break
+            }
+            case '/gw-rooms': {
+              presenceData.largeImageKey = Images.Logo
+              presenceData.smallImageKey = Assets.Premiere
+              presenceData.smallImageText = 'Group Watch'
+              presenceData.details = 'AnimeciX'
+              presenceData.state = 'İzleme odaları görüntüleniyor..'
 
-								break;
-							}
-							case "/search": {
-								presenceData.largeImageKey = Assets.Logo;
-								presenceData.smallImageKey = Assets.Search;
-								presenceData.smallImageText = "Aranıyor";
-								presenceData.details = "Aranıyor:";
-								presenceData.state =
-									document.querySelector(".query")?.textContent;
+              break
+            }
+            case '/search': {
+              presenceData.largeImageKey = Images.Logo
+              presenceData.smallImageKey = Assets.Search
+              presenceData.smallImageText = 'Aranıyor'
+              presenceData.details = 'Aranıyor:'
+              presenceData.state = document.querySelector('.query')?.textContent
 
-								break;
-							}
-							default: {
-								presenceData.largeImageKey = Assets.Logo;
-								presenceData.details = "AnimeciX";
-								presenceData.state = "Sayfa görüntüleniyor..";
-							}
-						}
-					}
-			}
-		}
+              break
+            }
+            default: {
+              presenceData.largeImageKey = Images.Logo
+              presenceData.details = 'AnimeciX'
+              presenceData.state = 'Sayfa görüntüleniyor..'
+            }
+          }
+        }
+    }
+  }
 
-		presence.setActivity(presenceData);
-	};
+  presence.setActivity(presenceData)
+}
 
-document.addEventListener("DOMContentLoaded", () => {
-	updatePresence();
-	observeDOMChanges(updatePresence);
-});
+document.addEventListener('DOMContentLoaded', () => {
+  updatePresence()
+  observeDOMChanges(updatePresence)
+})
