5b9111a1a64fa8ae5aba247305b470f752621976
PinguQT <162685518+PinguQT@users.noreply.github.com>
2025-02-23 23:41:30 +0800

    Update iframe.ts
    Signed-off-by: PinguQT <162685518+PinguQT@users.noreply.github.com>

--- websites/A/AnimeKai/iframe.ts
+++ websites/A/AnimeKai/iframe.ts
@@ -4,7 +4,6 @@ iframe.on('UpdateData', async () => {
   const video = document.querySelector<HTMLVideoElement>('video')
   if (video && !Number.isNaN(video.duration)) {
     iframe.send({
-      exists: true,
       duration: video.duration,
       currTime: video.currentTime,
       paused: video.paused,
