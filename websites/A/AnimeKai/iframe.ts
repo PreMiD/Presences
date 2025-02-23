4dd5d1306221ed3003dd7651e19c030affa54b18
PinguQT <162685518+PinguQT@users.noreply.github.com>
2025-02-23 23:41:17 +0800

    Update websites/A/AnimeKai/iframe.ts
    Co-authored-by: Bas van Zanten <bas.van.zanten007@gmail.com>
    Signed-off-by: PinguQT <162685518+PinguQT@users.noreply.github.com>

--- websites/A/AnimeKai/iframe.ts
+++ websites/A/AnimeKai/iframe.ts
@@ -1,15 +1,13 @@
 const iframe = new iFrame()
 
 iframe.on('UpdateData', async () => {
-  if (document.querySelector('video')) {
-    const video = document.querySelector<HTMLVideoElement>('video')
-    if (video && !Number.isNaN(video.duration)) {
-      iframe.send({
-        exists: true,
-        duration: video.duration,
-        currTime: video.currentTime,
-        paused: video.paused,
-      })
-    }
+  const video = document.querySelector<HTMLVideoElement>('video')
+  if (video && !Number.isNaN(video.duration)) {
+    iframe.send({
+      exists: true,
+      duration: video.duration,
+      currTime: video.currentTime,
+      paused: video.paused,
+    })
   }
 })
