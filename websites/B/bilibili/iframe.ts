b0d47448f8a7daf147e5adfcb963283275a9455a
TheWhiteDog9487 <gaojian520one@outlook.com>
2025-02-24 16:35:44 +0800

    style(bilibili): Apply suggestion

--- websites/B/bilibili/iframe.ts
+++ websites/B/bilibili/iframe.ts
@@ -1,7 +1,7 @@
 const iframe = new iFrame()
 
 iframe.on('UpdateData', async () => {
-  const title = document.querySelector('.smaller-title') === null ? document.querySelector('.small-title')?.getAttribute('title') : document.querySelector('.smaller-title')?.getAttribute('title')
+  const title = document.querySelector('.smaller-title')?.getAttribute('title') ?? document.querySelector('.small-title')?.getAttribute('title')
   const roomOwner = document.querySelector('.room-owner-username')?.getAttribute('title')
   if (title === undefined || roomOwner === undefined) {
     return
