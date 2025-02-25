ecc2ca58e81e17b185318845711c2fa6db6c431d
Toshi7878 <toshiyuki19991125@gmail.com>
2025-02-26 07:59:54 +0900

    remove document.location.hostname if

--- websites/Y/YTyping/iframe.ts
+++ websites/Y/YTyping/iframe.ts
@@ -1,15 +1,13 @@
 const iframe = new iFrame()
 
-if (document.location.hostname === 'www.youtube.com') {
-  iframe.on('UpdateData', async () => {
-    const { currentTime, duration, paused } = document.querySelector<HTMLVideoElement>(
-      'video.video-stream.html5-main-video',
-    )!
+iframe.on('UpdateData', async () => {
+  const { currentTime, duration, paused } = document.querySelector<HTMLVideoElement>(
+    'video.video-stream.html5-main-video',
+  )!
 
-    iframe.send({
-      currentTime,
-      duration,
-      paused,
-    })
+  iframe.send({
+    currentTime,
+    duration,
+    paused,
   })
-}
+})
